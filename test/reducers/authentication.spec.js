193f0f65b35ac33e2adf62c207580a8bc60d9e42
Phil Eaton <peaton@linode.com>
2016-04-21 11:28:51 -0400

    add authentication test

---
+++ test/reducers/authentication.spec.js
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+import authentication from '../../src/reducers/authentication';
+import * as actions from '../../src/actions/authentication';
+
+describe("authentication reducer", () => {
+  it('should handle initial state', () => {
+    expect(
+      authentication(undefined, {})
+    ).to.be.eql({ token: null });
+  });
+
+  it('should handle SET_TOKEN', () => {
+    const state = authentication(undefined, {});
+    const auth = { token: null };
+
+    deepFreeze(state);
+
+    expect(
+      authentication(state, {
+        type: SET_TOKEN,
+        scopes: [],
+        username: 'me',
+        email: 'me@example.org',
+        token: 'token'
+      })
+    ).to.be.eql({
+      scopes: [],
+      username: 'me',
+      email: 'me@example.org',
+      token: 'token'
+    });
+  });
+
+  it('should handle anything else', () => {
+    const state = authentication(undefined, {});
+    const auth = { token: null };
+
+    expect(
+      authentication(state, {
+        scopes: [],
+        username: 'me',
+        email: 'me@example.org',
+        token: 'token'
+      })
+    ).to.be.eql({ token: null });
+  });
+});
