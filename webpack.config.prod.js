d76ce8b178e59fc2931d413b577fc54c2ccbdc73
Drew DeVault <ddevault@linode.com>
2016-05-19 10:41:30 -0400

    Base the webpack prod config off of the dev config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,43 +1,20 @@
 var path = require('path');
 var webpack = require('webpack');
+var _ = require('./webpack.config.dev.js');
 
-module.exports = {
-  devtool: 'source-map',
-  entry: [
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src')
-      },
-      {
-        test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
-      }
-    ]
-  },
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, "./node_modules/bootstrap/scss/")]
-  }
-};
+_.entry = './src/index';
+_.plugins = [
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  })
+];
+
+module.exports = _;;
