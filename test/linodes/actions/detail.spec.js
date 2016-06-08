f33cd34351a9d69c04d339c7bd06786b2dde0476
Drew DeVault <ddevault@linode.com>
2016-06-08 11:14:01 -0400

    Add tests for commitChanges action

--- test/linodes/actions/detail.spec.js
+++ test/linodes/actions/detail.spec.js
@@ -1,5 +1,8 @@
+import sinon from 'sinon';
 import { expect } from 'chai';
 import * as actions from '~/linodes/actions/detail';
+import { UPDATE_LINODE } from '~/actions/api/linodes';
+import * as fetch from '~/fetch';
 
 describe('linodes/actions/detail', () => {
   describe('changeDetailTab', () => {
@@ -42,14 +45,83 @@ describe('linodes/actions/detail', () => {
   });
 
   describe('commitChanges', () => {
-    it('should return a function');
+    const sandbox = sinon.sandbox.create();
 
-    it('toggles the loading state twice');
+    afterEach(() => {
+      sandbox.restore();
+    });
 
-    it('performs the HTTP request');
+    const state = {
+      authentication: {
+        token: 'token',
+      },
+      linodes: {
+        detail: {
+          label: 'new label',
+          group: 'new group',
+        },
+      },
+    };
+    const getGetState = (_state = {}) => sandbox.stub().returns(_state);
+    const getDispatch = () => sandbox.spy();
+    const getFetchStub = (rsp) => sandbox.stub(fetch, 'fetch').returns({ json() { return rsp; } });
 
-    it('dispatches an UPDATE_LINODE action with the new linode details');
+    it('should return a function', () => {
+      const action = actions.commitChanges('linode_1234');
+      expect(action).to.be.a('function');
+    });
 
-    it('leaves edit mode');
+    it('toggles the loading state twice', async () => {
+      const action = actions.commitChanges('linode_1234');
+      expect(action).to.be.a('function');
+      const dispatch = getDispatch();
+      const getState = getGetState(state);
+      getFetchStub();
+      await action(dispatch, getState);
+      expect(dispatch.withArgs({ type: actions.TOGGLE_LOADING }).calledTwice)
+        .to.equal(true);
+    });
+
+    it('performs the HTTP request', async () => {
+      const action = actions.commitChanges('linode_1234');
+      expect(action).to.be.a('function');
+      const dispatch = getDispatch();
+      const getState = getGetState(state);
+      const fetchStub = getFetchStub();
+      await action(dispatch, getState);
+      expect(fetchStub.calledOnce).to.equal(true);
+      expect(fetchStub.calledWith(
+        state.authentication.token,
+        '/linodes/linode_1234', {
+          method: 'PUT',
+          body: JSON.stringify({
+            label: 'new label',
+            group: 'new group',
+          }),
+        })).to.equal(true);
+    });
+
+    it('dispatches an UPDATE_LINODE action with the new linode details', async () => {
+      const action = actions.commitChanges('linode_1234');
+      expect(action).to.be.a('function');
+      const dispatch = getDispatch();
+      const getState = getGetState(state);
+      getFetchStub({ fake: 'linode' });
+      await action(dispatch, getState);
+      expect(dispatch.withArgs({
+        type: UPDATE_LINODE,
+        linode: { fake: 'linode' },
+      }).calledOnce).to.equal(true);
+    });
+
+    it('leaves edit mode', async () => {
+      const action = actions.commitChanges('linode_1234');
+      expect(action).to.be.a('function');
+      const dispatch = getDispatch();
+      const getState = getGetState(state);
+      getFetchStub();
+      await action(dispatch, getState);
+      expect(dispatch.withArgs({ type: actions.TOGGLE_EDIT_MODE }).calledOnce).to.equal(true);
+    });
   });
 });
