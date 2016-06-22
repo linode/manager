5178c83b6b797e639b11c9bbad94602e7a214d58
Drew DeVault <ddevault@linode.com>
2016-06-22 11:52:18 -0400

    Fix existing tests

--- src/linodes/components/DatacenterSelection.js
+++ src/linodes/components/DatacenterSelection.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 
+export const countryMap = {
+  datacenter_2: 'us', // Dallas
+  datacenter_3: 'us', // Fremont
+  datacenter_4: 'us', // Atlanta
+  datacenter_6: 'us', // Newark
+  datacenter_7: 'gb', // London
+  datacenter_8: 'jp', // Tokyo
+  datacenter_9: 'sg', // Singapore
+  datacenter_10: 'de', // Frankfurt
+};
+
 export default class DatacenterSelection extends Component {
   constructor() {
     super();
