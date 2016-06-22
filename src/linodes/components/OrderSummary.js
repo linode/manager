9ea08e499c3a27daa04075ea49115168dfa339f8
Drew DeVault <ddevault@linode.com>
2016-06-22 11:43:33 -0400

    Implement create linode page skeleton

--- src/linodes/components/OrderSummary.js
+++ src/linodes/components/OrderSummary.js
@@ -1,157 +1,28 @@
-import React, { Component, PropTypes } from 'react';
-import ReactDOM from 'react-dom';
-import { distros as distroAssets, flags } from '~/assets';
-import Service from './Service';
-import { countryMap } from './DatacenterSelection';
+import React, { Component } from 'react';
 
-export default class Summary extends Component {
+export default class OrderSummary extends Component {
   constructor() {
     super();
     this.render = this.render.bind(this);
-    this.renderBack = this.renderBack.bind(this);
     this.renderHeader = this.renderHeader.bind(this);
-    this.componentDidUpdate = this.componentDidUpdate.bind(this);
-  }
-
-  componentDidUpdate() {
-    const { ui } = this.props;
-    if (ui.showPassword) {
-      const dom = ReactDOM.findDOMNode(this).querySelector('.pw');
-      const range = document.createRange();
-      range.selectNodeContents(dom);
-      const sel = window.getSelection();
-      sel.removeAllRanges();
-      sel.addRange(range);
-    }
-  }
-
-  renderBack() {
-    const { onBack } = this.props;
-    return (<a
-      href="#" className="back pull-right"
-      onClick={e => { e.preventDefault(); onBack(); }}
-    >Back</a>);
   }
 
   renderHeader() {
-    return <h2>Order Summary</h2>;
-  }
-
-  renderSourceSummary(distro) {
-    // TODO: other source types
-    return (<div
-      className="row summary-source"
-    >
-      <div className="col-md-1">
-        <img
-          src={distroAssets[distro.vendor]
-            ? distroAssets[distro.vendor] : '//placehold.it/50x50'}
-          width="24" height="24" alt={distro.vendor}
-        />
-      </div>
-      <div className="col-md-11">{distro.label}</div>
-    </div>);
-  }
-
-  renderDCSummary(dc) {
-    return (<div className="row summary-dc">
-      <div
-        className="col-md-1"
-      >
-        <img
-          src={flags[countryMap[dc.id]]
-            ? flags[countryMap[dc.id]] : '//placehold.it/50x50'}
-          width="24" height="24" alt={dc.label}
-        />
-      </div>
-      <div className="col-md-11">{dc.label}</div>
-    </div>);
+    return (
+      <header>
+        <h2>Order summary</h2>
+      </header>
+    );
   }
 
   render() {
-    const {
-      ui,
-      services,
-      distros,
-      datacenters,
-      onLabelChange,
-      onShowRootPassword,
-      onCreate,
-    } = this.props;
-    const done = ui.source && ui.datacenter && ui.service;
-    if (!done) {
-      return <div></div>;
-    }
-    const service = services.find(s => s.id === ui.service);
-    const distro = distros[ui.source];
-    const dc = datacenters[ui.datacenter];
-    return (<div className="card creation-step">
-      {this.renderBack()}
-      {this.renderHeader()}
-      <div className="summary">
-        <div className="row">
-          <div className="col-md-3">
-            <Service service={service} />
-          </div>
-          <div className="col-md-9">
-            <fieldset className="form-group">
-              <label htmlFor="label">Label</label>
-              <input
-                className="form-control" id="label"
-                value={ui.label} onChange={onLabelChange}
-                placeholder="Name this Linode..."
-              />
-            </fieldset>
-            <div className="summary">
-              {this.renderSourceSummary(distro)}
-              {this.renderDCSummary(dc)}
-              <hr />
-              <div className="row">
-                <div className="col-md-11 col-md-offset-1">
-                  Root password:
-                  {ui.showPassword ? <strong className="pw">
-                    {ui.password}
-                  </strong> : ' '}
-                  <a
-                    href="#" onClick={e => {
-                      e.preventDefault();
-                      onShowRootPassword();
-                    }}
-                  >{ui.showPassword ? '(hide)' : '(show)'}</a>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-11 col-md-offset-1">
-                  <label htmlFor="backups" className="checkbox">
-                    <input type="checkbox" id="backups" />
-                    Enable backups for this Linode? (+${
-                      (service.monthly_price / 4 / 100).toFixed(2)
-                    }/mo)
-                  </label>
-                </div>
-              </div>
-            </div>
-            <button
-              className="btn btn-primary order-button"
-              onClick={onCreate}
-            >
-              <i className="fa fa-chevron-right"></i>&nbsp;
-              Create Linode
-            </button>
-          </div>
+    return (
+      <div>
+        {this.renderHeader()}
+        <div className="card-body">
+          TODO
         </div>
       </div>
-    </div>);
+    );
   }
 }
-
-Summary.propTypes = {
-  ui: PropTypes.object.isRequired,
-  services: PropTypes.array.isRequired,
-  distros: PropTypes.object.isRequired,
-  datacenters: PropTypes.object.isRequired,
-  onBack: PropTypes.func,
-  onLabelChange: PropTypes.func,
-  onShowRootPassword: PropTypes.func,
-  onCreate: PropTypes.func,
-};
--- src/linodes/components/ServiceSelection.js
+++ src/linodes/components/ServiceSelection.js
@@ -1,185 +1,28 @@
-import React, { Component, PropTypes } from 'react';
-import { toggleAllPlans } from '../actions/create';
-import Service from './Service';
-
-const testServices = [
-  {
-    label: 'Linode 1024',
-    ram: 1024,
-    mbits_out: 25,
-    disk: 24,
-    id: 'service_112',
-    vcpus: 1,
-    monthly_price: 1000,
-    transfer: 2000,
-    hourly_price: 1,
-  },
-  {
-    label: 'Linode 2048',
-    ram: 2048,
-    mbits_out: 250,
-    disk: 48,
-    id: 'service_113',
-    vcpus: 2,
-    monthly_price: 2000,
-    transfer: 3000,
-    hourly_price: 3,
-  },
-  {
-    label: 'Linode 4096',
-    ram: 4096,
-    mbits_out: 500,
-    disk: 96,
-    id: 'service_114',
-    vcpus: 4,
-    monthly_price: 4000,
-    transfer: 4000,
-    hourly_price: 6,
-  },
-  {
-    label: 'Linode 8192',
-    ram: 8192,
-    mbits_out: 1000,
-    disk: 192,
-    id: 'service_115',
-    vcpus: 6,
-    monthly_price: 8000,
-    transfer: 8000,
-    hourly_price: 12,
-  },
-  {
-    label: 'Linode 16384',
-    ram: 16384,
-    mbits_out: 2000,
-    disk: 384,
-    id: 'service_116',
-    vcpus: 8,
-    monthly_price: 16000,
-    transfer: 16000,
-    hourly_price: 24,
-  },
-  {
-    label: 'Linode 32768',
-    ram: 32768,
-    mbits_out: 4000,
-    disk: 768,
-    id: 'service_117',
-    vcpus: 12,
-    monthly_price: 32000,
-    transfer: 20000,
-    hourly_price: 48,
-  },
-  {
-    label: 'Linode 49152',
-    ram: 49152,
-    mbits_out: 6000,
-    disk: 1152,
-    id: 'service_118',
-    vcpus: 16,
-    monthly_price: 48000,
-    transfer: 20000,
-    hourly_price: 72,
-  },
-  {
-    label: 'Linode 65536',
-    ram: 65536,
-    mbits_out: 8000,
-    disk: 1536,
-    id: 'service_119',
-    vcpus: 20,
-    monthly_price: 64000,
-    transfer: 20000,
-    hourly_price: 96,
-  },
-  {
-    label: 'Linode 98304',
-    ram: 98304,
-    mbits_out: 10000,
-    disk: 1920,
-    id: 'service_120',
-    vcpus: 20,
-    monthly_price: 96000,
-    transfer: 20000,
-    hourly_price: 144,
-  },
-];
+import React, { Component } from 'react';
 
 export default class ServiceSelection extends Component {
   constructor() {
     super();
     this.render = this.render.bind(this);
     this.renderHeader = this.renderHeader.bind(this);
-    this.renderBack = this.renderBack.bind(this);
-  }
-
-  renderBack() {
-    const { onBack, ui } = this.props;
-    if (ui.service === null) {
-      return (<a
-        href="#" className="back pull-right"
-        onClick={e => { e.preventDefault(); onBack(); }}
-      >Back</a>);
-    }
-    return null;
   }
 
   renderHeader() {
-    const { ui, services } = this.props;
-    if (ui.service === null) {
-      return <h2>Select a Plan</h2>;
-    }
-    const s = services.find(_ => _.id === ui.service);
-    return (<h2
-      className="text-right"
-    >
-      {s.label}
-      <span className="dodsoncube" width="24" height="24"></span>
-    </h2>);
+    return (
+      <header>
+        <h2>Select a plan</h2>
+      </header>
+    );
   }
 
   render() {
-    const { onSelection, ui, dispatch } = this.props;
-
-    if (ui.datacenter === null) {
-      return <div></div>;
-    }
-
-    const services = ui.showAllPlans ? /* this.props.services*/
-      testServices : testServices.slice(0, 4);
-
-    if (ui.service !== null) {
-      return (<div className="card creation-step step-done">
-        {this.renderBack()}
-        {this.renderHeader()}
-      </div>);
-    }
-
     return (
-      <div className="card creation-step">
-        {this.renderBack()}
+      <div>
         {this.renderHeader()}
-        <div className="service-list">
-          {services.map(s => <Service
-            key={s.id}
-            onSelection={onSelection}
-            service={s}
-          />)}
+        <div className="card-body">
+          TODO
         </div>
-        <button
-          className="btn btn-success btn-block"
-          onClick={() => dispatch(toggleAllPlans())}
-        >
-          {ui.showAllPlans ? 'Show Fewer' : 'Show More'}
-        </button>
       </div>
     );
   }
 }
-
-ServiceSelection.propTypes = {
-  onBack: PropTypes.func,
-  onSelection: PropTypes.func,
-  ui: PropTypes.object.isRequired,
-  services: PropTypes.array.isRequired,
-  dispatch: PropTypes.func.isRequired,
-};
