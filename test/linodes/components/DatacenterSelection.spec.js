9cac09bf135c24021ad4c8cde8b49a935f820800
Phil Eaton <peaton@linode.com>
2016-06-29 15:41:58 -0400

    Add tests for datacenter

--- test/linodes/components/DatacenterSelection.spec.js
+++ test/linodes/components/DatacenterSelection.spec.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import DatacenterSelection from '~/linodes/components/DatacenterSelection';
+import { mount } from 'enzyme';
+
+import DatacenterSelection, { countryMap } from '~/linodes/components/DatacenterSelection';
+import { flags } from '~/assets';
 
 describe('linodes/components/DatacenterSelection', () => {
-  it('renders the card header', () => {
-    const c = shallow(<DatacenterSelection />);
-    expect(c.contains(<h2>Select a datacenter</h2>)).to.equal(true);
+  const datacenters = {
+    datacenters: {
+      datacenter_2: {
+        id: 'datacenter_2',
+        label: 'Newark, NJ',
+      },
+    },
+  };
+
+  it('renders the datacenters', () => {
+    const c = mount(
+      <DatacenterSelection
+        datacenters={datacenters}
+        onDatacenterSelected={() => {}}
+      />
+    );
+
+    expect(c.find(<h2>Select a datacenter</h2>)).to.exist;
+    expect(c.find(<h3>North America</h3>)).to.exist;
+    expect(c.find(<header><div>Newark, NJ</div></header>)).to.exist;
+    expect(c.find(
+      <img
+        src={flags[countryMap.datacenter_2]}
+        width={64}
+        height={64}
+        alt="Newark, NJ"
+      />
+    )).to.exist;
   });
 });
--- test/linodes/layouts/CreateLinodePage.spec.js
+++ test/linodes/layouts/CreateLinodePage.spec.js
@@ -2,10 +2,12 @@ import React from 'react';
 import sinon from 'sinon';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
+
 import { CreateLinodePage } from '~/linodes/layouts/CreateLinodePage';
 import * as sourceActions from '~/linodes/actions/create/source';
 import * as fetch from '~/fetch';
 import { UPDATE_DISTROS } from '~/actions/api/distros';
+import { UPDATE_DATACENTERS } from '~/actions/api/datacenters';
 
 describe('linodes/layout/CreateLinodePage', () => {
   const sandbox = sinon.sandbox.create();
@@ -19,9 +21,20 @@ describe('linodes/layout/CreateLinodePage', () => {
         source: null,
         sourceTab: 0,
       },
+      datacenter: {
+        datacenter: null,
+      },
     },
     distros: {
       distributions: { },
+    },
+    datacenters: {
+      datacenters: {
+        datacenter_1: {
+          id: 'datacenter_2',
+          label: 'Newark, NJ',
+        },
+      },
     },
   };
 
@@ -32,6 +45,7 @@ describe('linodes/layout/CreateLinodePage', () => {
           dispatch={() => {}}
           create={state.create}
           distros={state.distros}
+          datacenters={state.datacenters}
         />);
       expect(page.find(thing).length).to.equal(1);
     };
@@ -51,6 +65,7 @@ describe('linodes/layout/CreateLinodePage', () => {
         dispatch={dispatch}
         distros={state.distros}
         create={state.create}
+        datacenters={state.datacenters}
       />);
     dispatch.reset();
     page.find('SourceSelection').props().onTabChange(2);
@@ -59,28 +74,42 @@ describe('linodes/layout/CreateLinodePage', () => {
       .to.equal(true);
   });
 
-  it('fetches distros when mounted', async () => {
+  it('fetches distros and datacenters when mounted', async () => {
     const dispatch = sandbox.spy();
     mount(
       <CreateLinodePage
         dispatch={dispatch}
         distros={state.distros}
         create={state.create}
+        datacenters={state.datacenters}
       />);
-    expect(dispatch.calledOnce).to.equal(true);
-    const dispatched = dispatch.firstCall.args[0];
-    // Assert that dispatched is a function that fetches linodes
+    expect(dispatch.calledTwice).to.equal(true);
+
+    const dispatchedDistros = dispatch.firstCall.args[0];
+    const dispatchedDatacenters = dispatch.secondCall.args[0];
+
+    // Assert that dispatchedDistros is a function that fetches distros
+    dispatch.reset();
     const fetchStub = sandbox.stub(fetch, 'fetch').returns({
       json: () => {},
     });
-    dispatch.reset();
-    await dispatched(dispatch, () => ({
+    await dispatchedDistros(dispatch, () => ({
       authentication: { token: 'token' },
     }));
     expect(fetchStub.calledOnce).to.equal(true);
     expect(fetchStub.firstCall.args[1]).to.equal('/distributions?page=1');
     expect(dispatch.calledOnce).to.equal(true);
     expect(dispatch.firstCall.args[0].type).to.equal(UPDATE_DISTROS);
+
+    // Assert that dispatchedDatacenters is a function that fetches datacenters
+    dispatch.reset();
+    await dispatchedDatacenters(dispatch, () => ({
+      authentication: { token: 'token' },
+    }));
+    expect(fetchStub.calledTwice).to.equal(true);
+    expect(fetchStub.secondCall.args[1]).to.equal('/datacenters?page=1');
+    expect(dispatch.calledOnce).to.equal(true);
+    expect(dispatch.firstCall.args[0].type).to.equal(UPDATE_DATACENTERS);
   });
 
   it('selects a source when appropriate', () => {
@@ -90,6 +119,7 @@ describe('linodes/layout/CreateLinodePage', () => {
         dispatch={dispatch}
         distros={state.distros}
         create={state.create}
+        datacenters={state.datacenters}
       />);
     dispatch.reset();
     page.find('SourceSelection').props().onSourceSelected({ id: 'distro_1234' });
