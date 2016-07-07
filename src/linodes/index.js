9c9fa4ab7df4c8e2a2681b0c9fdca23c7af4eb49
Phil Eaton <peaton@linode.com>
2016-07-07 16:14:12 -0400

    Remove placeholder

--- src/linodes/index.js
+++ src/linodes/index.js
@@ -9,12 +9,6 @@ import CreatePage from './create/layouts/IndexPage';
 import BackupsPage from './layouts/BackupsPage';
 import SettingsPage from './settings';
 
-/* eslint-disable react/prop-types */
-export function Placeholder(props) {
-  const { pathname } = props.location;
-  return <div>{pathname} placeholder</div>;
-}
-
 export default (
   <Route path="/linodes">
     <IndexRoute component={IndexPage} />
@@ -22,8 +16,8 @@ export default (
     <Route path=":linodeId" component={LinodeDetailPage}>
       <IndexRoute component={LinodeGeneral} />
       <Route path="networking" component={LinodeNetworking} />
-      <Route path="resize" component={Placeholder} />
-      <Route path="repair" component={Placeholder} />
+      <Route path="resize" component={null} />
+      <Route path="repair" component={null} />
       <Route path="backups" component={BackupsPage} />
       {SettingsPage}
     </Route>
