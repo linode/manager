a29190ed9f1588b7fded47974072474278b8d006
Nick <nidodson@gmail.com>
2016-08-16 10:28:28 -0400

    linode > settings > advanced skel

--- src/linodes/settings/index.js
+++ src/linodes/settings/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import { IndexPage, AlertsPage } from './layouts';
+import AdvancedPage from './layouts/AdvancedPage';
 
 export default (
   <Route path="settings" component={IndexPage}>
     <IndexRoute component={AlertsPage} />
-    <Route path="advanced" component={() => <p>TODO</p>} />
+    <Route path="advanced" component={AdvancedPage} />
   </Route>
 );
