c34cf26f002e0d0b46ae9c48b586d966af9832b4
Phil Eaton <peaton@linode.com>
2016-08-26 11:28:06 -0400

    Make Link implementation replaceable

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
+import { Link } from '~/components/Link';
 
 export default function Navigation(props) {
   const { username, emailHash } = props;
