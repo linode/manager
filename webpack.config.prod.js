90517deff9a288406edc1cea8c9b6331d5b01034
Phil Eaton <philneaton95@gmail.com>
2016-09-01 11:36:32 -0400

    Add env var lookup

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var process = require('process');
 var webpack = require('webpack');
 var _ = require('./webpack.config.dev.js');
 
@@ -7,7 +8,10 @@ _.plugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': JSON.stringify('production')
+      'NODE_ENV': JSON.stringify('production'),
+      'ENV_APP_ROOT': JSON.stringify(process.env.APP_ROOT),
+      'ENV_API_ROOT': JSON.stringify(process.env.API_ROOT),
+      'ENV_LOGIN_ROOT': JSON.stringify(process.env.LOGIN_ROOT)
     }
   }),
   new webpack.optimize.UglifyJsPlugin({
