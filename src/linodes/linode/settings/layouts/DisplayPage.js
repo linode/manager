22377db27915730242aa04e14b28c130d09ca8ab
Phil Eaton <peaton@linode.com>
2016-10-03 17:24:55 -0400

    Add display page

--- src/linodes/linode/settings/layouts/DisplayPage.js
+++ src/linodes/linode/settings/layouts/DisplayPage.js
@@ -1,9 +1,87 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
-export default function DisplayPage() {
-  return (
-    <section className="card">
-      TODO
-    </section>
-  );
+import { getLinode, loadLinode } from '~/linodes/linode/layouts/IndexPage';
+import { linodes } from '~/api';
+import { ErrorSummary, FormGroup, reduceErrors } from '~/errors';
+
+export class DisplayPage extends Component {
+  constructor() {
+    super();
+    this.getLinode = getLinode.bind(this);
+    this.loadLinode = loadLinode.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = { group: '', label: '', errors: {} };
+  }
+
+  async componentDidMount() {
+    await this.loadLinode();
+    const { group, label } = this.getLinode();
+    this.setState({ group, label });
+  }
+
+  async onSubmit(e) {
+    e.preventDefault();
+    const { dispatch } = this.props;
+    const { id } = this.getLinode();
+    const { group, label } = this.state;
+    try {
+      await dispatch(linodes.put({ group, label }, id));
+    } catch (response) {
+      const errors = await reduceErrors(response);
+      this.setState({ errors });
+    }
+  }
+
+  render() {
+    const { group, label, errors } = this.state;
+    return (
+      <section className="card">
+        <header>
+          <h2>Display</h2>
+        </header>
+        <form>
+          <FormGroup errors={errors} className="row" field="group">
+            <div className="col-sm-2">
+              <label htmlFor="">Group</label>
+            </div>
+            <div className="col-sm-4">
+              <input
+                className="form-control"
+                name="group"
+                value={group}
+                onChange={e => this.setState({ group: e.target.value })}
+              />
+            </div>
+          </FormGroup>
+          <FormGroup errors={errors} className="row" field="label">
+            <div className="col-sm-2">
+              <label htmlFor="label">Label</label>
+            </div>
+            <div className="col-sm-4">
+              <input
+                className="form-control"
+                name="label"
+                value={label}
+                onChange={e => this.setState({ label: e.target.value })}
+              />
+            </div>
+          </FormGroup>
+          <ErrorSummary errors={errors} />
+          <button className="btn btn-primary" type="submit" onClick={this.onSubmit}>Save</button>
+        </form>
+      </section>
+    );
+  }
 }
+
+DisplayPage.propTypes = {
+  linodes: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function select(state) {
+  return { linodes: state.api.linodes };
+}
+
+export default connect(select)(DisplayPage);
---
+++ test/linodes/linode/settings/layouts/DisplayPage.spec.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+import { expect } from 'chai';
+
+import { expectRequest } from '@/common';
+import { api } from '@/data';
+import { DisplayPage } from '~/linodes/linode/settings/layouts/DisplayPage';
+
+const { linodes } = api;
+
+describe('linodes/linode/settings/layouts/DisplayPage', () => {
+  const sandbox = sinon.sandbox.create();
+  const dispatch = sandbox.spy();
+
+  afterEach(() => {
+    dispatch.reset();
+    sandbox.restore();
+  });
+
+  const props = {
+    linodes,
+    dispatch,
+    params: { linodeId: '1234' },
+  };
+
+  it('renders display page', async () => {
+    const page = await mount(<DisplayPage {...props} />);
+
+    expect(page.find('header h2').text()).to.equal('Display');
+    expect(page.find('[name="group"]').props().value).to.equal('Test Group');
+    expect(page.find('[name="label"]').props().value).to.equal('Test Linode');
+    expect(page.find('button[type="submit"].btn.btn-primary').text()).to.equal('Save');
+  });
+
+  it('changes group and label values onChange', async () => {
+    const page = await mount(<DisplayPage {...props} />);
+
+    page.find('input').map((element) => {
+      const { value, name, onChange } = element.props();
+      const newValue = `${value} Changed`;
+      onChange({ target: { value: newValue } });
+      expect(page.state(name)).to.equal(newValue);
+      expect(element.props().value).to.equal(newValue);
+    });
+  });
+
+  it('makes request to save changes', async () => {
+    const page = shallow(<DisplayPage {...props} />);
+
+    const linode = linodes.linodes[props.params.linodeId];
+    page.find('button').simulate('click', { preventDefault() {} });
+    expect(dispatch.calledOnce).to.equal(true);
+    const fn = dispatch.firstCall.args[0];
+    const dispatched = () => ({ authentication: { token: 'hi' } });
+    await expectRequest(fn, `/linode/instances/${linode.id}`, dispatched,
+                        { group: linode.group, lable: linode.label }, { method: 'PUT' });
+  });
+
+  it('shows error if label is invalid', async () => {
+    const env = { dispatch() {} };
+    const error = 'this is my error string';
+    const dispatchStub = sandbox.stub(env, 'dispatch');
+    dispatchStub.throws({
+      json: () => ({
+        errors: [{ field: 'label', reason: error }],
+      }),
+    });
+
+    const page = await mount(
+      <DisplayPage
+        dispatch={dispatchStub}
+        linodes={props.linodes}
+        params={props.params}
+      />
+    );
+
+    expect(dispatchStub.callCount).to.equal(0);
+    await page.instance().onSubmit({ preventDefault() {} });
+    expect(dispatchStub.calledOnce).to.equal(true);
+
+    const label = page.find('FormGroup').at(1);
+    expect(label.find('.form-control-feedback > div').text()).to.equal(error);
+  });
+});
