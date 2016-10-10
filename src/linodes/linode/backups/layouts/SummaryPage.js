ae1d555a8c0ee4ff401e928c27c76fa256ce8935
Nick <nidodson@gmail.com>
2016-10-10 14:23:35 -0400

    put panels asside for now, as they are going to be in a sub-page

--- src/linodes/linode/backups/layouts/SummaryPage.js
+++ src/linodes/linode/backups/layouts/SummaryPage.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { snapshotPanel } from '~/linodes/linode/backups/components/SnapshotPanel';
-import { restorePanel } from '~/linodes/linode/backups/components/RestorePanel';
 
 export function SummaryPage() {
   return (
     <div>
-      {snapshotPanel()}
-      {restorePanel()}
+      TODO
     </div>
   );
 }
