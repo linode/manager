a6dc51c1d45c081da31b451e7f9b3c930bcffbbd
Nick <nidodson@gmail.com>
2016-10-19 14:28:46 -0400

    add analytics

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,7 +12,8 @@ _.plugins = [
     },
     'ENV_APP_ROOT': JSON.stringify(process.env.APP_ROOT),
     'ENV_API_ROOT': JSON.stringify(process.env.API_ROOT),
-    'ENV_LOGIN_ROOT': JSON.stringify(process.env.LOGIN_ROOT)
+    'ENV_LOGIN_ROOT': JSON.stringify(process.env.LOGIN_ROOT),
+    'ENV_TRACK': JSON.stringify(process.env.TRACK)
   }),
   new webpack.optimize.UglifyJsPlugin({
     compressor: {
