a284d50525523895850eb4edcd7a5f75113553f2
Brian Steffens <bsteffens@linode.com>
2016-10-28 15:59:21 -0400

    Don't hide enable backups

--- src/linodes/create/components/Details.js
+++ src/linodes/create/components/Details.js
@@ -112,17 +112,6 @@ export default class Details extends Component {
       </div>
     );
 
-    const inputRows = [
-      ..._.range(this.state.quantity).map(i =>
-        ({
-          label: i === 0 ? 'Label' : null,
-          content: labelInput(i),
-          errors: errors.label,
-          key: i,
-        })),
-      { label: 'Root password', content: passwordInput },
-    ];
-
     const renderBackupsPrice = () => {
       const price = (this.props.selectedType.backups_price / 100).toFixed(2);
       return ` ($${price})`;
@@ -145,7 +134,18 @@ export default class Details extends Component {
         </label>
       </div>
     );
-    const backups = this.renderRow({ label: 'Backups', content: backupInput });
+
+    const inputRows = [
+      ..._.range(this.state.quantity).map(i =>
+        ({
+          label: i === 0 ? 'Label' : null,
+          content: labelInput(i),
+          errors: errors.label,
+          key: i,
+        })),
+      { label: 'Root password', content: passwordInput },
+      { label: 'Backups', content: backupInput },
+    ];
 
     const showAdvancedOrHide = this.state.showAdvanced ? (
       <span>Hide additional details <span className="fa fa-angle-up" /></span>
@@ -169,7 +169,6 @@ export default class Details extends Component {
               {!this.state.showAdvanced ? null : (
                 <span>
                   {group}
-                  {backups}
                 </span>
                )}
             </section>
