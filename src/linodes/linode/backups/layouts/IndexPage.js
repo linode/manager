93b0b15e1c812c5f13beebfc9240510483d7d6cf
Brian Steffens <bsteffens@linode.com>
2016-11-04 15:09:15 -0400

    Add enable backups page

--- src/linodes/linode/backups/layouts/IndexPage.js
+++ src/linodes/linode/backups/layouts/IndexPage.js
@@ -1,15 +1,17 @@
-import { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { getLinode, loadLinode, renderTabs } from '~/linodes/linode/layouts/IndexPage';
 import { parallel } from '~/api/util';
 import { linodes } from '~/api';
+import { enableBackup } from '~/api/backups';
 import { setSource } from '~/actions/source';
 
 export class IndexPage extends Component {
   constructor() {
     super();
     this.getLinode = getLinode.bind(this);
+    this.enableBackups = this.enableBackups.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
     this.renderTabs = renderTabs.bind(this);
     this.loadLinode = loadLinode.bind(this);
@@ -24,9 +26,28 @@ export class IndexPage extends Component {
       linodes.all()));
   }
 
+  enableBackups() {
+    const { dispatch } = this.props;
+    dispatch(enableBackup(this.getLinode().id));
+  }
+
   render() {
     const linode = this.getLinode();
     if (!linode) return null;
+
+    if (!linode.backups.enabled) {
+      return (
+        <section className="card">
+          <form onSubmit={this.enableBackups}>
+            <p>
+              Backups not enabled. Enable backups for
+              ${(linode.type[0].backups_price / 100).toFixed(2)}.
+            </p>
+            <button className="btn btn-primary">Enable backups</button>
+          </form>
+        </section>
+      );
+    }
 
     const tabList = [
       { name: 'Summary', link: '/' },
