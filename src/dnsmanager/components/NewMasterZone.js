cab724d082580e7cb0ce9bc67582c44cef5b7616
Phil Eaton <peaton@linode.com>
2016-11-10 22:45:28 -0500

    Add dns master create page

---
+++ src/dnsmanager/components/NewMasterZone.js
@@ -0,0 +1,47 @@
+import React, { PropTypes } from 'react';
+
+export default function NewMasterZone(props) {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <div className="form-group row">
+        <div className="col-sm-2">
+          <label>Domain:</label>
+        </div>
+        <div className="col-sm-6">
+          <input
+            className="form-control"
+            placeholder="mydomain.net"
+            value={props.dnszone}
+            onChange={props.onChange('dnszone')}
+          />
+        </div>
+      </div>
+      <div className="form-group row">
+        <div className="col-sm-2">
+          <label>SOA email:</label>
+        </div>
+        <div className="col-sm-6">
+          <input
+            type="email"
+            value={props.soa_email}
+            className="form-control"
+            onChange={props.onChange('soa_email')}
+          />
+        </div>
+      </div>
+      <div className="form-group row">
+        <div className="col-sm-2"></div>
+        <div className="col-sm-6">
+          <button className="btn btn-primary">Create</button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+NewMasterZone.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  soa_email: PropTypes.string.isRequired,
+  dnszone: PropTypes.string.isRequired,
+};
---
+++ src/dnsmanager/layouts/CreatePage.js
@@ -0,0 +1,111 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { push } from 'react-router-redux';
+
+import { setSource } from '~/actions/source';
+import { dnszones } from '~/api';
+import NewMasterZone from '../components/NewMasterZone';
+
+export class CreatePage extends Component {
+  constructor(props) {
+    super(props);
+    this.addMasterZone = this.addMasterZone.bind(this);
+    this.newMasterZoneChange = this.newMasterZoneChange.bind(this);
+    this.state = {
+      tabIndex: 0,
+      newMasterZone: {
+        dnszone: '',
+        soa_email: props.email,
+        type: 'master',
+      },
+    };
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(setSource(__filename));
+  }
+
+  async addMasterZone(e) {
+    e.preventDefault();
+    const { dispatch } = this.props;
+    await dispatch(dnszones.post(this.state.newMasterZone));
+    await dispatch(push('/dnsmanager'));
+  }
+
+  newMasterZoneChange(field) {
+    return e => {
+      this.setState({ newMasterZone: {
+        ...this.state.newMasterZone,
+        [field]: e.target.value,
+      } });
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Add a zone</h1>
+        </header>
+        <section className="card">
+          <header>
+            <h2>Source</h2>
+          </header>
+          <div className="react-tabs">
+            <Tabs
+              onSelect={index => this.setState({ tabIndex: index })}
+              selectedIndex={this.state.tabIndex}
+            >
+              <TabList>
+                <Tab>New master</Tab>
+                <Tab>New slave</Tab>
+                <Tab>Import</Tab>
+                <Tab>Clone</Tab>
+              </TabList>
+              <TabPanel>
+                <section>
+                  <NewMasterZone
+                    onSubmit={this.addMasterZone}
+                    onChange={this.newMasterZoneChange}
+                    soa_email={this.state.newMasterZone.soa_email}
+                    dnszone={this.state.newMasterZone.dnszone}
+                  />
+                </section>
+              </TabPanel>
+              <TabPanel>
+                <section>
+                  TODO
+                </section>
+              </TabPanel>
+              <TabPanel>
+                <section>
+                  TODO
+                </section>
+              </TabPanel>
+              <TabPanel>
+                <section>
+                  TODO
+                </section>
+              </TabPanel>
+            </Tabs>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+CreatePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+function select(state) {
+  return {
+    email: state.authentication.email,
+  };
+}
+
+export default connect(select)(CreatePage);
