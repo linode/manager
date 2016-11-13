16a3c5072a15264b4293d1be1ebc9da26e684fb5
Phil Eaton <peaton@linode.com>
2016-11-13 11:19:14 -0500

    Basic work on the DNS zone page

--- src/dnsmanager/index.js
+++ src/dnsmanager/index.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import IndexPage from './layouts/IndexPage';
 import CreatePage from './layouts/CreatePage';
+import DNSZonePage from './layouts/DNSZonePage';
 
 export default (
   <Route path="/dnsmanager">
     <IndexRoute component={IndexPage} />
     <Route path="create" component={CreatePage} />
+    <Route path=":dnszoneId" component={DNSZonePage} />
   </Route>
 );
