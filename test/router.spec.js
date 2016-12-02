d826111dd195c48598954a72dd63b06cfda2f28f
Brian Steffens <bsteffens@linode.com>
2016-12-02 17:04:31 -0500

    Add tests to LoadingRouterContext

---
+++ test/router.spec.js
@@ -0,0 +1,82 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { LoadingRouterContext } from '~/router';
+
+describe('loading router context', () => {
+  it('should call preload methods during constructor', async () => {
+    const match = sinon.stub().callsArgWith(1, null, null, []);
+
+    new LoadingRouterContext({ // eslint-disable-line no-new
+      match,
+      routes: [],
+      location: {
+        pathname: 'a path',
+      },
+      params: 'some params',
+    });
+
+    expect(match.calledOnce).to.equal(true);
+  });
+
+  it('should call preload during componentWillReceiveProps', async () => {
+    const match = sinon.stub().callsArgWith(1, null, null, []);
+
+    const props = {
+      match,
+      routes: [],
+      location: {
+        pathname: 'a path',
+      },
+      params: 'some params',
+    };
+
+    const rc = new LoadingRouterContext(props);
+
+    match.reset();
+    rc.componentWillReceiveProps(props);
+    expect(match.calledOnce).to.equal(true);
+  });
+
+  it('should prevent updates while preload is running', async () => {
+    const preload = async (store, params) => {
+      expect(params).to.equal('some params');
+
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(), 100);
+      });
+    };
+
+    const rc = new LoadingRouterContext({
+      routes: [],
+      params: 'some params',
+      match: sinon.stub().callsArgWith(1, null, null, {
+        routes: [{ component: { preload } }],
+      }),
+      location: {
+        pathname: 'a path',
+      },
+    });
+
+    expect(rc.shouldComponentUpdate()).to.equal(false);
+
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(), 200);
+    });
+
+    expect(rc.shouldComponentUpdate()).to.equal(true);
+  });
+
+  it('should not render on initial load', async () => {
+    const rc = new LoadingRouterContext({
+      match: () => {},
+      routes: [],
+      params: 'some params',
+      location: {
+        pathname: 'a path',
+      },
+    });
+
+    expect(rc.render()).to.equal(null);
+  });
+});
