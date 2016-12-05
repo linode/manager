e61f773e0b95ff52b2f1c2f327e6ec768998956a
Brian Steffens <briansteffens@users.noreply.github.com>
2016-12-05 10:50:45 -0500

    Add tests to LoadingRouterContext (#761)
    * Add tests to LoadingRouterContext
    
    * Improve timeout mess

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import { Router, Route, RouterContext, IndexRedirect, browserHistory } from 'react-router';
+import { Router, Route, match, IndexRedirect, browserHistory } from 'react-router';
 import DevTools from './components/DevTools';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { match } from 'react-router';
 import ReactGA from 'react-ga';
 import { GA_ID } from './constants';
 
@@ -29,6 +28,7 @@ import Longview from './longview';
 import DNSManager from './dnsmanager';
 import Support from './support';
 import { hideModal } from '~/actions/modal';
+import { LoadingRouterContext } from '~/router';
 
 import { actions, thunks, reducer } from '~/api/configs/linodes';
 window.actions = actions; window.thunks = thunks; window.reducer = reducer;
@@ -39,68 +39,6 @@ function logPageView() {
   ReactGA.pageview(window.location.pathname);
 }
 
-class LoadingRouterContext extends RouterContext {
-  runPreload(newProps) {
-    // Suppress component update until after route preloads have finished
-    this.fetching = true;
-
-    match({
-      routes: newProps.routes,
-      location: newProps.location.pathname,
-    }, async (error, redirectLocation, redirectParams) => {
-      // Call preload (if present) on any components rendered by the route,
-      // down to the page level (Layout -> IndexPage -> EditConfigPage)
-      for (let i = 0; i < redirectParams.routes.length; i++) {
-        const component = redirectParams.routes[i].component;
-        if (component !== undefined && component.hasOwnProperty('preload')) {
-          await component.preload(store, newProps.params);
-        }
-      }
-
-      // Allow component update now that preloads are done
-      this.fetching = false;
-
-      // Set anything at all to force an update
-      if (!this.initialLoad) {
-        this.setState({
-          updateNow: 'please',
-        });
-      }
-    });
-  }
-
-  constructor(props) {
-    super();
-    this.fetching = false;
-    this.initialLoad = true;
-    this.runPreload(props);
-  }
-
-  componentWillReceiveProps(newProps) {
-    if (super.componentWillReceiveProps) {
-      super.componentWillReceiveProps(newProps);
-    }
-
-    this.runPreload(newProps);
-  }
-
-  shouldComponentUpdate() {
-    return !this.fetching;
-  }
-
-  render() {
-    if (this.initialLoad) {
-      this.initialLoad = false;
-
-      if (this.fetching) {
-        return null;
-      }
-    }
-
-    return super.render();
-  }
-}
-
 const init = () => {
   render(
     <Provider store={store}>
@@ -108,7 +46,7 @@ const init = () => {
         <Router
           history={history}
           onUpdate={logPageView}
-          render={props => <LoadingRouterContext {...props} />}
+          render={props => <LoadingRouterContext match={match} {...props} />}
         >
           <Route
             path="/logout"
---
+++ src/router.js
@@ -0,0 +1,65 @@
+import { RouterContext } from 'react-router';
+import store from './store';
+
+export class LoadingRouterContext extends RouterContext {
+  async runPreload(newProps) {
+    // Suppress component update until after route preloads have finished
+    this.fetching = true;
+
+    this.match({
+      routes: newProps.routes,
+      location: newProps.location.pathname,
+    }, async (error, redirectLocation, redirectParams) => {
+      // Call preload (if present) on any components rendered by the route,
+      // down to the page level (Layout -> IndexPage -> EditConfigPage)
+      for (let i = 0; i < redirectParams.routes.length; i++) {
+        const component = redirectParams.routes[i].component;
+        if (component !== undefined && component.hasOwnProperty('preload')) {
+          await component.preload(store, newProps.params);
+        }
+      }
+
+      // Allow component update now that preloads are done
+      this.fetching = false;
+
+      // Set anything at all to force an update
+      if (!this.initialLoad) {
+        this.setState({
+          updateNow: 'please',
+        });
+      }
+    });
+  }
+
+  constructor(props) {
+    super();
+    this.match = props.match;
+    this.fetching = false;
+    this.initialLoad = true;
+    this.runPreload(props);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (super.componentWillReceiveProps) {
+      super.componentWillReceiveProps(newProps);
+    }
+
+    this.runPreload(newProps);
+  }
+
+  shouldComponentUpdate() {
+    return !this.fetching;
+  }
+
+  render() {
+    if (this.initialLoad) {
+      this.initialLoad = false;
+
+      if (this.fetching) {
+        return null;
+      }
+    }
+
+    return super.render();
+  }
+}
