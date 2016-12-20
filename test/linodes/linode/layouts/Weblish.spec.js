ba87116bea4009138e9dea9e987b7b000f1a6b6e
Abe Massry <amassry@linode.com>
2016-12-20 15:36:04 -0500

    add first weblish test with thanks to @mparke

---
+++ test/linodes/linode/layouts/Weblish.spec.js
@@ -0,0 +1,24 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+import * as weblish from '~/linodes/linode/layouts/Weblish';
+
+describe('linodes/linode/layouts/Weblish', () => {
+  const sandbox = sinon.sandbox.create();
+
+  const dispatch = sandbox.spy();
+
+  afterEach(() => {
+    dispatch.reset();
+    sandbox.restore();
+  });
+
+  it('tests addCSSLink', () => {
+    const before = window.document.querySelector('head').querySelector('link');
+    expect(before).to.equal(null);
+    weblish.addCSSLink('http://example.com');
+    const after = window.document.querySelector('head').querySelector('link').outerHTML;
+    expect(after).to.equal('<link rel="stylesheet" type="text/css" href="http://example.com">');
+  });
+});
+
