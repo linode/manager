96ad52dae1187c265045f2832f29084478b64417
Abe Massry <amassry@linode.com>
2016-12-20 17:19:52 -0500

    test weblish constructor

--- test/linodes/linode/layouts/Weblish.spec.js
+++ test/linodes/linode/layouts/Weblish.spec.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import sinon from 'sinon';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import * as weblish from '~/linodes/linode/layouts/Weblish';
@@ -27,6 +29,30 @@ describe('linodes/linode/layouts/Weblish', () => {
     weblish.addJSScript('http://example.com');
     const after = window.document.querySelector('head').querySelector('script').outerHTML;
     expect(after).to.equal('<script src="http://example.com"></script>');
+  });
+
+  it('tests the Weblish constructor', () => {
+    shallow(<weblish.Weblish />);
+    const linksExpected = {
+      0: '<link rel="stylesheet" type="text/css" href="http://example.com">',
+      1: '<link rel="stylesheet" type="text/css" href="/assets/weblish/weblish-fonts.css">',
+      2: '<link rel="stylesheet" type="text/css" href="/assets/weblish/weblish.css">',
+      3: '<link rel="stylesheet" type="text/css" href="/assets/weblish/xterm.css">',
+      length: 4,
+    };
+    const links = window.document.querySelector('head').querySelectorAll('link');
+    expect(links[1].outerHTML).to.equal(linksExpected[1]);
+    expect(links[2].outerHTML).to.equal(linksExpected[2]);
+    expect(links[3].outerHTML).to.equal(linksExpected[3]);
+    expect(links.length).to.equal(4);
+    const scripts = window.document.querySelector('head').querySelectorAll('script');
+    const scriptsExpected = {
+      0: '<script src="http://example.com"></script>',
+      1: '<script src="/assets/weblish/xterm.js"></script>',
+      length: 2,
+    };
+    expect(scripts[1].outerHTML).to.equal(scriptsExpected[1]);
+    expect(scripts.length).to.equal(2);
   });
 });
 
