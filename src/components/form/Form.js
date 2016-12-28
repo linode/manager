9dd3119c90ac746b170f08089498cc8bf57ff336
Matthew Parke <mparke78@gmail.com>
2016-12-28 13:54:56 -0500

    Add a form button component which prevents default browser submit behavior by default

---
+++ src/components/form/Form.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react';
+
+export default function Form(props) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onSubmit(event);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >{props.children}</form>
+  );
+}
+
+Form.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired,
+};
