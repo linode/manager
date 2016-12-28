9884a46c33ac5bf3a1e6bd716240ae1867c1cd0d
Matthew Parke <mparke78@gmail.com>
2016-12-28 15:32:14 -0500

    Disk panel form consistency (#875)
    * Add a cancel button component
    
    * Add a submit button component
    
    * Add a form button component which prevents default browser submit behavior by default
    
    * Add an index for importing form components
    
    * Update disk panel to use new form and button components
    
    Buttons now use explicit and correct button types
    Click handlers have been changed to form submits so that enter to submit can easily be supported
    
    * Update tests for corresponding disk panel form changes
    
    * update paths and form prop type
    
    * capitalization fix

---
+++ src/components/form/index.js
@@ -0,0 +1,3 @@
+export { default as CancelButton } from '~/components/form/CancelButton';
+export { default as Form } from '~/components/form/Form';
+export { default as SubmitButton } from '~/components/form/SubmitButton';
