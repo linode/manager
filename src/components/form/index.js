bb0c8aa035caaed4ddb640fcfba0f1c699c7b06c
Matthew Parke <mparke78@gmail.com>
2016-12-28 14:53:14 -0500

    capitalization fix

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,3 +1,3 @@
 export { default as CancelButton } from '~/components/form/CancelButton';
-export { default as Form } from '~/components/form/form';
+export { default as Form } from '~/components/form/Form';
 export { default as SubmitButton } from '~/components/form/SubmitButton';
