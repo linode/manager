c793c4f7d93f66179b475719458dafbc2a11a93f
Matthew Parke <mparke78@gmail.com>
2017-01-04 09:35:43 -0500

    Initial Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,26 +27,26 @@ export default function Header(props) {
   const unseenNotifications = Object.values(events.events).reduce((unseen, e) =>
     e.seen ? unseen : unseen + 1, 0);
   const main = (
-    <div className="MainHeader clearfix">
-      <div className="MainHeader-brand">
+    <nav className="MainHeader navbar">
+      <div className="MainHeader-brand navbar-brand">
         <Link to="/">
-          <span className="MainHeader-logo">
-            <img
-              src={logoOnly}
-              alt="linode logo"
-              height={40}
-              width={35}
-            />
-          </span>
+          <img
+            src="../assets/logo/logo.svg"
+            alt="linode logo"
+            height={40}
+            width={35}
+          />
           <span className="MainHeader-title">Linode Manager</span>
         </Link>
       </div>
-      <div className="MainHeader-search">
-        <input placeholder="Search..." />
+      <div className="MainHeader-search form-inline float-xs-left" role="search">
+        <div className="form-group">
+          <input placeholder="Search..." type="text" />
+        </div>
       </div>
       {!username ? null :
         <div
-          className="MainHeader-session float-xs-right"
+          className="MainHeader-session navbar-right"
           onClick={hideShowNotifications}
         >
           <span className="MainHeader-username">
@@ -63,7 +63,7 @@ export default function Header(props) {
             <span className="MainHeader-badge">{unseenNotifications}</span>}
         </div>
       }
-    </div>
+    </nav>
   );
 
   return (
---
+++ src/components/navigation/index.js
@@ -0,0 +1,1 @@
+export { default as StackedNav } from '~/components/navigation/StackedNav';
