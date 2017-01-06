9b5991e1728d84d4983790922695522e219c2a73
Brian Steffens <bsteffens@linode.com>
2017-01-06 10:06:22 -0500

    Add take first snapshot link

--- src/api/backups.js
+++ src/api/backups.js
@@ -24,6 +24,7 @@ export function takeBackup(id) {
       { method: 'POST' });
     const json = await response.json();
     dispatch(actions.backups.one(json, id, json.id));
+    return json;
   };
 }
 
