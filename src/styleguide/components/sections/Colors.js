bf8915c00680018ce21e24ce7c91122216039143
Matthew Parke <mparke78@gmail.com>
2017-01-10 12:25:32 -0500

    ClassName cleanup

--- src/styleguide/components/sections/Colors.js
+++ src/styleguide/components/sections/Colors.js
@@ -9,22 +9,22 @@ export default function Colors() {
         <div className="row">
           <div className="col-sm-6">
             {/* Greys */}
-            <div className="Color">
+            <div className="StyleguideColor">
               <div>
-                <div className="Color-block Color-block__light-gray"></div>
-                <div className="Color-block Color-block__gray"></div>
-                <div className="Color-block Color-block__dark-gray"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__light-gray"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__gray"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__dark-gray"></div>
               </div>
-              <div className="Color-block-details">
-                <div className="Color-block-detail">
+              <div className="StyleguideColor-block-details">
+                <div className="StyleguideColor-block-detail">
                   <div>$light-gray</div>
                   <div>#FBFBFB</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>$gray</div>
                   <div>#F3F3F3</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>$dark-gray</div>
                   <div>#DDDDDD</div>
                 </div>
@@ -33,22 +33,22 @@ export default function Colors() {
           </div>
           <div className="col-sm-6">
             {/* Blacks */}
-            <div className="Color">
+            <div className="StyleguideColor">
               <div>
-                <div className="Color-block Color-block__lightest-black"></div>
-                <div className="Color-block Color-block__light-black"></div>
-                <div className="Color-block Color-block__black"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__lightest-black"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__light-black"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__black"></div>
               </div>
-              <div className="Color-block-details">
-                <div className="Color-block-detail">
+              <div className="StyleguideColor-block-details">
+                <div className="StyleguideColor-block-detail">
                   <div>$lightest-black</div>
                   <div>#ACACAC</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>$light-black</div>
                   <div>#777</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>$black</div>
                   <div>#4b4b4b</div>
                 </div>
@@ -60,22 +60,22 @@ export default function Colors() {
         <div className="row">
           <div className="col-sm-6">
             {/* Blues */}
-            <div className="Color">
+            <div className="StyleguideColor">
               <div>
-                <div className="Color-block Color-block__light-blue"></div>
-                <div className="Color-block Color-block__blue"></div>
-                <div className="Color-block Color-block__dark-blue"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__light-blue"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__blue"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__dark-blue"></div>
               </div>
-              <div className="Color-block-details">
-                <div className="Color-block-detail">
+              <div className="StyleguideColor-block-details">
+                <div className="StyleguideColor-block-detail">
                   <div>$light-blue</div>
                   <div>#DEEFF5</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>$blue</div>
                   <div>#4A90E2</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>darken($blue, 10%)</div>
                 </div>
               </div>
@@ -83,12 +83,12 @@ export default function Colors() {
           </div>
           <div className="col-sm-6">
             {/* Greens */}
-            <div className="Color">
+            <div className="StyleguideColor">
               <div>
-                <div className="Color-block Color-block__green"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__green"></div>
               </div>
-              <div className="Color-block-details">
-                <div className="Color-block-detail">
+              <div className="StyleguideColor-block-details">
+                <div className="StyleguideColor-block-detail">
                   <div>$green</div>
                   <div>#38D377</div>
                 </div>
@@ -100,12 +100,12 @@ export default function Colors() {
         <div className="row">
           <div className="col-sm-6">
             {/* Reds */}
-            <div className="Color">
+            <div className="StyleguideColor">
               <div>
-                <div className="Color-block Color-block__red"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__red"></div>
               </div>
-              <div className="card-block Color-block-details">
-                <div className="Color-block-detail">
+              <div className="card-block StyleguideColor-block-details">
+                <div className="StyleguideColor-block-detail">
                   <div>$red</div>
                   <div>#F24C2B</div>
                 </div>
@@ -114,22 +114,22 @@ export default function Colors() {
           </div>
           <div className="col-sm-6">
             {/* Yellows */}
-            <div className="Color">
+            <div className="StyleguideColor">
               <div>
-                <div className="Color-block Color-block__yellow"></div>
-                <div className="Color-block Color-block__dark-yellow"></div>
-                <div className="Color-block Color-block__darkest-yellow"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__yellow"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__dark-yellow"></div>
+                <div className="StyleguideColor-block StyleguideColor-block__darkest-yellow"></div>
               </div>
-              <div className="card-block Color-block-details">
-                <div className="Color-block-detail">
+              <div className="card-block StyleguideColor-block-details">
+                <div className="StyleguideColor-block-detail">
                   <div>$yellow</div>
                   <div>#FFFCF0</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>$dark-yellow</div>
                   <div>darken($yellow, 20)</div>
                 </div>
-                <div className="Color-block-detail">
+                <div className="StyleguideColor-block-detail">
                   <div>$darkest-yellow</div>
                   <div>darken($yellow, 40)</div>
                 </div>
