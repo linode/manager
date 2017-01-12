5966f1c56c44f3804bc5418f5abb93ccde73869e
Brian Steffens <bsteffens@linode.com>
2017-01-12 10:45:53 -0500

    Reduce title content

--- src/reducers/title.js
+++ src/reducers/title.js
@@ -3,7 +3,7 @@ import { SET_TITLE } from '~/actions/title';
 export default function title(state = null, action) {
   switch (action.type) {
     case SET_TITLE:
-      document.title = `${action.title} - Linode Manager`;
+      document.title = `${action.title} - Linode`;
       return { title: action.title };
     default:
       return { ...state };
