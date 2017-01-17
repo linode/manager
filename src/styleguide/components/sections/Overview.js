678c2c57d660818dcfc1ae0a1c3fa8a45f999f54
Matthew Parke <mparke78@gmail.com>
2017-01-17 14:13:22 -0500

    Styleguide overview (#1013)
    * Add simple overview description, fix sub section import
    
    * Address linting errors for long lines

--- src/styleguide/components/sections/Overview.js
+++ src/styleguide/components/sections/Overview.js
@@ -5,10 +5,34 @@ import { StyleguideSection } from '~/styleguide/components';
 export default function Overview() {
   return (
     <StyleguideSection name="overview" title="Overview">
-      <div className="StyleguideOverview col-sm-12">
-        <p>
-          At Linode, we want to create a consistent user experience.
-        </p>
+      <div className="col-sm-12">
+        <div className="StyleguideSubSection row">
+          <div className="col-sm-10">
+            <p>
+              At Linode, we want to create a consistent user experience.
+              This document describes the shared components and patterns
+              used to achieve a consistent experience custom to Linode.
+            </p>
+            <p>
+              The goals of the Style Guide include:
+            </p>
+            <ul>
+              <li>Code reuse through shared, atomic components</li>
+              <li>
+                Consistent building, allowing effort to be focused
+                on application logic and user flow
+              </li>
+              <li>
+                Documenting common patterns in a single place,
+                supporting fast iterations, and easy maintenance
+              </li>
+            </ul>
+            <p>
+              Documented patterns are flexible, and are intended
+              to be adapted to solve unique challenges.
+            </p>
+          </div>
+        </div>
       </div>
     </StyleguideSection>
   );
