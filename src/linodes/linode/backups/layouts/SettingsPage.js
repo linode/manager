af915e3e346315886170ca1b429fe3226e158979
Matthew Parke <mparke78@gmail.com>
2017-01-20 16:20:42 -0500

    Fix formatting

--- src/linodes/linode/backups/layouts/SettingsPage.js
+++ src/linodes/linode/backups/layouts/SettingsPage.js
@@ -14,137 +14,137 @@ import { showModal, hideModal } from '~/actions/modal';
 
 
 export class SettingsPage extends Component {
-    constructor(props) {
-        super(props);
-        this.getLinode = getLinode.bind(this);
-        const { day, window } = this.getLinode().backups.schedule;
-        this.state = { day, window, errors: {} };
+  constructor(props) {
+    super(props);
+    this.getLinode = getLinode.bind(this);
+    const { day, window } = this.getLinode().backups.schedule;
+    this.state = { day, window, errors: {} };
+  }
+
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(setSource(__filename));
+  }
+
+  async saveChanges() {
+    const { dispatch } = this.props;
+    const linode = this.getLinode();
+    const { day, window } = this.state;
+    try {
+      await dispatch(linodes.put({
+        backups: {
+          schedule: { day, window },
+        },
+      }, linode.id));
+    } catch (response) {
+      const errors = await reduceErrors(response);
+      this.setState({ errors });
     }
+  }
 
-    async componentDidMount() {
-        const { dispatch } = this.props;
-        dispatch(setSource(__filename));
-    }
+  render() {
+    const { dispatch } = this.props;
+    const { window, day, errors } = this.state;
+    const linode = this.getLinode();
 
-    async saveChanges() {
-        const { dispatch } = this.props;
-        const linode = this.getLinode();
-        const { day, window } = this.state;
-        try {
-            await dispatch(linodes.put({
-                backups: {
-                    schedule: { day, window },
-                },
-            }, linode.id));
-        } catch (response) {
-            const errors = await reduceErrors(response);
-            this.setState({ errors });
-        }
-    }
-
-    render() {
-        const { dispatch } = this.props;
-        const { window, day, errors } = this.state;
-        const linode = this.getLinode();
-
-        return (
-            <div>
-              <Card title="Schedule">
-                <Form onSubmit={() => this.saveChanges()}>
-                  <FormGroup name="window" errors={errors} className="row">
-                    <div className="label-col col-sm-2">
-                        <label htmlFor="window">Time of day (EST):</label>
-                    </div>
-                    <div className="col-sm-4">
-                      <select
-                          className="form-control"
-                          id="window"
-                          name="window"
-                          value={window}
-                          onChange={e => this.setState({ window: e.target.value })}
-                      >
-                        <option value="W0">12-2 AM</option>
-                        <option value="W2">2-4 AM</option>
-                        <option value="W4">4-6 AM</option>
-                        <option value="W6">6-8 AM</option>
-                        <option value="W8">8-10 AM</option>
-                        <option value="W10">10-12 AM</option>
-                        <option value="W12">12-2 PM</option>
-                        <option value="W14">2-4 PM</option>
-                        <option value="W16">4-6 PM</option>
-                        <option value="W18">6-8 PM</option>
-                        <option value="W20">8-10 PM</option>
-                        <option value="W22">10-12 PM</option>
-                      </select>
-                    </div>
-                  </FormGroup>
-                  <FormGroup name="day" errors={errors} className="row">
-                    <div className="label-col col-sm-2">
-                        <label htmlFor="day">Day of week:</label>
-                    </div>
-                    <div className="col-sm-4">
-                      <select
-                          className="form-control"
-                          id="day"
-                          name="day"
-                          value={day}
-                          onChange={e => this.setState({ day: e.target.value })}
-                      >
-                        <option value="Sunday">Sunday</option>
-                        <option value="Monday">Monday</option>
-                        <option value="Tuesday">Tuesday</option>
-                        <option value="Wednesday">Wednesday</option>
-                        <option value="Thursday">Thursday</option>
-                        <option value="Friday">Friday</option>
-                        <option value="Saturday">Saturday</option>
-                      </select>
-                    </div>
-                  </FormGroup>
-                  <ErrorSummary errors={errors} />
-                  <FormGroup className="row">
-                    <div className="offset-sm-2 col-sm-10">
-                      <SubmitButton />
-                    </div>
-                  </FormGroup>
-                </Form>
-              </Card>
-              <Card title="Cancel backup service">
-                <p>This will remove all existing backups.</p>
-                <button
-                    id="LinodesLinodeBackupsSettings-cancel"
-                    className="btn btn-delete btn-default"
-                    onClick={() => {
-                        dispatch(showModal('Cancel backup service', <ConfirmModalBody
-                            onOk={async () => {
-                                await dispatch(cancelBackup(linode.id));
-                                dispatch(hideModal());
-                            }}
-                            onCancel={() => dispatch(hideModal())}
-                            buttonText="Cancel backups service"
-                        >
-                            Are you sure you want to cancel backup service for this Linode?
-                            This cannot be undone.
-                        </ConfirmModalBody>))
-                    }}
+    return (
+      <div>
+        <Card title="Schedule">
+          <Form onSubmit={() => this.saveChanges()}>
+            <FormGroup name="window" errors={errors} className="row">
+              <div className="label-col col-sm-2">
+                <label htmlFor="window">Time of day (EST):</label>
+              </div>
+              <div className="col-sm-4">
+                <select
+                  className="form-control"
+                  id="window"
+                  name="window"
+                  value={window}
+                  onChange={e => this.setState({ window: e.target.value })}
                 >
-                  Cancel backup service
-                </button>
-              </Card>
-            </div>
-        );
-    }
+                  <option value="W0">12-2 AM</option>
+                  <option value="W2">2-4 AM</option>
+                  <option value="W4">4-6 AM</option>
+                  <option value="W6">6-8 AM</option>
+                  <option value="W8">8-10 AM</option>
+                  <option value="W10">10-12 AM</option>
+                  <option value="W12">12-2 PM</option>
+                  <option value="W14">2-4 PM</option>
+                  <option value="W16">4-6 PM</option>
+                  <option value="W18">6-8 PM</option>
+                  <option value="W20">8-10 PM</option>
+                  <option value="W22">10-12 PM</option>
+                </select>
+              </div>
+            </FormGroup>
+            <FormGroup name="day" errors={errors} className="row">
+              <div className="label-col col-sm-2">
+                <label htmlFor="day">Day of week:</label>
+              </div>
+              <div className="col-sm-4">
+                <select
+                  className="form-control"
+                  id="day"
+                  name="day"
+                  value={day}
+                  onChange={e => this.setState({ day: e.target.value })}
+                >
+                  <option value="Sunday">Sunday</option>
+                  <option value="Monday">Monday</option>
+                  <option value="Tuesday">Tuesday</option>
+                  <option value="Wednesday">Wednesday</option>
+                  <option value="Thursday">Thursday</option>
+                  <option value="Friday">Friday</option>
+                  <option value="Saturday">Saturday</option>
+                </select>
+              </div>
+            </FormGroup>
+            <ErrorSummary errors={errors} />
+            <FormGroup className="row">
+              <div className="offset-sm-2 col-sm-10">
+                <SubmitButton />
+              </div>
+            </FormGroup>
+          </Form>
+        </Card>
+        <Card title="Cancel backup service">
+          <p>This will remove all existing backups.</p>
+          <button
+            id="LinodesLinodeBackupsSettings-cancel"
+            className="btn btn-delete btn-default"
+            onClick={() => {
+              dispatch(showModal('Cancel backup service', <ConfirmModalBody
+                onOk={async() => {
+                  await dispatch(cancelBackup(linode.id));
+                  dispatch(hideModal());
+                }}
+                onCancel={() => dispatch(hideModal())}
+                buttonText="Cancel backups service"
+              >
+                Are you sure you want to cancel backup service for this Linode?
+                This cannot be undone.
+              </ConfirmModalBody>));
+            }}
+          >
+            Cancel backup service
+          </button>
+        </Card>
+      </div>
+    );
+  }
 }
 
 SettingsPage.propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    linodes: PropTypes.object.isRequired,
-    params: PropTypes.shape({
-        linodeLabel: PropTypes.string.isRequired,
-    }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+  linodes: PropTypes.object.isRequired,
+  params: PropTypes.shape({
+    linodeLabel: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 function select(state) {
-    return { linodes: state.api.linodes };
+  return { linodes: state.api.linodes };
 }
 
 export default connect(select)(SettingsPage);
