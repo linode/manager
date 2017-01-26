bbe9de3a91922e36e4edf7c2bf1e2faea17c7b8b
Phil Eaton <peaton@linode.com>
2017-01-26 11:45:51 -0500

    Added profile skeleton and password & auth page

--- src/profile/layouts/IndexPage.js
+++ src/profile/layouts/IndexPage.js
@@ -1,19 +1,45 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React, { PropTypes } from 'react';
+import { push } from 'react-router-redux';
 
-export class IndexPage extends Component {
-  render() {
-    return null;
-  }
+import { Tabs } from '~/components/tabs';
+
+export default function IndexPage() {
+  const tabs = [
+    { name: 'Display', link: '' },
+    { name: 'Password & Authentication', link: '/authentication' },
+    { name: 'Integrations', link: '/integrations' },
+    { name: 'Notifications', link: '/notifications' },
+    { name: 'Referrals', link: '/referrals' },
+  ].map(t => ({ ...t, link: `/profile${t.link}` }));
+
+  const pathname = location ? location.pathname : tabs[0].link;
+  const selected = tabs.reduce((last, current) =>
+    (pathname.indexOf(current.link) === 0 ? current : last));
+
+  return (
+    <div>
+      <header className="main-header">
+        <div className="container">
+          <h1>My profile</h1>
+        </div>
+      </header>
+      <div className="main-header-fix"></div>
+      <Tabs
+        tabs={tabs}
+        selected={selected}
+        onClick={(e, tab) => {
+          e.stopPropagation();
+          this.props.dispatch(push(tab.link));
+        }}
+      >
+        <div className="container">
+          {this.props.children}
+        </div>
+      </Tabs>
+    </div>
+  );
 }
 
 IndexPage.propTypes = {
-  dispatch: PropTypes.func,
+  children: PropTypes.node.isRequired,
 };
-
-
-function select(state) {
-  return {};
-}
-
-export default connect(select)(IndexPage);
