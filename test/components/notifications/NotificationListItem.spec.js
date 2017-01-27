4080919d99aa87ccf600cdddcc73422112e07d73
Matthew Parke <mparke78@gmail.com>
2017-01-27 15:08:46 -0500

    Cleanup

--- test/components/notifications/NotificationListItem.spec.js
+++ test/components/notifications/NotificationListItem.spec.js
@@ -4,7 +4,6 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 import { api } from '@/data';
-// import { makeEvent } from '@/data/events';
 
 import { NotificationListItem } from '~/components/notifications';
 
@@ -32,30 +31,4 @@ describe('components/notifications/NotificationListItem', () => {
     const notification = shallow(makeNotificationListItem());
     expect(notification.find('.NotificationList-listItem--unread').length).to.equal(0);
   });
-
-  // it('marks unread notification as read onclick', () => {
-  //   const readNotification = sandbox.spy();
-  //   const notification = shallow(makeNotificationListItem(
-  //     api.events.events[385], undefined, undefined, readNotification));
-  //   notification.find('.Notification').simulate('click');
-  //   expect(readNotification.callCount).to.equal(1);
-  //   expect(readNotification.args[0][0]).to.equal(385);
-  // });
-  //
-  // it('marks unread, redirects, and closes pane on view click', () => {
-  //   const readNotification = sandbox.spy();
-  //   const gotoPage = sandbox.spy();
-  //   const hideShowNotifications = sandbox.spy();
-  //   const notification = shallow(makeNotificationListItem(api.events.events[385],
-  //     undefined, gotoPage, readNotification, hideShowNotifications));
-  //   notification.find('.Notification-subject').simulate('click');
-  //
-  //   expect(readNotification.callCount).to.equal(1);
-  //   expect(readNotification.args[0][0]).to.equal(385);
-  //
-  //   expect(gotoPage.callCount).to.equal(1);
-  //   expect(gotoPage.args[0][0]).to.equal('/linodes/test-linode-3');
-  //
-  //   expect(hideShowNotifications.callCount).to.equal(1);
-  // });
 });
