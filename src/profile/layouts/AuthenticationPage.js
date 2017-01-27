2a02001e67383784dbdd0e27f5e83bc47a09d61b
Phil Eaton <peaton@linode.com>
2017-01-27 12:02:12 -0500

    Fix tests on case-sensitive systems

--- src/profile/layouts/AuthenticationPage.js
+++ src/profile/layouts/AuthenticationPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Card from '~/components/card';
+import Card from '~/components/Card';
 import {
   Select, PasswordInput, Form, FormGroup, FormGroupError, SubmitButton,
 } from '~/components/form';
