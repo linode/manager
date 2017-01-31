064bb9174d136ba3faf852cb6793a823860584a2
Matthew Parke <mparke78@gmail.com>
2017-01-31 11:41:30 -0500

    Add X-Filter header for updated (#1106)
    * Add X-Filter header for updated
    
    * Add comment to filter block

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -10,7 +10,7 @@ import { actions as eventsActions } from '~/api/configs/events';
 import { eventRead, eventSeen } from '~/api/events';
 import Header from '~/components/Header';
 import Sidebar from '~/components/Sidebar';
-import { NotificationList } from '~/components/notifications';
+import { NotificationList, sortEvents } from '~/components/notifications';
 import { ModalShell } from '~/components/modals';
 import Error from '~/components/Error';
 import Feedback from '~/components/Feedback';
@@ -100,7 +100,28 @@ export class Layout extends Component {
 
   async fetchEventsPage(page = 0, processedEvents = { events: [] }) {
     const { dispatch } = this.props;
-    const nextProcessedEvents = await dispatch(events.page(page, [], this.eventHandler, false));
+
+    // filter by most recently updated
+    const sortedEvents = sortEvents(this.props.events);
+    let headers = null;
+
+    // X-Filter header only applied when we have a known last updated datetime
+    if (sortedEvents.length) {
+      headers = {
+        'X-Filter': JSON.stringify({
+          updated: {
+            '+gt': sortedEvents[0].updated,
+          },
+        }),
+      };
+    }
+
+    const nextProcessedEvents = await dispatch(
+      events.page(page, [], this.eventHandler, false, null, {
+        headers,
+      })
+    );
+
     // If all the events are new, we want to fetch another page.
     const allUnseen = nextProcessedEvents.events.reduce((allUnseenEvents, { seen }) =>
       !seen && allUnseenEvents, true) && nextProcessedEvents.events.length;
@@ -220,7 +241,7 @@ export class Layout extends Component {
               await dispatch(eventRead(event.id));
             }
           }}
-          events={this.props.events}
+          events={sortEvents(this.props.events)}
           eventSeen={(id) => dispatch(eventSeen(id))}
         />
         <Feedback
