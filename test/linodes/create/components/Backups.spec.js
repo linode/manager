c242f152afe0a0eec008517914e531586c204734
Nick <nidodson@gmail.com>
2017-02-08 18:04:24 -0500

    emptying tests from create backups

--- test/linodes/create/components/Backups.spec.js
+++ test/linodes/create/components/Backups.spec.js
@@ -1,10 +1,4 @@
-import React from 'react';
 import sinon from 'sinon';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-
-import { Backups } from '~/linodes/create/components/Backups';
-import { api } from '@/data';
 
 describe('linodes/create/components/Backups', () => {
   const sandbox = sinon.sandbox.create();
