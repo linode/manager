8af729e0c8cfbad178812b0f6a3630eceee89ebd
Phil Eaton <peaton@linode.com>
2017-02-08 10:20:38 -0500

    Fix profile usage of Tabs

--- src/profile/layouts/IndexPage.js
+++ src/profile/layouts/IndexPage.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
-import { Tabs } from '~/components/Tabs';
+import Tabs from '~/components/Tabs';
 import { setSource } from '~/actions/source';
 import { setTitle } from '~/actions/title';
 
