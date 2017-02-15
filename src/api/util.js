e07e7ed40c87db7d5599bf517b6928d78c3a57f6
Phil Eaton <philneaton95@gmail.com>
2017-02-15 17:42:08 -0500

    404 on label lookup failure (#1235)
    * 404 on label lookup failure
    
    * Add tests
    
    * Clarify 404 response
    
    * Fix tests

--- src/api/util.js
+++ src/api/util.js
@@ -1,5 +1,14 @@
 import * as api from '~/api';
 
+// Extra cruft involving constructor / prototypes is for any `new Error404`s  to be shown as
+// instanceof Error404:
+// http://stackoverflow.com/a/38020283/1507139
+export function Error404() {
+  this.statusCode = 404;
+}
+
+Error404.prototype = new Error();
+
 export function getObjectByLabelLazily(pluralName, label, labelName = 'label') {
   return async (dispatch, getState) => {
     const oldResources = Object.values(getState().api[pluralName][pluralName]);
@@ -10,10 +19,16 @@ export function getObjectByLabelLazily(pluralName, label, labelName = 'label') {
       return oldResource;
     }
 
-    return (await dispatch(api[pluralName].all([], undefined, {
+    const response = (await dispatch(api[pluralName].all([], undefined, {
       headers: {
         'X-Filter': JSON.stringify({ [labelName]: label }),
       },
-    })))[pluralName][0];
+    })));
+
+    if (!response.total_results) {
+      throw new Error404();
+    }
+
+    return response[pluralName][0];
   };
 }
