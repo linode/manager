075318fed5ae36788e34ba7f7d522383d51d2e17
Nick <nidodson@gmail.com>
2017-02-16 10:44:13 -0500

    UI for Add New Slave (#1230)
    * UI for Add New Slave
    
    * Tests for New Slave
    
    * add error handling for slave and master
    
    * Update tests for DNS error handling
    
    * remove redundant code and fix tests
    
    * switching to Card and Tabs

--- src/dnsmanager/layouts/CreatePage.js
+++ src/dnsmanager/layouts/CreatePage.js
@@ -8,18 +8,30 @@ import { setSource } from '~/actions/source';
 import { setTitle } from '~/actions/title';
 import { dnszones } from '~/api';
 import NewMasterZone from '../components/NewMasterZone';
+import NewSlaveZone from '../components/NewSlaveZone';
+import { reduceErrors } from '~/errors';
 
 export class CreatePage extends Component {
   constructor(props) {
     super(props);
-    this.addMasterZone = this.addMasterZone.bind(this);
-    this.newMasterZoneChange = this.newMasterZoneChange.bind(this);
+    this.addZone = this.addZone.bind(this);
+    this.zoneStateChange = this.zoneStateChange.bind(this);
     this.state = {
       tabIndex: 0,
+      loading: false,
       newMasterZone: {
         dnszone: '',
         soa_email: props.email,
         type: 'master',
+        loading: false,
+        errors: {},
+      },
+      newSlaveZone: {
+        dnszone: '',
+        master_ips: [],
+        type: 'slave',
+        loading: false,
+        errors: {},
       },
     };
   }
@@ -30,18 +42,35 @@ export class CreatePage extends Component {
     dispatch(setTitle('Add a zone'));
   }
 
-  async addMasterZone(e) {
+  async addZone(e, tab) {
     e.preventDefault();
     const { dispatch } = this.props;
-    await dispatch(dnszones.post(this.state.newMasterZone));
-    await dispatch(push('/dnsmanager'));
+    this.setState({ [tab]: {
+      ...this.state[tab],
+      loading: true,
+    } });
+    try {
+      await dispatch(dnszones.post(this.state[tab]));
+      await dispatch(push('/dnsmanager'));
+    } catch (response) {
+      const errors = await reduceErrors(response);
+      this.setState({ [tab]: {
+        ...this.state[tab],
+        loading: false,
+        errors,
+      } });
+    }
   }
 
-  newMasterZoneChange(field) {
+  zoneStateChange(field, tab) {
     return e => {
-      this.setState({ newMasterZone: {
-        ...this.state.newMasterZone,
-        [field]: e.target.value,
+      let value = e.target.value;
+      if (field === 'master_ips') {
+        value = value.split(';');
+      }
+      this.setState({ [tab]: {
+        ...this.state[tab],
+        [field]: value,
       } });
     };
   }
@@ -52,16 +81,27 @@ export class CreatePage extends Component {
         name: 'New Master',
         children: (
           <NewMasterZone
-            onSubmit={this.addMasterZone}
-            onChange={this.newMasterZoneChange}
+            onSubmit={(e) => this.addZone(e, 'newMasterZone')}
+            onChange={(e) => this.zoneStateChange(e, 'newMasterZone')}
             soa_email={this.state.newMasterZone.soa_email}
             dnszone={this.state.newMasterZone.dnszone}
+            loading={this.state.newMasterZone.loading}
+            errors={this.state.newMasterZone.errors}
           />
         ),
       },
       {
         name: 'New Slave',
-        children: 'TODO',
+        children: (
+          <NewSlaveZone
+            onSubmit={(e) => this.addZone(e, 'newSlaveZone')}
+            onChange={(e) => this.zoneStateChange(e, 'newSlaveZone')}
+            master_ips={this.state.newSlaveZone.master_ips}
+            dnszone={this.state.newSlaveZone.dnszone}
+            loading={this.state.newSlaveZone.loading}
+            errors={this.state.newSlaveZone.errors}
+          />
+        ),
       },
       {
         name: 'Import',
@@ -81,7 +121,7 @@ export class CreatePage extends Component {
         <Card title="Source">
           <Tabs
             tabs={tabs}
-            onClick={index => this.setState({ tabIndex: index })}
+            onClick={(_, index) => this.setState({ tabIndex: index })}
             selected={this.state.tabIndex}
             isSubTabs
           />
