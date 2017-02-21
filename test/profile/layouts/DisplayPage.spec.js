79bf65fcea1fb52480e37a4b39376ea36e6f051c
Nick <nidodson@gmail.com>
2017-02-21 14:56:05 -0500

    test put to api

--- test/profile/layouts/DisplayPage.spec.js
+++ test/profile/layouts/DisplayPage.spec.js
@@ -3,6 +3,7 @@ import sinon from 'sinon';
 import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 
+import { expectRequest, expectObjectDeepEquals } from '@/common';
 import { DisplayPage } from '~/profile/layouts/DisplayPage';
 import { TIME_ZONES } from '~/constants';
 
@@ -33,16 +34,48 @@ describe('profile/layouts/DisplayPage', () => {
 
     expect(page.find('option').length).to.equal(TIME_ZONES.length);
     expect(page.find('#timezone').props().value).to.equal('US/Eastern');
+
+    page.instance().setState({ timezone: 'GMT' });
+    await page.instance().timezoneOnSubmit();
+    expect(dispatch.calledOnce).to.equal(true);
+    const fn = dispatch.firstCall.args[0];
+
+    function expectRequestOptions({ method, body }) {
+      expect(method).to.equal('PUT');
+      expectObjectDeepEquals(JSON.parse(body), {
+        timezone: 'GMT',
+      });
+    }
+
+    await expectRequest(fn, '/account/profile',
+                        () => {}, null, expectRequestOptions);
   });
 
   it('renders email', async () => {
-    const page = shallow(
+    const page = mount(
       <DisplayPage
         dispatch={dispatch}
         profile={profile}
       />
     );
 
-    expect(page.find('#email').props().value).to.equal('example@domain.com');
+    dispatch.reset();
+    const email = page.find('#email');
+    expect(email.props().value).to.equal('example@domain.com');
+
+    page.instance().setState({ email: 'example2@domain.com' });
+    await page.instance().emailOnSubmit();
+    expect(dispatch.calledOnce).to.equal(true);
+    const fn = dispatch.firstCall.args[0];
+
+    function expectRequestOptions({ method, body }) {
+      expect(method).to.equal('PUT');
+      expectObjectDeepEquals(JSON.parse(body), {
+        email: 'example2@domain.com',
+      });
+    }
+
+    await expectRequest(fn, '/account/profile',
+                        () => {}, null, expectRequestOptions);
   });
 });
