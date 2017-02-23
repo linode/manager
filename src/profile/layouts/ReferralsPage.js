63048ccb9a299f74b21b97e1cc58ea64dbb3b4ac
Matthew Parke <mparke78@gmail.com>
2017-02-23 10:01:53 -0500

    break out forms, update location of preload, and add placeholders

--- src/profile/layouts/ReferralsPage.js
+++ src/profile/layouts/ReferralsPage.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 import { Card } from '~/components/cards';
 
-export default function ReferralsPage() {
+function ReferralsPage(props) {
+  // TODO: Add referrals description
   return (
     <Card title="Referrals">
-      <p>No referral information.</p>
+      <div className="row">
+        <div className="col-sm-2 row-label">
+          Referral Code
+        </div>
+        <div className="col-sm-10">
+          {props.code}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-2 row-label">
+          Referral URL
+        </div>
+        <div className="col-sm-10">
+          {props.url}
+        </div>
+      </div>
     </Card>
   );
 }
+
+function select(state) {
+  return {
+    referrals: state.api.profile.profile.undefined.referrals,
+  };
+}
+
+ReferralsPage.propTypes = {
+  code: PropTypes.string,
+  url: PropTypes.string,
+};
+
+export default connect(select)(ReferralsPage);
+
