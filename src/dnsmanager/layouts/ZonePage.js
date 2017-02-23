61eb26f2c2821eb57bf9d8599e88a221f75966cf
Abe Massry <a@abemassry.com>
2017-02-23 18:05:39 -0500

    add / edit SRV records (#1271)
    * add / edit SRV records
    
    * answer pr feedback
    
    * change default ints

--- src/dnsmanager/layouts/ZonePage.js
+++ src/dnsmanager/layouts/ZonePage.js
@@ -9,6 +9,7 @@ import EditNSRecord from '../components/EditNSRecord';
 import EditMXRecord from '../components/EditMXRecord';
 import EditARecord from '../components/EditARecord';
 import EditTXTRecord from '../components/EditTXTRecord';
+import EditSRVRecord from '../components/EditSRVRecord';
 import EditCNAMERecord from '../components/EditCNAMERecord';
 import { setError } from '~/actions/errors';
 import { dnszones } from '~/api';
@@ -193,6 +194,11 @@ export class ZonePage extends Component {
     return this.renderEditRecord(title, EditTXTRecord, { id });
   }
 
+
+  renderEditSRVRecord(title, id) {
+    return this.renderEditRecord(title, EditSRVRecord, { id });
+  }
+
   renderEditCNAMERecord(title, id) {
     return this.renderEditRecord(title, EditCNAMERecord, { id });
   }
@@ -235,7 +241,7 @@ export class ZonePage extends Component {
     const txtRecords = formatSeconds(
       addNav(this.formatTXTRecords(), (id) => this.renderEditTXTRecord('Edit TXT Record', id)));
     const srvRecords = formatSeconds(
-      addNav(this.formatSRVRecords()));
+      addNav(this.formatSRVRecords(), (id) => this.renderEditSRVRecord('Edit SRV Record', id)));
     const soaRecord = {
       ...currentDNSZone,
       ttl_sec: formatDNSSeconds(currentDNSZone.ttl_sec),
@@ -321,8 +327,9 @@ export class ZonePage extends Component {
             title="SRV Records"
             id="srv"
             records={srvRecords}
+            navOnClick={this.renderEditSRVRecord('Add SRV Record')}
             labels={['Service', 'Priority', 'Domain', 'Weight', 'Port', 'Target', 'TTL', '']}
-            keys={['name', 'priority', 'domain', 'weight', 'port', 'target', 'ttl_sec', 'nav']}
+            keys={['service', 'priority', 'domain', 'weight', 'port', 'target', 'ttl_sec', 'nav']}
           />
         </div>
       </div>
