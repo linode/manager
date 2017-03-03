68457cf113ff56186266132656cccca7a5a86a1c
Matthew Parke <mparke78@gmail.com>
2017-03-03 10:40:52 -0500

    adjust stringify call

--- src/api/util.js
+++ src/api/util.js
@@ -21,7 +21,7 @@ export function getObjectByLabelLazily(pluralName, label, labelName = 'label') {
 
     const response = (await dispatch(api[pluralName].all([], undefined, {
       headers: {
-        'X-Filter': JSON.stringify({ [labelName]: label }),
+        'X-Filter': { [labelName]: label },
       },
     })));
 
