365b94ee6ef707d1cb6e0c666033e842517b3c17
Matthew Parke <mparke78@gmail.com>
2017-03-03 14:23:54 -0500

    Adds util functions (#1338)
    * Adds util functions
    
    * revert change
    
    * Adds tests

--- test/api/util.spec.js
+++ test/api/util.spec.js
@@ -3,7 +3,13 @@ import { expect } from 'chai';
 
 import { generateDefaultStateMany } from '~/api/apiResultActionReducerGenerator.js';
 import { config as linodeConfig } from '~/api/configs/linodes';
-import { getObjectByLabelLazily, Error404 } from '~/api/util';
+import {
+  getObjectByLabelLazily,
+  Error404,
+  lessThanDatetimeFilter,
+  lessThanNowFilter,
+  createHeaderFilter,
+} from '~/api/util';
 import { state } from '@/data';
 import { testLinode } from '@/data/linodes';
 import { expectRequest } from '@/common';
@@ -55,5 +61,50 @@ describe('api/util', async () => {
         throw Error;
       }
     }
+  });
+
+  it('provides a header structure utility function', function () {
+    const emptyHeader = createHeaderFilter({});
+
+    expect(emptyHeader).to.exist;
+    expect(emptyHeader).to.be.an('object');
+
+    expect(emptyHeader.headers).to.exist;
+    expect(emptyHeader.headers).to.be.an('object');
+
+    expect(emptyHeader.headers['X-Filter']).to.exist;
+    expect(emptyHeader.headers['X-Filter']).to.be.an('object');
+  });
+
+  it('provides createHeaderFilter which assigns an object to X-Filter', function () {
+    const emptyHeader = createHeaderFilter({ example: true });
+
+    expect(emptyHeader.headers['X-Filter']).to.exist;
+    expect(emptyHeader.headers['X-Filter']).to.be.an('object');
+    expect(emptyHeader.headers['X-Filter'].example).to.equal(true);
+  });
+
+  it('provides a lessThanDatetimeFilter function', function () {
+    const lessThanNow = lessThanDatetimeFilter('example', (new Date()).toString());
+
+    expect(lessThanNow).to.exist;
+    expect(lessThanNow).to.be.an('object');
+    expect(lessThanNow.example).to.exist;
+    expect(lessThanNow.example).to.be.an('object');
+    expect(lessThanNow.example['+lte']).to.exist;
+    expect(typeof lessThanNow.example['+lte']).to.equal('string');
+    expect(typeof (new Date(lessThanNow.example['+lte']))).to.equal('object');
+  });
+
+  it('provides a lessThanNowFilter function', function () {
+    const lessThanNow = lessThanNowFilter('example');
+
+    expect(lessThanNow).to.exist;
+    expect(lessThanNow).to.be.an('object');
+    expect(lessThanNow.example).to.exist;
+    expect(lessThanNow.example).to.be.an('object');
+    expect(lessThanNow.example['+lte']).to.exist;
+    expect(typeof lessThanNow.example['+lte']).to.equal('string');
+    expect(typeof (new Date(lessThanNow.example['+lte']))).to.equal('object');
   });
 });
