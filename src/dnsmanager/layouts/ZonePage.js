890efb500c145fd5c5f4921b5a4817d310672f21
Abe Massry <a@abemassry.com>
2017-03-03 11:21:01 -0500

    Dns delete record (#1341)
    * delete record functional
    
    * fixup from rebase
    
    * fix failing test
    
    * remove unnecessary variable

--- src/dnsmanager/layouts/ZonePage.js
+++ src/dnsmanager/layouts/ZonePage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import { showModal, hideModal } from '~/actions/modal';
+import ConfirmModalBody from '~/components/modals/ConfirmModalBody';
 import { formatDNSSeconds, ONE_DAY } from '../components/SelectDNSSeconds';
 import EditSOARecord from '../components/EditSOARecord';
 import EditNSRecord from '../components/EditNSRecord';
@@ -83,7 +84,10 @@ export class ZonePage extends Component {
             onClick={this.renderEditNSRecord('Edit NS Record', record.id)}
             className="btn-secondary"
           >Edit</Button>
-          <Button className="btn-secondary">Delete</Button>
+          <Button
+            onClick={() => this.renderDeleteRecord('Delete NS Record', record.id)}
+            className="btn-secondary"
+          >Delete</Button>
         </div>
       ),
     }));
@@ -135,14 +139,35 @@ export class ZonePage extends Component {
     }));
   }
 
-  renderRecords = ({ title, id, records, labels, keys, nav, navOnClick }) => {
+  renderRecords = ({ title, singularTitle, id, records, labels, keys, nav, navOnClick }) => {
     let cardContents = <p>No records created.</p>;
+    const editSingularTitle = `Edit ${singularTitle}`;
     if (records && records.length) {
       cardContents = (
         <SecondaryTable
           labels={labels}
           keys={keys}
           rows={records}
+          onRowClick={(e, record) => {
+            const classList = e.target.classList;
+            if (classList.contains('edit-button')) {
+              if (records[0].type === 'MX') {
+                this.renderEditRecord(editSingularTitle, EditMXRecord, { id });
+              } else if (records[0].type === 'NS') {
+                this.renderEditRecord(editSingularTitle, EditNSRecord, { id });
+              } else if (records[0].type === 'TXT') {
+                this.renderEditRecord(editSingularTitle, EditTXTRecord, { id });
+              } else if (records[0].type === 'A') {
+                this.renderEditRecord(editSingularTitle, EditARecord, { id });
+              } else if (records[0].type === 'CNAME') {
+                this.renderEditRecord(editSingularTitle, EditCNAMERecord, { id });
+              } else if (records[0].type === 'SRV') {
+                this.renderEditRecord(editSingularTitle, EditSRVRecord, { id });
+              }
+            } else if (classList.contains('delete-button')) {
+              this.renderDeleteRecord(`Delete ${singularTitle}`, record.id);
+            }
+          }}
         />
       );
     }
@@ -161,46 +186,63 @@ export class ZonePage extends Component {
     );
   }
 
+  renderDeleteRecord(title, id) {
+    const { dispatch } = this.props;
+    const { currentDNSZone: zone } = this.state;
+    dispatch(showModal(title,
+      <ConfirmModalBody
+        buttonText="Delete zone record"
+        onOk={async () => {
+          await dispatch(dnszones.records.delete(zone.id, id));
+          dispatch(hideModal());
+        }}
+        onCancel={() => dispatch(hideModal())}
+      >
+        Are you sure you want to delete the zone record?
+      </ConfirmModalBody>
+    ));
+  }
+
   renderEditRecord(title, component, props = {}) {
     const { dispatch } = this.props;
     const { currentDNSZone: zone } = this.state;
-    return () => dispatch(showModal(
+    dispatch(showModal(
       title,
       React.createElement(component, {
         ...props,
         dispatch,
         zone,
         close: () => dispatch(hideModal()),
-      })));
+      })
+    ));
   }
 
   renderEditSOARecord(title) {
-    return this.renderEditRecord(title, EditSOARecord);
-  }
-
-  renderEditNSRecord(title, id) {
-    return this.renderEditRecord(title, EditNSRecord, { id });
+    return () => { this.renderEditRecord(title, EditSOARecord); };
   }
 
   renderEditMXRecord(title, id) {
-    return this.renderEditRecord(title, EditMXRecord, { id });
+    return () => { this.renderEditRecord(title, EditMXRecord, { id }); };
+  }
+
+  renderEditNSRecord(title, id) {
+    return () => { this.renderEditRecord(title, EditNSRecord, { id }); };
   }
 
   renderEditARecord(title, id) {
-    return this.renderEditRecord(title, EditARecord, { id });
+    return () => { this.renderEditRecord(title, EditARecord, { id }); };
   }
 
   renderEditTXTRecord(title, id) {
-    return this.renderEditRecord(title, EditTXTRecord, { id });
+    return () => { this.renderEditRecord(title, EditTXTRecord, { id }); };
   }
 
-
   renderEditSRVRecord(title, id) {
-    return this.renderEditRecord(title, EditSRVRecord, { id });
+    return () => { this.renderEditRecord(title, EditSRVRecord, { id }); };
   }
 
   renderEditCNAMERecord(title, id) {
-    return this.renderEditRecord(title, EditCNAMERecord, { id });
+    return () => { this.renderEditRecord(title, EditCNAMERecord, { id }); };
   }
 
   render() {
@@ -214,34 +256,26 @@ export class ZonePage extends Component {
         ttl_sec: formatDNSSeconds(ttlSec, defaultTTLSec),
       };
     });
-    const addNav = (records, onEdit, onDelete) => records.map(record => ({
-      ...record, nav: (
-        <div>
-          <Button
-            onClick={onEdit && onEdit(record.id)}
-            className="btn-secondary"
-          >Edit</Button>
-          <Button
-            onClick={onDelete && onDelete(record.id)}
-            className="btn-secondary"
-          >Delete</Button>
-        </div>
-      ),
-    }));
 
-    const nsRecords = formatSeconds(
-      this.formatNSRecords());
-    const mxRecords = formatSeconds(
-      addNav(this.formatMXRecords(), (id) => this.renderEditMXRecord('Edit MX Record', id)));
-    const aRecords = formatSeconds(
-      addNav(this.formatARecords(), (id) => this.renderEditARecord('Edit A/AAAA Record', id)));
-    const cnameRecords = formatSeconds(
-      addNav(this.formatCNAMERecords(),
-        (id) => this.renderEditCNAMERecord('Edit CNAME Record', id)));
-    const txtRecords = formatSeconds(
-      addNav(this.formatTXTRecords(), (id) => this.renderEditTXTRecord('Edit TXT Record', id)));
-    const srvRecords = formatSeconds(
-      addNav(this.formatSRVRecords(), (id) => this.renderEditSRVRecord('Edit SRV Record', id)));
+    const addNav = (records = []) => (
+      records.map((record) => ({
+        ...record,
+        nav: (
+          <div>
+            <Button className="btn-secondary edit-button">Edit</Button>
+            <Button className="btn-secondary delete-button">Delete</Button>
+          </div>
+        ) })
+      )
+    );
+
+    const nsRecords = formatSeconds(this.formatNSRecords());
+    const mxRecords = formatSeconds(addNav(this.formatMXRecords()));
+    const aRecords = formatSeconds(addNav(this.formatARecords()));
+    const cnameRecords = formatSeconds(addNav(this.formatCNAMERecords()));
+    const txtRecords = formatSeconds(addNav(this.formatTXTRecords()));
+    const srvRecords = formatSeconds(addNav(this.formatSRVRecords()));
+
     const soaRecord = {
       ...currentDNSZone,
       ttl_sec: formatDNSSeconds(currentDNSZone.ttl_sec),
@@ -271,6 +305,7 @@ export class ZonePage extends Component {
         <div className="container">
           <this.renderRecords
             title="SOA Record"
+            singularTitle="SOA Record"
             id="soa"
             records={[soaRecord]}
             labels={['Primary DNS', 'Email', 'Default TTL', 'Refresh Rate', 'Retry Rate',
@@ -281,6 +316,7 @@ export class ZonePage extends Component {
           />
           <this.renderRecords
             title="NS Records"
+            singularTitle="NS Record"
             id="ns"
             records={nsRecords}
             navOnClick={this.renderEditNSRecord('Add NS Record')}
@@ -289,6 +325,7 @@ export class ZonePage extends Component {
           />
           <this.renderRecords
             title="MX Records"
+            singularTitle="MX Record"
             id="mx"
             records={mxRecords}
             navOnClick={this.renderEditMXRecord('Add MX Record')}
@@ -297,6 +334,7 @@ export class ZonePage extends Component {
           />
           <this.renderRecords
             title="A/AAAA Records"
+            singularTitle="A/AAAA Record"
             id="a"
             records={aRecords}
             navOnClick={this.renderEditARecord('Add A/AAAA Record')}
@@ -305,6 +343,7 @@ export class ZonePage extends Component {
           />
           <this.renderRecords
             title="CNAME Records"
+            singularTitle="CNAME Record"
             id="cname"
             navOnClick={this.renderEditCNAMERecord('Add CNAME Record')}
             records={cnameRecords}
@@ -313,6 +352,7 @@ export class ZonePage extends Component {
           />
           <this.renderRecords
             title="TXT Records"
+            singularTitle="TXT Record"
             id="txt"
             records={txtRecords}
             navOnClick={this.renderEditTXTRecord('Add TXT Record')}
--- test/dnsmanager/layouts/ZonePage.spec.js
+++ test/dnsmanager/layouts/ZonePage.spec.js
@@ -5,7 +5,6 @@ import { expect } from 'chai';
 
 import { formatDNSSeconds } from '~/dnsmanager/components/SelectDNSSeconds';
 import { ZonePage } from '~/dnsmanager/layouts/ZonePage';
-import { NAME_SERVERS } from '~/constants';
 import { api } from '@/data';
 
 const { dnszones } = api;
@@ -40,178 +39,174 @@ describe('dnsmanager/layouts/ZonePage', () => {
       expect(section.find('p').text()).to.equal('No records created.'));
   });
 
-  it('renders a zone with records', () => {
-    it('renders soa records', () => {
-      const currentZone = dnszones.dnszones[1];
+  it('renders soa records', () => {
+    const currentZone = dnszones.dnszones[1];
 
-      const page = mount(
-        <ZonePage
-          dispatch={dispatch}
-          params={{ dnszoneLabel: currentZone.dnszone }}
-          dnszones={dnszones}
-        />
-      );
+    const page = mount(
+      <ZonePage
+        dispatch={dispatch}
+        params={{ dnszoneLabel: currentZone.dnszone }}
+        dnszones={dnszones}
+      />
+    );
 
-      const soaRow = page.find('#soa .SecondaryTable-row');
-      expect(soaRow.length).to.equal(1);
+    const soaRow = page.find('#soa .SecondaryTable-row');
+    expect(soaRow.length).to.equal(1);
 
-      const soaValues = soaRow.find('td');
-      expect(soaValues.length).to.equal(7);
-      const fmt = formatDNSSeconds;
-      // Test all values in SOA row
-      [currentZone.dnszone, currentZone.soa_email, fmt(currentZone.ttl_sec),
-       fmt(currentZone.refresh_sec), fmt(currentZone.retry_sec),
-       fmt(currentZone.expire_sec, 604800)].forEach(
-         (value, i) => expect(soaValues.at(i).text()).to.equal(value));
+    const soaValues = soaRow.find('td');
+    expect(soaValues.length).to.equal(7);
+    const fmt = formatDNSSeconds;
+    // Test all values in SOA row
+    [currentZone.dnszone, currentZone.soa_email, fmt(currentZone.ttl_sec),
+     fmt(currentZone.refresh_sec), fmt(currentZone.retry_sec),
+     fmt(currentZone.expire_sec, 604800)].forEach(
+       (value, i) => expect(soaValues.at(i).text()).to.equal(value));
+  });
+
+  it('renders ns records', () => {
+    const currentZone = dnszones.dnszones[1];
+    const nsRecords = Object.values(currentZone._records.records).filter(
+      r => r.type === 'NS');
+
+    const page = mount(
+      <ZonePage
+        dispatch={dispatch}
+        params={{ dnszoneLabel: currentZone.dnszone }}
+        dnszones={dnszones}
+      />
+    );
+
+    const nsRows = page.find('#ns .SecondaryTable-row');
+    expect(nsRows.length).to.equal(5);
+    const nsValues = nsRows.at(4).find('td');
+    expect(nsValues.length).to.equal(4);
+    // Test all values in an NS row
+    const nsRecord = nsRecords[0];
+    [nsRecord.target, nsRecord.name || currentZone.dnszone,
+      '86400 (1 day)'].forEach((value, i) => expect(nsValues.at(i).text()).to.equal(value));
+  });
+
+  it('renders mx records', () => {
+    const currentZone = dnszones.dnszones[1];
+    const mxRecords = Object.values(currentZone._records.records).filter(
+      r => r.type === 'MX');
+
+    const page = mount(
+      <ZonePage
+        dispatch={dispatch}
+        params={{ dnszoneLabel: currentZone.dnszone }}
+        dnszones={dnszones}
+      />
+    );
+
+    const mxRows = page.find('#mx .SecondaryTable-row');
+    expect(mxRows.length).to.equal(mxRecords.length);
+
+    const mxValues = mxRows.at(0).find('td');
+    expect(mxValues.length).to.equal(4);
+    // Test all values in an MX row
+    const mxRecord = mxRecords[0];
+    [mxRecord.target, mxRecord.priority, mxRecord.name || currentZone.dnszone].forEach(
+      (value, i) => expect(mxValues.at(i).text()).to.equal(value.toString()));
+  });
+
+  it('renders a records', () => {
+    const currentZone = dnszones.dnszones[1];
+    const aRecords = Object.values(currentZone._records.records).filter(
+      r => ['A', 'AAAA'].indexOf(r.type) !== -1);
+
+    const page = mount(
+      <ZonePage
+        dispatch={dispatch}
+        params={{ dnszoneLabel: currentZone.dnszone }}
+        dnszones={dnszones}
+      />
+    );
+
+    const aRows = page.find('#a .SecondaryTable-row');
+    expect(aRows.length).to.equal(aRecords.length);
+
+    const aValues = aRows.at(0).find('td');
+    expect(aValues.length).to.equal(4);
+    // Test all values in a A/AAAA row
+    const aRecord = aRecords[0];
+    [aRecord.name, aRecord.target, formatDNSSeconds(aRecord.ttl_sec, currentZone.ttl_sec),
+    ].forEach((value, i) => expect(aValues.at(i).text()).to.equal(value));
+  });
+
+  it('renders cname records', () => {
+    const currentZone = dnszones.dnszones[1];
+    const cnameRecords = Object.values(currentZone._records.records).filter(
+      r => r.type === 'CNAME');
+
+    const page = mount(
+      <ZonePage
+        dispatch={dispatch}
+        params={{ dnszoneLabel: currentZone.dnszone }}
+        dnszones={dnszones}
+      />
+    );
+
+    const cnameRows = page.find('#cname .SecondaryTable-row');
+    expect(cnameRows.length).to.equal(cnameRecords.length);
+
+    const cnameValues = cnameRows.at(0).find('td');
+    expect(cnameValues.length).to.equal(4);
+    // Test all values in a CNAME row
+    const cnameRecord = cnameRecords[0];
+    [cnameRecord.name, cnameRecord.target,
+     formatDNSSeconds(cnameRecord.ttl_sec, currentZone.ttl_sec)].forEach((value, i) =>
+       expect(cnameValues.at(i).text()).to.equal(value));
+  });
+
+  it('renders txt records', () => {
+    const currentZone = dnszones.dnszones[1];
+    const txtRecords = Object.values(currentZone._records.records).filter(
+      r => r.type === 'TXT');
+
+    const page = mount(
+      <ZonePage
+        dispatch={dispatch}
+        params={{ dnszoneLabel: currentZone.dnszone }}
+        dnszones={dnszones}
+      />
+    );
+
+    const txtRows = page.find('#txt .SecondaryTable-row');
+    expect(txtRows.length).to.equal(txtRecords.length);
+
+    const txtValues = txtRows.at(0).find('td');
+    expect(txtValues.length).to.equal(4);
+    // Test all values in a TXT row
+    const txtRecord = txtRecords[0];
+    [txtRecord.name, txtRecord.target, formatDNSSeconds(txtRecord.ttl_sec, currentZone.ttl_sec),
+    ].forEach((value, i) => {
+      expect(txtValues.at(i).text()).to.equal(value);
     });
+  });
 
-    it('renders ns records', () => {
-      const currentZone = dnszones.dnszones[1];
-      const nsRecords = Object.values(currentZone._records.records).filter(
-        r => r.type === 'NS');
+  it('renders srv records', () => {
+    const currentZone = dnszones.dnszones[1];
+    const srvRecords = Object.values(currentZone._records.records).filter(
+      r => r.type === 'SRV');
 
-      const page = mount(
-        <ZonePage
-          dispatch={dispatch}
-          params={{ dnszoneLabel: currentZone.dnszone }}
-          dnszones={dnszones}
-        />
-      );
+    const page = mount(
+      <ZonePage
+        dispatch={dispatch}
+        params={{ dnszoneLabel: currentZone.dnszone }}
+        dnszones={dnszones}
+      />
+    );
 
-      const nsRows = page.find('#ns .SecondaryTable-row');
-      expect(nsRows.length).to.equal(NAME_SERVERS.length + nsRecords.length);
+    const srvRows = page.find('#srv .SecondaryTable-row');
+    expect(srvRows.length).to.equal(srvRecords.length);
 
-      const nsValues = nsRows.at(5).find('td');
-      expect(nsValues.length).to.equal(4);
-      // Test all values in an NS row
-      const nsRecord = nsRecords[0];
-      [nsRecord.target, nsRecord.name || currentZone.dnszone,
-       formatDNSSeconds(nsRecord.ttl, currentZone.ttl_sec)].forEach((value, i) =>
-         expect(nsValues.at(i).text()).to.equal(value));
-    });
-
-    it('renders mx records', () => {
-      const currentZone = dnszones.dnszones[1];
-      const mxRecords = Object.values(currentZone._records.records).filter(
-        r => r.type === 'MX');
-
-      const page = mount(
-        <ZonePage
-          dispatch={dispatch}
-          params={{ dnszoneLabel: currentZone.dnszone }}
-          dnszones={dnszones}
-        />
-      );
-
-      const mxRows = page.find('#mx .SecondaryTable-row');
-      expect(mxRows.length).to.equal(mxRecords.length);
-
-      const mxValues = mxRows.at(0).find('td');
-      expect(mxValues.length).to.equal(4);
-      // Test all values in an MX row
-      const mxRecord = mxRecords[0];
-      [mxRecord.target, mxRecord.priority, mxRecord.name || currentZone.dnszone].forEach(
-        (value, i) => expect(mxValues.at(i).text()).to.equal(value.toString()));
-    });
-
-    it('renders a records', () => {
-      const currentZone = dnszones.dnszones[1];
-      const aRecords = Object.values(currentZone._records.records).filter(
-        r => ['A', 'AAAA'].indexOf(r.type) !== -1);
-
-      const page = mount(
-        <ZonePage
-          dispatch={dispatch}
-          params={{ dnszoneLabel: currentZone.dnszone }}
-          dnszones={dnszones}
-        />
-      );
-
-      const aRows = page.find('#a .SecondaryTable-row');
-      expect(aRows.length).to.equal(aRecords.length);
-
-      const aValues = aRows.at(0).find('td');
-      expect(aValues.length).to.equal(4);
-      // Test all values in a A/AAAA row
-      const aRecord = aRecords[0];
-      [aRecord.name, aRecord.target, formatDNSSeconds(aRecord.ttl_sec, currentZone.ttl_sec),
-      ].forEach((value, i) => expect(aValues.at(i).text()).to.equal(value));
-    });
-
-    it('renders cname records', () => {
-      const currentZone = dnszones.dnszones[1];
-      const cnameRecords = Object.values(currentZone._records.records).filter(
-        r => r.type === 'CNAME');
-
-      const page = mount(
-        <ZonePage
-          dispatch={dispatch}
-          params={{ dnszoneLabel: currentZone.dnszone }}
-          dnszones={dnszones}
-        />
-      );
-
-      const cnameRows = page.find('#cname .SecondaryTable-row');
-      expect(cnameRows.length).to.equal(cnameRecords.length);
-
-      const cnameValues = cnameRows.at(0).find('td');
-      expect(cnameValues.length).to.equal(4);
-      // Test all values in a CNAME row
-      const cnameRecord = cnameRecords[0];
-      [cnameRecord.name, cnameRecord.target,
-       formatDNSSeconds(cnameRecord.ttl_sec, currentZone.ttl_sec)].forEach((value, i) =>
-         expect(cnameValues.at(i).text()).to.equal(value));
-    });
-
-    it('renders txt records', () => {
-      const currentZone = dnszones.dnszones[1];
-      const txtRecords = Object.values(currentZone._records.records).filter(
-        r => r.type === 'TXT');
-
-      const page = mount(
-        <ZonePage
-          dispatch={dispatch}
-          params={{ dnszoneLabel: currentZone.dnszone }}
-          dnszones={dnszones}
-        />
-      );
-
-      const txtRows = page.find('#txt .SecondaryTable-row');
-      expect(txtRows.length).to.equal(txtRecords.length);
-
-      const txtValues = txtRows.at(0).find('td');
-      expect(txtValues.length).to.equal(4);
-      // Test all values in a TXT row
-      const txtRecord = txtRecords[0];
-      [txtRecord.name, txtRecord.target, formatDNSSeconds(txtRecord.ttl_sec, currentZone.ttl_sec),
-      ].forEach((value, i) => {
-        expect(txtValues.at(i).text()).to.equal(value);
-      });
-    });
-
-    it('renders srv records', () => {
-      const currentZone = dnszones.dnszones[1];
-      const srvRecords = Object.values(currentZone._records.records).filter(
-        r => r.type === 'SRV');
-
-      const page = mount(
-        <ZonePage
-          dispatch={dispatch}
-          params={{ dnszoneLabel: currentZone.dnszone }}
-          dnszones={dnszones}
-        />
-      );
-
-      const srvRows = page.find('#srv .SecondaryTable-row');
-      expect(srvRows.length).to.equal(srvRecords.length);
-
-      const srvValues = srvRows.at(0).find('td');
-      expect(srvValues.length).to.equal(8);
-      // Test all values in a SRV row
-      const srvRecord = srvRecords[0];
-      [srvRecord.name, srvRecord.priority, currentZone.dnszone, srvRecord.weight, srvRecord.port,
-       srvRecord.target, formatDNSSeconds(srvRecord.ttl_sec, currentZone.ttl_sec),
-      ].forEach((value, i) => expect(srvValues.at(i).text()).to.equal(value.toString()));
-    });
+    const srvValues = srvRows.at(0).find('td');
+    expect(srvValues.length).to.equal(8);
+    // Test all values in a SRV row
+    const srvRecord = srvRecords[0];
+    [srvRecord.name, srvRecord.priority, currentZone.dnszone, srvRecord.weight, srvRecord.port,
+     srvRecord.target, formatDNSSeconds(srvRecord.ttl_sec, currentZone.ttl_sec),
+    ].forEach((value, i) => expect(srvValues.at(i).text()).to.equal(value.toString()));
   });
 });
