4f355b8ca51ea1275041908fc52f8dbdc1b43cbb
Phil Eaton <peaton@linode.com>
2017-03-06 17:14:32 -0500

    IP Sharing working

--- src/linodes/linode/networking/components/IPSharing.js
+++ src/linodes/linode/networking/components/IPSharing.js
@@ -1,34 +1,122 @@
-// TODO: GAHHH this page needs to know all other linodes' ips not the current one
+import React, { PropTypes, Component } from 'react';
 
-import React, { PropTypes } from 'react';
-
+import _ from 'lodash';
 import { Card } from '~/components/cards';
 import SecondaryTable from '~/components/SecondaryTable';
+import { Form, FormGroup, SubmitButton, Checkbox } from '~/components/form';
+import { Link } from '~/components/Link';
+import { ErrorSummary, reduceErrors } from '~/errors';
+import { setShared } from '~/api/linodes';
 
-export default function IPSharing(props) {
-  const { dispatch, linodes } = props;
+export default class IPSharing extends Component {
+  constructor(props) {
+    super(props);
 
-  const { _ips: ips } = linodes;
-  // TODO: Wrapper for slaac address until the API gives back the full object
-  const slaac = { address: ips.ipv6.slaac };
-  const shareableIps = [...ips.ipv4.public, slaac, ...ips.ipv6.addresses];
-  const rows = shareableIps.map(ip => ({
-    address: ip.address,
-    linode: <Link to={`/linodes/${}`}
-  }));
+    this.state = {
+      checked: {},
+      errors: {},
+      saving: false,
+    };
 
-  return (
-    <Card title="IP Sharing">
-      <SecondaryTable
-        labels={['IP Address', 'Linode', '']}
-        keys={['address', 'linode', 'nav']}
-        rows={rows}
-      />
-    </Card>
-  );
+    this.componentWillUpdate = (setState) => (props) => {
+      const { linode } = props;
+      const checked = {};
+      for (const ip of linode._ips.ipv4.shared) {
+        checked[ip.address] = true;
+      }
+      (setState || this.setState)({ checked });
+    };
+    this.componentWillUpdate((state) => {
+      this.state = { ...this.state, ...state };
+    })(props);
+  }
+
+  onSubmit = async () => {
+    const { dispatch, linode } = this.props;
+    const { checked } = this.state;
+
+    this.setState({ errors: {}, saving: true });
+
+    try {
+      const sharedIps = Object.keys(_.pickBy(checked, checked => checked));
+      await dispatch(setShared(linode.id, sharedIps));
+    } catch (response) {
+      if (!response.json) {
+        // eslint-disable-next-line no-console
+        return console.error(response);
+      }
+
+      const errors = await reduceErrors(response);
+      this.setState({ errors });
+    }
+
+    this.setState({ saving: false });
+  }
+
+  onChange = ({ target: { name, value } }) => this.setState({ [name]: value })
+
+  formatRows() {
+    const { linodes, linode: thisLinode } = this.props;
+    const { checked } = this.state;
+
+    let rows = [];
+    for (const linode of Object.values(linodes.linodes)) {
+      if (linode.id === thisLinode.id) {
+        continue;
+      }
+
+      const shareableIps = linode._ips.ipv4.public;
+      rows = rows.concat(shareableIps.map(ip => ({
+        address: (
+          <div>
+            <Checkbox
+              onChange={() => this.setState({
+                checked: { ...checked, [ip.address]: !checked[ip.address] } })}
+              id={ip.address}
+              checked={checked[ip.address]}
+              value={checked[ip.address]}
+              label={`${ip.address}${ip.rdns ? ` (${ip.rdns})` : ''}`}
+            />
+          </div>
+        ),
+        linode: <Link to={`/linodes/${linode.label}`}>{linode.label}</Link>,
+      })));
+    }
+
+    return rows;
+  }
+
+  render() {
+    const { errors, saving } = this.state;
+
+    const rows = this.formatRows();
+
+    return (
+      <Card title="IP Sharing">
+        <p>
+          <small>
+            The selected IP addresses can be brought up by this Linode if the original Linode's
+            host becomes unavailable.
+          </small>
+        </p>
+        <Form onSubmit={this.onSubmit}>
+          <FormGroup>
+            <SecondaryTable
+              labels={['IP Address', 'Linode', '']}
+              keys={['address', 'linode', '']}
+              rows={rows}
+            />
+          </FormGroup>
+          <SubmitButton disabled={saving}>Save</SubmitButton>
+          <ErrorSummary errors={errors} />
+        </Form>
+      </Card>
+    );
+  }
 }
 
 IPSharing.propTypes = {
   linode: PropTypes.object.isRequired,
+  linodes: PropTypes.object.isRequired,
   dispatch: PropTypes.func.isRequired,
 };
--- src/linodes/linode/networking/layouts/IPManagementPage.js
+++ src/linodes/linode/networking/layouts/IPManagementPage.js
@@ -1,26 +1,52 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 
 import { IPTransfer, IPSharing } from '../components';
+import { setError } from '~/actions/errors';
+import { linodes } from '~/api';
+import { linodeIPs } from '~/api/linodes';
 
-export function IPManagementPage(props) {
-  const { linodes, dispatch, params: { linodeLabel } } = props;
-
-  const linode = Object.values(linodes.linodes).reduce(
+export class IPManagementPage extends Component {
+  // TODO: this needs to be replaced with fetching all ips and filtering by dc
+  static async preload({ dispatch, getState }, { linodeLabel }) {
+    const linode = Object.values(getState().api.linodes.linodes).reduce(
       (match, linode) => linode.label === linodeLabel ? linode : match);
 
-  return (
-    <div>
-      <IPTransfer
-        dispatch={dispatch}
-        linode={linode}
-      />
-      <IPSharing
-        dispatch={dispatch}
-        linode={linode}
-      />
-    </div>
-  );
+    try {
+      const allLinodes = await dispatch(linodes.all([], undefined, {
+        headers: {
+          'X-Filter': { datacenter: linode.datacenter },
+        },
+      }));
+
+      for (const linode of Object.values(allLinodes.linodes)) {
+        await dispatch(linodeIPs(linode.id));
+      }
+    } catch (e) {
+      dispatch(setError(e));
+    }
+  }
+
+  render() {
+    const { linodes, dispatch, params: { linodeLabel } } = this.props;
+
+    const linode = Object.values(linodes.linodes).reduce(
+      (match, linode) => linode.label === linodeLabel ? linode : match);
+
+    return (
+      <div>
+        <IPTransfer
+          dispatch={dispatch}
+          linode={linode}
+        />
+        <IPSharing
+          dispatch={dispatch}
+          linode={linode}
+          linodes={linodes}
+        />
+      </div>
+    );
+  }
 }
 
 
