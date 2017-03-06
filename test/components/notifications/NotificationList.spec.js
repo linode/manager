b2ffdf7e9fefae2de4469c56d1a86c37985ca9d3
Matthew Parke <mparke78@gmail.com>
2017-03-06 12:22:05 -0500

    Adds a few tests and updates others

--- test/components/notifications/NotificationList.spec.js
+++ test/components/notifications/NotificationList.spec.js
@@ -5,21 +5,61 @@ import sinon from 'sinon';
 
 import { NotificationList } from '~/components/notifications';
 
-import { api } from '@/data';
+import { api, fakeAPI } from '@/data';
+import { events } from '@/data/events';
 
-const events = api.events;
 
-
+// NOTE: item click handling untested because of <Link> usage
 describe('components/notifications/NotificationList', () => {
   const sandbox = sinon.sandbox.create();
+  let notificationList;
+
+  beforeEach(() => {
+    notificationList = mount(<NotificationList events={api.events} />);
+  });
 
   afterEach(() => {
     sandbox.restore();
   });
 
+  it('implements default state', () => {
+      expect(notificationList.instance().state).to.exist;
+      expect(notificationList.instance().state.totalResults).to.equal(4);
+  });
+
+  it('updates total results based on events results', () => {
+    let eventsAPI = fakeAPI([[events, 'event', 'events', 200]]);
+    notificationList = mount(
+      <NotificationList
+        events={eventsAPI.events}
+      />
+    );
+    expect(notificationList.instance().state.totalResults).to.equal(200);
+
+    eventsAPI = fakeAPI([[events, 'event', 'events', 250]]);
+    notificationList.instance().updateTotalResults(eventsAPI.events);
+
+    expect(notificationList.instance().state.totalResults).to.equal(250);
+  });
+
+  it('updates total results but also retains the max number of results seen', () => {
+    let eventsAPI = fakeAPI([[events, 'event', 'events', 200]]);
+    notificationList = mount(
+      <NotificationList
+        events={eventsAPI.events}
+      />
+    );
+    expect(notificationList.instance().state.totalResults).to.equal(200);
+
+    eventsAPI = fakeAPI([[events, 'event', 'events', 100]]); // less total
+    notificationList.instance().updateTotalResults(eventsAPI.events);
+
+    expect(notificationList.instance().state.totalResults).to.equal(200);
+  });
+
   it('renders events', () => {
-    const notifications = mount(<NotificationList events={events} />);
-    const count = Object.values(events.events).filter(e => !e.seen).length;
+    const notifications = mount(<NotificationList events={api.events} />);
+    const count = Object.values(api.events.events).filter(e => !e.seen).length;
 
     expect(notifications.find('NotificationListItem').length).to.equal(count);
 
@@ -27,10 +67,41 @@ describe('components/notifications/NotificationList', () => {
     expect(notifications.find('NotificationListItem').at(0).props().event.id).to.equal(385);
   });
 
+
+  it('renders a show more link when more results are available', () => {
+    const eventsAPI = fakeAPI([
+      [events, 'event', 'events', 200]
+    ]);
+    notificationList = mount(
+      <NotificationList
+        events={eventsAPI.events}
+      />
+    );
+
+    expect(notificationList.find('.ShowMoreLink').length).to.equal(1);
+  });
+
+  it('supports show more click handling', () => {
+    const clickShowMoreHandlerSpy = sandbox.spy();
+    const eventsAPI = fakeAPI([
+      [events, 'event', 'events', 200]
+    ]);
+    notificationList = mount(
+      <NotificationList
+        events={eventsAPI.events}
+        onClickShowMore={clickShowMoreHandlerSpy}
+      />
+    );
+
+    notificationList.find('.ShowMoreLink').first().simulate('click');
+
+    expect(clickShowMoreHandlerSpy.callCount).to.equal(1);
+  });
+
   it('should show a loading message when flag is set', () => {
     const notifications = mount(
       <NotificationList
-        events={events}
+        events={api.events}
         loading
       />
     );
--- test/components/notifications/Notifications.spec.js
+++ test/components/notifications/Notifications.spec.js
@@ -5,22 +5,87 @@ import sinon from 'sinon';
 
 import { Notifications } from '~/components/notifications/Notifications';
 
-import { api } from '@/data';
+import { api, fakeAPI } from '@/data';
+import { seenEvent } from '@/data/events';
 
 const events = api.events;
 
 describe('components/Notifications', function () {
   const sandbox = sinon.sandbox.create();
-  let dispatch = sandbox.spy();
+  let dispatch;
+  let notifications;
+
+  beforeEach(() => {
+    dispatch = sandbox.spy();
+    notifications = shallow(<Notifications dispatch={dispatch} events={events} />);
+  });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  it('disables polling on unmount', () => {
-    const page = shallow(<Notifications dispatch={dispatch} events={events} />);
+  it('defines default state', () => {
+    expect(notifications.instance().state).to.exist;
+    expect(notifications.instance().state.loadingMore).to.equal(false);
+    expect(notifications.instance()._pollingTimeoutId).to.be.null;
+  });
 
-    page.instance().componentWillUnmount();
-    expect(page.instance()._pollingTimeoutId).to.equal(null);
+  it('disables polling on unmount', () => {
+    notifications.instance().componentWillUnmount();
+    expect(notifications.instance()._pollingTimeoutId).to.equal(null);
+  });
+
+  it('implements fetching an events page', () => {
+    expect(notifications.instance().fetchEventsPage).to.exist;
+
+    notifications.instance().fetchEventsPage();
+
+    expect(dispatch.callCount).to.equal(1);
+  });
+
+  it('implements fetching all events', () => {
+    expect(notifications.instance().fetchAllEvents).to.exist;
+
+    notifications.instance().fetchAllEvents();
+
+    expect(dispatch.callCount).to.equal(1);
+  });
+
+  it('implements making events seen', () => {
+    const emptyEventsAPI = fakeAPI([
+      [{}, 'event', 'events']
+    ]);
+    notifications = shallow(<Notifications dispatch={dispatch} events={emptyEventsAPI.events} />);
+    expect(notifications.instance().markEventsSeen).to.exist;
+
+    notifications.instance().markEventsSeen();
+
+    expect(dispatch.callCount).to.equal(0);
+  });
+
+  it('only marks unseen events seen', () => {
+    const seenEventsAPI = fakeAPI([
+      [
+        {
+          [seenEvent.id]: seenEvent
+        },
+        'event',
+        'events'
+      ]
+    ]);
+    notifications = shallow(<Notifications dispatch={dispatch} events={seenEventsAPI.events} />);
+    expect(notifications.instance().markEventsSeen).to.exist;
+
+    notifications.instance().markEventsSeen();
+
+    expect(dispatch.callCount).to.equal(0);
+  });
+
+  it('marks events seen for all unseen events', () => {
+    expect(notifications.instance().markEventsSeen).to.exist;
+
+    notifications.instance().markEventsSeen();
+
+    expect(dispatch.callCount).to.equal(1);
   });
 });
