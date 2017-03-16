82bfa0bdb56bdf0728cf233b07149fe5237bfb7b
Phil Eaton <peaton@linode.com>
2017-03-16 11:56:47 -0400

    Use explicit static function for SelectExpiration map

--- src/profile/components/SelectExpiration.js
+++ src/profile/components/SelectExpiration.js
@@ -1,31 +1,34 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import moment from 'moment';
 
 import { Select } from '~/components/form';
 
-export default function SelectExpiration(props) {
-  return (
-    <Select
-      id={props.id}
-      onChange={props.onChange}
-      value={props.value}
-      name={props.name}
-    >
-      <option value="0">Never</option>
-      <option value="30">In 1 month</option>
-      <option value="90">In 3 months</option>
-      <option value="180">In 6 months</option>
-    </Select>
-  );
-}
+export default class SelectExpiration extends Component {
+  static map(value) {
+    return value === '0' ? undefined : (
+      moment()
+        .add(value, 'days')
+        .utc()
+        .format('YYYY-MM-DDTHH:mm:ss')
+        .toString());
+  }
 
-SelectExpiration.map = value =>
-  value === '0' ? undefined : (
-    moment()
-      .add(value, 'days')
-      .utc()
-      .format('YYYY-MM-DDTHH:mm:ss')
-      .toString());
+  render() {
+    return (
+      <Select
+        id={this.props.id}
+        onChange={this.props.onChange}
+        value={this.props.value}
+        name={this.props.name}
+      >
+        <option value="0">Never</option>
+        <option value="30">In 1 month</option>
+        <option value="90">In 3 months</option>
+        <option value="180">In 6 months</option>
+      </Select>
+    );
+  }
+}
 
 SelectExpiration.propTypes = {
   value: PropTypes.string.isRequired,
