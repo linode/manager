c22045033d78adc9ea3231643886403e3b4f43d1
Matthew Parke <mparke78@gmail.com>
2017-04-07 11:40:32 -0400

    Update tests to use expectObjectDeepEquals

--- test/actions/select.spec.js
+++ test/actions/select.spec.js
@@ -1,16 +1,18 @@
 import { expect } from 'chai';
+import { expectObjectDeepEquals } from '@/common';
+
 import { TOGGLE_SELECTED } from '~/actions/select';
 import toggleSelected from '~/actions/select';
 
 describe('actions/select', () => {
   describe('toggleSelected', () => {
     it('should return a TOGGLE_SELECTED action', () => {
-      expect(toggleSelected('example', '1234'))
-        .to.deep.equal({
-          type: TOGGLE_SELECTED,
-          objectType: 'example',
-          selectedIds: ['1234'],
-        });
+      const state = toggleSelected('example', '1234');
+      expectObjectDeepEquals(state, {
+        type: TOGGLE_SELECTED,
+        objectType: 'example',
+        selectedIds: ['1234'],
+      });
     });
   });
 });
--- test/reducers/select.spec.js
+++ test/reducers/select.spec.js
@@ -1,14 +1,17 @@
 import { expect } from 'chai';
 import deepFreeze from 'deep-freeze';
+import { expectObjectDeepEquals } from '@/common';
+
 import select from '~/reducers/select';
 import { TOGGLE_SELECTED } from '~/actions/select';
 import toggleSelected from '~/actions/select';
 
 
+
 describe('reducers/select reducer', () => {
   it('should handle initial state', () => {
     expect(select(undefined, {}))
-      .to.be.eql({ selected: { } });
+      .to.deep.equal({ selected: { } });
   });
 
   it('should no-op on arbitrary actions', () => {
@@ -20,22 +23,32 @@ describe('reducers/select reducer', () => {
   });
 
   it('should add to the selection on TOGGLE_SELECTED', () => {
-    const state = { selected: { example: {} } };
+    const state = {selected: {example: {}}};
     deepFreeze(state);
 
-    expect(select(state, toggleSelected('example', '1234')))
-      .to.have.property('selected')
-      .which.has.property('example')
-      .which.has.property('1234');
+    const newState = select(state, toggleSelected('example', '1234'));
+    expectObjectDeepEquals(newState, {
+      selected: {
+        example: {
+          '1234': true
+        }
+      }
+    });
 
-    expect(select(state, {
+
+    const newState = select(state, {
       type: TOGGLE_SELECTED,
       objectType: 'example',
       selectedIds: ['1234', '1235'],
-    }))
-      .to.have.property('selected')
-      .which.has.property('example')
-      .which.has.keys('1234', '1235');
+    });
+    expectObjectDeepEquals(newState, {
+      selected: {
+        example: {
+          1234: true,
+          1235: true,
+        }
+      }
+    });
   });
 
   it('should remove from the selection on TOGGLE_SELECTED', () => {
@@ -49,14 +62,22 @@ describe('reducers/select reducer', () => {
     };
     deepFreeze(state);
 
-    expect(select(state, toggleSelected('example', '1234')))
-      .to.have.property('selected')
-      .which.has.property('example')
-      .which./* does*/not.have.property('1234');
+    const newState = select(state, toggleSelected('example', '1234'));
+    expectObjectDeepEquals(newState, {
+      selected: {
+        example: {
+          1234: true
+        }
+      }
+    });
 
-    expect(select(state, toggleSelected('example', '1234')))
-      .to.have.property('selected')
-      .to.have.property('example')
-      .which.has.keys('1235');
+    const newState = select(state, toggleSelected('example', '1234'));
+    expectObjectDeepEquals(newState, {
+      selected: {
+        example: {
+          1234: true
+        }
+      }
+    });
   });
 });
