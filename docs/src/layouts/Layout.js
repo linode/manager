f8ea8fd7d6ae6f540545772ecaff5524c94cb9a1
Matthew Parke <mparke78@gmail.com>
2017-04-19 10:44:44 -0400

    Adds basic docs layout Header, Sidebar, Body

---
+++ docs/src/layouts/Layout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import { Header } from 'linode-components/navigation';
+
+
+export default function Layout(props) {
+  return (
+    <div className="Layout">
+      <Header></Header>
+      <div className="Layout-container container">
+        <div className="Layout-navigation-container">
+          <div className="VerticalNav">
+            <div className="VerticalNav-section">
+              <h3>Getting Started</h3>
+              <ul>
+                <li><Link to="/introduction">Introduction</Link></li>
+                <li><Link to="/authorization">Authorization</Link></li>
+                <li><Link to="/pagination">Pagination</Link></li>
+                <li><Link to="/filtering">Filtering</Link></li>
+              </ul>
+            </div>
+            <div className="VerticalNav-section">
+              <h3>Reference</h3>
+              <ul>
+                <li><Link to="/linodes">Linodes</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="Layout-content">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
