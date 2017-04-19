aad81e315577f23700d61832baec9183bf9537ef
Matthew Parke <mparke78@gmail.com>
2017-04-19 17:25:57 -0400

    docs shell (#1611)
    * docs shell
    
    * address feedback
    
    * Share 404 page
    
    * Fix test
    
    * typo
    
    * another test fix

---
+++ docs/src/docs/IndexPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function DocsIndexPage(props) {
+  return (
+    <div className="Docs">
+      Hello Docs
+    </div>
+  );
+};
--- docs/src/layouts/index.js
+++ docs/src/layouts/index.js
@@ -1,2 +1,1 @@
-export { default as Layout } from './Layout';
-export { default as IndexLayout } from './IndexLayout';
+export { default as MainLayout } from './MainLayout';
