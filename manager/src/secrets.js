d15c7f2a089b119f388a02135d40be2e8a48401b
Phil Eaton <peaton@linode.com>
2017-04-19 11:46:16 -0400

    Set up top-level directories

---
+++ manager/src/secrets.js
@@ -0,0 +1,5 @@
+export const clientId = '7992532c99d6f37e2fc1';
+export const clientSecret = '342e3c5d970ccdb3e87c87013ae658445e78566185eebd67eb13b3520c46a778';
+// export const clientId = '2d9864e65229e4632a40';
+// export const clientSecret = '969091d86cc3a83cabd45a689d76982c02c0953b2833164b4e0383faf0ae428e';
+export const LISH_ROOT = 'wss://lish.alpha.linode.com';
