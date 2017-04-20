24af0d74ae6344620bbea75f8d4e72b7a1dd940e
Matthew Parke <mparke78@gmail.com>
2017-04-20 17:17:14 -0400

    Docs layout (#1614)
    * Adds basic docs layout Header, Sidebar, Body
    
    * linting
    
    * SUIT css syntax correction

---
+++ docs/src/docs/introduction/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+import Introduction from './Introduction';
+
+
+export default (
+  <Route path="introduction" component={Introduction} />
+);
--- docs/src/layouts/index.js
+++ docs/src/layouts/index.js
@@ -1,1 +1,1 @@
-export { default as MainLayout } from './MainLayout';
+export { default as Layout } from './Layout';
