6f322325a928fe5c7edbd951701bd49e508d79d0
Matthew Parke <mparke78@gmail.com>
2017-04-27 13:52:02 -0400

    Cleans up response section, adds examples

---
+++ docs/src/components/Tables/Cells/DescriptionCell.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+
+export default function DescriptionCell(props) {
+  const { column, record } = props;
+
+  return (
+    <td className={`Table-cell DescriptionCell`}>
+      <div>
+        {record.description}
+      </div>
+    </td>
+  );
+}
