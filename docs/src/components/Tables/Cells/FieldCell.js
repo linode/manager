c58e93766bfe22612e275faea8c1c64b73f234ab
Matthew Parke <mparke78@gmail.com>
2017-05-02 12:58:29 -0400

    Prop types

--- docs/src/components/Tables/Cells/FieldCell.js
+++ docs/src/components/Tables/Cells/FieldCell.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 
-// { label: 'Type', dataKey: 'type', headerClassName: 'TypeColumn' },
 export default function FieldCell(props) {
-  const { column, record } = props;
+  const { record } = props;
 
   return (
     <td className={`Table-cell FieldCell`}>
-      <div>
-        {record.name}
-      </div>
-      {/*<div className="FieldCell-type">*/}
-        {/*<small><i>{record.type}</i></small>*/}
-      {/*</div>*/}
+      {record.name}
     </td>
   );
 }
+
+FieldCell.propTypes = {
+  record: PropTypes.shape({
+    name: PropTypes.string,
+  }),
+};
