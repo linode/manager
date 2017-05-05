f03f27974d35cd0c4a300d818aeb33c277043166
Phil Eaton <peaton@linode.com>
2017-05-05 11:30:06 -0400

    Drop unneeded env variables

--- docs/webpack.config.prod.js
+++ docs/webpack.config.prod.js
@@ -13,10 +13,6 @@ _.plugins = [
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
     },
-    'ENV_DEVTOOLS_DISABLED': JSON.stringify(process.env.DEVTOOLS_DISBLED),
-    'ENV_APP_ROOT': JSON.stringify(process.env.APP_ROOT),
-    'ENV_API_ROOT': JSON.stringify(process.env.API_ROOT),
-    'ENV_LOGIN_ROOT': JSON.stringify(process.env.LOGIN_ROOT),
     'ENV_GA_ID': JSON.stringify(process.env.GA_ID),
     'ENV_VERSION': JSON.stringify(_package.version)
   }),
