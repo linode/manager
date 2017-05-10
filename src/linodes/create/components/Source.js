0eb76866548cd8a936901db493e5c24beee6b9fe
Phil Eaton <philneaton95@gmail.com>
2017-05-10 14:52:54 -0400

    Backups success states (#1723)
    * Finished cancel and schedule form
    
    * Fix backups event text
    
    * Formify summary page
    
    * Fix linter
    
    * Fix backups first enabled page
    
    * Fix weekly / biweekly bug

--- src/linodes/create/components/Source.js
+++ src/linodes/create/components/Source.js
@@ -1,25 +1,22 @@
-import _ from 'lodash';
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 import { Card, CardHeader } from 'linode-components/cards';
 
 import Distributions from '~/linodes/components/Distributions';
 
 
-export default class Source extends Component {
-  render() {
-    const { distributions, distribution, onDistroSelected } = this.props;
+export default function Source(props) {
+  const { distributions, distribution, onDistroSelected } = props;
 
-    return (
-      <Card header={<CardHeader title="Source" />}>
-        <Distributions
-          distributions={distributions}
-          distribution={distribution}
-          onSelected={onDistroSelected}
-        />
-      </Card>
-    );
-  }
+  return (
+    <Card header={<CardHeader title="Source" />}>
+      <Distributions
+        distributions={distributions}
+        distribution={distribution}
+        onSelected={onDistroSelected}
+      />
+    </Card>
+  );
 }
 
 Source.propTypes = {
