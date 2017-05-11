571c9e13ba1ca96ba74649c796fab5942f1ca9cc
Phil Eaton <philneaton95@gmail.com>
2017-05-11 11:18:01 -0400

    Config form success states (#1695)
    * Partial work on edit config
    
    Add DiskSelect and get it working
    
    Lint all the things
    
    Allow bool or string for Radio value
    
    Drop camelify / snakify and more testing
    
    * Updated config defaults
    
    * Fix lint

--- src/linodes/linode/settings/layouts/AddConfigPage.js
+++ src/linodes/linode/settings/layouts/AddConfigPage.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { EditConfigPage, select } from './EditConfigPage';
+import { Card, CardHeader } from 'linode-components/cards';
 
-export function AddConfigPage(props) {
-  return <EditConfigPage create {...props} />;
+import { setSource } from '~/actions/source';
+import { CreateOrEditConfig } from '../components';
+import { selectLinode } from '../../utilities';
+
+
+export class AddConfigPage extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(setSource(__filename));
+  }
+
+  render() {
+    const { dispatch, kernels, linode, account } = this.props;
+    const header = <CardHeader title="Add a Config" />;
+
+    return (
+      <Card header={header}>
+        <CreateOrEditConfig
+          linode={linode}
+          kernels={kernels}
+          account={account}
+          disks={linode._disks.disks}
+          dispatch={dispatch}
+          submitText="Add Config"
+          submitDisabledText="Adding Config"
+        />
+      </Card>
+    );
+  }
 }
 
-AddConfigPage.preload = EditConfigPage.preload;
-
 AddConfigPage.propTypes = {
-  ...EditConfigPage.propTypes,
+  linode: PropTypes.object.isRequired,
+  disks: PropTypes.object.isRequired,
+  account: PropTypes.object.isRequired,
+  kernels: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
-delete AddConfigPage.propTypes.create;
+export function select(state, props) {
+  const { linode } = selectLinode(state, props);
+  const { kernels, account } = state.api;
+  return { linode, kernels, account };
+}
 
 export default connect(select)(AddConfigPage);
