cc75ca7a3ed3e198eb547cc4d537895e0a78bcc6
Matthew Parke <mparke78@gmail.com>
2017-05-17 14:29:33 -0400

    Working nested structures

--- docs/src/components/MethodResponse.js
+++ docs/src/components/MethodResponse.js
@@ -1,61 +1,75 @@
 import React from 'react';
 
 import { Table, TableRow } from 'linode-components/tables';
+import Example from './Example';
 import { SchemaTableBody } from './tables';
-import { DescriptionCell, ExampleCell, FieldCell } from './tables/Cells';
+import { DescriptionCell, FieldCell } from './tables/Cells';
 
+const columns = [
+  { cellComponent: FieldCell, label: 'Field', headerClassName: 'FieldColumn' },
+  { label: 'Type', dataKey: 'type', headerClassName: 'TypeColumn' },
+  { cellComponent: DescriptionCell, label: 'Description', headerClassName: 'DescriptionColumn' }
+];
+
+function renderNestedSchemaTable(record) {
+  return (
+    <div>
+      <div>{renderSchemaTable(record.schema)}</div>
+      <div><Example example={JSON.stringify(record.example, null, 2)} /></div>
+    </div>
+  );
+}
+
+function renderSchemaTable(schemaData) {
+  return (
+    <Table
+      renderRowsFn={function(columns, data, onToggleSelect, selectedMap) {
+        const rows = [];
+        data.forEach(function (record, index) {
+          if (record.schema && record.example) {
+            const nestedSchemaId = `${record.name}-${index}`;
+
+            rows.push(<TableRow
+              className="NestedSchemaParent"
+              dataTargetId={nestedSchemaId}
+              key={record.id || index}
+              index={index}
+              columns={columns}
+              record={record}
+            />);
+            rows.push(
+              <tr id={nestedSchemaId} className="NestedSchemaRow collapse">
+                <td className="NestedSchemaCell" colSpan={columns.length}>
+                  {renderNestedSchemaTable(record)}
+                </td>
+              </tr>
+            );
+          } else {
+            rows.push(<TableRow
+              key={record.id || index}
+              index={index}
+              columns={columns}
+              record={record}
+            />);
+          }
+        });
+
+        return rows;
+      }}
+      className="Table--secondary"
+      columns={columns}
+      data={schemaData}
+    />
+  )
+}
 
 export default function MethodResponse(props) {
   const { schema } = props;
 
   return (
-    <div className="Method-section Method-response">
+    <div className="Method-section MethodResponse">
       <h4><b>Response</b></h4>
-      <Table
-        renderRowsFn={function(columns, data, onToggleSelect, selectedMap) {
-          const rows = [];
-          data.forEach(function (record, index) {
-            if (record.schema && record.example) {
-              rows.push((<TableRow
-                key={record.id || index}
-                index={index}
-                columns={columns}
-                record={record}
-              />));
-              // TODO: Nested rows:
-              {/*record.schema.forEach(function(nestedRecord, index) {*/}
-                {/*rows.push((<TableRow*/}
-                  {/*key={nestedRecord.id || index}*/}
-                  {/*index={index}*/}
-                  {/*columns={columns}*/}
-                  {/*record={nestedRecord}*/}
-                {/*/>));*/}
-              {/*});*/}
-              {/*rows.push((*/}
-                {/*<tr>*/}
-                  {/*<ExampleCell example={record.example} colSpan={3} />*/}
-                {/*</tr>*/}
-              {/*));*/}
-            } else {
-              rows.push((<TableRow
-                key={record.id || index}
-                index={index}
-                columns={columns}
-                record={record}
-              />))
-            }
-          });
-
-          return rows;
-        }}
-        className="Table--secondary"
-        columns={[
-          { cellComponent: FieldCell, label: 'Field', headerClassName: 'FieldColumn' },
-          { label: 'Type', dataKey: 'type', headerClassName: 'TypeColumn' },
-          { cellComponent: DescriptionCell, label: 'Description', headerClassName: 'DescriptionColumn' }
-        ]}
-        data={schema}
-      />
+      {renderSchemaTable(schema)}
     </div>
   );
 }
--- docs/src/components/MethodResponseExample.js
+++ docs/src/components/MethodResponseExample.js
@@ -7,7 +7,7 @@ export default function MethodResponseExample(props) {
   const { resource } = props;
 
   return (
-    <div className="Method-section Method-responseExample">
+    <div className="Method-section MethodResponseExample">
       <h4><b>Example</b></h4>
       <Example example={JSON.stringify(resource.example, null, 2)} />
     </div>
