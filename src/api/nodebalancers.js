5e1a20e4cde582a15421d299b1c89fe5174abfc7
Nick <nidodson@gmail.com>
2017-05-18 15:46:08 -0400

    Node graph (#1805)
    * init commit
    
    * fix linter
    
    * fix linter
    
    * remove node graph range

--- src/api/nodebalancers.js
+++ src/api/nodebalancers.js
@@ -1,5 +1,15 @@
 import { thunkFetch } from './apiActionReducerGenerator';
+import { actions } from './configs/nodebalancers';
 
 export function updateConfigSSL(data, nodebalancerId, configId) {
   return thunkFetch.post(`/nodebalancers/${nodebalancerId}/configs/${configId}/ssl`, data);
+}
+
+export function nodebalancerStats(nodebalancerId) {
+  return async (dispatch) => {
+    const { data: _stats } = await dispatch(
+      thunkFetch.get(`/nodebalancers/${nodebalancerId}/stats`)
+    );
+    dispatch(actions.one({ _stats }, nodebalancerId));
+  };
 }
