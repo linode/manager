45b3a7fb25eded5f0ce22f9bef87640713d6c216
Phil Eaton <peaton@linode.com>
2017-05-18 17:03:45 -0400

    Render only first ipv4 address
    Fix tests
    
    Bump patch version
    
    Added changelog message

--- test/linodes/linode/layouts/DashboardPage.spec.js
+++ test/linodes/linode/layouts/DashboardPage.spec.js
@@ -24,7 +24,8 @@ describe('linodes/linode/layouts/DashboardPage', async () => {
       />);
 
     const ipSection = page.find('#ips');
-    expect(ipSection.find('li').at(0).text()).to.equal(ipv4);
+    console.log('THIS REALLY SHOULDNT PASS');
+    expect(ipSection.find('li').at(0).text()).to.equal(ipv4[0]);
     expect(ipSection.find('li').at(1).text()).to.equal(ipv6.split('/')[0]);
   });
 
