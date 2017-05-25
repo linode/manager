b0f1da211733e2e4540151851f0550bc0238c0e4
Abe Massry <amassry@linode.com>
2017-05-25 08:56:49 -0400

    add curl guide to docs

--- docs/src/components/guides/index.js
+++ docs/src/components/guides/index.js
@@ -1,2 +1,6 @@
 export { default as Python } from './python/Python';
 export { default as PythonIntroduction } from './python/Introduction';
+export { default as Curl } from './curl/Index';
+export { default as CreateLinode } from './curl/CreateLinode';
+export { default as TestingWithCurl } from './curl/TestingWithCurl';
+
