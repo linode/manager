d76b3658f6970d2453631d6bff44905a8d8e633b
Nick <nidodson@gmail.com>
2017-05-26 15:43:18 -0400

    fix lint erros on ref page

--- src/profile/layouts/ReferralsPage.js
+++ src/profile/layouts/ReferralsPage.js
@@ -3,11 +3,11 @@ import { connect } from 'react-redux';
 
 import { Card, CardHeader } from 'linode-components/cards';
 
-function ReferralsPage(props) {
+export function ReferralsPage(props) {
   const { code, url, total, completed, pending, credit } = props.referrals;
   const ref = `You have ${total} total referrals: ${completed} completed
     ($${(credit).toFixed(2)}) and ${pending} pending`;
-  const header =  <CardHeader title="Referrals" nav={ref} />
+  const header = <CardHeader title="Referrals" nav={ref} />;
 
   return (
     <Card header={header}>
@@ -17,10 +17,6 @@ function ReferralsPage(props) {
           signs up using your referral code, you'll receive a credit of
           $20.00, so long as the person you referred remains an active
           customer for 90 days.
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-12">
         </div>
       </div>
       <div className="row">
@@ -48,15 +44,15 @@ function ReferralsPage(props) {
   );
 }
 
+ReferralsPage.propTypes = {
+  referrals: PropTypes.object,
+};
+
 function select(state) {
   return {
     referrals: state.api.profile.referrals,
   };
 }
-
-ReferralsPage.propTypes = {
-  referrals: PropTypes.object,
-};
 
 export default connect(select)(ReferralsPage);
 
