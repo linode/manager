394e8f0bc1998b97f9b004d7f3eca2312eaf0691
Abe Massry <amassry@linode.com>
2017-06-01 11:19:43 -0400

    working on other python objects

--- docs/src/App.js
+++ docs/src/App.js
@@ -127,6 +127,9 @@ export function init() {
             {pythonClientObjectTitles.map(function(pythonObject, index) {
               return generateLibraryRoutes({ index: index, libraryObject: pythonObject, prevCrumbs: []});
             })}
+            {pythonAPITitles.map(function(pythonObject, index) {
+              return generateLibraryRoutes({ index: index, libraryObject: pythonObject, prevCrumbs: []});
+            })}
           </Route>
         </Route>
         <Route path="*" component={NotFound} />
