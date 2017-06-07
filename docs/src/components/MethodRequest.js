fd9a37829adbabf67b77872f57ec9f69a36d09d4
Abe Massry <amassry@linode.com>
2017-06-07 10:31:44 -0400

    move the change from the render to the prebuild, add changes to CHANGELOG.md

--- docs/src/components/MethodRequest.js
+++ docs/src/components/MethodRequest.js
@@ -3,16 +3,13 @@ import React from 'react';
 import { Tabs } from 'linode-components/tabs';
 import { default as Example } from './Example';
 
-import { API_ROOT, API_VERSION } from '~/constants';
-
 export default function MethodRequest(props) {
   const { examples } = props;
 
   const tabs = examples.map(function(example, index) {
-    const exampleValue = example.value.replace(/https:\/\/\$api_root/g, API_ROOT).replace(/\$version/g, API_VERSION);
     return {
       name: example.name,
-      children: (<Example key={`${example.name}-index`} example={exampleValue} />)
+      children: (<Example key={`${example.name}-index`} example={example.value} />)
     };
   });
 
