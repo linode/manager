2114aed64b6cb36704fd729412057d3d8faf025f
Abe Massry <amassry@linode.com>
2017-06-07 09:41:19 -0400

    fills out $api_root and $version for the constants in the render step

--- docs/src/components/MethodRequest.js
+++ docs/src/components/MethodRequest.js
@@ -3,14 +3,18 @@ import React from 'react';
 import { Tabs } from 'linode-components/tabs';
 import { default as Example } from './Example';
 
+import { API_ROOT, API_VERSION } from '~/constants';
+
 
 export default function MethodRequest(props) {
   const { examples } = props;
 
   const tabs = examples.map(function(example, index) {
+    console.log('example.value', example.value);
+    const exampleValue = example.value.replace(/https:\/\/\$api_root/g, API_ROOT).replace(/\$version/g, API_VERSION);
     return {
       name: example.name,
-      children: (<Example key={`${example.name}-index`} example={example.value} />)
+      children: (<Example key={`${example.name}-index`} example={exampleValue} />)
     };
   });
 
