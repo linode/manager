5ce90dbf440f58e1e566da1e08585ddb84b18a69
Phil Eaton <philneaton95@gmail.com>
2017-06-15 17:04:06 -0400

    Update backups (#2084)
    * Add overwite modal and clarify wording
    
    * Fix linter
    
    * Filter by region and add backup type
    
    * Fix linter
    
    * Fix tests

--- src/linodes/linode/backups/components/BackupRestore.js
+++ src/linodes/linode/backups/components/BackupRestore.js
@@ -3,7 +3,9 @@ import { push } from 'react-router-redux';
 
 import { Card, CardHeader } from 'linode-components/cards';
 import { Checkbox, Form, FormGroup, Select, SubmitButton } from 'linode-components/forms';
+import { ConfirmModalBody } from 'linode-components/modals';
 
+import { showModal, hideModal } from '~/actions/modal';
 import { restoreBackup } from '~/api/backups';
 import { dispatchOrStoreErrors, FormSummary } from '~/components/forms';
 
@@ -24,10 +26,26 @@ export default class BackupRestore extends Component {
     const { dispatch, linode, backup, linodes } = this.props;
     const { target, overwrite } = this.state;
 
-    return dispatch(dispatchOrStoreErrors.call(this, [
+    const toRestoreTo = linodes.linodes[target].label;
+
+    const callback = () => dispatch(dispatchOrStoreErrors.call(this, [
       () => restoreBackup(linode.id, target, backup.id, overwrite),
-      () => push(`/linodes/${linodes.linodes[target].label}`),
+      () => push(`/linodes/${toRestoreTo}`),
     ]));
+
+    return dispatch(showModal('Confirm Backup Restore', (
+      <ConfirmModalBody
+        onCancel={() => dispatch(hideModal())}
+        onOk={callback}
+      >
+        Are you sure you want to restore backups to <strong>{toRestoreTo}</strong>?
+        {!overwrite ? null : (
+          <span>
+            &nbsp;This will destroy all disks and configs on <strong>{toRestoreTo}</strong>.
+          </span>
+        )}
+      </ConfirmModalBody>
+    )));
   }
 
   onChange = ({ target: { name, value, checked } }) =>
@@ -41,14 +59,16 @@ export default class BackupRestore extends Component {
       return null;
     }
 
-    const otherLinodes =
-      Object.values(linodes.linodes).filter(l => l.id !== linode.id);
+    const otherLinodes = Object.values(linodes.linodes).filter(
+      l => l.id !== linode.id && l.region.id === linode.region.id);
 
     const restoreTo = otherLinodes.map(
       l => <option value={l.id} key={l.id}>{l.label}</option>);
 
     restoreTo.splice(0, 0,
       <option value={linode.id} key={linode.id}>This Linode</option>);
+
+    const targetLabel = target === linode.id ? 'This Linode' : linodes.linodes[target].label;
 
     return (
       <Card header={<CardHeader title="Restore" />}>
@@ -63,6 +83,11 @@ export default class BackupRestore extends Component {
                 name="target"
                 onChange={this.onChange}
               >{restoreTo}</Select>
+              <div>
+                <small className="text-muted">
+                  You can only restore to Linodes within the same region.
+                </small>
+              </div>
             </div>
           </FormGroup>
           <FormGroup className="row">
@@ -72,7 +97,7 @@ export default class BackupRestore extends Component {
                 name="overwrite"
                 value={overwrite}
                 checked={overwrite}
-                label="Destroy all disks and backups"
+                label={<span>Destroy all disks and configs on <strong>{targetLabel}</strong></span>}
                 onChange={this.onChange}
               />
             </div>
