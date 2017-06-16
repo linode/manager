167ab9e9e41aaea3cbac524fe96c792f99a4163a
Phil Eaton <philneaton95@gmail.com>
2017-06-16 15:16:34 -0400

    Don't crash on no nodebalancer ipv6 (#2095)

--- src/components/tables/cells/IPAddressCell.js
+++ src/components/tables/cells/IPAddressCell.js
@@ -12,10 +12,9 @@ export default function IPAddressCell(props) {
     ipv4 = ipv4[0];
   }
 
-  const _ipv6 = record.ipv6.split('/')[0];
   let ipv6 = null;
-  if (_ipv6 !== 'None') {
-    ipv6 = <div className="text-muted">{_ipv6}</div>;
+  if (record.ipv6 && record.ipv6 !== 'None/64') {
+    ipv6 = <div className="text-muted">{record.ipv6}</div>;
   }
 
   return (
