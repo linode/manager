c1944bb657a78adec22d5796af2b913f89786aef
Nick <nidodson@gmail.com>
2017-06-16 08:25:32 -0400

    fix SRV rendering (#2061)

--- src/domains/components/MasterZone.js
+++ src/domains/components/MasterZone.js
@@ -499,7 +499,7 @@ export class MasterZone extends Component {
             <Table
               className="Table--secondary"
               columns={[
-                { dataKey: 'service', label: 'Service' },
+                { dataKey: 'name', label: 'Service' },
                 { dataKey: 'priority', label: 'Priority' },
                 {
                   cellComponent: LabelCell,
