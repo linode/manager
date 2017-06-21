0d049c4499775015ff84ce1e9ef7d5111ffd0273
Phil Eaton <philneaton95@gmail.com>
2017-06-21 13:25:41 -0400

    Fix reset rdns wording and only display when relevant (#2140)
    * Fix reset rdns wording and only display when relevant
    
    * Fix for linter

--- src/linodes/linode/networking/layouts/SummaryPage.js
+++ src/linodes/linode/networking/layouts/SummaryPage.js
@@ -71,7 +71,11 @@ export class SummaryPage extends Component {
         name: 'Edit RDNS',
         action: () => EditRDNS.trigger(dispatch, record),
       });
-      elements.splice(2, 0, { name: 'Reset RDNS', action: () => this.resetRDNS(record) });
+
+      if (record.rdns) {
+        const name = record.version === 'ipv4' ? 'Reset RDNS' : 'Remove RDNS';
+        elements.splice(2, 0, { name, action: () => this.resetRDNS(record) });
+      }
     }
 
     return (
