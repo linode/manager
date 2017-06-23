7b2964a82441301b3a84d5ee0022342baa55d18f
Abe Massry <amassry@linode.com>
2017-06-23 16:22:41 -0400

    docs sidebar nav collapse

--- docs/src/layouts/Layout.js
+++ docs/src/layouts/Layout.js
@@ -11,8 +11,9 @@ import { API_VERSION } from '~/constants';
 export default class Layout extends Component {
   constructor() {
     super();
-
+    document.addEventListener('click', this.toggleVerticalNav.bind(this));
     this.renderNavListItems = this.renderNavListItems.bind(this);
+    this.state = { verticalNav: false };
   }
 
   renderNavListItems(items, path, childParentMap) {
@@ -22,14 +23,25 @@ export default class Layout extends Component {
           className={(item.href === path || item.href === childParentMap[path]) ? 'active': ''}
           key={index}
         >
-          <Link to={item.href}>{item.label}</Link>
+          <Link to={item.href} id={`NavLink-${index}`}>{item.label}</Link>
         </li>
       )
     });
   }
 
+  toggleVerticalNav(e) {
+    if (e.target.id === 'ToggleNavButton' || e.target.id === 'ToggleNavButtonIcon') {
+      this.setState({ verticalNav: !this.state.verticalNav });
+      console.log('toggle button clicked');
+    }
+    if (e.target.id.match(/NavLink-.*/)) {
+      this.setState({ verticalNav: false });
+    }
+  }
+
   render() {
     const { route } = this.props;
+    const { verticalNav } = this.state;
     const { childParentMap, endpoints } = route;
     const path = this.props.location.pathname;
 
@@ -39,6 +51,8 @@ export default class Layout extends Component {
       Click <a href="https://linode.com/api">here</a> for APIv3 documentation.
       </div>
     );
+
+    const verticalNavShow = verticalNav ? 'Layout-navigationContainer-Show' : 'Layout-navigationContainer-Hide';
 
     return (
       <div className="Docs Layout">
@@ -56,9 +70,12 @@ export default class Layout extends Component {
             </Link>
           </div>
           <span className="MainHeader-title">Developers</span>
+          <button className="ToggleNav navbar-toggler navbar-toggler-right collapsed" type="button" id="ToggleNavButton">
+            <i className="fa fa-bars" id="ToggleNavButtonIcon"></i>
+          </button>
         </Header>
         <div className="Layout-container container">
-          <div className="Layout-navigationContainer">
+          <div className={`Layout-navigationContainer ${verticalNavShow}`}>
             <div className="VerticalNav">
               <div className="VerticalNav-section">
                 <h3>Getting Started</h3>
