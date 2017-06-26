33cc157ca73cb814100ee8a84df834437c0c9ecb
Matthew Parke <mparke78@gmail.com>
2017-06-26 11:48:31 -0400

    setup sentry (#2149)
    * setup raven and sentry
    
    * fix for tests

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,6 +18,7 @@ _.plugins = [
     'ENV_API_ROOT': JSON.stringify(process.env.API_ROOT),
     'ENV_LOGIN_ROOT': JSON.stringify(process.env.LOGIN_ROOT),
     'ENV_GA_ID': JSON.stringify(process.env.GA_ID),
+    'ENV_SENTRY_URL': JSON.stringify(process.env.SENTRY_URL),
     'ENV_VERSION': JSON.stringify(_package.version)
   }),
   new webpack.optimize.UglifyJsPlugin({
