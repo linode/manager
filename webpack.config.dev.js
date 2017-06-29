4e3c40fe959d5be2f347d6d10131bf052d97b1f3
Steven Jacobs <sjacobs@linode.com>
2017-06-29 12:28:29 -0400

    replace deprecated webpack error emit plugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
