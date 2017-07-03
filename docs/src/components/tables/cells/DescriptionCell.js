a603e13787ad9734f7ce435b6551d632b748d4c8
Abe Massry <amassry@linode.com>
2017-07-03 15:08:42 -0400

    add see also link to docs endpoint description

--- docs/src/components/tables/cells/DescriptionCell.js
+++ docs/src/components/tables/cells/DescriptionCell.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router';
+import { API_VERSION, API_ROOT } from '~/constants';
 
 
 export default function DescriptionCell(props) {
   const { column, record } = props;
-  const { description } = record;
+  const { description, seeAlso } = record;
+  const seeAlsoDisplay = [];
+  if (Array.isArray(seeAlso)) {
+    seeAlso.forEach((seeAlsoItem) => {
+      seeAlsoDisplay.push((<div>See also: <Link to={`/${API_VERSION}${seeAlsoItem}`}>{seeAlsoItem}</Link></div>));
+    });
+  } else {
+    seeAlsoDisplay.push(!seeAlso ? null : (<div>See also: <Link to={`/${API_VERSION}${seeAlso}`}>{seeAlso}</Link></div>));
+  }
 
   let descriptionEl;
   if (typeof description === 'object') {
@@ -17,10 +27,16 @@ export default function DescriptionCell(props) {
             })}
           </ul>
         </div>
+        {seeAlsoDisplay.map( (item) => item )}
       </div>
     );
   } else {
-    descriptionEl = (<span>{description}</span>);
+    descriptionEl = (
+      <div>
+        <span>{description}</span>
+        {seeAlsoDisplay.map( (item) => item )}
+      </div>
+    );
   }
 
   return (
