ddbe8233ea0409da5b5aa820e290c7f4fce1f21c
Abe Massry <a@abemassry.com>
2017-07-07 15:58:10 -0400

    styleguide vertical nav changes (#2218)
    * styleguide vertical nav changes
    
    1. Turns the VerticalNav from docs into a component in linode-components.
    2. Changes the styleguide to use the VerticalNav component that was from
       docs and is now in linode-components.
    3. Moves the scss to the components section from docs
    4. Adds a Vertical Navigation section to the styleguide
    
    * answer pr feedback

--- components/navigation/VerticalNavSection.js
+++ components/navigation/VerticalNavSection.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router';
+
+
+function renderNavListItems(navItems, path, checkActiveItem) {
+  return navItems.map((item, index) => {
+    let nameOfClass;
+    if (checkActiveItem) {
+      nameOfClass = checkActiveItem(path, item.href) ? 'active': '';
+    } else {
+      nameOfClass = item.href === path ? 'active': '';
+    }
+    return (
+      <li
+        key={index}
+        className={nameOfClass}
+      >
+        <Link to={item.href} id={`NavLink-${index}`}>{item.label}</Link>
+      </li>
+    )
+  });
+}
 
 
 export default function VerticalNavSection(props) {
-  const { navItems, title } = props;
+  const { navItems, title, path, checkActiveItem } = props;
 
   return (
     <div className="VerticalNav-section">
       <h3>{title}</h3>
       <ul>
-        {navItems.map(function(item) {
-          return (
-            <li><link to=""></link></li>
-          );
-        })}
+        {renderNavListItems(navItems, path, checkActiveItem)}
       </ul>
     </div>
   );
