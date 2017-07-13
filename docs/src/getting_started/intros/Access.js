a69e50786d1c7fefe06fa9d8c5ef83d7f3d51622
Matthew Parke <mparke78@gmail.com>
2017-07-13 10:12:43 -0400

    getting started access

--- docs/src/getting_started/intros/Access.js
+++ docs/src/getting_started/intros/Access.js
@@ -12,46 +12,55 @@ export default function Authentication() {
       <h1>Authentication</h1>
       <section>
         <p>
-          To use some API endpoints, authentication is required. We use the basic OAuth workflow where you can create
-          &nbsp;<em>applications</em> that integrate with Linode by registering those applications with us.
-          You then use OAuth to authenticate on behalf of the user to request access to resources from their account.
+          To use some API endpoints, authentication is required. We use the basic OAuth workflow
+          where you can create &nbsp;<em>applications</em> that integrate with Linode by
+          registering those applications with us. You then use OAuth to authenticate on behalf
+          of the user to request access to resources from their account.
         </p>
       </section>
       <section>
         <h2>The Access Code</h2>
         <p>
-          In the OAuth workflow it is a two step process to authenticate a user before you can start making API calls.
-          You will first need to request an <strong>access code</strong> that can then be exchanged for an
-          &nbsp;<strong>authorization token</strong>. To aid as many application developers as possible with their
-          design we provide two methods for requesting an access code.
+          In the OAuth workflow it is a two step process to authenticate a user before you can
+          start making API calls. You will first need to request an <strong>access code</strong>
+          that can then be exchanged for an &nbsp;<strong>authorization token</strong>.
+          To aid as many application developers as possible with their design we provide two
+          methods for requesting an access code.
         </p>
       </section>
       <section>
         <h2>Through The Web</h2>
         <p>
-          If you are making a web based application this is the recommended method for getting a user's authorization.
-          When you want a user to log into your service, you can direct them to a URL similar to this:
+          If you are making a web based application this is the recommended method for getting
+          a user's authorization. When you want a user to log into your service, you can
+          direct them to a URL similar to this:
         </p>
-        <Example example={`${LOGIN_ROOT}/oauth/authorize?client_id=client_id`} name="bash" noclipboard />
+        <Example
+          example={`${LOGIN_ROOT}/oauth/authorize?client_id=client_id`}
+          name="bash"
+          noclipboard
+        />
         <p>
-          The user logs in to Linode and is presented the scope levels your application is requesting.
-          Once the user accepts your request for access, we redirect them back to you with an <em>access code</em>.
-          You may then exchange the access code for an <em>authorization token</em>.
+          The user logs in to Linode and is presented the scope levels your application is
+          requesting. Once the user accepts your request for access, we redirect them back to
+          you with an <em>access code</em>. You may then exchange the access code for an
+          <em>authorization token</em>.
         </p>
         <p>
-          There are additional parameters that you can supply in the query string of the above request.
-          If you provided a redirect_uri when you sent the user to {LOGIN_ROOT},
-          they will be redirected to that URI. Otherwise, the default redirect URI associated with your
-          OAuth client application will be used.
+          There are additional parameters that you can supply in the query string of the
+          above request. If you provided a redirect_uri when you sent the user to {LOGIN_ROOT},
+          they will be redirected to that URI. Otherwise, the default redirect URI associated
+          with your OAuth client application will be used.
         </p>
         <p>
           When the user is redirected, two parameters will be added to the query string:
           <code>code</code> and <code>state</code>.
-          The last parameter will match the state you gave us at the start of the flow. This allows you to ensure
-          that the OAuth flow was initiated by your application,
+          The last parameter will match the state you gave us at the start of the flow.
+          This allows you to ensure that the OAuth flow was initiated by your application,
           rather than by someone manually navigating to {LOGIN_ROOT} with
           your <code>client_id</code>.
-          The important parameter is the code, this is the <em>access code</em> you need to continue the OAuth flow.
+          The important parameter is the code, this is the <em>access code</em>
+          you need to continue the OAuth flow.
         </p>
         <p>
           You can include the following parameters in the query string when
@@ -74,14 +83,15 @@ export default function Authentication() {
             },
             {
               parameter: 'redirect_uri',
-              description: 'Where to redirect the user after login. If provided, the redirect URI you ' +
-              'registered your application with should match the start of this string. If you registered ' +
-              ' your app with http://example.org as the redirect URI, then this parameter must start with ' +
-              '"http://example.org".',
+              description: 'Where to redirect the user after login. If provided, the redirect ' +
+              'URI you registered your application with should match the start of this string. ' +
+              'If you registered your app with http://example.org as the redirect URI, ' +
+              'then this parameter must start with "http://example.org".',
             },
             {
               parameter: 'state',
-              description: 'An arbitrary token that will be returned to you at the end of the process.',
+              description: 'An arbitrary token that will be returned to you at the end of ' +
+              'the process.',
             },
           ]}
         />
@@ -115,6 +125,7 @@ export default function Authentication() {
           You'll get a response like this:
         </p>
         <Example
+          // eslint-disable-next-line max-len
           example={`{
   "scopes": "linodes:create",
   "access_token": "03d084436a6c91fbafd5c4b20c82e5056a2e9ce1635920c30dc8d81dc7a6665c"
@@ -132,6 +143,7 @@ export default function Authentication() {
           this header to each request:
         </p>
         <Example
+          // eslint-disable-next-line max-len
           example="Authorization: token 03d084436a6c91fbafd5c4b20c82e5056a2e9ce1635920c30dc8d81dc7a6665c"
           name="bash"
           noclipboard
@@ -163,8 +175,8 @@ export default function Authentication() {
         </ul>
         <p>
           In addition to the level of access you request,
-          you will be granted each access level below it. For example, requesting <em>modify</em> access
-          will also grant you <em>view</em> access, and requesting <em>delete</em> access
+          you will be granted each access level below it. For example, requesting <em>modify</em>
+          access will also grant you <em>view</em> access, and requesting <em>delete</em> access
           will grant you <em>full</em> access to that resource (<code>resource:*</code> has
           the same effect).
         </p>
