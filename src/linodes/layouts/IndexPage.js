d8acaf37db02c899bf42d55104d191596b114114
Phil Eaton <peaton@linode.com>
2017-07-14 10:34:58 -0400

    Fix linter

--- src/linodes/layouts/IndexPage.js
+++ src/linodes/layouts/IndexPage.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-import moment from 'moment';
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
