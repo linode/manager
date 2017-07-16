1113ecc53f9a986df11fcfcaf5a0509e9594b120
Phil Eaton <peaton@linode.com>
2017-07-16 12:39:39 -0400

    Fix linter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import { LOGOUT } from '~/actions/authentication';
 import authentication from './authentication';
 import modal from './modal';
 import notifications from './notifications';
