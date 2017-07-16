0d9fa2f013ab1e9aedd1e55883cd8831d8e8853a
Phil Eaton <peaton@linode.com>
2017-07-16 12:05:26 -0400

    Fixes for font-awesome

--- bin/copy-assets.sh
+++ bin/copy-assets.sh
@@ -4,5 +4,9 @@ set -eux
 
 mkdir -p assets/weblish
 cp node_modules/xterm/dist/{xterm.js,xterm.css,xterm.js.map} assets/weblish
+
 cp node_modules/zxcvbn/dist/{zxcvbn.js,zxcvbn.js.map} assets/
-cp node_modules/font-awesome/css/font-awesome.min.css assets
+
+mkdir -p assets/font-awesome/css
+cp node_modules/font-awesome/css/font-awesome.min.css assets/font-awesome/css
+cp -r node_modules/font-awesome/fonts assets/font-awesome/
