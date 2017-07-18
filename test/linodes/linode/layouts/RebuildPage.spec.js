658991296c719a565bfd3023ddad759f51e582aa
Phil Eaton <philneaton95@gmail.com>
2017-07-18 10:10:07 -0400

    Add rebuild confirm check (#2302)

--- test/linodes/linode/layouts/RebuildPage.spec.js
+++ test/linodes/linode/layouts/RebuildPage.spec.js
@@ -36,6 +36,12 @@ describe('linodes/linode/layouts/RebuildPage', () => {
     await page.find('Form').props().onSubmit();
 
     expect(dispatch.callCount).to.equal(1);
+    const modal = mount(dispatch.firstCall.args[0].body);
+
+    dispatch.reset();
+    await modal.props().onOk();
+
+    expect(dispatch.callCount).to.equal(2);
     await expectDispatchOrStoreErrors(dispatch.firstCall.args[0], [
       ([fn]) => expectRequest(fn, '/linode/instances/1234/rebuild', {
         method: 'POST',
