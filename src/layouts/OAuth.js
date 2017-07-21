552542b95a831b556ca01f8040913b51f4dd18dd
Abe Massry <amassry@linode.com>
2017-07-21 17:48:04 -0400

    handle oauth token error

--- src/layouts/OAuth.js
+++ src/layouts/OAuth.js
@@ -2,7 +2,7 @@ import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
-import { LOGIN_ROOT } from '~/constants';
+import { LOGIN_ROOT, ENVIRONMENT } from '~/constants';
 import { rawFetch } from '~/fetch';
 import { clientId, clientSecret } from '~/secrets';
 import * as session from '~/session';
@@ -30,11 +30,24 @@ export class OAuthCallbackPage extends Component {
       data.append('code', code);
 
       // Exchange temporary code for access token.
-      const resp = await rawFetch(`${LOGIN_ROOT}/oauth/token`, {
-        method: 'POST',
-        body: data,
-        mode: 'cors',
-      });
+      let resp;
+      try {
+        resp = await rawFetch(`${LOGIN_ROOT}/oauth/token`, {
+          method: 'POST',
+          body: data,
+          mode: 'cors',
+        });
+      } catch (e) {
+        if (ENVIRONMENT === 'development') {
+          /* eslint-disable no-alert */
+          alert(e);
+          /* eslint-enable no-alert */
+        } else {
+          /* eslint-disable no-console */
+          console.log(e);
+          /* eslint-enable no-console */
+        }
+      }
       const { access_token, scopes } = await resp.json();
 
       // Token needs to be in redux state for all API calls
