6e587ddd8ed7e15e571613c22010068eeb386792
Phil Eaton <peaton@linode.com>
2017-09-12 09:55:06 -0400

    Fixes for linter

--- docs/prebuild.js
+++ docs/prebuild.js
@@ -414,7 +414,7 @@ allEndpoints = Object.keys(endpointMap).map(function (key) {
   const endpointIndex = endpointMap[key];
 
   // alphabetically sort groups and endpoints in groups
-    return _.assign(endpointIndex, {
+  return _.assign(endpointIndex, {
     groups: _.sortBy(endpointIndex.groups, g => g.label.toLowerCase()).map(
       group => _.assign(group, {
         endpoints: _.sortBy(group.endpoints, 'path'),
