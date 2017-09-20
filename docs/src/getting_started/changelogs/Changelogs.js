f3c664104fd73609bdc57c87bc350d6fbaded4f5
Will Smith <wsmith@linode.com>
2017-09-20 09:15:12 -0400

    Addressed linter feedback

--- docs/src/getting_started/changelogs/Changelogs.js
+++ docs/src/getting_started/changelogs/Changelogs.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import { Table } from 'linode-components/tables';
-import { Code } from 'linode-components/formats';
-
-import { LOGIN_ROOT, API_VERSION } from '~/constants';
-
+import { API_VERSION } from '~/constants';
 
 export default function Authentication() {
   return (
@@ -21,73 +17,89 @@ export default function Authentication() {
       </section>
       <section>
         <h2>2017-09-18</h2>
-        <hr/><br/>
-        <b>Breaking Changes:</b><br/>
+        <hr /><br />
+        <b>Breaking Changes:</b><br />
         <ul>
-          <li>Pagination envelope has changed</li>
+          <li>Pagination envelope has changed
             <ul>
               <li> total_pages => pages</li>
               <li> total_results => results</li>
               <li> endpoint-specific key is now always "data"</li>
             </ul>
-          <li>Region, Distribution, Type, and Kernel objects are now returned as slugs</li>
+          </li>
+          <li>Region, Distribution, Type, and Kernel objects are now returned as slugs
             <ul>
               <li> Previously, entire object was returned as part of other responses</li>
             </ul>
-          <li>POST linode/instances and POST linode/rebuild automatically issue a boot job</li>
+          </li>
+          <li>POST linode/instances and POST linode/rebuild automatically issue a boot job
             <ul>
               <li> This behavior can be suppressed by sending "boot": false in the request</li>
             </ul>
-          <li>Changed POST linode/instances</li>
+          </li>
+          <li>Changed POST linode/instances
             <ul>
               <li> with_backups => backups_enabled</li>
               <li> Now accepts "booted" - defaults to true if distribution is provided</li>
             </ul>
-          <li>Changed POST  linode/instances/:id/clone</li>
+          </li>
+          <li>Changed POST  linode/instances/:id/clone
             <ul>
               <li> with_backups => backups_enabled</li>
             </ul>
-          <li>Changed POST linode/instances/:id/rebuild</li>
+          </li>
+          <li>Changed POST linode/instances/:id/rebuild
             <ul>
               <li> Now accepts "booted" - defaults to true</li>
             </ul>
-          <li>Changed LinodeNetworkingResponse</li>
+          </li>
+          <li>Changed LinodeNetworkingResponse
             <ul>
               <li> region is now a slug instead of a nested object</li>
             </ul>
-          <li>Changed IPv6 object</li>
+          </li>
+          <li>Changed IPv6 object
             <ul>
               <li> region is now a slug instead of a nested object</li>
             </ul>
-          <li>Changed Invoice object</li>
+          </li>
+          <li>Changed Invoice object
             <ul>
               <li> Removed "paid"</li>
               <li> Removed "overdue"</li>
             </ul>
-          <li>Changed Region object</li>
+          </li>
+          <li>Changed Region object
             <ul>
               <li> Removed "label"</li>
             </ul>
-          <li>Changed Backup object</li>
+          </li>
+          <li>Changed Backup object
             <ul>
               <li> regions is now a slug instead of a nested object</li>
             </ul>
-          <li>Changed Distribution object</li>
+          </li>
+          <li>Changed Distribution object
             <ul>
               <li> Removed "created"</li>
               <li> Added "updated"</li>
               <li> minimum_storage_size => disk_minimum</li>
-              <li> x64 => architecture.  architecture is an enum returning either "x86_64" or "i386"</li>
+              <li> x64 => architecture.  architecture is an enum returning either
+              "x86_64" or "i386"</li>
             </ul>
-          <li>Changed IPAddress object</li>
+          </li>
+          <li>Changed IPAddress object
             <ul>
               <li> region is now a slug instead of a nested object</li>
             </ul>
-          <li>Changed Kernel object</li>
+          </li>
+          <li>Changed Kernel object
             <ul>
-              <li> x64 => architecture.  architecture is an enum returning either "x86_64" or "i386"</li>
+              <li> x64 => architecture.  architecture is an enum returning either
+              "x86_64" or "i386"</li>
             </ul>
-          <li>Changed Linode object</li>
+          </li>
+          <li>Changed Linode object
             <ul>
               <li> storage => disk</li>
               <li> total_transfer => transfer_total</li>
@@ -96,9 +108,10 @@ export default function Authentication() {
               <li> nested alert objects have been streamlined</li>
               <li>"enabled" and "threshold" have been removed</li>
               <li>a value of 0 now represents "disabled", any other value is "enabled" with
-                  that threshold</li>
+              that threshold</li>
             </ul>
-          <li>Changed LinodeConfig object</li>
+          </li>
+          <li>Changed LinodeConfig object
             <ul>
               <li> disable_updatedb => updatedb_disabled</li>
               <li> enable_distro_helper => distro</li>
@@ -107,11 +120,13 @@ export default function Authentication() {
               <li> ram_limit => memory_limit</li>
               <li> devtmpfs_autocommit moved into "helpers" envelope</li>
             </ul>
-          <li>Changed Nodebalancer object</li>
+          </li>
+          <li>Changed Nodebalancer object
             <ul>
               <li> region is now a slug instead of a nested object</li>
             </ul>
-          <li>Changed Type object</li>
+          </li>
+          <li>Changed Type object
             <ul>
               <li> hourly_price => price_hourly</li>
               <li> monthly_price => price_monthly</li>
@@ -119,9 +134,10 @@ export default function Authentication() {
               <li> storage => disk</li>
               <li> mbits_out => network_out</li>
               <li> backups_price is now a nested object containing "price_hourly" and
-                  "price_monthly"</li>
+              "price_monthly"</li>
             </ul>
-          <li>Changed StackScript object</li>
+          </li>
+          <li>Changed StackScript object
             <ul>
               <li> Removed "customer_id"</li>
               <li> distributions is now a list of slugs instead of a list of nested objects</li>
@@ -129,38 +145,44 @@ export default function Authentication() {
               <li> Added "username"</li>
               <li> Added "user_gravatar_id"</li>
             </ul>
-          <li>Changed Volume object</li>
+          </li>
+          <li>Changed Volume object
             <ul>
               <li> "status" can no longer contain "contact_support" - will return "offline"
-                  in that case</li>
+              in that case</li>
               <li> region is now a slug instead of a nested object</li>
             </ul>
-          <li>Changes SupportTicket</li>
+          </li>
+          <li>Changes SupportTicket
             <ul>
               <li> Removed "closed_by"</li>
             </ul>
+          </li>
           <li>IP Whitelist may not be enabled in PUT profile if it is already disabled</li>
         </ul>
-        <b>Changes:</b><br/>
+        <b>Changes:</b><br />
         <ul>
-          <li>Default page size increased to 100</li>
+          <li>Default page size increased to 100
             <ul>
               <li>Any page size between 25 and 100 may be requested in the url with ?page_size=</li>
             </ul>
+          </li>
           <li>Linode configs now accept deprecated kernels</li>
           <li>Linode configs now default kernel to latest, no longer required on POST</li>
-          <li>Added /profile/whitelist</li>
+          <li>Added /profile/whitelist
             <ul>
               <li>GET - list all IPs on user's whitelist</li>
               <li>POST - add IP to user's whitelist</li>
               <li>Endpoint return a 400 if IP Whitelist is disabled</li>
             </ul>
-          <li>Added /profile/whitelist/:id</li>
+          </li>
+          <li>Added /profile/whitelist/:id
             <ul>
               <li>GET - return one entry on whitelist</li>
               <li>DELETE - remove address from whitelist</li>
               <li>Endpoints return a 400 if IP Whitelist if disabled</li>
             </ul>
+          </li>
           <li>Disk filesystems now default to ext4, no longer required on POST</li>
         </ul>
       </section>
