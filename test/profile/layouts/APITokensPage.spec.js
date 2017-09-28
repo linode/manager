54fec3f0719f2b408cfb822a14d6d02241c3156d
Nick Dodson <nidodson@gmail.com>
2017-09-28 10:35:04 -0400

    my profile release 0.14.0 fixes
    Closes #2572

--- test/profile/layouts/APITokensPage.spec.js
+++ test/profile/layouts/APITokensPage.spec.js
@@ -3,9 +3,7 @@ import { mount } from 'enzyme';
 import React from 'react';
 import sinon from 'sinon';
 
-import { API_ROOT } from '~/constants';
 import { APITokensPage } from '~/profile/layouts/APITokensPage';
-
 import { expectRequest } from '@/common.js';
 import { api } from '@/data';
 
@@ -33,11 +31,9 @@ describe('profile/layouts/APITokensPage', () => {
     const token = page.find('.TableRow');
     expect(token.length).to.equal(Object.keys(tokens.tokens).length);
     const firstToken = token.at(1);
-    expect(firstToken.find('td img').props().src)
-      .to.equal(`${API_ROOT}/account/clients/d64b169cc95fde4e367g/thumbnail`);
-    expect(firstToken.find('td').at(2).text())
+    expect(firstToken.find('td').at(1).text())
       .to.equal('Test client');
-    expect(firstToken.find('td').at(3).text())
+    expect(firstToken.find('td').at(2).text())
       .to.equal('OAuth Client Token');
   });
 
