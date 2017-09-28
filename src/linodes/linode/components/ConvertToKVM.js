f734c6134e48566a7d0364c3ac33ece2ecb2e83f
Nick Dodson <nidodson@gmail.com>
2017-09-28 17:54:57 -0400

    kvmify add text and est

--- src/linodes/linode/components/ConvertToKVM.js
+++ src/linodes/linode/components/ConvertToKVM.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
+import { ExternalLink } from 'linode-components/buttons';
 import { FormModalBody } from 'linode-components/modals';
 import { showModal, hideModal } from '~/actions/modal';
 import { kvmifyLinode } from '~/api/linodes';
@@ -26,6 +27,7 @@ export default class ConvertToKVM extends Component {
   render() {
     const { dispatch, linode } = this.props;
     const { errors } = this.state;
+    const migrateEst = Math.round(linode.disk / (4000 / 60) / 60);
 
     return (
       <FormModalBody
@@ -39,7 +41,17 @@ export default class ConvertToKVM extends Component {
       >
         <div>
           <p>
+            Linode will need to shut down and migrate to KVM before receiving plan upgrade.
+            Specific changes from Xen to KVM will be detailed in
+            our <ExternalLink to="https://www.linode.com/docs/platform/kvm">
+              KVM Reference guide
+            </ExternalLink>.
+          </p>
+          <p>
             Are you sure you want to <strong>permanently</strong> convert {linode.label} to KVM?
+          </p>
+          <p>
+            To migrate {linode.disk} MB of disks will take about {migrateEst} minutes to complete.
           </p>
         </div>
       </FormModalBody>
