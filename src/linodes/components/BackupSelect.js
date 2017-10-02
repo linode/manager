888a8226c6a01af4abf1fbd734b7eeadba887578
Nick <nidodson@gmail.com>
2017-10-02 09:48:22 -0400

    dont display backup ids in select (#2564)
    * dont display backup ids in select
    
    * clean up backup select id to time pr

--- src/linodes/components/BackupSelect.js
+++ src/linodes/components/BackupSelect.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 
+import moment from 'moment-timezone';
 import { Select } from 'linode-components/forms';
 
 
@@ -10,18 +11,27 @@ export default function BackupSelect(props) {
   const options = [{ label: initialLabel, value: 0 }];
 
   if (daily) {
-    options[0] = { label: 'Daily', options: [{ label: daily.id, value: daily.id }] };
+    options[0] = { label: 'Daily', options: [{
+      label: moment.utc(daily.created).fromNow(),
+      value: daily.id,
+    }] };
   }
 
   if (weekly) {
-    options.push({ label: 'Weekly', options: weekly.map(w => ({ label: w.id, value: w.id })) });
+    options.push({ label: 'Weekly', options: weekly.map(w => ({
+      label: moment.utc(w.created).fromNow(),
+      value: w.id,
+    })) });
   }
 
   if (snapshot && snapshot.current) {
     const { current } = snapshot;
     const option = {
       label: 'Snapshot',
-      options: [{ label: current.label || current.id, value: current.id }],
+      options: [{
+        label: current.label || moment.utc(current.created).fromNow(),
+        value: current.id,
+      }],
     };
 
     if (!daily) {
