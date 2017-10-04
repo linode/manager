b513fbfce593b789bcf638a2dbfd503825eb250e
Nick Dodson <nidodson@gmail.com>
2017-10-04 10:14:09 -0400

    update kvm upgrade modal

--- src/linodes/linode/components/UpgradeToKVM.js
+++ src/linodes/linode/components/UpgradeToKVM.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
 import { ExternalLink } from 'linode-components/buttons';
-import { FormModalBody } from 'linode-components/modals';
+import { ConfirmModalBody } from 'linode-components/modals';
 import { showModal, hideModal } from '~/actions/modal';
 import { kvmifyLinode } from '~/api/linodes';
 import { dispatchOrStoreErrors } from '~/api/util';
@@ -27,34 +27,32 @@ export default class UpgradeToKVM extends Component {
   render() {
     const { dispatch, linode } = this.props;
     const { errors } = this.state;
-    const migrateEst = Math.round(linode.disk / (4000 / 60) / 60);
-
+    const migrateEst = Math.round(linode.specs.disk / (4000 / 60) / 60);
+    console.log(linode);
     return (
-      <FormModalBody
+      <ConfirmModalBody
         onSubmit={() => dispatch(dispatchOrStoreErrors.call(this, [
           () => kvmifyLinode(linode.id),
           hideModal,
         ]))}
+        buttonText="Confirm"
         onCancel={() => dispatch(hideModal())}
         analytics={{ title: UpgradeToKVM.title }}
         errors={errors}
       >
         <div>
           <p>
-            Linode will need to shut down and migrate to KVM before receiving plan upgrade.
+            <strong>{linode.label}</strong> will need to shut down and migrate to KVM before receiving plan upgrade.
             Specific changes from Xen to KVM are detailed in
             our <ExternalLink to="https://www.linode.com/docs/platform/kvm">KVM Reference guide</ExternalLink>.
           </p>
           <p>
-            Are you sure you want to <strong>permanently</strong> convert {linode.label} to KVM?
-          </p>
-          <p>
             {/* eslint-disable max-len */}
-            To migrate {(linode.disk / 1024)} GiB worth of disks will take about {migrateEst} minutes to complete.
+            To migrate {(linode.specs.disk / 1024)} GiB worth of disks will take about {migrateEst} minutes to complete.
             {/* eslint-enable max-len */}
           </p>
         </div>
-      </FormModalBody>
+      </ConfirmModalBody>
     );
   }
 }
