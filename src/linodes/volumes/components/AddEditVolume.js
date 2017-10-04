c206ef575fbfd6dd521b7fbd84db5af0916ea400
Nick Dodson <nidodson@gmail.com>
2017-10-04 08:08:45 -0400

    fix lint error for vol config select

--- src/linodes/volumes/components/AddEditVolume.js
+++ src/linodes/volumes/components/AddEditVolume.js
@@ -55,7 +55,7 @@ export default class AddEditVolume extends Component {
       linode_id: linode === LinodeSelect.EMPTY ? undefined : linode,
       config_id: config,
     };
-    
+
     if (!data.linode_id) {
       delete data.linode_id;
     }
