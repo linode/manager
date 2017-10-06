76a5b7c8dcb68d6feaa9a1336d482b5140d1d245
Phil Eaton <peaton@linode.com>
2017-10-06 15:32:05 -0400

    Formatting

--- src/api/ad-hoc/account.js
+++ src/api/ad-hoc/account.js
@@ -3,9 +3,7 @@ import { actions } from '../generic/account';
 
 export function transferPool() {
   return async (dispatch) => {
-    const _transferpool = await dispatch(
-      fetch.get('/account/transfer')
-    );
+    const _transferpool = await dispatch(fetch.get('/account/transfer'));
     dispatch(actions.one({ _transferpool }, 0));
   };
 }
