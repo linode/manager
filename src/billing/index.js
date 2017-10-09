c784e58dfbe1c824bc01cdc119c58c5177b567f0
Nick Dodson <nidodson@gmail.com>
2017-10-09 19:36:19 -0400

    invoice page init

--- src/billing/index.js
+++ src/billing/index.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import IndexPage from './layouts/IndexPage';
 import DashboardPage from './layouts/DashboardPage';
+import InvoicePage from './layouts/InvoicePage';
 
 export default (
   <Route path="/billing" component={IndexPage}>
     <IndexRoute component={DashboardPage} />
+    <Route path="invoice/:invoiceId" component={InvoicePage} />
   </Route>
 );
