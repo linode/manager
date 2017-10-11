04f4383dad2543932b73d47d6e1a81ca8247be6e
Marques Johansson <marques@displague.com>
2017-10-11 11:34:31 -0400

    additional longview client stubbing / templating

--- src/longview/layouts/IndexPage.js
+++ src/longview/layouts/IndexPage.js
@@ -1,19 +1,180 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fullyLoadedObject } from '~/api/util';
+import { actions } from '~/api/generic/lvclients';
+
+import { PrimaryButton } from 'linode-components/buttons';
+import { Input } from 'linode-components/forms';
+import CreateHelper from '~/components/CreateHelper';
+import { List } from 'linode-components/lists';
+import { Table } from 'linode-components/tables';
+import { ListBody } from 'linode-components/lists/bodies';
+import { ListHeader } from 'linode-components/lists/headers';
+import {
+  ButtonCell,
+  CheckboxCell,
+  LinkCell,
+} from 'linode-components/tables/cells';
+import {
+  RegionCell,
+  IPAddressCell,
+} from '~/components/tables/cells';
+import { MassEditControl } from 'linode-components/lists/controls';
+
+import { setAnalytics, setSource, setTitle } from '~/actions';
+import { default as toggleSelected } from '~/actions/select';
+import api from '~/api';
+import { transform } from '~/api/util';
+import { confirmThenDelete } from '~/utilities';
+import { AddLVClient } from '../components/AddLVClient';
+
+const OBJECT_TYPE = 'longview';
 
 export class IndexPage extends Component {
+  // async'ing because we don't want to forget when we add another await
+  static async preload({ dispatch }) {
+    // const lvclients = api.lvclients.all();
+    const lvclients = {
+      lvclients: [
+        {
+          id: 'FAKE', // *DONT COMMIT ME*
+          label: 'longview83735',
+        },
+      ],
+      pages: 1,
+      page: 1,
+      results: 1,
+    };
+
+    await dispatch(actions.many(lvclients));
+  }
+
+
   constructor(props) {
     super(props);
 
     this.state = { filter: '' };
   }
 
-  render() {
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(setSource(__filename));
+    dispatch(setTitle('Longview Clients'));
+    dispatch(setAnalytics(['lvclients']));
+  }
+
+  deleteLVClients = confirmThenDelete(
+    this.props.dispatch,
+    'Longview Client',
+    api.lvclients.delete,
+    OBJECT_TYPE).bind(this)
+
+
+  renderLVClients() {
+    const { dispatch, lvclients, selectedMap } = this.props;
+    const { filter } = this.state;
+    const { sorted } = transform(lvclients, {
+      filterBy: filter,
+    });
+
     return (
-      <div>Longview</div>
+      <List>
+        <ListHeader className="Menu">
+          <div className="Menu-item">
+            <MassEditControl
+              data={sorted}
+              dispatch={dispatch}
+              massEditGroups={[{ elements: [
+                { name: 'Delete', action: this.deleteLVClients },
+              ] }]}
+              selectedMap={selectedMap}
+              objectType={OBJECT_TYPE}
+              toggleSelected={toggleSelected}
+            />
+          </div>
+          <div className="Menu-item">
+            <Input
+              placeholder="Filter..."
+              onChange={({ target: { value } }) => this.setState({ filter: value })}
+              value={this.state.filter}
+            />
+          </div>
+        </ListHeader>
+        <ListBody>
+          <Table
+            columns={[
+              { cellComponent: CheckboxCell, headerClassName: 'CheckboxColumn' },
+              {
+                cellComponent: LinkCell,
+                hrefFn: (lvclient) => { return `/longview/clients/${lvclient.label}`; },
+                tooltipEnabled: true,
+              },
+              { cellComponent: IPAddressCell, headerClassName: 'LinodeIPAddressColumn' },
+              { cellComponent: RegionCell },
+              {
+                cellComponent: ButtonCell,
+                headerClassName: 'ButtonColumn',
+                onClick: this.deleteLVClients,
+                text: 'Delete',
+              },
+            ]}
+            noDataMessage={"No Longview Clients found."}
+            data={sorted}
+            selectedMap={selectedMap}
+            disableHeader
+            onToggleSelect={(record) => {
+              dispatch(toggleSelected(OBJECT_TYPE, record.id));
+            }}
+          />
+        </ListBody>
+      </List>
+    );
+  }
+
+
+  render() {
+    const { dispatch, lvclients } = this.props;
+
+    const addLVClient = () => AddLVClient.trigger(dispatch);
+
+    return (
+      <div className="PrimaryPage container">
+        <header className="PrimaryPage-header">
+          <div className="PrimaryPage-headerRow clearfix">
+            <h1 className="float-left">Longview Clients</h1>
+            <PrimaryButton onClick={addLVClient} className="float-right">
+              Add a Longview Client
+            </PrimaryButton>
+          </div>
+        </header>
+        <div className="PrimaryPage-body">
+          {Object.values(lvclients).length ? this.renderLVClients() : (
+            <CreateHelper
+              label="Longview Clients"
+              linkText="Add a Longview Client"
+              onClick={addLVClient}
+            />
+          )}
+        </div>
+      </div>
     );
   }
 }
 
 IndexPage.propTypes = {
   dispatch: PropTypes.func,
+  lvclients: PropTypes.object.isRequired,
+  selectedMap: PropTypes.object.isRequired,
 };
+
+function select(state) {
+  const lvclients = _.pickBy(state.api.lvclients.lvclients, fullyLoadedObject);
+
+  return {
+    lvclients,
+    selectedMap: state.select.selected[OBJECT_TYPE] || {},
+  };
+}
+
+export default connect(select)(IndexPage);
