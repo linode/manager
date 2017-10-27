ac1561a404c9f60fce540c25e330da68e0a6e0bc
Josh Sager <jsager@linode.com>
2017-10-27 11:16:32 -0400

    Created Managed service endpoint and contacts object
    Added managed to prebuild.js
    Created Manged Contacts object
    Created endpoints for managed services
    Included both GET, POST, and PUT endpoints (consolidating two API-based
    tasks into one docs PR)

--- docs/prebuild.js
+++ docs/prebuild.js
@@ -365,6 +365,12 @@ const endpointMap = {
     routePath: `${ROUTE_BASE_PATH}/images`,
     groups: {},
   },
+  managed: {
+    name: 'Managed',
+    path: '/managed',
+    routePath: `${ROUTE_BASE_PATH}/managed`,
+    groups: {},
+  },
 };
 
 allEndpoints.forEach(function (endpointContainer) {
