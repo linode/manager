73a13350631a19d539dabede174e08acaf67bba8
Marques Johansson <marques@displague.com>
2017-10-27 13:52:44 -0400

    convert BillingHistoryList to a pure component
    ```
    jscodeshift -t ~/repos/react-codemod/transforms/pure-component.js \
        src/billing/  --useArrows=true --destructuring=true
    ```

--- src/billing/components/BillingHistoryList.js
+++ src/billing/components/BillingHistoryList.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment-timezone';
 
@@ -7,66 +7,64 @@ import { getStorage } from '~/storage';
 import { Table } from 'linode-components/tables';
 import { LinkCell } from 'linode-components/tables/cells';
 
-export class BillingHistoryList extends Component {
-  render() {
-    const {
-        invoices,
-        payments,
-    } = this.props;
+export const BillingHistoryList = props => {
+  const {
+      invoices,
+      payments,
+  } = props;
 
-    const dateSort = (a, b) => new Date(a.date) - new Date(b.date);
-    const dateSince = (since) => ({ date }) => new Date(date) > since;
+  const dateSort = (a, b) => new Date(a.date) - new Date(b.date);
+  const dateSince = (since) => ({ date }) => new Date(date) > since;
 
-    const timezone = getStorage('profile/timezone') || 'UTC';
-    let history = [
-      ..._.map(invoices, invoice => ({ ...invoice, type: 'invoice' })),
-      ..._.map(payments, payment => ({
-        ...payment,
-        type: 'payment',
-        total: -payment.usd,
-        label: `Payment - #${payment.id}`,
-      })),
-    ];
+  const timezone = getStorage('profile/timezone') || 'UTC';
+  let history = [
+    ..._.map(invoices, invoice => ({ ...invoice, type: 'invoice' })),
+    ..._.map(payments, payment => ({
+      ...payment,
+      type: 'payment',
+      total: -payment.usd,
+      label: `Payment - #${payment.id}`,
+    })),
+  ];
 
-    history.sort(dateSort);
+  history.sort(dateSort);
 
-    if (this.props.days) {
-      const since = new Date();
-      since.setDate(since.getDate() - this.props.days);
+  if (props.days) {
+    const since = new Date();
+    since.setDate(since.getDate() - props.days);
 
-      history = history.filter(dateSince(since));
-    }
-
-    return (
-      <Table
-        columns={[
-          {
-            dataKey: 'date',
-            formatFn: (date) => {
-              const time = moment.utc(date, moment.iso_8601).tz(timezone);
-              return time.format('MMM D YYYY h:mm A z');
-            },
-          },
-          {
-            cellComponent: LinkCell,
-            hrefFn: (item) => `/billing/history/${item.type}/${item.id}`,
-            textKey: 'label',
-          },
-          {
-            dataKey: 'total',
-            className: 'ActionsCell',
-            formatFn: (total) => {
-              return total < 0 ? `($${(-total).toFixed(2)})` : `$${total.toFixed(2)}`;
-            },
-          },
-        ]}
-        noDataMessage="No history found."
-        data={Object.values(history).reverse()}
-        disableHeader
-      />
-    );
+    history = history.filter(dateSince(since));
   }
-}
+
+  return (
+    <Table
+      columns={[
+        {
+          dataKey: 'date',
+          formatFn: (date) => {
+            const time = moment.utc(date, moment.iso_8601).tz(timezone);
+            return time.format('MMM D YYYY h:mm A z');
+          },
+        },
+        {
+          cellComponent: LinkCell,
+          hrefFn: (item) => `/billing/history/${item.type}/${item.id}`,
+          textKey: 'label',
+        },
+        {
+          dataKey: 'total',
+          className: 'ActionsCell',
+          formatFn: (total) => {
+            return total < 0 ? `($${(-total).toFixed(2)})` : `$${total.toFixed(2)}`;
+          },
+        },
+      ]}
+      noDataMessage="No history found."
+      data={Object.values(history).reverse()}
+      disableHeader
+    />
+  );
+};
 
 BillingHistoryList.propTypes = {
   dispatch: PropTypes.func,
