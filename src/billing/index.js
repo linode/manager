13cd30a0032182b83b56f973639bc7caeaff360d
Nick Dodson <nidodson@gmail.com>
2017-11-01 18:52:42 -0400

    contact info init

--- src/billing/index.js
+++ src/billing/index.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 import IndexPage from './layouts/IndexPage';
 import DashboardPage from './layouts/DashboardPage';
+import ContactPage from './layouts/ContactPage';
 import HistoryPage from './layouts/HistoryPage';
 import InvoicePage from './layouts/InvoicePage';
 import CreditCardPage from './layouts/CreditCardPage';
@@ -13,6 +14,7 @@ export default (
   <Route path="/billing" component={IndexPage}>
     <IndexRoute component={DashboardPage} />
     <Route path="history" component={HistoryPage} />
+    <Route path="contact" component={ContactPage} />
     <Route path="history/invoice/:invoiceId" component={InvoicePage} />
     <Route path="history/payment/:paymentId" component={PaymentPage} />
     <Route path="creditcard" component={CreditCardPage} />
