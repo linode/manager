402e0cf7ccb4884b268c5e120c7980f03651a18a
Robert Stires <rstires@linode.com>
2017-11-28 17:50:14 -0500

    Adjust backup schedules to user timeszones. (#2751)
    * Adjust selection options for users local time.

--- src/linodes/linode/backups/components/ScheduleForm.js
+++ src/linodes/linode/backups/components/ScheduleForm.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import moment from 'moment-timezone';
 import {
   Form,
   FormGroup,
@@ -10,12 +11,63 @@ import {
   SubmitButton,
 } from 'linode-components/forms';
 import { onChange } from 'linode-components/forms/utilities';
-
+import { sortBy, find } from 'lodash';
 import api from '~/api';
 import { dispatchOrStoreErrors } from '~/api/util';
 
+const dayOptions = [
+  { value: 'Sunday', label: 'Sunday' },
+  { value: 'Monday', label: 'Monday' },
+  { value: 'Tuesday', label: 'Tuesday' },
+  { value: 'Wednesday', label: 'Wednesday' },
+  { value: 'Thursday', label: 'Thursday' },
+  { value: 'Friday', label: 'Friday' },
+  { value: 'Saturday', label: 'Saturday' },
+];
 
-export default class ScheduleForm extends Component {
+// If the adjusted time falls on an odd hour, we drop it back by one.
+const shift = (n) => {
+  if (n === 0) return n;
+  return (n % 2 === 0) ? n : n - 1;
+};
+
+/**
+ * Given a timezone return a list of options where the value is the
+ * adjusted window for the supplied timezone.
+ *
+ * Note; The addition of 1 and 2 hours is per an offset at the API level.
+ *
+ * @param {string} timezone
+ * @returns {ScheduleOptions[]}
+ */
+export function createAdjustedScheduleOptions(timezone, day) {
+  const format = 'HH:mm';
+  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  const idx = days.indexOf(day.toLowerCase());
+
+  const list = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22].map((hour) => {
+    const start = moment.utc({ hour })
+      .day(idx)
+      .add(1, 'hours')
+      .tz(timezone);
+
+    const finish = moment.utc({ hour })
+      .day(idx)
+      .add(3, 'hours')
+      .tz(timezone);
+
+    return {
+      start,
+      finish,
+      label: `${start.format(format)} - ${finish.format(format)}`,
+      value: `W${shift(moment.utc({ hour }).format('H'))}`,
+    };
+  });
+
+  return sortBy(list, (i) => i.label);
+}
+
+export class ScheduleForm extends Component {
   constructor(props) {
     super(props);
 
@@ -40,31 +92,11 @@ export default class ScheduleForm extends Component {
 
   render() {
     const { errors, loading, window, day } = this.state;
-
-    const windowOptions = [
-      { value: 'W0', label: '12AM - 2AM' },
-      { value: 'W2', label: '2AM - 4AM' },
-      { value: 'W4', label: '4AM - 6AM' },
-      { value: 'W6', label: '6AM - 8AM' },
-      { value: 'W8', label: '8AM - 10AM' },
-      { value: 'W10', label: '10AM - 12PM' },
-      { value: 'W12', label: '12PM - 2PM' },
-      { value: 'W14', label: '2PM - 4PM' },
-      { value: 'W16', label: '4PM - 6PM' },
-      { value: 'W18', label: '6PM - 8PM' },
-      { value: 'W20', label: '8PM - 10PM' },
-      { value: 'W22', label: '10PM - 12AM' },
-    ];
-
-    const dayOptions = [
-      { value: 'Sunday', label: 'Sunday' },
-      { value: 'Monday', label: 'Monday' },
-      { value: 'Tuesday', label: 'Tuesday' },
-      { value: 'Wednesday', label: 'Wednesday' },
-      { value: 'Thursday', label: 'Thursday' },
-      { value: 'Friday', label: 'Friday' },
-      { value: 'Saturday', label: 'Saturday' },
-    ];
+    const { tz } = this.props;
+    const adjustedScheduleOptions = createAdjustedScheduleOptions(tz, day);
+    const scheduleOption = find(adjustedScheduleOptions, (i) => i.value === window);
+    const { start, finish } = scheduleOption;
+    const timezoneAbbr = moment.tz(tz).format('z');
 
     return (
       <Form
@@ -72,15 +104,20 @@ export default class ScheduleForm extends Component {
         analytics={{ title: 'Backups Schedule' }}
       >
         <FormGroup name="window" errors={errors} className="row">
-          <label htmlFor="window" className="col-sm-2 col-form-label">Time of Day (EST)</label>
+          <label htmlFor="window" className="col-sm-2 col-form-label">
+            Time of Day ({timezoneAbbr})
+          </label>
           <div className="col-sm-10">
             <Select
               id="window"
               name="window"
               value={window || ''}
               onChange={this.onChange}
-              options={windowOptions}
+              options={adjustedScheduleOptions}
             />
+            <small className="form-text text-muted">
+            Daily backups will be attempted {start.format('HH:mm z')} - {finish.format('HH:mm z')}.
+            </small>
             <FormGroupError errors={errors} name="day" />
           </div>
         </FormGroup>
@@ -95,6 +132,10 @@ export default class ScheduleForm extends Component {
               className="float-sm-left"
               options={dayOptions}
             />
+            <small className="form-text text-muted">
+              Weekly Backups will be attempted between {start.format('dddd HH:mm z')}
+              &nbsp;and {finish.format('dddd HH:mm z')}.
+            </small>
             <FormGroupError errors={errors} name="day" />
           </div>
         </FormGroup>
@@ -108,10 +149,16 @@ export default class ScheduleForm extends Component {
     );
   }
 }
+const mapStateToProps = (state) => ({
+  tz: state.api.profile.timezone,
+});
+
+export default connect(mapStateToProps)(ScheduleForm);
 
 ScheduleForm.propTypes = {
   window: PropTypes.string.isRequired,
   day: PropTypes.string.isRequired,
   linode: PropTypes.object.isRequired,
   dispatch: PropTypes.func.isRequired,
+  tz: PropTypes.string.isRequired,
 };
