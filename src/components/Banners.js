ef964f99ee14b0ef28ff5bd3630607ce9167d111
Andrew Sauber <a3sauber@gmail.com>
2017-12-20 12:41:35 -0500

    use standard scss colors for banner levels

--- src/components/Banners.js
+++ src/components/Banners.js
@@ -24,13 +24,13 @@ export function filterBy(pred, data) {
 function importantTicket(banners) {
   if (banners.length > 1) {
     return (
-      <div className="notice">
+      <div className="warning">
         You have <Link to="/support">important tickets</Link> open!
       </div>
     );
   } else if (banners.length === 1) {
     return (
-      <div className="notice">
+      <div className="warning">
         You have an <Link to={`/support/${banners[0].entity.id}`}>important ticket</Link> open!
       </div>
     );
@@ -56,25 +56,20 @@ function abuseTicket(banners) {
   }
 }
 
-function migrations(banners, linode) {
-  if (!linode) {
-    return null;
-  }
-
-  const banner = banners.find(banner => banner.entity.id === linode.id);
-  if (banner) {
-    return (
-      <div className="notice" key={key}>
+function migrations(banners) {
+  return (
+    banners.map((banner, key) =>
+      <div className="warning" key={key}>
         You have a host migration {banner.type.split('_')[0]} for this linode!
       </div>
-    );
-  }
+    )
+  );
 }
 
 function scheduledReboot(banners) {
   return (
     banners.map((banner, key) =>
-      <div className="notice" key={key}>
+      <div className="warning" key={key}>
         {banner.entity.label} is scheduled to reboot.
       </div>
     )
@@ -108,7 +103,7 @@ function outage(banners) {
 
   if (datacenterNames.length) {
     return (
-      <div className="outage">
+      <div className="info">
         We are aware of issues affecting service in the following facilities:
         &nbsp;{datacenterNames.join(', ')}
       </div>
