972256bd2929fdd21f9b49817bc4803d53494619
Robert Stires <rstires@linode.com>
2017-12-20 09:44:34 -0500

    Add scheduledReboot, xsa, and outstandingBalance.

--- src/data/banners.js
+++ src/data/banners.js
@@ -16,6 +16,39 @@ export const testBillingBanner = {
   },
 };
 
+export const xsaBanner = {
+  entity: {
+    id: 1234,
+    label: 'my-linode',
+    type: 'linode',
+    url: '/linode/instances/1234',
+  },
+  type: 'xsa',
+  when: '2017-01-01T00:00:01',
+};
+
+export const outstandingBalanceBanner = {
+  entity: {
+    id: null,
+    label: 'user@email.com',
+    type: 'account',
+    url: '/account/settings',
+  },
+  type: 'outstanding_balance',
+  when: null,
+};
+
+export const scheduledRebootBanner = {
+  entity: {
+    id: 1234,
+    label: 'my-linode',
+    type: 'linode',
+    url: '/linode/instances/1234',
+  },
+  type: 'scheduled_reboot',
+  when: '2017-12-31T23:59:59',
+};
+
 export const testAbuseBanner = {
   ...testApiBanner,
   entity: {
