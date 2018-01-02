53c65e21be1e6adca2200f824b51bad5efd579a8
Andrew Sauber <a3sauber@gmail.com>
2018-01-02 15:54:26 -0500

    use monitor websocket to indicate linode power status

--- bin/copy-assets.sh
+++ bin/copy-assets.sh
@@ -5,9 +5,6 @@ set -eux
 mkdir -p assets/weblish
 cp node_modules/xterm/dist/{xterm.js,xterm.css,xterm.js.map} assets/weblish
 
-mkdir -p assets/glish/novnc
-cp -r node_modules/novnc/include/ assets/glish/novnc
-
 cp node_modules/zxcvbn/dist/{zxcvbn.js,zxcvbn.js.map} assets/
 
 mkdir -p assets/font-awesome/css
