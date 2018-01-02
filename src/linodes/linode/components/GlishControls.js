3d50c74a72b19f9f5c561589e90e3b4336832cf8
Andrew Sauber <a3sauber@gmail.com>
2018-01-02 17:16:37 -0500

    implement power and reboot buttons

--- src/linodes/linode/components/GlishControls.js
+++ src/linodes/linode/components/GlishControls.js
@@ -1,29 +1,92 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { Button } from 'linode-components';
+import { powerOnLinode, powerOffLinode, rebootLinode } from '~/api/ad-hoc/linodes';
 
-export default function GlishControls(props) {
-  let bgColor = 'bg-warning';
-  if (!props.powered) {
-    bgColor = 'bg-danger';
-  }
-  if (props.connected) {
-    bgColor = 'bg-success';
+export class GlishControls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      overrideMessage: null,
+    };
+    this.powerAction = this.powerAction.bind(this);
+    this.reboot = this.reboot.bind(this);
   }
 
-  return (
-    <div id="glish-controls" className={`p-2 text-center clearfix text-white ${bgColor}`}>
-      <Button className="float-left mr-2 fa fa-power-off power-btn" />
-      <Button className="float-left">Reboot</Button>
-      <span className="align-middle">{props.message}</span>
-      <Button className="float-right">Ctrl + Alt + Del</Button>
-    </div>
-  );
+  componentWillReceiveProps(nextProps) {
+    if (this.props.message !== nextProps.message) {
+      this.setState({ overrideMessage: null });
+    }
+  }
+
+  powerOff = () => {
+    if (this.props.linodeId) {
+      this.props.dispatch(powerOffLinode(this.props.linodeId));
+      this.setState({ overrideMessage: 'Linode is powering down' });
+    }
+  }
+
+  powerOn = () => {
+    if (this.props.linodeId) {
+      this.props.dispatch(powerOnLinode(this.props.linodeId));
+      this.setState({ overrideMessage: 'Linode is booting up' });
+    }
+  }
+
+  powerAction() {
+    this.props.powered ? this.powerOff() : this.powerOn();
+  }
+
+  reboot() {
+    if (this.props.linodeId) {
+      this.props.dispatch(rebootLinode(this.props.linodeId));
+      this.setState({ overrideMessage: 'Linode is rebooting' });
+    }
+  }
+
+  render() {
+    const { powered, connected, message } = this.props;
+    const { overrideMessage } = this.state;
+
+    let bgColor = 'bg-warning';
+    if (!overrideMessage) {
+      if (!powered) {
+        bgColor = 'bg-danger';
+      }
+      if (connected) {
+        bgColor = 'bg-success';
+      }
+    }
+
+    return (
+      <div id="glish-controls" className={`p-2 text-center clearfix text-white ${bgColor}`}>
+        <Button
+          onClick={this.powerAction}
+          className="float-left mr-2 fa fa-power-off power-btn"
+          disabled={overrideMessage}
+        />
+        <Button
+          onClick={this.reboot}
+          className="float-left"
+          disabled={overrideMessage}
+        >
+          Reboot
+        </Button>
+        <span className="align-middle">{overrideMessage || message}</span>
+        <Button className="float-right invisible">Ctrl + Alt + Del</Button>
+      </div>
+    );
+  }
 }
 
 GlishControls.propTypes = {
+  dispatch: PropTypes.func.isRequired,
   message: PropTypes.string.isRequired,
   powered: PropTypes.bool.isRequired,
   connected: PropTypes.bool.isRequired,
+  linodeId: PropTypes.number,
 };
+
+export default connect()(GlishControls);
