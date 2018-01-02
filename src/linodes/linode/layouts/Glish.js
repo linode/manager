b147313a04b7971df385135f46e257fcbc98cde4
Andrew Sauber <a3sauber@gmail.com>
2018-01-02 15:54:26 -0500

    use monitor websocket to indicate linode power status

--- src/linodes/linode/layouts/Glish.js
+++ src/linodes/linode/layouts/Glish.js
@@ -16,9 +16,9 @@ export class Glish extends Component {
     this.state = {
       linode: null,
       token: null,
+      activeVnc: true,
+      connected: false,
       powered: true,
-      vncState: 'warn',
-      message: '',
     };
     this.lastDisconnect = Date.now();
   }
@@ -31,45 +31,80 @@ export class Glish extends Component {
 
     const { lish_token: token } = await dispatch(lishToken(linode.id));
     this.setState({ token: token });
+    this.refreshVNCSession(linode.id);
+
+    const region = linode && ZONES[linode.region];
+    this.connectMonitor(region, token);
   }
 
   componentWillUnmount() {
-    clearInterval(this.renewInterval);
+    clearTimeout(this.refreshTimeout);
+    this.monitor.close();
   }
 
-  onUpdateVNCState = (rfb, newState) => {
-    this.setState({ vncState: newState });
 
+  onUpdateVNCState = (rfb, newState) => {
     switch (newState) {
       case 'normal':
-        this.setState({ message: `Connected to Linode ${this.state.linode.id}` });
-        clearInterval(this.reconnectInterval);
-        this.reconnectInterval = undefined;
+        this.setState({ connected: true });
         break;
       case 'disconnected':
       case 'failed':
       case 'fatal':
-        if (!this.reconnectInterval) {
-          this.setState({ message: 'Connecting...' });
-          this.reconnectInterval = setInterval(() => {
-            this.setState({ powered: false });
-            setTimeout(() => this.setState({ powered: true }), 1000);
-          }, 3000);
-        }
+        this.setState({ connected: false });
+        this.setState({ activeVnc: false });
+        setTimeout(() => this.setState({ activeVnc: true }), 3000);
         break;
       default:
         break;
     }
   }
 
+  refreshVNCSession = (linodeId) => {
+    this.refreshTimeout = setTimeout(async () => {
+      const { lish_token: token } = await this.props.dispatch(lishToken(linodeId));
+      this.setState({ token: token });
+      this.refreshVNCSession(linodeId); // refresh our VNC session every 5 minutes
+    }, 5 * 60 * 1000);
+  }
+
+  connectMonitor = (region, token) => {
+    const url = `wss://${region}.${LISH_ROOT}:8080/${token}/monitor`;
+    this.monitor = new WebSocket(url);
+    this.monitor.addEventListener('message', ev => {
+      const data = JSON.parse(ev.data);
+      if (data.poweredStatus === 'Running') {
+        this.setState({ powered: true });
+      } else {
+        this.setState({ powered: false });
+      }
+    });
+  }
+
   render() {
-    const { vncState, message, powered, token, linode } = this.state;
+    const {
+      linode,
+      token,
+      activeVnc,
+      connected,
+      powered,
+    } = this.state;
+
     const region = linode && ZONES[linode.region];
+
+    let message = 'Connecting...';
+    if (linode && powered === false) {
+      message = `Linode ${linode.id} is powered off`;
+    }
+    if (linode && connected === true) {
+      message = `Connected to Linode ${linode.id}`;
+    }
+
     return (
       <div id="Glish" className="h-100">
-        <GlishControls vncState={vncState} message={message} />
+        <GlishControls powered={powered} connected={connected} message={message} />
         <div className="text-center">
-          {powered && token && region &&
+          {activeVnc && token && region &&
             <VncDisplay
               url={`wss://${region}.${LISH_ROOT}:8080/${token}`}
               onUpdateState={this.onUpdateVNCState}
