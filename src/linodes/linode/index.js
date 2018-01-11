c57724cc93079231561f091808a7e7f8f0b5df59
Robert Stires <rstires@linode.com>
2018-01-11 10:25:05 -0500

    Add analytics and guard on linode dashboard.

--- src/linodes/linode/index.js
+++ src/linodes/linode/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Link,
   Redirect,
@@ -12,6 +12,7 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import TabsComponent from '~/components/Tabs';
 
+import { setAnalytics } from '~/actions';
 import api from '~/api';
 import { getObjectByLabelLazily } from '~/api/util';
 import { ChainedDocumentTitle, GroupLabel } from '~/components';
@@ -25,65 +26,76 @@ import ResizePage from './layouts/ResizePage';
 import RescuePage from './layouts/RescuePage';
 import RebuildPage from './layouts/RebuildPage';
 
-const LinodeIndex = (props) => {
-  const {
-    match: { path, url },
-    location: { pathname },
-    linode,
-    dispatch,
-  } = props;
+class LinodeIndex extends Component {
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(setAnalytics(['linodes', 'linode']));
+  }
 
-  const matched = (path, options) => Boolean(
-    matchPath(pathname, { path, ...options })
-  );
+  render() {
+    const {
+      match: { path, url },
+      location: { pathname },
+      linode,
+      dispatch,
+    } = this.props;
 
-  const tabData = [
-    { name: 'Dashboard', to: url, selected: matched(url, { exact: true }) },
-    { name: 'Networking', to: `${url}/networking`, selected: matched(`${url}/networking`) },
-    { name: 'Rebuild', to: `${url}/rebuild`, selected: matched(`${url}/rebuild`) },
-    { name: 'Resize', to: `${url}/resize`, selected: matched(`${url}/resize`) },
-    { name: 'Rescue', to: `${url}/rescue`, selected: matched(`${url}/rescue`) },
-    { name: 'Backups', to: `${url}/backups`, selected: matched(`${url}/backups`) },
-    { name: 'Settings', to: `${url}/settings`, selected: matched(`${url}/settings`) },
-  ];
+    if (!linode) {
+      return null;
+    }
 
-  return (
-    <div>
-      <ChainedDocumentTitle title={linode.label} />
-      <header className="main-header">
-        <div className="container">
-          <div className="float-sm-left">
-            <Link to="/linodes">Linodes</Link>
-            <h1 title={linode.id}>
-              <Link to={`/linodes/${linode.label}`}>
-                <GroupLabel object={linode} />
-              </Link>
-            </h1>
-            <div>{planStyle(linode.type)}</div>
+    const matched = (path, options) => Boolean(
+      matchPath(pathname, { path, ...options })
+    );
+
+    const tabData = [
+      { name: 'Dashboard', to: url, selected: matched(url, { exact: true }) },
+      { name: 'Networking', to: `${url}/networking`, selected: matched(`${url}/networking`) },
+      { name: 'Rebuild', to: `${url}/rebuild`, selected: matched(`${url}/rebuild`) },
+      { name: 'Resize', to: `${url}/resize`, selected: matched(`${url}/resize`) },
+      { name: 'Rescue', to: `${url}/rescue`, selected: matched(`${url}/rescue`) },
+      { name: 'Backups', to: `${url}/backups`, selected: matched(`${url}/backups`) },
+      { name: 'Settings', to: `${url}/settings`, selected: matched(`${url}/settings`) },
+    ];
+
+    return (
+      <div>
+        <ChainedDocumentTitle title={linode.label} />
+        <header className="main-header">
+          <div className="container">
+            <div className="float-sm-left">
+              <Link to="/linodes">Linodes</Link>
+              <h1 title={linode.id}>
+                <Link to={`/linodes/${linode.label}`}>
+                  <GroupLabel object={linode} />
+                </Link>
+              </h1>
+              <div>{planStyle(linode.type)}</div>
+            </div>
+            <span className="float-sm-right">
+              <StatusDropdown
+                shortcuts={false}
+                linode={linode}
+                short
+                dispatch={dispatch}
+              />
+            </span>
           </div>
-          <span className="float-sm-right">
-            <StatusDropdown
-              shortcuts={false}
-              linode={linode}
-              short
-              dispatch={dispatch}
-            />
-          </span>
-        </div>
-      </header>
-      <TabsComponent tabs={tabData} />
-      <Switch>
-        <Route path={`${path}/rebuild`} component={RebuildPage} />
-        <Route path={`${path}/resize`} component={ResizePage} />
-        <Route path={`${path}/rescue`} component={RescuePage} />
-        <Route path={`${path}/networking`} component={() => <div className="container"><h1>networking</h1></div>} />
-        <Route path={`${path}/backups`} component={() => <div className="container"><h1>backups</h1></div>} />
-        <Route path={`${path}/settings`} component={() => <div className="container"><h1>settings</h1></div>} />
-        <Route exact path={path} component={DashboardPage} />
-        <Redirect to="/not-found" />
-      </Switch>
-    </div>
-  );
+        </header>
+        <TabsComponent tabs={tabData} />
+        <Switch>
+          <Route path={`${path}/rebuild`} component={RebuildPage} />
+          <Route path={`${path}/resize`} component={ResizePage} />
+          <Route path={`${path}/rescue`} component={RescuePage} />
+          <Route path={`${path}/networking`} component={() => <div className="container"><h1>networking</h1></div>} />
+          <Route path={`${path}/backups`} component={() => <div className="container"><h1>backups</h1></div>} />
+          <Route path={`${path}/settings`} component={() => <div className="container"><h1>settings</h1></div>} />
+          <Route exact path={path} component={DashboardPage} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </div>
+    );
+  }
 };
 
 LinodeIndex.propTypes = {
