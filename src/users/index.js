b56f710cc98863a942de9b8acd208c2e8ee8ab04
Robert Stires <rstires@linode.com>
2018-01-13 12:51:43 -0500

    Update /users to use React Router 4

--- src/users/index.js
+++ src/users/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import IndexPage from './layouts/IndexPage';
 import UserPage from './user';
 
-export default (
-  <Route path="users">
-    <IndexRoute component={IndexPage} />
-    {UserPage}
-  </Route>
-);
+const UsersIndex = (props) => {
+  const { match: { path } } = props;
+  return (
+    <Switch>
+      <Route component={UserPage} path={`${path}/:username`} />
+      <Route component={IndexPage} exact path={path} />
+      <Redirect to="/not-found" />Î
+    </Switch >
+  );
+};
+
+UsersIndex.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default UsersIndex;
--- src/users/user/index.js
+++ src/users/user/index.js
@@ -1,13 +1,101 @@
-import React from 'react';
-import { Route, IndexRoute } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Route, Switch, matchPath } from 'react-router-dom';
+import TabsComponent from '~/components/Tabs';
 
-import IndexPage from './layouts/IndexPage';
+import { setAnalytics } from '~/actions';
+import { getObjectByLabelLazily } from '~/api/util';
+import { ChainedDocumentTitle } from '~/components';
+import { ComponentPreload as Preload } from '~/decorators/Preload';
+
 import EditUserPage from './layouts/EditUserPage';
 import PermissionsPage from './layouts/PermissionsPage';
 
-export default (
-  <Route path=":username" component={IndexPage}>
-    <IndexRoute component={EditUserPage} />
-    <Route path="permissions" component={PermissionsPage} />
-  </Route>
-);
+export class UserIndexPage extends Component {
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(setAnalytics(['users', 'user']));
+  }
+
+  render() {
+    if (!this.props.user) {
+      return null;
+    }
+
+    const {
+      match: { path, url },
+      location: { pathname },
+      user: { username, restricted },
+    } = this.props;
+
+    const matched = (path, options) => Boolean(
+      matchPath(pathname, { path, ...options })
+    );
+
+    const tabData = [
+      { name: 'Dashboard', to: url, selected: matched(url, { exact: true }) },
+    ];
+
+    if (restricted) {
+      tabData.push(
+        { name: 'Permissions', to: `${url}/permissions`, selected: matched(`${url}/permissions`) },
+      );
+    }
+
+    return (
+      <div>
+        <ChainedDocumentTitle title={username} />
+        <header className="main-header">
+          <div className="container">
+            <div className="float-sm-left">
+              <Link to="/users">Users</Link>
+              <h1 title={username}>
+                <Link to={`/users/${username}`}>
+                  {username}
+                </Link>
+              </h1>
+            </div>
+          </div>
+        </header>
+        <div className="main-header-fix"></div>
+        <TabsComponent tabs={tabData} />
+        <div className="container">
+          <Switch>
+            <Route path={`${path}/permissions`} component={PermissionsPage} />
+            <Route path={path} exact component={EditUserPage} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+UserIndexPage.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func,
+  user: PropTypes.object,
+};
+
+export function mapStateToProps(state, { match: { params: { username } } }) {
+  const { users } = state.api.users;
+  const user = users[username];
+  return { user };
+}
+
+const preloadRequest = async (dispatch, { match: { params: { username } } }) => {
+  await dispatch(getObjectByLabelLazily('users', username, 'username'));
+};
+
+export default compose(
+  connect(mapStateToProps),
+  Preload(preloadRequest),
+)(UserIndexPage);
