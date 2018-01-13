c574ed303e97f987d17808707cce10a6209b0e95
Robert Stires <rstires@linode.com>
2018-01-13 10:10:09 -0500

    Update /linode/images to use React Router 4

--- src/linodes/images/index.js
+++ src/linodes/images/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import IndexPage from './layouts/IndexPage';
 
+const ImagesIndex = (props) => {
+  const { match: { path } } = props;
+  return (
+    <Switch>
+      <Route exact path={path} component={IndexPage} />
+      <Redirect to="/not-found" />
+    </Switch >
+  );
+};
 
-export default (
-  <Route path="/images">
-    <IndexRoute component={IndexPage} />
-  </Route>
-);
+ImagesIndex.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ImagesIndex;
