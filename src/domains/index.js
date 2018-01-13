e86a43f8699147e60c4621b9f81e5c86277c74a7
Robert Stires <rstires@linode.com>
2018-01-13 06:59:22 -0500

    Gold plating

--- src/domains/index.js
+++ src/domains/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import IndexPage from './layouts/IndexPage';
-import ZonePage from './layouts/ZonePage';
+import DomainsList from './layouts/DomainsList';
+import DomainDetails from './layouts/DomainDetails';
 
 const DomainsIndex = (props) => {
   const { match: { path } } = props;
   return (
     <Switch>
-      <Route component={ZonePage} path={`${path}/:domainLabel`} />
-      <Route component={IndexPage} exact path={`${path}/`} />
+      <Route component={DomainDetails} path={`${path}/:domainLabel`} />
+      <Route component={DomainsList} exact path={`${path}/`} />
       <Redirect to="/not-found" />
     </Switch >
   );
@@ -23,17 +23,3 @@ DomainsIndex.propTypes = {
 };
 
 export default DomainsIndex;
-
-
-// import React from 'react';
-// import { Route, IndexRoute } from 'react-router-dom';
-
-// import IndexPage from './layouts/IndexPage';
-// import ZonePage from './layouts/ZonePage';
-
-// export default (
-//   <Route path="/domains">
-//     <IndexRoute component={IndexPage} />
-//     <Route path=":domainLabel" component={ZonePage} />
-//   </Route>
-// );
--- src/domains/layouts/DomainDetails.spec.js
+++ src/domains/layouts/DomainDetails.spec.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ZonePage } from './ZonePage';
+import { DomainDetails } from './DomainDetails';
 import { testDomain } from '~/data/domains';
 
 describe('components/ZonePage', () => {
   it('should render without error', () => {
     const mockDispatch = jest.fn();
     const wrapper = shallow(
-      <ZonePage
+      <DomainDetails
         dispatch={mockDispatch}
         domain={testDomain}
       />
