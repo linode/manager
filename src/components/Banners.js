664610adf6f9aaf70e6392566a0b000559930eee
Andrew Sauber <a3sauber@gmail.com>
2018-01-18 13:36:57 -0500

    Re-introduce Banners and Modals since the upgrade to React Router 4 (#2917)
    * Initial banner reimplementation.
    
    * Preload banners.
    
    * Re-implement original modals.

--- src/components/Banners.js
+++ src/components/Banners.js
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import isEmpty from 'lodash/isEmpty';
 
+import api from '~/api';
+import { ComponentPreload as Preload } from '~/decorators/Preload';
 /**
  * @param {[function]} pred Array of functions whos only arguement is the
  * data provided and must return a boolean.
@@ -202,7 +206,7 @@ function renderBanners(banners, linode = {}) {
   );
 }
 
-export default function Banners(props) {
+export function Banners(props) {
   const { banners, linode } = props;
   return banners.length ? renderBanners(banners, linode) : null;
 }
@@ -215,3 +219,31 @@ Banners.propTypes = {
 Banners.defaultProps = {
   banners: [],
 };
+
+const getLinodeIdFromStateByLabel = (state, label) => Object
+  .values(state.api.linodes.linodes)
+  .find((linode) => linode.label === label);
+
+const mapStateToProps = (state, { match: { params: { linodeLabel } } }) => ({
+  linode: linodeLabel && getLinodeIdFromStateByLabel(state, linodeLabel),
+  banners: state.api.banners.data,
+});
+
+const preloadRequest = async (dispatch) => {
+  await dispatch(api.banners.one());
+};
+
+const ConnectedBanners = compose(
+  connect(mapStateToProps),
+  Preload(preloadRequest),
+)(Banners);
+
+ConnectedBanners.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      linodeLabel: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default ConnectedBanners;
