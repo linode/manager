557fbabbe88f6eabc4f98e31109cb6501a1cf744
Andrew Sauber <a3sauber@gmail.com>
2018-01-17 19:30:20 -0500

    Implement PortalModal for Advanced Settings Volumes

--- src/linodes/linode/settings/advanced/components/Volumes.js
+++ src/linodes/linode/settings/advanced/components/Volumes.js
@@ -1,47 +1,92 @@
 import omitBy from 'lodash/omitBy';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
 import PrimaryButton from 'linode-components/dist/buttons/PrimaryButton';
 import Card from 'linode-components/dist/cards/Card';
 import CardHeader from 'linode-components/dist/cards/CardHeader';
 
+import { PortalModal } from '~/components/modal';
 import AddEditVolume from '~/volumes/components/AddEditVolume';
 import VolumesList from '~/volumes/components/VolumesList';
+import { hideModal } from '~/utilities';
 
 
-export default function Volumes(props) {
-  const { dispatch, linode, selectedMap, linodes } = props;
-  // Hack because the API is currently returning deleted volumes.
-  const volumes = omitBy(Object.values(linode._volumes.volumes), v => v.status === 'deleted');
+export default class Volumes extends Component {
+  static OBJECT_TYPE = 'linode-volumes';
 
-  const nav = (
-    <PrimaryButton
-      className="float-right"
-      buttonClass="btn-default"
-      onClick={() => AddEditVolume.trigger(dispatch, linodes, undefined, linode.id)}
-    >
-      Add a Volume
-    </PrimaryButton>
-  );
+  constructor(props) {
+    super(props);
 
-  const header = <CardHeader title="Volumes" nav={nav} />;
+    this.state = { modal: null };
 
-  return (
-    <Card header={header}>
-      <VolumesList
-        objectType={Volumes.OBJECT_TYPE}
-        linodes={linodes}
-        volumes={volumes}
-        selectedMap={selectedMap}
-        dispatch={dispatch}
-        className="Table--secondary"
-      />
-    </Card>
-  );
+    this.hideModal = hideModal.bind(this);
+  }
+
+  addEditVolumeModal = () => {
+    this.setState({
+      modal: {
+        name: 'addEditVolume',
+        title: AddEditVolume.title,
+      },
+    });
+  }
+
+  renderModal = () => {
+    const { dispatch, linodes, linode } = this.props;
+    if (!this.state.modal) {
+      return null;
+    }
+    const { name, title } = this.state.modal;
+    return (
+      <PortalModal
+        title={title}
+        onClose={this.hideModal}
+      >
+        {(name === 'addEditVolume') &&
+          <AddEditVolume
+            dispatch={dispatch}
+            close={this.hideModal}
+            linodes={linodes}
+            linode={linode.id}
+          />
+        }
+      </PortalModal>
+    );
+  }
+
+  render() {
+    const { dispatch, linode, selectedMap, linodes } = this.props;
+    // Hack because the API is currently returning deleted volumes.
+    const volumes = omitBy(Object.values(linode._volumes.volumes), v => v.status === 'deleted');
+
+    const nav = (
+      <PrimaryButton
+        className="float-right"
+        buttonClass="btn-default"
+        onClick={() => this.addEditVolumeModal()}
+      >
+        Add a Volume
+      </PrimaryButton>
+    );
+
+    const header = <CardHeader title="Volumes" nav={nav} />;
+
+    return (
+      <Card header={header}>
+        {this.renderModal()}
+        <VolumesList
+          objectType={Volumes.OBJECT_TYPE}
+          linodes={linodes}
+          volumes={volumes}
+          selectedMap={selectedMap}
+          dispatch={dispatch}
+          className="Table--secondary"
+        />
+      </Card>
+    );
+  }
 }
-
-Volumes.OBJECT_TYPE = 'linode-volumes';
 
 Volumes.propTypes = {
   dispatch: PropTypes.func.isRequired,
