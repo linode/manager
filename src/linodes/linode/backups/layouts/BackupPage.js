4a7857cd26e7b12c3a770792c59ae7fc111721da
Robert Stires <rstires@linode.com>
2018-01-23 12:00:21 -0500

    Address API 50 breaking changes to backup responses.

--- src/linodes/linode/backups/layouts/BackupPage.js
+++ src/linodes/linode/backups/layouts/BackupPage.js
@@ -1,13 +1,48 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { compose } from 'redux';
+// import { compose } from 'redux';
 import api from '~/api';
 
 import { BackupRestore, BackupDetails } from '../components';
 import { selectLinode } from '../../utilities';
 import { ComponentPreload as Preload } from '~/decorators/Preload';
 
+import unless from 'ramda/src/unless';
+import path from 'ramda/src/path';
+import propOr from 'ramda/src/propOr';
+import assoc from 'ramda/src/assoc';
+import prop from 'ramda/src/prop';
+import compose from 'ramda/src/compose';
+import over from 'ramda/src/over';
+import lensProp from 'ramda/src/lensProp';
+import isNil from 'ramda/src/isNil';
+import curry from 'ramda/src/curry';
+import propEq from 'ramda/src/propEq';
+
+const currentSnapshot = path(['snapshot', 'current']);
+
+const inProgressSnapshot = path(['snapshot', 'in_progress']);
+
+export const findById = curry((id, list) => list.find(propEq('id', id)));
+
+export const backupsToList = (backups) => compose(
+  prop('result'),
+  unless(
+    compose(isNil, inProgressSnapshot),
+    over(lensProp('result'), (result) => [...result, inProgressSnapshot(backups)])
+  ),
+  unless(
+    compose(isNil, currentSnapshot),
+    over(lensProp('result'), (result) => [...result, currentSnapshot(backups)])
+  ),
+  assoc('result', propOr([], 'automatic', backups)),
+)(backups);
+
+const mergeBackupsThenFindById = (id, backups) => compose(
+  findById(id),
+  backupsToList,
+)(backups);
 
 export const BackupPage = (props) => {
   const { dispatch, linode, backup } = props;
@@ -20,7 +55,7 @@ export const BackupPage = (props) => {
         linode={linode}
         backup={backup}
         dispatch={dispatch}
-        linodes={this.props.linodes}
+        linodes={props.linodes}
       />
     </div>
   );
@@ -38,31 +73,11 @@ function mapStateToProps(state, props) {
   const { linodes } = state.api;
   const backupId = parseInt(props.match.params.backupId);
 
-  const backups = linode._backups;
-  let backup;
-  if (backups) {
-    if (backups.daily && backups.daily.id === backupId) {
-      backup = backups.daily;
-    }
-
-    if (backups.snapshot) {
-      if (backups.snapshot.current && backups.snapshot.current.id === backupId) {
-        backup = backups.snapshot.current;
-      } else if (backups.snapshot.in_progress && backups.snapshot.in_progress.id === backupId) {
-        backup = backups.snapshot.in_progress;
-      }
-    }
-
-    if (backups.weekly && backups.weekly.length) {
-      if (backups.weekly[0] && backups.weekly[0].id === backupId) {
-        backup = backups.weekly[0];
-      } else if (backups.weekly[1] && backups.weekly[1].id === backupId) {
-        backup = backups.weekly[1];
-      }
-    }
-  }
-
-  return { linode, backup, linodes };
+  return {
+    linode,
+    backup: mergeBackupsThenFindById(backupId, linode._backups),
+    linodes,
+  };
 }
 
 const preloadRequest = async (dispatch) => {
