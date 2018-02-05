f0bbac19b80ef76c10f9764ccdbd8c9be59d529a
Robert Stires <rstires@linode.com>
2018-02-05 16:33:23 -0500

    Add tests for external.js

---
+++ src/api/actionCreatorGenerator.js
@@ -0,0 +1,53 @@
+import { DELETE, MANY, ONE, PUT, POST } from './internal';
+import {
+  genThunkOne,
+  genThunkPage,
+  genThunkAll,
+  genThunkDelete,
+  genThunkPut,
+  genThunkPost,
+} from './external';
+
+/**
+ * @typedef {Object} ApiActionReducer
+ * @prop {Object} one
+ * @prop {Object} page
+ * @prop {Object} all
+ * @prop {Object} delete
+ * @prop {Object} put
+ * @prop {Object} post
+ *
+ * Generates thunks for the provided config.
+ *
+ * @param { { supports: Array<string>} } config
+ * @param { Object } actions
+ * @returns {ApiActionReducer}
+ */
+export default function actionCreatorGenerator(config, actions) {
+  const thunks = {};
+  const supports = a => config.supports.indexOf(a) !== -1;
+  if (supports(ONE)) {
+    thunks.one = genThunkOne(config, actions);
+  }
+  if (supports(MANY)) {
+    thunks.page = genThunkPage(config, actions);
+    thunks.all = genThunkAll(config, actions, thunks.page);
+  }
+  if (supports(DELETE)) {
+    thunks.delete = genThunkDelete(config, actions);
+  }
+  if (supports(PUT)) {
+    thunks.put = genThunkPut(config, actions);
+  }
+  if (supports(POST)) {
+    thunks.post = genThunkPost(config, actions);
+  }
+  if (config.subresources) {
+    Object.keys(config.subresources).forEach((key) => {
+      const subresource = config.subresources[key];
+      thunks[subresource.name] = actionCreatorGenerator(subresource, actions[subresource.name]);
+    });
+  }
+  thunks.type = config.name;
+  return thunks;
+}
