b401fd825fbd23290d82c378931c7ebf4bc72953
Robert Stires <rstires@linode.com>
2018-02-08 14:47:05 -0500

    Additional testing of ReducerGenerator.

--- src/api/internal.spec.js
+++ src/api/internal.spec.js
@@ -12,10 +12,13 @@ import {
   generateDefaultStateOne,
   createDefaultState,
   parseIntIfActualInt,
+  ReducerGenerator,
 } from './internal';
 
 import { testConfigOne, testConfigMany, testConfigDelete } from '~/data/reduxGen';
 import { resource, page } from '~/data/reduxGen';
+
+const now = new Date();
 
 describe('internal', () => {
   describe('createDefaultState', () => {
@@ -157,9 +160,8 @@ describe('internal', () => {
   describe('genActions', () => {
     it('generates an action to update one resource', function () {
       const config = testConfigOne;
-
       const actions = genActions(config);
-      const actionOne = actions.one(resource, '23');
+      const actionOne = actions.one(resource, '23', now);
 
       expect(actionOne.resource.label).toBe('nodebalancer-1');
       expect(actionOne.type).toBe('GEN@nodebalancers/ONE');
@@ -285,5 +287,347 @@ describe('internal', () => {
     });
   });
 
-  // describe('ReducerGenerator');
+  describe('ReducerGenerator', () => {
+    describe('#one', () => {
+      it('should return state merged with action.resource if config does not support MANY', () => {
+        const config = {
+          supports: ['ONE'],
+        };
+
+        const state = {
+          stateKey: 'state',
+        };
+
+        const action = {
+          resource: {
+            resourceKey: 'resource',
+          },
+        };
+
+        const result = ReducerGenerator.one(config, state, action, now);
+
+        const expected = {
+          stateKey: 'state',
+          resourceKey: 'resource',
+        };
+
+        expect(result).toEqual(expected);
+      });
+
+      describe('when action does not contain IDs.', () => {
+        it('should use the primaryKey label in the config', () => {
+          const config = { supports: [MANY], primaryKey: 'id', name: 'test' };
+          const state = { stateKey: 'state', test: {} };
+          const action = {
+            resource: { id: 1, name: 'one' },
+          };
+
+          const result = ReducerGenerator.one(config, state, action, now);
+          const expected = {
+            stateKey: 'state',
+            test: {
+              1: {
+                id: 1,
+                name: 'one',
+                __updatedAt: now,
+              },
+            },
+          };
+
+          expect(result).toEqual(expected);
+        });
+      });
+
+      describe('when action contains IDs', () => {
+        it('should use the last ID provided as the new object key.', () => {
+          const config = { supports: [MANY], name: 'test' };
+          const state = { stateKey: 'state', test: {} };
+          const action = {
+            resource: { id: 1, name: 'one' },
+            ids: [999],
+          };
+
+          const result = ReducerGenerator.one(config, state, action, now);
+          const expected = {
+            stateKey: 'state',
+            test: {
+              999: {
+                id: 1,
+                name: 'one',
+                __updatedAt: now,
+              },
+            },
+          };
+
+          expect(result).toEqual(expected);
+        });
+      });
+
+      describe('when state contains an existing resource', () => {
+        it('should merge new resource onto old resource', () => {
+          const config = { supports: [MANY], name: 'test' };
+          const state = {
+            stateKey: 'state', test: {
+              999: { something: 'whatever', name: 'oldName' },
+            },
+          };
+          const action = {
+            resource: { id: 1, name: 'one' },
+            ids: [999],
+          };
+
+          const result = ReducerGenerator.one(config, state, action, now);
+          const expected = {
+            stateKey: 'state',
+            test: {
+              999: {
+                id: 1,
+                name: 'one',
+                something: 'whatever',
+                __updatedAt: now,
+              },
+            },
+          };
+
+          expect(result).toEqual(expected);
+        });
+      });
+
+      describe('when state does not contains an existing resource', () => {
+        it('should ...', () => {
+          const _sub1 = { supports: [MANY], name: 'sub1' };
+          const config = { supports: [MANY], name: 'test', subresources: { _sub1 } };
+          const state = {
+            stateKey: 'state', test: {},
+          };
+          const action = {
+            resource: { id: 1, name: 'one' },
+            ids: [999],
+          };
+
+          const result = ReducerGenerator.one(config, state, action, now);
+          const expected = {
+            stateKey: 'state',
+            test: {
+              999: {
+                ...(generateDefaultStateOne(config.subresources, action.resource)),
+                id: 1,
+                name: 'one',
+                __updatedAt: now,
+              },
+            },
+          };
+
+          expect(result).toEqual(expected);
+        });
+      });
+    });
+
+    describe('#many', () => {
+      describe('with simple config', () => {
+        const config = { supports: [MANY], name: 'test', primaryKey: 'id' };
+        const state = { stateKey: 'state', test: {} };
+        const action = {
+          page: {
+            test: [
+              { id: 234, name: '234' },
+              { id: 567, name: '567' },
+              { id: 890, name: '890' },
+            ],
+            pages: 5,
+            results: 100,
+          },
+        };
+
+        const result = ReducerGenerator.many(config, state, action, now);
+
+        it('should return a new state with resources', () => {
+          expect(result).toHaveProperty('test', {
+            234: { id: 234, name: '234', __updatedAt: now },
+            567: { id: 567, name: '567', __updatedAt: now },
+            890: { id: 890, name: '890', __updatedAt: now },
+          });
+        });
+
+        it('should return IDs', () => {
+          expect(result).toHaveProperty('ids', [234, 567, 890]);
+        });
+
+        it.skip('should return sorted IDs', () => { })
+
+        it('should return totalPages', () => {
+          expect(result).toHaveProperty('totalPages', 5);
+        });
+
+        it('should should return totalResults', () => {
+          expect(result).toHaveProperty('totalResults', 100);
+        });
+      });
+
+      describe('with a sort config', () => {
+        const config = {
+          supports: [MANY],
+          name: 'test',
+          primaryKey: 'id',
+          sortFn: (ids) => ids.sort((a, b) => {
+            return b - a;
+          }),
+        };
+        const state = { stateKey: 'state', test: {} };
+        const action = {
+          page: {
+            test: [
+              { id: 234, name: '234' },
+              { id: 567, name: '567' },
+              { id: 890, name: '890' },
+            ],
+            pages: 5,
+            results: 100,
+          },
+        };
+
+        const result = ReducerGenerator.many(config, state, action);
+        it('should return a sorted list', () => {
+          expect(result.ids[0]).toBe(890);
+          expect(result.ids[1]).toBe(567);
+          expect(result.ids[2]).toBe(234);
+        });
+      });
+    });
+
+    describe('#del', () => {
+      describe('with simple values', () => {
+        const config = { name: 'test' };
+        const state = {
+          stateKey: 'state',
+          test: {
+            5: { id: 5 },
+            7: { id: 7 },
+            9: { id: 9 },
+          },
+        };
+        const action = { ids: [5] };
+        const result = ReducerGenerator.del(config, state, action);
+
+        it('should returns updated IDs', () => {
+          expect(result).toHaveProperty('ids', [7, 9]);
+        });
+
+        it('should return updated state', () => {
+          expect(result).toHaveProperty('test', {
+            7: { id: 7 },
+            9: { id: 9 },
+          });
+        });
+
+        it('should leaves other state untouched.', () => {
+          expect(result).toHaveProperty('stateKey', 'state');
+        });
+      });
+    });
+
+    describe('#subresource', () => { });
+
+    describe('#reducer', () => {
+      beforeEach(() => {
+        jest.resetModules();
+      });
+
+      describe('when matches ONE', () => {
+        it('should call ReducerGenerator.one', () => {
+          jest.doMock('./internal.js');
+          ReducerGenerator.one = jest.fn();
+
+          const config = { name: 'shenanigans', supports: [] };
+          const state = { state: 'some state' };
+          const action = { type: 'GEN@shenanigans/ONE' };
+
+          ReducerGenerator.reducer(config, state, action);
+          expect(ReducerGenerator.one).toHaveBeenCalledWith(config, state, action);
+        });
+      });
+
+      describe('when matches MANY', () => {
+        it('should call ReducerGenerator.many', () => {
+          jest.doMock('./internal.js');
+          ReducerGenerator.many = jest.fn();
+
+          const config = { name: 'shenanigans', supports: [] };
+          const state = { state: 'some state' };
+          const action = { type: 'GEN@shenanigans/MANY' };
+
+          ReducerGenerator.reducer(config, state, action);
+          expect(ReducerGenerator.many).toHaveBeenLastCalledWith(config, state, action);
+        });
+      });
+
+      describe('when matches DELETE', () => {
+        it('should call ReducerGenerator.delete', () => {
+          jest.doMock('./internal.js');
+          ReducerGenerator.del = jest.fn();
+
+          const config = { name: 'shenanigans', supports: [] };
+          const state = { state: 'some state' };
+          const action = { type: 'GEN@shenanigans/DELETE' };
+
+          ReducerGenerator.reducer(config, state, action);
+          expect(ReducerGenerator.del).toHaveBeenLastCalledWith(config, state, action);
+        });
+      });
+
+      describe('when defaults', () => {
+        describe('and is a subresource', () => {
+          it('should call ReducerGenerator.subresource', () => {
+            jest.doMock('./internal.js');
+            ReducerGenerator.subresource = jest.fn();
+
+            const config = { name: 'whatever', supports: [], parent: { name: 'something' } };
+            const state = { state: 'some state' };
+            const action = { type: 'GEN@something.whatever/' };
+
+            ReducerGenerator.reducer(config, state, action);
+            expect(ReducerGenerator.subresource).toHaveBeenLastCalledWith(config, state, action);
+          });
+        });
+
+        describe('and is not a subresouce', () => {
+          it('should return unmodified state', () => {
+            const config = { name: 'whatever' };
+            const state = { state: 'some state' };
+            const action = { type: 'anything-else-in-the-world' };
+
+            const result = ReducerGenerator.reducer(config, state, action);
+            expect(result).toBe(state);
+          });
+        });
+      });
+    });
+
+    describe('constructor', () => {
+      beforeEach(() => {
+        jest.resetModules();
+      });
+
+      it('should have a class method reducer', () => {
+        const config = { supports: [ONE] };
+        const result = new ReducerGenerator(config);
+
+        expect(result).toBeInstanceOf(ReducerGenerator);
+        expect(result).toHaveProperty('reducer');
+      });
+
+      it('should call ReducerGenerator.reducer when invoked', () => {
+        const config = { supports: [ONE] };
+        const state = { state: 'something... something... state' };
+        const action = { type: 'something-action-y' };
+        jest.doMock('./internal.js');
+        ReducerGenerator.reducer = jest.fn();
+
+        const reducer = new ReducerGenerator(config);
+        reducer.reducer(state, action);
+
+        expect(ReducerGenerator.reducer).toHaveBeenCalledWith(config, state, action);
+      });
+    });
+  });
 });
