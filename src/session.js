60610365ad68925debefc4a23efefd3e830a443d
Andrew Sauber <a3sauber@gmail.com>
2018-02-14 13:42:15 -0500

    refresh OAuth token using 'silent authentication'

--- src/session.js
+++ src/session.js
@@ -2,22 +2,13 @@ import { stringify } from 'querystring';
 import { v4 } from 'uuid';
 
 import { setToken } from '~/actions/authentication';
-import { APP_ROOT, LOGIN_ROOT } from '~/constants';
+import { APP_ROOT, LOGIN_ROOT, OAUTH_TOKEN_REFRESH_INTERVAL } from '~/constants';
 import { clientId } from '~/secrets';
 import { getStorage, setStorage } from '~/storage';
 
 const AUTH_TOKEN = 'authentication/oauth-token';
 const AUTH_SCOPES = 'authentication/scopes';
 const AUTH_EXPIRES = 'authentication/expires';
-
-
-export function expire(dispatch) {
-  // Remove these from local storage so if login fails, next time we jump to login sooner.
-  setStorage(AUTH_TOKEN, '');
-  setStorage(AUTH_SCOPES, '');
-  setStorage(AUTH_EXPIRES, '');
-  dispatch(setToken(null, null));
-}
 
 export function start(oauthToken = '', scopes = '', expires) {
   return (dispatch) => {
@@ -31,27 +22,18 @@ export function start(oauthToken = '', scopes = '', expires) {
   };
 }
 
-export const genOAuthEndpoint = (redirectUri, scope = '*', nonce) => {
-  const query = {
-    client_id: clientId,
-    scope,
-    response_type: 'token',
-    redirect_uri: `${APP_ROOT}/oauth/callback?returnTo=${redirectUri}`,
-    state: nonce,
-  };
+export function refresh(dispatch) {
+  const authToken = getStorage(AUTH_TOKEN);
+  const scopes = getStorage(AUTH_SCOPES);
+  dispatch(setToken(authToken, scopes));
+}
 
-  return `${LOGIN_ROOT}/oauth/authorize?${stringify(query)}`;
-};
-
-export const prepareOAuthEndpoint = (redirectUri, scope = '*') => {
-  const nonce = v4();
-  setStorage('authentication/nonce', nonce);
-  genOAuthEndpoint(redirectUri, scope, nonce);
-};
-
-export function redirectToLogin(path, querystring) {
-  const redirectUri = `${path}${querystring && `%3F${querystring}`}`;
-  window.location = prepareOAuthEndpoint(redirectUri);
+export function expire(dispatch) {
+  // Remove these from local storage so if login fails, next time we jump to login sooner.
+  setStorage(AUTH_TOKEN, '');
+  setStorage(AUTH_SCOPES, '');
+  setStorage(AUTH_EXPIRES, '');
+  dispatch(setToken(null, null));
 }
 
 export function initialize(dispatch) {
@@ -65,4 +47,46 @@ export function initialize(dispatch) {
   const scopes = getStorage(AUTH_SCOPES) || null;
   // Calling this makes sure AUTH_EXPIRES is always set.
   dispatch(start(token, scopes, expires));
+}
+
+export function genOAuthEndpoint(redirectUri, scope = '*', nonce) {
+  const query = {
+    client_id: clientId,
+    scope,
+    response_type: 'token',
+    redirect_uri: `${APP_ROOT}/oauth/callback?returnTo=${redirectUri}`,
+    state: nonce,
+  };
+
+  return `${LOGIN_ROOT}/oauth/authorize?${stringify(query)}`;
+}
+
+export function prepareOAuthEndpoint(redirectUri, scope = '*') {
+  const nonce = v4();
+  setStorage('authentication/nonce', nonce);
+  return genOAuthEndpoint(redirectUri, scope, nonce);
+}
+
+export function redirectToLogin(path, querystring) {
+  const redirectUri = `${path}${querystring && `%3F${querystring}`}`;
+  window.location = prepareOAuthEndpoint(redirectUri);
+}
+
+export function refreshOAuthToken(dispatch) {
+  /**
+   * Open an iframe for two purposes
+   * 1. Hits the login service (extends the lifetime of login session)
+   * 2. Refreshes our OAuth token in localStorage
+   */
+  const iframe = document.createElement('iframe');
+  iframe.src = prepareOAuthEndpoint('/null');
+  iframe.style.display = 'none';
+  const iframeContainer = document.getElementById('session-iframe');
+  iframeContainer.appendChild(iframe);
+  // Remove the iframe once it refreshes OAuth token in localStorage
+  setTimeout(() => iframeContainer.removeChild(iframe), 5000);
+  // Move the OAuth token from localStorage into Redux
+  dispatch(refresh);
+  // Do this again in a little while
+  setTimeout(() => dispatch(refreshOAuthToken), OAUTH_TOKEN_REFRESH_INTERVAL);
 }
