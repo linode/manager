98b674b89d3c3798faef5b1457deace7892b9c8c
Robert Stires <rstires@linode.com>
2018-02-19 07:58:44 -0500

    Build updates (#2967)
    * Move chartjs to own bundle.
    
    * Strip moment locale data.
    
    * Correct manifest import with specific module import.
    
    * User react-loadable to load CodeEditor.
    
    CodeEditor uses react-ace and brace which are pretty large. Using loadable
    allows us to code split easily.
    
    * Remove unused async CCP.
    
    * Extract env variable handling to parts.
    
    * Extract index.html path to new paths.file. Add HTML minfication.
    
    * Extract manifest generation to parts.
    
    * Update S?CSS handling in dev and prod.
    
    * Use react-loadable to deferring loading LineGraph.js
    
    * Remove chartjs chunk.
    
    * gzip local dev server

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,37 +1,46 @@
-const process = require('process');
+const path = require('path');
 const webpack = require('webpack');
 const _ = require('./webpack.config.dev.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const _package = require('./package.json');
+const autoprefixer = require('autoprefixer');
 
+const parts = require('./config/webpack.parts');
+const paths = require('./config/paths');
+
+const cssFilename = '[name].[contenthash:8].css';
 _.entry = './src/index';
 _.plugins = [
-  /** Will not work if ExtractTextPlugin is removed from module.ruls */
-  new ExtractTextPlugin('[name]-[hash].css'),
-  new HtmlWebpackPlugin({
-    template: 'src/index.html',
+
+  new ExtractTextPlugin({
+    filename: cssFilename,
   }),
 
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'manifest',
+  new HtmlWebpackPlugin({
+    inject: true,
+    template: paths.appHtml,
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      keepClosingSlash: true,
+      minifyJS: true,
+      minifyCSS: true,
+      minifyURLs: true,
+    },
   }),
+
+  parts.manifest,
+
+  parts.ignoreMomentLocales,
 
   new webpack.HashedModuleIdsPlugin(),
 
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-    ENV_DEVTOOLS_DISABLED: JSON.stringify(process.env.DEVTOOLS_DISBLED),
-    ENV_APP_ROOT: JSON.stringify(process.env.APP_ROOT),
-    ENV_API_ROOT: JSON.stringify(process.env.API_ROOT),
-    ENV_LOGIN_ROOT: JSON.stringify(process.env.LOGIN_ROOT),
-    ENV_LISH_ROOT: JSON.stringify(process.env.LISH_ROOT),
-    ENV_GA_ID: JSON.stringify(process.env.GA_ID),
-    ENV_SENTRY_URL: JSON.stringify(process.env.SENTRY_URL),
-    ENV_VERSION: JSON.stringify(_package.version),
-  }),
+  parts.envVariables('production'),
+
   new webpack.optimize.UglifyJsPlugin({
     sourceMap: true,
     compressor: {
@@ -40,5 +49,83 @@ _.plugins = [
   }),
   new webpack.optimize.AggressiveMergingPlugin(),
 ];
+
+_.module = {
+  rules: [
+    {
+      oneOf: [
+        {
+          test: /\.md$/,
+          use: ['ignore-loader'],
+        },
+        {
+          test: /\.json$/,
+          use: ['json-loader'],
+        },
+        {
+          test: /\.s?css$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: {
+              loader: require.resolve('style-loader'),
+              options: {
+                hmr: false,
+              },
+            },
+            use: [
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  minimize: true,
+                  sourceMap: false,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  includePaths: [
+                    path.resolve(__dirname, './node_modules/bootstrap/scss/'),
+                  ],
+                },
+              },
+            ],
+          }),
+        },
+        {
+          test: /\.jsx?/,
+          loader: require.resolve('babel-loader'),
+          include: [
+            path.resolve(__dirname, 'src'),
+            path.resolve(__dirname, 'node_modules/react-vnc-display'),
+          ],
+        },
+        {
+          exclude: [/\.js$/, /\.html$/, /\.json$/],
+          use: ['file-loader'],
+          include: path.join(__dirname, 'node_modules'),
+        },
+      ],
+    },
+  ],
+};
 
 module.exports = _;
