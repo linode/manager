58cc104beda6550b0a430511d658d877e4fa884b
Robert Stires <rstires@linode.com>
2018-02-20 14:42:30 -0500

    Add react-router-dom.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './store';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root') as HTMLElement,
 );
