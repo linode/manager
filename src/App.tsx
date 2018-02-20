9ce9f18f7f076b0b528c575598ddcec573fd9236
Andrew Sauber <a3sauber@gmail.com>
2018-02-20 18:44:43 -0500

    begin implementing side navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,142 @@
 import * as React from 'react';
-import './App.css';
 
-import logoSvg from 'src/assets/logo/logo.svg';
+import { withStyles } from 'material-ui/styles';
+import Reboot from 'material-ui/Reboot';
+import AppBar from 'material-ui/AppBar';
+import Typography from 'material-ui/Typography';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import Hidden from 'material-ui/Hidden';
+import Drawer from 'material-ui/Drawer';
 
-class App extends React.Component {
+import MenuIcon from 'material-ui-icons/Menu';
+
+import PrimaryNav from 'src/components/PrimaryNav';
+
+const drawerWidth = 250;
+
+const styles = (theme: any): any => ({
+  root: {
+    width: '100%',
+    height: '100%',
+    zIndex: 1,
+    overflow: 'hidden',
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  appBar: {
+    backgroundColor: '#333',
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerPaper: {
+    height: '100%',
+    width: 250,
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      position: 'relative',
+      height: '100%',
+    },
+  },
+  drawerDocked: {
+    height: '100%',
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing.unit * 3,
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
+});
+
+class App extends React.Component<any, any> {
+  state = {
+    drawerOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({
+      drawerOpen: !this.state.drawerOpen,
+    });
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Project Voldemort</h1>
-        </header>
-        <img src={logoSvg} alt="Linode Logo"/>
-      </div>
+      <React.Fragment>
+        <Reboot />
+        <div className={classes.root}>
+          <div className={classes.appFrame}>
+            <AppBar className={classes.appBar}>
+              <Toolbar>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={this.handleDrawerToggle}
+                  className={classes.navIconHide}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="title" color="inherit" noWrap>
+                  Linode Manager
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Hidden mdUp>
+              <Drawer
+                variant="temporary"
+                open={this.state.drawerOpen}
+                classes={{
+                  paper: classes.drawerPaper,
+                }}
+                onClose={this.handleDrawerToggle}
+                ModalProps={{
+                  keepMounted: true, // Better open performance on mobile.
+                }}
+              >
+                <PrimaryNav />
+              </Drawer>
+            </Hidden>
+            <Hidden smDown implementation="css">
+              <Drawer
+                variant="permanent"
+                open
+                classes={{
+                  paper: classes.drawerPaper,
+                  docked: classes.drawerDocked,
+                }}
+              >
+                <PrimaryNav />
+              </Drawer>
+            </Hidden>
+            <main className={classes.content}>
+              <Typography variant="display1" noWrap>
+                Linode
+              </Typography>
+            </main>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
