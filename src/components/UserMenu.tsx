ffad55315fd321533e65a4bc5a180015f8406d0e
Andrew Sauber <a3sauber@gmail.com>
2018-02-22 15:03:48 -0500

    move UserMenu to its own component, dynamically generate menu items, add routes

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,52 +1,107 @@
 import * as React from 'react';
+import {
+  withRouter,
+  RouteComponentProps,
+} from 'react-router-dom';
+import { compose } from 'redux';
 
 import {
   withStyles,
-  StyledComponentProps,
+  WithStyles,
+  Theme,
+  StyleRules,
 } from 'material-ui/styles';
-import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
 
-import { menuWidth } from 'src/components/SideMenu';
 import { TodoAny } from 'src/utils';
 
-const styles = (theme: any): any => ({
-  appBar: {
-    backgroundColor: '#333',
-    position: 'absolute',
-    marginLeft: menuWidth,
-    [theme.breakpoints.up('md')]: {
-      width: `calc(100% - ${menuWidth}px)`,
-    },
-  },
-  navIconHide: {
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
+type MenuLink = {
+  display: string,
+  href: string,
+};
+
+const menuLinks: MenuLink[] = [
+  { display: 'Edit Profile', href: '/profile' },
+  { display: 'Log Out', href: '/logout' },
+];
+
+const styles = (theme: Theme): StyleRules => ({
+  leftIcon: {
+    marginRight: theme.spacing.unit,
   },
 });
 
-type Props = StyledComponentProps & {
-  toggleSideMenu: () => void, 
-};
+type Props = WithStyles<'leftIcon'> & RouteComponentProps<{}>;
 
-class TopMenu extends React.Component<Props> {
+interface State {
+  anchorEl?: HTMLElement;
+}
+
+class UserMenu extends React.Component<Props, State> {
+  state = {
+    anchorEl: undefined,
+  };
+
+  handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: undefined });
+  }
+
+  navigate(href: string) {
+    const { history } = this.props;
+    history.push(href);
+    this.handleClose();
+  }
+
+  renderMenuLink(menuLink: MenuLink) {
+    return (
+      <MenuItem key={menuLink.display} onClick={() => this.navigate(menuLink.href)}>
+        {menuLink.display}
+      </MenuItem>
+    );
+  }
+
   render() {
     const { classes } = this.props;
-
-    if (!classes) {
-      return null;
-    }
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
 
     return (
-      <div>
-        <Typography variant="button" align="right">
-          Hello
-        </Typography>
-      </div>
+      <React.Fragment>
+        <Button
+          onClick={this.handleMenu}
+          color="inherit"
+        >
+          <AccountCircle className={classes.leftIcon}/>
+          jsmith
+        </Button>
+        <Menu
+          anchorEl={anchorEl}
+          getContentAnchorEl={undefined}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={open}
+          onClose={this.handleClose}
+        >
+          {menuLinks.map(menuLink => this.renderMenuLink(menuLink))}
+        </Menu>
+      </React.Fragment>
     );
   }
 }
 
-export default withStyles(styles, { withTheme: true })(
-  TopMenu as TodoAny,
-) as TodoAny;
+export default compose<TodoAny, TodoAny, TodoAny>(
+  withStyles(styles, { withTheme: true }),
+  withRouter,
+)(UserMenu) as TodoAny;
