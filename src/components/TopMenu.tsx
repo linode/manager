e557438391e83f7b1198243ebcc2c4bf1f74ff4d
Andrew Sauber <a3sauber@gmail.com>
2018-02-22 13:47:07 -0500

    Add UserMenu menu and interaction

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -9,12 +9,15 @@ import {
 import AppBar from 'material-ui/AppBar';
 import Typography from 'material-ui/Typography';
 import Toolbar from 'material-ui/Toolbar';
+import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
+import Menu, { MenuItem } from 'material-ui/Menu';
 
 import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
 
 import { menuWidth } from 'src/components/SideMenu';
-import UserMenu from 'src/components/UserMenu';
+// import UserMenu from 'src/components/UserMenu';
 import { TodoAny } from 'src/utils';
 
 const styles = (theme: Theme): StyleRules => ({
@@ -34,15 +37,32 @@ const styles = (theme: Theme): StyleRules => ({
       display: 'none',
     },
   },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
 });
 
-interface Props extends WithStyles<'appBar' | 'navIconHide' | 'flex'> {
+interface Props extends WithStyles<'appBar' | 'navIconHide' | 'flex' | 'leftIcon'> {
   toggleSideMenu: () => void;
 }
 
 class TopMenu extends React.Component<Props> {
+  state = {
+    anchorEl: undefined,
+  };
+
+  handleMenu = (event: any) => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: undefined });
+  }
+
   render() {
     const { classes, toggleSideMenu } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
 
     return (
       <AppBar className={classes.appBar}>
@@ -58,7 +78,31 @@ class TopMenu extends React.Component<Props> {
           <Typography variant="title" color="inherit" className={classes.flex}>
             Linode Manager
           </Typography>
-          <UserMenu />
+          <div>
+            <Button
+              onClick={this.handleMenu}
+              color="inherit"
+            >
+              <AccountCircle className={classes.leftIcon}/>
+              jsmith
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={this.handleClose}>Edit Profile</MenuItem>
+              <MenuItem onClick={this.handleClose}>Log Out</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     );
