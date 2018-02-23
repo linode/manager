dbaf8cd517abce9b351181ba29cb2bd5cc592db7
Robert Stires <rstires@linode.com>
2018-02-23 17:22:23 -0500

    Add temporary route with Typography.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import {
   Theme,
 } from 'material-ui/styles';
 import Reboot from 'material-ui/Reboot';
+import Typography from 'material-ui/Typography';
+
 
 import TopMenu from 'src/components/TopMenu';
 import SideMenu from 'src/components/SideMenu';
@@ -41,6 +43,21 @@ const styles = (theme: Theme): StyleRules => ({
 
 type Props = WithStyles<'appFrame' | 'content'>;
 
+/**
+ * Temoporary route.
+ */
+const TempRoute = (props: any) => {
+  const { render, ...rest } = props;
+  return <Route
+    {...rest}
+    render={renderProps => (
+      <Typography variant="display1">
+        {render(renderProps)}
+      </Typography>
+    )}
+  />;
+};
+
 class App extends React.Component<Props> {
   state = {
     menuOpen: false,
@@ -63,19 +80,19 @@ class App extends React.Component<Props> {
           <TopMenu toggleSideMenu={this.toggleMenu} />
           <SideMenu open={menuOpen} toggle={this.toggleMenu} />
           <main className={classes.content}>
-              <Switch>
-                <Route exact path="/dashboard" render={() => 'Dashboard'} />
+            <Switch>
+              <TempRoute exact path="/dashboard" render={() => 'Dashboard'} />
               <Route exact path="/linodes" component={ListLinodes} />
-                <Route exact path="/volumes" render={() => 'Volumes'} />
-                <Route exact path="/nodebalancers" render={() => 'NodeBalancers'} />
-                <Route exact path="/domains" render={() => 'Domains'} />
-                <Route exact path="/managed" render={() => 'Managed'} />
-                <Route exact path="/longview" render={() => 'LongView'} />
-                <Route exact path="/stackscripts" render={() => 'StackScripts'} />
-                <Route exact path="/images" render={() => 'Images'} />
-                <Route exact path="/profile" render={() => 'Profile'} />
-                <Route exact path="/" render={() => (<Redirect to="/dashboard" />)} />
-              </Switch>
+              <TempRoute exact path="/volumes" render={() => 'Volumes'} />
+              <TempRoute exact path="/nodebalancers" render={() => 'NodeBalancers'} />
+              <TempRoute exact path="/domains" render={() => 'Domains'} />
+              <TempRoute exact path="/managed" render={() => 'Managed'} />
+              <TempRoute exact path="/longview" render={() => 'LongView'} />
+              <TempRoute exact path="/stackscripts" render={() => 'StackScripts'} />
+              <TempRoute exact path="/images" render={() => 'Images'} />
+              <TempRoute exact path="/profile" render={() => 'Profile'} />
+              <Route exact path="/" render={() => (<Redirect to="/dashboard" />)} />
+            </Switch>
           </main>
         </div>
       </React.Fragment>
