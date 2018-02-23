417b45f378ca882721dc581a6ebc30d5b02585d6
Robert Stires <rstires@linode.com>
2018-02-23 09:36:32 -0500

    M3-7 Add Google Analytics for pageviews. (#2979)
    * Add Google Analytics for pageviews.
    
    * Update pathIsOneOf test and docs.

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,7 @@
+const PRODUCTION = 'production';
+
+export const GA_ID = process.env.REACT_APP_GA_ID;
+
+export const isProduction = process.env.NODE_ENV === PRODUCTION;
+
 export const SENTRY_URL = process.env.REACT_APP_SENTRY_URL;
