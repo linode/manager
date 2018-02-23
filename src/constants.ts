80dea7b7bffca2d76e01d1b78baec9c69f1ffa59
Andrew Sauber <a3sauber@gmail.com>
2018-02-23 15:20:38 -0500

    add OAuth callback, Logout route, unthunk session start

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,11 @@ export const GA_ID = process.env.REACT_APP_GA_ID;
 
 export const isProduction = process.env.NODE_ENV === PRODUCTION;
 
-export const SENTRY_URL = process.env.REACT_APP_SENTRY_URL;
 export const APP_ROOT = process.env.REACT_APP_APP_ROOT || 'http://localhost:3000';
 export const LOGIN_ROOT = process.env.REACT_APP_LOGIN_ROOT || 'https://login.linode.com';
-
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+
+export const SENTRY_URL = process.env.REACT_APP_SENTRY_URL;
 
 export const LOGIN_SESSION_LIFETIME_MS = 45 * 60 * 1000;
 export const OAUTH_TOKEN_REFRESH_INTERVAL = LOGIN_SESSION_LIFETIME_MS / 2;
