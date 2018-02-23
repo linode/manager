97ba7e88485359e38c2549dcc6402a2ba0eeb5be
Robert Stires <rstires@linode.com>
2018-02-23 09:36:32 -0500

    M3-7 Add Google Analytics for pageviews. (#2979)
    * Add Google Analytics for pageviews.
    
    * Update pathIsOneOf test and docs.

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,33 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import isPathOneOf from 'src/utilities/routing/isPathOneOf';
+import store from 'src/store';
+import { isProduction, GA_ID } from 'src/constants';
+import analytics from 'src/analytics';
+
 import 'src/exceptionReporting';
 
 import store from './store';
 import App from './App';
 import './index.css';
+
+/**
+ * Initialize Analytics.
+ */
+analytics(GA_ID, isProduction);
+
+/**
+ * Send pageviews unless blacklisted.
+ */
+createBrowserHistory().listen(({ pathname }) => {
+  /** https://palantir.github.io/tslint/rules/strict-boolean-expressions/ */
+  if (isPathOneOf(['/oauth'], pathname) === false) {
+    (window as any).ga('send', 'pageview');
+  }
+});
 
 ReactDOM.render(
   <Provider store={store}>
