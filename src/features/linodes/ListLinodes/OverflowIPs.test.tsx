748f6c092f680da4b2fa5b06ffe3739ad0e6a20b
Andrew Sauber <a3sauber@gmail.com>
2018-03-01 16:28:20 -0500

    add tests for IPAddress and OverflowIPs

---
+++ src/features/linodes/ListLinodes/OverflowIPs.test.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import OverflowIPs from './OverflowIPs';
+
+describe('OverflowIPs', () => {
+  it('should render without error and display the number of IPs', () => {
+    const result = mount(
+      <OverflowIPs
+        ips={['8.8.8.8']}
+      />,
+    );
+    const rendered = result.find('OverflowIPs');
+    const numberText = result.find('span').text();
+
+    expect(rendered).toHaveLength(1);
+    expect(numberText).toBe('+1');
+  });
+
+  it('should render each IPAddress when the chip is clicked', () => {
+    const result = mount(
+      <OverflowIPs
+        ips={['8.8.8.8', '8.8.4.4', '192.168.100.112']}
+      />,
+    );
+    const chip = result.find('Chip');
+    chip.simulate('click');
+    const ipAddresses = result.find('IPAddress');
+
+    expect(ipAddresses).toHaveLength(3);
+  });
+});
