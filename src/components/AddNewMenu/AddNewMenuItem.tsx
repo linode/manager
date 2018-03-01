979b5891c86b55faee944e1520c1f7a8e5e22340
Robert Stires <rstires@linode.com>
2018-03-01 10:28:19 -0500

    Initial pass at AddNewMenu. (#2990)

---
+++ src/components/AddNewMenu/AddNewMenuItem.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+
+import {
+  withStyles,
+  WithStyles,
+  StyleRulesCallback,
+  Theme,
+} from 'material-ui';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
+
+type CSSClasses = 'root';
+
+const styles: StyleRulesCallback = (theme: Theme) => ({
+  root: {
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2,
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    maxWidth: '315px',
+    cursor: 'pointer',
+  },
+});
+
+export interface MenuItem {
+  title: string;
+  onClick: (e: React.MouseEvent<HTMLElement>) => void;
+  body: string;
+}
+
+interface Props extends MenuItem {
+  index: number;
+  count: number;
+}
+
+interface State {
+  anchorEl?: HTMLElement;
+}
+
+type PropsWithStyles = Props & WithStyles<CSSClasses>;
+
+class AddNewMenuItem extends React.Component<PropsWithStyles, State> {
+  render() {
+    const { classes, title, onClick, body, index, count } = this.props;
+
+    return (
+      <React.Fragment>
+        <div onClick={onClick} className={classes.root}>
+          <Typography><a href="#">{title}</a></Typography>
+          <Typography>{body}</Typography>
+        </div>
+        {index + 1 !== count && <Divider />}
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default withStyles(styles, { withTheme: true })<Props>(AddNewMenuItem);
