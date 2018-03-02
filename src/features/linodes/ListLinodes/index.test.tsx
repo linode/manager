dbc18217ecaf35368a3bbcbf85259d25669150cd
Andrew Sauber <a3sauber@gmail.com>
2018-03-02 12:10:06 -0500

    additional tests for ListLinodes page

--- src/features/linodes/ListLinodes/index.test.tsx
+++ src/features/linodes/ListLinodes/index.test.tsx
@@ -17,4 +17,38 @@ describe('ListLinodes', () => {
       </StaticRouter>,
     );
   });
+
+  it('renders an empty state with no linodes', () => {
+    const component = mount(
+      <StaticRouter location="/" context={{}}>
+        <RoutedListLinodes
+          linodes={[]}
+          images={images as Linode.Image[]}
+          types={types as Linode.LinodeType[]}
+        />
+      </StaticRouter>,
+    );
+
+    const emptyState = component.find('ListLinodesEmptyState');
+
+    expect(emptyState).toHaveLength(1);
+  });
+
+  it('renders menu actions when the kabob is clicked', () => {
+    const component = mount(
+      <StaticRouter location="/" context={{}}>
+        <RoutedListLinodes
+          linodes={linodes as Linode.Linode[]}
+          images={images as Linode.Image[]}
+          types={types as Linode.LinodeType[]}
+        />
+      </StaticRouter>,
+    );
+
+    const kabobButton = component.find('MoreHoriz').first();
+    kabobButton.simulate('click');
+
+    const menuItems = component.find('MenuItem');
+    expect(menuItems.length).toBe(7);
+  });
 });
