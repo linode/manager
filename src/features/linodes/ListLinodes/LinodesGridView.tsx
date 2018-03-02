2528352a45692735f49421c65bb7d756cf4628f2
Robert Stires <rstires@linode.com>
2018-03-01 19:42:42 -0500

    Add LinodesGridView.

---
+++ src/features/linodes/ListLinodes/LinodesGridView.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import Grid from 'material-ui/Grid';
+
+import LinodeCard from './LinodeCard';
+
+interface Props {
+  linodes: Linode.Linode[];
+  images: Linode.Image[];
+  types: Linode.LinodeType[];
+}
+
+const LinodesGridView: React.StatelessComponent<Props> = (props) => {
+  const { linodes, images, types } = props;
+
+  return (
+    <Grid container>
+      {linodes.map(linode =>
+        <LinodeCard
+          key={linode.id}
+          linode={linode}
+          image={images.find(image => linode.image === image.id)}
+          type={types.find(type => linode.type === type.id)}
+        />,
+      )}
+    </Grid>
+  );
+};
+
+export default LinodesGridView;
