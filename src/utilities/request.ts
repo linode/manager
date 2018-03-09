e08ceb1c7ef1db0422549f9cdeae23a81747d84e
Andrew Sauber <a3sauber@gmail.com>
2018-03-09 13:49:07 -0500

    add axios interceptor to redirect to login on 401 (#3017)

--- src/utilities/request.ts
+++ src/utilities/request.ts
@@ -1,5 +1,6 @@
-import Axios, { AxiosRequestConfig } from 'axios';
+import Axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 import store from 'src/store';
+import { expire } from 'src/session';
 
 Axios.interceptors.request.use((config: AxiosRequestConfig): AxiosRequestConfig => {
   const token = store.getState().authentication.token;
@@ -12,3 +13,27 @@ Axios.interceptors.request.use((config: AxiosRequestConfig): AxiosRequestConfig 
     },
   };
 });
+
+/*
+Interceptor that initiates re-authentication if:
+  * The response is HTTP 401 "Unauthorized"
+  * The API is in Maintainence mode
+Also rejects non-error responses if the API is in Maintainence mode
+*/
+Axios.interceptors.response.use(
+  (response: AxiosResponse) => {
+    if (!!response.headers['x-maintenance-mode']) {
+      expire();
+      Promise.reject(response);
+    }
+
+    return response;
+  },
+  (error: AxiosError) => {
+    if (!!error.config.headers['x-maintenance-mode']
+        || (error.response && error.response.status === 401)) {
+      expire();
+    }
+    return Promise.reject(error);
+  },
+);
