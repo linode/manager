f8c3971e2e6247d1a938c28d5cf04ec04d5d157d
Andrew Sauber <a3sauber@gmail.com>
2018-03-12 13:48:05 -0400

    event polling based on timeout

--- src/events.ts
+++ src/events.ts
@@ -7,11 +7,37 @@ function createDatestamp() {
   return moment().utc().format('YYYY-MM-DDTHH:mm:ss');
 }
 
+const linodeEvents$ = new Rx.Subject();
+
 let datestamp = createDatestamp();
+let pollIDs: number[] = [];
+
+/*
+ * Until we can filter on IDs, we can't use this logic
+function generateInFilter(keyName: string, arr: any[]) {
+  return {
+    '+or': arr.map(el => ({ [keyName]: el })),
+  };
+}
+*/
+
+function generatePollingFilter() {
+  /*
+   * Until we can filter on IDs, we can't use this logic
+  return pollIDs.length ?
+    {
+      '+or': [
+        { created: { '+gt': datestamp } },
+        generateInFilter('id', pollIDs),
+      ],
+    }
+    : { created: { '+gt': datestamp } };
+  */
+  return { created: { '+gt': datestamp } };
+}
 
 type EventResponse = AxiosResponse<Linode.ManyResourceState<Linode.Event>>;
 
-/** Get events. */
 const initial$ = Rx.Observable
   .defer(() =>
     Rx.Observable
@@ -22,20 +48,20 @@ const initial$ = Rx.Observable
           ),
   ));
 
-/** Get events. */
 const polling$ = Rx.Observable
-  .interval(5000)
+  .interval(2000)
   .flatMap(() =>
     Rx.Observable
       .fromPromise(
         Axios.get(
           `${API_ROOT}/account/events`,
-          { headers: { 'X-Filter': JSON.stringify({ created: { '+gt': datestamp } }) } })
-          .then((response: EventResponse) => response.data.data),
+          { headers: { 'X-Filter': JSON.stringify(
+            generatePollingFilter(),
+          ) } })
+        .then((response: EventResponse) => response.data.data),
       ),
   );
 
-const linodeEvents$ = new Rx.Subject();
 
 const stream$: Rx.Observable<void | null> = Rx.Observable
   .merge(initial$, polling$)
@@ -45,7 +71,19 @@ const stream$: Rx.Observable<void | null> = Rx.Observable
         datestamp = value[0].created;
       }
 
-      value.reverse().map(linEv => linodeEvents$.next(linEv));
+      value.reverse().map((linEv) => {
+        // if an Event completes it is removed from pollIDs
+        if (linEv.percent_complete === 100 && pollIDs.includes(linEv.id)) {
+          pollIDs = pollIDs.filter(id => id !== linEv.id);
+        }
+
+        // we poll for Event IDs that have not yet been completed
+        if (linEv.percent_complete !== null && linEv.percent_complete < 100) {
+          pollIDs.push(linEv.id);
+        }
+
+        linodeEvents$.next(linEv);
+      });
     },
     null,
   );
