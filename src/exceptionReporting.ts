bcdd13a35053c7b59327ae5d00a37d5303ec623c
Robert Stires <rstires@linode.com>
2018-03-13 10:51:07 -0400

    Report unhandled Promise rejections to raven. (#3031)

--- src/exceptionReporting.ts
+++ src/exceptionReporting.ts
@@ -1,6 +1,10 @@
 import * as Raven from 'raven-js';
 import { SENTRY_URL } from 'src/constants';
 
+window.addEventListener('unhandledrejection', (err: PromiseRejectionEvent) => {
+  Raven.captureException(err.reason);
+});
+
 if (SENTRY_URL) {
   Raven
     .config(SENTRY_URL)
