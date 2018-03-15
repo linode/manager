1454b84a7032ef315ae51aeb6b6b2d7105047f7a
Alban Bailly <abailly@linode.com>
2018-03-15 17:02:19 -0400

    styling tabs and configure story

--- src/components/TabbedPanel/TabbedPanel.stories.tsx
+++ src/components/TabbedPanel/TabbedPanel.stories.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import TabbedPanel from './TabbedPanel';
+import ThemeDecorator from '../../utilities/storybookDecorators';
 
 storiesOf('TabbedPanel', module)
+  .addDecorator(ThemeDecorator)
   .add('default', () => {
 
     return (
@@ -13,17 +15,25 @@ storiesOf('TabbedPanel', module)
       copy="This is an example of a tabbed panel."
       tabs={[
         {
-          title: 'One',
+          title: 'Tab One',
           render: (renderProps: { someOtherProp: number }) =>
             <div>Panel 1 {renderProps.someOtherProp}</div>,
         },
         {
-          title: 'Two',
+          title: 'Tab Two',
           render: (renderProps: any) => <div>Panel 2</div>,
         },
         {
-          title: 'Three',
+          title: 'Tab Three',
           render: (renderProps: any) => <div>Panel 3</div>,
+        },
+        {
+          title: 'Tab Four',
+          render: (renderProps: any) => <div>Panel 4</div>,
+        },
+        {
+          title: 'Tab Five',
+          render: (renderProps: any) => <div>Panel 5</div>,
         },
       ]}
     />);
