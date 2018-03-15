cd98b1efa4977afe4967a791d9f0f89160701302
Andrew Sauber <a3sauber@gmail.com>
2018-03-15 12:12:24 -0400

    add theme decorator for storybook

---
+++ src/utilities/storybookDecorators.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {
+  MuiThemeProvider, 
+  createMuiTheme,
+} from 'material-ui/styles';
+import LinodeTheme from '../theme';
+
+const theme = createMuiTheme(LinodeTheme as Linode.TodoAny);
+theme.shadows = theme.shadows.fill('none');
+
+const ThemeDecorator = (storyFn: Function) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      { storyFn() }
+    </MuiThemeProvider>
+  );
+};
+
+export default ThemeDecorator;
