f9535954221a2ef084e10d6d4acc66e5783b7413
Andrew Sauber <a3sauber@gmail.com>
2018-03-15 14:20:13 -0400

    use ThemeDecorator for TextField stories

--- src/components/TextField.stories.tsx
+++ src/components/TextField.stories.tsx
@@ -1,70 +1,53 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import {
-    MuiThemeProvider, 
-    createMuiTheme,
-  } from 'material-ui/styles';
-
-import LinodeTheme from '../../src/theme';
+import ThemeDecorator from '../utilities/storybookDecorators';
 import TextField from './TextField';
 
-const theme = createMuiTheme(LinodeTheme as Linode.TodoAny);
-theme.shadows = theme.shadows.fill('none');
-
 storiesOf('TextField', module)
+.addDecorator(ThemeDecorator)
 .add('Normal', () => (
-  <MuiThemeProvider theme={theme}>
-    <TextField
-      label="Input Label"
-      placeholder="Normal State"
-    >
-      Normal State
-    </TextField>
-  </MuiThemeProvider>
+  <TextField
+    label="Input Label"
+    placeholder="Normal State"
+  >
+    Normal State
+  </TextField>
 ))
 .add('Active', () => (
-  <MuiThemeProvider theme={theme}>
-    <TextField
-      label="Input Label"
-      placeholder="Active State"
-      autoFocus
-    >
-      Active State
-    </TextField>
-  </MuiThemeProvider>
+  <TextField
+    label="Input Label"
+    placeholder="Active State"
+    autoFocus
+  >
+    Active State
+  </TextField>
 ))
 .add('Error', () => (
-  <MuiThemeProvider theme={theme}>
-    <TextField
-      label="Input Label"
-      placeholder="Error State"
-      errorText="This input needs further attention"
-    >
-      Error State
-    </TextField>
-  </MuiThemeProvider>
+  <TextField
+    label="Input Label"
+    placeholder="Error State"
+    errorText="This input needs further attention"
+  >
+    Error State
+  </TextField>
 ))
 .add('Affirmative', () => (
-  <MuiThemeProvider theme={theme}>
-    <TextField
-      label="Input Label"
-      placeholder="Affirmative State"
-      affirmative
-    >
-      Affirmative State
-    </TextField>
-  </MuiThemeProvider>
+  <TextField
+    label="Input Label"
+    placeholder="Affirmative State"
+    affirmative
+  >
+    Affirmative State
+  </TextField>
 ))
 .add('Disabled', () => (
-  <MuiThemeProvider theme={theme}>
-    <TextField
-      label="Input Label"
-      placeholder="Disabled State"
-      disabled
-    >
-      Disabled State
-    </TextField>
-  </MuiThemeProvider>
+  <TextField
+    label="Input Label"
+    placeholder="Disabled State"
+    disabled
+  >
+    Disabled State
+  </TextField>
 ))
 ;
