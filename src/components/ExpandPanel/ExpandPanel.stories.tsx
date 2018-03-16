94690aa2315708d2215ef6f2a4e4e1cf773d2164
Andrew Sauber <a3sauber@gmail.com>
2018-03-16 15:36:37 -0400

    M3-41 ExpandPanel component (#3052)
    * add ExpandPanel component
    
    * styling and story config
    
    * fix linting issue
    
    * Remove Fragment.

---
+++ src/components/ExpandPanel/ExpandPanel.stories.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Typography from 'material-ui/Typography';
+
+import ThemeDecorator from '../../utilities/storybookDecorators';
+import ExpandPanel from './ExpandPanel';
+
+storiesOf('Expand Panel', module)
+.addDecorator(ThemeDecorator)
+.add('default', () => (
+  <ExpandPanel name="Show Older Images">
+    <Typography>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </Typography>
+  </ExpandPanel>
+));
---
+++ src/components/ExpandPanel/ExpandPanel.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+
+import {
+  withStyles,
+  WithStyles,
+  StyleRulesCallback,
+  Theme,
+} from 'material-ui';
+import Collapse from 'material-ui/transitions/Collapse';
+
+import KeyboardArrowRight from 'material-ui-icons/KeyboardArrowRight';
+
+type CSSClasses = 'root' | 'panel' | 'header' | 'caret' ;
+
+const styles: StyleRulesCallback = (theme: Theme & Linode.Theme) => ({
+  root: {
+    padding: `${theme.spacing.unit}px 0`,
+  },
+  header: {
+    cursor: 'pointer',
+    userSelect: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    fontWeight: 700,
+  },
+  caret: {
+    color: theme.palette.primary.main,
+    marginRight: theme.spacing.unit,
+    fontSize: '26px',
+    transition: 'transform .1s ease-in-out',
+    '&.rotate': {
+      transition: 'transform .3s ease-in-out',
+      transform: 'rotate(90deg)',
+    },
+  },
+  panel: {
+    padding: theme.spacing.unit,
+  },
+});
+
+interface Props {
+  name: string;
+}
+
+interface State {
+  open: boolean;
+}
+
+type FinalProps = Props & WithStyles<CSSClasses>;
+
+class ExpandPanel extends React.Component<FinalProps, State> {
+  state = {
+    open: false,
+  };
+
+  handleNameClick = () => {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  render() {
+    const { name, classes, children } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.header} onClick={this.handleNameClick}>
+          {open
+            ? <KeyboardArrowRight className={classes.caret + ' rotate'} />
+            : <KeyboardArrowRight className={classes.caret}  />
+          }
+          {name}
+        </div>
+        <Collapse in={open}>
+          {open
+            ? (
+              <div className={classes.panel}>
+                {children}
+              </div>
+            )
+            : null
+          }
+        </Collapse>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })<Props>(ExpandPanel);
