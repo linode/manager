daaa3507add17806f8229960017cb247b9032361
Andrew Sauber <a3sauber@gmail.com>
2018-03-16 14:52:17 -0400

    being SelectionCard component, add Linux Distro icon font

---
+++ src/components/SelectionCard/SelectionCard.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+
+import {
+  withStyles,
+  StyleRulesCallback,
+  Theme,
+  WithStyles,
+} from 'material-ui';
+
+import Grid from 'material-ui/Grid';
+
+type CSSClasses = 
+'root'
+| 'icon'
+| 'heading'
+| 'innerGrid'
+| 'subheading';
+
+const styles: StyleRulesCallback<CSSClasses> = (theme: Theme) => ({
+  root: {
+    maxWidth: '290px',
+    minHeight: '64px',
+    border: '1px solid #333',
+  },
+  icon: {
+    padding: '0 !important',
+    '& svg': {
+      fontSize: '32px',
+    },
+    '& span': {
+      fontSize: '32px',
+    },
+    '& img': {
+      maxheight: '32px',
+      maxWidth: '32px',
+    },
+  },
+  heading: {
+    fontWeight: 700,
+    fontSize: '1em', 
+  },
+  subheading: {
+    fontSize: '0.8em',
+  },
+  innerGrid: {
+    margin: theme.spacing.unit * 2,
+  },
+});
+
+const styled = withStyles(styles, { withTheme: true });
+
+interface Props {
+  renderIcon: () => JSX.Element;
+  heading: string;
+  subheadings: string[];
+}
+
+type CombinedProps = Props & WithStyles<CSSClasses>;
+
+const SelectionCard: React.StatelessComponent<CombinedProps> = (props) => {
+  const {
+    renderIcon,
+    heading,
+    subheadings,
+    classes,
+  } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container className={classes.innerGrid}>
+        <Grid item xs={2} className={classes.icon}>
+          {renderIcon()}
+        </Grid>
+        <Grid item xs={10} className="p0">
+          <div className={classes.heading}>
+            {heading} 
+          </div>
+          {subheadings.map((subheading) => {
+            return (
+              <div className={classes.subheading}>
+                {subheading}
+              </div>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default styled(SelectionCard);
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'font-logos/assets/font-logos.css';
 
 import createBrowserHistory from 'history/createBrowserHistory';
 
