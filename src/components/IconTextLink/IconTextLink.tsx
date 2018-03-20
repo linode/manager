7314f0437157df1b8b80bf0ae5fa8b5eb70e7430
Andrew Sauber <a3sauber@gmail.com>
2018-03-20 15:38:27 -0400

    begin icon text link component

---
+++ src/components/IconTextLink/IconTextLink.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  withStyles,
+  StyleRulesCallback,
+  WithStyles,
+  Theme,
+} from 'material-ui';
+import { SvgIconProps } from 'material-ui/SvgIcon';
+
+type CSSClasses = 'root' | 'text';
+
+const styles: StyleRulesCallback<CSSClasses> = (theme: Theme & Linode.Theme) => ({
+  root: {
+    display: 'inline-block',
+  },
+  text: {
+
+  },
+});
+
+interface Props {
+  SideIcon: React.ComponentClass<SvgIconProps>;
+  text: string;
+  to?: string;
+  onClick?: () => void;
+}
+
+type FinalProps = Props & WithStyles<CSSClasses>;
+
+const IconTextLink: React.StatelessComponent<FinalProps> = (props) => {
+  const { SideIcon, classes, text, to, onClick } = props;
+
+  return (
+    <div className={classes.root}>
+      <SideIcon />
+      {to &&
+        <Link to={to} className={classes.text}>
+          {text}
+        </Link>
+      }
+      {onClick &&
+        <span onClick={onClick} className={classes.text}>
+          {text}
+        </span>
+      }
+    </div>
+  );
+};
+
+export default withStyles(styles, { withTheme: true })(IconTextLink);
