eaf8a658fd240b65f2a8734ae4aaad81ae67d3aa
Andrew Sauber <a3sauber@gmail.com>
2018-03-21 14:44:01 -0400

    flesh out all states for CheckBox

--- src/components/CheckBox/CheckBox.stories.tsx
+++ src/components/CheckBox/CheckBox.stories.tsx
@@ -6,25 +6,73 @@ import CheckBox from './CheckBox';
 
 class InteractiveCheckboxes extends React.Component {
   state = {
-    checked: false,
+    checkedDefault: false,
+    checkedWarning: false,
+    checkedError: false,
   };
 
-  handleClick = () => {
-    this.setState({ checked: !this.state.checked });
+  handleClick = (type: string) => {
+    this.setState({ [type]: !this.state[type] });
   }
 
   render() {
     return (
       <React.Fragment>
         <CheckBox
-          checked={this.state.checked}
-          onClick={this.handleClick}
+          disabled
+          checked={false}
+          onClick={() => this.handleClick('checkedDefault')}
         />
         <span style={{ marginLeft: '8px' }} />
         <CheckBox
           disabled
+          checked={true}
+          onClick={() => this.handleClick('checkedDefault')}
+        />
+        <span style={{ marginLeft: '8px' }} />
+        <CheckBox
+          checked={this.state.checkedDefault}
+          onClick={() => this.handleClick('checkedDefault')}
+        />
+        <br /><br />
+        <CheckBox
+          variant="warning"
+          disabled
           checked={false}
-          onClick={this.handleClick}
+          onClick={() => this.handleClick('checkedWarning')}
+        />
+        <span style={{ marginLeft: '8px' }} />
+        <CheckBox
+          disabled
+          variant="warning"
+          checked={true}
+          onClick={() => this.handleClick('checkedDefault')}
+        />
+        <span style={{ marginLeft: '8px' }} />
+        <CheckBox
+          variant="warning"
+          checked={this.state.checkedWarning}
+          onClick={() => this.handleClick('checkedWarning')}
+        />
+        <br /><br />
+        <CheckBox
+          variant="error"
+          disabled
+          checked={false}
+          onClick={() => this.handleClick('checkedError')}
+        />
+        <span style={{ marginLeft: '8px' }} />
+        <CheckBox
+          disabled
+          variant="error"
+          checked={true}
+          onClick={() => this.handleClick('checkedDefault')}
+        />
+        <span style={{ marginLeft: '8px' }} />
+        <CheckBox
+          variant="error"
+          checked={this.state.checkedError}
+          onClick={() => this.handleClick('checkedError')}
         />
       </React.Fragment>
     );
