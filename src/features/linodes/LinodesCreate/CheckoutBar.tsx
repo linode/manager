60c8ded744132d66d9281015652f3f078c7bcc7d
Andrew Sauber <a3sauber@gmail.com>
2018-03-22 11:53:02 -0400

    continue working on Linode checkout bar

--- src/features/linodes/LinodesCreate/CheckoutBar.tsx
+++ src/features/linodes/LinodesCreate/CheckoutBar.tsx
@@ -6,6 +6,11 @@ import {
   Theme,
   StyleRules,
 } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+
+import LinodeTheme from 'src/theme';
+
+type ClassNames = 'root' | 'sidebarTitle';
 
 const styles = (theme: Theme & Linode.Theme): StyleRules => ({
   root: {
@@ -15,20 +20,32 @@ const styles = (theme: Theme & Linode.Theme): StyleRules => ({
     border: '1px solid black',
     backgroundColor: '#fff',
   },
+  sidebarTitle: {
+    fontSize: '1.5rem',
+    color: LinodeTheme.color.green,
+  },
 });
 
 interface Props {
   onDeploy: () => void;
+  label: string | null;
 }
 
-type CombinedProps = Props & StickyProps & WithStyles<'root'>;
+type CombinedProps = Props & StickyProps & WithStyles<ClassNames>;
 
 class CheckoutBar extends React.Component<CombinedProps> {
   render() {
-    const { classes, style, onDeploy } = this.props;
+    const {
+      classes,
+      style,
+      onDeploy,
+      label,
+    } = this.props;
     return (
       <div className={classes.root} style={style} onClick={onDeploy}>
-        I'm sticky!!
+        <Typography variant="title" className={classes.sidebarTitle}>
+          {label || 'Linode'} Summary
+        </Typography>
       </div>
     );
   }
