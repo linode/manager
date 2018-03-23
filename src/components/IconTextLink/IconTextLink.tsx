8649dd3b4e88ce24b9ea7b098bf5d24a99f52359
Andrew Sauber <a3sauber@gmail.com>
2018-03-23 09:36:45 -0400

    M3-118 Icon and Text Link (#3072)
    * begin icon text link component
    
    * continue with IconTextLink
    
    * add disabled and active state to IconTextLink
    
    * styling and story config

---
+++ src/components/IconTextLink/IconTextLink.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import * as classNames from 'classnames';
+
+import {
+  withStyles,
+  StyleRulesCallback,
+  WithStyles,
+  Theme,
+} from 'material-ui';
+import SvgIcon from 'material-ui/SvgIcon';
+
+type CSSClasses = 'root' | 'active' | 'disabled' | 'icon';
+
+const styles: StyleRulesCallback<CSSClasses> = (theme: Theme & Linode.Theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    cursor: 'pointer',
+    color: theme.palette.primary.main,
+    transition: 'color 225ms ease-in-out',
+    '&:hover, &:hover $icon': {
+      color: theme.palette.primary.light,
+    },
+    '&:hover $icon': {
+      borderColor: theme.palette.primary.light,
+    },
+  },
+  active: {
+    color: '#1f64b6',
+  },
+  disabled: {
+    color: '#939598',
+    pointerEvents: 'none',
+    '& $icon': {
+      color: '#939598',
+      borderColor: '#939598',
+    },
+  },
+  icon: {
+    fontSize: 18,
+    marginRight: 5,
+    color: theme.palette.primary.main,
+  },
+});
+
+interface Props {
+  SideIcon: typeof SvgIcon | React.ComponentClass;
+  text: string;
+  onClick: () => void;
+  active?: Boolean;
+  disabled?: Boolean;
+  title: string;
+}
+
+type FinalProps = Props & WithStyles<CSSClasses>;
+
+const IconTextLink: React.StatelessComponent<FinalProps> = (props) => {
+  const {
+    SideIcon,
+    classes,
+    text,
+    onClick,
+    active,
+    disabled,
+    title,
+  } = props;
+
+  return (
+    <a
+      className={
+        classNames({
+          [classes.root]: true,
+          [classes.disabled]: disabled === true,
+          [classes.active]: active === true,
+        })
+      }
+      title={title}
+      onClick={onClick}
+    >
+      <SideIcon className={classes.icon} />
+      <span>
+        {text}
+      </span>
+    </a>
+  );
+};
+
+export default withStyles(styles, { withTheme: true })(IconTextLink);
