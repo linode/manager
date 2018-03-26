16124f2bb030cda0120a18a2c02038f46ff1d467
Patrick Thiel <pthiel@linode.com>
2018-03-26 14:10:59 -0400

    Install wdio test framework, add test npm scripts, set min node version

---
+++ e2e/config/browser-config.js
@@ -0,0 +1,63 @@
+exports.browserConf = {
+    chrome: {
+        browserName: 'chrome',
+        maxInstances: 5,
+        acceptSslCerts: true,
+        acceptInsecureCerts: true,
+        chromeOptions: {
+        }
+    },
+    headlessChrome: {
+        browserName: 'chrome',
+        maxInstances: 5,
+        acceptSslCerts: true,
+        acceptInsecureCerts: true,
+        chromeOptions: {
+            args: [
+                'headless','no-sandbox','disable-gpu',
+                'window-size=1920x1080','--allow-running-insecure-content',
+                '--ignore-certificate-errors',
+            ]
+        }
+    },
+    firefox: {
+        browserName: 'firefox',
+        marionette: true,
+        maxInstances: 5,
+        acceptInsecureCerts: true
+    },
+    headlessFirefox: {
+        browserName: 'firefox',
+        marionette: true,
+        'moz:firefoxOptions' : {
+            'binary': '/Applications/Firefox Nightly.app/Contents/MacOS/firefox-bin',
+            'args': ['-headless']
+        },
+        maxInstances: 5,
+        acceptInsecureCerts: true
+    },
+    safari: {
+        browserName: 'safari',
+        'safari.options': {
+            acceptInsecureCerts: true
+        },
+        maxInstances: 1,
+        acceptInsecureCerts: true
+    },
+    safariPreview: {
+        browserName: 'safari',
+        'safari.options': {
+            technologyPreview: true,
+            acceptInsecureCerts: true
+        },
+        maxInstances: 1
+    },
+    mobileSafari: {
+        browserName: 'safari',
+        platformName: 'iOS',
+        platformVersion: '11.2',
+        deviceName: 'iPhone 8',
+        automationName: 'XCUITest',
+        maxInstances: 1
+    }
+}
