678967d577f82882756293144782bd1390d2f885
Alban Bailly <abailly@linode.com>
2018-03-28 16:48:32 -0400

    styling, icon

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 
-import { withStyles, WithStyles, Theme, StyleRulesCallback } from 'material-ui';
+import { Typography, withStyles, WithStyles, Theme, StyleRulesCallback } from 'material-ui';
 import Drawer, { DrawerProps } from 'material-ui/Drawer';
 import Grid from 'material-ui/Grid';
 import Button from 'material-ui/Button';
+import Close from 'material-ui-icons/Close';
 
 interface Props extends DrawerProps {
   title: string;
@@ -12,8 +13,22 @@ interface Props extends DrawerProps {
 type ClassNames = 'drawer' | 'button';
 
 const styles: StyleRulesCallback = (theme: Theme) => ({
-  paper: { minWidth: '480px' },
-  button: {},
+  paper: {
+    minWidth: '480px',
+    padding: theme.spacing.unit * 4,
+  },
+  button: {
+    minWidth: 'auto',
+    minHeight: 'auto',
+    padding: 0,
+    '& > span': {
+      padding: 2,
+    },
+    '& :hover, & :focus': {
+      color: 'white',
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
 });
 
 type CombinedProps = Props & WithStyles<ClassNames>;
@@ -28,16 +43,20 @@ const DDrawer: React.StatelessComponent<CombinedProps> = (props) => {
       classes={{ paper: classes.paper }}
       ModalProps={{ BackdropProps: { invisible: true } }}
       >
-      <Grid container>
-        <Grid item xs={11}>{title}</Grid>
-        <Grid item xs={2}>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <Typography variant="title">
+            {title}
+          </Typography>
+        </Grid>
+        <Grid item>
           <Button
             variant="raised"
             color="secondary"
             onClick={props.onClose}
             className={classes.button}
           >
-            x
+          <Close />
           </Button>
         </Grid>
       </Grid>
