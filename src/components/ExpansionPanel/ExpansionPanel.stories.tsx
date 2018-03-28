92879584bb595bcfb87cd7d12514ce3f43561582
Robert Stires <rstires@linode.com>
2018-03-28 18:41:20 -0400

    Add Notice to ExpansionPanel. Update theme with status colors.

--- src/components/ExpansionPanel/ExpansionPanel.stories.tsx
+++ src/components/ExpansionPanel/ExpansionPanel.stories.tsx
@@ -19,10 +19,49 @@ storiesOf('ExpansionPanel', module)
     </Grid>
   </Grid>
 ))
-.add('Interactive with actions.', () => (
+.add('Success!', () => (
   <Grid container alignItems="center">
     <Grid item xs={4}>
       <ExpansionPanel
+        success="You did it!"
+        heading="Why is Linode the best?"
+        actions={props => (
+            <ActionsPanel>
+              <Button variant="raised" color="primary">Save</Button>
+              <Button>Cancel</Button>
+            </ActionsPanel>
+          )
+        }
+      >
+        <p>Customer service!</p>
+      </ExpansionPanel>
+    </Grid>
+  </Grid>
+))
+.add('Warning!', () => (
+  <Grid container alignItems="center">
+    <Grid item xs={4}>
+      <ExpansionPanel
+        warning="Careful now..."
+        heading="Why is Linode the best?"
+        actions={props => (
+            <ActionsPanel>
+              <Button variant="raised" color="primary">Save</Button>
+              <Button>Cancel</Button>
+            </ActionsPanel>
+          )
+        }
+      >
+        <p>Customer service!</p>
+      </ExpansionPanel>
+    </Grid>
+  </Grid>
+))
+.add('Error!', () => (
+  <Grid container alignItems="center">
+    <Grid item xs={4}>
+      <ExpansionPanel
+        error="Oh no! Something broke!"
         heading="Why is Linode the best?"
         actions={props => (
             <ActionsPanel>
--- src/components/Notice/Notice.tsx
+++ src/components/Notice/Notice.tsx
@@ -1,36 +1,40 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
 
-import { withStyles, StyleRulesCallback, WithStyles, Theme } from 'material-ui';
+import { withStyles, StyleRulesCallback, WithStyles } from 'material-ui';
 
 type ClassNames = 'root'
   | 'error'
   | 'warning'
   | 'success';
 
-const styles: StyleRulesCallback<ClassNames> = (theme: Theme & Linode.Theme) => ({
-  root: {
-    paddingLeft: theme.spacing.unit * 3,
-    paddingRight: theme.spacing.unit * 3,
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-    /* TODO: remove this negative margin and apply to consuming Panel components */
-    marginTop: theme.spacing.unit * -1,
-    marginBottom: theme.spacing.unit * 1 + 2,
-  },
-  error: {
-    backgroundColor: '#f8dedf',
-    border: '1px solid #970d0d',
-  },
-  warning: {
-    backgroundColor: '#fdf4da',
-    border: '1px solid #ffd002',
-  },
-  success: {
-    backgroundColor: '#d9e6f5',
-    border: '1px solid #3682dd',
-  },
-});
+const styles: StyleRulesCallback = (theme: Linode.Theme) => {
+  const { spacing, palette: { status } } = theme;
+
+  return {
+    root: {
+      paddingLeft: spacing.unit * 3,
+      paddingRight: spacing.unit * 3,
+      paddingTop: spacing.unit * 2,
+      paddingBottom: spacing.unit * 2,
+      /* TODO: remove this negative margin and apply to consuming Panel components */
+      marginTop: spacing.unit * -1,
+      marginBottom: spacing.unit * 1 + 2,
+    },
+    error: {
+      backgroundColor: status.error,
+      border: `1px solid ${status.errorDark}`,
+    },
+    warning: {
+      backgroundColor: status.warning,
+      border: `1px solid ${status.warningDark}`,
+    },
+    success: {
+      backgroundColor: status.success,
+      border: `1px solid ${status.successDark}`,
+    },
+  };
+};
 
 interface Props {
   error?: boolean;
@@ -39,9 +43,9 @@ interface Props {
   text: string;
 }
 
-type FinalProps = Props & WithStyles<ClassNames>;
+type CombinedProps = Props & WithStyles<ClassNames>;
 
-const ErrorBanner: React.StatelessComponent<FinalProps> = (props) => {
+const Notice: React.StatelessComponent<CombinedProps> = (props) => {
   const { classes, text, error, warning, success } = props;
 
   return (
@@ -56,4 +60,6 @@ const ErrorBanner: React.StatelessComponent<FinalProps> = (props) => {
   );
 };
 
-export default withStyles(styles, { withTheme: true })(ErrorBanner);
+const styled = withStyles(styles, { withTheme: true });
+
+export default styled<Props>(Notice);
