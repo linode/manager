4983e02e09fd8799eacef5c909901925bf422668
Andrew Sauber <a3sauber@gmail.com>
2018-03-29 18:40:50 -0400

    add: connect tabs to react router

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -6,25 +6,67 @@ import {
   RouteComponentProps,
   Redirect,
 } from 'react-router-dom';
+import { Location } from 'history';
 import Typography from 'material-ui/Typography';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
 
 import APITokens from './APITokens';
 import OAuthClients from './OAuthClients';
 
 type Props = RouteComponentProps<{ linodeId?: number }>;
 
-class LinodesRoutes extends React.Component<Props> {
+type Tab = {
+  title: string,
+  routeName: string,
+  renderRoute: (path: string) => React.ReactNode;
+};
+
+class Profile extends React.Component<Props> {
+  genTab = (title: string, routeName: string, component: React.ComponentClass): Tab => {
+    return {
+      title,
+      routeName,
+      renderRoute: (path: string) => <Route component={component} path={`${path}/${routeName}`} />,
+    };
+  }
+
+  handleTabChange = (event: React.ChangeEvent<HTMLDivElement>, value: number) => {
+    const { history, match: { path } } = this.props;
+    const routeName = this.tabs[value].routeName;
+    history.push(`${path}/${routeName}`);
+  }
+
+  tabMatches = (location: Location, tab: Tab): boolean => {
+    const locationTail = location.pathname.split('/').pop();
+    return locationTail === tab.routeName;
+  }
+
+  tabs: Tab[] = [
+    this.genTab('API Tokens', 'tokens', APITokens),
+    this.genTab('OAuth Clients', 'clients', OAuthClients),
+  ];
+
   render() {
-    const { match: { path } } = this.props;
+    const { match: { path }, location } = this.props;
 
     return (
       <div>
-        <Typography variant="display1">
+        <Typography variant="headline">
           My Profile
         </Typography>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={this.tabs.findIndex(tab => this.tabMatches(location, tab))}
+            onChange={this.handleTabChange}
+            indicatorColor="primary"
+            textColor="primary"
+          >
+            {this.tabs.map(tab => <Tab key={tab.title} label={tab.title} />)}
+          </Tabs>
+        </AppBar>
         <Switch>
-          <Route component={APITokens} path={`${path}/tokens`}/>
-          <Route component={OAuthClients} path={`${path}/clients`} />
+          {this.tabs.map(tab => tab.renderRoute(path))}
           <Route exact path={`${path}/`} render={() => (<Redirect to={`${path}/tokens`} />)} />
         </Switch>
       </div>
@@ -32,4 +74,4 @@ class LinodesRoutes extends React.Component<Props> {
   }
 }
 
-export default withRouter(LinodesRoutes);
+export default withRouter(Profile);
