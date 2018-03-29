3a418913c21ea914e09d2232018a2759790529f3
Andrew Sauber <a3sauber@gmail.com>
2018-03-29 11:56:33 -0400

    add some test content for the Drawer component

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import ThemeDecorator from '../../utilities/storybookDecorators';
 import Drawer from './Drawer';
 import Button from 'material-ui/Button';
+import TextField from '../TextField';
 
 class Example extends React.Component {
   state = { open: false };
@@ -17,7 +18,25 @@ class Example extends React.Component {
       <React.Fragment>
         <Button onClick={this.toggleDrawer(true)}>Click</Button>
         <Drawer title="My Drawer" open={this.state.open} onClose={this.toggleDrawer(false)} >
-          Click
+          This is some test copy which acts as content for this Drawer component.
+          It's very interesting and you should read all of it. This text has to be sufficiently
+          long to test that it doesn't expand the drawer to an unreasonable width.
+          <TextField
+            label="Input Some Text"
+            placeholder="This is a placeholder"
+          />
+          <Button
+            variant="raised"
+            color="primary"
+            onClick={this.toggleDrawer(true)}
+          >
+            Save
+          </Button>
+          <Button
+            onClick={this.toggleDrawer(true)}
+          >
+            Cancel
+          </Button>
         </Drawer>
       </React.Fragment>
     );
