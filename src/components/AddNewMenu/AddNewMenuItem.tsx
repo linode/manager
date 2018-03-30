6a9f31280f79d55a2c3afffdc70b599ef9001a94
Patrick Thiel <pthiel@linode.com>
2018-03-30 15:16:11 -0400

    chg: Use volumes for src/e2e directories, ignore src/e2e when copying

--- src/components/AddNewMenu/AddNewMenuItem.tsx
+++ src/components/AddNewMenu/AddNewMenuItem.tsx
@@ -96,7 +96,7 @@ class AddNewMenuItem extends React.Component<PropsWithStyles, State> {
 
     return (
       <React.Fragment>
-        <li onClick={onClick} className={classes.root}>
+        <li data-qa-menu-item={title} onClick={onClick} className={classes.root}>
           <div className={classes.iconWrapper}>
             <ItemIcon />
           </div>
