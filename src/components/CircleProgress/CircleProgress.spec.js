95e95873b3fdb931c3b4c2b72582dafa4f4fe294
Patrick Thiel <pthiel@linode.com>
2018-03-30 14:12:58 -0400

    add: circle progress test, add waitForFocus util, update check test to use waitForFocus

---
+++ src/components/CircleProgress/CircleProgress.spec.js
@@ -0,0 +1,44 @@
+const { constants } = require('../../../e2e/constants');
+const { waitForFocus } = require('../../../e2e/utils/storybook');
+
+describe('Circle Progress Indicator Component Suite', () => {
+    beforeAll(() => {
+        browser.url(constants.routes.storybook);
+    });
+
+    it('should display circle progess Indicator in the navigation', () => {
+        const navigationItem = $('[data-name="Circle Progress Indicator"]');
+        expect(navigationItem.isVisible()).toBe(true);
+    });
+
+    it('should display indefinite story', () => {
+        browser.click('[data-name="Circle Progress Indicator"]');
+        waitForFocus('[data-name="Indefinite"]');
+
+        const indefiniteStory = $('[data-name="Indefinite"]');
+        expect(indefiniteStory.isVisible()).toBe(true);
+    });
+
+    it('should display indefinite progress indicator', () => {
+        browser.click('[data-name="Indefinite"]');
+        waitForFocus('[data-qa-circle-progress]');
+
+        const progressIndicator = $('[data-qa-circle-progress]');
+        const role = progressIndicator.getAttribute('role');
+
+        expect(role).toBe('progressbar');
+        expect(progressIndicator.isVisible()).toBe(true)
+    });
+
+    it('should contain an svg child element', () => {
+        waitForFocus('[data-qa-circle-progress]');
+        const svg = $('[data-qa-circle-progress] > svg' );
+        expect(svg.isVisible()).toBe(true);
+    });
+
+    it('should be indefinite variant', () => {
+        waitForFocus('[data-qa-circle-progress]');
+        const svgClasses = $('[data-qa-circle-progress] > svg' ).getAttribute('class');
+        expect(svgClasses).toContain('Indeterminate');
+    });
+});
