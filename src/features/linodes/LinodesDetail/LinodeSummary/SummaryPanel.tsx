569448b1e8f1cfda489ec97cf4942e5a73d2fdf6
Andrew Sauber <a3sauber@gmail.com>
2018-04-05 11:26:05 -0400

    add types for Volumes, flesh out Linode summary panel

--- src/features/linodes/LinodesDetail/LinodeSummary/SummaryPanel.tsx
+++ src/features/linodes/LinodesDetail/LinodeSummary/SummaryPanel.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
 import {
   withStyles,
   StyleRulesCallback,
@@ -11,12 +13,13 @@ import Typography from 'material-ui/Typography';
 import Hidden from 'material-ui/Hidden';
 
 import { typeLabelLong, formatRegion } from 'src/features/linodes/presentation';
+import IPAddress from 'src/features/linodes/LinodesLanding/IPAddress';
 
 type ClassNames = 'root' | 'text';
 
 const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
   root: {
-    padding: theme.spacing.unit * 2,
+    padding: theme.spacing.unit * 3,
     marginTop: theme.spacing.unit * 2,
   },
   text: {
@@ -28,12 +31,13 @@ interface Props {
   linode: Linode.Linode;
   image: Linode.Image;
   type: Linode.LinodeType;
+  volumes: Linode.Volume[];
 }
 
 type CombinedProps = Props & WithStyles<ClassNames>;
 
 const SummaryPanel: React.StatelessComponent<CombinedProps> = (props) => {
-  const { classes, linode, image, type } = props;
+  const { classes, linode, image, type, volumes } = props;
 
   return (
     <Paper className={classes.root}>
@@ -53,34 +57,49 @@ const SummaryPanel: React.StatelessComponent<CombinedProps> = (props) => {
         </Grid>
         <Grid item xs={12} sm={6} lg={4}>
           <Typography className={classes.text} variant="body1">
-            {linode.ipv4}
+            <IPAddress ips={linode.ipv4} copyRight />
           </Typography>
           <Typography className={classes.text} variant="body1">
-            {linode.ipv6}
+            <IPAddress ips={[linode.ipv6]} copyRight />
           </Typography>
         </Grid>
-        <Hidden lgUp>
-          <Grid item xs={12} sm={6}>
-            <Typography className={classes.text} variant="body1">
-              {formatRegion(linode.region)}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography className={classes.text} variant="body1">
-              Volumes: 2
-            </Typography>
-          </Grid>
-        </Hidden>
+
+        {/**
+          * Note: The following pair of Grid items must be kept in sync. The purpose of doing
+          * this is so that at lower breakpoints, these two items, which are not related, dont
+          * stack on top of each other, breaking the appearance of the grid, and, at larger
+          * breakpoints, they don't break the grid in the other direction.
+          **/}
+        {/* This is shown at "large" and above */}
         <Hidden mdDown>
           <Grid item xs={12} sm={6} lg={4}>
             <Typography className={classes.text} variant="body1">
               {formatRegion(linode.region)}
             </Typography>
             <Typography className={classes.text} variant="body1">
-              Volumes: 2
+              Volumes: <Link
+                className="btnLink"
+                to={`/linodes/${linode.id}/volumes`}>{volumes.length}</Link>
             </Typography>
           </Grid>
         </Hidden>
+
+        {/* This is shown at medium and below */}
+        <Hidden lgUp>
+          <Grid item xs={12} sm={6} lg={4}>
+            <Typography className={classes.text} variant="body1">
+              {formatRegion(linode.region)}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={4}>
+            <Typography className={classes.text} variant="body1">
+              Volumes: <Link
+                className="btnLink"
+                to={`/linodes/${linode.id}/volumes`}>{volumes.length}</Link>
+            </Typography>
+          </Grid>
+        </Hidden>
+
       </Grid>
     </Paper>
   );
