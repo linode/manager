353aac8dc6d442222a3392389e9f6bb0d2b3bdc4
Alban Bailly <abailly@linode.com>
2018-04-06 10:18:48 -0400

    add: cicrle progress vertical centering

--- src/components/CircleProgress/CircleProgress.tsx
+++ src/components/CircleProgress/CircleProgress.tsx
@@ -8,38 +8,41 @@ import {
 } from 'material-ui';
 import CircularProgress from 'material-ui/Progress/CircularProgress';
 
-type CSSClasses = 'root' | 'top' | 'progress';
+type CSSClasses = 'root'
+| 'top'
+| 'progress'
+| 'topWrapper';
 
 const styles: StyleRulesCallback<CSSClasses> = (theme: Theme & Linode.Theme) => ({
-
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    margin: '0 auto',
+    flex: 1,
+  },
   progress: {
     position: 'relative',
     '& circle': {
       transition: 'stroke-dasharray .5s linear, stroke-dashoffset .5s linear',
     },
   },
-
   top: {
     width: 120,
     height: 120,
     borderRadius: '50%',
     border: '1px solid #999',
-    position: 'absolute',
-    left: 0,
-    top: 15,
-    right: 0,
-    margin: '0 auto',
   },
-
-  root: {
+  topWrapper: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    position: 'relative',
-    width: 150,
-    height: 150,
-    margin: '0 auto',
-    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
 });
 
@@ -50,11 +53,15 @@ interface Props {
 const CircleProgressComponent = (props: Props & WithStyles<CSSClasses>) => {
   const variant = typeof props.value === 'number' ? 'static' : 'indeterminate';
   const value = typeof props.value === 'number' ? props.value : 0;
+  const { classes } = props;
+
   return (
-    <div className={props.classes.root}>
-      <div className={props.classes.top} />
+    <div className={classes.root}>
+      <div className={classes.topWrapper}>
+        <div className={classes.top} />
+      </div>
       <CircularProgress
-        className={props.classes.progress}
+        className={classes.progress}
         size={150}
         value={value}
         variant={variant}
