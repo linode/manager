9e5e1cc4d4481956b4eaef48f21e30f664d4dfba
Alban Bailly <abailly@linode.com>
2018-04-09 17:08:54 -0400

    --add beta notification styling

--- src/BetaNotification.tsx
+++ src/BetaNotification.tsx
@@ -7,13 +7,33 @@ import {
   WithStyles,
 } from 'material-ui';
 import Button from 'material-ui/Button';
+import Grid from 'src/components/Grid';
 import Snackbar, { SnackbarProps } from 'material-ui/Snackbar';
 
 
-type ClassNames = 'root';
+type ClassNames = 'root' | 'content' | 'actions';
 
 const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
   root: {},
+  content: {
+    textAlign: 'center',
+    [theme.breakpoints.up('md')]: {
+      textAlign: 'left',
+    },
+    '& a': {
+      color: '#000',
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+    },
+  },
+  actions: {
+    display: 'flex',
+    justifyContent: 'center',
+    [theme.breakpoints.up('md')]: {
+      justifyContent: 'flex-end',
+    },
+  },
 });
 
 interface Props extends SnackbarProps {
@@ -27,11 +47,28 @@ const BetaNotification: React.StatelessComponent<CombinedProps> = (props) => {
   return (<Snackbar
     {...rest}
     className={classes.root}
-    message={<span>
-      This is the early-access Linode Manager.<a href="https://manager.linode.com/">Click
-      here</a> to go back to the classic Linode Manager.
-      { onClose && <Button onClick={e => onClose(e, '')}>Close</Button> }
-      </span>}
+    message={
+      <Grid
+        container
+        alignItems="center"
+        justify="space-between"
+      >
+        <Grid item className={classes.content} xs={12} md={9} lg={10}>
+          This is the early-access Linode Manager. <a href="https://manager.linode.com/">Click
+          here</a> to go back to the classic Linode Manager.
+        </Grid>
+        { onClose &&
+        <Grid item className={classes.actions} xs={12} md={3} lg={2}>
+            <Button
+              onClick={e => onClose(e, '')}
+              color="primary"
+              variant="raised"
+            >
+              Close
+            </Button>
+          </Grid>
+        }
+      </Grid>}
   />);
 };
 
