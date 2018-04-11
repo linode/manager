8aa4cebdf79c02ba554bbafc2db340482b78f617
Andrew Sauber <a3sauber@gmail.com>
2018-04-11 16:30:48 -0400

    continue adding Weblish

---
+++ src/features/Weblish/Weblish.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import Axios from 'axios';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { Terminal } from 'xterm';
+import * as fit from 'xterm/lib/addons/fit/fit';
+
+import '../../assets/weblish/weblish.css';
+import '../../assets/weblish/weblish-fonts.css';
+import '../../assets/weblish/xterm.css';
+
+import { ZONES, LISH_ROOT, API_ROOT } from 'src/constants';
+
+export function weblishLaunch(linodeId: string) {
+  window.open(
+    `${window.location.protocol}//${window.location.host}/linodes/${linodeId}/weblish`,
+    `weblish_con_${linodeId}`,
+    'left=100,top=100,width=1024,height=655,toolbar=0,resizable=1',
+  );
+}
+
+interface Props {
+}
+
+interface State {
+  token: string;
+  renderingLish: boolean;
+  linode?: Linode.Linode;
+}
+
+type CombinedProps = Props & RouteComponentProps<{ linodeId?: number }>;
+
+export class Weblish extends React.Component<CombinedProps, State> {
+  state: State = {
+    token: '',
+    renderingLish: false,
+  };
+
+  constructor(props: CombinedProps) {
+    super(props);
+  }
+
+  componentWillMount() {
+    const { match: { params: { linodeId } } } = this.props;
+    Axios.get(`${API_ROOT}/linode/instances/${linodeId}`)
+      .then((response) => {
+        const { data: linode } = response;
+        this.setState({ linode }, this.connect);
+      });
+  }
+
+  getLishSchemeAndHostname(region: string): string {
+    if (LISH_ROOT.startsWith('wss')) {
+      /* Note: This is only the case for pre-production environments! */
+      return LISH_ROOT;
+    }
+    return `wss://${ZONES[region]}.${LISH_ROOT}`;
+  }
+
+  connect() {
+    const { linode } = this.state;
+    if (linode === undefined) {
+      throw new Error('No Linode data before attempting to connect to Weblish.');
+    }
+
+    const { id, region } = linode;
+
+    Axios.post(`${API_ROOT}/linode/instances/${id}/lish_token`)
+      .then((response) => {
+        const { data: { lish_token: token } } = response;
+        this.setState({ token });
+        const socket = new WebSocket(
+          `${this.getLishSchemeAndHostname(region)}:8181/${token}/weblish`);
+        socket.addEventListener('open', () =>
+          this.setState({ renderingLish: true }, () => this.renderTerminal(socket)));
+      });
+  }
+
+  renderTerminal(socket: WebSocket) {
+    const { linode } = this.state;
+    if (linode === undefined) {
+      throw new Error('No Linode data before attempting to render Weblish.');
+    }
+
+    const { group, label } = linode;
+
+    Terminal.applyAddon(fit);
+
+    const terminal = new Terminal({ cols: 120, rows: 40 });
+
+    terminal.on('data', (data: string) => socket.send(data));
+    terminal.open(document.body);
+
+    terminal.writeln('\x1b[32mLinode Lish Console\x1b[m');
+    (terminal as any).fit();
+
+    socket.addEventListener('message', evt => terminal.write(evt.data));
+
+    socket.addEventListener('close', () => {
+      terminal.destroy();
+      this.setState({ renderingLish: false });
+    });
+
+    const linodeLabel = group ? `${group}/${label}` : label;
+    window.document.title = `${linodeLabel} - Linode Lish Console`;
+  }
+
+  render() {
+    return this.state.renderingLish ? null : (
+      <div>
+        <div id="disconnected">
+          <h2>Connection Lost</h2>
+          <p>Lish appears to be temporarily unavailable.</p>
+          <button onClick={() => this.connect()}>Reload &#x27f3;</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Weblish);
