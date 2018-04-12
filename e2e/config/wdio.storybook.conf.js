57f57dd9b44e07314454f050ce11e30cf8a5c0fd
Pat Thiel <patthiel@gmail.com>
2018-04-12 17:28:38 -0400

    M3-329  - Integrate Component tests into build process (#3182)
    * chg: stop container on ctrl-c
    
    * Added: docker compose storybook file and needed config changes

--- e2e/config/wdio.storybook.conf.js
+++ e2e/config/wdio.storybook.conf.js
@@ -8,7 +8,7 @@ const specsToRun = argv.file ? [ argv.file ] : ['./src/components/**/*.spec.js']
 exports.config = merge(wdioMaster.config, {
     specs: specsToRun,
     baseUrl: process.env.DOCKER ? 'http://manager-storybook:6006' : 'http://localhost:6006',
-    maxInstances: 3,
+    maxInstances: process.env.DOCKER ? 1 : 3,
     before: function (capabilities, specs) {
         browserCommands();
         browser.url(constants.routes.storybook);
