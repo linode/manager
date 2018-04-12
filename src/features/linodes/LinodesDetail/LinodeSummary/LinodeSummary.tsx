cb5b66543390077563e17d3dab945d7861aff95e
Andrew Sauber <a3sauber@gmail.com>
2018-04-12 18:46:20 -0400

    flesh out all linode charts

--- src/features/linodes/LinodesDetail/LinodeSummary/LinodeSummary.tsx
+++ src/features/linodes/LinodesDetail/LinodeSummary/LinodeSummary.tsx
@@ -22,11 +22,61 @@ interface State {
 
 type FinalProps = Props;
 
+const chartOptions = {
+  maintainAspectRatio: false,
+  animation: {
+    duration: 0,
+  },
+  legend: {
+    display: false,
+  },
+  scales: {
+    yAxes: [{
+      gridLines: {
+        borderDash: [3, 6],
+        zeroLineWidth: 1,
+        zeroLineBorderDashOffset: 2,
+      },
+    }],
+    xAxes: [{
+      type: 'time',
+      gridLines: {
+        display: false,
+      },
+      time: {
+        displayFormats: {
+          hour: 'HH:00',
+          minute: 'HH:00',
+        },
+      },
+    }],
+  },
+};
+
+const lineOptions = {
+  backgroundColor: 'rgba(0, 0, 0, 0)',
+  borderWidth: 1,
+  borderJoinStyle: 'miter',
+  lineTension: 0,
+  pointRadius: 0,
+  pointHitRadius: 5,
+};
+
 const statToLabel = {
   cpu: 'CPU %',
   netv4: 'IPv4 Traffic',
   netv6: 'IPv6 Traffic',
   io: 'Disk I/O',
+};
+
+const statToColor = {
+  cpu: '#428ade',
+  in: '#3683dc',
+  out: '#01b159',
+  private_in: '#d01e1e',
+  private_out: '#ffd100',
+  io: '#ffd100',
+  swap: '#d01e1e',
 };
 
 class LinodeSummary extends React.Component<FinalProps, State> {
@@ -45,29 +95,16 @@ class LinodeSummary extends React.Component<FinalProps, State> {
     window.setInterval(() => this.getStats(), 15000);
   }
 
-  getChartJSDataFor(stat: string) {
-    const { stats } = this.state;
-    if (!stats) return {};
-
-    const { data: { [stat]: data } } = stats;
-
+  getChartJSDataFor(stat: string, data: [number, number][]) {
     const timeData = data.reduce((acc: any, point: any) => {
       acc.push({ t: point[0], y: point[1] });
       return acc;
     }, []);
 
     return {
-      datasets: [{
-        label: statToLabel[stat],
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        borderColor: '#428ade',
-        borderWidth: 1,
-        borderJoinStyle: 'miter',
-        lineTension: 0,
-        pointRadius: 0,
-        pointHitRadius: 5,
-        data: timeData,
-      }],
+      borderColor: statToColor[stat],
+      data: timeData,
+      ...lineOptions,
     };
   }
 
@@ -80,62 +117,137 @@ class LinodeSummary extends React.Component<FinalProps, State> {
           <LinodeBusyStatus linode={linode} />
         }
         <SummaryPanel linode={linode} type={type} image={image} volumes={volumes} />
+
         <ExpansionPanel
           heading={statToLabel.cpu}
         >
           {stats &&
-            <Line
-              height={400}
-              options={{
-                maintainAspectRatio: false,
-                animation: {
-                  duration: 0,
-                },
-                legend: {
-                  position: 'bottom',
-                },
-                scales: {
-                  yAxes: [{
-                    gridLines: {
-                      borderDash: [3, 6],
-                    },
-                    scaleLabel: {
-                      display: true,
-                      labelString: 'CPU %',
-                    },
-                  }],
-                  xAxes: [{
-                    type: 'time',
-                    gridLines: {
-                      display: false,
-                    },
-                    time: {
-                      displayFormats: {
-                        hour: 'HH:00',
-                        minute: 'HH:00',
-                      },
-                    },
-                  }],
-                },
-              }}
-              data={this.getChartJSDataFor('cpu') as any}
-            />
+            <React.Fragment>
+              <div>
+                CPU %
+              </div>
+              <div>
+                <Line
+                  height={300}
+                  options={chartOptions}
+                  data={{
+                    datasets: [
+                      this.getChartJSDataFor('cpu', stats.data.cpu) as any,
+                    ],
+                  }}
+                />
+              </div>
+              <div>
+                CPU %
+              </div>
+            </React.Fragment>
           }
         </ExpansionPanel>
+
         <ExpansionPanel
           heading="IPv4 Traffic"
         >
-          IPv4 Chart
+          {stats &&
+            <React.Fragment>
+              <div>
+                bits/sec
+              </div>
+              <div>
+                <Line
+                  height={300}
+                  options={chartOptions}
+                  data={{
+                    datasets: [
+                      this.getChartJSDataFor('in', stats.data.netv4.in) as any,
+                      this.getChartJSDataFor('out', stats.data.netv4.out) as any,
+                      this.getChartJSDataFor('private_in', stats.data.netv4.private_in) as any,
+                      this.getChartJSDataFor('private_out', stats.data.netv4.private_out) as any,
+                    ],
+                  }}
+                />
+              </div>
+              <div>
+                Public IPv4 Inbound
+              </div>
+              <div>
+                Public IPv4 Outbound
+              </div>
+              <div>
+                Private IPv4 Inbound
+              </div>
+              <div>
+                Private IPv4 Outbound
+              </div>
+            </React.Fragment>
+          }
         </ExpansionPanel>
+
         <ExpansionPanel
           heading="IPv6 Traffic"
         >
-          IPv6 Chart
+          {stats &&
+            <React.Fragment>
+              <div>
+                bits/sec
+              </div>
+              <div>
+                <Line
+                  height={300}
+                  options={chartOptions}
+                  data={{
+                    datasets: [
+                      this.getChartJSDataFor('in', stats.data.netv6.in) as any,
+                      this.getChartJSDataFor('out', stats.data.netv6.out) as any,
+                      this.getChartJSDataFor('private_in', stats.data.netv6.private_in) as any,
+                      this.getChartJSDataFor('private_out', stats.data.netv6.private_out) as any,
+                    ],
+                  }}
+                />
+              </div>
+              <div>
+                Public IPv6 Inbound
+              </div>
+              <div>
+                Public IPv6 Outbound
+              </div>
+              <div>
+                Private IPv6 Inbound
+              </div>
+              <div>
+                Private IPv6 Outbound
+              </div>
+            </React.Fragment>
+          }
         </ExpansionPanel>
+
         <ExpansionPanel
           heading="Disk I/O"
         >
-          Disk I/O
+          {stats &&
+            <React.Fragment>
+              <div>
+                bits/sec
+              </div>
+              <div>
+                <Line
+                  height={300}
+                  options={chartOptions}
+                  data={{
+                    datasets: [
+                      this.getChartJSDataFor('io', stats.data.io.io) as any,
+                      this.getChartJSDataFor('swap', stats.data.io.swap) as any,
+                    ],
+                  }}
+                />
+              </div>
+              <div>
+                I/O Rate
+              </div>
+              <div>
+                Swap Rate
+              </div>
+            </React.Fragment>
+          }
         </ExpansionPanel>
       </React.Fragment>
     );
