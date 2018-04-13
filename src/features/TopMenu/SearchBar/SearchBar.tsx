0ca970ae740d7019de0859664b3e2a55262a58b3
Andrew Sauber <a3sauber@gmail.com>
2018-04-13 14:06:10 -0400

    search Linodes and Volumes by label

--- src/features/TopMenu/SearchBar/SearchBar.tsx
+++ src/features/TopMenu/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Axios from 'axios';
 
 import {
   withStyles,
@@ -10,8 +11,12 @@ import Paper from 'material-ui/Paper';
 
 import Search from 'material-ui-icons/Search';
 
+import LinodeIcon from 'src/assets/addnewmenu/linode.svg';
+import VolumeIcon from 'src/assets/addnewmenu/volume.svg';
+import { API_ROOT } from 'src/constants';
 import LinodeTheme from 'src/theme';
 import TextField from 'src/components/TextField';
+
 
 type Styles =
   'root'
@@ -50,17 +55,26 @@ const styles = (theme: Theme & Linode.Theme): StyleRules => ({
     position: 'absolute',
     left: 0,
     right: 0,
-    marginTop: 42,
+    marginTop: 50,
     padding: theme.spacing.unit * 1,
   },
 });
+
+interface SearchResult {
+  title: string;
+  description: string;
+  Icon: React.ComponentClass<any>;
+}
 
 interface Props {}
 
 interface State {
   searchText: string;
-  searchElement?: HTMLElement;
-  lastFetch?: Date;
+  searchResults?: SearchResult[];
+  linodes?: Linode.Linode[];
+  volumes?: Linode.Volume[];
+  nodebalancers?: Linode.NodeBalancer[];
+  domains?: Linode.Domain[];
 }
 
 type FinalProps = Props & WithStyles<Styles>;
@@ -70,12 +84,84 @@ class SearchBar extends React.Component<FinalProps, State> {
     searchText: '',
   };
 
+  componentDidMount() {
+    Axios.get(`${API_ROOT}/linode/instances/`)
+      .then((response) => {
+        this.setState({ linodes: response.data.data });
+        this.search(this.state.searchText);
+      });
+
+    Axios.get(`${API_ROOT}/volumes`)
+      .then((response) => {
+        this.setState({ volumes: response.data.data });
+        this.search(this.state.searchText);
+      });
+
+    Axios.get(`${API_ROOT}/nodebalancers`)
+      .then((response) => {
+        this.setState({ nodebalancers: response.data.data });
+        this.search(this.state.searchText);
+      });
+
+    Axios.get(`${API_ROOT}/domains`)
+      .then((response) => {
+        this.setState({ domains: response.data.data });
+        this.search(this.state.searchText);
+      });
+  }
+
+  dataAvailable() {
+    return (
+      this.state.linodes
+      || this.state.volumes
+      || this.state.nodebalancers
+      || this.state.domains
+    );
+  }
+
   showResults() {
-    return this.state.searchText && this.state.searchText.length >= 3;
+    return (
+      this.dataAvailable()
+      && this.state.searchResults
+      && this.state.searchResults.length
+      && this.state.searchText
+      && this.state.searchText.length >= 3
+    );
+  }
+
+  search(query: string) {
+    if (!this.dataAvailable) return;
+
+    const searchResults = [];
+
+    if (this.state.linodes) {
+      const linodesByLabel = this.state.linodes.filter(
+        linode => linode.label.toLowerCase().includes(query.toLowerCase()),
+      );
+      searchResults.push(...(linodesByLabel.map(linode => ({
+        title: linode.label,
+        description: linode.type,
+        Icon: LinodeIcon,
+      }))));
+    }
+
+    if (this.state.volumes) {
+      const volumesByLabel = this.state.volumes.filter(
+        volume => volume.label.toLowerCase().includes(query.toLowerCase()),
+      );
+      searchResults.push(...(volumesByLabel.map(volume => ({
+        title: volume.label,
+        description: volume.size + ' G',
+        Icon: VolumeIcon,
+      }))));
+    }
+
+    this.setState({ searchResults });
   }
 
   handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     this.setState({ searchText: e.target.value });
+    this.search(e.target.value);
   }
 
   render() {
@@ -103,7 +189,17 @@ class SearchBar extends React.Component<FinalProps, State> {
             <Paper
               className={classes.searchResults}
             >
-              Search results go here
+              {this.state.searchResults && this.state.searchResults.map((result) => {
+                return (
+                  <div style={{ display: 'flex' }}>
+                    <div><result.Icon /></div>
+                    <div>
+                      <div>{result.title}</div>
+                      <div>{result.description}</div>
+                    </div>
+                  </div>
+                );
+              })}
             </Paper>
           }
         </div>
