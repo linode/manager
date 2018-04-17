2f8305eed73ae23056812db6c1efcd9625249b7a
Andrew Sauber <a3sauber@gmail.com>
2018-04-17 12:03:41 -0400

    add: reloadableWithRouter HOC to allow reloading component on matching route updates

---
+++ src/features/linodes/LinodesDetail/reloadableWithRouter.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import {
+  withRouter,
+  RouteComponentProps,
+} from 'react-router-dom';
+
+interface State {
+  rendering: boolean;
+  [name: string]: any;
+}
+
+type ReloadIf<R> = (
+  routePropsOld: RouteComponentProps<R>,
+  routePropsNew: RouteComponentProps<R>,
+) => boolean;
+
+export default function reloadableWithRouter<P, R>(reloadIf: ReloadIf<R>) {
+  return function (Component: React.ComponentType<P & RouteComponentProps<R>>) {
+    class ReloadableComponent extends React.Component<P & RouteComponentProps<R>, State> {
+      state = { rendering: true };
+
+      componentWillReceiveProps(nextProps: P & RouteComponentProps<R>) {
+        const routePropsOld = {
+          match: this.props.match,
+          location: this.props.location,
+          history: this.props.history,
+          staticContext: this.props.staticContext,
+        };
+
+        const routePropsNew = {
+          match: nextProps.match,
+          location: nextProps.location,
+          history: nextProps.history,
+          staticContext: nextProps.staticContext,
+        };
+
+        /* The reloadIf function provided as an argument to this HOC takes two arguments, the
+           previous route props and the new route props. If it returns true, then the component is
+           reloaded */
+        if (reloadIf(routePropsOld, routePropsNew)) {
+          this.setState({ rendering: false }, () => {
+            this.setState({ rendering: true });
+          });
+        }
+      }
+
+      render() {
+        const { rendering } = this.state;
+        if (rendering) {
+          return <Component {...this.props} />;
+        }
+        return null;
+      }
+    }
+
+    return withRouter(ReloadableComponent);
+  };
+}
