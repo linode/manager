1b84092b3d8211628742e80bce4eea1e67e52522
Andrew Sauber <a3sauber@gmail.com>
2018-04-18 14:03:40 -0400

    continue implementing backups

--- src/features/linodes/LinodesDetail/LinodeBackup/LinodeBackup.tsx
+++ src/features/linodes/LinodesDetail/LinodeBackup/LinodeBackup.tsx
@@ -5,6 +5,13 @@ import {
   Theme,
   WithStyles,
 } from 'material-ui';
+import Paper from 'material-ui/Paper';
+import Table from 'material-ui/Table';
+import TableHead from 'material-ui/Table/TableHead';
+import TableBody from 'material-ui/Table/TableBody';
+import TableRow from 'material-ui/Table/TableRow';
+import TableCell from 'material-ui/Table/TableCell';
+import Typography from 'material-ui/Typography';
 
 import { getLinodeBackups, enableBackups } from 'src/services/linode';
 import PromiseLoader, { PromiseLoaderResponse } from 'src/components/PromiseLoader';
@@ -12,10 +19,13 @@ import VolumeIcon from 'src/assets/addnewmenu/volume.svg';
 import Placeholder from 'src/components/Placeholder';
 import { resetEventsPolling } from 'src/events';
 
-type ClassNames = 'root';
+type ClassNames = 'root' | 'title';
 
 const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
   root: {},
+  title: {
+    margin: `${theme.spacing.unit * 2} 0`,
+  },
 });
 
 interface Props {
@@ -27,7 +37,7 @@ interface Props {
 
 interface PreloadedProps {
   /* PromiseLoader */
-  backups: PromiseLoaderResponse<Linode.LinodeBackup[]>;
+  backups: PromiseLoaderResponse<Linode.LinodeBackupsResponse>;
 }
 
 interface State {}
@@ -35,6 +45,8 @@ interface State {}
 type CombinedProps = Props & PreloadedProps & WithStyles<ClassNames>;
 
 class LinodeBackup extends React.Component<CombinedProps, State> {
+  state = {};
+
   enableBackups() {
     const { linodeID } = this.props;
     enableBackups(linodeID);
@@ -45,12 +57,13 @@ class LinodeBackup extends React.Component<CombinedProps, State> {
      **/
   }
 
-  Placeholder = (): JSX.Element | null => {
-    const { backupsEnabled, backupsMonthlyPrice } = this.props;
+  aggregateBackups = (): Linode.LinodeBackup[] => {
+    const { backups: { response: backups } } = this.props;
+    return [...backups.automatic, backups.snapshot.current].filter(b => Boolean(b));
+  }
 
-    if (backupsEnabled) {
-      return null;
-    }
+  Placeholder = (): JSX.Element | null => {
+    const { backupsMonthlyPrice } = this.props;
 
     const enableText = backupsMonthlyPrice
       ? `Enable Backups $${backupsMonthlyPrice.toFixed(2)}/mo`
@@ -69,20 +82,101 @@ class LinodeBackup extends React.Component<CombinedProps, State> {
     );
   }
 
-  state = {};
+  Table = (): JSX.Element | null => {
+    return (
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Label</TableCell>
+              <TableCell>Size</TableCell>
+              <TableCell>File System Path</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              attachedVolumes!.map((volume) => {
+                /** @todo Remove path defaulting when API releases filesystem_path. */
+                const label = pathOr('', ['label'], volume);
+                const size = pathOr('', ['size'], volume);
+                const filesystem_path = pathOr(
+                  `/dev/disk/by-id/scsi-0Linode_Volume_${label}`,
+                  ['filesystem_path'],
+                  volume,
+                );
 
-  render() {
+                return <TableRow key={volume.id}>
+                  <TableCell>{label}</TableCell>
+                  <TableCell>{size} GiB</TableCell>
+                  <TableCell>{filesystem_path}</TableCell>
+                  <TableCell>
+                    <ActionMenu
+                      volumeId={volume.id}
+                      onDetach={this.openUpdateDialog('detach', volume.id)}
+                      onDelete={this.openUpdateDialog('delete', volume.id)}
+                      onClone={this.openUpdatingDrawer(
+                        'clone',
+                        volume.id,
+                        volume.label,
+                        volume.size,
+                      )}
+                      onEdit={this.openUpdatingDrawer(
+                        'edit',
+                        volume.id,
+                        volume.label,
+                        volume.size,
+                      )}
+                      onResize={this.openUpdatingDrawer(
+                        'resize',
+                        volume.id,
+                        volume.label,
+                        volume.size,
+                      )}
+                    />
+                  </TableCell>
+                </TableRow>;
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+
+  Management = (): JSX.Element | null => {
+    const { classes } = this.props;
+
     return (
       <React.Fragment>
-        <this.Placeholder />
+        <Typography
+          variant="title"
+          className={classes.title}>
+          Backups
+        </Typography>
+        {this.aggregateBackups() &&
+          <this.Table />
+        }
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const { backupsEnabled } = this.props;
+
+    return (
+      <React.Fragment>
+        {backupsEnabled
+          ? <this.Management />
+          : <this.Placeholder />
+        }
       </React.Fragment>
     );
   }
 }
 
 const preloaded = PromiseLoader<Props>({
-  backups: (props: Props) => getLinodeBackups(props.linodeID)
-    .then(response => response.data),
+  backups: (props: Props) => getLinodeBackups(props.linodeID),
 });
 
 const styled = withStyles(styles, { withTheme: true });
