891c37e7baa444e90776071716b8a0775a3ec521
Patrick Thiel <pthiel@linode.com>
2018-04-19 17:34:51 -0400

    use chown to fix permissions

--- scripts/storybook_entrypoint.sh
+++ scripts/storybook_entrypoint.sh
@@ -2,5 +2,5 @@
 
 $PWD/scripts/wait-for-it.sh -t 250 -s manager-storybook:6006 -- yarn storybook:e2e --log
 status=$? # save it
-find . -not -uid $(stat -c "%u" .) -exec chown --reference=. {} \;
+chown -R jenkins **/storybook-test-results && chown -R jenkins **/e2e
 exit $status
