6a12a83e3ef58c6f4b20e6caaa25aaea6d6c2934
Pat Thiel <patthiel@gmail.com>
2018-04-19 18:46:27 -0400

    YABF - Build hotfix (#3220)
    * use chown to fix permissions
    
    * Permissions fix
    
    * add storybook results
    
    * Add logging

--- scripts/storybook_entrypoint.sh
+++ scripts/storybook_entrypoint.sh
@@ -2,5 +2,7 @@
 
 $PWD/scripts/wait-for-it.sh -t 250 -s manager-storybook:6006 -- yarn storybook:e2e --log
 status=$? # save it
+pwd
+ls -lah
 chown -R jenkins:jenkins **/storybook-test-results **/e2e **/src
 exit $status
