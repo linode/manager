8b8b60c36a908fcae16b223fcfceb4a99d246a00
Patrick Thiel <pthiel@linode.com>
2018-04-19 17:40:18 -0400

    Permissions fix

--- scripts/storybook_entrypoint.sh
+++ scripts/storybook_entrypoint.sh
@@ -2,5 +2,5 @@
 
 $PWD/scripts/wait-for-it.sh -t 250 -s manager-storybook:6006 -- yarn storybook:e2e --log
 status=$? # save it
-chown -R jenkins **/storybook-test-results && chown -R jenkins **/e2e
+chown -R jenkins:jenkins **/storybook-test-results **/e2e **/src
 exit $status
