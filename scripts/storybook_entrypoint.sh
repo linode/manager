de3f43fa1e080d178e707958970751e5050cd399
Pat Thiel <patthiel@gmail.com>
2018-04-23 14:17:05 -0400

    test build fix (#3228)

--- scripts/storybook_entrypoint.sh
+++ scripts/storybook_entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 $PWD/scripts/wait-for-it.sh -t 250 -s manager-storybook:6006 -- yarn storybook:e2e --log
-status=$? # save it
-pwd
-ls -lah
-chown -R jenkins:jenkins **/storybook-test-results **/e2e **/src
+status=$? # save previous command return status
+chmod 777 /src/storybook-test-results/*.xml
 exit $status
