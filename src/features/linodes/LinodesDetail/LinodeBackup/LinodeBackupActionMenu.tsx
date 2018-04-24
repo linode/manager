6175d722db871f400249355db9a12898c70ca8b6
Andrew Sauber <a3sauber@gmail.com>
2018-04-24 13:43:22 -0400

    add: linode backup actions, restore and deploy

---
+++ src/features/linodes/LinodesDetail/LinodeBackup/LinodeBackupActionMenu.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+import ActionMenu, { Action } from 'src/components/ActionMenu/ActionMenu';
+
+interface Props {
+  onRestore: () => void;
+  onDeploy: () => void;
+}
+
+type CombinedProps = Props & RouteComponentProps<{}>;
+
+class LinodeBackupActionMenu extends React.Component<CombinedProps> {
+  createActions = () => {
+    const {
+      onRestore,
+      onDeploy,
+    } = this.props;
+
+    return function (closeMenu: Function): Action[] {
+      const actions = [
+        {
+          title: 'Restore to Existing Linode',
+          onClick: (e: React.MouseEvent<HTMLElement>) => {
+            onRestore();
+            closeMenu();
+            e.preventDefault();
+          },
+        },
+        {
+          title: 'Deploy New Linode',
+          onClick: (e: React.MouseEvent<HTMLElement>) => {
+            onDeploy();
+            closeMenu();
+            e.preventDefault();
+          },
+        },
+      ];
+      return actions;
+    };
+  }
+
+  render() {
+    return (
+      <ActionMenu createActions={this.createActions()} />
+    );
+  }
+}
+
+export default withRouter(LinodeBackupActionMenu);
