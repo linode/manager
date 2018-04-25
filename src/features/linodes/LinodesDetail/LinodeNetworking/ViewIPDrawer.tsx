c243ccfc84818391de7bbb6234d5ff10359e8027
Andrew Sauber <a3sauber@gmail.com>
2018-04-25 18:01:57 -0400

    begin fleshing out view drawer for IPs

--- src/features/linodes/LinodesDetail/LinodeNetworking/ViewIPDrawer.tsx
+++ src/features/linodes/LinodesDetail/LinodeNetworking/ViewIPDrawer.tsx
@@ -5,8 +5,9 @@ import {
   Theme,
   WithStyles,
 } from 'material-ui';
-import Drawer from 'src/components/Drawer';
+import Typography from 'material-ui/Typography';
 
+import Drawer from 'src/components/Drawer';
 
 type ClassNames = 'root';
 
@@ -29,7 +30,29 @@ const ViewIPDrawer: React.StatelessComponent<CombinedProps> = (props) => {
       onClose={props.onClose}
       title={`Details for IP`}
     >
-      {props.ip && props.ip.address}
+      {props.ip &&
+        <React.Fragment>
+          <Typography variant="title">Address</Typography>
+          <Typography variant="body1">
+            {props.ip.address}
+          </Typography>
+
+          <Typography variant="title">Gateway</Typography>
+          <Typography variant="body1">
+            {props.ip.gateway}
+          </Typography>
+
+          <Typography variant="title">Subnet Mask</Typography>
+          <Typography variant="body1">
+            {props.ip.subnet_mask}
+          </Typography>
+
+          <Typography variant="title"></Typography>
+          <Typography variant="body1">
+            /{props.ip.prefix}
+          </Typography>
+        </React.Fragment>
+      }
     </Drawer>
   );
 };
