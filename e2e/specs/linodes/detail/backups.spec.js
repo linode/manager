3394f1f69672123191c7c95841def43b4f0941c5
Patrick Thiel <pthiel@linode.com>
2018-04-25 13:47:51 -0400

    Remove: example backups spec

--- e2e/specs/linodes/detail/backups.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Name of the group', () => {
-    beforeAll(() => {
-        // Create a linode here with backups disabled
-        // Navigate to Linode Detail Backups section of newly created Linode
-    });
-});
