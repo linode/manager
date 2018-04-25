70c8c8fb573fdb86f698085a8d8334f7e837fc72
Patrick Thiel <pthiel@linode.com>
2018-04-25 13:01:23 -0400

    Add: backup attributes, page object actions

--- e2e/pageobjects/backups.page.js
+++ e2e/pageobjects/backups.page.js
@@ -3,26 +3,46 @@ import Page from './page';
 class Backups extends Page {
     get placeholderText() { return $('[data-qa-placeholder-title]'); }
     get enableButton() { return $('[data-qa-placeholder-button]'); }
-    get heading() { return $(); }
-    get description() { return $(); }
-    get manualSnapshotHeading() { return $(); }
-    get manualDescription() { return $(); }
-    get manualSnapshotName() { return $(); }
-    get snapshotButton() { return $(); }
-    get settingsHeading() { return $(); }
-    get settingsDescription() { return $(); }
-    get timedaySelect() { return $(); }
-    get weekdaySelect() { return $(); }
-    get saveScheduleButton() { return $(); }
-    get cancelButton() { return $(); }
+    get heading() { return $('[data-qa-backup-heading]'); }
+    get description() { return $('[data-qa-backup-description]'); }
+    get manualSnapshotHeading() { return $('[data-qa-manual-heading]'); }
+    get manualDescription() { return $('[data-qa-manual-desc]'); }
+    get manualSnapshotName() { return $('[data-qa-manual-name]'); }
+    get snapshotButton() { return $('[data-qa-snapshot-button]'); }
+    get settingsHeading() { return $('[data-qa-settings-heading]'); }
+    get settingsDescription() { return $('[data-qa-settings-desc]'); }
+    get timedaySelect() { return $('[data-qa-time-select]'); }
+    get weekdaySelect() { return $('[data-qa-weekday-select]'); }
+    get saveScheduleButton() { return $('[data-qa-schedule]'); }
+    get cancelDescription() { return $('[data-qa-cancel-desc]'); }
+    get cancelButton() { return $('[data-qa-cancel]'); }
 
+    get backupInstances() { return $$('[data-qa-backup]'); }
+    get name() { return $('[data-qa-backup-name]'); }
+    get dateCreated() { return $('[data-qa-date-created]'); }
+    get type() { return $('[data-qa-backup-type]'); }
+    get duration() { return $('[data-qa-backup-duration]'); }
+    get disks() { return $('[data-qa-backup-disks]'); }
+    get spaceRequired() { $('[data-qa-space-required]'); }
 
     takeSnapshot(label) {
-        // wait for toast with message: "a snapshot is being taken"
+        this.manualSnapshotName.$('input').setValue(label);
+        this.snapshotButton.click();
+
+        browser.waitForVisible('[data-qa-toast]');
+        browser.waitUntil(function() {
+            return browser
+                .getText('[data-qa-toast-message]') === 'A snapshot is being taken';
+        }, 10000);
     }
 
     assertSnapshot(label) {
-        // assert snapshot with 'label' displays in table
+        browser.waitForVisible('[data-qa-backup]');
+
+        const backupInstance = this.backupInstances.map(i => {
+            return i.$(this.name.selector).getText();
+        });
+        expect(backupInstance).toContain(label);
     }
 
     scheduleSnapshots(dayTime, weekDay) {
@@ -33,8 +53,15 @@ class Backups extends Page {
 
     cancelBackups() {
         this.cancelButton.click();
-        this.placeholderText.waitForVisible(10000, true);
+        
+        browser.waitForVisible('[data-qa-toast]');
+        browser.waitUntil(function() {
+            return browser
+                .getText('[data-qa-toast]') === 'Backups are being cancelled for this Linode';
+        }, 10000);
+
+        this.placeholderText.waitForVisible();
     }
 }
 
-export default new Backup();
+export default new Backups();
