6334d8df318caedfaf241583b48fe8611e766925
Alban <albanbailly@gmail.com>
2018-04-26 11:56:07 -0400

    reversing plus/minus icons (#3256)

--- src/components/ExpansionPanel/ExpansionPanel.tsx
+++ src/components/ExpansionPanel/ExpansionPanel.tsx
@@ -116,7 +116,7 @@ class EExpansionPanel extends React.Component<CombinedProps> {
       >
         <ExpansionPanelSummary
           onClick={this.handleClick}
-          expandIcon={this.state.open ? <Minus /> : <Plus />}
+          expandIcon={this.state.open ? <Plus /> : <Minus />}
           {...summaryProps}
           // className={classNames({
           //   [classes.success]: Boolean(this.props.success),
