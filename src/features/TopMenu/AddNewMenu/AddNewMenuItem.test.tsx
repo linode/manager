e92dff96894ad4768ca554cdfacf351843ad65d4
Andrew Sauber <a3sauber@gmail.com>
2018-04-27 19:26:21 -0400

    update tests with LinodeThemeWrapper

--- src/features/TopMenu/AddNewMenu/AddNewMenuItem.test.tsx
+++ src/features/TopMenu/AddNewMenu/AddNewMenuItem.test.tsx
@@ -1,46 +1,57 @@
 import * as React from 'react';
 import AddNewMenuItem from './AddNewMenuItem';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import LinodeIcon from 'src/assets/addnewmenu/linode.svg';
+
+import LinodeThemeWrapper from 'src/LinodeThemeWrapper';
 
 describe('AddNewMenuItem', () => {
   it('should render without error', () => {
     shallow(
-      <AddNewMenuItem
-        index={1}
-        count={1}
-        title="shenanigans"
-        body="These be the stories of shennanigans."
-        ItemIcon={LinodeIcon}
-        onClick={jest.fn()}
-      />);
+      <LinodeThemeWrapper>
+        <AddNewMenuItem
+          index={1}
+          count={1}
+          title="shenanigans"
+          body="These be the stories of shennanigans."
+          ItemIcon={LinodeIcon}
+          onClick={jest.fn()}
+        />
+      </LinodeThemeWrapper>,
+     );
   });
 
   it('should render a divider if not the last item', () => {
-    const result = shallow(
-      <AddNewMenuItem
-        index={1}
-        count={1}
-        title="shenanigans"
-        body="These be the stories of shennanigans."
-        ItemIcon={LinodeIcon}
-        onClick={jest.fn()}
-      />);
+    const result = mount(
+      <LinodeThemeWrapper>
+        <AddNewMenuItem
+          index={1}
+          count={1}
+          title="shenanigans"
+          body="These be the stories of shennanigans."
+          ItemIcon={LinodeIcon}
+          onClick={jest.fn()}
+        />
+      </LinodeThemeWrapper>,
+    );
 
-    expect(result.dive().find('WithStyles(Divider)')).toHaveLength(1);
+    expect(result.find('WithStyles(Divider)')).toHaveLength(1);
   });
 
   it('should not render a divider if not the last item', () => {
-    const result = shallow(
-      <AddNewMenuItem
-        index={0}
-        count={1}
-        title="shenanigans"
-        body="These be the stories of shennanigans."
-        ItemIcon={LinodeIcon}
-        onClick={jest.fn()}
-      />);
+    const result = mount(
+      <LinodeThemeWrapper>
+        <AddNewMenuItem
+          index={0}
+          count={1}
+          title="shenanigans"
+          body="These be the stories of shennanigans."
+          ItemIcon={LinodeIcon}
+          onClick={jest.fn()}
+        />
+      </LinodeThemeWrapper>,
+    );
 
-    expect(result.dive().find('WithStyles(Divider)')).toHaveLength(0);
+    expect(result.find('WithStyles(Divider)')).toHaveLength(0);
   });
 });
