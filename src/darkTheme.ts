1ffa4f0f5b10d0df5c1755c363116b174d8cae7b
Alban Bailly <abailly@linode.com>
2018-04-30 11:51:33 -0400

    adding more palette variables

--- src/darkTheme.ts
+++ src/darkTheme.ts
@@ -4,8 +4,25 @@ const LinodeTheme: Linode.Theme = {
     navy: '#32363C',
     white: '#000',
   },
+  color: {
+    headline: '#f4f4f4',
+    red: '#CA0813',
+    green: '#00B159',
+    yellow: '#FECF2F',
+    border1: '#ABADAF',
+    border2: '#C5C6C8',
+    border3: '#eee',
+    grey1: '#abadaf',
+    grey2: '#E7E7E7',
+    grey3: '#ccc',
+  },
   palette: {
     divider: '#000',
+    primary: {
+      main: '#3B85D9',
+      light: '#5F99EA',
+      dark: '#3566AE',
+    },
     status: {
       success: '#d7e3EF',
       successDark: '#3682dd',
@@ -18,7 +35,95 @@ const LinodeTheme: Linode.Theme = {
       primary: '#fff',
     },
   },
+  typography: {
+    headline: {
+      color: '#fff',
+    },
+    title: {
+      color: '#f4f4f4',
+    },
+    subheading: {
+      color: '#fff',
+    },
+  },
   overrides: {
+    MuiAppBar: {
+      colorDefault: {
+        backgroundColor: '#000',
+      },
+    },
+    MuiButton: {
+      root: {
+        textTransform: 'inherit',
+        borderRadius: 0,
+        fontSize: '1rem',
+        fontWeight: 700,
+        color: '#3B85D9',
+        padding: '12px 28px 14px',
+        '&:hover, &:focus': {
+          backgroundColor: '#fff',
+        },
+      },
+      raisedPrimary: {
+        '&:hover, &:focus': {
+          backgroundColor: '#5F99EA',
+        },
+        '&:active': {
+          backgroundColor: '#3566AE',
+        },
+        '&$disabled': {
+          color: 'white',
+        },
+      },
+      raisedSecondary: {
+        backgroundColor: 'transparent',
+        color: '#3B85D9',
+        border: '1px solid #3B85D9',
+        padding: '11px 26px 13px',
+        transition: 'border 225ms ease-in-out, color 225ms ease-in-out',
+        '&:hover, &:focus': {
+          backgroundColor: 'transparent',
+          color: '#5F99EA',
+          borderColor: '#5F99EA',
+        },
+        '&:active': {
+          backgroundColor: 'transparent',
+          color: '#3566AE',
+          borderColor: '#3566AE',
+        },
+        '&$disabled': {
+          borderColor: '#C9CACB',
+          backgroundColor: 'transparent',
+          color: '#C9CACB',
+        },
+        '&.cancel': {
+          borderColor: 'transparent',
+          marginLeft: 0,
+          '&:hover, &:focus': {
+            borderColor: '#5F99EA',
+          },
+        },
+        '&.destructive': {
+          borderColor: '#C44742',
+          color: '#C44742',
+          '&:hover, &:focus': {
+            color: '#DF6560',
+            borderColor: '#DF6560',
+          },
+          '&:active': {
+            color: '#963530',
+            borderColor: '#963530',
+          },
+        },
+        '&.loading': {
+          '& svg': {
+            width: 22,
+            height: 22,
+            animation: 'rotate 2s linear infinite',
+          },
+        },
+      },
+    },
     MuiPaper: {
       root: {
         backgroundColor: '#000',
