916739eaa4b39c776a84dd47b2c75e9fe684a9e2
Andrew Sauber <a3sauber@gmail.com>
2018-04-30 14:27:18 -0400

    add IPv6 creation drawer

---
+++ src/features/linodes/LinodesDetail/LinodeNetworking/CreateIPv6Drawer.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  withStyles,
+  StyleRulesCallback,
+  Theme,
+  WithStyles,
+} from 'material-ui';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+
+import ActionsPanel from 'src/components/ActionsPanel';
+import Drawer from 'src/components/Drawer';
+
+type ClassNames = 'root';
+
+const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
+  root: {},
+});
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+}
+
+type CombinedProps = Props & WithStyles<ClassNames>;
+
+const CreateIPv6Drawer: React.StatelessComponent<CombinedProps> = (props) => {
+  return (
+    <Drawer
+      open={props.open}
+      onClose={props.onClose}
+      title="Allocate IPv6 Ranges"
+    >
+      <React.Fragment>
+        <Typography variant="body1">
+          IPv6 addresses are allocated as ranges, which you can choose to
+          distribute and further route yourself. These ranges can only be
+          allocated by our support team. Please open a <Link
+          to="/support">Support Ticket</Link> and request an IPv6 range for
+          this Linode.
+        </Typography>
+        <ActionsPanel style={{ marginTop: 16 }}>
+          <Button
+            variant="raised"
+            color="primary"
+            className="cancel"
+            onClick={props.onClose}
+          >
+            Close
+          </Button>
+        </ActionsPanel>
+      </React.Fragment>
+    </Drawer>
+  );
+};
+
+const styled = withStyles(styles, { withTheme: true });
+
+export default styled<Props>(CreateIPv6Drawer);
