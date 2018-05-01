aab8d89a15a6b9062e58d734874c9fc5639a65c0
Patrick Thiel <pthiel@linode.com>
2018-05-01 15:08:23 -0400

    Add: volume action specs

---
+++ e2e/specs/linodes/detail/detach-attach-volumes.spec.js
@@ -0,0 +1,61 @@
+const { constants } = require('../../../constants');
+
+import VolumeDetail from '../../../pageobjects/volume-detail.page';
+import ListLinodes from '../../../pageobjects/list-linodes';
+import LinodeDetail from '../../../pageobjects/linode-detail.page';
+
+describe('Linode - Volumes - Attach, Detach, Delete Suite', () => {
+    let linodeName,
+        volume;
+
+    const testVolume = {
+        label: `test-volume-${new Date().getTime()}`,
+        size: '20',
+    }
+
+    beforeAll(() => {
+        browser.url(constants.routes.linodes);
+        ListLinodes.linodeElem.waitForVisible();
+        linodeName = ListLinodes.linode[0].$(ListLinodes.linodeLabel.selector).getText();
+        ListLinodes.linode[0].$(ListLinodes.linodeLabel.selector).click();
+        LinodeDetail.landingElemsDisplay();
+        LinodeDetail.changeTab('Volumes');
+        
+        VolumeDetail.createVolume(testVolume);
+        browser.waitForVisible('[data-qa-volume-cell]', 25000);
+    });
+
+    it('should display detach linode dialog', () => {
+        volume = VolumeDetail.volumeCell[0];
+        testVolume['id'] = volume.getAttribute('data-qa-volume-cell');
+        testVolume['label'] = volume.$(VolumeDetail.volumeCellLabel.selector).getText();
+
+        VolumeDetail.detachVolume(volume);
+    });
+
+    it('should detach the volume', () => {
+        VolumeDetail.detachConfirm(testVolume.id);
+
+        if (VolumeDetail.placeholderText.isVisible()) {
+            const placeholderMsg = VolumeDetail.placeholderText.getText();
+            const expectedMsg = 'No volumes attached';
+            expect(placeholderMsg).toBe(expectedMsg);
+        }
+    });
+
+    it('should display attach drawer', () => {
+        const createButton = 
+            VolumeDetail.placeholderText.isVisible() ? VolumeDetail.createButton : VolumeDetail.createIconLink;
+
+        createButton.click();
+        // assert placeholder elems appear
+    });
+
+    it('should attach to linode', () => {
+        VolumeDetail.attachVolume(linodeName, testVolume);
+    });
+
+    it('should remove the volume', () => {
+        VolumeDetail.removeVolume($(`[data-qa-volume-cell="${testVolume.id}"]`));
+    });
+});
---
+++ e2e/specs/linodes/detail/edit-clone-resize-volumes.spec.js
@@ -0,0 +1,88 @@
+const { constants } = require('../../../constants');
+
+import VolumeDetail from '../../../pageobjects/volume-detail.page';
+import ListLinodes from '../../../pageobjects/list-linodes';
+import LinodeDetail from '../../../pageobjects/linode-detail.page';
+
+xdescribe('Edit - Clone - Resize Volumes Suite', () => {
+    let linodeName,
+        volume;
+
+    const testVolume = {
+        label: `test-volume-${new Date().getTime()}`,
+        size: '20',
+    }
+
+    beforeAll(() => {
+        browser.url(constants.routes.linodes);
+        ListLinodes.linodeElem.waitForVisible();
+        
+        if (ListLinodes.getStatus(ListLinodes.linode[0]) !== 'offline') {
+            ListLinodes.powerOff(ListLinodes.linode[0]);
+        }
+    });
+
+    afterAll(() => {
+        browser.url(constants.routes.linodes);
+
+        ListLinodes.linodeElem.waitForVisible();
+        ListLinodes.powerOn(ListLinodes.linode[0]);
+    });
+
+    beforeEach(() => {
+        browser.url(constants.routes.linodes);
+        ListLinodes.linodeElem.waitForVisible();
+        linodeName = ListLinodes.linode[0].$(ListLinodes.linodeLabel.selector).getText();
+
+        ListLinodes.linode[0].$(ListLinodes.linodeLabel.selector).click();
+        LinodeDetail.landingElemsDisplay();
+        LinodeDetail.changeTab('Volumes');
+        
+        VolumeDetail.createVolume(testVolume);
+        browser.waitForVisible('[data-qa-volume-cell]', 25000);
+
+        testVolume['id'] = VolumeDetail.volumeCell[0].getAttribute('data-qa-volume-cell');
+        volume = $(`[data-qa-volume-cell="${testVolume.id}"]`);
+    });
+
+    afterEach(() => {
+        browser.waitForVisible(`[data-qa-volume-cell="${testVolume.id}"] [data-qa-action-menu]`);
+        VolumeDetail.removeVolume($(`[data-qa-volume-cell="${testVolume.id}"]`));
+    });
+
+    it('should edit the volume label', () => {
+        VolumeDetail.selectActionMenuItem(volume, 'Edit');
+        VolumeDetail.editVolume(testVolume, 'new-name');
+    });
+
+    it('should dispay resize', () => {
+        VolumeDetail.selectActionMenuItem(volume, 'Resize');
+        const newSize = '30';
+
+        VolumeDetail.size.$('input').setValue(newSize);
+        VolumeDetail.submit.click();
+        VolumeDetail.drawerTitle.waitForVisible(10000, true);
+        
+        browser.waitUntil(function() {
+            return browser.getText(`[data-qa-volume-cell="${testVolume.id}"] [data-qa-volume-size]`).includes(newSize);
+        }, 10000);
+    });
+
+    it('should clone the volume', () => {
+        const getPath = /linodes\/\d.*/
+        const currentUrl = browser.getUrl();
+        const linodeId = currentUrl.match(getPath)[0].match(/\d/g).join('');
+        const numberOfVolumes = VolumeDetail.volumeCell.length;
+
+        VolumeDetail.selectActionMenuItem(volume, 'Clone');
+        VolumeDetail.drawerTitle.waitForVisible();
+
+        expect(VolumeDetail.size.$('input').getValue()).toBe(testVolume.size);
+        expect(VolumeDetail.attachedTo.$('input').getValue()).toBe(linodeId);
+
+        browser.trySetValue('[data-qa-clone-from] input', 'new-clone');
+
+        VolumeDetail.submit.click();
+        VolumeDetail.drawerTitle.waitForVisible(10000, true);
+    });
+});
