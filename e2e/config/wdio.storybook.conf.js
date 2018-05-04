de67aa43208a79d35cb8dea600e5371469c0d063
Patrick Thiel <pthiel@linode.com>
2018-05-04 09:40:09 -0400

    use selenium config file, run on headless by default, bump maxInstances

--- e2e/config/wdio.storybook.conf.js
+++ e2e/config/wdio.storybook.conf.js
@@ -3,19 +3,32 @@ const { argv } = require('yargs');
 const { browserCommands } = require('./custom-commands');
 const wdioMaster = require('./wdio.conf.js');
 const { constants } = require('../constants');
+const { browserConf } = require('./browser-config');
+const selectedBrowser = () => {
+    if (argv.b) {
+        return browserConf[argv.b];
+    }
+    if (process.env.DOCKER || argv.debug) {
+     browserConf['chrome'];
+    }
+    return browserConf['headlessChrome'];
+}
 const specsToRun = argv.file ? [ argv.file ] : ['./src/components/**/*.spec.js'];
 const servicesToStart = process.env.DOCKER || argv.debug ? [] : ['selenium-standalone'];
 
 exports.config = merge(wdioMaster.config, {
     specs: specsToRun,
     baseUrl: process.env.DOCKER ? 'http://manager-storybook:6006' : 'http://localhost:6006',
-    maxInstances: process.env.DOCKER ? 1 : 3,
+    capabilities: [selectedBrowser()],
+    maxInstances: process.env.DOCKER ? 1 : 4,
     reporterOptions: {
         junit: {
             outputDir: './storybook-test-results'
         }
     },
     services: servicesToStart,
+    seleniumInstallArgs: { config: './selenium-config.js' },
+    seleniumArgs: { config: './selenium-config.js' },
     before: function (capabilities, specs) {
         browserCommands();
         browser.url(constants.routes.storybook);
