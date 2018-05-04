34f3eed3edc1dcd7362382dfae0e73808f7356fd
Patrick Thiel <pthiel@linode.com>
2018-05-04 11:37:54 -0400

    Run tests in parallel, start storybook if its not running

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,20 +2,40 @@
 
 # Get files changed in commit
 changes=$( git diff --cached --name-status | awk '$1 != "D" { print $2 }' )
+status=0
 
+(
 yarn lint
-status=$?
-
+status=$(($status + $?))
+) &
+(
 yarn test
 status=$(($status + $?))
+) &
 
 # Run storybook tests if components are changed
 if [[ $changes =~ .*src\/components.* ]]; then
-    yarn storybook:e2e
+(
+    # Check if storybook is running
+    nc -z -w5 localhost 6006 > /dev/null 2>&1
+    storybookRunning=$?
+
+    if [[ $storybookRunning -eq "0" ]]; then
+        yarn storybook:e2e
+    else 
+        yarn storybook > /dev/null 2>&1 &
+        yarn storybook:e2e
+    fi
     status=$(($status + $?))
 
-    # Ensure we cleanup any leftover selenium processes
+    # Ensure we cleanup any leftover processes
     $( pkill -f selenium-standalone )
+    
+    if [[ $storybookRunning -eq "1" ]]; then
+        $( pkill -f storybook )
+    fi
+) &
 fi
 
+wait
 exit $status
