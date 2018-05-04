b7cd324d13233be190ba1b098093cf5ce3b18337
Alban <albanbailly@gmail.com>
2018-05-04 13:24:07 -0400

    add missing themeDecorator (#3282)

--- src/components/ErrorState/ErrorState.stories.tsx
+++ src/components/ErrorState/ErrorState.stories.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import ErrorState from './ErrorState';
+import ThemeDecorator from '../../utilities/storybookDecorators';
 
 storiesOf('Error Display', module)
+  .addDecorator(ThemeDecorator)
   .add('with text', () => (
     <ErrorState errorText="An error has occured." />
   ));
