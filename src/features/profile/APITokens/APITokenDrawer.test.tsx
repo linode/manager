303068dacda4b7f45e508fcd89353dbe861d20ab
Martin <mmckenna@linode.com>
2018-05-16 14:49:49 -0400

    M3-488 select all permissions (#3338)
    * add select all table row jsx
    
    * add: select all scopes
    
    * add select all test
    
    * add comments
    
    * Refactor handleSelectAllScopes

---
+++ src/features/profile/APITokens/APITokenDrawer.test.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import { APITokenDrawer } from './APITokenDrawer';
+
+describe('API Token Drawer', () => {
+  const component = shallow(
+    <APITokenDrawer
+      classes={{
+        permsTable: '',
+        accessCell: '',
+        noneCell: '',
+        readOnlyCell: '',
+        readWritecell: '',
+      }}
+      open={true}
+      mode="create"
+      closeDrawer={jest.fn()}
+      onChange={jest.fn()}
+      onCreate={jest.fn()}
+      onEdit={jest.fn()}
+    />,
+  );
+
+  it('scope permissions should be the same if "select all" radio is clicked', () => {
+    const selectAllBtn = component
+      .find('WithStyles(LinodeRadioControl)[value="1"][name="Select All"]');
+    // "value" could not be found on "currentTarget,"
+    // so we needed to manually make inject that value
+    selectAllBtn.simulate('change', { currentTarget: { value: '2' } });
+    const allScopesIndenticalToSelectAll =
+      component.state().scopes.every((scope: [string, number]) => {
+        // the selectAll value that was selected was 2, so all the scopes should be 2
+        return scope[1] === 2;
+      });
+    expect(allScopesIndenticalToSelectAll).toBeTruthy();
+  });
+});
