acdf6029e0dfef3a35906123cd36a30c03da500a
Marty McKenna <mmckenna.phila@gmail.com>
2018-05-17 10:17:42 -0400

    add nodebalancer types and requests

--- src/types/NodeBalancer.ts
+++ src/types/NodeBalancer.ts
@@ -10,4 +10,31 @@ namespace Linode {
     created: string;
     updated: string;
   }
+
+  export interface NodesStatus{
+    up: number;
+    down: number;
+  }
+
+  export interface NodeBalancerConfig{
+    id: number;
+    nodebalancer_id: number;
+    port: number;
+    check_passive: boolean;
+    ssl_cert: string;
+    nodes_status: NodesStatus;
+    protocol: string;
+    ssl_commonname: string;
+    check_interval: number;
+    check_attempts: number;
+    check_timeout: number;
+    check_body: string;
+    check_path: string;
+    check: string;
+    ssl_key: string;
+    stickiness: string;
+    algorithm: string;
+    ssl_fingerprint: string;
+    cipher_suite: string;
+  }
 }
