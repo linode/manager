5cd4511960d7c1d31fcb77a785d84be46f406690
Marty McKenna <mmckenna.phila@gmail.com>
2018-05-23 13:51:02 -0400

    abstract MB convesion to it's own utllity

--- src/features/NodeBalancers/NodeBalancersLanding.tsx
+++ src/features/NodeBalancers/NodeBalancersLanding.tsx
@@ -13,6 +13,8 @@ import TableRow from 'material-ui/Table/TableRow';
 import { getNodeBalancers, getNodeBalancerConfigs } from 'src/services/nodebalancers';
 import RegionIndicator from 'src/features/linodes/LinodesLanding/RegionIndicator';
 import IPAddress from 'src/features/linodes/LinodesLanding/IPAddress';
+import { convertMegabytesTo } from 'src/utilities/convertMegabytesTo';
+
 import SectionErrorBoundary from 'src/components/SectionErrorBoundary';
 import Grid from 'src/components/Grid';
 import Table from 'src/components/Table';
@@ -103,25 +105,6 @@ export class NodeBalancersLanding extends React.Component<CombinedProps, State> 
     },
   ];
 
-  formatTransferData = (transferTotal: number) => {
-    // API v4 always returns nodebalancer transfer in MB, so we want to clean it up if it's too
-    // big or too small
-    const gb = 1073741824;
-    const mb = 1048576;
-    const kb = 1024;
-    const totalToBytes = transferTotal * 1024 * 1024; // convert the MB to Bytes
-    if (totalToBytes >= gb) { // convert bytes to GB
-      return `${Math.max(Math.ceil(totalToBytes / gb)).toFixed(2)} GB`;
-    }
-    if (totalToBytes >= mb) { // convert bytes to MB
-      return `${Math.max(Math.ceil(totalToBytes / mb * 100) / 100).toFixed(2)} MB`;
-    }
-    if (totalToBytes >= kb) { // convert bytes to KB
-      return `${Math.max(Math.ceil(totalToBytes / kb * 100) / 100).toFixed(2)} KB`;
-    }
-    return `${totalToBytes} bytes`;
-  }
-
   renderEmptyState = () => {
     return <Placeholder />;
   }
@@ -188,7 +171,7 @@ export class NodeBalancersLanding extends React.Component<CombinedProps, State> 
                     </TableCell>
                     <TableCell>{`${nodeBalancer.up} up, ${nodeBalancer.down} down`}</TableCell>
                     <TableCell>
-                      {this.formatTransferData(nodeBalancer.transfer.total)}
+                      {convertMegabytesTo(nodeBalancer.transfer.total)}
                     </TableCell>
                     <TableCell>{nodeBalancer.ports.map((port, index, ports) => {
                       // we want a comma after the port number as long as the ports array
