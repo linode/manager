7821fb9cea1be9abcc6bcf49f7b933a154f9430d
Robert Stires <rstires@linode.com>
2018-05-25 14:13:12 -0400

    add joi-browser

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -96,10 +96,11 @@ module.exports = {
       '.jsx',
     ],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'joi': 'joi-browser',
       'src/': paths.appSrc,
     },
     plugins: [
