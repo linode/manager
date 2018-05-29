703b9564cbf9c36536c93d80fcbdfaf8644a3bad
Marty McKenna <mmckenna.phila@gmail.com>
2018-05-29 11:18:28 -0400

    change NodeBalancerSummary to stateless component

--- src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/NodeBalancerSummary.tsx
+++ src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/NodeBalancerSummary.tsx
@@ -20,24 +20,19 @@ interface Props {
   nodeBalancer: Linode.ExtendedNodeBalancer;
 }
 
-interface State { }
-
 type CombinedProps = Props & WithStyles<ClassNames>;
 
-class NodeBalancerSummary extends React.Component<CombinedProps, State> {
-  state: State = {};
-
-  render() {
-    const { nodeBalancer } = this.props;
-    return (
-      <React.Fragment>
-        <SummaryPanel nodeBalancer={nodeBalancer} />
-        <TablesPanel nodeBalancer={nodeBalancer} />
-      </React.Fragment>
-    );
-  }
-}
+const NodeBalancerSummary: React.StatelessComponent<CombinedProps> = (props) => {
+  const { nodeBalancer } = props;
+  return (
+    <React.Fragment>
+      <SummaryPanel nodeBalancer={nodeBalancer} />
+      <TablesPanel nodeBalancer={nodeBalancer} />
+    </React.Fragment>
+  );
+};
 
 const styled = withStyles(styles, { withTheme: true });
 
-export default styled(NodeBalancerSummary);
+export default styled<Props>(NodeBalancerSummary);
+
