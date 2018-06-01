432ba19d602696f22308acce3d1e1c081d3620f2
Andrew Sauber <a3sauber@gmail.com>
2018-06-01 10:25:01 -0400

    M3-269 - NodeBalancer Configurations View and Edit (#3397)
    * begin NodeBalancer configuration view and edit
    
    * implement basic editing of configs
    
    * success messages on config submit, revert to unmodified if error
    
    * implement cancel editing
    
    * display API errors in config edit
    
    * client side validation of configs
    
    * implement disable save button while submitting
    
    * don't show nodes if none were provided
    
    * reset errors on cancel
    
    * align ActionsPanel on NodeBalancer config edit panels
    
    * implement NodeBalancer configuration deletion

--- src/types/NodeBalancer.ts
+++ src/types/NodeBalancer.ts
@@ -32,7 +32,7 @@ namespace Linode {
     mode?: NodeBalancerConfigNodeMode;
   }
 
-  export interface NodeBalancerConfig{
+  export interface NodeBalancerConfig {
     id: number;
     nodebalancer_id: number;
     port: number;
