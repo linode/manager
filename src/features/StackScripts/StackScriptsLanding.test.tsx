670624a68a58479feb9579f0836951070ae75994
Marty McKenna <mmckenna.phila@gmail.com>
2018-06-04 10:17:21 -0400

    add StackScriptLanding husk

---
+++ src/features/StackScripts/StackScriptsLanding.test.tsx
@@ -0,0 +1,14 @@
+describe('NodeBalancers', () => {
+
+  it('should render 3 header tabs', () => {
+    //
+  });
+
+  it('should render 5 colums in a table', () => {
+    //
+  });
+
+  it('should render a select component for filtering', () => {
+    //
+  });
+});
