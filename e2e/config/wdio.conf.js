433ecc84cddc8c953b0113546722a4ea4f6cc2e1
Patrick Thiel <pthiel@linode.com>
2018-06-04 16:32:46 -0400

    Add back baseUrl handling for docker, only run e2e tests in docker compose

--- e2e/config/wdio.conf.js
+++ e2e/config/wdio.conf.js
@@ -97,7 +97,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: process.env.REACT_APP_APP_ROOT,
+    baseUrl: process.env.DOCKER ? 'https://manager-local:3000' : process.env.REACT_APP_APP_ROOT,
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: process.env.DOCKER ? 20000 : 10000,
