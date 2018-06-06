4f1185d571ea8c937cf3f1173012856544538441
Marty McKenna <mmckenna.phila@gmail.com>
2018-06-06 16:15:22 -0400

    add LinodesCreate test

--- src/features/linodes/LinodesCreate/LinodesCreate.test.tsx
+++ src/features/linodes/LinodesCreate/LinodesCreate.test.tsx
@@ -1,36 +1,39 @@
-// import * as React from 'react';
-// import { shallow } from 'enzyme';
-// import { LinodeCreate } from './LinodesCreate';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { LinodeCreate } from './LinodesCreate';
 
-// import { images, ExtendedType, LinodesWithBackups } from 'src/__data__/index';
+import { images, ExtendedType, LinodesWithBackups } from 'src/__data__/index';
 
-// import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps, MemoryRouter } from 'react-router';
 
-// const dummyProps = {
-//   types: ExtendedType(),
-//   regions: [],
-//   images: {
-//     response: images,
-//   },
-//   linodes: {
-//     response: LinodesWithBackups,
-//   },
-// };
+const dummyProps = {
+  types: ExtendedType,
+  regions: [],
+  images: {
+    response: images,
+  },
+  linodes: {
+    response: LinodesWithBackups,
+  },
+};
 
 describe('FromImageContent', () => {
-  // const component = shallow(
-  //   <LinodeCreate
-  //     {...({} as RouteComponentProps<any>)}
-  //     classes={{
-  //       root: '',
-  //       main: '',
-  //       sidebar: '',
-  //     }}
-  //     {...dummyProps}
-  //   />,
-  // );
+  const component = shallow(
+    <MemoryRouter initialEntries={['/']}>
+      <LinodeCreate
+        {...({} as RouteComponentProps<any>)}
+        classes={{
+          root: '',
+          main: '',
+          sidebar: '',
+        }}
+        {...dummyProps}
+      />
+    </MemoryRouter>,
+  );
 
-  it('should do something', () => {
-    console.log('test');
+  it('renders without error', () => {
+    console.log(component.debug());
+    expect(component.find('LinodeCreate')).toHaveLength(1);
   });
 });
