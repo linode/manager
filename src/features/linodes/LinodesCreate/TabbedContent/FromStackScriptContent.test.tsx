aa3388cb504a343f6baf4cc749b845b8a47bc351
Marty McKenna <mmckenna.phila@gmail.com>
2018-06-07 09:36:48 -0400

    add initial StackScript tab content

---
+++ src/features/linodes/LinodesCreate/TabbedContent/FromStackScriptContent.test.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { FromStackScriptContent } from './FromStackScriptContent';
+
+const mockProps = {
+  updateFormState: jest.fn(),
+  selectedImageID: '1',
+  selectedRegionID: '1',
+  selectedTypeID: '1',
+  images: [],
+  regions: [],
+  types: [],
+  backups: true,
+  privateIP: true,
+  getBackupsMonthlyPrice: jest.fn(),
+  label: 'my label',
+  password: 'helllo432.324',
+};
+
+describe('FromImageContent', () => {
+  // const componentWithNotice = shallow(
+  //   <FromStackScriptContent
+  //     classes={{ root: '' }}
+  //     {...mockProps}
+  //     notice={{
+  //       text: 'hello world',
+  //       level: 'warning' as 'warning' | 'error',
+  //     }}
+  //   />,
+  // );
+
+  const component = shallow(
+    <FromStackScriptContent
+      classes={{ root: '' }}
+      {...mockProps}
+    />,
+  );
+
+  // it('should render a notice when passed a Notice prop', () => {
+  //   expect(componentWithNotice.find('WithStyles(Notice)')).toHaveLength(1);
+  // });
+
+  it('should not render a notice when no notice prop passed', () => {
+    expect(component.find('WithStyles(Notice)')).toHaveLength(0);
+  });
+
+  // it('should render SelectStackScript panel', () => {
+  //   expect(component.find('SelectStackScript')).toHaveLength(1);
+  // });
+
+  // it('should render UserDefinedFields panel if user defined fields exist', () => {
+  //   expect(component.find('UserDefinedFieldsPanel')).toHaveLength(1);
+  // });
+
+  // it('should not render UserDefinedFields panel if no user defined fields exist', () => {
+  //   expect(component.find('UserDefinedFields')).toHaveLength(0);
+  // });
+
+  // it('should render SelectImage panel', () => {
+  //   expect(component.find('CreateFromImage')).toHaveLength(1);
+  // });
+
+  // it('should render SelectRegion panel', () => {
+  //   expect(component.find('WithStyles(SelectRegionPanel)')).toHaveLength(1);
+  // });
+
+  // it('should render SelectPlan panel', () => {
+  //   expect(component.find('WithStyles(SelectPlanPanel)')).toHaveLength(1);
+  // });
+
+  // it('should render SelectLabel panel', () => {
+  //   expect(component.find('WithStyles(InfoPanel)')).toHaveLength(1);
+  // });
+
+  // it('should render SelectPassword panel', () => {
+  //   expect(component.find('WithStyles(PasswordPanel)')).toHaveLength(1);
+  // });
+
+  // it('should render SelectAddOns panel', () => {
+  //   expect(component.find('WithStyles(AddonsPanel)')).toHaveLength(1);
+  // });
+});
---
+++ src/features/linodes/LinodesCreate/TabbedContent/FromStackScriptContent.tsx
@@ -0,0 +1,130 @@
+import * as React from 'react';
+import {
+  withStyles,
+  StyleRulesCallback,
+  Theme,
+  WithStyles,
+} from 'material-ui';
+
+import SelectPlanPanel, { ExtendedType } from '../SelectPlanPanel';
+import SelectImagePanel from '../SelectImagePanel';
+import PasswordPanel from '../PasswordPanel';
+import AddonsPanel from '../AddonsPanel';
+import { StateToUpdate as FormState } from '../LinodesCreate';
+
+import getAPIErrorsFor from 'src/utilities/getAPIErrorFor';
+
+import Notice from 'src/components/Notice';
+import SelectRegionPanel, { ExtendedRegion } from 'src/components/SelectRegionPanel';
+import LabelAndTagsPanel from 'src/components/LabelAndTagsPanel';
+
+
+type ClassNames = 'root';
+
+const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
+  root: {},
+});
+
+interface Notice {
+  text: string;
+  level: 'warning' | 'error'; // most likely only going to need these two
+}
+
+interface Props {
+  errors?: Linode.ApiFieldError[];
+  notice?: Notice;
+  updateFormState: (stateToUpdate: FormState[]) => void;
+  selectedImageID: string | null;
+  selectedRegionID: string | null;
+  selectedTypeID: string | null;
+  images: Linode.Image[];
+  regions: ExtendedRegion[];
+  types: ExtendedType[];
+  backups: boolean;
+  privateIP: boolean;
+  getBackupsMonthlyPrice: () => number | null;
+  label: string | null;
+  password: string | null;
+}
+
+type CombinedProps = Props & WithStyles<ClassNames>;
+
+const errorResources = {
+  type: 'A plan selection',
+  region: 'A region selection',
+  label: 'A label',
+  root_pass: 'A root password',
+};
+
+export const FromStackScriptContent: React.StatelessComponent<CombinedProps> = (props) => {
+  const { notice, errors, backups, privateIP, updateFormState,
+    getBackupsMonthlyPrice, label, images, regions, selectedImageID,
+    selectedRegionID, selectedTypeID, password, types } = props;
+
+  const hasErrorFor = getAPIErrorsFor(errorResources, errors);
+  const generalError = hasErrorFor('none');
+
+  return (
+    <React.Fragment>
+      {notice &&
+        <Notice
+          text={notice.text}
+          error={(notice.level) === 'error'}
+          warning={(notice.level === 'warning')}
+        />
+      }
+      {generalError &&
+        <Notice text={generalError} error={true} />
+      }
+      <SelectImagePanel
+        images={images}
+        handleSelection={(id: string) =>
+          updateFormState([{ stateKey: 'selectedImageID', newValue: id }])}
+        selectedImageID={selectedImageID}
+      />
+      <SelectRegionPanel
+        error={hasErrorFor('region')}
+        regions={regions}
+        handleSelection={(id: string) =>
+          updateFormState([{ stateKey: 'selectedRegionID', newValue: id }])}
+        selectedID={selectedRegionID}
+        copy="Determine the best location for your Linode."
+      />
+      <SelectPlanPanel
+        error={hasErrorFor('type')}
+        types={types}
+        onSelect={(id: string) =>
+          updateFormState([{ stateKey: 'selectedTypeID', newValue: id }])}
+        selectedID={selectedTypeID}
+      />
+      <LabelAndTagsPanel
+        labelFieldProps={{
+          label: 'Linode Label',
+          value: label || '',
+          onChange: e =>
+            updateFormState([{ stateKey: 'label', newValue: e.target.value }]),
+          errorText: hasErrorFor('label'),
+        }}
+      />
+      <PasswordPanel
+        error={hasErrorFor('root_pass')}
+        password={password}
+        handleChange={v =>
+          updateFormState([{ stateKey: 'password', newValue: v }])}
+      />
+      <AddonsPanel
+        backups={backups}
+        backupsMonthly={getBackupsMonthlyPrice()}
+        privateIP={privateIP}
+        changeBackups={() =>
+          updateFormState([{ stateKey: 'backups', newValue: !backups }])}
+        changePrivateIP={() =>
+          updateFormState([{ stateKey: 'privateIP', newValue: !privateIP }])}
+      />
+    </React.Fragment>
+  );
+};
+
+const styled = withStyles(styles, { withTheme: true });
+
+export default styled<Props>(FromStackScriptContent);
