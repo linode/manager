4fa3f12f2c98fcd555adb11dd8107958c8f8a6ee
Patrick Thiel <pthiel@linode.com>
2018-06-12 17:12:56 -0400

    Revert "Remove class based assertions"
    This reverts commit c66833160d8d6342b87ab95e13eb53cf457eb1cf.

--- e2e/pageobjects/linode-detail/linode-detail-backups.page.js
+++ e2e/pageobjects/linode-detail/linode-detail-backups.page.js
@@ -55,8 +55,7 @@ class Backups extends Page {
         expect(this.cancelDescription.isVisible()).toBe(true);
         expect(this.cancelButton.isVisible()).toBe(true);
         expect(this.manualSnapshotName.isVisible()).toBe(true);
-        expect(this.cancelButton.getTagName()).toBe('button');
-        this.cancelDescription.waitForText();
+        expect(this.cancelButton.getAttribute('class')).toContain('destructive');
     }
 
     enableBackups() {
