06f7589022940cbe79e6e53d15849d92ad663237
Alban Bailly <abailly@linode.com>
2018-06-13 16:37:14 -0400

    fix Checkout sidebar for all linode create tab instances

--- src/features/linodes/LinodesCreate/TabbedContent/FromImageContent.tsx
+++ src/features/linodes/LinodesCreate/TabbedContent/FromImageContent.tsx
@@ -36,7 +36,11 @@ type ClassNames = 'root' | 'main' | 'sidebar';
 const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
   root: {},
   main: {},
-  sidebar: {},
+  sidebar: {
+    [theme.breakpoints.up('lg')]: {
+      marginTop: -130,
+    },
+  },
 });
 
 interface Notice {
