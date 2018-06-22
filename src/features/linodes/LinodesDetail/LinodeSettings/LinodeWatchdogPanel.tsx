ccd55a428f3ebf576703394a6f649e100916b772
Robert Stires <rstires@linode.com>
2018-06-22 07:28:46 -0400

    Update state handling. Toggle submitting on error catch.

--- src/features/linodes/LinodesDetail/LinodeSettings/LinodeWatchdogPanel.tsx
+++ src/features/linodes/LinodesDetail/LinodeSettings/LinodeWatchdogPanel.tsx
@@ -7,9 +7,9 @@ import {
   StyleRulesCallback,
   Theme,
   WithStyles,
-} from 'material-ui';
-import Typography from 'material-ui/Typography';
-import FormControlLabel from 'material-ui/Form/FormControlLabel';
+} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import { updateLinode } from 'src/services/linodes';
 import ExpansionPanel from 'src/components/ExpansionPanel';
@@ -30,61 +30,52 @@ interface Props {
 }
 
 interface State {
+  linodeId: number;
+  currentStatus: boolean;
   submitting: boolean;
   success?: string;
-  error?: string;
+  errors?: string;
 }
 
 type CombinedProps = Props & RouteComponentProps<{}> & WithStyles<ClassNames>;
 
 class LinodeWatchdogPanel extends React.Component<CombinedProps, State> {
   state: State = {
+    currentStatus: this.props.currentStatus,
+    linodeId: this.props.linodeId,
     submitting: false,
   };
 
-  setSubmitting = (v: boolean) => this.setState(
-    set(lensPath(['submitting']), v),
-  )
-
-  setSuccess = (message: string) => this.setState(
-    set(
-      lensPath(['success']),
-      message,
-    ),
-  )
-
-  setError = (errors: Linode.ApiFieldError[]) => this.setState(
-    set(
-      lensPath(['error']),
-      errors,
-    ),
-  )
-
   toggleWatchdog = (e: React.ChangeEvent<HTMLElement>, value: boolean) => {
-    this.setSubmitting(true);
+    this.setState(setSubmitting(true));
 
     updateLinode(this.props.linodeId, { watchdog_enabled: value })
       .then((response) => {
-        this.setSubmitting(false);
-        this.setSuccess(`Watchdog succesfully ${value ? 'enabled' : 'disabled.'}`);
+        this.setState(compose(
+          setSubmitting(false),
+          setSuccess(`Watchdog succesfully ${value ? 'enabled' : 'disabled.'}`),
+          setCurrentStatus(response.data.watchdog_enabled),
+        ));
       })
       .catch(() => {
-        this.setSuccess(`Unable to ${value ? 'disable' : 'enable'} Watchdog.`);
+        this.setState(compose(
+          setSubmitting(false),
+          setErrors([{ field: 'none', reason: `Unable to ${value ? 'disable' : 'enable'} Watchdog.` }]),
+        ));
       });
   }
 
   render() {
-    const { currentStatus } = this.props;
-    const { success, error } = this.state;
+    const { currentStatus, submitting, success, errors } = this.state;
 
     return (
       <React.Fragment>
         <ExpansionPanel defaultExpanded heading="Shutdown Watchdog">
           <Grid container>
             {
-              (success || error) &&
+              (success || errors) &&
               <Grid item xs={12}>
-                <Notice success={Boolean(success)} error={Boolean(error)} text={success || error} />
+                <Notice success={Boolean(success)} error={Boolean(errors)} text={success || errors} />
               </Grid>
             }
             <Grid item xs={2}>
@@ -97,7 +88,7 @@ class LinodeWatchdogPanel extends React.Component<CombinedProps, State> {
                   />
                 }
                 label={currentStatus ? 'Enabled' : 'Disabled'}
-                disabled={this.state.submitting}
+                disabled={submitting}
               />
             </Grid>
             <Grid item xs={10}>
@@ -114,6 +105,21 @@ class LinodeWatchdogPanel extends React.Component<CombinedProps, State> {
     );
   }
 }
+
+const L = {
+  currentStatus: lensPath(['currentStatus']),
+  error: lensPath(['errors']),
+  submitting: lensPath(['submitting']),
+  success: lensPath(['success']),
+};
+
+const setCurrentStatus = (v: boolean) => set(L.currentStatus, v);
+
+const setErrors = (v: Linode.ApiFieldError[]) => set(L.error, v);
+
+const setSubmitting = (v: boolean) => set(L.submitting, v);
+
+const setSuccess = (v: string) => set(L.success, v);
 
 const styled = withStyles(styles, { withTheme: true });
 
