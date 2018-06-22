69105192d2dda37343c177d3aa6e17b5167d5ae9
Marty McKenna <mmckenna.phila@gmail.com>
2018-06-22 09:55:09 -0400

    add debounce to search and create request

--- src/features/StackScripts/SelectStackScriptPanel/SelectStackScriptPanel.tsx
+++ src/features/StackScripts/SelectStackScriptPanel/SelectStackScriptPanel.tsx
@@ -576,6 +576,33 @@ class Container extends React.Component<ContainerCombinedProps, ContainerState> 
   }
 
   handleSearch = (value: string) => {
+    const { request } = this.props;
+    const filter = {
+      ["+or"]: [
+        {
+          "label": {
+            ["+contains"]: "security"
+          }
+        },
+        {
+          "description": {
+            ["+contains"]: "security"
+          }
+        },
+        {
+          ["+and"]: [
+            {
+              "username": "linode"
+            }
+          ]
+        }
+      ]
+    }
+    request({ page: 1, page_size: 50 }, filter)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(e => console.log(e))
     console.log(`making request with ${value}`);
   }
 
