1329f354c9b07b1b36a206744e972be14375a473
Jared Kobos <jaredkobos@gmail.com>
2018-06-25 09:29:25 -0400

    * Combine label and client connection throttle into a single panel * Fix linter warnings * Remove ConnectionThrottlePanel

--- src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSettings/NodeBalancerSettings.tsx
+++ src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSettings/NodeBalancerSettings.tsx
@@ -4,23 +4,26 @@ import { clamp, compose, defaultTo } from 'ramda';
 
 import { StyleRulesCallback, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 
-import InputAdornment from '@material-ui/core/InputAdornment';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
-
-import LabelAndTagsPanel from 'src/components/LabelAndTagsPanel';
+import ActionsPanel from 'src/components/ActionsPanel';
+import Button from 'src/components/Button';
+import Grid from 'src/components/Grid';
 import Notice from 'src/components/Notice';
+import TextField from 'src/components/TextField';
+
+import { updateNodeBalancer } from 'src/services/nodebalancers';
 
 import defaultNumeric from 'src/utilities/defaultNumeric';
 import getAPIErrorFor from 'src/utilities/getAPIErrorFor';
-
-import { updateNodeBalancer } from 'src/services/nodebalancers';
 import scrollErrorIntoView from 'src/utilities/scrollErrorIntoView';
-import ClientConnectionThrottlePanel from '../../ClientConnectionThrottlePanel';
 
 type ClassNames = 'root'
 | 'title'
-| 'adornment';
+| 'adornment'
+| 'inner' 
+| 'expPanelButton';
 
 const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
   root: {},
@@ -31,6 +34,13 @@ const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
   adornment: {
     fontSize: '.9rem',
     marginRight: 10,
+  },
+  inner: {
+    paddingBottom: theme.spacing.unit * 3,
+  },
+  expPanelButton: {
+    padding: 0,
+    marginTop: theme.spacing.unit * 2,
   },
 });
 
@@ -43,67 +53,67 @@ interface Props {
 interface State {
   isSubmitting: boolean;
   errors?: Linode.ApiFieldError[];
-  success?: SuccessMessages;
+  success?: string;
   fields: FieldsState;
 }
-
-type updateNodeBalancerData = 'label' | 'client_conn_throttle';
 
 interface FieldsState {
   label?: string;
   client_conn_throttle?: number;
 }
 
-interface SuccessMessages {
-  label?: string;
-  client_conn_throttle?: string;
-}
-
 type CombinedProps = Props & WithStyles<ClassNames>;
 
 const errorResources = {
-  label: 'label',
   client_conn_throttle: 'client connection throttle',
+  label: 'label',
 };
 
 class NodeBalancerSettings extends React.Component<CombinedProps, State> {
   static defaultFieldsStates = (props: CombinedProps) => ({
-    label: props.nodeBalancerLabel,
     client_conn_throttle: props.nodeBalancerClientConnThrottle,
+    label: props.nodeBalancerLabel,
   })
 
-  static defaultSuccessMessages = {
-    label: undefined,
-    client_conn_throttle: undefined,
-  };
-
   state: State = {
-    isSubmitting: false,
     errors: undefined,
     fields: NodeBalancerSettings.defaultFieldsStates(this.props),
-    success: NodeBalancerSettings.defaultSuccessMessages,
+    isSubmitting: false,
+    success: undefined,
   };
 
-  updateNodeBalancer = (whichField: updateNodeBalancerData) => {
+  handleLabelInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { fields } = this.state;
+    this.setState({
+      fields: {
+        ...fields,
+        label: e.target.value,
+      },
+    })
+  }
+
+  handleThrottleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { fields } = this.state;
+    this.setState({
+      fields: {
+        ...fields,
+        client_conn_throttle: controlClientConnectionThrottle(e.target.value),
+      },
+    });
+  }
+
+  updateNodeBalancer = () => {
     const { label, client_conn_throttle } = this.state.fields;
     this.setState({
-      isSubmitting: true,
       errors: undefined,
-      success: NodeBalancerSettings.defaultSuccessMessages,
+      isSubmitting: true,
+      success: undefined,
     });
-    const data = (whichField === 'label') ? { label } : { client_conn_throttle };
-    updateNodeBalancer(this.props.nodeBalancerId, data).then((data) => {
+    const data = { label, client_conn_throttle };
+    updateNodeBalancer(this.props.nodeBalancerId, data).then(() => {
       this.setState({
         isSubmitting: false,
-        success: (whichField === 'label')
-          ? {
-            ...this.state.success,
-            label: 'Label updated successfully',
-          }
-          : {
-            ...this.state.success,
-            client_conn_throttle: 'Client Connection Throttle updated successfully',
-          },
+        success:'NodeBalancer settings updated successfully',
       });
     }).catch((error) => {
       this.setState({ isSubmitting: false, errors: error.response.data.errors }, () => {
@@ -116,60 +126,55 @@ class NodeBalancerSettings extends React.Component<CombinedProps, State> {
     const { fields, isSubmitting, success } = this.state;
     const { classes } = this.props;
     const hasErrorFor = getAPIErrorFor(errorResources, this.state.errors);
-    const generalError = hasErrorFor('none');
+    const generalError = hasErrorFor('none');  
 
     return (
       <React.Fragment>
         <Typography variant="headline" className={classes.title}>
           Settings
         </Typography>
-
-        {generalError && <Notice error>{generalError}</Notice>}
-        <LabelAndTagsPanel
-          isForm={{
-            action: () => this.updateNodeBalancer('label'),
-            isSubmitting,
-            success: success!.label,
-          }}
-          labelFieldProps={{
-            onChange: e => this.setState({
-              fields: {
-                ...fields,
-                label: e.target.value,
-              },
-            }),
-            value: this.state.fields.label || '',
-            label: 'NodeBalancer Label',
-            placeholder: 'Enter a label between 3 and 32 characters',
-            errorText: hasErrorFor('label'),
-          }}
-        />
-
-        <ClientConnectionThrottlePanel
-          isForm={{
-            action: () => this.updateNodeBalancer('client_conn_throttle'),
-            isSubmitting,
-            success: success!.client_conn_throttle,
-          }}
-          textFieldProps={{
-            label: 'Client Connection Throttle',
-            placeholder: '0',
-            InputProps: {
-              endAdornment:
-                <InputAdornment position="end" className={classes.adornment}>
-                  / second
-                </InputAdornment>,
-            },
-            errorText: hasErrorFor('client_conn_throttle'),
-            value: defaultTo(0, fields.client_conn_throttle),
-            onChange: e => this.setState({
-              fields: {
-                ...fields,
-                client_conn_throttle: controlClientConnectionThrottle(e.target.value),
-              },
-            }),
-          }}
-        />
+        <Paper style={{ padding: 24 }}>
+          <Grid item xs={12}>
+            {generalError && <Notice error text={generalError}/>}
+            {success && <Notice success text={success}/>}
+          </Grid>
+          <div className={classes.inner}>
+            <TextField 
+              data-qa-label-panel
+              errorText={hasErrorFor('label')}
+              label='Label'
+              placeholder='Enter a label between 3 and 32 characters'
+              onChange={this.handleLabelInputChange}
+              value={fields.label}
+              />
+          </div>
+          <div className={classes.inner}>
+            <TextField 
+                data-qa-connection-throttle
+                InputProps={{
+                  endAdornment: <span className={classes.adornment}>
+                  / second</span>,
+                }}
+                errorText={hasErrorFor('client_conn_throttle')}
+                label='Client Connection Throttle'
+                onChange={this.handleThrottleInputChange}
+                placeholder='0'
+                value={defaultTo(0, fields.client_conn_throttle)}
+              />
+            </div>
+          <ActionsPanel
+            className={classes.expPanelButton}
+          >
+            <Button
+              onClick={this.updateNodeBalancer}
+              type="primary"
+              disabled={isSubmitting}
+              data-qa-label-save
+            >
+              Save
+          </Button>
+          </ActionsPanel>
+        </Paper>
       </React.Fragment>
     );
   }
