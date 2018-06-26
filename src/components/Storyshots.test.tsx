459faf9788ee1afe91a78719d621489a49d0b257
Patrick Thiel <pthiel@linode.com>
2018-06-26 10:19:08 -0400

    set per pixel sensitivity threshold

--- src/components/Storyshots.test.tsx
+++ src/components/Storyshots.test.tsx
@@ -3,11 +3,15 @@ import initStoryshots, { imageSnapshot } from '@storybook/addon-storyshots';
 const getMatchOptions = ({context : {kind, story}, url}) => {
     return {
         // 
-        failureThreshold: '0.10',
+        failureThreshold: 0.10,
         failureThresholdType: 'percent',
+        customDiffConfig:  { threshold: 0.10 }
     }
 }
 
 jest.setTimeout(30000);
 
-initStoryshots({suite: 'Image storyshots', test: imageSnapshot({storybookUrl: 'http://localhost:6006', getMatchOptions})});
+initStoryshots({
+    suite: 'Image storyshots',
+    test: imageSnapshot({storybookUrl: 'http://localhost:6006', getMatchOptions})
+});
