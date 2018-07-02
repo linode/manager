17fcfba0f5b7d183bfb76ee929ecf66fbd3d721a
Marty McKenna <mmckenna.phila@gmail.com>
2018-07-02 16:56:57 -0400

    add conditional SelectStackScripts panel

--- src/features/StackScripts/StackScriptsLanding.tsx
+++ src/features/StackScripts/StackScriptsLanding.tsx
@@ -13,11 +13,13 @@ import { compose } from 'ramda';
 import setDocs, { SetDocsProps } from 'src/components/DocsSidebar/setDocs';
 import Grid from 'src/components/Grid';
 import IconTextLink from 'src/components/IconTextLink';
+import PromiseLoader from 'src/components/PromiseLoader';
+
 import PlusSquare from '../../../src/assets/icons/plus-square.svg';
 
 import SelectStackScriptPanel from './SelectStackScriptPanel';
 
-import { images } from 'src/__data__/images';
+import { getImages } from 'src/services/images';
 
 
 type ClassNames = 'root';
@@ -28,12 +30,22 @@ const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
 
 interface Props { }
 
+interface PreloadedProps {
+  images: { response: Linode.Image[] };
+}
+
 interface State {
 }
 
 type CombinedProps = Props
   & SetDocsProps
-  & WithStyles<ClassNames>;
+  & WithStyles<ClassNames>
+  & PreloadedProps;
+
+  const preloaded = PromiseLoader<Props>({
+    images: () => getImages()
+      .then(response => response.data || []),
+  });
 
 export class StackScriptsLanding extends React.Component<CombinedProps, State> {
   state: State = {
@@ -51,15 +63,17 @@ export class StackScriptsLanding extends React.Component<CombinedProps, State> {
     this.setState({ selectedTab: value });
   }
 
-  filterPublicImages = () => {
+  filterPublicImages = (images: Linode.Image[]) => {
     // get images and preloaded and give us just the public ones
     // to pass to selectstackscriptpanel
     // we dont' want to display the deprecated ones because
     // they're useless.
-    return;
+    return images.filter((image: Linode.Image) => image.is_public)
   }
 
   render() {
+
+    const { images } = this.props;
 
     return (
       <React.Fragment>
@@ -78,7 +92,7 @@ export class StackScriptsLanding extends React.Component<CombinedProps, State> {
             />
           </Grid>
           <SelectStackScriptPanel
-            publicImages={images}
+            publicImages={this.filterPublicImages(images.response)}
             noHeader={true}
           />
         </Grid>
@@ -90,6 +104,7 @@ export class StackScriptsLanding extends React.Component<CombinedProps, State> {
 const styled = withStyles(styles, { withTheme: true });
 
 export default compose(
+  preloaded,
   styled,
   setDocs(StackScriptsLanding.docs),
 )(StackScriptsLanding);
