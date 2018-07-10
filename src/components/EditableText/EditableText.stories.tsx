0d910cb95f8333f4c8da46a40c1e4972a9498ce2
Pat Thiel <patthiel@gmail.com>
2018-07-10 08:47:51 -0400

    M3-845 - Editable Text Storybook UI Test (#3515)
    * Add: editable text component ui test

--- src/components/EditableText/EditableText.stories.tsx
+++ src/components/EditableText/EditableText.stories.tsx
@@ -1,27 +1,50 @@
 import * as React from 'react';
+
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 
 import ThemeDecorator from '../../utilities/storybookDecorators';
 
 import EditableText from './EditableText';
 
+
+class InteractiveEditableText extends React.Component {
+  mounted: boolean = false;
+  state = {
+    text: 'Edit me!',
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  omponentDidMount() {
+    this.mounted = true;
+  }
+
+  editText = (value: string) => {
+    this.setState({ text: value });
+  }
+
+  cancelEdit = () => {
+    this.forceUpdate();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <EditableText
+          variant="headline"
+          text={this.state.text}
+          onEdit={this.editText}
+          onCancel={this.cancelEdit}
+        />
+      </React.Fragment>
+    )
+  }
+}
+
 storiesOf('Editable Text', module)
 .addDecorator(ThemeDecorator)
 .add('Headline & Title', () => (
-  <div>
-    <EditableText
-        variant="headline"
-        text="Edit me!"
-        onEdit={action('edit-text')}
-        onCancel={action('cancel-edit')}
-    />
-    <br /><br />
-    <EditableText
-        variant="title"
-        text="Edit me!"
-        onEdit={action('edit-text')}
-        onCancel={action('cancel-edit')}
-    />
-  </div>
+  <InteractiveEditableText />
 ));
