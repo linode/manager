5ae73cb79b4f6a3a1158a7464f878a5c18229bbb
Patrick Thiel <pthiel@linode.com>
2018-07-12 13:28:27 -0400

    add: storyshot integration

---
+++ src/components/Storyshots.test.tsx
@@ -0,0 +1,22 @@
+import initStoryshots, { imageSnapshot } from '@storybook/addon-storyshots';
+
+const getMatchOptions = ({context : {kind, story}, url}) => ({
+    failureThreshold: 0.2,
+    failureThresholdType: 'percent',
+    customDiffConfig:  { threshold: 0.15 }
+});
+
+jest.setTimeout(30000);
+
+const beforeScreenshot = (page, {context : {kind, story}, url}) => {
+  return new Promise(resolve =>
+      setTimeout(() => {
+          resolve();
+      }, 600)
+  )
+}
+
+initStoryshots({
+    suite: 'Image storyshots',
+    test: imageSnapshot({storybookUrl: 'http://localhost:6006', getMatchOptions, beforeScreenshot})
+});
