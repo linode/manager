527f3fb67bec8856f58a6b17319c4b87493e2ae0
Jared Kobos <jaredkobos@gmail.com>
2018-07-16 13:38:22 -0400

    * Add form for updating email/username * Add routing logic to Profile.tsx * Add DisplaySettings component (will later hold timezone as well) * Add tests for 2 new components

---
+++ src/features/profile/DisplaySettings/EmailChangeForm.test.tsx
@@ -0,0 +1,45 @@
+import { mount } from 'enzyme';
+import * as React from 'react';
+
+import LinodeThemeWrapper from 'src/LinodeThemeWrapper';
+
+import EmailChangeForm from './EmailChangeForm';
+
+describe('Email change form', () => {
+  const onSubmit = jest.fn();
+  const onChange = jest.fn();
+  
+  const component = mount(
+    <LinodeThemeWrapper>
+      <EmailChangeForm
+        email='example@example.com'
+        error={undefined}
+        username='ThisUser'
+        submitting={false}
+        handleChange={onChange}
+        onSubmit={onSubmit}
+      />
+    </LinodeThemeWrapper>,
+  );
+
+  const submitButton = component.find('Button[data-qa-submit]');
+
+  it('should render textfields for username and email.', () => {
+    expect(component.find('TextField')).toHaveLength(2);
+  });
+
+  it('the username field should be disabled.', () => {
+    expect(component.find('TextField[data-qa-username]').props().disabled).toBeTruthy();
+  });
+
+  describe('submit button', () => {
+    it('should exist', () => {
+      expect(submitButton.length).toBe(1)
+    });
+
+    it('should call onSubmit when clicked', () => {
+      submitButton.simulate('click');
+      expect(onSubmit).toHaveBeenCalledTimes(1);
+    });
+  });
+});
---
+++ src/features/profile/DisplaySettings/EmailChangeForm.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+
+import {
+    StyleRulesCallback,
+    Theme,
+    WithStyles,
+    withStyles,
+  } from '@material-ui/core/styles';  
+  
+import ActionsPanel from 'src/components/ActionsPanel';
+import Button from 'src/components/Button';
+import TextField from 'src/components/TextField';
+
+type ClassNames = 'root';
+
+const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
+  root: {},
+});
+
+interface Props {
+  error: string | undefined;
+  username: string;
+  email: string;
+  submitting: boolean;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  onSubmit: () => void;
+}
+
+type CombinedProps = Props & WithStyles<ClassNames>;
+
+const EmailChangeForm: React.StatelessComponent<CombinedProps> = (props) => {
+    const { email,
+            error, 
+            handleChange,
+            onSubmit,
+            submitting,
+            username, } = props;
+    return (
+        <React.Fragment>
+            <TextField
+                disabled
+                label="Username"
+                value={username}
+                errorGroup="display-settings-email"
+                data-qa-username
+            />
+            <TextField
+                label="Email"
+                value={email}
+                onChange={handleChange}
+                errorText={error}
+                errorGroup="display-settings-email"
+                error={Boolean(error)}
+                data-qa-email
+            />
+            <ActionsPanel>
+            <Button
+                type="primary"
+                onClick={onSubmit}
+                loading={submitting}
+                data-qa-submit
+            >
+                Save
+            </Button>
+            </ActionsPanel>
+        </ React.Fragment>
+    )
+}
+
+const styled = withStyles(styles, { withTheme: true });
+
+export default styled(EmailChangeForm);
