6ed80229fb90702acd2f3193083bdd4e2ccdebac
Patrick Thiel <pthiel@linode.com>
2018-07-24 11:27:32 -0400

    Add create image test

---
+++ e2e/specs/images/create.spec.js
@@ -0,0 +1,39 @@
+const { constants } = require('../../constants');
+const { getImages } = require('../../setup/setup');
+
+import ConfigureImage from '../../pageobjects/configure-image.page';
+
+import { apiCreateLinode, apiDeleteAllLinodes } from '../../utils/common';
+
+describe('Images - Create Suite', () => {
+    beforeAll(() => {
+        apiCreateLinode();
+        browser.url(constants.routes.images);
+    });
+
+    afterAll(() => {
+        apiDeleteAllLinodes();
+    })
+
+    it('should display create image drawer', () => {
+        ConfigureImage.placeholderMsg.waitForVisible(constants.wait.normal);
+        ConfigureImage.placeholderButton.waitForVisible(constants.wait.normal);
+        ConfigureImage.placeholderButton.click();
+        ConfigureImage.baseElementsDisplay();
+    });
+
+    it('should configure the image', () => {
+        const imageConfig = {
+            label: 'my-test-image',
+            description: 'some image description!'
+        }
+        ConfigureImage.configure(imageConfig);
+    });
+
+    it('should schedule the image for creation', () => {
+        ConfigureImage.create();
+        const token = browser.readToken();
+        const privateImages = getImages(token);
+        console.log(privateImages);
+    });
+});
