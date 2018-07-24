eda04087d39b56a9661952142c041dffb6b84208
Marty <mmckenna.phila@gmail.com>
2018-07-24 12:38:09 -0400

    table rows

--- src/features/Search/SearchLanding.tsx
+++ src/features/Search/SearchLanding.tsx
@@ -3,14 +3,25 @@ import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 import {compose} from 'ramda';
 
-import { parseQueryParams } from 'src/utilities/queryParams';
-
 import {
   StyleRulesCallback,
   Theme,
   withStyles,
   WithStyles,
 } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import ExpansionPanel from 'src/components/ExpansionPanel';
+import PaginationFooter from 'src/components/PaginationFooter';
+import TableRowEmptyState from 'src/components/TableRowEmptyState';
+
+import { parseQueryParams } from 'src/utilities/queryParams';
+
+import { getLinodes } from 'src/services/linodes';
 
 type ClassNames = 'root';
 
@@ -26,6 +37,11 @@ interface Query {
 
 interface State {
   query: Query;
+  linodes: Linode.Linode[];
+  volumes: Linode.Volume[];
+  domains: Linode.Domain[];
+  stackScripts: Linode.StackScript.Response[];
+  nodeBalancers: Linode.NodeBalancer[];
 }
 
 type CombinedProps = Props
@@ -37,19 +53,95 @@ class SearchLanding extends React.Component<CombinedProps, State> {
     super(props);
 
     /*
-    * Will look like: { query: 'hello world' }
+    * Will look like this: { query: 'hello world' }
     */
     const query = parseQueryParams(props.location.search.replace('?', '')) as Query;
 
     this.state = {
       query,
+      linodes: [],
+      volumes: [],
+      domains: [],
+      stackScripts: [],
+      nodeBalancers: [],
     }
   }
 
-  render() {
-    console.log(this.state.query);
+  componentDidMount() {
+    const { query: { query } } = this.state;
+    Promise.all([
+      getLinodes(
+        { page: 1, page_size: 25 },
+        {
+          "label": query,
+        }
+      )])
+      .then(response => {
+        const linodeData = response[0].data;
+        this.setState({
+          linodes: linodeData,
+        })
+      })
+      .catch(e => e);
+  }
+
+  renderPanels = () => {
+    const { linodes } = this.state;
+
+    const iterables = [
+      {
+        label: 'Linodes',
+        data: linodes,
+      }
+    ];
+
+    console.log(this.state.linodes);
+
+    return iterables.map(iterable => {
+      return (
+        <ExpansionPanel
+          heading="Linodes"
+          key={iterable.label}
+        >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell>{iterable.label}</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(!iterable.data.length)
+                ? <TableRowEmptyState colSpan={2} />
+                : iterable.data.map(eachEntity => this.renderPanelRow(eachEntity))}
+            </TableBody>
+          </Table>
+          {true &&
+            <PaginationFooter
+              count={25}
+              page={1}
+              pageSize={25}
+              handlePageChange={() => console.log('change page')}
+              handleSizeChange={() => console.log('change size')}
+            />
+          }
+        </ExpansionPanel>
+      )
+    })
+  }
+
+  renderPanelRow = (data: any) => {
     return (
-      <div>Hello World</div>
+      <TableRow key={data.label}>
+        <TableCell></TableCell>
+        <TableCell>{data.label}</TableCell>
+      </TableRow>
+    )
+  }
+
+  render() {
+    return (
+      this.renderPanels()
     );
   }
 }
