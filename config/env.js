2e5b5bd3fac1281799adcc1dc807139ab9342025
Robert Stires <rstires@linode.com>
2018-08-01 13:24:18 -0400

    Add product version to Sentry reporting. Add more API info on reports. (#3637)
    * Add product version to Sentry reporting. Add more API info on reports.
    
    * Move version to process.env
    
    * Remove console.log

--- config/env.js
+++ config/env.js
@@ -74,6 +74,7 @@ function getClientEnvironment(publicUrl) {
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
+        VERSION: paths.appVersion,
       }
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
