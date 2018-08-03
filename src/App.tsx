722107b882750a545c6512828f572eeea11a8ee3
Andrew Sauber <a3sauber@gmail.com>
2018-08-03 13:03:58 -0400

    use fullpath import for Observable

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { connect, Dispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { bindActionCreators, compose } from 'redux';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import 'typeface-lato';
 
 import { StyleRulesCallback, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
