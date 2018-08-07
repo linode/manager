ce8a1b7a4fbe4b7b07a02f42c63ba569ef4538d4
Marty McKenna <mmckenna.phila@gmail.com>
2018-08-07 10:02:24 -0400

    split panels into seperate components

--- src/features/Help/Panels/SearchPanel.tsx
+++ src/features/Help/Panels/SearchPanel.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import { Link } from 'react-router-dom';
-
 import Paper from '@material-ui/core/Paper';
 import {
   StyleRulesCallback,
@@ -11,19 +9,8 @@ import {
 } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-import Grid from 'src/components/Grid';
-
-import LinodeIcon from 'src/assets/addnewmenu/linode.svg';
-
 type ClassNames = 'root'
-  | 'searchHeading'
-  | 'popularPostsRight'
-  | 'popularPostsLeft'
-  | 'contentBlock'
-  | 'otherWaysHeading'
-  | 'otherWays'
-  | 'otherWaysWrapper'
-  | 'post';
+  | 'searchHeading';
 
 const styles: StyleRulesCallback<ClassNames> = (theme: Theme & Linode.Theme) => ({
   root: {
@@ -36,33 +23,6 @@ const styles: StyleRulesCallback<ClassNames> = (theme: Theme & Linode.Theme) => 
     color: theme.color.white,
     fontSize: '1.2em',
   },
-  popularPostsRight: {
-    borderLeft: `.2px solid ${theme.color.grey1}`,
-  },
-  popularPostsLeft: {
-    borderRight: `.2px solid ${theme.color.grey}`,
-  },
-  contentBlock: {
-    marginTop: theme.spacing.unit * 2,
-    backgroundColor: theme.color.white,
-    padding: theme.spacing.unit * 2,
-  },
-  otherWaysWrapper: {
-    marginTop: theme.spacing.unit * 2,
-    padding: theme.spacing.unit * 2,
-  },
-  otherWaysHeading: {
-    textAlign: 'center',
-    marginTop: theme.spacing.unit * 4,
-  },
-  otherWays: {
-    textAlign: 'center',
-    backgroundColor: theme.color.white,
-    margin: theme.spacing.unit * 2,
-  },
-  post: {
-    padding: theme.spacing.unit * 1,
-  }
 });
 
 interface Props {}
@@ -73,28 +33,6 @@ type CombinedProps = Props & WithStyles<ClassNames>;
 
 class SearchPanel extends React.Component<CombinedProps, State> {
   state: State = {};
-
-  renderPopularDocs = () => {
-    const { classes } = this.props;
-    return (
-      <React.Fragment>
-        <div className={classes.post}><Link target="_blank" to="/help">Hello World</Link></div>
-        <div className={classes.post}><Link target="_blank" to="/help">Hello World</Link></div>
-        <div className={classes.post}><Link target="_blank" to="/help">Hello World</Link></div>
-      </React.Fragment>
-    )
-  }
-
-  renderPopularForumPosts = () => {
-    const { classes } = this.props;
-    return (
-      <React.Fragment>
-        <div className={classes.post}><Link target="_blank" to="/help">Hello World</Link></div>
-        <div className={classes.post}><Link target="_blank" to="/help">Hello World</Link></div>
-        <div className={classes.post}><Link target="_blank" to="/help">Hello World</Link></div>
-      </React.Fragment>
-    )
-  }
 
   render() {
     const { classes } = this.props;
@@ -110,94 +48,6 @@ class SearchPanel extends React.Component<CombinedProps, State> {
             Ways to Get Help
         </Typography>
         </Paper>
-        <Grid
-          container
-          className={classes.contentBlock}
-        >
-          <Grid
-            item
-            xs={6}
-            className={classes.popularPostsLeft}
-          >
-            <Paper>
-              <Typography variant="subheading">
-                Most Popular Documentation:
-              </Typography>
-              {this.renderPopularDocs()}
-            </Paper>
-          </Grid>
-          <Grid
-            item
-            xs={6}
-            className={classes.popularPostsRight}
-          >
-            <Paper>
-              <Typography variant="subheading">
-                Most Popular Community Posts:
-              </Typography>
-              {this.renderPopularForumPosts()}
-            </Paper>
-          </Grid>
-        </Grid>
-        <Typography
-          variant="subheading"
-          className={classes.otherWaysHeading}
-        >
-          Other Ways to Get Help
-        </Typography>
-        <Grid
-          container
-          className={classes.otherWaysWrapper}
-        >
-          <Grid
-            item xs={5}
-            className={classes.otherWays}
-          >
-            <LinodeIcon/>
-            <Typography variant="subheading">
-              View Documentation
-            </Typography>
-            <Typography variant="caption">
-              View Linode Documentation
-            </Typography>
-          </Grid>
-          <Grid
-            item xs={5}
-            className={classes.otherWays}
-          >
-            <LinodeIcon/>
-            <Typography variant="subheading">
-              Search the Community
-            </Typography>
-            <Typography variant="caption">
-              Find help from other Linode users in the Community
-            </Typography>
-          </Grid>
-          <Grid
-            item xs={5}
-            className={classes.otherWays}
-          >
-            <LinodeIcon/>
-            <Typography variant="subheading">
-              Talk to Ada
-            </Typography>
-            <Typography variant="caption">
-              Chat with the Linode Support bot to help troubleshoot
-            </Typography>
-          </Grid>
-          <Grid
-            item xs={5}
-            className={classes.otherWays}
-          >
-            <LinodeIcon/>
-            <Typography variant="subheading">
-              Customer Support
-            </Typography>
-            <Typography variant="caption">
-              Reach out to the Linode Customer Support team for quick responses
-            </Typography>
-          </Grid>
-        </Grid>
       </React.Fragment>
     );
   }
