86e9d22f89cd80481ded75e4ce35ec9391a455e2
Marty McKenna <mmckenna.phila@gmail.com>
2018-08-10 08:58:37 -0400

    add: error handling for ada chat bot

--- src/features/Help/Panels/OtherWays.tsx
+++ src/features/Help/Panels/OtherWays.tsx
@@ -12,6 +12,7 @@ import {
 import Typography from '@material-ui/core/Typography';
 
 import Grid from 'src/components/Grid';
+import Notice from 'src/components/Notice';
 
 import Chat from 'src/assets/icons/chat.svg';
 import Community from 'src/assets/icons/community.svg';
@@ -65,7 +66,9 @@ const styles: StyleRulesCallback<ClassNames> = (theme: Theme & Linode.Theme) => 
 
 interface Props {}
 
-interface State {}
+interface State {
+  error?: string;
+}
 
 type CombinedProps = Props & WithStyles<ClassNames>;
 
@@ -88,9 +91,11 @@ export class OtherWays extends React.Component<CombinedProps, State> {
     /*
     * Show the Ada chat
     */
-    if (typeof this.ada !== 'undefined') {
-      this.ada.show();
+    if (typeof this.ada === 'undefined') {
+      this.setState({ error: 'There was an issue loading the chat at this time. Please try again later.' })
+      return;
     }
+    this.ada.show();
   }
 
   render() {
@@ -142,9 +147,12 @@ export class OtherWays extends React.Component<CombinedProps, State> {
               >
                 <a href="javascript:;" onClick={this.handleAdaInit} className="black">Talk to Ada</a>
               </Typography>
-                <Typography variant="caption">
-                  Chat with the Linode Support bot to help troubleshoot
-                </Typography>
+              {this.state.error &&
+                <Notice error={true} text={this.state.error} />
+              }
+              <Typography variant="caption">
+                Chat with the Linode Support bot to help troubleshoot
+              </Typography>
             </div>
           </Grid>
           <Grid item xs={12} sm={6}>
