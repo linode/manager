723c55cab4cfb05e7b679ea64aa32f9b431d0408
Patrick Thiel <pthiel@linode.com>
2018-08-15 09:16:16 -0400

    Remove hardcoded capabilities

--- e2e/config/browserstack.conf.js
+++ e2e/config/browserstack.conf.js
@@ -12,22 +12,12 @@ const seleniumSettings = require('./selenium-config');
 const username = process.env.MANAGER_USER;
 const password = process.env.MANAGER_PASS;
 
+selectedBrowser['browserstack.local'] = true;
+
 exports.config = merge(wdioMaster.config, {
     host: 'hub.browserstack.com',
     services: ['browserstack'],
-    capabilities: [{
-        browserName: 'chrome',
-        'browserstack.local': true,
-        acceptSslCerts: true,
-        acceptInsecureCerts: true,
-        chromeOptions: {
-            args: [
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-            '--window-size=1600,1080',
-            ]
-        }
-    }],
+    capabilities: [selectedBrowser],
     user: process.env.BROWSERSTACK_USERNAME,
     key: process.env.BROWSERSTACK_ACCESS_KEY,
     browserstackLocal: true,
