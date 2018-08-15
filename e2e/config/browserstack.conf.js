a84f9571cfeff2cf8cd12e5f3e6061fe9fc8070b
Patrick Thiel <pthiel@linode.com>
2018-08-15 16:45:25 -0400

    Add browserstack configs

--- e2e/config/browserstack.conf.js
+++ e2e/config/browserstack.conf.js
@@ -2,17 +2,26 @@ require('dotenv').config();
 
 const { merge } = require('ramda');
 const { argv } = require('yargs');
-const { constants } = require('../constants');
 const { browserConf } = require('./browser-config');
+const { constants } = require('../constants');
 
 const wdioMaster = require('./wdio.conf.js');
 const selectedBrowser = argv.browser ? browserConf[argv.browser] : browserConf['chrome'];
-const seleniumSettings = require('./selenium-config');
+// Enable When browserstack support Selenium 3.13.0
+// const seleniumSettings = require('./selenium-config');
 
 const username = process.env.MANAGER_USER;
 const password = process.env.MANAGER_PASS;
 
-selectedBrowser['browserstack.local'] = true;
+if (argv.local) {
+    selectedBrowser['browserstack.local'] = true;
+} else {
+    selectedBrowser['browserstack.local'] = false;
+}
+
+// Enable When browserstack support Selenium 3.13.0
+//selectedBrowser['browserstack.selenium_version'] = seleniumSettings.version;
+selectedBrowser['browserstack.selenium_version'] = '3.11.0';
 
 exports.config = merge(wdioMaster.config, {
     host: 'hub.browserstack.com',
@@ -20,5 +29,7 @@ exports.config = merge(wdioMaster.config, {
     capabilities: [selectedBrowser],
     user: process.env.BROWSERSTACK_USERNAME,
     key: process.env.BROWSERSTACK_ACCESS_KEY,
-    browserstackLocal: true,
+    browserstackLocal: argv.local ? true : false,
+    browserstackLocalForcedStop: true,
+    waitforTimeout: 25000,
 });
