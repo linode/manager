71ad8b7fa377835c2d18b145e3dffb0269bdc5ba
Marty <mmckenna.phila@gmail.com>
2018-08-22 15:08:04 -0400

    fix: issue where errored Lish sessions would not fallback to error state

--- src/features/Lish/Weblish.tsx
+++ src/features/Lish/Weblish.tsx
@@ -5,15 +5,27 @@ import { Terminal } from 'xterm';
 import { StyleRulesCallback, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 
 import CircleProgress from 'src/components/CircleProgress';
+import ErrorState from 'src/components/ErrorState';
 
 import { getLishSchemeAndHostname, resizeViewPort } from '.';
 
-type ClassNames = 'progress';
+type ClassNames = 'progress' | 'message' | 'errorState';
 
-const styles: StyleRulesCallback<ClassNames> = (theme: Theme) => ({
+const styles: StyleRulesCallback<ClassNames> = (theme: Theme & Linode.Theme) => ({
   progress: {
     height: 'auto',
   },
+  message: {
+    color: 'white',
+    textAlign: 'center',
+    minHeight: '30px',
+    margin: theme.spacing.unit * 2,
+  },
+  errorState: {
+    '& *': {
+      color: '#f4f4f4 !important',
+    }
+  }
 });
 
 interface Props {
@@ -23,18 +35,28 @@ interface Props {
 
 interface State {
   renderingLish: boolean;
+  retryingConnection: boolean;
+  retryAttempts: number;
+  error: string;
 }
 
 type CombinedProps = Props & WithStyles<ClassNames> & RouteComponentProps<{ linodeId?: number }>;
 
+const maxRetryAttempts: number = 3;
+
 export class Weblish extends React.Component<CombinedProps, State> {
   state: State = {
     renderingLish: true,
+    retryingConnection: false,
+    retryAttempts: 0,
+    error: '',
   };
 
   mounted: boolean = false;
 
   socket: WebSocket;
+
+  terminal: Terminal;
 
   componentDidMount() {
     this.mounted = true;
@@ -46,12 +68,26 @@ export class Weblish extends React.Component<CombinedProps, State> {
     this.mounted = false;
   }
 
+  componentDidUpdate(prevProps: CombinedProps, prevState: State) {
+    const { retryAttempts, retryingConnection } = this.state;
+    /*
+    * If our connection failed, and we did not surpass the max number of
+    * reconnection attempts, try to reconnect
+    */
+    if (prevState.retryAttempts !== retryAttempts && retryingConnection) {
+      setTimeout(() => {
+        this.connect();
+      }, 3000);
+    }
+  }
+
   connect() {
     const { linode, token } = this.props;
     const { region } = linode;
 
     this.socket = new WebSocket(
       `${getLishSchemeAndHostname(region)}:8181/${token}/weblish`);
+
     this.socket.addEventListener('open', () => {
       if (!this.mounted) { return; }
       this.setState(
@@ -63,25 +99,67 @@ export class Weblish extends React.Component<CombinedProps, State> {
 
   renderTerminal() {
     const { linode } = this.props;
+    const { retryAttempts } = this.state;
     const { group, label } = linode;
 
-    const terminal = new Terminal({
+    this.terminal = new Terminal({
       cols: 120,
       rows: 40,
       fontFamily: '"Ubuntu Mono", monospace, sans-serif',
     });
 
-    terminal.on('data', (data: string) => this.socket.send(data));
+    this.terminal.on('data', (data: string) => this.socket.send(data));
     const terminalDiv = document.getElementById('terminal');
-    terminal.open(terminalDiv as HTMLElement);
+    this.terminal.open(terminalDiv as HTMLElement);
 
-    terminal.writeln('\x1b[32mLinode Lish Console\x1b[m');
+    this.terminal.writeln('\x1b[32mLinode Lish Console\x1b[m');
 
-    this.socket.addEventListener('message', evt => terminal.write(evt.data));
+    this.socket.addEventListener('message', evt => {
+      let data;
+
+      /*
+      * data is either going to be command line strings
+      * or it's going to look like {type: 'error', reason: 'thing failed'}
+      * the latter be JSON parsed and the other cannot 
+      */
+      try {
+        data = JSON.parse(evt.data)
+      } catch{
+        data = evt.data
+      }
+
+      if (data.type
+        && data.type === 'error'
+        && data.reason.toLowerCase() === 'your session has expired.') {
+        /*
+        * We tried to reconnect 3 times
+        */
+        if (retryAttempts === maxRetryAttempts) {
+          this.setState({
+            error: 'Session could not be initialized. Please try again later'
+          });
+          return;
+        }
+        /*
+        * We've tried less than 3 reconnects
+        */
+        this.setState({
+          retryingConnection: true,
+          retryAttempts: retryAttempts + 1,
+        })
+        return;
+      }
+      this.setState({
+        retryingConnection: false,
+        retryAttempts: 0,
+      })
+      this.terminal.write(evt.data);
+    })
 
     this.socket.addEventListener('close', () => {
-      terminal.destroy();
+      this.terminal.destroy();
       if (!this.mounted) { return; }
+
       this.setState({ renderingLish: false });
     });
 
@@ -89,14 +167,46 @@ export class Weblish extends React.Component<CombinedProps, State> {
     window.document.title = `${linodeLabel} - Linode Lish Console`;
   }
 
+  renderErrorState = () => {
+    const { error } = this.state;
+    const { classes } = this.props;
+    return (
+      <div className={classes.errorState}>
+        <ErrorState errorText={error} />
+      </div>
+    )
+  }
+
+  renderRetryState = () => {
+    const { classes } = this.props;
+    const { retryAttempts } = this.state;
+
+    return (
+      <div className={classes.message}>
+        {`Connection could not be opened. Retrying in 3 seconds...
+          ${retryAttempts} / ${maxRetryAttempts}`}
+        <CircleProgress mini />
+      </div>
+    )
+  }
+
   render() {
     const { classes } = this.props;
+    const { error, retryingConnection } = this.state;
+
+    if (error) {
+      return this.renderErrorState()
+    }
+
+    if (retryingConnection) {
+      return this.renderRetryState()
+    }
 
     return (
       <React.Fragment>
         {this.socket && (this.socket.readyState === this.socket.OPEN)
           ? <div id="terminal" className="terminal" />
-          : <CircleProgress className={classes.progress} noInner/>
+          : <CircleProgress className={classes.progress} noInner />
         }
       </React.Fragment>
     );
