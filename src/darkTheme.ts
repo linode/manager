d45b8a550bed6a577442489d6573d707274ed0bb
Alban Bailly <abailly@linode.com>
2018-08-23 12:58:54 -0400

    fix to DT

--- src/darkTheme.ts
+++ src/darkTheme.ts
@@ -788,7 +788,7 @@ const LinodeTheme: Linode.Theme = {
         zIndex: 1,
         '&:hover': {
           '&$hover': {
-            backgroundColor: '#fbfbfb',
+            color: primaryColors.offBlack,
           },
         },
       },
