9eb2a7728adb767e740556528a2ef8a99e347ad6
Marty McKenna <mmckenna.phila@gmail.com>
2018-08-24 17:06:09 -0400

    abstract truncate function and implement in stackscripts and docs

--- src/components/DocsSidebar/DocComponent.tsx
+++ src/components/DocsSidebar/DocComponent.tsx
@@ -1,8 +1,9 @@
-import { take } from 'ramda';
 import * as React from 'react';
 
 import { StyleRulesCallback, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+
+import truncateText from 'src/utilities/truncateText';
 
 export interface Doc {
   title: string;
@@ -60,7 +61,5 @@ class DocComponent extends React.PureComponent<PropsWithStyles> {
     );
   }
 }
-
-const truncateText = (str: string, len: number) => `${take(len, str)}...`;
 
 export default withStyles(styles, { withTheme: true })<Props>(DocComponent);
