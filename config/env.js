ebd31ca1a0f5442c262eafb5ca776c3b0b44df8f
Jared Kobos <jaredkobos@gmail.com>
2018-08-27 11:22:26 -0400

    Error handling
    * Moved Algolia client initialization to cDM. If there is an error
    (most likely credentials were incorrect or environment variables
    weren't found), the search bar will be disabled and an error message
    will be displayed.

--- config/env.js
+++ config/env.js
@@ -56,7 +56,6 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
 const REACT_APP = /^REACT_APP_/i;
-
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
     .filter(key => REACT_APP.test(key))
