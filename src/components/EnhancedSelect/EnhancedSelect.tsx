c22052e3066a110de3b09f95f9b93fa34310d61b
Jared <jaredkobos@gmail.com>
2018-08-27 17:32:14 -0400

    M3-1102 get help search bar (#3719)
    * Add static search bar and partial (abysmal) styling
    
    * Working static layout
    
    * Working basic search
    
    * Add match highlighting to search results
    
    * Replace .css styles with inline jss
    
    * Remove unneeded declarations from EnhancedSelect.css
    
    * Move styling for highlighted <em>
    
    * Add error handling for Algolia API errors
    
    * Adjust item  highlighting to match comps
    
    * Refactor Algolia search bar to separate component
    
    * Refactor CSS into separate components
    
    * Revert SearchPanel to previous layout
    
    * container and search bar styling
    
    * search suggestion styles
    
    * Post-styling fixes
    
    * Change text to match comps
    * Make sure search results list is empty for empty inputs
    
    * Rename environment variables
    
    * Review feedback
    
    * Remove personal API creds
    * Type argument to initIndex
    
    * Replace _newtab with _blank
    
    * Add environment variables to Travis
    
    * Refactor travis.yml
    
    * Travis part 3/?
    
    * Error handling
    
    * Moved Algolia client initialization to cDM. If there is an error
    (most likely credentials were incorrect or environment variables
    weren't found), the search bar will be disabled and an error message
    will be displayed.
    
    * Replace blank with _blank

--- src/components/EnhancedSelect/EnhancedSelect.tsx
+++ src/components/EnhancedSelect/EnhancedSelect.tsx
@@ -2,8 +2,10 @@ import Downshift, { DownshiftState, StateChangeOptions } from 'downshift';
 import { compose, pathOr } from 'ramda';
 import * as React from 'react';
 
+import InputAdornment from '@material-ui/core/InputAdornment';
 import Paper from '@material-ui/core/Paper';
 import { StyleRulesCallback, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import Search from '@material-ui/icons/Search';
 
 import RenderGuard from 'src/components/RenderGuard';
 import TextField from 'src/components/TextField';
@@ -11,11 +13,16 @@ import TextField from 'src/components/TextField';
 import './EnhancedSelect.css';
 
 type ClassNames = 'root'
-  | 'searchSuggestions';
+  | 'searchSuggestions'
+  | 'searchIcon';
 
 const styles: StyleRulesCallback = (theme: Theme & Linode.Theme) => ({
   root: {
     position: 'relative',
+    width: '100%',
+  },
+  searchIcon: {
+    color: `${theme.color.grey1} !important`,
   },
   searchSuggestions: {
     position: 'absolute',
@@ -25,10 +32,8 @@ const styles: StyleRulesCallback = (theme: Theme & Linode.Theme) => ({
     padding: 0,
     borderRadius: 0,
     border: '1px solid #999',
-    maxHeight: 192,
     overflowY: 'auto',
-    width: '100%',
-    maxWidth: 415,
+    maxWidth: '100%',
     zIndex: 2,
     marginTop: -2,
     '& .enhancedSelect-menu-item': {
@@ -49,6 +54,11 @@ interface Props {
   placeholder?: string;
   inputValue: string;
   onInputValueChange: (input:string) => void;
+  renderItems?: (items:Item, index:number, highlighted:boolean, inputProps:any, classes:string) => React.ReactElement<any>[];
+  noFilter?: boolean;
+  search?: boolean;
+  maxHeight?: number;
+  className?: string;
 }
 
 interface State {}
@@ -56,6 +66,7 @@ interface State {}
 export interface Item {
   value: string;
   label: string;
+  data?: any;
 }
 
 type CombinedProps = Props & WithStyles<ClassNames>;
@@ -90,7 +101,8 @@ class EnhancedSelect extends React.Component<CombinedProps, State> {
   }
 
   getSearchSuggestions = (inputText:string) => {
-    const { options } = this.props;
+    const { options, noFilter } = this.props;
+    if (noFilter) { return options };
     const text = inputText.toLowerCase();
     return options.filter((item:Item) => {
       return item.label ? item.label.toLowerCase().includes(text) : false;
@@ -128,27 +140,33 @@ class EnhancedSelect extends React.Component<CombinedProps, State> {
       inputValue,
       highlightedIndex,
       openMenu,
+      selectHighlightedItem,
       selectedItem,
     } = downshift;
 
-    const { classes, disabled, errorText, helperText, label, placeholder } = this.props;
+    const { classes, className, maxHeight, disabled, errorText, helperText, label, placeholder, search } = this.props;
     const selectedIndex = this.getIndex(selectedItem);
     const placeholderText = placeholder ? placeholder : "Enter a value"
 
     return (
-      <div className={classes.root}>
+      <div className={`${classes.root} ${className}`}>
         <TextField
           data-qa-enhanced-select
+          InputProps={search && {
+            startAdornment: <InputAdornment position="end">
+              <Search className={classes.searchIcon} />
+            </InputAdornment>
+          }}
           {...getInputProps({
             placeholder: placeholderText,
             errorText,
             disabled,
             helperText,
             label,
-            onKeyPress: (e:React.KeyboardEvent<KeyboardEvent>) => {
+            onKeyDown: (e:React.KeyboardEvent<KeyboardEvent>) => {
               if (e.key === 'Enter') {
-                this.onSubmit();
-                e.preventDefault();
+                if (highlightedIndex !== null) { selectHighlightedItem(); }
+                else { this.onSubmit(); }
               }
             },
             onFocus: (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -158,7 +176,7 @@ class EnhancedSelect extends React.Component<CombinedProps, State> {
           })}
         />
         {isOpen &&
-          <Paper className={classes.searchSuggestions}>
+          <Paper className={classes.searchSuggestions} style={{ maxHeight: maxHeight || 192 }}>
             {this.getSearchSuggestions(inputValue).map((suggestion:Item, index:number) => {
               return this.renderSuggestion(
                 suggestion,
@@ -187,7 +205,9 @@ class EnhancedSelect extends React.Component<CombinedProps, State> {
     if ( isHighlighted ) { classes += " enhancedSelect-menu-item-highlighted"; }
     if ( isSelected )    { classes += " enhancedSelect-menu-item-selected"; }
 
-    return (
+    return this.props.renderItems 
+      ? this.props.renderItems(item, index, isHighlighted, itemProps, classes)
+      :
       <div className={classes}
         key={index} 
         {...itemProps}
@@ -195,7 +215,6 @@ class EnhancedSelect extends React.Component<CombinedProps, State> {
       >
         {item.label}
       </div>
-    )
   }
   
   render() {
---
+++ src/features/Help/Panels/SearchItem.tsx
@@ -0,0 +1,81 @@
+import { compose } from 'ramda';
+import * as React from 'react';
+
+import ListItem from '@material-ui/core/ListItem';
+import { StyleRulesCallback, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import OpenInNew from '@material-ui/icons/OpenInNew';
+
+import { Item } from 'src/components/EnhancedSelect';
+import RenderGuard from 'src/components/RenderGuard';
+
+type ClassNames = 'root'
+| 'label'
+| 'source'
+| 'icon'
+| 'row';
+
+const styles: StyleRulesCallback<ClassNames> = (theme: Theme & Linode.Theme) => ({
+  root: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  label: {
+    display: 'inline',
+    color: theme.palette.text.primary,
+    maxWidth: '95%',
+  },
+  icon: {
+    display: 'inline-block',
+    fontSize: '0.8em',
+    position: 'relative',
+    top: 5,
+    marginLeft: theme.spacing.unit / 2,
+  },
+  source: {
+    fontWeight: 700,
+  },
+  row: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+});
+
+interface Props {
+  item: Item;
+  highlighted: boolean;
+}
+
+type CombinedProps = Props & WithStyles<ClassNames>;
+
+const searchItem: React.StatelessComponent<CombinedProps> = (props) => {
+  const getLabel = () => {
+    if (isFinal) { 
+      return item.label ? `Search for "${item.label}"` : 'Search';
+    } else { return item.label}
+  }
+
+  const { classes, item } = props;
+  const source = item.data ? item.data.source : '';
+  const isFinal = source === 'finalLink';
+
+  return (
+    <React.Fragment>
+      <ListItem className={classes.root} component="div">
+        <div className={classes.row}>
+          <div className={classes.label} dangerouslySetInnerHTML={{__html: getLabel()}} />
+          {!isFinal && <OpenInNew className={classes.icon} />}
+        </div>
+        {!isFinal && <Typography variant="caption" className={classes.source}>{source}</Typography>}
+      </ListItem>
+    </React.Fragment>
+  );
+}
+
+const styled = withStyles(styles, { withTheme: true });
+
+export default compose<any, any, any>(
+  styled,
+  RenderGuard
+  )(searchItem);
