47b7a43d8a7b7e9c9c1a40f1896118c85fc4d827
Robert Stires <rstires@linode.com>
2018-08-28 16:08:24 -0400

    Update docs and community posts. (#3758)

--- src/features/Help/Panels/PopularPosts.tsx
+++ src/features/Help/Panels/PopularPosts.tsx
@@ -63,29 +63,29 @@ class PopularPosts extends React.Component<CombinedProps, State> {
     return (
       <React.Fragment>
         <div className={classes.post}>
-          <a target="_blank" href="https://linode.com/docs/platform/stackscripts/">
+          <a target="_blank" href="https://www.linode.com/docs/getting-started/">
             <Typography variant="subheading">
-              Automate Deployment with StackScripts
+              Getting Started with Linode
             </Typography>
           </a>
         </div>
         <div className={classes.post}>
           <a
             target="_blank"
-            href="https://linode.com/docs/platform/disk-images/linode-backup-service/"
+            href="https://www.linode.com/docs/security/securing-your-server/"
           >
             <Typography variant="subheading">
-              Linode Backup Service
+              How to Secure Your Server
             </Typography>
           </a>
         </div>
         <div className={classes.post}>
           <a
             target="_blank"
-            href="https://linode.com/docs/security/authentication/use-public-key-authentication-with-ssh/"
+            href="https://www.linode.com/docs/troubleshooting/troubleshooting/"
           >
             <Typography variant="subheading">
-              Public Key Authentication with SSH
+              Troubleshooting
             </Typography>
           </a>
         </div>
@@ -100,30 +100,30 @@ class PopularPosts extends React.Component<CombinedProps, State> {
         <div className={classes.post}>
           <a
             target="_blank"
-            href="https://linode.com/community/questions/16988/completing-install-of-phpmyadmin-on-centos-7"
+            href="https://www.linode.com/community/questions/323/my-linode-is-unreachable-after-maintenance"
           >
             <Typography variant="subheading">
-              Completing Install of phpMyAdmin on CentOS 7
+            My Linode is unreachable after maintenance
             </Typography>
           </a>
         </div>
         <div className={classes.post}>
           <a
             target="_blank"
-            href="https://linode.com/community/questions/16956/how-do-i-switch-from-a-commercial-ssl-to-lets-encrypt"
+            href="https://www.linode.com/community/questions/232/why-is-my-website-so-slow"
           >
             <Typography variant="subheading">
-              How do I switch from a commercial SSL to Let's Encrypt?
+              Why is my website so slow?
             </Typography>
           </a>
         </div>
         <div className={classes.post}>
           <a
             target="_blank"
-            href="https://linode.com/community/questions/16945/how-can-i-install-angularcli-on-ubuntu-1604-lts"
+            href="https://www.linode.com/community/questions/479/stackscript-guide"
           >
             <Typography variant="subheading">
-              How can I install @angular/cli on ubuntu 16.04 LTS?
+              Stackscript guide?
             </Typography>
           </a>
         </div>
