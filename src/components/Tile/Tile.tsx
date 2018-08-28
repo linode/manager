0740acc7851fc0372c8484b74d67cdb7f298e447
Patrick Thiel <pthiel@linode.com>
2018-08-28 14:53:13 -0400

    Add tile test

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -101,11 +101,11 @@ class Tile extends React.Component<CombinedProps> {
           [classes.clickableTile]: link !== undefined,
         },
         className,
-      )}>
+      )} data-qa-tile>
         {icon &&
-          <span className={classes.icon}>{icon}</span>
+          <span className={classes.icon} data-qa-tile-icon>{icon}</span>
         }
-        <Typography variant="subheading" className={classes.tileTitle}>
+        <Typography variant="subheading" className={classes.tileTitle} data-qa-tile-title>
           <React.Fragment>
             {link
               ?
@@ -116,7 +116,7 @@ class Tile extends React.Component<CombinedProps> {
           </React.Fragment>
         </Typography>
           {description &&
-            <Typography variant="caption" align="center">
+            <Typography variant="caption" align="center" data-qa-tile-desc>
               {description}
             </Typography>
           }
