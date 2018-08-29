552a0d19428b658e12c3e10a12ea599f0eaad108
Jared Kobos <jaredkobos@gmail.com>
2018-08-29 16:36:01 -0400

    Remove toast and stray console.log

--- src/features/linodes/LinodesDetail/LinodeVolumes/LinodeVolumes.tsx
+++ src/features/linodes/LinodesDetail/LinodeVolumes/LinodeVolumes.tsx
@@ -174,10 +174,6 @@ export class LinodeVolumes extends React.Component<CombinedProps, State> {
       .filter(e => !e._initial)
       .subscribe((v) => {
         if (this.mounted) {
-          console.log(v);
-          if (v.action === 'volume_detach' && ['finished','notification'].includes(v.status)) {
-            sendToast('Volume has been detached.')
-          }
           this.getVolumes();
         }
       });
