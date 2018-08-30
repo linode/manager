aee54307fd27498502e91663b2dafbf4f3899d77
Robert Stires <rstires@linode.com>
2018-08-30 14:15:31 -0400

    Add userSSHKeyHoc

---
+++ src/features/linodes/LinodesCreate/TabbedContent/userSSHKeyHoc.ts
@@ -0,0 +1,105 @@
+import { path } from 'ramda';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { UserSSHKeyObject } from 'src/components/AccessPanel';
+import { getUsers } from 'src/services/account';
+import { getSSHKeys } from 'src/services/profile';
+import { getEmailHash } from 'src/utilities/gravatar';
+
+interface Props {
+  username: string;
+  userEmailAddress: string;
+}
+
+interface State {
+  userSSHKeys: UserSSHKeyObject[];
+}
+
+export default (Component: React.ComponentType<any>) => {
+  class WrappedComponent extends React.PureComponent<Props, State> {
+    state = {
+      userSSHKeys: [],
+    }
+
+    mounted: boolean = false;
+
+    componentWillUnmount() {
+      this.mounted = false;
+    }
+
+    componentDidMount() {
+      this.mounted = true;
+      const { username, userEmailAddress } = this.props;
+      if (!username || !userEmailAddress) { return; }
+
+      getSSHKeys()
+        .then((response) => {
+          const keys = response.data
+          if (!this.mounted || !keys || keys.length === 0) { return; }
+
+          this.setState({
+            userSSHKeys: [
+              ...this.state.userSSHKeys,
+              this.createUserObject(username, userEmailAddress, keys.map(k => k.label)),
+            ],
+          })
+        })
+        .catch(console.error);
+
+      getUsers()
+        .then((response) => {
+          const users = response.data;
+          if (!this.mounted || !users || users.length === 0) { return; }
+
+          this.setState({
+            userSSHKeys: [
+              ...this.state.userSSHKeys,
+              ...users.reduce((users, user) => {
+                const keys = user.ssh_keys;
+                if (
+                  !keys || keys.length === 0 ||
+                  /** We don't want the current user added again. */
+                  user.username === this.props.username
+                ) {
+                  return users;
+                }
+
+                return [
+                  ...users,
+                  this.createUserObject(user.username, user.email, keys),
+                ];
+              }, [])
+            ],
+          })
+        })
+        .catch(console.error);
+    }
+
+    render() {
+      return React.createElement(Component, {
+        ...this.props,
+        ...this.state,
+      });
+    }
+
+    toggleSSHUserKeys = (username: string, result: boolean) => this.setState((state) => ({
+      ...state,
+      userSSHKeys: state.userSSHKeys.map((user) => (username === user.username) ? { ...user, selected: result } : user)
+    }));
+
+    createUserObject = (username: string, email: string, keys: string[]) => ({
+      keys,
+      username,
+      gravatarUrl: `https://www.gravatar.com/avatar/${getEmailHash(email)}?s=24`,
+      selected: false,
+      onChange: (_: any, result: boolean) => this.toggleSSHUserKeys(username, result),
+    })
+  }
+
+  return connected<Props>(WrappedComponent);
+}
+
+const connected = connect((state: Linode.AppState) => ({
+  username: path<string>(['data', 'username'], state.resources.profile),
+  userEmailAddress: path<string>(['data', 'email'], state.resources.profile),
+}))
