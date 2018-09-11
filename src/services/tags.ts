9d5aad86b169235db93367b9e3ce1bea87deaafd
Jared <jaredkobos@gmail.com>
2018-09-11 08:56:10 -0400

    Add tags.ts with get and create methods (#3816)
    * Add tags.ts with get and create methods
    
    * Remove params and filter from createTag method

---
+++ src/services/tags.ts
@@ -0,0 +1,29 @@
+import { API_ROOT } from 'src/constants';
+
+import Request, { setData, setMethod, setParams, setURL, setXFilter } from './index';
+
+type Page<T> = Linode.ResourcePage<T>;
+
+export interface Tag {
+  label: string;
+}
+
+export interface TagRequest {
+  label: string;
+  linodes?: number[];
+}
+
+export const getTags = (params?: any, filter?: any) =>
+  Request<Page<Tag>>(
+    setURL(`${API_ROOT}/tags`),
+    setMethod('GET'),
+    setParams(params),
+    setXFilter(filter),
+  ).then(response => response.data);
+
+export const createTag = (data:TagRequest) =>
+  Request<Tag>(
+    setURL(`${API_ROOT}/tags`),
+    setMethod('POST'),
+    setData(data),
+  ).then(response => response.data)
