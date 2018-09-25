64999b211d247832e16103bf8e340720202f69fd
Pat Thiel <patthiel@gmail.com>
2018-09-25 13:03:37 -0400

    M3-1354 - Automate Axe Accessibility Testing (#3862)
    * Automate accessibility testing
    * Sort results by critical
    * Add axe wdio config
    * Document axe testing

---
+++ e2e/utils/accessibility.js
@@ -0,0 +1,14 @@
+const axeSource = require('axe-core').source;
+
+export const axeTest = () => {
+
+    browser.execute(axeSource);
+    
+    const testResults = browser.executeAsync(function(done) {
+        axe.run(function(err, results) {
+            done(results);
+        });
+    });
+
+    return testResults.value.violations;
+}
