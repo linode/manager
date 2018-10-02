c449593a5995938040d204008b0938cc0c1e49a3
Patrick Thiel <pthiel@linode.com>
2018-10-02 14:02:42 -0400

    Remove duplicate data attribute

--- src/components/EnhancedSelect/components/SelectPlaceholder.tsx
+++ src/components/EnhancedSelect/components/SelectPlaceholder.tsx
@@ -20,7 +20,6 @@ type CombinedProps = Props & WithStyles<ClassNames>;
 const SelectPlaceholder: React.StatelessComponent<CombinedProps> = (props) => {
   return (
     <Typography
-      data-qa-multi-select={props.isMulti ? props.selectProps.placeholder : false}
       className={props.classes.root}
       {...props.innerProps}
       data-qa-enhanced-select
