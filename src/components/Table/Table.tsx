f9a58d49d9909e965bf0e39437677e207a1654be
Kayla Wilkins <k.beth.wilkins@gmail.com>
2018-10-05 17:05:06 -0400

    M3-1472 Access Panel mobile style bug (#3920)
    * changing display of empty table cells to display as a cell and not be hidden
    
    * make sure cells get only hidden if table is reponsive

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -14,6 +14,9 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => ({
   },
   responsive: {
     [theme.breakpoints.down('sm')]: {
+      '& .emptyCell': {
+        display: 'none',
+      },
       '& thead': {
         display: 'none',
       },
