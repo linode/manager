4a92ad3caba38e5ca42f4295f1598aa9ba469ba8
John Callahan <johnwcallahan@gmail.com>
2018-10-11 18:03:41 -0400

    add params and filters to appTokens and accessTokens requests

--- src/services/profile/accessTokens.ts
+++ src/services/profile/accessTokens.ts
@@ -1,6 +1,6 @@
 import { API_ROOT } from 'src/constants';
 
-import Request, { setData, setMethod, setURL } from '..';
+import Request, { setData, setMethod, setParams, setURL, setXFilter } from '..';
 import { createPersonalAccessTokenSchema } from './profile.schema';
 
 type Page<T> = Linode.ResourcePage<T>;
@@ -16,11 +16,13 @@ export interface TokenRequest {
  * getPersonalAccessTokens
  *
  * Returns a paginated list of Personal Access Tokens currently active for your User.
- * 
+ *
  */
-export const getPersonalAccessTokens = () =>
+export const getPersonalAccessTokens = (params: any = {}, filters: any = {}) =>
   Request<Page<Token>>(
     setMethod('GET'),
+    setParams(params),
+    setXFilter(filters),
     setURL(`${API_ROOT}/profile/tokens`),
   )
     .then(response => response.data);
@@ -29,9 +31,9 @@ export const getPersonalAccessTokens = () =>
  * getPersonalAccessToken
  *
  * Retrieve a single Personal Access Token.
- * 
+ *
  * @param ticketId { number } the ID of the token to view
- * 
+ *
  */
 export const getPersonalAccessToken = (id: number) =>
   Request<Token>(
@@ -47,7 +49,7 @@ export const getPersonalAccessToken = (id: number) =>
  * The raw token will be returned in the response. You may create a token with at most the scopes of
  * your current token. The created token will be able to access your Account until the given expiry,
  * or until it is revoked.
- * 
+ *
  * @param data { Object } Token request object
  * @param data.scope { string } The scopes to create the token with. These cannot be changed after creation,
  * and may not exceed the scopes of the acting token. If omitted, the new token will have the same
@@ -55,7 +57,7 @@ export const getPersonalAccessToken = (id: number) =>
  * @param data.expiry { string } Datetime string indictating when this token should be valid until.
  * If omitted, the new token will be valid until it is manually revoked.
  * @param data.label { string } String to identify this token. Used for organizational purposes only.
- * 
+ *
  */
 export const createPersonalAccessToken = (data: TokenRequest) =>
   Request<Token>(
@@ -69,12 +71,12 @@ export const createPersonalAccessToken = (data: TokenRequest) =>
  * updatePersonalAccessToken
  *
  * Change the label or expiry date of a Personal Access Token
- * 
+ *
  * @param tokenId { number } the ID of the token to be updated.
  * @param data { Object } JSON object to be sent as the X-Filter header.
  * @param data.label { string } update the Token's label.
  * @param data.expiry { string } Datetime string to update when the token should expire.
- * 
+ *
  */
 export const updatePersonalAccessToken = (tokenId: number, data: Partial<TokenRequest>) =>
   Request<Token>(
@@ -88,9 +90,9 @@ export const updatePersonalAccessToken = (tokenId: number, data: Partial<TokenRe
  * deletePersonalAccessToken
  *
  * Deletes a single Personal Access Token.
- * 
+ *
  * @param tokenId { number } the ID of the token to be deleted.
- * 
+ *
  */
 export const deletePersonalAccessToken = (tokenId: number) =>
   Request<{}>(
