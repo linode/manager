8458554c5cd60e1080b0fa12ace59d4db344c430
Patrick Thiel <pthiel@linode.com>
2018-10-15 10:12:27 -0400

    Only run parallel when manager_user2 is defined

--- e2e/config/wdio.conf.js
+++ e2e/config/wdio.conf.js
@@ -72,7 +72,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 2,
+    maxInstances: process.env.MANAGER_USER_2 ? 2 : 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
