7114a3760aad8a730e04059210140d015e0632cd
Jared Kobos <jaredkobos@gmail.com>
2018-10-17 16:13:01 -0400

    Update/clarify documentation

--- src/utilities/getAll.ts
+++ src/utilities/getAll.ts
@@ -14,7 +14,12 @@ export type GetFunction = (params?: any, filters?: any) => Promise<APIResponsePa
  * getAll
  * 
  * HOF that takes any paginated get function from the services library and returns a Promise
- * that resolves to an array of the passed object.
+ * that resolves to an array of the passed object. The function makes an initial request
+ * using the getter, then uses the response to determine the number of remaining pages.
+ * Subsequent requests are then made and the results combined into a single array. This
+ * procedure is necessary when retrieving all entities whenever the possible number of
+ * entities is greater than 100 (the max number of results the API will return in a single
+ * page).
  * 
  * @param getter { Function } one of the Get functions from the API services library. Accepts
  * pagination or filter parameters.
