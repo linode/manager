a25dcd18d36a473412789274da318345c068d8e8
Jared <jaredkobos@gmail.com>
2018-10-22 15:37:03 -0400

    M3-1589 Hide Hively feedback icons for old tickets (#3982)
    * Only show Hively for tickets less than 7 days old.
    * Use the ticket.updated field (rather than the date of a specific reply)
    to determine this. This matches behavior in the CF manager.

---
+++ src/features/Support/ExpandableTicketPanel.test.tsx
@@ -0,0 +1,49 @@
+import * as moment from 'moment';
+
+import { ExpandableTicketPanel } from './ExpandableTicketPanel';
+
+const classes = {
+  root: '',
+  userWrapper: '',
+  leftIcon: '',
+  userName: '',
+  paper: '',
+  paperOpen: '',
+  avatarCol: '',
+  userCol: '',
+  descCol: '',
+  expCol: '',
+  expButton: '',
+  toggle: '',
+  isCurrentUser: '',
+  formattedText: '',
+  hivelyLink: '',
+  hivelyImage: '',
+  hivelyContainer: ''
+}
+
+const component = new ExpandableTicketPanel({
+  isCurrentUser: false,
+  classes,
+})
+
+const recent = moment().subtract(6, 'days').format();
+const old = moment().subtract(3, 'months').format();
+
+describe("shouldRenderHively function", () => {
+  it("should return true if an improperly formatted date is passed", () => {
+    expect(component.shouldRenderHively(true, 'blah')).toBeTruthy();
+  });
+  it("should return true if the date is now", () => {
+    expect(component.shouldRenderHively(true, moment().format())).toBeTruthy();
+  });
+  it("should return true if the date is within the past 7 days", () => {
+    expect(component.shouldRenderHively(true, recent)).toBeTruthy();
+  });
+  it("should return false for dates older than 7 days", () => {
+    expect(component.shouldRenderHively(true, old)).toBeFalsy();
+  });
+  it("should return false if the fromLinode parameter is false", () => {
+    expect(component.shouldRenderHively(false, recent)).toBeFalsy();
+  });
+});
