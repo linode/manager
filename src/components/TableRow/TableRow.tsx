9538ec89429247244bec0b4eed0750e8cee422b1
Alban Bailly <abailly@linode.com>
2018-10-25 15:24:11 -0400

    change padding on table cells site-wide

--- src/components/TableRow/TableRow.tsx
+++ src/components/TableRow/TableRow.tsx
@@ -4,6 +4,7 @@ import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 import { StyleRulesCallback, withStyles, WithStyles } from '@material-ui/core/styles';
 import TableRow, { TableRowProps } from '@material-ui/core/TableRow';
+import { relative } from 'path';
 
 type ClassNames = 'root';
 
@@ -16,14 +17,11 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => ({
         display: 'table-cell',
         width: '0.01%',
         height: '100%',
-        borderLeftWidth: 5,
-        borderLeftStyle: 'solid',
-        borderBottom: `1px solid ${theme.palette.divider}`,
-        transition: theme.transitions.create(['border-color']),
+        backgroundColor: 'transparent',
+        borderBottom: `2px solid ${theme.palette.divider}`,
+        transition: theme.transitions.create(['background-color']),
+        paddingLeft: 5,
       },
-    },
-    '& > td:first-child, & > th:first-child': {
-      paddingLeft: 13,
     },
     '& .secondaryLink': {
       transition: theme.transitions.create('color'),
