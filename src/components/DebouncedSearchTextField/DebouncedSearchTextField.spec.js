4692b7b64f32033388452a4a0fe7fd728e7f1ee4
Patrick Thiel <pthiel@linode.com>
2018-10-29 17:49:37 -0400

    DRY up test spec

--- src/components/DebouncedSearchTextField/DebouncedSearchTextField.spec.js
+++ src/components/DebouncedSearchTextField/DebouncedSearchTextField.spec.js
@@ -5,9 +5,13 @@ describe('Debounced Search Suite', () => {
     const component = 'Debounced Search';
     const childStories = ['Select Field', 'Text Field'];
 
-
     describe('Select Field Suite', () => {
         let enhancedSelect, selectedOptionMsg, selectOptions;
+
+        const currentResultSelector = '[data-qa-selected-result]';
+        const optionSelector = '[data-qa-option]';
+        const emptyResult = 'You selected:';
+        const validQuery = 'keyboard';
 
         beforeAll(() => {
             navigateToStory(component, childStories[0]);
@@ -22,8 +26,7 @@ describe('Debounced Search Suite', () => {
         });
 
         it('should not have options selected on pageload', () => {
-            const emptyResult = 'You selected:';
-            selectedOptionMsg = $('[data-qa-selected-result]');
+            selectedOptionMsg = $(currentResultSelector);
             
             expect(selectedOptionMsg.getText()).toBe(emptyResult);
         });
@@ -36,16 +39,13 @@ describe('Debounced Search Suite', () => {
         });
 
         it('should display options on a valid search query', () => {
-            const validQuery = 'keyboard';
-
+            // Add the value twice to fix test flakiness when running the entire functional test suite
             enhancedSelect.$('..').$('input').setValue(validQuery);
-            
-            // Adding the value twice to fix test flakiness when running the entire functional test suite
             enhancedSelect.$('..').$('input').setValue(validQuery);
 
-            browser.waitForVisible('[data-qa-option]', constants.wait.normal);
+            browser.waitForVisible(optionSelector, constants.wait.normal);
 
-            selectOptions = $$('[data-qa-option]');
+            selectOptions = $$(optionSelector);
             expect(selectOptions.length).toBe(1);
         });
 
@@ -53,17 +53,29 @@ describe('Debounced Search Suite', () => {
             selectOptions[0].click();
 
             browser.waitForVisible(selectOptions[0].selector, constants.wait.normal, true);
-            selectedOptionMsg = $('[data-qa-selected-result]');
+            selectedOptionMsg = $(currentResultSelector);
             expect(selectedOptionMsg.getText()).toBe('You selected: keyboards');
         });
 
         xit('should clear the result on click clear', () => {
-            
+            validQuery.split('').forEach(i => {
+                enhancedSelect.$('..').$('input').addValue('\uE003');
+            });
+
+            browser.waitUntil(() => {
+                return $(currentResultSelector).getText() === emptyResult;
+            }, constants.wait.normal);
         });
     });
 
     describe('Text Field Suite', () => {
-        let searchTextfield, displayedListItems, validQuery, initialOptions = [];
+        let searchTextfield,
+            displayedListItems,
+            initialOptions = [];
+
+        const debouncedSearchSelector = '[data-qa-debounced-search]';
+        const listItemSelector = '[data-qa-list-item]';
+        const validQuery = 'apples';
 
         beforeAll(() => {
             navigateToStory(component, childStories[1]);
@@ -71,13 +83,13 @@ describe('Debounced Search Suite', () => {
 
         it('should display the debounced search text field', () => {
             const placeholderMsg = 'Search for something';
-            searchTextfield = $('[data-qa-debounced-search]').$('input');
+            searchTextfield = $(debouncedSearchSelector).$('input');
 
             expect(searchTextfield.getAttribute('placeholder')).toBe(placeholderMsg);
         });
 
         it('should display unfiltered list of options', () => {
-            displayedListItems = $$('[data-qa-list-item]');
+            displayedListItems = $$(listItemSelector);
             displayedListItems.forEach(i => {
                 expect(i.isVisible()).toBe(true);
                 expect(i.getText()).toMatch(/\w/ig);
@@ -94,12 +106,10 @@ describe('Debounced Search Suite', () => {
         });
 
         it('should display a single option on query of a single matching list item', () => {
-            validQuery = 'apples';
-
             searchTextfield.setValue(validQuery);
 
             browser.waitUntil(() => {
-                displayedListItems = $$('[data-qa-list-item]');
+                displayedListItems = $$(listItemSelector);
                 return displayedListItems.length === 1;
             }, constants.wait.normal);
 
@@ -108,15 +118,15 @@ describe('Debounced Search Suite', () => {
 
         it('should display all list options on clear', () => {
             validQuery.split('').forEach(i => {
-                $('[data-qa-debounced-search]').$('input').addValue('\uE003');
+                $(debouncedSearchSelector).$('input').addValue('\uE003');
             });
 
             browser.waitUntil(() => {
-                displayedListItems = $$('[data-qa-list-item]');
+                displayedListItems = $$(listItemSelector);
                 return displayedListItems.length === initialOptions.length;
             }, constants.wait.normal);
 
-            const currentOptions = $$('[data-qa-list-item]').map(i => i.getText());
+            const currentOptions = $$(listItemSelector).map(i => i.getText());
 
             expect(currentOptions).toEqual(initialOptions);
         });
