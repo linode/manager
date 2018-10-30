cf1156af6201650f1c7c19001cc1db2d70972169
Jared <jaredkobos@gmail.com>
2018-10-30 08:39:30 -0400

    M3-1400 Refactor services/volumes (#3997)
    * Add comments and schema
    
    * Review feedback
    
    * Add (response) => response.data for consistency in volumes.ts
    
    * Add better messaging for schema validation
    
    * Add min and max to size in volumes.schema.ts

---
+++ src/services/volumes/volumes.schema.ts
@@ -0,0 +1,22 @@
+import { number, object, string } from 'yup';
+
+export const CreateVolumeSchema = object({
+  region: string()
+    .when('linode_id', {
+      is: (id) => Boolean(id),
+      then: string().required("Must provide a region or a Linode ID."),
+    }),
+  linode_id: number(),
+  size: number()
+    .typeError("Size must be a number.")
+    .integer()
+    .min(10, "Size must be between 10 and 10240.")
+    .max(10240, "Size must be between 10 and 10240."),
+  label: string()
+    .required("Label is required.")
+    .ensure()
+    .trim()
+    .min(1, "Label must be between 1 and 32 characters.")
+    .max(32, "Label must be 32 characters or less."),
+  config_id: number().typeError("Config ID must be a number.")
+});
