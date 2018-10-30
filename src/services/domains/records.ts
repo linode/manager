131bac208fe1816ea4797a2e07d7060df8e6301c
John Callahan <johnwcallahan@gmail.com>
2018-10-30 14:17:55 -0400

    M3-1394 clean services/domains (#3994)
    * add documentation to domains and record methods
    
    * resolve data from request methods, update components accordingly
    
    * remove unnecessary typing
    
    * clean whitespace and documentation
    
    * clean
    
    * add validation to api methods
    
    * change arg name from pagination to params
    
    * review feedback
    
    * make importZone args required, in service method AND component
    
    * review feedback
    
    * remove whitespace

--- src/services/domains/records.ts
+++ src/services/domains/records.ts
@@ -4,37 +4,83 @@ import Request,
 {
   setData,
   setMethod,
+  setParams,
   setURL,
 } from '../index';
+
+import { createRecordSchema, updateRecordSchema } from './records.schema';
 
 type Page<T> = Linode.ResourcePage<T>;
 type Record = Linode.Record;
 
-export const getDomainRecords = (domainId: number) =>
-Request<Page<Record>>(
-  setURL(`${API_ROOT}/domains/${domainId}/records`),
-  setMethod('GET'),
-);
+/**
+ * Returns a paginated list of Records configured on a Domain in Linode's DNS Manager.
+ *
+ * @param domainId { number } The ID of the Domain we are accessing Records for.
+ * @param params { object }
+ */
+export const getDomainRecords = (domainId: number, params?: any) =>
+  Request<Page<Record>>(
+    setURL(`${API_ROOT}/domains/${domainId}/records`),
+    setParams(params),
+    setMethod('GET'),
+  )
+    .then(response => response.data);
 
+/**
+ * View a single Record on this Domain.
+ *
+ * @param domainId { number } The ID of the Domain whose Record you are accessing.
+ * @param recordId { number } The ID of the Record you are accessing.
+ */
+export const getDomainRecord = (domainId: number, recordId: number) =>
+  Request<Record>(
+    setURL(`${API_ROOT}/domains/${domainId}/records/${recordId}`),
+    setMethod('GET'),
+  )
+    .then(response => response.data);
+
+/**
+ * Adds a new Domain Record to the zonefile this Domain represents.
+ *
+ * @param domainId { number } The ID of the Domain we are accessing Records for.
+ * @param data { object } Options for type, name, etc.
+ */
 export const createDomainRecord = (domainId: number, data: Partial<Record>) =>
-Request<Record>(
-  setURL(`${API_ROOT}/domains/${domainId}/records`),
-  setMethod('POST'),
-  setData(data),
-).then(response => response.data);
+  Request<Record>(
+    setURL(`${API_ROOT}/domains/${domainId}/records`),
+    setMethod('POST'),
+    setData(data, createRecordSchema),
+  )
+    .then(response => response.data);
 
+/**
+ * Updates a single Record on this Domain.
+ *
+ * @param domainId { number } The ID of the Domain we are accessing Records for.
+ * @param recordId { number } The ID of the Record you are accessing.
+ * @param data { object } Options for type, name, etc.
+ */
 export const updateDomainRecord = (
-domainId: number,
-recordId: number,
-data: Partial<Record>,
-) => Request<Record>(
-setURL(`${API_ROOT}/domains/${domainId}/records/${recordId}`),
-setMethod('PUT'),
-setData(data),
-);
+  domainId: number,
+  recordId: number,
+  data: Partial<Record>,
+  ) => Request<Record>(
+    setURL(`${API_ROOT}/domains/${domainId}/records/${recordId}`),
+    setMethod('PUT'),
+    setData(data, updateRecordSchema),
+  )
+    .then(response => response.data);
 
+/**
+ * Deletes a Record on this Domain..
+ *
+ * @param domainId { number } The ID of the Domain whose Record you are deleting.
+ * @param recordId { number } The ID of the Record you are deleting.
+ */
 export const deleteDomainRecord = (domainID: number, recordId: number) =>
-Request<{}>(
-  setURL(`${API_ROOT}/domains/${domainID}/records/${recordId}`),
-  setMethod('DELETE'),
-);
+  Request<{}>(
+    setURL(`${API_ROOT}/domains/${domainID}/records/${recordId}`),
+    setMethod('DELETE'),
+  )
+    .then(response => response.data);
