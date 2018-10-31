7ebb0bde93511973278b22a2c95d189ca44182c7
John Callahan <johnwcallahan@gmail.com>
2018-10-31 08:54:37 -0400

    add tests

--- src/features/linodes/LinodesDetail/LinodeBackup/RestoreToLinodeDrawer.tsx
+++ src/features/linodes/LinodesDetail/LinodeBackup/RestoreToLinodeDrawer.tsx
@@ -43,7 +43,7 @@ interface State {
 
 type CombinedProps = Props & WithStyles<ClassNames>;
 
-class RestoreToLinodeDrawer extends React.Component<CombinedProps, State> {
+export class RestoreToLinodeDrawer extends React.Component<CombinedProps, State> {
   defaultState = {
     linodes: [],
     overwrite: false,
