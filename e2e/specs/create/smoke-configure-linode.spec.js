a8f59895a6f75fc5869634f90bc4302deae8b5d1
Nicholas Wisnewski <nwisnewski@Nicholass-MacBook-Pro.local>
2018-11-06 15:21:18 -0500

    Minor update to create-from-stackscript-with-tags and smoke-configure-linode

--- e2e/specs/create/smoke-configure-linode.spec.js
+++ e2e/specs/create/smoke-configure-linode.spec.js
@@ -5,9 +5,8 @@ import CheckoutSummary from '../../pageobjects/checkout-summary';
 
 describe('Create Linode - Configure Linode Suite', () => {
     beforeAll(() => {
-        Create.menuButton.click();
         Create.linode();
-    });
+    }); 
 
     it('should display configure elements', () => {
         ConfigureLinode.baseDisplay();
