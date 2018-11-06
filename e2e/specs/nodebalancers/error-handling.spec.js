f287407538e9885b212b5c6071d00027b68a0fbe
Patrick Thiel <pthiel@linode.com>
2018-11-06 16:45:33 -0500

    Fix service error assertion

--- e2e/specs/nodebalancers/error-handling.spec.js
+++ e2e/specs/nodebalancers/error-handling.spec.js
@@ -51,9 +51,9 @@ describe('NodeBalancer - Negative Tests Suite', () => {
     });
 
     it('should fail to create a configuration with an invalid. ip', () => {
-        const invalidIp = { privateIp:'192.168.1.1'};
+        const invalidIp = { privateIp:'192.168.1.1' };
         const invalidConfig = merge(linode, invalidIp);
-        const serviceError = 'Must be a valid IPv4 address.';
+        const serviceError = 'This address is not allowed.';
 
         NodeBalancers.configure(invalidConfig,  {
             label: `NB-${new Date().getTime()}`,
