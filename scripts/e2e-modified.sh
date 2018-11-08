e12ec1a90b6a60fb4295edc7846d394ab0844acd
Nicholas Wisnewski <nwisnewski@Nicholass-MacBook-Pro.local>
2018-11-08 16:34:59 -0500

    Added test for M3-1652, and added a script to correct the assumption in e2e:modified that the origin repo is saved as upstream in remotes

---
+++ scripts/e2e-modified.sh
@@ -0,0 +1,4 @@
+GET_ORIGIN=$(git remote -v | grep git@github.com:linode/manager.git)
+set -- $GET_ORIGIN
+ORIGIN=$1
+yarn e2e --spec=$(git diff --name-only $ORIGIN/develop | egrep 'e2e\/specs\/.*\/*spec.js' | tr '\n' ',' | sed 's/.$//')
