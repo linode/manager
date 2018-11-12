d99318e958fedbc09dff2a52d0b7ee364633cbf6
Russ Zhurba <rzhurba@linode.com>
2018-11-12 13:01:36 -0500

    M3-1435: add tagsPanel story

---
+++ src/components/TagsPanel/TagsPanel.stories.tsx
@@ -0,0 +1,73 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import  axios from 'axios';
+import  MockAdapter from 'axios-mock-adapter';
+
+import ThemeDecorator from '../../utilities/storybookDecorators';
+import TagsPanel from './TagsPanel';
+
+const mock = new MockAdapter(axios);
+
+const API_REQUEST = 'https://api.linode.com/v4/tags';
+
+interface Props {
+  tags: string[];
+}
+
+interface State {
+  tags: string[];
+
+}
+
+class TagsPanelDemo extends React.Component<Props, {}> {
+
+  state: State = {
+    tags: []
+  };
+
+
+  updateTags = (tags: string[]) => {
+    return new Promise<void>(resolve => {
+      setTimeout(() => {
+        this.setState({
+          tags
+        });
+        resolve();
+      }, 500);
+    });
+  }
+
+  componentDidMount() {
+    const { tags } = this.props;
+    this.setState({
+      tags
+    });
+  }
+  
+  render() {
+    const { tags } = this.state;
+
+    return (
+      <TagsPanel
+      tags={tags}
+      updateTags={this.updateTags}
+    />
+      );
+    }
+  }
+  
+storiesOf('Tags Panel', module)
+  .addDecorator(ThemeDecorator)
+  .addDecorator(story => {
+    mock.onGet(API_REQUEST).reply(200, {
+      data: ['tag1', 'tag2', 'tag3', 'tag4'].map(tag => ({label: tag}))
+    });
+    return <div>{story()}</div>;
+  })
+  .add('Tags panel with tags', () => {
+    return <TagsPanelDemo tags={['tagOne', 'tagTwo', 'someStrangeLongTagWithNumber123']} />
+  })
+  .add('Tags panel without tags', () => {
+    return <TagsPanelDemo tags={[]} />
+  })
