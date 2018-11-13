71d7e0b111843a8c80c83f67886f4abd97807674
Patrick Thiel <pthiel@linode.com>
2018-11-13 17:50:08 -0500

    M3-1829 - set max-parents to 1, allow non-standard ticket format

--- scripts/jira-changelog.sh
+++ scripts/jira-changelog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CHANGES_WITH_TIX=$(git log --color=always --oneline $1...$2 | egrep -i '(M3-[0-9]*)')
-CHANGES_WITHOUT_TIX=$(git log --color=always --oneline $1...$2 | egrep -iv '(M3-[0-9]*)')
-JQL_QUERY=$(echo "key in ($(git log --color=always --oneline $1...$2 | egrep -oi --color=always '(M3-[0-9]*)' | tr '\n' ',' | sed 's/.$//'))" )
+CHANGES_WITH_TIX=$(git log --color=always --no-merges --oneline $1...HEAD | egrep -i '(M3(-|\s)[0-9]*)')
+CHANGES_WITHOUT_TIX=$(git log --color=always --no-merges --oneline $1...HEAD | egrep -iv '(M3(-|\s)[0-9]*)')
+JQL_QUERY=$(echo "key in ($(git log --color=always --no-merges --oneline $1...HEAD | egrep -oi --color=always '(M3(-|\s)[0-9]*)' | tr '\n' ',' | sed 's/.$//' | tr ' ' '-'))" )
 
 echo -e "Tracked Changes in $1:\n"
 echo "$CHANGES_WITH_TIX"
