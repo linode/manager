a74d42cd6c7ae1b62b9d7301e48b770ed692f6d6
Kayla Wilkins <kwilkins@linode.com>
2018-11-14 14:57:04 -0500

    M3-1539 applying flex-start to support ticket header wrapper

--- src/features/Support/SupportTicketDetail/SupportTicketDetail.tsx
+++ src/features/Support/SupportTicketDetail/SupportTicketDetail.tsx
@@ -55,7 +55,7 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => ({
   },
   titleWrapper: {
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     marginTop: '8px',
     marginBottom: '8px'
   },
