c7fab0750396dfe2bf826d3963fb3010c47d012a
Patrick Thiel <pthiel@linode.com>
2018-11-14 17:13:53 -0500

    Revert bad commit

--- src/components/AddNewLink/AddNewLink.tsx
+++ src/components/AddNewLink/AddNewLink.tsx
@@ -25,7 +25,6 @@ const AddNewLink: React.StatelessComponent<CombinedProps> = (props) => {
       disabled={disabled}
       left={left}
       className={className}
-      data-test-foo
     >
       {display || label}
     </IconTextLink>
