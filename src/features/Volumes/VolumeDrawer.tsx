d2f71e051b61d7e1a9990634f3eb5c9c6179d01f
Nicholas Wisnewski <nwisnewski@Nicholass-MacBook-Pro.local>
2018-11-15 12:15:43 -0500

    Updated volume size helper text in volume creation drawer

--- src/features/Volumes/VolumeDrawer.tsx
+++ src/features/Volumes/VolumeDrawer.tsx
@@ -588,7 +588,7 @@ class VolumeDrawer extends React.Component<CombinedProps, State> {
           error={Boolean(sizeError)}
           errorText={sizeError}
           disabled={mode === modes.CLONING || mode === modes.EDITING}
-          helperText={`A single volume can range from 10 GiB to 10,240 GiB in size.`}
+          helperText={`A single volume can range from 10 GiB to 1,240 GiB in size.`}
           InputProps={{
             endAdornment:
               <InputAdornment position="end">
