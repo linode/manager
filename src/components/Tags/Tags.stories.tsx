571c066d430316e1e8323d0643ff6cf3413c2fa8
Russ Zhurba <rzhurba@linode.com>
2018-11-15 14:29:11 -0500

    M3-1434: move tags list story to Tags section in storybook

--- src/components/Tags/Tags.stories.tsx
+++ src/components/Tags/Tags.stories.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import ThemeDecorator from '../../utilities/storybookDecorators';
 import Tags from './Tags';
   
-storiesOf('Tags List', module)
+storiesOf('Tags', module)
   .addDecorator(ThemeDecorator)
   .add('Tags list without show more', () => {
     return <Tags tags={['tagOne', 'tagTwo', 'someStrangeLongTagWithNumber123']} />
