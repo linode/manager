5c681c304f26e50b90c21e9be4de9743d411d6b1
Robert Stires <rstires@linode.com>
2018-11-16 08:12:55 -0500

    Create abstractions for @material-ui/core components.

---
+++ src/components/core/TextField.ts
@@ -0,0 +1,6 @@
+import TextField, { TextFieldProps as _TextFieldProps } from '@material-ui/core/TextField';
+
+/* tslint:disable-next-line:no-empty-interface */
+export type TextFieldProps = _TextFieldProps & {}
+
+export default TextField;
