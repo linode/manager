94031ffeacf72dc5f4949f56643b40c17fec6614
Jared Kobos <jaredkobos@gmail.com>
2018-11-20 10:14:08 -0500

    Refactor attached file item to separate component

--- src/features/Support/AttachFileForm.tsx
+++ src/features/Support/AttachFileForm.tsx
@@ -1,22 +1,16 @@
+import { remove } from 'ramda';
 import * as React from 'react';
 
-import InputAdornment from 'src/components/core/InputAdornment';
 import { StyleRulesCallback, withStyles, WithStyles } from 'src/components/core/styles';
 
 import AttachFile from '@material-ui/icons/AttachFile';
-import Close from '@material-ui/icons/Close';
-import CloudUpload from '@material-ui/icons/CloudUpload';
 
 import Button from 'src/components/Button';
-import Grid from 'src/components/Grid';
-import LinearProgress from 'src/components/LinearProgress';
-import TextField from 'src/components/TextField';
+
+import AttachFileListItem from './AttachFileListItem';
 
 type ClassNames = 'root'
-| 'attachFileButton'
-| 'attachmentsContainer'
-| 'attachmentField'
-| 'uploadProgress';
+| 'attachFileButton';
 
 const styles: StyleRulesCallback<ClassNames> = (theme) => ({
   root: {},
@@ -25,28 +19,6 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => ({
     paddingRight: 20,
     marginTop: theme.spacing.unit,
     marginBottom: theme.spacing.unit * 2,
-  },
-  attachmentsContainer: {
-    maxWidth: 800,
-  },
-  attachmentField: {
-    marginTop: 0,
-    width: 415,
-    [theme.breakpoints.down('xs')]: {
-      width: 165,
-    },
-    '& > div ': {
-      backgroundColor: theme.bg.main,
-      border: 0,
-    },
-    '& svg': {
-      color: theme.palette.text.primary,
-      width: 24,
-      fontSize: 22,
-    },
-  },
-  uploadProgress: {
-    maxWidth: 415,
   },
 });
 
@@ -79,17 +51,14 @@ export class AttachFileForm extends React.Component<CombinedProps, {}> {
     }
   }
 
-  removeFile = (e: React.MouseEvent<HTMLButtonElement | HTMLDivElement>) => {
-    if (!e.target) { return; }
-    const aidx = e.currentTarget.getAttribute('data-file-idx');
-    if (!aidx) { return; }
-    const idx = +aidx;
-    const newFiles = this.props.files.filter((_, i) => i !== idx)
+  removeFile = (fileIdx: number) => {
+    const { files, updateFiles } = this.props;
+    const newFiles = remove(fileIdx, 1, files);
     if (this.inputRef.current) {
       this.inputRef.current.value = '';
     }
     // Send the updated file list to the parent component's state
-    this.props.updateFiles(newFiles);
+    updateFiles(newFiles);
   }
 
   selectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -125,48 +94,13 @@ export class AttachFileForm extends React.Component<CombinedProps, {}> {
           file.uploaded
             ? null /* this file has already been uploaded so don't show it */
             : (
-              <React.Fragment key={idx}>
-                <Grid container className={classes.attachmentsContainer}>
-                  <Grid item>
-                    <TextField
-                      className={classes.attachmentField}
-                      value={file.name}
-                      errorText={file.errors && file.errors.length && file.errors[0].reason}
-                      InputProps={{
-                        startAdornment:
-                        <InputAdornment position="end">
-                          <CloudUpload />
-                        </InputAdornment>,
-                        endAdornment:
-                          inlineDisplay &&
-                            <InputAdornment
-                              onClick={this.removeFile}
-                              position="end"
-                              data-file-idx={idx}
-                              data-qa-inline-delete
-                            >
-                              <Close />
-                            </InputAdornment>
-                      }}
-                    />
-                  </Grid>
-                  {!inlineDisplay &&
-                    <Grid item>
-                      <Button
-                        type="remove"
-                        data-file-idx={idx}
-                        data-qa-delete-button
-                        onClick={this.removeFile}
-                      />
-                    </Grid>
-                  }
-                  {file.uploading &&
-                    <Grid item xs={12}>
-                      <LinearProgress className={classes.uploadProgress} variant="indeterminate"/>
-                    </Grid>
-                  }
-                </Grid>
-              </React.Fragment>
+              <AttachFileListItem
+                key={idx}
+                inlineDisplay={Boolean(inlineDisplay)}
+                file={file}
+                fileIdx={idx}
+                removeFile={this.removeFile}
+              />
             )
         ))}
       </React.Fragment>
