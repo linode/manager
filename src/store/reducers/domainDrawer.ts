f96dae3502e58d00557f9445bcb9a77f7f9eae92
John Callahan <johnwcallahan@gmail.com>
2018-11-21 15:48:40 -0500

    M3-1769 Dismiss domain create drawer (#4119)
    * close domain drawer upon successful creation
    
    * redux-connect create domain drawer
    
    * domain drawer as global component with redux
    
    * fix e2e test, clean up
    
    * review feedback
    
    * more review feedback
    
    * fix typing error and clean up

---
+++ src/store/reducers/domainDrawer.ts
@@ -0,0 +1,78 @@
+import { Action } from 'redux';
+
+// ACTIONS
+export const OPEN = '@manager/domains/OPEN'
+export const CLOSE = '@manager/domains/CLOSE'
+export const CREATING = '@manager/domains/CREATING';
+export const CLONING = '@manager/domains/CLONING';
+export const RESET = '@manager/domains/RESET';
+
+type State = DomainDrawerState;
+
+interface Creating extends Action {
+  type: typeof CREATING;
+}
+interface Cloning extends Action {
+  type: typeof CLONING;
+  cloneId: number;
+  domain: string;
+}
+
+interface Close extends Action {
+  type: typeof CLOSE;
+}
+
+interface Reset extends Action {
+  type: typeof RESET;
+}
+
+type ActionCreator = (...args: any[]) => Action;
+
+// ACTION CREATORS
+export const openForCreating: ActionCreator = (): Creating => ({ type: CREATING });
+export const openForCloning: ActionCreator = (
+  domain: string,
+  cloneId: number
+  ): Cloning => ({
+    type: CLONING,
+    domain,
+    cloneId
+  });
+export const closeDrawer: ActionCreator = (): Close => ({ type: CLOSE });
+export const resetDrawer: ActionCreator = (): Reset => ({ type: RESET });
+
+// DEFAULT STATE
+export const defaultState: State = {
+  open: false,
+  mode: CREATING
+};
+
+type ActionTypes =
+  Creating
+| Cloning
+| Close
+| Reset
+
+export default (state: State = defaultState, action: ActionTypes) => {
+  switch (action.type) {
+    case CREATING:
+      return { mode: CREATING, open: true }
+
+    case CLONING:
+      return {
+        open: true,
+        mode: CLONING,
+        domain: action.domain,
+        cloneId: action.cloneId
+      }
+
+    case CLOSE:
+      return { ...state, open: false, };
+
+    case RESET:
+      return defaultState;
+
+    default:
+      return state;
+  }
+};
