de276156a5f5050aace47f9ac761363399cb0807
Jared Kobos <jaredkobos@gmail.com>
2018-11-26 13:20:07 -0500

    Let RS manage selected state internally

--- src/features/TopMenu/SearchBar/SearchBar.tsx
+++ src/features/TopMenu/SearchBar/SearchBar.tsx
@@ -203,21 +203,6 @@ class SearchBar extends React.Component<CombinedProps, State> {
     );
   }
 
-  linodeDescription(
-    typeLabel: string,
-    memory: number,
-    disk: number,
-    vcpus: number,
-    imageId: string,
-  ) {
-    const { images } = this.state;
-    const image = (images && images.find((img:Linode.Image) => img.id === imageId))
-      || { label: 'Unknown Image' };
-    const imageDesc = image.label;
-    const typeDesc = typeLabelLong(typeLabel, memory, disk, vcpus);
-    return `${imageDesc}, ${typeDesc}`;
-  }
-
   updateData = () => {
     getAllEntities(this.setEntitiesToState);
   }
@@ -363,7 +348,7 @@ class SearchBar extends React.Component<CombinedProps, State> {
             isLoading={resultsLoading}
             isClearable={false}
             isMulti={false}
-            value={null}
+            controlShouldRenderValue={false}
           />
           <IconButton
             color="inherit"
