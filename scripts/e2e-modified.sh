cb40227c392841ecd22deff8eaa159e0db8a1b1e
Jared Kobos <jaredkobos@gmail.com>
2018-11-27 13:50:17 -0500

    Why does this work?

--- scripts/e2e-modified.sh
+++ scripts/e2e-modified.sh
@@ -2,4 +2,4 @@ GET_ORIGIN=$(git remote -v | grep git@github.com:linode/manager.git)
 set -- $GET_ORIGIN
 ORIGIN=$1
 git fetch $ORIGIN
-yarn e2e --spec=$(git diff --name-only $ORIGIN/develop | egrep 'e2e/specs/.*\/*spec.js' | tr '\n' ',' | sed 's/.$//')
+yarn e2e --spec=$(git diff --name-only $ORIGIN develop | egrep 'e2e/specs/.*\/*spec.js' | tr '\n' ',' | sed 's/.$//')
