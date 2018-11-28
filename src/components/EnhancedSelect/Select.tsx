3123bd24a21aca9c378de02e1899ad831dbe6f42
Kayla Wilkins <k.beth.wilkins@gmail.com>
2018-11-28 17:22:03 -0500

    M3-1869 Tags going off screen in search bar (#4151)
    * M3-1869 adjusting search tag container to wrap, have padding and for suggestion labels to wrap/break if they extend the container
    
    * M3-1869 adding breakpoint for medium

--- src/components/EnhancedSelect/Select.tsx
+++ src/components/EnhancedSelect/Select.tsx
@@ -125,14 +125,16 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => ({
   },
   suggestionRoot: {
     cursor: 'pointer',
-    display: 'flex',
     width: 'calc(100% + 2px)',
     alignItems: 'space-between',
     justifyContent: 'space-between',
     borderBottom: `1px solid ${theme.palette.divider}`,
+    [theme.breakpoints.up('md')]: {
+     display: 'flex',
+    },
     '&:last-child': {
       borderBottom: 0,
-    }
+    },
   },
   highlight: {
     color: theme.palette.primary.main,
@@ -155,6 +157,7 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => ({
   suggestionTitle: {
     fontSize: '1rem',
     color: theme.palette.text.primary,
+    wordBreak: 'break-all',
   },
   suggestionDescription: {
     color: theme.color.headline,
@@ -168,7 +171,9 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => ({
   },
   tagContainer: {
     display: 'flex',
-    justifyContent: 'center',
+    flexWrap: 'wrap',
+    paddingRight: 8,
+    justifyContent: 'flex-end',
     alignItems: 'center',
     '& > div': {
       margin: '2px',
