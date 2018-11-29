79e2cc1ffddf276c94b8158d9cd10a040a5c8273
Kayla Wilkins <k.beth.wilkins@gmail.com>
2018-11-29 11:30:55 -0500

    M3-1927 updating caption variants to body text variant (#4157)
    * M3-1927 update text variants from caption to body
    
    * M3-1927 updating text with individually specced styles

--- src/components/Notice/Notice.tsx
+++ src/components/Notice/Notice.tsx
@@ -56,6 +56,7 @@ const styles: StyleRulesCallback = (theme) => {
     noticeText: {
       color: theme.palette.text.primary,
       fontFamily: 'LatoWebBold',
+      fontSize: '1rem',
       lineHeight: 1.2,
     },
     error: {
