f1f573e1e70383d861a8343980be2c088e9bd73c
Nicholas Wisnewski <nwisnewski@Nicholass-MacBook-Pro.local>
2018-12-06 18:01:18 -0500

    Compose for running automation against environments using linodemanager-e2e-v2

--- e2e/config/wdio.conf.js
+++ e2e/config/wdio.conf.js
@@ -125,7 +125,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: process.env.DOCKER ? 'https://manager-local:3000' : process.env.REACT_APP_APP_ROOT,
+    baseUrl: process.env.REACT_APP_APP_ROOT,
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: process.env.DOCKER || process.env.BROWSERSTACK_USERNAME ? 30000 : 10000,
