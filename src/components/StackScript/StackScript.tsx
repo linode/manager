93575a59b9ac3122bc1eda3ca93de67a58894e5c
rulikrulit <rulitrulik@gmail.com>
2018-12-06 15:11:58 -0500

    M3-880, M3-1703: add StackScript detail page with StackScript component (#4183)
    * M3-880, M3-1703: add StackScript detail page with StackScript component
    
    * M3-1703: implement Deploy StackScript button
    
    * updating code text colors, updating to new variants, fixing typo
    
    * updating heading to h2 so we don't have multi h1s
    
    * M3-880: fix syntax, add images list
    
    * M3-880: use extarnal link component for an external link
    
    * M3-880: add images failure fallback
    
    * updating test with new classname
    
    * M3-880: update to use getAll method for images list

---
+++ src/components/StackScript/StackScript.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { StyleRulesCallback, withStyles, WithStyles } from 'src/components/core/styles';
+import Typography from 'src/components/core/Typography';
+import DateTimeDisplay from 'src/components/DateTimeDisplay';
+import ExternalLink from 'src/components/ExternalLink';
+import ScriptCode from 'src/components/ScriptCode';
+import { getImages } from 'src/services/images';
+import { getAll } from 'src/utilities/getAll';
+
+const getAllImages = getAll(getImages);
+
+type CSSClasses = 'root' | 'deployments' | 'author' | 'description' | 'scriptHeading' | 'descriptionText';
+
+const styles: StyleRulesCallback<CSSClasses> = (theme) => {
+  return ({
+    root: {
+      backgroundColor: theme.color.white,
+      padding: theme.spacing.unit * 4
+    },
+    deployments: {
+      color: theme.color.grey1
+    },
+    author: {
+      marginTop: theme.spacing.unit * 2,
+      marginBottom: theme.spacing.unit * 2,
+    },
+    description: {
+      marginTop: theme.spacing.unit * 4,
+      marginBottom: theme.spacing.unit * 4,
+      paddingTop: theme.spacing.unit * 4,
+      paddingBottom: theme.spacing.unit * 4,
+      whiteSpace: 'pre-wrap',
+      borderTop: `1px solid ${theme.color.grey2}`,
+      borderBottom: `1px solid ${theme.color.grey2}`
+    },
+    scriptHeading: {
+      marginBottom: theme.spacing.unit,
+    },
+    descriptionText: {
+      marginBottom: theme.spacing.unit * 2,
+    }
+  });
+};
+
+export interface Props {
+ data: Linode.StackScript.Response;
+}
+
+export interface State {
+  imagesList: Linode.Image[];
+}
+
+
+type PropsWithStyles = Props & WithStyles<CSSClasses>;
+
+export class StackScript extends React.Component<PropsWithStyles, {}> {
+
+  state: State = {
+    imagesList: []
+  }
+
+  componentDidMount() {
+    const { data: { images } } = this.props;
+
+    getAllImages().then(allImages => {
+      const imagesList = allImages.filter((image: Linode.Image)  => images.indexOf(image.id) !== -1)
+      this.setState({ imagesList });
+    });
+  }
+
+  render() {
+    const {
+      classes,
+      data: {
+        username,
+        deployments_total,
+        deployments_active,
+        description,
+        script,
+        updated,
+      },
+    } = this.props;
+
+    const { imagesList } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Typography role="header" variant="h1" component="h2">
+          About the StackScript
+        </Typography>
+        <Typography variant="h3" className={classes.author}>
+          by&nbsp;
+          <ExternalLink text={username} link={`https://www.linode.com/stackscripts/profile/${username}`} />
+        </Typography>
+        <Typography variant="body2" className={classes.deployments}>
+          {deployments_total} deployments &bull; {deployments_active} still active &bull; last rev. <DateTimeDisplay value={updated} humanizeCutoff={"never"} />
+        </Typography>
+        <div className={classes.description}>
+          {description && <Typography variant="body2" className={classes.descriptionText}>
+            { description }
+          </Typography>}
+          {imagesList.length !== 0 && <Typography variant="body2">
+            <strong>Compatible with: </strong>
+            { imagesList.map(image => image.label).join(', ' ) }
+          </Typography>}
+        </div>
+        <Typography variant="h3" className={classes.scriptHeading}>
+          Script:
+        </Typography>
+        <ScriptCode script={script} />
+      </div>
+    )
+  }
+};
+
+const styled = withStyles(styles);
+
+export default styled(StackScript);
---
+++ src/features/StackScripts/StackScriptsDetail.tsx
@@ -0,0 +1,114 @@
+import { compose } from 'ramda';
+import * as React from 'react';
+
+import { RouteComponentProps } from 'react-router-dom';
+import Breadcrumb from 'src/components/Breadcrumb';
+import Button from 'src/components/Button';
+import CircleProgress from 'src/components/CircleProgress';
+import { StyleRulesCallback, withStyles, WithStyles } from 'src/components/core/styles';
+import setDocs, { SetDocsProps } from 'src/components/DocsSidebar/setDocs';
+import Grid from 'src/components/Grid';
+import NotFound from 'src/components/NotFound';
+import StackScript from 'src/components/StackScript';
+import { StackScripts as StackScriptsDocs } from 'src/documentation';
+
+import { getStackScript } from 'src/services/stackscripts';
+
+interface MatchProps { stackScriptId: number };
+type RouteProps = RouteComponentProps<MatchProps>;
+interface State {
+  loading: boolean;
+  stackScript?: Linode.StackScript.Response
+}
+
+type ClassNames = 'root' | 'titleWrapper' | 'cta' | 'button';
+
+const styles: StyleRulesCallback<ClassNames> = (theme) => ({
+  root: {},
+  titleWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  cta: {
+    marginTop: theme.spacing.unit,
+    [theme.breakpoints.down('sm')]: {
+      margin: 0,
+      display: 'flex',
+      flexBasis: '100%',
+    },
+  },
+  button: {
+    marginBottom: theme.spacing.unit * 4,
+  },
+});
+
+type CombinedProps = RouteProps & WithStyles<ClassNames> & SetDocsProps;
+
+export class StackScriptsDetail extends React.Component<CombinedProps, {}> {
+  
+  state: State = {
+    loading: true,
+    stackScript: undefined
+  }
+
+  componentDidMount() {
+    const { stackScriptId } = this.props.match.params;
+
+    getStackScript(stackScriptId)
+      .then(stackScript => {
+        this.setState({ stackScript, loading: false })
+      })
+      .catch(error => {
+        this.setState({ error, loading: false })
+      })
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { loading, stackScript } = this.state;
+
+    if (loading) {
+      return <CircleProgress />
+    }
+
+    if (!stackScript) {
+      return <NotFound />;
+    }
+
+
+
+    return (
+      <React.Fragment>
+        <Grid
+        container
+        justify="space-between"
+        >
+          <Grid item className={classes.titleWrapper}>
+            <div className={classes.titleWrapper}>
+              <Breadcrumb
+                linkTo="/stackscripts"
+                labelTitle={`${stackScript.username} / ${stackScript.label}`}
+              />
+            </div>
+          </Grid>
+          <Grid item className={classes.cta}>
+            <Button
+              type="primary"
+              className={classes.button}
+              href={`/linodes/create?type=fromStackScript&stackScriptID=${stackScript.id}&stackScriptUsername=${stackScript.username}`}
+            >
+              Deploy New Linode
+            </Button>
+          </Grid>
+        </Grid>
+        <StackScript data={stackScript} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default compose(
+  withStyles(styles),
+  setDocs([StackScriptsDocs])
+)(StackScriptsDetail)
