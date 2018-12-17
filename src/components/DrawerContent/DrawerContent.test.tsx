416d11b2fff11b02b039083a4c7d5bceb13cce62
Russ Zhurba <rzhurba@linode.com>
2018-12-17 09:39:34 -0500

    M3-1704: use setProps to avoid code dups

--- src/components/DrawerContent/DrawerContent.test.tsx
+++ src/components/DrawerContent/DrawerContent.test.tsx
@@ -3,45 +3,29 @@ import * as React from 'react';
 import { DrawerContent } from './DrawerContent';
 
 describe('DrawerContent', () => {
-  it('should show loading component while loading is in progress', () => {
-    const component = shallow(
-        <DrawerContent
-          title="Hello Drawer"
-          loading={true}
-          error={true}
-					>
-					<table />
-				</DrawerContent>
-      );
+	const component = shallow(
+		<DrawerContent
+			title="Hello Drawer"
+			loading={true}
+			error={true}
+			>
+			<table />
+		</DrawerContent>
+	);
 
+  it('should show loading component while loading is in progress', () => {
 		expect(component.name()).toEqual('WithStyles(circleProgressComponent)');
 		expect(component.find('table')).toHaveLength(0);
 	});
 
   it('should show error if loading is finished but the error persists', () => {
-    const component = shallow(
-        <DrawerContent
-          title="Hello Drawer"
-          loading={false}
-          error={true}
-					>
-					<table />
-				</DrawerContent>
-      );
+    component.setProps({ loading: false });
     
 		expect(component.name()).toEqual('WithStyles(Notice)');
 		expect(component.find('table')).toHaveLength(0);
   });
   it('should display content if there is no error nor loading', () => {
-    const component = shallow(
-        <DrawerContent
-          title="Hello Drawer"
-          loading={false}
-          error={false}
-        >
-					<table />
-				</DrawerContent>
-      );
+    component.setProps({ error: false });
 		
 		expect(component.find('CircleProgress')).toHaveLength(0);
 		expect(component.find('Notice')).toHaveLength(0);
