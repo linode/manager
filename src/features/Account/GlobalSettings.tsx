49a7328b29d1a909af9fb109e7e93c4488146743
Jared Kobos <jaredkobos@gmail.com>
2018-12-17 14:42:27 -0500

    Basic prototype

--- src/features/Account/GlobalSettings.tsx
+++ src/features/Account/GlobalSettings.tsx
@@ -6,9 +6,13 @@ import { compose } from 'recompose';
 import CircleProgress from 'src/components/CircleProgress';
 import { StyleRulesCallback, Theme, withStyles, WithStyles } from 'src/components/core/styles';
 import ErrorState from 'src/components/ErrorState';
+import TagImportDrawer from 'src/features/TagImport';
 import { handleOpen } from 'src/store/reducers/backupDrawer';
 import { updateAccountSettings } from 'src/store/reducers/resources/accountSettings';
+import { openGroupDrawer } from 'src/store/reducers/tagImportDrawer';
+import getEntitiesWithGroupsToImport, { GroupedEntitiesForImport } from 'src/store/selectors/getEntitiesWithGroupsToImport';
 import AutoBackups from './AutoBackups';
+import ImportGroupsAsTags from './ImportGroupsAsTags';
 import NetworkHelper from './NetworkHelper';
 
 type ClassNames = 'root';
@@ -24,11 +28,13 @@ interface StateProps {
   linodesWithoutBackups: Linode.Linode[];
   updateError?: Linode.ApiFieldError[];
   networkHelperEnabled: boolean;
+  entitiesWithGroupsToImport: GroupedEntitiesForImport;
 }
 
 interface DispatchProps {
   actions: {
     updateAccount: (data: Partial<Linode.AccountSettings>) => void;
+    openImportDrawer: () => void;
     openBackupsDrawer: () => void;
   }
 }
@@ -64,14 +70,21 @@ class GlobalSettings extends React.Component<CombinedProps, {}> {
 
   render() {
     const {
-      actions: { openBackupsDrawer },
+      actions: { openBackupsDrawer, openImportDrawer },
       backups_enabled,
       networkHelperEnabled,
       error,
       loading,
       linodesWithoutBackups,
-      updateError
+      updateError,
+      entitiesWithGroupsToImport,
     } = this.props;
+
+    // Only show the import groups option if the user has groups available to import
+    const hasGroupsToImport =
+    entitiesWithGroupsToImport.linodes.length >= 1
+    // @todo: Uncomment when domain support is added
+    // && entitiesWithGroupsToImport.domains.length >= 1
 
     if (loading) { return <CircleProgress /> }
     if (error) { return <ErrorState errorText={"There was an error retrieving your account data."} /> }
@@ -90,6 +103,12 @@ class GlobalSettings extends React.Component<CombinedProps, {}> {
           onChange={this.toggleNetworkHelper}
           networkHelperEnabled={networkHelperEnabled}
         />
+        {hasGroupsToImport &&
+          <ImportGroupsAsTags
+            openDrawer={openImportDrawer}
+          />
+        }
+        <TagImportDrawer />
       </React.Fragment>
     )
   }
@@ -101,14 +120,16 @@ const mapStateToProps: MapStateToProps<StateProps, {}, ApplicationState> = (stat
   error: path(['__resources', 'accountSettings', 'error'], state),
   updateError: path(['__resources', 'accountSettings', 'updateError'], state),
   linodesWithoutBackups: state.__resources.linodes.entities.filter(l => !l.backups.enabled),
-  networkHelperEnabled: pathOr(false, ['__resources', 'accountSettings', 'data', 'network_helper'], state)
+  networkHelperEnabled: pathOr(false, ['__resources', 'accountSettings', 'data', 'network_helper'], state),
+  entitiesWithGroupsToImport: getEntitiesWithGroupsToImport(state),
 });
 
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (dispatch, ownProps) => {
   return {
     actions: {
       updateAccount: (data: Partial<Linode.AccountSettings>) => dispatch(updateAccountSettings(data)),
-      openBackupsDrawer: () => dispatch(handleOpen())
+      openBackupsDrawer: () => dispatch(handleOpen()),
+      openImportDrawer: () => dispatch(openGroupDrawer())
     }
   };
 };
---
+++ src/features/Account/ImportGroupsAsTags.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Button from 'src/components/Button';
+import { StyleRulesCallback, withStyles, WithStyles } from 'src/components/core/styles';
+import Typography from 'src/components/core/Typography';
+import ExpansionPanel from 'src/components/ExpansionPanel';
+
+type ClassNames = 'root' | 'helperText';
+
+const styles: StyleRulesCallback<ClassNames> = (theme) => ({
+  root: {
+
+  },
+  helperText: {
+    marginBottom: theme.spacing.unit * 2,
+  }
+});
+
+interface Props {
+  openDrawer: () => void;
+}
+
+type CombinedProps = Props & WithStyles<ClassNames>;
+
+const ImportGroupsAsTags: React.StatelessComponent<CombinedProps> = (props) => {
+  const { classes, openDrawer } = props;
+  return (
+    <ExpansionPanel
+      className={classes.root}
+      defaultExpanded={true}
+      heading={"Import Display Groups"}
+    >
+      <Typography variant="body1" className={classes.helperText}>
+        Import Display Groups from Classic Manager and convert them to tags.
+        Your existing tags will not be affected.
+      </Typography>
+      <Button type="primary" onClick={openDrawer}>
+        Import Display Groups
+      </Button>
+    </ExpansionPanel>
+  );
+};
+
+const styled = withStyles(styles);
+
+export default styled(ImportGroupsAsTags);
