93658aaf440b193eca2c90e24d6cbb085263460e
rulikrulit <rulitrulik@gmail.com>
2018-12-18 09:15:38 -0500

    M3-2051: capitalize linode progress bar text (#4253)
    * M3-2051: capitalize linode progress bar text
    
    * M3-2051: fix mupltiple words status capitalization

--- src/utilities/capitalize.ts
+++ src/utilities/capitalize.ts
@@ -1,3 +1,9 @@
-export default (s:string) => {
-    return s.charAt(0).toUpperCase() + s.slice(1);
+const capitalize = (s:string) => {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+export default capitalize;
+
+export const capitalizeAllWords = (s:string) => {
+  return s.split(' ').map(capitalize).join(' ');
 }
