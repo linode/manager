1852776034992976aabbc6f7632a17c1cbb66ca5
mmckenna <mmckenna.phila@gmail.com>
2018-12-20 15:09:30 -0500

    cleanup

--- src/features/Profile/APITokens/APITokenTable.tsx
+++ src/features/Profile/APITokens/APITokenTable.tsx
@@ -213,7 +213,7 @@ export class APITokenTable extends React.Component<CombinedProps, State> {
   revokePersonalAccessToken = () => {
     const { dialog } = this.state;
     deletePersonalAccessToken(dialog.id as number)
-      .then(() => this.props.onDelete({ orderBy: 'created', order: 'asc' }))
+      .then(() => this.props.onDelete())
       .then(() => this.closeRevokeDialog())
       .catch((err: any) => this.showDialogError(err))
   }
@@ -221,9 +221,9 @@ export class APITokenTable extends React.Component<CombinedProps, State> {
   revokeAppToken = () => {
     const { dialog } = this.state;
     deleteAppToken(dialog.id as number)
-    .then(() => this.props.onDelete({ orderBy: 'created', order: 'asc' }))
-    .then(() => { this.closeRevokeDialog(); })
-    .catch((err: any) => this.showDialogError(err))
+      .then(() => this.props.onDelete())
+      .then(() => { this.closeRevokeDialog(); })
+      .catch((err: any) => this.showDialogError(err))
   }
 
   showDialogError(err: any) {
