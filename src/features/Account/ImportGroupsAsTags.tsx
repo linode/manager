a1e915823a90857fba65660621c0038457ffb226
John Callahan <johnwcallahan@gmail.com>
2018-12-20 17:15:02 -0500

    change group import CTA logic

--- src/features/Account/ImportGroupsAsTags.tsx
+++ src/features/Account/ImportGroupsAsTags.tsx
@@ -31,7 +31,7 @@ export const ImportGroupsAsTags: React.StatelessComponent<CombinedProps> = (prop
         You now have the ability to import your Display Groups from Classic Manager as tags and they will be associated with your Domains and Linodes. This will give you the ability to organize and view your Linodes by tags. <strong>Your existing tags will not be affected.</strong>
       </Typography>
       <Button type="primary" onClick={openDrawer} data-qa-open-group-import-drawer>
-        Import Display Groups Now
+        Import Display Groups
       </Button>
     </ExpansionPanel>
   );
