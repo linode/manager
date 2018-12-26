f2876265c90955752c460c29bb3be9708d88696b
John Callahan <johnwcallahan@gmail.com>
2018-12-26 15:02:22 -0500

    M3-1886 Add subject and user agent to "Provide Feedback" mailto link (#4290)
    * add subject and user-agent to feedback email
    
    * specifiy window object on navigator
    
    * include cloud manager version
    
    * tighten up test syntax

--- src/features/TopMenu/UserNotificationsMenu/UserNotificationsMenu.tsx
+++ src/features/TopMenu/UserNotificationsMenu/UserNotificationsMenu.tsx
@@ -65,7 +65,7 @@ const userAgentDetection = () => {
       return (
         <Typography>
           Your Web Browser (<strong>{b.name}</strong>) is not compatible with the Linode Manager.
-          Please update to <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" target="_blank">Miscrosoft Edge</a> for more security,
+          Please update to <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" target="_blank">Microsoft Edge</a> for more security,
           speed and the best experience on this site.
         </Typography>
       )
