6fedf70c592ff884ca76d620a433dd9a5f11917d
John Callahan <johnwcallahan@gmail.com>
2018-12-26 12:34:07 -0500

    include cloud manager version

--- src/features/Footer/createMailto.ts
+++ src/features/Footer/createMailto.ts
@@ -1,12 +1,23 @@
+const { VERSION } = process.env;
+
 const createMailto = (userAgent: string): string => {
   const recipient = 'feedback@linode.com';
   const subject = 'Cloud Manager User Feedback';
 
   let mailto = `mailto:${recipient}?Subject=${encodeURIComponent(subject)}`;
+  let body = '';
+
+  if (VERSION) {
+    body += `\nCloud Manager Version: ${VERSION}`;
+  }
 
   if (userAgent && typeof userAgent === 'string') {
-    const body = `\n\n${userAgent}`;
-    mailto += `&Body=${encodeURIComponent(body)}`;
+    body += `\n${userAgent}`;
+  }
+
+  if (body) {
+    const encodedBody = encodeURIComponent(`\n${body}`)
+    mailto += `&Body=${encodedBody}`;
   }
 
   return mailto;
