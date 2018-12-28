6fd925b5afc50f062d238ea94a15a37bec73f305
Martin <mmckenna@linode.com>
2018-12-28 09:03:25 -0500

    M3-1949 Change Pagination Controls to handle many pages (#4247)
    * add ellipses for page numbers over 5
    
    * add logic for going past page 5
    
    * handle last page logic
    
    * remove unnecesasry logic
    
    * cleanup and add tests
    
    * fix styling issue
    
    * fix issue with null image
    
    * add ellipses for page numbers over 5
    
    * add logic for going past page 5
    
    * handle last page logic
    
    * remove unnecesasry logic
    
    * cleanup and add tests
    
    * fix styling issue
    
    * fix issue with null image
    
    * fix stackscript regression
    
    * show less page number buttons at smaller breakpoints
    
    * fix test

--- src/components/PaginationControls/PaginationControls.tsx
+++ src/components/PaginationControls/PaginationControls.tsx
@@ -5,8 +5,11 @@ import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPage from '@material-ui/icons/LastPage';
 
-import PageButton, { Props as PageButtonProps } from 'src/components/PaginationControls/PageButton';
+import Hidden from 'src/components/core/Hidden';
+import PageButton from 'src/components/PaginationControls/PageButton';
 import { sendEvent } from 'src/utilities/analytics';
+
+import PageNumbers from './PageNumbers';
 
 interface Props {
   count: number;
@@ -16,60 +19,7 @@ interface Props {
   onClickHandler: (page?: number) => void;
 }
 
-interface PageObject {
-  disabled: boolean;
-  number: number;
-}
-
-interface State {
-  pages: PageObject[];
-};
-
-const calPages = (count: number, pageSize: number) => Math.ceil(count / pageSize);
-
-const createPagesArray = (count: number, pageSize: number) => Array.from(Array(calPages(count, pageSize)));
-
-const createPageObject = (handler: (n: number) => void, page: number) => (v: void, idx: number) => {
-  const number = idx + 1;
-  return ({
-    disabled: page === number,
-    number,
-  });
-};
-
-const createPages = (
-  count: number,
-  onClickHandler: (n: number) => void,
-  page: number,
-  pageSize: number,
-) => createPagesArray(count, pageSize).map(createPageObject(onClickHandler, page));
-
-export class PaginationControls extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    const { count, pageSize, onClickHandler, page } = props;
-
-    this.state = {
-      pages: createPages(count, onClickHandler, page, pageSize),
-    }
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (
-      prevProps.count === this.props.count
-      && prevProps.page === this.props.page
-      && prevProps.pageSize === this.props.pageSize
-    ) {
-      return;
-    }
-
-    const { count, onClickHandler, page, pageSize } = this.props;
-
-    this.setState({
-      pages: createPages(count, onClickHandler, page, pageSize),
-    });
-  }
-
+export class PaginationControls extends React.Component<Props, {}> {
   handleSendEvent = (eventLabel: string) => {
     const { eventCategory } = this.props;
 
@@ -124,40 +74,35 @@ export class PaginationControls extends React.Component<Props, State> {
   render() {
     /** API paging starts at 1. Don't they know arrays starts at 0? */
     const { count, page, pageSize, } = this.props;
-    const numPages = Math.ceil(count / pageSize);
+    const numPages = calNumOfPages(count, pageSize);
     const disableHead = page === 1;
     const disableTail = page >= numPages;
 
     return (
       <div>
-        <PageButton data-qa-page-first onClick={this.handleFirstPageClick} disabled={disableHead} aria-label="First Page" >
-          <FirstPage />
-        </PageButton>
+        <Hidden xsDown>
+          <PageButton data-qa-page-first onClick={this.handleFirstPageClick} disabled={disableHead} aria-label="First Page" >
+            <FirstPage />
+          </PageButton>
+        </Hidden>
 
         <PageButton data-qa-page-previous onClick={this.handlePreviousPageClick} disabled={disableHead} aria-label="Previous Page" >
           <KeyboardArrowLeft />
         </PageButton>
-        {
-          this.state.pages.map(({ number, disabled }) => (
-            <PageNumber
-              number={number}
-              handlePageClick={this.handlePageClick}
-              data-qa-page-to={number}
-              key={number}
-              disabled={disabled}
-              arial-label={`Page ${number}`}
-            >
-              {number}
-            </PageNumber>
-          ))
-        }
+        <PageNumbers
+          numOfPages={calNumOfPages(count, pageSize)}
+          handlePageClick={this.handlePageClick}
+          currentPage={page}
+        />
         <PageButton data-qa-page-next onClick={this.handleNextPageClick} disabled={disableTail} aria-label="Next Page" >
           <KeyboardArrowRight />
         </PageButton>
 
-        <PageButton data-qa-page-last onClick={this.handleLastPageClick} disabled={disableTail} aria-label="Last Page" >
-          <LastPage />
-        </PageButton>
+        <Hidden xsDown>
+          <PageButton data-qa-page-last onClick={this.handleLastPageClick} disabled={disableTail} aria-label="Last Page" >
+            <LastPage />
+          </PageButton>
+        </Hidden>
       </div >
     );
   }
@@ -166,21 +111,12 @@ export class PaginationControls extends React.Component<Props, State> {
 
 export default PaginationControls;
 
-
-interface PageNumberProps extends PageButtonProps {
-  number: number;
-  handlePageClick: (n: number) => void;
-}
-
-class PageNumber extends React.PureComponent<PageNumberProps> {
-  onClick = () => this.props.handlePageClick(this.props.number);
-
-  render() {
-    const { onClick, children, handlePageClick, ...rest } = this.props;
-
-    return (
-      <PageButton {...rest} onClick={this.onClick}>
-        {children}
-      </PageButton>);
-  }
-};
+/**
+ * 
+ * @param { number } count - the amount of total things returned from the API
+ * @param { number } pageSize - the selected page size filter returned from the API
+ * 
+ * @todo remove this function altogether. This information is returned from the API
+ * and can be passed down as props making this function unnecessary
+ */
+const calNumOfPages = (count: number, pageSize: number) => Math.ceil(count / pageSize);
