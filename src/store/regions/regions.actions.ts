e82a42d41768be465ec22017161087cff85bac3e
Robert Stires <rstires@linode.com>
2019-01-07 14:19:51 -0500

    Replace region context with Redux.

---
+++ src/store/regions/regions.actions.ts
@@ -0,0 +1,21 @@
+import { getRegions } from 'src/services/misc';
+import { actionCreatorFactory } from 'typescript-fsa';
+import { ThunkAction } from '../../../node_modules/redux-thunk';
+
+const actionCreator = actionCreatorFactory(`@@manager/regions`);
+
+export const regionsRequestActions = actionCreator.async<void, Linode.Region[], Linode.ApiFieldError[]>(`request`);
+
+export const requestRegions = (): ThunkAction<Promise<Linode.Region[]>, ApplicationState, undefined> => (dispatch) => {
+  dispatch(regionsRequestActions.started());
+
+  return getRegions()
+    .then(({ data }) => {
+      dispatch(regionsRequestActions.done({ result: data }));
+      return data;
+    })
+    .catch((error) => {
+      dispatch(regionsRequestActions.failed({ error }));
+      return error;
+    });
+}
