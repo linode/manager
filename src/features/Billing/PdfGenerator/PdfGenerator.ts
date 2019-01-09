17b53b5159f3b0685f12659632eaa5e5b38d6728
Russ Zhurba <rzhurba@linode.com>
2019-01-09 14:54:20 -0500

    M3-2078: wrapping PDF generation into try-catch

--- src/features/Billing/PdfGenerator/PdfGenerator.ts
+++ src/features/Billing/PdfGenerator/PdfGenerator.ts
@@ -113,161 +113,167 @@ const addTitle = (doc: jsPDF, title: string) => {
 }
 
 export const printInvoice = (account: Linode.Account, invoice: Linode.Invoice, items: Linode.InvoiceItem[]) => {
-
-  const itemsPerPage = 18;
-  const date = formatDate(invoice.date, {format: 'YYYY-MM-DD'});
-  const invoiceId = invoice.id;
-  const itemsChunks = items ? splitEvery(itemsPerPage, items) : [[]];
-  const tableEnd = tableBodyStart + cellHeight * itemsChunks[itemsChunks.length - 1].length
-
-
-  const doc = new jsPDF({
-    unit: 'px'
-  });
-
-  const addTable = (itemsChunk: Linode.InvoiceItem[]) => {
-    doc.setFontSize(10);
-
-    const header = [
-      {name: 'Description', prompt: 'Description', width: 235},
-      {name: 'From', prompt: 'From', width: 72},
-      {name: 'To', prompt: 'To', width: 72},
-      {name: 'Quantity', prompt: 'Quantity', width: 52},
-      {name: 'Unit Price', prompt: 'Unit Price', width: 67},
-      {name: 'Amount', prompt: 'Amount', width: 52}
-    ] as any[]; // assert type 'any' because per source code this is an extended and more advanced way of usage
-
-    const itemRows = itemsChunk.map(item => {
-      const { label, from, to, quantity, unit_price, amount } = item;
-      return {
-        Description: formatDescription(label),
-        From: renderDate(from),
-        To: renderDate(to),
-        Quantity: renderQuantity(quantity),
-        'Unit Price': renderUnitPrice(unit_price),
-        Amount: '$' + amount
+  try {
+    const itemsPerPage = 18;
+    const date = formatDate(invoice.date, {format: 'YYYY-MM-DD'});
+    const invoiceId = invoice.id;
+    const itemsChunks = items ? splitEvery(itemsPerPage, items) : [[]];
+    const tableEnd = tableBodyStart + cellHeight * itemsChunks[itemsChunks.length - 1].length
+  
+  
+    const doc = new jsPDF({
+      unit: 'px'
+    });
+  
+    const addTable = (itemsChunk: Linode.InvoiceItem[]) => {
+      doc.setFontSize(10);
+  
+      const header = [
+        {name: 'Description', prompt: 'Description', width: 235},
+        {name: 'From', prompt: 'From', width: 72},
+        {name: 'To', prompt: 'To', width: 72},
+        {name: 'Quantity', prompt: 'Quantity', width: 52},
+        {name: 'Unit Price', prompt: 'Unit Price', width: 67},
+        {name: 'Amount', prompt: 'Amount', width: 52}
+      ] as any[]; // assert type 'any' because per source code this is an extended and more advanced way of usage
+  
+      const itemRows = itemsChunk.map(item => {
+        const { label, from, to, quantity, unit_price, amount } = item;
+        return {
+          Description: formatDescription(label),
+          From: renderDate(from),
+          To: renderDate(to),
+          Quantity: renderQuantity(quantity),
+          'Unit Price': renderUnitPrice(unit_price),
+          Amount: '$' + amount
+        }
+      });
+  
+      // Place table header
+      doc.table(leftPadding, 140, [], header, {
+        fontSize: 10,
+        printHeaders: true,
+        autoSize: false,
+        margins: {
+          left: 15,
+          top: 10,
+          width: 800,
+          bottom: 0
+        }
+      });
+  
+      // Place table body
+      doc.table(leftPadding, tableBodyStart, itemRows, header, {
+        fontSize: 9,
+        printHeaders: false,
+        autoSize: false,
+        margins: {
+          left: leftPadding,
+          top: 10,
+          width: 800,
+          bottom: 0
+        }
+      });
+    };
+  
+    const addTotalAmount = () => {
+      doc.setFontSize(13);
+      doc.setFontStyle('bold');
+      // Empty line
+      doc.cell(leftPadding, tableEnd, 412.5, 10, ' ', 1, 'left');
+      // "Total" cell
+      doc.cell(leftPadding, tableEnd + 10, 374, 20, 'Total:  ', 2, 'right');
+      // Total value cell
+      doc.cell(leftPadding + 300, tableEnd + 10, 38.5, 20, `$${Number(invoice.total).toFixed(2)}`, 2, 'left');
+      // reset text format
+      doc.setFontStyle('normal');
+    };
+  
+    // Create a separate page for each set of invoice items
+    itemsChunks.forEach((itemsChunk, index) => {
+      doc.addImage(LinodeLogo, 'JPEG', 150, 5, 120, 50);
+      addLeftHeader(doc, index + 1, itemsChunks.length, date);
+      addRightHeader(doc, account);
+      addTitle(doc, `Invoice: #${invoiceId}`);
+      addTable(itemsChunk);
+      addFooter(doc);
+      if (index < itemsChunks.length - 1) {
+        doc.addPage();
       }
     });
-
-    // Place table header
-    doc.table(leftPadding, 140, [], header, {
-      fontSize: 10,
-      printHeaders: true,
-      autoSize: false,
-      margins: {
-        left: 15,
-        top: 10,
-        width: 800,
-        bottom: 0
-      }
-    });
-
-    // Place table body
-    doc.table(leftPadding, tableBodyStart, itemRows, header, {
-      fontSize: 9,
-      printHeaders: false,
-      autoSize: false,
-      margins: {
-        left: leftPadding,
-        top: 10,
-        width: 800,
-        bottom: 0
-      }
-    });
-  };
-
-  const addTotalAmount = () => {
-    doc.setFontSize(13);
-    doc.setFontStyle('bold');
-    // Empty line
-    doc.cell(leftPadding, tableEnd, 412.5, 10, ' ', 1, 'left');
-    // "Total" cell
-    doc.cell(leftPadding, tableEnd + 10, 374, 20, 'Total:  ', 2, 'right');
-    // Total value cell
-    doc.cell(leftPadding + 300, tableEnd + 10, 38.5, 20, `$${Number(invoice.total).toFixed(2)}`, 2, 'left');
-    // reset text format
-    doc.setFontStyle('normal');
-  };
-
-  // Create a separate page for each set of invoice items
-  itemsChunks.forEach((itemsChunk, index) => {
-    doc.addImage(LinodeLogo, 'JPEG', 150, 5, 120, 50);
-    addLeftHeader(doc, index + 1, itemsChunks.length, date);
-    addRightHeader(doc, account);
-    addTitle(doc, `Invoice: #${invoiceId}`);
-    addTable(itemsChunk);
-    addFooter(doc);
-    if (index < itemsChunks.length - 1) {
-      doc.addPage();
-    }
-  });
-
-  addTotalAmount();
-
-  doc.save(`invoice-${date}.pdf`);  
-
+  
+    addTotalAmount();
+  
+    doc.save(`invoice-${date}.pdf`);    
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export const printPayment = (account: Linode.Account, payment: Linode.Payment) => {
-
-  const date = formatDate(payment.date, {format: 'YYYY-MM-DD'});
-  const paymentId = payment.id;
-  const amount = payment.usd;
-  const tableEnd = tableBodyStart + cellHeight;
-  const doc = new jsPDF({
-    unit: 'px'
-  });
-
-  const addTable = () => {
-    doc.setFontSize(10);
-
-    const header = [
-      {name: 'Description', prompt: 'Description', width: 292},
-      {name: 'Date', prompt: 'Date', width: 128},
-      {name: 'Amount', prompt: 'Amount', width: 128}
-    ] as any[]; // assert type 'any' because per source code this is an extended and more advanced way of usage
-
-    const itemRows = [{
-      Description: 'Payment. Thank you.', // Automatic line breaks don't work well. Doing it manually
-      Date: renderDate(date),
-      Amount: '$' + amount
-    }]
-
-    doc.table(leftPadding, 140, itemRows, header, {
-      fontSize: 12,
-      printHeaders: true,
-      autoSize: false,
-      margins: {
-        left: 15,
-        top: 10,
-        width: 800,
-        bottom: 0
-      }
-    });
-  };
-
-  const addTotalAmount = () => {
-    doc.setFontSize(13);
-    doc.setFontStyle('bold');
-    // Empty line
-    doc.cell(leftPadding, tableEnd, 411, 10, ' ', 1, 'left');
-    // "Total" cell
-    doc.cell(leftPadding, tableEnd + 10, 374, 20, 'Payment Total:    ', 2, 'right');
-    // Total value cell
-    doc.cell(leftPadding + 300, tableEnd + 10, 37, 20, `$${Number(amount).toFixed(2)}`, 2, 'left');
-    // reset text format
-    doc.setFontStyle('normal');
-  };
-
-  doc.addImage(LinodeLogo, 'JPEG', 150, 5, 120, 50);
-  addLeftHeader(doc, 1, 1, date);
-  addRightHeader(doc, account);
-  addTitle(doc, `Receipt for Payment #${paymentId}`);
-  addTable();
-  addFooter(doc);
-  addTotalAmount();
   
-  doc.save(`payment-${date}.pdf`);  
+  try {
+    const date = formatDate(payment.date, {format: 'YYYY-MM-DD'});
+    const paymentId = payment.id;
+    const amount = payment.usd;
+    const tableEnd = tableBodyStart + cellHeight;
+    const doc = new jsPDF({
+      unit: 'px'
+    });
+  
+    const addTable = () => {
+      doc.setFontSize(10);
+  
+      const header = [
+        {name: 'Description', prompt: 'Description', width: 292},
+        {name: 'Date', prompt: 'Date', width: 128},
+        {name: 'Amount', prompt: 'Amount', width: 128}
+      ] as any[]; // assert type 'any' because per source code this is an extended and more advanced way of usage
+  
+      const itemRows = [{
+        Description: 'Payment. Thank you.', // Automatic line breaks don't work well. Doing it manually
+        Date: renderDate(date),
+        Amount: '$' + amount
+      }]
+  
+      doc.table(leftPadding, 140, itemRows, header, {
+        fontSize: 12,
+        printHeaders: true,
+        autoSize: false,
+        margins: {
+          left: 15,
+          top: 10,
+          width: 800,
+          bottom: 0
+        }
+      });
+    };
+  
+    const addTotalAmount = () => {
+      doc.setFontSize(13);
+      doc.setFontStyle('bold');
+      // Empty line
+      doc.cell(leftPadding, tableEnd, 411, 10, ' ', 1, 'left');
+      // "Total" cell
+      doc.cell(leftPadding, tableEnd + 10, 374, 20, 'Payment Total:    ', 2, 'right');
+      // Total value cell
+      doc.cell(leftPadding + 300, tableEnd + 10, 37, 20, `$${Number(amount).toFixed(2)}`, 2, 'left');
+      // reset text format
+      doc.setFontStyle('normal');
+    };
+  
+    doc.addImage(LinodeLogo, 'JPEG', 150, 5, 120, 50);
+    addLeftHeader(doc, 1, 1, date);
+    addRightHeader(doc, account);
+    addTitle(doc, `Receipt for Payment #${paymentId}`);
+    addTable();
+    addFooter(doc);
+    addTotalAmount();
+    
+    doc.save(`payment-${date}.pdf`);    
+  } catch (e) {
+    console.error(e);
+  }
 
 }
 
