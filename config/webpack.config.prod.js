33305c4878776e30549c82a3dcbc2fe11d2a85bf
mmckenna <mmckenna.phila@gmail.com>
2019-01-09 13:03:01 -0500

    remove old extracttextplugin config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -35,18 +35,6 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
   throw new Error('Production builds must have NODE_ENV=production.');
 }
 
-// Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css';
-
-// // ExtractTextPlugin expects the build output to be flat.
-// // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
-// // However, our output is structured with css, js and media folders.
-// // To have this structure working with relative paths, we have to use custom options.
-// const extractTextPluginOptions = shouldUseRelativeAssetPaths
-//   ? // Making sure that the publicPath goes back to to build folder.
-//     { publicPath: Array(cssFilename.split('/').length).join('../') }
-//   : {};
-
 // This is the production configuration.
 // It compiles slowly and is focused on producing a fast and minimal bundle.
 // The development configuration is different and lives in a separate file.
