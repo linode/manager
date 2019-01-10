f3694ca29811ef9cf65844d21c1fed1662ba9b10
Nicholas Wisnewski <nwisnewski@Nicholass-MacBook-Pro.local>
2019-01-10 16:52:31 -0500

    This will give you the ability to organize and view your Linodes and Domains by tags

--- src/features/Dashboard/GroupImportCard/GroupImportCard.tsx
+++ src/features/Dashboard/GroupImportCard/GroupImportCard.tsx
@@ -104,7 +104,7 @@ export const GroupImportCard: React.StatelessComponent<CombinedProps> = (props) 
       </Paper>
       <Paper className={classes.section}>
         <Typography variant="body1" data-qa-group-cta-body>
-        You now have the ability to import your Display Groups from Classic Manager as tags and they will be associated with your Domains and Linodes. This will give you the ability to organize and view your Linodes by tags. <strong>Your existing tags will not be affected.</strong>
+        You now have the ability to import your Display Groups from Classic Manager as tags and they will be associated with your Domains and Linodes. This will give you the ability to organize and view your Linodes and Domains by tags. <strong>Your existing tags will not be affected.</strong>
         </Typography>
         <Button
           type="primary"
