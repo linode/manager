6994a20b8d2cb9048a206d47214e3c303579a46e
Russ Zhurba <rzhurba@linode.com>
2019-01-17 15:46:22 -0500

    M3-2168: add status indicator for entities pages

--- src/components/StatusIndicator/index.ts
+++ src/components/StatusIndicator/index.ts
@@ -1,3 +1,9 @@
-import StatusIndicator, { Props as _Props } from './StatusIndicator';
+import StatusIndicator, {
+  getStatusForDomain as _getStatusForDomain,
+  getStatusForVolume as _getStatusForVolume,
+  Props as _Props,
+} from './StatusIndicator';
+export const getStatusForVolume = _getStatusForVolume;
+export const getStatusForDomain = _getStatusForDomain;
 export default StatusIndicator;
 export interface Props extends _Props { };
