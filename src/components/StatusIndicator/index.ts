24f05343f0fb7319cf38233d8e1dc15cf9994994
Russ Zhurba <rzhurba@linode.com>
2019-01-21 08:27:07 -0500

    Move imports to immediate exports

--- src/components/StatusIndicator/index.ts
+++ src/components/StatusIndicator/index.ts
@@ -1,9 +1,5 @@
-import StatusIndicator, {
-  getStatusForDomain as _getStatusForDomain,
-  getStatusForVolume as _getStatusForVolume,
-  Props as _Props,
-} from './StatusIndicator';
-export const getStatusForVolume = _getStatusForVolume;
-export const getStatusForDomain = _getStatusForDomain;
-export default StatusIndicator;
+import { Props as _Props } from './StatusIndicator';
+
+export { default, getStatusForDomain, getStatusForVolume } from './StatusIndicator';
+
 export interface Props extends _Props { };
