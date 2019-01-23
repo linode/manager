c5a45a39036a82138670339e62f46b0218234aec
Robert Stires <rstires@linode.com>
2019-01-23 16:25:19 -0500

    Redact access_tokens in URLs... (#4424)
    Required an update to @sentry/browser for the beforeSend hook.

--- src/exceptionReporting.ts
+++ src/exceptionReporting.ts
@@ -1,21 +1,32 @@
-import * as Raven from 'raven-js';
+import { BrowserOptions, captureException, init } from '@sentry/browser';
+import { lensPath, over } from 'ramda';
 import { SENTRY_URL } from 'src/constants';
+import redactAccessTokenFromUrl from 'src/utilities/redactAccessTokenFromUrl';
 
-window.addEventListener('unhandledrejection', (err: PromiseRejectionEvent) => {
-  Raven.captureException(err.reason);
-});
+const updateRequestUrl = over(lensPath(['request', 'url']), redactAccessTokenFromUrl);
+
+const beforeSend: BrowserOptions['beforeSend'] = (event, hint) => {
+  return updateRequestUrl(event);
+};
 
 if (SENTRY_URL) {
-  Raven
-    .config(SENTRY_URL, {
-      release: process.env.VERSION,
-    })
-    .install();
+
+  init({
+    dsn: SENTRY_URL,
+    release: process.env.VERSION,
+    environment: process.env.NODE_ENV,
+    beforeSend,
+  });
 }
+
+
+window.addEventListener('unhandledrejection', (err: PromiseRejectionEvent) => {
+  captureException(err.reason);
+});
 
 export const reportException = (error: string | Error, extra?: any) => {
   if (process.env.NODE_ENV === 'production' && SENTRY_URL) {
-    Raven.captureException(error, { extra })
+    captureException(error)
   } else {
     /* tslint:disable */
     console.error('====================================');
