449066506bfe73bf7022b8e37bb7629a2c44e09c
rulikrulit <rulitrulik@gmail.com>
2019-01-24 10:41:37 -0500

    M3-2168: remove volumes status indicator (#4428)

--- src/components/StatusIndicator/index.ts
+++ src/components/StatusIndicator/index.ts
@@ -1,5 +1,5 @@
 import { Props as _Props } from './StatusIndicator';
 
-export { default, getStatusForDomain, getStatusForVolume } from './StatusIndicator';
+export { default, getStatusForDomain } from './StatusIndicator';
 
 export interface Props extends _Props { };
