ab812d0a891748dc000ea4810fbfef518d68b818
Russ Zhurba <rzhurba@linode.com>
2019-01-28 14:16:13 -0500

    M3-2285: remove accordions from nodeBalancer summary

--- src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/TablesPanel.tsx
+++ src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/TablesPanel.tsx
@@ -1,12 +1,19 @@
 import { pathOr } from 'ramda';
 import * as React from 'react';
-import { StyleRulesCallback, withStyles, WithStyles } from 'src/components/core/styles';
+import CircleProgress from 'src/components/CircleProgress';
+import Paper from 'src/components/core/Paper';
+import {
+  StyleRulesCallback,
+  withStyles,
+  WithStyles
+} from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
-import ExtendedExpansionPanel from 'src/components/ExtendedExpansionPanel';
+import ErrorState from 'src/components/ErrorState';
 import LineGraph from 'src/components/LineGraph';
 import { getNodeBalancerStats } from 'src/services/nodebalancers';
 
-type ClassNames = 'chart'
+type ClassNames =
+  | 'chart'
   | 'leftLegend'
   | 'bottomLegend'
   | 'graphTitle'
@@ -16,20 +23,29 @@ type ClassNames = 'chart'
   | 'red'
   | 'yellow'
   | 'header'
+  | 'title'
+  | 'panel'
   | 'graphWrapper';
 
-const styles: StyleRulesCallback<ClassNames> = (theme) => {
+const styles: StyleRulesCallback<ClassNames> = theme => {
   return {
     header: {
+      padding: theme.spacing.unit * 2
+    },
+    panel: {
       padding: theme.spacing.unit * 2,
+      marginTop: theme.spacing.unit * 2
     },
     graphWrapper: {
-      marginTop: theme.spacing.unit * 2,
+      marginTop: theme.spacing.unit * 2
+    },
+    title: {
+      marginBottom: theme.spacing.unit * 2
     },
     chart: {
       position: 'relative',
       width: '100%',
-      padding: theme.spacing.unit * 4,
+      padding: theme.spacing.unit * 4
     },
     leftLegend: {
       position: 'absolute',
@@ -37,10 +53,12 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => {
       bottom: 36,
       transform: 'rotate(-90deg)',
       color: '#777',
-      fontSize: 14,
+      fontSize: 14
     },
     bottomLegend: {
-      margin: `${theme.spacing.unit * 2}px ${theme.spacing.unit}px ${theme.spacing.unit}px`,
+      margin: `${theme.spacing.unit * 2}px ${theme.spacing.unit}px ${
+        theme.spacing.unit
+      }px`,
       padding: theme.spacing.unit * 2,
       display: 'flex',
       flexWrap: 'wrap',
@@ -49,8 +67,8 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => {
       [theme.breakpoints.down('md')]: {
         flexDirection: 'column',
         '& > div': {
-          marginBottom: theme.spacing.unit * 2,
-        },
+          marginBottom: theme.spacing.unit * 2
+        }
       },
       '& > div': {
         display: 'flex',
@@ -60,39 +78,39 @@ const styles: StyleRulesCallback<ClassNames> = (theme) => {
           display: 'block',
           width: 20,
           height: 20,
-          marginRight: theme.spacing.unit,
-        },
-      },
+          marginRight: theme.spacing.unit
+        }
+      }
     },
     graphTitle: {
       marginRight: theme.spacing.unit * 2,
-      marginTop: theme.spacing.unit * 2,
+      marginTop: theme.spacing.unit * 2
     },
     graphControls: {
       margin: `${theme.spacing.unit * 2}px 0`,
       display: 'flex',
-      alignItems: 'center',
+      alignItems: 'center'
     },
     blue: {
       '&:before': {
-        backgroundColor: theme.palette.primary.main,
-      },
+        backgroundColor: theme.palette.primary.main
+      }
     },
     green: {
       '&:before': {
-        backgroundColor: theme.color.green,
-      },
+        backgroundColor: theme.color.green
+      }
     },
     red: {
       '&:before': {
-        backgroundColor: theme.color.red,
-      },
+        backgroundColor: theme.color.red
+      }
     },
     yellow: {
       '&:before': {
-        backgroundColor: theme.color.yellow,
-      },
-    },
+        backgroundColor: theme.color.yellow
+      }
+    }
   };
 };
 
@@ -111,6 +129,19 @@ type CombinedProps = Props & WithStyles<ClassNames>;
 
 const statsFetchInterval = 30000;
 
+const loading = () => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: 300
+    }}
+  >
+    <CircleProgress mini />
+  </div>
+);
+
 class TablesPanel extends React.Component<CombinedProps, State> {
   statsInterval?: number = undefined;
   mounted: boolean = false;
@@ -118,26 +149,29 @@ class TablesPanel extends React.Component<CombinedProps, State> {
   state: State = {
     stats: null,
     loadingStats: false,
-    openPanels: 0,
+    openPanels: 0
   };
 
   handleToggleExpand = (e: any, expanded: boolean) => {
     const { openPanels, stats } = this.state;
     if (expanded && !stats) {
       /* Only set loading state on initial load
-      *  so the graphs are not disrupted on future updates. */
+       *  so the graphs are not disrupted on future updates. */
       this.setState({ loadingStats: true });
       this.getStats();
     }
 
     if (expanded && openPanels <= 0) {
       /* We will regularly update the stats as long as at least one panel is open. */
-      this.statsInterval = window.setInterval(() => this.getStats(), statsFetchInterval);
+      this.statsInterval = window.setInterval(
+        () => this.getStats(),
+        statsFetchInterval
+      );
     }
 
     /* If the panel is opening, increment the number of open panels. Otherwise decrement.
-    *  This allows us to keep track of when all of the panels are closed.
-    */
+     *  This allows us to keep track of when all of the panels are closed.
+     */
     const updatedOpenPanels = expanded ? openPanels + 1 : openPanels - 1;
     this.setState({ openPanels: updatedOpenPanels });
 
@@ -145,21 +179,33 @@ class TablesPanel extends React.Component<CombinedProps, State> {
     if (!expanded && updatedOpenPanels <= 0) {
       window.clearInterval(this.statsInterval as number);
     }
-  }
+  };
 
   getStats = () => {
     const { nodeBalancer } = this.props;
     getNodeBalancerStats(nodeBalancer.id)
       .then((response: Linode.NodeBalancerStats) => {
-        if (!this.mounted) { return; }
-        this.setState({ stats: response, loadingStats: false, statsError: undefined });
+        if (!this.mounted) {
+          return;
+        }
+        this.setState({
+          stats: response,
+          loadingStats: false,
+          statsError: undefined
+        });
       })
-      .catch((errorResponse) => {
-        if (!this.mounted) { return; }
-        const statsError = pathOr("There was an error loading stats for this NodeBalancer.", ['reason'], errorResponse[0]);
+      .catch(errorResponse => {
+        if (!this.mounted) {
+          return;
+        }
+        const statsError = pathOr(
+          'There was an error loading stats for this NodeBalancer.',
+          ['reason'],
+          errorResponse[0]
+        );
         this.setState({ loadingStats: false, statsError });
       });
-  }
+  };
 
   componentDidMount() {
     this.mounted = true;
@@ -170,88 +216,96 @@ class TablesPanel extends React.Component<CombinedProps, State> {
     window.clearInterval(this.statsInterval as number);
   }
 
-  renderConnectionsChart = () => {
+  renderConnectionsChart = (
+    statsError: string | undefined,
+    loadingStats: boolean
+  ) => {
     const { classes } = this.props;
     const { stats } = this.state;
-    const data = pathOr([[]], ['data','connections'], stats);
+    const data = pathOr([[]], ['data', 'connections'], stats);
+
+    if (loadingStats) {
+      return loading();
+    }
+    if (statsError) {
+      return <ErrorState errorText={statsError} />;
+    }
     return (
       <React.Fragment>
-        <Typography
-          role="header"
-          variant="h3"
-          className={classes.header}
-        >
+        <Typography role="header" variant="h3" className={classes.header}>
           Connections (5 min avg.)
         </Typography>
         <React.Fragment>
           <div className={classes.chart}>
-            <div className={classes.leftLegend} style={{ left: -34, bottom: 60 }}>
+            <div
+              className={classes.leftLegend}
+              style={{ left: -34, bottom: 60 }}
+            >
               connections/sec
-              </div>
+            </div>
             <LineGraph
               showToday={true}
               data={[
                 {
                   label: 'Connections',
                   borderColor: '#990066',
-                  data,
-                },
+                  data
+                }
               ]}
             />
           </div>
         </React.Fragment>
       </React.Fragment>
-    )
-  }
+    );
+  };
 
-  renderTrafficChart = () => {
+  renderTrafficChart = (
+    statsError: string | undefined,
+    loadingStats: boolean
+  ) => {
     const { classes } = this.props;
     const { stats } = this.state;
     const trafficIn = pathOr([[]], ['data', 'traffic', 'in'], stats);
     const trafficOut = pathOr([[]], ['data', 'traffic', 'out'], stats);
 
+    if (loadingStats) {
+      return loading();
+    }
+    if (statsError) {
+      return <ErrorState errorText={statsError} />;
+    }
     return (
       <React.Fragment>
-        <Typography
-          role="header"
-          variant="h3"
-          className={classes.header}
-        >
+        <Typography role="header" variant="h3" className={classes.header}>
           Traffic (5 min avg.)
         </Typography>
         <React.Fragment>
           <div className={classes.chart}>
-            <div className={classes.leftLegend}>
-              bits/sec
-              </div>
+            <div className={classes.leftLegend}>bits/sec</div>
             <LineGraph
               showToday={true}
               data={[
                 {
                   label: 'Traffic In',
                   borderColor: '#3683dc',
-                  data: trafficIn,
+                  data: trafficIn
                 },
                 {
                   label: 'Traffic Out',
                   borderColor: '#01b159',
-                  data: trafficOut,
-                },
+                  data: trafficOut
+                }
               ]}
             />
           </div>
           <div className={classes.bottomLegend}>
-            <div className={classes.blue}>
-              Inbound
-              </div>
-            <div className={classes.green}>
-              Outbound
-              </div>
+            <div className={classes.blue}>Inbound</div>
+            <div className={classes.green}>Outbound</div>
           </div>
         </React.Fragment>
       </React.Fragment>
-    )
-  }
+    );
+  };
 
   render() {
     const { classes } = this.props;
@@ -260,25 +314,20 @@ class TablesPanel extends React.Component<CombinedProps, State> {
       <React.Fragment>
         <React.Fragment>
           <div className={classes.graphControls}>
-            <Typography role="header" variant="h2" className={classes.graphTitle}>Graphs</Typography>
+            <Typography
+              role="header"
+              variant="h2"
+              className={classes.graphTitle}
+            >
+              Graphs
+            </Typography>
           </div>
-
-          <ExtendedExpansionPanel
-            renderMainContent={this.renderConnectionsChart}
-            heading={"Connections"}
-            error={statsError}
-            loading={loadingStats}
-            onChange={this.handleToggleExpand}
-          />
-
-          <ExtendedExpansionPanel
-            className={classes.graphWrapper}
-            heading={"Traffic"}
-            renderMainContent={this.renderTrafficChart}
-            error={statsError}
-            loading={loadingStats}
-            onChange={this.handleToggleExpand}
-          />
+          <Paper className={classes.panel}>
+            {this.renderConnectionsChart(statsError, loadingStats)}
+          </Paper>
+          <Paper className={classes.panel}>
+            {this.renderTrafficChart(statsError, loadingStats)}
+          </Paper>
         </React.Fragment>
       </React.Fragment>
     );
