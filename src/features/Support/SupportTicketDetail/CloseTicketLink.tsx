de902be63475930330d7d816b54f7af053ef027d
Robert Stires <rstires@linode.com>
2019-01-29 14:21:31 -0500

    Support pagination of domain records.

--- src/features/Support/SupportTicketDetail/CloseTicketLink.tsx
+++ src/features/Support/SupportTicketDetail/CloseTicketLink.tsx
@@ -6,7 +6,7 @@ import ConfirmationDialog from 'src/components/ConfirmationDialog';
 import Typography from 'src/components/core/Typography';
 import Notice from 'src/components/Notice';
 import { closeSupportTicket } from 'src/services/support';
-import scrollToTop from 'src/utilities/scrollToTop';
+import scrollTo from 'src/utilities/scrollTo';
 
 interface Props {
   ticketId: number;
@@ -25,7 +25,7 @@ class CloseTicketLink extends React.Component<CombinedProps, State> {
   mounted: boolean = false;
   state: State = {
     dialogOpen: false,
-    isClosingTicket: false,
+    isClosingTicket: false
   };
 
   componentDidMount() {
@@ -37,41 +37,57 @@ class CloseTicketLink extends React.Component<CombinedProps, State> {
   }
 
   openConfirmationDialog = () => {
-    if (!this.mounted) { return; }
-    this.setState({ dialogOpen: true, isClosingTicket: false, ticketCloseError: undefined, });
-  }
+    if (!this.mounted) {
+      return;
+    }
+    this.setState({
+      dialogOpen: true,
+      isClosingTicket: false,
+      ticketCloseError: undefined
+    });
+  };
 
   closeConfirmationDialog = () => {
-    if (!this.mounted) { return; }
+    if (!this.mounted) {
+      return;
+    }
     this.setState({ dialogOpen: false });
-  }
+  };
 
   onClose = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     this.closeTicket();
-  }
+  };
 
   closeTicket = () => {
     const { closeTicketSuccess, ticketId } = this.props;
-    if (this.mounted) { this.setState({ isClosingTicket: true }); }
+    if (this.mounted) {
+      this.setState({ isClosingTicket: true });
+    }
     closeSupportTicket(ticketId)
       .then(() => {
-        if (this.mounted) { 
+        if (this.mounted) {
           this.setState({ isClosingTicket: false, dialogOpen: false });
-          scrollToTop();
+          scrollTo();
         }
         closeTicketSuccess();
-    })
-      .catch((errorResponse) => {
-        const defaultError = [{'reason': 'Your ticket could not be closed.'}];
-        const errors = pathOr(defaultError, ['response', 'data', 'errors'], errorResponse);
-        if (!this.mounted) { return; }
+      })
+      .catch(errorResponse => {
+        const defaultError = [{ reason: 'Your ticket could not be closed.' }];
+        const errors = pathOr(
+          defaultError,
+          ['response', 'data', 'errors'],
+          errorResponse
+        );
+        if (!this.mounted) {
+          return;
+        }
         this.setState({
           isClosingTicket: false,
-          ticketCloseError: errors[0].reason,
+          ticketCloseError: errors[0].reason
         });
-    })
-  }
+      });
+  };
 
   dialogActions = () => {
     return (
@@ -79,26 +95,32 @@ class CloseTicketLink extends React.Component<CombinedProps, State> {
         <Button
           type="cancel"
           onClick={this.closeConfirmationDialog}
-          data-qa-dialog-cancel>
+          data-qa-dialog-cancel
+        >
           Cancel
         </Button>
         <Button
           type="secondary"
           loading={this.state.isClosingTicket}
           onClick={this.onClose}
-          data-qa-dialog-submit>
+          data-qa-dialog-submit
+        >
           Confirm
         </Button>
       </ActionsPanel>
-    )
-  }
+    );
+  };
 
   render() {
     const { ticketCloseError } = this.state;
     return (
       <React.Fragment>
-        <Typography>{`If everything is resolved, you can `} 
-            <a onClick={this.openConfirmationDialog} data-qa-close-ticket-link >close this ticket</a>.
+        <Typography>
+          {`If everything is resolved, you can `}
+          <a onClick={this.openConfirmationDialog} data-qa-close-ticket-link>
+            close this ticket
+          </a>
+          .
         </Typography>
         <ConfirmationDialog
           open={this.state.dialogOpen}
@@ -106,7 +128,9 @@ class CloseTicketLink extends React.Component<CombinedProps, State> {
           onClose={this.closeConfirmationDialog}
           actions={this.dialogActions}
         >
-          {ticketCloseError && <Notice error text={ticketCloseError} data-qa-confirmation-error />}
+          {ticketCloseError && (
+            <Notice error text={ticketCloseError} data-qa-confirmation-error />
+          )}
           <Typography>
             {`Are you sure you want to close this ticket?`}
           </Typography>
@@ -115,6 +139,5 @@ class CloseTicketLink extends React.Component<CombinedProps, State> {
     );
   }
 }
-
 
 export default CloseTicketLink;
--- src/utilities/scrollToTop.ts
+++
@@ -1,9 +0,0 @@
-const scrollToTop = () => {
-  window.scroll({
-    behavior: 'smooth',
-    left: 0,
-    top: 0,
-  });
-}
-
-export default scrollToTop;
