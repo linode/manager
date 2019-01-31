3082eac04af37a033556c039947f71a60f93a457
Robert Stires <rstires@linode.com>
2019-01-31 13:47:44 -0500

    Add NodeBalancer context on detail.

--- src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/SummaryPanel.tsx
+++ src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/SummaryPanel.tsx
@@ -7,10 +7,12 @@ import {
   WithStyles
 } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
+import TagsPanel from 'src/components/TagsPanel';
 import styled, { StyleProps } from 'src/containers/SummaryPanels.styles';
 import IPAddress from 'src/features/linodes/LinodesLanding/IPAddress';
 import { formatRegion } from 'src/utilities';
 import { convertMegabytesTo } from 'src/utilities/convertMegabytesTo';
+import { NodeBalancerConsumer } from '../context';
 
 type ClassNames = 'IPgrouping' | 'nodeTransfer';
 
@@ -33,67 +35,76 @@ type CombinedProps = Props & StyleProps & WithStyles<ClassNames>;
 
 const SummaryPanel: React.StatelessComponent<CombinedProps> = props => {
   const { nodeBalancer, classes } = props;
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.summarySection}>
-        <Typography
-          role="header"
-          variant="h3"
-          className={classes.title}
-          data-qa-title
-        >
-          NodeBalancer Details
-        </Typography>
-        <div className={classes.section}>
-          <Typography variant="body1" data-qa-ports>
-            <strong>Ports: </strong> {nodeBalancer.ports.length === 0 && 'None'}
-            {nodeBalancer.ports.join(', ')}
-          </Typography>
-        </div>
-        <div className={classes.section}>
-          <Typography variant="body1" data-qa-node-status>
-            <strong>Node Status:</strong>{' '}
-            {`${nodeBalancer.up} up, ${nodeBalancer.down} down`}
-          </Typography>
-        </div>
-        <div className={classes.section}>
-          <Typography variant="body1" data-qa-transferred>
-            <strong>Transferred:</strong>{' '}
-            {convertMegabytesTo(nodeBalancer.transfer.total)}
-          </Typography>
-        </div>
-        <div className={classes.section}>
-          <Typography variant="body1" data-qa-hostname>
-            <strong>Host Name:</strong>
-            {nodeBalancer.hostname}
-          </Typography>
-        </div>
-        <div className={classes.section}>
-          <Typography variant="body1" data-qa-region>
-            <strong>Region:</strong> {formatRegion(nodeBalancer.region)}
-          </Typography>
-        </div>
-      </Paper>
 
-      <Paper className={classes.summarySection}>
-        <Typography
-          role="header"
-          variant="h3"
-          className={classes.title}
-          data-qa-title
-        >
-          IP Addresses
-        </Typography>
-        <div className={`${classes.section}`}>
-          <div className={classes.IPgrouping} data-qa-ip>
-            <IPAddress ips={[nodeBalancer.ipv4]} copyRight showMore />
-            {nodeBalancer.ipv6 && (
-              <IPAddress ips={[nodeBalancer.ipv6]} copyRight />
-            )}
+  return (
+    <NodeBalancerConsumer>
+      {({ updateTags }) => {
+        return (
+          <div className={classes.root}>
+            <Paper className={classes.summarySection}>
+              <Typography
+                role="header"
+                variant="h3"
+                className={classes.title}
+                data-qa-title
+              >
+                NodeBalancer Details
+              </Typography>
+              <TagsPanel tags={nodeBalancer.tags} updateTags={updateTags} />
+              <div className={classes.section}>
+                <Typography variant="body1" data-qa-ports>
+                  <strong>Ports: </strong>{' '}
+                  {nodeBalancer.ports.length === 0 && 'None'}
+                  {nodeBalancer.ports.join(', ')}
+                </Typography>
+              </div>
+              <div className={classes.section}>
+                <Typography variant="body1" data-qa-node-status>
+                  <strong>Node Status:</strong>{' '}
+                  {`${nodeBalancer.up} up, ${nodeBalancer.down} down`}
+                </Typography>
+              </div>
+              <div className={classes.section}>
+                <Typography variant="body1" data-qa-transferred>
+                  <strong>Transferred:</strong>{' '}
+                  {convertMegabytesTo(nodeBalancer.transfer.total)}
+                </Typography>
+              </div>
+              <div className={classes.section}>
+                <Typography variant="body1" data-qa-hostname>
+                  <strong>Host Name:</strong>
+                  {nodeBalancer.hostname}
+                </Typography>
+              </div>
+              <div className={classes.section}>
+                <Typography variant="body1" data-qa-region>
+                  <strong>Region:</strong> {formatRegion(nodeBalancer.region)}
+                </Typography>
+              </div>
+            </Paper>
+
+            <Paper className={classes.summarySection}>
+              <Typography
+                role="header"
+                variant="h3"
+                className={classes.title}
+                data-qa-title
+              >
+                IP Addresses
+              </Typography>
+              <div className={`${classes.section}`}>
+                <div className={classes.IPgrouping} data-qa-ip>
+                  <IPAddress ips={[nodeBalancer.ipv4]} copyRight showMore />
+                  {nodeBalancer.ipv6 && (
+                    <IPAddress ips={[nodeBalancer.ipv6]} copyRight />
+                  )}
+                </div>
+              </div>
+            </Paper>
           </div>
-        </div>
-      </Paper>
-    </div>
+        );
+      }}
+    </NodeBalancerConsumer>
   );
 };
 
