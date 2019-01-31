34b47bf0a2700ee878a6ce822bf8d18bf04a1779
Alban Bailly <abailly@linode.com>
2019-01-31 09:57:09 -0500

    summary panel styles container and nodebalancers summary styling

--- src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/NodeBalancerSummary.tsx
+++ src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/NodeBalancerSummary.tsx
@@ -5,16 +5,27 @@ import {
   WithStyles
 } from 'src/components/core/styles';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import Grid from 'src/components/Grid';
 import NodeBalancerCreationErrors, {
   ConfigOrNodeErrorResponse
 } from './NodeBalancerCreationErrors';
 import SummaryPanel from './SummaryPanel';
 import TablesPanel from './TablesPanel';
 
-type ClassNames = 'root';
+type ClassNames = 'root' | 'main' | 'sidebar';
 
 const styles: StyleRulesCallback<ClassNames> = theme => ({
-  root: {}
+  root: {},
+  main: {
+    [theme.breakpoints.up('md')]: {
+      order: 1
+    }
+  },
+  sidebar: {
+    [theme.breakpoints.up('md')]: {
+      order: 2
+    }
+  }
 });
 
 interface Props {
@@ -25,13 +36,19 @@ interface Props {
 type CombinedProps = Props & WithStyles<ClassNames>;
 
 const NodeBalancerSummary: React.StatelessComponent<CombinedProps> = props => {
-  const { nodeBalancer, errorResponses } = props;
+  const { nodeBalancer, errorResponses, classes } = props;
   return (
     <React.Fragment>
       <DocumentTitleSegment segment={`${nodeBalancer.label} - Summary`} />
       <NodeBalancerCreationErrors errors={errorResponses} />
-      <SummaryPanel nodeBalancer={nodeBalancer} />
-      <TablesPanel nodeBalancer={nodeBalancer} />
+      <Grid container>
+        <Grid item xs={12} md={3} className={classes.sidebar}>
+          <SummaryPanel nodeBalancer={nodeBalancer} />
+        </Grid>
+        <Grid item xs={12} md={9} className={classes.main}>
+          <TablesPanel nodeBalancer={nodeBalancer} />
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 };
--- src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/SummaryPanel.tsx
+++ src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerSummary/SummaryPanel.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { compose } from 'recompose';
 import Paper from 'src/components/core/Paper';
 import {
   StyleRulesCallback,
@@ -6,38 +7,18 @@ import {
   WithStyles
 } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
-import Grid from 'src/components/Grid';
+import styled, { StyleProps } from 'src/containers/SummaryPanels.styles';
 import IPAddress from 'src/features/linodes/LinodesLanding/IPAddress';
 import { formatRegion } from 'src/utilities';
 import { convertMegabytesTo } from 'src/utilities/convertMegabytesTo';
 
-type ClassNames =
-  | 'root'
-  | 'title'
-  | 'IPWrapper'
-  | 'IPgrouping'
-  | 'marginTop'
-  | 'nodeTransfer';
+type ClassNames = 'IPgrouping' | 'nodeTransfer';
 
 const styles: StyleRulesCallback<ClassNames> = theme => ({
-  root: {
-    padding: theme.spacing.unit * 2,
-    marginTop: theme.spacing.unit * 2
-  },
-  title: {
-    marginBottom: theme.spacing.unit * 2
-  },
-  IPWrapper: {
-    display: 'flex',
-    alignItems: 'flex-start'
-  },
   IPgrouping: {
     margin: '-2px 0 0 2px',
     display: 'flex',
     flexDirection: 'column'
-  },
-  marginTop: {
-    marginTop: theme.spacing.unit * 2
   },
   nodeTransfer: {
     marginTop: 12
@@ -48,75 +29,79 @@ interface Props {
   nodeBalancer: Linode.ExtendedNodeBalancer;
 }
 
-type CombinedProps = Props & WithStyles<ClassNames>;
+type CombinedProps = Props & StyleProps & WithStyles<ClassNames>;
 
 const SummaryPanel: React.StatelessComponent<CombinedProps> = props => {
   const { nodeBalancer, classes } = props;
   return (
-    <Paper className={classes.root}>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography
-            role="header"
-            variant="h1"
-            className={classes.title}
-            data-qa-title
-          >
-            Summary
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <div className={classes.IPWrapper}>
-            <Typography variant="body1">
-              <strong>IP:</strong>
-            </Typography>
-            <div className={classes.IPgrouping} data-qa-ip>
-              <IPAddress ips={[nodeBalancer.ipv4]} copyRight showMore />
-              {nodeBalancer.ipv6 && (
-                <IPAddress ips={[nodeBalancer.ipv6]} copyRight />
-              )}
-            </div>
-          </div>
-          <Typography
-            variant="body1"
-            data-qa-ports
-            className={classes.marginTop}
-          >
+    <div className={classes.root}>
+      <Paper className={classes.summarySection}>
+        <Typography
+          role="header"
+          variant="h3"
+          className={classes.title}
+          data-qa-title
+        >
+          NodeBalancer Details
+        </Typography>
+        <div className={classes.section}>
+          <Typography variant="body1" data-qa-ports>
             <strong>Ports: </strong> {nodeBalancer.ports.length === 0 && 'None'}
             {nodeBalancer.ports.join(', ')}
           </Typography>
-          <Grid container className={classes.nodeTransfer}>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1" data-qa-node-status>
-                <strong>Node Status:</strong>{' '}
-                {`${nodeBalancer.up} up, ${nodeBalancer.down} down`}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1" data-qa-transferred>
-                <strong>Transferred:</strong>{' '}
-                {convertMegabytesTo(nodeBalancer.transfer.total)}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="body1" data-qa-hostname>
-            <strong>Host Name:</strong> {nodeBalancer.hostname}
+        </div>
+        <div className={classes.section}>
+          <Typography variant="body1" data-qa-node-status>
+            <strong>Node Status:</strong>{' '}
+            {`${nodeBalancer.up} up, ${nodeBalancer.down} down`}
           </Typography>
-          <Typography
-            variant="body1"
-            data-qa-region
-            className={classes.marginTop}
-          >
+        </div>
+        <div className={classes.section}>
+          <Typography variant="body1" data-qa-transferred>
+            <strong>Transferred:</strong>{' '}
+            {convertMegabytesTo(nodeBalancer.transfer.total)}
+          </Typography>
+        </div>
+        <div className={classes.section}>
+          <Typography variant="body1" data-qa-hostname>
+            <strong>Host Name:</strong>
+            {nodeBalancer.hostname}
+          </Typography>
+        </div>
+        <div className={classes.section}>
+          <Typography variant="body1" data-qa-region>
             <strong>Region:</strong> {formatRegion(nodeBalancer.region)}
           </Typography>
-        </Grid>
-      </Grid>
-    </Paper>
+        </div>
+      </Paper>
+
+      <Paper className={classes.summarySection}>
+        <Typography
+          role="header"
+          variant="h3"
+          className={classes.title}
+          data-qa-title
+        >
+          IP Addresses
+        </Typography>
+        <div className={`${classes.section}`}>
+          <div className={classes.IPgrouping} data-qa-ip>
+            <IPAddress ips={[nodeBalancer.ipv4]} copyRight showMore />
+            {nodeBalancer.ipv6 && (
+              <IPAddress ips={[nodeBalancer.ipv6]} copyRight />
+            )}
+          </div>
+        </div>
+      </Paper>
+    </div>
   );
 };
 
-const styled = withStyles(styles);
+const localStyles = withStyles(styles);
 
-export default styled(SummaryPanel);
+const enhanced = compose<CombinedProps, Props>(
+  styled,
+  localStyles
+);
+
+export default enhanced(SummaryPanel);
