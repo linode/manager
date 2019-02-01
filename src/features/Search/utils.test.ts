60ed1b0f802e921ec20bbd7d850439cf7d1d036f
Russ Zhurba <rzhurba@linode.com>
2019-02-01 11:36:56 -0500

    Add getMatchingIps tests

--- src/features/Search/utils.test.ts
+++ src/features/Search/utils.test.ts
@@ -1,10 +1,27 @@
-import { filterMatched, getMatchingTags } from './utils';
+import { filterMatched, getMatchingIps, getMatchingTags } from './utils';
 
 describe('getMatchingTags method', () => {
   it('should find all lowercase and uppercase matches', () => {
     const query = 'tag';
     const tags = ['tag1', 'TAG2', 'Tag3', 'something else'];
     expect(getMatchingTags(tags, query)).toHaveLength(3);
+  });
+});
+
+describe('getMatchingIps method', () => {
+  it('should find all matching IPs', () => {
+    const query = '122.122';
+    const ips = [
+      '122.122.122.123',
+      '123.123.123.123',
+      '125.255.122.122',
+      '2342::2342:A234'
+    ];
+    expect(getMatchingIps(ips, query)).toHaveLength(2);
+  });
+  it('should return empty array if no IP is provided', () => {
+    const query = '122.122';
+    expect(getMatchingIps(undefined, query)).toHaveLength(0);
   });
 });
 
