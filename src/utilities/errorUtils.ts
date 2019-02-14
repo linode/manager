b31813fae959f12c81a4f60f8e64922462448a09
Jared Kobos <jaredkobos@gmail.com>
2019-02-14 14:51:11 -0500

    POC for error mapping function

--- src/utilities/errorUtils.ts
+++ src/utilities/errorUtils.ts
@@ -19,3 +19,50 @@ export const getErrorStringOrDefault = (
 ): string => {
   return pathOr<string>(defaultError, [0, 'reason'], errors);
 };
+
+/**
+ * Returns a mapping of error fields to responses. Intended for mapping
+ * API errors to input fields or other field-specific displays.
+ *
+ * Any errors which do not have a field, or do not match any specified field,
+ * will be assigned to the 'none' key. If there are multiple errors in this category,
+ * only one will be included in the map. This is not ideal, but the point of this
+ * function is to make sure that if a request returns an error, some usable feedback
+ * will be available to the user.
+ *
+ * @example getErrorMap(['label','password'], errors)
+ *
+ * {
+ *    'label': 'This label is too long.',
+ *    'password': 'Must contain special characters.',
+ *    'none': 'You forgot to check for region errors.'
+ * }
+ *
+ *
+ * @param fields optional list of fields to include in the response object
+ * @param errors an API error response
+ */
+export const getErrorMap = (
+  fields: string[] = [],
+  errors?: Linode.ApiFieldError[]
+): Record<string, string | undefined> => {
+  if (!errors) {
+    return {};
+  }
+  return errors.reduce(
+    (accum, thisError) => {
+      if (thisError.field && fields.includes(thisError.field)) {
+        return {
+          ...accum,
+          [thisError.field]: thisError.reason
+        };
+      } else {
+        return {
+          ...accum,
+          none: thisError.reason
+        };
+      }
+    },
+    { none: undefined }
+  );
+};
