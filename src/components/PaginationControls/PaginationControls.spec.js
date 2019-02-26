9dceecead5a4693fe141bedf6faf81ec24241bc0
Nicholas Wisnewski <nwisnewski@Nicholass-MacBook-Pro.local>
2019-02-26 13:45:25 -0500

    remove quotes on index

--- src/components/PaginationControls/PaginationControls.spec.js
+++ src/components/PaginationControls/PaginationControls.spec.js
@@ -22,25 +22,25 @@ describe('Pagination Controls Suite', () => {
     });
 
     it('The pagination control is disabled for the active page', () => {
-        expect(getPageControl('1').getAttribute('disabled')).not.toBeNull();
-        expect(getPageControl('2').getAttribute('disabled')).toBeNull();
-        getPageControl('2').click();
-        expect(getPageControl('2').getAttribute('disabled')).not.toBeNull();
-        expect(getPageControl('1').getAttribute('disabled')).toBeNull();
+        expect(getPageControl(1).getAttribute('disabled')).not.toBeNull();
+        expect(getPageControl(2).getAttribute('disabled')).toBeNull();
+        getPageControl(2).click();
+        expect(getPageControl(2).getAttribute('disabled')).not.toBeNull();
+        expect(getPageControl(1).getAttribute('disabled')).toBeNull();
     });
 
     it('The next and previous page button work as ', () => {
         $(previous).click();
-        expect(getPageControl('1').getAttribute('disabled')).not.toBeNull();
+        expect(getPageControl(1).getAttribute('disabled')).not.toBeNull();
         $(next).click();
-        expect(getPageControl('2').getAttribute('disabled')).not.toBeNull();
+        expect(getPageControl(2).getAttribute('disabled')).not.toBeNull();
     });
 
     it('There should be an ellipsis', () => {
         expect(ellipsis(6).isVisible()).toBe(true);
-        getPageControl('5').click();
+        getPageControl(5).click();
         expect(ellipsis(2).isVisible() && ellipsis(8).isVisible()).toBe(true);
-        getPageControl('10').click();
+        getPageControl(10).click();
         expect(ellipsis(2).isVisible()).toBe(true);
     });
 });
