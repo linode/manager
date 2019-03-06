d9926e066a6023678b885931db83a9d3d8fd8490
John Callahan <johnwcallahan@gmail.com>
2019-03-06 17:21:00 -0500

    M3 1127 Change: Rebuild from StackScript (#4606)
    * group images by 64-bit distros/older/images
    
    * reverting some changes to save for different PR
    
    * add rebuild from stackscript component
    
    * extract hook functionality into separate files
    
    * add basic tests
    
    * review feedback

---
+++ src/features/linodes/LinodesDetail/LinodeRebuild/RebuildFromStackScript.test.tsx
@@ -0,0 +1,51 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { images } from 'src/__data__/images';
+import { reactRouterProps } from 'src/__data__/reactRouterProps';
+import { RebuildFromStackScript } from './RebuildFromStackScript';
+
+describe('RebuildFromImage', () => {
+  const wrapper = shallow(
+    <RebuildFromStackScript
+      classes={{
+        root: '',
+        error: '',
+        emptyImagePanel: '',
+        emptyImagePanelText: ''
+      }}
+      linodeId={1234}
+      imagesData={images}
+      imagesLoading={false}
+      userSSHKeys={[]}
+      onPresentSnackbar={jest.fn()}
+      enqueueSnackbar={jest.fn()}
+      {...reactRouterProps}
+    />
+  );
+
+  it('renders without crashing', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('renders a SelectStackScript panel with images', () => {
+    expect(wrapper.find('[data-qa-select-stackscript]')).toHaveLength(1);
+  });
+
+  it('defaults the selectedImage to undefined', () => {
+    expect(
+      wrapper.find('[data-qa-select-stackscript]').prop('selectedId')
+    ).toBe(undefined);
+  });
+
+  it('renders an AccessPanel', () => {
+    expect(wrapper.find('[data-qa-access-panel]')).toHaveLength(1);
+  });
+
+  it('defaults the password to an empty string', () => {
+    expect(wrapper.find('[data-qa-access-panel]').prop('password')).toBe('');
+  });
+
+  it('renders an Rebuild Button', () => {
+    expect(wrapper.find('[data-qa-rebuild]')).toHaveLength(1);
+  });
+});
