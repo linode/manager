5f770095481c4c38b959753579cee827ec6d3024
Alban Bailly <abailly@linode.com>
2019-03-06 13:49:54 -0500

    implementation and styling

--- src/components/EnhancedSelect/components/MenuList.tsx
+++ src/components/EnhancedSelect/components/MenuList.tsx
@@ -1,10 +1,40 @@
+import {
+  StyleRulesCallback,
+  withStyles,
+  WithStyles
+} from '@material-ui/core/styles';
+import HelpOutline from '@material-ui/icons/HelpOutline';
 import * as React from 'react';
 import { components as reactSelectComponents } from 'react-select';
 import _MenuList, {
   MenuListComponentProps
 } from 'react-select/lib/components/Menu';
+import Typography from 'src/components/core/Typography';
 
-const Menu: React.StatelessComponent<MenuListComponentProps<any>> = props => {
+type ClassNames = 'guidance' | 'text' | 'helpIcon';
+
+const styles: StyleRulesCallback<ClassNames> = theme => ({
+  guidance: {
+    backgroundColor: theme.bg.offWhiteDT,
+    borderTop: `1px solid ${theme.palette.divider}`,
+    padding: theme.spacing.unit * 2
+  },
+  text: {
+    fontSize: '.8rem'
+  },
+  helpIcon: {
+    width: 16,
+    height: 16,
+    position: 'relative',
+    top: 3,
+    marginRight: theme.spacing.unit
+  }
+});
+
+type CombinedProps = MenuListComponentProps<any> & WithStyles<ClassNames>;
+
+const Menu: React.StatelessComponent<CombinedProps> = props => {
+  const { classes } = props;
   const { guidance } = props.selectProps;
 
   return (
@@ -12,9 +42,18 @@ const Menu: React.StatelessComponent<MenuListComponentProps<any>> = props => {
       <reactSelectComponents.MenuList {...props}>
         {props.children}
       </reactSelectComponents.MenuList>
-      {guidance !== undefined && <div>booyah</div>}
+      {guidance !== undefined && (
+        <div className={classes.guidance}>
+          <Typography className={classes.text}>
+            <HelpOutline className={classes.helpIcon} />
+            {guidance}
+          </Typography>
+        </div>
+      )}
     </React.Fragment>
   );
 };
 
-export default Menu;
+const styled = withStyles(styles);
+
+export default styled(Menu);
