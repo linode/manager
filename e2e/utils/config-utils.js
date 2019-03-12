2b33b566d194ed474f5290d27123a582d37d8dca
Patrick Thiel <pthiel@linode.com>
2019-03-11 21:54:50 -0400

    Refactor login helper

--- e2e/utils/config-utils.js
+++ e2e/utils/config-utils.js
@@ -42,69 +42,72 @@ exports.readToken = (username) => {
 * @returns {null} returns nothing
 */
 exports.login = (username, password, credFilePath) => {
-    let loginButton,letsGoButton;
 
     browser.url(constants.routes.linodes);
     try {
         browser.waitForVisible('#username', constants.wait.long);
     } catch (err) {
         console.log(browser.getSource());
-
     }
 
     browser.waitForVisible('#password', constants.wait.long);
     browser.trySetValue('#username', username);
     browser.trySetValue('#password', password);
-    loginButton = browser.getUrl().includes('dev') ? '.btn#submit' : '.btn-primary';
-    letsGoButton = browser.getUrl().includes('dev') ? '.btn#submit' : '[data-qa-welcome-button]';
-    browser.click(loginButton);
 
+    const loginButton = browser.getUrl().includes('dev') ? '.btn#submit' : '[data-qa-sign-in] input';
+    const letsGoButton = browser.getUrl().includes('dev') ? '.btn#submit' : '[data-qa-welcome-button]';
 
+    // Helper to check if on the Authorize 3rd Party App Income
     const isOauthAuthPage = () => {
-        /** 
+        /**
          * looking to determine if we're on the oauth/auth page
          */
-        return $('.oauthauthorize-page').isVisible();
+        try {
+            browser.waitForVisible('.oauthauthorize-page', constants.wait.short);
+            return true;
+        } catch (err) {
+            console.log('Not on the Oauth Page, continuing');
+            return false;
+        }
     }
 
-    const CSRFErrorExists = () => {
-        /** otherwise look for the CSRF error being on-screen */
-        return $('.form-warnings').isVisible() && $('.form-warnings').getText().includes('CSRF')
+    // Helper to check if CSRF error is displayed on the page
+    const csrfErrorExists = () => {
+        const sourceIncludesCSRF = browser.getSource().includes('CSRF');
+        return sourceIncludesCSRF;
     };
 
+    // Click the Login button
+    browser.click(loginButton);
 
-    try {
-        browser.waitUntil(function () {
-            if (isOauthAuthPage() && process.env.REACT_APP_APP_ROOT.includes('local')) {
-                console.log('attempting to click the oauth submit button')
-                $('.form-actions>.btn').click();
-            } else if (!isOauthAuthPage() && CSRFErrorExists()) {
-                console.log('attempting to login again')
-                browser.trySetValue('#password', password);
-                browser.trySetValue('#username', username);
-                $(loginButton).click();
-            }
-            /** we're either on the authorize page or we're on the manager home page */
-            return browser.isExisting('[data-qa-add-new-menu-button]');
-        }, constants.wait.normal);
-    } catch (err) {
-        console.log('failed to login!');
-        // if (!isOauthAuthPage() && CSRFErrorExists()) {
-        //     console.log($('.form-warnings').getText());
-        //     browser.trySetValue('#password', password);
-        //     browser.trySetValue('#username', username);
-        //     $(loginButton).click();
-        // }
+    const onOauthPage = isOauthAuthPage();
+    const csrfError = csrfErrorExists();
 
-        // /** click the submit button on the oauth/auth page if we're on the oauth page */
-        // if (isOAuthPage() && process.env.REACT_APP_APP_ROOT.includes('local')) {
-        //     if ($$('.oauthauthorize-page').length > 0 && browser.getUrl().includes('login')) {
-        //         $('.form-actions>.btn').click();
-        //     }
-        // }
+    if (onOauthPage || csrfError) {
+        // If on the authorize page, click the authorize button
+        if (onOauthPage) {
+            $('.form-actions>.btn').click();
+        }
+
+        // If still on the login page, check for a form error
+        if (csrfError) {
+            // Attempt to Login after encountering the CSRF Error
+            browser.trySetValue('#password', password);
+            browser.trySetValue('#username', username);
+            $(loginButton).click();
+        }
     }
 
+    // Wait for the add entity menu to exist
+    try {
+        browser.waitForExist('[data-qa-add-new-menu-button]', constants.wait.normal);
+    } catch (err) {
+        console.log('Add an entity menu failed to exist', 'Failed to login to the Manager for some reason.');
+        console.error(`Current URL is ${browser.getUrl()}`);
+        console.error(`Page source: \n ${browser.getSource()}`);
+    }
 
+    // Wait for the welcome modal to display, click it once it appears
     if (browser.waitForVisible('[role="dialog"]')) {
         browser.click(letsGoButton);
         browser.waitForVisible('[role="dialog"]', constants.wait.long, true)
