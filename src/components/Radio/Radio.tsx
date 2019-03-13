048e496964e38bb55ec11f4fbad986738a376acd
Alban Bailly <abailly@linode.com>
2019-03-13 15:21:32 -0400

    M3-2516 add aria labels to inputProps for textfields and radios

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -88,6 +88,11 @@ const LinodeRadioControl: React.StatelessComponent<FinalProps> = props => {
       icon={<RadioIcon />}
       checkedIcon={<RadioIconRadioed />}
       data-qa-radio={props.checked}
+      inputProps={
+        props.inputProps === undefined
+          ? { 'aria-label': props.name ? `${props.name}` : undefined }
+          : { ...props.inputProps }
+      }
     />
   );
 };
