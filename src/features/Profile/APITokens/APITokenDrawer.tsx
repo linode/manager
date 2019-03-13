efeebed09fa77f925a7a24502d22e1240f77c93a
Alban Bailly <abailly@linode.com>
2019-03-13 16:54:16 -0400

    make sure all radios inherit proper labeling

--- src/features/Profile/APITokens/APITokenDrawer.tsx
+++ src/features/Profile/APITokens/APITokenDrawer.tsx
@@ -248,6 +248,9 @@ export class APITokenDrawer extends React.Component<CombinedProps, State> {
                   value="0"
                   onChange={this.handleSelectAllScopes}
                   data-qa-perm-none-radio
+                  inputProps={{
+                    'aria-label': 'Select none for all'
+                  }}
                 />
               </TableCell>
               <TableCell
@@ -263,6 +266,9 @@ export class APITokenDrawer extends React.Component<CombinedProps, State> {
                   value="1"
                   onChange={this.handleSelectAllScopes}
                   data-qa-perm-read-radio
+                  inputProps={{
+                    'aria-label': 'Select read-only for all'
+                  }}
                 />
               </TableCell>
               <TableCell
@@ -278,6 +284,9 @@ export class APITokenDrawer extends React.Component<CombinedProps, State> {
                   value="2"
                   onChange={this.handleSelectAllScopes}
                   data-qa-perm-rw-radio
+                  inputProps={{
+                    'aria-label': 'Select read/write for all'
+                  }}
                 />
               </TableCell>
             </TableRow>
