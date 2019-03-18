efdf0188a8d927d746cab000dcd629fbf9d32c84
John Callahan <johnwcallahan@gmail.com>
2019-03-18 11:34:48 -0400

    add loading/error/empty states for buckets

--- src/features/ObjectStorage/ObjectStorageLanding.test.tsx
+++ src/features/ObjectStorage/ObjectStorageLanding.test.tsx
@@ -1,13 +1,18 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
+import { buckets } from 'src/__data__/buckets';
+// @todo: remove router import when bucket creation is supported
+import { reactRouterProps } from 'src/__data__/reactRouterProps';
 import { ObjectStorageLanding } from './ObjectStorageLanding';
 
 describe('ObjectStorageLanding', () => {
   const wrapper = shallow(
     <ObjectStorageLanding
       classes={{ root: '', title: '', titleWrapper: '' }}
-      bucketsData={[]}
+      bucketsData={buckets}
       bucketsLoading={false}
+      // @todo: remove router props when bucket creation is supported
+      {...reactRouterProps}
     />
   );
 
@@ -26,5 +31,23 @@ describe('ObjectStorageLanding', () => {
 
   it('renders an "OrderBy" component, ordered by label', () => {
     expect(wrapper.find('OrderBy').prop('orderBy')).toBe('label');
+  });
+
+  it('renders a loading state when the data is loading', () => {
+    wrapper.setProps({ bucketsLoading: true });
+    expect(wrapper.find('[data-qa-loading-state]')).toHaveLength(1);
+  });
+
+  it('renders an empty state when there is no data', () => {
+    wrapper.setProps({ bucketsData: [], bucketsLoading: false });
+    expect(wrapper.find('[data-qa-empty-state]')).toHaveLength(1);
+  });
+
+  it('renders an error state when there is an error', () => {
+    wrapper.setProps({
+      bucketsError: [{ reason: 'An error occurred.' }],
+      bucketsLoading: false
+    });
+    expect(wrapper.find('[data-qa-error-state]')).toHaveLength(1);
   });
 });
