bd755df9b76945a20e251c3ab05a497a138d3975
John Callahan <johnwcallahan@gmail.com>
2019-03-18 10:26:08 -0400

    M3-OBJ-54 Change: List buckets (#4665)
    * add buckets table components
    
    * clean
    
    * add key to mapped bucket rows
    
    * fix shadowed variable name

---
+++ src/features/ObjectStorage/ListBuckets.test.tsx
@@ -0,0 +1,38 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import { buckets } from 'src/__data__/buckets';
+import { ListBuckets } from './ListBuckets';
+
+describe('ListBuckets', () => {
+  const wrapper = shallow(
+    <ListBuckets
+      classes={{ root: '', label: '' }}
+      data={buckets}
+      orderBy="label"
+      order="asc"
+      handleOrderChange={jest.fn()}
+    />
+  );
+
+  const innerComponent = wrapper.dive();
+
+  it('renders without crashing', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('renders a "Name" column', () => {
+    expect(innerComponent.find('[data-qa-name]')).toHaveLength(1);
+  });
+  it('renders a "Size" column', () => {
+    expect(innerComponent.find('[data-qa-size]')).toHaveLength(1);
+  });
+  it('renders a "Region" column', () => {
+    expect(innerComponent.find('[data-qa-region]')).toHaveLength(1);
+  });
+  it('renders a "Created" column', () => {
+    expect(innerComponent.find('[data-qa-created]')).toHaveLength(1);
+  });
+  it('renders a RenderData component with the provided data', () => {
+    expect(innerComponent.find('RenderData').prop('data')).toEqual(buckets);
+  });
+});
