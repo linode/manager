229c543058337b91c6f96669f73c71c3c281cb58
Jared Kobos <jaredkobos@gmail.com>
2019-03-20 11:02:02 -0400

    remove links

--- src/features/Events/EventRow.tsx
+++ src/features/Events/EventRow.tsx
@@ -3,14 +3,12 @@ import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
-import HelpOutline from '@material-ui/icons/HelpOutline';
 import Hidden from 'src/components/core/Hidden';
 import {
   StyleRulesCallback,
   withStyles,
   WithStyles
 } from 'src/components/core/styles';
-import Tooltip from 'src/components/core/Tooltip';
 import Typography from 'src/components/core/Typography';
 import DateTimeDisplay from 'src/components/DateTimeDisplay';
 import EntityIcon from 'src/components/EntityIcon';
@@ -18,7 +16,6 @@ import renderGuard, { RenderGuardProps } from 'src/components/RenderGuard';
 import TableCell from 'src/components/TableCell';
 import TableRow from 'src/components/TableRow';
 import eventMessageGenerator from 'src/eventMessageGenerator';
-import getEventsActionLink from 'src/utilities/getEventsActionLink';
 
 import { getEntityByIDFromStore } from 'src/utilities/getEntityByIDFromStore';
 
@@ -47,16 +44,10 @@ export const EventRow: React.StatelessComponent<CombinedProps> = props => {
   const type = pathOr<string>('linode', ['entity', 'type'], event);
   const id = pathOr<string | number>(-1, ['entity', 'id'], event);
   const entity = getEntityByIDFromStore(type, id);
-  // Community likes and questions don't have an "entity" but they do have a link
-  const linkTarget =
-    ['community_like', 'community_question'].includes(type) || entity
-      ? getEventsActionLink(event.action, event.entity, false, (s: string) =>
-          props.history.push(s)
-        )
-      : undefined;
+
   const rowProps = {
     created: event.created,
-    linkTarget,
+    linkTarget: undefined, // no links for initial POC
     message: eventMessageGenerator(event),
     status: pathOr(undefined, ['status'], entity),
     type,
@@ -84,7 +75,7 @@ interface RowProps {
 const Row: React.StatelessComponent<
   RowProps & WithStyles<ClassNames>
 > = props => {
-  const { linkTarget, message, status, type, created, classes } = props;
+  const { linkTarget, message, status, type, created } = props;
 
   return (
     <TableRow rowLink={linkTarget}>
@@ -94,19 +85,7 @@ const Row: React.StatelessComponent<
         </TableCell>
       </Hidden>
       <TableCell parentColumn={'Event'} data-qa-event-message-cell compact>
-        {Boolean(!linkTarget) ? (
-          <div className={classes.tooltipWrapper}>
-            <Typography variant="body1">{message}</Typography>
-            <Tooltip
-              title="The entity for this event no longer exists."
-              placement="right"
-            >
-              <HelpOutline className={classes.helpIcon} />
-            </Tooltip>
-          </div>
-        ) : (
-          <Typography variant="body1">{message}</Typography>
-        )}
+        <Typography variant="body1">{message}</Typography>
       </TableCell>
       <TableCell parentColumn={'Time'} data-qa-event-created-cell compact>
         <DateTimeDisplay value={created} humanizeCutoff={'month'} />
