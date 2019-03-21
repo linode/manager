1c1ca59d3dfd914e1abbdbf527badb20e85da177
Alban Bailly <abailly@linode.com>
2019-03-21 09:46:06 -0400

    M3-2568 add missing typography for crash message

--- src/features/TheApplicationIsOnFire.tsx
+++ src/features/TheApplicationIsOnFire.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 import Dialog from 'src/components/core/Dialog';
 import DialogContent from 'src/components/core/DialogContent';
 import DialogTitle from 'src/components/core/DialogTitle';
+import Typography from 'src/components/core/Typography';
 
 const TheApplicationIsOnFire: React.StatelessComponent<{}> = props => {
   return (
     <Dialog open>
       <DialogTitle>Oh snap!</DialogTitle>
       <DialogContent>
-        Something went terribly wrong. Did you try {<ReloadLink />}?
+        <Typography>
+          Something went terribly wrong. Did you try {<ReloadLink />}?
+        </Typography>
       </DialogContent>
     </Dialog>
   );
