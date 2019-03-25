c693d6b82a8f3da47f3a9b41101a74f0c6efeafd
mmckenna <mmckenna.phila@gmail.com>
2019-03-25 09:33:13 -0400

    add unit tests

--- src/features/linodes/LinodesCreate/TabbedContent/formUtilities.ts
+++ src/features/linodes/LinodesCreate/TabbedContent/formUtilities.ts
@@ -9,7 +9,7 @@ export const filterPublicImages = (images: Linode.Image[]) => {
 };
 
 /**
- * filter out all the UDF errors from our error state.
+ * filter out all the API errors that aren't UDF errors from our error state.
  * To do this, we compare the keys from the error state to our "errorResources"
  * map and return all the errors that don't match the keys in that object
  *
