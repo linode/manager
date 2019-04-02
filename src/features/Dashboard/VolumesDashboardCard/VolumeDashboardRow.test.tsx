be43cfae8405081b3fcd2bbd4ef58adc7e53aed4
Jared Kobos <jaredkobos@gmail.com>
2019-04-02 16:32:54 -0400

    Fix tests

--- src/features/Dashboard/VolumesDashboardCard/VolumeDashboardRow.test.tsx
+++ src/features/Dashboard/VolumesDashboardCard/VolumeDashboardRow.test.tsx
@@ -4,16 +4,6 @@ import * as React from 'react';
 import { volumes } from 'src/__data__/volumes';
 import { VolumeDashboardRow } from './VolumeDashboardRow';
 
-const volumeWithLinodeLabel = {
-  ...volumes[2],
-  linodeLabel: 'thisLinode'
-};
-
-const unattachedVolume = {
-  ...volumes[0],
-  linodeLabel: ''
-};
-
 const classes = {
   root: '',
   icon: '',
@@ -26,7 +16,7 @@ const classes = {
 };
 const props = {
   classes,
-  volume: volumeWithLinodeLabel
+  volume: volumes[2]
 };
 
 const component = shallow(<VolumeDashboardRow {...props} />);
@@ -34,21 +24,13 @@ const component = shallow(<VolumeDashboardRow {...props} />);
 describe('Volumes dashboard card', () => {
   it.skip("should show the attached Linode's label for each Volume", () => {
     expect(
-      component
-        .find('[data-qa-attached-linode]')
-        .contains(volumeWithLinodeLabel.label)
+      component.find('[data-qa-attached-linode]').contains('Linode label') // @todo mock this
     ).toBeTruthy();
   });
 
   it('should show Unattached for each unattached Volume', () => {
-    const unattachedProps = { ...props, volume: unattachedVolume };
+    const unattachedProps = { ...props, volume: volumes[0] };
     const unattached = shallow(<VolumeDashboardRow {...unattachedProps} />);
-
-    expect(
-      unattached
-        .find('[data-qa-attached-linode]')
-        .contains(volumeWithLinodeLabel.label)
-    ).toBeFalsy();
     expect(
       unattached.find('[data-qa-attached-linode]').contains('Unattached')
     ).toBeTruthy();
