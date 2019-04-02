97f701dd371def9929ac2a9b0c31501c7edda9cf
Kayla Wilkins <kwilkins@linode.com>
2019-04-02 16:29:25 -0400

    having checkout bar stay fixed for md breakpoint up instead of only larger screens

--- src/features/linodes/LinodesCreate/TabbedContent/FromStackScriptContent.tsx
+++ src/features/linodes/LinodesCreate/TabbedContent/FromStackScriptContent.tsx
@@ -44,7 +44,7 @@ const styles: StyleRulesCallback<ClassNames> = theme => ({
   root: {},
   main: {},
   sidebar: {
-    [theme.breakpoints.up('lg')]: {
+    [theme.breakpoints.up('md')]: {
       marginTop: '-130px !important'
     }
   },
