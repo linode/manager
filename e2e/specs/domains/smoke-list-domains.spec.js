40f701969c0add29417201200f9cf07ec89ddf16
Kayla Wilkins <kwilkins@linode.com>
2019-04-03 16:51:19 -0400

    fixing test

--- e2e/specs/domains/smoke-list-domains.spec.js
+++ e2e/specs/domains/smoke-list-domains.spec.js
@@ -1,92 +1,97 @@
 const { constants } = require('../../constants');
-const { apiDeleteAllDomains } =  require('../../utils/common');
+const { apiDeleteAllDomains } = require('../../utils/common');
 
 import ListDomains from '../../pageobjects/list-domains.page';
 
 describe('Domains - List Suite', () => {
-    let domainId, domainElement;
-    const initialDomain = `a${new Date().getTime()}.com`;
-    const cloneDomain = `b${new Date().getTime()}.com`;
+  let domainId, domainElement;
+  const initialDomain = `a${new Date().getTime()}.com`;
+  const cloneDomain = `b${new Date().getTime()}.com`;
 
-    beforeAll(() => {
-        browser.url(constants.routes.domains);
-        ListDomains.globalCreate.waitForVisible(constants.wait.normal);
-        ListDomains.progressBar.waitForVisible(constants.wait.normal, true);
-    });
+  beforeAll(() => {
+    browser.url(constants.routes.domains);
+    ListDomains.globalCreate.waitForVisible(constants.wait.normal);
+    ListDomains.progressBar.waitForVisible(constants.wait.normal, true);
+  });
 
-    it('should display domains base elements', () => {
-        ListDomains.baseElemsDisplay(true);
-    });
+  it('should display domains base elements', () => {
+    ListDomains.baseElemsDisplay(true);
+  });
 
-    it('should create a domain', () => {
-        ListDomains.create(initialDomain,'foo@bar.com', true);
-    });
+  it('should create a domain', () => {
+    ListDomains.create(initialDomain, 'foo@bar.com', true);
+  });
 
-    it('should fail creating the same domain', () => {
-        try {
-            browser.url(constants.routes.domains);
-            ListDomains.baseElemsDisplay();
-            ListDomains.create(initialDomain,'foo@bar.com');
-        } catch (err) {
-            ListDomains.createDomainName.$('p').waitForVisible(constants.wait.normal);
-            ListDomains.cancel.click();
-            ListDomains.drawerTitle.waitForVisible(constants.wait.normal, true);
-        }
-    });
+  it('should fail creating the same domain', () => {
+    try {
+      browser.url(constants.routes.domains);
+      ListDomains.baseElemsDisplay();
+      ListDomains.create(initialDomain, 'foo@bar.com');
+    } catch (err) {
+      ListDomains.createDomainName.$('p').waitForVisible(constants.wait.normal);
+      ListDomains.cancel.click();
+      ListDomains.drawerTitle.waitForVisible(constants.wait.normal, true);
+    }
+  });
 
-    it('should display action menu options', () => {
-        browser.url(constants.routes.domains);
-        ListDomains.domainElem.waitForVisible(constants.wait.normal);
-        domainElement = `[data-qa-domain-cell="${initialDomain}"]`;
+  it('should display action menu options', () => {
+    browser.url(constants.routes.domains);
+    ListDomains.domainElem.waitForVisible(constants.wait.normal);
+    domainElement = `[data-qa-domain-cell="${initialDomain}"]`;
 
-        browser.jsClick(`${domainElement} [data-qa-action-menu]`);
+    browser.jsClick(`${domainElement} [data-qa-action-menu]`);
 
-        const expectedMenuItems = [
-            'Edit',
-            'Clone',
-            'Remove',
-            'Edit DNS Records',
-            'Check Zone',
-            'Zone File',
-        ];
+    const expectedMenuItems = [
+      'Edit',
+      'Clone',
+      'Delete',
+      'Edit DNS Records',
+      'Check Zone',
+      'Zone File'
+    ];
 
-        ListDomains.actionMenuItem.waitForVisible(constants.wait.normal);
-        const actionMenuItems = $$(ListDomains.actionMenuItem.selector);
-        actionMenuItems.forEach(i => expect(expectedMenuItems).toContain(i.getText()));
+    ListDomains.actionMenuItem.waitForVisible(constants.wait.normal);
+    const actionMenuItems = $$(ListDomains.actionMenuItem.selector);
+    actionMenuItems.forEach(i =>
+      expect(expectedMenuItems).toContain(i.getText())
+    );
 
-        browser.click('body');
-        ListDomains.actionMenuItem.waitForVisible(constants.wait.short, true);
-    });
+    browser.click('body');
+    ListDomains.actionMenuItem.waitForVisible(constants.wait.short, true);
+  });
 
-    it('should display clone domain drawer', () => {
-        ListDomains.selectActionMenuItemV2(domainElement, 'Clone');
-        ListDomains.cloneDrawerElemsDisplay();
+  it('should display clone domain drawer', () => {
+    ListDomains.selectActionMenuItemV2(domainElement, 'Clone');
+    ListDomains.cloneDrawerElemsDisplay();
 
-        ListDomains.closeDrawer();
-    });
+    ListDomains.closeDrawer();
+  });
 
-    it('should fail to clone with the same domain name', () => {
-        ListDomains.selectActionMenuItemV2(domainElement, 'Clone');
-        ListDomains.cloneDrawerElemsDisplay();
+  it('should fail to clone with the same domain name', () => {
+    ListDomains.selectActionMenuItemV2(domainElement, 'Clone');
+    ListDomains.cloneDrawerElemsDisplay();
 
-        browser.trySetValue(`${ListDomains.cloneDomainName.selector} input`, initialDomain);
+    browser.trySetValue(
+      `${ListDomains.cloneDomainName.selector} input`,
+      initialDomain
+    );
 
-        ListDomains.submit.click();
-        ListDomains.cloneDomainName.$('p').waitForVisible(constants.wait.normal);
-        ListDomains.closeDrawer();
-    });
+    ListDomains.submit.click();
+    ListDomains.cloneDomainName.$('p').waitForVisible(constants.wait.normal);
+    ListDomains.closeDrawer();
+  });
 
-    it('should clone domain', () => {
-        ListDomains.selectActionMenuItemV2(domainElement, 'Clone');
-        ListDomains.clone(cloneDomain);
-    });
+  it('should clone domain', () => {
+    ListDomains.selectActionMenuItemV2(domainElement, 'Clone');
+    ListDomains.clone(cloneDomain);
+  });
 
-    it('should remove domain', () => {
-        ListDomains.selectActionMenuItemV2(domainElement, 'Remove');
-        ListDomains.remove(initialDomain);
-    });
+  it('should delete domain', () => {
+    ListDomains.selectActionMenuItemV2(domainElement, 'Delete');
+    ListDomains.remove(initialDomain);
+  });
 
-    afterAll(() => {
-        apiDeleteAllDomains();
-    });
+  afterAll(() => {
+    apiDeleteAllDomains();
+  });
 });
