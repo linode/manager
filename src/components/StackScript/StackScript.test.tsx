dcb16024a28a02f6ea569bcef713be058c206bef
Jared Kobos <jaredkobos@gmail.com>
2019-04-05 16:30:08 -0400

    Sequence -> ordinal

--- src/components/StackScript/StackScript.test.tsx
+++ src/components/StackScript/StackScript.test.tsx
@@ -16,7 +16,7 @@ describe('StackScript', () => {
           user_defined_fields: [],
           is_public: true,
           user_gravatar_id: 'ead4da00f4fe6a4bd0b4f11a510c031d',
-          username: 'Liniode',
+          username: 'Linode',
           deployments_total: 2,
           deployments_active: 1,
           description: 'Some test script for fun',
@@ -24,7 +24,7 @@ describe('StackScript', () => {
           created: '2010-12-31T23:59:58',
           updated: '2010-12-31T23:59:59',
           rev_note: 'Initial import.',
-          sequence: 1, // default value
+          ordinal: 1, // default value
           logo_url: '' // default value
         }}
         classes={{
