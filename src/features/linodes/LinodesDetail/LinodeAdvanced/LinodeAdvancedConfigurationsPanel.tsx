92651d48af09a38124ca8eb1b045451fda920ded
Alban Bailly <abailly@linode.com>
2019-04-10 13:31:20 -0400

    place disk spaces in sidebar

--- src/features/linodes/LinodesDetail/LinodeAdvanced/LinodeAdvancedConfigurationsPanel.tsx
+++ src/features/linodes/LinodesDetail/LinodeAdvanced/LinodeAdvancedConfigurationsPanel.tsx
@@ -7,12 +7,13 @@ import {
   WithStyles
 } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
+import Grid from 'src/components/Grid';
 import { withLinodeDetailContext } from 'src/features/linodes/LinodesDetail/linodeDetailContext';
 import LinodeConfigs from './LinodeConfigs';
 import LinodeDisks from './LinodeDisks';
 import LinodeDiskSpace from './LinodeDiskSpace';
 
-type ClassNames = 'root' | 'title' | 'paper';
+type ClassNames = 'root' | 'title' | 'paper' | 'main' | 'sidebar';
 
 const styles: StyleRulesCallback<ClassNames> = theme => ({
   root: {},
@@ -23,6 +24,13 @@ const styles: StyleRulesCallback<ClassNames> = theme => ({
     padding: theme.spacing.unit * 3,
     paddingTop: theme.spacing.unit,
     marginBottom: theme.spacing.unit * 3
+  },
+  main: {},
+  sidebar: {
+    marginTop: -theme.spacing.unit * 2,
+    [theme.breakpoints.up('md')]: {
+      marginTop: theme.spacing.unit * 2 + 24
+    }
   }
 });
 
@@ -49,24 +57,28 @@ class LinodeAdvancedConfigurationsPanel extends React.Component<
     const { loaded } = this.state;
 
     return (
-      <React.Fragment>
-        <Typography variant="h2" className={classes.title}>
-          Advanced Configurations
-        </Typography>
-        <Paper className={classes.paper}>
-          <LinodeConfigs active={loaded} />
-        </Paper>
-        <Paper className={classes.paper}>
-          <LinodeDisks active={loaded} />
-        </Paper>
-        <Paper className={classes.paper}>
-          <LinodeDiskSpace
-            disks={disks}
-            loading={!loaded}
-            totalDiskSpace={linodeTotalDisk}
-          />
-        </Paper>
-      </React.Fragment>
+      <Grid container>
+        <Grid item xs={12} md={7} lg={9} className={classes.main}>
+          <Typography variant="h2" className={classes.title}>
+            Advanced Configurations
+          </Typography>
+          <Paper className={classes.paper}>
+            <LinodeConfigs active={loaded} />
+          </Paper>
+          <Paper className={classes.paper}>
+            <LinodeDisks active={loaded} />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={5} lg={3} className={classes.sidebar}>
+          <Paper className={classes.paper}>
+            <LinodeDiskSpace
+              disks={disks}
+              loading={!loaded}
+              totalDiskSpace={linodeTotalDisk}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
     );
   }
 }
