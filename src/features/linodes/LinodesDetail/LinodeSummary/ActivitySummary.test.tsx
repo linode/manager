fc669395013b3394e02f5e9fcee81fe21f4f90e4
Jared Kobos <jaredkobos@gmail.com>
2019-04-12 12:07:35 -0700

    Add tests for Activity Summary

---
+++ src/features/linodes/LinodesDetail/LinodeSummary/ActivitySummary.test.tsx
@@ -0,0 +1,34 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+const requests = require.requireMock('src/services/account');
+
+requests.getEvents = jest.fn(() => Promise.resolve([]));
+
+jest.mock('src/services/account', () => ({
+  getEvents: () => jest.fn()
+}));
+
+import { ActivitySummary } from './ActivitySummary';
+
+const props = {
+  linodeId: 123456,
+  classes: {
+    root: '',
+    header: ''
+  }
+};
+const component = shallow(<ActivitySummary {...props} />);
+
+describe('ActivitySummary component', () => {
+  it('should render', () => {
+    expect(component).toHaveLength(1);
+  });
+
+  it("should request the Linode's events on load", () => {
+    expect(requests.getEvents).toHaveBeenCalledWith(
+      {},
+      { 'entity.id': 123456, 'entity.type': 'linode' }
+    );
+  });
+});
