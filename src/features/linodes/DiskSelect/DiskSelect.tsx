b7cb822b24860856fd75149307cf54b70a968ad7
Jared Kobos <jaredkobos@gmail.com>
2019-04-16 15:53:24 -0400

    Fixes

--- src/features/linodes/DiskSelect/DiskSelect.tsx
+++ src/features/linodes/DiskSelect/DiskSelect.tsx
@@ -1,12 +1,12 @@
-import { compose } from 'ramda';
 import * as React from 'react';
+import { compose } from 'recompose';
 import {
   StyleRulesCallback,
   WithStyles,
   withStyles
 } from 'src/components/core/styles';
 import EnhancedSelect, { Item } from 'src/components/EnhancedSelect/Select';
-import RenderGuard from 'src/components/RenderGuard';
+import RenderGuard, { RenderGuardProps } from 'src/components/RenderGuard';
 
 type ClassNames = 'root';
 
@@ -29,6 +29,16 @@ const disksToOptions = (disks: Linode.Disk[]): Item<string>[] => {
   return disks.map(disk => ({ label: disk.label, value: String(disk.id) }));
 };
 
+const diskFromValue = (
+  disks: Item<string>[],
+  diskId?: string
+): Item<string> | undefined => {
+  if (!diskId) {
+    return;
+  }
+  return disks.find(thisDisk => thisDisk.value === diskId);
+};
+
 const DiskSelect: React.StatelessComponent<CombinedProps> = props => {
   const {
     disabled,
@@ -45,7 +55,7 @@ const DiskSelect: React.StatelessComponent<CombinedProps> = props => {
       placeholder={'Select a Disk'}
       disabled={disabled}
       options={options}
-      value={selectedDisk}
+      value={diskFromValue(options, selectedDisk)}
       onChange={(newDisk: Item<string>) => handleChange(newDisk.value)}
       errorText={generalError || diskError}
     />
@@ -54,7 +64,7 @@ const DiskSelect: React.StatelessComponent<CombinedProps> = props => {
 
 const styled = withStyles(styles);
 
-export default compose<any, any, any>(
+export default compose<CombinedProps, Props & RenderGuardProps>(
   styled,
   RenderGuard
 )(DiskSelect);
