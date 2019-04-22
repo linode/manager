316da5c86926df50aa6a4a1361e20655e2302b17
mmckenna <mmckenna.phila@gmail.com>
2019-04-22 09:50:40 -0400

    add tooltip for error state

--- src/components/PrimaryNav/AdditionalMenuItems.tsx
+++ src/components/PrimaryNav/AdditionalMenuItems.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import {
   StyleRulesCallback,
   withStyles,
   WithStyles
 } from '@material-ui/core/styles';
-import Divider from 'src/components/core/Divider';
-import ListItem from 'src/components/core/ListItem';
-import ListItemText from 'src/components/core/ListItemText';
+
+import { sendEvent } from 'src/utilities/analytics';
+import NavItem, { PrimaryLink } from './NavItem';
 
 type ClassNames = 'root';
 
@@ -18,119 +18,75 @@ const styles: StyleRulesCallback<ClassNames> = theme => ({
 
 interface Props {
   closeMenu: () => void;
-  navigate: (href: string) => void;
   linkClasses: (href?: string) => string;
   listItemClasses: string;
   dividerClasses: string;
 }
 
-interface State {
-  adaError?: string;
-}
-
 type CombinedProps = Props & WithStyles<ClassNames>;
 
-interface PrimaryLink {
-  href?: string;
-  onClick?: () => void;
-  key: string;
-  display: string;
-  logo?: React.ComponentType<any>;
-}
+const AdditionalMenuItems: React.FC<CombinedProps> = props => {
+  const [adaError, setAdaError] = React.useState<string>('');
 
-class AdditionalMenuItems extends React.PureComponent<CombinedProps, State> {
-  state: State = {};
-
-  componentDidMount() {
+  React.useEffect(() => {
     /*
      * Init Ada Chaperone chat app
      * Script is included in index.html
      */
     if ('AdaChaperone' in window) {
-      this.ada = new (window as any).AdaChaperone('linode');
+      ada = new (window as any).AdaChaperone('linode');
+    } else {
+      setAdaError(
+        'There was an issue loading the chat bot. Please try again later.'
+      );
     }
-  }
+  });
 
-  handleAdaInit = () => {
+  const handleAdaInit = () => {
     /*
      * Show the Ada chat
      */
-    if (typeof this.ada === 'undefined') {
-      this.setState({
-        adaError:
-          'There was an issue loading the chat at this time. Please try again later.'
-      });
+
+    if (typeof ada === 'undefined') {
       return;
     }
-    this.setState({ adaError: '' });
-    this.ada.show();
+
+    setAdaError('');
+    sendEvent({
+      category: 'Chat Bot',
+      action: 'Open',
+      label: location.pathname
+    });
+    ada.show();
   };
 
-  links: PrimaryLink[] = [
+  const links: PrimaryLink[] = [
     { display: 'Get Help', href: '/support', key: 'help' },
-    { display: 'Chat', key: 'chat', onClick: this.handleAdaInit }
+    {
+      display: 'Chat',
+      key: 'chat',
+      onClick: handleAdaInit,
+      isDisabled: () => adaError
+    }
   ];
 
   /** ada chat bot */
-  ada: any = undefined;
+  let ada: any;
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.links.map(eachLink => {
-          if (!eachLink.onClick && !eachLink.href) {
-            throw new Error(
-              'A Primary Link needs either an href or an onClick prop'
-            );
-          }
+  const { classes, ...restOfProps } = props;
 
-          /* 
-           href takes priority here. So if an href and onClick 
-           so if an href and onClick is provided, the onClick will not be applied
-          */
-          return (
-            <React.Fragment key={eachLink.key}>
-              {eachLink.href ? (
-                <Link
-                  role="menuitem"
-                  to={eachLink.href}
-                  onClick={this.props.closeMenu}
-                  data-qa-nav-item={eachLink.key}
-                  className={this.props.linkClasses(eachLink.href)}
-                >
-                  <ListItemText
-                    primary={eachLink.display}
-                    disableTypography={true}
-                    className={this.props.listItemClasses}
-                  />
-                </Link>
-              ) : (
-                <ListItem
-                  role="menuitem"
-                  onClick={() => {
-                    this.props.closeMenu();
-                    /** disregarding undefined is fine here becasue of the error thrown above */
-                    eachLink.onClick!();
-                  }}
-                  data-qa-nav-item={eachLink.key}
-                  className={this.props.linkClasses()}
-                >
-                  <ListItemText
-                    primary={eachLink.display}
-                    disableTypography={true}
-                    className={this.props.listItemClasses}
-                  />
-                </ListItem>
-              )}
-              <Divider className={this.props.dividerClasses} />
-            </React.Fragment>
-          );
-        })}
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      {links.map(eachLink => {
+        return <NavItem {...eachLink} {...restOfProps} key={eachLink.key} />;
+      })}
+    </React.Fragment>
+  );
+};
 
 const styled = withStyles(styles);
 
-export default styled(AdditionalMenuItems);
+export default compose<CombinedProps, Props>(
+  styled,
+  React.memo
+)(AdditionalMenuItems);
---
+++ src/components/core/Popper.ts
@@ -0,0 +1,6 @@
+import Popper, { PopperProps as _PopperProps } from '@material-ui/core/Popper';
+
+/* tslint:disable-next-line:no-empty-interface */
+export interface PopperProps extends _PopperProps {}
+
+export default Popper;
