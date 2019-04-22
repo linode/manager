3fa83f0a078a39951ae733047e7c53294b6a2d01
Jared Kobos <jaredkobos@gmail.com>
2019-04-22 17:40:58 -0400

    Using the isLoggedInAsCustomer value from Redux, set the timezone (also in Redux) to the browser's timezone when logged in as customer.

--- src/store/profile/profile.requests.ts
+++ src/store/profile/profile.requests.ts
@@ -1,4 +1,6 @@
+import { pathOr } from 'ramda';
 import { getMyGrants, getProfile } from 'src/services/profile';
+import { ApplicationState } from 'src/store';
 import { ThunkActionCreator } from 'src/store/types';
 import { getProfileActions } from './profile.actions';
 
@@ -12,15 +14,33 @@ const maybeRequestGrants: (
   return getMyGrants().then(grants => ({ ...profile, grants }));
 };
 
+export const getTimezone = (state: ApplicationState, timezone: string) => {
+  const isLoggedInAsCustomer = pathOr(
+    false,
+    ['authentication', 'loggedInAsCustomer'],
+    state
+  );
+  /** If logged in as customer (from Admin), use the browser's local time instead of the
+   *  value in profile. Otherwise, use the user's chosen value.
+   */
+  return isLoggedInAsCustomer
+    ? Intl.DateTimeFormat().resolvedOptions().timeZone
+    : timezone;
+};
+
 export const requestProfile: ThunkActionCreator<
   Promise<Linode.Profile>
-> = () => dispatch => {
+> = () => (dispatch, getState) => {
   const { started, done, failed } = getProfileActions;
 
   dispatch(started());
 
   return getProfile()
     .then(response => response.data)
+    .then(profile => ({
+      ...profile,
+      timezone: getTimezone(getState(), profile.timezone)
+    }))
     .then(maybeRequestGrants)
     .then(response => {
       dispatch(done({ result: response }));
