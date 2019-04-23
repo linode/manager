6dd983793bcedc780baecfa044a7850ff538a81f
John Callahan <johnwcallahan@gmail.com>
2019-04-23 10:17:29 -0400

    M3-2704 change: Validate presence of images during StackScript update (#4850)
    * make images required in updateStackScriptSchema
    
    * validate in the component instead of in the services library
    
    * review feedback

--- src/services/stackscripts/stackscripts.schema.ts
+++ src/services/stackscripts/stackscripts.schema.ts
@@ -19,7 +19,9 @@ export const updateStackScriptSchema = object({
   label: string()
     .min(3, 'Label must be between 3 and 128 characters.')
     .max(128, 'Label must be between 3 and 128 characters.'),
-  images: array().of(string()),
+  images: array()
+    .of(string())
+    .min(1, 'An image is required.'),
   description: string(),
   is_public: boolean(),
   rev_note: string()
