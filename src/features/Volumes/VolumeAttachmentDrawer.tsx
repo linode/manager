9bfe2d4d2d5f388e7773d1e8cec846599fb3a07e
Kayla Wilkins <kwilkins@linode.com>
2019-04-29 17:41:14 -0400

    updates for feedback

--- src/features/Volumes/VolumeAttachmentDrawer.tsx
+++ src/features/Volumes/VolumeAttachmentDrawer.tsx
@@ -174,22 +174,20 @@ class VolumeAttachmentDrawer extends React.Component<CombinedProps, State> {
             important
           />
         )}
-        <FormControl fullWidth>
-          <LinodeSelect
-            selectedLinode={selectedLinode}
-            region={linodeRegion}
-            handleChange={this.changeSelectedLinode}
-            linodeError={linodeError}
-            generalError={generalError}
-            disabled={disabled || readOnly}
-          />
-          {!linodeError ||
-            (!generalError && (
-              <FormHelperText>
-                Only Linodes in this volume's region are displayed.
-              </FormHelperText>
-            ))}
-        </FormControl>
+        <LinodeSelect
+          selectedLinode={selectedLinode}
+          region={linodeRegion}
+          handleChange={this.changeSelectedLinode}
+          linodeError={linodeError}
+          generalError={generalError}
+          disabled={disabled || readOnly}
+        />
+        {!linodeError ||
+          (!generalError && (
+            <FormHelperText>
+              Only Linodes in this Volume's region are displayed.
+            </FormHelperText>
+          ))}
 
         {/* Config Selection */}
         {configs.length > 1 && (
