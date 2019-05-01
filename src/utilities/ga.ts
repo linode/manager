476a0e139fe264a4ac8becb65a98a0be15ef89cf
Kayla Wilkins <kwilkins@linode.com>
2019-05-01 15:43:35 -0400

    combining analytics and ga files into one, updating test ref

--- src/utilities/ga.ts
+++ src/utilities/ga.ts
@@ -1,4 +1,18 @@
-import { sendEvent } from './analytics';
+import { event } from 'react-ga';
+
+interface AnalyticsEvent {
+  category: string;
+  action: string;
+  label?: string;
+  value?: number;
+}
+
+/*
+ * Will throw error unless analytics is initialized
+ */
+export const sendEvent = (eventPayload: AnalyticsEvent) => {
+  event(eventPayload);
+};
 
 // LinodeActionMenu.tsx
 export const sendLinodeActionEvent = () => {
