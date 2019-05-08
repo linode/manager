36b5d27dd19c5d151cedcaaa4063c67f05005e6e
Jared Kobos <jaredkobos@gmail.com>
2019-05-08 16:44:20 -0400

    Add integration test

--- src/features/Kubernetes/ClusterList/ClusterActionMenu.test.tsx
+++ src/features/Kubernetes/ClusterList/ClusterActionMenu.test.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
-import { cleanup, render } from 'react-testing-library';
+import { cleanup, fireEvent, render } from 'react-testing-library';
 import { ClusterActionMenu } from './ClusterActionMenu';
+const requests = require.requireMock('../../../services/kubernetes');
 
 import { includesActions, wrapWithTheme } from 'src/utilities/testHelpers';
 
 jest.mock('src/components/ActionMenu/ActionMenu');
+jest.mock('src/services/kubernetes', () => ({
+  getKubeConfig: () => jest.fn()
+}));
+
+requests.getKubeConfig = jest
+  .fn()
+  .mockResolvedValueOnce({ kubeconfig: 'SSBhbSBhIHRlYXBvdA==' });
 
 const props = {
   clusterId: '123456',
@@ -20,5 +28,13 @@ describe('Kubernetes cluster action menu', () => {
       wrapWithTheme(<ClusterActionMenu {...props} />)
     );
     includesActions(['Download kubeconfig'], queryByText);
+  });
+
+  it('should query the API for a config file when Download kubeconfig is clicked', () => {
+    const { getByText } = render(
+      wrapWithTheme(<ClusterActionMenu {...props} />)
+    );
+    fireEvent.click(getByText(/download/i));
+    expect(requests.getKubeConfig).toHaveBeenCalledWith('123456');
   });
 });
