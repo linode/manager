14244c1048f9fe4a3976d09e5bde111de6a4cf28
mmckenna <mmckenna.phila@gmail.com>
2019-05-09 17:14:56 -0400

    adds preview tab to the ticket reply

--- src/features/Support/SupportTicketDetail/TabbedReply/PreviewReply.tsx
+++ src/features/Support/SupportTicketDetail/TabbedReply/PreviewReply.tsx
@@ -5,22 +5,50 @@ import {
 } from '@material-ui/core/styles';
 import * as React from 'react';
 import { compose } from 'recompose';
+import { Converter } from 'showdown';
+import 'showdown-highlightjs-extension';
+
+import Paper from 'src/components/core/Paper';
+
+// import { sanitizeHTML } from 'src/utilities/sanitize-html';
 
 type ClassNames = 'root';
 
 const styles: StyleRulesCallback<ClassNames> = theme => ({
-  root: {}
+  root: {
+    border: '1px solid #ccc',
+    minHeight: '140px',
+    marginTop: theme.spacing.unit * 2,
+    padding: `9px 12px 9px 12px`
+  }
 });
 
-type CombinedProps = WithStyles<ClassNames>;
+interface Props {
+  value: string;
+}
+
+type CombinedProps = WithStyles<ClassNames> & Props;
 
 const PreviewReply: React.FC<CombinedProps> = props => {
-  return <div>hello world</div>;
+  const { classes, value } = props;
+
+  const markupToMarkdown = new Converter({
+    extensions: ['highlightjs']
+  }).makeHtml(value);
+
+  return (
+    <Paper
+      className={classes.root}
+      dangerouslySetInnerHTML={{
+        __html: markupToMarkdown
+      }}
+    />
+  );
 };
 
 const styled = withStyles(styles);
 
-export default compose<CombinedProps, {}>(
+export default compose<CombinedProps, Props>(
   styled,
   React.memo
 )(PreviewReply);
