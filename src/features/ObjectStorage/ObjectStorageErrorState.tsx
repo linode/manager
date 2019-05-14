b6d92860f96a4976a02f5393321a544f8f60a770
John Callahan <johnwcallahan@gmail.com>
2019-05-14 13:39:53 -0400

    Use React.FC instead of StatelessComponent

--- src/features/ObjectStorage/ObjectStorageErrorState.tsx
+++ src/features/ObjectStorage/ObjectStorageErrorState.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ErrorState from 'src/components/ErrorState';
 
-export const ObjectStorageErrorState: React.StatelessComponent<{}> = () => {
+export const ObjectStorageErrorState: React.FC<{}> = () => {
   return (
     <ErrorState errorText="An error has occurred. Please reload and try again." />
   );
--- src/features/ObjectStorage/ObjectStorageLoadingState.tsx
+++ src/features/ObjectStorage/ObjectStorageLoadingState.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import CircleProgress from 'src/components/CircleProgress';
 
-export const ObjectStorageLoadingState: React.StatelessComponent<{}> = () => {
+export const ObjectStorageLoadingState: React.FC<{}> = () => {
   return <CircleProgress />;
 };
 
