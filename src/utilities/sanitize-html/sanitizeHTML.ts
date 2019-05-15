d791d1f872ed5f26ca3ef7b75eeedf5d79797f23
Martin <mmckenna.phila@gmail.com>
2019-05-15 17:04:14 -0400

    Markdown: Render Blacklisted Tags as Text (#4938)
    * renders script tags as text
    
    * patches sanitize-html to escape blacklisted tags

--- src/utilities/sanitize-html/sanitizeHTML.ts
+++ src/utilities/sanitize-html/sanitizeHTML.ts
@@ -8,5 +8,29 @@ export const sanitizeHTML = (text: string) =>
       '*': allowedHTMLAttr,
       /** only allowing classes on spans for the sole purpose of code highlighting */
       span: ['class']
-    }
+    },
+    /**
+     * this option is not supported and was patched
+     * See: https://github.com/punkave/sanitize-html/pull/169
+     */
+    escapeDisallowedTags: true
+    /** this is basically just converting script tags to text */
+    // transformTags: {
+    //   script: (tagName, attrs: Record<string, string>) => {
+    //     /**
+    //      * get all attributes of the script tag and recreate them as they were typed
+    //      * i.e src="hello.js"
+    //      */
+    //     const attrsAsString = Object.keys(attrs).reduce((accum, eachKey) => {
+    //       return `${accum} ${eachKey}="${attrs[eachKey]}"`;
+    //     }, '');
+
+    //     /** return the script tag as text inside a p tag */
+    //     return {
+    //       tagName: 'script',
+    //       text: `&lt;${tagName}${attrsAsString}&gt;&lt;/${tagName}&gt;`,
+    //       attribs: {}
+    //     };
+    //   }
+    // }
   }).trim();
