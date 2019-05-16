e0298557c3ae492e046a60e37f5ddd356b84246b
John Callahan <johnwcallahan@gmail.com>
2019-05-16 11:50:01 -0400

    M3-2699 fix: Per-linode network transfer pool (#4944)
    * Add linode/instances/:id/transferpool endpoint
    
    * Add readableBytes() options
    
    Add unit, maxUnit, round, and handleNegatives params. Some refactoring as well.
    
    * Use new API endpoint in LinodeNetSummary
    
    * Add comment
    
    * Update API endpoint name

--- src/utilities/unitConversions.test.ts
+++ src/utilities/unitConversions.test.ts
@@ -1,12 +1,23 @@
 import { readableBytes } from './unitConversions';
 
 describe('readableBytes', () => {
-  it('should return "0 B" if bytes === 0', () => {
+  it('should return "0 bytes" if bytes === 0', () => {
     expect(readableBytes(0).formatted).toBe('0 bytes');
   });
 
+  it("should handle negative values, unless it' disabled by the handleNegatives option", () => {
+    expect(readableBytes(-123).formatted).toBe('-123 bytes');
+    expect(readableBytes(-123).value).toBe(-123);
+    expect(readableBytes(-1048576).formatted).toBe('-1 MB');
+    expect(readableBytes(-1048576).value).toBe(-1);
+
+    expect(readableBytes(-1048576, { handleNegatives: false }).formatted).toBe(
+      '0 bytes'
+    );
+  });
+
   it('should return B if < 1024', () => {
-    expect(readableBytes(1023).formatted).toBe('1023 B');
+    expect(readableBytes(1023).formatted).toBe('1023 bytes');
   });
 
   it('handles KB, MB, GB', () => {
@@ -29,5 +40,59 @@ describe('readableBytes', () => {
     expect(readableBytes(1024 * 100).formatted).toBe('100 KB');
     expect(readableBytes(1024 * 100.25).formatted).toBe('100 KB');
     expect(readableBytes(1024 * 100.5).formatted).toBe('101 KB');
+  });
+
+  it('respects rounding when specified with number', () => {
+    const round0 = { round: 0 };
+    const round1 = { round: 1 };
+    const round2 = { round: 2 };
+
+    expect(readableBytes(1024 * 9.72, round0).formatted).toBe('10 KB');
+    expect(readableBytes(1024 * 9.72, round1).formatted).toBe('9.7 KB');
+    expect(readableBytes(1024 * 9.72, round2).formatted).toBe('9.72 KB');
+
+    expect(readableBytes(1024 * 89.99, round0).formatted).toBe('90 KB');
+    expect(readableBytes(1024 * 89.99, round1).formatted).toBe('90 KB');
+    expect(readableBytes(1024 * 89.99, round2).formatted).toBe('89.99 KB');
+
+    expect(readableBytes(1024 * 100.25, round0).formatted).toBe('100 KB');
+    expect(readableBytes(1024 * 100.25, round1).formatted).toBe('100.3 KB');
+    expect(readableBytes(1024 * 100.25, round2).formatted).toBe('100.25 KB');
+  });
+
+  it('respects rounding when given specific units', () => {
+    expect(readableBytes(1024 * 9.723, { round: { KB: 3 } }).formatted).toBe(
+      '9.723 KB'
+    );
+    expect(readableBytes(1024 * 9.723, { round: { MB: 3 } }).formatted).toBe(
+      '9.72 KB'
+    );
+    expect(
+      readableBytes(1024 * 1024 * 143.22, { round: { MB: 2 } }).formatted
+    ).toBe('143.22 MB');
+  });
+
+  it("doesn't return units higher than the specific max unit", () => {
+    expect(
+      readableBytes(1024 * 1024 * 1024 * 50, { maxUnit: 'MB' }).formatted
+    ).toBe('51200 MB');
+    expect(
+      readableBytes(1024 * 1024 * 1024 * 50, { maxUnit: 'KB' }).formatted
+    ).toBe('52428800 KB');
+    expect(
+      readableBytes(1024 * 1024 * 1024 * 50, { maxUnit: 'bytes' }).formatted
+    ).toBe('53687091200 bytes');
+  });
+
+  it('returns the given unit if specified', () => {
+    expect(
+      readableBytes(1024 * 1024 * 1024 * 50, { unit: 'MB' }).formatted
+    ).toBe('51200 MB');
+    expect(
+      readableBytes(1024 * 1024 * 1024 * 50, { unit: 'GB' }).formatted
+    ).toBe('50 GB');
+    expect(
+      readableBytes(1024 * 1024 * 1024 * 50, { unit: 'TB' }).formatted
+    ).toBe('0.05 TB');
   });
 });
