ae82522254e5cf64fdd9595bd444f12da3739d22
Jared Kobos <jaredkobos@gmail.com>
2019-05-23 12:01:57 -0400

    Update pre commit and push scripts

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -17,31 +17,31 @@ yarn test --findRelatedTests $changes --passWithNoTests
 echo $? >| .tmp.test.status
 ) &
 
-# Run storybook tests if components are changed
-if [[ $changes =~ .*src\/components.* ]]; then
-(
-    # Check if storybook is running
-    nc -z -w5 localhost 6006 > /dev/null 2>&1
-    storybookRunning=$?
+# # Run storybook tests if components are changed
+# if [[ $changes =~ .*src\/components.* ]]; then
+# (
+#     # Check if storybook is running
+#     nc -z -w5 localhost 6006 > /dev/null 2>&1
+#     storybookRunning=$?
 
-    if [[ $storybookRunning -eq "0" ]]; then
-        yarn storybook:e2e
-    else
-        yarn storybook > /dev/null 2>&1 &
-        yarn storybook:e2e
-    fi
-    echo $? >| .tmp.storybook.status
+#     if [[ $storybookRunning -eq "0" ]]; then
+#         yarn storybook:e2e
+#     else
+#         yarn storybook > /dev/null 2>&1 &
+#         yarn storybook:e2e
+#     fi
+#     echo $? >| .tmp.storybook.status
 
-    # Ensure we cleanup any leftover processes
-    $( pkill -f selenium-standalone || : )
+#     # Ensure we cleanup any leftover processes
+#     $( pkill -f selenium-standalone || : )
 
-    if [[ $storybookRunning -eq "1" ]]; then
-        $( pkill -f storybook )
-    fi
-) &
-else
-    storybookStatus=0
-fi
+#     if [[ $storybookRunning -eq "1" ]]; then
+#         $( pkill -f storybook )
+#     fi
+# ) &
+# else
+#     storybookStatus=0
+# fi
 
 # Wait for sub-shells to exit
 wait
