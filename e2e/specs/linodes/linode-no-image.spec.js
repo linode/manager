1e55cc7160a73d233e2f1824581e84aaaac1e914
John Callahan <johnwcallahan@gmail.com>
2019-06-03 16:08:34 -0400

    e2e: fix linode-no-image test (#5000)
    * Prettier formatting
    
    * Fix linode-no-image e2e test
    
    The tooltip copy had changed, breaking the test.
    
    * Use text matching
    
    * Wait for tooltip to be visible

--- e2e/specs/linodes/linode-no-image.spec.js
+++ e2e/specs/linodes/linode-no-image.spec.js
@@ -1,45 +1,53 @@
 const { constants } = require('../../constants');
 import {
-    apiCreateMultipleLinodes,
-    apiDeleteAllLinodes,
-    timestamp,
+  apiCreateMultipleLinodes,
+  apiDeleteAllLinodes,
+  timestamp
 } from '../../utils/common';
 import ListLinodes from '../../pageobjects/list-linodes';
 import LinodeDetail from '../../pageobjects/linode-detail/linode-detail.page';
 
 describe('Can not boot a linode without an Image', () => {
-    const toolTipMessage = 'An image needs to be added before powering on a Linode';
-    const linode = {
-        linodeLabel: `Auto${timestamp()}`,
-        noImage: true
-    };
-    let testLinode;
+  const toolTipMessage =
+    'A config needs to be added before powering on a Linode';
+  const linode = {
+    linodeLabel: `Auto${timestamp()}`,
+    noImage: true
+  };
+  let testLinode;
 
-    beforeAll(() => {
-        testLinode = apiCreateMultipleLinodes([linode])[0];
-    });
+  beforeAll(() => {
+    testLinode = apiCreateMultipleLinodes([linode])[0];
+  });
 
-    afterAll(() => {
-        apiDeleteAllLinodes();
-    });
+  afterAll(() => {
+    apiDeleteAllLinodes();
+  });
 
-    it('Power on tool tip displays for a linode without an image on Linode listing page', () => {
-        ListLinodes.openActionMenu($(ListLinodes.getLinodeSelector(linode.linodeLabel)));
-        const toolTipIcon = ListLinodes.powerOnMenu.$(ListLinodes.toolTipIcon.selector);
-        toolTipIcon.waitForVisible(constants.wait.normal);
-        toolTipIcon.moveToObject();
-        expect(ListLinodes.toolTipMessage.getText()).toBe(toolTipMessage);
-    });
+  it('Power on tool tip displays for a linode without an image on Linode listing page', () => {
+    ListLinodes.openActionMenu(
+      $(ListLinodes.getLinodeSelector(linode.linodeLabel))
+    );
+    ListLinodes.toolTipIcon.waitForVisible(constants.wait.normal);
+    const toolTipIcon = ListLinodes.powerOnMenu.$(
+      ListLinodes.toolTipIcon.selector
+    );
+    toolTipIcon.waitForVisible(constants.wait.normal);
+    toolTipIcon.moveToObject();
+    expect(ListLinodes.toolTipMessage.getText()).toMatch(toolTipMessage);
+  });
 
-    it('Power on tool tip displays for a linode without an image on Linode detail page', () => {
-        browser.url(`${constants.routes.linodes}/${testLinode.id}/summary`)
-        browser.pause(500);
-        LinodeDetail.powerControl.waitForVisible(constants.wait.normal);
-        LinodeDetail.powerControl.click();
-        browser.pause(500);
-        LinodeDetail.setPowerOn.waitForVisible(constants.wait.normal);
-        const toolTipIcon = LinodeDetail.setPowerOn.$(ListLinodes.toolTipIcon.selector);
-        toolTipIcon.moveToObject();
-        expect(ListLinodes.toolTipMessage.getText()).toBe(toolTipMessage);
-    });
+  it('Power on tool tip displays for a linode without an image on Linode detail page', () => {
+    browser.url(`${constants.routes.linodes}/${testLinode.id}/summary`);
+    browser.pause(500);
+    LinodeDetail.powerControl.waitForVisible(constants.wait.normal);
+    LinodeDetail.powerControl.click();
+    browser.pause(500);
+    LinodeDetail.setPowerOn.waitForVisible(constants.wait.normal);
+    const toolTipIcon = LinodeDetail.setPowerOn.$(
+      ListLinodes.toolTipIcon.selector
+    );
+    toolTipIcon.moveToObject();
+    expect(ListLinodes.toolTipMessage.getText()).toBe(toolTipMessage);
+  });
 });
