2cf7918c3fdde8bef7ff85acb6b77bbe2a1624d5
Alban Bailly <abailly@linode.com>
2019-06-04 13:25:07 -0400

    abuse ticket banner fix

--- src/components/AbuseTicketBanner/AbuseTicketBanner.tsx
+++ src/components/AbuseTicketBanner/AbuseTicketBanner.tsx
@@ -11,38 +11,40 @@ interface Props {
   abuseTickets: Linode.Notification[];
 }
 
-export const AbuseTicketBanner: React.FunctionComponent<Props> = props => {
-  const { abuseTickets } = props;
+export class AbuseTicketBanner extends React.Component<Props> {
+  render() {
+    const { abuseTickets } = this.props;
 
-  if (!abuseTickets || abuseTickets.length === 0) {
-    return null;
+    if (!abuseTickets || abuseTickets.length === 0) {
+      return null;
+    }
+
+    const count = abuseTickets.length;
+    const multiple = count > 1;
+
+    const message = `You have ${multiple ? count : `an`} open abuse ticket${
+      multiple ? 's' : ''
+    }.`;
+
+    /**
+     * The ticket list page doesn't indicate which tickets are abuse tickets
+     * so for now, the link can just take the user to the first ticket.
+     */
+    const href = abuseTickets[0].entity!.url;
+
+    return (
+      <Grid item xs={12}>
+        <Notice important error>
+          {message} Please{' '}
+          <Link data-testid="abuse-ticket-link" to={href}>
+            click here
+          </Link>{' '}
+          to view {`${multiple ? 'these tickets' : 'this ticket'}.`}
+        </Notice>
+      </Grid>
+    );
   }
-
-  const count = abuseTickets.length;
-  const multiple = count > 1;
-
-  const message = `You have ${multiple ? count : `an`} open abuse ticket${
-    multiple ? 's' : ''
-  }.`;
-
-  /**
-   * The ticket list page doesn't indicate which tickets are abuse tickets
-   * so for now, the link can just take the user to the first ticket.
-   */
-  const href = abuseTickets[0].entity!.url;
-
-  return (
-    <Grid item xs={12}>
-      <Notice important error>
-        {message} Please{' '}
-        <Link data-testid="abuse-ticket-link" to={href}>
-          click here
-        </Link>{' '}
-        to view {`${multiple ? 'these tickets' : 'this ticket'}.`}
-      </Notice>
-    </Grid>
-  );
-};
+}
 
 const mapStateToProps: MapState<Props, {}> = (state, ownProps) => ({
   abuseTickets: getAbuseTicket(state.__resources)
