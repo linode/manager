4392218e5321fd1d87640bc6b62d136cf4e93860
Jared Kobos <jaredkobos@gmail.com>
2019-06-06 12:36:14 -0400

    Add routing and cluster detail boilerplate

--- src/features/Kubernetes/KubernetesClusterDetail/KubernetesClusterDetail.tsx
+++ src/features/Kubernetes/KubernetesClusterDetail/KubernetesClusterDetail.tsx
@@ -2,38 +2,33 @@ import * as React from 'react';
 import { compose } from 'recompose';
 
 import Breadcrumb from 'src/components/Breadcrumb';
-import Button from 'src/components/Button';
 import Grid from 'src/components/core/Grid';
-import Paper from 'src/components/core/Paper';
 import {
   StyleRulesCallback,
   WithStyles,
   withStyles
 } from 'src/components/core/styles';
-import Typography from 'src/components/core/Typography';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
-import TagsPanel from 'src/components/TagsPanel';
-import withTypes, { WithTypesProps } from 'src/containers/types.container';
-import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
-import NodePoolPanel from '../CreateCluster/NodePoolPanel';
-import KubeSummaryPanel from './KubeSummaryPanel';
-import NodePoolsDisplay from './NodePoolsDisplay';
+import { ExtendedPoolNode } from '.././types';
 
 type ClassNames =
   | 'root'
   | 'title'
   | 'titleWrapper'
   | 'backButton'
-  | 'section'
-  | 'panelItem'
-  | 'button';
+  | 'listParent'
+  | 'label'
+  | 'labelIcon'
+  | 'status'
+  | 'open'
+  | 'ticketLabel'
+  | 'closed';
 
 const styles: StyleRulesCallback<ClassNames> = theme => ({
   root: {},
   title: {
     display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing.unit
+    alignItems: 'center'
   },
   titleWrapper: {
     display: 'flex',
@@ -47,25 +42,50 @@ const styles: StyleRulesCallback<ClassNames> = theme => ({
     },
     padding: 0
   },
-  section: {
-    margin: theme.spacing.unit * 2
+  label: {
+    marginBottom: theme.spacing.unit
   },
-  panelItem: {
-    padding: theme.spacing.unit
+  ticketLabel: {
+    position: 'relative',
+    top: -3
   },
-  button: {
-    marginLeft: theme.spacing.unit * 2
+  labelIcon: {
+    paddingRight: 0,
+    '& .outerCircle': {
+      fill: theme.bg.offWhiteDT,
+      stroke: theme.bg.main
+    },
+    '& .circle': {
+      stroke: theme.bg.main
+    }
+  },
+  listParent: {},
+  status: {
+    marginLeft: theme.spacing.unit,
+    color: theme.color.white
+  },
+  open: {
+    backgroundColor: theme.color.green
+  },
+  closed: {
+    backgroundColor: theme.color.red
   }
 });
 
-type CombinedProps = WithTypesProps & WithStyles<ClassNames>;
+interface ClusterEditingState {
+  nodePools: ExtendedPoolNode[];
+}
+
+type CombinedProps = {} & WithStyles<ClassNames>;
 
 export const KubernetesClusterDetail: React.FunctionComponent<
   CombinedProps
 > = props => {
   const [editing, setEditing] = React.useState<boolean>(false);
-
-  const { classes, typesData, typesError, typesLoading } = props;
+  const [fields, updateFields] = React.useState<ClusterEditingState>({
+    nodePools: []
+  });
+  const { classes } = props;
 
   return (
     <React.Fragment>
@@ -88,65 +108,8 @@ export const KubernetesClusterDetail: React.FunctionComponent<
         </Grid>
       </Grid>
 
-      <Grid container direction="row" className={classes.section}>
-        <Grid container item direction="column" xs={9}>
-          <Grid item>
-            <NodePoolsDisplay
-              editing={editing}
-              toggleEditing={() => setEditing(!editing)}
-              pools={[]}
-              types={[]}
-            />
-          </Grid>
-          <Grid item>
-            <NodePoolPanel
-              hideTable
-              pools={[]}
-              types={typesData || []}
-              nodeCount={0}
-              addNodePool={() => null}
-              deleteNodePool={() => null}
-              handleTypeSelect={() => null}
-              updateNodeCount={() => null}
-              updatePool={() => null}
-              typesLoading={typesLoading}
-              typesError={
-                typesError
-                  ? getAPIErrorOrDefault(
-                      typesError,
-                      'Error loading Linode type information.'
-                    )[0].reason
-                  : undefined
-              }
-            />
-          </Grid>
-          <Grid item className={classes.section}>
-            <Button destructive type="secondary" onClick={() => null}>
-              Delete Cluster
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid container item direction="column" xs={3}>
-          <Grid item className={classes.button}>
-            <Button type="primary">Download kubeconfig</Button>
-          </Grid>
-          <Grid item className={classes.section}>
-            <KubeSummaryPanel />
-          </Grid>
-          <Grid item className={classes.section}>
-            <Paper>
-              <Typography variant="h3" className={classes.title} data-qa-title>
-                Cluster Tags
-              </Typography>
-              <div className={classes.panelItem}>
-                <TagsPanel
-                  tags={['tag1', 'tag2']}
-                  updateTags={() => Promise.resolve()}
-                />
-              </div>
-            </Paper>
-          </Grid>
-        </Grid>
+      <Grid container className={classes.listParent}>
+        Hello
       </Grid>
     </React.Fragment>
   );
@@ -154,9 +117,6 @@ export const KubernetesClusterDetail: React.FunctionComponent<
 
 const styled = withStyles(styles);
 
-const enhanced = compose<CombinedProps, {}>(
-  styled,
-  withTypes
-);
+const enhanced = compose<CombinedProps, {}>(styled);
 
 export default enhanced(KubernetesClusterDetail);
