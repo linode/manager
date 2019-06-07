3636763b3d5a73f432c1a2816650dcd49b77709a
John Callahan <johnwcallahan@gmail.com>
2019-06-06 21:52:55 -0400

    Clone <Disks /> component

--- src/features/linodes/CloneLanding/Disks.tsx
+++ src/features/linodes/CloneLanding/Disks.tsx
@@ -1,7 +1,105 @@
 import * as React from 'react';
+import { compose } from 'recompose';
+import CheckBox from 'src/components/CheckBox';
+import {
+  StyleRulesCallback,
+  withStyles,
+  WithStyles
+} from 'src/components/core/styles';
+import TableBody from 'src/components/core/TableBody';
+import TableCell from 'src/components/core/TableCell';
+import TableHead from 'src/components/core/TableHead';
+import TableRow from 'src/components/core/TableRow';
+import Grid from 'src/components/Grid';
+import Paginate from 'src/components/Paginate';
+import PaginationFooter from 'src/components/PaginationFooter';
+import Table from 'src/components/Table';
 
-export const Disks: React.FC<{}> = () => {
-  return <div>Disks Component</div>;
+type ClassNames = 'root' | 'tableCell' | 'labelCol' | 'sizeCol';
+
+const styles: StyleRulesCallback<ClassNames> = theme => ({
+  root: {},
+  tableCell: {
+    borderBottom: 'none',
+    padding: 0
+  },
+  labelCol: {
+    width: '65%'
+  },
+  sizeCol: {
+    width: '35%'
+  }
+});
+
+interface Props {
+  disks: Linode.Disk[];
+  selectedDisks: Record<number, boolean>;
+  handleSelect: (id: number) => void;
+}
+
+type CombinedProps = Props & WithStyles<ClassNames>;
+
+export const Configs: React.FC<CombinedProps> = props => {
+  const { classes, disks, handleSelect, selectedDisks } = props;
+
+  return (
+    <Paginate data={disks}>
+      {({
+        data: paginatedData,
+        handlePageChange,
+        handlePageSizeChange,
+        page,
+        pageSize,
+        count
+      }) => {
+        return (
+          <React.Fragment>
+            <Grid container>
+              <Grid item xs={12} md={9}>
+                <Table
+                  isResponsive={false}
+                  aria-label="List of Configurations"
+                  border
+                >
+                  <TableHead>
+                    <TableRow>
+                      <TableCell className={classes.labelCol}>Label</TableCell>
+                      <TableCell className={classes.sizeCol}>Size</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {paginatedData.map(disk => (
+                      <TableRow key={disk.id} data-qa-disk={disk.label}>
+                        <TableCell>
+                          <CheckBox
+                            checked={selectedDisks[disk.id]}
+                            onChange={() => handleSelect(disk.id)}
+                            text={disk.label}
+                          />
+                        </TableCell>
+                        <TableCell>{disk.size} MB</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Grid>
+            </Grid>
+            <PaginationFooter
+              count={count}
+              page={page}
+              pageSize={pageSize}
+              handlePageChange={handlePageChange}
+              handleSizeChange={handlePageSizeChange}
+              eventCategory="linode disks"
+            />
+          </React.Fragment>
+        );
+      }}
+    </Paginate>
+  );
 };
 
-export default Disks;
+const styled = withStyles(styles);
+const enhanced = compose<CombinedProps, Props>(styled);
+
+export default enhanced(Configs);
