e03bee1de91678143d75f6392efeda1fab8fd05f
Martin <mmckenna.phila@gmail.com>
2019-06-11 16:53:20 -0400

    Add: Maintenance Window to CSV (#5047)
    * adds maintenance start window to csv
    
    * updates timezone

--- src/features/linodes/LinodesLanding/utils.ts
+++ src/features/linodes/LinodesLanding/utils.ts
@@ -1,24 +1,20 @@
 import { reportException } from 'src/exceptionReporting';
-import { formatDate } from 'src/utilities/formatDate';
 
 export const parseMaintenanceStartTime = (startTime?: string | null) => {
   if (!startTime) {
     return 'No Maintenance Needed';
   }
-
-  const parsedDate = formatDate(startTime);
-
   /**
    * this should never happen as long as the API is returning
    * a date format we can parse, but this is a good failsafe.
    */
-  if (parsedDate.match(/invalid/i)) {
+  if (startTime.match(/valid/i)) {
     reportException('Error parsing maintenance start time', {
       rawDate: startTime,
-      convertedDate: parsedDate
+      convertedDate: startTime
     });
     return 'Maintenance Window Unknown';
   }
 
-  return parsedDate;
+  return startTime;
 };
