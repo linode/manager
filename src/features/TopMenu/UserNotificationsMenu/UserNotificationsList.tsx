e7a08eacd831f3399679155181df06fd7bb8bbcf
Martin <mmckenna.phila@gmail.com>
2019-06-19 09:46:12 -0400

    Change: Notification Copy (#5093)
    * updates notification copy
    
    * updates default text

--- src/features/TopMenu/UserNotificationsMenu/UserNotificationsList.tsx
+++ src/features/TopMenu/UserNotificationsMenu/UserNotificationsList.tsx
@@ -1,4 +1,4 @@
-import { compose, path, pathOr } from 'ramda';
+import { compose, path } from 'ramda';
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import {
@@ -106,16 +106,17 @@ const interceptNotification = (
     notification.entity.type === 'linode'
   ) {
     /** replace "this Linode" with the name of the Linode */
+    const linodeAttachedToNotification = path(['label'], notification.entity);
     return {
       ...notification,
       label: `Maintenance Scheduled`,
       severity: 'major',
-      message: notification.message
-        .toLowerCase()
-        .replace(
-          'this linode',
-          `Linode ${pathOr('this Linode', ['label'], notification.entity)}`
-        )
+      message: `${
+        linodeAttachedToNotification
+          ? `Linode ${linodeAttachedToNotification}`
+          : `This Linode`
+      }
+          has scheduled maintenance`
     };
   }
 
