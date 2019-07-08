c0e22cadca6dac2ae4b05b9ed7d8e11b106f4ea9
Jason Stamis <jstamis@linode.com>
2019-07-08 09:26:52 -0400

    Fixed ActionMenu test we can just click the tooltip and check the text

--- src/components/ActionMenu/ActionMenu.spec.js
+++ src/components/ActionMenu/ActionMenu.spec.js
@@ -6,21 +6,19 @@ describe('Action Menu Suite', () => {
         'Action Menu',
         'Action Menu with disabled menu item %26 tooltip',
     ];
-    const singleItemStory = 'Action Menu with one menu item';
     const actionMenu = '[data-qa-action-menu]';
     const actionMenuItem = '[data-qa-action-menu-item]';
-    const actionMenuSingleItem = '[data-qa-action-menu-link]';
 
     it('should display action menu element', () => {
         navigateToStory(component, menuStories, () => {
-            browser.waitForVisible(actionMenu);
+            $(actionMenu).waitForDisplayed();
         });
     });
 
     it('should display action menu items', () => {
         executeInAllStories(component, menuStories, () => {
-            browser.click(actionMenu);
-            browser.waitForVisible(actionMenuItem);
+            $(actionMenu).click();
+            $(actionMenuItem).waitForDisplayed();
             expect($$(actionMenuItem).length).toBeGreaterThan(1);
             $$(actionMenuItem).forEach(i => expect(i.getTagName()).toBe('li'));
         });
@@ -28,10 +26,12 @@ describe('Action Menu Suite', () => {
 
     it('should hide the menu items on select of an item', () => {
         navigateToStory(component, menuStories[0]);
-        browser.click(actionMenu);
-        browser.waitForVisible(actionMenuItem);
-        browser.click(actionMenuItem);
-        browser.waitForVisible(actionMenuItem, 1000, true);
+        $(actionMenu).click();
+        $(actionMenuItem).waitForDisplayed();
+        $(actionMenuItem).click();
+        
+            $(actionMenuItem).waitForDisplayed(1500, true);
+        
     });
 
     it('should display tooltip menu item', () => {
@@ -41,28 +41,15 @@ describe('Action Menu Suite', () => {
         const tooltipIcon = '[data-qa-tooltip-icon]';
         const tooltip = '[data-qa-tooltip]';
 
-        browser.waitForVisible(actionMenu);
-        browser.click(actionMenu);
+        $(actionMenu).waitForDisplayed();
+        $(actionMenu).click();
         
-        $(disabledMenuItem).waitForVisible();
-
-        // HACK** REPLACE WITH ACTIONS API
-        browser.moveToObject(tooltipIcon);
-
-        expect($(disabledMenuItem).getAttribute('class')).toContain('disabled');
-        expect($(tooltipIcon).isVisible()).toBe(true);
-        expect($(tooltipIcon).getTagName()).toBe('button');
+        $(disabledMenuItem).waitForDisplayed();
+        $(tooltipIcon).click();
+        $(tooltip).waitForDisplayed();
         expect($(tooltip).getText()).toBe('An explanation as to why this item is disabled');
+        expect($(disabledMenuItem).getAttribute('class')).toContain('disabled');
+        expect($(tooltipIcon).isDisplayed()).toBe(true);
+        expect($(tooltipIcon).getTagName()).toBe('button');
     });
-
-    // it('should display menu item as a link when only one menu item', () => {
-    //     navigateToStory(component, singleItemStory);
-    //     browser.waitForVisible(actionMenuSingleItem);
-
-    //     const tagType = browser.getTagName(actionMenuSingleItem);
-    //     const linkUrl = browser.getAttribute(actionMenuSingleItem, 'href');
-
-    //     expect(linkUrl).toContain('iframe.html?selectedKind=Action%20Menu&selectedStory=Action%20Menu%20with%20one%20menu%20item#');
-    //     expect(tagType).toBe('a');
-    // });
 });
