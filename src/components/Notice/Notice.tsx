fc445a6ffaac1c9aa78818ee138447491ce3bb1a
Kayla Wilkins <kwilkins@linode.com>
2019-07-10 11:24:20 -0400

    fixing important notice icon placement

--- src/components/Notice/Notice.tsx
+++ src/components/Notice/Notice.tsx
@@ -45,6 +45,7 @@ const styles = (theme: Theme) => {
       }
     },
     root: {
+      position: 'relative',
       marginBottom: theme.spacing(2),
       padding: '4px 16px',
       maxWidth: '100%',
@@ -64,7 +65,7 @@ const styles = (theme: Theme) => {
     icon: {
       color: 'white',
       position: 'absolute',
-      left: theme.spacing(1) + 7
+      left: -25
     },
     closeIcon: {
       paddingLeft: theme.spacing(1)
