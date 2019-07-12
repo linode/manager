80b22f4193b45ca0ac9b1c49ac2cf4a2a32f5c4a
mmckenna <mmckenna.phila@gmail.com>
2019-07-12 11:38:56 -0400

    adds better error handling

--- src/utilities/updateById.ts
+++ src/utilities/updateById.ts
@@ -1,4 +1,5 @@
 import { adjust } from 'ramda';
+import { reportException } from 'src/exceptionReporting';
 
 export default <T extends { id: string | number }>(
   updater: (v: T) => T,
@@ -24,5 +25,20 @@ export default <T extends { id: string | number }>(
    * ramda: ^0.25.0
    * @types/ramda: 0.25.16 (0.25.17 switches the argument order)
    */
-  return adjust(updater, foundIndex, list);
+  try {
+    return adjust(updater, foundIndex, list);
+  } catch (e) {
+    /**
+     * we have logic in place to not bomb the app, but we should throw an error
+     * here and not fail silently.
+     *
+     * this will most likely be invoked whenever we upgrade ramda
+     */
+
+    reportException(`Error with running ramda adjust: ${e}`);
+    console.error(`Error with running ramda adjust: ${e}`);
+    console.error(`Check the ramda docs and make sure you are invoking the function
+    with the arguments in the correct order.`);
+    return list;
+  }
 };
