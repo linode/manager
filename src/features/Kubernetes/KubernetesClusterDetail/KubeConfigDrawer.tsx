601157bf1b7d36d4282ec7cc6f322defd23213e7
Jared Kobos <jaredkobos@gmail.com>
2019-07-18 16:09:11 -0400

    Add copy tooltip

--- src/features/Kubernetes/KubernetesClusterDetail/KubeConfigDrawer.tsx
+++ src/features/Kubernetes/KubernetesClusterDetail/KubeConfigDrawer.tsx
@@ -1,30 +1,41 @@
 import * as React from 'react';
 
+import Download from 'src/assets/icons/download.svg';
+import CopyTooltip from 'src/components/CopyTooltip';
+import Grid from 'src/components/core/Grid';
 import {
   createStyles,
   Theme,
   withStyles,
   WithStyles
 } from 'src/components/core/styles';
+import Typography from 'src/components/core/Typography';
 import Drawer from 'src/components/Drawer';
 import DrawerContent from 'src/components/DrawerContent';
 
 import { Converter } from 'showdown';
 import 'showdown-highlightjs-extension';
+import { downloadFile } from 'src/utilities/downloadFile';
 import { sanitizeHTML } from 'src/utilities/sanitize-html';
 
-type ClassNames = 'root';
+type ClassNames = 'root' | 'icon' | 'tooltip';
 
 const styles = (theme: Theme) =>
   createStyles({
-    root: {
-      wordWrap: 'break-word',
-      overflowX: 'hidden'
+    root: {},
+    icon: {
+      color: '#3683dc'
+    },
+    tooltip: {
+      position: 'fixed',
+      right: 40,
+      marginTop: theme.spacing(0.5)
     }
   });
 
 interface Props {
   kubeConfig: string;
+  clusterLabel: string;
   open: boolean;
   closeDrawer: () => void;
 }
@@ -32,8 +43,8 @@ interface Props {
 export type CombinedProps = Props & WithStyles<ClassNames>;
 
 const splitCode = (code: string): string => {
-  const MAX_LENGTH = 65;
-  const lines = code.split('\n');
+  const MAX_LENGTH = 75;
+  const lines = code.trim().split('\n');
   const shortLines = lines.map(thisLine =>
     thisLine.length > MAX_LENGTH
       ? splitCode(
@@ -45,8 +56,7 @@ const splitCode = (code: string): string => {
 };
 
 export const KubeConfigDrawer: React.FC<CombinedProps> = props => {
-  const { classes, kubeConfig, closeDrawer, open } = props;
-  const title = 'blah';
+  const { classes, clusterLabel, kubeConfig, closeDrawer, open } = props;
   const error = false;
   const loading = false;
 
@@ -55,14 +65,24 @@ export const KubeConfigDrawer: React.FC<CombinedProps> = props => {
     simplifiedAutoLink: true,
     openLinksInNewWindow: true
   }).makeHtml(splitCode('```\n' + kubeConfig + '\n```'));
+
   return (
     <Drawer title={'View Kubeconfig'} open={open} onClose={closeDrawer} chubby>
-      <DrawerContent title={title} error={error} loading={loading}>
-        {/* <ScriptCode script={script} /> */}
-        <pre
-          className={classes.root}
-          dangerouslySetInnerHTML={{ __html: sanitizeHTML(html) }}
-        />
+      <DrawerContent title={clusterLabel} error={error} loading={loading}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <Typography variant="h3">{clusterLabel}</Typography>
+          </Grid>
+          <Grid item>
+            <a onClick={() => downloadFile('kubeconfig.yaml', kubeConfig)}>
+              <Download className={classes.icon} />
+            </a>
+          </Grid>
+        </Grid>
+        <div>
+          <CopyTooltip className={classes.tooltip} text={kubeConfig} />
+          <div dangerouslySetInnerHTML={{ __html: sanitizeHTML(html) }} />
+        </div>
       </DrawerContent>
     </Drawer>
   );
