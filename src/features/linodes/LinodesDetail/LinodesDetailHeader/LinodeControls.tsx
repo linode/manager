5acf5fd2088dc0e1c77156efeecaf745dedbeb05
Martin <mmckenna.phila@gmail.com>
2019-07-19 08:47:52 -0400

    fixes issue with power control not updating (#5242)

--- src/features/linodes/LinodesDetail/LinodesDetailHeader/LinodeControls.tsx
+++ src/features/linodes/LinodesDetail/LinodesDetailHeader/LinodeControls.tsx
@@ -144,7 +144,7 @@ const LinodeControls: React.StatelessComponent<CombinedProps> = props => {
         </Button>
         <LinodePowerControl
           status={linode.status}
-          recentEvent={linode.recentEvent}
+          recentEvent={linode._events[0]}
           id={linode.id}
           label={linode.label}
           disabled={disabled}
