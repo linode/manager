51559614b855f8c511723367035e31ab389f9800
Jason Stamis <jstamis@linode.com>
2019-07-22 10:21:19 -0400

    PR review M3-2992 Switched out hardcoded url to base url Fixed indentation rules with the existing config rules Added nested describes for stories and future enhancements

--- src/components/Breadcrumb/Breadcrumb.spec.js
+++ src/components/Breadcrumb/Breadcrumb.spec.js
@@ -1,7 +1,7 @@
 const { navigateToStory, executeInAllStories } = require('../../../e2e/utils/storybook');
 const { constants } = require('../../../e2e/constants');
 
-describe('Breadcrumb Suite', () => { 
+describe('Breadcrumb Suite', () => {
     const component = 'Breadcrumb';
     const childStories = ['Basic Breadcrumb', 'Breadcrumb with custom label', 'Breadcrumb with editable text'];
     const link = '[data-qa-link="true"]';
@@ -17,99 +17,102 @@ describe('Breadcrumb Suite', () => {
         executeInAllStories(component, childStories, () => {
             $(link).waitForDisplayed(constants.wait.normal);
             expect($(link).getAttribute('href'))
-            .withContext(`href link should not be missing`).not.toBeNull();
+              .withContext(`href link should not be missing`).not.toBeNull();
             expect($$(link)[2].getAttribute('href'))
-            .withContext('Url should be').toEqual('http://localhost:6006/linodes/9872893679817/test')
+              .withContext('Url should be').toEqual(`${browser.options.baseUrl}/linodes/9872893679817/test`)
         });
     });
 
-    it('Static text is not editable, and does not contain link', () => {
+    describe('Static text and links', () => {
+      it('Static text is not editable, and does not contain link', () => {
         navigateToStory(component, childStories[0]);
         expect($(editButton).isDisplayed())
-        .withContext(`${editBtnMessage}`).toBe(false);
+          .withContext(`${editBtnMessage}`).toBe(false);
         expect($(staticText).$('..').$('..').getAttribute('href'))
-        .withContext(`href link should be blank`).toBeNull();
-    });
+          .withContext(`href link should be blank`).toBeNull();
+      });
 
-    it('Static text is not editable, and does contain link', () => {
+      it('Static text is not editable, and does contain link', () => {
         navigateToStory(component, childStories[1]);
         expect($(editButton).isDisplayed())
-        .withContext(`${editBtnMessage}`).toBe(false);
+          .withContext(`${editBtnMessage}`).toBe(false);
         expect($$(link)[2].getAttribute('href'))
-        .withContext(`href link should not be blank`).not.toBeNull();
-        
-    });
+          .withContext(`href link should not be blank`).not.toBeNull();
+      });
+    })
 
-    it('Editable text header should be editable, but not contain a link', () => {
+    describe('Editable breadcrumb text', () => {
+      it('Editable text header should be editable, but not contain a link', () => {
         navigateToStory(component, childStories[2]);
         $(editableText).click()
-        expect($(editButton).isDisplayed()).
-        withContext(`edit button should be visible`).toBe(true);
+        expect($(editButton).isDisplayed())
+          .withContext(`edit button should be visible`).toBe(true);
         expect($(editableText).$('..').getAttribute('href'))
-        .withContext(`href link should be blank`).toBeNull();
-    });
+          .withContext(`href link should be blank`).toBeNull();
+      });
 
-    it('Only clicking the edit icon displays the edit input field', () => {
-        executeInAllStories(component, [childStories[2]], () => {
+      it('Only clicking the edit icon displays the edit input field', () => {
+          executeInAllStories(component, [childStories[2]], () => {
             $(editableText).waitForDisplayed(true);
             $(editableText).click();
             expect($(editableTextInput).isDisplayed())
-            .withContext(`text field should not be displayed`).toBe(false);
+              .withContext(`text field should not be displayed`).toBe(false);
             $(editButton).click();
             $(editableTextInput).waitForDisplayed(constants.wait.short);
-        });
-    });
+          });
+      });
 
-    it('Text input field should have a save and close button', () => {
-        navigateToStory(component, childStories[2]);
-        $(editableText).waitForDisplayed(constants.wait.short);
-        $(editableText).click();
-        $(editButton).click();
-        expect($(editableTextInput).isDisplayed())
-        .withContext(`editable text field should displayed`).toBe(true);
-        expect($(saveEditButton).isDisplayed())
-        .withContext(`save edit button should be displayed`).toBe(true);
-        expect($(cancelButton).isDisplayed())
-        .withContext(`cancel button should be displayed`).toBe(true);
-        $('body').click();
-    });
+      it('Text input field should have a save and close button', () => {
+          navigateToStory(component, childStories[2]);
+          $(editableText).waitForDisplayed(constants.wait.short);
+          $(editableText).click();
+          $(editButton).click();
+          expect($(editableTextInput).isDisplayed())
+            .withContext(`editable text field should displayed`).toBe(true);
+          expect($(saveEditButton).isDisplayed())
+            .withContext(`save edit button should be displayed`).toBe(true);
+          expect($(cancelButton).isDisplayed())
+            .withContext(`cancel button should be displayed`).toBe(true);
+          $('body').click();
+      });
 
-    it('Clicking the cancel button clears the text input', () => {
-        navigateToStory(component, childStories[2]);
-        $(editableText).waitForDisplayed(constants.wait.short);
-        const originalValue = $(editableText).getText();
-        $(editableText).click();
-        $(editButton).click();
-        $(editableTextInput).waitForDisplayed(constants.wait.short);
-        $(editableTextInput).setValue('test clear');
-        $(cancelButton).click();
-        expect($(editableText).getText())
-        .withContext(`text should not be changed on cancel`).toEqual(originalValue);
-    });
+      it('Clicking the cancel button clears the text input', () => {
+          navigateToStory(component, childStories[2]);
+          $(editableText).waitForDisplayed(constants.wait.short);
+          const originalValue = $(editableText).getText();
+          $(editableText).click();
+          $(editButton).click();
+          $(editableTextInput).waitForDisplayed(constants.wait.short);
+          $(editableTextInput).setValue('test clear');
+          $(cancelButton).click();
+          expect($(editableText).getText())
+            .withContext(`text should not be changed on cancel`).toEqual(originalValue);
+      });
 
-    it('Clicking the save button saves the text input', () => {
-        navigateToStory(component, childStories[2]);
-        $(editableText).waitForDisplayed(constants.wait.short);
-        const updatedText = 'test save';
-        $(editableText).click();
-        $(editButton).click();
-        $(editableTextInput).waitForDisplayed(constants.wait.short);
-        $(editableTextInput).setValue(updatedText);
-        $(saveEditButton).click();
-        expect($(editableText).getText())
-        .withContext(`text should have been updated`).toEqual(updatedText);
-    });
+      it('Clicking the save button saves the text input', () => {
+          navigateToStory(component, childStories[2]);
+          $(editableText).waitForDisplayed(constants.wait.short);
+          const updatedText = 'test save';
+          $(editableText).click();
+          $(editButton).click();
+          $(editableTextInput).waitForDisplayed(constants.wait.short);
+          $(editableTextInput).setValue(updatedText);
+          $(saveEditButton).click();
+          expect($(editableText).getText())
+            .withContext(`text should have been updated`).toEqual(updatedText);
+      });
 
-    it('Clicking out of the editable input without saving does not save the text input', () => {
-        navigateToStory(component, childStories[2]);
-        $(editableText).waitForDisplayed(constants.wait.short);
-        const originalText = $(editableText).getText();
-        $(editableText).click();
-        $(editButton).click();
-        $(editableTextInput).waitForDisplayed(constants.wait.short);
-        $(editableTextInput).setValue('test do not save input');
-        $('body').click();
-        expect($(editableText).getText())
-        .withContext(`text should not have been saved`).toEqual(originalText);
-    });
+      it('Clicking out of the editable input without saving does not save the text input', () => {
+          navigateToStory(component, childStories[2]);
+          $(editableText).waitForDisplayed(constants.wait.short);
+          const originalText = $(editableText).getText();
+          $(editableText).click();
+          $(editButton).click();
+          $(editableTextInput).waitForDisplayed(constants.wait.short);
+          $(editableTextInput).setValue('test do not save input');
+          $('body').click();
+          expect($(editableText).getText())
+            .withContext(`text should not have been saved`).toEqual(originalText);
+      });
+    })
 });
