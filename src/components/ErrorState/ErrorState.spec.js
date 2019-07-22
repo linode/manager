fe0d000d440f1ab09e4acecb33be6958b1e7e4e1
Jason Stamis <jstamis@linode.com>
2019-07-22 09:57:09 -0400

    M3-3019 Updated ErrorState tests corrected error typo Need to add withContext values

--- src/components/ErrorState/ErrorState.spec.js
+++ src/components/ErrorState/ErrorState.spec.js
@@ -6,21 +6,21 @@ describe('Error State Component Suite', () => {
     const childStories = [
         'with text'
     ]
-    const icon = '[data-qa-error-icon]';
-    const errorMsg = '[data-qa-error-msg]';
+    const icon = '[data-qa-error-icon="true"]';
+    const errorMsg = '[data-qa-error-msg="true"]';
 
     beforeAll(() => {
         navigateToStory(component, childStories[0]);
     });
 
     it('should display error state with text elements', () => {
-        browser.waitForVisible('[data-qa-error-icon]');
+        $(icon).waitForDisplayed();
 
         const errorIcon = $(icon);
         const errorText = $(errorMsg);
 
         expect(errorIcon.getTagName()).toBe('svg');
-        expect(errorIcon.isVisible()).toBe(true);
-        expect(errorText.getText()).toBe('An error has occured.');
+        expect(errorIcon.isDisplayed()).toBe(true);
+        expect(errorText.getText()).toBe('An error has occurred.');
     });
 });
