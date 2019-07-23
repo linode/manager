41c432b8bf0acf8639ca3f5920579441d071326a
Jared Kobos <jaredkobos@gmail.com>
2019-07-23 17:08:10 -0400

    Add tests for domain action menu

---
+++ packages/manager/src/features/Domains/DomainActionMenu.test.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { cleanup } from 'react-testing-library';
+import { reactRouterProps } from 'src/__data__/reactRouterProps';
+import { DomainActionMenu } from './DomainActionMenu';
+
+import { includesActions, renderWithTheme } from 'src/utilities/testHelpers';
+
+jest.mock('src/components/ActionMenu/ActionMenu');
+
+const props = {
+  onClone: jest.fn(),
+  onRemove: jest.fn(),
+  onEdit: jest.fn(),
+  type: 'master',
+  domain: '',
+  id: 1234456,
+  ...reactRouterProps
+};
+
+afterEach(cleanup);
+
+describe('Domain action menu', () => {
+  it('should include standard Domain actions', () => {
+    const { queryByText } = renderWithTheme(<DomainActionMenu {...props} />);
+    includesActions(
+      ['Edit', 'Check Zone', 'Zone File', 'Clone', 'Delete'],
+      queryByText
+    );
+  });
+
+  it('master Domains should include Edit DNS records action', () => {
+    const { queryByText } = renderWithTheme(<DomainActionMenu {...props} />);
+    includesActions(['Edit DNS Records'], queryByText);
+  });
+
+  it('slave Domains should not include Edit DNS records action', () => {
+    const { queryByText } = renderWithTheme(
+      <DomainActionMenu {...props} type={'slave'} />
+    );
+    includesActions(['Edit DNS Records'], queryByText, false);
+  });
+});
