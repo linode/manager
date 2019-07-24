cceef477ecee03649ce4828ece3a7674bdb57336
Jared Kobos <jaredkobos@gmail.com>
2019-07-24 11:24:58 -0400

    Show Linode tax ID on invoices for users in EU

--- packages/manager/e2e/pageobjects/account/invoice.page.js
+++ packages/manager/e2e/pageobjects/account/invoice.page.js
@@ -7,14 +7,14 @@ class Invoice extends Page {
     get invoiceID() { return $('[data-qa-invoice-id]'); }
     get invoiceTotal() { return $('[data-qa-total]'); }
     get landingInvoiceTotal() { return $('[data-qa-total]'); }
-    get descritionRows() { return $$('[data-qa-descrition]'); }
+    get descriptionRows() { return $$('[data-qa-description]'); }
     get fromRows() { return $$('data-qa-from'); }
     get toRows() { return $$('[data-qa-to]'); }
     get quantityRows() { return $$('[data-qa-quantity]'); }
     get unitPriceRows() { return $$('[data-qa-unit-price]'); }
     get amountRows() { return $$('[data-qa-amount]'); }
     get openPrintableInvoice() { return $('[data-qa-printable-invoice]'); }
-    //Prinatable page
+    //Printable page
     get linodeLogo() { return $('[data-qa-linode-logo]'); }
     get remitToHeader() { return $('[data-qa-remit-to]'); }
     get invoiceToHeader() { return $('[data-qa-invoice-to]'); }
