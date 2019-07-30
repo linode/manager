0d38984f73fe947c28288f66a3b0381cd0e40176
John Callahan <johnwcallahan@gmail.com>
2019-07-30 10:11:19 -0400

    Add confirmation text req for Linode delete

--- packages/manager/src/features/linodes/LinodesLanding/DeleteDialog.tsx
+++ packages/manager/src/features/linodes/LinodesLanding/DeleteDialog.tsx
@@ -4,7 +4,16 @@ import { compose } from 'recompose';
 import ActionsPanel from 'src/components/ActionsPanel';
 import Button from 'src/components/Button';
 import Dialog from 'src/components/ConfirmationDialog';
+import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
+import TextField from 'src/components/TextField';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {},
+  confirmationCopy: {
+    marginTop: theme.spacing(1)
+  }
+}));
 
 interface Props {
   linodeID?: number;
@@ -21,6 +30,9 @@ const DeleteLinodeDialog: React.FC<CombinedProps> = props => {
   const [errors, setErrors] = React.useState<
     Linode.ApiFieldError[] | undefined
   >(undefined);
+  const [confirmationText, setConfirmationText] = React.useState<string>('');
+
+  const classes = useStyles();
 
   React.useEffect(() => {
     if (props.open) {
@@ -29,6 +41,7 @@ const DeleteLinodeDialog: React.FC<CombinedProps> = props => {
        */
       setErrors(undefined);
       setDeleting(false);
+      setConfirmationText('');
     }
   }, [props.open]);
 
@@ -58,6 +71,7 @@ const DeleteLinodeDialog: React.FC<CombinedProps> = props => {
       error={errors ? errors[0].reason : ''}
       actions={
         <Actions
+          disabled={confirmationText !== props.linodeLabel}
           onClose={props.onClose}
           loading={isDeleting}
           onSubmit={handleSubmit}
@@ -68,6 +82,16 @@ const DeleteLinodeDialog: React.FC<CombinedProps> = props => {
         Are you sure you want to delete your Linode? This will result in
         permanent data loss.
       </Typography>
+      <Typography className={classes.confirmationCopy}>
+        To confirm deletion, type the name of the Linode ({props.linodeLabel})
+        in the field below:
+      </Typography>
+      <TextField
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setConfirmationText(e.target.value)
+        }
+        expand
+      />
     </Dialog>
   );
 };
@@ -76,6 +100,7 @@ interface ActionsProps {
   onClose: () => void;
   onSubmit: () => void;
   loading: boolean;
+  disabled: boolean;
 }
 
 const Actions: React.FC<ActionsProps> = props => {
@@ -85,6 +110,7 @@ const Actions: React.FC<ActionsProps> = props => {
         Cancel
       </Button>
       <Button
+        disabled={props.disabled}
         onClick={props.onSubmit}
         loading={props.loading}
         destructive
