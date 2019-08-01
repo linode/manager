9e8114b18e90d351b88f2dbb4fc960b4d938fee9
mmckenna <mmckenna.phila@gmail.com>
2019-08-01 13:22:17 -0400

    removes sentry reporting

--- packages/manager/src/eventMessageGenerator.ts
+++ packages/manager/src/eventMessageGenerator.ts
@@ -1,4 +1,5 @@
 import { path } from 'ramda';
+import { isProduction } from 'src/constants';
 import { reportException } from 'src/exceptionReporting';
 
 type EventMessageCreator = (e: Linode.Event) => string;
@@ -493,12 +494,15 @@ export default (e: Linode.Event): string => {
 
   /** we couldn't find the event in our list above */
   if (!fn) {
-    /**
-     * always report to Sentry that we've got an event type we're not accounting for
-     */
-    reportException(`Unknown API Event Received`, {
-      event: e
-    });
+    /** log unknown events to the console */
+    if (!isProduction) {
+      /* tslint:disable */
+      console.error('============================================');
+      console.error('Unknown API Event Received');
+      console.log(e);
+      console.error('============================================');
+      /* tslint:enable */
+    }
 
     /** finally return some default fallback text */
     return `${e.action}${e.entity ? ` on ${e.entity.label}` : ''}`;
