0640bf379a56a0e27a708310ec53481a3af6d26b
mmckenna <mmckenna.phila@gmail.com>
2019-08-08 11:04:29 -0400

    updates typings and active element logic

--- packages/manager/src/components/PrimaryNav/utils.ts
+++ packages/manager/src/components/PrimaryNav/utils.ts
@@ -1,13 +1,19 @@
 import isPathOneOf from 'src/utilities/routing/isPathOneOf';
 
 export const linkIsActive = (href: string) => {
-  const currentlyOnOneClickPage = location.search.match(/one-click/gi);
+  const currentlyOnCreateLinodeFlow = location.pathname.match(
+    /linodes[/]create/gi
+  );
+  const currentlyOnOneClickTab = location.search.match(/one-click/gi);
   const isOneClickTab = href.match(/one-click/gi);
 
-  /** one click and linodes have similar pathnames so make sure their not both marked active */
-  if (currentlyOnOneClickPage) {
-    if (isOneClickTab) {
-      return true;
+  /**
+   * mark as active if the tab is "one click" and we're on the one-click route
+   * any other create flow tabs don't need an active nav element
+   */
+  if (currentlyOnCreateLinodeFlow) {
+    if (currentlyOnOneClickTab) {
+      return isOneClickTab;
     }
     return false;
   }
