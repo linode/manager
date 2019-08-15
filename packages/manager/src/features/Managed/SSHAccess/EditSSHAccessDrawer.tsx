49ab651d9be7c6871f43e90d012a31965288605f
John Callahan <johnwcallahan@gmail.com>
2019-08-15 09:51:14 -0400

    Clean scaffolded drawer
    Removed styles to make easier to review.

--- packages/manager/src/features/Managed/SSHAccess/EditSSHAccessDrawer.tsx
+++ packages/manager/src/features/Managed/SSHAccess/EditSSHAccessDrawer.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import Drawer from 'src/components/Drawer';
 
-import { makeStyles, Theme } from 'src/components/core/styles';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {}
-}));
-
 interface Props {
   linodeSetting?: Linode.ManagedLinodeSetting;
   isOpen: boolean;
@@ -17,7 +11,6 @@ type CombinedProps = Props;
 
 const EditSSHAccessDrawer: React.FC<CombinedProps> = props => {
   const { isOpen, closeDrawer, linodeSetting } = props;
-  const classes = useStyles();
 
   const drawerTitle = linodeSetting
     ? `Edit SSH Access for ${linodeSetting.label}`
