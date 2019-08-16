423604c2b9341d1194a8a5a772ad4d93caf59fe8
mmckenna <mmckenna.phila@gmail.com>
2019-08-16 09:16:35 -0400

    runs tsc and babel in watch mode concurrently

--- packages/linode-js-sdk/src/account/types.ts
+++ packages/linode-js-sdk/src/account/types.ts
@@ -1,7 +1,17 @@
-interface CreditCard {
+export interface CreditCard {
   expiry: string;
   last_four: string;
   cvv?: string;
+}
+
+export interface ActivePromotion {
+  description: string;
+  summary: string;
+  expire_dt: string | null;
+  credit_remaining: string;
+  this_month_credit_remaining: string;
+  credit_monthly_cap: string;
+  image_url: string;
 }
 
 export interface Account {
@@ -21,6 +31,7 @@ export interface Account {
   city: string;
   phone: string;
   company: string;
+  active_promotions: ActivePromotion[];
   // [BETA]
   // @todo: Uncomment this when it becomes generally available
   // capabilities: AccountCapability[];
