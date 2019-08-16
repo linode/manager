d66b0bb0a191879396fc500a56b6bd9de48deae3
John Callahan <johnwcallahan@gmail.com>
2019-08-16 16:52:01 -0400

    Add customizeOptions method to IPSelect

--- packages/manager/src/components/IPSelect/IPSelect.tsx
+++ packages/manager/src/components/IPSelect/IPSelect.tsx
@@ -7,10 +7,12 @@ interface Props {
   linodeId: number;
   value: Item<string>;
   handleChange: (ip: string) => void;
+  customizeOptions?: (options: Item<string>[]) => Item<string>[];
+  errorText?: string;
 }
 
 interface WithLinodesProps {
-  linodesData: Linode.Linode[];
+  linode?: Linode.Linode;
   linodesLoading: boolean;
   linodesError?: Linode.ApiFieldError[];
 }
@@ -19,34 +21,41 @@ type CombinedProps = Props & WithLinodesProps;
 
 const IPSelect: React.FC<CombinedProps> = props => {
   const {
-    linodeId,
+    linode,
     value,
     handleChange,
-    linodesData,
     linodesLoading,
-    linodesError
+    linodesError,
+    customizeOptions
   } = props;
 
-  const thisLinode = linodesData.find(linode => linode.id === linodeId);
-  const ips = thisLinode ? [...thisLinode.ipv4, thisLinode.ipv6] : [];
+  const ips: string[] = [];
 
-  const options: Item<string>[] = [
-    {
-      label: 'Any',
-      value: 'any'
-    },
-    ...ips.map(ip => {
-      const removedRange = removeRange(ip);
-      return {
-        label: removedRange,
-        value: removedRange
-      };
-    })
-  ];
+  // If we have a Linode (from Redux) matching the given ID, use it's IPv4 address(es)
+  if (linode) {
+    ips.push(...linode.ipv4);
 
-  const errorText = linodesError
-    ? 'There was an error retrieving your IP addresses.'
-    : '';
+    // If the Linode has an IPv6 address, use it as well.
+    if (linode.ipv6) {
+      ips.push(linode.ipv6);
+    }
+  }
+
+  // Create React-Select-friendly options.
+  let options: Item<string>[] = ips.map(ip => ({ value: ip, label: ip }));
+
+  // If a customizeOptions function was provided, apply it here.
+  if (customizeOptions) {
+    options = customizeOptions(options);
+  }
+
+  let errorText = '';
+
+  if (props.errorText) {
+    errorText = props.errorText;
+  } else if (linodesError) {
+    errorText = "There was an error retrieving this Linode's IP addresses.";
+  }
 
   return (
     <Select
@@ -57,19 +66,21 @@ const IPSelect: React.FC<CombinedProps> = props => {
       onChange={(selected: Item<string>) => handleChange(selected.value)}
       errorText={errorText}
       isClearable={false}
+      placeholder="Select an IP Address..."
     />
   );
 };
 
 const enhanced = compose<CombinedProps, Props>(
-  withLinodes((ownProps, linodesData, linodesLoading, linodesError) => ({
-    ...ownProps,
-    linodesData,
-    linodesLoading,
-    linodesError
-  }))
+  withLinodes<WithLinodesProps, Props>(
+    (ownProps, linodesData, linodesLoading, linodesError) => ({
+      ...ownProps,
+      // Find the Linode in Redux that corresponds with the given ID
+      linode: linodesData.find(linode => linode.id === ownProps.linodeId),
+      linodesLoading,
+      linodesError
+    })
+  )
 );
 
 export default enhanced(IPSelect);
-
-const removeRange = (ip: string) => ip.slice(0, ip.indexOf('/'));
