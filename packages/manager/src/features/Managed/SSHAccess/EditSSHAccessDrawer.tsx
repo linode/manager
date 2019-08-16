b44bfc354d6cc10b095e446454bb32f90671fab5
John Callahan <johnwcallahan@gmail.com>
2019-08-16 16:53:22 -0400

    Finish SSH Access form

--- packages/manager/src/features/Managed/SSHAccess/EditSSHAccessDrawer.tsx
+++ packages/manager/src/features/Managed/SSHAccess/EditSSHAccessDrawer.tsx
@@ -1,18 +1,29 @@
-import { Formik } from 'formik';
+import { Formik, FormikActions } from 'formik';
 import * as React from 'react';
+import ActionsPanel from 'src/components/ActionsPanel';
+import Button from 'src/components/Button';
 import FormControlLabel from 'src/components/core/FormControlLabel';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Drawer from 'src/components/Drawer';
 import Grid from 'src/components/Grid';
 import IPSelect from 'src/components/IPSelect';
+import Notice from 'src/components/Notice';
 import TextField from 'src/components/TextField';
 import Toggle from 'src/components/Toggle';
-import { updateManagedLinodeSchema } from 'src/services/managed/managed.schema';
+// @todo: Extract these utils out of Volumes
+import {
+  handleFieldErrors,
+  handleGeneralErrors
+} from 'src/features/Volumes/VolumeDrawer/utils';
+import { updateLinodeSettings } from 'src/services/managed';
+import { removePrefixLength } from 'src/utilities/ipUtils';
+
+const DEFAULTS = {
+  user: 'root',
+  port: 22
+};
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    // width: '100%'
-  },
   ip: {
     [theme.breakpoints.down('sm')]: {
       paddingBottom: '0px !important'
@@ -28,6 +39,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 interface Props {
   isOpen: boolean;
   closeDrawer: () => void;
+  updateOne: (linodeSetting: Linode.ManagedLinodeSetting) => void;
   linodeSetting?: Linode.ManagedLinodeSetting;
 }
 
@@ -36,13 +48,51 @@ type CombinedProps = Props;
 const EditSSHAccessDrawer: React.FC<CombinedProps> = props => {
   const classes = useStyles();
 
-  const { isOpen, closeDrawer, linodeSetting } = props;
+  const { isOpen, closeDrawer, linodeSetting, updateOne } = props;
 
   const title = linodeSetting
     ? `Edit SSH Access for ${linodeSetting.label}`
     : 'Edit SSH Access';
 
-  const onSubmit = () => alert('submit');
+  const onSubmit = (
+    values: Linode.ManagedLinodeSetting,
+    {
+      setErrors,
+      setSubmitting,
+      setStatus
+    }: FormikActions<Linode.ManagedLinodeSetting>
+  ) => {
+    // It probably isn't possible to end up here without linodeSetting,
+    // but we'll include an early return to make TypeScript happy.
+    if (!linodeSetting) {
+      return;
+    }
+
+    // If the user has cleared this field, replace it with the default.
+    const port =
+      !values.ssh.port && values.ssh.port !== 0
+        ? DEFAULTS.port
+        : values.ssh.port;
+
+    updateLinodeSettings(linodeSetting.id, {
+      ssh: { ...values.ssh, port }
+    })
+      .then(updatedLinodeSetting => {
+        setSubmitting(false);
+        updateOne(updatedLinodeSetting);
+        closeDrawer();
+      })
+      .catch(err => {
+        setSubmitting(false);
+        const defaultMessage = `Unable to update SSH Access for this Linode. Please try again later.`;
+        const mapErrorToStatus = (generalError: string) =>
+          setStatus({ generalError });
+
+        setSubmitting(false);
+        handleFieldErrors(setErrors, err);
+        handleGeneralErrors(mapErrorToStatus, err, defaultMessage);
+      });
+  };
 
   return (
     <Drawer title={title} open={isOpen} onClose={closeDrawer}>
@@ -50,14 +100,13 @@ const EditSSHAccessDrawer: React.FC<CombinedProps> = props => {
         <>
           <Formik
             initialValues={{
-              access: linodeSetting.ssh.access,
-              user: linodeSetting.ssh.user || 'root',
-              port: linodeSetting.ssh.port || 22,
-              ip: linodeSetting.ssh.ip === 'any' ? 'Any' : linodeSetting.ssh.ip
+              ssh: {
+                access: linodeSetting.ssh.access,
+                user: linodeSetting.ssh.user,
+                ip: linodeSetting.ssh.ip,
+                port: linodeSetting.ssh.port
+              }
             }}
-            validationSchema={updateManagedLinodeSchema}
-            validateOnChange={false}
-            validateOnBlur={false}
             onSubmit={onSubmit}
           >
             {({
@@ -70,55 +119,97 @@ const EditSSHAccessDrawer: React.FC<CombinedProps> = props => {
               isSubmitting,
               setFieldValue
             }) => {
+              const { access, user, ip, port } = values.ssh;
+
+              const userError = errors['ssh.user'];
+
+              // API oddity: IP errors come back as {field: 'ip'} instead of {field: 'ssh.ip'} liked we'd expect.
+              // tslint:disable-next-line
+              const ipError = errors['ssh.user'] || errors['ip'];
+
+              const portError = errors['ssh.port'];
+
               return (
-                <form className={classes.root}>
-                  <FormControlLabel
-                    control={
-                      <Toggle
-                        name="access"
-                        checked={!values.access}
-                        onChange={() => setFieldValue('access', !values.access)}
-                      />
-                    }
-                    label={values.access ? 'Access disabled' : 'Access enabled'}
-                  />
+                <>
+                  {status && (
+                    <Notice key={status} text={status.generalError} error />
+                  )}
 
-                  <TextField
-                    name="user"
-                    label="User Account"
-                    value={values.user}
-                    error={!!errors.user}
-                    errorText={errors.user}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
+                  <form>
+                    <FormControlLabel
+                      control={
+                        <Toggle
+                          name="ssh.access"
+                          checked={!access}
+                          onChange={() => setFieldValue('ssh.access', !access)}
+                        />
+                      }
+                      label={access ? 'Access disabled' : 'Access enabled'}
+                    />
 
-                  <Grid container>
-                    <Grid item xs={12} md={8} className={classes.ip}>
-                      <IPSelect
-                        linodeId={linodeSetting.id}
-                        value={{
-                          label: values.ip === 'any' ? 'Any' : values.ip,
-                          value: values.ip
-                        }}
-                        handleChange={(ip: string) => setFieldValue('ip', ip)}
-                      />
+                    <TextField
+                      name="ssh.user"
+                      label="User Account"
+                      value={user}
+                      error={!!userError}
+                      errorText={userError}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder={user || DEFAULTS.user}
+                    />
+
+                    <Grid container>
+                      <Grid item xs={12} md={8} className={classes.ip}>
+                        <IPSelect
+                          linodeId={linodeSetting.id}
+                          value={{
+                            label: ip === 'any' ? 'Any' : ip,
+                            value: ip
+                          }}
+                          customizeOptions={options => [
+                            // The first option should always be "Any".
+                            {
+                              label: 'Any',
+                              value: 'any'
+                            },
+                            // Remove the prefix length from each option.
+                            ...options.map(option => ({
+                              label: removePrefixLength(option.value),
+                              value: removePrefixLength(option.value)
+                            }))
+                          ]}
+                          handleChange={(selectedIp: string) =>
+                            setFieldValue('ssh.ip', selectedIp)
+                          }
+                          errorText={ipError}
+                        />
+                      </Grid>
+
+                      <Grid item xs={12} md={4} className={classes.port}>
+                        <TextField
+                          name="ssh.port"
+                          label="Port"
+                          type="number"
+                          value={port}
+                          error={!!portError}
+                          errorText={portError}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          placeholder={String(port || DEFAULTS.port)}
+                        />
+                      </Grid>
                     </Grid>
-
-                    <Grid item xs={12} md={4} className={classes.port}>
-                      <TextField
-                        name="port"
-                        label="Port"
-                        type="number"
-                        value={values.port}
-                        error={!!errors.port}
-                        errorText={errors.port}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                    </Grid>
-                  </Grid>
-                </form>
+                    <ActionsPanel>
+                      <Button
+                        buttonType="primary"
+                        onClick={() => handleSubmit()}
+                        loading={isSubmitting}
+                      >
+                        Save
+                      </Button>
+                    </ActionsPanel>
+                  </form>
+                </>
               );
             }}
           </Formik>
