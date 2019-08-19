0daaac64a4aeb423fcb16b52766e9fa001480e11
John Callahan <johnwcallahan@gmail.com>
2019-08-19 15:54:13 -0400

    Move privateIPRegex to utility file

--- packages/manager/src/utilities/ipUtils.ts
+++ packages/manager/src/utilities/ipUtils.ts
@@ -4,3 +4,8 @@
  * @param ip The IPv6 address to remove the prefix length from.
  */
 export const removePrefixLength = (ip: string) => ip.replace(/\/\d+/, '');
+
+/**
+ * Regex for determining if a string is a private IP Addresses
+ */
+export const privateIPRegex = /^10\.|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-1]\.|^192\.168\.|^fd/;
