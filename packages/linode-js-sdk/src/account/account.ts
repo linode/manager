ce78149fad59857d542ff7b2fdc24a72ff383d9e
Martin <mmckenna@linode.com>
2019-08-20 13:37:37 -0400

    Add: APIv4 JS SDK (#5338)
    * adds js sdk project
    
    * transpiles project with babel instead of tsc
    
    * updates travisfile
    
    * updates travisfile
    
    * updates package json
    
    * upgrades ramda and explcitly uses commonjs build
    
    * adds webpack configs
    
    * removes unnecessary build steps
    
    * runs tsc and babel in watch mode concurrently
    
    * compiles lib files to commonjs
    
    * adds precommit and prepublish scripts
    
    * updates precommit script
    
    * updates precommit to lint
    
    * updates package json
    
    * updates package json
    
    * updates package.json
    
    * upgrades npm-dts-webpack-plugin
    
    * updates dockerfile
    
    * updates dockerfile
    
    * updates package.json volume mounting
    
    * updates mounted volume paths
    
    * allows for base url override

---
+++ packages/linode-js-sdk/src/account/account.ts
@@ -0,0 +1,16 @@
+import { API_ROOT } from '../constants';
+import Request, { setMethod, setURL } from '../request';
+import { Account } from './types';
+
+/**
+ * getAccountInfo
+ *
+ * Return account information,
+ * including contact and billing info.
+ *
+ */
+export const getAccountInfo = () => {
+  return Request<Account>(setURL(`${API_ROOT}/account`), setMethod('GET')).then(
+    response => response.data,
+  );
+};
