df05daaadf3e95c54a4e1e254c5f2a6b940ef699
Kayla Wilkins <kwilkins@linode.com>
2019-08-21 11:28:28 -0400

    adjustments to grid and styles for managed dashboard widget

--- packages/manager/src/features/Dashboard/ManagedDashboardCard/Unhealthy.tsx
+++ packages/manager/src/features/Dashboard/ManagedDashboardCard/Unhealthy.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import MonitorFailed from 'src/assets/icons/monitor-failed.svg';
 import Typography from 'src/components/core/Typography';
 import Grid from 'src/components/Grid';
-
 import { useStyles } from './Healthy';
 
 interface Props {
@@ -16,20 +15,25 @@ export const Unhealthy: React.FC<Props> = props => {
   const { monitorsDown } = props;
   return (
     <>
-      <Grid item xs={1} className={classes.icon}>
-        <MonitorFailed height={45} width={45} />
-      </Grid>
-      <Grid container item direction="column" justify="space-around">
-        <Grid item className={classes.text}>
-          <Typography variant="subtitle1">
-            {/** This does not quite match the designs, but we don't have a way of checking failure time yet. */}
-            <strong>
-              {monitorsDown} of your Managed Service Monitors{' '}
-              {monitorsDown === 1 ? 'has' : 'have'} failed.
-            </strong>
-          </Typography>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        className={classes.root}
+        spacing={0}
+        xs={12}
+      >
+        <Grid item>
+          <Grid item xs={12} className={classes.icon}>
+            <MonitorFailed height={48} width={48} />
+          </Grid>
         </Grid>
-        <Grid item className={classes.text}>
+        <Grid item className={classes.container}>
+          <Typography variant="h3" className={classes.header}>
+            {/** This does not quite match the designs, but we don't have a way of checking failure time yet. */}
+            {monitorsDown} of your Managed Service Monitors{' '}
+            {monitorsDown === 1 ? 'has' : 'have'} failed.
+          </Typography>
           <Typography>
             Please check your
             <Link to="/support/tickets"> Support tickets</Link> for details.
