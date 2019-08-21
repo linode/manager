68484f220e569f5aab7765251e8b3f83b68e1513
Martin <mmckenna@linode.com>
2019-08-21 13:25:56 -0400

    Add: Inter-DC Migrations (#5366)
    * adds initial migrate landing skeleton (#5333)
    
    * Inter-DC Migration Part 2: Abstract New Region Select (#5344)
    
    * adds current region
    
    * refactors region select and base select
    
    * removes unused component
    
    * Inter-DC Migration Part 3: Add Migration Logic and Error States (#5346)
    
    * adds migration logic
    
    * changes human-readable region formatting
    
    * updates text and disabled states
    
    * safe access image.id
    
    * Inter-DC Migration Part 4: Add CTA on Advanced Tab with GA Integration (#5350)
    
    * adds migration CTA on Linode detail advanced tab
    
    * fixes linode detail progress and adds migration imminent banner
    
    * fixes country code issue and removes client-side restrictions
    
    previously we were restricting more than 1 migration at a time, but now API will handle those restrictions
    
    * adds unit tests
    
    * changes ga event metadata
    
    * updates copy

--- packages/manager/src/components/SelectRegionPanel/index.ts
+++ packages/manager/src/components/SelectRegionPanel/index.ts
@@ -1,7 +1,1 @@
-import SelectRegionPanel, {
-  ExtendedRegion as _ExtendedRegion
-} from './SelectRegionPanel';
-/* tslint:disable */
-export interface ExtendedRegion extends _ExtendedRegion {}
-
-export default SelectRegionPanel;
+export { default } from './SelectRegionPanel';
