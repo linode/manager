09b3c9a6883cfcf39ead1c963a46cb26d2d85a4c
Jason <jstamis@linode.com>
2019-08-22 17:35:55 -0400

    Modified scripts
    Updated automated tests to run with lerna commands so that we can still use yarn "xyz" command to run testing scripts
    Tests still need to be updated to run with V5 of webdriver
    Updated Breadcrumb and AccessPanel tests
    
    Copied over previous V5 fixes
    Updated AccessPanel formatting
    Updated ActionMenu story
    
    All tests are passing
    Updated CircleProgress
    
    All tests are passing
    Updated Checkbox test
    Updated  ConfirmationDialog test
    Updated DebouncedSearchTextField tests
    Updated Drawer test
    Updated EditableText test
    Updated EnhancedSelect test
    Updated EntityIcon test
    Updated ErrorState test
    Updated ExpansionPanel test
    Updated HelpIcon test
    Updated IconLinkText
    Upgraded LinearProgress test
    Updated Notice test
    Updated PaginationControls test
    Updated PasswordInput test
    Updated Radio test

--- packages/manager/src/components/EditableText/EditableText.spec.js
+++ packages/manager/src/components/EditableText/EditableText.spec.js
@@ -1,52 +1,94 @@
 const { navigateToStory } = require('../../../e2e/utils/storybook');
 
-describe('Editable Text Suite', () => {
+describe('Editable Text', () => {
     const component = 'Editable Text';
     const childStories = [
         'Headline %26 Title',
     ];
 
-    const editableTextField = '[data-qa-editable-text]';
-    const editableTextButton = '[data-qa-edit-button]';
-    const editField = '[data-qa-edit-field]';
-    const saveEdit = '[data-qa-save-edit]';
-    const cancelEdit = '[data-qa-cancel-edit]';
+    const editableTextField = '[data-testid="editable-text"]';
+    const editableTextButton = '[data-qa-edit-button=true]';
+    const editField = '[data-qa-edit-field="true"]';
+    const saveEdit = '[data-qa-save-edit="true"]';
+    const cancelEdit = '[data-qa-cancel-edit="true"]';
     const newLabel = 'someNewValue!';
+    const shouldString = 'should have been';
+    const shouldNotString = 'should not have been'
+
 
     let originalLabel;
 
-    beforeAll(() => {
+    beforeEach(() => {
         navigateToStory(component, childStories[0]);
-        browser.waitForVisible(editableTextField);
+        $(editableTextField).waitForDisplayed();
     });
 
     it('should become an editable field on click', () => {
         originalLabel = $(editableTextField).getText();
-
-        $(editableTextField).moveToObject();
-        browser.waitForVisible(editableTextButton);
-
+        //checking that editable text values are not displayed
+        expect($(editField).isDisplayed())
+          .withContext(`Edit field ${shouldNotString} enabled`).toBe(false);
+        expect($(saveEdit).isDisplayed())
+          .withContext(`Save button ${shouldNotString} enabled`).toBe(false);
+        expect($(cancelEdit).isDisplayed())
+          .withContext(`Cancel button ${shouldNotString} enabled`).toBe(false);
+        $(editableTextField).click();
+        //Edit text button should be displayed after clicking editable text
+        expect($(editableTextButton).isDisplayed())
+          .withContext(`Edit text ${shouldString} displayed`).toBe(true);
         $(editableTextButton).click();
-        browser.waitForVisible(editField);
+        $(editField).waitForDisplayed();
+        //checking that edit field is enabled and save and cancel buttons are displayed
+        expect($(editField).isEnabled())
+          .withContext(`Edit field ${shouldString} enabled`).toBe(true);
+        expect($(saveEdit).isDisplayed())
+          .withContext(`Save button ${shouldString} displayed`).toBe(true);
+        expect($(cancelEdit).isDisplayed())
+          .withContext(`Cancel button ${shouldString} displayed`).toBe(true);
     });
 
-    it('should edit the textfield', () => {
+    it('should edit the text field with save button', () => {
+        setUpEditField();
         $(editField).$('input').setValue(newLabel);
-        browser.click(saveEdit);
-        expect($(editableTextField).getText()).toBe(newLabel);
+        $(saveEdit).click();
+        //check that the edit field value is set correctly
+        expect($(editableTextField).getText())
+          .withContext(`Field text ${shouldString}: '${newLabel}'`).toBe(newLabel);
     });
 
-    it('should not update the textfield on cancel', () => {
-        $(editableTextField).moveToObject();
-        browser.waitForVisible(editableTextButton);
+    it('should edit the text field with the enter key', () => {
+        setUpEditField();
+        $(editField).$('input').setValue(newLabel);
+        browser.keys('\uE007');
+        //check that the edit field value is set correctly
+        expect($(editableTextField).getText())
+          .withContext(`Field text ${shouldString}: '${newLabel}'`).toBe(newLabel);
+    });
 
-        $(editableTextButton).click();
-        $(cancelEdit).waitForVisible();
-
-        $(editField).$('input').setValue(originalLabel);
-        browser.click(cancelEdit);
-
+    it('should not update the text field on cancel', () => {
+        originalLabel = $(editableTextField).getText();
+        setUpEditField()
+        $(cancelEdit).waitForDisplayed();
+        $(editField).$('input').setValue('sadfgertg');
+        $(cancelEdit).click();
         // Should contain the new label from the prior test
-        expect($(editableTextField).getText()).toBe(newLabel);
+        expect($(editableTextField).getText())
+          .withContext(`Field text ${shouldString}: '${originalLabel}'`).toBe(originalLabel);
     });
+
+    function setUpEditField(){
+        $(editableTextField).click();
+        //Edit text button should be displayed after clicking editable text
+        expect($(editableTextButton).isDisplayed())
+          .withContext(`Edit text ${shouldString} displayed`).toBe(true);
+        $(editableTextButton).click();
+        $(editField).waitForDisplayed();
+        //checking that edit field is enabled and save and cancel buttons are displayed
+        expect($(editField).isEnabled())
+          .withContext(`Edit field ${shouldString} enabled`).toBe(true);
+        expect($(saveEdit).isDisplayed())
+          .withContext(`Save button ${shouldString} displayed`).toBe(true);
+        expect($(cancelEdit).isDisplayed())
+          .withContext(`Cancel button ${shouldString} displayed`).toBe(true);
+    }
 });
--- packages/manager/src/components/HelpIcon/HelpIcon.spec.js
+++ packages/manager/src/components/HelpIcon/HelpIcon.spec.js
@@ -1,36 +1,42 @@
 const { executeInAllStories } = require('../../../e2e/utils/storybook');
 
 describe('Help Icon Component Suite', () => {
-    const component = 'HelpIcon';
-    const childStories = [
-        'default',
-        'center',
-        'left',
-        'right',
-    ]
+  const component = 'HelpIcon';
+  const childStories = [
+    'default',
+    'center',
+    'left',
+    'right',
+  ]
 
-    const helpButton = '[data-qa-help-button]';
-    const popoverText = '[role="tooltip"]';
+  const helpButton = '[data-qa-help-button]';
+  const popoverText = '[role="tooltip"]';
 
-    it('should display icon on the page', () => {
-        executeInAllStories(component, childStories, () => {
-            browser.waitForVisible(helpButton);
-        });
+  it('should display icon on the page', () => {
+    executeInAllStories(component, childStories, () => {
+        $(helpButton).waitForDisplayed();
     });
+  });
 
-    it('should display popover text on Mouse Over', () => {
-        executeInAllStories(component, childStories, () => {
-            browser.waitForVisible(helpButton);
-            browser.moveToObject(helpButton);
-            expect($(helpButton).getAttribute('aria-describedby')).not.toBeNull();
-            expect($(popoverText).getText()).toBe('There is some help text! Yada, yada, yada...');
-        });
+  it('should display popover text on Mouse Over', () => {
+    executeInAllStories(component, childStories, () => {
+      $(helpButton).waitForDisplayed();
+      $(helpButton).click();
+      expect($(helpButton).getAttribute('aria-describedby'))
+        .withContext(`aria-describedby attribute is missing`)
+        .not.toBeNull();
+      expect($(popoverText).getText())
+        .withContext(`Incorrect hover text`)
+        .toBe('There is some help text! Yada, yada, yada...');
     });
+  });
 
-    it('should hide popover text on Mouse Out', () => {
-        executeInAllStories(component, childStories, () => {
-            browser.waitForVisible(helpButton);
-            expect($(helpButton).getAttribute('aria-describedby')).toBeNull();
-        });
+  it('should hide popover text on Mouse Out', () => {
+    executeInAllStories(component, childStories, () => {
+      $(helpButton).waitForDisplayed();
+      expect($(helpButton).getAttribute('aria-describedby'))
+        .withContext(`aria-describedby attribute should be empty`)
+        .toBeNull();
     });
+  });
 });
