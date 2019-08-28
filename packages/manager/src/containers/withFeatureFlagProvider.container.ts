4945fc6e3d5366161c16b8b68e988049e7e50218
mmckenna <mmckenna.phila@gmail.com>
2019-08-28 15:06:23 -0400

    adds ga events for inter-dc migrations

--- packages/manager/src/containers/withFeatureFlagProvider.container.ts
+++ packages/manager/src/containers/withFeatureFlagProvider.container.ts
@@ -5,7 +5,7 @@ import { LAUNCH_DARKLY_API_KEY } from 'src/constants';
 export { useLDClient };
 
 export default withLDProvider({
-  clientSideID: LAUNCH_DARKLY_API_KEY,
+  clientSideID: '1234' || LAUNCH_DARKLY_API_KEY,
   /**
    * Initialize the app with an anonymous user.
    */
