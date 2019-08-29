b20257c8375cb7d7a17a0f610a30425f8f7e1cd6
John Callahan <johnwcallahan@gmail.com>
2019-08-29 17:21:05 -0400

    OBJ-58 Part II: Bucket Detail page (#5403)
    * Initial pass at Object table
    
    * clean
    
    * Add object icon
    
    * Remove pagination

--- packages/manager/src/features/ObjectStorage/BucketDetail/BucketDetail.tsx
+++ packages/manager/src/features/ObjectStorage/BucketDetail/BucketDetail.tsx
@@ -1,20 +1,93 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-// import { makeStyles, Theme } from 'src/components/core/styles';
+import Breadcrumb from 'src/components/Breadcrumb';
+import Box from 'src/components/core/Box';
+import Divider from 'src/components/core/Divider';
+import Paper from 'src/components/core/Paper';
+import { makeStyles, Theme } from 'src/components/core/styles';
+import TableBody from 'src/components/core/TableBody';
+import TableHead from 'src/components/core/TableHead';
+import DocumentationButton from 'src/components/DocumentationButton';
+import Table from 'src/components/Table';
+import TableCell from 'src/components/TableCell';
+import TableRow from 'src/components/TableRow';
+import { useAPIRequest } from 'src/hooks/useAPIRequest';
+import { getBucketObjects } from 'src/services/objectStorage/buckets';
+import ObjectTableContent from './ObjectTableContent';
 
-// const useStyles = makeStyles((theme: Theme) => ({
-//   root: {}
-// }));
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {},
+  divider: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(2),
+    backgroundColor: theme.color.grey3
+  },
+  objectTable: {
+    marginTop: theme.spacing(4)
+  },
+  objectNameColumn: {
+    width: '50%'
+  }
+}));
 
 type CombinedProps = RouteComponentProps<{
-  regionId: string;
+  clusterId: Linode.ClusterID;
   bucketName: string;
 }>;
 
 const BucketDetail: React.FC<CombinedProps> = props => {
-  // const classes = useStyles();
+  const { clusterId, bucketName } = props.match.params;
 
-  return <>Bucket Detail for {props.match.params.bucketName}</>;
+  const { data, error, loading } = useAPIRequest<Linode.Object[]>(
+    () =>
+      getBucketObjects(clusterId, bucketName).then(response => response.data),
+    []
+  );
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Breadcrumb
+          // The actual pathname doesn't match what we want in the Breadcrumb,
+          // so we create a custom one.
+          pathname={`/object-storage/${bucketName}`}
+          crumbOverrides={[
+            {
+              position: 1,
+              label: 'Object Storage'
+            }
+          ]}
+          labelOptions={{ noCap: true }}
+        />
+        {/* @todo: What should this link be? */}
+        <DocumentationButton href="https://www.linode.com/docs/platform/object-storage/how-to-use-object-storage/" />
+      </Box>
+      <Divider className={classes.divider} />
+      <Paper className={classes.objectTable}>
+        <Table removeLabelonMobile aria-label="List of Bucket Objects">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.objectNameColumn}>Object</TableCell>
+              <TableCell>Size</TableCell>
+              <TableCell>Region</TableCell>
+              <TableCell>Last Modified</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <ObjectTableContent
+              clusterId={clusterId}
+              bucketName={bucketName}
+              data={data}
+              loading={loading}
+              error={error}
+            />
+          </TableBody>
+        </Table>
+      </Paper>
+    </>
+  );
 };
 
 export default BucketDetail;
