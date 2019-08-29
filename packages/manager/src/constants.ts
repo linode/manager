b4e75b3265c2282720da9a44e95f87be77c323a1
Austin Balarin <abalarin@outlook.com>
2019-08-29 07:24:48 -0400

    [M3-3177] Change: region block storage refactor (#5401)
    * region bloack storage refactor
    
    * removes compose from export, changes block storage capability check with exact string match

--- packages/manager/src/constants.ts
+++ packages/manager/src/constants.ts
@@ -174,9 +174,6 @@ export const dcContinent: Record<string, ContinentKey> = {
   'ap-west': 'AS'
 };
 
-// At this time, the following regions do not support block storage.
-export const regionsWithoutBlockStorage = ['us-southeast', 'ap-northeast-1a'];
-
 // Default error message for non-API errors
 export const DEFAULT_ERROR_MESSAGE = 'An unexpected error occurred.';
 
