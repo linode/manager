a725c06c07c02c9f8fbfaef994069f8cead4c057
Jared <jaredkobos@gmail.com>
2019-09-03 09:39:41 -0400

    M3-2717 Part I: Add NB config nodes to Redux (TDT) (#5340)
    * Add NB configs container
    
    * Add nodebal config nodes to redux
    
    * Rename nbconfignode actions to reflect that they're for nodes not configs

---
+++ packages/manager/src/__data__/nodeBalConfigs.ts
@@ -0,0 +1,32 @@
+export const nodes: Linode.NodeBalancerConfigNode[] = [
+  {
+    label: 'config 1',
+    nodebalancer_id: 642,
+    mode: 'accept',
+    address: '192.168.160.160:80',
+    status: 'UP',
+    config_id: 1,
+    id: 571,
+    weight: 100
+  },
+  {
+    label: 'config 2',
+    nodebalancer_id: 642,
+    mode: 'accept',
+    address: '192.168.160.160:80',
+    status: 'UP',
+    config_id: 1,
+    id: 572,
+    weight: 100
+  },
+  {
+    label: 'config 3',
+    nodebalancer_id: 642,
+    mode: 'accept',
+    address: '192.168.160.160:80',
+    status: 'UP',
+    config_id: 1,
+    id: 573,
+    weight: 100
+  }
+];
