b72c397b70449830a3405c6654d6ebc8cbea7186
mmckenna <mmckenna.phila@gmail.com>
2019-09-04 15:44:35 -0400

    updates mock adapter

--- packages/manager/src/components/TagsInput/TagsInput.stories.tsx
+++ packages/manager/src/components/TagsInput/TagsInput.stories.tsx
@@ -1,14 +1,10 @@
 import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-
-import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-
-import { API_ROOT } from '../../constants';
-
-import TagsInput from './TagsInput';
-
+import { baseRequest } from 'linode-js-sdk/lib/request';
+import * as React from 'react';
 import { Item } from 'src/components/EnhancedSelect/Select';
+import { API_ROOT } from '../../constants';
+import TagsInput from './TagsInput';
 
 const API_REQUEST = `${API_ROOT}/tags`;
 
@@ -36,7 +32,7 @@ class TagsInputDemo extends React.Component<{}, {}> {
 
 storiesOf('Tags Input', module)
   .add('Tags Input', () => {
-    const mock = new MockAdapter(axios);
+    const mock = new MockAdapter(baseRequest);
 
     mock.onGet(API_REQUEST).reply(200, {
       data: ['tag1', 'tag2', 'tag3', 'tag4'].map(tag => ({ label: tag }))
@@ -44,7 +40,7 @@ storiesOf('Tags Input', module)
     return <TagsInputDemo />;
   })
   .add('Tags Input with an error', () => {
-    const mock = new MockAdapter(axios);
+    const mock = new MockAdapter(baseRequest);
 
     mock.onGet(API_REQUEST).reply(500, {});
     return <TagsInputDemo />;
