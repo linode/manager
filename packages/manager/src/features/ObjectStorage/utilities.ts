822ad771299056e5c82682334c29259cfb5771fd
John Callahan <johnwcallahan@gmail.com>
2019-09-10 14:44:09 -0400

    OBJ-58 Part III: Folder Traversal (#5440)
    * Pagination and traversal
    
    * Refactor for clarity
    
    * Remove unused function and comment
    
    * Add comment
    
    * Add tests and comments
    
    * Review feedback
    
    - Use constant for delimiter
    - Change copy for empty folder state
    - Clear data before requesting new data due to prefix change
    
    * Use APIError from SDK

--- packages/manager/src/features/ObjectStorage/utilities.ts
+++ packages/manager/src/features/ObjectStorage/utilities.ts
@@ -1,4 +1,4 @@
-import { OBJECT_STORAGE_ROOT } from 'src/constants';
+import { OBJECT_STORAGE_DELIMITER, OBJECT_STORAGE_ROOT } from 'src/constants';
 
 export const generateObjectUrl = (
   clusterId: Linode.ClusterID,
@@ -9,5 +9,62 @@ export const generateObjectUrl = (
   return {
     path,
     absolute: 'https://' + path
+  };
+};
+
+// If an Object does not have an etag, last_modified, owner, or size, it can
+// be considered a "folder".
+export const isFolder = (object: Linode.Object) =>
+  !object.etag && !object.last_modified && !object.owner && !object.size;
+
+/**
+ * Returns the basename of the given path.
+ *
+ * @example
+ * basename('my-path/folder/test.txt') // test.txt
+ */
+export const basename = (
+  path: string,
+  delimiter = OBJECT_STORAGE_DELIMITER
+): string => {
+  const idx = path.lastIndexOf(delimiter);
+
+  // If the delimiter is not found in the string, there's nothing to do.
+  if (idx === -1) {
+    return path;
+  }
+
+  return path.substr(idx + 1);
+};
+
+export interface ExtendedObject extends Linode.Object {
+  _isFolder: boolean;
+  _displayName: string;
+  _shouldDisplayObject: boolean;
+}
+
+export const extendObject = (
+  object: Linode.Object,
+  prefix: string
+): ExtendedObject => {
+  const _isFolder = isFolder(object);
+
+  // In many cases, we don't want to display the entire object name, since
+  // it could include a path, like "my-folder/another-folder/file.txt". We only
+  // want the basename (the part after the last slash). If the object ends with
+  // a slash (i.e. the object is a folder), get the basename, ignoring the
+  // trailing slash.
+  const _displayName = object.name.endsWith('/')
+    ? basename(object.name.substr(0, object.name.length - 1))
+    : basename(object.name);
+
+  return {
+    ...object,
+    _isFolder,
+    _displayName,
+    // If we're in a folder called "my-folder", we don't want to show the object
+    // called "my-folder/". We can look at the prefix to make this decision,
+    // since it will also be "my-folder/".
+    _shouldDisplayObject: object.name !== prefix
   };
 };
