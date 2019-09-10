48ed8cc58cba4950effbb9582e0cb02eeb624066
Jason Stamis <jstamis@linode.com>
2019-09-10 11:36:30 -0400

    updated billing page

--- packages/manager/e2e/utils/assertionLog.js
+++ packages/manager/e2e/utils/assertionLog.js
@@ -2,6 +2,8 @@ export const assertLog = {
   displayed: 'should be displayed',
   notDisplayed: 'should not be displayed',
   incorrectText: 'incorrect text found',
+  incorrectRegExVal: 'incorrect value found for regex with',
   incorrectNum: 'incorrect number of items',
-  incorrectVal: 'incorrect value found'
+  incorrectVal: 'incorrect value found',
+  shouldNotBeNull: 'value should not be null'
 }
