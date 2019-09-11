08e81a63c1679bd71a3371d9520a14bd1ceabb20
Jason Stamis <jstamis@linode.com>
2019-09-11 14:58:05 -0400

    stackscripts detail page updated, formatted, and withContext added

--- packages/manager/e2e/pageobjects/stackscripts/stackscript-detail.page.js
+++ packages/manager/e2e/pageobjects/stackscripts/stackscript-detail.page.js
@@ -2,56 +2,72 @@ const { constants } = require('../../constants');
 import Page from '../page';
 
 class StackScriptDetail extends Page {
-    get stackScriptTitleElement() { return $('[data-qa-stack-title]'); }
-    get stackScriptAuthorElement() { return $('[data-qa-stack-author]'); }
-    get stackScriptDeployments() { return $('[data-qa-stack-deployments]'); }
-    get deployStackScriptButton() { return $('[data-qa-stack-deploy]'); }
-    get stackScriptDescription() { return $('[data-qa-stack-description]'); }
-    get compatibleDistributions() { return $('[data-qa-compatible-distro]'); }
-    get stackScriptCode() { return $('[data-qa-script-code]'); }
-    get stackScript() { return $$('[data-qa-script]'); }
+  get stackScriptTitleElement() {
+    return $('[data-qa-stack-title]');
+  }
+  get stackScriptAuthorElement() {
+    return $('[data-qa-stack-author]');
+  }
+  get stackScriptDeployments() {
+    return $('[data-qa-stack-deployments]');
+  }
+  get deployStackScriptButton() {
+    return $('[data-qa-stack-deploy]');
+  }
+  get stackScriptDescription() {
+    return $('[data-qa-stack-description]');
+  }
+  get compatibleDistributions() {
+    return $('[data-qa-compatible-distro]');
+  }
+  get stackScriptCode() {
+    return $('[data-qa-script-code]');
+  }
+  get stackScript() {
+    return $$('[data-qa-script]');
+  }
 
-    stackScriptDetailPageDisplays() {
-        this.breadcrumbBackLink.waitForVisible(constants.wait.normal);
-        this.breadcrumbStaticText.waitForVisible(constants.wait.normal);
-        this.deployStackScriptButton.waitForVisible(constants.wait.normal);
-        this.stackScriptDetailDisplays();
-    }
+  stackScriptDetailPageDisplays() {
+    this.breadcrumbBackLink.waitForDisplayed(constants.wait.normal);
+    this.breadcrumbStaticText.waitForDisplayed(constants.wait.normal);
+    this.deployStackScriptButton.waitForDisplayed(constants.wait.normal);
+    this.stackScriptDetailDisplays();
+  }
 
-    stackScriptDetailDrawerDisplays() {
-        this.drawerBase.waitForVisible(constants.wait.normal);
-        this.drawerTitle.waitForVisible(constants.wait.normal);
-        this.stackScriptDetailDisplays();
-    }
+  stackScriptDetailDrawerDisplays() {
+    this.drawerBase.waitForDisplayed(constants.wait.normal);
+    this.drawerTitle.waitForDisplayed(constants.wait.normal);
+    this.stackScriptDetailDisplays();
+  }
 
-    stackScriptDetailDisplays(){
-        this.stackScriptTitleElement.waitForVisible(constants.wait.normal);
-        this.stackScriptAuthorElement.waitForVisible(constants.wait.normal);
-        this.stackScriptDeployments.waitForVisible(constants.wait.normal);
-        this.compatibleDistributions.waitForVisible(constants.wait.normal);
-        this.stackScriptCode.waitForVisible(constants.wait.normal);
-    }
+  stackScriptDetailDisplays() {
+    this.stackScriptTitleElement.waitForDisplayed(constants.wait.normal);
+    this.stackScriptAuthorElement.waitForDisplayed(constants.wait.normal);
+    this.stackScriptDeployments.waitForDisplayed(constants.wait.normal);
+    this.compatibleDistributions.waitForDisplayed(constants.wait.normal);
+    this.stackScriptCode.waitForDisplayed(constants.wait.normal);
+  }
 
-    stackScriptTitle(stackScriptTitle){
-          const selector = this.stackScriptTitleElement.selector.replace(']','');
-          return $(`${selector}="${stackScriptTitle}"]`);
-    }
+  stackScriptTitle(stackScriptTitle) {
+    const selector = this.stackScriptTitleElement.selector.replace(']', '');
+    return $(`${selector}="${stackScriptTitle}"]`);
+  }
 
-    stackScriptAuthor(stackScriptAuthor){
-        const selector = this.stackScriptAuthorElement.selector.replace(']','');
-        return $(`${selector}="${stackScriptAuthor}"]`);
-    }
+  stackScriptAuthor(stackScriptAuthor) {
+    const selector = this.stackScriptAuthorElement.selector.replace(']', '');
+    return $(`${selector}="${stackScriptAuthor}"]`);
+  }
 
-    getStackScriptCompatibleDistributions(){
-        this.compatibleDistributions.waitForVisible(constants.wait.normal);
-        const distroListText = this.compatibleDistributions.getText();
-        const cleanText = distroListText.replace('Compatible with: ','');
-        return cleanText.split(',').map(distro => distro.trim());
-    }
+  getStackScriptCompatibleDistributions() {
+    this.compatibleDistributions.waitForDisplayed(constants.wait.normal);
+    const distroListText = this.compatibleDistributions.getText();
+    const cleanText = distroListText.replace('Compatible with: ', '');
+    return cleanText.split(',').map(distro => distro.trim());
+  }
 
-    getStackScriptCode(){
-        return this.stackScript.map(script => script.getText());
-    }
+  getStackScriptCode() {
+    return this.stackScript.map(script => script.getText());
+  }
 }
 
 export default new StackScriptDetail();
