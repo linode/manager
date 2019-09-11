3ce878520bb9b115981cb2fe0d8530e590f0a23e
Jason Stamis <jstamis@linode.com>
2019-09-11 15:11:04 -0400

    support: landing, search-landing, and tickets pages updated, formatted, and withContext added

--- packages/manager/e2e/pageobjects/support/tickets.page.js
+++ packages/manager/e2e/pageobjects/support/tickets.page.js
@@ -1,70 +1,163 @@
 const { constants } = require('../../constants');
+const { assertLog } = require('../../utils/assertionLog');
 
 import Page from '../page';
 
 export class SupportTickets extends Page {
-    get openTicketsTab() { return $('[data-qa-tab="Open Tickets"]'); }
-    get closedTicketsTab() { return $('[data-qa-tab="Closed Tickets"]'); }
-    get supportHeader() { return $(this.breadcrumbStaticText.selector); }
-    get openTicketButton() { return this.addIcon('Open New Ticket'); }
+  get openTicketsTab() {
+    return $('[data-qa-tab="Open Tickets"]');
+  }
+  get closedTicketsTab() {
+    return $('[data-qa-tab="Closed Tickets"]');
+  }
+  get supportHeader() {
+    return $(this.breadcrumbStaticText.selector);
+  }
+  get openTicketButton() {
+    return this.addIcon('Open New Ticket');
+  }
 
-    get supportCreateDateHeader() { return $('[data-qa-support-date-header]'); }
-    get supportSubjectHeader() { return $('[data-qa-support-subject-header]'); }
-    get supportIdHeader() { return $('[data-qa-support-id-header]'); }
-    get supportEntityHeader() { return $('[data-qa-support-regarding-header]'); }
-    get supportUpdateHeader() { return $('[data-qa-support-updated-header]'); }
+  get supportCreateDateHeader() {
+    return $('[data-qa-support-date-header]');
+  }
+  get supportSubjectHeader() {
+    return $('[data-qa-support-subject-header]');
+  }
+  get supportIdHeader() {
+    return $('[data-qa-support-id-header]');
+  }
+  get supportEntityHeader() {
+    return $('[data-qa-support-regarding-header]');
+  }
+  get supportUpdateHeader() {
+    return $('[data-qa-support-updated-header]');
+  }
 
-    get supportTicket() { return $('[data-qa-support-ticket]'); }
-    get supportTickets() { return $$('[data-qa-support-ticket]'); }
-    get supportSubject() { return $('[data-qa-support-subject]'); }
-    get supportId() { return $('[data-qa-support-id]'); }
-    get supportEntity() { return $('[data-qa-support-entity]'); }
-    get supportCreateDate() { return $('[data-qa-support-date]'); }
-    get supportUpdateDate() { return $('[data-qa-support-updated]'); }
+  get supportTicket() {
+    return $('[data-qa-support-ticket]');
+  }
+  get supportTickets() {
+    return $$('[data-qa-support-ticket]');
+  }
+  get supportSubject() {
+    return $('[data-qa-support-subject]');
+  }
+  get supportId() {
+    return $('[data-qa-support-id]');
+  }
+  get supportEntity() {
+    return $('[data-qa-support-entity]');
+  }
+  get supportCreateDate() {
+    return $('[data-qa-support-date]');
+  }
+  get supportUpdateDate() {
+    return $('[data-qa-support-updated]');
+  }
 
-    get ticketHelpText() { return $('[data-qa-support-ticket-helper-text]'); }
-    get ticketEntityType() { return $('[data-qa-ticket-entity-type]'); }
-    get ticketSummary() { return $('[data-qa-ticket-summary] input'); }
-    get ticketDescription() { return $('[data-qa-ticket-description] textarea'); }
+  get ticketHelpText() {
+    return $('[data-qa-support-ticket-helper-text]');
+  }
+  get ticketEntityType() {
+    return $('[data-qa-ticket-entity-type]');
+  }
+  get ticketSummary() {
+    return $('[data-qa-ticket-summary] input');
+  }
+  get ticketDescription() {
+    return $('[data-qa-ticket-description] textarea');
+  }
 
-    get submit() { return $(this.submitButton.selector); }
-    get cancel() { return $(this.cancelButton.selector); }
+  get submit() {
+    return $(this.submitButton.selector);
+  }
+  get cancel() {
+    return $(this.cancelButton.selector);
+  }
 
-    baseElemsDisplay() {
-        this.supportHeader.waitForVisible(constants.wait.normal);
-        this.openTicketsTab.waitForVisible(constants.wait.normal);
+  baseElemsDisplay() {
+    this.supportHeader.waitForDisplayed(constants.wait.normal);
+    this.openTicketsTab.waitForDisplayed(constants.wait.normal);
 
-        expect(this.closedTicketsTab.isVisible()).toBe(true);
-        expect(this.openTicketButton.isVisible()).toBe(true);
-        expect(this.supportCreateDateHeader.isVisible()).toBe(true);
-        expect(this.supportSubjectHeader.isVisible()).toBe(true);
-        expect(this.supportIdHeader.isVisible()).toBe(true);
-        expect(this.supportEntityHeader.isVisible()).toBe(true);
-        expect(this.supportUpdateHeader.isVisible()).toBe(true);
-    }
+    expect(this.closedTicketsTab.isDisplayed())
+      .withContext(
+        `"${this.closedTicketsTab.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.openTicketButton.isDisplayed())
+      .withContext(
+        `"${this.openTicketButton.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.supportCreateDateHeader.isDisplayed())
+      .withContext(
+        `"${this.supportCreateDateHeader.selector}" selector ${
+          assertLog.displayed
+        }`
+      )
+      .toBe(true);
+    expect(this.supportSubjectHeader.isDisplayed())
+      .withContext(
+        `"${this.supportSubjectHeader.selector}" selector ${
+          assertLog.displayed
+        }`
+      )
+      .toBe(true);
+    expect(this.supportIdHeader.isDisplayed())
+      .withContext(
+        `"${this.supportIdHeader.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.supportEntityHeader.isDisplayed())
+      .withContext(
+        `"${this.supportEntityHeader.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.supportUpdateHeader.isDisplayed())
+      .withContext(
+        `"${this.supportUpdateHeader.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+  }
 
-    openCreateTicketDrawer() {
-        this.openTicketButton.click();
-        this.drawerTitle.waitForVisible(constants.wait.normal);
-        this.ticketHelpText.waitForVisible(constants.wait.normal);
+  openCreateTicketDrawer() {
+    this.openTicketButton.click();
+    this.drawerTitle.waitForDisplayed(constants.wait.normal);
+    this.ticketHelpText.waitForDisplayed(constants.wait.normal);
 
-        expect(this.ticketEntityType.isVisible()).toBe(true);
-        expect(this.ticketSummary.isVisible()).toBe(true);
-        expect(this.ticketDescription.isVisible()).toBe(true);
-        expect(this.submit.isVisible()).toBe(true);
-        expect(this.cancel.isVisible()).toBe(true);
-    }
+    expect(this.ticketEntityType.isDisplayed())
+      .withContext(
+        `"${this.ticketEntityType.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.ticketSummary.isDisplayed())
+      .withContext(
+        `"${this.ticketSummary.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.ticketDescription.isDisplayed())
+      .withContext(
+        `"${this.ticketDescription.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.submit.isDisplayed())
+      .withContext(`"${this.submit.selector}" selector ${assertLog.displayed}`)
+      .toBe(true);
+    expect(this.cancel.isDisplayed())
+      .withContext(`"${this.cancel.selector}" selector ${assertLog.displayed}`)
+      .toBe(true);
+  }
 
-    openTicket(ticketObj) {
-        this.ticketEntityType.click();
-        browser.waitForVisible('[data-value]', constants.wait.normal);
-        browser.click(`[data-value="${ticketObj.entity}"]`);
-        browser.waitForVisible('[data-value]', constants.wait.normal, true);
+  openTicket(ticketObj) {
+    this.ticketEntityType.click();
+    $('[data-value]').waitForDisplayed(constants.wait.normal);
+    $(`[data-value="${ticketObj.entity}"]`).click();
+    $('[data-value]').waitForDisplayed(constants.wait.normal, true);
 
-        this.ticketSummary.setValue(ticketObj.summary);
-        this.ticketDescription.setValue(ticketObj.description);
+    this.ticketSummary.setValue(ticketObj.summary);
+    this.ticketDescription.setValue(ticketObj.description);
 
-        this.submit.click();
-    }
+    this.submit.click();
+  }
 }
 export default new SupportTickets();
