6e13e26590f4df15e887622a6467afe3cbe9f711
Jason Stamis <jstamis@linode.com>
2019-09-11 11:39:35 -0400

    updated and formatted linode details resisze page

--- packages/manager/e2e/pageobjects/linode-detail/linode-detail-resize.page.js
+++ packages/manager/e2e/pageobjects/linode-detail/linode-detail-resize.page.js
@@ -1,28 +1,82 @@
+const { assertLog } = require('../../utils/assertionLog');
+
 import Page from '../page';
 
 class Resize extends Page {
-    get title() { return this.pageTitle; }
-    get description() { return $('[data-qa-description]'); }
-    get currentHeader() { return $('[data-qa-current-header]'); }
-    get currentSelection() { return $('[data-qa-current-container] [data-qa-selection-card]'); }
-    get tierTabs() { return $$('[data-qa-tab]'); }
-    get planCards() { return $$('[data-qa-tp="Linode Plan"] [data-qa-selection-card]'); }
-    get submit() { return this.submitButton; }
+  get title() {
+    return this.pageTitle;
+  }
+  get description() {
+    return $('[data-qa-description]');
+  }
+  get currentHeader() {
+    return $('[data-qa-current-header]');
+  }
+  get currentSelection() {
+    return $('[data-qa-current-container] [data-qa-selection-card]');
+  }
+  get tierTabs() {
+    return $$('[data-qa-tab]');
+  }
+  get planCards() {
+    return $$('[data-qa-tp="Linode Plan"] [data-qa-selection-card]');
+  }
+  get submit() {
+    return this.submitButton;
+  }
 
-    landingElemsDisplay() {
-        const subHeader = 'Current Plan';
-        const selectedPlanTab = this.tierTabs.filter(tier => tier.getAttribute('aria-selected').includes('true') && !tier.getText().includes('Resize'));
-        const checkedCards = $$('[data-qa-checked] > svg');
+  landingElemsDisplay() {
+    const subHeader = 'Current Plan';
+    const selectedPlanTab = this.tierTabs.filter(
+      tier =>
+        tier.getAttribute('aria-selected').includes('true') &&
+        !tier.getText().includes('Resize')
+    );
+    const checkedCards = $$('[data-qa-checked] > svg');
 
-        expect(this.title.getText()).toBe('Resize');
-        expect(this.description.isVisible()).toBe(true);
-        expect(this.currentHeader.getText()).toBe(subHeader);
-        expect(this.currentSelection.isVisible()).toBe(true);
-        expect(this.submit.isVisible()).toBe(true);
-        expect(this.planCards.length).toBeGreaterThan(0);
-        expect(checkedCards.length).toBe(0);
-        expect(selectedPlanTab[0].getAttribute('data-qa-tab')).toBe('Standard');
-    }
+    expect(this.title.getText())
+      .withContext(
+        `${assertLog.incorrectText} for "${this.title.selector}" selector`
+      )
+      .toBe('Resize');
+    expect(this.description.isDisplayed())
+      .withContext(
+        `"${this.description.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.currentHeader.getText())
+      .withContext(
+        `${assertLog.incorrectText} for "${
+          this.currentHeader.selector
+        }" selector`
+      )
+      .toBe(subHeader);
+    expect(this.currentSelection.isDisplayed())
+      .withContext(
+        `"${this.currentSelection.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.submit.isDisplayed())
+      .withContext(`"${this.submit.selector}" selector ${assertLog.displayed}`)
+      .toBe(true);
+    expect(this.planCards.length)
+      .withContext(
+        `${assertLog.incorrectNum} for "${this.planCards.selector}" selector`
+      )
+      .toBeGreaterThan(0);
+    expect(checkedCards.length)
+      .withContext(
+        `${assertLog.incorrectNum} for "${this.checkedCards.selector}" selector`
+      )
+      .toBe(0);
+    expect(selectedPlanTab[0].getAttribute('data-qa-tab'))
+      .withContext(
+        `${assertLog.incorrectAttr} for "${
+          this.selectedPlanTab.selector
+        }" selector`
+      )
+      .toBe('Standard');
+  }
 }
 
 export default new Resize();
