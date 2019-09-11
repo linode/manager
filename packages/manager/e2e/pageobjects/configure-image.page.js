f539cdd4cc2cbbb0fd4363d164175499ffa390c4
Jason Stamis <jstamis@linode.com>
2019-09-11 15:31:22 -0400

    configure-image, cofigure-linode, and configure-stackscript pages updated, formatted, and withContext added

--- packages/manager/e2e/pageobjects/configure-image.page.js
+++ packages/manager/e2e/pageobjects/configure-image.page.js
@@ -1,26 +1,62 @@
 const { constants } = require('../constants');
+const { assertLog } = require('../utils/assertionLog');
 
 import Page from './page';
 
-
 class ConfigureImage extends Page {
-  get placeholderMsg() { return $('[data-qa-placeholder-title]'); }
-  get placeholderButton () { return $('[data-qa-placeholder-button]'); }
-  get linodeSelect() { return $('[data-qa-linode-select]'); }
-  get diskSelect() { return $('[data-qa-disk-select]'); }
-  get label() { return $('[data-qa-image-label]'); }
-  get description() { return $('[data-qa-image-description]'); }
-  get createButton() { return $(this.submitButton.selector); }
-  get linodesMenuItems() { return $$('[data-qa-linode-menu-item]'); }
-  get diskMenuItems() { return $$('[data-qa-disk-menu-item]'); }
+  get placeholderMsg() {
+    return $('[data-qa-placeholder-title]');
+  }
+  get placeholderButton() {
+    return $('[data-qa-placeholder-button]');
+  }
+  get linodeSelect() {
+    return $('[data-qa-linode-select]');
+  }
+  get diskSelect() {
+    return $('[data-qa-disk-select]');
+  }
+  get label() {
+    return $('[data-qa-image-label]');
+  }
+  get description() {
+    return $('[data-qa-image-description]');
+  }
+  get createButton() {
+    return $(this.submitButton.selector);
+  }
+  get linodesMenuItems() {
+    return $$('[data-qa-linode-menu-item]');
+  }
+  get diskMenuItems() {
+    return $$('[data-qa-disk-menu-item]');
+  }
 
   baseElementsDisplay() {
     this.linodeSelect.waitForDisplayed(constants.wait.normal);
-    expect(this.diskSelect.isDisplayed()).toBe(true);
-    expect(this.label.isDisplayed()).toBe(true);
-    expect(this.description.isDisplayed()).toBe(true);
-    expect(this.createButton.isDisplayed()).toBe(true);
-    expect(this.cancelButton.isDisplayed()).toBe(true);
+    expect(this.diskSelect.isDisplayed())
+      .withContext(
+        `"${this.diskSelect.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.label.isDisplayed())
+      .withContext(`"${this.label.selector}" selector ${assertLog.displayed}`)
+      .toBe(true);
+    expect(this.description.isDisplayed())
+      .withContext(
+        `"${this.description.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.createButton.isDisplayed())
+      .withContext(
+        `"${this.createButton.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.cancelButton.isDisplayed())
+      .withContext(
+        `"${this.cancelButton.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
   }
 
   configure(config) {
--- packages/manager/e2e/pageobjects/configure-stackscript.page.js
+++ packages/manager/e2e/pageobjects/configure-stackscript.page.js
@@ -1,155 +1,255 @@
 const { constants } = require('../constants');
+const { assertLog } = require('../utils/assertionLog');
 
 import Page from './page.js';
 import ListStackScripts from './list-stackscripts.page';
 
 const stackConfig = {
-    label: `${new Date().getTime()}-MyStackScript`,
-    description: 'test stackscript example',
-    revisionNote: new Date().getTime(),
-    script: '#!bin/bash',
-}
+  label: `${new Date().getTime()}-MyStackScript`,
+  description: 'test stackscript example',
+  revisionNote: new Date().getTime(),
+  script: '#!bin/bash'
+};
 
 const stackConfigWithRequiredUDFs = {
-    label: `${new Date().getTime()}-MyStackScript`,
-    description: 'test stackscript example',
-    revisionNote: new Date().getTime(),
-    script: `#!bin/bash<br># <UDF name="DB_PASSWORD" Label="MySQL root Password" />`,
-}
+  label: `${new Date().getTime()}-MyStackScript`,
+  description: 'test stackscript example',
+  revisionNote: new Date().getTime(),
+  script: `#!bin/bash<br># <UDF name="DB_PASSWORD" Label="MySQL root Password" />`
+};
 
 class ConfigureStackScript extends Page {
+  get createHeader() {
+    return $(this.breadcrumbStaticText.selector);
+  }
+  get editHeader() {
+    return $(this.breadcrumbStaticText.selector);
+  }
+  get label() {
+    return $('[data-qa-stackscript-label]');
+  }
+  get labelHelp() {
+    return $('[data-qa-stackscript-label]')
+      .$('..')
+      .$(this.toolTipIcon.selector);
+  }
+  get description() {
+    return $('[data-qa-stackscript-description]');
+  }
+  get descriptionHelp() {
+    return $('[data-qa-stackscript-description]')
+      .$('..')
+      .$(this.toolTipIcon.selector);
+  }
+  get targetImagesSelect() {
+    return $('#image-select>div>div>div');
+  }
+  get targetImages() {
+    return $$('[data-qa-stackscript-image]');
+  }
+  get targetImagesHelp() {
+    return $('[data-qa-stackscript-target-select]')
+      .$('..')
+      .$(this.toolTipIcon.selector);
+  }
+  get script() {
+    return $('[data-qa-stackscript-script]');
+  }
+  get revisionNote() {
+    return $('[data-qa-stackscript-revision]');
+  }
+  get saveButton() {
+    return $('[data-qa-save]');
+  }
+  get imageTags() {
+    return $$('[data-qa-multi-option]');
+  }
 
-    get createHeader() { return $(this.breadcrumbStaticText.selector); }
-    get editHeader() { return $(this.breadcrumbStaticText.selector); }
-    get label() { return $('[data-qa-stackscript-label]'); }
-    get labelHelp() { return $('[data-qa-stackscript-label]').$('..').$(this.toolTipIcon.selector); }
-    get description() { return $('[data-qa-stackscript-description]'); }
-    get descriptionHelp() { return $('[data-qa-stackscript-description]').$('..').$(this.toolTipIcon.selector); }
-    get targetImagesSelect() { return $('#image-select>div>div>div'); }
-    get targetImages() { return $$('[data-qa-stackscript-image]'); }
-    get targetImagesHelp() { return $('[data-qa-stackscript-target-select]').$('..').$(this.toolTipIcon.selector); }
-    get script() { return $('[data-qa-stackscript-script]'); }
-    get revisionNote() { return $('[data-qa-stackscript-revision]'); }
-    get saveButton() { return $('[data-qa-save]'); }
-    get imageTags() { return $$('[data-qa-multi-option]'); }
+  save() {
+    this.saveButton.click();
+    ListStackScripts.baseElementsDisplay();
+    $('[data-qa-notice]').waitForDisplayed(constants.wait.normal);
+    ListStackScripts.stackScriptRow.waitForDisplayed(constants.wait.normal);
+  }
 
-    save() {
-        this.saveButton.click();
-        ListStackScripts.baseElementsDisplay();
-        browser.waitForVisible('[data-qa-notice]', constants.wait.normal);
-        ListStackScripts.stackScriptRow.waitForVisible(constants.wait.normal);
+  cancel() {
+    this.cancelButton.click();
+    this.dialogTitle.waitForText(constants.wait.normal);
+    this.dialogContent.waitForText(constants.wait.normal);
+    this.dialogConfirm.waitForDisplayed(constants.wait.normal);
+    this.dialogCancel.waitForDisplayed(constants.wait.normal);
+    this.dialogConfirm.click();
+    this.dialogTitle.waitForDisplayed(constants.wait.normal, true);
+  }
+
+  baseElementsDisplay() {
+    this.createHeader.waitForDisplayed(constants.wait.normal);
+    this.label.waitForDisplayed(constants.wait.normal);
+    this.description.waitForDisplayed(constants.wait.normal);
+    this.targetImagesSelect.waitForDisplayed(constants.wait.normal);
+    this.saveButton.waitForDisplayed(constants.wait.normal);
+  }
+
+  editElementsDisplay() {
+    this.editHeader.waitForDisplayed(constants.wait.normal);
+
+    expect(this.description.isDisplayed())
+      .withContext(
+        `"${this.description.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.targetImagesSelect.isDisplayed())
+      .withContext(
+        `"${this.targetImagesSelect.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    this.imageTags.forEach(tag =>
+      expect(tag.isDisplayed())
+        .withContext(`"${this.tag.selector}" selector ${assertLog.displayed}`)
+        .toBe(true)
+    );
+
+    expect(this.script.isDisplayed())
+      .withContext(`"${this.script.selector}" selector ${assertLog.displayed}`)
+      .toBe(true);
+    expect(this.revisionNote.isDisplayed())
+      .withContext(
+        `"${this.revisionNote.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.saveButton.isDisplayed())
+      .withContext(
+        `"${this.saveButton.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+    expect(this.cancelButton.isDisplayed())
+      .withContext(
+        `"${this.cancelButton.selector}" selector ${assertLog.displayed}`
+      )
+      .toBe(true);
+  }
+
+  configure(config) {
+    $(`${this.label.selector}`).waitForDisplayed();
+    $(`${this.description.selector}`).waitForDisplayed();
+
+    $(`${this.label.selector} input`).setValue(config.label);
+    $(`${this.description.selector} textarea`).setValue(config.description);
+
+    // Choose an image from the multi select
+
+    const selectedImage = this.multiOption.selector.replace(']', '');
+
+    if (config.images) {
+      config.images.forEach(i => {
+        this.targetImagesSelect.click();
+        const imageElement = $(`[data-qa-option="linode/${i}"]`);
+        browser.pause(500);
+        imageElement.click();
+        $(`${selectedImage}="${i}"`).waitForDisplayed(constants.wait.normal);
+      });
+    } else {
+      this.targetImagesSelect.click();
+      browser.pause(500);
+      const imageElement = $(`[data-qa-option="linode/arch"]`);
+      imageElement.click();
+      imageElement.waitForDisplayed(constants.wait.normal, true);
+      $(`${selectedImage}="arch"`).waitForDisplayed(constants.wait.normal);
     }
 
-    cancel() {
-        this.cancelButton.click();
-        this.dialogTitle.waitForText(constants.wait.normal);
-        this.dialogContent.waitForText(constants.wait.normal);
-        this.dialogConfirm.waitForVisible(constants.wait.normal);
-        this.dialogCancel.waitForVisible(constants.wait.normal);
-        this.dialogConfirm.click();
-        this.dialogTitle.waitForVisible(constants.wait.normal, true);
+    // Click outside the select
+    $('body').click();
+    $('#react-select__menu').waitForDisplayed(constants.wait.normal, true);
+
+    this.script.$('textarea').click();
+    this.script.$('textarea').setValue(config.script);
+    if (config.revisionNote) {
+      this.revisionNote.$('input').setValue(config.revisionNote);
     }
+  }
 
-    baseElementsDisplay() {
-        this.createHeader.waitForVisible(constants.wait.normal);
-        this.label.waitForVisible(constants.wait.normal);
-        this.description.waitForVisible(constants.wait.normal);
-        this.targetImagesSelect.waitForVisible(constants.wait.normal);
-        this.saveButton.waitForVisible(constants.wait.normal);
-    }
+  create(config, update = false) {
+    this.save();
 
+    const myStackscript = ListStackScripts.stackScriptRows.filter(t =>
+      t
+        .$(ListStackScripts.stackScriptTitle.selector)
+        .getText()
+        .includes(config.label)
+    );
 
-    editElementsDisplay() {
-        this.editHeader.waitForVisible(constants.wait.normal);
+    expect(myStackscript.length).toBe(1);
+    expect(
+      myStackscript[0]
+        .$(ListStackScripts.stackScriptDescription.selector)
+        .getText()
+    )
+      .withContext(
+        `${assertLog.incorrectText} for ${
+          ListStackScripts.stackScriptDescription.selector
+        } selector`
+      )
+      .toContain(config.description);
+    expect(
+      myStackscript[0].$(ListStackScripts.stackScriptDeploys.selector).getText()
+    )
+      .withContext(
+        `${assertLog.incorrectText} for ${
+          ListStackScripts.stackScriptDescription.selector
+        } selector`
+      )
+      .toBe('0');
+    expect(
+      myStackscript[0]
+        .$(ListStackScripts.stackScriptRevision.selector)
+        .isDisplayed()
+    )
+      .withContext(
+        `"${ListStackScripts.stackScriptRevision.selector}" selector ${
+          assertLog.displayed
+        }`
+      )
+      .toBe(true);
+    expect(
+      myStackscript[0]
+        .$(ListStackScripts.stackScriptActionMenu.selector)
+        .isDisplayed()
+    )
+      .withContext(
+        `"${ListStackScripts.stackScriptActionMenu.selector}" selector ${
+          assertLog.displayed
+        }`
+      )
+      .toBe(true);
+    ListStackScripts.waitForNotice(
+      `${config.label} successfully ${update ? 'updated' : 'created'}`
+    );
+  }
 
-        expect(this.description.isVisible()).toBe(true);
-        expect(this.targetImagesSelect.isVisible()).toBe(true);
-        this.imageTags.forEach(tag => expect(tag.isVisible()).toBe(true));
+  removeImage(imageName) {
+    this.imageTags
+      .filter(i => i.getText().includes(imageName))
+      .forEach(i => {
+        i.$('..')
+          .$('svg')
+          .click();
+        i.waitForDisplayed(constants.wait.normal, true);
+      });
+  }
 
-        expect(this.script.isVisible()).toBe(true);
-        expect(this.revisionNote.isVisible()).toBe(true);
-        expect(this.saveButton.isVisible()).toBe(true);
-        expect(this.cancelButton.isVisible()).toBe(true);
-    }
+  createStackScriptWithRequiredUDFs() {
+    /** create the stackscript */
+    browser.url(constants.routes.createStackScript);
+    this.configure(stackConfigWithRequiredUDFs);
+    this.create(stackConfigWithRequiredUDFs);
+  }
 
-    configure(config) {
-        $(`${this.label.selector}`).waitForVisible()
-        $(`${this.description.selector}`).waitForVisible()
-
-        $(`${this.label.selector} input`).setValue(config.label);
-        $(`${this.description.selector} textarea`).setValue(config.description);
-
-        // Choose an image from the multi select
-
-        const selectedImage = this.multiOption.selector.replace(']', '');
-
-        if (config.images) {
-            config.images.forEach(i => {
-                this.targetImagesSelect.click();
-                const imageElement = $(`[data-qa-option="linode/${i}"]`);
-                browser.pause(500);
-                imageElement.click();
-                $(`${selectedImage}="${i}"`).waitForVisible(constants.wait.normal);
-            });
-        } else {
-            this.targetImagesSelect.click();
-            browser.pause(500);
-            const imageElement = $(`[data-qa-option="linode/arch"]`);
-            imageElement.click();
-            imageElement.waitForVisible(constants.wait.normal, true);
-            $(`${selectedImage}="arch"`).waitForVisible(constants.wait.normal);
-        }
-
-        // Click outside the select
-        browser.click('body');
-        browser.waitForVisible('#react-select__menu', constants.wait.normal, true);
-
-        this.script.$('textarea').click();
-        this.script.$('textarea').setValue(config.script);
-        if(config.revisionNote){
-            this.revisionNote.$('input').setValue(config.revisionNote);
-        }
-    }
-
-    create(config, update=false) {
-        this.save();
-
-        const myStackscript =
-            ListStackScripts.stackScriptRows
-                .filter(t => t.$(ListStackScripts.stackScriptTitle.selector).getText().includes(config.label));
-
-        expect(myStackscript.length).toBe(1);
-        expect(myStackscript[0].$(ListStackScripts.stackScriptDescription.selector).getText())
-            .toContain(config.description);
-        expect(myStackscript[0].$(ListStackScripts.stackScriptDeploys.selector).getText()).toBe('0');
-        expect(myStackscript[0].$(ListStackScripts.stackScriptRevision.selector).isVisible()).toBe(true);
-        expect(myStackscript[0].$(ListStackScripts.stackScriptActionMenu.selector).isVisible()).toBe(true);
-        ListStackScripts.waitForNotice(`${config.label} successfully ${update ? 'updated' : 'created'}`);
-    }
-
-    removeImage(imageName) {
-        this.imageTags
-            .filter(i => i.getText().includes(imageName))
-            .forEach(i => {
-                i.$('..').$('svg').click();
-                i.waitForVisible(constants.wait.normal, true);
-            });
-
-    }
-
-    createStackScriptWithRequiredUDFs() {
-        /** create the stackscript */
-        browser.url(constants.routes.createStackScript);
-        this.configure(stackConfigWithRequiredUDFs);
-        this.create(stackConfigWithRequiredUDFs);
-    }
-
-    createStackScriptNoUDFs() {
-        /** create the stackscript */
-        browser.url(constants.routes.createStackScript);
-        this.configure(stackConfig);
-        this.create(stackConfig);
-    }
+  createStackScriptNoUDFs() {
+    /** create the stackscript */
+    browser.url(constants.routes.createStackScript);
+    this.configure(stackConfig);
+    this.create(stackConfig);
+  }
 }
 
 export default new ConfigureStackScript();
