b05bd413325ca8569b5d691b108e5c34e0f13428
mmckenna <mmckenna.phila@gmail.com>
2019-09-12 13:27:26 -0400

    uses ramda path for getting error

--- packages/manager/src/containers/withImages.container.ts
+++ packages/manager/src/containers/withImages.container.ts
@@ -1,4 +1,5 @@
 import { Image } from 'linode-js-sdk/lib/images';
+import { path } from 'ramda';
 import { connect } from 'react-redux';
 import { ApplicationState } from 'src/store';
 
@@ -17,10 +18,8 @@ export default <TInner extends {}, TOuter extends {}>(
   ) => TInner
 ) =>
   connect((state: ApplicationState, ownProps: TOuter) => {
-    const images = state.__resources.images.data;
-    const imagesLoading = state.__resources.images.loading;
-    const { error } = state.__resources.images;
-    const imageError = (error || {}).read ? error!.read![0].reason : undefined;
+    const { data: images, error, loading } = state.__resources.images;
+    const imageError = path<undefined | string>(['read', 0, 'reason'], error);
 
-    return mapImagesToProps(ownProps, images, imagesLoading, imageError);
+    return mapImagesToProps(ownProps, images, loading, imageError);
   });
