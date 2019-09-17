76ee5c42a3be59b090b474df4c901d56bf29f74c
Martin <mmckenna@linode.com>
2019-09-17 11:50:41 -0400

    Fix: Error with Placeholders missing key prop (#5483)
    * adds key to placeholder iterables
    
    * Make key extra-unique

--- packages/manager/src/components/Placeholder/Placeholder.tsx
+++ packages/manager/src/components/Placeholder/Placeholder.tsx
@@ -139,8 +139,8 @@ const Placeholder: React.StatelessComponent<CombinedProps> = props => {
           alignItems="center"
           justify="center"
         >
-          {buttonProps.map(thisButton => (
-            <Grid item>
+          {buttonProps.map((thisButton, index) => (
+            <Grid item key={`placeholder-button-${index}`}>
               <Button
                 buttonType="primary"
                 className={classes.button}
