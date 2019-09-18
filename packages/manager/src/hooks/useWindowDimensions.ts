6734f4d9036f8775dccb7510368922d0902a5740
John Callahan <johnwcallahan@gmail.com>
2019-09-18 10:59:26 -0400

    Throttle useWindowDimensions by default

--- packages/manager/src/hooks/useWindowDimensions.ts
+++ packages/manager/src/hooks/useWindowDimensions.ts
@@ -1,5 +1,6 @@
 /** This nice little hook courtesy of: https://stackoverflow.com/a/36862446 */
 import { useEffect, useState } from 'react';
+import { throttle } from 'throttle-debounce';
 
 /**
  * Give a component easy access to window height and width.
@@ -13,15 +14,20 @@ const getWindowDimensions = () => {
   };
 };
 
-export const useWindowDimensions = () => {
+export const useWindowDimensions = (
+  shouldThrottle = true,
+  throttleInMs = 300
+) => {
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
   );
 
   useEffect(() => {
-    const handleResize = () => {
-      setWindowDimensions(getWindowDimensions());
-    };
+    const handleDimensions = () => setWindowDimensions(getWindowDimensions());
+
+    const handleResize = shouldThrottle
+      ? throttle(throttleInMs, handleDimensions)
+      : handleDimensions;
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
