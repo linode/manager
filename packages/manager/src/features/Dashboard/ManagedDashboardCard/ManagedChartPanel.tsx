965b1f10a1b3e06cd43ebf659efbc7b316fd6696
Jared Kobos <jaredkobos@gmail.com>
2019-09-26 11:37:43 -0400

    Add static chart to ManagedchartsPanel

--- packages/manager/src/features/Dashboard/ManagedDashboardCard/ManagedChartPanel.tsx
+++ packages/manager/src/features/Dashboard/ManagedDashboardCard/ManagedChartPanel.tsx
@@ -1,23 +1,54 @@
 import * as React from 'react';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
+import LineGraph from 'src/components/LineGraph';
 import TabbedPanel from 'src/components/TabbedPanel';
 import { Tab } from 'src/components/TabbedPanel/TabbedPanel';
+import useTimezone from 'src/utilities/useTimezone';
+
+// Temporary
+import { fakeData } from './fakeData';
+const data: [number, number][] = fakeData.map(thisPoint => [
+  thisPoint.x,
+  thisPoint.y
+]);
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {}
+  root: {},
+  inner: {
+    paddingTop: 0
+  }
 }));
 
 interface Props {
   data: any;
 }
 
+const chartHeight = 300;
+
+// const statsFetchInterval = 30000;
+
+const rangeSelection = '24';
+
 const tabs: Tab[] = [
   {
     render: () => {
+      const timezone = useTimezone();
       return (
         <>
-          <Typography>Empty CPU charts panel</Typography>
+          <LineGraph
+            timezone={timezone}
+            chartHeight={chartHeight}
+            showToday={rangeSelection === '24'}
+            data={[
+              {
+                borderColor: 'rgba(54, 131, 220, 1)',
+                backgroundColor: 'rgba(54, 131, 220, .5)',
+                data,
+                label: 'CPU %'
+              }
+            ]}
+          />
         </>
       );
     },
@@ -58,6 +89,7 @@ export const ManagedChartPanel: React.FC<Props> = props => {
   return (
     <TabbedPanel
       rootClass={`${classes.root} tabbedPanel`}
+      innerClass={classes.inner}
       error={undefined}
       header={''}
       copy={''}
