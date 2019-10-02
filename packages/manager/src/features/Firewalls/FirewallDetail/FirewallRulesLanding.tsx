6030d3db2761cecbad78bec4103a027fb5576709
mmckenna <mmckenna.phila@gmail.com>
2019-10-02 11:52:19 -0400

    adds firewall detail routing

---
+++ packages/manager/src/features/Firewalls/FirewallDetail/FirewallRulesLanding.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { compose } from 'recompose';
+// import { makeStyles, Theme } from 'src/components/core/styles'
+
+// const useStyles = makeStyles((theme: Theme) => ({
+//   root: {}
+// }))
+
+// interface Props { }
+
+type CombinedProps = RouteComponentProps;
+
+const FirewallRulesLanding: React.FC<CombinedProps> = props => {
+  // const classes = useStyles();
+
+  return <div>hello world</div>;
+};
+
+export default compose<CombinedProps, {}>(React.memo)(FirewallRulesLanding);
