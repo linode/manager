1de5a6e6355076048b3eff4c5100c6a68765eab9
Alban Bailly <abailly@linode.com>
2019-10-17 15:05:18 -0400

    remove fixes for other PR

--- packages/manager/src/features/Dashboard/BlogDashboardCard/BlogDashboardCard.tsx
+++ packages/manager/src/features/Dashboard/BlogDashboardCard/BlogDashboardCard.tsx
@@ -65,7 +65,7 @@ export class BlogDashboardCard extends React.Component<CombinedProps, State> {
     this.mounted = true;
 
     req
-      .get(`https://linode.com/blog/feed/`, { responseType: 'text' })
+      .get(`https://blog.linode.com/feed/`, { responseType: 'text' })
       .then(({ data }) => parseXMLStringPromise(data))
       .then(processXMLData)
       .then(
@@ -128,7 +128,7 @@ export class BlogDashboardCard extends React.Component<CombinedProps, State> {
   renderAction = () => (
     <ViewAllLink
       text="Read More"
-      link={'https://linode.com/blog/'}
+      link={'https://blog.linode.com/'}
       data-qa-read-more
       external
     />
