e83e7972a1a768d873a69871bfa925ffa2bd4909
Alban Bailly <abailly@linode.com>
2019-10-21 17:50:21 -0400

    add logic for building columns

--- packages/manager/src/components/Skeleton/Skeleton.stories.tsx
+++ packages/manager/src/components/Skeleton/Skeleton.stories.tsx
@@ -2,4 +2,6 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Skeleton from './Skeleton';
 
-storiesOf('Skeleton Screen', module).add('default', () => <Skeleton />);
+storiesOf('Skeleton', module).add('default', () => (
+  <Skeleton variant="rect" width="100%" height="30px" columns={4} />
+));
--- packages/manager/src/components/Skeleton/Skeleton.tsx
+++ packages/manager/src/components/Skeleton/Skeleton.tsx
@@ -1,17 +1,54 @@
 import * as React from 'react';
-import Skeleton from 'src/components/core/Skeleton';
+import Skeleton, { SkeletonProps } from 'src/components/core/Skeleton';
 import { makeStyles, Theme } from 'src/components/core/styles';
 
+import Grid from 'src/components/Grid';
+
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {}
+  root: {},
+  column: {}
 }));
 
-interface Props {}
+interface Props {
+  columns?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+}
 
-const _Skeleton: React.FC<Props> = () => {
+type combinedProps = SkeletonProps & Props;
+
+const _Skeleton: React.FC<combinedProps> = props => {
   const classes = useStyles();
+  const { columns } = props;
 
-  return <Skeleton className={classes.root} />;
+  const cols: any = [];
+  const renderCols = (colCount: any) => {
+    for (
+      colCount = 0;
+      colCount <= (columns !== undefined ? columns - 1 : 1);
+      colCount++
+    ) {
+      cols.push(
+        <Grid
+          item
+          style={{ width: `${100 / (columns !== undefined ? columns : 1)}%` }}
+        >
+          <Skeleton {...props} className={classes.column} />
+        </Grid>
+      );
+    }
+    return;
+  };
+
+  renderCols(columns);
+
+  return (
+    <>
+      {!columns ? (
+        <Skeleton {...props} className={classes.root} />
+      ) : (
+        <Grid container>{cols}</Grid>
+      )}
+    </>
+  );
 };
 
 export default _Skeleton;
