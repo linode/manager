cd5a0e7c6f53286a325cb43fb1411be70777f354
Alban Bailly <abailly@linode.com>
2019-10-22 11:36:21 -0400

    add logic for building columns width

--- packages/manager/src/components/Skeleton/Skeleton.stories.tsx
+++ packages/manager/src/components/Skeleton/Skeleton.stories.tsx
@@ -2,6 +2,6 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Skeleton from './Skeleton';
 
-storiesOf('Skeleton', module).add('default', () => (
-  <Skeleton variant="rect" width="100%" height="30px" columns={4} />
+storiesOf('Skeleton', module).add('Table', () => (
+  <Skeleton variant="rect" width="100%" height="30px" table />
 ));
