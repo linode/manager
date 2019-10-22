45a9f4290264e07f2e624139f830bebf288556fc
Alban Bailly <abailly@linode.com>
2019-10-22 11:42:31 -0400

    update storybook

--- packages/manager/src/components/Skeleton/Skeleton.stories.tsx
+++ packages/manager/src/components/Skeleton/Skeleton.stories.tsx
@@ -3,5 +3,12 @@ import * as React from 'react';
 import Skeleton from './Skeleton';
 
 storiesOf('Skeleton', module).add('Table', () => (
-  <Skeleton variant="rect" width="100%" height="30px" table columns={6} />
+  <Skeleton
+    variant="rect"
+    width="100%"
+    height="30px"
+    table
+    columns={6}
+    firstColWidth={40}
+  />
 ));
