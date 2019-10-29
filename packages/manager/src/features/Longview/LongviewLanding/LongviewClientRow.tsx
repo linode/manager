219f4c6740a02978d80965765ca07f384e719c5b
mmckenna <mmckenna.phila@gmail.com>
2019-10-29 09:07:22 -0400

    fixes issue with guage state not being set post error

--- packages/manager/src/features/Longview/LongviewLanding/LongviewClientRow.tsx
+++ packages/manager/src/features/Longview/LongviewLanding/LongviewClientRow.tsx
@@ -1,3 +1,4 @@
+import { pathOr } from 'ramda';
 import * as React from 'react';
 import { compose } from 'recompose';
 
@@ -38,12 +39,20 @@ const LongviewClientRow: React.FC<CombinedProps> = props => {
 
   const { clientID, clientLabel, clientAPIKey, ...actionHandlers } = props;
 
-  const [lastUpdated, setLastUpdated] = React.useState<number>(0);
+  /* 
+   lastUpdated _might_ come back from the endpoint as 0, so it's important
+   that we differentiate between _0_ and _undefined_
+   */
+  const [lastUpdated, setLastUpdated] = React.useState<number | undefined>();
 
   const requestAndSetLastUpdated = () => {
     return getLastUpdated(clientAPIKey)
       .then(response => {
-        if (response.updated > lastUpdated) {
+        /* 
+          only update _lastUpdated_ state if it hasn't already been set
+          or the API response is in a time past what's already been set.
+        */
+        if (!lastUpdated || pathOr(0, ['updated'], response) > lastUpdated) {
           setLastUpdated(response.updated);
         }
       })
@@ -68,7 +77,7 @@ const LongviewClientRow: React.FC<CombinedProps> = props => {
     <TableRow className={classes.root} rowLink={`longview/clients/${clientID}`}>
       <TableCell>{`${clientLabel}`}</TableCell>
       <TableCell>
-        <CPUGauge clientAPIKey={clientAPIKey} lastUpdated={lastUpdated} />
+        <CPUGauge clientAPIKey={clientAPIKey} lastUpdated={lastUpdated || 0} />
       </TableCell>
       <TableCell>
         <RAMGauge />
@@ -77,7 +86,7 @@ const LongviewClientRow: React.FC<CombinedProps> = props => {
         <SwapGauge />
       </TableCell>
       <TableCell>
-        <LoadGauge lastUpdated={lastUpdated} token={clientAPIKey} />
+        <LoadGauge lastUpdated={lastUpdated || 0} token={clientAPIKey} />
       </TableCell>
       <TableCell>
         <NetworkGauge />
