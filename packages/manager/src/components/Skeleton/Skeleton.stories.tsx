9bc933e85e4d7f6ec18cc4f7479fcabe517bd42e
Alban Bailly <abailly@linode.com>
2019-10-31 14:21:57 -0400

    address feedback part 2

--- packages/manager/src/components/Skeleton/Skeleton.stories.tsx
+++ packages/manager/src/components/Skeleton/Skeleton.stories.tsx
@@ -2,13 +2,15 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Skeleton from './Skeleton';
 
-storiesOf('Skeleton', module).add('Table', () => (
-  <Skeleton
-    variant="rect"
-    width="100%"
-    height="30px"
-    table
-    columns={5}
-    firstColWidth={30}
-  />
-));
+storiesOf('Skeleton', module)
+  .add('Default', () => <Skeleton />)
+  .add('Table', () => (
+    <Skeleton
+      variant="rect"
+      width="100%"
+      height="30px"
+      table
+      columns={5}
+      firstColWidth={30}
+    />
+  ));
