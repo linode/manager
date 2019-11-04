d8de228bc21aff01b03b57efe221d997e0034f73
Jared <jaredkobos@gmail.com>
2019-11-04 13:57:35 -0500

    M3-3513 Create longview client take 2 (#5686)
    * Out with the old...
    
    Remove AddClientDrawer for LongviewLanding
    Remove related state variables and handler functions
    
    Directly call the createClient method from LongviewLanding when
    the AddNewMenu is clicked
    
    This commit includes basic error and loading state management;
    however, final designs are pending, so the approaches here will
    need to be revisited.
    
    * show mock loading state if no client data is available
    
    * Add action menu to dummy loading rows
    :
    
    * Add comments

--- packages/manager/src/features/Longview/LongviewLanding/LongviewClientRow.tsx
+++ packages/manager/src/features/Longview/LongviewLanding/LongviewClientRow.tsx
@@ -43,7 +43,8 @@ const LongviewClientRow: React.FC<CombinedProps> = props => {
    lastUpdated _might_ come back from the endpoint as 0, so it's important
    that we differentiate between _0_ and _undefined_
    */
-  const [lastUpdated, setLastUpdated] = React.useState<number | undefined>();
+  const [lastUpdated, setLastUpdated] = React.useState<number | undefined>(0);
+  const [authed, setAuthed] = React.useState<boolean>(true);
 
   const requestAndSetLastUpdated = () => {
     return getLastUpdated(clientAPIKey)
@@ -56,7 +57,16 @@ const LongviewClientRow: React.FC<CombinedProps> = props => {
           setLastUpdated(response.updated);
         }
       })
-      .catch(e => e);
+      .catch(e => {
+        /**
+         * The first request we make after creating a new client will almost always
+         * return an authentication failed error.
+         */
+        const reason = pathOr('', [0, 'reason'], e);
+        if (reason.match(/authentication/i)) {
+          setAuthed(false);
+        }
+      });
   };
 
   /** request on first mount */
@@ -72,6 +82,27 @@ const LongviewClientRow: React.FC<CombinedProps> = props => {
 
     return () => clearInterval(requestInterval);
   });
+
+  /**
+   * We want to show a "waiting for data" state
+   * until data has been returned.
+   */
+  if (!authed || lastUpdated === 0) {
+    return (
+      <TableRow>
+        <TableCell colSpan={7}>
+          Waiting for data...(installation instructions go here)
+        </TableCell>
+        <TableCell>
+          <ActionMenu
+            longviewClientID={clientID}
+            longviewClientLabel={clientLabel}
+            {...actionHandlers}
+          />
+        </TableCell>
+      </TableRow>
+    );
+  }
 
   return (
     <TableRow className={classes.root} rowLink={`longview/clients/${clientID}`}>
