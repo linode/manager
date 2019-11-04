228a36046815dd6d033d44e296728049e4b01877
Kayla Wilkins <kwilkins@linode.com>
2019-11-04 17:19:53 -0500

    initial scaffolding for details, adding skeleton for overview page

--- packages/manager/src/features/Longview/LongviewDetail/LongviewDetail.tsx
+++ packages/manager/src/features/Longview/LongviewDetail/LongviewDetail.tsx
@@ -2,6 +2,17 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { compose } from 'recompose';
 
+import Breadcrumb from 'src/components/Breadcrumb';
+import AppBar from 'src/components/core/AppBar';
+import Box from 'src/components/core/Box';
+import Tab from 'src/components/core/Tab';
+import Tabs from 'src/components/core/Tabs';
+
+import DocumentationButton from 'src/components/DocumentationButton';
+
+import { safeGetTabRender } from 'src/utilities/safeGetTabRender';
+import Overview from './LongviewDetailOverview';
+
 import withLongviewClients, {
   DispatchProps,
   Props as LVProps
@@ -34,7 +45,97 @@ const LongviewDetail: React.FC<CombinedProps> = props => {
 
   const client = clients[id];
 
-  return <div>hello {client ? client.label : 'loading...'}</div>;
+  const tabOptions = [
+    {
+      title: 'Overview',
+      display: true,
+      render: () => {
+        return <Overview />;
+      }
+    },
+    {
+      title: 'Processes',
+      display: true,
+      render: () => {
+        // return <Overview />;
+      }
+    },
+    {
+      title: 'Network',
+      display: true,
+      render: () => {
+        // return <Overview />;
+      }
+    },
+    {
+      title: 'Disks',
+      display: true,
+      render: () => {
+        // return <Overview />;
+      }
+    },
+    {
+      title: 'Apache',
+      display: client.apps.apache,
+      render: () => {
+        // return <Overview />;
+      }
+    },
+    {
+      title: 'Nginx',
+      display: client.apps.nginx,
+      render: () => {
+        // return <Overview />;
+      }
+    },
+    {
+      title: 'MySQL',
+      display: client.apps.mysql,
+      render: () => {
+        // return <Overview />;
+      }
+    },
+    {
+      title: 'Installation',
+      display: true,
+      render: () => {
+        // return <Overview />;
+      }
+    }
+  ];
+
+  const tabs = tabOptions.filter(tab => tab.display === true);
+
+  const tabRender = safeGetTabRender(tabs, 0);
+
+  return (
+    <React.Fragment>
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Breadcrumb
+          pathname={props.location.pathname}
+          removeCrumbX={2}
+          labelTitle={client.label}
+        />
+        <DocumentationButton href={'https://google.com'} />
+      </Box>
+      <AppBar position="static" color="default">
+        {/* TODO make this functional */}
+        <Tabs
+          value={0}
+          // onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="on"
+        >
+          {tabs.map(tab => (
+            <Tab key={tab.title} label={tab.title} data-qa-tab={tab.title} />
+          ))}
+        </Tabs>
+      </AppBar>
+      {tabRender()}
+    </React.Fragment>
+  );
 };
 
 export default compose<CombinedProps, {}>(
