758be68d32b4adebb8ab39ff9a80935add99e419
mmckenna <mmckenna.phila@gmail.com>
2019-11-06 12:39:10 -0500

    adds longview client data to redux

---
+++ packages/manager/src/containers/longview.stats.container.ts
@@ -0,0 +1,71 @@
+import { pathOr } from 'ramda';
+import { connect } from 'react-redux';
+import { ApplicationState } from 'src/store';
+import { ReturnType } from 'src/store/longviewStats/longviewStats.actions';
+import { getClientStats } from 'src/store/longviewStats/longviewStats.requests';
+import { EntityError, ThunkDispatch } from 'src/store/types';
+
+export interface LVClientData {
+  longviewClientData: ReturnType;
+  longviewClientDataLoading: boolean;
+  longviewClientDataError: Partial<EntityError>;
+}
+
+export interface DispatchProps {
+  getClientStats: (api_key: string) => Promise<ReturnType>;
+}
+
+export type Props = DispatchProps & LVClientData;
+
+/**
+ *
+ * @param supplyClientID callback function that should return the Longview Client
+ * ID for the stats you'd like to work with
+ *
+ * @example
+ *
+ * import withLongviewStats, { Props as LVDataProps }
+ *
+ * interface Props {
+ *  clientID: number;
+ *  apiKey: string;
+ * }
+ *
+ * type CombinedProps = Props & LVDataProps;
+ *
+ * const MyComponent: React.FC<CombinedProps> = props => {
+ *   React.useEffect(() => {
+ *       props.getClientStats(props.apiKey)
+ *   }, [])
+ *
+ *   return (
+ *      <div>hello world</div>
+ *   )
+ * }
+ *
+ * export default withLongviewStats<Props>((ownProps) => ownProps.clientID)(MyComponent)
+ */
+const connected = <OwnProps extends {}>(
+  supplyClientID: (ownProps: OwnProps) => number
+) =>
+  connect<LVClientData, DispatchProps, OwnProps, ApplicationState>(
+    (state, ownProps) => {
+      const { longviewStats } = state;
+
+      const foundClient = longviewStats[supplyClientID(ownProps)];
+
+      return {
+        longviewClientData: pathOr({}, ['data'], foundClient),
+        longviewClientDataLoading: pathOr(true, ['loading'], foundClient),
+        longviewClientDataError: pathOr({}, ['error'], foundClient)
+      };
+    },
+    (dispatch: ThunkDispatch, ownProps: OwnProps) => ({
+      getClientStats: api_key =>
+        dispatch(
+          getClientStats({ clientID: supplyClientID(ownProps), api_key })
+        )
+    })
+  );
+
+export default connected;
---
+++ packages/manager/src/store/longviewStats/longviewStats.reducer.ts
@@ -0,0 +1,65 @@
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { RelationalDataSet } from '../types';
+import {
+  requestClientStats,
+  ReturnType as LVClientData
+} from './longviewStats.actions';
+
+export type State = RelationalDataSet<LVClientData>;
+
+export const defaultState: State = {};
+
+const reducer = reducerWithInitialState(defaultState)
+  /** START GET ACTIONS */
+  .caseWithAction(requestClientStats.started, (state, { payload }) => {
+    const statsAlreadyExist = !!state[payload.clientID];
+
+    /**
+     * only set a loading state if we haven't already gotten
+     * stats for this particular Longview Client
+     */
+    return statsAlreadyExist
+      ? state
+      : {
+          ...state,
+          [payload.clientID]: {
+            loading: true
+          }
+        };
+  })
+  .caseWithAction(
+    requestClientStats.done,
+    (state, { payload: { result, params } }) => ({
+      ...state,
+      [params.clientID]: {
+        data: result,
+        loading: false,
+        error: {}
+      }
+    })
+  )
+  .caseWithAction(
+    requestClientStats.failed,
+    (state, { payload: { error, params } }) => {
+      /**
+       * at this point, we are guaranteed to have state[params.clientID].loading
+       * but we only want to set an error if we don't already have data
+       */
+      const statsAlreadyExist = !!(state[params.clientID] || {}).data;
+
+      return statsAlreadyExist
+        ? state
+        : {
+            ...state,
+            [params.clientID]: {
+              loading: false,
+              error: {
+                read: error
+              }
+            }
+          };
+    }
+  )
+  .default(state => state);
+
+export default reducer;
