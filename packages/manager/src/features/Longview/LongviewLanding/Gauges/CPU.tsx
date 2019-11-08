23057421bc3dbd6b062b68404d6bfec7b9a02c1a
Martin <mmckenna@linode.com>
2019-11-08 10:02:05 -0500

    Change: Hydrate CPU Gauge from Redux (#5712)
    * hydrates cpu gauge from redux state
    
    * removes unused props

--- packages/manager/src/features/Longview/LongviewLanding/Gauges/CPU.tsx
+++ packages/manager/src/features/Longview/LongviewLanding/Gauges/CPU.tsx
@@ -1,86 +1,52 @@
 import { APIError } from 'linode-js-sdk/lib/types';
-import { clamp, path, pathOr } from 'ramda';
+import { clamp, pathOr } from 'ramda';
 import * as React from 'react';
 import Typography from 'src/components/core/Typography';
 import GaugePercent from 'src/components/GaugePercent';
 import { pluralize } from 'src/utilities/pluralize';
-import requestStats from '../../request';
 import { CPU } from '../../request.types';
 import { baseGaugeProps } from './common';
 
+import withClientStats, {
+  Props as LVDataProps
+} from 'src/containers/longview.stats.container';
+
 interface Props {
-  clientAPIKey: string;
-  lastUpdated?: number;
+  clientID: number;
 }
 
-const CPUGauge: React.FC<Props> = props => {
-  const { clientAPIKey, lastUpdated } = props;
+const CPUGauge: React.FC<Props & LVDataProps> = props => {
+  const {
+    longviewClientDataLoading: loading,
+    longviewClientDataError: error,
+    longviewClientData
+  } = props;
 
-  const [dataHasResolvedAtLeastOnce, setDataResolved] = React.useState<boolean>(
-    false
+  const numberOfCores = pathOr(
+    0,
+    ['SysInfo', 'cpu', 'cores'],
+    longviewClientData
   );
-  const [loading, setLoading] = React.useState<boolean>(true);
-  const [error, setError] = React.useState<APIError | undefined>();
-
-  const [usedCPU, setUsedCPU] = React.useState<number>(0);
-  const [numCores, setNumCores] = React.useState<number>(0);
-
-  React.useEffect(() => {
-    let mounted = true;
-
-    requestStats(clientAPIKey, 'getLatestValue', ['cpu', 'sysinfo'])
-      .then(data => {
-        if (mounted) {
-          setLoading(false);
-          setError(undefined);
-
-          const cores = path<number>(['SysInfo', 'cpu', 'cores'], data);
-
-          // If we don't have the number of cores, we can't determine the value.
-          if (!cores) {
-            return;
-          }
-
-          if (!dataHasResolvedAtLeastOnce) {
-            setDataResolved(true);
-          }
-
-          setNumCores(cores);
-
-          const used = sumCPUUsage(data.CPU);
-          const normalizedUsed = normalizeValue(used, cores);
-          setUsedCPU(normalizedUsed);
-        }
-      })
-      .catch(_ => {
-        if (mounted && !dataHasResolvedAtLeastOnce) {
-          setError({
-            reason: 'Error' // @todo: Error message?
-          });
-          setLoading(false);
-        }
-      });
-
-    return () => {
-      mounted = false;
-    };
-  }, [lastUpdated]);
+  const usedCPU = sumCPUUsage(longviewClientData.CPU);
+  const finalUsedCPU = normalizeValue(usedCPU, numberOfCores);
 
   return (
     <GaugePercent
       {...baseGaugeProps}
       // The MAX depends on the number of CPU cores. Default to 1 if cores
       // doesn't exist or is 0.
-      max={100 * numCores}
+      max={100 * numberOfCores}
       value={usedCPU}
-      innerText={innerText(usedCPU || 0, loading, error)}
+      innerText={innerText(finalUsedCPU || 0, loading, error)}
       subTitle={
         <>
           <Typography>
             <strong>CPU</strong>
           </Typography>
           {!error && !loading && (
-            <Typography>{pluralize('Core', 'Cores', numCores || 0)}</Typography>
+            <Typography>
+              {pluralize('Core', 'Cores', numberOfCores || 0)}
+            </Typography>
           )}
         </>
       }
@@ -88,7 +54,7 @@ const CPUGauge: React.FC<Props> = props => {
   );
 };
 
-export default CPUGauge;
+export default withClientStats<Props>(ownProps => ownProps.clientID)(CPUGauge);
 
 // UTILITIES
 export const sumCPUUsage = (CPUData: Record<string, CPU> = {}) => {
@@ -112,10 +78,10 @@ export const normalizeValue = (value: number, numCores: number) => {
 export const innerText = (
   value: number,
   loading: boolean,
-  error?: APIError
+  error?: APIError[]
 ) => {
   if (error) {
-    return error.reason;
+    return 'Error';
   }
 
   if (loading) {
