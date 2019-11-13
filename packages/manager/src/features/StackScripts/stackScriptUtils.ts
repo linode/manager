da14dc659c9c6e16c81ba35130de64af8db3eaca
hmorris <hmorris@linode.com>
2019-11-12 23:37:18 -0500

    add mongodb oca

--- packages/manager/src/features/StackScripts/stackScriptUtils.ts
+++ packages/manager/src/features/StackScripts/stackScriptUtils.ts
@@ -36,7 +36,9 @@ export const baseApps = {
   '401719': 'OpenVPN - Latest One-Click',
   '401697': 'WordPress - Latest One-Click',
   '595742': 'cPanel One-Click',
-  '593835': 'Plesk One-Click'
+  '593835': 'Plesk One-Click',
+  '609195': 'MongoDB One-Click'
+
 };
 
 const oneClickFilter = [
