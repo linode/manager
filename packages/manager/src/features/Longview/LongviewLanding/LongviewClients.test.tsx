892bdf0d6c52125ae4b81d301b9e5fba55824c7b
Jared <jaredkobos@gmail.com>
2019-11-15 09:52:59 -0500

    M3-3512 Longview: sort clients by values (#5770)
    * WIP
    
    * Add handling for displaying last updated client time
    
    * De-debounce updated client time utility and extract from component
    
    * Styling complete for header row
    
    * Add loading state for last updated time display
    
    * Update longviewStats.reducer.tests to account for new lastUpdated values
    
    * Feedback
    
    - Make search field and sort Select `small`
    - Use modified logic to figure out when the "Data updated at..." should
    show a loading state

--- packages/manager/src/features/Longview/LongviewLanding/LongviewClients.test.tsx
+++ packages/manager/src/features/Longview/LongviewLanding/LongviewClients.test.tsx
@@ -38,6 +38,8 @@ const props = {
   accountSettingsLoading: false,
   accountSettingsError: {},
   accountSettingsLastUpdated: 0,
+  lvClientData: {},
+  lvClientsLoading: false,
   ...reactRouterProps
 };
 
