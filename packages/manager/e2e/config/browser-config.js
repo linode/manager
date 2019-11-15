59670fb9de0431eb49f83c2e365d0133d8ac46b2
Adrien Courdavault <acourdavault@linode.com>
2019-11-15 16:20:58 -0500

    reactivate storybook

--- packages/manager/e2e/config/browser-config.js
+++ packages/manager/e2e/config/browser-config.js
@@ -8,7 +8,6 @@ exports.browserConf = {
         '--no-sandbox',
         '--disable-dev-shm-usage',
         '--window-size=1600,1080',
-        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36 WebdriverIO'
       ]
     }
   },
