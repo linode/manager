009e9e43f7148a05fe34932819b3341480a53e41
Adrien Courdavault <acourdavault@linode.com>
2019-11-15 19:50:16 -0500

    still issues with storybook on travis

--- packages/manager/e2e/config/browser-config.js
+++ packages/manager/e2e/config/browser-config.js
@@ -35,7 +35,6 @@ exports.browserConf = {
         '--allow-running-insecure-content',
         '--ignore-certificate-errors',
         '--disable-dev-shm-usage',
-        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36 WebdriverIO'
       ]
     }
   },
