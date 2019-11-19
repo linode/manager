d60572e20faf4a098b4f54e721b26feeeea9eb5b
Martin <mmckenna@linode.com>
2019-11-19 10:05:39 -0500

    [NodeBalancer Refactor] - Edit Flow Skeleton (#5695)
    * renders expansion panels for new and existing configs
    
    * updates comments and syntax
    
    * convert configs to array of data

---
+++ packages/manager/src/features/NodeBalancers/NodeBalancerDetail/__new__/EditNodeBalancerConfigsLanding.tsx
@@ -0,0 +1,116 @@
+import {
+  NEWCreateNodeBalancerConfigPayload,
+  NodeBalancerConfig
+} from 'linode-js-sdk/lib/nodebalancers';
+import * as React from 'react';
+import { compose } from 'recompose';
+import { makeStyles, Theme } from 'src/components/core/styles';
+
+import Button from 'src/components/Button';
+import Typography from 'src/components/core/Typography';
+import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import ExpansionPanel from 'src/components/ExpansionPanel';
+
+import withNodeBalancerConfigs, {
+  DispatchProps,
+  StateProps as ConfigStateProps
+} from 'src/containers/__new__/nodeBalancerConfigs.container';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  title: {
+    marginTop: theme.spacing(),
+    marginBottom: theme.spacing(2)
+  }
+}));
+
+interface Props {
+  nodeBalancerID: number;
+  nodeBalancerRegion: string;
+  nodeBalancerLabel: string;
+}
+
+type CombinedProps = Props & DispatchProps & ReduxStateProps;
+
+const EditNodeBalancerConfigs: React.FC<CombinedProps> = props => {
+  const classes = useStyles();
+
+  const [newConfigs, setNewConfigs] = React.useState<
+    NEWCreateNodeBalancerConfigPayload[]
+  >([]);
+
+  const {
+    nodeBalancerLabel,
+    // nodeBalancerRegion,
+    configs
+  } = props;
+
+  const hasNoConfigs = !Object.keys(configs).length;
+
+  return (
+    <React.Fragment>
+      <DocumentTitleSegment segment={`${nodeBalancerLabel} - Configurations`} />
+      <Typography variant="h1" data-qa-title className={classes.title}>
+        NodeBalancer Configurations
+      </Typography>
+      {/** create an expansion panel for each existing config */
+      configs.map(eachConfig => {
+        return (
+          <ExpansionPanel
+            key={`config-${eachConfig.id}`}
+            heading="hello world"
+          />
+        );
+      })}
+      {/** create an expansion panel for each new config */
+      newConfigs.map((eachConfig, idx) => {
+        return <ExpansionPanel key={`config-${idx}`} heading="hello world" />;
+      })}
+      <Button
+        buttonType="secondary"
+        onClick={() =>
+          setNewConfigs([...newConfigs, generateDefaultNodeBalancerConfig()])
+        }
+        data-qa-add-config
+      >
+        {hasNoConfigs ? 'Add a Configuration' : 'Add another Configuration'}
+      </Button>
+    </React.Fragment>
+  );
+};
+
+interface ReduxStateProps
+  extends Omit<ConfigStateProps, 'nodeBalancerConfigsData'> {
+  configs: NodeBalancerConfig[];
+}
+
+export default compose<CombinedProps, Props>(
+  React.memo,
+  withNodeBalancerConfigs<ReduxStateProps, Props>(
+    (ownProps, { nodeBalancerConfigsData, ...rest }) => ({
+      configs: Object.values(nodeBalancerConfigsData).filter(
+        config => config.nodebalancer_id === ownProps.nodeBalancerID
+      ),
+      ...rest
+    })
+  )
+)(EditNodeBalancerConfigs);
+
+/**
+ * generates a new nodebalancer config with defaults
+ */
+const generateDefaultNodeBalancerConfig = (): NEWCreateNodeBalancerConfigPayload => ({
+  port: 0,
+  protocol: 'http',
+  algorithm: 'roundrobin',
+  stickiness: 'table',
+  check_attempts: 2,
+  check_interval: 5,
+  check_passive: true,
+  check_timeout: 3,
+  check: 'none',
+  ssl_cert: '',
+  ssl_key: '',
+  check_body: '',
+  check_path: '',
+  cipher_suite: 'recommended'
+});
