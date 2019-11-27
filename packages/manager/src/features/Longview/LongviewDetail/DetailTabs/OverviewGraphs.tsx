cbe10b6994b45c2472c059144b189ed6fe0b7fb3
Jared Kobos <jaredkobos@gmail.com>
2019-11-27 11:21:15 -0500

    Add layout for overview tables

--- packages/manager/src/features/Longview/LongviewDetail/DetailTabs/OverviewGraphs.tsx
+++ packages/manager/src/features/Longview/LongviewDetail/DetailTabs/OverviewGraphs.tsx
@@ -4,6 +4,9 @@ import Paper from 'src/components/core/Paper';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
 import Grid from 'src/components/Grid';
+import LongviewLineGraph from 'src/components/LongviewLineGraph';
+
+import TimeRangeSelect from '../../shared/TimeRangeSelect';
 
 const useStyles = makeStyles((theme: Theme) => ({
   paperSection: {
@@ -19,20 +22,80 @@ export type CombinedProps = Props;
 export const OverviewGraphs: React.FC<Props> = props => {
   const classes = useStyles();
   return (
-    <Grid
-      container
-      alignItems="flex-end"
-      justify="space-between"
-      item
-      xs={12}
-      spacing={0}
-    >
-      <Grid item>
-        <Typography variant="h2">Resource Allocation History</Typography>
+    <Grid container alignItems="flex-end" item xs={12} spacing={0}>
+      <Grid
+        container
+        item
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+        spacing={0}
+        className="py0"
+      >
+        <Grid item>
+          <Typography variant="h2">Resource Allocation History</Typography>
+        </Grid>
+        <Grid item>
+          <TimeRangeSelect handleStatsChange={(a, b) => null} />
+        </Grid>
       </Grid>
       <Grid item />
       <Grid item xs={12}>
-        <Paper className={classes.paperSection}>Graphs here</Paper>
+        <Paper className={classes.paperSection}>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            spacing={4}
+          >
+            <Grid item xs={6}>
+              <LongviewLineGraph
+                title="CPU"
+                subtitle="%"
+                showToday={false}
+                timezone="GMT"
+                data={[]}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <LongviewLineGraph
+                title="RAM"
+                subtitle="GB"
+                showToday={false}
+                timezone="GMT"
+                data={[]}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <LongviewLineGraph
+                title="Network"
+                subtitle="KB/s"
+                showToday={false}
+                timezone="GMT"
+                data={[]}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <LongviewLineGraph
+                title="Disk I/O"
+                subtitle="ops/s"
+                showToday={false}
+                timezone="GMT"
+                data={[]}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <LongviewLineGraph
+                title="Load"
+                subtitle="Target < 1.00"
+                showToday={false}
+                timezone="GMT"
+                data={[]}
+              />
+            </Grid>
+          </Grid>
+        </Paper>
       </Grid>
     </Grid>
   );
