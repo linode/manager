7f652234b309d90fc85aef6ca7161814ef142b1e
Martin <mmckenna@linode.com>
2019-12-04 09:59:16 -0500

    Change: Disks Cards (#5837)
    * adds paper elements and select
    
    * adds paper elements and select
    
    * removes default select option
    
    * changes default time range
    
    * updates loading state markup

--- packages/manager/src/features/Longview/LongviewDetail/DetailTabs/Disks/DiskPaper.tsx
+++ packages/manager/src/features/Longview/LongviewDetail/DetailTabs/Disks/DiskPaper.tsx
@@ -1,3 +1,4 @@
+import { pathOr } from 'ramda';
 import * as React from 'react';
 import { compose } from 'recompose';
 import { makeStyles, Theme } from 'src/components/core/styles';
@@ -5,18 +6,24 @@ import { makeStyles, Theme } from 'src/components/core/styles';
 import Paper from 'src/components/core/Paper';
 import Typography from 'src/components/core/Typography';
 
-import { LongviewDisk } from '../../../request.types';
+import { Disk } from '../../../request.types';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     marginBottom: theme.spacing(2),
-    padding: theme.spacing(2)
+    padding: theme.spacing(2),
+    '& > h6': {
+      color: theme.color.grey1,
+      '& > strong': {
+        color: theme.color.headline
+      }
+    }
   }
 }));
 
 interface Props {
   diskLabel?: string;
-  stats: Partial<LongviewDisk['Disk']>;
+  stats: Partial<Disk>;
 }
 
 type CombinedProps = Props;
@@ -24,13 +31,23 @@ type CombinedProps = Props;
 const DiskPaper: React.FC<CombinedProps> = props => {
   const classes = useStyles();
 
-  const { diskLabel } = props;
+  const { diskLabel, stats } = props;
+
+  const isSwap = pathOr(0, ['isswap'], stats);
 
   return (
     <Paper className={classes.root}>
       <Typography variant="subtitle1">
         <strong>{diskLabel}</strong>
+        {isSwap === 1 && (
+          <React.Fragment>
+            {' '}
+            (swap) &ndash; Longview doesn't gather data on swap partitions.
+          </React.Fragment>
+        )}
       </Typography>
+
+      {isSwap === 0 && <div>graphs go here, ya dig?</div>}
     </Paper>
   );
 };
