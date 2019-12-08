c856bde1c0115a49547e808369e790016257b6c9
Alban Bailly <abailly@linode.com>
2019-12-08 13:10:01 -0500

    Revert "M3-3652 making modals and drawers more accessible"
    This reverts commit e0e5898f4acf280af11c81073d26efb0cc2a0b2a.

--- packages/manager/src/features/TheApplicationIsOnFire.tsx
+++ packages/manager/src/features/TheApplicationIsOnFire.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Dialog from 'src/components/core/Dialog';
 import DialogContent from 'src/components/core/DialogContent';
+import DialogTitle from 'src/components/core/DialogTitle';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
-import DialogTitle from 'src/components/DialogTitle';
 
 const useStyles = makeStyles((theme: Theme) => ({
   restartButton: {
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 const TheApplicationIsOnFire: React.StatelessComponent<{}> = props => {
   return (
     <Dialog open>
-      <DialogTitle title="Oh snap!" />
+      <DialogTitle>Oh snap!</DialogTitle>
       <DialogContent>
         <Typography>
           Something went terribly wrong. Did you try {<ReloadLink />}?
