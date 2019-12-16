cb0b379215a252f957280e65f0df5a06ba579129
Alban Bailly <abailly@linode.com>
2019-12-16 15:03:28 -0500

    M3-3700 Allow primary nav to set multiple active links

--- packages/manager/src/components/PrimaryNav/utils.ts
+++ packages/manager/src/components/PrimaryNav/utils.ts
@@ -1,11 +1,11 @@
 import isPathOneOf from 'src/utilities/routing/isPathOneOf';
 
-export const linkIsActive = (href: string) => {
+export const linkIsActive = (href: string, activeLinks?: Array<string>) => {
   const currentlyOnCreateLinodeFlow = location.pathname.match(
     /linodes[/]create/gi
   );
   const currentlyOnOneClickTab = location.search.match(/one-click/gi);
-  const isOneClickTab = href.match(/one-click/gi);
+  const isOneClickTab = href && href.match(/one-click/gi);
 
   /**
    * mark as active if the tab is "one click" and we're on the one-click route
@@ -18,5 +18,7 @@ export const linkIsActive = (href: string) => {
     return false;
   }
 
-  return isPathOneOf([href], location.pathname);
+  return !activeLinks
+    ? href && isPathOneOf([href], location.pathname)
+    : isPathOneOf(activeLinks, location.pathname);
 };
