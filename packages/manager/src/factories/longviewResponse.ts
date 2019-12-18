0bbc8612bd10e017253aa2d38f958c91d32c20c3
Jared Kobos <jaredkobos@gmail.com>
2019-12-18 14:53:33 -0500

    Rename method (fix typo)

--- packages/manager/src/factories/longviewResponse.ts
+++ packages/manager/src/factories/longviewResponse.ts
@@ -2,7 +2,9 @@ import * as Factory from 'factory.ts';
 import { LongviewResponse } from 'src/features/Longview/request.types';
 import { longviewDiskFactory } from './longviewDisks';
 
-export const longviewPortFactory = Factory.Sync.makeFactory<LongviewResponse>({
+export const longviewResponseFactory = Factory.Sync.makeFactory<
+  LongviewResponse
+>({
   VERSION: 0.4,
   NOTIFICATIONS: [],
   ACTION: 'getValues',
