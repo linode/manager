b0a2a3687bbd7ee51329f30961c36689d8df87fb
Jared <jaredkobos@gmail.com>
2020-01-16 10:27:34 -0500

    M3-3779 ADD: Ability to edit axfr for slave Domains (#5999)
    * M3-3779 Allow editing IP transfer addresses from Slave edit form
    
    We had a field to allow users to update axfr IPs (addresses allowed
    to transfer for this Domain) when editing the SOA record for a master
    Domain, but nothing was available for slave domains, since there's
    only a single drawer for all slave-related editing actions.
    
    Abstracted the axfr textfield so that it could be used in both places.
    Also updated the warning text to more closely match Classic (there
    is a limit of 6 IPs that we didn't mention).
    
    * Remove text about limit since the API doesn't seem to care
    
    * Update comments
    
    * Add React.memo to DomainTransferInput to avoid rerenders

--- packages/manager/src/features/Domains/DomainRecordDrawer.tsx
+++ packages/manager/src/features/Domains/DomainRecordDrawer.tsx
@@ -34,6 +34,7 @@ import {
 import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
 import getAPIErrorsFor from 'src/utilities/getAPIErrorFor';
 import scrollErrorIntoView from 'src/utilities/scrollErrorIntoView';
+import DomainTransferInput from './DomainTransferInput';
 import { isValidCNAME, isValidDomainRecord } from './domainUtils';
 
 const TextField: React.StatelessComponent<TextFieldProps> = props => (
@@ -162,15 +163,7 @@ class DomainRecordDrawer extends React.Component<CombinedProps, State> {
     expire_sec: 'expire rate'
   };
 
-  handleTransferUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    /**
-     * This is a textarea input type, and users
-     * are expected to input a comma-separated list of IPs.
-     * However, the API is expecting an array
-     * of strings. If the user clears the input, set axfr_ips to [].
-     * Otherwise, split the list into an array.
-     */
-    const transferIPs = e.target.value === '' ? [] : e.target.value.split(',');
+  handleTransferUpdate = (transferIPs: string[]) => {
     this.updateField('axfr_ips')(transferIPs);
   };
 
@@ -402,18 +395,11 @@ class DomainRecordDrawer extends React.Component<CombinedProps, State> {
   };
 
   DomainTransferField = () => (
-    <TextField
-      multiline
-      label="Domain Transfers"
-      placeholder="192.0.2.0,192.0.2.1"
-      errorText={getAPIErrorsFor(
+    <DomainTransferInput
+      error={getAPIErrorsFor(
         DomainRecordDrawer.errorFields,
         this.state.errors
       )('axfr_ips')}
-      // Include some warnings and info from the API docs.
-      helperText={`Comma-separated list of IPs that may perform a zone transfer for this Domain.
-        This is potentially dangerous, and should be left empty unless you intend to use it.`}
-      rows="3"
       value={defaultTo(
         DomainRecordDrawer.defaultFieldsState(this.props).axfr_ips,
         (this.state.fields as EditableDomainFields).axfr_ips
