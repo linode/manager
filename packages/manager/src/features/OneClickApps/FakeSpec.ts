905d034ef4347a17958ee911239a9d4f77a499a3
hmorris <hmorris@linode.com>
2020-01-29 03:48:33 -0500

    adjust tips

--- packages/manager/src/features/OneClickApps/FakeSpec.ts
+++ packages/manager/src/features/OneClickApps/FakeSpec.ts
@@ -90,7 +90,8 @@ export const oneClickApps: OCA[] = [
       }
     ],
     tips: [
-      `Once the script finishes, go to <em>https://[your-Linode's-IP-address]:8000</em> in a browser to visit the default web page. You can also visit <em>https://[your-Linode's-IP-address]:8000/admin</em> to view the Djanogo admin panel`
+       `Once the script finishes, go to <em>https://[your-Linode's-IP-address]:8000</em> in a browser to visit the default web page.`,
+       `You can also visit <em>https://[your-Linode's-IP-address]:8000/admin</em> to view the Djanogo admin panel`
     ],
     logo_url: 'assets/django_color.svg'
   },
