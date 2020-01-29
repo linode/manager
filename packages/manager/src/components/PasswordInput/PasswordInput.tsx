73c34fef8959262421d43422d6476e24784929d5
Adrien Courdavault <acourdavault@linode.com>
2020-01-29 13:58:57 -0500

    revert changes from PR 5781 and add zxcvbn as ext script

--- packages/manager/src/components/PasswordInput/PasswordInput.tsx
+++ packages/manager/src/components/PasswordInput/PasswordInput.tsx
@@ -1,4 +1,3 @@
-import * as classNames from 'classnames';
 import { isEmpty } from 'ramda';
 import * as React from 'react';
 import {
@@ -10,10 +9,8 @@ import {
 import Typography from 'src/components/core/Typography';
 import Grid from 'src/components/Grid';
 import { Props as TextFieldProps } from 'src/components/TextField';
+import StrengthIndicator from '../PasswordInput/StrengthIndicator';
 import HideShowText from './HideShowText';
-
-import Alert from 'src/assets/icons/alert.svg';
-import Check from 'src/assets/icons/check.svg';
 
 type Props = TextFieldProps & {
   value?: string;
@@ -25,55 +22,29 @@ type Props = TextFieldProps & {
 };
 
 interface State {
-  isValidPassword: boolean;
-  lengthRequirement: boolean;
-  active: boolean;
+  strength: null | 0 | 1 | 2 | 3;
 }
 
-type ClassNames =
-  | 'container'
-  | 'listItem'
-  | 'reqList'
-  | 'valid'
-  | 'check'
-  | 'active';
+type ClassNames = 'container' | 'strengthIndicator' | 'infoText';
 
 const styles = (theme: Theme) =>
   createStyles({
     container: {
       position: 'relative',
+      marginBottom: theme.spacing(1),
       paddingBottom: theme.spacing(1) / 2
     },
-    reqList: {
-      listStyleType: 'none',
-      margin: 0,
-      width: '100%',
-      padding: `${theme.spacing(1)}px ${theme.spacing(2) - 2}px `,
-      backgroundColor: theme.bg.offWhiteDT,
-      border: `1px solid ${theme.palette.divider}`,
-      [theme.breakpoints.up('sm')]: {
-        width: 415
-      }
-    },
-    check: {
-      color: theme.color.grey1,
-      marginRight: theme.spacing(1),
+    strengthIndicator: {
       position: 'relative',
-      top: -1
-    },
-    active: {
-      color: theme.color.red,
-      '&$valid': {
-        color: theme.color.green
+      top: -5,
+      width: '100%',
+      [theme.breakpoints.down('xs')]: {
+        maxWidth: '100%'
       }
     },
-    valid: {},
-    listItem: {
-      display: 'flex',
-      margin: `${theme.spacing(1)}px 0`,
-      '& > span': {
-        display: 'block'
-      }
+    infoText: {
+      fontSize: '0.85rem',
+      marginTop: 12
     }
   });
 
@@ -81,18 +52,12 @@ type CombinedProps = Props & WithStyles<ClassNames>;
 
 class PasswordInput extends React.Component<CombinedProps, State> {
   state: State = {
-    isValidPassword: false,
-    lengthRequirement: false,
-    active: false
+    strength: maybeStrength(this.props.value)
   };
 
   UNSAFE_componentWillReceiveProps(nextProps: CombinedProps) {
     const { value } = nextProps;
-    this.setState({
-      isValidPassword: passwordValidity(value),
-      lengthRequirement: value && value.length >= 6 ? true : false,
-      active: value && value.length !== 0 ? true : false
-    });
+    this.setState({ strength: maybeStrength(value) });
   }
 
   onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -101,24 +66,11 @@ class PasswordInput extends React.Component<CombinedProps, State> {
     if (this.props.onChange) {
       this.props.onChange(e);
     }
-
-    this.setState({
-      isValidPassword: passwordValidity(value),
-      lengthRequirement: value.length >= 6 ? true : false,
-      active: true
-    });
-  };
-
-  onBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const value: string = e.currentTarget.value;
-
-    this.setState({
-      active: value.length !== 0 && true
-    });
+    this.setState({ strength: maybeStrength(value) });
   };
 
   render() {
-    const { isValidPassword, lengthRequirement, active } = this.state;
+    const { strength } = this.state;
     const {
       classes,
       value,
@@ -127,13 +79,12 @@ class PasswordInput extends React.Component<CombinedProps, State> {
       hideStrengthLabel,
       hideHelperText,
       hideValidation,
-      onBlur,
       ...rest
     } = this.props;
 
     return (
       <React.Fragment>
-        <Grid container alignItems="flex-end" className={classes.container}>
+        <Grid container className={classes.container}>
           <Grid item xs={12}>
             <HideShowText
               {...rest}
@@ -142,81 +93,38 @@ class PasswordInput extends React.Component<CombinedProps, State> {
               onChange={this.onChange}
               fullWidth
               required={required}
-              onBlur={this.onBlur}
             />
           </Grid>
-          {!hideHelperText && (
-            <Grid item xs={12}>
-              <ul className={classes.reqList}>
-                <Typography>Password must:</Typography>
-                <li
-                  className={classes.listItem}
-                  aria-label={
-                    lengthRequirement
-                      ? 'Password contains enough characters'
-                      : 'Password should be at least 6 chars'
-                  }
-                >
-                  <span
-                    className={classNames({
-                      [classes.check]: true,
-                      [classes.active]: active,
-                      [classes.valid]: lengthRequirement
-                    })}
-                  >
-                    {active && !lengthRequirement ? <Alert /> : <Check />}
-                  </span>{' '}
-                  <Typography component={'span'}>
-                    Be at least <strong>6 characters</strong>
-                  </Typography>
-                </li>
-                <li
-                  className={classes.listItem}
-                  aria-label={
-                    isValidPassword
-                      ? "Password's strength is valid"
-                      : "Increase password's strength by adding uppercase letters, lowercase letters, numbers, or punctuation"
-                  }
-                >
-                  <span
-                    className={classNames({
-                      [classes.check]: true,
-                      [classes.active]: active,
-                      [classes.valid]: isValidPassword
-                    })}
-                  >
-                    {active && !isValidPassword ? <Alert /> : <Check />}
-                  </span>{' '}
-                  <Typography component={'span'}>
-                    Contain at least{' '}
-                    <strong>two of the following character classes</strong>:
-                    uppercase letters, lowercase letters, numbers, and
-                    punctuation.
-                  </Typography>
-                </li>
-              </ul>
+          {!hideValidation && (
+            <Grid item xs={12} className={`${classes.strengthIndicator} py0`}>
+              <StrengthIndicator
+                strength={strength}
+                hideStrengthLabel={hideStrengthLabel}
+              />
             </Grid>
           )}
         </Grid>
+        {!hideHelperText && (
+          <Typography variant="body1" className={classes.infoText}>
+            Password must be at least 6 characters and contain at least two of
+            the following character classes: uppercase letters, lowercase
+            letters, numbers, and punctuation.
+          </Typography>
+        )}
       </React.Fragment>
     );
   }
 }
 
-const passwordValidity = (value?: string) => {
-  // This regex checks for the password validity only (no length) which is
-  // two of the following character classes: uppercase letters, lowercase letters, numbers, and punctuation.
-  // This check aligns with the backend validation
-  const regex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[!@#$%^&*(),.?":{}|<>]))|((?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]))|((?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>])))/;
-
+const maybeStrength = (value?: string) => {
   if (!value || isEmpty(value)) {
-    return false;
+    return null;
   } else {
-    if (regex.test(value)) {
-      return true;
-    } else {
-      return false;
+    const score = (window as any).zxcvbn(value).score;
+    if (score === 4) {
+      return 3;
     }
+    return score;
   }
 };
 
