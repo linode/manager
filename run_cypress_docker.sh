c0af1d02b9492f2e00d5f390549dbc616e084aa3
Adrien Courdavault <acourdavault@linode.com>
2020-02-12 15:31:54 -0500

    clean

--- run_cypress_docker.sh
+++ run_cypress_docker.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
 echo "starting server"
 nohup yarn workspace linode-manager start&
-echo "sleeping 60sec"
+echo "sleeping 60 sec for server to start on localhost:3000"
 sleep 60
 yarn cy:e2e
-
-
