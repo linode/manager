d7de841baa8d5211dedd0212477e48ab70b62217
Jared Kobos <jaredkobos@gmail.com>
2020-02-13 15:08:19 -0500

    Update labels and allow all addresses for v4 and v6

--- packages/manager/src/features/Firewalls/shared.ts
+++ packages/manager/src/features/Firewalls/shared.ts
@@ -5,96 +5,126 @@ export interface FirewallOption {
 
 export const predefinedFirewalls: FirewallOption[] = [
   {
-    label: 'SSH (TCP 22)',
+    label: 'SSH (TCP 22 - All IPv4, All IPv6)',
     value: {
       inbound: [
         {
           ports: '22',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ],
       outbound: [
         {
           ports: '22',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ]
     }
   },
   {
-    label: 'HTTP (TCP 80)',
+    label: 'HTTP (TCP 80 - All IPv4, All IPv6)',
     value: {
       inbound: [
         {
           ports: '80',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ],
       outbound: [
         {
           ports: '80',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ]
     }
   },
   {
-    label: 'HTTPS (TCP 443)',
+    label: 'HTTPS (TCP 443 - All IPv4, All IPv6)',
     value: {
       inbound: [
         {
           ports: '443',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ],
       outbound: [
         {
           ports: '443',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ]
     }
   },
   {
-    label: 'MySQL (TCP 3306)',
+    label: 'MySQL (TCP 3306 - All IPv4, All IPv6)',
     value: {
       inbound: [
         {
           ports: '3306',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ],
       outbound: [
         {
           ports: '3306',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ]
     }
   },
   {
-    label: 'DNS (TCP 53)',
+    label: 'DNS (TCP 53 - All IPv4, All IPv6)',
     value: {
       inbound: [
         {
           ports: '53',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ],
       outbound: [
         {
           ports: '53',
           protocol: 'TCP',
-          addresses: {}
+          addresses: {
+            ipv4: '0.0.0.0/0',
+            ipv6: '::0/0'
+          }
         }
       ]
     }
