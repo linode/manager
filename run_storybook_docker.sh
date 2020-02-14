804671905103e3b931a1ce626ad8b9624d31ac0d
Adrien Courdavault <acourdavault@linode.com>
2020-02-14 12:37:34 -0500

    Fix docker images version and update version of selenium in config to match

--- run_storybook_docker.sh
+++ run_storybook_docker.sh
@@ -3,5 +3,5 @@ echo "starting server"
 nohup yarn storybook&
 echo "waiting for storybook server to start"
 # need npx for docker container
-npx wait-on http://localhost:6006
+yarn run wait-on http://localhost:6006
 yarn storybook:e2e
