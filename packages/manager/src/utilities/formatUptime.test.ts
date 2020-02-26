bd6aad4350056e899ad4ede3ae242893fbd6a706
Jared Kobos <jaredkobos@gmail.com>
2020-02-26 15:38:57 -0500

    Fix uptime calculation in Longview

--- packages/manager/src/utilities/formatUptime.test.ts
+++ packages/manager/src/utilities/formatUptime.test.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { formatUptime } from './formatUptime';
 
 describe('Formatting uptime', () => {
@@ -27,5 +28,15 @@ describe('Formatting uptime', () => {
     expect(
       formatUptime(60 * 60 * 24 * 9 + 60 * 60 * 19 + 60 * 45 + 45)
     ).toMatch('9d 19h 45m');
+  });
+
+  it('should handle durations longer than a month', () => {
+    expect(
+      formatUptime(
+        moment
+          .duration({ years: 1, months: 2, days: 12, hours: 10, minutes: 15 })
+          .asSeconds()
+      )
+    ).toMatch('438d 10h 15m');
   });
 });
