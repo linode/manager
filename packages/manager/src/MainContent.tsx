linode/manager

commit 6753a8d3688d36546c36e7c3d507eaf53061d224

Remove: OBJ and Managed Feature Flags (#5785)
* removes obj and managed feature flags

* removes obj and managed feature flags

packages/manager/src/MainContent.tsx
@@ -326,8 +326,7 @@ const MainContent: React.FC<CombinedProps> = props => {
                       {getObjectStorageRoute(
                         props.accountLoading,
                         props.accountCapabilities,
-                        props.accountError,
-                        Boolean(props.flags.objectStorage)
+                        props.accountError
                       )}
                       {isKubernetesEnabled && (
                         <Route path="/kubernetes" component={Kubernetes} />
@@ -385,15 +384,10 @@ const MainContent: React.FC<CombinedProps> = props => {
 const getObjectStorageRoute = (
   accountLoading: boolean,
   accountCapabilities: AccountCapability[],
-  accountError?: Error | APIError[],
-  featureFlag?: boolean
+  accountError?: Error | APIError[]
 ) => {
   let component;
-  // If the feature flag is on, we want to see Object Storage regardless of
-  // the state of account.
-  if (featureFlag) {
-    component = ObjectStorage;
-  } else if (accountLoading) {
+  if (accountLoading) {
     component = () => <LandingLoading delayInMS={1000} />;
   } else if (accountError) {
     component = () => (

packages/manager/src/components/PrimaryNav/PrimaryNav.tsx
@@ -144,9 +144,7 @@ export class PrimaryNav extends React.Component<CombinedProps, State> {
 
     return [
       {
-        conditionToAdd: () =>
-          isObjectStorageEnabled(accountCapabilities) ||
-          Boolean(flags.objectStorage),
+        conditionToAdd: () => isObjectStorageEnabled(accountCapabilities),
         insertAfter: 'Volumes',
         link: {
           display: 'Object Storage',

packages/manager/src/featureFlags.ts
@@ -8,8 +8,6 @@ interface TaxBanner {
 type OneClickApp = Record<string, string>;
 
 interface Flags {
-  managed: boolean;
-  objectStorage: boolean;
   promos: boolean;
   vatBanner: TaxBanner;
   oneClickLocation: 'sidenav' | 'createmenu';
@@ -17,8 +15,6 @@ interface Flags {
   firewalls: boolean;
   oneClickApps: OneClickApp;
   longview: boolean;
-  objectStorageBilling: boolean;
-  objectStorageCancel: boolean;
 }
 
 /**

packages/manager/src/features/Account/EnableManaged.tsx
@@ -13,7 +13,6 @@ import SupportLink from 'src/components/SupportLink';
 import withLinodes, {
   DispatchProps
 } from 'src/containers/withLinodes.container';
-import useFlags from 'src/hooks/useFlags';
 import { pluralize } from 'src/utilities/pluralize';
 
 interface Props {
@@ -70,7 +69,6 @@ export const ManagedContent: React.FC<ContentProps> = props => {
 
 export const EnableManaged: React.FC<CombinedProps> = props => {
   const { isManaged, linodeCount, push, update } = props;
-  const flags = useFlags();
   const [isOpen, setOpen] = React.useState<boolean>(false);
   const [error, setError] = React.useState<string | undefined>();
   const [isLoading, setLoading] = React.useState<boolean>(false);
@@ -112,10 +110,6 @@ export const EnableManaged: React.FC<CombinedProps> = props => {
       </Button>
     </ActionsPanel>
   );
-
-  if (!flags.managed) {
-    return null;
-  }
 
   return (
     <>

packages/manager/src/features/Account/GlobalSettings.tsx
@@ -11,9 +11,6 @@ import { AnyAction } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
 import CircleProgress from 'src/components/CircleProgress';
 import ErrorState from 'src/components/ErrorState';
-import withFeatureFlags, {
-  FeatureFlagConsumerProps
-} from 'src/containers/withFeatureFlagConsumer.container';
 import TagImportDrawer from 'src/features/TagImport';
 import { ApplicationState } from 'src/store';
 import { updateSettingsInStore } from 'src/store/accountSettings/accountSettings.actions';
@@ -62,8 +59,7 @@ interface DispatchProps {
 type CombinedProps = StateProps &
   DispatchProps &
   WithSnackbarProps &
-  RouteComponentProps<{}> &
-  FeatureFlagConsumerProps;
+  RouteComponentProps<{}>;
 
 class GlobalSettings extends React.Component<CombinedProps, {}> {
   toggleAutomaticBackups = () => {
@@ -111,7 +107,6 @@ class GlobalSettings extends React.Component<CombinedProps, {}> {
       backups_enabled,
       networkHelperEnabled,
       error,
-      flags,
       loading,
       linodesWithoutBackups,
       entitiesWithGroupsToImport,
@@ -143,12 +138,10 @@ class GlobalSettings extends React.Component<CombinedProps, {}> {
           onChange={this.toggleNetworkHelper}
           networkHelperEnabled={networkHelperEnabled}
         />
-        {flags.objectStorageCancel && (
-          <EnableObjectStorage
-            object_storage={object_storage}
-            update={this.props.actions.updateAccountSettingsInStore}
-          />
-        )}
+        <EnableObjectStorage
+          object_storage={object_storage}
+          update={this.props.actions.updateAccountSettingsInStore}
+        />
         <EnableManaged
           isManaged={isManaged}
           update={this.props.actions.updateAccountSettingsInStore}
@@ -220,8 +213,7 @@ const connected = connect(
 
 const enhanced = compose<CombinedProps, {}>(
   connected,
-  withSnackbar,
-  withFeatureFlags
+  withSnackbar
 )(GlobalSettings);
 
 export default enhanced;

packages/manager/src/features/Dashboard/Dashboard.tsx
@@ -108,7 +108,7 @@ export const Dashboard: React.StatelessComponent<CombinedProps> = props => {
             Dashboard
           </Typography>
         </Grid>
-        {flags.managed && managed && (
+        {managed && (
           <Grid item xs={12}>
             <ManagedDashboardCard />
           </Grid>

packages/manager/src/features/Managed/ManagedLanding.tsx
@@ -3,9 +3,7 @@ import { RouteComponentProps } from 'react-router-dom';
 import { compose } from 'recompose';
 import setDocs from 'src/components/DocsSidebar/setDocs';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
-import useFlags from 'src/hooks/useFlags';
 import ManagedLandingContent from './ManagedLandingContent';
-import ManagedPlaceholder from './ManagedPlaceholder';
 
 export type CombinedProps = RouteComponentProps<{}>;
 
@@ -18,12 +16,6 @@ const docs: Linode.Doc[] = [
 ];
 
 export const ManagedLanding: React.FunctionComponent<CombinedProps> = props => {
-  const flags = useFlags();
-
-  if (!flags.managed) {
-    return <ManagedPlaceholder />;
-  }
-
   return (
     <React.Fragment>
       <DocumentTitleSegment segment="Managed" />

packages/manager/src/features/Managed/ManagedPlaceholder.tsx
@@ -1,24 +1,0 @@
-import * as React from 'react';
-import ManagedIcon from 'src/assets/icons/managed.svg';
-import Placeholder from 'src/components/Placeholder';
-
-const ManagedPlaceholder = () => (
-  <Placeholder
-    icon={ManagedIcon}
-    title="Linode Managed"
-    copy={`Linode Managed is only available in the Classic Manager.`}
-    buttonProps={[
-      {
-        onClick: () =>
-          window.open(
-            'https://manager.linode.com/account#managed',
-            '_blank',
-            'noopener'
-          ),
-        children: 'Navigate to Classic Manager'
-      }
-    ]}
-  />
-);
-
-export default ManagedPlaceholder;

commit 2bb4865406a1ddeed5db8bfd22866d793c4a5498

Show OBJ for restricted users
We actually introduced handling for restricted users in #5640, but this was reversed by a regression in #5785.

Re-adding handling for restricted users is especially important for the upcoming promo.

I also adjusted padding and margins.

packages/manager/src/MainContent.tsx
@@ -21,9 +21,7 @@ import TopMenu from 'src/features/TopMenu';
 import VolumeDrawer from 'src/features/Volumes/VolumeDrawer';
 
 import DefaultLoader from 'src/components/DefaultLoader';
-import ErrorState from 'src/components/ErrorState';
 import Grid from 'src/components/Grid';
-import LandingLoading from 'src/components/LandingLoading';
 import NotFound from 'src/components/NotFound';
 import PreferenceToggle, { ToggleProps } from 'src/components/PreferenceToggle';
 import SideMenu from 'src/components/SideMenu';
@@ -38,10 +36,7 @@ import withFeatureFlags, {
 
 import Logo from 'src/assets/logo/logo-text.svg';
 
-import {
-  isKubernetesEnabled as _isKubernetesEnabled,
-  isObjectStorageEnabled
-} from './utilities/accountCapabilities';
+import { isKubernetesEnabled as _isKubernetesEnabled } from './utilities/accountCapabilities';
 
 const useStyles = makeStyles((theme: Theme) => ({
   appFrame: {
@@ -330,11 +325,10 @@ const MainContent: React.FC<CombinedProps> = props => {
                         <Route exact strict path="/images" component={Images} />
                         <Redirect path="/images*" to="/images" />
                         <Route path="/stackscripts" component={StackScripts} />
-                        {getObjectStorageRoute(
-                          props.accountLoading,
-                          props.accountCapabilities,
-                          props.accountError
-                        )}
+                        <Route
+                          path="/object-storage"
+                          component={ObjectStorage}
+                        />
                         {isKubernetesEnabled && (
                           <Route path="/kubernetes" component={Kubernetes} />
                         )}
@@ -384,34 +378,6 @@ const MainContent: React.FC<CombinedProps> = props => {
       }}
     </PreferenceToggle>
   );
-};
-
-// Render the correct <Route /> component for Object Storage,
-// depending on whether /account is loading or has errors, and
-// whether or not the feature is enabled for this account.
-const getObjectStorageRoute = (
-  accountLoading: boolean,
-  accountCapabilities: AccountCapability[],
-  accountError?: Error | APIError[]
-) => {
-  let component;
-  if (accountLoading) {
-    component = () => <LandingLoading delayInMS={1000} />;
-  } else if (accountError) {
-    component = () => (
-      <ErrorState errorText="An error has occurred. Please reload and try again." />
-    );
-  } else if (isObjectStorageEnabled(accountCapabilities)) {
-    component = ObjectStorage;
-  }
-
-  // If Object Storage is not enabled for this account, return `null`,
-  // which will appear as a 404
-  if (!component) {
-    return null;
-  }
-
-  return <Route path="/object-storage" component={component} />;
 };
 
 export default compose<CombinedProps, Props>(

packages/manager/src/components/PrimaryNav/PrimaryNav.tsx
@@ -18,10 +18,7 @@ import withFeatureFlagConsumer, {
   FeatureFlagConsumerProps
 } from 'src/containers/withFeatureFlagConsumer.container';
 import { MapState } from 'src/store/types';
-import {
-  isKubernetesEnabled,
-  isObjectStorageEnabled
-} from 'src/utilities/accountCapabilities';
+import { isKubernetesEnabled } from 'src/utilities/accountCapabilities';
 import { sendOneClickNavigationEvent } from 'src/utilities/ga';
 import AdditionalMenuItems from './AdditionalMenuItems';
 import styled, { StyleProps } from './PrimaryNav.styles';
@@ -145,20 +142,6 @@ export class PrimaryNav extends React.Component<CombinedProps, State> {
 
     return [
       {
-        conditionToAdd: () => isObjectStorageEnabled(accountCapabilities),
-        insertAfter: 'Volumes',
-        link: {
-          display: 'Object Storage',
-          href: '/object-storage/buckets',
-          activeLinks: [
-            '/object-storage/buckets',
-            '/object-storage/access-keys'
-          ],
-          key: 'object-storage',
-          icon: <Storage />
-        }
-      },
-      {
         conditionToAdd: () => isManagedAccount,
         insertAfter: 'Longview',
         link: {
@@ -226,6 +209,13 @@ export class PrimaryNav extends React.Component<CombinedProps, State> {
         href: '/volumes',
         key: 'volumes',
         icon: <Volume />
+      },
+      {
+        display: 'Object Storage',
+        href: '/object-storage/buckets',
+        activeLinks: ['/object-storage/buckets', '/object-storage/access-keys'],
+        key: 'object-storage',
+        icon: <Storage />
       },
       {
         display: 'NodeBalancers',

packages/manager/src/components/PromotionalOfferCard/PromotionalOfferCard.tsx
@@ -19,14 +19,13 @@ const useStyles = makeStyles((theme: Theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    padding: theme.spacing(2),
+    paddingBottom: 4,
     backgroundColor: theme.bg.main
   },
   fullWidth: {
     flexDirection: 'row',
     alignContent: 'center',
     justifyContent: 'flex-start',
-    padding: theme.spacing(1),
     '& svg': {
       marginRight: theme.spacing(2),
       marginBottom: theme.spacing(1) - 2

packages/manager/src/features/ObjectStorage/ObjectStorageLanding.tsx
@@ -147,11 +147,9 @@ export const ObjectStorageLanding: React.FunctionComponent<CombinedProps> = prop
         </Tabs>
       </AppBar>
       {objPromotionalOffers.map(promotionalOffer => (
-        <PromotionalOfferCard
-          key={promotionalOffer.name}
-          {...promotionalOffer}
-          fullWidth
-        />
+        <div style={{ marginBottom: 4 }} key={promotionalOffer.name}>
+          <PromotionalOfferCard {...promotionalOffer} fullWidth />
+        </div>
       ))}
       <Switch>
         <Route