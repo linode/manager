f31420bbd8d50212b3a151969ee3c48576f4fe28
Adrien Courdavault <acourdavault@linode.com>
2020-02-29 15:16:50 -0500

    add perf check on dashboard

--- packages/manager/cypress/integration/smoke-dashboard.spec.js
+++ packages/manager/cypress/integration/smoke-dashboard.spec.js
@@ -16,4 +16,31 @@ describe('dashboard', () => {
       'NodeBalancers'
     );
   });
+  it.only('checks load time and nb of GET', ()=>{
+    let xhrData = []
+    cy.wrap(xhrData).as('xhrData');
+    cy.server({
+      // Here we handle all requests passing through Cypress' server
+      onRequest: (req) => {
+          console.log(xhrData)
+          xhrData.push(req);
+
+      },
+    })
+    cy.route({
+      method:'GET',
+      url:'/v4/*'
+    }).as('apiGet');
+
+    const MAX_LOAD_TIME_MS = 3000;
+    const MAX_GET_REQ_TO_API = 8;
+    cy.visit('/');
+    cy.get('[data-qa-header]').should('have.text', 'Dashboard');
+    cy.get('[data-qa-card="Linodes"] h2').should('have.text', 'Linodes');
+    cy.window().should('exist')
+    cy.window().then(win=>cy.wrap(win.performance.now()).should('be.lte',MAX_LOAD_TIME_MS));
+
+    cy.get('@xhrData').its('length').should('be.lte',MAX_GET_REQ_TO_API);
+
+  })
 });
