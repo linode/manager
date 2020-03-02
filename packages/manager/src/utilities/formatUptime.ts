4e1f3f3d0725e43c669c842b5fcf6684f3ab63b7
Jared <jaredkobos@gmail.com>
2020-03-02 12:07:07 -0500

    M3-4018 Fix uptime calculation in Longview (#6144)
    * Fix uptime calculation in Longview
    
    * Add test case

--- packages/manager/src/utilities/formatUptime.ts
+++ packages/manager/src/utilities/formatUptime.ts
@@ -6,8 +6,9 @@ export const formatUptime = (uptime: number) => {
    * seconds.
    */
   const duration = moment.duration(uptime, 'seconds');
-  if (duration.days() > 0) {
-    return `${duration.days()}d ${duration.hours()}h ${duration.minutes()}m`;
+  const days = Math.floor(duration.asDays());
+  if (days > 0) {
+    return `${days}d ${duration.hours()}h ${duration.minutes()}m`;
   } else if (duration.hours() > 0) {
     return `${duration.hours()}h ${duration.minutes()}m`;
   } else if (duration.minutes() > 0) {
