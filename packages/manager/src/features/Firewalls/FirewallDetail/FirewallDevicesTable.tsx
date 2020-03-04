b3b17565b4d51c24ccecb128730ad047e9ce73d8
Jared <jaredkobos@gmail.com>
2020-03-04 09:21:57 -0500

    Use TableContentWrapper to replace FirewallRows (#6155)

--- packages/manager/src/features/Firewalls/FirewallDetail/FirewallDevicesTable.tsx
+++ packages/manager/src/features/Firewalls/FirewallDetail/FirewallDevicesTable.tsx
@@ -10,7 +10,7 @@ import OrderBy from 'src/components/OrderBy';
 import Paginate from 'src/components/Paginate';
 import PaginationFooter from 'src/components/PaginationFooter';
 import Table from 'src/components/Table';
-import TableContentWrapper from 'src/components/TableContentWrapper.tsx';
+import TableContentWrapper from 'src/components/TableContentWrapper';
 import TableSortCell from 'src/components/TableSortCell';
 import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
 import FirewallDeviceRow from './FirewallDeviceRow';
