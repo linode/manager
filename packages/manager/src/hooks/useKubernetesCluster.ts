49814f7879219b0978b76e25f06d13399ae078bc
Jared Kobos <jaredkobos@gmail.com>
2020-03-06 13:35:21 -0500

    Add useKubernetesClusters

---
+++ packages/manager/src/hooks/useKubernetesCluster.ts
@@ -0,0 +1,22 @@
+import { KubernetesCluster } from 'linode-js-sdk/lib/kubernetes/types';
+import { useDispatch, useSelector } from 'react-redux';
+import { ApplicationState } from 'src/store';
+import { State } from 'src/store/kubernetes/kubernetes.reducer';
+import { requestKubernetesClusters as _request } from 'src/store/kubernetes/kubernetes.requests';
+
+export interface DomainsProps {
+  kubernetesClusters: State;
+  requestKubernetesClusters: () => Promise<KubernetesCluster[]>;
+}
+
+export const useDomains = () => {
+  const dispatch = useDispatch();
+  const kubernetesClusters = useSelector(
+    (state: ApplicationState) => state.__resources.kubernetes
+  );
+  const requestKubernetesClusters = dispatch(_request());
+
+  return { kubernetesClusters, requestKubernetesClusters };
+};
+
+export default useDomains;
