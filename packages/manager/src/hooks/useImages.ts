e5a4de842f726edae7ce84414bfc57a7516c02ec
Jared Kobos <jaredkobos@gmail.com>
2020-03-06 12:57:17 -0500

    Add useImages

---
+++ packages/manager/src/hooks/useImages.ts
@@ -0,0 +1,22 @@
+import { Image } from 'linode-js-sdk/lib/images/types';
+import { useDispatch, useSelector } from 'react-redux';
+import { ApplicationState } from 'src/store';
+import { State } from 'src/store/image/image.reducer';
+import { requestImages as _request } from 'src/store/image/image.requests';
+
+export interface ImagesProps {
+  images: State;
+  requestImages: () => Promise<Image[]>;
+}
+
+export const useImages = () => {
+  const dispatch = useDispatch();
+  const images = useSelector(
+    (state: ApplicationState) => state.__resources.images
+  );
+  const requestImages = dispatch(_request());
+
+  return { images, requestImages };
+};
+
+export default useImages;
