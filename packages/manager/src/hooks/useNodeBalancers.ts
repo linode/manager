3319f82c2621fbd441281f4bb3ad3c17e1a33c06
Jared Kobos <jaredkobos@gmail.com>
2020-03-06 13:32:01 -0500

    Add useNodeBalancers

---
+++ packages/manager/src/hooks/useNodeBalancers.ts
@@ -0,0 +1,22 @@
+import { NodeBalancer } from 'linode-js-sdk/lib/nodebalancers/types';
+import { useDispatch, useSelector } from 'react-redux';
+import { ApplicationState } from 'src/store';
+import { State } from 'src/store/nodeBalancer/nodeBalancer.reducer';
+import { getAllNodeBalancers as _request } from 'src/store/nodeBalancer/nodeBalancer.requests';
+
+export interface NodeBalancersProps {
+  nodeBalancers: State;
+  requestNodeBalancers: () => Promise<NodeBalancer[]>;
+}
+
+export const useNodeBalancers = () => {
+  const dispatch = useDispatch();
+  const nodeBalancers = useSelector(
+    (state: ApplicationState) => state.__resources.nodeBalancers
+  );
+  const requestNodeBalancers = dispatch(_request());
+
+  return { nodeBalancers, requestNodeBalancers };
+};
+
+export default useNodeBalancers;
