0e0b2ccb7be3ab1c7ddf44cd8a6b3f6dcf4a90b2
Adrien courdavault <adrien.courdavault@gmail.com>
2020-03-13 20:27:27 -0400

    fix expireDate for login that was hardcoded :facepalm: (#6195)
    Co-authored-by: Adrien Courdavault <acourdavault@linode.com>

--- packages/manager/cypress/support/commands.js
+++ packages/manager/cypress/support/commands.js
@@ -72,7 +72,6 @@ Cypress.Commands.add('login', () => {
     cy.request(options).then(resp => {
       cy.log(resp);
       expect(resp.status).equals(200);
-      // cy.visit('/');
     });
   });
 });
@@ -90,25 +89,21 @@ Cypress.Commands.add('login2', () => {
   );
   window.localStorage.setItem('authentication/scopes', '*');
   cy.log(window.localStorage.getItem('authentication/oauth-token'));
-  window.localStorage.setItem(
-    'authentication/expires',
-    '2020-04-10T15:10:16.295Z'
-  );
-  //window.localStorage.setItem('authentication/latest-refresh', '1553782241459');
+  const expireDate = Cypress.moment().add(30, 'days');
+  const isoExpire = expireDate.toISOString();
+  cy.log(isoExpire);
+  window.localStorage.setItem('authentication/expires', isoExpire);\
+
   window.localStorage.setItem(
     'authentication/expire-datetime',
-    'Fri Mar 13 2020 11:36:50 GMT-0400 (Eastern Daylight Time)'
+    isoExpire
   );
   window.localStorage.setItem(
     'authentication/token',
     'Bearer ' + Cypress.env('oauthtoken')
   );
-  // window.localStorage.setItem(
-  //   'authentication/nonce',
-  //   'd660f305-25c6-42e1-99ab-91fd04dad159'
-  // );
   window.localStorage.setItem(
     'authentication/expire',
-    'Fri Mar 13 2020 11:54:07 GMT-0500 (Eastern Standard Time)'
+    isoExpire
   );
 });
