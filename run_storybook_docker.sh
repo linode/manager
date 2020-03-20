fa007462b2b7b19c7d4c0ec6650cc2ca16de67d3
Adrien courdavault <adrien.courdavault@gmail.com>
2020-03-20 14:21:44 -0400

    docs for yarn commands and clean package.json (#6171)
    * docs for yarn commands and clean package.json
    * clean docs
    * clean lockfile and dockerfile
    
    Co-authored-by: Jared <jaredkobos@gmail.com>
    Co-authored-by: Adrien Courdavault <acourdavault@linode.com>

--- run_storybook_docker.sh
+++ run_storybook_docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "starting server"
 nohup yarn storybook&
-echo "waiting for storybook server to start"
+PID=$!
+echo "waiting for storybook server to start, PID $PID"
 # need npx for docker container
 yarn run wait-on http://localhost:6006
 yarn storybook:e2e
+kill $PID
