5ebe13e0e6d307a79c7d51b95836a796a13f3859
Adrien courdavault <adrien.courdavault@gmail.com>
2020-03-25 14:59:41 -0400

    add e2e deeplink test (#6220)
    * add deeplink test and factorize constants
    * adding profile routes and first/skip features
    * skip by default as issues with tab exist (reach refactor)
    
    Co-authored-by: Adrien Courdavault <acourdavault@linode.com>

---
+++ packages/manager/cypress/integration/smoke-deep-link.spec.js
@@ -0,0 +1,27 @@
+import { pages } from '../support/ui/constants';
+
+describe('smoke - deep link', () => {
+  //check if we run oinly one test
+
+  pages.forEach(page => {
+    if (!page.goWithUI) {
+      return;
+    }
+    describe.skip(`Got to ${page.name}`, () => {
+      beforeEach(() => {
+        cy.login2();
+      });
+
+      page.goWithUI.forEach(uiPath => {
+        (page.first ? it.only : page.skip ? it.skip : it)(
+          `by ${uiPath.name}`,
+          () => {
+            expect(uiPath.name).not.to.be.empty;
+            uiPath.go();
+            cy.url().should('be.eq', `${Cypress.config('baseUrl')}${page.url}`);
+          }
+        );
+      });
+    });
+  });
+});
