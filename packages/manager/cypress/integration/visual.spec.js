6944767b0f8888235def45708f18f058e8be6ecf
Adrien Courdavault <acourdavault@linode.com>
2020-03-25 15:32:46 -0400

    clean visual regression test and ignore pattenrs

--- packages/manager/cypress/integration/visual.spec.js
+++ packages/manager/cypress/integration/visual.spec.js
@@ -2,10 +2,11 @@ describe('visual regressions', () => {
   beforeEach(() => {
     cy.login2();
   });
-  it('should display the help search correcly', () => {
+  it('should display the nav bar correcly', () => {
     cy.visit('/support');
-    cy.get('[data-qa-tile="Linode Support Bot"]')
-      .checkSnapshot('support-bot-tile', 0.0)
+    cy.get('nav')
+      .should('be.visible')
+      .checkSnapshot('nav-menu', 0.0)
       .should('be.true');
   });
 });
