9a8d207776e96b7ddd1d5a8cf1c637537bc6f0e2
Jared <jaredkobos@gmail.com>
2020-03-25 14:29:04 -0400

    Bump Sanitize-html to 1.22.1 and remove unneeded patch (#6227)

--- packages/manager/src/utilities/sanitize-html/sanitizeHTML.ts
+++ packages/manager/src/utilities/sanitize-html/sanitizeHTML.ts
@@ -26,30 +26,7 @@ export const sanitizeHTML = (text: string) =>
         };
       }
     },
-    /**
-     * this option is not supported and was patched
-     * See: https://github.com/punkave/sanitize-html/pull/169
-     */
-    escapeDisallowedTags: true
-    /** this is basically just converting script tags to text */
-    // transformTags: {
-    //   script: (tagName, attrs: Record<string, string>) => {
-    //     /**
-    //      * get all attributes of the script tag and recreate them as they were typed
-    //      * i.e src="hello.js"
-    //      */
-    //     const attrsAsString = Object.keys(attrs).reduce((accum, eachKey) => {
-    //       return `${accum} ${eachKey}="${attrs[eachKey]}"`;
-    //     }, '');
-
-    //     /** return the script tag as text inside a p tag */
-    //     return {
-    //       tagName: 'script',
-    //       text: `&lt;${tagName}${attrsAsString}&gt;&lt;/${tagName}&gt;`,
-    //       attribs: {}
-    //     };
-    //   }
-    // }
+    disallowedTagsMode: 'escape'
   }).trim();
 
 export const isURLValid = (url: string) =>
