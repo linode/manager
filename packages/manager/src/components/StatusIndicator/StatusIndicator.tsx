a99bf34b153f0b5160cfe27586e21a9a4987c3dd
John Callahan <johnwcallahan@gmail.com>
2020-03-31 18:07:34 -0400

    Repurpose StatusIndicator component
    This was unused, so I repurposed it for use in the NodePools component.

--- packages/manager/src/components/StatusIndicator/StatusIndicator.tsx
+++ packages/manager/src/components/StatusIndicator/StatusIndicator.tsx
@@ -1,66 +1,40 @@
-import Cached from '@material-ui/icons/Cached';
+import * as classnames from 'classnames';
 import * as React from 'react';
-import {
-  createStyles,
-  Theme,
-  withStyles,
-  WithStyles
-} from 'src/components/core/styles';
+import Circle from 'src/assets/icons/circle.svg';
+import { makeStyles, Theme } from 'src/components/core/styles';
 
 export interface Props {
-  status: 'loading' | 'active' | 'inactive' | 'error';
+  status: 'active' | 'other';
 }
 
-type CSSClasses = 'loading' | 'active' | 'inactive' | 'error';
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    alignContent: 'center'
+  },
+  active: {
+    color: '#0ac267'
+  },
+  other: {
+    color: theme.color.grey5
+  }
+}));
 
-const defaultStyles = {
-  fontSize: '1.5rem',
-  'user-select': 'none'
-};
+const StatusIndicator: React.FC<Props> = props => {
+  const { status } = props;
 
-const styles = (theme: Theme) =>
-  createStyles({
-    '@keyframes rotate': {
-      from: {
-        transform: 'rotate(0deg)'
-      },
-      to: {
-        transform: 'rotate(360deg)'
-      }
-    },
-    loading: {
-      fontSize: '1.0rem',
-      position: 'relative',
-      top: 1,
-      left: -1,
-      '& svg': {
-        animation: '$rotate 2s linear infinite',
-        fill: theme.color.offBlack
-      }
-    },
-    active: {
-      color: '#01b159',
-      ...defaultStyles
-    },
-    error: {
-      color: '#d01e1e',
-      ...defaultStyles
-    },
-    inactive: {
-      color: '#efefef',
-      ...defaultStyles
-    }
-  });
+  const classes = useStyles();
 
-const StatusIndicator = (props: Props & WithStyles<CSSClasses>) => {
-  const { classes, status } = props;
   return (
-    <span className={`${classes[status]}`} data-qa-status={status}>
-      {status === 'loading' ? <Cached /> : <span>&#x25CF;</span>}
+    <span
+      className={classnames({
+        [classes.root]: true,
+        [classes.active]: status === 'active'
+      })}
+    >
+      <Circle />
     </span>
   );
 };
 
-const styled = withStyles(styles);
-
-export default styled(StatusIndicator);
+export default React.memo(StatusIndicator);
