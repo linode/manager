6308e5ecdea373d3a652659b9693cdcb0fad81fa
John Callahan <johnwcallahan@gmail.com>
2020-04-07 14:34:43 -0400

    More Sentry improvements (#6277)
    * Ignore Safari extension errors
    
    The Evernote Web Clipper extension was throwing errors.
    
    To reproduce:
    
    1. Install the Evernote Web Clipper and login to ensure proper installation.
    2. Quit Safari.
    3. Open Safari and go to cloud.linode.com.
    4. Observe: console errors from Evernote.
    
    To silence these errors, I added "safari-extension" to the blacklist and check the stack trace against it.
    
    I also renamed a variable for clarity.
    
    * Ignore chrome, xterm, and mui errros

--- packages/manager/src/exceptionReporting.ts
+++ packages/manager/src/exceptionReporting.ts
@@ -1,17 +1,25 @@
 import { captureException, configureScope, withScope } from '@sentry/browser';
-import { pathOr } from 'ramda';
 import { SENTRY_URL } from 'src/constants';
 import { initSentry } from 'src/initSentry';
 
 initSentry();
 
-const promiseRejectionsToIgnore: string[] = [
+const errorsToIgnore: string[] = [
   'Invalid OAuth Token',
-  'Not Found'
+  'Not Found',
+  'You are not authorized',
+  'safari-extension',
+  'chrome-extension',
+  // We know this is a problem. @todo: implement flow control in Lish.
+  'write data discarded, use flow control to avoid losing data',
+  // This is an error coming from the MUI Ripple effect.
+  'TouchRipple'
 ];
 
-window.addEventListener('unhandledrejection', err => {
-  if (pathOr('', ['reason', 'stack'], err).match(/launchdarkly/i)) {
+window.addEventListener('unhandledrejection', event => {
+  const stack: string = event?.reason?.stack ?? '';
+
+  if (stack.match(/launchdarkly/i)) {
     /**
      * This doesn't affect error reporting, but avoids some console noise
      * until we've cleaned up Sentry.
@@ -19,7 +27,7 @@ window.addEventListener('unhandledrejection', err => {
     return;
   }
 
-  const firstReason = pathOr('', [0, 'reason'], err.reason);
+  const firstReason = event.reason?.[0]?.reason ?? '';
 
   /*
     if our error is an error we want to ignore, don't report to Sentry
@@ -30,15 +38,16 @@ window.addEventListener('unhandledrejection', err => {
     to Sentry
    */
   if (
-    promiseRejectionsToIgnore.some(
-      eachString => !!firstReason.match(new RegExp(eachString, 'gmi'))
-    )
+    errorsToIgnore.some(eachString => {
+      const regex = new RegExp(eachString, 'gmi');
+      return !!firstReason.match(regex) || stack.match(regex);
+    })
   ) {
     return;
   }
 
   /* otherwise report to sentry as normal */
-  reportException(err.reason);
+  reportException(event.reason);
 });
 
 // Wrapper around Sentry's `captureException` function. A local scope is created
