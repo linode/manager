be3c8b3f3b1ee76419aed9f4eab401f4ae537e38
Adrien Courdavault <acourdavault@linode.com>
2020-04-14 09:36:34 -0400

    replace login command and update Nav bar sreenshot

--- packages/manager/cypress/integration/visual.spec.js
+++ packages/manager/cypress/integration/visual.spec.js
@@ -1,9 +1,6 @@
 describe('visual regressions', () => {
-  beforeEach(() => {
-    cy.login2();
-  });
   it('should display the nav bar correcly', () => {
-    cy.visit('/support');
+    cy.visitWithLogin('/support');
     cy.get('nav')
       .should('be.visible')
       .checkSnapshot('nav-menu', 0.0)
