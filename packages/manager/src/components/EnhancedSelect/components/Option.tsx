ad050e7c9ea1caff15e61a93901e756b7046f48f
Jared Kobos <jaredkobos@gmail.com>
2020-04-21 15:25:00 -0400

    Try and fail to fix option props import

--- packages/manager/src/components/EnhancedSelect/components/Option.tsx
+++ packages/manager/src/components/EnhancedSelect/components/Option.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import _Option, { OptionProps } from 'react-select/src/components/Option';
+import { components } from 'react-select';
+import { OptionProps } from 'react-select/src/components/Option';
 
 interface Props extends OptionProps<any> {
   value: number | string;
@@ -9,7 +10,7 @@ interface Props extends OptionProps<any> {
 const Option: React.StatelessComponent<Props> = props => {
   return (
     <div data-qa-option={String(props.value)} {...props.attrs}>
-      <_Option {...props} />
+      <components.Option {...props} />
     </div>
   );
 };
