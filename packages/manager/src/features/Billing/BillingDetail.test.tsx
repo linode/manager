329a3ee896d58055483bca0922b70d72712c075b
John Callahan <johnwcallahan@gmail.com>
2020-04-28 15:21:28 -0400

    Use wait() instead of act()
    [review feedback]

--- packages/manager/src/features/Billing/BillingDetail.test.tsx
+++ packages/manager/src/features/Billing/BillingDetail.test.tsx
@@ -1,7 +1,7 @@
-import { act, cleanup } from '@testing-library/react';
+import { cleanup, wait } from '@testing-library/react';
 import * as React from 'react';
-import { history, match, mockLocation } from 'src/__data__/reactRouterProps';
 import { renderWithTheme } from 'src/utilities/testHelpers';
+import { history, match, mockLocation } from 'src/__data__/reactRouterProps';
 import { BillingDetail } from './BillingDetail';
 
 afterEach(cleanup);
@@ -16,18 +16,16 @@ jest.mock('linode-js-sdk/lib/account', () => {
 
 describe('Account Landing', () => {
   it('should render', async () => {
-    await act(async () => {
-      const { getByTestId } = renderWithTheme(
-        <BillingDetail
-          history={history}
-          location={mockLocation}
-          match={match}
-          setDocs={jest.fn()}
-          clearDocs={jest.fn()}
-        />
-      );
-      getByTestId('billing-detail');
-      // Todo: add some get-by-texts once the correct text is available
-    });
+    const { getByTestId } = renderWithTheme(
+      <BillingDetail
+        history={history}
+        location={mockLocation}
+        match={match}
+        setDocs={jest.fn()}
+        clearDocs={jest.fn()}
+      />
+    );
+    await wait(() => getByTestId('billing-detail'));
+    // Todo: add some get-by-texts once the correct text is available
   });
 });
