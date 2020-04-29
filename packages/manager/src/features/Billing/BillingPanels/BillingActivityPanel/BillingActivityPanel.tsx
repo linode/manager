ba97965ce594e5d3603c70e32e1325a4ce161d22
John Callahan <johnwcallahan@gmail.com>
2020-04-29 16:22:54 -0400

    M3-4107 [Billing] Activity Feed (#6346)
    * WIP activity feed
    
    * Fix component props
    
    * Add error handling
    
    * Clean up and fix test
    
    * Fix props.children in TableContentWrapper
    
    * Wrap test in async act
    
    Rendering the component was resulting in an "Updated outside of act()" warning. Solution from https://github.com/facebook/react/issues/15379#issuecomment-482101020.
    
    * Use wait() instead of act()
    
    [review feedback]
    
    * Update Currency component
    
    We needed to be able to use a custom format for currency display. The library we were using allows you to do this, but is poorly documented. There are other problems too, like the fact that it is out of date, there aren't many tests, etc. Since we weren't using many features of the library, I opted to go with a custom implementation instead.
    
    * Adjustments to currency display
    
    All payments and credits are displayed as negative numbers wrapped in parentheses.
    
    * Remove react-currency-formatter
    
    * Move pagination footer
    
    [review feedback]
    Move pagination footer inside <Paper />
    
    * Use Intl.NumberFormatter to handle commas
    
    A unit test caught a regression, which is that quantities >= 1000 were no longer separated by a comma. I used the Intl.NumberFormatter to handle for this, and added a unit test to <Currency/>
    
    * Right-align amount column
    
    * Add tests for BillingActivityPanel

--- packages/manager/src/features/Billing/BillingPanels/BillingActivityPanel/BillingActivityPanel.tsx
+++ packages/manager/src/features/Billing/BillingPanels/BillingActivityPanel/BillingActivityPanel.tsx
@@ -1,135 +1,211 @@
-import { Account, getInvoices, Invoice } from 'linode-js-sdk/lib/account';
-import { APIError } from 'linode-js-sdk/lib/types';
-import { compose, pathOr } from 'ramda';
+import {
+  getInvoices,
+  getPayments,
+  Invoice,
+  Payment
+} from 'linode-js-sdk/lib/account';
+import { makeStyles, Theme } from 'src/components/core/styles';
 import * as React from 'react';
-import { connect } from 'react-redux';
 import Paper from 'src/components/core/Paper';
 import TableBody from 'src/components/core/TableBody';
 import TableHead from 'src/components/core/TableHead';
 import Typography from 'src/components/core/Typography';
-import paginate, { PaginationProps } from 'src/components/Pagey';
+import Currency from 'src/components/Currency';
+import DateTimeDisplay from 'src/components/DateTimeDisplay';
+import OrderBy from 'src/components/OrderBy';
+import Paginate from 'src/components/Paginate';
 import PaginationFooter from 'src/components/PaginationFooter';
 import Table from 'src/components/Table';
 import TableCell from 'src/components/TableCell';
-import TableRow from 'src/components/TableRow';
-import TableRowEmptyState from 'src/components/TableRowEmptyState';
-import TableRowError from 'src/components/TableRowError';
-import TableRowLoading from 'src/components/TableRowLoading';
-import { ApplicationState } from 'src/store';
-import { requestAccount } from 'src/store/account/account.requests';
-import { ThunkDispatch } from 'src/store/types';
-import RecentInvoiceRow from './BillingItemRow';
+import TableContentWrapper from 'src/components/TableContentWrapper';
+import TableRow, { TableRowProps } from 'src/components/TableRow';
+import { getAll } from 'src/utilities/getAll';
+import { APIError } from 'linode-js-sdk/lib/types';
+import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
 
-type CombinedProps = PaginationProps<Invoice> & StateProps;
+const getAllInvoices = getAll<Invoice>(getInvoices);
+const getAllPayments = getAll<Payment>(getPayments);
 
-interface State {
-  loading: boolean;
+const useStyles = makeStyles((theme: Theme) => ({
+  header: {
+    marginBottom: theme.spacing()
+  },
+  totalColumn: {
+    textAlign: 'right',
+    width: '10%'
+  }
+}));
+
+interface ActivityFeedItem {
+  label: string;
+  total: number;
+  date: string;
+  type: 'payment' | 'invoice';
+  id: number;
 }
 
-class BillingActivityPanel extends React.Component<CombinedProps, State> {
-  state: State = {
-    loading: false
-  };
+export const BillingActivityPanel: React.FC<{}> = () => {
+  const classes = useStyles();
 
-  componentDidMount() {
-    if (!this.props.data) {
-      this.setState({ loading: true });
-      this.props.handleOrderChange('date', 'desc');
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<APIError[] | undefined>();
+  const [combinedData, setCombinedData] = React.useState<ActivityFeedItem[]>(
+    []
+  );
+
+  React.useEffect(() => {
+    setLoading(true);
+
+    Promise.all([getAllInvoices(), getAllPayments()])
+      .then(([invoices, payments]) => {
+        const _combinedData: ActivityFeedItem[] = [
+          ...invoices.data.map(invoiceToActivityFeedItem),
+          ...payments.data.map(paymentToActivityFeedItem)
+        ];
+        setCombinedData(_combinedData);
+        setLoading(false);
+      })
+      .catch(_error => {
+        setError(
+          getAPIErrorOrDefault(
+            _error,
+            'There was an error retrieving your billing activity.'
+          )
+        );
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h2" className={classes.header}>
+        Activity
+      </Typography>
+      <OrderBy data={combinedData} orderBy={'date'} order={'desc'}>
+        {({ data: orderedData }) => (
+          <Paginate pageSize={25} data={orderedData}>
+            {({
+              data: paginatedAndOrderedData,
+              count,
+              handlePageChange,
+              handlePageSizeChange,
+              page,
+              pageSize
+            }) => (
+              <>
+                <Paper>
+                  <Table aria-label="List of Invoices and Payments">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell style={{ width: '17%' }}>
+                          Description
+                        </TableCell>
+                        <TableCell style={{ width: '10%' }}>Date</TableCell>
+                        <TableCell className={classes.totalColumn}>
+                          Amount
+                        </TableCell>
+                        <TableCell />
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableContentWrapper
+                        length={paginatedAndOrderedData.length}
+                        loading={loading}
+                        error={error}
+                      >
+                        {paginatedAndOrderedData.map(thisItem => (
+                          <ActivityFeedItem
+                            key={`${thisItem.type}-${thisItem.id}`}
+                            {...thisItem}
+                          />
+                        ))}
+                      </TableContentWrapper>
+                    </TableBody>
+                  </Table>
+                </Paper>
+                <PaginationFooter
+                  count={count}
+                  handlePageChange={handlePageChange}
+                  handleSizeChange={handlePageSizeChange}
+                  page={page}
+                  pageSize={pageSize}
+                  eventCategory="Billing Activity Table"
+                />
+              </>
+            )}
+          </Paginate>
+        )}
+      </OrderBy>
+    </>
+  );
+};
+
+export const ActivityFeedItem: React.FC<ActivityFeedItem> = React.memo(
+  props => {
+    const classes = useStyles();
+
+    const { date, label, total, id, type } = props;
+
+    const rowProps: TableRowProps = {};
+    if (type === 'invoice') {
+      rowProps.rowLink = `/account/billing/invoices/${id}`;
     }
-  }
-
-  render() {
-    const { data, page, pageSize, count } = this.props;
 
     return (
-      <>
-        <Typography variant="h2">Activity</Typography>
-        <Paper>
-          <Table aria-label="List of Recent Invoices">
-            <TableHead>
-              <TableRow>
-                <TableCell>Date Created</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell>Amount</TableCell>
-                <TableCell />
-              </TableRow>
-            </TableHead>
-            <TableBody>{this.renderContent()}</TableBody>
-          </Table>
-          {data && data.length > 0 && (
-            <PaginationFooter
-              count={count}
-              page={page}
-              pageSize={pageSize}
-              handlePageChange={this.props.handlePageChange}
-              handleSizeChange={this.props.handlePageSizeChange}
-              eventCategory="recent invoices panel"
-            />
-          )}
-        </Paper>
-      </>
+      <TableRow {...rowProps} data-testid={`${type}-${id}`}>
+        <TableCell>{label}</TableCell>
+        <TableCell>
+          <DateTimeDisplay format="YYYY-MM-DD" value={date} />
+        </TableCell>
+        <TableCell className={classes.totalColumn}>
+          <Currency quantity={total} wrapInParentheses={total < 0} />
+        </TableCell>
+        {/* @todo icon */}
+        <TableCell />
+      </TableRow>
     );
   }
-
-  renderContent = () => {
-    const {
-      data,
-      error,
-      loading,
-      account,
-      accountLoading,
-      accountError
-    } = this.props;
-
-    if (loading || accountLoading) {
-      return <TableRowLoading colSpan={4} />;
-    }
-
-    if (error || accountError) {
-      return (
-        <TableRowError
-          colSpan={4}
-          message="We were unable to load your invoices."
-        />
-      );
-    }
-
-    return data && data.length > 0 && account ? (
-      data.map((eachInvoice, index) => (
-        <RecentInvoiceRow key={index} invoice={eachInvoice} account={account} />
-      ))
-    ) : (
-      <TableRowEmptyState colSpan={4} />
-    );
-  };
-}
-
-interface ReduxState {
-  account?: Account;
-  accountLoading: boolean;
-  accountError?: APIError[] | Error;
-}
-
-interface StateProps extends ReduxState {
-  requestAccount: () => void;
-}
-
-const connected = connect(
-  (state: ApplicationState): ReduxState => ({
-    account: state.__resources.account.data,
-    accountLoading: pathOr(false, ['__resources', 'account', 'loading'], state),
-    accountError: state.__resources.account.error.read
-  }),
-  (dispatch: ThunkDispatch): { requestAccount: () => void } => ({
-    requestAccount: () => dispatch(requestAccount())
-  })
 );
 
-const updatedRequest = (ownProps: any, params: any, filters: any) =>
-  getInvoices(params, filters).then(response => response);
+export default React.memo(BillingActivityPanel);
 
-const paginated = paginate(updatedRequest);
+// =============================================================================
+// Utilities
+// =============================================================================
+export const invoiceToActivityFeedItem = (
+  invoice: Invoice
+): ActivityFeedItem => {
+  const { id, label, date, total } = invoice;
 
-const enhanced = compose(connected, paginated);
+  // Negative invoice totals are effectively credits, so replace the label.
+  const adjustedLabel = total < 0 ? label.replace('Invoice', 'Credit') : label;
 
-export default enhanced(BillingActivityPanel);
+  return {
+    id,
+    label: adjustedLabel,
+    date,
+    total,
+    type: 'invoice'
+  };
+};
+
+export const paymentToActivityFeedItem = (
+  payment: Payment
+): ActivityFeedItem => {
+  const { date, id, usd } = payment;
+
+  // Negative payments are effectively credits.
+  const label = usd < 0 ? 'Credit' : 'Payment';
+
+  // We always want Payments (and negative Payments, i.e. Credits) to appear as a negative number.
+  // The delineation between these two types comes from the label ("Credit" or "Payment").
+  const total = usd > 0 ? -usd : usd;
+
+  return {
+    label,
+    date,
+    id,
+    total,
+    type: 'payment'
+  };
+};
