052aaf886d2e6998a24afd6b9a277e7c6ec09553
Jared Kobos <jaredkobos@gmail.com>
2020-04-30 09:13:36 -0400

    Add unit tests

--- packages/manager/src/store/account/account.requests.ts
+++ packages/manager/src/store/account/account.requests.ts
@@ -4,20 +4,15 @@ import {
   updateAccountInfo
 } from 'linode-js-sdk/lib/account';
 
-import { ThunkActionCreator } from 'src/store/types';
 import { createRequestThunk } from '../store.helpers';
-import {
-  profileRequest,
-  profileRequestFail,
-  profileRequestSuccess,
-  updateAccountActions
-} from './account.actions';
+import { requestAccountActions, updateAccountActions } from './account.actions';
 
 export const stripInvalidPromos = (data: Account) => {
   /**
    * It's possible for an incorrectly created Expiring Credit
    * to have an expire_dt of null. Admin and billing ignore
-   * such promos, but since we only display the first promo we find
+   * such promos, but since when showing billing info
+   * we only display the first promo we find
    * (there's only supposed to be one active per account), once
    * a user has one of these "phantom" credits on their account
    * that's all we'll ever show.
@@ -36,21 +31,9 @@ export const stripInvalidPromos = (data: Account) => {
   };
 };
 
-export const requestAccount: ThunkActionCreator<Promise<
-  Account
->> = () => dispatch => {
-  dispatch(profileRequest());
-  return getAccountInfo()
-    .then(stripInvalidPromos)
-    .then(response => {
-      dispatch(profileRequestSuccess(response));
-      return response;
-    })
-    .catch(err => {
-      dispatch(profileRequestFail(err));
-      return err;
-    });
-};
+export const requestAccount = createRequestThunk(requestAccountActions, () =>
+  getAccountInfo().then(stripInvalidPromos)
+);
 
 export const updateAccount = createRequestThunk(
   updateAccountActions,
