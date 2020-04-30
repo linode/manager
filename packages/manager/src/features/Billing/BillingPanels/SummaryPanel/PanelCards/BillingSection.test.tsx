0654199bb59bb72282e262193be1c6b01d77eb8b
Jared Kobos <jaredkobos@gmail.com>
2020-04-29 20:59:46 -0400

    Add helper for getting text across markup

--- packages/manager/src/features/Billing/BillingPanels/SummaryPanel/PanelCards/BillingSection.test.tsx
+++ packages/manager/src/features/Billing/BillingPanels/SummaryPanel/PanelCards/BillingSection.test.tsx
@@ -1,7 +1,7 @@
 import { cleanup, render } from '@testing-library/react';
 import * as React from 'react';
 
-import { wrapWithTheme } from 'src/utilities/testHelpers';
+import { withMarkup, wrapWithTheme } from 'src/utilities/testHelpers';
 
 import BillingSection from './BillingSection';
 
@@ -21,20 +21,20 @@ describe('BillingSection component', () => {
   });
 
   it('should render balance due as a positive number', () => {
-    const { queryAllByText } = renderComponent({ balance: 10.0 });
-    expect(queryAllByText('$10.00')).toHaveLength(1);
+    const { getByText } = renderComponent({ balance: 10.0 });
+    expect(withMarkup(getByText)('$10.00'));
   });
 
   it('should show a negative balance as a credit (parenthesized positive number) if the balance is negative', () => {
-    const { queryAllByText } = renderComponent({
+    const { getByText } = renderComponent({
       balance: -10.0,
       showNegativeAsCredit: true
     });
-    expect(queryAllByText('($10.00)')).toHaveLength(1);
+    expect(withMarkup(getByText)('($10.00)'));
   });
 
   it('should display a positive credit in parentheses', () => {
-    const { queryAllByText } = renderComponent({ credit: 10.0 });
-    expect(queryAllByText('($10.00)')).toHaveLength(1);
+    const { getByText } = renderComponent({ credit: 10.0 });
+    expect(withMarkup(getByText)('($10.00)'));
   });
 });
--- packages/manager/src/utilities/testHelpers.tsx
+++ packages/manager/src/utilities/testHelpers.tsx
@@ -1,4 +1,4 @@
-import { render, RenderResult } from '@testing-library/react';
+import { MatcherFunction, render, RenderResult } from '@testing-library/react';
 import { ResourcePage } from 'linode-js-sdk/lib/types';
 import * as React from 'react';
 import { Provider } from 'react-redux';
@@ -141,3 +141,16 @@ export const includesActions = (
       : expect(query(action)).not.toBeInTheDocument();
   }
 };
+
+type Query = (f: MatcherFunction) => HTMLElement;
+
+export const withMarkup = (query: Query) => (text: string): HTMLElement =>
+  query((content: string, node: HTMLElement) => {
+    const hasText = (node: HTMLElement) => node.textContent === text;
+    const childrenDontHaveText = Array.from(node.children).every(
+      child => !hasText(child as HTMLElement)
+    );
+    return hasText(node) && childrenDontHaveText;
+  });
+
+export default withMarkup;
