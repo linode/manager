eac6343a08c168b6185b1ebb850127ede68174a8
Jared <jaredkobos@gmail.com>
2020-05-15 12:02:58 -0400

    Move recentEvent from SDK Volume type and add to ExtendedType in features/volumes/types (#6406)

--- packages/manager/src/features/Volumes/types.ts
+++ packages/manager/src/features/Volumes/types.ts
@@ -1,6 +1,8 @@
-import { Volume } from 'linode-js-sdk/lib/volumes';
+import { Event } from 'linode-js-sdk/lib/account/types';
+import { Volume } from 'linode-js-sdk/lib/volumes/types';
 
 export interface ExtendedVolume extends Volume {
+  recentEvent?: Event;
   linodeLabel?: string;
   linodeStatus?: string;
 }
