151fb43fb9d3d6641aa921c33094538b57b13d22
Jared Kobos <jaredkobos@gmail.com>
2020-05-15 16:05:31 -0400

    Refactor Managed issues reducer

--- packages/manager/src/store/managed/issues.actions.ts
+++ packages/manager/src/store/managed/issues.actions.ts
@@ -1,6 +1,7 @@
 import { ManagedIssue } from 'linode-js-sdk/lib/managed';
 import { APIError } from 'linode-js-sdk/lib/types';
 import actionCreatorFactory from 'typescript-fsa';
+import { GetAllData } from 'src/utilities/getAll';
 
 export const actionCreator = actionCreatorFactory(`@@manager/managed`);
 
@@ -11,6 +12,6 @@ export interface ExtendedIssue extends ManagedIssue {
 
 export const requestManagedIssuesActions = actionCreator.async<
   void,
-  ExtendedIssue[],
+  GetAllData<ExtendedIssue>,
   APIError[]
 >('request-issues');
