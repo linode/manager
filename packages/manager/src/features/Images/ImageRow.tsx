95d73c0fe250ac2c137ecd037f7b35bc959e386b
Dajahi Wiley <dwiley@linode.com>
2020-05-26 18:51:52 -0400

    Initial commit for progress bar in ImageRow

--- packages/manager/src/features/Images/ImageRow.tsx
+++ packages/manager/src/features/Images/ImageRow.tsx
@@ -1,4 +1,5 @@
 import { Image } from '@linode/api-v4/lib/images';
+import { Event } from '@linode/api-v4/lib/account';
 import * as React from 'react';
 import {
   createStyles,
@@ -11,6 +12,7 @@ import RenderGuard from 'src/components/RenderGuard';
 import TableCell from 'src/components/TableCell';
 import { formatDate } from 'src/utilities/format-date-iso8601';
 import ActionMenu from './ImagesActionMenu';
+import LinearProgress from 'src/components/LinearProgress';
 
 type ClassNames = 'root' | 'label';
 
@@ -30,14 +32,36 @@ interface Props {
   onDelete: (image: string, imageID: string) => void;
   onRestore: (imageID: string) => void;
   onDeploy: (imageID: string) => void;
-  image: Image;
+  image: ImageWithEvent;
+}
+export interface ImageWithEvent extends Image {
+  event?: Event;
 }
 
 type CombinedProps = Props & WithStyles<ClassNames>;
 
+const progressFromEvent = (e?: Event) => {
+  return e?.status === 'started' && e?.percent_complete
+    ? e.percent_complete
+    : undefined;
+};
+
 const ImageRow: React.FC<CombinedProps> = props => {
   const { classes, image, ...rest } = props;
-  return (
+  return isImageUpdating(image.event) ? (
+    <TableRow key={image.id} data-qa-image-cell={image.id}>
+      <TableCell
+        parentColumn="Label"
+        className={classes.label}
+        data-qa-image-label
+      >
+        {image.label}
+      </TableCell>
+      <TableCell colSpan={4}>
+        <LinearProgress value={progressFromEvent(image.event)} />
+      </TableCell>
+    </TableRow>
+  ) : (
     <TableRow key={image.id} data-qa-image-cell={image.id}>
       <TableCell
         parentColumn="Label"
@@ -59,6 +83,16 @@ const ImageRow: React.FC<CombinedProps> = props => {
         <ActionMenu image={image} {...rest} />
       </TableCell>
     </TableRow>
+  );
+};
+
+export const isImageUpdating = (e?: Event) => {
+  // Make Typescript happy, since this function can otherwise technically return undefined
+  if (!e) {
+    return false;
+  }
+  return (
+    e?.action === 'disk_imagize' && ['scheduled', 'started'].includes(e.status)
   );
 };
 
