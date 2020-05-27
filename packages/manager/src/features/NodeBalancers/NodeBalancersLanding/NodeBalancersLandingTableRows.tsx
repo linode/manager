6e8a500fcdcd300fe9ccaa313b36a46c6673e8bf
Kayla Wilkins <kwilkins@linode.com>
2020-05-27 14:25:58 -0400

    M3-4128 Refactor User details, NodeBalancer details, Profile, Longview landing and details (#6423)
    * changing tab patterns for user details, Nodebalancer details, and profile + cleanup
    
    * attempting to fix oauth and NB issues
    
    * removing dupe props issue
    
    * changing tab patterns for user details, Nodebalancer details, and profile + cleanup
    
    * attempting to fix oauth and NB issues
    
    * removing stray again after branch cleanup debacle
    
    * adding new SafeTabPanel abstraction to load content only on active tab selection instead of all
    
    * longview POC with safe tab panel
    
    * adding longview tabs with SafeTabPanel to fix graphs loading issue
    
    * checking for possible indexes based on existence of apps for a LV client
    
    * Add comment to SafeTabPanel
    
    Co-authored-by: John Callahan <johnwcallahan@gmail.com>

--- packages/manager/src/features/NodeBalancers/NodeBalancersLanding/NodeBalancersLandingTableRows.tsx
+++ packages/manager/src/features/NodeBalancers/NodeBalancersLanding/NodeBalancersLandingTableRows.tsx
@@ -73,7 +73,7 @@ const NodeBalancersLandingTableRows: React.StatelessComponent<CombinedProps> = p
           <TableRow
             key={nodeBalancer.id}
             data-qa-nodebalancer-cell={nodeBalancer.label}
-            rowLink={`/nodebalancers/${nodeBalancer.id}`}
+            rowLink={`/nodebalancers/${nodeBalancer.id}/summary`}
             className="fade-in-table"
             aria-label={nodeBalancer.label}
           >
@@ -83,7 +83,10 @@ const NodeBalancersLandingTableRows: React.StatelessComponent<CombinedProps> = p
                   <EntityIcon variant="nodebalancer" marginTop={1} />
                 </Grid>
                 <Grid item>
-                  <Link to={`/nodebalancers/${nodeBalancer.id}`} tabIndex={0}>
+                  <Link
+                    to={`/nodebalancers/${nodeBalancer.id}/summary`}
+                    tabIndex={0}
+                  >
                     <Typography variant="h3">{nodeBalancer.label}</Typography>
                   </Link>
                 </Grid>
