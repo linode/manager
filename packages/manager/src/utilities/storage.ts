01d96a34457a80c3c88894757b781e1a2ad12a15
Jared <jaredkobos@gmail.com>
2020-06-08 14:03:53 -0400

    M3-4179 Save in-progress work when creating a StackScript (#6506)
    * First attempt
    
    * Cache all of this.state (mostly) to prevent overwriting pending changes
    
    * Clear stored stackscript form state when user explicitly logs out
    
    * Clear revision note and description in authentication.helpers

--- packages/manager/src/utilities/storage.ts
+++ packages/manager/src/utilities/storage.ts
@@ -1,3 +1,4 @@
+import { StackScriptPayload } from '@linode/api-v4/lib/stackscripts/types';
 const localStorageCache = {};
 
 export const getStorage = (key: string, fallback?: any) => {
@@ -45,6 +46,7 @@ const NONCE = 'authentication/nonce';
 const SCOPES = 'authentication/scopes';
 const EXPIRE = 'authentication/expire';
 const SUPPORT = 'support';
+const STACKSCRIPT = 'stackscript';
 
 export type PageSize = number;
 
@@ -80,6 +82,10 @@ export interface Storage {
   supportText: {
     get: () => SupportText;
     set: (v: SupportText) => void;
+  };
+  stackScriptInProgress: {
+    get: () => StackScriptPayload;
+    set: (s: StackScriptPayload) => void;
   };
 }
 
@@ -128,7 +134,21 @@ export const storage: Storage = {
   supportText: {
     get: () => getStorage(SUPPORT, { title: '', description: '' }),
     set: v => setStorage(SUPPORT, JSON.stringify(v))
+  },
+  stackScriptInProgress: {
+    get: () =>
+      getStorage(STACKSCRIPT, {
+        script: '',
+        label: '',
+        images: []
+      }),
+    set: s => setStorage(STACKSCRIPT, JSON.stringify(s))
   }
 };
 
-export const { authentication, BackupsCtaDismissed, supportText } = storage;
+export const {
+  authentication,
+  BackupsCtaDismissed,
+  stackScriptInProgress,
+  supportText
+} = storage;
