a2dbf4f0aac6e0487d96b6b1426e8256b92f341c
John Callahan <johnwcallahan@gmail.com>
2020-06-09 10:37:07 -0400

    Dismissible main content banner (#6516)
    This makes the main content banner dismissible. 
    
    The data shape for the main content banner flag now requires a unique `key` so we can map dismissals to specific banners (since we could potentially reuse this flag in the future).
    
    * Dismissible main content banner
    
    * Clean up and add local state
    
    * Change url
    
    * Add aria label
    
    * adjustment for mobile
    
    Co-authored-by: Kayla Wilkins <kwilkins@linode.com>

--- packages/manager/src/components/MainContentBanner.tsx
+++ packages/manager/src/components/MainContentBanner.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
+import Close from '@material-ui/icons/Close';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Grid from 'src/components/core/Grid';
 import Typography from 'src/components/core/Typography';
 import { Link } from 'src/components/Link';
+import withPreferences, {
+  Props as PreferencesProps
+} from 'src/containers/preferences.container';
+import { compose } from 'recompose';
 
 const useStyles = makeStyles((theme: Theme) => ({
   bannerOuter: {
@@ -10,15 +15,29 @@ const useStyles = makeStyles((theme: Theme) => ({
     padding: theme.spacing(2),
     position: 'sticky',
     top: 0,
-    zIndex: 1110
+    zIndex: 1110,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   header: {
     color: '#fff',
-    textAlign: 'center'
+    textAlign: 'center',
+    [theme.breakpoints.only('xs')]: {
+      paddingRight: 30
+    }
   },
   link: {
     color: '#fff',
     textDecoration: 'underline'
+  },
+  closeIcon: {
+    position: 'absolute',
+    right: 4,
+    cursor: 'pointer',
+    border: 'none',
+    color: theme.palette.text.primary,
+    backgroundColor: 'transparent'
   }
 }));
 
@@ -26,12 +45,40 @@ interface Props {
   bannerText: string;
   url: string;
   linkText: string;
+  bannerKey: string;
+  onClose: () => void;
 }
 
-const MainContentBanner: React.FC<Props> = props => {
-  const { bannerText, url, linkText } = props;
+type CombinedProps = Props & PreferencesProps;
+
+const MainContentBanner: React.FC<CombinedProps> = props => {
+  const {
+    bannerText,
+    url,
+    linkText,
+    bannerKey,
+    getUserPreferences,
+    updateUserPreferences,
+    onClose
+  } = props;
 
   const classes = useStyles();
+
+  const dismiss = () => {
+    onClose();
+    getUserPreferences()
+      .then(preferences => {
+        updateUserPreferences({
+          ...preferences,
+          main_content_banner_dismissal: {
+            ...preferences.main_content_banner_dismissal,
+            [bannerKey]: true
+          }
+        });
+      })
+      // It's OK if this fails (the banner is still dismissed in the UI due to local state).
+      .catch(_ => null);
+  };
 
   return (
     <Grid className={classes.bannerOuter} item xs={12}>
@@ -43,8 +90,17 @@ const MainContentBanner: React.FC<Props> = props => {
           </Link>
         )}
       </Typography>
+      <button
+        className={classes.closeIcon}
+        onClick={dismiss}
+        aria-label="Close"
+      >
+        <Close />
+      </button>
     </Grid>
   );
 };
 
-export default React.memo(MainContentBanner);
+const enhanced = compose<CombinedProps, Props>(React.memo, withPreferences());
+
+export default enhanced(MainContentBanner);
