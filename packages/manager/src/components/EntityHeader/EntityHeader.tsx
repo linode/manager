4b4d76071c767872ed7ed436675f8bd65f361ded
Jared Kobos <jaredkobos@gmail.com>
2020-06-09 15:22:54 -0400

    Review feedback
    - Clean up useStyles and rename component exports
    - Wrap headers in a Paper

--- packages/manager/src/components/EntityHeader/EntityHeader.tsx
+++ packages/manager/src/components/EntityHeader/EntityHeader.tsx
@@ -1,40 +1,34 @@
 import * as React from 'react';
 
-import { makeStyles, Theme } from 'src/components/core/styles';
 import Grid from 'src/components/Grid';
+import Paper from 'src/components/core/Paper';
 import HeaderBreadCrumb, { BreadCrumbProps } from './HeaderBreadCrumb';
-
-const useStyles = makeStyles((theme: Theme) => ({ root: {} }));
 
 export interface HeaderProps extends BreadCrumbProps {
   actions: JSX.Element;
 }
 
-export const LandingHeader: React.FC<HeaderProps> = props => {
+export const EntityHeader: React.FC<HeaderProps> = props => {
   const { actions, iconType, parentLink, parentText, title } = props;
-  const classes = useStyles();
 
   return (
-    <Grid
-      container
-      alignItems="center"
-      justify="space-between"
-      className={classes.root}
-    >
-      <Grid item>
-        <Grid container direction="row" alignItems="center">
-          <HeaderBreadCrumb
-            iconType={iconType}
-            title={title}
-            parentLink={parentLink}
-            parentText={parentText}
-          />
-          {props.children && <Grid item>{props.children}</Grid>}
+    <Paper>
+      <Grid container alignItems="center" justify="space-between">
+        <Grid item>
+          <Grid container direction="row" alignItems="center">
+            <HeaderBreadCrumb
+              iconType={iconType}
+              title={title}
+              parentLink={parentLink}
+              parentText={parentText}
+            />
+            {props.children && <Grid item>{props.children}</Grid>}
+          </Grid>
         </Grid>
+        <Grid item>{actions}</Grid>
       </Grid>
-      <Grid item>{actions}</Grid>
-    </Grid>
+    </Paper>
   );
 };
 
-export default LandingHeader;
+export default EntityHeader;
