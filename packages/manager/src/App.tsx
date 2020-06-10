a701d2762e563d314c32499977412c83686e1967
John Callahan <johnwcallahan@gmail.com>
2020-06-10 17:36:59 -0400

    M3-4196 Primary Nav organization (#6508)
    * WIP link organization
    
    * CSS adjustments
    
    * Styling adjustments
    
    * Cleanup
    
    * Hack solution for mobile
    
    * Remove commented-out code
    
    * Add unit tests
    
    * Change background mode color
    
    * Fix visible scrollbar

--- packages/manager/src/App.tsx
+++ packages/manager/src/App.tsx
@@ -1,3 +1,4 @@
+import '@reach/menu-button/styles.css';
 import { Account, AccountCapability } from '@linode/api-v4/lib/account';
 import { Image } from '@linode/api-v4/lib/images';
 import { Linode } from '@linode/api-v4/lib/linodes';
