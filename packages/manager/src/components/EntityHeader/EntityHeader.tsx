1bd2c8d82d64438ae1770e169c135ed6c38f76cf
Tiffany Wong <wong.tyan@gmail.com>
2020-06-11 16:54:16 -0400

    Fix regression for MuiChip and move it to appropriate CMR file

--- packages/manager/src/components/EntityHeader/EntityHeader.tsx
+++ packages/manager/src/components/EntityHeader/EntityHeader.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import Grid from 'src/components/Grid';
 import Paper from 'src/components/core/Paper';
+import { makeStyles, Theme } from 'src/components/core/styles';
 import HeaderBreadCrumb, { BreadCrumbProps } from './HeaderBreadCrumb';
 
 export interface HeaderProps extends BreadCrumbProps {
@@ -9,8 +10,23 @@ export interface HeaderProps extends BreadCrumbProps {
   body: JSX.Element;
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  chip: {
+    '& .MuiChip-root': {
+      height: 30,
+      borderRadius: 15,
+      marginTop: 1,
+      marginRight: 10,
+      fontSize: '.875rem',
+      letterSpacing: '.5px',
+      minWidth: 140
+    }
+  }
+}));
+
 export const EntityHeader: React.FC<HeaderProps> = props => {
   const { actions, body, iconType, parentLink, parentText, title } = props;
+  const classes = useStyles();
 
   return (
     <Paper>
@@ -23,7 +39,11 @@ export const EntityHeader: React.FC<HeaderProps> = props => {
               parentLink={parentLink}
               parentText={parentText}
             />
-            {body && <Grid item>{body}</Grid>}
+            {body && (
+              <Grid className={classes.chip} item>
+                {body}
+              </Grid>
+            )}
           </Grid>
         </Grid>
         <Grid item>{actions}</Grid>
