38990705783616b92e743f2fb7c6db91f2ce6b2b
Adrien Courdavault <acourdavault@linode.com>
2020-06-12 15:18:15 -0400

    remove moment from linode/events

--- packages/manager/src/features/linodes/events.ts
+++ packages/manager/src/features/linodes/events.ts
@@ -1,37 +1,35 @@
-// import { Event } from '@linode/api-v4/lib/account';
-// import {DateTime} from 'luxon'
-// let newLinodeEvents: (time: DateTime) => (event: Event) => boolean;
+import { Event } from '@linode/api-v4/lib/account';
 
-// const actionWhitelist = [
-//   'linode_boot',
-//   'linode_reboot',
-//   'linode_shutdown',
-//   'backups_enable',
-//   'backups_cancel',
-//   'backups_restore',
-//   'linode_snapshot',
-//   'linode_rebuild',
-//   'linode_resize',
-//   'disk_resize',
-//   'linode_migrate'
-// ];
+const actionWhitelist = [
+  'linode_boot',
+  'linode_reboot',
+  'linode_shutdown',
+  'backups_enable',
+  'backups_cancel',
+  'backups_restore',
+  'linode_snapshot',
+  'linode_rebuild',
+  'linode_resize',
+  'disk_resize',
+  'linode_migrate'
+];
 
-// const statusWhitelist = [
-//   'started',
-//   'finished',
-//   'scheduled',
-//   'failed',
-//   'notification'
-// ];
+const statusWhitelist = [
+  'started',
+  'finished',
+  'scheduled',
+  'failed',
+  'notification'
+];
 
-// newLinodeEvents = (time?: DateTime) => (e: Event): boolean => {
-//   return (
-//     e.entity !== null &&
-//     e.entity.type === 'linode' &&
-//     statusWhitelist.includes(e.status) &&
-//     actionWhitelist.includes(e.action) &&
-//     !e._initial
-//   );
-// };
+const newLinodeEvents = (_time?: any) => (e: Event): boolean => {
+  return (
+    e.entity !== null &&
+    e.entity.type === 'linode' &&
+    statusWhitelist.includes(e.status) &&
+    actionWhitelist.includes(e.action) &&
+    !e._initial
+  );
+};
 
-// export { newLinodeEvents };
+export { newLinodeEvents };
