7492d4e1fafbda3c99542355cbdedb1e360d9586
ndukatz <ndukatz@linode.com>
2020-06-18 18:43:13 -0400

    refactor of rescue test

--- packages/manager/cypress/integration/linodes/rescue-linode.spec.ts
+++ packages/manager/cypress/integration/linodes/rescue-linode.spec.ts
@@ -1,13 +1,39 @@
 import { createLinode, deleteLinodeById } from '../../support/api/linodes';
 import { assertToast } from '../../support/ui/events';
+
 const rebootInRescueMode = () => {
   cy.findByText('Reboot into Rescue Mode').click();
 };
+
 describe('rescue linode', () => {
   it('rescue a linode', () => {
     cy.visitWithLogin('/support');
     createLinode().then(linode => {
       cy.server();
+      //mock 200 response
+      cy.route({
+        method: 'POST',
+        url: '*/linode/instances/*/rescue',
+        response: {}
+      }).as('postRebootInRescueMode');
+      const rescueUrl = `/linodes/${linode.id}/rescue`;
+      cy.visit(rescueUrl);
+      rebootInRescueMode();
+      //check mocked response and make sure UI responded correctly
+      cy.wait('@postRebootInRescueMode')
+        .its('status')
+        .should('eq', 200);
+      assertToast('Linode rescue started.');
+      cy.url().should('endWith', `linodes/${linode.id}/summary`);
+      deleteLinodeById(linode.id);
+    });
+  });
+
+  it('rescue blocked', () => {
+    cy.visitWithLogin('/support');
+    createLinode().then(linode => {
+      cy.server();
+      //not mocking response here
       cy.route({
         method: 'POST',
         url: '*/linode/instances/*/rescue'
@@ -15,22 +41,11 @@ describe('rescue linode', () => {
       const rescueUrl = `/linodes/${linode.id}/rescue`;
       cy.visit(rescueUrl);
       rebootInRescueMode();
-
+      //check response, verify bad request and UI response (toast)
       cy.wait('@postRebootInRescueMode')
         .its('status')
         .should('eq', 400);
       assertToast('Linode busy.');
-      // wait for booting info
-      cy.findByText('Booting', { exact: false });
-      cy.get('[data-qa-entity-status="running"]', { timeout: 60000 });
-
-      rebootInRescueMode();
-      cy.wait('@postRebootInRescueMode')
-        .its('status')
-        .should('eq', 200);
-      assertToast('Linode rescue started.');
-      cy.url().should('endWith', `linodes/${linode.id}/summary`);
-      cy.findByText('Rebooting', { exact: false });
       deleteLinodeById(linode.id);
     });
   });
