14b62e6ce192e91871598973ae93ff7ad3eaf782
Tiffany Wong <7692354+tiffwong@users.noreply.github.com>
2020-06-22 11:10:33 -0400

    [Tabs] M3-4119: Refactor Tabs - Lish (#6467)
    * Refactor to Reach UI
    
    * Add keyboardActivation prop
    
    * Working on focusing on tabs
    
    * Add ref to tabs
    
    * removing redundant a11y attributes
    
    * Switch to SafeTabPanel
    
    * Move styles to Lish and get rid of ref
    
    * Remove TabPanel import
    
    Co-authored-by: Kayla Wilkins <kwilkins@linode.com>

--- packages/manager/src/components/TabLinkList/TabLinkList.tsx
+++ packages/manager/src/components/TabLinkList/TabLinkList.tsx
@@ -8,24 +8,24 @@ const useStyles = makeStyles((theme: Theme) => ({
   tab: {
     '&[data-reach-tab]': {
       // This was copied over from our MuiTab styling in themeFactory. Some of this could probably be cleaned up.
-      color: theme.color.tableHeaderText,
-      minWidth: 50,
-      textTransform: 'inherit',
-      fontSize: '0.93rem',
-      padding: '6px 16px',
-      position: 'relative',
-      overflow: 'hidden',
-      maxWidth: 264,
-      boxSizing: 'border-box',
-      borderBottom: '2px solid transparent',
-      minHeight: theme.spacing(1) * 6,
-      flexShrink: 0,
       display: 'inline-flex',
       alignItems: 'center',
+      flexShrink: 0,
       verticalAlign: 'middle',
       justifyContent: 'center',
       appearance: 'none',
+      borderBottom: '2px solid transparent',
+      boxSizing: 'border-box',
+      color: theme.color.tableHeaderText,
+      fontSize: '0.93rem',
       lineHeight: 1.3,
+      maxWidth: 264,
+      minHeight: theme.spacing(1) * 6,
+      minWidth: 50,
+      overflow: 'hidden',
+      padding: '6px 16px',
+      position: 'relative',
+      textTransform: 'inherit',
       [theme.breakpoints.up('md')]: {
         minWidth: 75
       },
@@ -40,6 +40,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
   },
   tabList: {
+    color: theme.color.tableHeaderText,
     '&[data-reach-tab-list]': {
       background: 'none !important',
       boxShadow: `inset 0 -1px 0 ${theme.color.border2}`,
@@ -65,9 +66,8 @@ interface Props {
 type CombinedProps = Props;
 
 export const TabLinkList: React.FC<CombinedProps> = props => {
-  const { tabs } = props;
-
   const classes = useStyles();
+  const { tabs } = props;
 
   return (
     <TabList className={classes.tabList}>
