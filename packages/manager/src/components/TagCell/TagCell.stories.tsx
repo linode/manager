7ccf11e4e88a3831dd071c6391efe6d9e47a1e8e
Jared Kobos <jaredkobos@gmail.com>
2020-06-22 08:14:45 -0400

    Use useCallback instead of a ref to track overflow status

--- packages/manager/src/components/TagCell/TagCell.stories.tsx
+++ packages/manager/src/components/TagCell/TagCell.stories.tsx
@@ -1,47 +1,36 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
-import Table from 'src/components/core/Table';
+import Table from 'src/components/Table';
 import TableRow from 'src/components/TableRow';
-import TableHeader from 'src/components/TableHeader';
+import TableHead from 'src/components/core/TableHead';
 import TableBody from 'src/components/core/TableBody';
 import TableCell from 'src/components/TableCell';
 import TagCell from './TagCell';
 
-storiesOf('TagCell', module)
-  .add('small number of tags', () => (
-    <div style={{ width: '50%', margin: 'auto' }}>
-      <Table>
-        <TableHeader title="Tag cell story">
-          <TableRow>
-            {' '}
-            <TableCell style={{ width: '500px' }}>Tags</TableCell>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TagCell tags={['tag1', 'tag2']} addTag={() => null} />
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
-  ))
-  .add('large number of tags', () => (
-    <div style={{ width: '50%', margin: 'auto' }}>
-      <Table>
-        <Table>
-          <TableHeader title="Tag cell story">
-            <TableRow>
-              {' '}
-              <TableCell>Tags</TableCell>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TagCell tags={['tag1', 'tag2']} addTag={() => null} />
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Table>
-    </div>
-  ));
+const tags = [
+  'tag1',
+  'tag2',
+  'tagwithextremelylongname',
+  'tag3',
+  'tag4',
+  'tag5',
+  'yet another tag with a very long name'
+];
+
+storiesOf('TagCell', module).add('small number of tags', () => (
+  <div style={{ width: '500px', margin: 'auto' }}>
+    <Table>
+      <TableHead title="Tag cell story">
+        <TableRow>
+          <TableCell>Tags</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TagCell tags={tags} addTag={() => null} width={'500px'} />
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
+));
--- packages/manager/src/components/TagCell/TagCell.tsx
+++ packages/manager/src/components/TagCell/TagCell.tsx
@@ -1,53 +1,115 @@
+import * as classnames from 'classnames';
 import * as React from 'react';
 import Plus from 'src/assets/icons/plusSign.svg';
 
 import { makeStyles, Theme } from 'src/components/core/styles';
 import TableCell from 'src/components/TableCell';
-import Tag from 'src/components/Tag';
-import Grid from 'src/components/Grid';
+import Tag from 'src/components/Tag/Tag_CMR';
+import Grid from 'src/components/Grid'; // Have to use the MUI variant to get around ref issues
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {},
+  root: {
+    overflow: 'hidden',
+    height: '40px'
+  },
   addTag: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     width: '30px',
     height: '30px',
+    marginRight: theme.spacing(),
     backgroundColor: theme.bg.lightBlue,
     '& svg': {
       color: theme.palette.primary.main
+    },
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+      color: 'white',
+      '& svg': {
+        color: 'white'
+      }
+    }
+  },
+  tagList: {
+    position: 'relative',
+    height: '50px',
+    width: '100%'
+  },
+  tagListOverflow: {
+    '&:before': {
+      content: '""',
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      background: `linear-gradient(to right, transparent 150px, white)`
     }
   }
 }));
 
 interface Props {
   tags: string[];
+  width: string; // Required so we can fade out after a certain point
   addTag: (newTag: string) => void;
 }
+
+// https://stackoverflow.com/questions/143815/determine-if-an-html-elements-content-overflows
+const checkOverflow = (el: any) => {
+  const curOverflow = el.style.overflow;
+
+  if (!curOverflow || curOverflow === 'visible') {
+    el.style.overflow = 'hidden';
+  }
+
+  const isOverflowing =
+    el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
+
+  el.style.overflow = curOverflow;
+
+  return isOverflowing;
+};
 
 export type CombinedProps = Props;
 
 export const TagCell: React.FC<Props> = props => {
-  const { tags } = props;
+  const { tags, width } = props;
+  const [hasOverflow, setOverflow] = React.useState<boolean>(false);
   const classes = useStyles();
+  const overflowRef = React.useCallback(node => {
+    if (node !== null) {
+      setOverflow(checkOverflow(node));
+    }
+  }, []);
+
   return (
     <TableCell className={classes.root}>
-      <Grid container direction="row" alignItems="center">
-        <Grid item className={classes.addTag}>
-          <Plus />
+      <div ref={overflowRef} style={{ width }}>
+        <Grid container direction="row" alignItems="center" wrap="nowrap">
+          <Grid item className={classes.addTag}>
+            <Plus />
+          </Grid>
+          <Grid container wrap="nowrap" direction="row">
+            <Grid
+              item
+              className={classnames({
+                [classes.tagList]: true,
+                [classes.tagListOverflow]: hasOverflow
+              })}
+            >
+              {tags.map(thisTag => (
+                <Tag
+                  key={`tag-item-${thisTag}`}
+                  colorVariant="lightBlue"
+                  label={thisTag}
+                  onDelete={() => null}
+                />
+              ))}
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item>
-          {tags.map(thisTag => (
-            <Tag
-              key={`tag-item-${thisTag}`}
-              colorVariant="lightBlue"
-              label="thisTag"
-              onDelete={() => null}
-            />
-          ))}
-        </Grid>
-      </Grid>
+      </div>
     </TableCell>
   );
 };
