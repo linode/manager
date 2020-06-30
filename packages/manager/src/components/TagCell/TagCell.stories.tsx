3e793e597fc64859e8a43bdee94619c48b5e1009
Jared <jaredkobos@gmail.com>
2020-06-30 11:25:14 -0400

    M3-4233 [CMR] Tag cell for entity tables (#6560)
    * Boilerplate and story template
    
    * Use existing Tag component
    
    * Use useCallback instead of a ref to track overflow status
    
    * Pending
    
    * Restructure markup so that the overflow detection actually works
    
    * Enable deletion in tag cell story
    
    * WIP adding tags
    
    * Add ability to add new tag
    
    * Add tag drawer for show-all functionality
    
    * Clean up imports
    
    * Clean up story; swallow error in AddTag
    
    * Refactor styles to avoid repetition
    
    * Re-add autofocus
    
    * initial work to fix scroll bar and hover issues on tags
    
    * some cleanup
    
    * Shrink select field and remove padding
    
    * adding fixed prop to select so it overlays table instead of being hidden
    
    * Add tag input to tag drawer
    
    * Fix fade-out color on dark theme
    
    * updating theme color
    
    Co-authored-by: Kayla Wilkins <kwilkins@linode.com>

---
+++ packages/manager/src/components/TagCell/TagCell.stories.tsx
@@ -0,0 +1,71 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import TableBody from 'src/components/core/TableBody';
+import TableHead from 'src/components/core/TableHead';
+import Table from 'src/components/Table';
+import TableCell from 'src/components/TableCell';
+import TableRow from 'src/components/TableRow';
+import TagCell from './TagCell';
+import TagDrawer from './TagDrawer';
+
+const tags = [
+  'tag1',
+  'tag2',
+  'tag2.5',
+  'tag3',
+  'tagtagtagtagtagtag',
+  'tag4',
+  'tagggg'
+];
+
+const TagTableCellStory: React.FC<{ tags: string[] }> = props => {
+  const [_tags, setTags] = React.useState<string[]>(props.tags);
+  const [drawerOpen, setDrawerOpen] = React.useState<boolean>(false);
+
+  const deleteTag = (thisTag: string) => {
+    setTags((currentTags: string[]) => {
+      return currentTags.filter(currentTag => currentTag !== thisTag);
+    });
+  };
+
+  const addTag = (newTag: string) => {
+    setTags([..._tags, newTag]);
+  };
+
+  return (
+    <div style={{ width: '500px', margin: 'auto' }}>
+      <Table>
+        <TableHead title="Tag cell story">
+          <TableRow>
+            <TableCell>Tags</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TagCell
+              tags={_tags}
+              addTag={addTag}
+              deleteTag={deleteTag}
+              listAllTags={() => setDrawerOpen(true)}
+              width={500}
+            />
+          </TableRow>
+        </TableBody>
+      </Table>
+      <TagDrawer
+        entityLabel="MyLinode"
+        open={drawerOpen}
+        tags={_tags}
+        addTag={addTag}
+        deleteTag={deleteTag}
+        onClose={() => setDrawerOpen(false)}
+      />
+    </div>
+  );
+};
+
+storiesOf('TagCell', module)
+  .add('Large number of tags (overflow state)', () => (
+    <TagTableCellStory tags={tags} />
+  ))
+  .add('Fewer tags', () => <TagTableCellStory tags={tags.slice(0, 2)} />);
