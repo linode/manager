6610dd1349c96815548777819a6b0ee792058a67
Jared Kobos <jaredkobos@gmail.com>
2020-07-07 13:17:43 -0400

    Refactor worker init logic

--- packages/manager/src/mocks/testBrowser.ts
+++ packages/manager/src/mocks/testBrowser.ts
@@ -3,14 +3,11 @@ import { setupWorker } from 'msw';
 import { MOCK_SERVICE_WORKER } from 'src/constants';
 import { handlers } from './serverHandlers';
 
-export const worker = setupWorker(...handlers);
-
 /**
  * If the .env tells us to mock the API, load
  * the mocks.
  */
 if (MOCK_SERVICE_WORKER) {
+  const worker = setupWorker(...handlers);
   worker.start();
-} else {
-  worker.stop();
 }
