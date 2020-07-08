784450cee0604e4d167a6738f2ca6fdb43b94f2d
Dajahi Wiley <dwiley@linode.com>
2020-07-08 17:32:41 -0400

    First pass at Root Device logic (not functional yet)

--- packages/manager/src/features/linodes/LinodesDetail/LinodeAdvanced/ConfigRow_CMR.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeAdvanced/ConfigRow_CMR.tsx
@@ -1,4 +1,4 @@
-import { Config } from '@linode/api-v4/lib/linodes';
+import { Config, Disk, getLinodeDisks } from '@linode/api-v4/lib/linodes';
 import * as React from 'react';
 import LinodeConfigActionMenu from '../LinodeSettings/LinodeConfigActionMenu_CMR';
 import TableCell from 'src/components/core/TableCell';
@@ -30,8 +30,25 @@ export const ConfigRow: React.FC<CombinedProps> = props => {
     readOnly
   } = props;
 
-  // If config.memory_limit === 0, use linodeMemory; the API interprets a memory limit of 0 as the RAM of the Linode itself.
+  const assembleRootDeviceInfo = (config: Config, linodeId: number) => {
+    const rootDevice = config.root_device;
+    const device = rootDevice.slice(-3); // Isolate the 'sda', 'sdc', etc. piece
 
+    const deviceId = config.devices[device].disk_id;
+
+    getLinodeDisks(linodeId)
+      .then(response => {
+        const label = response.data.filter(
+          (disk: Disk) => disk.id === deviceId
+        );
+        return `${rootDevice} – ${label}`;
+      })
+      .catch(() => {
+        return null;
+      });
+  };
+
+  // If config.memory_limit === 0, use linodeMemory; the API interprets a memory limit of 0 as the RAM of the Linode itself.
   return (
     <TableRow key={config.id} data-qa-config={config.label}>
       <TableCell>{config.label}</TableCell>
@@ -46,7 +63,7 @@ export const ConfigRow: React.FC<CombinedProps> = props => {
           ? `${linodeMemory} GB`
           : `${config.memory_limit} MB`}
       </TableCell>
-      <TableCell>/dev/sda</TableCell>
+      <TableCell></TableCell>
       <TableCell>
         <LinodeConfigActionMenu
           config={config}
