1be21fd545d133f3b9c8834f886580b40253288a
Adrien Courdavault <acourdavault@linode.com>
2020-07-10 10:12:11 -0400

    test delete linode from action menu

--- packages/manager/cypress/integration/linodes/smoke-linode-landing-table.spec.ts
+++ packages/manager/cypress/integration/linodes/smoke-linode-landing-table.spec.ts
@@ -1,16 +1,26 @@
-import { createLinode, deleteLinodeById } from '../../support/api/linodes';
-import { getLinodeLandingRow } from '../../support/ui/linodes';
+import { createLinode } from '../../support/api/linodes';
 
 describe('linode landing', () => {
-  it('linode row menu', () => {
+  it('delete linode with action menu', () => {
     createLinode().then(linode => {
+      cy.server();
+      cy.route({
+        url: '*/linode/instances/*',
+        method: 'DELETE'
+      }).as('deleteLinode');
       cy.visitWithLogin('/linodes');
-      getLinodeLandingRow(linode.label).within(_el => {
-        cy.get(`[data-qa-action-menu]`).should('exist');
-      });
-      deleteLinodeById(linode.id).then(resp => {
-        expect(resp.status).to.eq(200);
-      });
+      cy.findAllByLabelText(`Action menu for Linode ${linode.label}`).click();
+      // the visible filter is to ignore all closed action menus
+      cy.get(`[data-qa-action-menu-item="Delete"]`)
+        .filter(`:visible`)
+        .click();
+      // There is 2 visible delete on the page, this is why i used this strategy
+      cy.findAllByRole('button')
+        .filter(':contains("Delete")')
+        .click();
+      cy.wait('@deleteLinode')
+        .its('status')
+        .should('eq', 200);
     });
   });
 });
