ce694c1e6f2c0a44af48df8054b4e9f58658a4e1
John Callahan <johnwcallahan@gmail.com>
2020-07-14 12:20:33 -0400

    useLinodeNetworkInfo hook

---
+++ packages/manager/src/hooks/useLinodeNetworkInfo.ts
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import {
+  // getLinodeStats,
+  Stats,
+  getLinodeStatsByDate,
+  getLinodeTransferByDate
+} from '@linode/api-v4/lib/linodes';
+import { NetworkTransfer } from '@linode/api-v4/lib/account';
+
+export const useLinodeNetworkInfo = (
+  linodeID: number,
+  year: string,
+  month: string
+) => {
+  const [loading, setLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState<string | undefined>();
+
+  const [transferData, setTransferData] = React.useState<
+    NetworkTransfer | undefined
+  >();
+  const [statsData, setStatsData] = React.useState<Stats | undefined>();
+
+  // @todo:
+  //   - handle request type (by month)
+  //   - add logic for refresh?
+
+  React.useEffect(() => {
+    setLoading(true);
+    setErrorMessage(undefined);
+    Promise.all([
+      getLinodeStatsByDate(linodeID, year, month),
+      getLinodeTransferByDate(linodeID, year, month)
+    ])
+      .then(([stats, transfer]) => {
+        setLoading(false);
+        setStatsData(stats);
+        setTransferData(transfer);
+      })
+      .catch(_ => {
+        setLoading(false);
+        setErrorMessage('There was an error retrieving stats for this Linode.');
+      });
+  }, [linodeID, year, month]);
+
+  return { loading, errorMessage, transfer: transferData, stats: statsData };
+};
