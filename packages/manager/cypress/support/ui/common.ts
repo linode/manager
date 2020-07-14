2f76ea3a719c7956ef5b6ed0d88e75fcf9e5b90c
ndukatz <ndukatz@linode.com>
2020-07-14 16:53:48 -0400

    this is the actual fix for the notification event. the url just changed slightly adding ?page_size=100 on the end

--- packages/manager/cypress/support/ui/common.ts
+++ packages/manager/cypress/support/ui/common.ts
@@ -56,7 +56,7 @@ export const waitForAppLoad = (path = '/', withLogin = true) => {
 
   cy.route({
     method: 'GET',
-    url: '*/account/notifications'
+    url: '*/account/notifications**'
   }).as('getNotifications');
 
   withLogin ? cy.visitWithLogin(path) : cy.visit(path);
@@ -65,5 +65,5 @@ export const waitForAppLoad = (path = '/', withLogin = true) => {
   cy.wait('@getAccountSettings');
   cy.wait('@getProfilePreferences');
   cy.wait('@getProfile');
-  // cy.wait('@getNotifications');
+  cy.wait('@getNotifications');
 };
