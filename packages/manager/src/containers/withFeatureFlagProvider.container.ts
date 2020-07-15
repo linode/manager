eaee04917698aaf231096ecabb180adcf0c74f0e
Tiffany Wong <wong.tyan@gmail.com>
2020-07-15 12:13:49 -0400

    Remove local test

--- packages/manager/src/containers/withFeatureFlagProvider.container.ts
+++ packages/manager/src/containers/withFeatureFlagProvider.container.ts
@@ -21,11 +21,6 @@ const featureFlagProvider = LAUNCH_DARKLY_API_KEY
       user: {
         key: 'anonymous',
         anonymous: true
-      },
-      options: {
-        bootstrap: {
-          cmr: true
-        }
       }
     })
   : (component: React.ComponentType) => component;
