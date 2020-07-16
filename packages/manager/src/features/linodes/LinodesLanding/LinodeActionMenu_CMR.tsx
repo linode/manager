5109a068f9e6f7f4edf4f747227b554baf170357
Kayla Wilkins <kwilkins@linode.com>
2020-07-16 10:00:33 -0400

    Fix CMR action menu positioning + styling updates (#6580)
    * fixing action menu positioning issue
    
    * fixing styling for outer button items
    
    * cleanup and fixing borderRadius for main case
    
    * additional styling cleanup
    
    * adjustments for dark mode, fixing padding on button
    
    * removing border radius

--- packages/manager/src/features/linodes/LinodesLanding/LinodeActionMenu_CMR.tsx
+++ packages/manager/src/features/linodes/LinodesLanding/LinodeActionMenu_CMR.tsx
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     '&:hover': {
       backgroundColor: '#3683dc',
       '& span': {
-        color: theme.color.white
+        color: '#fff'
       }
     },
     '& span': {
@@ -56,7 +56,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     whiteSpace: 'nowrap',
     '&:hover': {
       backgroundColor: '#3683dc',
-      color: theme.color.white
+      color: '#fff'
     },
     '&[disabled]': {
       color: '#cdd0d5',
