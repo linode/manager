84e0179051ca3fdb2c70aa13d8d11e3858bce896
Dajahi Wiley <dwiley@linode.com>
2020-07-16 16:28:04 -0400

    First pieces of Dashboard for Multiple Linodes view

--- packages/manager/src/features/Dashboard_CMR/LinodeDashboardContent/MultipleLinodes.tsx
+++ packages/manager/src/features/Dashboard_CMR/LinodeDashboardContent/MultipleLinodes.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { makeStyles } from 'src/components/core/styles';
 import TabbedPanel from 'src/components/TabbedPanel';
 import { Tab } from 'src/components/TabbedPanel/TabbedPanel';
+import LinodesLanding from 'src/features/linodes/LinodesLanding';
+import DomainsLanding from 'src/features/Domains/DomainsLanding';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -15,7 +17,23 @@ export const MultipleLinodes: React.FC<{}> = _ => {
   const tabs: Tab[] = [
     {
       title: 'Linodes',
-      render: () => <div>Linodes Table</div>
+      render: () => <LinodesLanding />
+    },
+    {
+      title: 'Domains',
+      render: () => <DomainsLanding />
+    },
+    {
+      title: 'Volumes',
+      render: () => <div>Volumes Table</div>
+    },
+    {
+      title: 'Object Storage',
+      render: () => <div>Object Storage Table</div>
+    },
+    {
+      title: 'Databases',
+      render: () => <div>Databases Table</div>
     }
   ];
 
