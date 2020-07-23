67aa16669903b86ca93f53b39586d45b83612092
Jared <jaredkobos@gmail.com>
2020-07-23 13:58:35 -0400

    CMR: Add balance check and invoice request to PastDue.tsx (#6671)
    * Add balance check and invoice request to PastDue.tsx
    
    * Add payment drawer

--- packages/manager/src/features/NotificationCenter/PastDue.tsx
+++ packages/manager/src/features/NotificationCenter/PastDue.tsx
@@ -1,9 +1,14 @@
+import { getInvoices, Invoice } from '@linode/api-v4/lib/account';
 import * as React from 'react';
 import CreditCard from 'src/assets/icons/credit-card.svg';
 import InvoiceIcon from 'src/assets/icons/invoice.svg';
+import CircleProgress from 'src/components/CircleProgress';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
+import Currency from 'src/components/Currency';
 import IconTextLink from 'src/components/IconTextLink';
+import PaymentDrawer from 'src/features/Billing/BillingPanels/BillingSummary/PaymentDrawer';
+import { formatDate } from 'src/utilities/formatDate';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -11,7 +16,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     padding: theme.spacing(3) + 1,
     border: `solid 1px ${theme.color.borderBilling}`,
     borderRadius: 8,
-    backgroundColor: theme.bg.billingHeader
+    backgroundColor: theme.bg.billingHeader,
+    height: 160
   },
   iconButton: {
     paddingLeft: 0,
@@ -37,38 +43,90 @@ const useStyles = makeStyles((theme: Theme) => ({
   }
 }));
 
-interface Props {}
+interface Props {
+  balance: number;
+}
 
-export const PastDue: React.FC<Props> = _ => {
-  const mostRecentInvoiceId = 5;
-  const openPaymentDrawer = () => null;
+export const PastDue: React.FC<Props> = props => {
+  const { balance } = props;
   const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Typography className={classes.headline}>$50.00 Past Due</Typography>
-      <Typography>
-        Your payment was due on 01-July-2020. Please make a payment immediately
-        to avoid disruption.
-      </Typography>
-      <div className={classes.actions}>
-        <IconTextLink
-          SideIcon={CreditCard}
-          text="Make a payment"
-          title="Make a payment"
-          onClick={openPaymentDrawer}
-          className={classes.iconButton}
-        />
 
-        <IconTextLink
-          SideIcon={InvoiceIcon}
-          text="View most recent invoice"
-          title="View most recent invoice"
-          to={`/account/billing/invoices/${mostRecentInvoiceId}`}
-          className={classes.iconButton}
-          disabled={!mostRecentInvoiceId}
-        />
+  /**
+   * Payment Drawer Handlers
+   */
+  const [paymentDrawerOpen, setPaymentDrawerOpen] = React.useState(false);
+  const openPaymentDrawer = React.useCallback(
+    () => setPaymentDrawerOpen(true),
+    []
+  );
+  const closePaymentDrawer = React.useCallback(
+    () => setPaymentDrawerOpen(false),
+    []
+  );
+
+  /**
+   * Request Invoices
+   */
+  const [invoiceLoading, setInvoiceLoading] = React.useState(false);
+  const [mostRecentInvoice, setMostRecentInvoice] = React.useState<Invoice>();
+
+  React.useEffect(() => {
+    setInvoiceLoading(true);
+    getInvoices({}, { '+order': 'desc', '+order_by': 'date' })
+      .then(response => {
+        setMostRecentInvoice(response.data?.[0]);
+        setInvoiceLoading(false);
+      })
+      .catch(_ => setInvoiceLoading(false)); // Just leave the invoice as undefined
+  }, []);
+
+  if (invoiceLoading) {
+    return (
+      <div
+        className={classes.root}
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <CircleProgress mini />
       </div>
-    </div>
+    );
+  }
+  return (
+    <>
+      <div className={classes.root}>
+        <Typography className={classes.headline}>
+          <Currency quantity={balance} /> Past Due
+        </Typography>
+        <Typography>
+          {mostRecentInvoice && (
+            <>
+              Your payment was due on{' '}
+              {formatDate(mostRecentInvoice.date, { format: 'dd-LLL-yyyy' })}
+              {`. `}
+            </>
+          )}
+          Please make a payment immediately to avoid service disruption.
+        </Typography>
+        <div className={classes.actions}>
+          <IconTextLink
+            SideIcon={CreditCard}
+            text="Make a payment"
+            title="Make a payment"
+            onClick={openPaymentDrawer}
+            className={classes.iconButton}
+          />
+
+          <IconTextLink
+            SideIcon={InvoiceIcon}
+            text="View most recent invoice"
+            title="View most recent invoice"
+            to={`/account/billing/invoices/${mostRecentInvoice?.id}`}
+            className={classes.iconButton}
+            disabled={!mostRecentInvoice?.id}
+          />
+        </div>
+      </div>
+      <PaymentDrawer open={paymentDrawerOpen} onClose={closePaymentDrawer} />
+    </>
   );
 };
 
