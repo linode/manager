867a251d50d6beffaeee6a2b0db2e6a99380bc3a
John Callahan <johnwcallahan@gmail.com>
2020-07-24 09:33:28 -0400

    Utility/Easter Egg: user preferences editor (#6668)
    Since user preferences are tied to OAuth Clients, to change Cloud Manager preferences you must grab the short-lived token from the app and use that to curl.
    
    This adds a secret "user preference" editor directly in Cloud, accessible by going to /profile/settings?preferenceEditor=true. The editor is pretty simple; it allows you to enter arbitrary JSON and submit it (validating that it's valid JSON first). This makes it easier to quickly edit preferences when developing features that depend on it.
    
    * Add preference editor
    
    * Use Link component so doc opens in new tab
    
    * Fix dialog styling
    
    * Add documentation
    
    Co-authored-by: Jared Kobos <jaredkobos@gmail.com>

---
+++ packages/manager/src/features/Profile/Settings/PreferenceEditor.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import Dialog, { DialogProps as _DialogProps } from 'src/components/Dialog';
+import Typography from 'src/components/core/Typography';
+import Link from 'src/components/Link';
+import withPreferences, {
+  Props as PreferencesProps
+} from 'src/containers/preferences.container';
+import Button from 'src/components/Button';
+import Notice from 'src/components/Notice';
+import { compose } from 'recompose';
+
+type DialogProps = Pick<_DialogProps, 'onClose' | 'open'>;
+
+type CombinedProps = DialogProps & PreferencesProps;
+
+const PreferenceEditor: React.FC<CombinedProps> = props => {
+  const { getUserPreferences, updateUserPreferences } = props;
+
+  const [userPrefs, setUserPrefs] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('');
+  const [successMessage, setSuccessMessage] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [submitting, setSubmitting] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+    getUserPreferences()
+      .then(userPreferences => {
+        setLoading(false);
+        setUserPrefs(JSON.stringify(userPreferences, null, 2));
+      })
+      .catch(() => {
+        setErrorMessage('Unable to load user preferences');
+      });
+  }, [getUserPreferences]);
+
+  const handleSavePreferences = () => {
+    try {
+      const parsed = JSON.parse(userPrefs);
+      setSubmitting(true);
+      setSuccessMessage('');
+      setErrorMessage('');
+      updateUserPreferences(parsed as any)
+        .then(() => {
+          setSuccessMessage('Preferences updated successfully');
+          setSubmitting(false);
+        })
+        .catch(() => {
+          setErrorMessage('Unable to set preferences');
+        });
+    } catch {
+      setErrorMessage('Invalid JSON');
+    }
+  };
+
+  return (
+    <Dialog
+      title="Edit Preferences"
+      open={props.open}
+      onClose={props.onClose}
+      maxWidth="sm"
+    >
+      {errorMessage && <Notice spacingBottom={8} error text={errorMessage} />}
+      {successMessage && (
+        <Notice spacingBottom={8} success text={successMessage} />
+      )}
+      <Typography>
+        Update user preferences tied to Cloud Manager. See the{' '}
+        <Link to="https://developers.linode.com/api/v4/profile-preferences">
+          Linode API documentation
+        </Link>{' '}
+        for more information about user preferences.
+      </Typography>
+      {loading && <Typography>Loading...</Typography>}
+      <div>
+        <textarea
+          value={userPrefs}
+          style={{
+            marginTop: 16,
+            width: 400,
+            height: 300,
+            fontFamily: '"Ubuntu Mono", monospace"'
+          }}
+          onChange={e => setUserPrefs(e.target.value)}
+        ></textarea>
+      </div>
+      <Button
+        style={{ marginTop: 8 }}
+        buttonType="primary"
+        onClick={handleSavePreferences}
+        loading={submitting}
+      >
+        Submit
+      </Button>
+    </Dialog>
+  );
+};
+
+const enhanced = compose<CombinedProps, DialogProps>(withPreferences());
+
+export default enhanced(PreferenceEditor);
