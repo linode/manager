f8b67505df21bdeea24225ab91ae190c4804e1fc
Dajahi Wiley <dwiley@linode.com>
2020-07-30 17:42:25 -0400

    Add 'rescue' to types.ts; create RescueDialog.tsx, add it to LinodesLanding and LinodeDetailHeader_CMR; modify LinodeActionMenu_CMR to open Rescue dialog upon click

--- packages/manager/src/features/linodes/LinodesLanding/LinodesLanding.tsx
+++ packages/manager/src/features/linodes/LinodesLanding/LinodesLanding.tsx
@@ -63,6 +63,7 @@ import PowerDialogOrDrawer, { Action } from '../PowerActionsDialogOrDrawer';
 import { linodesInTransition as _linodesInTransition } from '../transitions';
 import CardView from './CardView';
 import DeleteDialog from './DeleteDialog';
+import RescueDialog from '../LinodesDetail/LinodeRescue/RescueDialog';
 import DisplayGroupedLinodes from './DisplayGroupedLinodes';
 import DisplayLinodes from './DisplayLinodes';
 import styled, { StyleProps } from './LinodesLanding.styles';
@@ -80,6 +81,7 @@ interface State {
   selectedLinodeID?: number;
   selectedLinodeLabel?: string;
   deleteDialogOpen: boolean;
+  rescueDialogOpen: boolean;
   groupByTag: boolean;
   CtaDismissed: boolean;
   linodeResizeOpen: boolean;
@@ -108,6 +110,7 @@ export class ListLinodes extends React.Component<CombinedProps, State> {
   state: State = {
     powerDialogOpen: false,
     deleteDialogOpen: false,
+    rescueDialogOpen: false,
     groupByTag: false,
     CtaDismissed: BackupsCtaDismissed.get(),
     linodeResizeOpen: false,
@@ -170,6 +173,10 @@ export class ListLinodes extends React.Component<CombinedProps, State> {
           linodeMigrateOpen: true
         });
         break;
+      case 'rescue':
+        this.setState({
+          rescueDialogOpen: true
+        });
     }
     this.setState({
       selectedLinodeID: linodeID,
@@ -181,6 +188,7 @@ export class ListLinodes extends React.Component<CombinedProps, State> {
     this.setState({
       powerDialogOpen: false,
       deleteDialogOpen: false,
+      rescueDialogOpen: false,
       linodeResizeOpen: false,
       linodeMigrateOpen: false
     });
@@ -303,6 +311,11 @@ export class ListLinodes extends React.Component<CombinedProps, State> {
               open={this.state.linodeMigrateOpen}
               onClose={this.closeDialogs}
               linodeID={this.state.selectedLinodeID ?? -1}
+            />
+            <RescueDialog
+              open={this.state.rescueDialogOpen}
+              onClose={this.closeDialogs}
+              linodeId={this.state.selectedLinodeID ?? -1}
             />
           </>
         )}
