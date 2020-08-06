44cf86f0a14514b2738521bc28f4f95660fe92d2
Jared Kobos <jaredkobos@gmail.com>
2020-08-06 11:31:09 -0400

    Update empty messages to match designs

--- packages/manager/src/features/NotificationCenter/Alerts.tsx
+++ packages/manager/src/features/NotificationCenter/Alerts.tsx
@@ -2,7 +2,13 @@ import * as React from 'react';
 import NotificationSection from './NotificationSection';
 
 export const Alerts: React.FC<{}> = _ => {
-  return <NotificationSection content={[]} header="Alerts" />;
+  return (
+    <NotificationSection
+      content={[]}
+      header="Alerts"
+      emptyMessage="There are no threshold alerts since your last login."
+    />
+  );
 };
 
 export default React.memo(Alerts);
