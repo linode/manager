b9e4df20eb6aeb14d2c22b7edd0d80e9a39f2624
Jared <jaredkobos@gmail.com>
2020-08-06 16:44:08 -0400

    Add usePreferences hook (#6721)
    * Add usePreferences hook
    
    * Return Promise from updatePreferences

---
+++ packages/manager/src/hooks/usePreferences.ts
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { ApplicationState } from 'src/store';
+import { UserPreferences } from 'src/store/preferences/preferences.actions';
+import {
+  getUserPreferences,
+  updateUserPreferences
+} from 'src/store/preferences/preferences.requests';
+import { Dispatch } from './types';
+
+export interface Preferences {
+  preferences: UserPreferences;
+  updateUserPreferences: (
+    preferences: UserPreferences
+  ) => Promise<UserPreferences>;
+}
+
+export const usePreferences = () => {
+  const dispatch: Dispatch = useDispatch();
+  const preferences = useSelector(
+    (state: ApplicationState) => state.preferences.data
+  );
+
+  const updatePreferences = (newPreferences: UserPreferences) =>
+    dispatch(getUserPreferences()).then(currentPreferences => {
+      dispatch(
+        updateUserPreferences({ ...currentPreferences, ...newPreferences })
+      );
+    });
+
+  return {
+    preferences,
+    updatePreferences
+  };
+};
+
+export default usePreferences;
