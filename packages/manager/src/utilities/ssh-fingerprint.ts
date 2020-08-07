e865784a2faad2ab1eb609f9b8a96a47c45eaf60
Jared Kobos <jaredkobos@gmail.com>
2020-08-07 16:06:04 -0400

    Add comments

--- packages/manager/src/utilities/ssh-fingerprint.ts
+++ packages/manager/src/utilities/ssh-fingerprint.ts
@@ -1,6 +1,13 @@
 import * as crypto from 'crypto';
 import { reportException } from 'src/exceptionReporting';
 
+/**
+ * Return the fingerprint of an SSH pubkey.
+ * Defaults to md5 as that's what e.g. GitHub displays.
+ *
+ * Originally taken from https://github.com/bahamas10/node-ssh-fingerprint/blob/master/ssh-fingerprint.js
+ * As of 8/20, replaced because the regex-based method does not handle ed25519 keys correctly.
+ */
 export default (pub: string, alg: string = 'md5') => {
   try {
     const cleanpub = pub.split(' ')?.[1] ?? '';
