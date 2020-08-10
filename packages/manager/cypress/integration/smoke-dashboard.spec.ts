62a3998add0cdd01aa812e9e2ab60462882f55b8
ndukatz <ndukatz@linode.com>
2020-08-10 11:32:44 -0400

    added linodes to basic dasboard test, cleaned up a few other things

--- packages/manager/cypress/integration/smoke-dashboard.spec.ts
+++ packages/manager/cypress/integration/smoke-dashboard.spec.ts
@@ -1,34 +1,38 @@
 import { Route } from '../support/api/routes';
+import { createLinode, deleteAllTestLinodes } from '../support/api/linodes';
+import { waitForAppLoad } from '../support/ui/common';
 
 describe('dashboard', () => {
-  it('checks the dashboard page', () => {
-    cy.visitWithLogin('/');
-    cy.get('[data-qa-add-new-menu-button="true"]').should('be.visible');
-    cy.findByLabelText('Main search').should('be.visible');
-    cy.get('[aria-label="Click to view notifications drawer"]').should(
-      'be.visible'
-    );
-    cy.findByText('Make a payment').should('be.visible');
-    cy.findByText('Uninvoiced Charges*').should('be.visible');
-    cy.findByText('Payment & Credits').should('be.visible');
-    cy.findByText('Next Cycle Estimated Balance*').should('be.visible');
-    cy.findByText('Pending Actions').should('be.visible');
-    cy.findByText('View history').should('be.visible');
-    cy.findByText('Open Support Tickets').should('be.visible');
-    cy.findByText('View all tickets').should('be.visible');
-    cy.findByText('Alerts').should('be.visible');
-    cy.findByText('Community Updates').should('be.visible');
-    cy.findByText('Visit the Community').should('be.visible');
-    cy.findByText('System Status').should('be.visible');
-    cy.findByText('View fleet status').should('be.visible');
-    cy.get('[data-qa-loading="true"]').should('be.visible');
-    cy.get('[data-qa-tp]').should('be.visible');
-    cy.get('[aria-controls="tabpanel-linodes"]').should('be.visible');
-    cy.findByText('Docs').should('be.visible');
-    cy.get('[data-qa-tab-body="true"]').should('be.visible');
+  it('checks the dashboard page with 2+ linodes', () => {
+    createLinode().then(() => {
+      createLinode().then(() => {
+        cy.visitWithLogin('/');
+        cy.get('[data-qa-add-new-menu-button="true"]').should('be.visible');
+        cy.findByLabelText('Main search').should('be.visible');
+        cy.get('[aria-label="Click to view notifications drawer"]').should(
+          'be.visible'
+        );
+        cy.findByText('Make a payment').should('be.visible');
+        cy.findByText('Uninvoiced Charges*').should('be.visible');
+        cy.findByText('Payment & Credits').should('be.visible');
+        cy.findByText('Next Cycle Estimated Balance*').should('be.visible');
+        cy.findByText('Pending Actions').should('be.visible');
+        cy.findByText('View history').should('be.visible');
+        cy.findByText('Open Support Tickets').should('be.visible');
+        cy.findByText('View all tickets').should('be.visible');
+        cy.findByText('Community Updates').should('be.visible');
+        cy.findByText('Visit the Community').should('be.visible');
+        cy.findByText('System Status').should('be.visible');
+        cy.findByText('View fleet status').should('be.visible');
+        cy.get('[data-qa-loading="true"]').should('be.visible');
+        cy.get('[data-qa-tp]').should('be.visible');
+        cy.findByText('Docs').should('be.visible');
+        deleteAllTestLinodes();
+      });
+    });
   });
 
-  it('caps page load time and counts GET requests', () => {
+  it('caps page load time and GET requests', () => {
     const MAX_GET_REQ_TO_API = 4;
     const xhrData: any = [];
     cy.wrap(xhrData).as('xhrData');
@@ -49,17 +53,6 @@ describe('dashboard', () => {
     cy.get('@xhrData')
       .its('length')
       .should('be.lte', MAX_GET_REQ_TO_API);
-  });
-});
-describe('check we display', () => {
-  it(`Linode list empty with no data`, () => {
-    cy.visitWithLogin('/');
-    cy.server();
-    Route.getLinodes({
-      response: { data: [] }
-    });
-    cy.get('[data-qa-tab-body="true"]').should('be.visible');
-    cy.findByText('0 RUNNING');
   });
 
   it('Verify correct error message when unable to get linodes', () => {
