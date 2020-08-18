4125a7befb2d50db5344f5d2a6a73af24d9147b1
Jared Kobos <jaredkobos@gmail.com>
2020-08-18 16:37:07 -0400

    Add local toggle of chronological view

--- packages/manager/src/features/NotificationCenter/NotificationDrawer.tsx
+++ packages/manager/src/features/NotificationCenter/NotificationDrawer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Clock from 'src/assets/icons/clock.svg';
 import { makeStyles, Theme } from 'src/components/core/styles';
+import Typography from 'src/components/core/Typography';
 import Drawer from 'src/components/Drawer';
 import Community from './Community';
 import Maintenance from './Maintenance';
@@ -8,7 +9,9 @@ import OpenSupportTickets from './OpenSupportTickets';
 import PastDue from './PastDue';
 import PendingActions from './PendingActions';
 import useAccount from 'src/hooks/useAccount';
+import IconButton from 'src/components/core/IconButton';
 import { NotificationData } from './NotificationData/useNotificationData';
+import { ContentRow, NotificationItem } from './NotificationSection';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -40,6 +43,18 @@ interface Props {
   onClose: () => void;
 }
 
+const chronologicalSort = (a: NotificationItem, b: NotificationItem) => {
+  const timeA = a.timeStamp ?? Infinity;
+  const timeB = b.timeStamp ?? Infinity;
+  if (timeA < timeB) {
+    return 1;
+  }
+  if (timeA > timeB) {
+    return -1;
+  }
+  return 0;
+};
+
 export const NotificationDrawer: React.FC<Props> = props => {
   const { data, open, onClose } = props;
   const { account } = useAccount();
@@ -47,28 +62,71 @@ export const NotificationDrawer: React.FC<Props> = props => {
   const balance = (account.data?.balance ?? 0) + 50;
   const { community, pendingActions, support } = data;
 
+  const [chronologicalView, setChronologicalView] = React.useState(false);
+
+  const handleToggleView = () => {
+    setChronologicalView(currentView => !currentView);
+  };
+
+  const chronologicalNotificationList = React.useMemo(() => {
+    return [...community.events, ...pendingActions, ...support.data].sort(
+      chronologicalSort
+    );
+  }, [community.events, pendingActions, support.data]);
+
   return (
     <Drawer open={open} onClose={onClose} title="" className={classes.root}>
       {balance > 0 ? <PastDue balance={balance} /> : null}
       <div className={classes.actionHeader}>
-        <Clock />
+        <IconButton
+          aria-label="Toggle chronological display"
+          aria-describedby={'displayViewDescription'}
+          title={`Toggle chronological display`}
+          onClick={handleToggleView}
+          disableRipple
+        >
+          <Clock />
+        </IconButton>
       </div>
-      <div className={classes.notificationSectionContainer}>
-        <PendingActions pendingActions={pendingActions} />
-        <Maintenance />
-        <OpenSupportTickets
-          loading={support.loading}
-          error={Boolean(support.error)}
-          openTickets={support.data}
-        />
-        <Community
-          communityEvents={community.events}
-          loading={community.loading}
-          error={Boolean(community.error)}
-        />
-      </div>
+
+      {chronologicalView ? (
+        <ChronologicalView notifications={chronologicalNotificationList} />
+      ) : (
+        <div className={classes.notificationSectionContainer}>
+          <PendingActions pendingActions={pendingActions} />
+          <Maintenance />
+          <OpenSupportTickets
+            loading={support.loading}
+            error={Boolean(support.error)}
+            openTickets={support.data}
+          />
+          <Community
+            communityEvents={community.events}
+            loading={community.loading}
+            error={Boolean(community.error)}
+          />
+        </div>
+      )}
     </Drawer>
   );
 };
 
 export default React.memo(NotificationDrawer);
+
+interface ChronoProps {
+  notifications: NotificationItem[];
+}
+const ChronologicalView: React.FC<ChronoProps> = props => {
+  const { notifications } = props;
+  if (notifications.length === 0) {
+    return <Typography>No notifications to display.</Typography>;
+  }
+  return (
+    <>
+      {' '}
+      {notifications.map(thisItem => (
+        <ContentRow key={`chronological-list-${thisItem.id}`} item={thisItem} />
+      ))}
+    </>
+  );
+};
