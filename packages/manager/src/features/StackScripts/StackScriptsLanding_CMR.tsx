4d9751f016563081affac39a6853755023f52128
Tiffany Wong <wong.tyan@gmail.com>
2020-08-19 11:45:15 -0400

    Replace with LandingHeader

--- packages/manager/src/features/StackScripts/StackScriptsLanding_CMR.tsx
+++ packages/manager/src/features/StackScripts/StackScriptsLanding_CMR.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { compose } from 'recompose';
-import AddNewLink from 'src/components/AddNewLink/AddNewLink_CMR';
-import Breadcrumb from 'src/components/Breadcrumb';
 import CircleProgress from 'src/components/CircleProgress';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import Grid from 'src/components/Grid';
+import LandingHeader from 'src/components/LandingHeader';
 import Notice from 'src/components/Notice';
 import withImagesContainer, {
   WithImages
@@ -19,34 +18,26 @@ const useStyles = makeStyles((theme: Theme) => ({
   root: {
     backgroundColor: theme.color.white,
     margin: 0,
-    marginRight: 15,
     padding: 0,
     width: '100%'
   },
-  title: {
-    marginTop: theme.spacing(3),
-    marginLeft: 15
-  },
-  addNewLinkWrapper: {
-    marginRight: 15,
-    padding: 0
-  },
-  addNewLink: {
-    height: 34
+  panel: {
+    '& > div': {
+      padding: theme.spacing(2)
+    }
   }
 }));
 
 type CombinedProps = WithImages & RouteComponentProps<{}>;
 
 export const StackScriptsLanding: React.FC<CombinedProps> = props => {
-  const { history, imagesData, location } = props;
+  const { history, imagesData } = props;
+  const { _loading } = useReduxLoad(['images']);
   const classes = useStyles();
 
   const goToCreateStackScript = () => {
     history.push('/stackscripts/create');
   };
-
-  const { _loading } = useReduxLoad(['images']);
 
   return (
     <React.Fragment>
@@ -54,38 +45,18 @@ export const StackScriptsLanding: React.FC<CombinedProps> = props => {
       {!!history.location.state && !!history.location.state.successMessage && (
         <Notice success text={history.location.state.successMessage} />
       )}
-      <Grid
-        className={classes.root}
-        container
-        justify="space-between"
-        alignItems="flex-end"
-      >
-        <Grid item className="py0">
-          <Breadcrumb
-            pathname={location.pathname}
-            labelTitle="StackScripts"
-            data-qa-title
-            className={classes.title}
-          />
-        </Grid>
-        <Grid item className="py0">
-          <Grid container alignItems="flex-end">
-            <Grid item className={classes.addNewLinkWrapper}>
-              <AddNewLink
-                className={classes.addNewLink}
-                data-qa-create-new-stackscript
-                label="Create a StackScript..."
-                onClick={goToCreateStackScript}
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      <LandingHeader
+        title="StackScripts"
+        entity="StackScript"
+        onAddNew={goToCreateStackScript}
+        docsLink="https://www.linode.com/docs/platform/stackscripts/"
+        createButtonWidth={180}
+      />
       <Grid container>
         {_loading ? (
           <CircleProgress />
         ) : (
-          <Grid item xs={12}>
+          <Grid className={classes.panel} item xs={12}>
             <StackScriptPanel
               publicImages={imagesData}
               queryString={props.location.search}
