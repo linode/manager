babc7b6d88b38c450ba60097235966eef74e5750
Adrien Courdavault <acourdavault@linode.com>
2020-08-21 13:32:16 -0400

    add mock of disk

--- packages/manager/cypress/integration/images/smoke-create-image.spec.ts
+++ packages/manager/cypress/integration/images/smoke-create-image.spec.ts
@@ -14,7 +14,40 @@ describe('create linode', () => {
         pages: 1
       }
     }).as('getImages');
+    cy.route({
+      method: 'POST',
+      url: '/v4/images*'
+    }).as('postImages');
     createLinode().then(linode => {
+      cy.route({
+        method: 'GET',
+        url: `/v4/linode/instances/${linode.id}/disks*`,
+        response: {
+          results: 2,
+          data: [
+            {
+              id: 44311273,
+              status: 'ready',
+              label: 'Debian 10 Disk',
+              created: '2020-08-21T17:26:14',
+              updated: '2020-08-21T17:26:30',
+              filesystem: 'ext4',
+              size: 81408
+            },
+            {
+              id: 44311274,
+              status: 'ready',
+              label: '512 MB Swap Image',
+              created: '2020-08-21T17:26:14',
+              updated: '2020-08-21T17:26:31',
+              filesystem: 'swap',
+              size: 512
+            }
+          ],
+          page: 1,
+          pages: 1
+        }
+      }).as('getDisks');
       cy.visitWithLogin('/images');
       cy.wait('@getImages');
       const imageLabel = makeImageLabel();
@@ -25,15 +58,20 @@ describe('create linode', () => {
       cy.findByText('Select a Linode').type(`${linode.label}{enter}`);
       cy.findByText('Select a Disk')
         .click()
-        .type('{enter}');
+        .type(`Debian{enter}`);
 
       cy.findAllByLabelText('Label').type(`${imageLabel}{enter}`);
       // Line should be removed once bug fixed
-      cy.findByText('Snap', { exact: false, timeout: 1000 }).should('not.exist');
+      cy.findByText('Snap', { exact: false, timeout: 1000 }).should(
+        'not.exist'
+      );
       // here we should also stube the post to catch the call
-    //   cy.findByText('Create')
-    //     .should('be.visible', { timeout: 100 })
-    //     .click();
+      cy.findByText('Create')
+        .should('be.visible')
+        .click();
+      cy.wait('@postImages')
+        .its('status')
+        .should('eq', 200);
     });
 
     deleteAllTestImages();
