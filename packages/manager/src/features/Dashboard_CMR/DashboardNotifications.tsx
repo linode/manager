f8dccb5ec9b7188ad64faf8e87611dd147b76a94
Tiffany Wong <wong.tyan@gmail.com>
2020-08-25 17:56:29 -0400

    Initial commit

--- packages/manager/src/features/Dashboard_CMR/DashboardNotifications.tsx
+++ packages/manager/src/features/Dashboard_CMR/DashboardNotifications.tsx
@@ -1,21 +1,29 @@
 import { getInvoices } from '@linode/api-v4/lib/account';
+import { pathOr } from 'ramda';
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import Hidden from 'src/components/core/Hidden';
 import Paper from 'src/components/core/Paper';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Grid from 'src/components/Grid';
-import useNotificationData from 'src/features/NotificationCenter/NotificationData/useNotificationData';
-import useAccount from 'src/hooks/useAccount';
-import { useAPIRequest } from 'src/hooks/useAPIRequest';
 import BillingSummary from 'src/features/Billing/BillingPanels/BillingSummary';
-import LinodeNews from './LinodeNews';
-
 import {
   Community,
   Maintenance,
   OpenSupportTickets,
   PendingActions
 } from 'src/features/NotificationCenter';
-import Hidden from 'src/components/core/Hidden';
+import useNotificationData from 'src/features/NotificationCenter/NotificationData/useNotificationData';
+import useAccount from 'src/hooks/useAccount';
+import { useAPIRequest } from 'src/hooks/useAPIRequest';
+import { MapState } from 'src/store/types';
+import LinodeNews from './LinodeNews';
+import ManagedDashboardCard from '../Dashboard/ManagedDashboardCard/ManagedDashboardCard_CMR';
+
+interface StateProps {
+  managed: boolean;
+}
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -32,12 +40,15 @@ const useStyles = makeStyles((theme: Theme) => ({
   }
 }));
 
-export const Notifications: React.FC<{}> = _ => {
+export type CombinedProps = StateProps;
+
+export const Notifications: React.FC<CombinedProps> = props => {
   const classes = useStyles();
   const { account } = useAccount();
   const balance = account.data?.balance ?? 0;
   const balanceUninvoiced = account.data?.balance_uninvoiced ?? 0;
 
+  const { managed } = props;
   const { community, pendingActions, support } = useNotificationData();
 
   const mostRecentInvoiceRequest = useAPIRequest<number | undefined>(
@@ -57,6 +68,7 @@ export const Notifications: React.FC<{}> = _ => {
           mostRecentInvoiceId={mostRecentInvoiceRequest.data}
         />
       </Hidden>
+      {managed && <ManagedDashboardCard />}
       <Paper className={classes.root}>
         <Grid
           container
@@ -126,4 +138,18 @@ export const Notifications: React.FC<{}> = _ => {
   );
 };
 
-export default React.memo(Notifications);
+const mapStateToProps: MapState<StateProps, {}> = state => {
+  return {
+    managed: pathOr(
+      false,
+      ['__resources', 'accountSettings', 'data', 'managed'],
+      state
+    )
+  };
+};
+
+const connected = connect(mapStateToProps);
+
+const enhanced = compose<CombinedProps, {}>(connected)(Notifications);
+
+export default enhanced;
