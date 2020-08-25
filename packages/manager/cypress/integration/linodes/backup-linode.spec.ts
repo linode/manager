e3484250bcb79ed5a4b3a7f1302ad898c6427c06
ndukatz <ndukatz@linode.com>
2020-08-25 09:36:10 -0400

    backups

--- packages/manager/cypress/integration/linodes/backup-linode.spec.ts
+++ packages/manager/cypress/integration/linodes/backup-linode.spec.ts
@@ -1,25 +1,49 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import { createLinode } from '../../support/api/linodes';
+import { waitForAppLoad } from '../../support/ui/common';
 
 describe('linode backups', () => {
-  it('create basic backup', () => {
+  it('enable backups', () => {
     cy.visitWithLogin('/dashboard');
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: '*/linodes/*/backups/enable'
-    }).as('enableBackups');
     createLinode().then(linode => {
-      cy.visit(`/${linode.id}/backup`);
-      cy.findByText('Enable Backups')
+      cy.server();
+      cy.route({
+        method: 'POST',
+        url: `*/linode/instances/${linode.id}/backups/enable`
+      }).as('enableBackups');
+      waitForAppLoad();
+      cy.visit(`/linodes/${linode.id}/backup`);
+      cy.contains('Enable Backups')
         .should('be.visible')
         .click();
-      cy.findByText('Enable Backups')
+      cy.get('[data-qa-confirm-enable-backups="true"]')
         .should('be.visible')
         .click();
       cy.wait('@enableBackups');
-      cy.get('[data-qa-manual-name="true"]').type(`${linode.name} backup`);
-      cy.findByText('Take Snapshot').click();
-      cy.findByText('Take Snapshot').click();
+      cy.findByText(`${linode.label}`).should('be.visible');
+      cy.findByText('Automatic and manual backups will be listed here');
+    });
+  });
+
+  it('enable backups', () => {
+    cy.visitWithLogin('/dashboard');
+    createLinode().then(linode => {
+      cy.server();
+      cy.route({
+        method: 'POST',
+        url: `*/linode/instances/${linode.id}/backups/enable`,
+        response: {}
+      }).as('enableBackups');
+      cy.wait('@enableBackups');
+      // test for creating snapshot, test for new linode from backup
+      // cy.get('[data-qa-manual-name="true"]').type(`${linode.label} backup`);
+      // cy.findByText('Take Snapshot')
+      //   .should('be.visible')
+      //   .click();
+      // cy.get('[data-qa-confirm="true"]')
+      //   .should('be.visible')
+      //   .click();
+      // cy.findByText('Linode busy.').should('be.visible');
     });
   });
 });
