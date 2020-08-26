81448ec03097c7116a33cb797123da44c7f798cf
Adrien Courdavault <acourdavault@linode.com>
2020-08-26 13:12:39 -0400

    cleanup

--- packages/manager/src/analytics.ts
+++ packages/manager/src/analytics.ts
@@ -1,17 +1,21 @@
+/* eslint-disable no-unused-expressions */
 import { GTM_ID } from './constants';
 
 /* tslint:disable */
 const gaInit = (i: any, s: any, o: any, g: any, r: any, a: any, m: any) => {
   const currdate: any = new Date();
   i['GoogleAnalyticsObject'] = r;
+
   (i[r] =
     i[r] ||
     function() {
+      // eslint-disable-next-line prefer-rest-params
       (i[r].q = i[r].q || []).push(arguments);
     }),
     (i[r].l = 1 * currdate);
   (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
   a.async = 1;
+  // eslint-disable-next-line scanjs-rules/assign_to_src
   a.src = g;
   m.parentNode.insertBefore(a, m);
 };
@@ -22,10 +26,11 @@ const initGTM = (w: any, d: any, s: any, l: any, i: any) => {
     'gtm.start': new Date().getTime(),
     event: 'gtm.js'
   });
-  var f = d.getElementsByTagName(s)[0],
+  const f = d.getElementsByTagName(s)[0],
     j = d.createElement(s),
     dl = l != 'dataLayer' ? '&l=' + l : '';
   j.async = true;
+  // eslint-disable-next-line scanjs-rules/assign_to_src
   j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
   f.parentNode.insertBefore(j, f);
 };
@@ -78,5 +83,5 @@ export const initGTMUser = (userId: string) => {
     gtmAccountID: GTM_ID,
     gtmIndividualUserId: userId
   });
-  initTagManager(GTM_ID)
+  initTagManager(GTM_ID);
 };
