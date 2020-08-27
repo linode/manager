88dae378f063c95886c2483a64296bbb8932451b
John Callahan <johnwcallahan@gmail.com>
2020-08-27 10:05:59 -0400

    Dev Tools Environment Switcher (#6689)
    This adds an environment switcher to the dev tools. Environment options are set in packages/manager/.env (see comment for explanation).
    
    Also added "Firewalls" feature flag and a little styling.
    
    * Env switcher
    
    * Track tool component with git
    
    * Improve option generator function
    
    * Add unit tests
    
    * More unit tests
    
    * Refactoring
    
    * Adjustments to tool
    
    * Only enable env switch in development mode
    
    * Cleanup
    
    * More cleanup
    
    * Check validity of values in local storage
    
    * Adjust valid check
    
    * Use label to determine selected ENV

--- packages/manager/src/dev-tools/load.ts
+++ packages/manager/src/dev-tools/load.ts
@@ -1,20 +1,10 @@
 // Thanks to https://kentcdodds.com/blog/make-your-own-dev-tools
 
 function loadDevTools(callback: () => any) {
-  // this allows you to explicitly disable it in development for example
-  const explicitlyDisabled =
-    window.location.search.includes('dev-tools=false') ||
-    window.localStorage.getItem('dev-tools') === 'false';
-  const explicitlyEnabled =
-    window.location.search.includes('dev-tools=true') ||
-    window.localStorage.getItem('dev-tools') === 'true';
   // we want it enabled by default everywhere but production and we also want
   // to support the dev tools in production (to make us more productive triaging production issues).
   // you can enable the DevTools via localStorage or the query string.
-  if (
-    !explicitlyDisabled &&
-    (process.env.NODE_ENV === 'development' || explicitlyEnabled)
-  ) {
+  if (devToolsEnabled()) {
     // use a dynamic import so the dev-tools code isn't bundled with the regular
     // app code so we don't worry about bundle size.
     import('./dev-tools')
@@ -26,3 +16,22 @@ function loadDevTools(callback: () => any) {
   }
 }
 export default loadDevTools;
+
+// Defaults to FALSE in production builds, but can be explicity enabled with
+// query param or local storage.
+//
+// Defaults to TRUE in development mode, but can be explicity disabled with
+// query param or local storage.
+export const devToolsEnabled = () => {
+  const explicitlyDisabled =
+    window.location.search.includes('dev-tools=false') ||
+    window.localStorage.getItem('dev-tools') === 'false';
+  const explicitlyEnabled =
+    window.location.search.includes('dev-tools=true') ||
+    window.localStorage.getItem('dev-tools') === 'true';
+
+  return (
+    !explicitlyDisabled &&
+    (process.env.NODE_ENV === 'development' || explicitlyEnabled)
+  );
+};
--- packages/manager/src/request.tsx
+++ packages/manager/src/request.tsx
@@ -1,20 +1,21 @@
-import { AxiosError, AxiosResponse } from 'axios';
 import { baseRequest } from '@linode/api-v4/lib/request';
 import { APIError } from '@linode/api-v4/lib/types';
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import * as React from 'react';
-
-import { ACCESS_TOKEN, DEFAULT_ERROR_MESSAGE } from 'src/constants';
-import { interceptErrors } from 'src/utilities/interceptAPIError';
-
 import { AccountActivationError } from 'src/components/AccountActivation';
 import { MigrateError } from 'src/components/MigrateError';
 import { VerificationError } from 'src/components/VerificationError';
-
+import {
+  ACCESS_TOKEN,
+  API_ROOT,
+  DEFAULT_ERROR_MESSAGE,
+  LOGIN_ROOT
+} from 'src/constants';
 import store from 'src/store';
 import { handleLogout } from 'src/store/authentication/authentication.actions';
 import { setErrors } from 'src/store/globalErrors/globalErrors.actions';
-
-import { API_ROOT, LOGIN_ROOT } from 'src/constants';
+import { interceptErrors } from 'src/utilities/interceptAPIError';
+import { getEnvLocalStorageOverrides } from './utilities/storage';
 
 const handleSuccess: <T extends AxiosResponse<any>>(
   response: T
@@ -112,20 +113,11 @@ baseRequest.interceptors.request.use(config => {
   /** Will end up being "Admin: 1234" or "Bearer 1234" */
   const token = ACCESS_TOKEN || (state.authentication?.token ?? '');
 
-  let finalUrl = '';
-
-  /**
-   * override the base URL with the one we have defined in the .env file
-   */
-  if (config.url && config.baseURL) {
-    finalUrl = config.baseURL.includes('login')
-      ? config.url.replace(config.baseURL, LOGIN_ROOT)
-      : config.url.replace(config.baseURL, API_ROOT);
-  }
+  const url = getURL(config);
 
   return {
     ...config,
-    url: finalUrl || config.url,
+    url,
     headers: {
       ...config.headers,
       ...(token && { Authorization: `${token}` })
@@ -140,3 +132,26 @@ Interceptor that initiates re-authentication if:
 Also rejects non-error responses if the API is in Maintenance mode
 */
 baseRequest.interceptors.response.use(handleSuccess, handleError);
+
+export const getURL = ({ url, baseURL }: AxiosRequestConfig) => {
+  if (!url || !baseURL) {
+    return;
+  }
+
+  const isLogin = baseURL.includes('login');
+
+  const localStorageOverrides = getEnvLocalStorageOverrides();
+
+  // If we have environment overrides in local storage, use those.
+  if (localStorageOverrides) {
+    return isLogin
+      ? url.replace(baseURL, localStorageOverrides.loginRoot)
+      : url.replace(baseURL, localStorageOverrides.apiRoot);
+  }
+
+  // Override the baseURL (from @linode/api-v4) with the one we have defined
+  // in the environment (via .env file).
+  return isLogin
+    ? url.replace(baseURL, LOGIN_ROOT)
+    : url.replace(baseURL, API_ROOT);
+};
