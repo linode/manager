5e9af80da20a4a3c8ae43d40c3356162bb0dfbda
Kayla Wilkins <kwilkins@linode.com>
2020-08-27 14:57:06 -0400

    table styles for access key table

--- packages/manager/src/features/ObjectStorage/AccessKeyLanding/AccessKeyTable.tsx
+++ packages/manager/src/features/ObjectStorage/AccessKeyLanding/AccessKeyTable.tsx
@@ -16,10 +16,15 @@ import Typography from 'src/components/core/Typography';
 import { PaginationProps } from 'src/components/Pagey';
 import Table from 'src/components/Table';
 import TableCell from 'src/components/TableCell';
+import Table_CMR from 'src/components/Table/Table_CMR';
+import TableCell_CMR from 'src/components/TableCell/TableCell_CMR';
+import TableRow_CMR from 'src/components/TableRow/TableRow_CMR';
 import TableRowEmptyState from 'src/components/TableRowEmptyState';
 import TableRowError from 'src/components/TableRowError';
 import TableRowLoading from 'src/components/TableRowLoading';
 import AccessKeyMenu from './AccessKeyMenu';
+import AccessKeyMenu_CMR from './AccessKeyMenu_CMR';
+import useFlags from 'src/hooks/useFlags';
 
 type ClassNames = 'root' | 'headline' | 'paper' | 'labelCell' | 'copyIcon';
 
@@ -67,6 +72,8 @@ export const AccessKeyTable: React.FC<CombinedProps> = props => {
     openDrawerForEditing
   } = props;
 
+  const flags = useFlags();
+
   const renderContent = () => {
     if (isRestrictedUser) {
       return <TableRowEmptyState colSpan={12} />;
@@ -93,56 +100,108 @@ export const AccessKeyTable: React.FC<CombinedProps> = props => {
   };
 
   const renderRows = (objectStorageKeys: ObjectStorageKey[]) => {
-    return objectStorageKeys.map((eachKey: ObjectStorageKey) => (
-      <TableRow key={eachKey.id} data-qa-table-row={eachKey.label}>
-        <TableCell parentColumn="Label">
-          <Typography variant="h3" data-qa-key-label>
-            {eachKey.label}
-          </Typography>
-        </TableCell>
-        <TableCell parentColumn="Access Key">
-          <Typography variant="body1" data-qa-key-created>
-            {eachKey.access_key}
-            <CopyTooltip
-              text={eachKey.access_key}
-              className={classes.copyIcon}
+    return objectStorageKeys.map((eachKey: ObjectStorageKey) =>
+      flags.cmr ? (
+        <TableRow_CMR key={eachKey.id} data-qa-table-row={eachKey.label}>
+          <TableCell_CMR>
+            <Typography variant="body2" component="h3" data-qa-key-label>
+              {eachKey.label}
+            </Typography>
+          </TableCell_CMR>
+          <TableCell_CMR>
+            <Typography variant="body1" data-qa-key-created>
+              {eachKey.access_key}
+              <CopyTooltip
+                text={eachKey.access_key}
+                className={classes.copyIcon}
+              />
+            </Typography>
+          </TableCell_CMR>
+          <TableCell_CMR>
+            <AccessKeyMenu_CMR
+              objectStorageKey={eachKey}
+              openRevokeDialog={openRevokeDialog}
+              openDrawerForEditing={openDrawerForEditing}
+              label={eachKey.label}
             />
-          </Typography>
-        </TableCell>
-        <TableCell>
-          <AccessKeyMenu
-            objectStorageKey={eachKey}
-            openRevokeDialog={openRevokeDialog}
-            openDrawerForEditing={openDrawerForEditing}
-            label={eachKey.label}
-          />
-        </TableCell>
-      </TableRow>
-    ));
+          </TableCell_CMR>
+        </TableRow_CMR>
+      ) : (
+        <TableRow key={eachKey.id} data-qa-table-row={eachKey.label}>
+          <TableCell parentColumn="Label">
+            <Typography variant="h3" data-qa-key-label>
+              {eachKey.label}
+            </Typography>
+          </TableCell>
+          <TableCell parentColumn="Access Key">
+            <Typography variant="body1" data-qa-key-created>
+              {eachKey.access_key}
+              <CopyTooltip
+                text={eachKey.access_key}
+                className={classes.copyIcon}
+              />
+            </Typography>
+          </TableCell>
+          <TableCell>
+            <AccessKeyMenu
+              objectStorageKey={eachKey}
+              openRevokeDialog={openRevokeDialog}
+              openDrawerForEditing={openDrawerForEditing}
+              label={eachKey.label}
+            />
+          </TableCell>
+        </TableRow>
+      )
+    );
   };
 
   return (
     <React.Fragment>
       <Paper className={classes.paper}>
-        <Table
-          aria-label="List of Object Storage Access Keys"
-          rowCount={data && data.length}
-          colCount={2}
-        >
-          <TableHead>
-            <TableRow data-qa-table-head role="rowgroup">
-              <TableCell className={classes.labelCell} data-qa-header-label>
-                Label
-              </TableCell>
-              <TableCell className={classes.labelCell} data-qa-header-key>
-                Access Key
-              </TableCell>
-              {/* empty cell for kebab menu */}
-              <TableCell />
-            </TableRow>
-          </TableHead>
-          <TableBody>{renderContent()}</TableBody>
-        </Table>
+        {flags.cmr ? (
+          <Table_CMR
+            aria-label="List of Object Storage Access Keys"
+            rowCount={data && data.length}
+            colCount={2}
+          >
+            <TableHead>
+              <TableRow_CMR data-qa-table-head>
+                <TableCell_CMR
+                  className={classes.labelCell}
+                  data-qa-header-label
+                >
+                  Label
+                </TableCell_CMR>
+                <TableCell_CMR className={classes.labelCell} data-qa-header-key>
+                  Access Key
+                </TableCell_CMR>
+                {/* empty cell for kebab menu */}
+                <TableCell_CMR />
+              </TableRow_CMR>
+            </TableHead>
+            <TableBody>{renderContent()}</TableBody>
+          </Table_CMR>
+        ) : (
+          <Table
+            aria-label="List of Object Storage Access Keys"
+            rowCount={data && data.length}
+            colCount={2}
+          >
+            <TableHead>
+              <TableRow data-qa-table-head role="rowgroup">
+                <TableCell className={classes.labelCell} data-qa-header-label>
+                  Label
+                </TableCell>
+                <TableCell className={classes.labelCell} data-qa-header-key>
+                  Access Key
+                </TableCell>
+                {/* empty cell for kebab menu */}
+                <TableCell />
+              </TableRow>
+            </TableHead>
+            <TableBody>{renderContent()}</TableBody>
+          </Table>
+        )}
       </Paper>
     </React.Fragment>
   );
