519af8786ac4243315d090342b15dff6523d360c
ndukatz <ndukatz@linode.com>
2020-09-01 15:54:26 -0600

    second backups test, commented out third test. need to find a way to have a precreated snapshot to create linode from

--- packages/manager/cypress/integration/linodes/backup-linode.spec.ts
+++ packages/manager/cypress/integration/linodes/backup-linode.spec.ts
@@ -1,9 +1,12 @@
 /* eslint-disable sonarjs/no-duplicate-string */
-import { createLinode } from '../../support/api/linodes';
+import {
+  createLinode,
+  createLinodeWithBackupsEnabled
+} from '../../support/api/linodes';
 import { waitForAppLoad } from '../../support/ui/common';
 
 describe('linode backups', () => {
-  it('enable backups', () => {
+  it.skip('enable backups', () => {
     cy.visitWithLogin('/dashboard');
     createLinode().then(linode => {
       cy.server();
@@ -25,25 +28,42 @@ describe('linode backups', () => {
     });
   });
 
-  it('enable backups', () => {
+  it.skip('cant snapshot while booting linode', () => {
     cy.visitWithLogin('/dashboard');
-    createLinode().then(linode => {
-      cy.server();
-      cy.route({
-        method: 'POST',
-        url: `*/linode/instances/${linode.id}/backups/enable`,
-        response: {}
-      }).as('enableBackups');
-      cy.wait('@enableBackups');
-      // test for creating snapshot, test for new linode from backup
-      // cy.get('[data-qa-manual-name="true"]').type(`${linode.label} backup`);
-      // cy.findByText('Take Snapshot')
-      //   .should('be.visible')
-      //   .click();
-      // cy.get('[data-qa-confirm="true"]')
-      //   .should('be.visible')
-      //   .click();
-      // cy.findByText('Linode busy.').should('be.visible');
+    createLinodeWithBackupsEnabled().then(linode => {
+      cy.visit(`/linodes/${linode.id}/backup`);
+      cy.findByText('Take Snapshot')
+        .should('be.visible')
+        .click();
+      cy.findByText('Label is required.');
+      cy.get('[data-qa-manual-name="true"]').type(`${linode.label} backup`);
+      cy.findByText('Take Snapshot')
+        .should('be.visible')
+        .click();
+      cy.get('[data-qa-confirm="true"]')
+        .should('be.visible')
+        .click();
+      cy.contains('Linode busy.').should('be.visible');
     });
   });
+
+  // it('create linode from snapshot', () => {
+  //   cy.visitWithLogin('/dashboard');
+  //   createLinodeWithBackupsEnabled().then(linode => {
+  //     cy.visit(`/linodes/${linode.id}/backup`);
+  //     cy.server();
+  //     cy.route({
+  //       method: 'POST',
+  //       url: `*/linode/instances/${linode.id}/backups`,
+  //       response: {}
+  //     }).as('enableBackups');
+  //     cy.get('[data-qa-manual-name="true"]').type(`${linode.label} backup`);
+  //     cy.findByText('Take Snapshot')
+  //       .should('be.visible')
+  //       .click();
+  //     cy.get('[data-qa-confirm="true"]')
+  //       .should('be.visible')
+  //       .click();
+  //   });
+  // });
 });
