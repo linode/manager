9f304d84cc2a8a731ec9f10b4c558905d4ba8b1c
John Callahan <johnwcallahan@gmail.com>
2020-09-04 16:15:30 -0400

    M3-2083 Stabilize table sort (#6855)
    Previously, in <OrderBy />, each time the order changed (like when clicking a table sort cell), the original data was sorted with the new order.
    
    This means:
    
    Have two Linodes, “a-linode”, “b-linode”, both in the same region.
    On LinodesLanding, click the “Label” cell, so they are sorted: “b-linode”, “a-linode”.
    Click the Region cell.
    Observe: the order has switched, even though both regions are the same.
    To correct this, the previously sorted data needs to be re-sorted, not the original data.
    
    It’s kind of convoluted, but the way I solved this is to keep “sortedData” in state, updating it when the order changes.
    
    I thought about using a simple ref to capture the last state, but there were enough odd scenarios (data being lazy loaded, preferences changing and thus “order” and “orderBy” changes, etc.) that this made the most sense to me. If you feel it’s over-engineered and have a better idea, I’m eager to hear it and make this better.
    
    Note: I wanted to include a test, so instead of writing more Enzyme tests, I first converted this component to an FC.

--- packages/manager/src/components/OrderBy.tsx
+++ packages/manager/src/components/OrderBy.tsx
@@ -1,25 +1,21 @@
 import { DateTime } from 'luxon';
-import { pathOr, sort, splitAt } from 'ramda';
+import { equals, pathOr, sort, splitAt } from 'ramda';
 import * as React from 'react';
-import { compose } from 'recompose';
-import { debounce } from 'throttle-debounce';
-
 import { Order } from 'src/components/Pagey';
-import withPreferences, {
-  Props as PreferencesProps
-} from 'src/containers/preferences.container';
+import usePreferences from 'src/hooks/usePreferences';
+import usePrevious from 'src/hooks/usePrevious';
 import {
   SortKey,
   UserPreferences
 } from 'src/store/preferences/preferences.actions';
-import { isArray } from 'util';
-
 import {
   sortByArrayLength,
   sortByNumber,
   sortByString,
   sortByUTFDate
 } from 'src/utilities/sort-by';
+import { debounce } from 'throttle-debounce';
+import { isArray } from 'util';
 
 export interface OrderByProps extends State {
   handleOrderChange: (orderBy: string, order: Order) => void;
@@ -39,7 +35,7 @@ interface Props {
   preferenceKey?: SortKey; // If provided, will store/read values from user preferences
 }
 
-type CombinedProps = Props & PreferencesProps;
+export type CombinedProps = Props;
 
 /**
  * Given a set of UserPreferences (returned from the API),
@@ -69,8 +65,8 @@ export const getInitialValuesFromUserPreferences = (
   );
 };
 
-export const sortData = (orderBy: string, order: Order) =>
-  sort((a, b) => {
+export const sortData = (orderBy: string, order: Order) => {
+  return sort((a, b) => {
     /* If the column we're sorting on is an array (e.g. 'tags', which is string[]),
      *  we want to sort by the length of the array. Otherwise, do a simple comparison.
      */
@@ -124,70 +120,79 @@ export const sortData = (orderBy: string, order: Order) =>
     }
     return sortByNumber(aValue, bValue, order);
   });
+};
 
-export class OrderBy extends React.Component<CombinedProps, State> {
-  state: State = getInitialValuesFromUserPreferences(
-    this.props.preferenceKey ?? '',
-    this.props.preferences,
-    this.props.orderBy ?? 'label',
-    this.props.order ?? 'asc'
+export const OrderBy: React.FC<CombinedProps> = props => {
+  const { preferences, updatePreferences } = usePreferences();
+
+  const initialValues = getInitialValuesFromUserPreferences(
+    props.preferenceKey ?? '',
+    preferences ?? {},
+    props.orderBy ?? 'label',
+    props.order ?? 'desc'
   );
 
-  _updateUserPreferences = (orderBy: string, order: Order) => {
-    /**
-     * If the preferenceKey is provided, we will store the passed
-     * order props in user preferences. They will be read the next
-     * time this component is loaded.
-     */
-    const {
-      getUserPreferences,
-      preferenceKey,
-      updateUserPreferences
-    } = this.props;
-    if (preferenceKey) {
-      getUserPreferences()
-        .then(preferences => {
-          updateUserPreferences({
-            ...preferences,
-            sortKeys: {
-              ...preferences.sortKeys,
-              [preferenceKey]: { order, orderBy }
-            }
-          });
-        })
-        // It doesn't matter if this fails, the value simply won't be preserved.
-        .catch(_ => null);
+  const [orderBy, setOrderBy] = React.useState<string>(initialValues.orderBy);
+  const [order, setOrder] = React.useState<Order>(initialValues.order);
+
+  // Give this initial value in the form of a function so it's run only once.
+  const [sortedData, setSortedData] = React.useState<any[]>(() => {
+    return sortData(initialValues.orderBy, initialValues.order)(props.data);
+  });
+
+  // Re-sort the data when it changes.
+  const prevData = usePrevious(props.data);
+  React.useEffect(() => {
+    if (equals(props.data, prevData)) {
+      return;
     }
+
+    const newlySortedData = sortData(orderBy, order)(props.data);
+    setSortedData(newlySortedData);
+  }, [props.data, order, orderBy, prevData]);
+
+  const debouncedUpdateUserPreferences = React.useRef(
+    debounce(1500, false, (orderBy: string, order: Order) => {
+      /**
+       * If the preferenceKey is provided, we will store the passed
+       * order props in user preferences. They will be read the next
+       * time this component is loaded.
+       */
+      if (props.preferenceKey) {
+        updatePreferences({
+          sortKeys: {
+            ...(preferences?.sortKeys ?? {}),
+            [props.preferenceKey]: { order, orderBy }
+          }
+        });
+      }
+    })
+  ).current;
+
+  const handleOrderChange = (newOrderBy: string, newOrder: Order) => {
+    setOrderBy(newOrderBy);
+    setOrder(newOrder);
+
+    const newlySortedData = sortData(newOrderBy, newOrder)(sortedData);
+    setSortedData(newlySortedData);
+
+    debouncedUpdateUserPreferences(newOrderBy, newOrder);
   };
 
-  updateUserPreferences = debounce(1500, false, this._updateUserPreferences);
-
-  handleOrderChange = (orderBy: string, order: Order) => {
-    this.setState({ orderBy, order });
-    this.updateUserPreferences(orderBy, order);
+  const downstreamProps = {
+    ...props,
+    order,
+    orderBy,
+    handleOrderChange,
+    data: sortedData,
+    count: props.data.length
   };
 
-  render() {
-    const sortedData = sortData(
-      this.state.orderBy,
-      this.state.order
-    )(this.props.data);
+  // eslint-disable-next-line
+  return <>{props.children(downstreamProps)}</>;
+};
 
-    const props = {
-      ...this.props,
-      ...this.state,
-      handleOrderChange: this.handleOrderChange,
-      data: sortedData,
-      count: this.props.data.length
-    };
-
-    return this.props.children(props);
-  }
-}
-
-const enhanced = compose<CombinedProps, Props>(withPreferences());
-
-export default enhanced(OrderBy);
+export default React.memo(OrderBy);
 
 const isValidDate = (date: any) => {
   return DateTime.fromISO(date).isValid;
