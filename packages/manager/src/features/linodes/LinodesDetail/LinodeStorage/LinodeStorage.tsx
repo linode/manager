b594bc48519645213d83323d2d8dab7d5737343d
Dajahi Wiley <dwiley@linode.com>
2020-09-17 16:08:28 -0400

    Undo LinodeStorage.tsx DocumentTitle changes

--- packages/manager/src/features/linodes/LinodesDetail/LinodeStorage/LinodeStorage.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeStorage/LinodeStorage.tsx
@@ -1,20 +1,10 @@
 import * as React from 'react';
 import LinodeDisks from '../LinodeAdvanced/LinodeDisks_CMR';
 import LinodeVolumes from 'src/features/linodes/LinodesDetail/LinodeAdvanced/LinodeVolumes';
-import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 
-interface Props {
-  linodeLabel?: string;
-}
-
-export const LinodeStorage: React.FC<Props> = props => {
-  const { linodeLabel } = props;
-
+export const LinodeStorage: React.FC = () => {
   return (
     <>
-      <DocumentTitleSegment
-        segment={`${linodeLabel ? `${linodeLabel} - ` : ''} Storage`}
-      />
       <LinodeDisks />
       <LinodeVolumes />
     </>
