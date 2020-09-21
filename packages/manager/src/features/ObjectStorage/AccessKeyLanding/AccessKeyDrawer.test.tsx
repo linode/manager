01268cd716addfb96b1af3b9c4360967d32fd2ad
Jared <jaredkobos@gmail.com>
2020-09-21 16:45:05 -0400

    M3-4478 Add: access management for OBJ access keys (#6909)
    * Add toggle and related boilerplate to AccessKeyDrawer
    
    * Add table with static content
    
    * Extract/update types and move to JS client
    
    * Add logic for default scope generation; pass these down to the access table
    
    * Rewrite existing unit test in RTL
    
    * Add a few more tests
    
    * Add logic for updating scopes
    
    * handle select all logic
    
    * Send permissions in API request body
    
    - rename scopes to permissions in the UI code to match API
    - conditionally send permissions in the response block iff
    limited access has been selected by the user.
    
    * Use underscores instead of dashes in access types
    
    * Rename things to make the API happy
    
    * Update access table styles
    
    * Add disabled state for table
    
    * Add explanatory text below limited access toggle
    
    * Fix build
    
    * Review feedback
    
    - Memoize access table
    - Disable toggle when not in create mode
    - Update aria label on table
    - Set max-height on table and make scrollable
    
    * Memoize table

--- packages/manager/src/features/ObjectStorage/AccessKeyLanding/AccessKeyDrawer.test.tsx
+++ packages/manager/src/features/ObjectStorage/AccessKeyLanding/AccessKeyDrawer.test.tsx
@@ -1,23 +1,89 @@
-import { shallow } from 'enzyme';
-import { AccountSettings } from '@linode/api-v4/lib/account';
+import { Scope } from '@linode/api-v4/lib/object-storage/types';
+import { screen } from '@testing-library/react';
 import * as React from 'react';
-import { AccessKeyDrawer, MODES, Props } from './AccessKeyDrawer';
+import { objectStorageBucketFactory } from 'src/factories/objectStorage';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+import {
+  AccessKeyDrawer,
+  getDefaultScopes,
+  MODES,
+  Props
+} from './AccessKeyDrawer';
+import { getUpdatedScopes } from './LimitedAccessControls';
 
 describe('AccessKeyDrawer', () => {
-  const props = {
-    classes: { root: '' },
+  const props: Props = {
     open: true,
     onSubmit: jest.fn(),
     onClose: jest.fn(),
-    label: 'test-label',
-    updateLabel: jest.fn(),
-    isLoading: false,
     mode: 'creating' as MODES,
-    isRestrictedUser: false,
-    object_storage: 'active' as AccountSettings['object_storage']
+    isRestrictedUser: false
   };
-  const wrapper = shallow<Props>(<AccessKeyDrawer {...props} />);
+  renderWithTheme(<AccessKeyDrawer {...props} />);
   it('renders without crashing', () => {
-    expect(wrapper).toHaveLength(1);
+    expect(screen.getByText(/create an access key/i)).toBeInTheDocument();
+  });
+
+  describe('default scopes helper method', () => {
+    const mockBuckets = objectStorageBucketFactory.buildList(5);
+    it('should return an item for each bucket', () => {
+      expect(getDefaultScopes(mockBuckets)).toHaveLength(mockBuckets.length);
+    });
+
+    it('should return objects with the correct shape', () => {
+      const bucket = mockBuckets[0];
+      expect(getDefaultScopes([bucket])[0]).toEqual({
+        cluster: bucket.cluster,
+        bucket_name: bucket.label,
+        permissions: 'none'
+      });
+    });
+
+    it('should sort the permissions by cluster', () => {
+      const usaBucket = objectStorageBucketFactory.build({
+        cluster: 'us-east-1'
+      });
+      const germanBucket = objectStorageBucketFactory.build({
+        cluster: 'eu-central-1'
+      });
+      const asiaBucket = objectStorageBucketFactory.build({
+        cluster: 'ap-south-1'
+      });
+      const unsortedBuckets = [usaBucket, germanBucket, asiaBucket];
+      expect(
+        getDefaultScopes(unsortedBuckets).map(scope => scope.cluster)
+      ).toEqual(['ap-south-1', 'eu-central-1', 'us-east-1']);
+    });
+  });
+
+  describe('Updating scopes', () => {
+    const mockBuckets = objectStorageBucketFactory.buildList(3);
+
+    const mockScopes = getDefaultScopes(mockBuckets);
+
+    it('should update the correct scope', () => {
+      const newScope = { ...mockScopes[2], permissions: 'read_write' } as Scope;
+      expect(getUpdatedScopes(mockScopes, newScope)[2]).toHaveProperty(
+        'permissions',
+        'read_write'
+      );
+    });
+
+    it('should leave other scopes unchanged', () => {
+      const newScope = { ...mockScopes[2], access: 'read_write' } as Scope;
+      const updatedScopes = getUpdatedScopes(mockScopes, newScope);
+      expect(updatedScopes[0]).toEqual(mockScopes[0]);
+      expect(updatedScopes[1]).toEqual(mockScopes[1]);
+      expect(updatedScopes.length).toEqual(mockScopes.length);
+    });
+
+    it('should handle crappy input', () => {
+      const newScope = {
+        cluster: 'totally-fake',
+        bucket_name: 'not-real',
+        permissions: 'read_only'
+      } as Scope;
+      expect(getUpdatedScopes(mockScopes, newScope)).toEqual(mockScopes);
+    });
   });
 });
