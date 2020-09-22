b48eee8d04b6800b15188c6070ec6e29e6e5b2f6
Jared Kobos <jaredkobos@gmail.com>
2020-09-22 13:25:52 -0400

    Prevent overflow of resolvers

--- packages/manager/src/features/linodes/LinodesDetail/LinodeNetworking/NetworkingSummaryPanel/NetworkingSummaryPanel.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeNetworking/NetworkingSummaryPanel/NetworkingSummaryPanel.tsx
@@ -9,11 +9,16 @@ import Hidden from 'src/components/core/Hidden';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
+    display: 'flex',
+    flexFlow: 'row-nowrap',
+    justifyContent: 'flex-start',
+    alignItems: 'start',
     padding: theme.spacing(3),
     paddingBottom: theme.spacing(2) + theme.spacing(1) / 2
   },
   transferHistoryContainer: {
     padding: '16px 0px',
+    flex: 1.3,
     [theme.breakpoints.up('sm')]: {
       padding: '0px 16px'
     },
@@ -22,6 +27,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
   },
   dnsResolverContainer: {
+    flex: 1,
     paddingTop: 8,
     [theme.breakpoints.up('md')]: {
       paddingTop: 0
@@ -44,31 +50,17 @@ const LinodeNetworkingSummaryPanel: React.FC<CombinedProps> = props => {
 
   return (
     <Paper className={classes.root}>
-      <Grid
-        container
-        direction="row"
-        justify="space-between"
-        alignItems="flex-start"
-      >
-        <Grid xs={12} sm={3} item>
-          <NetworkTransfer linodeID={linodeID} linodeLabel={linodeLabel} />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={9}
-          md={6}
-          lg={7}
-          className={classes.transferHistoryContainer}
-        >
-          <TransferHistory linodeID={linodeID} linodeCreated={linodeCreated} />
-        </Grid>
-        <Hidden smDown>
-          <Grid item md={3} lg={2} className={classes.dnsResolverContainer}>
-            <DNSResolvers region={linodeRegion} />
-          </Grid>
-        </Hidden>
+      <Grid item>
+        <NetworkTransfer linodeID={linodeID} linodeLabel={linodeLabel} />
       </Grid>
+      <Grid item className={classes.transferHistoryContainer}>
+        <TransferHistory linodeID={linodeID} linodeCreated={linodeCreated} />
+      </Grid>
+      <Hidden smDown>
+        <Grid item className={classes.dnsResolverContainer}>
+          <DNSResolvers region={linodeRegion} />
+        </Grid>
+      </Hidden>
     </Paper>
   );
 };
