a0457c6822975fe1009f38608f49ccf36bb2c0eb
Tiffany Wong <wong.tyan@gmail.com>
2020-09-29 12:35:29 -0400

    Revert path

--- packages/manager/src/features/Vlans/index.tsx
+++ packages/manager/src/features/Vlans/index.tsx
@@ -21,7 +21,7 @@ const VLan: React.FC<CombinedProps> = props => {
         <DocumentTitleSegment segment="VLANs" />
         <Switch>
           <Route component={VlanLanding} path={path} exact />
-          <Route component={VlanDetail} path={`${path}/detail`} />
+          <Route component={VlanDetail} path={`${path}/:id`} />
           <Redirect to={path} />
         </Switch>
       </React.Fragment>
