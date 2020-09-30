d8ad1224ca9a190be46f7efbe2f9331b5a8ce611
Jared Kobos <jaredkobos@gmail.com>
2020-09-30 16:56:42 -0400

    Refactor

--- packages/manager/src/features/ObjectStorage/BucketDetail/BucketSSL.tsx
+++ packages/manager/src/features/ObjectStorage/BucketDetail/BucketSSL.tsx
@@ -7,9 +7,11 @@ import {
 import * as React from 'react';
 import ActionsPanel from 'src/components/ActionsPanel';
 import Button from 'src/components/Button';
+import CircleProgress from 'src/components/CircleProgress';
 import Paper from 'src/components/core/Paper';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import ConfirmationDialog from 'src/components/ConfirmationDialog';
+import ErrorState from 'src/components/ErrorState';
 import Grid from 'src/components/Grid';
 import Notice from 'src/components/Notice';
 import Typography from 'src/components/core/Typography';
@@ -25,7 +27,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     ...theme.applyLinkStyles
   },
   helperText: {
-    paddingTop: theme.spacing()
+    paddingTop: theme.spacing(),
+    lineHeight: 1.5
   }
 }));
 
@@ -36,17 +39,64 @@ interface Props {
 
 export const BucketSSL: React.FC<Props> = props => {
   const { bucketName, clusterId } = props;
-  const [certificate, setCertificate] = React.useState('');
-  const [sslKey, setSSLKey] = React.useState('');
-  const [submitting, setSubmitting] = React.useState(false);
-  const [error, setError] = React.useState<APIError[] | undefined>(undefined);
-
   const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h2">SSL/TLS Certificate</Typography>
+      <Typography className={classes.helperText}>
+        Object Storage buckets are automatically served with a default TLS
+        certificate that is valid for subdomains of linodeobjects.com. You can
+        upload a custom certificate that will be used for the TLS portion of the
+        HTTPS request instead.
+      </Typography>
+      <SSLBody bucketName={bucketName} clusterId={clusterId} />
+    </Paper>
+  );
+};
+
+interface BodyProps {
+  bucketName: string;
+  clusterId: string;
+}
+
+export const SSLBody: React.FC<BodyProps> = props => {
+  const { bucketName, clusterId } = props;
 
   const request = useAPIRequest(
     () => getSSLCert(clusterId, bucketName).then(response => response.ssl),
     false
   );
+
+  const hasSSL = request.data;
+
+  if (request.loading) {
+    return <CircleProgress />;
+  }
+
+  if (request.error) {
+    return <ErrorState errorText="Error loading TSL cert data" />;
+  }
+
+  if (hasSSL) {
+    return <RemoveCertForm {...props} update={request.update} />;
+  }
+
+  return <AddCertForm {...props} update={request.update} />;
+};
+
+export interface FormProps extends BodyProps {
+  update: () => void;
+}
+
+export const AddCertForm: React.FC<FormProps> = props => {
+  const { bucketName, clusterId, update } = props;
+  const [certificate, setCertificate] = React.useState('');
+  const [sslKey, setSSLKey] = React.useState('');
+  const [submitting, setSubmitting] = React.useState(false);
+  const [error, setError] = React.useState<APIError[] | undefined>(undefined);
+
+  const errorMap = getErrorMap(['certificate', 'private_key'], error);
 
   const onSubmit = () => {
     setSubmitting(true);
@@ -54,7 +104,7 @@ export const BucketSSL: React.FC<Props> = props => {
     uploadSSLCert(clusterId, bucketName, { certificate, private_key: sslKey })
       .then(_ => {
         setSubmitting(false);
-        request.update();
+        update();
         setCertificate('');
         setSSLKey('');
       })
@@ -64,6 +114,55 @@ export const BucketSSL: React.FC<Props> = props => {
       });
   };
 
+  return (
+    <>
+      {errorMap.none && (
+        <Notice error text={errorMap.none} spacingTop={8} spacingBottom={0} />
+      )}
+      <Grid container>
+        <Grid item xs={6}>
+          <TextField
+            label="Certificate"
+            onChange={(e: any) => setCertificate(e.target.value)}
+            value={certificate}
+            multiline
+            fullWidth={false}
+            rows="4"
+            // className={classes.keyTextarea}
+            data-testid="ssl-cert-input"
+            errorText={errorMap.certificate}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            label="Private Key"
+            onChange={(e: any) => setSSLKey(e.target.value)}
+            value={sslKey}
+            multiline
+            rows="4"
+            // className={classes.keyTextarea}
+            data-testid="ssl-cert-input"
+            errorText={errorMap.private_key}
+          />
+        </Grid>
+        <Grid item>
+          <ActionsPanel>
+            <Button
+              onClick={onSubmit}
+              loading={submitting}
+              buttonType="primary"
+            >
+              Upload Certificate
+            </Button>
+          </ActionsPanel>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export const RemoveCertForm: React.FC<FormProps> = props => {
+  const { bucketName, clusterId, update } = props;
   const [open, setOpen] = React.useState(false);
 
   const [submittingDialog, setSubmittingDialog] = React.useState(false);
@@ -73,19 +172,17 @@ export const BucketSSL: React.FC<Props> = props => {
 
   const removeCertificate = () => {
     setSubmittingDialog(true);
-    setError(undefined);
     deleteSSLCert(clusterId, bucketName)
       .then(() => {
         setOpen(false);
         setSubmittingDialog(false);
-        request.update();
+        update();
       })
       .catch(error => {
         setDialogError(error[0].reason);
         setSubmittingDialog(false);
       });
   };
-
   const createActions = () => (
     <ActionsPanel>
       <Button
@@ -104,92 +201,15 @@ export const BucketSSL: React.FC<Props> = props => {
       </Button>
     </ActionsPanel>
   );
-
-  const hasSSL = request.data;
-
-  const errorMap = getErrorMap(['certificate', 'private_key'], error);
-
-  const getNoticeText = () => {
-    return request.loading ? (
-      'Loading...'
-    ) : (
-      <>
-        This bucket {hasSSL ? 'has' : 'does not have'} a TLS certificate
-        available.{' '}
-        {hasSSL ? (
-          <button className={classes.button} onClick={() => setOpen(true)}>
-            Remove
-          </button>
-        ) : null}
-      </>
-    );
-  };
-
   return (
     <>
-      <Paper className={classes.root}>
-        <Typography variant="h2">SSL/TLS Certificate</Typography>
-        <Notice
-          success={hasSSL}
-          warning={!hasSSL}
-          spacingTop={8}
-          spacingBottom={0}
-          text={getNoticeText()}
-        />
-        <Typography className={classes.helperText}>
-          Object Storage buckets are automatically served with a default TLS
-          certificate that is valid for subdomains of linodeobjects.com. You can
-          upload a custom certificate that will be used for the TLS portion of
-          the HTTPS request instead.
-        </Typography>
-        <Grid container>
-          <Grid item xs={6}>
-            <TextField
-              label="Certificate"
-              onChange={(e: any) => setCertificate(e.target.value)}
-              value={certificate}
-              multiline
-              fullWidth={false}
-              rows="4"
-              // className={classes.keyTextarea}
-              data-testid="ssl-cert-input"
-              errorText={errorMap.certificate}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Private Key"
-              onChange={(e: any) => setSSLKey(e.target.value)}
-              value={sslKey}
-              multiline
-              rows="4"
-              // className={classes.keyTextarea}
-              data-testid="ssl-cert-input"
-              errorText={errorMap.private_key}
-            />
-          </Grid>
-
-          <Grid item>
-            {errorMap.none && (
-              <Notice
-                error
-                text={errorMap.none}
-                spacingTop={8}
-                spacingBottom={0}
-              />
-            )}
-            <ActionsPanel>
-              <Button
-                onClick={onSubmit}
-                loading={submitting}
-                buttonType="primary"
-              >
-                Upload Certificate
-              </Button>
-            </ActionsPanel>
-          </Grid>
-        </Grid>
-      </Paper>
+      <Notice success spacingTop={8}>
+        A TLS certificate has already been uploaded for this Bucket. To upload a
+        new certificate, remove the current certificate.{` `}
+      </Notice>
+      <Button destructive onClick={() => setOpen(true)} buttonType="secondary">
+        Remove Certificate
+      </Button>
       <ConfirmationDialog
         title={'Remove TLS certificate'}
         open={open}
