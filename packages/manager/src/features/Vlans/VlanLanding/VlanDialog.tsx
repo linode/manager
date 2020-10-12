0e477fb565c8f1ca85e4781ba9bbd7c56951110e
Kayla Wilkins <kwilkins@linode.com>
2020-10-12 16:09:49 -0400

    M3-4540 VLAN Landing Table (#6935)
    * adding props to fix landing header issues
    
    * adding action menu and making adjustments to tables
    
    * adding a basic mock for vlans, adjustments to row and action menu
    
    * adding delete dialog and handling for linode listing
    
    * adjustments for delete and vlans container
    
    * updating to use actual withVlans HOC instead of factory
    
    * removing placeholder props
    
    * fixing typo
    
    * makingg factory default for testing
    
    * addressing feedback part 1
    
    * fixing carryover
    
    * adjustments for state mgt (feedback part 2)
    
    * reverting to for real conditional
    
    * adjustments for action menu, adding container and reduxLoad
    
    * Fix linter error
    
    * Addressing pr feedback (delete issue and rename of file
    
    Co-authored-by: John Callahan <johnwcallahan@gmail.com>

---
+++ packages/manager/src/features/Vlans/VlanLanding/VlanDialog.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import ActionsPanel from 'src/components/ActionsPanel';
+import Button from 'src/components/Button';
+import Dialog from 'src/components/ConfirmationDialog';
+import { VlanActionsProps } from 'src/containers/vlans.container';
+import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
+
+interface Props extends Pick<VlanActionsProps, 'deleteVlan'> {
+  open: boolean;
+  closeDialog: () => void;
+  selectedVlanID?: number;
+  selectedVlanLabel: string;
+}
+
+type CombinedProps = Props;
+
+const VlanDialog: React.FC<CombinedProps> = props => {
+  const [isSubmitting, setSubmitting] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>(undefined);
+
+  const {
+    open,
+    closeDialog,
+    deleteVlan,
+    selectedVlanID,
+    selectedVlanLabel: label
+  } = props;
+
+  /** reset error on open */
+  React.useEffect(() => {
+    if (open) {
+      setError(undefined);
+    }
+  }, [open]);
+
+  const handleSubmit = () => {
+    const defaultError = 'There was an issue deleting this Virtual LAN.';
+    if (!selectedVlanID) {
+      return setError(defaultError);
+    }
+
+    setSubmitting(true);
+    setError(undefined);
+
+    deleteVlan(selectedVlanID)
+      .then(_ => {
+        setSubmitting(false);
+        closeDialog();
+      })
+      .catch(e => {
+        setSubmitting(false);
+        setError(getAPIErrorOrDefault(e, defaultError)[0].reason);
+      });
+  };
+
+  const _label = label ? label : 'this Virtual LAN';
+
+  return (
+    <Dialog
+      open={open}
+      title={`Delete ${_label}?`}
+      onClose={props.closeDialog}
+      error={error}
+      actions={
+        <Actions
+          onClose={props.closeDialog}
+          isSubmitting={isSubmitting}
+          onSubmit={handleSubmit}
+        />
+      }
+    >
+      Are you sure you want to delete {label}?
+    </Dialog>
+  );
+};
+
+interface ActionsProps {
+  onClose: () => void;
+  onSubmit: () => void;
+  isSubmitting: boolean;
+}
+
+const Actions: React.FC<ActionsProps> = props => {
+  return (
+    <ActionsPanel>
+      <Button onClick={props.onClose} buttonType="cancel">
+        Cancel
+      </Button>
+      <Button
+        onClick={props.onSubmit}
+        loading={props.isSubmitting}
+        destructive
+        buttonType="secondary"
+      >
+        Delete
+      </Button>
+    </ActionsPanel>
+  );
+};
+
+export default React.memo(VlanDialog);
---
+++ packages/manager/src/features/Vlans/VlanLanding/VlanRow.tsx
@@ -0,0 +1,111 @@
+import { VLAN } from '@linode/api-v4/lib/vlans';
+import { APIError } from '@linode/api-v4/lib/types';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { compose } from 'recompose';
+import { makeStyles, Theme } from 'src/components/core/styles';
+import TableCell from 'src/components/TableCell/TableCell_CMR';
+import TableRow from 'src/components/TableRow/TableRow_CMR';
+import ActionMenu, { ActionHandlers } from './VlanActionMenu';
+import { getLinodeLabel } from 'src/features/Dashboard/VolumesDashboardCard/VolumeDashboardRow';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  link: {
+    display: 'block',
+    fontFamily: theme.font.bold,
+    fontSize: '.875rem',
+    lineHeight: '1.14rem'
+  },
+  linodesWrapper: {
+    padding: '8px 0'
+  },
+  labelWrapper: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+    whiteSpace: 'nowrap'
+  }
+}));
+
+export type CombinedProps = VLAN & ActionHandlers;
+
+export const VlanRow: React.FC<CombinedProps> = props => {
+  const {
+    id,
+    description,
+    region,
+    linodes,
+    loading,
+    error,
+    ...actionHandlers
+  } = props;
+
+  const classes = useStyles();
+
+  const getLinodesCellString = (
+    data: number[],
+    loading: boolean,
+    error?: APIError[]
+  ): string | JSX.Element => {
+    if (loading) {
+      return 'Loading...';
+    }
+
+    if (error) {
+      return 'Error retrieving Linodes';
+    }
+
+    if (data.length === 0) {
+      return 'None assigned';
+    }
+
+    return getLinodeLinks(data);
+  };
+
+  const getLinodeLinks = (data: number[]): JSX.Element => {
+    return (
+      // eslint-disable-next-line
+      <>
+        {data.map(linodeID => (
+          <Link
+            className={classes.link}
+            key={linodeID}
+            to={`/linodes/${linodeID}`}
+            data-testid="vlan-row-link"
+          >
+            {getLinodeLabel(linodeID)}
+          </Link>
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <TableRow
+      key={`vlan-row-${id}`}
+      data-testid={`vlan-row-${id}`}
+      ariaLabel={`Virtual LAN ${description}`}
+    >
+      <TableCell>
+        <div className={classes.labelWrapper}>
+          <Link className={classes.link} to={`/vlans/${id}`}>
+            {description}
+          </Link>
+        </div>
+      </TableCell>
+      <TableCell>{region}</TableCell>
+
+      <TableCell className={classes.linodesWrapper}>
+        {getLinodesCellString(linodes, loading, error)}
+      </TableCell>
+
+      <TableCell>
+        <ActionMenu vlanID={id} vlanLabel={description} {...actionHandlers} />
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default compose<CombinedProps, ActionHandlers & VLAN>(React.memo)(
+  VlanRow
+);
