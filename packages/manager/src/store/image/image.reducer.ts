41f031abc5d1f375738aff3630ecd6857202bee0
Jared Kobos <jaredkobos@gmail.com>
2020-10-20 09:12:01 -0400

    Refactor a bit

--- packages/manager/src/store/image/image.reducer.ts
+++ packages/manager/src/store/image/image.reducer.ts
@@ -39,7 +39,7 @@ const reducer: Reducer<State> = (state = defaultState, action) => {
     const { result } = action.payload;
     const images = result.filter(thisImage => {
       // NOTE: Temporarily hide public Kubernetes images.
-      return thisImage.is_public ? !thisImage.label.match(/kube/i) : true;
+      return !thisImage.is_public || !thisImage.label.match(/kube/i);
     });
     return onGetAllSuccess(images, state, Object.keys(images).length);
   }
