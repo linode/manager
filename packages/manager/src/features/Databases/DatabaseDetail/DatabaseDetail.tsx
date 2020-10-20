ca8a2145dc6780cfcd9d01b83b3b9d0d286f656a
Tiffany Wong <wong.tyan@gmail.com>
2020-10-20 15:22:10 -0400

    todo: routing

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseDetail.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseDetail.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { compose } from 'recompose';
-
+import DatabaseDetailNavigation from './DatabaseDetailNavigation';
 import DatabaseEntityDetail from './DatabaseEntityDetail';
+
 type CombinedProps = RouteComponentProps<{ id: string }>;
 
 const DatabaseDetail: React.FC<CombinedProps> = () => {
-  return <DatabaseEntityDetail database="" />;
+  return (
+    <>
+      <DatabaseEntityDetail database="" />
+      <DatabaseDetailNavigation />
+    </>
+  );
 };
 
 export default compose<CombinedProps, {}>(React.memo)(DatabaseDetail);
