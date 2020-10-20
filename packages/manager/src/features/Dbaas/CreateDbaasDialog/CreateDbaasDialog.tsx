96ea92d285676fe2ae45646184df26dfddd5f560
Dajahi Wiley <dwiley@linode.com>
2020-10-20 15:10:14 -0400

    A few minor additions to CreateDbaasDialog before rebasing

--- packages/manager/src/features/Dbaas/CreateDbaasDialog/CreateDbaasDialog.tsx
+++ packages/manager/src/features/Dbaas/CreateDbaasDialog/CreateDbaasDialog.tsx
@@ -2,16 +2,37 @@ import * as React from 'react';
 import { useFormik } from 'formik';
 import Dialog from 'src/components/Dialog';
 import { dbaasContext } from 'src/context';
+import { makeStyles, Theme } from 'src/components/core/styles';
+import Typography from 'src/components/core/Typography';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  form: {},
+  formSection: {
+    marginBottom: theme.spacing(3)
+  },
+  helperText: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(),
+    lineHeight: 1.5,
+    fontSize: '1rem'
+  }
+}));
 
 export const CreateDbaasDialog: React.FC<{}> = _ => {
   const context = React.useContext(dbaasContext);
+  const classes = useStyles();
 
   const { resetForm, ...formik } = useFormik({
-    initialValues: {},
+    initialValues: {
+      label: '',
+      region: '',
+      root_pass: ''
+    },
     validationSchema: {},
     validateOnChange: false,
     onSubmit: values => submitForm(values)
   });
+
   /** Reset errors and state when the modal opens */
   React.useEffect(() => {
     if (context.isOpen) {
