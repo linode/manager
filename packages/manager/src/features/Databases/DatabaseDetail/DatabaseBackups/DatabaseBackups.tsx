c345f1c74aeb73ad1445baef203da5793722e3f1
Kayla Wilkins <kwilkins@linode.com>
2020-10-22 14:59:11 -0600

    initial work adding new table for db backups

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseBackups/DatabaseBackups.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseBackups/DatabaseBackups.tsx
@@ -1,7 +1,42 @@
+import { DatabaseBackup } from '@linode/api-v4/lib/databases';
 import * as React from 'react';
+import Paper from 'src/components/core/Paper';
+import TableBody from 'src/components/core/TableBody';
+import TableHead from 'src/components/core/TableHead';
+import TableRow from 'src/components/core/TableRow';
+import Table from 'src/components/Table';
+import TableCell from 'src/components/TableCell';
+import BackupTableRow from './DatabaseBackupTableRow';
 
-export const DatabaseBackups: React.FC<{}> = () => {
-  return <span>Backups</span>;
+interface Props {
+  backups: DatabaseBackup[];
+}
+
+type CombinedProps = Props;
+
+export const DatabaseBackups: React.FC<CombinedProps> = props => {
+  const { backups } = props;
+
+  return (
+    <Paper style={{ padding: 0 }}>
+      <Table aria-label="List of Backups">
+        <TableHead>
+          <TableRow>
+            <TableCell>Backup ID</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Date Created</TableCell>
+            <TableCell>Duration</TableCell>
+            <TableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {backups.map((backup: DatabaseBackup, idx: number) => (
+            <BackupTableRow key={idx} backup={backup} />
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
 };
 
 export default DatabaseBackups;
