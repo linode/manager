a6b1a2b25bab667e5923d594f4fa4356481a40a6
Kayla Wilkins <kwilkins@linode.com>
2020-10-22 13:17:44 -0600

    new combobox component- initial work

---
+++ packages/manager/src/components/Combobox/Combobox.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete/Autocomplete';
+// import { makeStyles, Theme } from 'src/components/core/styles'
+import TextField from '../core/TextField';
+
+interface Props {
+  id: string;
+  label: string;
+  placeholder: string;
+  options: any[];
+  groups?: () => string;
+  onChange?: () => void;
+  loading: boolean;
+  disabled: boolean;
+}
+
+type CombinedProps = Props;
+
+export const Combobox: React.FC<CombinedProps> = props => {
+  const {
+    id,
+    label,
+    placeholder,
+    options,
+    groups,
+    onChange,
+    disabled,
+    loading
+  } = props;
+
+  return (
+    <Autocomplete
+      id={id}
+      options={options}
+      groupBy={groups}
+      onChange={onChange}
+      getOptionLabel={option => option.label}
+      renderInput={(params: any) => (
+        <TextField
+          {...params}
+          label={label}
+          placeholder={placeholder}
+          variant="outlined"
+        />
+      )}
+      style={{ width: 415 }}
+      disabled={disabled}
+      loading={loading}
+    />
+
+    // <Autocomplete
+    //   id='image-select'
+    //   options={filteredImages as any}
+    //   groupBy={imagesToGroupedItems => imagesToGroupedItems.vendor}
+    //   onChange={onChange}
+    //   getOptionLabel={(option: Image) => option.label}
+    //   renderInput={(params: any) => <TextField {...params} label="Images" placeholder="Select an Image" variant="outlined" />}
+    //   style={{width: 415}}
+    //   disabled={disabled}
+    //   loading={_loading}
+    //   //className={classNames}
+    // />
+  );
+};
+
+export default Combobox;
