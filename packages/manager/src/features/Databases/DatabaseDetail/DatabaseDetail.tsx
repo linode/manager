dfa73692fe8c07a601589e72e9a8e5f650ba797d
John Callahan <johnwcallahan@gmail.com>
2020-10-27 16:15:06 -0400

    M3-4599 Database Detail Header (#7035)
    EntityDetail component for DBaaS. Currently implemented on the Details page only (not Card view).
    
    The duplication is disheartening here, since this was basically copied over from Linodes -> VLANS. Now that we see how this component is used across entity types, maybe it's time for some tech debt work (especially the TagCell/TagDrawer, which I chose to render right inside the Footer portion).
    
    What _should_ work:
    
    - "Delete"
    - Adding/removing tags + tag drawer
    - All data is real
    - "Connection Details" loading + error states.
    
    What _doesn't_ work:
    - General error state (needs some tuning of useReduxLoad that @Jskobos and I talked about, plus waiting on https://github.com/linode/manager/pull/7031)
    - "Region" (needs API work)
    - "Plan" (needs API work)
    - "Resize" (needs API work)

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseDetail.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseDetail.tsx
@@ -1,15 +1,29 @@
 import * as React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, useRouteMatch } from 'react-router-dom';
 import NavTabs from 'src/components/NavTabs';
 import { NavTab } from 'src/components/NavTabs/NavTabs';
+import useDatabases from 'src/hooks/useDatabases';
+import useReduxLoad from 'src/hooks/useReduxLoad';
 import DatabaseBackups from './DatabaseBackups';
 import DatabaseEntityDetail from './DatabaseEntityDetail';
 import DatabaseSettings from './DatabaseSettings';
 
 type CombinedProps = RouteComponentProps<{ id: string }>;
 
-const DatabaseDetail: React.FC<CombinedProps> = props => {
-  const baseURL = props.match.url;
+const DatabaseDetail: React.FC<CombinedProps> = () => {
+  const match = useRouteMatch<{ id: string }>('/databases/:id');
+  const databases = useDatabases();
+  useReduxLoad(['databases']);
+
+  const thisDatabaseID = match?.params?.id;
+
+  const thisDatabase = databases.databases.itemsById[thisDatabaseID ?? '-1'];
+
+  if (!thisDatabase || !match) {
+    return null;
+  }
+
+  const baseURL = match.url;
 
   const tabs: NavTab[] = [
     {
@@ -28,7 +42,7 @@ const DatabaseDetail: React.FC<CombinedProps> = props => {
 
   return (
     <>
-      <DatabaseEntityDetail database="" />
+      <DatabaseEntityDetail database={thisDatabase} />
       <NavTabs tabs={tabs} />
     </>
   );
