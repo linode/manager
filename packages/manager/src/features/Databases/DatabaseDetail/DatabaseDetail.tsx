71d98391f5cad31353d2930ec23b4866cd83961c
John Callahan <johnwcallahan@gmail.com>
2020-10-27 11:07:44 -0400

    DBaaS Routing + NavTabs component (#7031)
    This cleans up the DBaaS routing a bit, and also adds a new `<NavTabs />` component which aims to addressing the following (subtle) issues with tabbed navigation:
    
    - Changing tabs pushes _two_ events to history, so you have to click "Back" twice to get to the previous tab.
    - Bogus routes are accepted (the first tab is selected by default, but the bogus path remains in the URL).
    - If any part of the path is bogus, the first tab is always selected, even if part of the part is correct (e.g. `/profile/keys/bogus` displays the first tab, "Display", rather than the "SSH Keys" tab, which would be expected).
    
    Most importantly, there's a lot of duplicated code which has been following us around for a while. (How many times have each of us copy/pasted the comment: `/* NB: These must correspond to the routes inside the Switch */`?)
    
    This is an attempt to simplify things behind a single component.

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseDetail.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseDetail.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import DatabaseDetailNavigation from './DatabaseDetailNavigation';
+import NavTabs from 'src/components/NavTabs';
+import { NavTab } from 'src/components/NavTabs/NavTabs';
+import DatabaseBackups from './DatabaseBackups';
 import DatabaseEntityDetail from './DatabaseEntityDetail';
+import DatabaseSettings from './DatabaseSettings';
 
 type CombinedProps = RouteComponentProps<{ id: string }>;
 
-const DatabaseDetail: React.FC<CombinedProps> = () => {
+const DatabaseDetail: React.FC<CombinedProps> = props => {
+  const baseURL = props.match.url;
+
+  const tabs: NavTab[] = [
+    {
+      title: 'Backups',
+      routeName: `${baseURL}/backups`,
+      component: DatabaseBackups,
+      backgroundRendering: true
+    },
+    {
+      title: 'Settings',
+      routeName: `${baseURL}/settings`,
+      component: DatabaseSettings,
+      backgroundRendering: true
+    }
+  ];
+
   return (
     <>
       <DatabaseEntityDetail database="" />
-      <DatabaseDetailNavigation />
+      <NavTabs tabs={tabs} />
     </>
   );
 };
