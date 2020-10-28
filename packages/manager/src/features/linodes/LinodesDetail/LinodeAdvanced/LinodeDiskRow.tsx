f9061a89208ff14bc587bff09a25ddfb6c4ac3c5
Jared Kobos <jaredkobos@gmail.com>
2020-10-28 14:36:33 -0400

    Add first pass at progress bar

--- packages/manager/src/features/linodes/LinodesDetail/LinodeAdvanced/LinodeDiskRow.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeAdvanced/LinodeDiskRow.tsx
@@ -1,5 +1,6 @@
 import { Disk } from '@linode/api-v4/lib/linodes';
 import * as React from 'react';
+import BarPercent from 'src/components/BarPercent/BarPercent_CMR';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import TableCell from 'src/components/TableCell/TableCell_CMR';
 import TableRow from 'src/components/TableRow/TableRow_CMR';
@@ -7,13 +8,13 @@ import LinodeDiskActionMenu from './LinodeDiskActionMenu_CMR';
 
 const useStyles = makeStyles((theme: Theme) => ({
   diskLabel: {
-    width: '23%'
+    width: '20%'
   },
   diskType: {
-    width: '8%'
+    width: '10%'
   },
   diskSize: {
-    width: '9%'
+    width: '30%'
   },
   actionMenu: {
     paddingTop: 0,
@@ -47,11 +48,23 @@ export const LinodeDiskRow: React.FC<Props> = props => {
     readOnly
   } = props;
 
+  const isResizing = true;
+
   return (
     <TableRow data-qa-disk={disk.label}>
       <TableCell className={classes.diskLabel}>{disk.label}</TableCell>
       <TableCell className={classes.diskType}>{disk.filesystem}</TableCell>
-      <TableCell className={classes.diskSize}>{disk.size} MB</TableCell>
+
+      <TableCell className={classes.diskSize}>
+        {isResizing ? (
+          <div>
+            Resizing ({25}%)
+            <BarPercent max={100} value={100 ?? 0} rounded narrow />
+          </div>
+        ) : (
+          `${disk.size} MB`
+        )}
+      </TableCell>
       <TableCell className={classes.actionMenu}>
         <LinodeDiskActionMenu
           linodeStatus={status || 'offline'}
