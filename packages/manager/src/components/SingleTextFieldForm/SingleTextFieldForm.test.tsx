13c7270f900432f8cb3fc83a9f79e897c1f22bf1
John Callahan <johnwcallahan@gmail.com>
2020-11-02 12:00:41 -0500

    M3-3140 Add: Ability to update username from profile -> display (#7064)
    This makes the "Username" field on Profile -> Display editable.
    
    Same difficulty present in #4931 exists here, namely:
    
    a) Username is only editable at /account
    b) Email is only editable at /profile
    
    This means we need separate submit buttons for each.
    
    I created a new component called SingleTextFieldForm, since this is a pattern we end up using quite a bit (I used it twice on this page).
    Delted EmailChangeForm.tsx in favor of putting everything in DisplaySettings.tsx.
    Removed the tests for EmailChangeForm.tsx and DisplaySettings.tsx in favor of new tests for SingleTextFieldForm.

---
+++ packages/manager/src/components/SingleTextFieldForm/SingleTextFieldForm.test.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import SingleTextFieldForm, { Props } from './SingleTextFieldForm';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('SingleTextFieldForm', () => {
+  const props: Props = {
+    label: 'Username',
+    submitForm: jest.fn(() => Promise.resolve()),
+    initialValue: 'jane-doe'
+  };
+
+  it('Renders a TextField with the given label and initial value', () => {
+    renderWithTheme(<SingleTextFieldForm {...props} />);
+    expect(screen.getByText('Username')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('jane-doe')).toBeInTheDocument();
+  });
+
+  it('Renders a success message on success', async () => {
+    renderWithTheme(<SingleTextFieldForm {...props} />);
+    userEvent.click(screen.getByText('Save'));
+    await waitFor(() =>
+      expect(screen.getByText(/username updated/i)).toBeInTheDocument()
+    );
+  });
+
+  it('Renders an error message on error', async () => {
+    renderWithTheme(
+      <SingleTextFieldForm
+        {...props}
+        submitForm={jest.fn(() =>
+          Promise.reject([{ reason: 'Error updating username.' }])
+        )}
+      />
+    );
+    userEvent.click(screen.getByText('Save'));
+    await waitFor(() =>
+      expect(screen.getByText(/error updating/i)).toBeInTheDocument()
+    );
+  });
+
+  it('Calls submitForm function with new value', () => {
+    renderWithTheme(<SingleTextFieldForm {...props} />);
+    const input = screen.getByTestId('textfield-input');
+    fireEvent.change(input, { target: { value: 'my-new-username' } });
+    expect(screen.getByDisplayValue('my-new-username')).toBeInTheDocument();
+  });
+});
