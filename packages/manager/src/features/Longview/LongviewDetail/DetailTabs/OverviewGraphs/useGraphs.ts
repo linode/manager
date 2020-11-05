fa414fdb6073d041a3998bb537dbb693f0b55336
Jared Kobos <jaredkobos@gmail.com>
2020-11-05 15:27:37 -0500

    Fix polling in useGraphs (no request from inactive tab)

--- packages/manager/src/features/Longview/LongviewDetail/DetailTabs/OverviewGraphs/useGraphs.ts
+++ packages/manager/src/features/Longview/LongviewDetail/DetailTabs/OverviewGraphs/useGraphs.ts
@@ -8,57 +8,62 @@ export const useGraphs = (
   start: number,
   end: number
 ) => {
-  let mounted = true;
-  let requestInterval: NodeJS.Timeout;
+  const requestInterval = React.useRef<number>(0);
+  const mounted = React.useRef(false);
 
   const [data, setData] = React.useState<Partial<AllData>>({});
   const [loading, setLoading] = React.useState<boolean>(false);
   const [error, setError] = React.useState<string | undefined>();
-  const request = (isLoading: boolean = true) => {
-    if (!mounted) {
-      return;
-    }
-    if (!start || !end || !clientAPIKey) {
-      return;
-    }
-    if (isLoading) {
-      setLoading(true);
-      setData({});
-    }
-    return getValues(clientAPIKey, {
-      fields: requestFields,
-      start,
-      end
-    })
-      .then(response => {
-        if (mounted) {
-          setLoading(false);
-          setError(undefined);
-          setData(response.DATA);
-        }
+  const request = React.useCallback(
+    (isLoading: boolean = true) => {
+      if (!mounted) {
+        return;
+      }
+      if (!start || !end || !clientAPIKey) {
+        return;
+      }
+      if (isLoading) {
+        setLoading(true);
+        setData({});
+      }
+      return getValues(clientAPIKey, {
+        fields: requestFields,
+        start,
+        end
       })
-      .catch(e => {
-        if (mounted) {
-          setLoading(false);
-          setError(e.NOTIFICATIONS?.[0]?.TEXT ?? 'Unable to retrieve data.');
-        }
-      });
-  };
+        .then(response => {
+          if (mounted.current) {
+            setLoading(false);
+            setError(undefined);
+            setData(response.DATA);
+          }
+        })
+        .catch(e => {
+          if (mounted.current) {
+            setLoading(false);
+            setError(e.NOTIFICATIONS?.[0]?.TEXT ?? 'Unable to retrieve data.');
+          }
+        });
+    },
+    [start, end, clientAPIKey, requestFields]
+  );
 
   // Poll the data. Reset/refresh the interval if the clientAPIKey changes.
   React.useEffect(() => {
     if (!clientAPIKey) {
       return;
     }
-    requestInterval = setInterval(() => {
-      request(false);
+    requestInterval.current = window.setInterval(() => {
+      if (document.visibilityState === 'visible') {
+        request(false);
+      }
     }, 10000);
 
     return () => {
-      mounted = false;
-      clearInterval(requestInterval);
+      mounted.current = false;
+      clearInterval(requestInterval.current);
     };
-  }, [clientAPIKey]);
+  }, [clientAPIKey, request]);
 
   return { error, data, loading, request };
 };
