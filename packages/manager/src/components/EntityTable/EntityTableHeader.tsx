e62393308f4f8b09da6f578de2cba217a6ec723f
Kayla Wilkins <kwilkins@linode.com>
2020-11-05 10:23:00 -0500

    some cleanup to file via new cell subcomponents (pr feedback)

--- packages/manager/src/components/EntityTable/EntityTableHeader.tsx
+++ packages/manager/src/components/EntityTable/EntityTableHeader.tsx
@@ -34,12 +34,56 @@ interface Props extends Omit<OrderByProps, 'data'> {
   headers: HeaderCell[];
 }
 
+interface SortCellProps extends Omit<Props, 'headers'> {
+  thisCell: HeaderCell;
+}
+
+interface NormalCellProps {
+  thisCell: HeaderCell;
+}
+
 export const EntityTableHeader: React.FC<Props> = props => {
   const { headers, handleOrderChange, order, orderBy } = props;
   const classes = useStyles();
   const flags = useFlags();
 
   const SortCell = flags.cmr ? TableSortCell_CMR : TableSortCell;
+
+  const _SortCell: React.FC<SortCellProps> = props => {
+    const { orderBy, order, thisCell, handleOrderChange } = props;
+    return (
+      <SortCell
+        active={orderBy === thisCell.dataColumn}
+        label={thisCell.dataColumn}
+        direction={order}
+        handleClick={handleOrderChange}
+        style={{ width: `${thisCell.widthPercent}%` }}
+        data-testid={`${thisCell.label}-header-cell`}
+      >
+        {thisCell.label}
+      </SortCell>
+    );
+  };
+
+  const _NormalCell: React.FC<NormalCellProps> = props => {
+    const { thisCell } = props;
+    return (
+      <TableCell
+        data-testid={`${thisCell.label}-header-cell`}
+        className={classes.thead}
+        style={{ width: `${thisCell.widthPercent}%` }}
+      >
+        <span
+          className={
+            thisCell.visuallyHidden ? classes.hiddenHeaderCell : undefined
+          }
+        >
+          {thisCell.label}
+        </span>
+      </TableCell>
+    );
+  };
+
   return (
     <TableHead>
       <TableRow>
@@ -47,98 +91,42 @@ export const EntityTableHeader: React.FC<Props> = props => {
           thisCell.sortable ? (
             thisCell.hideOnTablet ? (
               <Hidden smDown key={thisCell.dataColumn}>
-                <SortCell
-                  active={orderBy === thisCell.dataColumn}
-                  label={thisCell.dataColumn}
-                  direction={order}
-                  handleClick={handleOrderChange}
-                  style={{ width: `${thisCell.widthPercent}%` }}
-                  data-testid={`${thisCell.label}-header-cell`}
-                >
-                  {thisCell.label}
-                </SortCell>
+                <_SortCell
+                  thisCell={thisCell}
+                  order={order}
+                  orderBy={orderBy}
+                  handleOrderChange={handleOrderChange}
+                />
               </Hidden>
             ) : thisCell.hideOnMobile ? (
               <Hidden xsDown key={thisCell.dataColumn}>
-                <SortCell
-                  active={orderBy === thisCell.dataColumn}
-                  label={thisCell.dataColumn}
-                  direction={order}
-                  handleClick={handleOrderChange}
-                  style={{ width: `${thisCell.widthPercent}%` }}
-                  data-testid={`${thisCell.label}-header-cell`}
-                >
-                  {thisCell.label}
-                </SortCell>
+                <_SortCell
+                  thisCell={thisCell}
+                  order={order}
+                  orderBy={orderBy}
+                  handleOrderChange={handleOrderChange}
+                />
               </Hidden>
             ) : (
-              <SortCell
-                key={thisCell.dataColumn}
-                active={orderBy === thisCell.dataColumn}
-                label={thisCell.dataColumn}
-                direction={order}
-                handleClick={handleOrderChange}
-                style={{ width: `${thisCell.widthPercent}%` }}
-                data-testid={`${thisCell.label}-header-cell`}
-              >
-                {thisCell.label}
-              </SortCell>
+              <_SortCell
+                thisCell={thisCell}
+                order={order}
+                orderBy={orderBy}
+                handleOrderChange={handleOrderChange}
+              />
             )
           ) : (
             [
               thisCell.hideOnTablet ? (
                 <Hidden smDown key={thisCell.dataColumn}>
-                  <TableCell
-                    data-testid={`${thisCell.label}-header-cell`}
-                    className={classes.thead}
-                    style={{ width: `${thisCell.widthPercent}%` }}
-                  >
-                    <span
-                      className={
-                        thisCell.visuallyHidden
-                          ? classes.hiddenHeaderCell
-                          : undefined
-                      }
-                    >
-                      {thisCell.label}
-                    </span>
-                  </TableCell>
+                  <_NormalCell thisCell={thisCell} />
                 </Hidden>
               ) : thisCell.hideOnMobile ? (
                 <Hidden xsDown key={thisCell.dataColumn}>
-                  <TableCell
-                    data-testid={`${thisCell.label}-header-cell`}
-                    className={classes.thead}
-                    style={{ width: `${thisCell.widthPercent}%` }}
-                  >
-                    <span
-                      className={
-                        thisCell.visuallyHidden
-                          ? classes.hiddenHeaderCell
-                          : undefined
-                      }
-                    >
-                      {thisCell.label}
-                    </span>
-                  </TableCell>
+                  <_NormalCell thisCell={thisCell} />
                 </Hidden>
               ) : (
-                <TableCell
-                  key={thisCell.dataColumn}
-                  data-testid={`${thisCell.label}-header-cell`}
-                  className={classes.thead}
-                  style={{ width: `${thisCell.widthPercent}%` }}
-                >
-                  <span
-                    className={
-                      thisCell.visuallyHidden
-                        ? classes.hiddenHeaderCell
-                        : undefined
-                    }
-                  >
-                    {thisCell.label}
-                  </span>
-                </TableCell>
+                <_NormalCell thisCell={thisCell} />
               )
             ]
           )
