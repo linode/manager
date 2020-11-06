ec7f1421d9c2ecd083369b0aa10a91f2b61f8ce4
John Callahan <johnwcallahan@gmail.com>
2020-11-06 15:18:59 -0500

    M3-4479 Object ACLs (#7086)
    Adds ACL selection to the Object Details drawer.
    
    - Two new methods for the JS Client.
    - GET the Object's ACL when the drawer is opened.
    - Allow the user the make an ACL selection.
    - If they've selected "Public Read/Write" display a confirmation modal upon clicking "Save".
    - Fix the EnhancedSelect mock to correctly account for the selected value
    
    I created a new component, ACLSelect, since we'll need almost identical components/logic for Buckets.

--- packages/manager/src/features/ObjectStorage/BucketDetail/ObjectDetailsDrawer.test.tsx
+++ packages/manager/src/features/ObjectStorage/BucketDetail/ObjectDetailsDrawer.test.tsx
@@ -1,28 +1,55 @@
+import { screen, waitFor } from '@testing-library/react';
 import * as React from 'react';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 import ObjectDetailsDrawer, { Props } from './ObjectDetailsDrawer';
+
+jest.mock('@linode/api-v4/lib/object-storage/objects', () => {
+  return {
+    getObjectACL: jest.fn().mockResolvedValue({
+      acl: 'public-read',
+      acl_xml: 'long xml string'
+    }),
+    updateObjectACL: jest.fn().mockResolvedValue({})
+  };
+});
+
+jest.mock('src/components/EnhancedSelect/Select');
 
 const props: Props = {
   onClose: jest.fn(),
   open: true,
   lastModified: '2019-12-31T23:59:59Z',
-  name: 'my-image.png',
+  displayName: 'my-image.png',
+  name: 'my-dir/my-image.png',
   size: 12345,
-  url: 'https://my-bucket.cluster-id.linodeobjects.com/my-image.png'
+  url: 'https://my-bucket.cluster-id.linodeobjects.com/my-image.png',
+  bucketName: 'my-bucket',
+  clusterId: 'cluster-id'
 };
 
 describe('ObjectDetailsDrawer', () => {
-  it('renders formatted size, formatted last modified, truncated URL', () => {
-    const { getByText } = renderWithTheme(<ObjectDetailsDrawer {...props} />);
-    getByText('12.1 KB');
-    getByText(/^Last modified: 2019-12-31/);
-    getByText(/^https:\/\/my-bucket/);
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('renders formatted size, formatted last modified, truncated URL', async () => {
+    renderWithTheme(<ObjectDetailsDrawer {...props} />);
+    await waitFor(() =>
+      expect(screen.getByText('12.1 KB')).toBeInTheDocument()
+    );
+    await waitFor(() =>
+      expect(screen.getByText(/^Last modified: 2019-12-31/)).toBeInTheDocument()
+    );
+    await waitFor(() =>
+      expect(screen.getByText(/^https:\/\/my-bucket/)).toBeInTheDocument()
+    );
   });
 
-  it("doesn't show last modified if the the time is invalid", () => {
-    const { queryByTestId } = renderWithTheme(
+  it("doesn't show last modified if the the time is invalid", async () => {
+    renderWithTheme(
       <ObjectDetailsDrawer {...props} lastModified="INVALID DATE" />
     );
-    expect(queryByTestId('lastModified')).not.toBeInTheDocument();
+    await waitFor(() =>
+      expect(screen.queryByTestId('lastModified')).not.toBeInTheDocument()
+    );
   });
 });
