6e816abb02573fa56f79d287404037c4e09161a1
Jared <jaredkobos@gmail.com>
2020-11-12 15:13:04 -0500

    M3-4655 Change: Handle warnings after a successful payment. (#7101)
    * Change: Handle warnings after a successful payment.
    
    The API recently added an optional "warnings" field to the envelope
    for responses. It's currently only used when making a payment (for
    a rare edge case where jailbreaking fails even when the payment succeeds).
    
    This PR adds that to the appropriate types, and displays any warnings
    to the user in the payment drawer.
    
    Note: we've had some feedback that leaving the drawer open after a successful
    payment is confusing and has led to multiple payments. In a separate PR, we can
    move all of this up a level to the billing info page.
    
    * Clear warning on drawer open
    
    * Review feedback: safe access and make regex matching consistent
    
    * Add unit tests

--- packages/manager/src/features/Billing/BillingPanels/BillingSummary/PaymentDrawer/PaymentDrawer.test.tsx
+++ packages/manager/src/features/Billing/BillingPanels/BillingSummary/PaymentDrawer/PaymentDrawer.test.tsx
@@ -1,6 +1,27 @@
-import { getMinimumPayment } from './PaymentDrawer';
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { paymentFactory } from 'src/factories/billing';
+import { rest, server } from 'src/mocks/testServer';
+import PaymentDrawer, {
+  CombinedProps,
+  getMinimumPayment
+} from './PaymentDrawer';
+import { wrapWithTheme } from 'src/utilities/testHelpers';
 
 import { isAllowedUSDAmount, shouldEnablePaypalButton } from './Paypal';
+
+const props: CombinedProps = {
+  open: true,
+  onClose: jest.fn(),
+  accountLoading: false,
+  balance: 50,
+  lastFour: '9999',
+  expiry: '',
+  requestAccount: jest.fn(),
+  updateAccount: jest.fn(),
+  saveCreditCard: jest.fn()
+};
 
 describe('Make a Payment Panel', () => {
   it('should return false for invalid USD amount', () => {
@@ -40,6 +61,34 @@ describe('Make a Payment Panel', () => {
 
     it('should return 5 if the balance due is greater than 5', () => {
       expect(getMinimumPayment(100000)).toBe('5.00');
+    });
+  });
+
+  describe('Jailbreak warnings', () => {
+    it('should display a jailbreak warning if returned from the API', async () => {
+      render(wrapWithTheme(<PaymentDrawer {...props} />));
+      userEvent.click(screen.getByText(/pay now/i));
+      userEvent.click(screen.getByTestId('credit-card-submit'));
+      expect(
+        await screen.findByText(/your payment has been processed but/i)
+      ).toBeInTheDocument();
+    });
+
+    it('should not display a warning for a normal successful payment', async () => {
+      server.use(
+        rest.post('*/account/payments', (req, res, ctx) => {
+          return res(ctx.json(paymentFactory.build()));
+        })
+      );
+      render(wrapWithTheme(<PaymentDrawer {...props} />));
+      userEvent.click(screen.getByText(/pay now/i));
+      userEvent.click(screen.getByTestId('credit-card-submit'));
+      expect(
+        await screen.findByText(/submitted successfully/i)
+      ).toBeInTheDocument();
+      expect(
+        screen.queryByText(/your payment has been processed but/i)
+      ).not.toBeInTheDocument();
     });
   });
 });
