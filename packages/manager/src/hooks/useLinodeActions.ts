391e1bfaafae77dc434f65aabbcc1a6ee092e819
Jared Kobos <jaredkobos@gmail.com>
2020-11-18 12:55:06 -0500

    Hotfix: separate linode actions from useLinodes hook
    We were using const { updateLinode } = useLinodes() in several places.
    Because of hooks weirdness and rerendering, this was causing an enormous
    amount of processing (we were mapping through Linodes to extend them
    ~10 times per each actual Linode). By separating the actions to a separate
    file, we can use this hook from consumers without triggering any of the state-based
    Redux logic.

---
+++ packages/manager/src/hooks/useLinodeActions.ts
@@ -0,0 +1,41 @@
+import { Linode } from '@linode/api-v4/lib/linodes/types';
+import { useDispatch } from 'react-redux';
+import {
+  deleteLinode as _deleteLinode,
+  getLinode as _getLinode,
+  requestLinodes as _requestLinodes,
+  updateLinode as _updateLinode
+} from 'src/store/linodes/linode.requests';
+import { UpdateLinodeParams } from 'src/store/linodes/linodes.actions';
+import { Dispatch } from './types';
+
+export interface LinodesProps {
+  requestLinodes: () => Promise<Linode[]>;
+  getLinode: (linodeId: number) => Promise<Linode>;
+  deleteLinode: (linodeId: number) => Promise<{}>;
+  updateLinode: (params: UpdateLinodeParams) => Promise<Linode>;
+}
+
+export const useLinodes = (): LinodesProps => {
+  const dispatch: Dispatch = useDispatch();
+
+  const requestLinodes = () =>
+    dispatch(_requestLinodes({})).then(response => response.data);
+
+  const getLinode = (linodeId: number) => dispatch(_getLinode({ linodeId }));
+
+  const deleteLinode = (linodeId: number) =>
+    dispatch(_deleteLinode({ linodeId }));
+
+  const updateLinode = (params: UpdateLinodeParams) =>
+    dispatch(_updateLinode(params));
+
+  return {
+    requestLinodes,
+    getLinode,
+    deleteLinode,
+    updateLinode
+  };
+};
+
+export default useLinodes;
