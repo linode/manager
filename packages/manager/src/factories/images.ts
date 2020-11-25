17cf67f68625c58fc3b4aff60885c2f82179b110
Jared <jaredkobos@gmail.com>
2020-11-25 09:06:18 -0500

    Change: Delete mock image data (#7153)
    * Remove __data__/images and replace with usage of factories/images
    
    * Fix test
    
    * Fix Image factories

--- packages/manager/src/factories/images.ts
+++ packages/manager/src/factories/images.ts
@@ -3,10 +3,10 @@ import { Image } from '@linode/api-v4/lib/images/types';
 
 export const imageFactory = Factory.Sync.makeFactory<Image>({
   id: Factory.each(id => `private/${id}`),
-  label: Factory.each(i => `event-entity-${i}`),
+  label: Factory.each(i => `image-${i}`),
   description: 'An image',
   deprecated: false,
-  created: new Date().toString(),
+  created: new Date().toISOString(),
   created_by: 'prod-test-004',
   is_public: false,
   size: 1500,
