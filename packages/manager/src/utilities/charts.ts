bd7a9a0a4d83f55f166ef9f497b978ab6c942410
Jared Kobos <jaredkobos@gmail.com>
2020-11-30 11:33:30 -0500

    post-merge fixes

--- packages/manager/src/utilities/charts.ts
+++ packages/manager/src/utilities/charts.ts
@@ -1,10 +1,9 @@
 // Here i do `import * as Chart` instead of `import Chart` because of issues with jest
 // jest not be able to access the namespce variable correctly
-import Chart from 'chart.js';
+import { Chart } from 'chart.js';
 
 export const setUpCharts = () => {
   Chart.defaults.fontFamily = '"LatoWeb", sans-serif';
   Chart.defaults.fontStyle = '700';
   Chart.defaults.fontSize = 13;
 };
-
