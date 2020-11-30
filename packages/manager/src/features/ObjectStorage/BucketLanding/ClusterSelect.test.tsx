2a99b885c7b04be0335964e92dc8e64b84f46f21
John Callahan <johnwcallahan@gmail.com>
2020-11-30 11:47:10 -0500

    Mock data improvements

--- packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.test.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.test.tsx
@@ -1,14 +1,13 @@
-import { objectStorageClusters } from 'src/__data__/objectStorageClusters';
-import { regions } from 'src/__data__/regionsData';
+import { objectStorageClusterFactory } from 'src/factories';
 import { objectStorageClusterToExtendedRegion } from './ClusterSelect';
+
+const regions = require('src/cachedData/regions.json');
 
 describe('objectStorageClusterToExtendedRegion', () => {
   it('transforms a list of OBJ clusters to a list of extended regions', () => {
-    const result = objectStorageClusterToExtendedRegion(
-      objectStorageClusters,
-      regions
-    );
-    expect(result.length).toBe(objectStorageClusters.length);
+    const clusters = objectStorageClusterFactory.buildList(2);
+    const result = objectStorageClusterToExtendedRegion(clusters, regions.data);
+    expect(result.length).toBe(clusters.length);
     result.forEach(thisExtendedRegion => {
       expect(thisExtendedRegion).toHaveProperty('country');
       expect(thisExtendedRegion).toHaveProperty('display');
