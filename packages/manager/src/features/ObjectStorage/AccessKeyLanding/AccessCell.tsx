8a06c6011d63876ea495e522d0ec47d855d9df7a
Jared Kobos <jaredkobos@gmail.com>
2020-12-01 14:39:45 -0500

    M3-4582 Change: Use a shared component for view-only permissions
    For OBJ access tokens and API access tokens, instead of
    showing a disabled version of the scopes form
    (with a bunch of radio buttons), if a user is in view-only
    mode instead use check marks for selected permissions.
    I opted to go with a shared (new) component for this,
    though it isn't clear if it's worth it in this case.
    
    Use check marks instead of disabled/greyed table cells
    
    Refactor out an AccessCell component
    
    Fix aria label

---
+++ packages/manager/src/features/ObjectStorage/AccessKeyLanding/AccessCell.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Check from 'src/assets/icons/monitor-ok.svg';
+import Radio from 'src/components/Radio';
+import { makeStyles } from 'src/components/core/styles';
+
+const useStyles = makeStyles(() => ({
+  checkIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '& svg': {
+      height: 25,
+      width: 25
+    }
+  }
+}));
+
+interface RadioButton extends HTMLInputElement {
+  name: string;
+}
+
+interface AccessCellProps {
+  disabled: boolean;
+  active: boolean;
+  viewOnly: boolean;
+  scope: string;
+  scopeDisplay: string;
+  onChange: (e: React.SyntheticEvent<RadioButton>) => void;
+}
+
+export const AccessCell: React.FC<AccessCellProps> = props => {
+  const { active, disabled, onChange, scope, scopeDisplay, viewOnly } = props;
+  const classes = useStyles();
+
+  if (viewOnly) {
+    if (!active) {
+      return null;
+    }
+    return (
+      <span className={classes.checkIcon}>
+        <Check />
+      </span>
+    );
+  }
+
+  return (
+    <Radio
+      name={scopeDisplay}
+      disabled={disabled}
+      checked={active}
+      value={scope}
+      onChange={onChange}
+      data-testid={`perm-${scopeDisplay}-radio`}
+      inputProps={{
+        'aria-label': `${scope} for ${scopeDisplay}`
+      }}
+    />
+  );
+};
+
+export default React.memo(AccessCell);
