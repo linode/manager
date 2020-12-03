92456a46ba50dcb0857ad7f3aba64e82e9f0ae87
Holden Morris <morrisholdenx@gmail.com>
2020-12-03 15:40:29 -0500

    update description v4

--- packages/manager/src/features/OneClickApps/FakeSpec.ts
+++ packages/manager/src/features/OneClickApps/FakeSpec.ts
@@ -596,8 +596,8 @@ export const oneClickApps: OCA[] = [
   },
   {
     name: 'Secure Your Server',
-    description: `Save time on securing your Linode a by deploying an instance preconfigured with some basic security best practices: limited user account access, hardened SSH, and Fail2Ban for SSH Login Protection.',
-    summary: `Harden your Linode before you deploy with the Secure Your Server One-Click App`,
+     description: `Save time on securing your Linode a by deploying an instance preconfigured with some basic security best practices: limited user account access, hardened SSH, and Fail2Ban for SSH Login Protection.`,
+    summary: `Harden your Linode before you deploy with the Secure Your Server One-Click App.`,
     related_guides: [
       {
         title: 'Securing your Server',
