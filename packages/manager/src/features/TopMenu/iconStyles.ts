7a694615c28cddee5eed263d103251c25ab232a7
Jared Kobos <jaredkobos@gmail.com>
2020-12-07 15:35:54 -0500

    CMR fixes
    - Fix the badge icon for notifications in the top menu so that
    it's dynamic and the text doesn't overflow the badge for any number
    of events
    
    - Add the username inline next to the user menu (hidden on mobile).
    - Remove the display-username-on-hover logic

--- packages/manager/src/features/TopMenu/iconStyles.ts
+++ packages/manager/src/features/TopMenu/iconStyles.ts
@@ -20,15 +20,16 @@ export const useStyles = makeStyles((theme: Theme) => ({
   badge: {
     display: 'flex',
     position: 'absolute',
+    padding: theme.spacing(0.25),
     top: 5,
-    left: 23,
+    left: 20,
     alignItems: 'center',
     justifyContent: 'center',
     color: 'white',
     backgroundColor: theme.color.green,
-    fontSize: '0.75rem',
+    fontSize: '0.72rem',
     borderRadius: '50%',
-    height: 12,
-    width: 12
+    minWidth: 16,
+    minHeight: 16
   }
 }));
