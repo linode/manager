c5bb3683bd2c0f68e36f258a6d26f896ad712dc2
John Callahan <johnwcallahan@gmail.com>
2020-12-10 10:01:08 -0500

    Override CMR flag so that it's always true (#7165)

--- packages/manager/src/containers/withFeatureFlagConsumer.container.ts
+++ packages/manager/src/containers/withFeatureFlagConsumer.container.ts
@@ -28,7 +28,8 @@ export const withFeatureFlagConsumer = (
         flags: {
           // Real LD flags from `withLDConsumer()`.
           ...this.props.flags,
-          // @todo: REMOVE BEFORE MERGE
+          // !!! Override the CMR flag so that it's officially "released" !!!!
+          // @todo: clean up all conditional logic based on this flag.
           cmr: true,
           // Mock flags from Redux.
           ...this.props.mockFlags
