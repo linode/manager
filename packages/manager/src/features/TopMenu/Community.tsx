cd11e881a5e41399a996ba38a385e940dd9fa645
Jared Kobos <jaredkobos@gmail.com>
2020-12-14 13:35:43 -0500

    Keep original community.svg icon

--- packages/manager/src/features/TopMenu/Community.tsx
+++ packages/manager/src/features/TopMenu/Community.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Community from 'src/assets/icons/community.svg';
+import Community from 'src/assets/icons/community_nav.svg';
 import Link from 'src/components/Link';
 import { useStyles } from './iconStyles';
 
