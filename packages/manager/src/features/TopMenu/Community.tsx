4825bb7fb1ae3e1c26a8319b4cf44b6561ad60a0
John Callahan <johnwcallahan@gmail.com>
2020-12-16 11:10:30 -0500

    Add className prop to Community component

--- packages/manager/src/features/TopMenu/Community.tsx
+++ packages/manager/src/features/TopMenu/Community.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
+import * as classNames from 'classnames';
 import Community from 'src/assets/icons/community_nav.svg';
 import Link from 'src/components/Link';
 import { useStyles } from './iconStyles';
 
-export const Help: React.FC<{}> = _ => {
+export const Help: React.FC<{ className?: string }> = ({ className }) => {
   const classes = useStyles();
   return (
     <Link
       aria-label="Link to Linode Community site"
-      className={classes.icon}
+      className={classNames(className, { [classes.icon]: true })}
       to="https://linode.com/community"
     >
       <Community />
