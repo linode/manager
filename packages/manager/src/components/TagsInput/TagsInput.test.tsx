eff8d0f5c07cb7e823214c7f04f8a70611c4ba3a
Jared Kobos <jaredkobos@gmail.com>
2020-12-23 11:54:42 -0500

    Add missing prop to LinodeMultiSelect.test

--- packages/manager/src/components/TagsInput/TagsInput.test.tsx
+++ packages/manager/src/components/TagsInput/TagsInput.test.tsx
@@ -15,7 +15,7 @@ describe('TagsInput', () => {
   it('sets account tags based on API request', async () => {
     const { getByTestId, queryAllByTestId } = renderWithTheme(
       <TagsInput
-        value={'mockvalue' as any} // We're mocking this component so ignore the Props typing
+        value={['mockvalue'] as any} // We're mocking this component so ignore the Props typing
         onChange={onChange}
       />
     );
