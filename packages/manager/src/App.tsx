4fe9a685142d126e723d9375acca3ae3e0e8ab7f
John Callahan <johnwcallahan@gmail.com>
2021-01-04 11:25:41 -0500

    POC: "Go To" functionality (#7262)
    I was inspired by @Jskobos's keyboard shortcut for light/dark mode toggle, so I started this on FTFF. This one is "CMD+K" and brings up a "Go To" component for quick navigation around the app.

--- packages/manager/src/App.tsx
+++ packages/manager/src/App.tsx
@@ -31,6 +31,7 @@ import { MapState } from './store/types';
 
 import IdentifyUser from './IdentifyUser';
 import MainContent from './MainContent';
+import GoTo from './GoTo';
 
 interface Props {
   toggleTheme: () => void;
@@ -43,6 +44,7 @@ interface State {
   menuOpen: boolean;
   welcomeBanner: boolean;
   hasError: boolean;
+  goToOpen: boolean;
 }
 
 type CombinedProps = Props &
@@ -59,7 +61,8 @@ export class App extends React.Component<CombinedProps, State> {
   state: State = {
     menuOpen: false,
     welcomeBanner: false,
-    hasError: false
+    hasError: false,
+    goToOpen: false
   };
 
   componentDidCatch() {
@@ -84,6 +87,13 @@ export class App extends React.Component<CombinedProps, State> {
     document.addEventListener('keydown', (event: KeyboardEvent) => {
       if (event.ctrlKey && event.shiftKey && event.key === 'D') {
         this.props.toggleTheme();
+      }
+
+      if (event.metaKey && event.key === 'k') {
+        this.setState(prevState => ({
+          ...prevState,
+          goToOpen: !prevState.goToOpen
+        }));
       }
     });
 
@@ -118,6 +128,10 @@ export class App extends React.Component<CombinedProps, State> {
         }
       });
   }
+
+  goToClose = () => {
+    this.setState({ goToOpen: false });
+  };
 
   render() {
     const { hasError } = this.state;
@@ -187,6 +201,7 @@ export class App extends React.Component<CombinedProps, State> {
             Opens an external site in a new window
           </span>
         </div>
+        <GoTo open={this.state.goToOpen} onClose={this.goToClose} />
         {/** Update the LD client with the user's id as soon as we know it */}
         <IdentifyUser
           userID={userId}
