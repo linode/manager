608a25fdc9b45c69951fcfa856781dc5ae8fc1ec
Jared Kobos <jaredkobos@gmail.com>
2021-01-19 11:17:28 -0500

    Add regions query

---
+++ packages/manager/src/queries/regions.ts
@@ -0,0 +1,14 @@
+import { getRegions, Region } from '@linode/api-v4/lib/regions';
+import { APIError } from '@linode/api-v4/lib/types';
+import { useQuery } from 'react-query';
+import { queryPresets } from './base';
+
+import { data } from 'src/cachedData/regions.json';
+
+export const _getRegions = () => getRegions().then(({ data }) => data);
+
+export const useManagedSSHKey = () =>
+  useQuery<Region[], APIError[]>('managedSSHKey', _getRegions, {
+    ...queryPresets.longLived,
+    placeholderData: data as Region[]
+  });
