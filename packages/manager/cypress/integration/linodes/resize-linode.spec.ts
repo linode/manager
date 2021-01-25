7524aa02460ae6ba8e57dfe90ddc30805d933146
ndukatz <ndukatz@linode.com>
2021-01-25 15:04:54 -0500

    cleaning up

--- packages/manager/cypress/integration/linodes/resize-linode.spec.ts
+++ packages/manager/cypress/integration/linodes/resize-linode.spec.ts
@@ -1,13 +1,3 @@
-// create linode with helper
-// go to http://localhost:3000/linodes/<linodeID>?resize=true
-// select contains text 'Linode 2GB' id="g6-standard-1"
-// type linode name into data-testid="textfield-input" or id="linode-label" or both
-// click button data-qa-resize="true" or contains 'Resize'
-// wait for POST "https://api.linode.com/v4/linode/instances/<linodeID>/resize"
-// look for text Your Linode will soon be automatically powered off, migrated, and restored to its previous state (booted or powered off).
-// look for text 'OFFLINE'
-// wait for 'RESIZING'?
-
 import { createLinode } from '../../support/api/linodes';
 import { containsVisible, fbtVisible, getClick } from '../../support/helpers';
 
