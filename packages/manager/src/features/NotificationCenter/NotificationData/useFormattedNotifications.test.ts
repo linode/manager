44bdf864ccf5e94fee01b9f3b8ee973e82050c84
Dajahi Wiley <32860776+DevDW@users.noreply.github.com>
2021-02-10 10:15:54 -0500

    M3-4839: Notifications section in Notifications drawer follow-up (#7386)
    feat(Notifications drawer): format notifications as intended from mockups; add some unit tests

---
+++ packages/manager/src/features/NotificationCenter/NotificationData/useFormattedNotifications.test.ts
@@ -0,0 +1,49 @@
+import { adjustSeverity } from './useFormattedNotifications';
+import { notificationFactory } from 'src/factories';
+
+// Migration types are considered as types of maintenance notifications.
+const migrationScheduled = notificationFactory.build({
+  type: 'migration_scheduled',
+  entity: { id: 0, type: 'linode', label: 'linode-0' },
+  severity: 'major'
+});
+
+const migrationPending = notificationFactory.build({
+  type: 'migration_pending',
+  entity: { id: 1, type: 'linode', label: 'linode-1' },
+  severity: 'critical'
+});
+
+const maintenanceNotice = notificationFactory.build();
+const maintenanceWithLowSeverity = notificationFactory.build({
+  type: 'migration_pending',
+  entity: { id: 4, type: 'linode', label: 'linode-4' },
+  severity: 'minor'
+});
+
+const emailBounce = notificationFactory.build({
+  type: 'billing_email_bounce',
+  entity: null,
+  when: null,
+  message: 'We are unable to send emails to your billing email address!',
+  label: 'We are unable to send emails to your billing email address!',
+  severity: 'major',
+  until: null,
+  body: null
+});
+
+describe('Notification Severity', () => {
+  describe('adjustSeverity helper', () => {
+    it("should return 'major' severity for all maintenance types", () => {
+      expect(adjustSeverity(migrationScheduled)).toMatch('major');
+      expect(adjustSeverity(migrationPending)).toMatch('major');
+      expect(adjustSeverity(migrationPending)).toMatch('major');
+      expect(adjustSeverity(maintenanceNotice)).toMatch('major');
+      expect(adjustSeverity(maintenanceWithLowSeverity)).toMatch('major');
+    });
+
+    it('should return severity as returned by the API with no modification for non-maintenance types', () => {
+      expect(adjustSeverity(emailBounce)).toMatch(emailBounce.severity);
+    });
+  });
+});
