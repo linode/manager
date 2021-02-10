b835c3049f696a85d11571836bd24948411d3414
Jared <jaredkobos@gmail.com>
2021-02-10 16:48:54 -0500

    M3-4784 Add: port presets in FirewallRuleDrawer (#7383)
    * WIP
    
    * Add helpers for converting strings to port presets and vice versa
    
    * add state variables for preset ports dropdown
    
    * Working ports flow; ready for cleanup and checking
    
    * Handle undefined ports better
    
    * Handle ICMP better
    
    * Handle All Ports explicitly
    
    * Fix tests
    
    * Feedback 1: clear ports inputs on drawer open; rename text to Custom Port Range
    
    * Feedback 2: handle all ports correctly when opening drawer
    
    * Feedback 3: only show All in the multi-select if it has been selected
    
    * Feedback 4: reorder options and reduce copy font size on rules landing

--- packages/manager/src/features/Firewalls/FirewallDetail/Rules/shared.test.ts
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/shared.test.ts
@@ -1,60 +1,78 @@
-import { parseFirewallRuleError } from './shared';
+import { parseFirewallRuleError, sortPortString } from './shared';
 
 const generateError = (field: string) => ({
   reason: 'An error occurred.',
   field
 });
 
-describe('', () => {
-  it('returns `null` when no field', () => {
-    expect(parseFirewallRuleError({ reason: 'Error!' })).toBeNull();
+describe('shared Firewall rule utilities', () => {
+  describe('parseFirewallRuleError', () => {
+    it('returns `null` when no field', () => {
+      expect(parseFirewallRuleError({ reason: 'Error!' })).toBeNull();
+    });
+
+    const error = generateError('rules.inbound[0].ports');
+
+    it('includes the reason', () => {
+      expect(parseFirewallRuleError(error)).toHaveProperty(
+        'reason',
+        'An error occurred.'
+      );
+    });
+
+    it('parses the category', () => {
+      expect(
+        parseFirewallRuleError(generateError('rules.inbound[0].ports'))
+      ).toHaveProperty('category', 'inbound');
+      expect(
+        parseFirewallRuleError(generateError('rules.outbound[0].ports'))
+      ).toHaveProperty('category', 'outbound');
+    });
+
+    it('parses the rule index', () => {
+      expect(parseFirewallRuleError(error)).toHaveProperty('idx', 0);
+    });
+
+    it('parses the form field', () => {
+      expect(
+        parseFirewallRuleError(generateError('rules.inbound[0].ports'))
+      ).toHaveProperty('formField', 'ports');
+      expect(
+        parseFirewallRuleError(generateError('rules.inbound[0].protocol'))
+      ).toHaveProperty('formField', 'protocol');
+      expect(
+        parseFirewallRuleError(generateError('rules.inbound[0].addresses'))
+      ).toHaveProperty('formField', 'addresses');
+    });
+
+    it('parses IPs', () => {
+      const ipv4Error = generateError('rules.inbound[0].addresses.ipv4[1]');
+      const ipv4Result = parseFirewallRuleError(ipv4Error);
+      expect(ipv4Result).toHaveProperty('ip');
+      expect(ipv4Result!.ip).toHaveProperty('type', 'ipv4');
+      expect(ipv4Result!.ip).toHaveProperty('idx', 1);
+
+      const ipv6Error = generateError('rules.inbound[0].addresses.ipv6[1]');
+      const ipv6Result = parseFirewallRuleError(ipv6Error);
+      expect(ipv6Result).toHaveProperty('ip');
+      expect(ipv6Result!.ip).toHaveProperty('type', 'ipv6');
+      expect(ipv6Result!.ip).toHaveProperty('idx', 1);
+    });
   });
 
-  const error = generateError('rules.inbound[0].ports');
+  describe('sortPortString utility', () => {
+    it('should sort ports numerically', () => {
+      expect(sortPortString('80, 22')).toMatch('22, 80');
+      expect(sortPortString('443, 22, 80-81')).toMatch('22, 80-81, 443');
+    });
 
-  it('includes the reason', () => {
-    expect(parseFirewallRuleError(error)).toHaveProperty(
-      'reason',
-      'An error occurred.'
-    );
-  });
+    it('should handle whitespace variations', () => {
+      expect(sortPortString('443,22,80-81')).toMatch('22, 80-81, 443');
+    });
 
-  it('parses the category', () => {
-    expect(
-      parseFirewallRuleError(generateError('rules.inbound[0].ports'))
-    ).toHaveProperty('category', 'inbound');
-    expect(
-      parseFirewallRuleError(generateError('rules.outbound[0].ports'))
-    ).toHaveProperty('category', 'outbound');
-  });
-
-  it('parses the rule index', () => {
-    expect(parseFirewallRuleError(error)).toHaveProperty('idx', 0);
-  });
-
-  it('parses the form field', () => {
-    expect(
-      parseFirewallRuleError(generateError('rules.inbound[0].ports'))
-    ).toHaveProperty('formField', 'ports');
-    expect(
-      parseFirewallRuleError(generateError('rules.inbound[0].protocol'))
-    ).toHaveProperty('formField', 'protocol');
-    expect(
-      parseFirewallRuleError(generateError('rules.inbound[0].addresses'))
-    ).toHaveProperty('formField', 'addresses');
-  });
-
-  it('parses IPs', () => {
-    const ipv4Error = generateError('rules.inbound[0].addresses.ipv4[1]');
-    const ipv4Result = parseFirewallRuleError(ipv4Error);
-    expect(ipv4Result).toHaveProperty('ip');
-    expect(ipv4Result!.ip).toHaveProperty('type', 'ipv4');
-    expect(ipv4Result!.ip).toHaveProperty('idx', 1);
-
-    const ipv6Error = generateError('rules.inbound[0].addresses.ipv6[1]');
-    const ipv6Result = parseFirewallRuleError(ipv6Error);
-    expect(ipv6Result).toHaveProperty('ip');
-    expect(ipv6Result!.ip).toHaveProperty('type', 'ipv6');
-    expect(ipv6Result!.ip).toHaveProperty('idx', 1);
+    it('should return the string unaltered with bad input', () => {
+      expect(sortPortString('')).toBe('');
+      expect(sortPortString('33, XX-gibberish')).toMatch('33, XX-gibberish');
+    });
   });
 });
