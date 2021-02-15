cd4fed10bca3ee9719647c021b7985dc8cd98bd6
Jared Kobos <jaredkobos@gmail.com>
2021-02-15 14:51:43 -0500

    Finish modal layout more or less

--- packages/manager/src/features/EntityTransfers/EntityTransfersLanding/CreateTransferSuccessDialog.tsx
+++ packages/manager/src/features/EntityTransfers/EntityTransfersLanding/CreateTransferSuccessDialog.tsx
@@ -10,13 +10,21 @@ import { entityTransferFactory } from 'src/factories/entityTransfers';
 import { formatDate } from 'src/utilities/formatDate';
 
 const useStyles = makeStyles((theme: Theme) => ({
+  root: {},
   tokenInput: {
-    width: '100vw !important'
+    maxWidth: '100%'
   },
   copyButton: {
     marginTop: theme.spacing(),
-    marginBottom: theme.spacing(),
-    float: 'right'
+    maxWidth: 220,
+    alignSelf: 'flex-end'
+  },
+  text: {
+    marginBottom: theme.spacing()
+  },
+  inputSection: {
+    display: 'flex',
+    flexFlow: 'column nowrap'
   }
 }));
 
@@ -34,10 +42,10 @@ export const CreateTransferSuccessDialog: React.FC<Props> = props => {
   const transfer = _transfer;
 
   const draftEmail = `This token authorizes transfer of Linodes to you:\n
-   ${transfer.token}\n\t
+  ${transfer.token}\n\t
    1) Log in to your account on Linode Cloud Manager.\t
    2) Navigate to the Transfers tab on your Account page.\t
-   3) Copy and paste the token into the Enter Transfer Token field to view details and accept the transfer.\n
+   3) Copy and paste the token into the Enter Transfer Token field to view\n\t details and accept the transfer.\n
    If you do not have an account with Linode you will need to create one.\n
    This token will expire on ${formatDate(transfer.expiry, {
      format: 'HH:MM:S'
@@ -48,8 +56,9 @@ export const CreateTransferSuccessDialog: React.FC<Props> = props => {
       title="Token for Transfer"
       open={isOpen}
       onClose={onClose}
+      className={classes.root}
     >
-      <Typography>
+      <Typography className={classes.text}>
         This token authorizes the transfer of {600} Linodes.
       </Typography>
       <Typography>
@@ -57,37 +66,41 @@ export const CreateTransferSuccessDialog: React.FC<Props> = props => {
         secure delivery method. It make take up to an hour for the transfer to
         take effect once accepted by the recipient.
       </Typography>
-      <CopyableTextField
-        className={classes.tokenInput}
-        value={transfer.token}
-        label="Transfer Token"
-        hideIcon
-        fullWidth
-        aria-disabled
-      />
-      <Button
-        buttonType="secondary"
-        onClick={() => copy(transfer.token)}
-        className={classes.copyButton}
-      >
-        Copy Transfer Token
-      </Button>
-      <CopyableTextField
-        className={classes.tokenInput}
-        value={draftEmail}
-        label="Draft Email"
-        hideIcon
-        fullWidth
-        aria-disabled
-        multiline
-      />
-      <Button
-        buttonType="primary"
-        className={classes.copyButton}
-        onClick={() => copy(draftEmail)}
-      >
-        Copy Draft Email
-      </Button>
+      <div className={classes.inputSection}>
+        <CopyableTextField
+          className={classes.tokenInput}
+          value={transfer.token}
+          label="Transfer Token"
+          hideIcon
+          fullWidth
+          aria-disabled
+        />
+        <Button
+          buttonType="primary"
+          onClick={() => copy(transfer.token)}
+          className={classes.copyButton}
+        >
+          Copy Transfer Token
+        </Button>
+      </div>
+      <div className={classes.inputSection}>
+        <CopyableTextField
+          className={classes.tokenInput}
+          value={draftEmail}
+          label="Draft Email"
+          hideIcon
+          fullWidth
+          aria-disabled
+          multiline
+        />
+        <Button
+          buttonType="primary"
+          className={classes.copyButton}
+          onClick={() => copy(draftEmail)}
+        >
+          Copy Draft Email
+        </Button>
+      </div>
     </ConfirmationDialog>
   );
 };
