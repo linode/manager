a6301ab389846bc0c9e3d9b864462c3a9f581c5e
Jared Kobos <jaredkobos@gmail.com>
2021-02-16 11:07:27 -0500

    remove more vlans stuff

--- packages/manager/src/context/vlanContext.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { defaultContext, DialogContextProps } from './useDialogContext';
-
-export const vlanContext = React.createContext<DialogContextProps>(
-  defaultContext
-);
