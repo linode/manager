514ba54fb6c50fb8998fc513e91ebc61a8dbabe4
ndukatz <ndukatz@linode.com>
2021-02-24 21:43:55 -0500

    fixing stackscripts delete and addressing other pr feedback

--- packages/manager/cypress/integration/stackscripts/stackscripts.spec.ts
+++ packages/manager/cypress/integration/stackscripts/stackscripts.spec.ts
@@ -3,35 +3,19 @@ import {
   fbtClick,
   fbtVisible,
   getClick,
-  getVisible
+  getVisible,
 } from '../../support/helpers';
 import strings from '../../support/cypresshelpers';
 import { deleteAllTestStackscripts } from '../../support/api/stackscripts';
-import {
-  deleteAllTestLinodes,
-  deleteLinodeByLabel
-} from '../../support/api/linodes';
+import { deleteAllTestLinodes } from '../../support/api/linodes';
 
-const createLinodeBasic = () => {
+const createLinode = () => {
   const password = strings.randomPass();
   fbtClick('Select a Region');
   fbtClick('Newark, NJ');
   getClick('[id="g6-nanode-1"]');
   getClick('[id="root-password"]').type(password);
   getClick('[data-qa-deploy-linode]');
-};
-
-const createLinodeFromSs = (community: boolean = false) => {
-  fbtVisible('Create From:');
-  if (community) {
-    getClick('[id="serverid"]').type(strings.randomPass());
-    getClick('[id="service-secret"]').type(strings.randomPass());
-    fbtClick('Choose an image');
-    cy.get('[data-qa-option]')
-      .first()
-      .click();
-  }
-  createLinodeBasic();
 };
 
 describe('stackscripts', () => {
@@ -52,37 +36,12 @@ describe('stackscripts', () => {
     getVisible(`[data-qa-table-row="${ssLabel}"]`).within(() => {
       fbtClick('Deploy New Linode');
     });
-    createLinodeFromSs();
-    cy.wait('@createLinode', { timeout: 300000 }).then(linode => {
+    createLinode();
+    cy.wait('@createLinode', { timeout: 300000 }).then((linode) => {
       cy.visit(`/linodes/${linode.response?.body.id}/storage`);
       fbtVisible(linode.response?.body.label);
     });
-    cy.contains('RUNNING', { timeout: 300000 });
-    cy.reload();
-    fbtVisible(`${disk} Disk`);
     deleteAllTestLinodes();
     deleteAllTestStackscripts();
-  });
-
-  it('use community stackscript to deploy linode', () => {
-    cy.intercept('POST', `*/linode/instances`).as('createLinode');
-    cy.visitWithLogin('/stackscripts');
-    cy.url().should('endWith', '/account');
-    fbtClick('Community StackScripts');
-    cy.contains('Deploy New Linode')
-      .first()
-      .click();
-    createLinodeFromSs(true);
-    cy.wait('@createLinode', { timeout: 300000 }).then(linode => {
-      cy.visit(`/linodes/${linode.response?.body.id}/storage`);
-      fbtVisible(linode.response?.body.label);
-      cy.contains('RUNNING', { timeout: 300000 });
-      cy.reload();
-      cy.get('[aria-label="List of Disks"]')
-        .find('tr')
-        .should('have.length', 3);
-      deleteLinodeByLabel(linode.response?.body.label);
-      deleteAllTestStackscripts();
-    });
   });
 });
