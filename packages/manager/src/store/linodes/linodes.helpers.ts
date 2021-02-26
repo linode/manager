88a8c12187004ab9653d1c48aa2c458ea680045d
John Callahan <johnwcallahan@gmail.com>
2021-02-25 22:46:24 -0500

    Source maintenance from endpoint

--- packages/manager/src/store/linodes/linodes.helpers.ts
+++ packages/manager/src/store/linodes/linodes.helpers.ts
@@ -1,59 +1,33 @@
-import { Notification } from '@linode/api-v4/lib/account';
+import { AccountMaintenance } from '@linode/api-v4/lib/account';
 import { Linode } from '@linode/api-v4/lib/linodes';
 
-/**
- * _when_ is not guaranteed to exist if this is a maintenance notification
- *
- * _when_ could be in the past
- *
- * In the case of maintenance, _until_ is always going to be _null_,
- * so we cannot tell the user when their maintenance window will end. :(
- */
-
-type Type = 'reboot-scheduled' | 'migration-pending';
-
 export interface Maintenance {
-  type: Type;
   when: string | null;
-  until: string | null;
 }
 
 export interface LinodeWithMaintenance extends Linode {
   maintenance?: Maintenance | null;
 }
 
-export const addNotificationsToLinodes = (
-  notifications: Notification[],
+export const addMaintenanceToLinodes = (
+  accountMaintenance: AccountMaintenance[],
   linodes: Linode[]
 ): LinodeWithMaintenance[] => {
-  const maintenanceNotifications = notifications.filter(eachNotification => {
-    return eachNotification.type === 'maintenance';
-  });
+  return linodes.map((thisLinode) => {
+    const foundMaintenance = accountMaintenance.find((thisMaintenance) => {
+      return (
+        thisMaintenance.entity.type === 'linode' &&
+        thisMaintenance.entity.id === thisLinode.id
+      );
+    });
 
-  /** add the "maintenance" key to the Linode if we have one */
-  return linodes.map(eachLinode => {
-    const foundNotification = maintenanceNotifications.find(
-      eachNotification => {
-        return eachNotification.entity!.id === eachLinode.id;
-      }
-    );
-
-    return foundNotification
+    return foundMaintenance
       ? {
-          ...eachLinode,
+          ...thisLinode,
           maintenance: {
-            /**
-             * "when" and "until" are not guaranteed to exist
-             * if we have a maintenance notification
-             */
-            when: foundNotification.when,
-            until: foundNotification.until,
-            type: foundNotification.label as Type
-          }
+            when: foundMaintenance.when,
+          },
         }
-      : {
-          ...eachLinode,
-          maintenance: null
-        };
+      : { ...thisLinode, maintenance: null };
   });
 };
