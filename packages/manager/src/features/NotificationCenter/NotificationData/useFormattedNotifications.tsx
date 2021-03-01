cc2790e5a4463611d8af05d4919a9d4e1da607cd
Jared Kobos <jaredkobos@gmail.com>
2021-03-01 14:04:59 -0500

    Edit comment

--- packages/manager/src/features/NotificationCenter/NotificationData/useFormattedNotifications.tsx
+++ packages/manager/src/features/NotificationCenter/NotificationData/useFormattedNotifications.tsx
@@ -15,7 +15,7 @@ export const useFormattedNotifications = () => {
 
   const dayOfMonth = DateTime.local().day;
 
-  // Filter out the late payment notification from the API (since we are using a custom one), and any bounced email notifications and abuse tickets because users are alerted to those by global notification banners already.
+  // Filter out any bounced email notifications and abuse tickets because users are alerted to those by global notification banners already.
   const combinedNotifications = [...notifications].filter(
     (notification) =>
       !['billing_email_bounce', 'user_email_bounce', 'ticket_abuse'].includes(
