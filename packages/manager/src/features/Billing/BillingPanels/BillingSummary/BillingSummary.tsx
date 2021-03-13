d973b8b2a59fa0d0be1204469cda7d42b309fc26
John Callahan <johnwcallahan@gmail.com>
2021-03-12 23:18:36 -0500

    Styling, add details

--- packages/manager/src/features/Billing/BillingPanels/BillingSummary/BillingSummary.tsx
+++ packages/manager/src/features/Billing/BillingPanels/BillingSummary/BillingSummary.tsx
@@ -1,4 +1,6 @@
 import { ActivePromotion } from '@linode/api-v4/lib/account/types';
+import { GridSize } from '@material-ui/core/Grid';
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
 import * as classnames from 'classnames';
 import * as React from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
@@ -17,9 +19,11 @@ const useStyles = makeStyles((theme: Theme) => ({
   root: {
     marginTop: 0,
     marginBottom: 20,
+    minHeight: 97,
   },
   paper: {
     padding: `15px 20px`,
+    height: '100%',
   },
   divider: {
     marginTop: 8,
@@ -28,8 +32,12 @@ const useStyles = makeStyles((theme: Theme) => ({
   helpIcon: {
     padding: `0px 8px`,
     color: '#888f91',
+    '& svg': {
+      height: 20,
+      width: 20,
+    },
   },
-  neutralBalance: {
+  noBalance: {
     color: theme.palette.text.primary,
   },
   positiveBalance: {
@@ -38,126 +46,35 @@ const useStyles = makeStyles((theme: Theme) => ({
   credit: {
     color: '#02b159',
   },
+  makeAPaymentButton: {
+    ...theme.applyLinkStyles,
+  },
   text: {
     color: '#606469',
   },
   accruedCharges: {
     color: theme.palette.text.primary,
   },
-  header: {
-    marginBottom: theme.spacing(2) - 1,
-  },
-  unpaidBalance: {
-    marginBottom: theme.spacing(1) + 2,
-    fontSize: 32,
-    color: '#cf1e1e',
-  },
-  gridItem: {
-    padding: `0 0 ${theme.spacing(3) + 1}px`,
-    [theme.breakpoints.up('md')]: {
-      padding: `0 ${theme.spacing(4) - 2}px`,
-    },
-    '&:first-of-type': {
-      [theme.breakpoints.up('md')]: {
-        paddingLeft: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      },
-    },
-    '&:nth-of-type(2)': {
-      [theme.breakpoints.up('md')]: {
-        borderLeft: `1px solid ${theme.cmrBorderColors.borderBillingSummary}`,
-      },
-    },
-    '&:last-of-type': {
-      paddingBottom: 0,
-      [theme.breakpoints.up('md')]: {
-        paddingRight: 0,
-      },
-    },
-  },
-  balanceOuter: {
-    paddingTop: theme.spacing(1) - 3,
-    borderTop: `1px dashed ${theme.cmrBorderColors.borderBalance}`,
-  },
-  label: {
-    margin: `${theme.spacing(1) - 3}px 0`,
-    fontWeight: 'bold',
-    color: theme.color.headline,
-    fontSize: '1rem',
-    letterSpacing: 0.07,
-    lineHeight: 1.25,
-  },
-  field: {
-    margin: `${theme.spacing(1) - 3}px 0`,
-    color: `${theme.color.headline}`,
-    fontSize: '1rem',
-    lineHeight: 1.13,
-  },
-  caption: {
-    marginTop: theme.spacing(1) + 2,
-    textAlign: 'right',
-  },
-  // text: {
-  //   lineHeight: 1.43,
-  //   letterSpacing: 0.1,
-  //   color: theme.color.billingText,
-  // },
-  iconButtonOuter: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    '& button, & a': {
-      justifyContent: 'flex-start',
-    },
-    [theme.breakpoints.down('md')]: {
-      justifyContent: 'flex-start',
-    },
-    [theme.breakpoints.only('xs')]: {
-      flexDirection: 'column',
-    },
-    [theme.breakpoints.only('md')]: {
-      flexDirection: 'column',
-    },
-  },
-  invoiceButton: {
-    [theme.breakpoints.only('sm')]: {
-      paddingLeft: 32,
-    },
-    '& button': {
-      paddingLeft: '0',
-    },
-  },
-  iconButton: {
-    paddingLeft: 0,
-    [theme.breakpoints.up('md')]: {
-      paddingBottom: 0,
-    },
-    '&:hover': {
-      textDecoration: 'none',
-      '& svg': {
-        color: `${theme.palette.primary.main} !important`,
-      },
-    },
-    '&:focus': {
-      textDecoration: 'none',
-    },
-  },
-  infoIcon: {
-    padding: `0px ${theme.spacing(1) + 2}px`,
-    top: -2,
-  },
 }));
 
-interface Props {
+// =============================================================================
+// <BillingSummary />
+// =============================================================================
+interface BillingSummaryProps {
   promotions?: ActivePromotion[];
   balanceUninvoiced: number;
   balance: number;
   mostRecentInvoiceId?: number;
 }
 
-export const BillingSummary: React.FC<Props> = (props) => {
+export const BillingSummary: React.FC<BillingSummaryProps> = (props) => {
+  const classes = useStyles();
+
   const { promotions, balanceUninvoiced, balance } = props;
+
+  //
+  // Payment Drawer
+  //
 
   // On-the-fly route matching so this component can open the drawer itself.
   const makePaymentRouteMatch = Boolean(
@@ -186,9 +103,11 @@ export const BillingSummary: React.FC<Props> = (props) => {
     }
   }, [makePaymentRouteMatch, openPaymentDrawer]);
 
-  const classes = useStyles();
-
+  //
+  // Account Balance logic
+  //
   let accountBalanceText = 'You have no balance at this time.';
+  // @todo: In the future make this account for grace period, etc.
   if (balance > 0) {
     accountBalanceText = 'Payment Overdue';
   }
@@ -196,10 +115,20 @@ export const BillingSummary: React.FC<Props> = (props) => {
     accountBalanceText = 'Credit';
   }
 
+  const accountBalanceClassnames = classnames({
+    [classes.noBalance]: balance === 0,
+    [classes.positiveBalance]: balance > 0,
+    [classes.credit]: balance < 0,
+  });
+
+  // The layout changes if there are promotions.
+  const gridDimensions: Partial<Record<Breakpoint, GridSize>> =
+    promotions && promotions.length > 0 ? { xs: 12, md: 4 } : { xs: 12, sm: 6 };
+
   return (
     <>
       <Grid container spacing={3} className={classes.root}>
-        <Grid item xs={12} md={4}>
+        <Grid item {...gridDimensions}>
           <Paper className={classes.paper}>
             <Box display="flex" alignItems="center">
               <Typography variant="h3">Account Balance</Typography>
@@ -218,43 +147,45 @@ export const BillingSummary: React.FC<Props> = (props) => {
               <Typography
                 variant={balance === 0 ? 'body1' : 'h3'}
                 style={{ marginRight: 8 }}
-                className={classnames({
-                  [classes.neutralBalance]: balance === 0,
-                  [classes.positiveBalance]: balance > 0,
-                  [classes.credit]: balance < 0,
-                })}
+                className={accountBalanceClassnames}
               >
                 {accountBalanceText}
               </Typography>
-              <Typography
-                variant="h3"
-                className={classnames({
-                  [classes.neutralBalance]: balance === 0,
-                  [classes.positiveBalance]: balance > 0,
-                  [classes.credit]: balance < 0,
-                })}
-              >
+              <Typography variant="h3" className={accountBalanceClassnames}>
                 <Currency quantity={Math.abs(balance)} />
               </Typography>
             </Box>
+            {balance > 0 ? (
+              <Typography style={{ marginTop: 16 }}>
+                <button
+                  className={classes.makeAPaymentButton}
+                  onClick={openPaymentDrawer}
+                >
+                  Make a payment immediately
+                </button>{' '}
+                to avoid service disruption.
+              </Typography>
+            ) : null}
           </Paper>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper className={classes.paper}>
-            <Box display="flex" alignItems="center">
-              <Typography variant="h3">Promotions</Typography>
-              <HelpIcon
-                className={classes.helpIcon}
-                text="Some helper text about promotions."
-              />
-            </Box>
-            <Divider className={classes.divider} />
-            {promotions?.map((thisPromo) => (
-              <PromoDisplay key={thisPromo.summary} {...thisPromo} />
-            ))}
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={4}>
+        {promotions && promotions?.length > 0 ? (
+          <Grid item {...gridDimensions}>
+            <Paper className={classes.paper}>
+              <Box display="flex" alignItems="center">
+                <Typography variant="h3">Promotions</Typography>
+                <HelpIcon
+                  className={classes.helpIcon}
+                  text="Some helper text about promotions."
+                />
+              </Box>
+              <Divider className={classes.divider} />
+              {promotions?.map((thisPromo) => (
+                <PromoDisplay key={thisPromo.summary} {...thisPromo} />
+              ))}
+            </Paper>
+          </Grid>
+        ) : null}
+        <Grid item {...gridDimensions}>
           <Paper className={classes.paper}>
             <Box display="flex" alignItems="center">
               <Typography variant="h3">Accrued Charges</Typography>
@@ -285,12 +216,24 @@ export const BillingSummary: React.FC<Props> = (props) => {
 
 export default React.memo(BillingSummary);
 
+// =============================================================================
+// PromoDisplay
+// =============================================================================
 export type PromoDisplayProps = ActivePromotion;
 
-export const PromoDisplay: React.FC<PromoDisplayProps> = (props) => {
+export const PromoDisplay: React.FC<PromoDisplayProps> = React.memo((props) => {
   const classes = useStyles();
 
-  const { summary, description, credit_remaining, expire_dt } = props;
+  const {
+    summary,
+    description,
+    credit_remaining,
+    expire_dt,
+    credit_monthly_cap,
+  } = props;
+
+  const parsedCreditMonthlyCap = Number.parseFloat(credit_monthly_cap);
+
   return (
     <Box marginTop="12px">
       <Box display="flex" justifyContent="space-between">
@@ -304,11 +247,17 @@ export const PromoDisplay: React.FC<PromoDisplayProps> = (props) => {
           <Currency quantity={Number.parseFloat(credit_remaining)} /> remaining
         </Typography>
       </Box>
+      {/* @todo: Add support for service_type ("Applies to: Linodes"). */}
       {expire_dt ? (
         <Typography>
-          Expires <DateTimeDisplay value={expire_dt} displayTime={false} />
+          Expires: <DateTimeDisplay value={expire_dt} displayTime={false} />
+        </Typography>
+      ) : null}
+      {parsedCreditMonthlyCap > 0 ? (
+        <Typography>
+          Monthly cap: <Currency quantity={parsedCreditMonthlyCap} />
         </Typography>
       ) : null}
     </Box>
   );
-};
+});
