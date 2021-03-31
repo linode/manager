490195435974787f24e85d98941b3cde25e241a6
Dajahi Wiley <32860776+DevDW@users.noreply.github.com>
2021-03-31 11:56:21 -0400

    Changes for VLAN attachment in Linode Create flow (#7535)
    * Revised showVlans logic in AddonsPanel.tsx
    
    * Pass region from AddonsPanel --> AttachVLAN --> InterfaceSelect so that only VLANs for the given region populate the dropdown
    
    * Add utility file for checking if region supports VLANs; only show 'Attach a VLAN' section if the region supports VLANs; only include interfaces in the Create Linode payload if the region supports VLANs
    
    * Adjust position of menu dropdown
    
    * Adjust position of menu dropdown in Create flow a bit more
    
    * Remove loading state and change noOptionsMessage
    
    Co-authored-by: John Callahan <johnwcallahan@gmail.com>

--- packages/manager/src/features/linodes/LinodesCreate/AddonsPanel.tsx
+++ packages/manager/src/features/linodes/LinodesCreate/AddonsPanel.tsx
@@ -11,7 +11,8 @@ import Currency from 'src/components/Currency';
 import Grid from 'src/components/Grid';
 import useAccount from 'src/hooks/useAccount';
 import useFlags from 'src/hooks/useFlags';
-import { isFeatureEnabled } from 'src/utilities/accountCapabilities';
+import { useRegionsQuery } from 'src/queries/regions';
+import { doesRegionSupportVLANs } from 'src/utilities/doesRegionSupportVLANs';
 import AttachVLAN from './AttachVLAN';
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -101,17 +102,22 @@ const AddonsPanel: React.FC<CombinedProps> = (props) => {
     ipamAddress,
     ipamError,
     handleVLANChange,
+    selectedRegionID,
   } = props;
 
   const classes = useStyles();
   const flags = useFlags();
   const { account } = useAccount();
 
-  const showVlans = isFeatureEnabled(
-    'Vlans',
-    Boolean(flags.vlans),
-    account?.data?.capabilities ?? []
-  );
+  const regions = useRegionsQuery().data ?? [];
+  const selectedRegion = selectedRegionID || '';
+
+  // Making this an && instead of the usual hasFeatureEnabled, which is || based.
+  // Doing this so that we can toggle our flag without enabling vlans for all customers.
+  const capabilities = account?.data?.capabilities ?? [];
+  const showVlans = capabilities.includes('Vlans') && flags.vlans;
+
+  const regionSupportsVLANs = doesRegionSupportVLANs(selectedRegion, regions);
 
   const renderBackupsPrice = () => {
     const { backupsMonthly } = props;
@@ -129,7 +135,7 @@ const AddonsPanel: React.FC<CombinedProps> = (props) => {
   return (
     <Paper className={classes.root} data-qa-add-ons>
       <div className={classes.inner}>
-        {showVlans ? (
+        {showVlans && regionSupportsVLANs ? (
           <AttachVLAN
             vlanLabel={vlanLabel}
             labelError={labelError}
@@ -137,6 +143,7 @@ const AddonsPanel: React.FC<CombinedProps> = (props) => {
             ipamError={ipamError}
             readOnly={disabled}
             handleVLANChange={handleVLANChange}
+            region={selectedRegionID}
           />
         ) : null}
         <Typography variant="h2" className={classes.title}>
