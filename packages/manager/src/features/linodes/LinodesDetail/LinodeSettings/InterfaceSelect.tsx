07abf768d1fbcc55e041930f6803407a9d776379
John Callahan <johnwcallahan@gmail.com>
2021-03-31 11:12:11 -0400

    Remove loading state and change noOptionsMessage

--- packages/manager/src/features/linodes/LinodesDetail/LinodeSettings/InterfaceSelect.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeSettings/InterfaceSelect.tsx
@@ -139,7 +139,6 @@ export const InterfaceSelect: React.FC<Props> = (props) => {
                 className={fromAddonsPanel ? classes.vlanLabelField : ''}
                 errorText={labelError}
                 options={vlanOptions}
-                isLoading={isLoading}
                 label="Label"
                 placeholder="Create or select a VLAN"
                 variant="creatable"
@@ -148,6 +147,11 @@ export const InterfaceSelect: React.FC<Props> = (props) => {
                 onChange={handleLabelChange}
                 isClearable
                 disabled={readOnly}
+                noOptionsMessage={() =>
+                  isLoading
+                    ? 'Loading...'
+                    : 'You have no VLANs in this region. Type to create one.'
+                }
               />
             </Grid>
             <Grid
