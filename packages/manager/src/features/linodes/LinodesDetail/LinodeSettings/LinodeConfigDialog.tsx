f52ec10e02651d7780d3a810418f3ba1e3982649
John Callahan <johnwcallahan@gmail.com>
2021-04-08 17:29:07 -0400

    Update button text

--- packages/manager/src/features/linodes/LinodesDetail/LinodeSettings/LinodeConfigDialog.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeSettings/LinodeConfigDialog.tsx
@@ -879,7 +879,7 @@ const LinodeConfigDialog: React.FC<CombinedProps> = (props) => {
                   disabled={readOnly}
                   loading={formik.isSubmitting}
                 >
-                  {linodeConfigId ? 'Save' : 'Add'} Configuration
+                  {linodeConfigId ? 'Save Changes' : 'Add Configuration'}
                 </Button>
                 <Button
                   buttonType="secondary"
