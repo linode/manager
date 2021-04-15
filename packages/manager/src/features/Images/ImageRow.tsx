960408f46bddeb6055df513da66ceab70c0a88c9
Tiffany Wong <wong.tyan@gmail.com>
2021-04-15 13:22:10 -0400

    Initial commit

--- packages/manager/src/features/Images/ImageRow.tsx
+++ packages/manager/src/features/Images/ImageRow.tsx
@@ -2,19 +2,14 @@ import { Event } from '@linode/api-v4/lib/account';
 import { Image } from '@linode/api-v4/lib/images';
 import * as React from 'react';
 import Hidden from 'src/components/core/Hidden';
-import { makeStyles, Theme } from 'src/components/core/styles';
-import Typography from 'src/components/core/Typography';
-import LinearProgress from 'src/components/LinearProgress';
+import { makeStyles } from 'src/components/core/styles';
 import TableCell from 'src/components/TableCell/TableCell_CMR';
 import TableRow from 'src/components/TableRow/TableRow_CMR';
 import { capitalizeAllWords } from 'src/utilities/capitalize';
 import { formatDate } from 'src/utilities/formatDate';
 import ActionMenu, { Handlers } from './ImagesActionMenu';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  loadingStatus: {
-    marginBottom: theme.spacing() / 2,
-  },
+const useStyles = makeStyles(() => ({
   actionMenu: {
     display: 'flex',
     justifyContent: 'flex-end',
@@ -42,21 +37,7 @@ const ImageRow: React.FC<CombinedProps> = (props) => {
     ...rest
   } = props;
 
-  return isImageUpdating(event) ? (
-    <TableRow key={id} data-qa-image-cell={id}>
-      <TableCell data-qa-image-label>
-        <ProgressDisplay
-          className={classes.loadingStatus}
-          text="Creating"
-          progress={progressFromEvent(event)}
-        />
-        {label}
-      </TableCell>
-      <TableCell colSpan={4}>
-        <LinearProgress value={progressFromEvent(event)} />
-      </TableCell>
-    </TableRow>
-  ) : (
+  return (
     <TableRow key={id} data-qa-image-cell={id}>
       <TableCell data-qa-image-label>{label}</TableCell>
       <Hidden xsDown>
@@ -65,7 +46,9 @@ const ImageRow: React.FC<CombinedProps> = (props) => {
         ) : null}
         <TableCell data-qa-image-date>{formatDate(created)}</TableCell>
       </Hidden>
-      <TableCell data-qa-image-size>{size} MB</TableCell>
+      <TableCell data-qa-image-size>
+        {isImageUpdating(event) ? 'N/A' : `${size} MB`}
+      </TableCell>
       <Hidden xsDown>
         {expiry ? (
           <TableCell data-qa-image-date>{formatDate(expiry)}</TableCell>
@@ -91,27 +74,6 @@ export const isImageUpdating = (e?: Event) => {
   }
   return (
     e?.action === 'disk_imagize' && ['scheduled', 'started'].includes(e.status)
-  );
-};
-
-const progressFromEvent = (e?: Event) => {
-  return e?.status === 'started' && e?.percent_complete
-    ? e.percent_complete
-    : undefined;
-};
-
-const ProgressDisplay: React.FC<{
-  className: string;
-  progress: undefined | number;
-  text: string;
-}> = (props) => {
-  const { progress, text, className } = props;
-  const displayProgress = progress ? `${progress}%` : `scheduled`;
-
-  return (
-    <Typography variant="body1" className={className}>
-      {text}: {displayProgress}
-    </Typography>
   );
 };
 
