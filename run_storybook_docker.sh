bcb6a55341c06fd9424d513613a5bdd4e2caa4e2
John Callahan <johnwcallahan@gmail.com>
2021-04-15 17:38:28 -0400

    Major doc edits and repo cleanup (#7581)
    My main goal was to consolidate documentation. We've accumulated a lot of content over the years and honestly a lot of it is outdated. I also removed red herrings like everything related to Docker (since we don't actually use it).
    
    I deleted A LOT of stuff. I tried to ensure everything that I deleted was either a) covered more succinctly somewhere else, or b) outdated. The goal is to lower the barrier of entry to get going with the repo.
    
    Highlights:
    
    - Rewrite GETTING_STARTED.md.
    - Rewrite CONTRIBUTING.md.
    - Delete a lot of guides.
    - Move most existing guides to a new directory, /docs.
    - Remove Docker integration
    - Update pull request template.
    
    Co-authored-by: Jared Kobos <jaredkobos@gmail.com>

--- run_storybook_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "starting server"
-nohup yarn storybook&
-PID=$!
-echo "waiting for storybook server to start, PID $PID"
-# need npx for docker container
-yarn run wait-on http://localhost:6006
-yarn storybook:e2e
-kill $PID
