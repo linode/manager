168685eca7142bc8ec52b8fead8dbfacf7cc1c74
Hana Xu <hanaxu12@gmail.com>
2021-04-27 10:42:58 -0400

    PR feedback 2

--- packages/manager/src/features/linodes/transitions.test.ts
+++ packages/manager/src/features/linodes/transitions.test.ts
@@ -1,25 +1,17 @@
 import { transitionText } from './transitions';
+import { eventFactory } from 'src/factories/events';
 
 describe('transitionText helper', () => {
   it('should remove "linode" from the event and capitalize it', () => {
-    expect(
-      transitionText('loading', 123, {
-        id: 123,
-        action: 'linode_snapshot',
-        secondary_entity: null,
-        created: '2012-12-12',
-        entity: null,
-        percent_complete: null,
-        rate: null,
-        read: true,
-        seen: true,
-        status: 'started',
-        time_remaining: null,
-        username: 'linode',
-        duration: 0,
-        message: null,
-      })
-    ).toEqual('Snapshot');
+    const mockEvent = eventFactory.build({
+      action: 'linode_snapshot',
+      percent_complete: null,
+      read: true,
+      seen: true,
+    });
+    expect(transitionText('loading', mockEvent.id, mockEvent)).toEqual(
+      'Snapshot'
+    );
   });
 
   it('should use status if an event is missing and capitalize it', () => {
@@ -27,49 +19,29 @@ describe('transitionText helper', () => {
   });
 
   it('should use status if an event is not a transition event and capitalize it', () => {
-    expect(
-      transitionText('optimus', 123, {
-        id: 123,
-        action: 'linode_addip',
-        secondary_entity: null,
-        created: '2012-12-12',
-        entity: null,
-        percent_complete: null,
-        rate: null,
-        read: true,
-        seen: true,
-        status: 'started',
-        time_remaining: null,
-        username: 'linode',
-        duration: 0,
-        message: null,
-      })
-    ).toEqual('Optimus');
+    const mockEvent = eventFactory.build({
+      action: 'linode_addip',
+      percent_complete: null,
+      read: true,
+      seen: true,
+    });
+    expect(transitionText('optimus', mockEvent.id, mockEvent)).toEqual(
+      'Optimus'
+    );
   });
 
-  it('should use present tense if an event is a transition event and capitalize it', () => {
-    expect(
-      transitionText('running', 123, {
-        id: 123,
-        action: 'disk_imagize',
-        secondary_entity: {
-          id: 12345,
-          label: 'Alpine 3.13 Disk',
-          type: 'image',
-          url: '/v4/images/private/12345',
-        },
-        created: '2020-12-12',
-        entity: null,
-        percent_complete: 34,
-        rate: null,
-        read: false,
-        seen: false,
-        status: 'started',
-        time_remaining: null,
-        username: 'linode',
-        duration: 12,
-        message: null,
-      })
-    ).toEqual('Capturing Image');
+  it('should use present continuous tense if an event is a transition event and capitalize it', () => {
+    const mockEvent = eventFactory.build({
+      action: 'disk_imagize',
+      secondary_entity: {
+        id: 12345,
+        label: 'Alpine 3.13 Disk',
+        type: 'image',
+        url: '/v4/images/private/12345',
+      },
+    });
+    expect(transitionText('running', mockEvent.id, mockEvent)).toEqual(
+      'Capturing Image'
+    );
   });
 });
