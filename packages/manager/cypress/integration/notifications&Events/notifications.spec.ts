2c19706219f9063f44bfde28226b312b33fe4b88
ndukatz <ndukatz@linode.com>
2021-04-28 17:27:46 -0400

    3 tests in one

--- packages/manager/cypress/integration/notifications&Events/notifications.spec.ts
+++ packages/manager/cypress/integration/notifications&Events/notifications.spec.ts
@@ -1,73 +1,46 @@
-// import { worker } from '@src/mocks/testBrowser';
-import { waitForAppLoad } from '../../support/ui/common';
-const oauthtoken = Cypress.env('MANAGER_OAUTH');
-import {
-  notificationFactory,
-  abuseTicketNotificationFactory,
-} from '@src/factories/notification';
+import { notificationFactory } from '@src/factories/notification';
 import { createLinode } from '../../support/api/linodes';
-import {
-  containsVisible,
-  fbtVisible,
-  getClick,
-  getVisible,
-} from '../../support/helpers';
+import { containsVisible, getClick } from '../../support/helpers';
 import { makeResourcePage } from '@src/mocks/serverHandlers';
 
-// interface MN {
-//   type: string;
-//   entity: Record<string, unknown>;
-//   message: string;
-//   severity: string;
-// }
-
-// const defaultMN = {
-//   type: null,
-//   entity: null,
-//   message: null,
-//   severity: null
-// };
-
-// const updateMN = (mn: MN, update: Partial<MN>) => {
-//   return {...mn, ...update}
-// };
-
-const migrationTicket = (linode) => {
-  return notificationFactory.buildList(1, {
+const migrationTicket = (label) => {
+  return {
     type: 'migration_pending',
-    entity: { id: 0, type: 'linode', label: linode.label },
+    message: `You have a migration pending! ${label} must be offline before starting the migration.`,
+    entity: { id: 0, type: 'linode', label },
     severity: 'critical',
-  });
+  };
 };
 
-const minorSeverityTicket = (_linode) => {
-  return notificationFactory.buildList(1, {
+const minorSeverityTicket = (_label) => {
+  return {
     type: 'notice',
     message: 'Testing for minor notification',
     severity: 'minor',
-  });
+  };
 };
 
-const data = [migrationTicket, minorSeverityTicket];
+const balanceNotification = (_label) => {
+  return {
+    type: 'payment_due',
+    message: 'You have an overdue balance!',
+    severity: 'major',
+  };
+};
 
-// const balanceNotification = notificationFactory.build({
-//   type: 'payment_due',
-//   message: 'You have an overdue balance!',
-//   severity: 'major',
-// });
+const data = [migrationTicket, minorSeverityTicket, balanceNotification];
 
-// const abuseTicket = abuseTicketNotificationFactory;
-
+// to do: add icon validation for each notification type and add abuseTicket = abuseTicketNotificationFactory
 describe('notifications', () => {
-  data.forEach((d) => {
-    it(`Verify ${d} Notifications`, () => {
+  data.forEach((notification) => {
+    it(`${notification('').type} notification`, () => {
       createLinode().then((linode) => {
-        const payload = d(linode);
-        const message = payload.toString;
-        console.log(message);
+        const request: Record<string, unknown> = notification(linode.label);
         cy.intercept('GET', '*/account/notifications*', (req) => {
           req.reply((res) => {
-            res.send(makeResourcePage(payload));
+            res.send(
+              makeResourcePage(notificationFactory.buildList(1, request))
+            );
           });
         }).as('mockNotification');
         cy.visitWithLogin('/linodes');
@@ -76,28 +49,8 @@ describe('notifications', () => {
           containsVisible('Running');
         });
         getClick('button[aria-label="Notifications"]');
-        containsVisible(
-          `You have a migration pending! ${linode.label} must be offline before starting the migration.`
-        );
+        containsVisible(request.message);
       });
     });
   });
-
-  //   it.skip('Verify Minor Severity Ticket Notifications', () => {
-  //     const message = 'Testing for minor notification';
-  //     createLinode().then((linode) => {
-  //       cy.intercept('GET', '*/account/notifications*', (req) => {
-  //         req.reply((res) => {
-  //           res.send(makeResourcePage(minorSeverityTicket));
-  //         });
-  //       }).as('mockNotification');
-  //       cy.visitWithLogin('/linodes');
-  //       cy.wait('@mockNotification');
-  //       cy.get(`[data-qa-linode="${linode.label}"]`).within(() => {
-  //         containsVisible('Running');
-  //       });
-  //       getClick('button[aria-label="Notifications"]');
-  //       containsVisible(message);
-  //     });
-  //   });
 });
