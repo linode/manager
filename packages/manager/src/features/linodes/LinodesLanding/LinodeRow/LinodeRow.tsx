eeef197db4d48310728d5f4397f9248937984fba
codyfinn <codyfinn@protonmail.com>
2021-04-30 09:35:18 -0400

    fixed incorrect variable usage

--- packages/manager/src/features/linodes/LinodesLanding/LinodeRow/LinodeRow.tsx
+++ packages/manager/src/features/linodes/LinodesLanding/LinodeRow/LinodeRow.tsx
@@ -211,7 +211,7 @@ export const LinodeRow: React.FC<CombinedProps> = (props) => {
 
       <Hidden xsDown>
         <TableCell className={classes.planCell} data-qa-ips>
-          <div className={classes.planCell}>{label}</div>
+          <div className={classes.planCell}>{type?.label}</div>
         </TableCell>
         <TableCell className={classes.ipCell} data-qa-ips>
           <div className={classes.ipCellWrapper}>
