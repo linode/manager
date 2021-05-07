e855fabe0f1edde2cb0a8bc989ab24f704a04788
Tiffany Wong <wong.tyan@gmail.com>
2021-05-07 12:59:11 -0400

    Initial commit

--- packages/manager/.storybook/preview.tsx
+++ packages/manager/.storybook/preview.tsx
@@ -1,4 +1,28 @@
 // .storybook/preview.js
+import { select } from '@storybook/addon-knobs';
 import React from 'react';
+import '../public/fonts/fonts.css';
+import CssBaseline from '../src/components/core/CssBaseline';
+import { ThemeProvider } from '../src/components/core/styles';
+import '../src/index.css';
+import { dark, light } from '../src/themes';
 import { wrapWithTheme } from '../src/utilities/testHelpers';
-export const decorators = [(Story) => wrapWithTheme(<Story />)];
+
+const options = {
+  dark,
+  light,
+};
+
+const key = select('theme', ['light', 'dark'], 'light');
+
+export const decorators = [
+  (Story) =>
+    wrapWithTheme(
+      <ThemeProvider theme={options[key]}>
+        <CssBaseline />
+        <div style={{ margin: '1rem' }}>
+          <Story />
+        </div>
+      </ThemeProvider>
+    ),
+];
--- packages/manager/src/components/Button/button.stories.tsx
+++ packages/manager/src/components/Button/button.stories.tsx
@@ -1,267 +1,191 @@
-import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp';
 import * as React from 'react';
 import Button from 'src/components/Button';
-import ButtonLink from 'src/components/Button/ButtonLink';
-import { default as MDivider } from 'src/components/core/Divider';
-
-const Divider = () => (
-  <MDivider style={{ marginBottom: '8px', marginTop: '8px' }} />
-);
 
 export default {
+  component: Button,
   title: 'Button',
 };
 
-export const Types = () => (
-  <React.Fragment>
+// const Template = (args) => <Button {...args}>Primary</Button>;
+
+// export const Default = Template.bind({});
+// Default.args = {
+//   buttonType: 'primary',
+// };
+
+export const Default = () => (
+  <>
     <Button buttonType="primary" data-qa-button="primary">
       Primary
     </Button>
-    <Divider />
     <Button buttonType="secondary" data-qa-button="secondary">
       Secondary
     </Button>
-    <Divider />
     <Button buttonType="secondary" outline data-qa-button="secondary">
-      Secondary with Outline
+      Secondary with outline
     </Button>
-    <Divider />
     <Button buttonType="cancel" data-qa-button="cancel">
       Cancel
     </Button>
-    <Divider />
     <Button buttonType="remove" data-qa-button="remove" />
-    <Divider />
-    <ButtonLink to="javascript:void(0)" linkText="Link as Primary" />
-    <Divider />
-    <ButtonLink
-      secondary
-      to="javascript:void(0)"
-      linkText="Link as Secondary"
-    />
-  </React.Fragment>
+  </>
 );
 
-export const Disabled = () => (
-  <React.Fragment>
-    <Button disabled buttonType="primary" data-qa-button="primary">
-      Primary
-    </Button>
-    <Divider />
-    <Button disabled buttonType="secondary" data-qa-button="secondary">
-      Secondary
-    </Button>
-    <Divider />
-    <Button buttonType="secondary" outline data-qa-button="secondary">
-      Secondary with Outline
-    </Button>
-    <Divider />
-    <Button
-      disabled
-      destructive
-      buttonType="primary"
-      data-qa-button="destructive"
-    >
-      Destructive
-    </Button>
-    <Divider />
-    <Button disabled buttonType="cancel" data-qa-button="cancel">
-      Cancel
-    </Button>
-    <Divider />
-  </React.Fragment>
-);
+// export const Disabled = () => (
+//   <React.Fragment>
+//     <Button disabled buttonType="primary" data-qa-button="primary">
+//       Primary
+//     </Button>
+//     <Divider />
+//     <Button disabled buttonType="secondary" data-qa-button="secondary">
+//       Secondary
+//     </Button>
+//     <Divider />
+//     <Button buttonType="secondary" outline data-qa-button="secondary">
+//       Secondary with Outline
+//     </Button>
+//     <Divider />
+//     <Button
+//       disabled
+//       destructive
+//       buttonType="primary"
+//       data-qa-button="destructive"
+//     >
+//       Destructive
+//     </Button>
+//     <Divider />
+//     <Button disabled buttonType="cancel" data-qa-button="cancel">
+//       Cancel
+//     </Button>
+//     <Divider />
+//   </React.Fragment>
+// );
 
-export const Loading = () => (
-  <React.Fragment>
-    <Button loading buttonType="primary" data-qa-button="primary">
-      Primary
-    </Button>
-    <Divider />
-    <Button loading buttonType="secondary" data-qa-button="secondary">
-      Secondary
-    </Button>
-    <Divider />
-    <Button loading buttonType="cancel" data-qa-button="cancel">
-      Cancel
-    </Button>
-    <Divider />
-  </React.Fragment>
-);
+// export const Loading = () => (
+//   <React.Fragment>
+//     <Button loading buttonType="primary" data-qa-button="primary">
+//       Primary
+//     </Button>
+//     <Divider />
+//     <Button loading buttonType="secondary" data-qa-button="secondary">
+//       Secondary
+//     </Button>
+//     <Divider />
+//     <Button loading buttonType="cancel" data-qa-button="cancel">
+//       Cancel
+//     </Button>
+//     <Divider />
+//   </React.Fragment>
+// );
 
-export const LoadingWithText = () => {
-  return (
-    <React.Fragment>
-      <Button
-        loading
-        buttonType="primary"
-        data-qa-button="primary"
-        loadingText="Fetching Linodes..."
-      >
-        Primary
-      </Button>
-      <Divider />
-      <Button
-        loading
-        buttonType="secondary"
-        data-qa-button="secondary"
-        loadingText="Fetching Volumes..."
-      >
-        Secondary
-      </Button>
-      <Divider />
-      <Button
-        loading
-        buttonType="cancel"
-        data-qa-button="cancel"
-        loadingText="Fetching Domains..."
-      >
-        Cancel
-      </Button>
-      <Divider />
-      <Button
-        loading
-        buttonType="primary"
-        compact
-        data-qa-button="primary"
-        loadingText="Fetching Linodes..."
-      >
-        Primary Compact
-      </Button>
-      <Divider />
-      <Button
-        loading
-        compact
-        buttonType="secondary"
-        data-qa-button="secondary"
-        loadingText="Fetching Volumes..."
-      >
-        Secondary Compact
-      </Button>
-      <Divider />
-      <Button
-        loading
-        compact
-        buttonType="cancel"
-        data-qa-button="cancel"
-        loadingText="Fetching Domains..."
-      >
-        Cancel Compact
-      </Button>
-      <Divider />
-    </React.Fragment>
-  );
-};
+// export const LoadingWithText = () => {
+//   return (
+//     <React.Fragment>
+//       <Button
+//         loading
+//         buttonType="primary"
+//         data-qa-button="primary"
+//         loadingText="Fetching Linodes..."
+//       >
+//         Primary
+//       </Button>
+//       <Divider />
+//       <Button
+//         loading
+//         buttonType="secondary"
+//         data-qa-button="secondary"
+//         loadingText="Fetching Volumes..."
+//       >
+//         Secondary
+//       </Button>
+//       <Divider />
+//       <Button
+//         loading
+//         buttonType="cancel"
+//         data-qa-button="cancel"
+//         loadingText="Fetching Domains..."
+//       >
+//         Cancel
+//       </Button>
+//       <Divider />
+//       <Button
+//         loading
+//         buttonType="primary"
+//         compact
+//         data-qa-button="primary"
+//         loadingText="Fetching Linodes..."
+//       >
+//         Primary Compact
+//       </Button>
+//       <Divider />
+//       <Button
+//         loading
+//         compact
+//         buttonType="secondary"
+//         data-qa-button="secondary"
+//         loadingText="Fetching Volumes..."
+//       >
+//         Secondary Compact
+//       </Button>
+//       <Divider />
+//       <Button
+//         loading
+//         compact
+//         buttonType="cancel"
+//         data-qa-button="cancel"
+//         loadingText="Fetching Domains..."
+//       >
+//         Cancel Compact
+//       </Button>
+//       <Divider />
+//     </React.Fragment>
+//   );
+// };
 
-export const Destructive = () => (
-  <React.Fragment>
-    <Button
-      destructive
-      buttonType="primary"
-      data-qa-button="primary"
-      data-qa-btn-type="destructive"
-    >
-      Primary
-    </Button>
-    <Divider />
-    <Button
-      disabled
-      destructive
-      buttonType="secondary"
-      data-qa-button="secondary"
-    >
-      Secondary
-    </Button>
-    <Divider />
-  </React.Fragment>
-);
+// export const Destructive = () => (
+//   <React.Fragment>
+//     <Button
+//       destructive
+//       buttonType="primary"
+//       data-qa-button="primary"
+//       data-qa-btn-type="destructive"
+//     >
+//       Primary
+//     </Button>
+//     <Divider />
+//     <Button
+//       disabled
+//       destructive
+//       buttonType="secondary"
+//       data-qa-button="secondary"
+//     >
+//       Secondary
+//     </Button>
+//     <Divider />
+//   </React.Fragment>
+// );
 
-export const LoadingDestructive = () => (
-  <React.Fragment>
-    <Button
-      loading
-      destructive
-      buttonType="primary"
-      data-qa-button="primary"
-      data-qa-btn-type="destructive"
-    >
-      Primary
-    </Button>
-    <Divider />
-    <Button
-      loading
-      destructive
-      buttonType="secondary"
-      data-qa-button="secondary"
-      data-qa-btn-type="destructive"
-    >
-      Secondary
-    </Button>
-    <Divider />
-  </React.Fragment>
-);
-
-export const PrimaryDropdown = () => (
-  <React.Fragment>
-    <Button
-      buttonType="primary"
-      className="button-dropdown"
-      data-qa-button="dropdown"
-    >
-      Primary Dropdown
-      <KeyboardArrowDown className="caret" />
-    </Button>
-    <Divider />
-    <Button
-      buttonType="primary"
-      className="button-dropdown"
-      data-qa-button="dropdown"
-    >
-      Primary Dropdown Active
-      <KeyboardArrowUp className="caret" />
-    </Button>
-    <Divider />
-    <Button
-      buttonType="primary"
-      disabled
-      className="button-dropdown"
-      data-qa-button="dropdown"
-    >
-      Primary Dropdown
-      <KeyboardArrowDown className="caret" />
-    </Button>
-  </React.Fragment>
-);
-
-export const SecondaryDropdown = () => (
-  <React.Fragment>
-    <Button
-      buttonType="secondary"
-      className="button-dropdown"
-      data-qa-button="dropdown-secondary"
-    >
-      Secondary Dropdown
-      <KeyboardArrowDown className="caret" />
-    </Button>
-    <Divider />
-    <Button
-      buttonType="secondary"
-      className="button-dropdown"
-      data-qa-button="dropdown-secondary"
-    >
-      Secondary Dropdown Active
-      <KeyboardArrowUp className="caret" />
-    </Button>
-    <Divider />
-    <Button
-      buttonType="secondary"
-      disabled
-      className="button-dropdown"
-      data-qa-button="dropdown-secondary"
-    >
-      Secondary Dropdown
-      <KeyboardArrowDown className="caret" />
-    </Button>
-  </React.Fragment>
-);
+// export const LoadingDestructive = () => (
+//   <React.Fragment>
+//     <Button
+//       loading
+//       destructive
+//       buttonType="primary"
+//       data-qa-button="primary"
+//       data-qa-btn-type="destructive"
+//     >
+//       Primary
+//     </Button>
+//     <Divider />
+//     <Button
+//       loading
+//       destructive
+//       buttonType="secondary"
+//       data-qa-button="secondary"
+//       data-qa-btn-type="destructive"
+//     >
+//       Secondary
+//     </Button>
+//     <Divider />
+//   </React.Fragment>
+// );
