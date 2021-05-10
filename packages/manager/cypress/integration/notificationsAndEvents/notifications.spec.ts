0c1ffe41b46dd8d7a6562e86dfb6d6bca06f9345
Nate <66748958+ndukatz@users.noreply.github.com>
2021-05-10 17:36:17 -0400

    E2e notifications test (#7622)
    * initial notficiations test setup
    
    * cypress.json changes
    
    * verify pending migration
    
    * working on getting rid of redundancies
    
    * 3 tests in one
    
    * Update tsconfig.json
    
    * addressing pr feedback. simplifying to one test, no real linode, checking all notifications and icons, added some data-test-id attributes to help with this
    
    * updating tsconfig to get rid of errors

---
+++ packages/manager/cypress/integration/notificationsAndEvents/notifications.spec.ts
@@ -0,0 +1,40 @@
+import { Notification } from '@linode/api-v4/lib/account';
+import { notificationFactory } from '@src/factories/notification';
+import { makeResourcePage } from '@src/mocks/serverHandlers';
+import { getClick } from '../../support/helpers';
+const notifications: Notification[] = [
+  notificationFactory.build({
+    type: 'migration_scheduled',
+    severity: 'critical',
+  }),
+  notificationFactory.build({ type: 'migration_pending', severity: 'major' }),
+  notificationFactory.build({ type: 'reboot_scheduled', severity: 'minor' }),
+  notificationFactory.build({ type: 'outage', severity: 'critical' }),
+  notificationFactory.build({ type: 'payment_due', severity: 'major' }),
+  notificationFactory.build({ type: 'ticket_important', severity: 'minor' }),
+  notificationFactory.build({ type: 'ticket_abuse', severity: 'critical' }),
+  notificationFactory.build({ type: 'notice', severity: 'major' }),
+  notificationFactory.build({ type: 'maintenance', severity: 'minor' }),
+  notificationFactory.build({ type: 'promotion', severity: 'critical' }),
+];
+
+describe('verify notification types and icons', () => {
+  it(`notifications`, () => {
+    cy.intercept('GET', '*/account/notifications*', (req) => {
+      req.reply((res) => {
+        res.send(makeResourcePage(notifications));
+      });
+    }).as('mockNotification');
+    cy.visitWithLogin('/linodes');
+    cy.waitFor('@mockNotifications');
+    getClick('button[aria-label="Notifications"]');
+    getClick('[data-test-id="showMoreButton"');
+    notifications.forEach((notification) => {
+      cy.get(`[data-test-id="${notification.type}"]`).within(() => {
+        if (notification.severity != 'minor') {
+          cy.get(`[data-test-id="${notification.severity + 'Icon'}"]`);
+        }
+      });
+    });
+  });
+});
