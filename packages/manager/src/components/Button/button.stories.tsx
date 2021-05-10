63c3439c070fb055c7e6b67727e18668617a3755
Tiffany Wong <wong.tyan@gmail.com>
2021-05-10 10:44:04 -0400

    Uncomment code

--- packages/manager/src/components/Button/button.stories.tsx
+++ packages/manager/src/components/Button/button.stories.tsx
@@ -31,161 +31,143 @@ export const Default = () => (
   </>
 );
 
-// export const Disabled = () => (
-//   <React.Fragment>
-//     <Button disabled buttonType="primary" data-qa-button="primary">
-//       Primary
-//     </Button>
-//     <Divider />
-//     <Button disabled buttonType="secondary" data-qa-button="secondary">
-//       Secondary
-//     </Button>
-//     <Divider />
-//     <Button buttonType="secondary" outline data-qa-button="secondary">
-//       Secondary with Outline
-//     </Button>
-//     <Divider />
-//     <Button
-//       disabled
-//       destructive
-//       buttonType="primary"
-//       data-qa-button="destructive"
-//     >
-//       Destructive
-//     </Button>
-//     <Divider />
-//     <Button disabled buttonType="cancel" data-qa-button="cancel">
-//       Cancel
-//     </Button>
-//     <Divider />
-//   </React.Fragment>
-// );
+export const Disabled = () => (
+  <>
+    <Button disabled buttonType="primary" data-qa-button="primary">
+      Primary
+    </Button>
+    <Button disabled buttonType="secondary" data-qa-button="secondary">
+      Secondary
+    </Button>
+    <Button disabled buttonType="secondary" outline data-qa-button="secondary">
+      Secondary with Outline
+    </Button>
+    <Button
+      disabled
+      destructive
+      buttonType="primary"
+      data-qa-button="destructive"
+    >
+      Destructive
+    </Button>
+    <Button disabled buttonType="cancel" data-qa-button="cancel">
+      Cancel
+    </Button>
+  </>
+);
 
-// export const Loading = () => (
-//   <React.Fragment>
-//     <Button loading buttonType="primary" data-qa-button="primary">
-//       Primary
-//     </Button>
-//     <Divider />
-//     <Button loading buttonType="secondary" data-qa-button="secondary">
-//       Secondary
-//     </Button>
-//     <Divider />
-//     <Button loading buttonType="cancel" data-qa-button="cancel">
-//       Cancel
-//     </Button>
-//     <Divider />
-//   </React.Fragment>
-// );
+export const Loading = () => (
+  <>
+    <Button loading buttonType="primary" data-qa-button="primary">
+      Primary
+    </Button>
+    <Button loading buttonType="secondary" data-qa-button="secondary">
+      Secondary
+    </Button>
+    <Button loading buttonType="cancel" data-qa-button="cancel">
+      Cancel
+    </Button>
+  </>
+);
 
-// export const LoadingWithText = () => {
-//   return (
-//     <React.Fragment>
-//       <Button
-//         loading
-//         buttonType="primary"
-//         data-qa-button="primary"
-//         loadingText="Fetching Linodes..."
-//       >
-//         Primary
-//       </Button>
-//       <Divider />
-//       <Button
-//         loading
-//         buttonType="secondary"
-//         data-qa-button="secondary"
-//         loadingText="Fetching Volumes..."
-//       >
-//         Secondary
-//       </Button>
-//       <Divider />
-//       <Button
-//         loading
-//         buttonType="cancel"
-//         data-qa-button="cancel"
-//         loadingText="Fetching Domains..."
-//       >
-//         Cancel
-//       </Button>
-//       <Divider />
-//       <Button
-//         loading
-//         buttonType="primary"
-//         compact
-//         data-qa-button="primary"
-//         loadingText="Fetching Linodes..."
-//       >
-//         Primary Compact
-//       </Button>
-//       <Divider />
-//       <Button
-//         loading
-//         compact
-//         buttonType="secondary"
-//         data-qa-button="secondary"
-//         loadingText="Fetching Volumes..."
-//       >
-//         Secondary Compact
-//       </Button>
-//       <Divider />
-//       <Button
-//         loading
-//         compact
-//         buttonType="cancel"
-//         data-qa-button="cancel"
-//         loadingText="Fetching Domains..."
-//       >
-//         Cancel Compact
-//       </Button>
-//       <Divider />
-//     </React.Fragment>
-//   );
-// };
+export const LoadingWithText = () => {
+  return (
+    <>
+      <Button
+        loading
+        buttonType="primary"
+        data-qa-button="primary"
+        loadingText="Fetching Linodes..."
+      >
+        Primary
+      </Button>
+      <Button
+        loading
+        buttonType="secondary"
+        data-qa-button="secondary"
+        loadingText="Fetching Volumes..."
+      >
+        Secondary
+      </Button>
+      <Button
+        loading
+        buttonType="cancel"
+        data-qa-button="cancel"
+        loadingText="Fetching Domains..."
+      >
+        Cancel
+      </Button>
+      <Button
+        loading
+        buttonType="primary"
+        compact
+        data-qa-button="primary"
+        loadingText="Fetching Linodes..."
+      >
+        Primary Compact
+      </Button>
+      <Button
+        loading
+        compact
+        buttonType="secondary"
+        data-qa-button="secondary"
+        loadingText="Fetching Volumes..."
+      >
+        Secondary Compact
+      </Button>
+      <Button
+        loading
+        compact
+        buttonType="cancel"
+        data-qa-button="cancel"
+        loadingText="Fetching Domains..."
+      >
+        Cancel Compact
+      </Button>
+    </>
+  );
+};
 
-// export const Destructive = () => (
-//   <React.Fragment>
-//     <Button
-//       destructive
-//       buttonType="primary"
-//       data-qa-button="primary"
-//       data-qa-btn-type="destructive"
-//     >
-//       Primary
-//     </Button>
-//     <Divider />
-//     <Button
-//       disabled
-//       destructive
-//       buttonType="secondary"
-//       data-qa-button="secondary"
-//     >
-//       Secondary
-//     </Button>
-//     <Divider />
-//   </React.Fragment>
-// );
+export const Destructive = () => (
+  <>
+    <Button
+      destructive
+      buttonType="primary"
+      data-qa-button="primary"
+      data-qa-btn-type="destructive"
+    >
+      Primary
+    </Button>
+    <Button
+      disabled
+      destructive
+      buttonType="secondary"
+      data-qa-button="secondary"
+    >
+      Secondary
+    </Button>
+  </>
+);
 
-// export const LoadingDestructive = () => (
-//   <React.Fragment>
-//     <Button
-//       loading
-//       destructive
-//       buttonType="primary"
-//       data-qa-button="primary"
-//       data-qa-btn-type="destructive"
-//     >
-//       Primary
-//     </Button>
-//     <Divider />
-//     <Button
-//       loading
-//       destructive
-//       buttonType="secondary"
-//       data-qa-button="secondary"
-//       data-qa-btn-type="destructive"
-//     >
-//       Secondary
-//     </Button>
-//     <Divider />
-//   </React.Fragment>
-// );
+export const LoadingDestructive = () => (
+  <>
+    <Button
+      loading
+      destructive
+      buttonType="primary"
+      data-qa-button="primary"
+      data-qa-btn-type="destructive"
+    >
+      Primary
+    </Button>
+    <Button
+      loading
+      destructive
+      buttonType="secondary"
+      data-qa-button="secondary"
+      data-qa-btn-type="destructive"
+    >
+      Secondary
+    </Button>
+  </>
+);
