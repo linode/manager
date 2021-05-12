96fe7a7b986f6d9629db738eb1a1b72ad112d507
Banks Nussman <banks@nussman.us>
2021-05-12 17:16:02 -0400

    removed unused import

--- packages/manager/src/features/Kubernetes/KubeCheckoutBar/KubeCheckoutBar.tsx
+++ packages/manager/src/features/Kubernetes/KubeCheckoutBar/KubeCheckoutBar.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { classNames } from 'react-select/src/utils';
 import CheckoutBar from 'src/components/CheckoutBar';
 import ExternalLink from 'src/components/ExternalLink';
 import Notice from 'src/components/Notice';
