268b2a1f599f1ea5264d4dcd5273153abaa1c113
Dajahi Wiley <dwiley@linode.com>
2021-05-14 14:27:44 -0400

    Refactor to make ImagesPricingBanner.tsx more flexible

--- packages/manager/src/features/Images/ImagesPricingBanner.tsx
+++ packages/manager/src/features/Images/ImagesPricingBanner.tsx
@@ -1,8 +1,10 @@
+import { isEmpty } from 'ramda';
 import * as React from 'react';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
 import DismissibleBanner from 'src/components/DismissibleBanner';
 import Link from 'src/components/Link';
+import { FlagSet } from 'src/featureFlags';
 import useFlags from 'src/hooks/useFlags';
 import useImages from 'src/hooks/useImages';
 
@@ -20,14 +22,31 @@ export const ImagesPricingBanner: React.FC<{}> = (_) => {
   const flags = useFlags();
   const classes = useStyles();
 
-  if (images.results === 0 || !flags.imagesPricingBanner) {
+  const bannerHasContents = checkFlagsForImagesPricingBanner(flags);
+
+  if (images.results === 0 || !bannerHasContents) {
     return null;
   }
 
-  const bannerLede = flags.imagesPricingBanner.lede;
-  const bannerCoreText = flags.imagesPricingBanner.text;
-  const bannerLinkUrl = flags.imagesPricingBanner.link.url;
-  const bannerLinkText = flags.imagesPricingBanner.link.text;
+  const bannerCoreText = flags.imagesPricingBanner?.text;
+
+  const bannerLede = flags.imagesPricingBanner?.lede ?? '';
+  const bannerLinkUrl = flags.imagesPricingBanner?.link?.url ?? '';
+  const bannerLinkText = flags.imagesPricingBanner?.link?.text ?? '';
+
+  const generateLede = () => {
+    return bannerLede ? (
+      <span>
+        <strong>{bannerLede}</strong>:{' '}
+      </span>
+    ) : null;
+  };
+
+  const generateLinkifiedText = () => {
+    return bannerLinkUrl && bannerLinkText ? (
+      <Link to={bannerLinkUrl}>{bannerLinkText}</Link>
+    ) : null;
+  };
 
   return (
     <DismissibleBanner
@@ -35,11 +54,20 @@ export const ImagesPricingBanner: React.FC<{}> = (_) => {
       preferenceKey="images-pricing-notification"
     >
       <Typography>
-        <strong>{bannerLede}</strong>: {bannerCoreText}{' '}
-        <Link to={bannerLinkUrl}>{bannerLinkText}</Link>.
+        {generateLede()}
+        {bannerCoreText}
+        {generateLinkifiedText()}
       </Typography>
     </DismissibleBanner>
   );
 };
 
 export default ImagesPricingBanner;
+
+export const checkFlagsForImagesPricingBanner = (flags: FlagSet) => {
+  return Boolean(
+    flags.imagesPricingBanner &&
+      !isEmpty(flags.imagesPricingBanner) &&
+      flags.imagesPricingBanner.key
+  );
+};
