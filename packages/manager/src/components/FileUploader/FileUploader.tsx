2d2967fdfe7ad465b1cc258aa5271ecc9e3b17b2
Dajahi Wiley <dwiley@linode.com>
2021-05-18 17:04:04 -0400

    Functional drag-and-drop uploading & uploading via Browse; some cleanup

--- packages/manager/src/components/FileUploader/FileUploader.tsx
+++ packages/manager/src/components/FileUploader/FileUploader.tsx
@@ -1,4 +1,3 @@
-import { getObjectURL } from '@linode/api-v4/lib/object-storage';
 import * as classNames from 'classnames';
 import { withSnackbar, WithSnackbarProps } from 'notistack';
 import * as React from 'react';
@@ -8,15 +7,12 @@ import Button from 'src/components/Button';
 import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
 import { readableBytes } from 'src/utilities/unitConversions';
-import { debounce } from 'throttle-debounce';
-// import { uploadObject } from 'src/features/ObjectStorage/requests';
 import { uploadImageFile } from 'src/features/Images/requests';
 import FileUpload from 'src/features/ObjectStorage/ObjectUploader/FileUpload';
 import {
   curriedObjectUploaderReducer,
   defaultState,
   MAX_FILE_SIZE_IN_BYTES,
-  MAX_NUM_UPLOADS,
   MAX_PARALLEL_UPLOADS,
   ObjectUploaderAction,
   pathOrFileName,
@@ -120,6 +116,9 @@ type CombinedProps = Props & WithSnackbarProps;
 const FileUploader: React.FC<CombinedProps> = (props) => {
   const { label, description, region } = props;
 
+  const [error, setError] = React.useState<APIError[] | undefined>();
+  const [uploadToURL, setUploadToURL] = React.useState<string>('');
+
   const classes = useStyles();
 
   const [state, dispatch] = React.useReducer(
@@ -127,61 +126,19 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
     defaultState
   );
 
-  const [error, setError] = React.useState<APIError[] | undefined>();
-
   const dispatchAction: Dispatch = useDispatch();
 
-  // This function is fired when objects are dropped in the upload area.
-  const onDrop = (file: File) => {
-    dispatchAction(
-      uploadImage({
-        label,
-        description: description || undefined,
-        region,
-      })
-    )
-      .then((response) => {
-        // console.log(response);
-        const uploadUrl = response.upload_to;
-        console.log(uploadUrl);
-
-        uploadImageFile(uploadUrl, file, () => console.log('Processing...'))
-          .then(() => console.log('Successfully uploaded to URL'))
-          .catch(() => console.log('Error uploading to URL'));
-      })
-      .catch((e) => {
-        console.log(e);
-        setError(e);
-      });
-    // Look at the files queued and in-progress, along with the new files,
-    // to see if we'll go over the limit.
-    if (state.numInProgress + state.numQueued + 1 > MAX_NUM_UPLOADS) {
-      props.enqueueSnackbar(`Upload up to ${MAX_NUM_UPLOADS} files at a time`, {
-        variant: 'error',
-      });
-      return;
-    }
-
-    // @analytics
-    // sendObjectsQueuedForUploadEvent(files.length);
-
-    // We bind each file to the prefix at the time of onDrop. The prefix could
-    // change later, if the user navigates to a different folder before the
-    // upload is complete.
-    const files = [file];
+  // This function is fired when files are dropped in the upload area.
+  const onDrop = (files: File[]) => {
     const prefix = '';
     dispatch({ type: 'ENQUEUE', files, prefix });
   };
 
   // This function will be called for dropped files that are over the max size.
-  const onDropRejected = (files: File[]) => {
-    let errorMessage = `Max file size (${
+  const onDropRejected = () => {
+    const errorMessage = `Max file size (${
       readableBytes(MAX_FILE_SIZE_IN_BYTES).formatted
     }) exceeded`;
-
-    if (files.length > 1) {
-      errorMessage += ' for some files';
-    }
 
     props.enqueueSnackbar(errorMessage, {
       variant: 'error',
@@ -189,7 +146,7 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
   };
 
   const nextBatch = React.useMemo(() => {
-    if (state.numQueued === 0 || state.numInProgress >= MAX_PARALLEL_UPLOADS) {
+    if (state.numQueued === 0 || state.numInProgress > 0) {
       return [];
     }
 
@@ -198,16 +155,7 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
     );
 
     return queuedUploads.slice(0, MAX_PARALLEL_UPLOADS - state.numInProgress);
-  }, [state.numQueued, state.numInProgress]);
-
-  //   const debouncedGetBucket = React.useRef(
-  //     debounce(400, false, () =>
-  //       props
-  //         .getBucket({ cluster: props.clusterId, label: props.bucketName })
-  //         // It's OK to swallow the error here, since this request is for a silent UI update.
-  //         .catch((_) => null)
-  //     )
-  //   ).current;
+  }, [state.numQueued, state.numInProgress, state.files]);
 
   // When `nextBatch` changes, upload the files.
   React.useEffect(() => {
@@ -228,30 +176,10 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
       const { file } = fileUpload;
 
       const path = pathOrFileName(fileUpload.file);
-      //       const isInFolder = path.startsWith('/');
-
-      //       // We want to upload the object to the current "folder" the user is
-      //       // viewing, so we prepend the file name with the prefix. If the object
-      //       // being uploaded is itself in a folder, we drop the leading slash.
-      //       //
-      //       // We need to use the prefix on each object (bound at onDrop) because the
-      //       // prefix from the parent might have changed if the user has navigated to
-      //       // a different folder.
-      //       const fullObjectName =
-      //         fileUpload.prefix + (isInFolder ? path.substring(1) : path);
 
       const onUploadProgress = onUploadProgressFactory(dispatch, path);
 
       const handleSuccess = () => {
-        //         // Request the Bucket again so the updated size is reflected on the Bucket Landing page.
-        //         // This request is debounced since many Objects can be uploaded at once.
-        //         debouncedGetBucket();
-
-        //         // We may want to add the object to the table, depending on the prefix
-        //         // the user is currently viewing. Do this in the parent, which has the
-        //         // current prefix in scope.
-        //         props.maybeAddObjectToTable(fullObjectName, file.size);
-
         dispatch({
           type: 'UPDATE_FILES',
           filesToUpdate: [path],
@@ -272,41 +200,30 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
         });
       };
 
-      // If we've already gotten the URL (i.e. we've confirmed this file should
-      // be overwritten) we can go ahead and upload it.
-      if (fileUpload.url) {
-        uploadImageFile(fileUpload.url, file, onUploadProgress)
-          .then((_) => handleSuccess())
-          .catch((_) => handleError());
-      } else {
-        handleError();
-        console.log('Error: need to handle case of no URL');
-        //   // Otherwise, we need to make an API request to get the URL.
-        //   dispatchAction(
-        //     uploadImage({
-        //       label,
-        //       description: description || undefined,
-        //       region,
-        //     })
-        //   )
-        //   getObjectURL(clusterId, bucketName, fullObjectName, 'PUT', {
-        //     content_type: file.type,
-        //   })
-        //     .then(({ url, exists }) => {
-        //       if (exists) {
-        //         dispatch({
-        //           type: 'NOTIFY_FILE_EXISTS',
-        //           fileName: path,
-        //           url,
-        //         });
-        //         return;
-        //       }
+      if (!uploadToURL) {
+        dispatchAction(
+          uploadImage({
+            label,
+            description: description || undefined,
+            region,
+          })
+        )
+          .then((response) => {
+            setUploadToURL(response.upload_to);
 
-        //       return uploadImageFile(url, file, onUploadProgress)
-        //         .then((_) => handleSuccess())
-        //         .catch((_) => handleError());
-        //     })
-        //     .catch((_) => handleError());
+            uploadImageFile(response.upload_to, file, onUploadProgress) // response.upload_to used here instead of uploadToURL b/c of race condition
+              .then(() => handleSuccess())
+              .catch(() => handleError());
+          })
+          .catch((e) => {
+            console.log(e);
+            setError(e);
+          });
+      } else {
+        // Overwrite any file that was previously uploaded to the upload_to URL.
+        uploadImageFile(uploadToURL, file, onUploadProgress)
+          .then(() => handleSuccess())
+          .catch(() => handleError());
       }
     });
   }, [nextBatch]);
@@ -323,6 +240,7 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
     onDropRejected,
     noClick: true,
     noKeyboard: true,
+    accept: '.gz', // Uploaded files must be compressed using the gzip compression algorithm.
     maxSize: MAX_FILE_SIZE_IN_BYTES,
   });
 
@@ -354,19 +272,18 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
 
         <div className={classes.fileUploads}>
           {state.files.map((upload, idx) => {
-            const path = (upload.file as any).path || upload.file.name;
+            const fileName = upload.file.name;
             return (
               <FileUpload
                 key={idx}
-                displayName={upload.file.name}
-                fileName={path}
+                displayName={fileName}
+                fileName={fileName}
                 sizeInBytes={upload.file.size || 0}
                 percentCompleted={upload.percentComplete || 0}
                 overwriteNotice={upload.status === 'OVERWRITE_NOTICE'}
-                url={upload.url}
                 dispatch={dispatch}
                 error={
-                  upload.status === 'ERROR' ? 'Error uploading object.' : ''
+                  upload.status === 'ERROR' ? 'Error uploading image.' : ''
                 }
               />
             );
@@ -388,6 +305,7 @@ const FileUploader: React.FC<CombinedProps> = (props) => {
             buttonType="primary"
             onClick={open}
             className={classes.uploadButton}
+            disabled={state.numInProgress > 0}
           >
             Browse Files
           </Button>
