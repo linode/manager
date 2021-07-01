a6d2b486d2e6ad0c0cbeffc2ab83471418aa3150
Tiffany Wong <wong.tyan@gmail.com>
2021-07-01 11:54:51 -0400

    Address feedback

--- packages/manager/src/features/Profile/Referrals/Referrals.tsx
+++ packages/manager/src/features/Profile/Referrals/Referrals.tsx
@@ -115,7 +115,7 @@ export const Referrals: React.FC<CombinedProps> = (props) => {
     credit,
   } = props;
 
-  const allowReferral = url !== '';
+  const allowReferral = Boolean(url);
 
   return (
     <Paper>
