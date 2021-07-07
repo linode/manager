2017ea2858d8f2231c72b5a106251d9aa6ced714
Banks Nussman <banks@nussman.us>
2021-07-07 16:30:57 -0400

    Storybook can now read .mdx files (#7745)
    * Initial commit
    
    * Working mdx
    
    * Removed unneeded config
    
    * Use correct imports and add md example
    
    * Fixed SVGs and use proper font
    
    Co-authored-by: Tiffany Wong <wong.tyan@gmail.com>

--- packages/manager/.storybook/preview.tsx
+++ packages/manager/.storybook/preview.tsx
@@ -1,5 +1,7 @@
 // .storybook/preview.js
 import { select, withKnobs } from '@storybook/addon-knobs';
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { configure } from '@storybook/react';
 import React from 'react';
 import '../public/fonts/fonts.css';
 import CssBaseline from '../src/components/core/CssBaseline';
@@ -7,12 +9,13 @@ import { ThemeProvider } from '../src/components/core/styles';
 import '../src/index.css';
 import { dark, light } from '../src/themes';
 import { wrapWithTheme } from '../src/utilities/testHelpers';
-import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const options = {
   dark,
   light,
 };
+
+configure(require.context('../src', true, /\.stories\.mdx$/), module);
 
 export const decorators = [
   withKnobs,
