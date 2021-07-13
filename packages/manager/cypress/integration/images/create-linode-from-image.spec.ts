06086ab4764339fd741846a4a7778dbad5e11321
ndukatz <ndukatz@linode.com>
2021-07-13 14:07:10 -0400

    a few test additions

--- packages/manager/cypress/integration/images/create-linode-from-image.spec.ts
+++ packages/manager/cypress/integration/images/create-linode-from-image.spec.ts
@@ -1,8 +1,10 @@
 import { mockImage } from 'cypress/support/api/images';
 import { containsClick, fbtClick, getClick } from 'cypress/support/helpers';
 import { contains } from 'cypress/types/jquery';
+import strings from '../../support/cypresshelpers';
 
 const imageLabel = 'cy-test-image';
+const rootpass = strings.randomPass();
 
 describe('create linode from image', () => {
   it('creates linode from image on images tab', () => {
@@ -24,5 +26,7 @@ describe('create linode from image', () => {
     containsClick('Fremont, CA');
 
     getClick('[id="g6-nanode-1"][type="radio"]');
+    cy.get('[id="root-password"]').type(rootpass);
+    getClick('[data-qa-deploy-linode="true"]');
   });
 });
