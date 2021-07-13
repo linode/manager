2e01c8d7a370981237ee127d475e4e666fe8879c
codyfinn <codyfinn@protonmail.com>
2021-07-13 11:27:03 -0400

    added feedback while script runs and added info in docs

--- getting_started.sh
+++ getting_started.sh
@@ -40,15 +40,18 @@ cat << EOF
 
 EOF
 
-Install homebrew https://brew.sh
+echo "Installling homebrew from https://brew.sh"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update --force
 # Use brew bundle to install git, volta, and github cli
+echo "installing Git, Volta, and Github CLI using homebrew"
 brew bundle --no-lock --file=- <<EOF
      brew "git"
      brew "volta"
      brew "gh"
 EOF
+
+echo "Adding paths to your zsh and bash configs"
 
 bash_config=".bash_profile"
 zsh_config=".zshrc"
@@ -57,7 +60,9 @@ echo export PATH='$VOLTA_HOME/bin:$PATH' >> "$HOME/$bash_config"
 echo export VOLTA_HOME='$HOME/.volta' >> "$HOME/$zsh_config"
 echo export PATH='$VOLTA_HOME/bin:$PATH' >> "$HOME/$zsh_config"
 
+echo "Installing Nodejs v14.15.4"
 volta install node@14.15.4
+echo "Installing Yarn"
 volta install yarn@1
 if [ "$1" == '--no-fork' ]; then
     echo "You are ready to run and make changes to the cloud manager!"
