8d1c1c3fd12b5bfd0c5fa2649642c684f5b4e417
ndukatz <ndukatz@linode.com>
2021-07-22 14:33:45 -0400

    add test for creating linode from image page

--- packages/manager/cypress/integration/images/create-linode-from-image.spec.ts
+++ packages/manager/cypress/integration/images/create-linode-from-image.spec.ts
@@ -5,6 +5,7 @@ import {
   fbtClick,
   fbtVisible,
   getClick,
+  getVisible,
 } from 'cypress/support/helpers';
 import strings from '../../support/cypresshelpers';
 
@@ -23,42 +24,52 @@ const mockLinode = createMockLinode({
   type,
 });
 
+const createLinodeWithImageMock = () => {
+  cy.intercept('*/images*', (req) => {
+    req.reply(createMockImage());
+  }).as('mockImage');
+  cy.intercept('POST', '*/linode/instances', (req) => {
+    req.reply({
+      body: mockLinode,
+      headers: { image: imageId },
+    });
+  }).as('mockLinodeRequest');
+  cy.intercept('GET', `*/linode/instances/${linodeId}`, (req) => {
+    req.reply(mockLinode);
+  }).as('mockLinodeResponse');
+
+  cy.visitWithLogin('/linodes/create?type=Images');
+  cy.wait('@mockImage');
+  cy.get('[data-qa-enhanced-select="Choose an image"]').within(() => {
+    containsClick('Choose an image');
+  });
+  getVisible('span[class="fl-tux"]');
+  fbtClick(imageLabel);
+  getClick('[data-qa-enhanced-select="Select a Region"]').within(() => {
+    containsClick('Select a Region');
+  });
+  containsClick(regionSelect);
+  getClick('[id="g6-nanode-1"][type="radio"]');
+  cy.get('[id="root-password"]').type(rootpass);
+  getClick('[data-qa-deploy-linode="true"]');
+
+  cy.wait('@mockLinodeRequest');
+  cy.wait('@mockLinodeResponse');
+
+  fbtVisible(mockLinode.label);
+  fbtVisible(regionSelect);
+  fbtVisible(linodeId);
+};
+
 describe('create linode from image, mocked data', () => {
-  it('creates linode from image on images tab', () => {
-    // const linode2 = createMockLinode();
+  it.skip('creates linode from image on images tab', () => {
+    createLinodeWithImageMock();
+  });
 
-    cy.intercept('*/images*', (req) => {
-      req.reply(createMockImage());
-    }).as('mockImage');
-    cy.intercept('POST', '*/linode/instances', (req) => {
-      req.reply({
-        body: mockLinode,
-        headers: { image: imageId },
-      });
-    }).as('mockLinodeRequest');
-    cy.intercept('GET', `*/linode/instances/${linodeId}`, (req) => {
-      req.reply(mockLinode);
-    }).as('mockLinodeResponse');
-
-    cy.visitWithLogin('/linodes/create?type=Images');
-    cy.wait('@mockImage');
-    cy.get('[data-qa-enhanced-select="Choose an image"]').within(() => {
-      containsClick('Choose an image');
-    });
-    fbtClick(imageLabel);
-    getClick('[data-qa-enhanced-select="Select a Region"]').within(() => {
-      containsClick('Select a Region');
-    });
-    containsClick(regionSelect);
-    getClick('[id="g6-nanode-1"][type="radio"]');
-    cy.get('[id="root-password"]').type(rootpass);
-    getClick('[data-qa-deploy-linode="true"]');
-
-    cy.wait('@mockLinodeRequest');
-    cy.wait('@mockLinodeResponse');
-
-    fbtVisible(mockLinode.label);
-    fbtVisible(regionSelect);
-    fbtVisible(linodeId);
+  it('creates linode from image on image page', () => {
+    cy.visitWithLogin(
+      `/linodes/create/?type=Images&imageID=private/${imageId}`
+    );
+    createLinodeWithImageMock();
   });
 });
