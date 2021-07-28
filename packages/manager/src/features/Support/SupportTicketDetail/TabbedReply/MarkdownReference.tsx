3f9c110395a819ef25e662abdcb819cb156a3b80
Banks Nussman <banks@nussman.us>
2021-07-28 11:12:32 -0400

    Made same changes to reply

--- packages/manager/src/features/Support/SupportTicketDetail/TabbedReply/MarkdownReference.tsx
+++ packages/manager/src/features/Support/SupportTicketDetail/TabbedReply/MarkdownReference.tsx
@@ -21,7 +21,6 @@ const styles = (theme: Theme) =>
         theme.name === 'darkTheme' ? theme.bg.white : theme.bg.offWhite,
       margin: `${theme.spacing(2)}px 0`,
       padding: theme.spacing(2),
-      // border: `1px solid ${theme.color.border2}`
     },
   });
 
