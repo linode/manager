04fd6024172b151a74fb9c4e2dc11ddb6cfa8eaa
Nate <66748958+ndukatz@users.noreply.github.com>
2021-08-24 12:21:09 -0400

    M3-5418 Swap shared and dedicated cpu tabs (#7877)
    * swapping dedicated and shared cpu tabs
    
    * changed dedicated to standard, updated linode type class
    
    * updated qa attributes
    
    * I was going to switch dedicated to standard but realized api still has shared as standard
    
    * switching qa tags back
    
    * adding test updates for this change

--- packages/manager/src/features/linodes/LinodesCreate/SelectPlanPanel.tsx
+++ packages/manager/src/features/linodes/LinodesCreate/SelectPlanPanel.tsx
@@ -348,24 +348,6 @@ export class SelectPlanPanel extends React.Component<CombinedProps> {
 
     const shared = [...nanodes, ...standards];
 
-    if (!isEmpty(shared)) {
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Typography data-qa-standard className={classes.copy}>
-                Shared CPU instances are good for medium-duty workloads and are
-                a good mix of performance, resources, and price.
-              </Typography>
-              {this.renderPlanContainer(shared)}
-            </>
-          );
-        },
-        title: 'Shared CPU',
-      });
-      tabOrder.push('standard');
-    }
-
     if (!isEmpty(dedicated)) {
       tabs.push({
         render: () => {
@@ -382,6 +364,24 @@ export class SelectPlanPanel extends React.Component<CombinedProps> {
         title: 'Dedicated CPU',
       });
       tabOrder.push('dedicated');
+    }
+
+    if (!isEmpty(shared)) {
+      tabs.push({
+        render: () => {
+          return (
+            <>
+              <Typography data-qa-standard className={classes.copy}>
+                Shared CPU instances are good for medium-duty workloads and are
+                a good mix of performance, resources, and price.
+              </Typography>
+              {this.renderPlanContainer(shared)}
+            </>
+          );
+        },
+        title: 'Shared CPU',
+      });
+      tabOrder.push('standard');
     }
 
     if (!isEmpty(highmem)) {
