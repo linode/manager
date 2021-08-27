621cf64b28b091c29a94bf1a51f9bbe13e7db505
Hana Xu <hanaxu12@gmail.com>
2021-08-27 16:14:32 -0400

    address feedback

--- packages/manager/src/queries/accountAgreements.ts
+++ packages/manager/src/queries/accountAgreements.ts
@@ -1,11 +1,7 @@
-import {
-  Agreements,
-  AgreementType,
-  getAccountAgreements,
-} from '@linode/api-v4/lib/account';
+import { Agreements, getAccountAgreements } from '@linode/api-v4/lib/account';
 import { APIError } from '@linode/api-v4/lib/types';
 import { useQuery } from 'react-query';
-import { queryClient, queryPresets } from './base';
+import { queryPresets } from './base';
 
 export const queryKey = 'account-agreements';
 
@@ -15,6 +11,3 @@ export const useAccountAgreements = () =>
     getAccountAgreements,
     queryPresets.oneTimeFetch
   );
-
-export const isAgreementSigned = (agreement: AgreementType) =>
-  Boolean(queryClient.getQueryData<Agreements>(queryKey)![agreement]);
