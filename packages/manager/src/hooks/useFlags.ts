321133966190cac0cc51df48ec8bd325a838a933
Dajahi Wiley <dwiley@linode.com>
2021-08-30 12:04:27 -0400

    Remove temp code

--- packages/manager/src/hooks/useFlags.ts
+++ packages/manager/src/hooks/useFlags.ts
@@ -30,11 +30,6 @@ export const useFlags = () => {
     // !!! Override the CMR flag so that it's officially "released" !!!!
     // @todo: clean up all conditional logic based on this flag.
     cmr: true,
-    // imagesPricingCopy: {
-    //   captureImage: '',
-    //   uploadImage: '',
-    // },
-    // imagesPriceInfo: false,
     ...mockFlags,
   };
 };
