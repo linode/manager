2b99ae978fa305d1313d7e76d5e3c8cd159c9232
John Callahan <johnwcallahan@gmail.com>
2021-09-03 09:23:40 -0400

    update factory

--- packages/manager/src/factories/featureFlags.ts
+++ packages/manager/src/factories/featureFlags.ts
@@ -6,7 +6,7 @@ export const productInformationBannerFactory = Factory.Sync.makeFactory<ProductI
     key: Factory.each((i) => `product-information-banner-${i}`),
     // safe
     message:
-      'Store critical data and media files with S3-Compatible Object Storage. <a href="https://www.linode.com/docs/products/storage/object-storage" target="_blank">New Availability: Atlanta</a>',
+      'Store critical data and media files with S3-Compatible Object Storage. [New Availability: Atlanta](https://www.linode.com/docs/products/storage/object-storage/)',
     bannerLocation: 'Object Storage',
   }
 );
