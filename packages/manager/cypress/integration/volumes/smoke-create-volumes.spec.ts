38fe6554cecaf8a004540819de8cbd5fa3e85244
ndukatz <ndukatz@linode.com>
2021-09-03 15:27:09 -0400

    refactor of older volumes tests with mocks, added a new test to create volume from linode with block storage (Atlanta), fixed inability to add tags to volumes through linode with block storage

--- packages/manager/cypress/integration/volumes/smoke-create-volumes.spec.ts
+++ packages/manager/cypress/integration/volumes/smoke-create-volumes.spec.ts
@@ -1,62 +1,41 @@
 /* eslint-disable sonarjs/no-duplicate-string */
-import {
-  makeVolumeLabel,
-  clickVolumeActionMenu,
-  createVolume,
-} from '../../support/api/volumes';
+import { clickVolumeActionMenu } from '../../support/api/volumes';
 import { assertToast } from '../../support/ui/events';
-import { createLinode } from '../../support/api/linodes';
+import { createMockLinode } from '../../support/api/linodes';
 import { selectRegionString } from '../../support/ui/constants';
 import {
-  containsClick,
   containsVisible,
   fbtClick,
   fbtVisible,
   getClick,
   getVisible,
 } from '../../support/helpers';
+import { makeResourcePage } from '@src/mocks/serverHandlers';
+import { eventFactory, tagFactory, volumeFactory } from '@src/factories';
+import { Method } from 'axios';
+import { RouteMatcher } from 'cypress/types/net-stubbing';
 
-const urlExtension = '/volumes/create';
-const tag = 'cy-test';
 const region = 'Newark, NJ';
-const clickCreate = () => {
-  getClick('[data-qa-deploy-linode]');
-};
-const getVolumeLabelInput = () => {
-  return cy.findAllByLabelText('Label (required)');
-};
+
 const clickDetach = () => {
   getClick('[data-qa-action-menu-item="Detach"]:visible');
 };
 
-const createBasicVolume = (linodeLabel?: string) => {
-  const volLabel = makeVolumeLabel();
-  // catch create volume post
-  cy.intercept('POST', '*/volumes').as('volumeCreated');
-  cy.visitWithLogin(urlExtension);
-  cy.findByText('volumes');
-  clickCreate();
-  fbtVisible('Label is required.');
-  getVolumeLabelInput().type(volLabel);
-  /* validating that volume won't create without region.
-       region not necessary if attaching linode */
-  clickCreate();
-  fbtVisible('Must provide a region or a Linode ID.');
-  if (linodeLabel) {
-    fbtClick('Select a Linode').type(`${linodeLabel} {enter}`);
-    cy.findByText('My Debian 10 Disk Profile');
-  } else {
-    fbtClick(selectRegionString).type('new {enter}');
-  }
-  fbtClick('Type to choose or create a tag.').type(`${tag} {enter}`);
-  clickCreate();
-  return cy.wait('@volumeCreated').then((xhr) => {
-    expect(xhr.response?.statusCode).to.equal(200);
-    return { label: volLabel, id: xhr.response?.body['id'] };
-  });
-};
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-const validateBasicVolume = (volLabel: string, volId: string) => {
+const validateBasicVolume = (
+  volLabel: string,
+  volId: number,
+  isAttached: boolean = false
+) => {
+  let attached = 'Unattached';
+  if (isAttached) {
+    attached = 'Attached';
+  }
   containsVisible('Volume Configuration');
   cy.findByDisplayValue(`mkdir "/mnt/${volLabel}"`);
   getClick('[data-qa-close-drawer="true"]');
@@ -66,40 +45,143 @@ const validateBasicVolume = (volLabel: string, volId: string) => {
     fbtVisible(region);
   });
   cy.get(`[data-qa-volume-cell="${volId}"]`).within(() => {
-    fbtVisible('Unattached');
+    fbtVisible(attached);
   });
 };
 
+const tagList = makeResourcePage(tagFactory.buildList(5));
+const tag = tagList.data[getRandomNumber(0, 4)];
+const tagLabel = tag.label;
+const linodeList = createMockLinode(3, { region: 'us-southeast' });
+const linode = linodeList.data[1];
+const linodeLabel = linode.label;
+const linodeId = linode.id;
+const volumeList = makeResourcePage(volumeFactory.buildList(2));
+const volume = volumeList.data[1];
+const volumeLabel = volume.label;
+const volumeId = volume.id;
+
+const interceptOnce = (method: Method, url: RouteMatcher, response: {}) => {
+  let count = 0;
+  return cy.intercept(method, url, (req) => {
+    count += 1;
+    if (count < 2) {
+      req.reply(response);
+    }
+  });
+};
+const getIntercepts = () => {
+  cy.intercept('GET', `*/tags`, (req) => {
+    req.reply(tagList);
+  }).as('getTags');
+  // cy.intercept('GET', '*/volumes', (req) => {
+  //   req.reply(volumeList);
+  // }).as('getVolumes');
+  cy.intercept('POST', `*/volumes`, (req) => {
+    req.reply(volume);
+  }).as('createVolume');
+};
+
 describe('volumes', () => {
-  it('creates a volume without linode', () => {
-    createBasicVolume().then(({ label, id }) => {
-      validateBasicVolume(label, id);
-      clickVolumeActionMenu(label);
-      getVisible('[data-qa-action-menu-item="Delete"]');
+  it('creates a volume with tag but without linode from volumes page', () => {
+    getIntercepts();
+    cy.visitWithLogin('/volumes');
+    fbtClick('Create Volume');
+    cy.wait('@getTags');
+    cy.findByText('volumes');
+    fbtClick('Create Volume');
+    fbtVisible('Label is required.');
+    getClick('[id="label"][data-testid="textfield-input"]').type(volumeLabel);
+    fbtClick('Create Volume');
+    fbtVisible('Must provide a region or a Linode ID.');
+    fbtClick(selectRegionString).type('new {enter}');
+    getClick('[data-qa-multi-select="Type to choose or create a tag."]');
+    fbtClick(tagLabel);
+    fbtClick('Create Volume');
+    cy.wait('@createVolume');
+    validateBasicVolume(volumeLabel, volumeId);
+    clickVolumeActionMenu(volumeLabel);
+    getVisible('[data-qa-action-menu-item="Delete"]');
+  });
+
+  it('creates volume with tag from linode with block storage support (Atlanta)', () => {
+    cy.intercept('GET', '*/linode/instances/*', (req) => {
+      req.reply(linodeList);
+    }).as('getLinodes');
+    cy.intercept('GET', `*/linode/instances/${linodeId}*`, (req) => {
+      req.reply(linode);
+    }).as('getLinodeDetail');
+    getIntercepts();
+    cy.visitWithLogin('/linodes');
+    cy.wait('@getLinodes');
+    fbtClick(linodeLabel);
+    cy.wait('@getLinodeDetail');
+    getVisible(
+      '[href="https://www.linode.com/products/block-storage/"]'
+    ).within(() => {
+      fbtVisible('NVMe Block Storage');
     });
+    fbtClick('Create a Volume');
+    cy.wait('@getTags');
+    getVisible(`[data-qa-drawer-title="Create Volume for ${linodeLabel}"]`);
+    getClick('[data-qa-volume-label="true"]').type('cy-test-volume');
+    getClick('[data-qa-multi-select="Type to choose or create a tag."]');
+    fbtClick(tagLabel);
+    getClick('[data-qa-submit="true"]');
+    cy.wait('@createVolume');
+    containsVisible('Volume Configuration');
+    cy.findByDisplayValue(`mkdir "/mnt/${volumeLabel}"`);
   });
 
   it('Detaches attached volume', () => {
+    const attachedVolumeList = makeResourcePage(
+      volumeFactory.buildList(2, { linode_id: linodeId })
+    );
+    const attachedVolume = attachedVolumeList.data[1];
+    const attachedVolumeLabel = attachedVolume.label;
+    const attachedVolumeId = attachedVolume.id;
+    interceptOnce('GET', `*/volumes*`, attachedVolumeList).as(
+      'getAttachedVolumes'
+    );
+    cy.intercept('GET', '*/linode/instances/*', (req) => {
+      req.reply(linodeList);
+    }).as('getLinodes');
+    interceptOnce(
+      'GET',
+      '*/account/events*',
+      makeResourcePage(
+        eventFactory.buildList(1, {
+          action: 'volume_detach',
+          percent_complete: 100,
+          entity: {
+            label: attachedVolumeLabel,
+            id: attachedVolumeId,
+            type: 'volume',
+            url: `/v4/volumes/${attachedVolumeId}`,
+          },
+          status: 'finished',
+          secondary_entity: null,
+          message: '',
+        })
+      )
+    ).as('getEvent');
+
+    getIntercepts();
     cy.visitWithLogin('/volumes');
-    createLinode().then((linode) => {
-      const linodeId = linode.id;
-      const linodeLabel = linode.label;
-      createVolume(linodeId).then((volume) => {
-        const volumeLabel = volume.label;
-        // catch detach volume post
-        cy.intercept('POST', '*/volumes/' + volume.id + '/detach').as(
-          'volumeDetached'
-        );
-        cy.reload();
-        containsVisible(linodeLabel);
-        containsVisible(volumeLabel);
-        clickVolumeActionMenu(volume.label);
-        clickDetach();
-        cy.contains(`Detach Volume ${volumeLabel}?`);
-        getClick('[data-qa-confirm="true"]');
-        cy.wait('@volumeDetached').its('response.statusCode').should('eq', 200);
-        assertToast('Volume detachment started', 2);
-      });
-    });
+    cy.wait('@getLinodes');
+    cy.wait('@getAttachedVolumes');
+    cy.intercept('POST', '*/volumes/' + attachedVolumeId + '/detach', (req) => {
+      req.reply({ statusCode: 200 });
+    }).as('volumeDetached');
+    containsVisible(linodeLabel);
+    containsVisible(attachedVolumeLabel);
+    clickVolumeActionMenu(attachedVolumeLabel);
+    clickDetach();
+    cy.contains(`Detach Volume ${attachedVolumeLabel}?`);
+    getClick('[data-qa-confirm="true"]');
+    cy.wait('@volumeDetached').its('response.statusCode').should('eq', 200);
+    cy.wait('@getEvent');
+    assertToast('Volume detachment started', 2);
+    // fbtVisible('Unnattached');
   });
 });
