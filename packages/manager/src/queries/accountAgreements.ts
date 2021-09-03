8488fa6f464fc49686e3d9ef6c06665ebcb66e19
John Callahan <johnwcallahan@gmail.com>
2021-09-03 09:42:00 -0400

    M3-5403: Functionality for EU Contract on Volumes Create (#7912)
    Adds functionality for the GDPR agreement on the Volumes Create page.

--- packages/manager/src/queries/accountAgreements.ts
+++ packages/manager/src/queries/accountAgreements.ts
@@ -5,6 +5,7 @@ import {
 } from '@linode/api-v4/lib/account';
 import { APIError } from '@linode/api-v4/lib/types';
 import { useMutation, useQuery } from 'react-query';
+import { reportException } from 'src/exceptionReporting';
 import { queryPresets, simpleMutationHandlers } from './base';
 
 export const queryKey = 'account-agreements';
@@ -21,4 +22,16 @@ export const useMutateAccountAgreements = () => {
     (data) => signAgreement(data),
     simpleMutationHandlers<Agreements, Partial<Agreements>>(queryKey)
   );
+};
+
+export const reportAgreementSigningError = (err: any) => {
+  let customErrorMessage =
+    'Excepted to sign the EU agreement, but the request resulted in an error';
+  const apiErrorMessage = err?.[0]?.reason;
+
+  if (apiErrorMessage) {
+    customErrorMessage += `: ${apiErrorMessage}`;
+  }
+
+  reportException(customErrorMessage);
 };
