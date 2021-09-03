c51ae913c4f068faa922f34e34a9171353910fdc
John Callahan <johnwcallahan@gmail.com>
2021-09-03 09:02:27 -0400

    use 'safe' instead of disabling the eslint rule

--- packages/manager/src/factories/featureFlags.ts
+++ packages/manager/src/factories/featureFlags.ts
@@ -4,7 +4,7 @@ import { ProductInformationBanner } from 'src/featureFlags';
 export const productInformationBannerFactory = Factory.Sync.makeFactory<ProductInformationBanner>(
   {
     key: Factory.each((i) => `product-information-banner-${i}`),
-    // eslint-disable-next-line xss/no-mixed-html
+    // safe
     message:
       'Store critical data and media files with S3-Compatible Object Storage. <a href="https://www.linode.com/docs/products/storage/object-storage" target="_blank">New Availability: Atlanta</a>',
     bannerLocation: 'Object Storage',
