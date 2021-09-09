1a2cc6f89354acbf0b986d263b91dffd480c2799
ndukatz <ndukatz@linode.com>
2021-09-09 16:18:21 -0400

    checks sorting by label and region

--- packages/manager/cypress/integration/linodes/smoke-linode-landing-table.spec.ts
+++ packages/manager/cypress/integration/linodes/smoke-linode-landing-table.spec.ts
@@ -31,9 +31,7 @@ const linodeLabel = (number) => {
 
 const deleteLinodeFromActionMenu = (linodeLabel) => {
   getClick(`[aria-label="Action menu for Linode ${linodeLabel}"]`);
-  // the visible filter is to ignore all closed action menus
   cy.get(`[data-qa-action-menu-item="Delete"]`).filter(`:visible`).click();
-  // There is 2 visible delete on the page, this is why i used this strategy
   cy.findAllByRole('button').filter(':contains("Delete")').click();
   cy.wait('@deleteLinode').its('response.statusCode').should('eq', 200);
 };
