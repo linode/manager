b524b2c8edbbb1132faf2a80b05eb46a26917e91
John Callahan <johnwcallahan@gmail.com>
2021-09-17 09:46:24 -0400

    M3-5431 EU Model Contract agreement banner + notification  (#7918)
    This adds two things:
    
    1. Banner indicating the user must accept the EU Model Contract agreement
    2/ Handling for the EU Model Contract agreement notification
    
    The new modal component sits in the GlobalNotifications wrapper component, and context is used in order to control the component from across the app. 
    
    I also did a slight refactor of the DismissibleBanner component to be able to use the functionality from another component.

--- packages/manager/src/components/ConfirmationDialog/index.ts
+++ packages/manager/src/components/ConfirmationDialog/index.ts
@@ -1,2 +1,7 @@
-import ConfirmationDialog from './ConfirmationDialog';
+import ConfirmationDialog, {
+  Props as _ConfirmationDialogProps,
+} from './ConfirmationDialog';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ConfirmationDialogProps extends _ConfirmationDialogProps {}
 export default ConfirmationDialog;
--- packages/manager/src/features/GlobalNotifications/GlobalNotifications.tsx
+++ packages/manager/src/features/GlobalNotifications/GlobalNotifications.tsx
@@ -3,6 +3,8 @@ import AbuseTicketBanner from 'src/components/AbuseTicketBanner';
 import { useRegionsQuery } from 'src/queries/regions';
 import RegionStatusBanner from './RegionStatusBanner';
 import { EmailBounceNotificationSection } from './EmailBounce';
+import ComplianceBanner from './ComplianceBanner';
+import ComplianceUpdateModal from './ComplianceUpdateModal';
 
 const GlobalNotifications: React.FC<{}> = () => {
   const regions = useRegionsQuery().data ?? [];
@@ -12,6 +14,8 @@ const GlobalNotifications: React.FC<{}> = () => {
       <EmailBounceNotificationSection />
       <RegionStatusBanner regions={regions} />
       <AbuseTicketBanner />
+      <ComplianceBanner />
+      <ComplianceUpdateModal />
     </>
   );
 };
