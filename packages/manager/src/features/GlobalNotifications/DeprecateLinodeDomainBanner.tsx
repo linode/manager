575652509c197f9b5a71785d33484bd2b2a1cdca
Tiffany Wong <wong.tyan@gmail.com>
2021-10-11 17:38:42 +0200

    Address feedback

--- packages/manager/src/features/GlobalNotifications/DeprecateLinodeDomainBanner.tsx
+++ packages/manager/src/features/GlobalNotifications/DeprecateLinodeDomainBanner.tsx
@@ -1,21 +1,12 @@
 import * as React from 'react';
-import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
 import { useDismissibleBanner } from 'src/components/DismissibleBanner/DismissibleBanner';
 import Link from 'src/components/Link';
 import Notice from 'src/components/Notice';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  domains: {
-    fontFamily: theme.font.bold,
-  },
-}));
-
 export const DeprecateLinodeDomainBanner: React.FC<{}> = () => {
-  const classes = useStyles();
-
   const { hasDismissedBanner, handleDismiss } = useDismissibleBanner(
-    'linode-domain'
+    'deprecate-linode-domain'
   );
 
   if (hasDismissedBanner) {
@@ -25,12 +16,10 @@ export const DeprecateLinodeDomainBanner: React.FC<{}> = () => {
   return (
     <Notice important warning dismissible onClose={handleDismiss}>
       <Typography>
-        Linode is deprecating the{' '}
-        <span className={classes.domains}>members.linode.com</span> and{' '}
-        <span className={classes.domains}>nodebalancer.linode.com</span> domains
-        provided to all Linodes and NodeBalancers. By December 31st, 2021,
-        update your scripts, applications, or settings to use the new default
-        domain.{' '}
+        Linode is deprecating the <b>members.linode.com</b> and{' '}
+        <b>nodebalancer.linode.com</b> domains provided to all Linodes and
+        NodeBalancers. By December 31st, 2021, update your scripts,
+        applications, or settings to use the new default domain.{' '}
         <Link to="https://www.linode.com/docs/guides/members-linode-com-migration">
           Review documentation here
         </Link>
