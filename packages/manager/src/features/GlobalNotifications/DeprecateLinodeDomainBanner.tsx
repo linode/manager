77bf7f74db0383308f1f98e90493c5e44f054f82
Tiffany Wong <wong.tyan@gmail.com>
2021-10-11 15:58:45 +0200

    Add copy

--- packages/manager/src/features/GlobalNotifications/DeprecateLinodeDomainBanner.tsx
+++ packages/manager/src/features/GlobalNotifications/DeprecateLinodeDomainBanner.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
+import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
 import { useDismissibleBanner } from 'src/components/DismissibleBanner/DismissibleBanner';
 import Link from 'src/components/Link';
 import Notice from 'src/components/Notice';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  domains: {
+    fontFamily: theme.font.bold,
+  },
+}));
+
 export const DeprecateLinodeDomainBanner: React.FC<{}> = () => {
+  const classes = useStyles();
+
   const { hasDismissedBanner, handleDismiss } = useDismissibleBanner(
     'linode-domain'
   );
@@ -16,9 +25,12 @@ export const DeprecateLinodeDomainBanner: React.FC<{}> = () => {
   return (
     <Notice important warning dismissible onClose={handleDismiss}>
       <Typography>
-        Effective Monday, January 31, 2022, all Linodes with members.linode.com
-        and nodebalancer.linode.com forward or reverse DNS entries will be
-        updated to linodeusercontent.com. <Link to="">Read more</Link>.
+        Linode is deprecating the{' '}
+        <span className={classes.domains}>members.linode.com</span> and{' '}
+        <span className={classes.domains}>nodebalancer.linode.com</span> domains
+        provided to all Linodes and NodeBalancers. By December 31st, 2021,
+        update your scripts, applications, or settings to use the new default
+        domain. <Link to="">Review documentation here</Link>.
       </Typography>
     </Notice>
   );
