56620a6ec591f2084a5035539d4fe8d3bca56500
Banks Nussman <banks@nussman.us>
2021-10-28 16:35:00 -0400

    No wrap file name

--- packages/manager/src/features/Kubernetes/KubernetesClusterDetail/KubeSummaryPanel.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesClusterDetail/KubeSummaryPanel.tsx
@@ -77,6 +77,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     color: theme.cmrTextColors.linkActiveLight,
     cursor: 'pointer',
     marginRight: theme.spacing(1),
+    whiteSpace: 'nowrap',
   },
   kubeconfigIcons: {
     color: theme.cmrTextColors.linkActiveLight,
