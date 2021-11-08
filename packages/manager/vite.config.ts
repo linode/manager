5a38aebca05fc41c596859672e645c7ee045ce9a
Banks Nussman <banks@nussman.us>
2021-11-08 18:11:27 -0500

    more

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -1,21 +1,19 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import path from 'path'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path';
 import reactSvgPlugin from 'vite-plugin-react-svg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    reactSvgPlugin(),
-  ],
+  plugins: [reactRefresh(), reactSvgPlugin()],
   envDir: './',
   define: { 'process.env': process.env },
   resolve: {
     alias: {
-      'src': path.resolve(__dirname, './src'),
+      src: path.resolve(__dirname, './src'),
       '@linode/validation': path.resolve(__dirname, '../validation/'),
       '@linode/api-v4': path.resolve(__dirname, '../api-v4/'),
     },
   },
-})
+});
+
