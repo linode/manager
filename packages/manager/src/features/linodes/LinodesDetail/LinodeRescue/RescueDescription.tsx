898462ea79a0853008b511070a2cd267fa332da0
Dajahi Wiley <dwiley@linode.com>
2021-11-22 14:15:33 -0500

    Add getLinodeFirewalls method to JS Client; add linodeFirewalls query; make linodeId a required prop in <RescueDescription /> & modify logic for Bare Metal copy

--- packages/manager/src/features/linodes/LinodesDetail/LinodeRescue/RescueDescription.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeRescue/RescueDescription.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import Typography from 'src/components/core/Typography';
-import { lishLaunch } from 'src/features/Lish/lishUtils';
 import { makeStyles, Theme } from 'src/components/core/styles';
+import Typography from 'src/components/core/Typography';
 import Link from 'src/components/Link';
+import Notice from 'src/components/Notice';
+import { lishLaunch } from 'src/features/Lish/lishUtils';
+import { useLinodeFirewalls } from 'src/queries/linodeFirewalls';
 
 const rescueDescription = {
   text: `If you suspect that your primary filesystem is corrupt, use the Linode Manager to boot your Linode into Rescue Mode. This is a safe environment for performing many system recovery and disk management tasks.`,
@@ -12,35 +14,43 @@ const rescueDescription = {
 };
 
 interface Props {
-  linodeId?: number;
+  linodeId: number;
+  isBareMetal?: boolean;
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
   copy: {
     marginTop: theme.spacing(1),
   },
+  notice: {
+    marginTop: theme.spacing(2),
+  },
   lishLink: theme.applyLinkStyles,
 }));
 
 const RescueDescription: React.FC<Props> = (props) => {
-  const { linodeId } = props;
+  const { linodeId, isBareMetal } = props;
   const classes = useStyles();
 
-  /**
-   * Pass the prop 'linodeId' when you want to include a note about
-   * connection with LISH upon reboot into Rescue Mode. This is
-   * intended for the Bare Metal dialog.
-   */
+  const { data: linodeFirewallsData } = useLinodeFirewalls(linodeId);
+  const firewallsLinodeAssignedTo = linodeFirewallsData?.data ?? [];
+  const displayFirewallWarning = firewallsLinodeAssignedTo.length > 0;
+
   return (
     <React.Fragment>
+      {displayFirewallWarning ? (
+        <Notice
+          warning
+          text={rescueDescription.firewallWarning}
+          spacingTop={0}
+          spacingBottom={16}
+        />
+      ) : null}
       <Typography>
         {rescueDescription.text}{' '}
         <Link to={rescueDescription.link}>Learn more.</Link>
       </Typography>
-      <Typography className={classes.copy}>
-        {rescueDescription.firewallWarning}
-      </Typography>
-      {linodeId ? (
+      {linodeId && isBareMetal ? (
         <Typography className={classes.copy}>
           {`When your Linode has successfully rebooted into Rescue Mode, use the `}
           <button
