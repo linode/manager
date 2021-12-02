ba3f8d5456b692a1a84b583cef054aa2788632e4
codyfinn <codyfinn@protonmail.com>
2021-12-02 16:27:52 -0500

    enable beta badge on k8s

--- packages/manager/src/components/PrimaryNav/PrimaryNav.tsx
+++ packages/manager/src/components/PrimaryNav/PrimaryNav.tsx
@@ -181,6 +181,7 @@ export const PrimaryNav: React.FC<Props> = (props) => {
           href: '/kubernetes/clusters',
           activeLinks: ['/kubernetes/create'],
           icon: <Kubernetes />,
+          isBeta: true,
         },
         {
           hide: !showDatabases,
