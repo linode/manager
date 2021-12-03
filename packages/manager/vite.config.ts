a5e54f3c786434e19a8b10ff58d2ced019f26d5c
Banks Nussman <banks@nussman.us>
2021-12-02 19:47:31 -0500

    Fix process.env

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import reactSvgPlugin from 'vite-plugin-react-svg';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import reactSvgPlugin from "vite-plugin-react-svg";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactSvgPlugin({
-    defaultExport: 'component',
-    svgoConfig: {
-      removeViewBox: false,
-    },
-  })],
-  define: { 'process.env': process.env },
+  plugins: [
+    react(),
+    reactSvgPlugin({
+      defaultExport: "component",
+      svgoConfig: {
+        removeViewBox: false,
+      },
+    }),
+  ],
+  define: { "process.env": JSON.stringify("import.meta.env") },
   resolve: {
     alias: {
-      src: path.resolve(__dirname, './src'),
+      src: path.resolve(__dirname, "./src"),
     },
   },
-  envPrefix: 'REACT_APP_'
-})
+  envPrefix: "REACT_APP_",
+});
