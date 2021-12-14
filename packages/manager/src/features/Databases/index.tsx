ec1322952d2186b6d39af7cd7fbc1600459fb846
Tiffany Wong <wong.tyan@gmail.com>
2021-12-14 18:44:50 +0100

    Remove RouteComponentProps

--- packages/manager/src/features/Databases/index.tsx
+++ packages/manager/src/features/Databases/index.tsx
@@ -1,25 +1,14 @@
 import * as React from 'react';
-import { Route, RouteComponentProps, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import SuspenseLoader from 'src/components/SuspenseLoader';
 import useFlags from 'src/hooks/useFlags';
-import useReduxLoad from 'src/hooks/useReduxLoad';
 
 const DatabaseLanding = React.lazy(() => import('./DatabaseLanding'));
 const DatabaseDetail = React.lazy(() => import('./DatabaseDetail'));
 const DatabaseCreate = React.lazy(() => import('./DatabaseCreate'));
 
-type Props = RouteComponentProps<{}>;
-
-type CombinedProps = Props;
-
-const Database: React.FC<CombinedProps> = (props) => {
-  const {
-    match: { path },
-  } = props;
-
-  useReduxLoad(['databases']);
-
+const Database: React.FC = () => {
   // @TODO: Remove when Database goes to GA
   const flags = useFlags();
   if (!flags.databases) {
@@ -30,9 +19,9 @@ const Database: React.FC<CombinedProps> = (props) => {
     <React.Suspense fallback={<SuspenseLoader />}>
       <DocumentTitleSegment segment="Databases" />
       <Switch>
-        <Route component={DatabaseCreate} path={`${path}/create`} />
-        <Route component={DatabaseDetail} path={`${path}/:databaseId`} />
-        <Route component={DatabaseLanding} />
+        <Route component={DatabaseCreate} path={`/databases/create`} />
+        <Route component={DatabaseDetail} path={`/databases/:databaseId`} />
+        <Route component={DatabaseLanding} exact strict />
       </Switch>
     </React.Suspense>
   );
