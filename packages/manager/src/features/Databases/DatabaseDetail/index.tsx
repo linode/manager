5d286fd807482f377c593b5b39acd3ac2240dee1
Tiffany Wong <wong.tyan@gmail.com>
2021-12-21 18:08:19 +0000

    Initial commit

--- packages/manager/src/features/Databases/DatabaseDetail/index.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/index.tsx
@@ -34,7 +34,14 @@ export const DatabaseDetail: React.FC = () => {
     const tabChoice = tabs.findIndex((tab) =>
       Boolean(matchPath(tab.routeName, { path: location.pathname }))
     );
-    return tabChoice < 0 ? 0 : tabChoice;
+
+    // Redirect to the landing page if the path does not exist
+    if (tabChoice < 0) {
+      history.push('/databases');
+      return 0;
+    } else {
+      return tabChoice;
+    }
   };
 
   const handleTabChange = (index: number) => {
