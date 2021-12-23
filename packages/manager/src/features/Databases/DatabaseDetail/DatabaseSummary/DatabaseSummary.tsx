6da8defcf222538cb7cc1f485609129c65026d21
bmertz <bennett.mertz@gmail.com>
2021-12-23 14:56:04 -0500

    more closely match spec

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummary.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummary.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
+import Paper from 'src/components/core/Paper';
+import { makeStyles, Theme } from 'src/components/core/styles';
+import Divider from 'src/components/core/Divider';
 // import useDatabases from 'src/hooks/useDatabases';
+
 import ConnectionDetails from './DatabaseSummaryConnectionDetails';
 import ClusterConfiguration from './DatabaseSummaryClusterConfiguration';
 import AccessControls from './DatabaseSummaryAccessControls';
 
-// 
+const useStyles = makeStyles((theme: Theme) => ({
+  divider: {
+    marginTop: '1.5rem',
+    marginBottom: '1rem',
+  },
+}));
+
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {
   // databaseID: number;
@@ -12,17 +23,18 @@ interface Props {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const DatabaseSummary: React.FC<Props> = (props) => {
+  const classes = useStyles();
   // const databases = useDatabases();
   // const { databaseID } = props;
   // const thisDatabase = databases.databases.itemsById[databaseID];
 
   return (
-    <>
-      Database Summary
-      <ConnectionDetails />
+    <Paper>
+      <ConnectionDetails /> 
       <ClusterConfiguration />
+      <Divider className={classes.divider} />
       <AccessControls />
-    </>
+    </Paper>
   );
 };
 
