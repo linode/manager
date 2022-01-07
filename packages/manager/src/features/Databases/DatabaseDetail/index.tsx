f4cf50b6c08521f58746feb21d8a409c946a9c52
Tiffany Wong <7692354+tiffwong@users.noreply.github.com>
2022-01-07 17:17:29 +0100

    DBAAS1-82: Database Details - Cluster Configs (#8103)
    ## Description
    
    This PR adds the breadcrumb to the Database Details page and the Cluster Configs section to the Summary tab.
    
    ![2022-01-05 18 44 20 localhost 40e930f8cdba](https://user-images.githubusercontent.com/7692354/148271577-8178651e-54ee-48cd-ab0b-1eae5e1478e4.png)
    
    ## How to test
    
    Turn on MSW and go to any Database Details page.

--- packages/manager/src/features/Databases/DatabaseDetail/index.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/index.tsx
@@ -1,19 +1,51 @@
 import * as React from 'react';
 import { matchPath, useHistory, useParams } from 'react-router-dom';
+import Breadcrumb from 'src/components/Breadcrumb';
+import CircleProgress from 'src/components/CircleProgress';
 import TabPanels from 'src/components/core/ReachTabPanels';
 import Tabs from 'src/components/core/ReachTabs';
-// import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import ErrorState from 'src/components/ErrorState';
 import SafeTabPanel from 'src/components/SafeTabPanel';
 import TabLinkList from 'src/components/TabLinkList';
 import useFlags from 'src/hooks/useFlags';
+import { getDatabaseEngine, useDatabaseQuery } from 'src/queries/databases';
+import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
 
 const DatabaseSummary = React.lazy(() => import('./DatabaseSummary'));
 const DatabaseBackups = React.lazy(() => import('./DatabaseBackups'));
 const DatabaseSettings = React.lazy(() => import('./DatabaseSettings'));
 
 export const DatabaseDetail: React.FC = () => {
+  const flags = useFlags();
   const history = useHistory();
+
   const { databaseId } = useParams<{ databaseId: string }>();
+
+  const id = Number(databaseId);
+
+  const { data: database, isLoading, error } = useDatabaseQuery(
+    getDatabaseEngine(id),
+    id
+  );
+
+  if (error) {
+    return (
+      <ErrorState
+        errorText={
+          getAPIErrorOrDefault(error, 'Error loading your database.')[0].reason
+        }
+      />
+    );
+  }
+
+  if (isLoading) {
+    return <CircleProgress />;
+  }
+
+  if (!database || !flags.databases) {
+    return null;
+  }
 
   const tabs = [
     {
@@ -47,21 +79,26 @@ export const DatabaseDetail: React.FC = () => {
     history.push(tabs[index].routeName);
   };
 
-  // @TODO: Remove when Database goes to GA
-  const flags = useFlags();
-  if (!flags.databases) {
-    return null;
-  }
-
   return (
     <>
-      {/* <DocumentTitleSegment segment={thisDatabase.label} /> */}
-      Database Details
+      <DocumentTitleSegment segment={database.label} />
+      <Breadcrumb
+        pathname={location.pathname}
+        labelTitle={database.label}
+        firstAndLastOnly
+        crumbOverrides={[
+          {
+            position: 1,
+            label: 'Database Clusters',
+          },
+        ]}
+        labelOptions={{ noCap: true }}
+      />
       <Tabs defaultIndex={getDefaultTabIndex()} onChange={handleTabChange}>
         <TabLinkList tabs={tabs} />
         <TabPanels>
           <SafeTabPanel index={0}>
-            <DatabaseSummary />
+            <DatabaseSummary database={database} />
           </SafeTabPanel>
           <SafeTabPanel index={1}>
             <DatabaseBackups />
