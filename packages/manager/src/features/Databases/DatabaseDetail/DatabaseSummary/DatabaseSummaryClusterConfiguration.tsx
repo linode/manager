303b60e5cb688e679da32885931b73c0d07f1886
Tiffany Wong <7692354+tiffwong@users.noreply.github.com>
2022-01-07 17:17:29 +0100

    DBAAS1-82: Database Details - Cluster Configs (#8103)
    ## Description
    
    This PR adds the breadcrumb to the Database Details page and the Cluster Configs section to the Summary tab.
    
    ![2022-01-05 18 44 20 localhost 40e930f8cdba](https://user-images.githubusercontent.com/7692354/148271577-8178651e-54ee-48cd-ab0b-1eae5e1478e4.png)
    
    ## How to test
    
    Turn on MSW and go to any Database Details page.

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummaryClusterConfiguration.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummaryClusterConfiguration.tsx
@@ -1,18 +1,100 @@
+import { Database, DatabaseInstance } from '@linode/api-v4/lib/databases/types';
 import * as React from 'react';
-// import useDatabases from 'src/hooks/useDatabases';
+import Box from 'src/components/core/Box';
+import { makeStyles, Theme } from 'src/components/core/styles';
+import Typography from 'src/components/core/Typography';
+import StatusIcon from 'src/components/StatusIcon';
+import { useDatabaseTypesQuery } from 'src/queries/databases';
+import {
+  databaseEngineMap,
+  databaseStatusMap,
+} from '../../DatabaseLanding/DatabaseRow';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+const useStyles = makeStyles((theme: Theme) => ({
+  header: {
+    fontFamily: theme.font.bold,
+    marginBottom: theme.spacing(2),
+  },
+  configs: {
+    fontSize: '0.875rem',
+    lineHeight: '22px',
+  },
+  label: {
+    fontFamily: theme.font.bold,
+    lineHeight: '22px',
+    width: theme.spacing(7),
+  },
+  status: {
+    display: 'flex',
+    alignItems: 'center',
+    textTransform: 'capitalize',
+  },
+}));
+
 interface Props {
-  // databaseID: number;
+  database: Database;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const DatabaseSummaryClusterConfiguration: React.FC<Props> = (props) => {
-  // const databases = useDatabases();
-  // const { databaseID } = props;
-  // const thisDatabase = databases.databases.itemsById[databaseID];
+export const getDatabaseVersionNumber = (
+  version: DatabaseInstance['version']
+) => version.split('/')[1];
 
-  return <>Cluster Configuration</>;
+export const DatabaseSummaryClusterConfiguration: React.FC<Props> = (props) => {
+  const classes = useStyles();
+
+  const { database } = props;
+
+  const { data: types } = useDatabaseTypesQuery();
+
+  const type = types?.find((type) => type.id === database?.type);
+
+  if (!database || !type) {
+    return null;
+  }
+
+  const configuration =
+    database.failover_count === 0
+      ? 'Primary'
+      : `Primary +${database.failover_count} replicas`;
+
+  return (
+    <>
+      <Typography className={classes.header}>Cluster Configuration</Typography>
+      <div className={classes.configs}>
+        <Box display="flex">
+          <Typography className={classes.label}>Status</Typography>
+          <span className={classes.status}>
+            <StatusIcon status={databaseStatusMap[database.status]} />
+            {database.status}
+          </span>
+        </Box>
+        <Box display="flex">
+          <Typography className={classes.label}>Version</Typography>
+          {databaseEngineMap[database.engine]} v{database.version}
+        </Box>
+        <Box display="flex" style={{ marginBottom: 12 }}>
+          <Typography className={classes.label}>Nodes</Typography>
+          {configuration}
+        </Box>
+        <Box display="flex">
+          <Typography className={classes.label}>Plan</Typography>
+          {type.label}
+        </Box>
+        <Box display="flex">
+          <Typography className={classes.label}>RAM</Typography>
+          {type.memory / 1024} GB
+        </Box>
+        <Box display="flex">
+          <Typography className={classes.label}>CPUs</Typography>
+          {type.vcpus}
+        </Box>
+        <Box display="flex">
+          <Typography className={classes.label}>Storage</Typography>
+          {type.disk} GB
+        </Box>
+      </div>
+    </>
+  );
 };
 
 export default DatabaseSummaryClusterConfiguration;
