e0cccd719571cebd2c84f393932fcba0eaa2f07c
codyfinn <codyfinn@protonmail.com>
2022-01-14 16:56:28 -0500

    Added modals for password reset and cluster delete

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseSettings/DatabaseSettings.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseSettings/DatabaseSettings.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import Paper from 'src/components/core/Paper';
-import Typography from 'src/components/core/Typography';
 import Divider from 'src/components/core/Divider';
 import { Engine } from '@linode/api-v4/lib/databases/types';
 import DatabaseSettingsMenuItem from './DatabaseSettingsMenuItem';
+import DatabaseSettingsDeleteClusterDialog from './DatabaseSettingsDeleteClusterDialog';
+import DatabaseSettingsResetPasswordDialog from './DatabaseSettingsResetPasswordDialog';
 
 interface Props {
   databaseID: number;
@@ -11,35 +12,49 @@ interface Props {
 }
 
 export const DatabaseSettings: React.FC<Props> = (props) => {
-  const { databaseID, databaseEngine } = props;
+  // const { databaseID, databaseEngine } = props;
   const accessControlsCopy = "Add the IP addresses or IP range(s) for other instances or users that should have the authorization to view this cluster’s database. By default, all public and private connections are denied. Learn more.";
 
   const resetRootPasswordCopy = "Resetting your root password will automatically generate a new password. You can view the updated password on your Database Cluster Summary page. ";
 
   const deleteClusterCopy = "Deleting a database cluster is permenant and cannot be undone";
 
-  const onAddAccessControl = () => {
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = React.useState(false);
+  const [isAddAccessControlDialogOpen, setIsAddAccessControlDialogOpen] = React.useState(false);
+  const [isResetRootPasswordDialogOpen, setIsResetRootPasswordDialogOpen] = React.useState(false);
 
+  const onAddAccessControl = () => {
+    setIsAddAccessControlDialogOpen(true);
   };
 
   const onResetRootPassword = () => {
-
+    setIsResetRootPasswordDialogOpen(true);
   };
 
   const onDeleteCluster = () => {
-
+    console.log('Open Delete Cluster Dialog');
+    setIsDeleteDialogOpen(true);
   };
 
+  const onDeleteClusterClose = () => {
+    setIsDeleteDialogOpen(false);
+  }
+
+  const onResetRootPasswordClose = () => {
+    setIsResetRootPasswordDialogOpen(false);
+  }
+
   return (
-    <Paper>
-      <DatabaseSettingsMenuItem
-        buttonText="Add Access Controls"
-        descriptiveText={accessControlsCopy}
-        onClick={onAddAccessControl}
-        sectionTitle="Access Controls">
-        <
+    <>
+      <Paper>
+        <DatabaseSettingsMenuItem
+          buttonText="Add Access Controls"
+          descriptiveText={accessControlsCopy}
+          onClick={onAddAccessControl}
+          sectionTitle="Access Controls">
+          Access Controls Component
       </DatabaseSettingsMenuItem>
-      <Divider />
+        <Divider />
         <DatabaseSettingsMenuItem
           buttonText="Reset Root Password"
           descriptiveText={resetRootPasswordCopy}
@@ -51,7 +66,14 @@ export const DatabaseSettings: React.FC<Props> = (props) => {
           descriptiveText={deleteClusterCopy}
           buttonText="Delete Cluster"
           onClick={onDeleteCluster} />
-    </Paper>
+      </Paper>
+      <DatabaseSettingsDeleteClusterDialog
+        open={isDeleteDialogOpen}
+        onClose={onDeleteClusterClose} />
+      <DatabaseSettingsResetPasswordDialog
+        open={isResetRootPasswordDialogOpen}
+        onClose={onResetRootPasswordClose} />
+    </>
   );
 };
 
