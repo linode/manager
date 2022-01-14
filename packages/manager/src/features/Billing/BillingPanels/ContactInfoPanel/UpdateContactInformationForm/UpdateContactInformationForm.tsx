d71c1ac0c755429055eed246d267025eb18aed91
Banks Nussman <banks@nussman.us>
2022-01-14 13:58:18 -0500

    Update placeholder and update test to use a test id

--- packages/manager/src/features/Billing/BillingPanels/ContactInfoPanel/UpdateContactInformationForm/UpdateContactInformationForm.tsx
+++ packages/manager/src/features/Billing/BillingPanels/ContactInfoPanel/UpdateContactInformationForm/UpdateContactInformationForm.tsx
@@ -426,7 +426,9 @@ class UpdateContactInformationForm extends React.Component<
               isClearable={false}
               onChange={this.updateState}
               options={filteredRegionResults}
-              placeholder="Select region"
+              placeholder={`Select ${
+                fields.country === 'US' ? 'state' : 'region'
+              }`}
               required={flags.regionDropdown}
               value={
                 filteredRegionResults.find(({ value }) =>
@@ -437,7 +439,7 @@ class UpdateContactInformationForm extends React.Component<
               }
               textFieldProps={{
                 dataAttrs: {
-                  'data-qa-contact-province': true,
+                  'data-qa-contact-state-province': true,
                 },
               }}
             />
@@ -455,7 +457,7 @@ class UpdateContactInformationForm extends React.Component<
               required={flags.regionDropdown}
               value={fields.state || ''}
               dataAttrs={{
-                'data-qa-contact-province': true,
+                'data-qa-contact-state-province': true,
               }}
             />
           )}
