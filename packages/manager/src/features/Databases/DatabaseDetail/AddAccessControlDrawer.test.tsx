c0a9452975e7bc95d295d759289c9921517f1332
Dajahi Wiley <dwiley@linode.com>
2022-01-18 11:18:28 -0500

    Additional 2 tests in AddAccessControlDrawer.test.tsx

--- packages/manager/src/features/Databases/DatabaseDetail/AddAccessControlDrawer.test.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/AddAccessControlDrawer.test.tsx
@@ -1,10 +1,12 @@
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import * as React from 'react';
 import { QueryClient } from 'react-query';
 import { databaseFactory } from 'src/factories';
+import { IPv4List } from 'src/factories/databases';
+import { stringToExtendedIP } from 'src/utilities/ipUtils';
 import { mockMatchMedia, renderWithTheme } from 'src/utilities/testHelpers';
 import AccessControls from './AccessControls';
-// import { IPv4List } from 'src/factories/databases';
+import AddAccessControlDrawer from './AddAccessControlDrawer';
 
 const queryClient = new QueryClient();
 
@@ -27,7 +29,45 @@ describe('Add Access Controls drawer', () => {
     expect(getByTestId('drawer')).toBeVisible();
   });
 
-  it('Should open with a full list of current inbound sources that are allow listed', async () => {});
+  it('Should open with a full list of current inbound sources that are allow listed', async () => {
+    const { getAllByTestId } = renderWithTheme(
+      <AddAccessControlDrawer
+        open={true}
+        onClose={() => null}
+        updateDatabase={() => null}
+        allowList={IPv4List.map(stringToExtendedIP)}
+      />
+    );
 
-  it('Should have a disabled Add Inbound Sources button until an inbound source field is touched', () => {});
+    expect(getAllByTestId('domain-transfer-input')).toHaveLength(
+      IPv4List.length
+    );
+
+    await screen.findByDisplayValue(IPv4List[0]);
+    await screen.findByDisplayValue(IPv4List[1]);
+    await screen.findByDisplayValue(IPv4List[2]);
+  });
+
+  it('Should have a disabled Add Inbound Sources button until an inbound source field is touched', () => {
+    const { getByText } = renderWithTheme(
+      <AddAccessControlDrawer
+        open={true}
+        onClose={() => null}
+        updateDatabase={() => null}
+        allowList={IPv4List.map(stringToExtendedIP)}
+      />
+    );
+
+    const addInboundSourcesButton = getByText('Add Inbound Sources').closest(
+      'button'
+    );
+
+    // Before making a change to the IP addresses, the "Add Inbound Sources" button should be disabled.
+    expect(addInboundSourcesButton).toBeDisabled();
+
+    const addAnIPButton = getByText('Add an IP');
+    fireEvent.click(addAnIPButton);
+
+    expect(addInboundSourcesButton).toBeEnabled();
+  });
 });
