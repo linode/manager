a0ffcb814ddd08b564382379f91af6c57600367f
bmertz <bmertz@linode.com>
2022-01-19 15:49:55 -0500

    remove unused

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseSettings/DatabaseSettings.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseSettings/DatabaseSettings.tsx
@@ -1,29 +1,17 @@
 import * as React from 'react';
-// import DatabaseSettingsLabelPanel from './DatabaseSettingsLabelPanel';
-// import DatabaseSettingsMaintenancePanel from './DatabaseSettingsMaintenancePanel';
-// import DatabaseSettingsPasswordPanel from './DatabaseSettingsPasswordPanel';
 
 interface Props {
   // databaseID: number;
 }
 
 export const DatabaseSettings: React.FC<Props> = (props) => {
-  // const databases = useDatabases();
   // const { databaseID } = props;
   // const thisDatabase = databases.databases.itemsById[databaseID];
 
   return (
     <>
       Database Settings
-      {/* <DatabaseSettingsLabelPanel
-        databaseID={thisDatabase.id}
-        databaseLabel={thisDatabase.label}
-      />
-      <DatabaseSettingsPasswordPanel databaseID={thisDatabase.id} />
-      <DatabaseSettingsMaintenancePanel
-        databaseID={thisDatabase.id}
-        databaseMaintenanceSchedule={thisDatabase.maintenance_schedule}
-      /> */}
+      {/* */}
     </>
   );
 };
