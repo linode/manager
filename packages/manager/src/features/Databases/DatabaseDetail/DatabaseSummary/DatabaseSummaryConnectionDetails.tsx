519b839c6298f550876f9521080914a4a67415fe
Banks Nussman <banks@nussman.us>
2022-01-27 10:56:26 -0500

    M3-5683: Add Secondary Host and Tooltip (#8148)

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummaryConnectionDetails.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummaryConnectionDetails.tsx
@@ -12,6 +12,8 @@ import { DB_ROOT_USERNAME } from 'src/constants';
 import { useDatabaseCredentialsQuery } from 'src/queries/databases';
 import { downloadFile } from 'src/utilities/downloadFile';
 import { getErrorStringOrDefault } from 'src/utilities/errorUtils';
+import Box from 'src/components/core/Box';
+import HelpIcon from 'src/components/HelpIcon';
 
 const useStyles = makeStyles((theme: Theme) => ({
   header: {
@@ -78,11 +80,18 @@ const useStyles = makeStyles((theme: Theme) => ({
     color: theme.color.red,
     marginLeft: theme.spacing(2),
   },
+  helpIcon: {
+    padding: 0,
+    marginLeft: theme.spacing(),
+  },
 }));
 
 interface Props {
   database: Database;
 }
+
+const privateHostCopy =
+  'A private network host and a private IP can only be used to access a database cluster from Linodes in the same data center and will not incur transfer costs.';
 
 export const DatabaseSummaryConnectionDetails: React.FC<Props> = (props) => {
   const { database } = props;
@@ -196,6 +205,14 @@ export const DatabaseSummaryConnectionDetails: React.FC<Props> = (props) => {
         <Typography>
           <span>host</span> = {database.hosts?.primary}
         </Typography>
+        {database.hosts.secondary ? (
+          <Box display="flex" flexDirection="row" alignItems="center">
+            <Typography>
+              <span>private network host</span> = {database.hosts.secondary}
+            </Typography>
+            <HelpIcon className={classes.helpIcon} text={privateHostCopy} />
+          </Box>
+        ) : null}
         <Typography>
           <span>port</span> = {database.port}
         </Typography>
