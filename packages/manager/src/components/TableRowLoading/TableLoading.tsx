f6beed497e27681fb7383c5323206dbd9ea6d5b3
Banks Nussman <banks@nussman.us>
2022-01-31 11:18:37 -0500

    update React import

--- packages/manager/src/components/TableRowLoading/TableLoading.tsx
+++ packages/manager/src/components/TableRowLoading/TableLoading.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import Hidden, { HiddenProps } from '../core/Hidden';
 import Skeleton from '../core/Skeleton';
 import { makeStyles } from '../core/styles';
