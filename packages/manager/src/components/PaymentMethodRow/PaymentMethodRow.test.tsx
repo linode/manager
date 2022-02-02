77fc1ba655decd10e4f0cc7851169204fc84a147
Joe D'Amore <jdamore@linode.com>
2022-02-02 14:24:16 -0500

    Add payment method row tests for "Make a Payment" and "Make Default" action menu items

--- packages/manager/src/components/PaymentMethodRow/PaymentMethodRow.test.tsx
+++ packages/manager/src/components/PaymentMethodRow/PaymentMethodRow.test.tsx
@@ -1,8 +1,16 @@
 import userEvent from '@testing-library/user-event';
 import PaymentMethodRow from './PaymentMethodRow';
+import BillingSummary from 'src/features/Billing/BillingPanels/BillingSummary';
 import { paymentMethodFactory } from 'src/factories';
 import { renderWithTheme } from 'src/utilities/testHelpers';
+import { makeDefaultPaymentMethod } from '@linode/api-v4/lib';
 import * as React from 'react';
+
+jest.mock('@linode/api-v4/lib', () => {
+  return {
+    makeDefaultPaymentMethod: jest.fn().mockResolvedValue({}),
+  };
+});
 
 describe('Payment Method Row', () => {
   it('Displays "Default" chip if payment method is set as default', () => {
@@ -124,7 +132,55 @@ describe('Payment Method Row', () => {
     expect(mockFunction).toBeCalledTimes(1);
   });
 
-  // TODO Test that history is pushed and that payment method is passed as state.
-  // it('Opens "Make a Payment" drawer if "Make a Payment" action is clicked', async () => {
-  // });
+  it('Makes payment method default when "Make Default" action is clicked', () => {
+    const paymentMethod = paymentMethodFactory.build({
+      is_default: false,
+      data: {
+        card_type: 'Visa',
+        last_four: '1111',
+      },
+    });
+
+    const { getByText, getByLabelText } = renderWithTheme(
+      <PaymentMethodRow paymentMethod={paymentMethod} onDelete={jest.fn()} />
+    );
+
+    const actionMenu = getByLabelText('Action menu for card ending in 1111');
+    userEvent.click(actionMenu);
+
+    const makeDefaultButton = getByText('Make Default');
+    expect(makeDefaultButton).toBeVisible();
+    userEvent.click(makeDefaultButton);
+
+    expect(makeDefaultPaymentMethod).toBeCalledTimes(1);
+  });
+
+  it('Opens "Make a Payment" drawer if "Make a Payment" action is clicked', () => {
+    const paymentMethod = paymentMethodFactory.build();
+
+    /*
+     * The <BillingSummary /> component is responsible for rendering the "Make a Payment" drawer,
+     * and is required for this test. We may want to consider decoupling these components in the future.
+     */
+    const { getByText, getByLabelText, getByTestId } = renderWithTheme(
+      <>
+        <BillingSummary
+          paymentMethods={[paymentMethod]}
+          balanceUninvoiced={0}
+          balance={0}
+        />
+        <PaymentMethodRow paymentMethod={paymentMethod} onDelete={jest.fn()} />
+      </>
+    );
+
+    const actionMenu = getByLabelText('Action menu for card ending in 1881');
+    userEvent.click(actionMenu);
+
+    const makePaymentButton = getByText('Make a Payment');
+    expect(makePaymentButton).toBeVisible();
+    userEvent.click(makePaymentButton);
+
+    expect(getByTestId('drawer')).toBeVisible();
+    expect(getByTestId('drawer-title').textContent).toEqual('Make a Payment');
+  });
 });
