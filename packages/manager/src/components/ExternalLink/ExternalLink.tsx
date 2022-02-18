ba73d0bd86b804efb1440eefad1bf92a9dddeb2b
Banks Nussman <banks@nussman.us>
2022-02-18 11:43:26 -0500

    M3-5606: Storybook for Links (#8207)
    * Updated link stories
    
    * Changed Link -> Links
    
    * Make copy consistant

--- packages/manager/src/components/ExternalLink/ExternalLink.tsx
+++ packages/manager/src/components/ExternalLink/ExternalLink.tsx
@@ -1,57 +1,47 @@
-import classNames from 'classnames';
 import * as React from 'react';
-
+import classNames from 'classnames';
 import OpenInNew from '@material-ui/icons/OpenInNew';
-
 import Arrow from 'src/assets/icons/diagonalArrow.svg';
-import {
-  createStyles,
-  Theme,
-  withStyles,
-  WithStyles,
-} from 'src/components/core/styles';
+import { makeStyles, Theme } from 'src/components/core/styles';
 
-type ClassNames = 'root' | 'icon' | 'absoluteIcon' | 'black' | 'fixedIcon';
-
-const styles = (theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'inline-flex',
-      alignItems: 'baseline',
-      '&:hover': {
-        '& $icon': {
-          opacity: 1,
-        },
-      },
-    },
-    icon: {
-      color: theme.palette.primary.main,
-      position: 'relative',
-      left: theme.spacing(1),
-      opacity: 0,
-      width: 14,
-      height: 14,
-    },
-    absoluteIcon: {
-      display: 'inline',
-      position: 'relative',
-      paddingRight: 26,
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'inline-flex',
+    alignItems: 'baseline',
+    '&:hover': {
       '& $icon': {
-        position: 'absolute',
-        right: 0,
-        bottom: 2,
-        opacity: 0,
-        left: 'initial',
+        opacity: 1,
       },
     },
-    fixedIcon: {
-      display: 'inline-block',
-      fontSize: '0.8em',
+  },
+  icon: {
+    color: theme.palette.primary.main,
+    position: 'relative',
+    left: theme.spacing(1),
+    opacity: 0,
+    width: 14,
+    height: 14,
+  },
+  absoluteIcon: {
+    display: 'inline',
+    position: 'relative',
+    paddingRight: 26,
+    '& $icon': {
+      position: 'absolute',
+      right: 0,
+      bottom: 2,
+      opacity: 0,
+      left: 'initial',
     },
-    black: {
-      color: theme.palette.text.primary,
-    },
-  });
+  },
+  fixedIcon: {
+    display: 'inline-block',
+    fontSize: '0.8em',
+  },
+  black: {
+    color: theme.palette.text.primary,
+  },
+}));
 
 interface Props {
   link: string;
@@ -64,51 +54,45 @@ interface Props {
   onClick?: () => void;
 }
 
-type CombinedProps = Props & WithStyles<ClassNames>;
+const ExternalLink: React.FC<Props> = (props) => {
+  const classes = useStyles();
+  const {
+    link,
+    text,
+    className,
+    absoluteIcon,
+    black,
+    fixedIcon,
+    hideIcon,
+    onClick,
+  } = props;
 
-class ExternalLink extends React.Component<CombinedProps> {
-  render() {
-    const {
-      classes,
-      link,
-      text,
-      className,
-      absoluteIcon,
-      black,
-      fixedIcon,
-      hideIcon,
-      onClick,
-    } = this.props;
+  return (
+    <a
+      onClick={onClick}
+      target="_blank"
+      aria-describedby="external-site"
+      rel="noopener noreferrer"
+      href={link}
+      className={classNames(
+        {
+          [classes.root]: true,
+          [classes.absoluteIcon]: absoluteIcon,
+          [classes.black]: black,
+        },
+        className
+      )}
+      data-qa-external-link
+    >
+      {text}
+      {!hideIcon &&
+        (fixedIcon ? (
+          <OpenInNew className={classes.fixedIcon} />
+        ) : (
+          <Arrow className={classes.icon} />
+        ))}
+    </a>
+  );
+};
 
-    return (
-      <a
-        onClick={onClick}
-        target="_blank"
-        aria-describedby="external-site"
-        rel="noopener noreferrer"
-        href={link}
-        className={classNames(
-          {
-            [classes.root]: true,
-            [classes.absoluteIcon]: absoluteIcon,
-            [classes.black]: black,
-          },
-          className
-        )}
-        data-qa-external-link
-      >
-        {text}
-        {!hideIcon &&
-          (fixedIcon ? (
-            <OpenInNew className={classes.fixedIcon} />
-          ) : (
-            <Arrow className={classes.icon} />
-          ))}
-      </a>
-    );
-  }
-}
-
-const styled = withStyles(styles);
-
-export default styled(ExternalLink);
+export default ExternalLink;
