801f4c8a76df13142f1fcc0b0d8cbc3024cdc378
Banks Nussman <banks@nussman.us>
2022-02-21 13:52:39 -0500

    Clean up

--- packages/manager/src/features/Managed/ManagedLanding.tsx
+++ packages/manager/src/features/Managed/ManagedLanding.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
-import { compose } from 'recompose';
-import setDocs from 'src/components/DocsSidebar/setDocs';
+import { NavTab } from 'src/components/NavTabs/NavTabs';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
-import ManagedLandingContent from './ManagedLandingContent';
+import LandingHeader from 'src/components/LandingHeader';
+import NavTabs from 'src/components/NavTabs';
+import ManagedDashboardCard from './ManagedDashboardCard';
+import SupportWidget from './SupportWidget';
 
-const docs: Linode.Doc[] = [
+const Contacts = React.lazy(() => import('./Contacts/Contacts'));
+const Monitors = React.lazy(() => import('./Monitors'));
+const SSHAccess = React.lazy(() => import('./SSHAccess'));
+const CredentialList = React.lazy(() => import('./Credentials/CredentialList'));
+
+const tabs: NavTab[] = [
   {
-    title: 'Linode Managed',
-    src: 'https://linode.com/docs/platform/linode-managed/',
-    body: `How to configure service monitoring with Linode Managed.`,
+    title: 'Summary',
+    routeName: `/managed/summary`,
+    component: ManagedDashboardCard,
+  },
+  {
+    title: 'Monitors',
+    routeName: `/managed/monitors`,
+    render: <Monitors />,
+  },
+  {
+    title: 'SSH Access',
+    routeName: `/managed/ssh-access`,
+    component: SSHAccess,
+  },
+  {
+    title: 'Credentials',
+    routeName: `/managed/credentials`,
+    render: <CredentialList />,
+  },
+  {
+    title: 'Contacts',
+    routeName: `/managed/contacts`,
+    render: <Contacts />,
   },
 ];
 
@@ -16,11 +43,16 @@ export const ManagedLanding = () => {
   return (
     <React.Fragment>
       <DocumentTitleSegment segment="Managed" />
-      <ManagedLandingContent />
+      <LandingHeader
+        title="Managed"
+        entity="Managed"
+        docsLink="https://www.linode.com/docs/platform/linode-managed/"
+        extraActions={<SupportWidget />}
+        removeCrumbX={1}
+      />
+      <NavTabs tabs={tabs} />
     </React.Fragment>
   );
 };
 
-const enhanced = compose(setDocs(docs));
-
-export default enhanced(ManagedLanding);
+export default ManagedLanding;
