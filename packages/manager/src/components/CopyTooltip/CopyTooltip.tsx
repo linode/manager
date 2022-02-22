614421e95a8a7b3e8cd2251fe96375b006992dcf
Hana Xu <hanaxu12@gmail.com>
2022-02-22 16:31:32 -0500

    M3-5617: Update Tooltip stories (#8215)
    ## Description
    - Converts CopyTooltip to a functional component
        - Hovering over the component now displays a "Copy" tooltip
        - Remove unused `standAlone` and `ariaLabel` prop
    - Converts CopyTooltip story to mdx format
    - Converts HelpIcon story to mdx format
    - Removed unnecessary `size` prop from HelpIcon
    
    ## How to test
    - `yarn storybook`
    - Check stories under http://localhost:6006/?path=/docs/ui-elements-tooltip
    - Check the app for any regressions related to the refactoring
    
    Co-authored-by: Joe D'Amore <jdamore@linode.com>

--- packages/manager/src/components/CopyTooltip/CopyTooltip.tsx
+++ packages/manager/src/components/CopyTooltip/CopyTooltip.tsx
@@ -2,139 +2,83 @@ import classNames from 'classnames';
 import * as copy from 'copy-to-clipboard';
 import * as React from 'react';
 import FileCopy from 'src/assets/icons/copy.svg';
-import {
-  createStyles,
-  Theme,
-  withStyles,
-  WithStyles,
-} from 'src/components/core/styles';
+import { makeStyles, Theme } from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
+import ToolTip from 'src/components/core/Tooltip';
 
 interface Props {
   text: string;
   className?: string;
-  standAlone?: boolean;
-  ariaLabel?: string;
   displayText?: string;
   onClickCallback?: () => void;
 }
 
-interface State {
-  copied: boolean;
-}
-
-type CSSClasses = 'root' | 'copied' | 'standAlone' | 'displayText' | 'flex';
-
-const styles = (theme: Theme) =>
-  createStyles({
-    '@keyframes popUp': {
-      from: {
-        opacity: 0,
-        top: -10,
-        transform: 'scale(.1)',
-      },
-      to: {
-        opacity: 1,
-        top: -45,
-        transform: 'scale(1)',
-      },
-    },
-    root: {
-      position: 'relative',
-      padding: 4,
-      backgroundColor: 'transparent',
-      transition: theme.transitions.create(['background-color']),
-      borderRadius: 4,
-      border: 'none',
-      cursor: 'pointer',
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    position: 'relative',
+    padding: 4,
+    backgroundColor: 'transparent',
+    transition: theme.transitions.create(['background-color']),
+    borderRadius: 4,
+    border: 'none',
+    cursor: 'pointer',
+    color: theme.color.grey1,
+    '& svg': {
+      transition: theme.transitions.create(['color']),
       color: theme.color.grey1,
-      '& svg': {
-        transition: theme.transitions.create(['color']),
-        color: theme.color.grey1,
-        margin: 0,
-        position: 'relative',
-        width: 20,
-        height: 20,
-      },
-      '&:hover': {
-        backgroundColor: theme.color.white,
-      },
+      margin: 0,
+      position: 'relative',
+      width: 20,
+      height: 20,
     },
-    copied: {
-      fontSize: '.85rem',
-      left: -16,
-      color: theme.palette.text.primary,
-      padding: '6px 8px',
+    '&:hover': {
       backgroundColor: theme.color.white,
-      position: 'absolute',
-      boxShadow: `0 0 5px ${theme.color.boxShadow}`,
-      transition: 'opacity .5s ease-in-out',
-      animation: '$popUp 200ms ease-in-out forwards',
     },
-    standAlone: {
-      marginLeft: theme.spacing(1),
-      '& svg': {
-        width: 14,
-      },
-    },
-    flex: {
-      display: 'flex',
-      width: 'auto !important',
-    },
-    displayText: {
-      color: theme.cmrTextColors.linkActiveLight,
-      marginLeft: 6,
-    },
-  });
+  },
+  flex: {
+    display: 'flex',
+    width: 'auto !important',
+  },
+  displayText: {
+    color: theme.cmrTextColors.linkActiveLight,
+    marginLeft: 6,
+  },
+}));
 
-type CombinedProps = Props & WithStyles<CSSClasses>;
+export const CopyTooltip: React.FC<Props> = (props) => {
+  const classes = useStyles();
+  const [copied, setCopied] = React.useState<boolean>(false);
 
-class CopyTooltip extends React.Component<CombinedProps, State> {
-  state = {
-    copied: false,
-  };
+  const { text, className, displayText, onClickCallback } = props;
 
-  clickIcon = () => {
-    this.setState({
-      copied: true,
-    });
-    window.setTimeout(() => this.setState({ copied: false }), 1500);
-    copy(this.props.text);
-    if (this.props.onClickCallback) {
-      this.props.onClickCallback();
+  const handleIconClick = () => {
+    setCopied(true);
+    window.setTimeout(() => setCopied(false), 1500);
+    copy(text);
+    if (onClickCallback) {
+      onClickCallback();
     }
   };
 
-  render() {
-    const { classes, text, className, standAlone, displayText } = this.props;
-    const { copied } = this.state;
-
-    return (
+  return (
+    <ToolTip title={copied ? 'Copied!' : 'Copy'} placement="top" data-qa-copied>
       <button
         aria-label={`Copy ${text} to clipboard`}
         name={text}
         type="button"
-        onClick={this.clickIcon}
+        onClick={handleIconClick}
         className={classNames(className, {
           [classes.root]: true,
-          [classes.standAlone]: standAlone,
           [classes.flex]: Boolean(displayText),
         })}
       >
-        {copied && (
-          <span className={classes.copied} data-qa-copied>
-            Copied!
-          </span>
-        )}
         <FileCopy />
         {displayText && (
           <Typography className={classes.displayText}>{displayText}</Typography>
         )}
       </button>
-    );
-  }
-}
+    </ToolTip>
+  );
+};
 
-const styled = withStyles(styles);
-
-export default styled(CopyTooltip);
+export default CopyTooltip;
