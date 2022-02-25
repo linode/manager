5619d52fb893509559434f552712937e9d435f41
Tiffany Wong <wong.tyan@gmail.com>
2022-02-25 10:43:34 -0500

    Initial commit

---
+++ packages/manager/.storybook/YourTheme.js
@@ -0,0 +1,40 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+import Logo from '../src/assets/logo/logo-text.svg';
+
+export default create({
+  base: 'light',
+
+  colorPrimary: 'hotpink',
+  colorSecondary: 'deepskyblue',
+
+  // UI
+  appBg: 'white',
+  appContentBg: 'silver',
+  appBorderColor: 'grey',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+
+  // Text colors
+  textColor: 'black',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // Toolbar default and active colors
+  barTextColor: 'silver',
+  barSelectedColor: 'black',
+  barBg: 'hotpink',
+
+  // Form colors
+  inputBg: 'white',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Linode',
+  brandUrl: 'https://www.linode.com',
+  brandImage: Logo,
+});
