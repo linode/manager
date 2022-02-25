d29a6af20f414cbe2abe1a0b6915ba581b51fca1
Banks Nussman <banks@nussman.us>
2022-02-25 11:44:17 -0500

    initial work

--- packages/manager/.storybook/preview.tsx
+++ packages/manager/.storybook/preview.tsx
@@ -1,20 +1,27 @@
 // .storybook/preview.js
+import React from 'react';
+import CssBaseline from '../src/components/core/CssBaseline';
 import { select, withKnobs } from '@storybook/addon-knobs';
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
-import React from 'react';
-import '../public/fonts/fonts.css';
-import CssBaseline from '../src/components/core/CssBaseline';
 import { ThemeProvider } from '../src/components/core/styles';
-import '../src/index.css';
 import { dark, light } from '../src/themes';
 import { wrapWithTheme } from '../src/utilities/testHelpers';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+import { handlers } from '../src/mocks/serverHandlers';
+import '../public/fonts/fonts.css';
+import '../src/index.css';
 
 const options = {
   dark,
   light,
 };
 
+initialize({
+  onUnhandledRequest: 'warn',
+});
+
 export const decorators = [
+  mswDecorator,
   withKnobs,
   (Story) => {
     const _key = select('theme', ['light', 'dark'], 'light');
@@ -51,14 +58,5 @@ export const parameters = {
   viewport: {
     viewports: MINIMAL_VIEWPORTS,
   },
+  msw: handlers,
 };
-
-// Use the Mock Service Worker to mock API requests.
-if (typeof global.process === 'undefined') {
-  try {
-    const { worker } = require('../src/mocks/testBrowser');
-    worker.start();
-  } catch (e) {
-    console.warn('Unable to start the MSW', e);
-  }
-}
