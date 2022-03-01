7388848243e9c9a482d68960f8b75533df2ea2af
Tiffany Wong <wong.tyan@gmail.com>
2022-03-01 17:09:56 -0500

    Fix broken test

--- packages/manager/src/features/Backups/AutoEnroll.tsx
+++ packages/manager/src/features/Backups/AutoEnroll.tsx
@@ -1,32 +1,40 @@
 import * as React from 'react';
 import FormControlLabel from 'src/components/core/FormControlLabel';
 import Paper from 'src/components/core/Paper';
-import { makeStyles, Theme } from 'src/components/core/styles';
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles,
+} from 'src/components/core/styles';
 import Typography from 'src/components/core/Typography';
 import ExternalLink from 'src/components/ExternalLink';
 import Grid from 'src/components/Grid';
 import Notice from 'src/components/Notice';
 import Toggle from 'src/components/Toggle';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: theme.bg.offWhite,
-    padding: theme.spacing(1),
-  },
-  header: {
-    marginBottom: theme.spacing(1),
-    fontSize: 17,
-  },
-  toggleLabel: {
-    display: 'flex',
-    alignItems: 'flex-start',
-    marginLeft: 0,
-    marginBottom: theme.spacing(1),
-  },
-  toggleLabelText: {
-    marginTop: theme.spacing(1) + theme.spacing(1) / 2,
-  },
-}));
+type ClassNames = 'root' | 'header' | 'toggleLabel' | 'toggleLabelText';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.bg.offWhite,
+      padding: theme.spacing(1),
+    },
+    header: {
+      marginBottom: theme.spacing(1),
+      fontSize: 17,
+    },
+    toggleLabel: {
+      display: 'flex',
+      alignItems: 'flex-start',
+      marginLeft: 0,
+      marginBottom: theme.spacing(1),
+    },
+    toggleLabelText: {
+      marginTop: theme.spacing(1) + theme.spacing(1) / 2,
+    },
+  });
 
 interface Props {
   enabled: boolean;
@@ -34,11 +42,10 @@ interface Props {
   toggle: () => void;
 }
 
-type CombinedProps = Props;
+type CombinedProps = Props & WithStyles<ClassNames>;
 
 export const AutoEnroll: React.FC<CombinedProps> = (props) => {
-  const classes = useStyles();
-  const { enabled, error, toggle } = props;
+  const { classes, enabled, error, toggle } = props;
 
   return (
     <Paper className={classes.root}>
@@ -82,4 +89,6 @@ export const AutoEnroll: React.FC<CombinedProps> = (props) => {
   );
 };
 
-export default AutoEnroll;
+const styled = withStyles(styles);
+
+export default styled(AutoEnroll);
