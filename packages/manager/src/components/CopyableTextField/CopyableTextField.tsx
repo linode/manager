f148637ed1edf89a8b8693f18e1b9a0703ab7393
Tiffany Wong <7692354+tiffwong@users.noreply.github.com>
2022-03-18 20:41:20 +0100

    M3-5633: Prevent referral link from being modified (#8261)
    ## Description
    
    This PR disables the referral link textfield to prevent it from being modified.
    
    ## How to test
    
    Go to `/profile/referrals` and check the referral link on light and dark mode.

--- packages/manager/src/components/CopyableTextField/CopyableTextField.tsx
+++ packages/manager/src/components/CopyableTextField/CopyableTextField.tsx
@@ -4,6 +4,13 @@ import { makeStyles, Theme } from 'src/components/core/styles';
 import TextField, { Props as TextFieldProps } from 'src/components/TextField';
 
 const useStyles = makeStyles((theme: Theme) => ({
+  removeDisabledStyles: {
+    '&.Mui-disabled': {
+      borderColor: theme.name === 'lightTheme' ? '#ccc' : '#222',
+      color: theme.name === 'lightTheme' ? 'inherit' : '#fff !important',
+      opacity: 1,
+    },
+  },
   copyIcon: {
     marginRight: theme.spacing(0.5),
     '& svg': {
@@ -22,20 +29,20 @@ type CombinedProps = Props;
 
 export const CopyableTextField: React.FC<CombinedProps> = (props) => {
   const classes = useStyles();
-
   const { value, className, hideIcon, ...restProps } = props;
 
   return (
     <TextField
       value={value}
       {...restProps}
-      className={`${className} ${'copy'}`}
-      data-qa-copy-tooltip
+      className={`${className} ${'copy'} ${classes.removeDisabledStyles}`}
+      disabled
       InputProps={{
         endAdornment: hideIcon ? undefined : (
           <CopyTooltip text={`${value}`} className={classes.copyIcon} />
         ),
       }}
+      data-qa-copy-tooltip
     />
   );
 };
