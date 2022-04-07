e36e321c5139c8dc7775b78f0e40c39288dd497a
Joe D'Amore <jdamore@linode.com>
2022-04-07 13:20:20 -0400

    Use random label for test images, remove beforeEach hook

--- packages/manager/cypress/integration/images/machine-image-upload.spec.ts
+++ packages/manager/cypress/integration/images/machine-image-upload.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 /* eslint-disable sonarjs/no-identical-functions */
-import { createMockImage, deleteAllTestImages } from '../../support/api/images';
+import { createMockImage } from '../../support/api/images';
 import { fbtClick, fbtVisible, getClick } from '../../support/helpers';
 import 'cypress-file-upload';
 import { assertToast } from 'cypress/support/ui/events';
@@ -11,10 +11,10 @@ import { interceptOnce } from 'cypress/support/ui/common';
 import { RecPartial } from 'factory.ts';
 import { EventStatus } from '../../../../api-v4/lib/account/types';
 import { ImageStatus } from '../../../../api-v4/lib/images/types';
-
-const imageLabel = 'cy-test-image';
+import { randomLabel } from 'support/util/random';
 
 const eventIntercept = (
+  label: string,
   id: number,
   status: RecPartial<EventStatus>,
   message?: string,
@@ -28,7 +28,7 @@ const eventIntercept = (
         created: created ? created : DateTime.local().toISO(),
         action: 'image_upload',
         entity: {
-          label: imageLabel,
+          label: label,
           id,
           type: 'image',
           url: `/v4/images/private/${id}`,
@@ -41,37 +41,34 @@ const eventIntercept = (
   ).as('getEvent');
 };
 
-const imageIntercept = (id: number, status: ImageStatus) => {
+const imageIntercept = (label: string, id: number, status: ImageStatus) => {
   cy.intercept('GET', `*/images*`, (req) => {
-    req.reply(createMockImage({ label: imageLabel, id, status }));
+    req.reply(createMockImage({ label, id, status }));
   }).as('getImage');
 };
 
-const assertFailed = (imageId: number, message: string) => {
-  assertToast(
-    `There was a problem processing image ${imageLabel}: ${message}`,
-    2
-  );
+const assertFailed = (label: string, imageId: number, message: string) => {
+  assertToast(`There was a problem processing image ${label}: ${message}`, 2);
   cy.get(`[data-qa-image-cell="${imageId}"]`).within(() => {
-    fbtVisible(imageLabel);
+    fbtVisible(label);
     fbtVisible('Failed');
     fbtVisible('N/A');
   });
 };
 
-const assertProcessing = (imageId: number) => {
+const assertProcessing = (label: string, imageId: number) => {
   cy.get(`[data-qa-image-cell="${imageId}"]`).within(() => {
-    fbtVisible(imageLabel);
+    fbtVisible(label);
     fbtVisible('Processing');
     fbtVisible('Pending');
   });
 };
 
-const uploadImage = () => {
+const uploadImage = (label: string) => {
   const regionSelect = 'Fremont, CA';
   const upload = 'testImage.gz';
   cy.visitWithLogin('/images/create/upload');
-  getClick('[id="label"][data-testid="textfield-input"]').type(imageLabel);
+  getClick('[id="label"][data-testid="textfield-input"]').type(label);
   getClick('[id="description"]').type('This is a machine image upload test');
   fbtClick('Select a Region');
   fbtClick(regionSelect);
@@ -86,67 +83,67 @@ const uploadImage = () => {
 };
 
 describe('machine image', () => {
-  beforeEach(() => {
-    deleteAllTestImages();
-  });
-
   it('uploads machine image, mock finished event', () => {
+    const label = randomLabel();
     const status = 'finished';
-    uploadImage();
+    uploadImage(label);
     cy.wait('@imageUpload').then((xhr) => {
       const imageId = xhr.response?.body.image.id;
-      imageIntercept(imageId, 'available');
-      eventIntercept(imageId, status);
+      imageIntercept(label, imageId, 'available');
+      eventIntercept(label, imageId, status);
       assertToast(
-        `Image ${imageLabel} uploaded successfully. It is being processed and will be available shortly.`
+        `Image ${label} uploaded successfully. It is being processed and will be available shortly.`
       );
       cy.wait('@getImage');
       cy.wait('@getEvent');
-      assertToast(`Image ${imageLabel} is now available.`, 2);
+      assertToast(`Image ${label} is now available.`, 2);
       cy.get(`[data-qa-image-cell="${imageId}"]`).within(() => {
-        fbtVisible(imageLabel);
+        fbtVisible(label);
         fbtVisible('Ready');
       });
     });
   });
 
   it('uploads machine image, mock upload canceled failed event', () => {
+    const label = randomLabel();
     const status = 'failed';
     const message = 'Upload canceled';
-    uploadImage();
+    uploadImage(label);
     cy.wait('@imageUpload').then((xhr) => {
       const imageId = xhr.response?.body.image.id;
-      assertProcessing(imageId);
-      eventIntercept(imageId, status, message);
+      assertProcessing(label, imageId);
+      eventIntercept(label, imageId, status, message);
       cy.wait('@getEvent');
-      assertFailed(imageId, message);
+      assertFailed(label, imageId, message);
     });
   });
 
   it('uploads machine image, mock failed to decompress failed event', () => {
+    const label = randomLabel();
     const status = 'failed';
     const message = 'Failed to decompress image';
-    uploadImage();
+    uploadImage(label);
     cy.wait('@imageUpload').then((xhr) => {
       const imageId = xhr.response?.body.image.id;
-      assertProcessing(imageId);
-      eventIntercept(imageId, status, message);
+      assertProcessing(label, imageId);
+      eventIntercept(label, imageId, status, message);
       cy.wait('@getEvent');
-      assertFailed(imageId, message);
+      assertFailed(label, imageId, message);
     });
   });
 
   it('uploads machine image, mock expired upload event', () => {
+    const label = randomLabel();
     const status = 'failed';
     const message = 'Upload window expired';
     const expiredDate = DateTime.local().minus({ days: 1 }).toISO();
-    uploadImage();
+    uploadImage(label);
     cy.wait('@imageUpload').then((xhr) => {
       const imageId = xhr.response?.body.image.id;
-      assertProcessing(imageId);
-      eventIntercept(imageId, status, message, expiredDate);
+      assertProcessing(label, imageId);
+      eventIntercept(label, imageId, status, message, expiredDate);
       cy.wait('@getEvent');
-      assertFailed(imageId, message);
+      assertFailed(label, imageId, message);
     });
   });
 });
