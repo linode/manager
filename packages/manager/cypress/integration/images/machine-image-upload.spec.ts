92a87c03d8eae1f5b4f559311bd3b90a6dc43cf9
Joe D'Amore <jdamore@linode.com>
2022-04-07 18:54:47 -0400

    Add docs, improve function parameter name consistency

--- packages/manager/cypress/integration/images/machine-image-upload.spec.ts
+++ packages/manager/cypress/integration/images/machine-image-upload.spec.ts
@@ -26,6 +26,18 @@ const numericImageIdFromString = (imageIdString: string): number => {
   const substring = imageIdString.split('/')[1];
   return parseInt(substring, 10);
 };
+
+/**
+ * Intercepts the response from the next events poll request.
+ *
+ * Responds with an `image_upload` event associated with an image.
+ *
+ * @param label - Label of image that is associated with the event.
+ * @param id - ID of image that is associated with the event. Expected to be prefixed with a string (e.g. 'private/12345').
+ * @param status - Event status.
+ * @param message - Optional event message.
+ * @param created - Optional event created data. If omitted, the current time is used.
+ */
 const eventIntercept = (
   label: string,
   id: string,
@@ -55,6 +67,15 @@ const eventIntercept = (
   ).as('getEvent');
 };
 
+/**
+ * Intercepts the response for an image GET request.
+ *
+ * Responds with an image with the given label, ID, and status.
+ *
+ * @param label - Response image label.
+ * @param id - Response image ID. Expected to be prefixed with a string (e.g. 'private/12345').
+ * @param status - Image status.
+ */
 const imageIntercept = (label: string, id: string, status: ImageStatus) => {
   cy.intercept('GET', `*/images/${id}*`, (req) => {
     req.reply(
@@ -67,23 +88,43 @@ const imageIntercept = (label: string, id: string, status: ImageStatus) => {
   }).as('getImage');
 };
 
-const assertFailed = (label: string, imageId: number, message: string) => {
-  assertToast(`There was a problem processing image ${label}: ${message}`, 2);
-  cy.get(`[data-qa-image-cell="${imageId}"]`).within(() => {
+/**
+ * Asserts that provisioning an image fails.
+ *
+ * @param label - Label for image that is expected to fail.
+ * @param id - ID for image that is expected to fail. Expected to be prefixed with a string (e.g. 'private/12345').
+ * @param message - Expected failure message.
+ */
+const assertFailed = (label: string, id: string, message: string) => {
+  assertToast(`There was a problem processing image ${label}: ${message}`);
+  cy.get(`[data-qa-image-cell="${id}"]`).within(() => {
     fbtVisible(label);
     fbtVisible('Failed');
     fbtVisible('N/A');
   });
 };
 
-const assertProcessing = (label: string, imageId: number) => {
-  cy.get(`[data-qa-image-cell="${imageId}"]`).within(() => {
+/**
+ * Asserts that an image is in the process of being provisioned.
+ *
+ * @param label - Label for image that is expected to be processing.
+ * @param id - ID for image that is expected to be processing. Expected to be prefixed with a string (e.g. 'private/12345').
+ */
+const assertProcessing = (label: string, id: string) => {
+  cy.get(`[data-qa-image-cell="${id}"]`).within(() => {
     fbtVisible(label);
     fbtVisible('Processing');
     fbtVisible('Pending');
   });
 };
 
+/**
+ * Uploads an image and intercepts the request response.
+ *
+ * The image is uploaded to a random region.
+ *
+ * @param label - Label to apply to uploaded image.
+ */
 const uploadImage = (label: string) => {
   const regionSelect = randomItem(regionsFriendly);
   const upload = 'testImage.gz';
@@ -92,7 +133,8 @@ const uploadImage = (label: string) => {
   getClick('[id="description"]').type('This is a machine image upload test');
   fbtClick('Select a Region');
   fbtClick(regionSelect);
-  cy.fixture(upload).then((fileContent) => {
+  // Pass `null` to `cy.fixture()` to ensure file is encoded as a Cypress buffer object.
+  cy.fixture(upload, null).then((fileContent) => {
     cy.get('input[accept="application/x-gzip"]').attachFile({
       fileContent,
       fileName: 'testImage',
@@ -103,20 +145,20 @@ const uploadImage = (label: string) => {
 };
 
 describe('machine image', () => {
-  it('uploads machine image, mock finished event', () => {
+  /*
+   * - Uploads machine image.
+   * - Confirms that image uploads successfully.
+   * - Confirms that machine image is listed in landing page as expected.
+   * - Confirms that notifications appear that describe the image's success status.
+   */
+  it.skip('uploads machine image, end-to-end', () => {
+    // @TODO Unskip 'uploads machine image, end-to-end' when we have a better path forward for segmenting slower tests.
+    // See also: M3-5723 "Investigate creating a second 'tier' of Cypress tests".
     const label = randomLabel();
-    const status = 'finished';
     uploadImage(label);
     cy.wait('@imageUpload').then((xhr) => {
       const imageId = xhr.response?.body.image.id;
-      imageIntercept(label, imageId, 'available');
-      eventIntercept(label, imageId, status);
-      assertToast(
-        `Image ${label} uploaded successfully. It is being processed and will be available shortly.`
-      );
-      cy.wait('@getImage');
-      cy.wait('@getEvent');
-      assertToast(`Image ${label} is now available.`, 2);
+      assertToast(`Image ${label} is now available.`);
       cy.get(`[data-qa-image-cell="${imageId}"]`).within(() => {
         fbtVisible(label);
         fbtVisible('Ready');
@@ -124,6 +166,39 @@ describe('machine image', () => {
     });
   });
 
+  /*
+   * - Uploads machine image.
+   * - Mocks events and image requests to simulate successful upload.
+   * - Confirms that machine image is listed in landing page as expected.
+   * - Confirms that notifications appear that describe the image's success status.
+   */
+  it('uploads machine image, mock finish event', () => {
+    const label = randomLabel();
+    const status = 'finished';
+    uploadImage(label);
+    cy.wait('@imageUpload').then((xhr) => {
+      const imageId = xhr.response?.body.image.id;
+      assertProcessing(label, imageId);
+      imageIntercept(label, imageId, 'available');
+      eventIntercept(label, imageId, status);
+      assertToast(
+        `Image ${label} uploaded successfully. It is being processed and will be available shortly.`
+      );
+      cy.wait('@getImage');
+      assertToast(`Image ${label} is now available.`);
+      cy.get(`[data-qa-image-cell="${imageId}"]`).within(() => {
+        fbtVisible(label);
+        fbtVisible('Ready');
+      });
+    });
+  });
+
+  /*
+   * - Uploads machine image.
+   * - Mocks events to simulate cancelled upload.
+   * - Confirms that machine image is listed in landing page as expected.
+   * - Confirms that notifications appear that describe the image's failed status.
+   */
   it('uploads machine image, mock upload canceled failed event', () => {
     const label = randomLabel();
     const status = 'failed';
@@ -138,6 +213,12 @@ describe('machine image', () => {
     });
   });
 
+  /*
+   * - Uploads machine image.
+   * - Mocks events to simulate decompression failure.
+   * - Confirms that machine image is listed in landing page as expected.
+   * - Confirms that notifications appear that describe the image's failed status.
+   */
   it('uploads machine image, mock failed to decompress failed event', () => {
     const label = randomLabel();
     const status = 'failed';
@@ -152,6 +233,12 @@ describe('machine image', () => {
     });
   });
 
+  /*
+   * - Uploads machine image.
+   * - Mocks events to simulate upload expiration failure.
+   * - Confirms that machine image is listed in landing page as expected.
+   * - Confirms that notifications appear that describe the image's failed status.
+   */
   it('uploads machine image, mock expired upload event', () => {
     const label = randomLabel();
     const status = 'failed';
