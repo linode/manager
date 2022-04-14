086f2de7352d7fc67cb4d26be51c373d77d5f8e6
Joe D'Amore <jdamore@linode.com>
2022-04-14 15:54:41 -0400

    Add utility function to depaginate data from API

--- packages/manager/cypress/support/util/paginate.ts
+++ packages/manager/cypress/support/util/paginate.ts
@@ -1,4 +1,6 @@
+import { ResourcePage } from '@linode/api-v4/lib/types';
 import { Response } from 'support/util/response';
+
 /**
  * Paginated data.
  */
@@ -24,6 +26,51 @@ export const paginate = (data: any | any[]): PaginatedData => {
     page: 1,
     pages: 1,
   };
+};
+
+/**
+ * Depaginates data from a function that returns paginated results.
+ *
+ * Accomplishes this by fetching the first page of results using the given
+ * function, and fetching any remaining results if the first response does not
+ * contain all of the data.
+ *
+ * @example
+ * // Create a generator function, then use it to depaginate buckets.
+ * const bucketsPage = (page: number) => getBuckets({ page });
+ * const buckets: ObjectStorageBucket[] = await depaginate(bucketsPage);
+ *
+ * @param resultGenerator - A function which generates a paginated result.
+ *
+ * @returns Promise that resolves to an array of data.
+ */
+export const depaginate = async <T>(resultGenerator: (number) => Promise<ResourcePage<T>>): Promise<Array<T>> => {
+  const firstResult: ResourcePage<T> = await resultGenerator(1);
+  let data = firstResult.data;
+  if (firstResult.pages > 1) {
+    const remainingResults: number = (firstResult.pages - 1);
+    const remainingResultsPromises = Array(remainingResults)
+      .fill(null)
+      .map(async (_element: null, index: number) => {
+        const pageNumber = index + 2;
+        const results = await resultGenerator(pageNumber);
+        return results.data;
+      });
+
+    const responseResults = (await Promise.all(remainingResultsPromises)).reduce((acc, cur) => {
+      return [
+        ...acc,
+        ...cur,
+      ];
+    }, []);
+
+    return [
+      ...data,
+      ...responseResults,
+    ];
+  }
+
+  return data;
 };
 
 /**
