c535a99a195e3e3803d3f01b48636fda5603e7d7
Banks Nussman <banks@nussman.us>
2022-04-15 15:33:45 -0400

    enable LinodeSummary.test.tsx

--- packages/manager/src/features/linodes/LinodesDetail/LinodeSummary/LinodeSummary.test.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeSummary/LinodeSummary.test.tsx
@@ -4,7 +4,7 @@ import { linodes } from 'src/__data__/linodes';
 import { light } from 'src/themes';
 import { LinodeSummary } from './LinodeSummary';
 
-describe.skip('LinodeSummary', () => {
+describe('LinodeSummary', () => {
   const wrapper = shallow(
     <LinodeSummary
       isBareMetalInstance={false}
