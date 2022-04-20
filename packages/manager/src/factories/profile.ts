8ed3dd6622185f80ef8b5b4ad7141aa2b422f088
Joe D'Amore <jdamore@linode.com>
2022-04-20 16:11:33 -0400

    Add alt payment method API functions and types, factories, and MSW handlers

--- packages/manager/src/factories/profile.ts
+++ packages/manager/src/factories/profile.ts
@@ -1,5 +1,9 @@
 import * as Factory from 'factory.ts';
-import { Profile } from '@linode/api-v4/lib/profile/types';
+import {
+  Profile,
+  PhoneNumberVerificationCode,
+  SecurityQuestions,
+} from '@linode/api-v4/lib/profile/types';
 
 export const profileFactory = Factory.Sync.makeFactory<Profile>({
   uid: 9999,
@@ -21,4 +25,19 @@ export const profileFactory = Factory.Sync.makeFactory<Profile>({
   two_factor_auth: false,
   restricted: false,
   authentication_type: 'password',
+  phone_number: '+15555555555',
 });
+
+export const phoneNumberVerificationCodeFactory = Factory.Sync.makeFactory<PhoneNumberVerificationCode>(
+  {
+    otp_code: Factory.each((i) => 10000 + i),
+  }
+);
+
+export const securityQuestionsFactory = Factory.Sync.makeFactory<SecurityQuestions>(
+  {
+    'Security question 1': 'Answer 1',
+    'Security question 2': 'Answer 2',
+    'Security question 3': 'Answer 3',
+  }
+);
