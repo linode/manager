a1fbc69f2bc8e981de5f29deb5dd5cf8a01d7365
Dajahi Wiley <32860776+DevDW@users.noreply.github.com>
2022-05-16 12:22:25 -0400

    M3-5813: Types and related adjustments for PostgreSQL and MongoDB (#8369)
    feat(DBaaS): Types, schema, and related adjustments for PostgreSQL and MongoDB

--- packages/manager/src/features/Databases/DatabaseCreate/DatabaseCreate.tsx
+++ packages/manager/src/features/Databases/DatabaseCreate/DatabaseCreate.tsx
@@ -1,12 +1,12 @@
 import {
   ClusterSize,
+  ComprehensiveReplicationType,
   CreateDatabasePayload,
   DatabaseClusterSizeObject,
   DatabaseEngine,
   DatabasePriceObject,
   DatabaseType,
   Engine,
-  ReplicationType,
 } from '@linode/api-v4/lib/databases/types';
 import { APIError } from '@linode/api-v4/lib/types';
 import { createDatabaseSchema } from '@linode/validation/lib/databases.schema';
@@ -318,7 +318,8 @@ const DatabaseCreate: React.FC<{}> = () => {
       region: '',
       type: '',
       cluster_size: -1 as ClusterSize,
-      replication_type: 'none' as ReplicationType,
+      replication_type: 'none' as ComprehensiveReplicationType,
+      replication_commit_type: undefined, // specific to Postgres
       allow_list: [
         {
           address: '',
@@ -326,6 +327,8 @@ const DatabaseCreate: React.FC<{}> = () => {
         },
       ],
       ssl_connection: true,
+      storage_engine: undefined, // specific to MongoDB
+      compression_type: undefined, // specific to MongoDB
     },
     validationSchema: createDatabaseSchema,
     validateOnChange: false,
@@ -407,8 +410,14 @@ const DatabaseCreate: React.FC<{}> = () => {
     );
     setFieldValue(
       'replication_type',
-      values.cluster_size === 1 ? 'none' : 'semi_synch'
+      determineReplicationType(values.cluster_size, values.engine)
     );
+    setFieldValue(
+      'replication_commit_type',
+      determineReplicationCommitType(values.engine)
+    );
+    setFieldValue('storage_engine', determineStorageEngine(values.engine));
+    setFieldValue('compression_type', determineCompressionType(values.engine));
   }, [dbtypes, setFieldValue, values.cluster_size, values.type, values.engine]);
 
   if (regionsLoading || !regionsData || enginesLoading || typesLoading) {
@@ -622,6 +631,51 @@ const DatabaseCreate: React.FC<{}> = () => {
       </Grid>
     </form>
   );
+};
+
+const determineReplicationType = (clusterSize: number, engine: string) => {
+  if (Boolean(engine.match(/mongo/))) {
+    return undefined;
+  }
+
+  // If engine is a MySQL or Postgres one and it's a standalone DB instance
+  if (clusterSize === 1) {
+    return 'none';
+  }
+
+  // MySQL engine & cluster = semi_synch. PostgreSQL engine & cluster = asynch.
+  if (Boolean(engine.match(/mysql/))) {
+    return 'semi_synch';
+  } else {
+    return 'asynch';
+  }
+};
+
+const determineReplicationCommitType = (engine: string) => {
+  // 'local' is the default.
+  if (Boolean(engine.match(/postgres/))) {
+    return 'local';
+  }
+
+  return undefined;
+};
+
+const determineStorageEngine = (engine: string) => {
+  // 'wiredtiger' is the default.
+  if (Boolean(engine.match(/mongo/))) {
+    return 'wiredtiger';
+  }
+
+  return undefined;
+};
+
+const determineCompressionType = (engine: string) => {
+  // 'none' is the default.
+  if (Boolean(engine.match(/mongo/))) {
+    return 'none';
+  }
+
+  return undefined;
 };
 
 export default DatabaseCreate;
