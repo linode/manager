d50db95b8ad12f92f1885c6225b506be68b866d3
Banks Nussman <banks@nussman.us>
2022-06-21 15:45:46 -0400

    update test

--- packages/manager/src/features/linodes/LinodesDetail/LinodeSummary/ActivitySummary.test.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeSummary/ActivitySummary.test.tsx
@@ -1,6 +1,6 @@
-import * as account from '@linode/api-v4/lib/account/events';
-import { shallow } from 'enzyme';
+const account = require('@linode/api-v4/lib/account/events');
 import * as React from 'react';
+import { renderWithTheme } from 'src/utilities/testHelpers';
 import { ActivitySummary } from './ActivitySummary';
 
 const props = {
@@ -14,15 +14,16 @@ const props = {
     viewMore: '',
   },
 };
-const component = shallow(<ActivitySummary {...props} />);
 
 describe('ActivitySummary component', () => {
   it('should render', () => {
-    expect(component).toHaveLength(1);
+    const { getByText } = renderWithTheme(<ActivitySummary {...props} />);
+    expect(getByText('Activity Feed')).toBeInTheDocument();
   });
 
   it.skip("should request the Linode's events on load", () => {
     const mockGetEvents = jest.spyOn<any, any>(account, 'getEvents');
+    renderWithTheme(<ActivitySummary {...props} />);
     expect(mockGetEvents).toHaveBeenCalledWith(
       {},
       { 'entity.id': 123456, 'entity.type': 'linode' }
