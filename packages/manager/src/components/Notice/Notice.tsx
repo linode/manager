603d641459971694dd014a9f837f7abe3b67fcbd
Banks Nussman <banks@nussman.us>
2022-06-22 14:56:43 -0400

    2FA with Security Questions Fixes (#8422)

--- packages/manager/src/components/Notice/Notice.tsx
+++ packages/manager/src/components/Notice/Notice.tsx
@@ -137,8 +137,8 @@ interface Props extends GridProps {
   className?: string;
   flag?: boolean;
   notificationList?: boolean;
-  spacingTop?: 0 | 8 | 12 | 16 | 24;
-  spacingBottom?: 0 | 8 | 12 | 16 | 20 | 24 | 32;
+  spacingTop?: 0 | 4 | 8 | 12 | 16 | 24;
+  spacingBottom?: 0 | 4 | 8 | 12 | 16 | 20 | 24 | 32;
   spacingLeft?: number;
   breakWords?: boolean;
   onClick?: (e: React.MouseEvent<HTMLElement>) => void;
