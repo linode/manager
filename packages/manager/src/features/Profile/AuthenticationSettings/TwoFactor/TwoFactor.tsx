e432db7949d99eebabdfdd3153c96e5a8c80531e
Banks Nussman <banks@nussman.us>
2022-06-29 14:54:41 -0400

    update font size

--- packages/manager/src/features/Profile/AuthenticationSettings/TwoFactor/TwoFactor.tsx
+++ packages/manager/src/features/Profile/AuthenticationSettings/TwoFactor/TwoFactor.tsx
@@ -222,6 +222,7 @@ export const TwoFactor: React.FC<Props> = (props) => {
                     warning
                     text={needSecurityQuestionsCopy}
                     style={{ marginTop: '8px' }}
+                    typeProps={{ style: { fontSize: '0.875rem' } }}
                   />
                 )}
                 {twoFactorEnabled && (
