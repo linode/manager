e3c6962852e2148509fe7faf9c19f41c45350aaf
Banks Nussman <banks@nussman.us>
2022-07-05 15:31:27 -0400

    include a build for the browser

--- packages/api-v4/tsup.config.ts
+++ packages/api-v4/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
+    format: ['esm', 'cjs', 'iife'],
     target: 'es6',
     outDir: 'lib',
     legacyOutput: true,
