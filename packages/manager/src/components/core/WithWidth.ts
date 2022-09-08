beb259c6f141fa6e64ba723eb98bcb3bf39c308c
Banks Nussman <banks@nussman.us>
2022-09-08 17:43:32 -0400

    remove WithWith generated file

--- packages/manager/src/components/core/WithWidth.ts
+++
@@ -1,7 +0,0 @@
-// FIXME checkout https://mui.com/components/use-media-query/#migrating-from-withwidth
-const withWidth = () => (WrappedComponent) => (props) => <WrappedComponent {...props} width="xs" />;
-
-/* tslint:disable-next-line:no-empty-interface */
-export interface WithWidth extends _WithWidth {}
-
-export default withWidth;
