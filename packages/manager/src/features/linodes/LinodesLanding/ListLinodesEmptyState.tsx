7405a6a09032bc89f926d7dd571a1b79b349d3c6
Cody Finn <cfinn@linode.com>
2022-09-16 10:25:30 -0400

    M3-5905: Events tags on Linodes landing page (#8534)
    Adds GA events to the docs links on the empty Linode landing page.

--- packages/manager/src/features/linodes/LinodesLanding/ListLinodesEmptyState.tsx
+++ packages/manager/src/features/linodes/LinodesLanding/ListLinodesEmptyState.tsx
@@ -4,9 +4,15 @@ import LinodeSvg from 'src/assets/icons/entityIcons/linode.svg';
 import Typography from 'src/components/core/Typography';
 import Link from 'src/components/Link';
 import Placeholder from 'src/components/Placeholder';
+import { sendEvent } from 'src/utilities/ga';
 
 export const ListLinodesEmptyState: React.FC<{}> = (_) => {
   const { push } = useHistory();
+
+  const emptyLinodeLandingGAEventTemplate = {
+    category: 'Linodes landing page empty',
+    action: 'Click:link',
+  };
 
   return (
     <Placeholder
@@ -15,7 +21,14 @@ export const ListLinodesEmptyState: React.FC<{}> = (_) => {
       isEntity
       buttonProps={[
         {
-          onClick: () => push('/linodes/create'),
+          onClick: () => {
+            sendEvent({
+              category: 'Linodes landing page empty',
+              action: 'Click:button',
+              label: 'Create Linode',
+            });
+            push('/linodes/create');
+          },
           children: 'Create Linode',
         },
       ]}
@@ -25,11 +38,27 @@ export const ListLinodesEmptyState: React.FC<{}> = (_) => {
         getting started?
       </Typography>
       <Typography variant="subtitle1">
-        <Link to="https://linode.com/docs/getting-started-new-manager/">
+        <Link
+          to="https://linode.com/docs/getting-started-new-manager/"
+          onClick={() => {
+            sendEvent({
+              ...emptyLinodeLandingGAEventTemplate,
+              label: 'Learn more about getting started',
+            });
+          }}
+        >
           Learn more about getting started
         </Link>
         &nbsp;or&nbsp;
-        <Link to="https://www.linode.com/docs/">
+        <Link
+          to="https://www.linode.com/docs/"
+          onClick={() => {
+            sendEvent({
+              ...emptyLinodeLandingGAEventTemplate,
+              label: 'visit our guides and tutorials',
+            });
+          }}
+        >
           visit our guides and tutorials.
         </Link>
       </Typography>
