ad4637553027ea566ca1bcac524310acf3dce8aa
Joe D'Amore <jdamore@linode.com>
2022-09-27 12:49:36 -0400

    Add factories for user preferences, Managed contacts, and Managed issues

--- packages/manager/src/factories/managed.ts
+++ packages/manager/src/factories/managed.ts
@@ -1,14 +1,40 @@
 import * as Factory from 'factory.ts';
 import {
+  ManagedContact,
   ManagedCredential,
+  ManagedIssue,
   ManagedServiceMonitor,
   ManagedStatsData,
 } from '@linode/api-v4/lib/managed/types';
+
+export const contactFactory = Factory.Sync.makeFactory<ManagedContact>({
+  email: 'john.doe@example.com',
+  group: 'on-call',
+  id: Factory.each((i) => i),
+  name: 'John Doe',
+  phone: {
+    primary: '555-555-5555',
+    secondary: null,
+  },
+  updated: '2019-08-01T20:29:14',
+});
 
 export const credentialFactory = Factory.Sync.makeFactory<ManagedCredential>({
   id: Factory.each((i) => i),
   last_decrypted: '2019-07-01',
   label: 'credential-1',
+});
+
+export const issueFactory = Factory.Sync.makeFactory<ManagedIssue>({
+  id: Factory.each((i) => i),
+  services: Factory.each((i) => [i]),
+  created: '2019-08-01T20:29:14',
+  entity: {
+    id: Factory.each((i) => i),
+    label: Factory.each((i) => `Managed issue #${i}`),
+    type: 'ticket',
+    url: Factory.each((i) => `/support/tickets/${i}`),
+  },
 });
 
 export const monitorFactory = Factory.Sync.makeFactory<ManagedServiceMonitor>({
