ef3668558fc99215ffe80fb7ff6f69b616b6ff69
Banks Nussman <banks@nussman.us>
2022-10-03 12:21:27 -0400

    actually fix tsc error

--- packages/manager/src/components/core/Grid.tsx
+++ packages/manager/src/components/core/Grid.tsx
@@ -13,7 +13,7 @@ import * as React from 'react';
 import clsx from 'clsx';
 import { Theme, withStyles } from './styles';
 import { Breakpoint } from '@mui/material';
-import { GridProps } from '@mui/material/Grid';
+import type { GridProps } from '@mui/material/Grid';
 
 const SPACINGS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const GRID_SIZES = ['auto', true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
