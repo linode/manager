3410137d3bcdffaa7d8fbf43836333a37232f0e3
Hana Xu <hanaxu12@gmail.com>
2022-10-10 16:04:29 -0400

    M3-5764: Add HA chip to Kubernetes landing page (#8567)
    ## Description 📝
    - Add HA chip to the Kubernetes landing page
    - Update HA chip size in the Kubernetes details page to match the landing page chip
    - Minor refactoring
    
    ## How to test 🧪
    Check the view in `/kubernetes/clusters` and `/kubernetes/clusters/<cluster_id>/summary`
    ```
    yarn test ClusterRow
    ```

--- packages/manager/src/features/Kubernetes/kubeUtils.ts
+++ packages/manager/src/features/Kubernetes/kubeUtils.ts
@@ -1,3 +1,4 @@
+import { Account } from '@linode/api-v4/lib/account';
 import {
   KubernetesCluster,
   KubernetesVersion,
@@ -149,4 +150,22 @@ export const getNextVersion = (
     return null;
   }
   return versionStrings[currentIdx + 1];
+};
+
+export const getKubeHighAvailability = (
+  account: Account | undefined,
+  cluster?: ExtendedCluster | null
+) => {
+  const showHighAvailability = account?.capabilities.includes(
+    'LKE HA Control Planes'
+  );
+
+  const isClusterHighlyAvailable = Boolean(
+    showHighAvailability && cluster?.control_plane.high_availability
+  );
+
+  return {
+    showHighAvailability,
+    isClusterHighlyAvailable,
+  };
 };
