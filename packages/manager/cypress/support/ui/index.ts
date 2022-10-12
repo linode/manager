0f5367b674176c23a8555aad73747f0e7df08a1a
Joe D'Amore <jdamore@linode.com>
2022-10-12 13:12:58 -0400

    Add Cypress UI helpers for notification toasts and action menus

--- packages/manager/cypress/support/ui/index.ts
+++ packages/manager/cypress/support/ui/index.ts
@@ -1,3 +1,4 @@
+import * as actionMenu from './action-menu';
 import * as buttons from './buttons';
 import * as dialog from './dialog';
 import * as drawer from './drawer';
@@ -5,15 +6,18 @@ import * as entityHeader from './entity-header';
 import * as heading from './heading';
 import * as nav from './nav';
 import * as tabList from './tab-list';
+import * as toast from './toast';
 import * as toggle from './toggle';
 
 export const ui = {
+  ...actionMenu,
   ...buttons,
   ...dialog,
   ...drawer,
   ...entityHeader,
   ...heading,
   ...nav,
+  ...toast,
   ...tabList,
   ...toggle,
 };
