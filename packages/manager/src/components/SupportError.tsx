300491912c4b7fb989a25cf2ed22517f8391398f
mjac0bs <mjacobs@akamai.com>
2022-10-27 07:51:27 -0700

    Replace support text anywhere in message

--- packages/manager/src/components/SupportError.tsx
+++ packages/manager/src/components/SupportError.tsx
@@ -9,14 +9,24 @@ interface Props {
 
 export const SupportError: React.FC<Props> = (props) => {
   const { errors } = props;
-  const [reason] = errors[0].reason.split(/open a support ticket\./i);
+  const errorMsg = errors[0].reason.split(/(open a support ticket)/i);
+  const ticketLinkText =
+    errorMsg[0].length === 0
+      ? 'Open a support ticket'
+      : 'open a support ticket';
 
   const classes = useStyles();
 
   return (
     <Typography className={classes.noticeText}>
-      {reason} {` `}
-      <SupportLink text="open a support ticket" />.
+      {errorMsg.map((substring: string) => {
+        const openTicket = substring.match(/open a support ticket/i);
+        if (openTicket) {
+          return <SupportLink text={ticketLinkText} />;
+        } else {
+          return substring;
+        }
+      })}
     </Typography>
   );
 };
