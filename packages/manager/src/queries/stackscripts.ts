4e972d6909f483fd206163875986574c2609044a
Hana Xu <115299789+hana-linode@users.noreply.github.com>
2022-10-31 14:15:58 -0400

    Add pagination support to marketplace (#8588)

--- packages/manager/src/queries/stackscripts.ts
+++ packages/manager/src/queries/stackscripts.ts
@@ -1,16 +1,24 @@
 import { StackScript } from '@linode/api-v4/lib/stackscripts';
-import { APIError, ResourcePage } from '@linode/api-v4/lib/types';
+import { APIError } from '@linode/api-v4/lib/types';
 import { useQuery } from 'react-query';
 import { getOneClickApps } from 'src/features/StackScripts/stackScriptUtils';
+import { getAll } from 'src/utilities/getAll';
+import { queryPresets } from './base';
 
 export const queryKey = 'stackscripts';
 
-export const useStackScriptsOCA = (enabled: boolean) => {
-  return useQuery<ResourcePage<StackScript>, APIError[]>(
-    `${queryKey}-oca`,
-    getOneClickApps,
+export const useStackScriptsOCA = (enabled: boolean, params: any = {}) => {
+  return useQuery<StackScript[], APIError[]>(
+    [`${queryKey}-oca-all`, params],
+    () => getAllOCAsRequest(params),
     {
       enabled,
+      ...queryPresets.oneTimeFetch,
     }
   );
 };
+
+export const getAllOCAsRequest = (passedParams: any = {}) =>
+  getAll<StackScript>((params) =>
+    getOneClickApps({ ...params, ...passedParams })
+  )().then((data) => data.data);
