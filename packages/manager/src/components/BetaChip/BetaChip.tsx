9bd1b269caa5ae524888cf8f9cacfd28a1346781
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2022-11-14 08:56:52 -0800

    M3-5101: Storybook beta stories (#8603)
    * Add initial story for beta chip
    
    * Reorder and reword copy
    
    * Attempt to add variant to Chip
    
    * Add 'beta' variant with styling to Chip
    
    * Add Storybook documentation for beta banners
    
    * Link beta chip story to other beta references
    
    * Update existing beta chips to use variant
    
    * Fix link paths, add beta variant to argstable
    
    * Use BetaChip component instead of Chip variant
    
    * Update link to BetaChip story

---
+++ packages/manager/src/components/BetaChip/BetaChip.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Chip, { ChipProps } from 'src/components/core/Chip';
+import classNames from 'classnames';
+import { makeStyles, Theme } from 'src/components/core/styles';
+
+interface Props extends ChipProps {
+  className?: string;
+  color?: 'default' | 'primary';
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    fontFamily: theme.font.bold,
+    fontSize: '0.625rem',
+    height: 16,
+    letterSpacing: '.25px',
+    textTransform: 'uppercase',
+    marginLeft: theme.spacing(),
+  },
+}));
+
+const BetaChip: React.FC<Props> = (props) => {
+  const classes = useStyles();
+
+  const { className, color } = props;
+
+  return (
+    <Chip
+      {...props}
+      label="beta"
+      color={color}
+      className={classNames(className, {
+        [classes.root]: true,
+      })}
+    />
+  );
+};
+
+export default BetaChip;
