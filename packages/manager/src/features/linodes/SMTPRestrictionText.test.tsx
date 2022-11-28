07ccce2d2b9b5b49b26a0de2fbda127446612c22
mjac0bs <mjacobs@akamai.com>
2022-11-28 14:43:39 -0800

    Forgot to place a constant

--- packages/manager/src/features/linodes/SMTPRestrictionText.test.tsx
+++ packages/manager/src/features/linodes/SMTPRestrictionText.test.tsx
@@ -35,7 +35,9 @@ describe('accountCreatedAfterRestrictions', () => {
   it('only returns true when the account was created after the magic date', () => {
     expect(accountCreatedAfterRestrictions('2022-11-27 00:00:00Z')).toBe(false);
     expect(accountCreatedAfterRestrictions('2022-11-28 23:59:59Z')).toBe(false);
-    expect(accountCreatedAfterRestrictions('2022-11-29 00:00:00Z')).toBe(true);
+    expect(accountCreatedAfterRestrictions(MAGIC_IMPLEMENTATION_DATE)).toBe(
+      true
+    );
     expect(accountCreatedAfterRestrictions('2022-11-29 00:00:01Z')).toBe(true);
   });
 });
