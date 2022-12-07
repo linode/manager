7a4c8866789bf0759bfe2cfce91b0326d381297a
cpathipa <119517080+cpathipa@users.noreply.github.com>
2022-12-07 06:00:13 -0800

    Test coverage for adding sufix to deprecated images.

--- packages/manager/src/components/ImageSelect/ImageSelect.test.tsx
+++ packages/manager/src/components/ImageSelect/ImageSelect.test.tsx
@@ -1,8 +1,10 @@
-import { imagesToGroupedItems } from './ImageSelect';
+import { DateTime } from 'luxon';
 import { imageFactory } from 'src/factories';
 
+import { imagesToGroupedItems } from './ImageSelect';
+
 describe('imagesToGroupedItems', () => {
-  it('should filter deprecated images when eol date is beyond 6 months ', () => {
+  it('should filter deprecated images when end of life is past beyond 6 months ', () => {
     const images = [
       ...imageFactory.buildList(2, {
         label: 'Debian 9',
@@ -37,6 +39,42 @@ describe('imagesToGroupedItems', () => {
             created: '2022-10-20T14:05:30',
             label: 'Slackware 14.1',
             value: 'private/5',
+            className: 'fl-tux',
+          },
+        ],
+      },
+    ];
+    expect(imagesToGroupedItems(images)).toStrictEqual(expected);
+  });
+  it('should add suffix `deprecated` to images at end of life ', () => {
+    const images = [
+      ...imageFactory.buildList(2, {
+        label: 'Debian 9',
+        deprecated: true,
+        created: '2017-06-16T20:02:29',
+        eol: DateTime.now().toISODate(),
+      }),
+      ...imageFactory.buildList(2, {
+        label: 'Debian 10',
+        deprecated: false,
+        created: '2017-06-16T20:02:29',
+        eol: '1970-01-01T14:05:30',
+      }),
+    ];
+    const expected = [
+      {
+        label: 'My Images',
+        options: [
+          {
+            created: '2017-06-16T20:02:29',
+            label: 'Debian 9 (Deprecated)',
+            value: 'private/6',
+            className: 'fl-tux',
+          },
+          {
+            created: '2017-06-16T20:02:29',
+            label: 'Debian 9 (Deprecated)',
+            value: 'private/7',
             className: 'fl-tux',
           },
         ],
