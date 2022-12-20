e0fa7a76e0cf3491bb2709fd52af0a8e5166d46d
Joe D'Amore <jdamore@linode.com>
2022-12-20 15:14:38 -0500

    Break a test to make sure automated tests fail accordingly

--- packages/manager/cypress/e2e/account/billing-contact.spec.ts
+++ packages/manager/cypress/e2e/account/billing-contact.spec.ts
@@ -70,6 +70,7 @@ describe('Billing Contact', () => {
     cy.intercept('GET', '*/account', accountData).as('getAccount');
 
     cy.visitWithLogin('/account/billing', { mockRequests: false });
+    cy.findByText('This string does not exist.');
     checkAccountContactDisplay(accountData);
   });
   it('Edit Contact Info', () => {
