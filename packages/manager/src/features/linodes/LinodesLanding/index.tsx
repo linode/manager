159560e36b51a217cad848e499abcd96aff7d8be
Dajahi Wiley <dwiley@linode.com>
2023-01-05 13:41:53 -0500

    Remove Images from useReduxLoad and update LinodesLanding/index.tsx to use isLoading from useAllImagesQuery() accordingly

--- packages/manager/src/features/linodes/LinodesLanding/index.tsx
+++ packages/manager/src/features/linodes/LinodesLanding/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import CircleProgress from 'src/components/CircleProgress';
 import { useReduxLoad } from 'src/hooks/useReduxLoad';
+import { useAllImagesQuery } from 'src/queries/images';
 
 const LinodesLanding: React.FC<Props> = (props) => {
-  const { _loading } = useReduxLoad(['linodes', 'images']);
-  return _loading ? <CircleProgress /> : <_LinodesLanding {...props} />;
+  const { _loading } = useReduxLoad(['linodes']);
+  const { isLoading } = useAllImagesQuery();
+
+  return _loading || isLoading ? (
+    <CircleProgress />
+  ) : (
+    <_LinodesLanding {...props} />
+  );
 };
 
 import _LinodesLanding, { Props } from './LinodesLanding';
