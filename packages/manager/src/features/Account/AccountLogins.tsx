e84d66ee79c9fe4148faecf9e6dcd290140f43ba
John Callahan <114753608+jcallahan-akamai@users.noreply.github.com>
2023-01-09 13:24:10 -0500

    Feature: show login history on Account page (#8681)
    This consumes the /account/logins API endpoint and displays the data on a new "Logins" tab/page in the Account section.
    
    I used existing patterns for RQ, API paginated tables, etc.
    
    How to test 🧪
    - Visit localhost:3000/account/logins.
    - Check error states, loading state, responsiveness, etc.
    - Check sortable table columns
    - Try viewing it with a restricted user (you'll receive an "Unauthorized" error from the API)
    - Populate it with a variety of data by creating a restricted user on your account and logging in with it

---
+++ packages/manager/src/features/Account/AccountLogins.tsx
@@ -0,0 +1,146 @@
+import { AccountLogin } from '@linode/api-v4/lib/account/types';
+import Typography from 'src/components/core/Typography';
+import * as React from 'react';
+import Hidden from 'src/components/core/Hidden';
+import { makeStyles, Theme } from 'src/components/core/styles';
+import TableBody from 'src/components/core/TableBody';
+import TableHead from 'src/components/core/TableHead';
+import PaginationFooter from 'src/components/PaginationFooter';
+import Table from 'src/components/Table/Table';
+import TableCell from 'src/components/TableCell';
+import TableRow from 'src/components/TableRow/TableRow';
+import TableRowEmptyState from 'src/components/TableRowEmptyState';
+import TableRowError from 'src/components/TableRowError';
+import { TableRowLoading } from 'src/components/TableRowLoading/TableRowLoading';
+import TableSortCell from 'src/components/TableSortCell/TableSortCell';
+import { useOrder } from 'src/hooks/useOrder';
+import usePagination from 'src/hooks/usePagination';
+import { useAccountLoginsQuery } from 'src/queries/accountLogins';
+import AccountLoginsTableRow from './AccountLoginsTableRow';
+
+const preferenceKey = 'account-logins';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  cell: {
+    width: '12%',
+  },
+  copy: {
+    lineHeight: '20px',
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+const AccountLogins = () => {
+  const classes = useStyles();
+  const pagination = usePagination(1, preferenceKey);
+
+  const { order, orderBy, handleOrderChange } = useOrder(
+    {
+      orderBy: 'datetime',
+      order: 'desc',
+    },
+    `${preferenceKey}-order}`
+  );
+
+  const filter = {
+    ['+order_by']: orderBy,
+    ['+order']: order,
+  };
+
+  const { data, isLoading, error } = useAccountLoginsQuery(
+    {
+      page: pagination.page,
+      page_size: pagination.pageSize,
+    },
+    filter
+  );
+
+  const renderTableContent = () => {
+    if (isLoading) {
+      return (
+        <TableRowLoading
+          rows={1}
+          columns={4}
+          responsive={{
+            1: { xsDown: true },
+            3: { smDown: true },
+          }}
+        />
+      );
+    }
+    if (error) {
+      return <TableRowError colSpan={5} message={error[0].reason} />;
+    }
+    if (data?.results == 0) {
+      return <TableRowEmptyState message="No account logins" colSpan={6} />;
+    }
+    if (data) {
+      return data.data.map((item: AccountLogin) => (
+        <AccountLoginsTableRow key={item.id} {...item} />
+      ));
+    }
+
+    return null;
+  };
+
+  return (
+    <>
+      <Typography variant="body1" className={classes.copy}>
+        Logins across all users on your account over the last 90 days.
+      </Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableSortCell
+              active={orderBy === 'datetime'}
+              direction={order}
+              label="datetime"
+              handleClick={handleOrderChange}
+              className={classes.cell}
+            >
+              Date
+            </TableSortCell>
+            <TableSortCell
+              active={orderBy === 'username'}
+              direction={order}
+              label="username"
+              handleClick={handleOrderChange}
+              className={classes.cell}
+            >
+              Username
+            </TableSortCell>
+            <Hidden xsDown>
+              <TableSortCell
+                active={orderBy === 'ip'}
+                direction={order}
+                label="ip"
+                handleClick={handleOrderChange}
+                className={classes.cell}
+              >
+                IP
+              </TableSortCell>
+            </Hidden>
+            <Hidden smDown>
+              <TableCell className={classes.cell}>Permission Level</TableCell>
+            </Hidden>
+          </TableRow>
+        </TableHead>
+        <TableBody>{renderTableContent()}</TableBody>
+      </Table>
+      <PaginationFooter
+        count={data?.results || 0}
+        handlePageChange={pagination.handlePageChange}
+        handleSizeChange={pagination.handlePageSizeChange}
+        page={pagination.page}
+        pageSize={pagination.pageSize}
+        eventCategory="Account Logins Table"
+      />
+    </>
+  );
+};
+
+export default AccountLogins;
