4670c4dab48db40fa04028f6ffee06c5fc04a5f0
Banks Nussman <banks@nussman.us>
2023-01-18 12:44:06 -0500

    clean up backup dialog

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseBackups/BackupDialog.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseBackups/BackupDialog.tsx
@@ -1,32 +1,21 @@
-import { Database } from '@linode/api-v4/lib/databases';
-import { useSnackbar } from 'notistack';
 import * as React from 'react';
-import { compose } from 'recompose';
-import { useHistory } from 'react-router-dom';
+import TextField from 'src/components/TextField';
 import ActionsPanel from 'src/components/ActionsPanel';
 import Button from 'src/components/Button';
 import ConfirmationDialog from 'src/components/ConfirmationDialog';
-import TypeToConfirm from 'src/components/TypeToConfirm';
-import { DialogProps } from 'src/components/Dialog';
-import Notice from 'src/components/Notice';
-import withPreferences, {
-  Props as PreferencesProps,
-} from 'src/containers/preferences.container';
+import { useSnackbar } from 'notistack';
 import { useBackupMutation } from 'src/queries/databases';
 import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
+import { Database } from '@linode/api-v4/lib/databases';
 
-interface Props extends Omit<DialogProps, 'title'> {
+interface Props {
   open: boolean;
   onClose: () => void;
   database: Database;
 }
 
-export type CombinedProps = Props & PreferencesProps;
-
-export const BackupDialog: React.FC<CombinedProps> = (props) => {
-  const { database, preferences, onClose, open, ...rest } = props;
-
-  const history = useHistory();
+export const BackupDialog = (props: Props) => {
+  const { database, onClose, open } = props;
   const { enqueueSnackbar } = useSnackbar();
 
   const [backupLabel, setBackupLabel] = React.useState('');
@@ -36,9 +25,9 @@ export const BackupDialog: React.FC<CombinedProps> = (props) => {
     database.id,
     backupLabel
   );
-  const handleBackupDatabase = (backupLabel: string) => {
+
+  const handleBackupDatabase = () => {
     backup().then(() => {
-      history.push('summary');
       enqueueSnackbar(
         `${database.label} is being backed up with label ${backupLabel}`,
         {
@@ -49,6 +38,12 @@ export const BackupDialog: React.FC<CombinedProps> = (props) => {
     });
   };
 
+  React.useEffect(() => {
+    if (open) {
+      setBackupLabel('');
+    }
+  }, [open]);
+
   const actions = (
     <ActionsPanel style={{ padding: 0 }}>
       <Button buttonType="secondary" onClick={onClose}>
@@ -56,8 +51,8 @@ export const BackupDialog: React.FC<CombinedProps> = (props) => {
       </Button>
       <Button
         buttonType="primary"
-        onClick={() => handleBackupDatabase(backupLabel)}
-        disabled={preferences?.type_to_confirm !== false && backupLabel === ''}
+        onClick={handleBackupDatabase}
+        disabled={!backupLabel}
         loading={isLoading}
       >
         Backup Database
@@ -65,47 +60,27 @@ export const BackupDialog: React.FC<CombinedProps> = (props) => {
     </ActionsPanel>
   );
 
-  React.useEffect(() => {
-    if (open) {
-      setBackupLabel('');
-    }
-  }, [open]);
-
   return (
     <ConfirmationDialog
-      {...rest}
       title={`Backup ${database.label}`}
       open={open}
       onClose={onClose}
       actions={actions}
-    >
-      {error ? (
-        <Notice
-          error
-          text={
-            getAPIErrorOrDefault(error, 'Unable to create this backup.')[0]
+      error={
+        error
+          ? getAPIErrorOrDefault(error, 'Unable to create this backup.')[0]
               .reason
-          }
-        />
-      ) : null}
-      <TypeToConfirm
-        backupLabel={
-          <span>
-            To backup <strong>{database.label}</strong>, give your backup a name
-            in the field below.
-          </span>
-        }
-        onChange={(input) => setBackupLabel(input)}
+          : undefined
+      }
+    >
+      <TextField
+        onChange={(e) => setBackupLabel(e.target.value)}
         value={backupLabel}
         label="Backup Label"
-        visible={true}
         placeholder={'My backup label'}
-        hideInstructions={true}
       />
     </ConfirmationDialog>
   );
 };
 
-const enhanced = compose<CombinedProps, Props>(withPreferences());
-
-export default enhanced(BackupDialog);
+export default BackupDialog;
