ba3a55cbbe4ee32013dadaa60f673f02b8088fd4
Joe D'Amore <jdamore@linode.com>
2023-01-24 16:59:45 -0500

    Add intercept to mock entity transfer GET requests

--- packages/manager/cypress/support/intercepts/account.ts
+++ packages/manager/cypress/support/intercepts/account.ts
@@ -2,7 +2,9 @@
  * @file Cypress intercepts and mocks for Cloud Manager account requests.
  */
 
-import type { AccountSettings } from '@linode/api-v4/types';
+import type { AccountSettings, EntityTransfer } from '@linode/api-v4/types';
+import { paginateResponse } from 'support/util/paginate';
+import { getFilters } from 'support/util/request';
 
 /**
  * Intercepts GET request to fetch account user information.
@@ -22,6 +24,62 @@ export const interceptGetUser = (username: string): Cypress.Chainable<null> => {
  */
 export const interceptInitiateEntityTransfer = (): Cypress.Chainable<null> => {
   return cy.intercept('POST', '*/account/entity-transfers');
+};
+
+/**
+ * Intercepts GET request to fetch entity transfers and mocks the response.
+ *
+ * This intercept will catch any request to the GET entity transfer endpoint,
+ * but will respond according to the contents of the `x-filter` header.
+ *
+ * If the filter indicates a request for pending transfers, the response will
+ * contain the contents of the `pending` array. Likewise, if the filter
+ * indicates a request for either received or sent transfers, the response
+ * will reflect the given `received` or `sent` array, respectively. If the
+ * request contains an unexpected filter or no filter, the response will not
+ * be mocked at all.
+ *
+ * @param pending - Mocked entity transfers with which to respond for pending entity requests.
+ * @param received - Mocked entity transfers with which to respond for received entity requests.
+ * @param sent - Mocked entity transfers with which to respond for sent entity requests.
+ */
+export const mockGetEntityTransfers = (
+  pending: EntityTransfer[],
+  received: EntityTransfer[],
+  sent: EntityTransfer[]
+) => {
+  return cy.intercept('GET', '*/account/entity-transfers*', (req) => {
+    const filters = getFilters(req);
+
+    if (filters?.['status'] === 'pending') {
+      req.reply(paginateResponse(pending));
+      return;
+    }
+
+    if (filters?.['+and'] && Array.isArray(filters['+and'])) {
+      const compositeFilters: Record<string, unknown>[] = filters['+and'];
+
+      // Confirm that `is_sender` is set, and, if so, that it has the expected value.
+      const hasTrueSenderValue = compositeFilters.some(
+        (compositeFilter) => compositeFilter['is_sender'] === true
+      );
+      const hasFalseSenderValue = compositeFilters.some(
+        (compositeFilter) => compositeFilter['is_sender'] === false
+      );
+
+      if (hasTrueSenderValue) {
+        req.reply(paginateResponse(sent));
+        return;
+      }
+
+      if (hasFalseSenderValue) {
+        req.reply(paginateResponse(received));
+        return;
+      }
+    }
+
+    req.continue();
+  });
 };
 
 /**
