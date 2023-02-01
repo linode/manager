d965df996b1b4538b9d3fc12f0ca77f4f002319b
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2023-02-01 13:39:05 -0500

    M3-6065: API Tokens Refactors and Fixes (#8742)
    - Refactors all of /profile/tokens to use functional React components and React Query
    - Uses Formik for token forms
    - These changes subsequently fix unexpected cache invalidation behavior when revoking API tokens
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

---
+++ packages/manager/src/features/Profile/APITokens/CreateAPITokenDrawer.test.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { CreateAPITokenDrawer } from './CreateAPITokenDrawer';
+import { act, waitFor } from '@testing-library/react';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+import { rest, server } from 'src/mocks/testServer';
+import { appTokenFactory } from 'src/factories';
+import userEvent from '@testing-library/user-event';
+
+const props = {
+  open: true,
+  onClose: jest.fn(),
+  showSecret: jest.fn(),
+};
+
+describe('Create API Token Drawer', () => {
+  it('checks API Token Drawer rendering', () => {
+    const { getByText, getByTestId } = renderWithTheme(
+      <CreateAPITokenDrawer {...props} />
+    );
+    const drawerTitle = getByText('Add Personal Access Token');
+    expect(drawerTitle).toBeVisible();
+
+    const labelTitle = getByText(/Label/);
+    const labelField = getByTestId('textfield-input');
+    expect(labelTitle).toBeVisible();
+    expect(labelField).toBeEnabled();
+
+    const expiry = getByText(/Expiry/);
+    expect(expiry).toBeVisible();
+
+    const submitBtn = getByTestId('create-button');
+    expect(submitBtn).toBeVisible();
+    expect(submitBtn).toBeEnabled();
+
+    const cancelBtn = getByText(/Cancel/);
+    expect(cancelBtn).toBeEnabled();
+    expect(cancelBtn).toBeVisible();
+  });
+  it('Should see secret modal with secret when you type a label and submit the form successfully', async () => {
+    server.use(
+      rest.post('*/profile/tokens', (req, res, ctx) => {
+        return res(ctx.json(appTokenFactory.build({ token: 'secret-value' })));
+      })
+    );
+
+    const { getByTestId, getByText } = renderWithTheme(
+      <CreateAPITokenDrawer {...props} />
+    );
+
+    await act(async () => {
+      const labelField = getByTestId('textfield-input');
+
+      userEvent.type(labelField, 'my-test-token');
+
+      const submit = getByText('Create Token');
+
+      userEvent.click(submit);
+
+      await waitFor(() =>
+        expect(props.showSecret).toBeCalledWith('secret-value')
+      );
+    });
+  });
+  it('Should default to read/write for all scopes', () => {
+    const { getByLabelText } = renderWithTheme(
+      <CreateAPITokenDrawer {...props} />
+    );
+    const selectAllReadWriteRadioButton = getByLabelText(
+      'Select read/write for all'
+    );
+    expect(selectAllReadWriteRadioButton).toBeChecked();
+  });
+  it('Should default to 6 months for expiration', () => {
+    const { getByText } = renderWithTheme(<CreateAPITokenDrawer {...props} />);
+    getByText('In 6 months');
+  });
+  it('Should close when Cancel is pressed', () => {
+    const { getByText } = renderWithTheme(<CreateAPITokenDrawer {...props} />);
+    const cancelButton = getByText(/Cancel/);
+    userEvent.click(cancelButton);
+    expect(props.onClose).toBeCalled();
+  });
+});
