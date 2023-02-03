9d31222d447f0bf29c30d7ea64bed398dc2e3431
CodyFinn-Akamai <cfinn@akamai.com>
2023-02-03 07:12:10 -0500

    fixed tag and vlan parsing

--- packages/manager/src/utilities/generate-cli.ts
+++ packages/manager/src/utilities/generate-cli.ts
@@ -4,13 +4,41 @@ const convertObjectToCLIArg = (data: {} | null) => {
   return `'${JSON.stringify(data).replace(':', ': ')}'`;
 };
 
+const parseObject = (key: string, value: {}) => {
+  const result = Object.entries(value)
+    .map(([_key, _value]) => {
+      return `--${key}.${_key} ${JSON.stringify(_value)}`;
+    })
+    .join(' ');
+  return result.padStart(result.length + 2);
+};
+
+const parseArray = (key: string, value: any[]) => {
+  const results: string[] = [];
+  if (key === 'interfaces') {
+    results.push(
+      value
+        .map((item) => {
+          return parseObject('interfaces', item);
+        })
+        .join('\\\n')
+    );
+  } else {
+    value.forEach((item) => {
+      results.push(`  --${key} ${JSON.stringify(item)}`);
+    });
+  }
+  return results.join('\\\n');
+};
+
 const dataEntriesReduce = (acc: string[], [key, value]: JSONFieldToArray) => {
   if (value === undefined || value === null) {
     return acc;
   } else if (Array.isArray(value)) {
-    value.forEach((item) => {
-      acc.push(`  --${key} ${item}`);
-    });
+    if (value.length === 0) {
+      return acc;
+    }
+    acc.push(parseArray(key, value));
     return acc;
   } else if (typeof value === 'object') {
     const valueAsString = convertObjectToCLIArg(value);
