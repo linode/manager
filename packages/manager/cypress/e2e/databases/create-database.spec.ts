848800ae87c8ec39711448fffbadbe74218f151e
Joe D'Amore <jdamore@linode.com>
2023-02-27 18:38:03 -0500

    Remove unused type

--- packages/manager/cypress/e2e/databases/create-database.spec.ts
+++ packages/manager/cypress/e2e/databases/create-database.spec.ts
@@ -1,8 +1,4 @@
-import {
-  ClusterSize,
-  Engine,
-  CreateDatabasePayload,
-} from '@linode/api-v4/types';
+import { ClusterSize, Engine } from '@linode/api-v4/types';
 import { databaseInstanceFactory } from 'src/factories/databases';
 import { eventFactory } from 'src/factories/events';
 import { randomLabel } from 'support/util/random';
