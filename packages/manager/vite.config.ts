2cc944a2d6af78c048f919aa3a9d3d36ee792d24
Banks Nussman <banks@nussman.us>
2023-03-01 22:15:04 -0500

    build manager into the `build` folder to match current Webpack setup

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [react(), svgr({ exportAsDefault: true })],
   envPrefix: 'REACT_APP_',
   server: {
