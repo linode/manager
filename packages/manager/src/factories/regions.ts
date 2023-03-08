326820c24058632f3fef1285122eba8764ceb8cd
Banks Nussman <banks@nussman.us>
2023-03-08 10:22:50 -0500

    do not used cached data in regions factory

--- packages/manager/src/factories/regions.ts
+++ packages/manager/src/factories/regions.ts
@@ -1,17 +1,14 @@
 import * as Factory from 'factory.ts';
 import { DNSResolvers, Region } from '@linode/api-v4/lib/regions/types';
-import regionData from 'src/cachedData/regions.json';
 
 export const resolverFactory = Factory.Sync.makeFactory<DNSResolvers>({
   ipv4: '1.1.1.1',
   ipv6: '2600:3c03::',
 });
 
-const regions = regionData.data;
-
 export const regionFactory = Factory.Sync.makeFactory<Region>({
-  id: Factory.each((id) => regions[id % regions.length].id),
-  label: Factory.each((id) => regions[id % regions.length].label),
+  id: Factory.each((id) => `us-${id}`),
+  label: Factory.each((id) => `${id}, NJ`),
   status: 'ok',
   country: 'US',
   capabilities: ['Block Storage'],
