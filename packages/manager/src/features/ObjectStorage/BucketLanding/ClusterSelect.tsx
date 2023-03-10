e3dc15dd12f7ad191a06b14cd387c5455e8dbf87
Banks Nussman <banks@nussman.us>
2023-03-10 15:51:44 -0500

    fix object storage region select

--- packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.tsx
@@ -1,5 +1,7 @@
+import { Region } from '@linode/api-v4/lib/regions';
 import * as React from 'react';
 import RegionSelect from 'src/components/EnhancedSelect/variants/RegionSelect';
+import { useObjectStorageClusters } from 'src/queries/objectStorage';
 import { useRegionsQuery } from 'src/queries/regions';
 
 interface Props {
@@ -13,13 +15,19 @@ interface Props {
 export const ClusterSelect: React.FC<Props> = (props) => {
   const { selectedCluster, error, onChange, onBlur, disabled } = props;
 
-  const { data, error: regionsError } = useRegionsQuery();
+  const { data: clusters, error: clustersError } = useObjectStorageClusters();
+  const { data: regions } = useRegionsQuery();
 
-  const regions =
-    data?.filter((r) => r.capabilities.includes('Object Storage')) ?? [];
+  const regionOptions = clusters?.reduce<Region[]>((acc, cluster) => {
+    const region = regions?.find((r) => r.id === cluster.region);
+    if (region) {
+      acc.push({ ...region, id: cluster.id });
+    }
+    return acc;
+  }, []);
 
   // Error could be: 1. General Clusters error, 2. Field error, 3. Nothing
-  const errorText = regionsError
+  const errorText = clustersError
     ? 'Error loading regions'
     : error
     ? error
@@ -30,7 +38,7 @@ export const ClusterSelect: React.FC<Props> = (props) => {
       data-qa-select-cluster
       name="cluster"
       label="Region"
-      regions={regions}
+      regions={regionOptions ?? []}
       selectedID={selectedCluster}
       placeholder="Select a Region"
       handleSelection={(id) => onChange(id)}
