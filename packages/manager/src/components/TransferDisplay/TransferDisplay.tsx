8f0dfb65d2e41c8a1bf4294c088d37ab39d56629
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-03-14 13:13:32 -0400

    Stick with convention for now

--- packages/manager/src/components/TransferDisplay/TransferDisplay.tsx
+++ packages/manager/src/components/TransferDisplay/TransferDisplay.tsx
@@ -129,67 +129,66 @@ interface DialogProps {
   onClose: () => void;
 }
 
-export const TransferDialog = React.memo(
-  ({ isOpen, onClose, poolUsagePct, quota, used }: DialogProps) => {
-    const classes = useStyles();
-    const daysRemainingInMonth = getDaysRemaining();
+export const TransferDialog = React.memo((props: DialogProps) => {
+  const { isOpen, onClose, poolUsagePct, quota, used } = props;
+  const classes = useStyles();
+  const daysRemainingInMonth = getDaysRemaining();
 
-    return (
-      <Dialog
-        open={isOpen}
-        classes={{ paper: classes.paper }}
-        onClose={onClose}
-        title="Monthly Network Transfer Pool"
-        maxWidth="sm"
+  return (
+    <Dialog
+      open={isOpen}
+      classes={{ paper: classes.paper }}
+      onClose={onClose}
+      title="Monthly Network Transfer Pool"
+      maxWidth="sm"
+    >
+      <Grid
+        container
+        justifyContent="space-between"
+        style={{ marginBottom: 0 }}
       >
-        <Grid
-          container
-          justifyContent="space-between"
-          style={{ marginBottom: 0 }}
-        >
-          <Grid item style={{ marginRight: 10 }}>
-            <Typography>{used} GB Used</Typography>
-          </Grid>
-          <Grid item>
-            <Typography>
-              {quota >= used ? (
-                <span>{quota - used} GB Available</span>
-              ) : (
-                <span>
-                  {(quota - used).toString().replace(/\-/, '')} GB Over Quota
-                </span>
-              )}
-            </Typography>
-          </Grid>
+        <Grid item style={{ marginRight: 10 }}>
+          <Typography>{used} GB Used</Typography>
         </Grid>
-        <BarPercent
-          max={100}
-          value={Math.ceil(poolUsagePct)}
-          className={classes.poolUsageProgress}
-          rounded
-        />
-
-        <Typography className={classes.proratedNotice}>
-          <strong>
-            Your account&rsquo;s monthly network transfer allotment will reset
-            in {daysRemainingInMonth} days.
-          </strong>
-        </Typography>
-        <Typography className={classes.proratedNotice}>
-          Your account&rsquo;s network transfer pool adds up all the included
-          transfer associated with the active Linode services on your account,
-          and is prorated based on service creation and deletion dates.
-        </Typography>
-        <div className={classes.link}>
+        <Grid item>
           <Typography>
-            Optimize your network usage and avoid billing surprises related to
-            network transfer.
+            {quota >= used ? (
+              <span>{quota - used} GB Available</span>
+            ) : (
+              <span>
+                {(quota - used).toString().replace(/\-/, '')} GB Over Quota
+              </span>
+            )}
           </Typography>
-          <Link to="https://www.linode.com/docs/guides/network-transfer-quota/">
-            <OpenInNew />
-          </Link>
-        </div>
-      </Dialog>
-    );
-  }
-);
+        </Grid>
+      </Grid>
+      <BarPercent
+        max={100}
+        value={Math.ceil(poolUsagePct)}
+        className={classes.poolUsageProgress}
+        rounded
+      />
+
+      <Typography className={classes.proratedNotice}>
+        <strong>
+          Your account&rsquo;s monthly network transfer allotment will reset in{' '}
+          {daysRemainingInMonth} days.
+        </strong>
+      </Typography>
+      <Typography className={classes.proratedNotice}>
+        Your account&rsquo;s network transfer pool adds up all the included
+        transfer associated with the active Linode services on your account, and
+        is prorated based on service creation and deletion dates.
+      </Typography>
+      <div className={classes.link}>
+        <Typography>
+          Optimize your network usage and avoid billing surprises related to
+          network transfer.
+        </Typography>
+        <Link to="https://www.linode.com/docs/guides/network-transfer-quota/">
+          <OpenInNew />
+        </Link>
+      </div>
+    </Dialog>
+  );
+});
