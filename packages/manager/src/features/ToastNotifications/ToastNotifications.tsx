796aae6fb688a48e4b1618b16f9dd47a7ffeaf9f
mjac0bs <mjacobs@akamai.com>
2023-03-15 09:50:49 -0700

    Refactor ToastNotifications.tsx to use non-curried function

--- packages/manager/src/features/ToastNotifications/ToastNotifications.tsx
+++ packages/manager/src/features/ToastNotifications/ToastNotifications.tsx
@@ -1,6 +1,5 @@
 import { Event, EventStatus } from '@linode/api-v4/lib/account/types';
 import { withSnackbar, WithSnackbarProps } from 'notistack';
-import { curry } from 'ramda';
 import * as React from 'react';
 import 'rxjs/add/operator/bufferTime';
 import 'rxjs/add/operator/filter';
@@ -11,55 +10,55 @@ import Link from 'src/components/Link';
 import { events$ } from 'src/events';
 import { sendEvent } from 'src/utilities/ga';
 
-/**
- * Boilerplate for sending a success toast
- * when an event succeeds and a failure toast
- * when it fails. Intended to be partially applied
- * with the snackbar method and event status.
- *
- * Note: due to the nature of currying, if you want to
- * omit the success or failure message, you must manually pass `undefined`
- * as the final argument, otherwise you get a function that's expecting
- * a failureMessage instead of the toasts you want.
- */
-export const toastSuccessAndFailure = curry(
-  (
-    enqueueSnackbar: WithSnackbarProps['enqueueSnackbar'],
-    eventStatus: EventStatus,
-    persistSuccessMessage: boolean | undefined,
-    persistFailureMessage: boolean | undefined,
-    successMessage: string | undefined,
-    failureMessage: string | undefined,
-    includesLink: boolean | undefined,
-    link: JSX.Element | undefined
-  ) => {
-    let linkedMessage;
-    if (
-      ['finished', 'notification'].includes(eventStatus) &&
-      Boolean(successMessage)
-    ) {
-      return enqueueSnackbar(successMessage, {
-        variant: 'success',
-        persist: persistSuccessMessage,
-      });
-    } else if (['failed'].includes(eventStatus) && Boolean(failureMessage)) {
-      if (includesLink) {
-        linkedMessage = (
-          <>
-            {failureMessage}&nbsp;
-            {link}
-          </>
-        );
-      }
-      return enqueueSnackbar(linkedMessage ?? failureMessage, {
-        variant: 'error',
-        persist: persistFailureMessage,
-      });
-    } else {
-      return;
+interface ToastOptions {
+  enqueueSnackbar: WithSnackbarProps['enqueueSnackbar'];
+  eventStatus: EventStatus;
+  persistSuccessMessage?: boolean;
+  persistFailureMessage?: boolean;
+  successMessage?: string;
+  failureMessage?: string;
+  includesLink?: boolean;
+  link?: JSX.Element;
+}
+
+const toastSuccessAndFailure = (options: ToastOptions) => {
+  const {
+    enqueueSnackbar,
+    eventStatus,
+    persistSuccessMessage,
+    persistFailureMessage,
+    successMessage,
+    failureMessage,
+    includesLink,
+    link,
+  } = options;
+  let linkedMessage;
+
+  if (
+    ['finished', 'notification'].includes(eventStatus) &&
+    Boolean(successMessage)
+  ) {
+    return enqueueSnackbar(successMessage, {
+      variant: 'success',
+      persist: persistSuccessMessage,
+    });
+  } else if (['failed'].includes(eventStatus) && Boolean(failureMessage)) {
+    if (includesLink) {
+      linkedMessage = (
+        <>
+          {failureMessage}&nbsp;
+          {link}
+        </>
+      );
     }
+    return enqueueSnackbar(linkedMessage ?? failureMessage, {
+      variant: 'error',
+      persist: persistFailureMessage,
+    });
+  } else {
+    return;
   }
-);
+};
 
 export const getLabel = (event: Event) => event.entity?.label ?? '';
 export const getSecondaryLabel = (event: Event) =>
@@ -73,62 +72,53 @@ class ToastNotifications extends React.PureComponent<WithSnackbarProps, {}> {
       .filter((e) => !e._initial)
       .map((event) => {
         const { enqueueSnackbar } = this.props;
-        const _toastWithPersist = toastSuccessAndFailure(
-          enqueueSnackbar,
-          event.status
-        );
-        const _toast = toastSuccessAndFailure(
-          enqueueSnackbar,
-          event.status,
-          false,
-          false
-        );
         const label = getLabel(event);
         const secondaryLabel = getSecondaryLabel(event);
         switch (event.action) {
           case 'volume_attach':
-            return _toast(
-              `Volume ${label} successfully attached.`,
-              `Error attaching Volume ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Volume ${label} successfully attached.`,
+              failureMessage: `Error attaching Volume ${label}.`,
+            });
           case 'volume_detach':
-            return _toast(
-              `Volume ${label} successfully detached.`,
-              `Error detaching Volume ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Volume ${label} successfully detached.`,
+              failureMessage: `Error detaching Volume ${label}.`,
+            });
           case 'volume_create':
-            return _toast(
-              `Volume ${label} successfully created.`,
-              `Error creating Volume ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Volume ${label} successfully created.`,
+              failureMessage: `Error creating Volume ${label}.`,
+            });
           case 'volume_delete':
-            return _toast(
-              `Volume successfully deleted.`,
-              `Error deleting Volume.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Volume successfully deleted.`,
+              failureMessage: `Error deleting Volume.`,
+            });
           case 'disk_imagize':
-            return _toast(
-              `Image ${secondaryLabel} created successfully.`,
-              `Error creating Image ${secondaryLabel}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Image ${secondaryLabel} created successfully.`,
+              failureMessage: `Error creating Image ${secondaryLabel}.`,
+            });
           case 'disk_resize':
-            return _toastWithPersist(
-              false,
-              true,
-              `Disk ${secondaryLabel} resized successfully.`,
-              `Disk resize failed.`,
-              true, // includesLink
-              formatLink(
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              persistFailureMessage: true,
+              successMessage: `Disk ${secondaryLabel} resized successfully.`,
+              failureMessage: `Disk resize failed.`,
+              includesLink: true,
+              link: formatLink(
                 'Learn more about resizing restrictions.',
                 'https://www.linode.com/docs/products/compute/compute-instances/guides/disks-and-storage/',
                 sendEvent({
@@ -136,129 +126,120 @@ class ToastNotifications extends React.PureComponent<WithSnackbarProps, {}> {
                   action: `Click:link`,
                   label: 'Disk resize failed toast',
                 })
-              )
-            );
+              ),
+            });
           case 'image_upload':
             const isDeletion = event.message === 'Upload cancelled.';
-            return _toastWithPersist(
-              false,
-              true,
-              `Image ${label} is now available.`,
-              isDeletion
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              persistFailureMessage: true,
+              successMessage: `Image ${label} is now available.`,
+              failureMessage: isDeletion
                 ? undefined
                 : `There was a problem processing image ${label}: ${event.message?.replace(
                     'cancelled',
                     'canceled'
                   )}`,
-              undefined,
-              undefined
-            );
+            });
           case 'image_delete':
-            return _toast(
-              `Image ${label} deleted successfully.`,
-              `Error deleting Image ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Image ${label} deleted successfully.`,
+              failureMessage: `Error deleting Image ${label}.`,
+            });
           case 'disk_delete':
-            return _toast(
-              `Disk ${secondaryLabel} deleted successfully.`,
-              `Unable to delete disk ${secondaryLabel} ${
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Disk ${secondaryLabel} deleted successfully.`,
+              failureMessage: `Unable to delete disk ${secondaryLabel} ${
                 label ? ` on ${label}` : ''
               }. Is it attached to a configuration profile that is in use?`,
-              undefined,
-              undefined
-            );
+            });
           case 'linode_snapshot':
-            return _toast(
-              undefined,
-              `There was an error creating a snapshot on Linode ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `There was an error creating a snapshot on Linode ${label}.`,
+            });
           /**
            * These create/delete failures are hypothetical.
            * We don't know if it's possible for these to fail,
            * but are including handling to be safe.
            */
           case 'linode_config_delete':
-            return _toast(
-              undefined,
-              `Error deleting config ${secondaryLabel}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `Error deleting config ${secondaryLabel}.`,
+            });
           case 'linode_config_create':
-            return _toast(
-              undefined,
-              `Error creating config ${secondaryLabel}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `Error creating config ${secondaryLabel}.`,
+            });
           case 'linode_clone':
-            return _toast(
-              `Linode ${label} has been cloned successfully to ${secondaryLabel}.`,
-              `Error cloning Linode ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Linode ${label} has been cloned successfully to ${secondaryLabel}.`,
+              failureMessage: `Error cloning Linode ${label}.`,
+            });
           case 'linode_migrate_datacenter':
           case 'linode_migrate':
-            return _toast(
-              `Linode ${label} has been migrated successfully.`,
-              `Error migrating Linode ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Linode ${label} has been migrated successfully.`,
+              failureMessage: `Error migrating Linode ${label}.`,
+            });
           case 'linode_resize':
-            return _toast(
-              `Linode ${label} has been resized successfully.`,
-              `Error resizing Linode ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Linode ${label} has been resized successfully.`,
+              failureMessage: `Error resizing Linode ${label}.`,
+            });
           case 'firewall_enable':
-            return _toast(
-              undefined,
-              `Error enabling Firewall ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `Error enabling Firewall ${label}.`,
+            });
           case 'firewall_disable':
-            return _toast(
-              undefined,
-              `Error disabling Firewall ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `Error disabling Firewall ${label}.`,
+            });
           case 'firewall_delete':
-            return _toast(
-              undefined,
-              `Error deleting Firewall ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `Error deleting Firewall ${label}.`,
+            });
           case 'firewall_device_add':
-            return _toast(
-              undefined,
-              `Error adding ${secondaryLabel} to Firewall ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `Error adding ${secondaryLabel} to Firewall ${label}.`,
+            });
           case 'firewall_device_remove':
-            return _toast(
-              undefined,
-              `Error removing ${secondaryLabel} from Firewall ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              failureMessage: `Error removing ${secondaryLabel} from Firewall ${label}.`,
+            });
           case 'longviewclient_create':
-            return _toast(
-              `Longview Client ${label} successfully created.`,
-              `Error creating Longview Client ${label}.`,
-              undefined,
-              undefined
-            );
+            return toastSuccessAndFailure({
+              enqueueSnackbar,
+              eventStatus: event.status,
+              successMessage: `Longview Client ${label} successfully created.`,
+              failureMessage: `Error creating Longview Client ${label}.`,
+            });
           default:
             // eslint-disable-next-line array-callback-return
             return;
