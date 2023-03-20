40085d40c9b1c57e5bb96540976776c3d58b0001
Cassie Liu <cliu@akamai.com>
2023-03-20 16:39:54 -0400

    M3-5945: Parameterize DBaaS tests for multiple engines

--- packages/manager/cypress/e2e/databases/create-database.spec.ts
+++ packages/manager/cypress/e2e/databases/create-database.spec.ts
@@ -1,65 +1,12 @@
-import { ClusterSize, Engine } from '@linode/api-v4/types';
 import { databaseInstanceFactory } from 'src/factories/databases';
 import { eventFactory } from 'src/factories/events';
-import { randomLabel } from 'support/util/random';
 import { sequentialStub } from 'support/stubs/sequential-stub';
 import { paginateResponse } from 'support/util/paginate';
 import { apiMatcher } from 'support/util/intercepts';
-
-interface databaseClusterConfiguration {
-  label: string;
-  linodeType: string;
-  clusterSize: ClusterSize;
-  dbType: Engine;
-  region: string;
-  regionTypeahead: string;
-  engine: string;
-  version: string;
-}
-
-// Array of database cluster configurations for which to test creation.
-const databaseConfigurations: databaseClusterConfiguration[] = [
-  {
-    label: randomLabel(),
-    linodeType: 'g6-nanode-1',
-    clusterSize: 1,
-    dbType: 'mysql',
-    region: 'us-east',
-    regionTypeahead: 'Newark',
-    engine: 'MySQL',
-    version: '8',
-  },
-  {
-    label: randomLabel(),
-    linodeType: 'g6-dedicated-16',
-    clusterSize: 3,
-    dbType: 'mysql',
-    region: 'us-southeast',
-    regionTypeahead: 'Atlanta',
-    engine: 'MySQL',
-    version: '5',
-  },
-  // {
-  //   label: randomLabel(),
-  //   linodeType: 'g6-dedicated-16',
-  //   clusterSize: 1,
-  //   dbType: 'mongodb',
-  //   regionTypeahead: 'Atlanta',
-  //   region: 'us-southeast',
-  //   engine: 'MongoDB',
-  //   version: '4',
-  // },
-  {
-    label: randomLabel(),
-    linodeType: 'g6-nanode-1',
-    clusterSize: 3,
-    dbType: 'postgresql',
-    region: 'us-east',
-    regionTypeahead: 'Newark',
-    engine: 'PostgreSQL',
-    version: '13',
-  },
-];
+import {
+  databaseClusterConfiguration,
+  databaseConfigurations,
+} from './database-configuration';
 
 describe('create a database cluster, mocked data', () => {
   databaseConfigurations.forEach(
--- packages/manager/cypress/e2e/databases/delete-database.spec.ts
+++ packages/manager/cypress/e2e/databases/delete-database.spec.ts
@@ -2,124 +2,132 @@
  * @file DBaaS integration tests for delete operations.
  */
 
-import { regions } from 'support/constants/regions';
 import { databaseFactory } from 'src/factories/databases';
-import {
-  randomLabel,
-  randomItem,
-  randomNumber,
-  randomIp,
-} from 'support/util/random';
+import { randomNumber, randomIp } from 'support/util/random';
 import {
   mockDeleteDatabase,
   mockDeleteProvisioningDatabase,
   mockGetDatabase,
 } from 'support/intercepts/databases';
 import { ui } from 'support/ui';
+import {
+  databaseClusterConfiguration,
+  databaseConfigurations,
+} from './database-configuration';
 
 describe('Delete database clusters', () => {
-  /*
-   * - Tests database deletion UI flow using mocked data.
-   * - Confirms that database deletion flow can be completed.
-   * - Confirms that user is redirected to database landing page upon completion.
-   */
-  it('Can delete active database clusters', () => {
-    const allowedIp = randomIp();
-    const database = databaseFactory.build({
-      id: randomNumber(1, 1000),
-      type: 'g6-nanode-1',
-      label: randomLabel(),
-      region: randomItem(regions),
-      engine: 'mysql',
-      status: 'active',
-      allow_list: [allowedIp],
-    });
+  databaseConfigurations.forEach(
+    (configuration: databaseClusterConfiguration) => {
+      it(`Deletes a ${configuration.linodeType} ${configuration.engine} v${configuration.version}.x ${configuration.clusterSize}-node cluster at ${configuration.region}`, () => {
+        /*
+         * - Tests database deletion UI flow using mocked data.
+         * - Confirms that database deletion flow can be completed.
+         * - Confirms that user is redirected to database landing page upon completion.
+         */
+        it('Can delete active database clusters', () => {
+          const allowedIp = randomIp();
+          const database = databaseFactory.build({
+            id: randomNumber(1, 1000),
+            type: configuration.linodeType,
+            label: configuration.label,
+            region: configuration.region,
+            engine: configuration.dbType,
+            status: 'active',
+            allow_list: [allowedIp],
+          });
 
-    mockGetDatabase(database).as('getDatabase');
-    mockDeleteDatabase(database.id, database.engine).as('deleteDatabase');
+          mockGetDatabase(database).as('getDatabase');
+          mockDeleteDatabase(database.id, database.engine).as('deleteDatabase');
 
-    cy.visitWithLogin(`/databases/${database.engine}/${database.id}/settings`);
-    cy.wait('@getDatabase');
+          cy.visitWithLogin(
+            `/databases/${database.engine}/${database.id}/settings`
+          );
+          cy.wait('@getDatabase');
 
-    // Click "Delete Cluster" button.
-    ui.button
-      .findByAttribute('data-qa-settings-button', 'Delete Cluster')
-      .should('be.visible')
-      .click();
+          // Click "Delete Cluster" button.
+          ui.button
+            .findByAttribute('data-qa-settings-button', 'Delete Cluster')
+            .should('be.visible')
+            .click();
 
-    // Fill out type-to-confirm, click "Delete Cluster" button, confirm error.
-    ui.dialog
-      .findByTitle(`Delete Database Cluster ${database.label}`)
-      .should('be.visible')
-      .within(() => {
-        cy.findByLabelText('Cluster Name').click().type(database.label);
+          // Fill out type-to-confirm, click "Delete Cluster" button, confirm error.
+          ui.dialog
+            .findByTitle(`Delete Database Cluster ${database.label}`)
+            .should('be.visible')
+            .within(() => {
+              cy.findByLabelText('Cluster Name').click().type(database.label);
 
-        ui.buttonGroup
-          .findButtonByTitle('Delete Cluster')
-          .should('be.visible')
-          .should('be.enabled')
-          .click();
+              ui.buttonGroup
+                .findButtonByTitle('Delete Cluster')
+                .should('be.visible')
+                .should('be.enabled')
+                .click();
 
-        cy.wait('@deleteDatabase');
+              cy.wait('@deleteDatabase');
+            });
+
+          cy.url().should('endWith', '/databases');
+          ui.toast.assertMessage('Database Cluster deleted successfully.');
+        });
+
+        /*
+         * - Tests provisioning database deletion UI flow using mocked data.
+         * - Confirms that error is shown to user upon deletion attempt.
+         */
+        it('Cannot delete provisioning database clusters', () => {
+          const database = databaseFactory.build({
+            id: randomNumber(1, 1000),
+            type: configuration.linodeType,
+            label: configuration.label,
+            region: configuration.region,
+            engine: configuration.dbType,
+            status: 'provisioning',
+            allow_list: [],
+            hosts: {
+              primary: undefined,
+              secondary: undefined,
+            },
+          });
+
+          const errorMessage =
+            'Your database is provisioning; please wait until provisioning is complete to perform this operation.';
+
+          mockGetDatabase(database).as('getDatabase');
+          mockDeleteProvisioningDatabase(
+            database.id,
+            database.engine,
+            errorMessage
+          ).as('deleteDatabase');
+
+          cy.visitWithLogin(
+            `/databases/${database.engine}/${database.id}/settings`
+          );
+          cy.wait('@getDatabase');
+
+          // Click "Delete Cluster" button.
+          ui.button
+            .findByAttribute('data-qa-settings-button', 'Delete Cluster')
+            .should('be.visible')
+            .click();
+
+          // Fill out type-to-confirm, click "Delete Cluster" button, confirm error.
+          ui.dialog
+            .findByTitle(`Delete Database Cluster ${database.label}`)
+            .should('be.visible')
+            .within(() => {
+              cy.findByLabelText('Cluster Name').click().type(database.label);
+
+              ui.buttonGroup
+                .findButtonByTitle('Delete Cluster')
+                .should('be.visible')
+                .should('be.enabled')
+                .click();
+
+              cy.wait('@deleteDatabase');
+              cy.findByText(errorMessage).should('be.visible');
+            });
+        });
       });
-
-    cy.url().should('endWith', '/databases');
-    ui.toast.assertMessage('Database Cluster deleted successfully.');
-  });
-
-  /*
-   * - Tests provisioning database deletion UI flow using mocked data.
-   * - Confirms that error is shown to user upon deletion attempt.
-   */
-  it('Cannot delete provisioning database clusters', () => {
-    const database = databaseFactory.build({
-      id: randomNumber(1, 1000),
-      type: 'g6-nanode-1',
-      label: randomLabel(),
-      region: randomItem(regions),
-      engine: 'mysql',
-      status: 'provisioning',
-      allow_list: [],
-      hosts: {
-        primary: undefined,
-        secondary: undefined,
-      },
-    });
-
-    const errorMessage =
-      'Your database is provisioning; please wait until provisioning is complete to perform this operation.';
-
-    mockGetDatabase(database).as('getDatabase');
-    mockDeleteProvisioningDatabase(
-      database.id,
-      database.engine,
-      errorMessage
-    ).as('deleteDatabase');
-
-    cy.visitWithLogin(`/databases/${database.engine}/${database.id}/settings`);
-    cy.wait('@getDatabase');
-
-    // Click "Delete Cluster" button.
-    ui.button
-      .findByAttribute('data-qa-settings-button', 'Delete Cluster')
-      .should('be.visible')
-      .click();
-
-    // Fill out type-to-confirm, click "Delete Cluster" button, confirm error.
-    ui.dialog
-      .findByTitle(`Delete Database Cluster ${database.label}`)
-      .should('be.visible')
-      .within(() => {
-        cy.findByLabelText('Cluster Name').click().type(database.label);
-
-        ui.buttonGroup
-          .findButtonByTitle('Delete Cluster')
-          .should('be.visible')
-          .should('be.enabled')
-          .click();
-
-        cy.wait('@deleteDatabase');
-        cy.findByText(errorMessage).should('be.visible');
-      });
-  });
+    }
+  );
 });
