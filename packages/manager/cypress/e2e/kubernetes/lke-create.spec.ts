838130216e0cce654070b932f5e4628dbd43b1cb
Joe D'Amore <jdamore@linode.com>
2023-03-21 12:01:26 -0400

    Use LKE constants, add assertions for created cluster summary page

--- packages/manager/cypress/e2e/kubernetes/lke-create.spec.ts
+++ packages/manager/cypress/e2e/kubernetes/lke-create.spec.ts
@@ -2,52 +2,23 @@
  * @file LKE creation end-to-end tests.
  */
 
+import { KubernetesCluster } from '@linode/api-v4/types';
 import { ui } from 'support/ui';
+import { lkeClusterPlans } from 'support/constants/lke';
 import { regionsFriendly } from 'support/constants/regions';
+import { interceptCreateCluster } from 'support/intercepts/lke';
 import { randomLabel, randomNumber, randomItem } from 'support/util/random';
-
-const availableClusterPlans = [
-  {
-    name: 'Dedicated 4 GB',
-    count: randomNumber(3, 10),
-    tab: 'Dedicated CPU',
-  },
-  {
-    name: 'Dedicated 16 GB',
-    count: randomNumber(3, 10),
-    tab: 'Dedicated CPU',
-  },
-  {
-    name: 'Linode 2 GB',
-    count: randomNumber(3, 10),
-    tab: 'Shared CPU',
-  },
-  {
-    name: 'Linode 4 GB',
-    count: randomNumber(3, 10),
-    tab: 'Shared CPU',
-  },
-  {
-    name: 'Linode 16 GB',
-    count: randomNumber(3, 10),
-    tab: 'Shared CPU',
-  },
-  {
-    name: 'Linode 24 GB',
-    count: randomNumber(3, 6),
-    tab: 'High Memory',
-  },
-];
 
 describe('LKE Cluster Creation', () => {
   it('can create an LKE cluster', () => {
     const clusterLabel = randomLabel();
     const clusterRegion = randomItem(regionsFriendly);
-    const clusterVersion = randomItem(['1.24', '1.25']);
-    const clusterPlans = new Array(randomNumber(2, 4))
+    const clusterVersion = '1.25';
+    const clusterPlans = new Array(3)
       .fill(null)
-      .map(() => randomItem(availableClusterPlans));
+      .map(() => randomItem(lkeClusterPlans));
 
+    interceptCreateCluster().as('createCluster');
     cy.visitWithLogin('/kubernetes/create');
 
     cy.findByLabelText('Cluster Label')
@@ -66,18 +37,20 @@ describe('LKE Cluster Creation', () => {
       .type(`${clusterVersion}{enter}`);
 
     clusterPlans.forEach((clusterPlan) => {
-      cy.log(
-        `Adding ${clusterPlan.count}x ${clusterPlan.name} ${clusterPlan.tab} node(s)`
-      );
+      const nodeCount = randomNumber(3, 8);
+      const planName = `${clusterPlan.type} ${clusterPlan.size} GB`;
+      const checkoutName = `${clusterPlan.type} ${clusterPlan.size}GB Plan`;
+
+      cy.log(`Adding ${nodeCount}x ${planName} ${clusterPlan.tab} node(s)`);
       cy.findByText(clusterPlan.tab).should('be.visible').click();
-      cy.findByText(clusterPlan.name)
+      cy.findByText(planName)
         .should('be.visible')
         .closest('tr')
         .within(() => {
           cy.get('[name="Quantity"]')
             .should('be.visible')
             .click()
-            .type(`{selectall}${clusterPlan.count}`);
+            .type(`{selectall}${nodeCount}`);
 
           ui.button
             .findByTitle('Add')
@@ -85,6 +58,49 @@ describe('LKE Cluster Creation', () => {
             .should('be.enabled')
             .click();
         });
+
+      cy.get('[data-testid="kube-checkout-bar"]')
+        .should('be.visible')
+        .within(() => {
+          // It's possible that multiple clusters of the same type get added.
+          // We're taking a naive approach here by confirming that at least one
+          // instance of the cluster appears in the checkout bar.
+          cy.findAllByText(checkoutName).first().should('be.visible');
+        });
+    });
+
+    cy.get('[data-testid="kube-checkout-bar"]')
+      .should('be.visible')
+      .within(() => {
+        ui.button
+          .findByTitle('Create Cluster')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+    cy.wait('@createCluster').then(({ response }) => {
+      if (!response) {
+        throw new Error(
+          `Error creating LKE cluster ${clusterLabel}; API request failed`
+        );
+      }
+      const cluster: KubernetesCluster = response.body;
+      cy.url().should('endWith', `/kubernetes/clusters/${cluster.id}/summary`);
+      cy.findByText(clusterLabel);
+      // Confirm that each Node pool is created.
+      clusterPlans.forEach((clusterPlan) => {
+        const nodePoolLabel = `${clusterPlan.type} ${clusterPlan.size}GB`;
+        const similarNodePoolCount = clusterPlans.filter((otherClusterPlan) => {
+          const otherNodePoolLabel = `${otherClusterPlan.type} ${otherClusterPlan.size}GB`;
+          return nodePoolLabel === otherNodePoolLabel;
+        }).length;
+
+        cy.findAllByText(nodePoolLabel, { selector: 'h2' })
+          .should('have.length', similarNodePoolCount)
+          .first()
+          .should('be.visible');
+      });
     });
   });
 });
