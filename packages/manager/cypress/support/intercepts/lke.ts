cdbc1d54fe5f768118410de4694c7675852f7908
Joe D'Amore <jdamore@linode.com>
2023-03-21 16:05:50 -0400

    Clean up LKE smoke test

--- packages/manager/cypress/support/intercepts/lke.ts
+++ packages/manager/cypress/support/intercepts/lke.ts
@@ -2,7 +2,9 @@
  * @file Cypress intercepts and mocks for Cloud Manager LKE operations.
  */
 
+import type { KubernetesCluster } from '@linode/api-v4/types';
 import { apiMatcher } from 'support/util/intercepts';
+import { makeResponse } from 'support/util/response';
 
 /**
  * Intercepts POST request to create an LKE cluster.
@@ -11,4 +13,21 @@ import { apiMatcher } from 'support/util/intercepts';
  */
 export const interceptCreateCluster = (): Cypress.Chainable<null> => {
   return cy.intercept('POST', apiMatcher('lke/clusters'));
+};
+
+/**
+ * Intercepts POST request to create an LKE cluster and mocks the response.
+ *
+ * @param cluster - LKE cluster with which to mock response.
+ *
+ * @returns Cypress chainable.
+ */
+export const mockCreateCluster = (
+  cluster: KubernetesCluster
+): Cypress.Chainable<null> => {
+  return cy.intercept(
+    'POST',
+    apiMatcher('lke/clusters'),
+    makeResponse(cluster)
+  );
 };
