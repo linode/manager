2c515ade8195580f0db2aab9faf3c081ff22f98e
Hussain Khalil <hkhalil@akamai.com>
2023-03-22 09:47:47 -0400

    More consistent query keys
    Co-authored-by: Hussain Khalil <hussain@sanpilot.co>

--- packages/manager/src/queries/types.ts
+++ packages/manager/src/queries/types.ts
@@ -11,25 +11,28 @@ import { getAll } from 'src/utilities/getAll';
 import { queryPresets } from './base';
 
 const queryKey = 'types';
-const queryKeySpecificTypes = queryKey + '-specificTypes';
 
 const getAllTypes = () =>
   getAll(getLinodeTypes)().then((results) => results.data);
 
 const getSingleType = async (type: string, queryClient: QueryClient) => {
-  const allTypesCache = queryClient.getQueryData<LinodeType[]>(queryKey);
+  const allTypesCache = queryClient.getQueryData<LinodeType[]>(
+    allTypesQueryKey
+  );
   return (
     allTypesCache?.find((cachedType) => cachedType.id === type) ?? getType(type)
   );
 };
 
+const allTypesQueryKey = [queryKey, 'all'];
 export const useAllTypes = (enabled = true) => {
-  return useQuery<LinodeType[], APIError[]>([queryKey], getAllTypes, {
+  return useQuery<LinodeType[], APIError[]>(allTypesQueryKey, getAllTypes, {
     enabled,
     ...queryPresets.oneTimeFetch,
   });
 };
 
+const specificTypesQueryKey = (type: string) => [queryKey, 'detail', type];
 /**
  * Some Linodes may have types that aren't returned by the /types and /types-legacy endpoints. This
  * hook may be useful in fetching these "shadow plans".
@@ -38,7 +41,7 @@ export const useSpecificTypes = (types: string[], enabled = true) => {
   const queryClient = useQueryClient();
   return useQueries(
     types.map<UseQueryOptions<LinodeType, APIError[]>>((type) => ({
-      queryKey: [queryKeySpecificTypes, type],
+      queryKey: specificTypesQueryKey(type),
       queryFn: () => getSingleType(type, queryClient),
       enabled,
       ...queryPresets.oneTimeFetch,
