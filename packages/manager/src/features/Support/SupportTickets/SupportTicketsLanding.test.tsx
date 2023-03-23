2703784cc6ac1ca0b0d37361dd91d9dfe2c4ff4c
Banks Nussman <banks@nussman.us>
2023-03-23 19:30:05 -0400

    rewrite enzyme test

--- packages/manager/src/features/Support/SupportTickets/SupportTicketsLanding.test.tsx
+++ packages/manager/src/features/Support/SupportTickets/SupportTicketsLanding.test.tsx
@@ -1,21 +1,11 @@
-import { shallow } from 'enzyme';
 import * as React from 'react';
+import { renderWithTheme } from 'src/utilities/testHelpers';
 import SupportTicketsLanding from './SupportTicketsLanding';
 
 describe('Support Tickets Landing', () => {
-  const component = shallow(<SupportTicketsLanding />);
+  const { getByText } = renderWithTheme(<SupportTicketsLanding />);
 
-  it('should render a <LandingHeader />', () => {
-    expect(component.find('LandingHeader')).toHaveLength(1);
-  });
-
-  it('should render a <LandingHeader /> with a title prop', () => {
-    expect(component.find('LandingHeader').prop('title')).toBeDefined();
-  });
-
-  it('should render a <LandingHeader /> with a createButtonText prop', () => {
-    expect(
-      component.find('LandingHeader').prop('createButtonText')
-    ).toBeDefined();
+  it('should render a header', () => {
+    getByText('Tickets');
   });
 });
