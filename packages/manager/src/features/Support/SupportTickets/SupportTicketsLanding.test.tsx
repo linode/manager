6a56b4d14224b553dfe145de2738206066bc09cd
Banks Nussman <banks@nussman.us>
2023-03-23 18:03:55 -0400

    make SupportTicketsLanding work properly

--- packages/manager/src/features/Support/SupportTickets/SupportTicketsLanding.test.tsx
+++ packages/manager/src/features/Support/SupportTickets/SupportTicketsLanding.test.tsx
@@ -1,20 +1,9 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import { reactRouterProps } from 'src/__data__/reactRouterProps';
-import {
-  getSelectedTabFromQueryString,
-  SupportTicketsLanding,
-} from './SupportTicketsLanding';
+import SupportTicketsLanding from './SupportTicketsLanding';
 
 describe('Support Tickets Landing', () => {
-  const component = shallow(
-    <SupportTicketsLanding
-      globalErrors={{}}
-      setErrors={jest.fn()}
-      clearErrors={jest.fn()}
-      {...reactRouterProps}
-    />
-  );
+  const component = shallow(<SupportTicketsLanding />);
 
   it('should render a <LandingHeader />', () => {
     expect(component.find('LandingHeader')).toHaveLength(1);
@@ -28,25 +17,5 @@ describe('Support Tickets Landing', () => {
     expect(
       component.find('LandingHeader').prop('createButtonText')
     ).toBeDefined();
-  });
-});
-
-describe('getSelectedTabFromQueryString utility function', () => {
-  it('should return 0 if ?type=open', () => {
-    const url = 'cloud.linode.com/support/tickets?type=open';
-    expect(getSelectedTabFromQueryString(url)).toBe(0);
-  });
-
-  it('should return 1 if ?type=closed', () => {
-    const url = 'cloud.linode.com/support/tickets?type=closed';
-    expect(getSelectedTabFromQueryString(url)).toBe(1);
-  });
-
-  it('should return 0 if type is unrecognized or not defined', () => {
-    let url = 'cloud.linode.com/support/tickets?type=unknown';
-    expect(getSelectedTabFromQueryString(url)).toBe(0);
-
-    url = 'cloud.linode.com/support/tickets';
-    expect(getSelectedTabFromQueryString(url)).toBe(0);
   });
 });
