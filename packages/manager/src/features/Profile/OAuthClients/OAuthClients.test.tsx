2647b2fc32c7b3fa3b96cbc4c374c6a0141d1b9a
Banks Nussman <banks@nussman.us>
2023-03-29 16:32:57 -0400

    add some unit tests

--- packages/manager/src/features/Profile/OAuthClients/OAuthClients.test.tsx
+++ packages/manager/src/features/Profile/OAuthClients/OAuthClients.test.tsx
@@ -1,1 +1,28 @@
-// @todo write new test :D
+import React from 'react';
+import OAuthClients from './OAuthClients';
+import { oauthClientFactory } from 'src/factories/accountOAuth';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+import { rest, server } from 'src/mocks/testServer';
+import { makeResourcePage } from 'src/mocks/serverHandlers';
+import { waitForElementToBeRemoved } from '@testing-library/react';
+
+describe('Maintenance Table Row', () => {
+  const clients = oauthClientFactory.buildList(3);
+
+  it('should render oauth clients that come from the API', async () => {
+    server.use(
+      rest.get('*/account/oauth-clients', (req, res, ctx) => {
+        return res(ctx.json(makeResourcePage(clients)));
+      })
+    );
+
+    const { getByText, getByTestId } = renderWithTheme(<OAuthClients />);
+
+    await waitForElementToBeRemoved(getByTestId('table-row-loading'));
+
+    for (const client of clients) {
+      getByText(client.label);
+      getByText(client.id);
+    }
+  });
+});
