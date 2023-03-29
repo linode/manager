11af61e3741e4d59d59383da174fa2b924f3914a
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-03-29 10:44:41 -0500

    reduce redundant code in create Tabs

--- packages/manager/src/features/linodes/LinodesCreate/SelectPlanPanel/SelectPlanPanel.tsx
+++ packages/manager/src/features/linodes/LinodesCreate/SelectPlanPanel/SelectPlanPanel.tsx
@@ -13,7 +13,6 @@ import withRegions, {
   DefaultProps as RegionsProps,
 } from 'src/containers/regions.container';
 import arrayToList from 'src/utilities/arrayToDelimiterSeparatedList';
-import { gpuPlanText } from '../utilities';
 import { ExtendedType } from 'src/utilities/extendType';
 import { filterPlanSelectionTypes } from './utils';
 import RenderPlanContainer from './RenderPlanContainer';
@@ -69,7 +68,7 @@ export const SelectPlanPanel: React.FC<CombinedProps> = (props) => {
 
   const classes = useSelectPlanPanelStyles();
 
-  const onSelect = (id: string) => () => props.onSelect(id);
+  const onSelect = (id: string) => props.onSelect(id);
 
   const getDisabledClass = (thisClass: string) => {
     const disabledClasses = (props.disabledClasses as string[]) ?? []; // Not a big fan of the casting here but it works
@@ -82,6 +81,50 @@ export const SelectPlanPanel: React.FC<CombinedProps> = (props) => {
       .filter((thisRegion) => thisRegion.capabilities.includes(capability))
       .map((thisRegion) => thisRegion.label);
     return arrayToList(withCapability);
+  };
+
+  const GPUNotice = () => {
+    const programInfo = getDisabledClass('gpu') ? (
+      <>
+        GPU instances are not available in the selected region. Currently these
+        plans are only available in {getRegionsWithCapability('GPU Linodes')}.
+      </>
+    ) : (
+      <div className={classes.gpuGuideLink}>
+        Linode GPU plans have limited availability and may not be available at
+        the time of your request. Some additional verification may be required
+        to access these services.
+        <a
+          href="https://www.linode.com/docs/platform/linode-gpu/getting-started-with-gpu/"
+          target="_blank"
+          aria-describedby="external-site"
+          rel="noopener noreferrer"
+        >
+          {` `}Here is a guide
+        </a>{' '}
+        with information on getting started.
+      </div>
+    );
+    return <Notice warning>{programInfo}</Notice>;
+  };
+
+  const MetalNotice = () => {
+    const programInfo = getDisabledClass('metal') ? (
+      // Until BM-426 is merged, we aren't filtering for regions in getDisabledClass
+      // so this branch will never run.
+      <Typography>
+        Bare Metal instances are not available in the selected region. Currently
+        these plans are only available in{' '}
+        {getRegionsWithCapability('Bare Metal')}.
+      </Typography>
+    ) : (
+      <Typography className={classes.gpuGuideLink}>
+        Bare Metal Linodes have limited availability and may not be available at
+        the time of your request. Some additional verification may be required
+        to access these services.
+      </Typography>
+    );
+    return <Notice warning>{programInfo}</Notice>;
   };
 
   const createTabs = (): [Tab[], LinodeTypeClass[]] => {
@@ -101,248 +144,112 @@ export const SelectPlanPanel: React.FC<CombinedProps> = (props) => {
 
     const shared = [...nanodes, ...standards];
 
-    if (!isEmpty(proDedicated)) {
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Typography data-qa-prodedi className={classes.copy}>
-                Pro Dedicated CPU instances are for very demanding workloads.
-                They only have AMD 2nd generation processors or newer.
-              </Typography>
-              <RenderPlanContainer
-                header={header}
-                isCreate={isCreate}
-                plans={proDedicated}
-                showTransfer={showTransfer}
-                selectedDiskSize={props.selectedDiskSize}
-                currentPlanHeading={currentPlanHeading}
-                disabledClasses={props.disabledClasses}
-                disabled={disabled}
-                selectedID={selectedID}
-                linodeID={linodeID}
-                onSelect={onSelect}
-              />
-            </>
-          );
-        },
+    // type plansTabContentTypes = {
+    //   typography: string;
+    //   plans: PlanSelectionType[];
+    //   title: string;
+    //   key: LinodeTypeClass;
+    //   dataId: string;
+    //   notice?: string;
+    // };
+
+    const plansTabContent = [
+      {
+        typography:
+          'Pro Dedicated CPU instances are for very demanding workloads. They only have AMD 2nd generation processors or newer.',
+        plans: proDedicated,
         title: 'Pro Dedicated CPU',
-      });
-      tabOrder.push('prodedicated');
-    }
-
-    if (!isEmpty(dedicated)) {
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Typography data-qa-dedicated className={classes.copy}>
-                Dedicated CPU instances are good for full-duty workloads where
-                consistent performance is important.
-              </Typography>
-              <RenderPlanContainer
-                header={header}
-                isCreate={isCreate}
-                plans={dedicated}
-                showTransfer={showTransfer}
-                selectedDiskSize={props.selectedDiskSize}
-                currentPlanHeading={currentPlanHeading}
-                disabledClasses={props.disabledClasses}
-                disabled={disabled}
-                selectedID={selectedID}
-                linodeID={linodeID}
-                onSelect={onSelect}
-              />
-            </>
-          );
-        },
+        key: 'prodedicated',
+        dataId: 'data-qa-prodedi',
+      },
+      {
+        typography:
+          'Dedicated CPU instances are good for full-duty workloads where consistent performance is important.',
+        plans: dedicated,
         title: 'Dedicated CPU',
-      });
-      tabOrder.push('dedicated');
-    }
-
-    if (!isEmpty(shared)) {
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Typography data-qa-standard className={classes.copy}>
-                Shared CPU instances are good for medium-duty workloads and are
-                a good mix of performance, resources, and price.
-              </Typography>
-              <RenderPlanContainer
-                header={header}
-                isCreate={isCreate}
-                plans={shared}
-                showTransfer={showTransfer}
-                selectedDiskSize={props.selectedDiskSize}
-                currentPlanHeading={currentPlanHeading}
-                disabledClasses={props.disabledClasses}
-                disabled={disabled}
-                selectedID={selectedID}
-                linodeID={linodeID}
-                onSelect={onSelect}
-              />
-            </>
-          );
-        },
+        key: 'dedicated',
+        dataId: 'data-qa-dedicated',
+      },
+      {
+        typography:
+          ' Shared CPU instances are good for medium-duty workloads and are a good mix of performance, resources, and price.',
+        plans: shared,
         title: 'Shared CPU',
-      });
-      tabOrder.push('standard');
-    }
-
-    if (!isEmpty(highmem)) {
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Typography data-qa-highmem className={classes.copy}>
-                High Memory instances favor RAM over other resources, and can be
-                good for memory hungry use cases like caching and in-memory
-                databases. All High Memory plans use dedicated CPU cores.
-              </Typography>
-              <RenderPlanContainer
-                header={header}
-                isCreate={isCreate}
-                plans={highmem}
-                showTransfer={showTransfer}
-                selectedDiskSize={props.selectedDiskSize}
-                currentPlanHeading={currentPlanHeading}
-                disabledClasses={props.disabledClasses}
-                disabled={disabled}
-                selectedID={selectedID}
-                linodeID={linodeID}
-                onSelect={onSelect}
-              />
-            </>
-          );
-        },
+        key: 'shared',
+        dataId: 'data-qa-standard',
+      },
+      {
+        typography:
+          'High Memory instances favor RAM over other resources, and can be good for memory hungry use cases like caching and in-memory databases. All High Memory plans use dedicated CPU cores.',
+        plans: highmem,
         title: 'High Memory',
-      });
-      tabOrder.push('highmem');
-    }
-
-    if (!isEmpty(gpu)) {
-      const programInfo = getDisabledClass('gpu') ? (
-        <>
-          GPU instances are not available in the selected region. Currently
-          these plans are only available in{' '}
-          {getRegionsWithCapability('GPU Linodes')}.
-        </>
-      ) : (
-        <div className={classes.gpuGuideLink}>{gpuPlanText()}</div>
-      );
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Notice warning>{programInfo}</Notice>
-              <Typography data-qa-gpu className={classes.copy}>
-                Linodes with dedicated GPUs accelerate highly specialized
-                applications such as machine learning, AI, and video
-                transcoding.
-              </Typography>
-              <RenderPlanContainer
-                header={header}
-                isCreate={isCreate}
-                plans={gpu}
-                showTransfer={showTransfer}
-                selectedDiskSize={props.selectedDiskSize}
-                currentPlanHeading={currentPlanHeading}
-                disabledClasses={props.disabledClasses}
-                disabled={disabled}
-                selectedID={selectedID}
-                linodeID={linodeID}
-                onSelect={onSelect}
-              />
-            </>
-          );
-        },
+        key: 'highmem',
+        dataId: 'data-qa-highmem',
+      },
+      {
+        typography:
+          'Linodes with dedicated GPUs accelerate highly specialized applications such as machine learning, AI, and video transcoding.',
+        plans: gpu,
         title: 'GPU',
-      });
-      tabOrder.push('gpu');
-    }
-
-    if (!isEmpty(metal)) {
-      const programInfo = getDisabledClass('metal') ? (
-        // Until BM-426 is merged, we aren't filtering for regions in getDisabledClass
-        // so this branch will never run.
-        <Typography>
-          Bare Metal instances are not available in the selected region.
-          Currently these plans are only available in{' '}
-          {getRegionsWithCapability('Bare Metal')}.
-        </Typography>
-      ) : (
-        <Typography className={classes.gpuGuideLink}>
-          Bare Metal Linodes have limited availability and may not be available
-          at the time of your request. Some additional verification may be
-          required to access these services.
-        </Typography>
-      );
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Notice warning>{programInfo}</Notice>
-              <Typography data-qa-gpu className={classes.copy}>
-                Bare Metal Linodes give you full, dedicated access to a single
-                physical machine. Some services, including backups, VLANs, and
-                disk management, are not available with these plans.
-              </Typography>
-              <RenderPlanContainer
-                header={header}
-                isCreate={isCreate}
-                plans={metal}
-                showTransfer={showTransfer}
-                selectedDiskSize={props.selectedDiskSize}
-                currentPlanHeading={currentPlanHeading}
-                disabledClasses={props.disabledClasses}
-                disabled={disabled}
-                selectedID={selectedID}
-                linodeID={linodeID}
-                onSelect={onSelect}
-              />
-            </>
-          );
-        },
+        key: 'gpu',
+        dataId: 'data-qa-gpu',
+      },
+      {
+        typography:
+          'Bare Metal Linodes give you full, dedicated access to a single physical machine. Some services, including backups, VLANs, and disk management, are not available with these plans.',
+        plans: metal,
         title: 'Bare Metal',
-      });
-      tabOrder.push('metal');
-    }
-
-    if (!isEmpty(premium)) {
-      tabs.push({
-        render: () => {
-          return (
-            <>
-              <Notice warning>
-                This plan is only available in the Washington, DC region.
-              </Notice>
-              <Typography data-qa-gpu className={classes.copy}>
-                Premium CPU instances guarantee a minimum processor model, AMD
-                Epyc<sup>TM</sup> 7713 or higher, to ensure consistent high
-                performance for more demanding workloads.
-              </Typography>
-              <RenderPlanContainer
-                header={header}
-                isCreate={isCreate}
-                plans={premium}
-                showTransfer={showTransfer}
-                selectedDiskSize={props.selectedDiskSize}
-                currentPlanHeading={currentPlanHeading}
-                disabledClasses={props.disabledClasses}
-                disabled={disabled}
-                selectedID={selectedID}
-                linodeID={linodeID}
-                onSelect={onSelect}
-              />
-            </>
-          );
-        },
+        key: 'metal',
+        dataId: 'data-qa-gpu',
+      },
+      {
+        typography:
+          'Premium CPU instances guarantee a minimum processor model, AMD Epyc\u2122 7713 or higher, to ensure consistent high performance for more demanding workloads.',
+        notice: 'This plan is only available in the Washington, DC region.',
+        plans: premium,
         title: 'Premium',
-      });
-      tabOrder.push('premium');
-    }
+        key: 'premium',
+        dataId: 'data-qa-premium',
+      },
+    ];
+
+    plansTabContent.forEach(
+      ({ typography, notice, plans, title, key, dataId }) => {
+        if (!isEmpty(plans)) {
+          tabs.push({
+            render: () => {
+              return (
+                <>
+                  {key === 'gpu' ? <GPUNotice /> : null}
+                  {key === 'metal' ? <MetalNotice /> : null}
+                  {key !== 'gpu' && key !== 'metal' && notice ? (
+                    <Notice warning>{notice}</Notice>
+                  ) : null}
+                  <Typography {...[dataId]} className={classes.copy}>
+                    {typography}
+                  </Typography>
+                  <RenderPlanContainer
+                    header={header}
+                    isCreate={isCreate}
+                    plans={plans}
+                    showTransfer={showTransfer}
+                    selectedDiskSize={props.selectedDiskSize}
+                    currentPlanHeading={currentPlanHeading}
+                    disabledClasses={props.disabledClasses}
+                    disabled={disabled}
+                    selectedID={selectedID}
+                    linodeID={linodeID}
+                    onSelect={onSelect}
+                  />
+                </>
+              );
+            },
+            title,
+          });
+          // tabOrder.push(key);
+        }
+      }
+    );
 
     return [tabs, tabOrder];
   };
