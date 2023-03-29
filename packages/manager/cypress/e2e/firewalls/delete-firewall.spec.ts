71b957ad020be501af1ab14395706bc6556bc61d
Cassie Liu <cliu@akamai.com>
2023-03-29 18:43:36 -0400

    M3-6247: Add Cypress Firewall e2e Create/Delete tests

---
+++ packages/manager/cypress/e2e/firewalls/delete-firewall.spec.ts
@@ -0,0 +1,64 @@
+import { createFirewall } from '@linode/api-v4/lib/firewalls';
+import { Firewall } from '@linode/api-v4/types';
+import { firewallFactory } from 'src/factories/firewalls';
+import { authenticate } from 'support/api/authentication';
+import { randomLabel } from 'support/util/random';
+import { ui } from 'support/ui';
+import { fbtVisible, fbtClick } from 'support/helpers';
+
+authenticate();
+describe('delete firewall', () => {
+  /*
+   * - Clicks "Delete" action menu item for firewall but cancels operation.
+   * - Clicks "Delete" action menu item for firewall and confirms operation.
+   * - Confirms that firewall is still in landing page list after canceled operation.
+   * - Confirms that firewall is removed from landing page list after confirmed operation.
+   */
+  it('deletes a firewall', () => {
+    const firewallRequest = firewallFactory.build({
+      label: randomLabel(),
+    });
+
+    cy.defer(createFirewall(firewallRequest)).then((firewall: Firewall) => {
+      cy.visitWithLogin('/firewalls');
+
+      // Confirm that firewall is listed and initiate deletion.
+      cy.findByText(firewall.label).should('be.visible');
+      fbtVisible('Delete');
+      fbtClick('Delete');
+
+      // Cancel deletion when prompted to confirm.
+      ui.dialog
+        .findByTitle(`Delete Firewall ${firewall.label}?`)
+        .should('be.visible')
+        .within(() => {
+          ui.buttonGroup
+            .findButtonByTitle('Cancel')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+        });
+
+      // Confirm that firewall is still listed and initiate deletion again.
+      cy.findByText(firewall.label).should('be.visible');
+      fbtVisible('Delete');
+      fbtClick('Delete');
+
+      // Confirm deletion.
+      ui.dialog
+        .findByTitle(`Delete Firewall ${firewall.label}?`)
+        .should('be.visible')
+        .within(() => {
+          ui.buttonGroup
+            .findButtonByTitle('Delete Firewall')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+        });
+
+      // Confirm that firewall is deleted.
+      cy.visitWithLogin('/firewalls');
+      cy.findByText(firewall.label).should('not.exist');
+    });
+  });
+});
