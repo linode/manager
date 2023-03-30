11e4ce1aebaebabecdc0a3c23e9767dbd8c97ef4
mjac0bs <mjacobs@akamai.com>
2023-03-30 07:49:16 -0700

    Fix notification menu button styling and badge count

--- packages/manager/src/components/NotificationMenu/NotificationMenu.tsx
+++ packages/manager/src/components/NotificationMenu/NotificationMenu.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
 import Paper from 'src/components/core/Paper';
 import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
 import Events from 'src/features/NotificationCenter/Events';
 import Notifications from 'src/features/NotificationCenter/Notifications';
-import useDismissibleNotifications from 'src/hooks/useDismissibleNotifications';
-import usePrevious from 'src/hooks/usePrevious';
-import { markAllSeen } from 'src/store/events/event.request';
-import { ThunkDispatch } from 'src/store/types';
-import { useNotificationsQuery } from 'src/queries/accountNotifications';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -24,22 +18,8 @@ export interface Props {
 }
 
 export const NotificationMenu = (props: Props) => {
-  const { open } = props;
-
   const classes = useStyles();
-  const { dismissNotifications } = useDismissibleNotifications();
-  const { data: notifications } = useNotificationsQuery();
-  const dispatch = useDispatch<ThunkDispatch>();
-
-  const wasOpen = usePrevious(open);
-
-  React.useEffect(() => {
-    if (wasOpen && !open) {
-      // User has closed the menu.
-      dispatch(markAllSeen());
-      dismissNotifications(notifications ?? [], { prefix: 'notificationMenu' });
-    }
-  }, [dismissNotifications, notifications, dispatch, open, wasOpen]);
+  const { open } = props;
 
   return (
     <Paper className={classes.root}>
