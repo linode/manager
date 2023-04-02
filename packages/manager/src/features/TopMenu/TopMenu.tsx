0beaf2a1839585acedd62bf543a15d94e3dfbfd9
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-04-01 22:52:15 -0400

    Work In Progress

--- packages/manager/src/features/TopMenu/TopMenu.tsx
+++ packages/manager/src/features/TopMenu/TopMenu.tsx
@@ -4,7 +4,8 @@ import AppBar from 'src/components/core/AppBar';
 import Hidden from 'src/components/core/Hidden';
 import IconButton from 'src/components/core/IconButton';
 import { makeStyles } from '@mui/styles';
-import { Theme } from '@mui/material/styles';
+import { Theme, useTheme } from '@mui/material/styles';
+import clsx from 'clsx';
 import Toolbar from 'src/components/core/Toolbar';
 import Typography from 'src/components/core/Typography';
 import AddNewMenu from './AddNewMenu';
@@ -31,11 +32,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     height: `50px !important`,
     width: '100%',
   },
-  communityIcon: {
-    [theme.breakpoints.down(370)]: {
-      ...theme.visually.hidden,
-    },
-  },
 }));
 
 interface Props {
@@ -46,9 +42,7 @@ interface Props {
   username: string;
 }
 
-type PropsWithStyles = Props;
-
-const TopMenu: React.FC<PropsWithStyles> = (props) => {
+const TopMenu = (props: Props) => {
   const {
     isSideMenuOpen,
     openSideMenu,
@@ -57,7 +51,14 @@ const TopMenu: React.FC<PropsWithStyles> = (props) => {
     desktopMenuToggle,
   } = props;
 
+  const theme = useTheme();
   const classes = useStyles();
+
+  const communityIconStyles = {
+    [theme.breakpoints.down(370)]: {
+      ...theme.visually.hidden,
+    },
+  };
 
   const navHoverText = isSideMenuOpen
     ? 'Collapse side menu'
@@ -108,7 +109,7 @@ const TopMenu: React.FC<PropsWithStyles> = (props) => {
           <AddNewMenu />
           <SearchBar />
           <Help />
-          <Community className={classes.communityIcon} />
+          <Community className={clsx(communityIconStyles)} />
           <NotificationMenu />
           <UserMenu />
         </Toolbar>
