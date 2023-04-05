6a51253a74670fdcc06f66c0646430cc1e0034c4
Joe D'Amore <jdamore@linode.com>
2023-04-05 17:50:45 -0400

    Decouple polling utils and backoff logic, add simple backoff implementation

---
+++ packages/manager/cypress/support/util/backoff.ts
@@ -0,0 +1,167 @@
+/**
+ * @file Utilities to handle retries with configurable backoff logic.
+ */
+
+// Util to wait a given number of milliseconds before resolving.
+const timeout = (timeout: number) =>
+  new Promise((resolve) => setTimeout(resolve, timeout));
+
+// Util to calculate fibonacci number for a given index.
+const fibonacci = (index: number): number => {
+  if (index <= 1) {
+    return 1;
+  }
+  return fibonacci(index - 1) + fibonacci(index - 2);
+};
+
+/**
+ * Options that apply to all backoff method implementations.
+ */
+export interface BackoffOptions {
+  /// Maximum number of attempts to make before rejecting with an error.
+  maxAttempts: number;
+
+  /// Length of time to wait (in milliseconds) before making first attempt.
+  initialDelay: number;
+}
+
+/**
+ * Default backoff method options.
+ */
+export const defaultBackoffOptions: BackoffOptions = {
+  maxAttempts: 10,
+  initialDelay: 0,
+};
+
+/**
+ * Attempts to resolve a Promise using a given backoff method to limit and delay attempts.
+ *
+ * @param backoffMethod - Backoff method to use to calculate delay between attempts.
+ * @param promiseCallback - Callback to generate Promise to attempt to resolve.
+ *
+ * @returns Promise that resolves when `promiseCallback` Promise succeeds, or rejects after a given number of attempts.
+ */
+export const attemptWithBackoff = async <T>(
+  backoffMethod: BackoffMethod,
+  promiseCallback: () => Promise<T>
+): Promise<T> => {
+  const { initialDelay, maxAttempts } = backoffMethod.options;
+
+  if (initialDelay) {
+    await timeout(initialDelay);
+  }
+
+  // Disable ESLint rule because we do not want to parallelize the async operations.
+  /* eslint-disable no-await-in-loop */
+  for (let attempt = 1; attempt < maxAttempts; attempt++) {
+    const nextAttempt = attempt + 1;
+    try {
+      const result = await promiseCallback();
+      return result;
+    } catch (e) {
+      if (nextAttempt < maxAttempts) {
+        const backoffTime = backoffMethod.calculateBackoff(nextAttempt);
+        await timeout(backoffTime);
+      }
+    }
+  }
+
+  throw new Error(`Failed to resolve promise after ${maxAttempts} attempt(s)`);
+};
+
+/**
+ * Tries to resolve a Promise a given number of times using some backoff logic.
+ */
+export abstract class BackoffMethod {
+  /// Backoff method options.
+  public readonly options: BackoffOptions;
+
+  /**
+   * Constructor.
+   *
+   * @param options - Backoff method options.
+   */
+  constructor(options?: Partial<BackoffOptions> | undefined) {
+    this.options = {
+      ...defaultBackoffOptions,
+      ...(options || {}),
+    };
+  }
+
+  /**
+   * Returns the amount of time to delay for the given attempt.
+   *
+   * @param attempt - Attempt number for which to calculate backoff delay.
+   *
+   * @returns Time (in milliseconds) to delay.
+   */
+  public abstract calculateBackoff(attempt: number): number;
+}
+
+/**
+ * Tries to resolve a Promise a given number of times using a constant delay between attempts.
+ */
+export class SimpleBackoffMethod extends BackoffMethod {
+  /// Delay between attempts (in milliseconds).
+  public readonly timeout: number;
+
+  /**
+   * Constructor.
+   *
+   * @param timeout - Timeout between each attempt.
+   * @param options - Backoff method options.
+   */
+  constructor(timeout: number, options?: Partial<BackoffOptions>) {
+    super(options);
+    this.timeout = timeout;
+  }
+
+  /**
+   * Returns the same backoff timeout for every attempt.
+   *
+   * @returns Backoff timeout (in milliseconds) corresponding to the value of `timeout`.
+   */
+  public calculateBackoff(_attempt: number): number {
+    return this.timeout;
+  }
+}
+
+/**
+ * Tries to resolve a Promise a given number of times using Fibonacci backoff.
+ */
+export class FibonacciBackoffMethod extends BackoffMethod {
+  /// Optional maximum timeout, in milliseconds.
+  public readonly maxTimeout: number | undefined;
+
+  /// Fibonacci starting index.
+  public readonly offset: number;
+
+  /**
+   * Constructor.
+   *
+   * @param options - Backoff method options.
+   * @param maxTimeout - Optional maximum backoff timeout (in milliseconds).
+   * @param offset - Fibonacci starting index; useful for increasing delay between attempts.
+   */
+  constructor(
+    options?: Partial<BackoffOptions>,
+    maxTimeout: number | undefined = undefined,
+    offset: number = 0
+  ) {
+    super(options);
+    this.maxTimeout = maxTimeout;
+    this.offset = offset;
+  }
+
+  /**
+   * Returns backoff derived from Fibonacci sequence.
+   *
+   * @returns Backoff timeout (in milliseconds) from Fibonacci sequence.
+   */
+  public calculateBackoff(attempt: number): number {
+    const fibonacciTimeout = fibonacci(attempt + this.offset) * 1000;
+    return !!this.maxTimeout
+      ? Math.min(fibonacciTimeout, this.maxTimeout)
+      : fibonacciTimeout;
+  }
+}
