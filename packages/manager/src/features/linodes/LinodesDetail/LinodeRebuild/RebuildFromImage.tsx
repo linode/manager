70a0288327b3a0515d72416da5bddb50a7c17564
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2023-04-11 15:00:24 -0400

    fix: Unit Tests for Metadata and Improvements to Typesafety (#8984)
    * improve types and fix tests
    
    * add type and remove some default exports
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

--- packages/manager/src/features/linodes/LinodesDetail/LinodeRebuild/RebuildFromImage.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeRebuild/RebuildFromImage.tsx
@@ -20,7 +20,7 @@ import Grid from 'src/components/Grid';
 import ImageSelect from 'src/components/ImageSelect';
 import TypeToConfirm from 'src/components/TypeToConfirm';
 import { resetEventsPolling } from 'src/eventsPolling';
-import UserDataAccordion from 'src/features/linodes/LinodesCreate/UserDataAccordion/UserDataAccordion';
+import { UserDataAccordion } from 'src/features/linodes/LinodesCreate/UserDataAccordion/UserDataAccordion';
 import useFlags from 'src/hooks/useFlags';
 import { useAllImagesQuery } from 'src/queries/images';
 import { usePreferences } from 'src/queries/preferences';
