49d049b1a5bf6315a4c85d0376372ee28b943b07
Joe D'Amore <jdamore@linode.com>
2023-04-12 13:53:09 -0400

    Remove Jest dependencies and config

--- packages/manager/src/features/Account/AutoBackups.test.tsx
+++ packages/manager/src/features/Account/AutoBackups.test.tsx
@@ -1,5 +1,4 @@
 import { vi } from 'vitest';
-import * as jaxe from 'jest-axe';
 import * as React from 'react';
 import { renderWithTheme, toPassAxeCheck } from 'src/utilities/testHelpers';
 import AutoBackups from './AutoBackups';
@@ -7,9 +6,7 @@ import AutoBackups from './AutoBackups';
 const TEST_TIMEOUT_MS = 10000;
 
 expect.extend(toPassAxeCheck);
-expect.extend(jaxe.toHaveNoViolations);
 
-// expect.extend(jaxe.toHaveNoViolations);
 describe('AutoBackups simple sanity check', () => {
   it(
     'not managed not auto backups with linodes nobackups',
@@ -24,32 +21,7 @@ describe('AutoBackups simple sanity check', () => {
       };
       const res = renderWithTheme(<AutoBackups {...props} />);
       expect(res).toPassAxeCheck();
-      // expect(await jaxe.axe(res.container)).toHaveNoViolations();
     },
     TEST_TIMEOUT_MS
   );
-
-  // it('should FAIL but does not', async () => {
-  //   // pass anything that outputs html to axe
-  //   const html = render(<a href="#">test</a>)
-  //   expect(await jaxe.axe(html.container)).toHaveNoViolations()
-  //   expect(html).toPassAxeCheck()
-
-  // })
-  // it('should FAIL but does not', async () => {
-  //   const html = render(<a href="">test</a>)
-  //   expect(await jaxe.axe(html.container)).toHaveNoViolations()
-  //   expect(html).toPassAxeCheck()
-  // })
-  // it('should PASS', async () => {
-  //   const html = render(<img src="#" alt="test"/>)
-  //   expect(await jaxe.axe(html.container)).toHaveNoViolations()
-  //   expect(html).toPassAxeCheck()
-
-  // })
-  // it('should FAIL', async () => {
-  //   const html = render(<img src="#" />)
-  //   expect(await jaxe.axe(html.container)).toHaveNoViolations()
-  //   expect(html).toPassAxeCheck()
-  // })
 });
