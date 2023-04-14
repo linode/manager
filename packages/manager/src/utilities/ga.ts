9f7b914070660bf4cf749d90800fdaf6638c796a
mjac0bs <mjacobs@akamai.com>
2023-04-14 13:46:17 -0700

    Enable direct call rule for AA custom event tracking

--- packages/manager/src/utilities/ga.ts
+++ packages/manager/src/utilities/ga.ts
@@ -13,13 +13,12 @@ interface AnalyticsEvent {
  */
 export const sendEvent = (eventPayload: AnalyticsEvent): void => {
   // Send a Direct Call Rule to Adobe
-  // TODO: uncomment in forthcoming PR once basic tracking is verified
-  // (window as any)._satellite.track('custom event', {
-  //   category: eventPayload.category,
-  //   action: eventPayload.action,
-  //   label: eventPayload.label,
-  //   value: eventPayload.value,
-  // });
+  (window as any)._satellite.track('custom event', {
+    category: eventPayload.category,
+    action: eventPayload.action,
+    label: eventPayload.label,
+    value: eventPayload.value,
+  });
 
   /** only send events if we have a GA ID */
   return !!GA_ID ? event(eventPayload) : undefined;
