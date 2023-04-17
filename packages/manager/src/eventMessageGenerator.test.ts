053ef67ea75356702b69363ef17dacfd713105e0
Alban Bailly <abailly@akamai.com>
2023-04-17 16:19:54 -0400

    fix: [M3-6430] - use more factories in unit test

--- packages/manager/src/eventMessageGenerator.test.ts
+++ packages/manager/src/eventMessageGenerator.test.ts
@@ -90,48 +90,38 @@ describe('Event message generation', () => {
   });
 
   describe('apply linking to labels', () => {
+    const entity = entityFactory.build({ id: 10, label: 'foo' });
+
     it('should return empty string if message is falsy', () => {
-      const mockEvent = {
-        action: 'create',
-        entity: null,
-        secondary_entity: null,
-      };
+      const mockEvent = eventFactory.build({ action: 'domain_record_create' });
       const message = null;
-      const result = applyLinking(mockEvent as Event, message as any);
+      const result = applyLinking(mockEvent, message as any); // casting since message is a required prop
 
       expect(result).toEqual('');
     });
 
-    it('should replace entity label with link if entity and link exist', () => {
-      const entity = { id: '123', label: 'foo', type: 'linode' };
-      const mockEvent = { action: 'create', entity, secondary_entity: null };
+    it('should replace entity label with link if entity and link exist', async () => {
+      const mockEvent = eventFactory.build({ entity });
       const message = 'created entity foo';
-      const link = '/linodes/123';
-      const result = applyLinking(mockEvent as any, message);
+      const result = applyLinking(mockEvent, message);
 
-      expect(result).toEqual(`created entity <a href="${link}">foo</a> `);
+      expect(result).toEqual(`created entity <a href="/linodes/10">foo</a> `);
     });
 
     it('should replace secondary entity label with link if entity and link exist', () => {
-      jest.clearAllMocks();
-
-      const entity = { id: '123', label: 'foo', type: 'linode' };
-      const secondary_entity = { id: '456', label: 'bar' };
-      const mockEvent = { action: 'create', entity, secondary_entity };
+      const mockEvent = eventFactory.build({ entity });
       const message = 'created secondary_entity for foo';
-      const link = '/linodes/123';
-      const result = applyLinking(mockEvent as any, message);
+      const result = applyLinking(mockEvent, message);
 
       expect(result).toEqual(
-        `created secondary_entity for <a href="${link}">foo</a> `
+        `created secondary_entity for <a href="/linodes/10">foo</a> `
       );
     });
 
     it('should not replace entity label if label is inside backticks', () => {
-      const entity = { id: '123', label: 'foo' };
-      const mockEvent = { action: 'create', entity, secondary_entity: null };
+      const mockEvent = eventFactory.build({ entity });
       const message = 'created `foo`';
-      const result = applyLinking(mockEvent as any, message);
+      const result = applyLinking(mockEvent, message);
 
       expect(result).toEqual('created `foo`');
     });
