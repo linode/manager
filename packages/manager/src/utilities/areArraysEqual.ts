fc2b93fd61978c8d17ca57e3fd6a08382cef4a98
Alban Bailly <abailly@akamai.com>
2023-04-20 08:56:24 -0400

    fix: [M3-5976] - handle IP sharing modal

---
+++ packages/manager/src/utilities/areArraysEqual.ts
@@ -0,0 +1,16 @@
+/**
+ * Specify props to compare string arrays.
+ * Note: This function is not recursive and will only be true if arrays are sorted equally.
+ * The reason is that this function is intended to do the bare minimum for the sake of performance and its current use cases.
+ *
+ * @param array1
+ * @param array2
+ *
+ * @returns boolean
+ */
+export const areArraysEqual = <T>(array1: T[], array2: T[]): boolean => {
+  return (
+    array1.length === array2.length &&
+    array1.every((v: T, i: number) => v === array2[i])
+  );
+};
