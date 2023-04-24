77a385dcc6bf7fcfdbb7a544506f2fa7a94ec2a1
Joe D'Amore <jdamore@linode.com>
2023-04-24 11:59:46 -0400

    Add integration test for LKE deletion via details page

--- packages/manager/cypress/e2e/kubernetes/lke-delete.spec.ts
+++ packages/manager/cypress/e2e/kubernetes/lke-delete.spec.ts
@@ -1,4 +1,3 @@
-// import { KubernetesCluster } from '@linode/api-v4';
 import { kubernetesClusterFactory } from 'src/factories';
 import {
   mockGetCluster,
@@ -8,11 +7,34 @@ import {
 import { ui } from 'support/ui';
 import { randomLabel } from 'support/util/random';
 
-const deletionWarning = `Deleting a cluster is permanent and can't be undone.`;
+/*
+ * Fills out and submits Type to Confirm deletion dialog for cluster with the given label.
+ */
+const completeTypeToConfirmDialog = (clusterLabel: string) => {
+  const deletionWarning = `Deleting a cluster is permanent and can't be undone.`;
+
+  ui.dialog
+    .findByTitle(`Delete Cluster ${clusterLabel}`)
+    .should('be.visible')
+    .within(() => {
+      cy.findByText(deletionWarning, { exact: false }).should('be.visible');
+      cy.findByLabelText('Cluster Name')
+        .should('be.visible')
+        .click()
+        .type(clusterLabel);
+
+      ui.buttonGroup
+        .findButtonByTitle('Delete Cluster')
+        .should('be.visible')
+        .should('be.enabled')
+        .click();
+    });
+};
 
 describe('LKE cluster deletion', () => {
   /*
-   * -
+   * - Confirms LKE cluster deletion flow via landing page.
+   * - Confirms that landing page updates to reflect deleted cluster.
    */
   it('can delete an LKE cluster from summary page', () => {
     const mockCluster = kubernetesClusterFactory.build({
@@ -38,22 +60,7 @@ describe('LKE cluster deletion', () => {
 
     // Fill out and submit type-to-confirm.
     mockGetClusters([]).as('getClusters');
-    ui.dialog
-      .findByTitle(`Delete Cluster ${mockCluster.label}`)
-      .should('be.visible')
-      .within(() => {
-        cy.findByText(deletionWarning, { exact: false }).should('be.visible');
-        cy.findByLabelText('Cluster Name')
-          .should('be.visible')
-          .click()
-          .type(mockCluster.label);
-
-        ui.buttonGroup
-          .findButtonByTitle('Delete Cluster')
-          .should('be.visible')
-          .should('be.enabled')
-          .click();
-      });
+    completeTypeToConfirmDialog(mockCluster.label);
 
     // Confirm that cluster is no longer listed on landing page.
     cy.wait(['@deleteCluster', '@getClusters']);
@@ -63,25 +70,37 @@ describe('LKE cluster deletion', () => {
     cy.findByText('Fully managed Kubernetes infrastructure').should(
       'be.visible'
     );
+
     ui.button
       .findByTitle('Create Cluster')
       .should('be.visible')
       .should('be.enabled');
   });
 
+  /*
+   * - Confirms LKE cluster deletion flow via details page.
+   * - Confirms that user is redirected to landing page upon cluster deletion.
+   */
   it('can delete an LKE cluster from landing page', () => {
     const mockCluster = kubernetesClusterFactory.build({
       label: randomLabel(),
     });
 
+    // Navigate to details page for mocked LKE cluster.
     mockGetCluster(mockCluster).as('getCluster');
+    mockDeleteCluster(mockCluster.id).as('deleteCluster');
     cy.visitWithLogin(`/kubernetes/clusters/${mockCluster.id}/summary`);
     cy.wait('@getCluster');
 
+    // Press "Delete Cluster" button, complete type-to-confirm, and confirm redirect.
     ui.button
       .findByTitle('Delete Cluster')
       .should('be.visible')
       .should('be.enabled')
       .click();
+
+    completeTypeToConfirmDialog(mockCluster.label);
+    cy.wait('@deleteCluster');
+    cy.url().should('endWith', 'kubernetes/clusters');
   });
 });
