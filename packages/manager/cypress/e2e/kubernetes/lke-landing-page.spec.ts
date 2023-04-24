a3dd67c1f0a13bc98c7895fa4513dab639a278a5
Joe D'Amore <jdamore@linode.com>
2023-04-24 13:28:07 -0400

    Add integration test to verify kubeconfig download flow via landing page

--- packages/manager/cypress/e2e/kubernetes/lke-landing-page.spec.ts
+++ packages/manager/cypress/e2e/kubernetes/lke-landing-page.spec.ts
@@ -1,7 +1,16 @@
 import type { KubernetesCluster } from '@linode/api-v4';
-import { mockGetClusters, mockGetClusterPools } from 'support/intercepts/lke';
-import { kubernetesClusterFactory, nodePoolFactory } from 'src/factories';
+import {
+  mockGetClusters,
+  mockGetClusterPools,
+  mockGetKubeconfig,
+} from 'support/intercepts/lke';
+import {
+  kubernetesClusterFactory,
+  nodePoolFactory,
+  kubeconfigFactory,
+} from 'src/factories';
 import { regionsMap } from 'support/constants/regions';
+import { readDownload } from 'support/util/downloads';
 import { ui } from 'support/ui';
 
 describe('LKE landing page', () => {
@@ -60,5 +69,44 @@ describe('LKE landing page', () => {
 
     cy.findByText('Getting Started Guides').should('be.visible');
     cy.findByText('Video Playlist').should('be.visible');
+  });
+
+  /*
+   * - Confirms UI flow for Kubeconfig file downloading using mocked data.
+   * - Confirms that downloaded Kubeconfig contains expected content.
+   */
+  it('can download kubeconfig', () => {
+    const mockCluster = kubernetesClusterFactory.build();
+    const mockClusterNodePools = nodePoolFactory.buildList(2);
+    const mockKubeconfigFilename = `${mockCluster.label}-kubeconfig.yaml`;
+    const mockKubeconfigContents = '---'; // Valid YAML.
+    const mockKubeconfigResponse = kubeconfigFactory.build({
+      kubeconfig: btoa(mockKubeconfigContents),
+    });
+
+    mockGetClusters([mockCluster]).as('getClusters');
+    mockGetClusterPools(mockCluster.id, mockClusterNodePools).as(
+      'getNodePools'
+    );
+    mockGetKubeconfig(mockCluster.id, mockKubeconfigResponse).as(
+      'getKubeconfig'
+    );
+
+    cy.visitWithLogin('/kubernetes/clusters');
+    cy.wait(['@getClusters', '@getNodePools']);
+
+    cy.findByText(mockCluster.label)
+      .should('be.visible')
+      .closest('tr')
+      .within(() => {
+        ui.button
+          .findByTitle('Download kubeconfig')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+    cy.wait('@getKubeconfig');
+    readDownload(mockKubeconfigFilename).should('eq', mockKubeconfigContents);
   });
 });
