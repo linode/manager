8ff8c4fb9ffe8a22f6a22b955f2afabb4d18ef19
Joe D'Amore <jdamore@linode.com>
2023-04-24 14:55:58 -0400

    Add LKE mock utils to update cluster, get Kubernetes versions, and reset kubeconfig

--- packages/manager/cypress/support/intercepts/lke.ts
+++ packages/manager/cypress/support/intercepts/lke.ts
@@ -6,10 +6,33 @@ import type {
   KubernetesCluster,
   KubeNodePoolResponse,
   KubeConfigResponse,
+  KubernetesVersion,
 } from '@linode/api-v4/types';
+import { kubernetesVersions } from 'support/constants/lke';
 import { apiMatcher } from 'support/util/intercepts';
 import { paginateResponse } from 'support/util/paginate';
 import { makeResponse } from 'support/util/response';
+
+/**
+ * Intercepts GET request to retrieve Kubernetes versions and mocks response.
+ *
+ * @param versions - Optional array of strings containing mocked versions.
+ *
+ * @returns Cypress chainable.
+ */
+export const mockGetKubernetesVersions = (versions?: string[] | undefined) => {
+  const versionObjects = (versions ? versions : kubernetesVersions).map(
+    (kubernetesVersionString: string): KubernetesVersion => {
+      return { id: kubernetesVersionString };
+    }
+  );
+
+  return cy.intercept(
+    'GET',
+    apiMatcher('lke/versions*'),
+    paginateResponse(versionObjects)
+  );
+};
 
 /**
  * Intercepts GET request to retrieve LKE clusters and mocks response.
@@ -46,9 +69,28 @@ export const mockGetCluster = (
 };
 
 /**
+ * Intercepts PUT request to update an LKE cluster and mocks response.
+ *
+ * @param clusterId - ID of cluster for which to intercept PUT request.
+ * @param cluster - Updated Kubernetes cluster with which to mock response.
+ *
+ * @returns Cypress chainable.
+ */
+export const mockUpdateCluster = (
+  clusterId: number,
+  cluster: KubernetesCluster
+): Cypress.Chainable<null> => {
+  return cy.intercept(
+    'PUT',
+    apiMatcher(`lke/clusters/${clusterId}`),
+    makeResponse(cluster)
+  );
+};
+
+/**
  * Intercepts GET request to retrieve an LKE cluster's node pools and mocks response.
  *
- * @param clusterId - ID of cluster for which to mock response.
+ * @param clusterId - ID of cluster for which to intercept GET request.
  * @param pools - Array of LKE node pools with which to mock response.
  *
  * @returns Cypress chainable.
@@ -112,7 +154,7 @@ export const mockCreateCluster = (
 /**
  * Intercepts DELETE request to delete an LKE cluster and mocks the response.
  *
- * @param clusterId - Numeric ID for LKE cluster for which to mock response.
+ * @param clusterId - Numeric ID of LKE cluster for which to mock response.
  *
  * @returns Cypress chainable.
  */
@@ -123,5 +165,22 @@ export const mockDeleteCluster = (
     'DELETE',
     apiMatcher(`lke/clusters/${clusterId}`),
     makeResponse()
+  );
+};
+
+/**
+ * Intercepts DELETE request to reset Kubeconfig and mocks the response.
+ *
+ * @param clusterId - Numberic ID of LKE cluster for which to mock response.
+ *
+ * @returns Cypress chainable.
+ */
+export const mockResetKubeconfig = (
+  clusterId: number
+): Cypress.Chainable<null> => {
+  return cy.intercept(
+    'DELETE',
+    apiMatcher(`lke/clusters/${clusterId}/kubeconfig`),
+    makeResponse({})
   );
 };
