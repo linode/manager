2e79eb9640fe1a6fce29a686f658cecb82433258
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2023-04-25 15:41:26 -0400

    Update packages/manager/src/components/DialogTitle/DialogTitle.tsx
    Co-authored-by: Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>

--- packages/manager/src/components/DialogTitle/DialogTitle.tsx
+++ packages/manager/src/components/DialogTitle/DialogTitle.tsx
@@ -10,7 +10,7 @@ interface DiaglogTitleProps {
   title: string;
 }
 
-const DialogTitle = (props: DiaglogTitleProps) => {
+const DialogTitle = (props: DialogTitleProps) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const { className, onClose, title } = props;
 
