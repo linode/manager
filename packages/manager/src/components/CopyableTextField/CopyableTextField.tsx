79933ebce743ef1f409be3658ada5cfb6f764a7a
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-04-26 16:56:32 -0500

    refactor styles using styled

--- packages/manager/src/components/CopyableTextField/CopyableTextField.tsx
+++ packages/manager/src/components/CopyableTextField/CopyableTextField.tsx
@@ -1,33 +1,7 @@
 import * as React from 'react';
-import { makeStyles } from 'tss-react/mui';
 import { CopyTooltip } from 'src/components/CopyTooltip/CopyTooltip';
-import { Theme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import TextField, { Props as TextFieldProps } from 'src/components/TextField';
-
-const useStyles = makeStyles()((theme: Theme) => ({
-  removeDisabledStyles: {
-    '& .MuiInput-input': {
-      borderColor: theme.name === 'light' ? '#ccc' : '#222',
-      color:
-        theme.name === 'light'
-          ? `${theme.palette.text.primary} !important`
-          : '#fff !important',
-      opacity: 1,
-      '-webkit-text-fill-color': 'unset !important',
-    },
-    '&& .MuiInput-root': {
-      borderColor: theme.name === 'light' ? '#ccc' : '#222',
-      opacity: 1,
-    },
-  },
-  copyIcon: {
-    marginRight: theme.spacing(0.5),
-    '& svg': {
-      height: 14,
-      top: 1,
-    },
-  },
-}));
 
 type CopyableTextFieldProps = TextFieldProps & {
   className?: string;
@@ -35,21 +9,50 @@ type CopyableTextFieldProps = TextFieldProps & {
 };
 
 export const CopyableTextField = (props: CopyableTextFieldProps) => {
-  const { classes } = useStyles();
   const { value, className, hideIcon, ...restProps } = props;
 
   return (
-    <TextField
+    <StyledTextField
       value={value}
       {...restProps}
-      className={`${className} ${'copy'} ${classes.removeDisabledStyles}`}
+      className={`${className} copy removeDisabledStyles`}
       disabled
       InputProps={{
         endAdornment: hideIcon ? undefined : (
-          <CopyTooltip text={`${value}`} className={classes.copyIcon} />
+          <CopyTooltip text={`${value}`} className="copyIcon" />
         ),
       }}
       data-qa-copy-tooltip
     />
   );
 };
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  '.removeDisabledStyles': {
+    '& .MuiInput-input': {
+      borderColor: theme.name === 'light' ? '#ccc' : '#222',
+      color:
+        theme.name === 'light'
+          ? `${theme.palette.text.primary} !important`
+          : '#fff !important',
+      opacity: theme.name === 'dark' ? 0.5 : 0.8,
+      '-webkit-text-fill-color': 'unset !important',
+    },
+    '&& .MuiInput-root': {
+      borderColor: theme.name === 'light' ? '#ccc' : '#222',
+      opacity: 1,
+    },
+  },
+  '.copyIcon': {
+    marginRight: theme.spacing(0.5),
+    '& svg': {
+      height: 14,
+      top: 1,
+      color: '#3683dc',
+    },
+  },
+  '&.copy > div': {
+    backgroundColor: theme.name === 'dark' ? '#2f3236' : '#f4f4f4',
+    opacity: 1,
+  },
+}));
