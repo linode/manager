a8eda941b230818206d0a26e2f177f5698794f9e
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2023-05-03 16:06:49 -0400

    fix: [M3-6558] - Remove 'MongoDB' from ClusterControl description in Marketplace (#9081)
    Co-authored-by: Jaalah Ramos <jaalah.ramos@gmail.com>

--- packages/manager/src/features/OneClickApps/FakeSpec.ts
+++ packages/manager/src/features/OneClickApps/FakeSpec.ts
@@ -313,7 +313,7 @@ export const oneClickApps: OCA[] = [
     alt_description:
       'SQL and NoSQL database interface and monitoring for MySQL, MongoDB, PostgreSQL, and more.',
     categories: ['Databases'],
-    description: `All-in-one interface for scripting and monitoring databases, including MySQL, MariaDB, Percona, MongoDB, PostgreSQL, Galera Cluster and more. Easily deploy database instances, manage with an included CLI, and automate performance monitoring.`,
+    description: `All-in-one interface for scripting and monitoring databases, including MySQL, MariaDB, Percona, PostgreSQL, Galera Cluster and more. Easily deploy database instances, manage with an included CLI, and automate performance monitoring.`,
     summary:
       'All-in-one database deployment, management, and monitoring system.',
     related_guides: [
