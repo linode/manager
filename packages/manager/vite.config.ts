7f44eb7c043c56f27e19b0bd7140f365abaaa8de
Alban Bailly <abailly@akamai.com>
2023-05-03 07:52:10 -0400

    chore: [M3-6548] revert polyfills implementation

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -1,7 +1,3 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
@@ -9,13 +5,6 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   build: {
     outDir: 'build',
-    rollupOptions: {
-      plugins: [
-        // Enable rollup polyfills plugin
-        // used during production bundling
-        rollupNodePolyFill() as any,
-      ],
-    },
   },
   plugins: [react(), svgr({ exportAsDefault: true })],
   envPrefix: 'REACT_APP_',
@@ -25,22 +14,6 @@ export default defineConfig({
   resolve: {
     alias: {
       src: `${__dirname}/src`,
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
     },
   },
 });
