f71eb530f147c52559b1d8d74bad87d93a557027
Hussain Khalil <hkhalil@akamai.com>
2023-05-03 17:05:51 -0400

    Completed initial implementation of useEvents hook
    Co-authored-by: Hussain Khalil <hussain@sanpilot.co>

--- packages/manager/src/queries/events.ts
+++ packages/manager/src/queries/events.ts
@@ -1,5 +1,10 @@
-import { APIError } from '@linode/api-v4/lib/types';
-import { useQuery } from 'react-query';
+import { APIError, ResourcePage } from '@linode/api-v4/lib/types';
+import {
+  useQuery,
+  useInfiniteQuery,
+  useQueryClient,
+  InfiniteData,
+} from 'react-query';
 import {
   Entity,
   Event,
@@ -7,6 +12,7 @@ import {
   markEventSeen,
 } from '@linode/api-v4/lib/account';
 import { isInProgressEvent } from 'src/store/events/event.helpers';
+import { isNotNullOrUndefined } from 'src/utilities/nullOrUndefined';
 
 export interface ExtendedEvent extends Event {
   _deleted?: string;
@@ -34,9 +40,78 @@ export const useEventsPolling = (
       retryDelay: 5000,
       refetchOnMount: false,
       refetchOnWindowFocus: false,
-      onSuccess: (data) => eventHandler && data.forEach(eventHandler),
+      onSuccess: (events) =>
+        eventHandler && events.reverse().forEach(eventHandler),
     }
   );
+};
+
+export const useEvents = () => {
+  const queryClient = useQueryClient();
+
+  const eventsQueryKey = [queryKey, 'infinite', 'events'];
+  const eventsPageZeroKey = [queryKey, 'infinite', 'zero'];
+  const eventsLookupKey = [queryKey, 'infinite', 'lookup'];
+
+  const eventsCache = queryClient.getQueryData<InfiniteData<number[]>>(
+    eventsQueryKey
+  );
+
+  const pageZero =
+    queryClient.getQueryData<number[]>(eventsPageZeroKey) ??
+    queryClient.setQueryData<number[]>(eventsPageZeroKey, []);
+
+  // Maintain a look-up table for efficiently updating events
+  const eventsLookup =
+    queryClient.getQueryData<Map<number, ExtendedEvent>>(eventsLookupKey) ??
+    queryClient.setQueryData<Map<number, ExtendedEvent>>(
+      eventsLookupKey,
+      new Map()
+    );
+
+  // Update the cache with new/updated events
+  useEventsPolling((event) => {
+    if (eventsCache) {
+      if (!eventsLookup.has(event.id)) {
+        pageZero.unshift(event.id);
+      }
+      eventsLookup.set(event.id, event);
+    }
+  });
+
+  const mostRecentEventId = eventsCache?.pages?.[0][0];
+  const mostRecentEvent = mostRecentEventId
+    ? eventsLookup.get(mostRecentEventId)
+    : undefined;
+
+  const eventsData = useInfiniteQuery<ResourcePage<number>, APIError[]>(
+    eventsQueryKey,
+    async ({ pageParam }) => {
+      const eventsPage = await getEvents(
+        { page: pageParam, page_size: 25 },
+        mostRecentEvent ? { created: { '+lte': mostRecentEvent?.created } } : {} // ignore new events since these will change cached pages
+      );
+
+      // update the lookup table
+      eventsPage.data.forEach((event) => eventsLookup?.set(event.id, event));
+
+      return { ...eventsPage, data: eventsPage.data.map((event) => event.id) };
+    },
+    {
+      getNextPageParam: ({ page, pages }) =>
+        page < pages ? page + 1 : undefined,
+    }
+  );
+  return {
+    ...eventsData,
+    data: [pageZero, ...(eventsData.data?.pages.map((page) => page.data) ?? [])]
+      .map((page) =>
+        page
+          .map((eventId) => eventsLookup.get(eventId))
+          .filter(isNotNullOrUndefined)
+      )
+      .reduce((page, events) => [...page, ...events], []),
+  };
 };
 
 const requestUnseenEvents = (): Promise<ExtendedEvent[]> =>
