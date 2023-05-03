6c6a098b433da7d21a14f9288690bd9a60fd909e
Alban Bailly <130582365+abailly-akamai@users.noreply.github.com>
2023-05-03 09:02:49 -0400

    feat: [M3-6520] - Add new flag to deliver DC availability notice for premium plans (#9066)
    * feat: [M3-6520] - Add new flag to deliver DC availability notice for premium plans
    
    * feat: [M3-6520] - Move to new component for reusability
    
    * feat: [M3-6520] - feedback

---
+++ packages/manager/src/features/linodes/LinodesCreate/PremiumPlansAvailabilityNotice.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Notice from 'src/components/Notice';
+import useFlags from 'src/hooks/useFlags';
+
+// Ideally we should add premium as a capability so the availability is returned by the API,
+// but it's not there yet so we're using a flag for now.
+export const PremiumPlansAvailabilityNotice = React.memo(() => {
+  const { premiumPlansAvailabilityNotice } = useFlags();
+
+  if (premiumPlansAvailabilityNotice) {
+    return <Notice warning>{premiumPlansAvailabilityNotice}</Notice>;
+  }
+
+  return null;
+});
