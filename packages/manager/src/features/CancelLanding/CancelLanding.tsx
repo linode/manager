a4a8db2936e3711ea9c27e8aab9f709c31538c65
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-05-15 09:55:27 -0500

    code cleanup

--- packages/manager/src/features/CancelLanding/CancelLanding.tsx
+++ packages/manager/src/features/CancelLanding/CancelLanding.tsx
@@ -37,7 +37,7 @@ const useStyles = makeStyles()((theme: Theme) => ({
   },
 }));
 
-const CancelLanding = () => {
+export const CancelLanding = React.memo(() => {
   const { classes } = useStyles();
   const location = useLocation();
 
@@ -72,6 +72,6 @@ const CancelLanding = () => {
       </Button>
     </div>
   );
-};
+});
 
-export default React.memo(CancelLanding);
+export default CancelLanding;
