1dac15a1214b17b280b220b61d0bb37fa0e70b0b
Alban Bailly <abailly@akamai.com>
2023-05-16 00:46:14 -0400

    feat: [M3-6511] save progress with commenting script

--- packages/manager/scripts/changelog/changeset-bot.mjs
+++ packages/manager/scripts/changelog/changeset-bot.mjs
@@ -3,7 +3,9 @@ import { Octokit } from '@octokit/rest';
 import { OWNER, REPO } from './utils/constants.mjs';
 
 const octokit = new Octokit({
-  log: console,
+  // Uncomment to debug
+  // log: console,
+  auth: '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',
 });
 
 export const findChangesetInPr = async ({ owner, repo }) => {
@@ -19,11 +21,43 @@ export const findChangesetInPr = async ({ owner, repo }) => {
     });
 
     const changesetCommitted = PrData.includes(`pr-${pullRequestId}`);
+    try {
+      // Get the authenticated user's information
+      // const { data: authUser } = await octokit.users.getAuthenticated();
+      const { data: comments } = await octokit.issues.listComments({
+        owner,
+        repo,
+        issue_number: pullRequestId,
+      });
 
-    if (changesetCommitted) {
-      console.warn(`PR #${pullRequestId} has a changeset.`);
-    } else {
-      console.warn(`PR #${pullRequestId} does not have a changeset.`);
+      const botComment = comments.find(
+        // TODO: Change this to the bot's actual username
+        (comment) => comment.user.login === 'linode-changeset-bot'
+      );
+
+      // console.warn('comments', comments);
+
+      const comment = changesetCommitted
+        ? `:heavy_check_mark: **Changeset Found**\n\nPR #${pullRequestId} has a changeset!`
+        : `:warning: **No Changeset Found**\n\nPR #${pullRequestId} does not have a changeset.\nNot every PR needs one, but if this PR is a user-facing change, or is relevant to an 'Added', 'Fixed', 'Changed', 'Removed' or 'Tech Stories' type, please consider adding one.`;
+
+      if (botComment) {
+        await octokit.issues.updateComment({
+          owner,
+          repo,
+          comment_id: botComment.id,
+          body: comment,
+        });
+      } else {
+        await octokit.issues.createComment({
+          owner,
+          repo,
+          issue_number: pullRequestId,
+          body: comment,
+        });
+      }
+    } catch (error) {
+      console.warn(`Error retrieving comments: ${error}`);
     }
   } catch (error) {
     console.warn(`An error occurred trying to check for a changeset: ${error}`);
