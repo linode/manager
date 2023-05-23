773eb3b399aa9fe4306fce00c165e2fb66899979
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-05-23 13:48:57 -0500

    Create MetalNotice as reusable component

---
+++ packages/manager/src/features/linodes/LinodesCreate/SelectPlanPanel/MetalNotice.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Capabilities } from '@linode/api-v4/lib/regions/types';
+import Typography from 'src/components/core/Typography';
+import { Notice } from 'src/components/Notice/Notice';
+import { useRegionsQuery } from 'src/queries/regions';
+import arrayToList from 'src/utilities/arrayToDelimiterSeparatedList';
+import { useSelectPlanPanelStyles } from './Styles/selectPlanPanelStyles';
+
+interface Props {
+  hasDisabledClass: boolean;
+}
+
+export const MetalNotice = ({ hasDisabledClass }: Props) => {
+  const { classes } = useSelectPlanPanelStyles();
+  const { data: regions } = useRegionsQuery();
+
+  const getRegionsWithCapability = (capability: Capabilities) => {
+    const withCapability = regions
+      ?.filter((thisRegion) => thisRegion.capabilities.includes(capability))
+      .map((thisRegion) => thisRegion.label);
+    return arrayToList(withCapability ?? []);
+  };
+
+  const programInfo = hasDisabledClass ? (
+    // Until BM-426 is merged, we aren't filtering for regions in getDisabledClass
+    // so this branch will never run.
+    <Typography className={hasDisabledClass ? classes.gpuGuideLink : ''}>
+      hasDisabledClass? `Bare Metal instances are not available in the selected
+      region. Currently these plans are only available in $
+      {getRegionsWithCapability('Bare Metal')}
+      .` : ' Bare Metal Linodes have limited availability and may not be
+      available at the time of your request. Some additional verification may be
+      required to access these services.'
+    </Typography>
+  ) : (
+    <Typography></Typography>
+  );
+  return <Notice warning>{programInfo}</Notice>;
+};
