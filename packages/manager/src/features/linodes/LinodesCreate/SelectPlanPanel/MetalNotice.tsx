fd487f9ac3c07e83b10f17b7d72b76afff11dd67
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-05-24 00:23:17 -0500

    code cleanup

--- packages/manager/src/features/linodes/LinodesCreate/SelectPlanPanel/MetalNotice.tsx
+++ packages/manager/src/features/linodes/LinodesCreate/SelectPlanPanel/MetalNotice.tsx
@@ -22,23 +22,22 @@ export const MetalNotice = ({ hasDisabledClass }: Props) => {
     return arrayToList(withCapability ?? []);
   };
 
-  const programInfo = hasDisabledClass ? (
-    // Until BM-426 is merged, we aren't filtering for regions in getDisabledClass
-    // so this branch will never run.
-    <Typography className={hasDisabledClass ? classes.gpuGuideLink : ''}>
-      hasDisabledClass? `Bare Metal instances are not available in the selected
-      region. Currently these plans are only available in $
-      {getRegionsWithCapability('Bare Metal')}
-      .` : ' Bare Metal Linodes have limited availability and may not be
-      available at the time of your request. Some additional verification may be
-      required to access these services.'
-    </Typography>
-  ) : (
-    <Typography></Typography>
-  );
+  // Until BM-426 is merged, we aren't filtering for regions in getDisabledClass
+  // so this branch will never run.
+  const programInfo = hasDisabledClass
+    ? `Bare Metal instances are not available in the selected
+  region. Currently these plans are only available in ${getRegionsWithCapability(
+    'Bare Metal'
+  )}.`
+    : `Bare Metal Linodes have limited availability and may not be
+  available at the time of your request. Some additional verification may be
+  required to access these services.`;
+
   return (
     <Notice warning dataTestId="metal-notice">
-      {programInfo}
+      <Typography className={hasDisabledClass ? classes.gpuGuideLink : ''}>
+        {programInfo}
+      </Typography>
     </Notice>
   );
 };
