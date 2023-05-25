a486a6223cf341f1da696db58ab17635fb332c86
Banks Nussman <banks@nussman.us>
2023-05-25 13:55:11 -0400

    re-add testing attribute

--- packages/manager/src/components/LinearProgress.tsx
+++ packages/manager/src/components/LinearProgress.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { default as _LinearProgress } from '@mui/material/LinearProgress';
-import { LinearProgressProps as _LinearProgressProps } from '@mui/material/LinearProgress';
+import type { LinearProgressProps as _LinearProgressProps } from '@mui/material/LinearProgress';
 
 export const LinearProgress = (props: _LinearProgressProps) => {
-  return <_LinearProgress {...props} />;
+  return <_LinearProgress {...props} data-testid="linear-progress" />;
 };
 
+// I'd like to do this but storybook won't generate args for this
 // export { LinearProgress } from '@mui/material';
 // export type { LinearProgressProps } from '@mui/material/LinearProgress';
