b1888fe262c578e14670af590ed740baedcf7e64
Alban Bailly <abailly@akamai.com>
2023-05-30 12:03:04 -0400

    Refactor: [M3-6278]: CreateBucketDrawer

--- packages/manager/src/features/ObjectStorage/BucketLanding/CreateBucketDrawer.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/CreateBucketDrawer.tsx
@@ -1,38 +1,27 @@
 import * as React from 'react';
+import ActionsPanel from 'src/components/ActionsPanel';
+import Button from 'src/components/Button';
+import ClusterSelect from './ClusterSelect';
 import Drawer from 'src/components/Drawer';
-import { useProfile } from 'src/queries/profile';
+import EnableObjectStorageModal from '../EnableObjectStorageModal';
+import EUAgreementCheckbox from 'src/features/Account/Agreements/EUAgreementCheckbox';
+import TextField from 'src/components/TextField';
+import { getErrorMap } from 'src/utilities/errorUtils';
+import { isEURegion } from 'src/utilities/formatRegion';
+import { Notice } from 'src/components/Notice/Notice';
+import { styled } from '@mui/material/styles';
+import { useAccountSettings } from 'src/queries/accountSettings';
 import { useFormik } from 'formik';
+import { useProfile } from 'src/queries/profile';
+import {
+  useAccountAgreements,
+  useMutateAccountAgreements,
+} from 'src/queries/accountAgreements';
 import {
   useCreateBucketMutation,
   useObjectStorageBuckets,
   useObjectStorageClusters,
 } from 'src/queries/objectStorage';
-import { isEURegion } from 'src/utilities/formatRegion';
-import { Notice } from 'src/components/Notice/Notice';
-import TextField from 'src/components/TextField';
-import { getErrorMap } from 'src/utilities/errorUtils';
-import ClusterSelect from './ClusterSelect';
-import EUAgreementCheckbox from 'src/features/Account/Agreements/EUAgreementCheckbox';
-import { makeStyles } from 'tss-react/mui';
-import { Theme } from '@mui/material/styles';
-import ActionsPanel from 'src/components/ActionsPanel';
-import Button from 'src/components/Button';
-import EnableObjectStorageModal from '../EnableObjectStorageModal';
-import { useAccountSettings } from 'src/queries/accountSettings';
-import {
-  useAccountAgreements,
-  useMutateAccountAgreements,
-} from 'src/queries/accountAgreements';
-
-const useStyles = makeStyles()((theme: Theme) => ({
-  textWrapper: {
-    marginBottom: theme.spacing(1.25),
-  },
-  agreement: {
-    marginTop: theme.spacing(3),
-    marginButton: theme.spacing(3),
-  },
-}));
 
 interface Props {
   isOpen: boolean;
@@ -40,25 +29,20 @@ interface Props {
 }
 
 export const CreateBucketDrawer = (props: Props) => {
-  const { classes } = useStyles();
   const { data: profile } = useProfile();
   const { isOpen, onClose } = props;
   const isRestrictedUser = profile?.restricted;
-
   const { data: clusters } = useObjectStorageClusters();
   const { data: buckets } = useObjectStorageBuckets(clusters);
-
   const {
     mutateAsync: createBucket,
     isLoading,
     error,
     reset,
   } = useCreateBucketMutation();
-
   const { data: agreements } = useAccountAgreements();
   const { mutateAsync: updateAccountAgreements } = useMutateAccountAgreements();
   const { data: accountSettings } = useAccountSettings();
-
   const [isEnableObjDialogOpen, setIsEnableObjDialogOpen] = React.useState(
     false
   );
@@ -143,8 +127,7 @@ export const CreateBucketDrawer = (props: Props) => {
           disabled={isRestrictedUser}
         />
         {showAgreement ? (
-          <EUAgreementCheckbox
-            className={classes.agreement}
+          <StyledEUAgreementCheckbox
             checked={Boolean(agreements?.eu_model)}
             onChange={(e) =>
               updateAccountAgreements({ eu_model: e.target.checked })
@@ -173,3 +156,10 @@ export const CreateBucketDrawer = (props: Props) => {
     </Drawer>
   );
 };
+
+const StyledEUAgreementCheckbox = styled(EUAgreementCheckbox, {
+  label: 'StyledEUAgreementCheckbox',
+})(({ theme }) => ({
+  marginTop: theme.spacing(3),
+  marginButton: theme.spacing(3),
+}));
