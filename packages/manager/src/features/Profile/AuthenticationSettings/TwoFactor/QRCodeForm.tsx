6d94da894c12a3c89d4ec56bcd961be9d893ed51
Alban Bailly <abailly@akamai.com>
2023-05-31 15:59:54 -0400

    QRCodeForm

--- packages/manager/src/features/Profile/AuthenticationSettings/TwoFactor/QRCodeForm.tsx
+++ packages/manager/src/features/Profile/AuthenticationSettings/TwoFactor/QRCodeForm.tsx
@@ -1,21 +1,8 @@
-import QRCode from 'qrcode.react';
 import * as React from 'react';
-import { CopyableTextField } from 'src/components/CopyableTextField/CopyableTextField';
-import { makeStyles } from 'tss-react/mui';
-import { Theme } from '@mui/material/styles';
+import QRCode from 'qrcode.react';
 import Typography from 'src/components/core/Typography';
-
-const useStyles = makeStyles()((theme: Theme) => ({
-  root: {},
-  instructions: {
-    marginTop: theme.spacing(2),
-  },
-  qrcodeContainer: {
-    margin: `${theme.spacing(2)} 0`,
-    border: `5px solid #fff`,
-    display: 'inline-block',
-  },
-}));
+import { CopyableTextField } from 'src/components/CopyableTextField/CopyableTextField';
+import { styled } from '@mui/material/styles';
 
 interface Props {
   secret: string;
@@ -23,30 +10,38 @@ interface Props {
 }
 
 export const QRCodeForm = (props: Props) => {
-  const { classes } = useStyles();
   const { secret, secretLink } = props;
   return (
     <React.Fragment>
-      <Typography variant="h3" data-qa-copy className={classes.instructions}>
+      <StyledInstructions variant="h3" data-qa-copy>
         Scan this QR code to add your Linode account to your 2FA app:
-      </Typography>
-      <div className={classes.qrcodeContainer}>
+      </StyledInstructions>
+      <StyledQRCodeContainer>
         <QRCode
           size={200}
           level="H" // QR code error checking level ("High"); gives a higher resolution code
           value={secretLink}
           data-qa-qr-code
         />
-      </div>
-      <Typography variant="h3" data-qa-copy className={classes.instructions}>
+      </StyledQRCodeContainer>
+      <StyledInstructions variant="h3" data-qa-copy>
         If your 2FA app does not have a scanner, you can use this secret key:
-      </Typography>
-      <CopyableTextField
-        className={classes.root}
-        value={secret}
-        label="Secret Key"
-        hideLabel
-      />
+      </StyledInstructions>
+      <CopyableTextField value={secret} label="Secret Key" hideLabel />
     </React.Fragment>
   );
 };
+
+const StyledInstructions = styled(Typography, {
+  label: 'StyledInstructions',
+})(({ theme }) => ({
+  marginTop: theme.spacing(2),
+}));
+
+const StyledQRCodeContainer = styled('div', {
+  label: 'StyledQRCodeContainer',
+})(({ theme }) => ({
+  margin: `${theme.spacing(2)} 0`,
+  border: `5px solid #fff`,
+  display: 'inline-block',
+}));
