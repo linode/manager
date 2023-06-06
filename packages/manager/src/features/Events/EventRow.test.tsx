c45ca3230b1b5a1e5c8b8115db3ebc7d0b02c0c5
mjac0bs <mjacobs@akamai.com>
2023-06-06 11:19:03 -0700

    Update RowProps for EventRow test; do not test timestamp

--- packages/manager/src/features/Events/EventRow.test.tsx
+++ packages/manager/src/features/Events/EventRow.test.tsx
@@ -1,73 +1,31 @@
 import * as React from 'react';
 import { renderWithTheme, wrapWithTableBody } from 'src/utilities/testHelpers';
 import { Row, RowProps } from './EventRow';
-import { getEventTimestamp } from 'src/utilities/eventUtils';
-import { eventFactory } from 'src/factories';
+import { DateTime } from 'luxon';
 
-const inProgressMessage = 'this is an in-progress message.';
-const finishedMessage = 'this is a finished message';
-const createdTimestamp = '2018-12-02T20:23:43';
-const finishedTimestamp = '2018-12-02T20:24:43';
-
-const mockInProgressEvent = eventFactory.build({
-  status: 'started',
-  created: createdTimestamp,
-  duration: 0,
-});
-const mockFinishedEvent = eventFactory.build({
-  status: 'finished',
-  duration: 60,
-  created: createdTimestamp,
-});
-
-const inProgressEventProps: RowProps = {
+const message = 'this is a message.';
+const props: RowProps = {
   action: 'linode_boot',
-  message: inProgressMessage,
+  message,
   type: 'linode',
   username: null,
-  timestamp: getEventTimestamp(mockInProgressEvent),
-};
-
-const finishedEventProps: RowProps = {
-  action: 'linode_boot',
-  message: finishedMessage,
-  type: 'linode',
-  username: null,
-  timestamp: getEventTimestamp(mockFinishedEvent),
+  timestamp: DateTime.now(),
 };
 
 describe('EventRow component', () => {
   it('should render an event with a message', () => {
     const { getByText } = renderWithTheme(
-      wrapWithTableBody(<Row {...inProgressEventProps} />)
+      wrapWithTableBody(<Row {...props} />)
     );
 
-    expect(getByText(inProgressMessage)).toBeInTheDocument();
+    expect(getByText(message)).toBeInTheDocument();
   });
 
   it("shouldn't render events without a message", () => {
-    const emptyMessageProps = { ...inProgressEventProps, message: undefined };
+    const emptyMessageProps = { ...props, message: undefined };
     const { container } = renderWithTheme(
       wrapWithTableBody(<Row {...emptyMessageProps} />)
     );
     expect(container.closest('tr')).toBeNull();
-  });
-
-  // TODO: figure out why table row doesn't display Absolute Date column
-  xit('should render an in-progress event with the original "created" timestamp', () => {
-    const { getByText } = renderWithTheme(
-      wrapWithTableBody(<Row {...finishedEventProps} />)
-    );
-
-    expect(getByText(createdTimestamp)).toBeInTheDocument();
-  });
-
-  // TODO: figure out why table row doesn't display Absolute Date column
-  xit('should render a finished event with a finished timestamp', () => {
-    const { getByText } = renderWithTheme(
-      wrapWithTableBody(<Row {...finishedEventProps} />)
-    );
-
-    expect(getByText(finishedTimestamp)).toBeInTheDocument();
   });
 });
