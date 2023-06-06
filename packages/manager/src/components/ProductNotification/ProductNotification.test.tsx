3bdd1c8927c2f90133ced09313f03f081dc204cd
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-06-05 21:33:50 -0500

    refactor: [M3-6378] - MUI v5 - Components > ProductNotification

---
+++ packages/manager/src/components/ProductNotification/ProductNotification.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+import {
+  ProductNotification,
+  ProductNotificationProps,
+} from './ProductNotification';
+
+describe('ProductNotification', () => {
+  test('renders a notice with the correct severity level and text', () => {
+    const props: ProductNotificationProps = {
+      severity: 'critical',
+      text: 'This is a critical notification',
+    };
+
+    const { getByText, container } = renderWithTheme(
+      <ProductNotification {...props} />
+    );
+
+    const noticeElement = getByText('This is a critical notification');
+    expect(noticeElement).toBeInTheDocument();
+    const noticeRoot = container.firstChild;
+
+    expect(noticeRoot).toHaveClass('error-for-scroll');
+  });
+});
