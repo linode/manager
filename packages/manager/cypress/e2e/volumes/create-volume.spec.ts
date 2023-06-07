f908f675c2e5aea355eac874ffd20247edc4409e
ecarrill <ecarrill@akamai.com>
2023-06-07 00:45:41 -0700

    refactor: [M3-6063] - Fix failing test

--- packages/manager/cypress/e2e/volumes/create-volume.spec.ts
+++ packages/manager/cypress/e2e/volumes/create-volume.spec.ts
@@ -32,7 +32,8 @@ describe('volume create flow', () => {
 
     interceptCreateVolume().as('createVolume');
     cy.visitWithLogin('/volumes/create', {
-      localStorageOverrides: pageSizeOverride,
+      // localStorageOverrides: pageSizeOverride,
+      localStorageOverrides: undefined,
     });
 
     // Fill out and submit volume create form.
@@ -82,7 +83,8 @@ describe('volume create flow', () => {
     cy.defer(createLinode(linodeRequest)).then((linode) => {
       interceptCreateVolume().as('createVolume');
       cy.visitWithLogin('/volumes/create', {
-        localStorageOverrides: pageSizeOverride,
+        // localStorageOverrides: pageSizeOverride,
+        localStorageOverrides: undefined,
       });
 
       // Fill out and submit volume create form.
@@ -138,7 +140,8 @@ describe('volume create flow', () => {
       };
 
       cy.visitWithLogin(`/linodes/${linode.id}/storage`, {
-        localStorageOverrides: pageSizeOverride,
+        // localStorageOverrides: pageSizeOverride,
+        localStorageOverrides: undefined,
       });
 
       // Click "Create Volume" button, fill out and submit volume create drawer form.
@@ -167,7 +170,8 @@ describe('volume create flow', () => {
 
       // Confirm that volume is listed on landing page with expected configuration.
       cy.visitWithLogin('/volumes', {
-        localStorageOverrides: pageSizeOverride,
+        // localStorageOverrides: pageSizeOverride,
+        localStorageOverrides: undefined,
       });
       cy.findByText(volume.label)
         .closest('tr')
