76f23d14d2a26b93079a1a88270287863fc93641
Banks Nussman <banks@nussman.us>
2023-06-07 11:43:36 -0400

    initial refactor

--- packages/manager/src/features/TopMenu/AddNewMenu/AddNewMenu.test.tsx
+++ packages/manager/src/features/TopMenu/AddNewMenu/AddNewMenu.test.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import LinodeThemeWrapper from 'src/LinodeThemeWrapper';
 import { renderWithTheme } from 'src/utilities/testHelpers';
-import { fireEvent } from '@testing-library/react';
 import { AddNewMenu } from './AddNewMenu';
 
 describe('AddNewMenu', () => {
@@ -11,17 +10,5 @@ describe('AddNewMenu', () => {
         <AddNewMenu />
       </LinodeThemeWrapper>
     );
-  });
-
-  it('should have an entry for database', () => {
-    const { getByText } = renderWithTheme(
-      <LinodeThemeWrapper>
-        <AddNewMenu />
-      </LinodeThemeWrapper>,
-      { flags: { databases: true } }
-    );
-    const createButton = getByText('Create');
-    fireEvent.click(createButton);
-    expect(getByText('Database')).not.toBe(undefined);
   });
 });
