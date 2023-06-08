eac865478518348af9e6cc989b02a2fa0fca2de0
Banks Nussman <banks@nussman.us>
2023-06-08 11:39:12 -0400

    update comments

--- packages/manager/src/utilities/queryParams.ts
+++ packages/manager/src/utilities/queryParams.ts
@@ -1,9 +1,15 @@
 /**
- * Parses a string of key/value pairs starting with ? and
- * separated by '&', with the key and value separated by '='
+ * Parses a query string (aka search string) and returns its values
+ * as key value pairs.
+ *
+ * A leading '?', if present, is ignored.
  *
  * @param queryString The search string aka the query string to parse. This must ONLY be the query string and not the entire URL.
  * @returns An object of the parsed key/value pairs
+ *
+ * @example
+ * getQueryParamsFromQueryString('?query=false&this=that')
+ * // { query: 'false', this: 'that' }
  */
 export const getQueryParamsFromQueryString = (queryString: string) =>
   Object.fromEntries(new URLSearchParams(queryString));
@@ -14,6 +20,10 @@ export const getQueryParamsFromQueryString = (queryString: string) =>
  * @param paramName The name or key of the paramater you want the value of
  * @param defaultValue an optional default to fallback to if the query param is not found
  * @returns the value of the query paramater you requested
+ *
+ * @example
+ * getQueryParamFromQueryString('?query=false&this=that', 'this')
+ * // "that"
  */
 export const getQueryParamFromQueryString = (
   queryString: string,
