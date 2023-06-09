702df9aad337f3c871863333ae976be83f5ad1e0
ecarrill <ecarrill@akamai.com>
2023-06-09 07:09:20 -0700

    refactor: [M3-6357] - Add labels and export props

--- packages/manager/src/components/LinodeCLIModal/LinodeCLIModal.tsx
+++ packages/manager/src/components/LinodeCLIModal/LinodeCLIModal.tsx
@@ -4,37 +4,37 @@ import { Dialog } from 'src/components/Dialog/Dialog';
 import { sendCLIClickEvent } from 'src/utilities/ga';
 import { styled } from '@mui/material/styles';
 
-interface Props {
+export interface ImageUploadSuccessDialogProps {
   isOpen: boolean;
   onClose: () => void;
   command: string;
   analyticsKey?: string;
 }
 
-export type CombinedProps = Props;
+export const LinodeCLIModal = React.memo(
+  (props: ImageUploadSuccessDialogProps) => {
+    const { isOpen, onClose, command, analyticsKey } = props;
 
-export const LinodeCLIModal = React.memo((props: CombinedProps) => {
-  const { isOpen, onClose, command, analyticsKey } = props;
-
-  return (
-    <StyledLinodeCLIModal
-      title="Upload Image with the Linode CLI"
-      open={isOpen}
-      onClose={onClose}
-      fullWidth
-    >
-      <StyledCommandDisplay>
-        <StyledCLIText>{command}</StyledCLIText>{' '}
-        <StyledCopyTooltip
-          text={command}
-          onClickCallback={
-            analyticsKey ? () => sendCLIClickEvent(analyticsKey) : undefined
-          }
-        />
-      </StyledCommandDisplay>
-    </StyledLinodeCLIModal>
-  );
-});
+    return (
+      <StyledLinodeCLIModal
+        title="Upload Image with the Linode CLI"
+        open={isOpen}
+        onClose={onClose}
+        fullWidth
+      >
+        <StyledCommandDisplay>
+          <StyledCLIText>{command}</StyledCLIText>{' '}
+          <StyledCopyTooltip
+            text={command}
+            onClickCallback={
+              analyticsKey ? () => sendCLIClickEvent(analyticsKey) : undefined
+            }
+          />
+        </StyledCommandDisplay>
+      </StyledLinodeCLIModal>
+    );
+  }
+);
 
 const StyledLinodeCLIModal = styled(Dialog, {
   label: 'StyledLinodeCLIModal',
@@ -46,7 +46,9 @@ const StyledLinodeCLIModal = styled(Dialog, {
   },
 }));
 
-const StyledCommandDisplay = styled('div')(({ theme }) => ({
+const StyledCommandDisplay = styled('div', {
+  label: 'StyledCommandDisplay',
+})(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
@@ -62,14 +64,18 @@ const StyledCommandDisplay = styled('div')(({ theme }) => ({
   width: '100%',
 }));
 
-const StyledCLIText = styled('div')(() => ({
+const StyledCLIText = styled('div', {
+  label: 'StyledCLIText',
+})(() => ({
   overflowY: 'hidden', // For Edge
   overflowX: 'auto',
   height: '1rem',
   paddingRight: 15,
 }));
 
-const StyledCopyTooltip = styled(CopyTooltip)(() => ({
+const StyledCopyTooltip = styled(CopyTooltip, {
+  label: 'StyledCopyTooltip',
+})(() => ({
   display: 'flex',
   '& svg': {
     width: '1em',
