f4b8ddee470fa8516f2a37eee787078404bef2d3
Dajahi Wiley <dwiley@linode.com>
2023-06-09 17:31:20 -0400

    styled() API and u pdated code conventions for ScriptCode.tsx

--- packages/manager/src/components/ScriptCode/ScriptCode.tsx
+++ packages/manager/src/components/ScriptCode/ScriptCode.tsx
@@ -1,78 +1,71 @@
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
-import { makeStyles } from '@mui/styles';
-import { Theme } from '@mui/material/styles';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    maxWidth: '100%',
-    overflow: 'auto',
-    border: `1px solid ${theme.color.grey2}`,
-  },
-  table: {
-    width: '100%',
-    backgroundColor: theme.color.white,
-    borderCollapse: 'collapse',
-    tableLayout: 'fixed',
-  },
-  numberCell: {
-    backgroundColor: theme.color.grey2,
-    paddingLeft: theme.spacing(0.5),
-    paddingRight: theme.spacing(0.5),
-    paddingTop: `calc(${theme.spacing(1)} / 4)`,
-    paddingBottom: `calc(${theme.spacing(1)} / 4)`,
-    fontSize: 14,
-    textAlign: 'center',
-    color: theme.color.headline,
-    userSelect: 'none',
-    width: 35,
-  },
-  codeCell: {
-    paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1),
-  },
-  code: {
-    fontSize: '1em',
-    margin: 0,
-    color: theme.color.headline,
-    whiteSpace: 'pre-wrap',
-    width: '100%',
-  },
-}));
-
-export interface Props {
+interface Props {
   script: string;
 }
 
-type PropsWithStyles = Props;
-
-export const ScriptCode: React.FC<PropsWithStyles> = (props) => {
-  const classes = useStyles();
-
+export const ScriptCode = (props: Props) => {
   const { script } = props;
 
   return (
-    <div className={classes.container}>
-      <table className={classes.table}>
+    <StyledContainer>
+      <StyledTable>
         <tbody data-qa-script-code>
           {script.split('\n').map((line, counter) => (
             <tr key={'scriptCodeLine' + counter}>
-              <td className={classes.numberCell}>{counter + 1}</td>
-              <td className={classes.codeCell}>
-                <pre className={classes.code} data-qa-script>
-                  {line}
-                </pre>
-              </td>
+              <StyledNumberCell>{counter + 1}</StyledNumberCell>
+              <StyledCodeCell>
+                <StyledPre data-qa-script>{line}</StyledPre>
+              </StyledCodeCell>
             </tr>
           ))}
           {/* Empty row at the end */}
           <tr>
-            <td className={classes.numberCell}>&nbsp;</td>
-            <td className={classes.codeCell} />
+            <StyledNumberCell>&nbsp;</StyledNumberCell>
+            <StyledCodeCell />
           </tr>
         </tbody>
-      </table>
-    </div>
+      </StyledTable>
+    </StyledContainer>
   );
 };
 
-export default ScriptCode;
+const StyledContainer = styled('div')(({ theme }) => ({
+  maxWidth: '100%',
+  overflow: 'auto',
+  border: `1px solid ${theme.color.grey2}`,
+}));
+
+const StyledTable = styled('table')(({ theme }) => ({
+  width: '100%',
+  backgroundColor: theme.color.white,
+  borderCollapse: 'collapse',
+  tableLayout: 'fixed',
+}));
+
+const StyledPre = styled('pre')(({ theme }) => ({
+  fontSize: '1em',
+  margin: 0,
+  color: theme.color.headline,
+  whiteSpace: 'pre-wrap',
+  width: '100%',
+}));
+
+const StyledNumberCell = styled('td')(({ theme }) => ({
+  backgroundColor: theme.color.grey2,
+  paddingLeft: theme.spacing(0.5),
+  paddingRight: theme.spacing(0.5),
+  paddingTop: `calc(${theme.spacing(1)} / 4)`,
+  paddingBottom: `calc(${theme.spacing(1)} / 4)`,
+  fontSize: 14,
+  textAlign: 'center',
+  color: theme.color.headline,
+  userSelect: 'none',
+  width: 35,
+}));
+
+const StyledCodeCell = styled('td')(({ theme }) => ({
+  paddingLeft: theme.spacing(1),
+  paddingRight: theme.spacing(1),
+}));
