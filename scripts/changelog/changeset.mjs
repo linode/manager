0a542ff0f2d5dc308d7fc4a63c5fb867469adb5d
Alban Bailly <130582365+abailly-akamai@users.noreply.github.com>
2023-06-14 10:31:13 -0400

    Chore: [M3-6667]: Support changesets and changelog generation for all packages (#9221)
    * Chore: [M3-6667]: initial commit - move files around
    
    * Chore: [M3-6667]: multi packages changesets
    
    * Chore: [M3-6667]: multi packages changelog generation
    
    * Chore: [M3-6667]: delete changesets fix
    
    * Chore: [M3-6667]: cleanup and icons
    
    * Chore: [M3-6667]: reset entries within loop
    
    * Chore: [M3-6667]: Uodate contributing doc

---
+++ scripts/changelog/changeset.mjs
@@ -0,0 +1,118 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+import { execSync } from "child_process";
+import { getPullRequestId } from "./utils/getPullRequestId.mjs";
+import { promisify } from "util";
+import { logger } from "./utils/logger.mjs";
+import {
+  changelogPath,
+  changesetDirectory,
+  CHANGESET_TYPES,
+  PACKAGES,
+} from "./utils/constants.mjs";
+
+const writeFileAsync = promisify(fs.writeFile);
+
+async function generateChangeset() {
+  /**
+   * Check if the "gh" command-line tool is installed.
+   */
+  try {
+    execSync("gh version");
+  } catch (error) {
+    logger.error({
+      message: 'Error: The "gh" command-line tool is not installed.',
+      info:
+        "Please install it from https://github.com/cli/cli#installation\nand sign in with your GitHub account.",
+    });
+  }
+
+  /**
+   * Get the pull request number for the current branch.
+   * This will fail if the current branch is not a pull request and use will get a message to open a pull request.
+   */
+  const pullRequestId = await getPullRequestId();
+
+  /**
+   * Prompt the user for the linode package, type of change, a description and a commit option.
+   */
+  const { linodePackage } = await inquirer.prompt([
+    {
+      type: "list",
+      prefix: `📦 Linode Package`,
+      name: "linodePackage",
+      message: "\nWhat package is this changeset for? (default: manager)",
+      choices: PACKAGES,
+      default: "manager",
+    },
+  ]);
+  const { type } = await inquirer.prompt([
+    {
+      type: "list",
+      prefix: `🆕 Semver`,
+      name: "type",
+      message: "\nWhat type of change is this?",
+      choices: CHANGESET_TYPES,
+    },
+  ]);
+  const { description } = await inquirer.prompt([
+    {
+      type: "input",
+      prefix: `📝 Description`,
+      name: "description",
+      message: "\nDescribe the change (don't include the PR number):",
+    },
+  ]);
+  const { commit } = await inquirer.prompt([
+    {
+      type: "confirm",
+      prefix: `✅ Commit`,
+      name: "commit",
+      message:
+        "\nDo you want to commit the changeset file? (Y or enter for yes)",
+      default: true,
+    },
+  ]);
+
+  const prLink = `https://github.com/linode/manager/pull/${pullRequestId}`;
+  const changesetPath = changesetDirectory(linodePackage);
+  const changesetFile = `${changesetPath}/pr-${pullRequestId}-${type
+    .toLowerCase()
+    .replace(/\s/g, "-")}-${Date.now()}.md`;
+  const changesetContent = `---\n"@linode/${linodePackage}": ${type}\n---\n\n${description} ([#${pullRequestId}](${prLink}))\n`;
+
+  /**
+   * Create the changeset file.
+   */
+  try {
+    await writeFileAsync(changesetFile, changesetContent, {
+      encoding: "utf-8",
+    });
+    logger.success({ message: "🚀 Changeset created!", info: changesetFile });
+  } catch (error) {
+    logger.error({ message: error });
+  }
+
+  /**
+   * Commit file with generic message if user opts in.
+   */
+  if (!commit) {
+    return;
+  }
+
+  try {
+    const addCmd = `git add ${changesetFile}`;
+    const commitCmd = `git commit -m "Added changeset: ${description}"`;
+    execSync(addCmd);
+    execSync(commitCmd);
+
+    logger.success({ message: "✅ Changeset committed!", info: changesetFile });
+  } catch (error) {
+    logger.error({ message: error });
+  }
+}
+
+/**
+ * Run the script
+ */
+generateChangeset();
