42aab3bbbcba59271c0812cef38417dd0dd12093
Cassie Liu <cliu@akamai.com>
2023-06-14 17:14:22 -0400

    M3-6506: Add StackScript Landing Page Integration Tests

--- packages/manager/cypress/support/intercepts/stackscripts.ts
+++ packages/manager/cypress/support/intercepts/stackscripts.ts
@@ -2,7 +2,9 @@
  * @file Cypress intercepts and mocks for Cloud Manager StackScript operations.
  */
 
+import type { StackScript } from '@linode/api-v4/types';
 import { apiMatcher } from 'support/util/intercepts';
+import { paginateResponse } from 'support/util/paginate';
 
 /**
  * Intercepts GET request to list StackScripts.
@@ -11,6 +13,23 @@ import { apiMatcher } from 'support/util/intercepts';
  */
 export const interceptGetStackScripts = (): Cypress.Chainable<null> => {
   return cy.intercept('GET', apiMatcher('linode/stackscripts*'));
+};
+
+/**
+ * Intercepts GET request to mock StackScript data.
+ *
+ * @param stackScripts - an array of mock StackScript objects
+ *
+ * @returns Cypress chainable.
+ */
+export const mockGetStackScripts = (
+  stackScripts: StackScript[]
+): Cypress.Chainable<null> => {
+  return cy.intercept(
+    'GET',
+    apiMatcher('linode/stackscripts*'),
+    paginateResponse(stackScripts)
+  );
 };
 
 /**
