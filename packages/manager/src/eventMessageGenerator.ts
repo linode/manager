2f574b8954200bda764c037256a1c72efa52b4d2
Hussain Khalil <hkhalil@akamai.com>
2023-06-16 16:57:00 -0400

    Escape regex

--- packages/manager/src/eventMessageGenerator.ts
+++ packages/manager/src/eventMessageGenerator.ts
@@ -7,6 +7,7 @@ import {
   formatEventWithUsername,
   formatEventWithAppendedText,
 } from './features/Events/Event.helpers';
+import { escapeRegExp } from './utilities/escapeRegExp';
 
 type EventMessageCreator = (e: Event) => string;
 
@@ -873,7 +874,7 @@ export function applyLinking(event: Event, message: string) {
 
   if (event.entity && entityLinkTarget) {
     const label = event.entity.label;
-    const nonTickedLabels = new RegExp(`(?<!\`)${label}`, 'g');
+    const nonTickedLabels = new RegExp(`(?<!\`)${escapeRegExp(label)}`, 'g');
 
     newMessage = newMessage.replace(
       nonTickedLabels,
