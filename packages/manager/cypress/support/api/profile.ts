f4f4dda02b92547e4ef03e3f88544febfda4f374
cliu-akamai <126020611+cliu-akamai@users.noreply.github.com>
2023-06-22 12:10:25 -0400

    test: [M3-6549] - Add Cypress Integration tests for OAuth Apps (#9163)
    * M3-6549: Add Cypress Integration tests for OAuth Apps
    
    * Fixed comments

---
+++ packages/manager/cypress/support/api/profile.ts
@@ -0,0 +1,30 @@
+import { isTestLabel } from 'support/api/common';
+import {
+  getOAuthClients,
+  OAuthClient,
+  deleteOAuthClient,
+} from '@linode/api-v4';
+import { depaginate } from 'support/util/paginate';
+import { pageSize } from 'support/constants/api';
+
+/**
+ * Deletes all oauth apps which are prefixed with the test entity prefix.
+ *
+ * @returns Promise that resolves when oauth apps have been deleted.
+ */
+export const deleteAllTestOAuthApps = async (): Promise<void> => {
+  const oauthApps = await depaginate<OAuthClient>((page: number) =>
+    getOAuthClients({ page_size: pageSize, page })
+  );
+
+  console.log(`oauth apps: ${JSON.stringify(oauthApps)}`);
+
+  const deletionPromises = oauthApps
+    .filter((oauthApp: OAuthClient) => isTestLabel(oauthApp.label))
+    .map(async (oauthApp: OAuthClient) => {
+      console.log(`deleting ${oauthApp.label}`);
+      await deleteOAuthClient(oauthApp.id);
+    });
+
+  await Promise.all(deletionPromises);
+};
