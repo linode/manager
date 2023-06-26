4bc2cf5692a90a9a25b739b1df06bee1bae4f8a9
Alban Bailly <abailly@akamai.com>
2023-06-26 13:41:54 -0400

    Refactor: [M3-6522-link] argsTypes

--- packages/manager/src/components/Link.tsx
+++ packages/manager/src/components/Link.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react';
-import { Link as RouterLink, LinkProps } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import type { LinkProps } from 'react-router-dom';
 
 const isExternal = (href: string) => {
   return href.match(/http/) || href.match(/mailto/);
 };
 
-export const Link = (props: LinkProps) => {
+interface Props extends LinkProps {
+  /**
+   * The link's destination. If the value contains `http` or `mailto`, it will be considered an external link and open in a new window.
+   */
+  to: string;
+  /**
+   * The clickable text or content.
+   */
+  children?: React.ReactNode;
+  /**
+   * When `true`, clicking the link will replace the current entry in the history stack instead of adding a new one.
+   * @default false
+   * @see https://reactrouter.com/web/api/Link/replace-bool
+   */
+  replace?: boolean;
+  /**
+   * Optional CSS class names that are applied to the component.
+   */
+  className?: string;
+  /**
+   * A function that will be called onClick.
+   */
+  onClick?: (e: React.SyntheticEvent<HTMLElement>) => void;
+}
+
+export const Link = (props: Props) => {
   const isLinkExternal = isExternal(props.to as string);
 
   return isLinkExternal ? (
