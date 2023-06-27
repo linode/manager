2399d9b7ce0b00e6116add1ad65a62e4f369e01b
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2023-06-26 23:37:31 -0400

    refactor: [M3-6354] - MUI v5 Migration - `Components > LandingLoading` (#9282)
    Co-authored-by: Jaalah Ramos <jaalah.ramos@gmail.com>

--- packages/manager/src/features/Longview/LongviewDetail/DetailTabs/Disks/Disks.tsx
+++ packages/manager/src/features/Longview/LongviewDetail/DetailTabs/Disks/Disks.tsx
@@ -6,7 +6,7 @@ import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import { ErrorState } from 'src/components/ErrorState/ErrorState';
-import LandingLoading from 'src/components/LandingLoading';
+import { LandingLoading } from 'src/components/LandingLoading/LandingLoading';
 import { Placeholder } from 'src/components/Placeholder/Placeholder';
 import { WithStartAndEnd } from '../../../request.types';
 import TimeRangeSelect from '../../../shared/TimeRangeSelect';
