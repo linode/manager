171d9339cf52b161a089e3b699aa1a66761fc545
Alban Bailly <abailly@akamai.com>
2023-06-27 12:12:04 -0400

    Refactor: [M3-6522-copy-tooltip] Styled component and v7 story

--- packages/manager/src/components/CopyTooltip/CopyTooltip.tsx
+++ packages/manager/src/components/CopyTooltip/CopyTooltip.tsx
@@ -1,57 +1,38 @@
-import { Theme } from '@mui/material/styles';
-import copy from 'copy-to-clipboard';
 import * as React from 'react';
+import copy from 'copy-to-clipboard';
 import FileCopy from 'src/assets/icons/copy.svg';
 import ToolTip from 'src/components/core/Tooltip';
-import { makeStyles } from 'tss-react/mui';
+import { isPropValid } from 'src/utilities/isPropValid';
+import { styled } from '@mui/material/styles';
 
-interface Props {
-  text: string;
+export interface CopyTooltipProps {
+  /**
+   * Additional classes to be applied to the root element.
+   */
   className?: string;
+  /**
+   * If true, the text will be displayed in the tooltip.
+   * @default false
+   */
   copyableText?: boolean;
+  /**
+   * Callback to be executed when the icon is clicked.
+   */
   onClickCallback?: () => void;
+  /**
+   * The text to be copied to the clipboard.
+   */
+  text: string;
 }
 
-const useStyles = makeStyles()((theme: Theme) => ({
-  root: {
-    position: 'relative',
-    padding: 4,
-    backgroundColor: 'transparent',
-    transition: theme.transitions.create(['background-color']),
-    borderRadius: 4,
-    border: 'none',
-    cursor: 'pointer',
-    color: theme.color.grey1,
-    '& svg': {
-      transition: theme.transitions.create(['color']),
-      color: theme.color.grey1,
-      margin: 0,
-      position: 'relative',
-      width: 20,
-      height: 20,
-    },
-    '& svg:hover': {
-      color: theme.palette.primary.main,
-    },
-  },
-  flex: {
-    display: 'flex',
-    width: 'auto !important',
-  },
-  copyableTextBtn: {
-    padding: 0,
-    backgroundColor: 'transparent',
-    border: 'none',
-    cursor: 'pointer',
-    font: 'inherit',
-    color: theme.palette.text.primary,
-  },
-}));
+/**
+ *
+ * Content that is to be copied should be displayed before (to the left of) the clipboard icon.<br />
+ * If horizontal space is limited, truncate the content.
+ */
 
-export const CopyTooltip = (props: Props) => {
-  const { classes, cx } = useStyles();
+export const CopyTooltip = (props: CopyTooltipProps) => {
   const [copied, setCopied] = React.useState<boolean>(false);
-
   const { text, className, copyableText, onClickCallback } = props;
 
   const handleIconClick = () => {
@@ -65,18 +46,49 @@ export const CopyTooltip = (props: Props) => {
 
   return (
     <ToolTip title={copied ? 'Copied!' : 'Copy'} placement="top" data-qa-copied>
-      <button
+      <StyledCopyTooltipButton
         aria-label={`Copy ${text} to clipboard`}
         name={text}
         type="button"
         onClick={handleIconClick}
-        className={cx(classes.root, className, {
-          [classes.copyableTextBtn]: copyableText,
-        })}
+        className={className}
         data-qa-copy-btn
       >
         {copyableText ? text : <FileCopy />}
-      </button>
+      </StyledCopyTooltipButton>
     </ToolTip>
   );
 };
+
+const StyledCopyTooltipButton = styled('button', {
+  label: 'StyledCopyTooltipButton',
+  shouldForwardProp: (prop) => isPropValid(['copyableText', 'text'], prop),
+})<Omit<CopyTooltipProps, 'text'>>(({ theme, ...props }) => ({
+  position: 'relative',
+  padding: 4,
+  backgroundColor: 'transparent',
+  transition: theme.transitions.create(['background-color']),
+  borderRadius: 4,
+  border: 'none',
+  cursor: 'pointer',
+  color: theme.color.grey1,
+  '& svg': {
+    transition: theme.transitions.create(['color']),
+    color: theme.color.grey1,
+    margin: 0,
+    position: 'relative',
+    width: 20,
+    height: 20,
+  },
+  '& svg:hover': {
+    color: theme.palette.primary.main,
+  },
+  ...(props.copyableText && {
+    padding: 0,
+    backgroundColor: 'transparent',
+    border: 'none',
+    cursor: 'pointer',
+    font: 'inherit',
+    color: theme.palette.text.primary,
+  }),
+}));
