369b26211dac2f2e3561668d5516c97688d83233
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-06-29 14:57:38 -0400

    Review updates

--- packages/manager/src/components/InlineMenuAction/InlineMenuAction.tsx
+++ packages/manager/src/components/InlineMenuAction/InlineMenuAction.tsx
@@ -5,13 +5,21 @@ import { StyledActionButton } from 'src/components/Button/StyledActionButton';
 import { styled } from '@mui/material/styles';
 
 interface InlineMenuActionProps {
+  /** Required action text */
   actionText: string;
+  /** Optional class names */
   className?: string;
+  /** Optional href */
   href?: string;
+  /** Optional disabled */
   disabled?: boolean;
+  /** Optional tooltip text for help icon */
   tooltip?: string;
-  loading?: boolean;
+  /** Optional onClick handler */
   onClick?: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  /** Optional loading state */
+  loading?: boolean;
+  /** Optional tooltip event handler for sending analytics */
   tooltipAnalyticsEvent?: () => void;
 }
 
@@ -33,21 +41,21 @@ export const InlineMenuAction = (props: InlineMenuActionProps) => {
         <span>{actionText}</span>
       </StyledLink>
     );
-  } else {
-    return (
-      <StyledActionButton
-        // TODO: We need to define what buttonType this will be in the future for now 'secondary' works...
-        buttonType="secondary"
-        onClick={onClick}
-        disabled={disabled}
-        loading={loading}
-        tooltipText={tooltip}
-        tooltipAnalyticsEvent={tooltipAnalyticsEvent}
-      >
-        {actionText}
-      </StyledActionButton>
-    );
   }
+
+  return (
+    <StyledActionButton
+      // TODO: We need to define what buttonType this will be in the future for now 'secondary' works...
+      buttonType="secondary"
+      onClick={onClick}
+      disabled={disabled}
+      loading={loading}
+      tooltipText={tooltip}
+      tooltipAnalyticsEvent={tooltipAnalyticsEvent}
+    >
+      {actionText}
+    </StyledActionButton>
+  );
 };
 
 const StyledLink = styled(Link)(() => ({
