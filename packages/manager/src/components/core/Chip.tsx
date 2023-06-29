5def3103b5bf37b55372c58a8f9a9edd03ca74cd
Alban Bailly <abailly@akamai.com>
2023-06-29 17:00:27 -0400

    Chore: [M3-6527]: Remove fix present in other PR

--- packages/manager/src/components/core/Chip.tsx
+++ packages/manager/src/components/core/Chip.tsx
@@ -37,9 +37,6 @@ export const Chip = ({
 
 const StyledChip = styled(_Chip, {
   label: 'StyledChip',
-  shouldForwardProp(propName) {
-    return propName !== 'inTable' && propName !== 'outlineColor';
-  },
 })<ChipProps>(({ theme, ...props }) => ({
   ...(props.inTable && {
     marginTop: 0,
