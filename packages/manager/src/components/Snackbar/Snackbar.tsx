b69653e382deca5139f52a930de2aa4e7c9c6455
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-07-03 12:15:57 -0400

    Revert back to v2

--- packages/manager/src/components/Snackbar/Snackbar.tsx
+++ packages/manager/src/components/Snackbar/Snackbar.tsx
@@ -1,91 +1,16 @@
+import { SnackbarProvider, SnackbarProviderProps } from 'notistack';
 import * as React from 'react';
+import { makeStyles } from 'tss-react/mui';
+import { Theme } from '@mui/material/styles';
 import CloseSnackbar from './CloseSnackbar';
-import {
-  // MaterialDesignContent,
-  SnackbarProvider,
-  SnackbarProviderProps,
-} from 'notistack';
-import { styled } from '@mui/material/styles';
 
-// const StyledVariants = styled(MaterialDesignContent)(({ theme }) => ({
-//   '&.notistack-MuiContent': {
-//     backgroundColor: `${theme.bg.white}`,
-//     color: theme.palette.text.primary,
-//     fontSize: '0.875rem',
-//     [theme.breakpoints.down('md')]: {
-//       '& .SnackbarItem-contentRoot': {
-//         flexWrap: 'nowrap',
-//       },
-//       '& .SnackbarItem-message': {
-//         display: 'unset',
-//       },
-//     },
-//     [theme.breakpoints.down('sm')]: {
-//       '& .SnackbarItem-action': {
-//         paddingLeft: 0,
-//       },
-//     },
-//   },
-//   '&.notistack-MuiContent-error': {
-//     borderLeft: `6px solid ${theme.palette.error.dark}`,
-//   },
-//   '&.notistack-MuiContent-info': {
-//     borderLeft: `6px solid ${theme.palette.primary.main}`,
-//   },
-//   '&.notistack-MuiContent-success': {
-//     borderLeft: `6px solid ${theme.color.green}`,
-//   },
-//   '&.notistack-MuiContent-warning': {
-//     borderLeft: `6px solid ${theme.palette.warning.dark}`,
-//   },
-// }));
-
-// interface CustomSnackbarProps extends SnackbarProviderProps {
-//   'data-qa-toast'?: boolean;
-// }
-
-export const Snackbar = (props: SnackbarProviderProps) => {
-  const { children, ...rest } = props;
-
-  /**
-   * This pattern is taken from the Notistack docs:
-   * https://iamhosseindhv.com/notistack/demos#action-for-all-snackbars
-   */
-  const notistackRef: React.Ref<SnackbarProvider> = React.createRef();
-  const onClickDismiss = (key: string | number | undefined) => () => {
-    if (notistackRef.current) {
-      notistackRef.current.closeSnackbar(key);
-    }
-  };
-
-  return (
-    <StyledSnackbarProvider
-      {...rest}
-      ref={notistackRef}
-      action={(key) => (
-        <CloseSnackbar
-          onClick={onClickDismiss(key)}
-          text="Dismiss Notification"
-        />
-      )}
-      // classes={{
-      //   root: classes.root,
-      //   variantSuccess: classes.success,
-      //   variantError: classes.error,
-      //   variantWarning: classes.warning,
-      //   variantInfo: classes.info,
-      // }}
-    >
-      {children}
-    </StyledSnackbarProvider>
-  );
-};
-
-const StyledSnackbarProvider = styled(SnackbarProvider)(({ theme }) => ({
-  '&.notistack-MuiContent': {
-    backgroundColor: `${theme.bg.white}`,
-    color: theme.palette.text.primary,
-    fontSize: '0.875rem',
+const useStyles = makeStyles()((theme: Theme) => ({
+  root: {
+    '& div': {
+      backgroundColor: `${theme.bg.white} !important`,
+      color: theme.palette.text.primary,
+      fontSize: '0.875rem',
+    },
     [theme.breakpoints.down('md')]: {
       '& .SnackbarItem-contentRoot': {
         flexWrap: 'nowrap',
@@ -100,16 +25,54 @@ const StyledSnackbarProvider = styled(SnackbarProvider)(({ theme }) => ({
       },
     },
   },
-  '&.notistack-MuiContent-error': {
-    borderLeft: `6px solid ${theme.palette.error.dark}`,
-  },
-  '&.notistack-MuiContent-info': {
+  info: {
     borderLeft: `6px solid ${theme.palette.primary.main}`,
   },
-  '&.notistack-MuiContent-success': {
+  success: {
     borderLeft: `6px solid ${theme.color.green}`,
   },
-  '&.notistack-MuiContent-warning': {
+  error: {
+    borderLeft: `6px solid ${theme.palette.error.dark}`,
+  },
+  warning: {
     borderLeft: `6px solid ${theme.palette.warning.dark}`,
   },
 }));
+
+export const Snackbar = (props: SnackbarProviderProps) => {
+  const { classes } = useStyles();
+  /**
+   * This pattern is taken from the Notistack docs:
+   * https://iamhosseindhv.com/notistack/demos#action-for-all-snackbars
+   */
+  const notistackRef: React.Ref<SnackbarProvider> = React.createRef();
+  const onClickDismiss = (key: string | number | undefined) => () => {
+    if (notistackRef.current) {
+      notistackRef.current.closeSnackbar(key);
+    }
+  };
+
+  const { children, ...rest } = props;
+
+  return (
+    <SnackbarProvider
+      ref={notistackRef}
+      {...rest}
+      classes={{
+        root: classes.root,
+        variantSuccess: classes.success,
+        variantError: classes.error,
+        variantWarning: classes.warning,
+        variantInfo: classes.info,
+      }}
+      action={(key) => (
+        <CloseSnackbar
+          onClick={onClickDismiss(key)}
+          text="Dismiss Notification"
+        />
+      )}
+    >
+      {children}
+    </SnackbarProvider>
+  );
+};
