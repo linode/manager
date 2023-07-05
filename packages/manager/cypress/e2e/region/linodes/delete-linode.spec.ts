cd2b6446a4dada16595ba64ad1bfc43590ccb6ce
Joe D'Amore <jdamore@linode.com>
2023-07-05 16:49:14 -0400

    Remove bulk Linode create logic, clean up delete tests

--- packages/manager/cypress/e2e/region/linodes/delete-linode.spec.ts
+++ packages/manager/cypress/e2e/region/linodes/delete-linode.spec.ts
@@ -1,95 +1,66 @@
 import { createLinodeRequestFactory } from '@src/factories';
-import { testRegions, getTestableRegions } from 'support/util/regions';
+import { testRegions } from 'support/util/regions';
 import { randomLabel, randomString } from 'support/util/random';
-import { getLinodes, Region } from '@linode/api-v4';
+import { Region } from '@linode/api-v4';
 import { createLinode } from '@linode/api-v4';
-import { poll } from 'support/util/polling';
-import { depaginate } from 'support/util/paginate';
 import type { Linode } from '@linode/api-v4';
 import { ui } from 'support/ui';
 import { authenticate } from 'support/api/authentication';
-import { resolveInBatches } from 'support/util/promises';
-
-/**
- * Creates a Linode for each testable region, and waits for each to boot.
- */
-const createLinodesAndWait = async () => {
-  const createLinodePromiseGenerators = getTestableRegions()
-    .map((region: Region) => {
-      const linodeRequest = createLinodeRequestFactory.build({
-        label: randomLabel(),
-        region: region.id,
-        root_password: randomString(32),
-      });
-
-      return () => createLinode(linodeRequest);
-    });
-
-  //const newLinodes = await Promise.all(createLinodePromises);
-  const newLinodes = await resolveInBatches(createLinodePromiseGenerators, 2, 6500);
-  const checkLinodeStatuses = (allLinodes: Linode[]) => {
-    return allLinodes
-      .filter((linode) => newLinodes.some((newLinode) => newLinode.label === linode.label))
-      .every((linode) => linode.status === 'running');
-  };
-
-  await poll<Linode[]>(
-    async () => depaginate((page) => getLinodes({ page })),
-    (linodes: Linode[]) => checkLinodeStatuses(linodes),
-  );
-
-  return newLinodes;
-};
+import {
+  interceptGetLinodeDetails,
+  interceptGetLinodes,
+} from 'support/intercepts/linodes';
 
 authenticate();
 describe('Delete Linodes', () => {
-  // Create Linodes to delete first.
-  let linodes: Linode[];
-  before(() => {
-    const deferOptions = {
-      label: 'creating Linodes to delete',
-      timeout: 180000,
-    };
-    cy.defer(createLinodesAndWait(), deferOptions).then((newLinodes: Linode[]) => {
-      linodes = newLinodes;
-    });
-  });
-
   testRegions('can delete a Linode', (region: Region) => {
-    const linode = linodes.find((aLinode) => aLinode.region === region.id);
-    if (!linode) {
-      throw new Error(`Unable to find created Linode for region ${region.id}`);
-    }
-    cy.visitWithLogin(`/linodes/${linode.id}`);
-    ui.actionMenu
-      .findByTitle(`Action menu for Linode ${linode.label}`)
-      .should('be.visible')
-      .click();
+    const linodeCreatePayload = createLinodeRequestFactory.build({
+      label: randomLabel(),
+      region: region.id,
+      root_password: randomString(32),
+    });
 
-    ui.actionMenuItem
-      .findByTitle('Delete')
-      .should('be.visible')
-      .click();
+    // Create a Linode before navigating to its details page to delete it.
+    cy.defer(
+      createLinode(linodeCreatePayload),
+      `creating Linode in ${region.label}`
+    ).then((linode: Linode) => {
+      interceptGetLinodeDetails(linode.id).as('getLinode');
+      cy.visitWithLogin(`/linodes/${linode.id}`);
+      cy.wait('@getLinode');
 
-    ui.dialog
-      .findByTitle(`Delete ${linode.label}?`)
-      .should('be.visible')
-      .within(() => {
-        cy.findByLabelText('Linode Label')
-          .should('be.visible')
-          .click()
-          .type(linode.label);
+      // Delete Linode via action menu.
+      ui.actionMenu
+        .findByTitle(`Action menu for Linode ${linode.label}`)
+        .should('be.visible')
+        .click();
 
-        ui.buttonGroup
-          .findButtonByTitle('Delete')
-          .should('be.visible')
-          .should('be.enabled')
-          .click();
-      });
+      ui.actionMenuItem.findByTitle('Delete').should('be.visible').click();
 
-    // Cloud currently does not navigate back to landing page.
-    // Remove call to `cy.visitWithLogin()` once redirect is restored.
-    cy.visitWithLogin('/linodes');
-    cy.findByText(linode.label).should('not.exist');
+      // Confirm deletion via type-to-confirm dialog.
+      ui.dialog
+        .findByTitle(`Delete ${linode.label}?`)
+        .should('be.visible')
+        .within(() => {
+          cy.findByLabelText('Linode Label')
+            .should('be.visible')
+            .click()
+            .type(linode.label);
+
+          ui.buttonGroup
+            .findButtonByTitle('Delete')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+        });
+
+      // Confirm that Linode is no longer listed on landing page.
+      // Cloud currently does not navigate back to landing page.
+      // Remove call to `cy.visitWithLogin()` once redirect is restored.
+      interceptGetLinodes().as('getLinodes');
+      cy.visitWithLogin('/linodes');
+      cy.wait('@getLinodes');
+      cy.findByText(linode.label).should('not.exist');
+    });
   });
 });
