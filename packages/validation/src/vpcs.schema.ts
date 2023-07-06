776ecc2f1e24424418413cdaa13167a7176fb826
Dajahi Wiley <dwiley@linode.com>
2023-07-06 10:56:48 -0400

    Feedback

--- packages/validation/src/vpcs.schema.ts
+++ packages/validation/src/vpcs.schema.ts
@@ -4,6 +4,11 @@ const LABEL_MESSAGE = 'VPC label must be between 1 and 64 characters.';
 
 export const createVPCSchema = object({
   label: string()
+    .test(
+      'no two dashes in a row',
+      'Must not contain two dashes in a row',
+      (value) => !value?.includes('--')
+    )
     .required('Label is required')
     .min(1, LABEL_MESSAGE)
     .max(64, LABEL_MESSAGE)
@@ -17,6 +22,18 @@ export const createVPCSchema = object({
 });
 
 export const updateVPCSchema = object({
-  label: string().notRequired().min(3, LABEL_MESSAGE).max(32, LABEL_MESSAGE),
+  label: string()
+    .notRequired()
+    .test(
+      'no two dashes in a row',
+      'Must not contain two dashes in a row',
+      (value) => !value?.includes('--')
+    )
+    .min(1, LABEL_MESSAGE)
+    .max(64, LABEL_MESSAGE)
+    .matches(
+      /[a-zA-Z0-9-]+/,
+      'Must include only ASCII letters, numbers, and dashes'
+    ),
   description: string().notRequired(),
 });
