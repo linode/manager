bacab424a272a6516acbf9646a054759c805404e
cliu-akamai <126020611+cliu-akamai@users.noreply.github.com>
2023-07-07 15:23:52 -0400

    test: [M3-6505] - Add StackScript Update/Delete E2E Tests (#9315)
    * M3-6505: Add StackScript Update/Delete E2E Tests
    
    * Fixed comments

---
+++ packages/manager/cypress/e2e/core/stackscripts/delete-stackscripts.spec.ts
@@ -0,0 +1,102 @@
+import { authenticate } from 'support/api/authentication';
+import { stackScriptFactory } from 'src/factories';
+import {
+  mockDeleteStackScript,
+  mockGetStackScripts,
+} from 'support/intercepts/stackscripts';
+import { ui } from 'support/ui';
+
+authenticate();
+describe('Delete stackscripts', () => {
+  /*
+   * - Deletes the stackscripts.
+   * - Confirms that the stackscript item still exist when cancelling the delete operation.
+   * - Confirms that the stackscript item can be deleted successfully.
+   * - Confirms that "Automate Deployment with StackScripts!" welcome page appears when user has no StackScript.
+   */
+  it('deletes the stackscripts', () => {
+    const stackScripts = stackScriptFactory.buildList(2, {
+      is_public: false,
+    });
+    mockGetStackScripts(stackScripts).as('getStackScripts');
+    cy.visitWithLogin('/stackscripts/account');
+    cy.wait('@getStackScripts');
+
+    // Do nothing when cancelling
+    cy.get(`[aria-label="${stackScripts[0].label}"]`)
+      .closest('tr')
+      .within(() => {
+        ui.actionMenu
+          .findByTitle(`Action menu for StackScript ${stackScripts[0].label}`)
+          .should('be.visible')
+          .click();
+      });
+    ui.actionMenuItem.findByTitle('Delete').should('be.visible').click();
+    ui.dialog
+      .findByTitle(`Delete StackScript ${stackScripts[0].label}?`)
+      .should('be.visible')
+      .within(() => {
+        ui.button.findByTitle('Cancel').should('be.visible').click();
+      });
+
+    cy.findByText(stackScripts[0].label)
+      .should('be.visible')
+      .closest('tr')
+      .within(() => {
+        cy.findByText(stackScripts[0].description).should('be.visible');
+      });
+
+    // The StackScript is deleted successfully.
+    cy.get(`[aria-label="${stackScripts[0].label}"]`)
+      .closest('tr')
+      .within(() => {
+        ui.actionMenu
+          .findByTitle(`Action menu for StackScript ${stackScripts[0].label}`)
+          .should('be.visible')
+          .click();
+      });
+    mockDeleteStackScript(stackScripts[0].id).as('deleteStackScript');
+    mockGetStackScripts([stackScripts[1]]).as('getUpdatedStackScripts');
+    ui.actionMenuItem.findByTitle('Delete').should('be.visible').click();
+    ui.dialog
+      .findByTitle(`Delete StackScript ${stackScripts[0].label}?`)
+      .should('be.visible')
+      .within(() => {
+        ui.button
+          .findByTitle('Delete StackScript')
+          .should('be.visible')
+          .click();
+      });
+    cy.wait('@deleteStackScript');
+    cy.wait('@getUpdatedStackScripts');
+
+    cy.findByText(stackScripts[0].label).should('not.exist');
+
+    // The "Automate Deployment with StackScripts!" welcome page appears when no StackScript exists.
+    cy.get(`[aria-label="${stackScripts[1].label}"]`)
+      .closest('tr')
+      .within(() => {
+        ui.actionMenu
+          .findByTitle(`Action menu for StackScript ${stackScripts[1].label}`)
+          .should('be.visible')
+          .click();
+      });
+    mockDeleteStackScript(stackScripts[1].id).as('deleteStackScript');
+    mockGetStackScripts([]).as('getUpdatedStackScripts');
+    ui.actionMenuItem.findByTitle('Delete').should('be.visible').click();
+    ui.dialog
+      .findByTitle(`Delete StackScript ${stackScripts[1].label}?`)
+      .should('be.visible')
+      .within(() => {
+        ui.button
+          .findByTitle('Delete StackScript')
+          .should('be.visible')
+          .click();
+      });
+    cy.wait('@deleteStackScript');
+    cy.wait('@getUpdatedStackScripts');
+
+    cy.findByText(stackScripts[1].label).should('not.exist');
+    cy.findByText('Automate deployment scripts').should('be.visible');
+  });
+});
--- packages/manager/cypress/support/intercepts/stackscripts.ts
+++ packages/manager/cypress/support/intercepts/stackscripts.ts
@@ -5,6 +5,7 @@
 import type { StackScript } from '@linode/api-v4/types';
 import { apiMatcher } from 'support/util/intercepts';
 import { paginateResponse } from 'support/util/paginate';
+import { makeResponse } from 'support/util/response';
 
 /**
  * Intercepts GET request to list StackScripts.
@@ -33,10 +34,90 @@ export const mockGetStackScripts = (
 };
 
 /**
+ * Intercepts GET request to mock a StackScript.
+ *
+ * @param id - StackScript instance identifier
+ * @param stackscript - a mock StackScript object
+ *
+ * @returns Cypress chainable.
+ */
+export const mockGetStackScript = (
+  id: number,
+  stackscript: StackScript
+): Cypress.Chainable<null> => {
+  return cy.intercept(
+    'GET',
+    apiMatcher(`linode/stackscripts/${id}`),
+    stackscript
+  );
+};
+
+/**
  * Intercepts POST request to create a StackScript.
  *
  * @returns Cypress chainable.
  */
 export const interceptCreateStackScript = (): Cypress.Chainable<null> => {
   return cy.intercept('POST', apiMatcher('linode/stackscripts'));
+};
+
+/**
+ * Mock DELETE request to remove a StackScript.
+ *
+ * @returns Cypress chainable.
+ */
+export const mockDeleteStackScript = (id: number): Cypress.Chainable<null> => {
+  return cy.intercept('DELETE', apiMatcher(`linode/stackscripts/${id}`), {
+    statusCode: 200,
+    body: {},
+  });
+};
+
+/**
+ * Intercept PUT request to update a StackScript.
+ *
+ * @param id - StackScript instance identifier
+ * @param stackscript - a mock StackScript object
+ *
+ * @returns Cypress chainable.
+ */
+export const mockUpdateStackScript = (
+  id: number,
+  stackscript: StackScript
+): Cypress.Chainable<null> => {
+  return cy.intercept(
+    'PUT',
+    apiMatcher(`linode/stackscripts/${id}`),
+    makeResponse(stackscript)
+  );
+};
+
+/**
+ * Intercept PUT request to mock StackScript update error.
+ *
+ * @param id - StackScript instance identifier
+ * @param err_message - the error message if is_err is true
+ *
+ * @returns Cypress chainable.
+ */
+export const mockUpdateStackScriptError = (
+  id: number,
+  err_field: string | null = null,
+  err_message: string | null = null
+): Cypress.Chainable<null> => {
+  return cy.intercept(
+    'PUT',
+    apiMatcher(`linode/stackscripts/${id}`),
+    makeResponse(
+      {
+        errors: [
+          {
+            reason: err_message,
+            field: err_field,
+          },
+        ],
+      },
+      400
+    )
+  );
 };
