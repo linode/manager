0f5178f82e688def24baee7bb6bc9e4c3702886a
Alban Bailly <abailly@akamai.com>
2023-07-07 17:46:24 -0400

    feat: [M3-6781] initial commit of factories

--- packages/api-v4/src/aglb/types.ts
+++ packages/api-v4/src/aglb/types.ts
@@ -27,6 +27,7 @@ type Policy = 'ROUND_ROBIN';
 export interface RoutePayload {
   label: string;
   rules: Rule[];
+  default_target: ServiceTargetPayload;
 }
 
 export interface Route extends RoutePayload {
@@ -38,7 +39,11 @@ export interface EntrypointPayload {
   port: number;
   protocol: Protocol;
   certificate_table: CertificateTable[];
-  routes: RoutePayload[];
+  // TODO: is it how we want to do it?
+  // for updating an entrypoint we want a list of IDs,
+  // but when creating a load balancer with all children it needs the full payload
+  // maybe we should have a different type for each case but for now I'll just use this
+  routes: RoutePayload[] | Route['id'][];
 }
 
 export type CreateEntrypointPayload = Omit<EntrypointPayload, 'routes'>;
@@ -58,13 +63,18 @@ export interface CertificateTable {
 }
 
 export interface Rule {
-  match_condition: Match;
+  match_condition: MatchCondition;
   service_targets: ServiceTargetPayload[];
-  default_target: ServiceTargetPayload;
 }
 
-export interface Match {
+export interface MatchCondition {
+  host: string;
   path: string;
+}
+
+export interface RouteServiceTargetPayload {
+  service_target_name: string;
+  service_target_percentage: number;
 }
 
 export interface ServiceTargetPayload {
@@ -89,4 +99,26 @@ export interface Endpoint {
   port: number;
   capacity: number;
   hard_rate_limit: number;
+}
+
+// I don't know if we gonna need this, but the RoutePayload differs when creating an entrypoint
+// Hopefully this is a mistake in the in-progress API docs and we can consolidate these types
+// For now I care to match the API docs for the factories
+export interface RoutePayload2 {
+  label: string;
+  rules: Rule2[];
+}
+
+export interface Route2 extends RoutePayload2 {
+  id: number;
+}
+
+export interface Rule2 {
+  match_condition: MatchCondition;
+  service_targets: ServiceTargetPayload2[];
+}
+
+export interface ServiceTargetPayload2 {
+  service_target_name: string;
+  service_target_percentage: number;
 }
