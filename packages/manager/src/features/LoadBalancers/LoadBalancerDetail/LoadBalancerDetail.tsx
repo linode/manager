f5c453d463eccbe7b498c02a5431c83e658f63b8
mjac0bs <mjacobs@akamai.com>
2023-07-10 10:03:40 -0700

    Add LoadBalancerDetail

---
+++ packages/manager/src/features/LoadBalancers/LoadBalancerDetail/LoadBalancerDetail.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { DocumentTitleSegment } from 'src/components/DocumentTitle/DocumentTitle';
+
+export const LoadBalancerDetail = () => {
+  return (
+    <>
+      <DocumentTitleSegment segment="Load Balancers" />
+      TODO: AGLB M3-6818, M3-681, M3-6820, M3-6821: Load Balancer Details
+    </>
+  );
+};
