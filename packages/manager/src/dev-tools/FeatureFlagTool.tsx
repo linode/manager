b3f7cfd1578d0c8d0f5960183861c450d754d4a8
Alban Bailly <abailly@akamai.com>
2023-07-13 14:45:10 -0400

    Chore: Enable local storage for MSW feature flags

--- packages/manager/src/dev-tools/FeatureFlagTool.tsx
+++ packages/manager/src/dev-tools/FeatureFlagTool.tsx
@@ -5,6 +5,9 @@ import { Dispatch } from 'src/hooks/types';
 import useFlags from 'src/hooks/useFlags';
 import { setMockFeatureFlags } from 'src/store/mockFeatureFlags';
 import Grid from '@mui/material/Unstable_Grid2';
+import { getStorage, setStorage } from 'src/utilities/storage';
+
+const MOCK_FEATURE_FLAGS_STORAGE_KEY = 'devTools/mock-feature-flags';
 
 const options: { label: string; flag: keyof Flags }[] = [
   { label: 'Metadata', flag: 'metadata' },
@@ -17,11 +20,23 @@ const FeatureFlagTool: React.FC<{}> = () => {
   const dispatch: Dispatch = useDispatch();
   const flags = useFlags();
 
+  React.useEffect(() => {
+    const storedFlags = getStorage(MOCK_FEATURE_FLAGS_STORAGE_KEY);
+    if (storedFlags) {
+      dispatch(setMockFeatureFlags(storedFlags));
+    }
+  }, [dispatch]);
+
   const handleCheck = (
     e: React.ChangeEvent<HTMLInputElement>,
     flag: keyof FlagSet
   ) => {
     dispatch(setMockFeatureFlags({ [flag]: e.target.checked }));
+    const updatedFlags = JSON.stringify({
+      ...getStorage(MOCK_FEATURE_FLAGS_STORAGE_KEY),
+      [flag]: e.target.checked,
+    });
+    setStorage(MOCK_FEATURE_FLAGS_STORAGE_KEY, updatedFlags);
   };
 
   return (
