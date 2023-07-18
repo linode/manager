690e28361a55d400b32c8458e3e58cb6423f5b87
Alban Bailly <abailly@akamai.com>
2023-07-18 15:48:21 -0400

    chore: [M3-6795] Docs Links

--- packages/manager/src/components/DocsLink/DocsLink.tsx
+++ packages/manager/src/components/DocsLink/DocsLink.tsx
@@ -1,37 +1,9 @@
-import { Theme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
-import { makeStyles } from 'tss-react/mui';
 
 import DocsIcon from 'src/assets/icons/docs.svg';
+import { Link } from 'src/components/Link';
 import { sendHelpButtonClickEvent } from 'src/utilities/analytics';
-
-import { IconTextLink } from '../IconTextLink/IconTextLink';
-
-const useStyles = makeStyles()((theme: Theme) => ({
-  '@supports (-moz-appearance: none)': {
-    /* Fix text alignment for Firefox */
-    root: {
-      '& span': {
-        top: 1,
-      },
-    },
-  },
-  root: {
-    '& svg': {
-      marginRight: theme.spacing(),
-    },
-    '&:hover': {
-      color: theme.textColors.linkActiveLight,
-      textDecoration: 'underline',
-    },
-    alignItems: 'center',
-    fontFamily: theme.font.normal,
-    fontSize: '.875rem',
-    lineHeight: 'normal',
-    margin: 0,
-    minWidth: 'auto',
-  },
-}));
 
 interface Props {
   analyticsLabel?: string;
@@ -41,27 +13,41 @@ interface Props {
 }
 
 export const DocsLink = (props: Props) => {
-  const { classes } = useStyles();
-
   const { analyticsLabel, href, label, onClick } = props;
 
   return (
-    <IconTextLink
+    <StyledDocsLink
       onClick={() => {
         if (onClick === undefined) {
           sendHelpButtonClickEvent(href, analyticsLabel);
         } else {
           onClick();
         }
-        window.open(href, '_blank', 'noopener');
       }}
-      SideIcon={DocsIcon}
-      aria-describedby="external-site"
-      className={`${classes.root} docsButton`}
-      text={label ?? 'Docs'}
-      title={label ?? 'Docs'}
-    />
+      className="docsButton"
+      to={href}
+    >
+      <DocsIcon />
+      {label ?? 'Docs'}
+    </StyledDocsLink>
   );
 };
 
-export default DocsLink;
+const StyledDocsLink = styled(Link, {
+  label: 'StyledDocsLink',
+})(({ theme }) => ({
+  '& svg': {
+    marginRight: theme.spacing(),
+  },
+  '&:hover': {
+    color: theme.textColors.linkActiveLight,
+    textDecoration: 'underline',
+  },
+  alignItems: 'center',
+  display: 'flex',
+  fontFamily: theme.font.normal,
+  fontSize: '.875rem',
+  lineHeight: 'normal',
+  margin: 0,
+  minWidth: 'auto',
+}));
