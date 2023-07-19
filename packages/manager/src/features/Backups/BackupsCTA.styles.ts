c8511e5fb7a2d2d995b3d97e9f74e41b33c8e26a
TylerWJ <tylerwjones99@gmail.com>
2023-07-19 14:06:30 -0400

    converted sx props to styled api

---
+++ packages/manager/src/features/Backups/BackupsCTA.styles.ts
@@ -0,0 +1,18 @@
+import { styled } from '@mui/material/styles';
+import Paper from 'src/components/core/Paper';
+import { IconButton } from 'src/components/IconButton';
+
+export const StyledPaper = styled(Paper)(({ theme }) => ({
+  alignItems: 'center',
+  display: 'flex',
+  justifyContent: 'space-between',
+  margin: `${theme.spacing(1)} 0 ${theme.spacing(3)} 0`,
+  padding: theme.spacing(1),
+  paddingRight: theme.spacing(2),
+}));
+
+export const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  ...theme.applyLinkStyles,
+  marginLeft: 12,
+  lineHeight: '0.5rem',
+}));
