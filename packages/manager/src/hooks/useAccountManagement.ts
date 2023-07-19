b2455f07cd5046d15f5a468d98faf38e09d5af64
Hussain Khalil <hkhalil@akamai.com>
2023-07-19 12:26:48 -0400

    Remove unused AccountManagementProps interface

--- packages/manager/src/hooks/useAccountManagement.ts
+++ packages/manager/src/hooks/useAccountManagement.ts
@@ -1,19 +1,8 @@
-import { Account, GlobalGrantTypes } from '@linode/api-v4/lib/account';
-import { Profile } from '@linode/api-v4/lib/profile';
+import { GlobalGrantTypes } from '@linode/api-v4/lib/account';
 
 import { useAccount } from 'src/queries/account';
 import { useAccountSettings } from 'src/queries/accountSettings';
 import { useGrants, useProfile } from 'src/queries/profile';
-
-export interface AccountManagementProps {
-  _hasAccountAccess: boolean;
-  _hasGrant: (grant: GlobalGrantTypes) => boolean;
-  _isLargeAccount: boolean;
-  _isManagedAccount: boolean;
-  _isRestrictedUser: boolean;
-  account: Account;
-  profile: Profile;
-}
 
 export const useAccountManagement = () => {
   const { data: account } = useAccount();
