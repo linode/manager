cfbf2ea035e44137dd0693030928bc17fba9fba0
jdamore-linode <97627410+jdamore-linode@users.noreply.github.com>
2023-07-19 13:15:11 -0400

    refactor: [M3-6625] - Make changes to facilitate Cloud Manager region test pipeline (#9347)
    * Add regions test Jenkinsfile
    
    * Allow region and test suite env vars to be passed, add region test runner service
    
    * Disable recording by default in Dockerfile, override in Compose file
    
    * Improve Cypress handling of API authentication
    
    * Optionally log test user name when `CY_LOG_TEST_USER` is set

--- packages/manager/cypress/support/plugins/fetch-linode-regions.ts
+++ packages/manager/cypress/support/plugins/fetch-linode-regions.ts
@@ -1,25 +1,6 @@
-import { baseRequest, getRegions } from '@linode/api-v4';
-
 import type { CypressPlugin } from './plugin';
-import type { Region, ResourcePage } from '@linode/api-v4';
-
-/*
- * Authenticate API requests. Necessary to fetch list of Linode regions.
- */
-const authenticateWithToken = (apiOauthToken: string) => {
-  baseRequest.interceptors.request.use((config) => {
-    return {
-      ...config,
-      headers: {
-        ...config.headers,
-        common: {
-          ...config.headers.common,
-          authorization: `Bearer ${apiOauthToken}`,
-        },
-      },
-    };
-  });
-};
+import type { ResourcePage, Region } from '@linode/api-v4';
+import { getRegions } from '@linode/api-v4';
 
 /**
  * Fetches Linode regions and stores data in Cypress `cloudManagerRegions` env.
@@ -27,14 +8,6 @@ const authenticateWithToken = (apiOauthToken: string) => {
  * Throws an error if no OAuth token (used for regions API request) is defined.
  */
 export const fetchLinodeRegions: CypressPlugin = async (on, config) => {
-  const oauthToken = config?.env?.['MANAGER_OAUTH'];
-  if (!oauthToken) {
-    throw new Error(
-      `Unable to fetch Linode regions because 'MANAGER_OAUTH' environment variable is not defined.`
-    );
-  }
-
-  authenticateWithToken(oauthToken);
   const regions: ResourcePage<Region> = await getRegions({ page_size: 500 });
 
   return {
