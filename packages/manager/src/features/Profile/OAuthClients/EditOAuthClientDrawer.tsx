7646aca249ad41dd9c0f706ce4063311ccd364c1
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2023-07-19 17:46:20 -0400

    refactor: [M3-6900] - MUI v5 Migration - `SRC > Components > Drawer` (#9423)
    * migrate `Drawer` and update storybook
    
    * Added changeset: MUI v5 Migration - SRC > Components > Drawer
    
    * feedback @abailly-akamai
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

--- packages/manager/src/features/Profile/OAuthClients/EditOAuthClientDrawer.tsx
+++ packages/manager/src/features/Profile/OAuthClients/EditOAuthClientDrawer.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import ActionsPanel from 'src/components/ActionsPanel';
 import { Button } from 'src/components/Button/Button';
 import { Checkbox } from 'src/components/Checkbox';
-import Drawer from 'src/components/Drawer';
+import { Drawer } from 'src/components/Drawer';
 import { Notice } from 'src/components/Notice/Notice';
 import { TextField } from 'src/components/TextField';
 import FormControl from 'src/components/core/FormControl';
