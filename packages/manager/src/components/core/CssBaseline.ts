b1f8428e193017690cbf893634af8811a02588c2
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2023-07-25 11:28:52 -0400

    refactor: [M3-6899] - MUI v5 Migration - `SRC > Components > Dialog` (#9419)
    * clean up `Dialog` abstractions
    
    * Added changeset: refactor: [M3-6899] - MUI v5 Migration - SRC > Components > Dialog
    
    * feedback @abailly-akamai - remove mdx and move `TypeToConfirmDialog` to proper place in storybook
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

--- packages/manager/src/components/core/CssBaseline.ts
+++
@@ -1,8 +0,0 @@
-import CssBaseline, {
-  CssBaselineProps as _CssBaselineProps,
-} from '@mui/material/CssBaseline';
-
-/* tslint:disable-next-line:no-empty-interface */
-export type CssBaselineProps = _CssBaselineProps;
-
-export default CssBaseline;
