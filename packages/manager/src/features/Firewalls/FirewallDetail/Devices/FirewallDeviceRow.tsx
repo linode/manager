c821bb6b33505d612aaa232d8b8b4a990d5de4fe
coliu-akamai <coliu@akamai.com>
2023-07-25 09:25:13 -0400

    address changes

--- packages/manager/src/features/Firewalls/FirewallDetail/Devices/FirewallDeviceRow.tsx
+++ packages/manager/src/features/Firewalls/FirewallDetail/Devices/FirewallDeviceRow.tsx
@@ -6,25 +6,27 @@ import { TableRow } from 'src/components/TableRow';
 
 import {
   FirewallDeviceActionMenu,
-  FirewallDeviceActionMenuProps as ActionProps,
+  FirewallDeviceActionMenuProps,
 } from './FirewallDeviceActionMenu';
 
-export const FirewallDeviceRow = React.memo((props: ActionProps) => {
-  const { deviceEntityID, deviceID, deviceLabel } = props;
+export const FirewallDeviceRow = React.memo(
+  (props: FirewallDeviceActionMenuProps) => {
+    const { deviceEntityID, deviceID, deviceLabel } = props;
 
-  return (
-    <TableRow
-      ariaLabel={`Device ${deviceLabel}`}
-      data-testid={`firewall-device-row-${deviceID}`}
-    >
-      <TableCell>
-        <Link tabIndex={0} to={`/linodes/${deviceEntityID}`}>
-          {deviceLabel}
-        </Link>
-      </TableCell>
-      <TableCell actionCell>
-        <FirewallDeviceActionMenu {...props} />
-      </TableCell>
-    </TableRow>
-  );
-});
+    return (
+      <TableRow
+        ariaLabel={`Device ${deviceLabel}`}
+        data-testid={`firewall-device-row-${deviceID}`}
+      >
+        <TableCell>
+          <Link tabIndex={0} to={`/linodes/${deviceEntityID}`}>
+            {deviceLabel}
+          </Link>
+        </TableCell>
+        <TableCell actionCell>
+          <FirewallDeviceActionMenu {...props} />
+        </TableCell>
+      </TableRow>
+    );
+  }
+);
