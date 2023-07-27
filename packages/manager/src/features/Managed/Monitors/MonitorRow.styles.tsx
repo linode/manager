420f011de087f5407e9762a90d4d38b6313aeff7
TylerWJ <tylerwjones99@gmail.com>
2023-07-27 15:59:06 -0400

    fixed table row bug

--- packages/manager/src/features/Managed/Monitors/MonitorRow.styles.tsx
+++ packages/manager/src/features/Managed/Monitors/MonitorRow.styles.tsx
@@ -1,7 +1,7 @@
+import Grid from '@mui/material/Unstable_Grid2';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
-import Grid from 'src/components/Grid';
 import { TableCell } from 'src/components/TableCell';
 import { TableRow } from 'src/components/TableRow';
 import { Typography } from 'src/components/Typography';
