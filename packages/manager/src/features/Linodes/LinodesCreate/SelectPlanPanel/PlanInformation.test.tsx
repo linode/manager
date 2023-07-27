29135eca12863a19647da67330297a391abf539f
Banks Nussman <banks@nussman.us>
2023-07-27 12:00:46 -0400

    remove unneeded mocks

--- packages/manager/src/features/Linodes/LinodesCreate/SelectPlanPanel/PlanInformation.test.tsx
+++ packages/manager/src/features/Linodes/LinodesCreate/SelectPlanPanel/PlanInformation.test.tsx
@@ -7,11 +7,6 @@ import { PlanInformation } from './PlanInformation';
 
 import type { PlanInformationProps } from './PlanInformation';
 
-jest.mock('src/hooks/useFlags', () => ({
-  __esModule: true,
-  useFlags: jest.fn(),
-}));
-
 const mockProps: PlanInformationProps = {
   hasSelectedRegion: true,
   isSelectedRegionEligibleForPlan: false,
--- packages/manager/src/features/Search/ResultGroup.test.tsx
+++ packages/manager/src/features/Search/ResultGroup.test.tsx
@@ -11,9 +11,6 @@ jest.mock('@mui/styles', () => ({
   ...(jest.requireActual('@mui/styles') as any),
   makeStyles: jest.fn(() => () => ({})),
 }));
-jest.mock('src/hooks/useFlags', () => ({
-  useFlags: jest.fn().mockReturnValue({}),
-}));
 
 const props = {
   entity: 'linodes',
