6b9277b82d82d69b5d07477a0c3dc98b43f18443
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2023-07-28 15:32:26 -0400

    refactor: [M3 6730] - LinodeSelect - Allow fully custom noOptionsText (#9452)
    Co-authored-by: Jaalah Ramos <jaalah.ramos@gmail.com>
    Co-authored-by: Joe D'Amore <jdamore@linode.com>

--- packages/manager/src/features/Linodes/LinodeSelect/LinodeSelect.stories.tsx
+++ packages/manager/src/features/Linodes/LinodeSelect/LinodeSelect.stories.tsx
@@ -1,3 +1,4 @@
+import { Linode } from '@linode/api-v4/lib/linodes';
 import { action } from '@storybook/addon-actions';
 import React from 'react';
 
@@ -8,6 +9,13 @@ import type {
   LinodeSingleSelectProps,
 } from './LinodeSelect';
 import type { Meta, StoryObj } from '@storybook/react';
+
+const linodes = [
+  { id: 1, label: 'Linode 1' },
+  { id: 2, label: 'Linode 2' },
+  { id: 3, label: 'Linode 3' },
+  { id: 4, label: 'Linode 4' },
+];
 
 const meta: Meta<LinodeMultiSelectProps | LinodeSingleSelectProps> = {
   argTypes: {
@@ -28,12 +36,32 @@ type Story = StoryObj<typeof LinodeSelect>;
 
 /** Default Linode Select */
 export const Default: Story = {
-  args: {},
+  args: {
+    options: linodes as Linode[],
+  },
+  render: (args) => <LinodeSelect {...args} />,
+};
+
+export const noOptionsMessage: Story = {
+  args: {
+    label: 'Select a Linode',
+    noOptionsMessage:
+      'This is a custom message when there are no options to display.',
+    options: [],
+    placeholder: 'Select a Linode',
+    value: null,
+  },
   render: (args) => <LinodeSelect {...args} />,
 };
 
 /* Linode Multi-select */
 export const MultiSelect: Story = {
-  args: {},
+  args: {
+    multiple: true,
+    onSelectionChange: (selected) => {
+      action('onSelectionChange')(selected.map((linode) => linode.id));
+    },
+    value: [1, 2],
+  },
   render: (args) => <LinodeSelect {...args} />,
 };
