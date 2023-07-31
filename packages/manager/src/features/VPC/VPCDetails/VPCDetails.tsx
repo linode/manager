d1221f81067b18566742c07201c1193a26c17a3a
coliu-akamai <coliu@akamai.com>
2023-07-31 14:33:43 -0400

    skeleton files for vpc routes

---
+++ packages/manager/src/features/VPC/VPCDetails/VPCDetails.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const VPCDetails = () => {
+  return <>TODO: VPC M3-6736 Create VPC Detail page with Summary Section</>;
+};
+
+export default VPCDetails;
---
+++ packages/manager/src/features/VPC/VPCLanding/VPCLanding.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const VPCLanding = () => {
+  return <>TODO: VPC M3-6733 Add VPC Landing page</>;
+};
+
+export default VPCLanding;
