d17b3415a8459dc2b094cc3e7b5f9b821b0ca354
coliu-akamai <coliu@akamai.com>
2023-08-01 10:07:23 -0400

    vpc routes

--- packages/manager/src/features/VPC/VPCLanding/VPCLanding.tsx
+++ packages/manager/src/features/VPC/VPCLanding/VPCLanding.tsx
@@ -1,7 +1,29 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
+import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import LandingHeader from 'src/components/LandingHeader/LandingHeader';
 
+const VPC_CREATE_ROUTE = 'vpc/create';
 const VPCLanding = () => {
-  return <>TODO: VPC M3-6733 Add VPC Landing page</>;
+  const history = useHistory();
+
+  const createVPC = () => {
+    history.push(VPC_CREATE_ROUTE);
+  };
+
+  return (
+    <>
+      <DocumentTitleSegment segment="VPC" />
+      <LandingHeader
+        createButtonText={'Create VPC'}
+        docsLabel="Docs"
+        docsLink="" // TODO: VPC -  Add docs link
+        onButtonClick={createVPC}
+        title="Virtual Private Cloud (VPC)"
+      />
+      TODO: VPC M3-6733 Add VPC Landing page (PR #9467)
+    </>
+  );
 };
 
 export default VPCLanding;
--- packages/manager/src/features/VPC/index.tsx
+++ packages/manager/src/features/VPC/index.tsx
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import SuspenseLoader from 'src/components/SuspenseLoader';
 
 const VPCCreate = React.lazy(() => import('./VPCCreate/VPCCreate'));
-const VPCDetails = React.lazy(() => import('./VPCDetails/VPCDetails'));
+const VPCDetail = React.lazy(() => import('./VPCDetail/VPCDetail'));
 const VPCLanding = React.lazy(() => import('./VPCLanding/VPCLanding'));
 
 const VPC = () => {
@@ -11,7 +11,7 @@ const VPC = () => {
     <React.Suspense fallback={<SuspenseLoader />}>
       <Switch>
         <Route component={VPCCreate} path="/vpc/create" />
-        <Route component={VPCDetails} path="/vpc/:vpcId/:tab?" />
+        <Route component={VPCDetail} path="/vpc/:vpcId/:tab?" />
         <Route component={VPCLanding} path="/vpc" />
       </Switch>
     </React.Suspense>
