3d8c50ee46bcbdd5d68491b5eabacbc00518f2cc
coliu-akamai <coliu@akamai.com>
2023-08-07 14:07:27 -0400

    add landing header for vpc create

--- packages/manager/src/features/VPC/VPCCreate/VPCCreate.tsx
+++ packages/manager/src/features/VPC/VPCCreate/VPCCreate.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import { LandingHeader } from 'src/components/LandingHeader';
 
 const VPCCreate = () => {
   return (
     <>
-      <DocumentTitleSegment segment="VPC" />
-      TODO: VPC M3-6757 Add Create VPC page
+      <DocumentTitleSegment segment="Create VPC" />
+      <LandingHeader
+        breadcrumbProps={{
+          crumbOverrides: [
+            {
+              label: 'Virtual Private Cloud',
+              position: 1,
+            },
+          ],
+          pathname: `/vpc/create`,
+        }}
+        docsLabel="Getting Started"
+        docsLink="#" // TODO: VPC - add correct docs link
+        title="Create"
+      />
     </>
   );
 };
