e454d61633fc68a81dfb07dfdb2e9a05f57a2526
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-08-08 21:01:17 -0400

    Work In Progress

--- packages/manager/src/components/Autocomplete/Autocomplete.stories.tsx
+++ packages/manager/src/components/Autocomplete/Autocomplete.stories.tsx
@@ -6,15 +6,20 @@ import React from 'react';
 import { Autocomplete } from './Autocomplete';
 import { SelectedIcon } from './Autocomplete.styles';
 
-import type {
-  AutocompleteMultiSelectProps,
-  AutocompleteSingleSelectProps,
-} from './Autocomplete';
+import type { AutocompleteAllProps, OptionsType } from './Autocomplete';
 import type { Meta, StoryFn, StoryObj } from '@storybook/react';
 
 interface FruitProps {
   label: string;
   value: string;
+}
+
+interface MultiSelectExample {
+  onClearOptions: () => void;
+  onSelectAll: (isSelected: boolean) => void;
+  onToggleOption: (selectedOptions: OptionsType[]) => void;
+  options: OptionsType[];
+  selectedValues: OptionsType[];
 }
 
 const fruits: FruitProps[] = [
@@ -40,9 +45,7 @@ const fruits: FruitProps[] = [
   },
 ];
 
-const meta: Meta<
-  AutocompleteMultiSelectProps | AutocompleteSingleSelectProps
-> = {
+const meta: Meta<AutocompleteAllProps> = {
   argTypes: {
     onSelectionChange: {
       action: 'onSelectionChange',
@@ -87,7 +90,6 @@ export const Default: Story = {
 
 export const noOptionsMessage: Story = {
   args: {
-    defaultValue: null,
     noOptionsMessage:
       'This is a custom message when there are no options to display.',
     options: [],
@@ -129,27 +131,27 @@ export const customRenderOptions: Story = {
 
 export const MultiSelect: Story = {
   args: {
-    // defaultValue: [fruits[0]],
+    defaultValue: [fruits[0]],
     multiple: true,
-    // onSelectionChange: (selected) => {
-    //   action('onSelectionChange')(selected.map((options) => options.value));
-    // },
+    onSelectionChange: (selected: OptionsType[]) => {
+      action('onSelectionChange')(selected.map((options) => options.value));
+    },
   },
   render: (args) => <MultiSelectExample {...args} />,
 };
 
-const MultiSelectExample = (props: any) => {
+const MultiSelectExample = (props: AutocompleteAllProps) => {
   const { ...args } = props;
   const [selectedOptions, setSelectedOptions] = React.useState<FruitProps[]>(
     []
   );
 
-  const handleToggleOption = (selectedOptions: any) =>
+  const handleToggleOption = (selectedOptions: OptionsType[]) =>
     setSelectedOptions(selectedOptions);
 
   const handleClearOptions = () => setSelectedOptions([]);
 
-  const handleSelectAll = (isSelected: any) => {
+  const handleSelectAll = (isSelected: boolean) => {
     if (isSelected) {
       setSelectedOptions(fruits);
     } else {
