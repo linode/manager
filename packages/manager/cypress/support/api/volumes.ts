1098e5dd1f2a9a0a44a969b536cc769d68f4ef72
Joe D'Amore <jdamore@linode.com>
2023-08-09 15:31:04 -0400

    Improve volume clean up resiliency to detach issues

--- packages/manager/cypress/support/api/volumes.ts
+++ packages/manager/cypress/support/api/volumes.ts
@@ -1,8 +1,9 @@
 import { Volume, deleteVolume, detachVolume, getVolumes } from '@linode/api-v4';
 import { pageSize } from 'support/constants/api';
 import { depaginate } from 'support/util/paginate';
-
+import { timeout } from 'support/util/backoff';
 import { isTestLabel } from './common';
+import { attemptWithBackoff, SimpleBackoffMethod } from 'support/util/backoff';
 
 /**
  * Delete all Volumes whose labels are prefixed "cy-test-".
@@ -23,7 +24,21 @@ export const deleteAllTestVolumes = async (): Promise<void> => {
       if (volume.linode_id) {
         await detachVolume(volume.id);
       }
-      await deleteVolume(volume.id);
+      /*
+       * Make 10 attempts to delete the volume, waiting 10 seconds between
+       * each attempt.
+       *
+       * This is necessary when the volume has been recently detached; the API
+       * request to detach the volume responds successfully, indicating that the
+       * volume has been detached, but subsequent attempts to delete the volume
+       * often fail with an error message indicating that the volume has not yet
+       * been detached.
+       */
+      const backoff = new SimpleBackoffMethod(10000, {
+        maxAttempts: 10,
+      });
+
+      await attemptWithBackoff(backoff, () => deleteVolume(volume.id));
     });
 
   await Promise.all(detachDeletePromises);
