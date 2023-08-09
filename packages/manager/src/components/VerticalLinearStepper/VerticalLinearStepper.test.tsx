c1a94ed323fabbee1e7c627dd5c77240604135e4
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-08-09 08:20:44 -0500

    VerticalLinearStepper unit tests

---
+++ packages/manager/src/components/VerticalLinearStepper/VerticalLinearStepper.test.tsx
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+
+import { renderWithTheme } from 'src/utilities/testHelpers';
+
+import { VerticalLinearStepper } from './VerticalLinearStepper';
+
+const steps = [
+  {
+    content: <div>Step 1 Content</div>,
+    handler: jest.fn(), // Mock function for testing
+    label: 'Details',
+  },
+  {
+    content: <div>Step 2 Content</div>,
+    handler: jest.fn(),
+    label: 'Summary',
+  },
+  {
+    content: <div>Step 3 Content</div>,
+    handler: jest.fn(),
+    label: 'Next',
+  },
+];
+
+test('renders all steps and navigates', () => {
+  renderWithTheme(<VerticalLinearStepper steps={steps} />);
+});
