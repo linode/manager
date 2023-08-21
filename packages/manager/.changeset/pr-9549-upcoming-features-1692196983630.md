ddfcb9846c23bd0988e1517b84704dfd610987a8
Hana Xu <115299789+hana-linode@users.noreply.github.com>
2023-08-21 12:03:42 -0400

    feat: [M3-6736] - VPC detail summary (#9549)
    ## Description 📝
    Add VPC Detail Summary section with Subnets placeholder
    
    ## How to test 🧪
    ```
    yarn test VPCDetail
    ```
    ```
    yarn test packages/manager/src/features/VPC/utils.test.ts
    ```
    - Create a VPC, then go to it's details page and confirm the summary
    - If there is no description for the VPC, the description field should not display
    - If there is a description, you should see the field
    - Try editing/deleting the VPC
    
    ---------
    
    Co-authored-by: Joe D'Amore <jdamore@linode.com>

---
+++ packages/manager/.changeset/pr-9549-upcoming-features-1692196983630.md
@@ -0,0 +1,5 @@
+---
+"@linode/manager": Upcoming Features
+---
+
+VPC detail summary ([#9549](https://github.com/linode/manager/pull/9549))
---
+++ packages/manager/src/features/VPC/VPCDetail/VPCDetail.test.tsx
@@ -0,0 +1,114 @@
+import { fireEvent } from '@testing-library/react';
+import { waitForElementToBeRemoved } from '@testing-library/react';
+import * as React from 'react';
+import { QueryClient } from 'react-query';
+
+import { vpcFactory } from 'src/factories/vpcs';
+import { rest, server } from 'src/mocks/testServer';
+import { mockMatchMedia, renderWithTheme } from 'src/utilities/testHelpers';
+
+import VPCDetail from './VPCDetail';
+
+const queryClient = new QueryClient();
+
+beforeAll(() => mockMatchMedia());
+afterEach(() => {
+  queryClient.clear();
+});
+
+const loadingTestId = 'circle-progress';
+
+describe('VPC Detail Summary section', () => {
+  it('should display number of subnets and linodes, region, id, creation and update dates', async () => {
+    const vpcFactory1 = vpcFactory.build({ id: 100 });
+    server.use(
+      rest.get('*/vpcs/:vpcId', (req, res, ctx) => {
+        return res(ctx.json(vpcFactory1));
+      })
+    );
+
+    const { getAllByText, getByTestId } = renderWithTheme(<VPCDetail />, {
+      queryClient,
+    });
+
+    // Loading state should render
+    expect(getByTestId(loadingTestId)).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(getByTestId(loadingTestId));
+
+    getAllByText('Subnets');
+    getAllByText('Linodes');
+    getAllByText('0');
+
+    getAllByText('Region');
+    getAllByText('Newark, NJ');
+
+    getAllByText('VPC ID');
+    getAllByText(vpcFactory1.id);
+
+    getAllByText('Created');
+    getAllByText(vpcFactory1.created);
+
+    getAllByText('Updated');
+    getAllByText(vpcFactory1.updated);
+  });
+
+  it('should display description if one is provided', async () => {
+    const vpcFactory1 = vpcFactory.build({
+      description: `VPC for webserver and database.`,
+    });
+    server.use(
+      rest.get('*/vpcs/:vpcId', (req, res, ctx) => {
+        return res(ctx.json(vpcFactory1));
+      })
+    );
+
+    const { getByTestId, getByText } = renderWithTheme(<VPCDetail />, {
+      queryClient,
+    });
+
+    await waitForElementToBeRemoved(getByTestId(loadingTestId));
+
+    getByText('Description');
+    getByText(vpcFactory1.description);
+  });
+
+  it('should hide description if none is provided', async () => {
+    server.use(
+      rest.get('*/vpcs/:vpcId', (req, res, ctx) => {
+        return res(ctx.json(vpcFactory.build()));
+      })
+    );
+
+    const { getByTestId, queryByText } = renderWithTheme(<VPCDetail />, {
+      queryClient,
+    });
+
+    await waitForElementToBeRemoved(getByTestId(loadingTestId));
+
+    expect(queryByText('Description')).not.toBeInTheDocument();
+  });
+
+  it('should display read more/less button in description if there are more than 150 characters', async () => {
+    const vpcFactory1 = vpcFactory.build({
+      description: `VPC for webserver and database. VPC for webserver and database. VPC for webserver and database. VPC for webserver and database. VPC for webserver. VPC for webserver.`,
+    });
+    server.use(
+      rest.get('*/vpcs/:vpcId', (req, res, ctx) => {
+        return res(ctx.json(vpcFactory1));
+      })
+    );
+
+    const { getAllByRole, getByTestId } = renderWithTheme(<VPCDetail />, {
+      queryClient,
+    });
+
+    await waitForElementToBeRemoved(getByTestId(loadingTestId));
+
+    const readMoreButton = getAllByRole('button')[2];
+    expect(readMoreButton.innerHTML).toBe('Read More');
+
+    fireEvent.click(readMoreButton);
+    expect(readMoreButton.innerHTML).toBe('Read Less');
+  });
+});
