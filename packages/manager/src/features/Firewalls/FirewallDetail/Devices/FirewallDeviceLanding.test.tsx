71af81d8434ea4ae76e648114f7354da7e5bc7ac
TylerWJ <tylerwjones99@gmail.com>
2023-08-25 17:58:16 -0400

    fixed unit tests

--- packages/manager/src/features/Firewalls/FirewallDetail/Devices/FirewallDeviceLanding.test.tsx
+++ packages/manager/src/features/Firewalls/FirewallDetail/Devices/FirewallDeviceLanding.test.tsx
@@ -9,55 +9,47 @@ import {
   FirewallDeviceLandingProps,
 } from './FirewallDeviceLanding';
 
-const linodeProps: FirewallDeviceLandingProps = {
+import type { FirewallDeviceEntityType } from '@linode/api-v4';
+
+const baseProps = (
+  type: FirewallDeviceEntityType
+): FirewallDeviceLandingProps => ({
   disabled: true,
   firewallID: 1,
   firewallLabel: 'test',
-  type: 'linode',
-};
-
-const nodeBalancerProps: FirewallDeviceLandingProps = {
-  ...linodeProps,
-  type: 'nodebalancer',
-};
-
-describe('Firewall linode device', () => {
-  let addLinodesButton: HTMLElement;
-
-  beforeEach(() => {
-    server.use(
-      rest.get('*/firewalls/*', (req, res, ctx) => {
-        return res(ctx.json(makeResourcePage([])));
-      })
-    );
-    const { getByTestId } = renderWithTheme(
-      <FirewallDeviceLanding {...linodeProps} />
-    );
-    addLinodesButton = getByTestId('add-device-button');
-  });
-
-  it('should render an add Linodes button', () => {
-    expect(addLinodesButton).toBeInTheDocument();
-  });
-
-  it('should render a disabled add Linodes button', () => {
-    expect(addLinodesButton).toBeDisabled();
-  });
+  type,
 });
 
-describe('Firewall nodebalancer device', () => {
-  let addNodeBalancersButton: HTMLElement;
+const devices = ['linode', 'nodebalancer'];
 
-  beforeEach(() => {
-    const { getByTestId } = renderWithTheme(
-      <FirewallDeviceLanding {...nodeBalancerProps} />
-    );
-    addNodeBalancersButton = getByTestId('add-device-button');
-  });
-  it('should render an add NodeBalancer button', () => {
-    expect(addNodeBalancersButton).toBeInTheDocument();
-  });
-  it('should render a disabled add NodeBalancer button', () => {
-    expect(addNodeBalancersButton).toBeDisabled();
+devices.forEach((device) => {
+  describe(`Firewall ${device} device`, () => {
+    let addButton: HTMLElement;
+
+    beforeEach(() => {
+      server.use(
+        rest.get('*/firewalls/*', (req, res, ctx) => {
+          return res(ctx.json(makeResourcePage([])));
+        })
+      );
+      const { getByTestId } = renderWithTheme(
+        <FirewallDeviceLanding
+          {...baseProps(device as FirewallDeviceEntityType)}
+        />
+      );
+      addButton = getByTestId('add-device-button');
+    });
+
+    it(`should render an add ${
+      device.charAt(0).toUpperCase() + device.slice(1)
+    } button`, () => {
+      expect(addButton).toBeInTheDocument();
+    });
+
+    it(`should render a disabled add ${
+      device.charAt(0).toUpperCase() + device.slice(1)
+    } button`, () => {
+      expect(addButton).toBeDisabled();
+    });
   });
 });
