cebb0da2cc966eb846de968c421a42e340f7611a
tyler-akamai <139489745+tyler-akamai@users.noreply.github.com>
2023-08-28 14:25:29 -0400

    refactor: [M3-6264] - MUI v5 Migration - SRC > Features > Events (#9565)
    * initial migration changes
    
    * Added changeset: MUI v5 Migration - SRC > Features > Events
    
    * fixed label alignement issue
    
    * fixed pr suggestions
    
    * removed render guard in EventRow
    
    ---------
    
    Co-authored-by: TylerWJ <tylerwjones99@gmail.com>

---
+++ packages/manager/src/features/Events/EventsLanding.styles.ts
@@ -0,0 +1,43 @@
+import { styled } from '@mui/material/styles';
+
+import { H1Header } from 'src/components/H1Header/H1Header';
+import { TableCell } from 'src/components/TableCell';
+import { Typography } from 'src/components/Typography';
+
+export const StyledTableCell = styled(TableCell, {
+  label: 'StyledTableCell',
+})(({ theme }) => ({
+  color: theme.textColors.tableHeader,
+  fontFamily: theme.font.bold,
+  fontSize: '0.875rem',
+}));
+
+export const StyledLabelTableCell = styled(TableCell, {
+  label: 'StyledLabelTableCell',
+})(({ theme }) => ({
+  color: theme.textColors.tableHeader,
+  fontFamily: theme.font.bold,
+  fontSize: '0.875rem',
+  minWidth: 200,
+  paddingLeft: 10,
+  [theme.breakpoints.down('sm')]: {
+    width: '70%',
+  },
+  width: '60%',
+}));
+
+export const StyledH1Header = styled(H1Header, {
+  label: 'StyledH1Header',
+})(({ theme }) => ({
+  marginBottom: theme.spacing(1),
+  [theme.breakpoints.down('md')]: {
+    marginLeft: theme.spacing(),
+  },
+}));
+
+export const StyledTypography = styled(Typography, {
+  label: 'StyledTyography',
+})(({ theme }) => ({
+  padding: theme.spacing(4),
+  textAlign: 'center',
+}));
