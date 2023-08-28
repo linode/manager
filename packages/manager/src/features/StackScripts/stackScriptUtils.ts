b18a95014a538283ef8fad252b1f43ba614aa239
Hana Xu <hxu@akamai.com>
2023-08-28 16:09:39 -0400

    feature flag the new apps

--- packages/manager/src/features/StackScripts/stackScriptUtils.ts
+++ packages/manager/src/features/StackScripts/stackScriptUtils.ts
@@ -115,8 +115,9 @@ export const baseApps = {
   '1008125': 'Easypanel',
   '1017300': 'Kali Linux',
   '1037036': 'Budibase',
-  '1226544': 'HashiCorp Nomad Cluster',
-  '1226545': 'HashiCorp Nomad Clients Cluster',
+  // uncomment clusters after marketplace release
+  // '1226544': 'HashiCorp Nomad Cluster',
+  // '1226545': 'HashiCorp Nomad Clients Cluster',
   '1037037': 'HashiCorp Nomad',
   '1037038': 'HashiCorp Vault',
   '1051714': 'Microweber',
