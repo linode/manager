59716a26dde198f6bb571f97859acc1df16a445b
mjac0bs <mjacobs@akamai.com>
2023-08-28 06:55:54 -0700

    Add test for backup price util function

--- packages/manager/src/utilities/pricing/linodes.test.tsx
+++ packages/manager/src/utilities/pricing/linodes.test.tsx
@@ -1,6 +1,7 @@
 import { linodeTypeFactory } from 'src/factories';
 
 import {
+  getLinodeRegionBackupPrice,
   getLinodeRegionPrice,
   isLinodeTypeDifferentPriceInSelectedRegion,
 } from './linodes';
@@ -32,6 +33,57 @@ describe('getLinodeRegionPrice', () => {
     expect(getLinodeRegionPrice(type, 'id-cgk')).toEqual({
       hourly: 0.2,
       monthly: 29.99,
+    });
+  });
+});
+
+describe('getLinodeRegionBackupPrice', () => {
+  it('gets a linode backup price without a region override', () => {
+    const type = linodeTypeFactory.build({
+      addons: {
+        backups: {
+          price: {
+            hourly: 0.004,
+            monthly: 2.5,
+          },
+          region_prices: [],
+        },
+      },
+    });
+
+    expect(getLinodeRegionBackupPrice(type, 'us-east')).toEqual({
+      hourly: 0.004,
+      monthly: 2.5,
+    });
+  });
+
+  it('gets a linode price with a backup override', () => {
+    const type = linodeTypeFactory.build({
+      addons: {
+        backups: {
+          price: {
+            hourly: 0.004,
+            monthly: 2.5,
+          },
+          region_prices: [
+            {
+              hourly: 0.0048,
+              id: 'id-cgk',
+              monthly: 3.57,
+            },
+            {
+              hourly: 0.0056,
+              id: 'br-gru',
+              monthly: 4.17,
+            },
+          ],
+        },
+      },
+    });
+
+    expect(getLinodeRegionBackupPrice(type, 'id-cgk')).toEqual({
+      hourly: 0.0048,
+      monthly: 3.57,
     });
   });
 });
