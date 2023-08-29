1def8399a65d1ebd67acfb7ba20d1c84f6c6955b
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2023-08-29 12:57:25 -0700

    feat: [M3-7035] - Add DC-specific pricing to Linode backups (#9588)
    * Add dynamic pricing backups util functions
    
    * Update Enable Backups drawer with dynamic pricing
    
    * Update Backups tab placeholder and confirmation dialog with dynamic pricing
    
    * Update mocks for now
    
    * Revert mock updates after rebase
    
    * Add test for backup price util function
    
    * Add DC-specific pricing to Linode Create flow
    
    * Update cached regions file to include Jakarta and Sao Paulo
    
    * Feature flag changes
    
    * Improve consistency with price variable names
    
    * Update tests
    
    * Renamed util function for consistency with #9570
    
    * Added changeset: Add DC-specific pricing to Linode backups
    
    * Update backups drawer total cost util function to use FF
    
    * Address feedback: && over ternary
    
    * Address feedback: util and types
    
    * Missed a spot

--- packages/manager/src/features/Backups/AutoEnroll.tsx
+++ packages/manager/src/features/Backups/AutoEnroll.tsx
@@ -35,6 +35,7 @@ export const AutoEnroll = (props: AutoEnrollProps) => {
               >
                 Backups pricing page
               </Link>
+              .
             </Typography>
           </StyledDiv>
         }
