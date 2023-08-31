9a492133514f101133adcc714ae0819e51e565cc
Banks Nussman <banks@nussman.us>
2023-08-31 14:29:52 -0400

    improve validation

--- packages/validation/src/loadbalancers.schema.ts
+++ packages/validation/src/loadbalancers.schema.ts
@@ -1,8 +1,11 @@
 import { object, string } from 'yup';
 
 export const CreateCertificateSchema = object({
-  certificate: string().required(),
-  key: string(),
-  label: string().required(),
-  type: string().oneOf(['downstream', 'ca']).required(),
+  certificate: string().required('Certificate is required.'),
+  key: string().when('type', {
+    is: 'downstream',
+    then: string().required('Private Key is required.'),
+  }),
+  label: string().required('Label is required.'),
+  type: string().oneOf(['downstream', 'ca']).required('Type is required.'),
 });
