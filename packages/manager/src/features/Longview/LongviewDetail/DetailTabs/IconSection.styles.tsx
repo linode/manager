28b0993269199ccfcd0021c30fe60576d329c7cd
coliu-akamai <coliu@akamai.com>
2023-09-01 15:40:54 -0400

    fix styling regressions icon section

--- packages/manager/src/features/Longview/LongviewDetail/DetailTabs/IconSection.styles.tsx
+++ packages/manager/src/features/Longview/LongviewDetail/DetailTabs/IconSection.styles.tsx
@@ -1,6 +1,5 @@
 import { styled, SxProps } from '@mui/material/styles';
-
-import { Grid } from 'src/components/Grid';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import { IconTextLink } from 'src/components/IconTextLink/IconTextLink';
 
 const sxGrid: SxProps = {
@@ -11,24 +10,20 @@ const sxGrid: SxProps = {
   wrap: 'nowrap',
 };
 
-export const StyledHeaderGrid = styled(Grid, { label: 'StyledHeaderGrid' })(
-  ({ theme }) => ({
-    ...sxGrid,
-    alignItems: 'flex-start',
-    marginBottom: `calc(${theme.spacing(3)} - 2px)`,
-  })
-);
+export const StyledHeaderGrid = styled(Grid, { label: 'StyledHeaderGrid' })({
+  ...sxGrid,
+  alignItems: 'flex-start',
+});
 
 export const StyledIconContainerGrid = styled(Grid, {
   label: 'StyledIconContainerGrid',
-})(({ theme }) => ({
+})({
   '&:last-of-type': {
     marginBottom: 0,
   },
   alignItems: 'center',
-  marginBottom: `calc(${theme.spacing(2)} - 2px)`,
   ...sxGrid,
-}));
+});
 
 export const StyledIconGrid = styled(Grid, { label: 'StyledIconGrid' })({
   '& svg': {
@@ -44,7 +39,7 @@ export const StyledIconTextLink = styled(IconTextLink, {
     stroke: theme.palette.primary.main,
   },
   '& svg': {
-    marginRight: 15,
+    marginRight: 18,
   },
   fontSize: '0.875rem',
   padding: 0,
@@ -53,5 +48,4 @@ export const StyledIconTextLink = styled(IconTextLink, {
 export const StyledPackageGrid = styled(Grid, { label: 'StyledPackageGrid' })({
   alignSelf: 'center',
   boxSizing: 'border-box',
-  marginLeft: '8px',
 });
