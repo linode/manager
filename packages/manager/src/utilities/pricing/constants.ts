24d8ba6c732c3a57b49b72ccd0e7db201541b295
Alban Bailly <130582365+abailly-akamai@users.noreply.github.com>
2023-08-31 21:31:54 -0400

    feat: [M3-6966] - Add DC-specific pricing to Linode Create flows (#9598)
    * feat: [M3-6966] Initial commit: selection panels
    
    * feat: [M3-6966] Linode: Checkout summary
    
    * feat: [M3-6966] Fix display conditional
    
    * Added changeset: Add DC specific Linode Create pricing support
    
    * feat: [M3-6967] add comment
    
    * feat: [M3-6967] reset flag state on component update
    
    * feat: [M3-6967] consolidate tests and utils
    
    * feat: [M3-6967] cleanup
    
    * feat: [M3-6967] moar cleanup and comments
    
    * feat: [M3-6967] quick display fix
    
    * feat: [M3-6967] fix for DB create flow
    
    * feat: [M3-6967] address feedback
    
    * feat: [M3-6967] add tests
    
    * feat: [M3-6967] cleanup tests
    
    * feat: [M3-6967] cleanup tests
    
    * feat: [M3-6967] address feedback (notice)
    
    * feat: [M3-6967] small fix to flag prop and cleanup

--- packages/manager/src/utilities/pricing/constants.ts
+++ packages/manager/src/utilities/pricing/constants.ts
@@ -1,9 +1,10 @@
 // These values will eventually come from the API, but for now they are hardcoded and
 // used to generate the region based dynamic pricing.
-
 export const NODEBALANCER_PRICE = 10;
-
 export const LKE_HA_PRICE = 60;
 
+// Other constants
+export const PLAN_SELECTION_NO_REGION_SELECTED_MESSAGE =
+  'Select a region to view plans and prices.';
 export const LKE_CREATE_CLUSTER_CHECKOUT =
   'Select a Region, HA choice, and add a Node Pool to view pricing and create a cluster.';
