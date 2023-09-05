b559be3574c6a52d4e24932e5bf29bbb29d6406d
coliu-akamai <coliu@akamai.com>
2023-09-05 09:34:51 -0400

    simple test for delete dialogue

--- packages/manager/src/features/VPCs/VPCDetail/SubnetDeleteDialog.test.tsx
+++ packages/manager/src/features/VPCs/VPCDetail/SubnetDeleteDialog.test.tsx
@@ -1,1 +1,23 @@
-// todo
+import * as React from 'react';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+
+import { SubnetDeleteDialog } from './SubnetDeleteDialog';
+
+const props = {
+  onClose: jest.fn(),
+  open: true,
+  subnetID: 1,
+  subnetLabel: 'some subnet',
+  vpcID: 1,
+};
+
+describe('Delete Subnet drawer', () => {
+  it('should render a SubnetDeleteDrawer', () => {
+    const { getByText } = renderWithTheme(<SubnetDeleteDialog {...props} />);
+
+    getByText('Delete subnet some subnet');
+    getByText('Subnet label');
+    getByText('Cancel');
+    getByText('Delete');
+  });
+});
