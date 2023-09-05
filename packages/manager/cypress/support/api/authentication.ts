a8c6e9ca97caa9934911feff6a12b7c449a33e8a
Joe D'Amore <jdamore@linode.com>
2023-09-05 12:24:53 -0400

    Apply API base URL configuration in tests

--- packages/manager/cypress/support/api/authentication.ts
+++ packages/manager/cypress/support/api/authentication.ts
@@ -5,8 +5,35 @@
 import { baseRequest } from '@linode/api-v4/lib/request';
 import { oauthToken } from 'support/constants/api';
 
+const defaultApiRoot = 'https://api.linode.com/v4';
+
+/**
+ * Returns an object containing an overridden API request URL.
+ *
+ * If no request URL or base override URL is passed, an empty object is returned.
+ *
+ * @param requestUrl - API request URL.
+ *
+ * @returns API request URL override object.
+ */
+const getApiRequestUrlOverride = (requestUrl?: string) => {
+  const baseUrl = Cypress.env('REACT_APP_API_ROOT');
+
+  // Short-circuit and return an empty object if no base URL is passed.
+  if (!baseUrl || !requestUrl) {
+    return {};
+  }
+  const overriddenUrl = requestUrl.replace(defaultApiRoot, baseUrl);
+  return {
+    url: overriddenUrl,
+  };
+};
+
 /**
  * Intercepts all Linode API v4 requests and applies an authorization header.
+ *
+ * Optionally overrides the request URL to correspond with the 'REACT_APP_API_ROOT'
+ * environment variable.
  *
  * This function must be executed in each Cypress spec file that intends to
  * communicate with the API directly.
@@ -25,6 +52,7 @@ export const authenticate = function () {
           authorization: `Bearer ${oauthToken}`,
         },
       },
+      ...getApiRequestUrlOverride(config.url),
     };
   });
 };
