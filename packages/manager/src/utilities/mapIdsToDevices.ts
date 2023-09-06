8c8423e7a63a415211448a2f95b47ba34679c829
TylerWJ <tylerwjones99@gmail.com>
2023-09-06 12:33:33 -0400

    erased commented out code

--- packages/manager/src/utilities/mapIdsToDevices.ts
+++ packages/manager/src/utilities/mapIdsToDevices.ts
@@ -4,20 +4,6 @@ import { isNotNullOrUndefined } from './nullOrUndefined';
 
 type Device = Linode | NodeBalancer;
 
-// export const mapIdsToDevices = (
-//   ids: null | number | number[],
-//   devices: Device[] = []
-// ): Device | Device[] | null => {
-//   const deviceMap = new Map(devices.map((device) => [device.id, device]));
-//   if (Array.isArray(ids)) {
-//     return ids.map((id) => deviceMap.get(id)).filter(isNotNullOrUndefined);
-//   } else if (ids !== null) {
-//     return deviceMap.get(ids) ?? null;
-//   } else {
-//     return null;
-//   }
-// };
-
 export const mapIdsToDevices = <T extends Device>(
   ids: null | number | number[],
   devices: T[] = []
