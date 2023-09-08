53ba5bdc0e90edd525b2e4bae3ad2b87b270693e
jdamore-linode <97627410+jdamore-linode@users.noreply.github.com>
2023-09-08 14:23:41 -0400

    test: [M3-7070] - Add DC pricing tests for Linode Clone flow (#9642)
    * Add assertions for region selection price change notice
    
    * Refactor existing clone e2e test

---
+++ packages/manager/cypress/support/constants/dc-specific-pricing.ts
@@ -0,0 +1,43 @@
+/**
+ * @file Constants related to tiered pricing.
+ */
+
+import { linodeTypeFactory } from '@src/factories';
+
+/** Notice shown to users when selecting a region. */
+export const dcPricingRegionNotice =
+  'Prices for plans, products, and services may vary based on Region.';
+
+/** Notice shown to users when selecting a region with a different price structure. */
+export const dcPricingRegionDifferenceNotice =
+  'The selected region has a different price structure.';
+
+/** Notice shown to users trying to choose a plan before selecting a region. */
+export const dcPricingPlanPlaceholder =
+  'Select a region to view plans and prices.';
+
+/** DC-specific pricing docs link label. */
+export const dcPricingDocsLabel = 'How Data Center Pricing Works';
+
+/** DC-specific pricing docs link destination. */
+export const dcPricingDocsUrl = 'https://www.linode.com/pricing';
+
+/** DC-specific pricing Linode type mocks. */
+export const dcPricingMockLinodeTypes = linodeTypeFactory.buildList(3, {
+  region_prices: [
+    {
+      hourly: 0.021,
+      // Use `us-east` and `us-west` so we do not have to mock regions request,
+      // which otherwise may not include the actual regions which have DC-specific pricing applied.
+      id: 'us-east',
+      monthly: 14,
+    },
+    {
+      hourly: 0.018,
+      // Use `us-east` and `us-west` so we do not have to mock regions request,
+      // which otherwise may not include the actual regions which have DC-specific pricing applied.
+      id: 'us-west',
+      monthly: 12,
+    },
+  ],
+});
