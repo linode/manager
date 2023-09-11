9190e20e740f56c681be1ad55ac474369bb545cc
Hana Xu <hxu@akamai.com>
2023-09-11 13:10:34 -0400

    add restricted user logic

--- packages/manager/src/features/VPCs/VPCDetail/SubnetEditDrawer.tsx
+++ packages/manager/src/features/VPCs/VPCDetail/SubnetEditDrawer.tsx
@@ -1,5 +1,5 @@
-import { Subnet } from '@linode/api-v4/lib/vpcs/types';
 import { ModifySubnetPayload } from '@linode/api-v4/lib/vpcs/types';
+import { Subnet } from '@linode/api-v4/lib/vpcs/types';
 import { useFormik } from 'formik';
 import * as React from 'react';
 
@@ -7,6 +7,7 @@ import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
 import { Drawer } from 'src/components/Drawer';
 import { Notice } from 'src/components/Notice/Notice';
 import { TextField } from 'src/components/TextField';
+import { useGrants, useProfile } from 'src/queries/profile';
 import { useUpdateSubnetMutation } from 'src/queries/vpcs';
 import { getErrorMap } from 'src/utilities/errorUtils';
 
@@ -48,13 +49,32 @@ export const SubnetEditDrawer = (props: Props) => {
     }
   }, [open]);
 
+  const { data: profile } = useProfile();
+  const { data: grants } = useGrants();
+
+  const vpcPermissions = grants?.vpc.find((v) => v.id === vpcId);
+
+  // there isn't a 'view VPC/Subnet' grant that does anything, so all VPCs get returned even for restricted users
+  // with permissions set to 'None'. Therefore, we're treating those as read_only as well
+  const readOnly =
+    Boolean(profile?.restricted) &&
+    (vpcPermissions?.permissions === 'read_only' || grants?.vpc.length === 0);
+
   const errorMap = getErrorMap(['label'], error);
 
   return (
     <Drawer onClose={onClose} open={open} title="Edit Subnet">
       {errorMap.none && <Notice text={errorMap.none} variant="error" />}
+      {readOnly && (
+        <Notice
+          important
+          text={`You don't have permissions to edit ${subnet?.label}. Please contact an account administrator for details.`}
+          variant="error"
+        />
+      )}
       <form onSubmit={form.handleSubmit}>
         <TextField
+          disabled={readOnly}
           errorText={errorMap.label}
           label="Label"
           name="label"
