d82d57a244e1bcc26bda327f730bdc8b300b4f80
jdamore-linode <97627410+jdamore-linode@users.noreply.github.com>
2023-09-14 08:56:39 -0400

    M3-7080: Improve Cypress support for non-Prod environments (#9631)
    * Rename API auth file, override base URL using environment variable
    
    * Apply API base URL configuration in tests

--- packages/manager/cypress.config.ts
+++ packages/manager/cypress.config.ts
@@ -9,7 +9,7 @@ import { loadEnvironmentConfig } from './cypress/support/plugins/load-env-config
 import { nodeVersionCheck } from './cypress/support/plugins/node-version-check';
 import { regionOverrideCheck } from './cypress/support/plugins/region-override-check';
 import { vitePreprocess } from './cypress/support/plugins/vite-preprocessor';
-import { authenticateApi } from './cypress/support/plugins/authenticate-api';
+import { configureApi } from './cypress/support/plugins/configure-api';
 import { fetchLinodeRegions } from './cypress/support/plugins/fetch-linode-regions';
 import { splitCypressRun } from './cypress/support/plugins/split-run';
 import { enableJunitReport } from './cypress/support/plugins/junit-report';
@@ -52,7 +52,7 @@ export default defineConfig({
       return setupPlugins(on, config, [
         loadEnvironmentConfig,
         nodeVersionCheck,
-        authenticateApi,
+        configureApi,
         configureFileWatching,
         configureTestSuite,
         vitePreprocess,
