1133a901d56d7fa7551fc40b7ee7aceb439e6d6b
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-09-15 11:28:44 -0400

    Update events

--- packages/manager/src/features/Events/eventMessageGenerator.ts
+++ packages/manager/src/features/Events/eventMessageGenerator.ts
@@ -513,10 +513,13 @@ export const eventMessageCreators: { [index: string]: CreatorsForStatus } = {
       `Linode ${e.entity?.label ?? ''} is scheduled for resizing.`,
     started: (e) => `Linode ${e.entity?.label ?? ''} is resizing.`,
   },
-  // This event type isn't currently being displayed, but I added a message here just in case.
   linode_resize_create: {
     notification: (e) =>
       `Resize for Linode ${e.entity!.label} has been initiated.`,
+  },
+  linode_resize_warm_create: {
+    notification: (e) =>
+      `A warm resize for Linode ${e.entity!.label} has been initiated.`,
   },
   linode_shutdown: {
     failed: (e) => `Linode ${e.entity!.label} could not be shut down.`,
