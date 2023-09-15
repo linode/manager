d6bafe8f326db96ee625831eb4478fd28f3b9928
Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
2023-09-15 11:14:25 -0400

    feat: [M3-6993] – Add VPC data to Linode Detail header (#9645)

--- packages/manager/src/queries/vpcs.ts
+++ packages/manager/src/queries/vpcs.ts
@@ -28,11 +28,15 @@ export const vpcQueryKey = 'vpcs';
 export const subnetQueryKey = 'subnets';
 
 // VPC queries
-export const useVPCsQuery = (params: Params, filter: Filter) => {
+export const useVPCsQuery = (
+  params: Params,
+  filter: Filter,
+  enabled: boolean = true
+) => {
   return useQuery<ResourcePage<VPC>, APIError[]>(
     [vpcQueryKey, 'paginated', params, filter],
     () => getVPCs(params, filter),
-    { keepPreviousData: true }
+    { enabled, keepPreviousData: true }
   );
 };
 
