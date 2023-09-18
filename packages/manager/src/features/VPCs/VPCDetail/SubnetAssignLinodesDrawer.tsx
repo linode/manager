531837591dee289105e106d209a864b96041c00f
coliu-akamai <coliu@akamai.com>
2023-09-18 11:15:45 -0400

    using autocomplete for unassigning linodes for now

--- packages/manager/src/features/VPCs/VPCDetail/SubnetAssignLinodesDrawer.tsx
+++ packages/manager/src/features/VPCs/VPCDetail/SubnetAssignLinodesDrawer.tsx
@@ -1,5 +1,13 @@
 import { useFormik } from 'formik';
-import { appendConfigInterface, Config, InterfacePayload, InterfacePurpose, Linode, Subnet } from '@linode/api-v4';
+import {
+  appendConfigInterface,
+  deleteLinodeConfigInterface,
+  Config,
+  InterfacePayload,
+  InterfacePurpose,
+  Linode,
+  Subnet,
+} from '@linode/api-v4';
 import * as React from 'react';
 import { useQueryClient } from 'react-query';
 
@@ -23,8 +31,8 @@ import { getErrorMap } from 'src/utilities/errorUtils';
 
 import { StyledButtonBox } from './SubnetAssignLinodesDrawer.styles';
 
-// @TODO VPC - if all subnet action menu item related components use this as their props, might be worth 
-// putting this in a common file and naming it something like SubnetActionMenuItemProps or somthing 
+// @TODO VPC - if all subnet action menu item related components use this as their props, might be worth
+// putting this in a common file and naming it something like SubnetActionMenuItemProps or somthing
 interface Props {
   onClose: () => void;
   open: boolean;
@@ -34,22 +42,26 @@ interface Props {
 }
 
 type LinodeAndConfigData = Linode & {
-  configId: number,
-  interfaceId: number,
-}
+  configId: number;
+  interfaceId: number;
+};
 
-const REBOOT_LINODE_MESSAGE = 'Assigning a Linode to a subnet requires you to reboot the Linode to update its configuration.';
+const REBOOT_LINODE_MESSAGE =
+  'Assigning a Linode to a subnet requires you to reboot the Linode to update its configuration.';
 const REGIONAL_LINODE_MESSAGE = `Select the Linodes you would like to assign to this subnet. Only Linodes in this VPC's region are displayed.`;
-const MULTIPLE_CONFIGURATIONS_MESSAGE = 'This Linode has multiple configurations. Select which configuration you would like added to the subnet.';
+const MULTIPLE_CONFIGURATIONS_MESSAGE =
+  'This Linode has multiple configurations. Select which configuration you would like added to the subnet.';
 
 export const SubnetAssignLinodesDrawer = (props: Props) => {
-  // console.log('these are the linodes associated w subnet', props.subnet?.linodes)
   const queryClient = useQueryClient();
   const { onClose, open, subnet, vpcId, vpcRegion } = props;
   const [errorMap, setErrorMap] = React.useState<
     Record<string, string | undefined>
   >({});
-  const [assignedLinodesAndConfigData, setAssignedLinodesAndConfigData] = React.useState<LinodeAndConfigData[]>([]);
+  const [
+    assignedLinodesAndConfigData,
+    setAssignedLinodesAndConfigData,
+  ] = React.useState<LinodeAndConfigData[]>([]);
   const [linodeConfigs, setLinodeConfigs] = React.useState<Config[]>([]);
   const [autoAssignIPv4, setAutoAssignIPv4] = React.useState<boolean>(true);
 
@@ -63,25 +75,20 @@ export const SubnetAssignLinodesDrawer = (props: Props) => {
   const userCannotAssignLinodes =
     Boolean(profile?.restricted) &&
     (vpcPermissions?.permissions === 'read_only' || grants?.vpc.length === 0);
-  
-  // get all linodes from the same region as the VPC is in  
-  const { data: linodes} = useAllLinodesQuery(
+
+  // get all linodes from the same region as the VPC is in
+  const { data: linodes } = useAllLinodesQuery(
     {},
     {
       region: vpcRegion,
-    },
+    }
   );
 
   // since the interface type we will add is vpc, we want to invalidate vpc related queries as well
   const invalidateQueries = (linodeId: number, configId: number) => {
     queryClient.invalidateQueries([vpcQueryKey, 'paginated']);
     queryClient.invalidateQueries([vpcQueryKey, 'vpc', vpcId]);
-    queryClient.invalidateQueries([
-      vpcQueryKey,
-      'vpc',
-      vpcId,
-      subnetQueryKey,
-    ]);
+    queryClient.invalidateQueries([vpcQueryKey, 'vpc', vpcId, subnetQueryKey]);
     queryClient.invalidateQueries([
       queryKey,
       'linode',
@@ -97,27 +104,31 @@ export const SubnetAssignLinodesDrawer = (props: Props) => {
     const { selectedLinode, chosenIP, selectedConfig } = values;
     const configId = selectedConfig?.id ?? linodeConfigs[0]?.id ?? -1;
     const interfacePayload: InterfacePayload = {
-      purpose: 'vpc' as InterfacePurpose, 
-      label: null, 
+      purpose: 'vpc' as InterfacePurpose,
+      label: null,
       ipam_address: null,
       subnet_id: subnet?.id,
-      ...(!autoAssignIPv4 && { ipv4: {vpc: chosenIP  }}),
+      ...(!autoAssignIPv4 && { ipv4: { vpc: chosenIP } }),
     };
-    console.log('the payload and values', interfacePayload, selectedLinode?.id, configId)
     try {
-      const newInterface = await appendConfigInterface(selectedLinode?.id ?? -1, configId, interfacePayload);
-      invalidateQueries(selectedLinode?.id ?? -``, configId);
+      const newInterface = await appendConfigInterface(
+        selectedLinode?.id ?? -1,
+        configId,
+        interfacePayload
+      );
+      invalidateQueries(selectedLinode?.id ?? -1, configId);
       resetForm();
-      console.log('do we get here)')
       setLinodeConfigs([]);
       if (selectedLinode) {
         setAssignedLinodesAndConfigData([
-          ...assignedLinodesAndConfigData, 
+          ...assignedLinodesAndConfigData,
           {
-            ...selectedLinode, 
-            configId, 
+            ...selectedLinode,
+            configId,
             interfaceId: newInterface.id,
-            label: `${selectedLinode?.label}${selectedConfig?.label ? ` (${selectedConfig.label})` : ''}`
+            label: `${selectedLinode?.label}${
+              selectedConfig?.label ? ` (${selectedConfig.label})` : ''
+            }`,
           },
         ]);
       }
@@ -132,32 +143,41 @@ export const SubnetAssignLinodesDrawer = (props: Props) => {
       const newErrors = getErrorMap(['ipv4.vpc'], errors);
       setErrorMap(newErrors);
     }
-  }
+  };
 
-  const { dirty, handleSubmit, resetForm, setFieldValue, setValues, values } = useFormik({
+  const {
+    dirty,
+    handleSubmit,
+    resetForm,
+    setFieldValue,
+    setValues,
+    values,
+  } = useFormik({
     enableReinitialize: true,
     initialValues: {
       selectedLinode: null as Linode | null,
       chosenIP: '',
-      selectedConfig: null as Config | null, 
+      selectedConfig: null as Config | null,
     },
     onSubmit: onAssignLinode,
     validateOnBlur: false,
     validateOnChange: false,
   });
 
-  const getLinodeConfigData = React.useCallback(async (linode: Linode | null) => {
-    if (linode) {
-      const data = await getAllLinodeConfigs(linode.id);
-      setLinodeConfigs(data);
-    }
-  }, []);
+  const getLinodeConfigData = React.useCallback(
+    async (linode: Linode | null) => {
+      if (linode) {
+        const data = await getAllLinodeConfigs(linode.id);
+        setLinodeConfigs(data);
+      }
+    },
+    []
+  );
 
   React.useEffect(() => {
-    console.log("are we even reaching here?? useEffect", values)
-    getLinodeConfigData(values.selectedLinode).catch(console.error);
+    getLinodeConfigData(values.selectedLinode).catch();
   }, [values.selectedLinode, getLinodeConfigData]);
-  
+
   React.useEffect(() => {
     if (open) {
       resetForm();
@@ -169,7 +189,11 @@ export const SubnetAssignLinodesDrawer = (props: Props) => {
   }, [open]);
 
   return (
-    <Drawer onClose={onClose} open={open} title={`Assign Linodes to subnet: ${subnet?.label} (${subnet?.ipv4})`}>
+    <Drawer
+      onClose={onClose}
+      open={open}
+      title={`Assign Linodes to subnet: ${subnet?.label} (${subnet?.ipv4})`}
+    >
       {errorMap.none && <Notice text={errorMap.none} variant="error" />}
       {userCannotAssignLinodes && (
         <Notice
@@ -178,72 +202,99 @@ export const SubnetAssignLinodesDrawer = (props: Props) => {
           variant="error"
         />
       )}
-      <Notice variant='warning' text={`${REBOOT_LINODE_MESSAGE}`} />
+      <Notice variant="warning" text={`${REBOOT_LINODE_MESSAGE}`} />
       <form onSubmit={handleSubmit}>
         <FormHelperText>{REGIONAL_LINODE_MESSAGE}</FormHelperText>
-        <Autocomplete 
+        <Autocomplete
           disabled={userCannotAssignLinodes}
           inputValue={values.selectedLinode?.label || ''}
           label={'Linodes'}
-          onChange={(_, value: Linode) => setFieldValue('selectedLinode', value)}
+          onChange={(_, value: Linode) =>
+            setFieldValue('selectedLinode', value)
+          }
           // figure out a way to optimize this
-          options={linodes?.filter((linode) => !assignedLinodesAndConfigData.map(data => data.id).includes(linode.id) && !subnet?.linodes.includes(linode.id)) ?? []}
-          placeholder='Select Linodes or type to search'
+          options={
+            linodes?.filter(
+              (linode) =>
+                !assignedLinodesAndConfigData
+                  .map((data) => data.id)
+                  .includes(linode.id) && !subnet?.linodes.includes(linode.id)
+            ) ?? []
+          }
+          placeholder="Select Linodes or type to search"
           value={values.selectedLinode || null}
-          sx={{ marginBottom: '8px'}}
+          sx={{ marginBottom: '8px' }}
         />
-        <Checkbox 
+        <Checkbox
           checked={autoAssignIPv4}
           disabled={userCannotAssignLinodes}
           text={'Auto-assign a VPC IPv4 address for this Linode'}
-          toolTipText={'A range of non-internet facing IP used in an internal network'}
-          onChange={(_) => setAutoAssignIPv4(!autoAssignIPv4)} 
-          sx={{ marginLeft: `2px`, marginTop: `8px`}}
+          toolTipText={
+            'A range of non-internet facing IP used in an internal network'
+          }
+          onChange={(_) => setAutoAssignIPv4(!autoAssignIPv4)}
+          sx={{ marginLeft: `2px`, marginTop: `8px` }}
         />
-        {(!autoAssignIPv4 && 
-          <TextField 
+        {!autoAssignIPv4 && (
+          <TextField
             disabled={userCannotAssignLinodes}
             errorText={errorMap['ipv4.vpc']}
-            label={'VPC IPv4'} 
+            label={'VPC IPv4'}
             onChange={(e) => setFieldValue('chosenIP', e.target.value)}
-            sx={{marginBottom: '8px'}}
+            sx={{ marginBottom: '8px' }}
             value={values.chosenIP}
           />
         )}
-        {(linodeConfigs.length > 1 && 
+        {linodeConfigs.length > 1 && (
           <>
-            <FormHelperText sx={{ marginTop: `16px`}}>
-              {MULTIPLE_CONFIGURATIONS_MESSAGE} 
+            <FormHelperText sx={{ marginTop: `16px` }}>
+              {MULTIPLE_CONFIGURATIONS_MESSAGE}
               {/* @TODO: VPC - add docs link */}
-              <Link to='#'> Learn more</Link>.
+              <Link to="#"> Learn more</Link>.
             </FormHelperText>
-            <Autocomplete 
+            <Autocomplete
               disabled={userCannotAssignLinodes}
               inputValue={values.selectedConfig?.label || ''}
               label={'Configuration profile'}
-              onChange={(_, value: Config) => setFieldValue('selectedConfig', value)}      
+              onChange={(_, value: Config) =>
+                setFieldValue('selectedConfig', value)
+              }
               options={linodeConfigs}
-              placeholder='Select a configuration profile'
+              placeholder="Select a configuration profile"
               value={values.selectedConfig || null}
             />
           </>
         )}
         <StyledButtonBox>
-          <Button 
-            buttonType='primary' 
+          <Button
+            buttonType="primary"
             disabled={userCannotAssignLinodes || !dirty}
-            type='submit'
+            type="submit"
           >
             Assign Linode
           </Button>
         </StyledButtonBox>
       </form>
-      {/* figure out the typography for this. somehow also need to include list */}
-      <Typography variant="body1"><strong>Linodes Assigned to Subnet ({assignedLinodesAndConfigData.length})</strong></Typography>
-      todo: figure out list and downloading
+      {/* TODO: currently using autocomplete for this (pretty interesting), but will eventually replace with a List + download CSV as well */}
+      <Typography variant="body1">
+        <strong>
+          Linodes Assigned to Subnet ({assignedLinodesAndConfigData.length})
+        </strong>
+      </Typography>
+      <Autocomplete
+        disabled={userCannotAssignLinodes}
+        label={`Linodes Assigned to Subnet (${assignedLinodesAndConfigData.length})`}
+        onChange={(_, value: LinodeAndConfigData) => {
+          // must unassign linode
+        }}
+        options={assignedLinodesAndConfigData}
+        placeholder="Select a Linode to unassign"
+      />
       <StyledButtonBox>
-        <Button buttonType="outlined" onClick={onClose}>Done</Button>
+        <Button buttonType="outlined" onClick={onClose}>
+          Done
+        </Button>
       </StyledButtonBox>
     </Drawer>
   );
-}
+};
