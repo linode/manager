cd37bd66bdac872fadc3b7938d4093e8bb98a63c
mjac0bs <mjacobs@akamai.com>
2023-09-19 14:42:01 -0700

    Add TODO and update price increase map for beta

--- packages/manager/src/utilities/pricing/dynamicPricing.ts
+++ packages/manager/src/utilities/pricing/dynamicPricing.ts
@@ -25,16 +25,17 @@ export const priceIncreaseMap = {
   'id-cgk': 0.2, // Jakarta
 };
 
+// TODO: DC Pricing - M3-6893: Update these values when beta pricing ends.
 export const objectStoragePriceIncreaseMap = {
   'br-gru': {
-    monthly: 7.0,
-    storage_overage: 0.028,
-    transfer_overage: 0.007,
+    monthly: 0.0,
+    storage_overage: 0.0,
+    transfer_overage: 0.0,
   },
   'id-cgk': {
-    monthly: 6.0,
-    storage_overage: 0.024,
-    transfer_overage: 0.015,
+    monthly: 0.0,
+    storage_overage: 0.0,
+    transfer_overage: 0.0,
   },
 };
 
