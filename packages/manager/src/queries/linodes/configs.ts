ed628f72e06d324b6a0fc20cad7a95cc3c5f3b33
Dajahi Wiley <dwiley@linode.com>
2023-09-21 14:28:30 -0400

    Remove now unneeded changes in src/queries/linodes/configs.ts

--- packages/manager/src/queries/linodes/configs.ts
+++ packages/manager/src/queries/linodes/configs.ts
@@ -12,15 +12,11 @@ import {
   deleteLinodeConfigInterface,
   getConfigInterface,
   getConfigInterfaces,
-  getLinodeConfigs,
   updateConfigInterface,
   updateLinodeConfig,
   updateLinodeConfigOrder,
 } from '@linode/api-v4';
-import { Params } from '@linode/api-v4/src/types';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
-
-import { getAll } from 'src/utilities/getAll';
 
 import { queryKey } from './linodes';
 import { getAllLinodeConfigs } from './requests';
@@ -32,14 +28,6 @@ export const useAllLinodeConfigsQuery = (id: number, enabled = true) => {
     { enabled }
   );
 };
-
-export const getAllLinodeConfigsRequest = (
-  linodeID: number,
-  passedParams: Params = {}
-) =>
-  getAll<Config>((params) =>
-    getLinodeConfigs(linodeID, { ...params, ...passedParams })
-  )().then((data) => data.data);
 
 const configQueryKey = 'configs';
 const interfaceQueryKey = 'interfaces';
