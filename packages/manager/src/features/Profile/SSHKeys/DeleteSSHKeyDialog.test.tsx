9a601b6562dffd17e1d9398b5d0e329dece153b5
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2023-09-27 14:00:51 -0700

    fix: [M3-7191] - Missing primary and secondary action button labels in Delete SSH Key dialog and Clone Domain drawer (#9726)
    * Fix missing label in button and make prop required
    
    * Add unit test
    
    * Added changeset: Missing button labels in Delete SSH Key dialog and Clone Domain drawer

---
+++ packages/manager/src/features/Profile/SSHKeys/DeleteSSHKeyDialog.test.tsx
@@ -0,0 +1,68 @@
+import { act, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as React from 'react';
+
+import { rest, server } from 'src/mocks/testServer';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+
+import DeleteSSHKeyDialog from './DeleteSSHKeyDialog';
+
+const props = {
+  id: 0,
+  onClose: jest.fn(),
+  open: true,
+};
+
+describe('DeleteSSHKeyDialog', () => {
+  it('should display all expected dialog titles, primary, and secondary buttons', async () => {
+    const { getByText } = renderWithTheme(<DeleteSSHKeyDialog {...props} />);
+    // Check for title
+    getByText('Delete SSH Key');
+
+    // Check for buttons
+    getByText('Delete');
+    getByText('Cancel');
+  });
+
+  it('should include the SSH key label when provided', () => {
+    const { getByText } = renderWithTheme(
+      <DeleteSSHKeyDialog {...props} label="my-ssh-key" />
+    );
+
+    getByText(/my-ssh-key/);
+  });
+
+  it('should submit and call onClose', async () => {
+    server.use(
+      rest.delete('*/profile/sshkeys/0', (req, res, ctx) => {
+        return res(ctx.json({}));
+      })
+    );
+
+    const { getByTestId } = renderWithTheme(<DeleteSSHKeyDialog {...props} />);
+
+    const submitButton = getByTestId('confirm-delete');
+
+    act(() => {
+      userEvent.click(submitButton);
+    });
+
+    await waitFor(() => {
+      expect(props.onClose).toBeCalled();
+    });
+  });
+
+  it('should Cancel and call onClose', async () => {
+    const { getByTestId } = renderWithTheme(<DeleteSSHKeyDialog {...props} />);
+
+    const cancelButton = getByTestId('cancel-delete');
+
+    act(() => {
+      userEvent.click(cancelButton);
+    });
+
+    await waitFor(() => {
+      expect(props.onClose).toBeCalled();
+    });
+  });
+});
