c3d8b054c924fc8e048fa44739a9e3475853d9c7
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2023-10-02 15:19:40 -0400

    feat: [M3-7109] - Add AGLB Delete Route Dialog  (#9735)
    * add delete functionality
    
    * Added changeset: Add AGLB Route Delete Dialog
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

--- packages/manager/src/queries/aglb/routes.ts
+++ packages/manager/src/queries/aglb/routes.ts
@@ -1,5 +1,5 @@
-import { getLoadbalancerRoutes } from '@linode/api-v4';
-import { useQuery } from 'react-query';
+import { deleteLoadbalancerRoute, getLoadbalancerRoutes } from '@linode/api-v4';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
 import { QUERY_KEY } from './loadbalancers';
 
@@ -20,5 +20,25 @@ export const useLoadBalancerRoutesQuery = (
     [QUERY_KEY, 'loadbalancer', id, 'routes', params, filter],
     () => getLoadbalancerRoutes(id, params, filter),
     { keepPreviousData: true }
+  );
+};
+
+export const useLoadBalancerRouteDeleteMutation = (
+  loadbalancerId: number,
+  routeId: number
+) => {
+  const queryClient = useQueryClient();
+  return useMutation<{}, APIError[]>(
+    () => deleteLoadbalancerRoute(loadbalancerId, routeId),
+    {
+      onSuccess() {
+        queryClient.invalidateQueries([
+          QUERY_KEY,
+          'loadbalancer',
+          loadbalancerId,
+          'routes',
+        ]);
+      },
+    }
   );
 };
