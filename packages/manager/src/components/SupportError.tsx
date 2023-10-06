0b8f4a4a89fa8bfecc7b6eda34cd5896fbb0ded8
Dave Green <dgreen@linode.com>
2023-10-06 15:24:00 -0400

    feat: [SD-449] - update support error links
    * added ticket page link to errors containing the string "contact support"

--- packages/manager/src/components/SupportError.tsx
+++ packages/manager/src/components/SupportError.tsx
@@ -12,7 +12,9 @@ interface Props {
 export const SupportError = (props: Props) => {
   const theme = useTheme();
   const { errors } = props;
-  const errorMsg = errors[0].reason.split(/(open a support ticket)/i);
+  const errorMsg = errors[0].reason.split(
+    /(open a support ticket|contact support)/i
+  );
 
   return (
     <Typography
@@ -24,6 +26,7 @@ export const SupportError = (props: Props) => {
     >
       {errorMsg.map((substring: string, idx) => {
         const openTicket = substring.match(/open a support ticket/i);
+        const contact = substring.match(/contact support/i);
         if (openTicket) {
           return (
             <SupportLink
@@ -31,6 +34,17 @@ export const SupportError = (props: Props) => {
                 substring.match(/Open.*/)
                   ? 'Open a support ticket'
                   : 'open a support ticket'
+              }
+              key={`${substring}-${idx}`}
+            />
+          );
+        } else if (contact) {
+          return (
+            <SupportLink
+              text={
+                substring.match(/Contact.*/)
+                  ? 'Contact support'
+                  : 'contact support'
               }
               key={`${substring}-${idx}`}
             />
