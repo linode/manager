228f32d70d45accf917300eb06b3b0095b182885
Alban Bailly <abailly@akamai.com>
2023-10-06 15:58:17 -0400

    Implement new UI

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeResize/LinodeResizeUnifiedMigrationPanel.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeResize/LinodeResizeUnifiedMigrationPanel.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import { Box } from 'src/components/Box';
 import { FormControl } from 'src/components/FormControl';
 import { FormControlLabel } from 'src/components/FormControlLabel';
-import { FormLabel } from 'src/components/FormLabel';
 import { Radio } from 'src/components/Radio/Radio';
 import { RadioGroup } from 'src/components/RadioGroup';
 import { TooltipIcon } from 'src/components/TooltipIcon';
@@ -14,6 +13,9 @@ import { capitalize } from 'src/utilities/capitalize';
 import type { MigrationTypes } from '@linode/api-v4/lib/linodes';
 import type { ResizeLinodePayload } from '@linode/api-v4/lib/linodes/types';
 import type { FormikProps } from 'formik';
+import { Divider } from 'src/components/Divider';
+
+const RADIO_GROUP_ID = 'resize-migration-types';
 
 interface Props {
   formik: FormikProps<ResizeLinodePayload>;
@@ -26,69 +28,73 @@ export const UnifiedMigrationPanel = (props: Props) => {
   const theme = useTheme();
 
   return (
-    <Box
-      sx={{
-        backgroundColor: theme.bg.offWhite,
-        marginBottom: '8px',
-        padding: '16px 22px 0 22px',
-      }}
-    >
-      <Typography variant="h2">Choose Your Resize Type</Typography>
-      <Box
-        sx={{
-          marginTop: 1,
-        }}
-        component="p"
-      >
-        During a <strong>warm resize</strong>, your Compute Instance will remain
-        up and running for the duration of the process. It will stay up while it
-        is migrated to a new host machine and then rebooted to complete the warm
-        resize. In some cases, you will need to reboot the instance manually
-        (you will receive a notification to do so if necessary). During a{' '}
-        <strong>cold resize</strong>, your Compute Instance will be shut down,
-        migrated to a new host machine, and restored to its previous state
-        (booted or powered off) once the resize is complete.
-      </Box>
-      <FormControl sx={{ marginTop: 0 }}>
-        <FormLabel id="resize-migration-types" sx={{ marginBottom: 0 }}>
-          Resize Types
-        </FormLabel>
+    <Box>
+      <Typography id={RADIO_GROUP_ID} variant="h2">
+        Resize Type
+      </Typography>
+      <FormControl>
         <RadioGroup
-          aria-labelledby="resize-migration-types"
+          aria-labelledby={RADIO_GROUP_ID}
           onChange={(e, value) => formik.setFieldValue('migration_type', value)}
           row
           sx={{ marginTop: '0 !important' }}
           value={formik.values.migration_type}
         >
-          <FormControlLabel
-            control={<Radio />}
-            data-qa-radio={migrationTypeOptions.warm}
-            disabled={isLinodeOffline}
-            key={migrationTypeOptions.warm}
-            label={`${capitalize(migrationTypeOptions.warm)} resize`}
-            value={migrationTypeOptions.warm}
-          />
-          <FormControlLabel
-            control={<Radio />}
-            data-qa-radio={migrationTypeOptions.cold}
-            disabled={isLinodeOffline}
-            key={migrationTypeOptions.cold}
-            label={`${capitalize(migrationTypeOptions.cold)} resize`}
-            value={migrationTypeOptions.cold}
-          />
-          {isLinodeOffline && (
+          <Box width="100%">
+            <FormControlLabel
+              control={<Radio />}
+              data-qa-radio={migrationTypeOptions.warm}
+              disabled={isLinodeOffline}
+              key={migrationTypeOptions.warm}
+              label={`${capitalize(migrationTypeOptions.warm)} resize`}
+              value={migrationTypeOptions.warm}
+            />
             <TooltipIcon
               sxTooltipIcon={{
                 marginLeft: '-15px',
               }}
+              text={
+                <>
+                  During a <strong>warm resize</strong>, your Linode will remain
+                  up and running for the duration of the process and will be
+                  rebooted to complete the resize.{' '}
+                  <strong>(Recommended)</strong>
+                </>
+              }
               status="help"
-              text={`The warm resize option is currently disabled in this context as the instance is offline, and a cold resize is required to initiate the instance restart.`}
               tooltipPosition="right"
-              width={300}
+              width={[theme.breakpoints.up('sm')] ? 375 : 300}
             />
-          )}
+          </Box>
+          <Box width="100%">
+            <FormControlLabel
+              control={<Radio />}
+              data-qa-radio={migrationTypeOptions.cold}
+              disabled={isLinodeOffline}
+              key={migrationTypeOptions.cold}
+              label={`${capitalize(migrationTypeOptions.cold)} resize`}
+              value={migrationTypeOptions.cold}
+            />
+            <TooltipIcon
+              sxTooltipIcon={{
+                marginLeft: '-15px',
+              }}
+              text={
+                <>
+                  During a <strong>cold resize</strong>, your Linode will be
+                  shut down, migrated to a new host machine, and restored to its
+                  previous state (booted or powered off) once the resize is
+                  complete.
+                </>
+              }
+              status="help"
+              tooltipPosition="right"
+              width={[theme.breakpoints.up('sm')] ? 450 : 300}
+            />
+          </Box>
         </RadioGroup>
       </FormControl>
+      <Divider />
     </Box>
   );
 };
