722e9aa36b6857cb5c28aa9b0287c2a932698116
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-10-10 09:29:37 -0400

    UI Updates based on review meeting

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeResize/LinodeResizeUnifiedMigrationPanel.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeResize/LinodeResizeUnifiedMigrationPanel.tsx
@@ -2,8 +2,10 @@ import { useTheme } from '@mui/material/styles';
 import * as React from 'react';
 
 import { Box } from 'src/components/Box';
+import { Divider } from 'src/components/Divider';
 import { FormControl } from 'src/components/FormControl';
 import { FormControlLabel } from 'src/components/FormControlLabel';
+import { Link } from 'src/components/Link';
 import { Radio } from 'src/components/Radio/Radio';
 import { RadioGroup } from 'src/components/RadioGroup';
 import { TooltipIcon } from 'src/components/TooltipIcon';
@@ -13,7 +15,6 @@ import { capitalize } from 'src/utilities/capitalize';
 import type { MigrationTypes } from '@linode/api-v4/lib/linodes';
 import type { ResizeLinodePayload } from '@linode/api-v4/lib/linodes/types';
 import type { FormikProps } from 'formik';
-import { Divider } from 'src/components/Divider';
 
 const RADIO_GROUP_ID = 'resize-migration-types';
 
@@ -40,7 +41,7 @@ export const UnifiedMigrationPanel = (props: Props) => {
           sx={{ marginTop: '0 !important' }}
           value={formik.values.migration_type}
         >
-          <Box width="100%">
+          <Box alignItems="center" display="flex" width="100%">
             <FormControlLabel
               control={<Radio />}
               data-qa-radio={migrationTypeOptions.warm}
@@ -49,18 +50,25 @@ export const UnifiedMigrationPanel = (props: Props) => {
               label={`${capitalize(migrationTypeOptions.warm)} resize`}
               value={migrationTypeOptions.warm}
             />
-            <TooltipIcon
-              sxTooltipIcon={{
-                marginLeft: '-15px',
+            <Box
+              sx={{
+                marginLeft: '-10px',
               }}
+            >
+              <strong>(Recommended)</strong>
+            </Box>
+            <TooltipIcon
               text={
                 <>
                   During a <strong>warm resize</strong>, your Linode will remain
                   up and running for the duration of the process and will be
                   rebooted to complete the resize.{' '}
-                  <strong>(Recommended)</strong>
+                  <Link to="https://www.linode.com/docs/products/compute/compute-instances/guides/resize/">
+                    Learn more.
+                  </Link>
                 </>
               }
+              interactive
               status="help"
               tooltipPosition="right"
               width={[theme.breakpoints.up('sm')] ? 375 : 300}
@@ -84,9 +92,13 @@ export const UnifiedMigrationPanel = (props: Props) => {
                   During a <strong>cold resize</strong>, your Linode will be
                   shut down, migrated to a new host machine, and restored to its
                   previous state (booted or powered off) once the resize is
-                  complete.
+                  complete.{' '}
+                  <Link to="https://www.linode.com/docs/products/compute/compute-instances/guides/resize/">
+                    Learn more.
+                  </Link>
                 </>
               }
+              interactive
               status="help"
               tooltipPosition="right"
               width={[theme.breakpoints.up('sm')] ? 450 : 300}
