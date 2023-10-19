0af912ca2b468ff69d43e8e9bdfa9d1ad9ed573c
Hana Xu <hxu@akamai.com>
2023-10-19 13:28:18 -0400

    fix multiple select bug

--- packages/manager/src/features/VPCs/VPCDetail/SubnetUnassignLinodesDrawer.tsx
+++ packages/manager/src/features/VPCs/VPCDetail/SubnetUnassignLinodesDrawer.tsx
@@ -303,7 +303,6 @@ export const SubnetUnassignLinodesDrawer = React.memo(
               <Autocomplete
                 disabled={userCannotUnassignLinodes}
                 errorText={linodesError ? linodesError[0].reason : undefined}
-                // isOptionEqualToValue={() => true} // Ignore the multi-select warning since it isn't helpful https://github.com/mui/material-ui/issues/29727
                 label="Linodes"
                 multiple
                 onChange={(_, value) => setSelectedLinodes(value)}
