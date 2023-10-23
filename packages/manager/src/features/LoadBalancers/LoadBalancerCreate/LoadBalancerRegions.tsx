c767907262e92e151e44f5c321f30ef6f95fc9cd
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-10-23 15:20:29 -0500

    Render Regions as per the mockup

--- packages/manager/src/features/LoadBalancers/LoadBalancerCreate/LoadBalancerRegions.tsx
+++ packages/manager/src/features/LoadBalancers/LoadBalancerCreate/LoadBalancerRegions.tsx
@@ -1,7 +1,22 @@
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Unstable_Grid2';
 import { useTheme } from '@mui/material/styles';
 import * as React from 'react';
 
+import { BetaChip } from 'src/components/BetaChip/BetaChip';
+import { Country } from 'src/components/EnhancedSelect/variants/RegionSelect/utils';
+import { Flag } from 'src/components/Flag';
 import { Paper } from 'src/components/Paper';
+import { Typography } from 'src/components/Typography';
+
+// TODO: Regious should be updated with the real values
+const regions = [
+  { country: 'ga', id: 'us-southeast', label: 'Atlanta, GA' },
+  { country: 'ca', id: 'ca-central', label: 'Toronto, CA' },
+  { country: 'au', id: 'ap-southeast', label: 'Sydney, AU' },
+  { country: 'gb', id: 'eu-west', label: 'London, UK' },
+  { country: 'fr', id: 'fr-par', label: 'Paris, FR' },
+];
 
 export const LoadBalancerRegions = () => {
   const theme = useTheme();
@@ -14,7 +29,45 @@ export const LoadBalancerRegions = () => {
       }}
       data-qa-label-header
     >
-      TODO: Implement Regions Field.
+      <Typography
+        data-qa-tp-title
+        sx={(theme) => ({ marginBottom: theme.spacing(2) })}
+        variant="h2"
+      >
+        Regions
+      </Typography>
+      <Stack>
+        <Typography sx={(theme) => ({ marginBottom: theme.spacing(1) })}>
+          Where this Load Balancer instance will be deployed.
+        </Typography>
+        <Grid container>
+          <BetaChip sx={{ marginLeft: '0 !important' }} />
+          <Typography sx={(theme) => ({ marginBottom: theme.spacing(1) })}>
+            Load Balancers will be automatically provisioned in these 5 Regions.
+            No charges with be incured.
+          </Typography>
+        </Grid>
+        <Grid marginTop={theme.spacing(2)}>
+          {regions.map((region) => (
+            <Grid
+              alignItems={'center'}
+              container
+              direction="row"
+              justifyContent="flex-start"
+              key={region.id}
+              marginBottom={theme.spacing(1)}
+              spacing={2}
+            >
+              <Grid className="py0">
+                <Flag country={region.country as Lowercase<Country>} />
+              </Grid>
+              <Grid>
+                <Typography>{`${region.label} (${region.id})`}</Typography>
+              </Grid>
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
     </Paper>
   );
 };
