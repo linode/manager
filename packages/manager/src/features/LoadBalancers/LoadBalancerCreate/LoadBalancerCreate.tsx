0d772415a61f389bb21b5f1e59ee68a255ecfbe9
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-10-23 11:46:09 -0500

    Add Actions buttons

--- packages/manager/src/features/LoadBalancers/LoadBalancerCreate/LoadBalancerCreate.tsx
+++ packages/manager/src/features/LoadBalancers/LoadBalancerCreate/LoadBalancerCreate.tsx
@@ -1,12 +1,23 @@
 import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
 
+import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
+import { Button } from 'src/components/Button/Button';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle/DocumentTitle';
 import { LandingHeader } from 'src/components/LandingHeader';
 
 import { LoadBalancerConfiguration } from './LoadBalancerConfiguration';
 import { LoadBalancerLabel } from './LoadBalancerLable';
 import { LoadBalancerRegions } from './LoadBalancerRegions';
+
+export const StyledAddConfigurationButton = styled(Button, {
+  label: 'StyledAddConfigurationButton',
+})(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 const LoadBalancerCreate = () => {
   return (
@@ -24,6 +35,38 @@ const LoadBalancerCreate = () => {
       />
       <LoadBalancerRegions />
       <LoadBalancerConfiguration />
+      {/* TODO:
+       * Implement Review Load Balancer Action Behavior
+       * Implement Cancel Behavior
+       * Implement Add Another Configuration Behavior
+       */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginTop: '16px',
+          width: '100%',
+        }}
+      >
+        <div>
+          <StyledAddConfigurationButton
+            buttonType="outlined"
+            data-qa-api-cli-linode
+            onClick={() => null}
+          >
+            Add Another Configuration
+          </StyledAddConfigurationButton>
+        </div>
+
+        <ActionsPanel
+          primaryButtonProps={{
+            label: 'Review Load Balancer',
+            onClick: () => null,
+          }}
+          secondaryButtonProps={{ label: 'Cancel', onClick: () => null }}
+          sx={{ margin: 0, padding: 0 }}
+        />
+      </div>
     </Grid>
   );
 };
