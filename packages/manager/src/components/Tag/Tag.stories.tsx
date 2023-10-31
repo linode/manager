a6b17ec15c29bf03d2bfc166cb96f13acaf0ff77
Alban Bailly <130582365+abailly-akamai@users.noreply.github.com>
2023-10-31 15:47:11 -0400

    change: [M3-6522] - Tag Component v7 story & cleanup (#9840)
    * Tag v7 story & cleanup
    
    * cleanup
    
    * Added changeset: Tag Component v7 Story
    
    * Feedback

---
+++ packages/manager/src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Tag } from './Tag';
+
+import type { TagProps } from './Tag';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export const Default: StoryObj<TagProps> = {
+  render: (args: TagProps) => <Tag {...args} />,
+};
+
+export const WithMaxLength: StoryObj<TagProps> = {
+  render: (args: TagProps) => (
+    <Tag {...args} label="Long Label" maxLength={5} />
+  ),
+};
+
+const meta: Meta<TagProps> = {
+  args: {
+    colorVariant: 'lightBlue',
+    label: 'Tag',
+  },
+  component: Tag,
+  title: 'Components/Chip/Tag',
+};
+export default meta;
