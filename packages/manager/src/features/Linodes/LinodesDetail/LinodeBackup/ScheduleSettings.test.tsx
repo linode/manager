5fb224074a0b6d1bb34283fd2a10fbb2a616d7c4
Banks Nussman <banks@nussman.us>
2023-11-06 12:46:06 -0500

    improve mocks and tests

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeBackup/ScheduleSettings.test.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeBackup/ScheduleSettings.test.tsx
@@ -1,3 +1,4 @@
+import { Settings } from 'luxon';
 import * as React from 'react';
 
 import { profileFactory } from 'src/factories';
@@ -6,7 +7,6 @@ import { rest, server } from 'src/mocks/testServer';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
 import { ScheduleSettings } from './ScheduleSettings';
-import { DateTime } from 'luxon';
 
 describe('ScheduleSettings', () => {
   it('renders heading and copy', async () => {
@@ -63,7 +63,7 @@ describe('ScheduleSettings', () => {
     await findByText('Time displayed in utc');
   });
 
-  it('renders with the linode schedule taking into account the user timezone (America/New_York)', async () => {
+  it('renders with the linode schedule taking into account the user timezone (America/New_York) (EDT)', async () => {
     server.use(
       rest.get('*/linode/instances/1', (req, res, ctx) => {
         return res(
@@ -88,18 +88,59 @@ describe('ScheduleSettings', () => {
       })
     );
 
+    // Mock that today's date is May 25th, 2018 so that it is daylight savings time
+    Settings.now = () => new Date(2018, 4, 25).valueOf();
+
     const { findByText } = renderWithTheme(
       <ScheduleSettings isReadOnly={false} linodeId={1} />
     );
 
     await findByText('Wednesday');
 
-    // Because of Daylight savings time, offset will be -4 or -5.
-    const offset = DateTime.now().setZone('America/New_York').offset / 60;
+    // W10 indicates that your backups should be taken between 10:00 and 12:00 UTC.
+    // W10 in America/New_York during daylight savings is 06:00 - 08:00 EDT (UTC-4).
+    await findByText(`06:00 - 08:00`);
+
+    await findByText('Time displayed in America/New York');
+  });
+
+  it('renders with the linode schedule taking into account the user timezone (America/New_York) (EST)', async () => {
+    server.use(
+      rest.get('*/linode/instances/1', (req, res, ctx) => {
+        return res(
+          ctx.json(
+            linodeFactory.build({
+              backups: {
+                enabled: true,
+                schedule: {
+                  day: 'Wednesday',
+                  window: 'W10',
+                },
+              },
+              id: 1,
+            })
+          )
+        );
+      }),
+      rest.get('*/profile', (req, res, ctx) => {
+        return res(
+          ctx.json(profileFactory.build({ timezone: 'America/New_York' }))
+        );
+      })
+    );
+
+    // Mock that today's date is Nov 7th, 2023 so that it is *not* daylight savings time
+    Settings.now = () => new Date(2023, 11, 7).valueOf();
+
+    const { findByText } = renderWithTheme(
+      <ScheduleSettings isReadOnly={false} linodeId={1} />
+    );
+
+    await findByText('Wednesday');
 
     // W10 indicates that your backups should be taken between 10:00 and 12:00 UTC.
-    // W10 in America/New_York will be 06:00 - 08:00 EDT (UTC-4) or 05:00 - 07:00 EST (UTC-5).
-    await findByText(`0${10 + offset}:00 - 0${10 + 2 + offset}:00`);
+    // W10 in America/New_York when it is not daylight savings is 05:00 - 07:00 EST (UTC-5).
+    await findByText(`05:00 - 07:00`);
 
     await findByText('Time displayed in America/New York');
   });
