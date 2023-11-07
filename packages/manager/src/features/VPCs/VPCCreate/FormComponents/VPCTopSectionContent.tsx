8b7cb3a66d1f44538e0de717bf3dd2d57b8fef0e
Connie Liu <139280159+coliu-akamai@users.noreply.github.com>
2023-11-07 10:27:54 -0500

    feat: [M3-7284, M3-7361] - Create VPC drawer in Linode Create flow; Update widths for Create VPC and Create Firewall buttons (#9847)
    * update api types and fix resulting errors
    
    * add changesets
    
    * increase timeout of specific test?
    
    * Update packages/manager/.changeset/pr-9824-upcoming-features-1697827056392.md
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    
    * Update packages/api-v4/.changeset/pr-9824-upcoming-features-1697827003419.md
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    
    * address feedback for factories
    
    * fix array size for subnet factories, update naming
    
    * remove extra timeout
    
    * fix cypress test: make mock subnet start with 0 linodes
    
    * initial commit
    
    * initial commit v2
    
    * undo some changes, looking around
    
    * need to fix styling and abstract everything but functionality exists
    
    * fix VPCCreate styling as per ticket
    
    * update spacing drawer
    
    * create custom hook to abstract create VPC logic
    
    * update custom useCreateVPC hook logic
    
    * first of the form abstractions
    
    * abstract form components
    
    * styling of drawer
    
    * changeset + some tests
    
    * tests + update naming
    
    * ensure that at least 1 subnet must be created for createVPC drawer
    
    * some cleanup
    
    * hide create link if vpc is not supported in region
    
    * fix label that caused cypress failure
    
    * address feedback p1
    
    * feedback pt2
    
    * small bit of cleanup
    
    * Update packages/manager/src/features/VPCs/VPCCreate/SubnetNode.tsx
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    
    * feedback p1
    
    * renamed file + updated imports
    
    * update test comments
    
    * remove bottom margin (see m3-7361)
    
    * vpc panel unit tests
    
    * Update pr-9847-upcoming-features-1698425054230.md
    
    * fix: `ScheduleSettings.test.tsx` failing due to daylight savings time ending (#9876)
    
    * use utc for unit test
    
    * more strict assertion
    
    * support est and edt
    
    * fix comment
    
    * improve mocks and tests
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>
    
    * remove refetch on windows focus for vpc query
    
    * update comment
    
    ---------
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    Co-authored-by: Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
    Co-authored-by: Banks Nussman <banks@nussman.us>

---
+++ packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.tsx
@@ -0,0 +1,63 @@
+import { Region } from '@linode/api-v4';
+import { FormikErrors } from 'formik';
+import * as React from 'react';
+
+import { RegionSelect } from 'src/components/EnhancedSelect/variants/RegionSelect';
+import { Link } from 'src/components/Link';
+import { TextField } from 'src/components/TextField';
+import { CreateVPCFieldState } from 'src/hooks/useCreateVPC';
+
+import { VPC_CREATE_FORM_VPC_HELPER_TEXT } from '../../constants';
+import { StyledBodyTypography } from './VPCCreateForm.styles';
+
+interface Props {
+  disabled?: boolean;
+  errors: FormikErrors<CreateVPCFieldState>;
+  isDrawer?: boolean;
+  onChangeField: (field: string, value: string) => void;
+  regions: Region[];
+  values: CreateVPCFieldState;
+}
+
+export const VPCTopSectionContent = (props: Props) => {
+  const { disabled, errors, isDrawer, onChangeField, regions, values } = props;
+  return (
+    <>
+      <StyledBodyTypography isDrawer={isDrawer} variant="body1">
+        {VPC_CREATE_FORM_VPC_HELPER_TEXT}
+        <Link to="#"> Learn more</Link>.{/* @TODO VPC: learn more link here */}
+      </StyledBodyTypography>
+      <RegionSelect
+        aria-label="Choose a region"
+        disabled={isDrawer ? true : disabled}
+        errorText={errors.region}
+        handleSelection={(region: string) => onChangeField('region', region)}
+        isClearable
+        regions={regions}
+        selectedID={values.region}
+      />
+      <TextField
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          onChangeField('label', e.target.value)
+        }
+        aria-label="Enter a label"
+        disabled={disabled}
+        errorText={errors.label}
+        label="VPC Label"
+        value={values.label}
+      />
+      <TextField
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          onChangeField('description', e.target.value)
+        }
+        disabled={disabled}
+        errorText={errors.description}
+        label="Description"
+        maxRows={1}
+        multiline
+        optional
+        value={values.description}
+      />
+    </>
+  );
+};
