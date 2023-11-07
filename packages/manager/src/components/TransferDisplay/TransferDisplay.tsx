3893e96fa957edfdc0d506f03a86408fc34c3d9a
mjac0bs <mjacobs@akamai.com>
2023-11-07 07:54:39 -0800

    Remove flag from backups

--- packages/manager/src/components/TransferDisplay/TransferDisplay.tsx
+++ packages/manager/src/components/TransferDisplay/TransferDisplay.tsx
@@ -3,12 +3,10 @@ import * as React from 'react';
 import { Box } from 'src/components/Box';
 import { CircleProgress } from 'src/components/CircleProgress';
 import { Typography } from 'src/components/Typography';
-import { useFlags } from 'src/hooks/useFlags';
 import { useAccountTransfer } from 'src/queries/accountTransfer';
 import { useRegionsQuery } from 'src/queries/regions';
 
 import { StyledLinkButton } from '../Button/StyledLinkButton';
-import { LegacyTransferDisplay } from './LegacyTransferDisplay';
 import { StyledTransferDisplayContainer } from './TransferDisplay.styles';
 import { TransferDisplayDialog } from './TransferDisplayDialog';
 import {
