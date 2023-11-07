70cde072b2a2f575fc45d77bcf5f7d6ee9e102f4
Connie Liu <139280159+coliu-akamai@users.noreply.github.com>
2023-11-07 10:27:54 -0500

    feat: [M3-7284, M3-7361] - Create VPC drawer in Linode Create flow; Update widths for Create VPC and Create Firewall buttons (#9847)
    * update api types and fix resulting errors
    
    * add changesets
    
    * increase timeout of specific test?
    
    * Update packages/manager/.changeset/pr-9824-upcoming-features-1697827056392.md
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    
    * Update packages/api-v4/.changeset/pr-9824-upcoming-features-1697827003419.md
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    
    * address feedback for factories
    
    * fix array size for subnet factories, update naming
    
    * remove extra timeout
    
    * fix cypress test: make mock subnet start with 0 linodes
    
    * initial commit
    
    * initial commit v2
    
    * undo some changes, looking around
    
    * need to fix styling and abstract everything but functionality exists
    
    * fix VPCCreate styling as per ticket
    
    * update spacing drawer
    
    * create custom hook to abstract create VPC logic
    
    * update custom useCreateVPC hook logic
    
    * first of the form abstractions
    
    * abstract form components
    
    * styling of drawer
    
    * changeset + some tests
    
    * tests + update naming
    
    * ensure that at least 1 subnet must be created for createVPC drawer
    
    * some cleanup
    
    * hide create link if vpc is not supported in region
    
    * fix label that caused cypress failure
    
    * address feedback p1
    
    * feedback pt2
    
    * small bit of cleanup
    
    * Update packages/manager/src/features/VPCs/VPCCreate/SubnetNode.tsx
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    
    * feedback p1
    
    * renamed file + updated imports
    
    * update test comments
    
    * remove bottom margin (see m3-7361)
    
    * vpc panel unit tests
    
    * Update pr-9847-upcoming-features-1698425054230.md
    
    * fix: `ScheduleSettings.test.tsx` failing due to daylight savings time ending (#9876)
    
    * use utc for unit test
    
    * more strict assertion
    
    * support est and edt
    
    * fix comment
    
    * improve mocks and tests
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>
    
    * remove refetch on windows focus for vpc query
    
    * update comment
    
    ---------
    
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
    Co-authored-by: Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
    Co-authored-by: Banks Nussman <banks@nussman.us>

---
+++ packages/manager/.changeset/pr-9847-upcoming-features-1698425054230.md
@@ -0,0 +1,5 @@
+---
+"@linode/manager": Upcoming Features
+---
+
+Add Create VPC drawer to Linode Create flow and update Create Firewall button width ([#9847](https://github.com/linode/manager/pull/9847))
---
+++ packages/manager/src/features/VPCs/VPCCreate/FormComponents/SubnetContent.tsx
@@ -0,0 +1,55 @@
+import { APIError } from '@linode/api-v4';
+import * as React from 'react';
+
+import { Link } from 'src/components/Link';
+import { Notice } from 'src/components/Notice/Notice';
+import { SubnetFieldState } from 'src/utilities/subnets';
+
+import { MultipleSubnetInput } from '../MultipleSubnetInput';
+import {
+  StyledBodyTypography,
+  StyledHeaderTypography,
+} from './VPCCreateForm.styles';
+
+import { VPC_CREATE_FORM_SUBNET_HELPER_TEXT } from '../../constants';
+
+interface Props {
+  disabled?: boolean;
+  isDrawer?: boolean;
+  onChangeField: (field: string, value: SubnetFieldState[]) => void;
+  subnetErrors?: APIError[];
+  subnets: SubnetFieldState[];
+}
+
+export const SubnetContent = (props: Props) => {
+  const { disabled, isDrawer, onChangeField, subnetErrors, subnets } = props;
+
+  return (
+    <>
+      <StyledHeaderTypography isDrawer={isDrawer} variant="h2">
+        Subnets
+      </StyledHeaderTypography>
+      <StyledBodyTypography isDrawer={isDrawer} variant="body1">
+        {VPC_CREATE_FORM_SUBNET_HELPER_TEXT}
+        <Link to="#"> Learn more</Link>.
+        {/* @TODO VPC: subnet learn more link here */}
+      </StyledBodyTypography>
+      {subnetErrors
+        ? subnetErrors.map((apiError: APIError) => (
+            <Notice
+              key={apiError.reason}
+              spacingBottom={8}
+              text={apiError.reason}
+              variant="error"
+            />
+          ))
+        : null}
+      <MultipleSubnetInput
+        disabled={disabled}
+        isDrawer={isDrawer}
+        onChange={(subnets) => onChangeField('subnets', subnets)}
+        subnets={subnets}
+      />
+    </>
+  );
+};
