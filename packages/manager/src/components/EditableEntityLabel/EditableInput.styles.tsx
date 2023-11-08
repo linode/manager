23bf3c963edd843de05f873cbeb642af32b48eb6
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-11-08 06:37:21 -0500

    refactor: [M3-6271] - MUI v5 Migration - SRC > Components > EditableInput

--- packages/manager/src/components/EditableEntityLabel/EditableInput.styles.tsx
+++ packages/manager/src/components/EditableEntityLabel/EditableInput.styles.tsx
@@ -3,11 +3,7 @@ import { styled } from '@mui/material/styles';
 
 import { Box } from 'src/components/Box';
 import { Button } from 'src/components/Button/Button';
-import { TextField, TextFieldProps } from 'src/components/TextField';
 import { Typography } from 'src/components/Typography';
-import { fadeIn } from 'src/styles/keyframes';
-
-import { EditableInputProps } from './EditableInput';
 
 export const StyledTypography = styled(Typography)(({ theme, ...props }) => ({
   ...(!props.className && {
@@ -70,33 +66,4 @@ export const StyledEditingContainer = styled(Box)(() => ({
   gap: 2,
   justifyContent: 'flex-start',
   position: 'relative',
-}));
-
-export const StyledTextField = styled(TextField)<
-  Partial<EditableInputProps> & TextFieldProps
->(({ theme, ...props }) => ({
-  '& .MuiInputBase-input': {
-    padding: '5px 8px',
-    ...theme.typography.body1,
-    ...(props.typeVariant === 'h1' && {
-      ...theme.typography.h1,
-    }),
-    ...(props.typeVariant === 'h2' && {
-      ...theme.typography.h2,
-    }),
-  },
-  '& .MuiInputBase-root': {
-    backgroundColor: 'transparent',
-    borderColor: `${theme.palette.primary.main} !important`,
-    boxShadow: 'none',
-    maxWidth: 170,
-    minHeight: 40,
-    [theme.breakpoints.up('md')]: {
-      maxWidth: 415,
-      width: '100%',
-    },
-  },
-  animation: `${fadeIn} .3s ease-in-out forwards`,
-  margin: 0,
-  opacity: 0,
 }));
--- packages/manager/src/components/EditableEntityLabel/EditableInput.tsx
+++ packages/manager/src/components/EditableEntityLabel/EditableInput.tsx
@@ -1,17 +1,19 @@
 import Check from '@mui/icons-material/Check';
 import Close from '@mui/icons-material/Close';
+import { useTheme } from '@mui/material/styles';
 import * as React from 'react';
 
 import { ClickAwayListener } from 'src/components/ClickAwayListener';
 import { IconButton } from 'src/components/IconButton';
-import { TextFieldProps } from 'src/components/TextField';
+import { TextField, TextFieldProps } from 'src/components/TextField';
+import { fadeIn } from 'src/styles/keyframes';
 
 import {
   StyledButton,
   StyledEdit,
   StyledEditingContainer,
   StyledTextContainer,
-  StyledTextField,
+  // StyledTextField,
   StyledTypography,
 } from './EditableInput.styles';
 
@@ -51,6 +53,8 @@ export const EditableInput = (props: FilteredEditableInputProps) => {
     ...rest
   } = props;
 
+  const theme = useTheme();
+
   /** confirm or cancel edits if the enter or escape keys are pressed, respectively */
   const handleKeyPress = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter') {
@@ -72,6 +76,35 @@ export const EditableInput = (props: FilteredEditableInputProps) => {
     </StyledTypography>
   );
 
+  const textFieldStyles = {
+    '& .MuiInputBase-input': {
+      padding: '5px 8px',
+      ...theme.typography.body1,
+      ...(props.typeVariant === 'h1' && {
+        ...theme.typography.h1,
+        color: 'red',
+      }),
+      ...(props.typeVariant === 'h2' && {
+        ...theme.typography.h2,
+        color: 'blue',
+      }),
+    },
+    '& .MuiInputBase-root': {
+      backgroundColor: 'transparent',
+      borderColor: `${theme.palette.primary.main} !important`,
+      boxShadow: 'none',
+      maxWidth: 170,
+      minHeight: 40,
+      [theme.breakpoints.up('md')]: {
+        maxWidth: 415,
+        width: '100%',
+      },
+    },
+    animation: `${fadeIn} .3s ease-in-out forwards`,
+    margin: 0,
+    opacity: 0,
+  };
+
   return !isEditing && !errorText ? (
     <StyledTextContainer className={className} data-testid={'editable-text'}>
       {labelText}
@@ -87,7 +120,7 @@ export const EditableInput = (props: FilteredEditableInputProps) => {
   ) : (
     <ClickAwayListener mouseEvent="onMouseDown" onClickAway={cancelEdit}>
       <StyledEditingContainer className={className} data-qa-edit-field>
-        <StyledTextField
+        <TextField
           {...rest}
           // eslint-disable-next-line jsx-a11y/no-autofocus
           autoFocus={true}
@@ -98,6 +131,7 @@ export const EditableInput = (props: FilteredEditableInputProps) => {
           loading={loading}
           onChange={(e: any) => onInputChange(e.target.value)}
           onKeyDown={handleKeyPress}
+          sx={textFieldStyles}
           type="text"
           value={inputText}
         />
