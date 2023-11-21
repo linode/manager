f23d53724386422de520663fded578f9153c3598
Banks Nussman <banks@nussman.us>
2023-11-21 18:11:12 -0500

    add more configuration e2e tests

--- packages/manager/cypress/e2e/core/loadBalancers/load-balancer-configurations.spec.ts
+++ packages/manager/cypress/e2e/core/loadBalancers/load-balancer-configurations.spec.ts
@@ -10,6 +10,10 @@ import {
   routeFactory,
 } from '@src/factories';
 import {
+  mockCreateLoadBalancerConfiguration,
+  mockCreateLoadBalancerConfigurationError,
+  mockDeleteLoadBalancerConfiguration,
+  mockDeleteLoadBalancerConfigurationError,
   mockGetLoadBalancer,
   mockGetLoadBalancerCertificates,
   mockGetLoadBalancerConfigurations,
@@ -43,75 +47,426 @@ describe('Akamai Global Load Balancer configurations page', () => {
       cy.findByText(configuration.label).should('be.visible');
     }
   });
-  it('should create a HTTPS configuration', () => {
-    const loadbalancer = loadbalancerFactory.build();
-    const certificates = certificateFactory.buildList(1);
-    const routes = routeFactory.buildList(1);
+  describe('create', () => {
+    it('creates a HTTPS configuration', () => {
+      const loadbalancer = loadbalancerFactory.build();
+      const certificates = certificateFactory.buildList(1);
+      const routes = routeFactory.buildList(1);
+      const configuration = configurationFactory.build();
 
-    mockAppendFeatureFlags({
-      aglb: makeFeatureFlagData(true),
-    }).as('getFeatureFlags');
-    mockGetFeatureFlagClientstream().as('getClientStream');
-    mockGetLoadBalancer(loadbalancer).as('getLoadBalancer');
-    mockGetLoadBalancerConfigurations(loadbalancer.id, []).as(
-      'getConfigurations'
-    );
-    mockGetLoadBalancerCertificates(loadbalancer.id, certificates);
-    mockGetLoadBalancerRoutes(loadbalancer.id, routes);
+      mockAppendFeatureFlags({
+        aglb: makeFeatureFlagData(true),
+      }).as('getFeatureFlags');
+      mockGetFeatureFlagClientstream().as('getClientStream');
+      mockGetLoadBalancer(loadbalancer).as('getLoadBalancer');
+      mockGetLoadBalancerConfigurations(loadbalancer.id, []).as(
+        'getConfigurations'
+      );
+      mockGetLoadBalancerCertificates(loadbalancer.id, certificates);
+      mockGetLoadBalancerRoutes(loadbalancer.id, routes);
+      mockCreateLoadBalancerConfiguration(loadbalancer.id, configuration).as(
+        'createConfiguration'
+      );
 
-    cy.visitWithLogin(`/loadbalancers/${loadbalancer.id}/configurations`);
-    cy.wait([
-      '@getFeatureFlags',
-      '@getClientStream',
-      '@getLoadBalancer',
-      '@getConfigurations',
-    ]);
+      cy.visitWithLogin(`/loadbalancers/${loadbalancer.id}/configurations`);
+      cy.wait([
+        '@getFeatureFlags',
+        '@getClientStream',
+        '@getLoadBalancer',
+        '@getConfigurations',
+      ]);
 
-    ui.button.findByTitle('Add Configuration').click();
+      ui.button.findByTitle('Add Configuration').click();
 
-    cy.findByLabelText('Configuration Label')
-      .should('be.visible')
-      .type('config-0');
-
-    ui.button.findByTitle('Apply Certificates').should('be.visible').click();
-
-    ui.drawer.findByTitle('Apply Certificates').within(() => {
-      cy.findByLabelText('Host Header').should('be.visible').type('*');
-
-      cy.findByLabelText('Certificate').should('be.visible').click();
-
-      ui.autocompletePopper
-        .findByTitle(certificates[0].label)
+      cy.findByLabelText('Configuration Label')
         .should('be.visible')
-        .click();
+        .type(configuration.label);
+
+      ui.button.findByTitle('Apply Certificates').should('be.visible').click();
+
+      ui.drawer.findByTitle('Apply Certificates').within(() => {
+        cy.findByLabelText('Host Header').should('be.visible').type('*');
+
+        cy.findByLabelText('Certificate').should('be.visible').click();
+
+        ui.autocompletePopper
+          .findByTitle(certificates[0].label)
+          .should('be.visible')
+          .click();
+
+        ui.button
+          .findByTitle('Save')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+      cy.findByText(certificates[0].label);
+      cy.findByText('*');
+
+      ui.button.findByTitle('Add Route').click();
+
+      ui.drawer.findByTitle('Add Route').within(() => {
+        cy.findByLabelText('Route').click();
+
+        ui.autocompletePopper.findByTitle(routes[0].label).click();
+
+        cy.get("[type='submit']")
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
 
       ui.button
-        .findByTitle('Save')
+        .findByTitle('Create Configuration')
         .should('be.visible')
         .should('be.enabled')
         .click();
+
+      cy.wait(['@createConfiguration', '@getConfigurations']);
     });
+    it('creates a HTTP configuration', () => {
+      const loadbalancer = loadbalancerFactory.build();
+      const routes = routeFactory.buildList(1, { protocol: 'http' });
+      const configuration = configurationFactory.build({
+        port: 80,
+        protocol: 'http',
+      });
 
-    cy.findByText(certificates[0].label);
-    cy.findByText('*');
+      mockAppendFeatureFlags({
+        aglb: makeFeatureFlagData(true),
+      }).as('getFeatureFlags');
+      mockGetFeatureFlagClientstream().as('getClientStream');
+      mockGetLoadBalancer(loadbalancer).as('getLoadBalancer');
+      mockGetLoadBalancerConfigurations(loadbalancer.id, []).as(
+        'getConfigurations'
+      );
+      mockGetLoadBalancerRoutes(loadbalancer.id, routes);
+      mockCreateLoadBalancerConfiguration(loadbalancer.id, configuration).as(
+        'createConfiguration'
+      );
 
-    ui.button.findByTitle('Add Route').click();
+      cy.visitWithLogin(`/loadbalancers/${loadbalancer.id}/configurations`);
+      cy.wait([
+        '@getFeatureFlags',
+        '@getClientStream',
+        '@getLoadBalancer',
+        '@getConfigurations',
+      ]);
 
-    ui.drawer.findByTitle('Add Route').within(() => {
-      cy.findByLabelText('Route').click();
+      ui.button.findByTitle('Add Configuration').click();
 
-      ui.autocompletePopper.findByTitle(routes[0].label).click();
+      cy.findByLabelText('Configuration Label')
+        .should('be.visible')
+        .type(configuration.label);
 
-      cy.get("[type='submit']")
+      cy.findByLabelText('Protocol').click();
+
+      ui.autocompletePopper
+        .findByTitle(configuration.protocol.toUpperCase())
+        .click();
+
+      cy.findByLabelText('Port').clear().type(configuration.port);
+
+      // Certificates do not apply to HTTP, so make sure there is not mention of them
+      cy.findByText('Details')
+        .closest('form')
+        .findByText('Certificate')
+        .should('not.exist');
+
+      ui.button.findByTitle('Add Route').click();
+
+      ui.drawer.findByTitle('Add Route').within(() => {
+        cy.findByLabelText('Route').click();
+
+        ui.autocompletePopper.findByTitle(routes[0].label).click();
+
+        cy.get("[type='submit']")
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+      ui.button
+        .findByTitle('Create Configuration')
         .should('be.visible')
         .should('be.enabled')
         .click();
-    });
 
-    ui.button
-      .findByTitle('Create Configuration')
-      .should('be.visible')
-      .should('be.enabled')
-      .click();
+      cy.wait(['@createConfiguration', '@getConfigurations']);
+    });
+    it('creates a TCP configuration', () => {
+      const loadbalancer = loadbalancerFactory.build();
+      const routes = routeFactory.buildList(1, { protocol: 'http' });
+      const configuration = configurationFactory.build({
+        port: 80,
+        protocol: 'http',
+      });
+
+      mockAppendFeatureFlags({
+        aglb: makeFeatureFlagData(true),
+      }).as('getFeatureFlags');
+      mockGetFeatureFlagClientstream().as('getClientStream');
+      mockGetLoadBalancer(loadbalancer).as('getLoadBalancer');
+      mockGetLoadBalancerConfigurations(loadbalancer.id, []).as(
+        'getConfigurations'
+      );
+      mockGetLoadBalancerRoutes(loadbalancer.id, routes);
+      mockCreateLoadBalancerConfiguration(loadbalancer.id, configuration).as(
+        'createConfiguration'
+      );
+
+      cy.visitWithLogin(`/loadbalancers/${loadbalancer.id}/configurations`);
+      cy.wait([
+        '@getFeatureFlags',
+        '@getClientStream',
+        '@getLoadBalancer',
+        '@getConfigurations',
+      ]);
+
+      ui.button.findByTitle('Add Configuration').click();
+
+      cy.findByLabelText('Configuration Label')
+        .should('be.visible')
+        .type(configuration.label);
+
+      cy.findByLabelText('Protocol').click();
+
+      ui.autocompletePopper
+        .findByTitle(configuration.protocol.toUpperCase())
+        .click();
+
+      cy.findByLabelText('Port').clear().type(configuration.port);
+
+      // Certificates do not apply to HTTP, so make sure there is not mention of them
+      cy.findByText('Details')
+        .closest('form')
+        .findByText('Certificate')
+        .should('not.exist');
+
+      ui.button.findByTitle('Add Route').click();
+
+      ui.drawer.findByTitle('Add Route').within(() => {
+        cy.findByLabelText('Route').click();
+
+        ui.autocompletePopper.findByTitle(routes[0].label).click();
+
+        cy.get("[type='submit']")
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+      ui.button
+        .findByTitle('Create Configuration')
+        .should('be.visible')
+        .should('be.enabled')
+        .click();
+
+      cy.wait(['@createConfiguration', '@getConfigurations']);
+    });
+    it.only('shows API errors when creating an HTTPS configuration', () => {
+      const loadbalancer = loadbalancerFactory.build();
+      const certificates = certificateFactory.buildList(1);
+      const routes = routeFactory.buildList(1);
+
+      mockAppendFeatureFlags({
+        aglb: makeFeatureFlagData(true),
+      }).as('getFeatureFlags');
+      mockGetFeatureFlagClientstream().as('getClientStream');
+      mockGetLoadBalancer(loadbalancer).as('getLoadBalancer');
+      mockGetLoadBalancerConfigurations(loadbalancer.id, []).as(
+        'getConfigurations'
+      );
+      mockGetLoadBalancerCertificates(loadbalancer.id, certificates);
+      mockGetLoadBalancerRoutes(loadbalancer.id, routes);
+
+      const errors = [
+        { field: 'label', reason: 'Must be greater than 2 characters.' },
+        { field: 'port', reason: 'Must be a number.' },
+        { field: 'protocol', reason: "Can't use UDP." },
+        { field: 'route_ids', reason: 'Your routes are messed up.' },
+        {
+          field: 'certificates',
+          reason: 'Something about your certs is not correct.',
+        },
+      ];
+
+      mockCreateLoadBalancerConfigurationError(loadbalancer.id, errors).as(
+        'createConfiguration'
+      );
+
+      cy.visitWithLogin(`/loadbalancers/${loadbalancer.id}/configurations`);
+      cy.wait([
+        '@getFeatureFlags',
+        '@getClientStream',
+        '@getLoadBalancer',
+        '@getConfigurations',
+      ]);
+
+      ui.button.findByTitle('Add Configuration').click();
+
+      cy.findByLabelText('Configuration Label')
+        .should('be.visible')
+        .type('test');
+
+      ui.button.findByTitle('Apply Certificates').should('be.visible').click();
+
+      ui.drawer.findByTitle('Apply Certificates').within(() => {
+        cy.findByLabelText('Host Header').should('be.visible').type('*');
+
+        cy.findByLabelText('Certificate').should('be.visible').click();
+
+        ui.autocompletePopper
+          .findByTitle(certificates[0].label)
+          .should('be.visible')
+          .click();
+
+        ui.button
+          .findByTitle('Save')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+      cy.findByText(certificates[0].label);
+      cy.findByText('*');
+
+      ui.button.findByTitle('Add Route').click();
+
+      ui.drawer.findByTitle('Add Route').within(() => {
+        cy.findByLabelText('Route').click();
+
+        ui.autocompletePopper.findByTitle(routes[0].label).click();
+
+        cy.get("[type='submit']")
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+      ui.button
+        .findByTitle('Create Configuration')
+        .should('be.visible')
+        .should('be.enabled')
+        .click();
+
+      cy.wait(['@createConfiguration']);
+
+      for (const { reason } of errors) {
+        cy.findByText(reason).should('be.visible');
+      }
+    });
+  });
+  describe('delete', () => {
+    it('deletes a configuration', () => {
+      const loadbalancer = loadbalancerFactory.build();
+      const configurations = configurationFactory.buildList(1);
+
+      mockAppendFeatureFlags({
+        aglb: makeFeatureFlagData(true),
+      }).as('getFeatureFlags');
+      mockGetFeatureFlagClientstream().as('getClientStream');
+      mockGetLoadBalancer(loadbalancer).as('getLoadBalancer');
+      mockGetLoadBalancerConfigurations(loadbalancer.id, configurations).as(
+        'getConfigurations'
+      );
+
+      cy.visitWithLogin(`/loadbalancers/${loadbalancer.id}/configurations`);
+      cy.wait([
+        '@getFeatureFlags',
+        '@getClientStream',
+        '@getLoadBalancer',
+        '@getConfigurations',
+      ]);
+
+      const configuration = configurations[0];
+
+      const accordionHeader = cy.findByText(configuration.label);
+
+      // Click the accordion header to open the accordion
+      accordionHeader.click();
+
+      // Get the Configuration's entire accordion area
+      accordionHeader.closest('[data-qa-panel]').within(() => {
+        // Click the Delete button to open the delete dialog
+        ui.button.findByTitle('Delete').click();
+      });
+
+      mockDeleteLoadBalancerConfiguration(loadbalancer.id, configuration.id).as(
+        'deleteConfiguration'
+      );
+      mockGetLoadBalancerConfigurations(loadbalancer.id, []).as(
+        'getConfigurations'
+      );
+
+      ui.dialog
+        .findByTitle(`Delete Configuration ${configuration.label}?`)
+        .within(() => {
+          cy.findByText(
+            'Are you sure you want to delete this configuration?'
+          ).should('be.visible');
+
+          ui.button.findByTitle('Delete').click();
+        });
+
+      cy.wait(['@deleteConfiguration', '@getConfigurations']);
+
+      cy.findByText(configuration.label).should('not.exist');
+    });
+    it('shows API errors when deleting a configuration', () => {
+      const loadbalancer = loadbalancerFactory.build();
+      const configurations = configurationFactory.buildList(1);
+
+      mockAppendFeatureFlags({
+        aglb: makeFeatureFlagData(true),
+      }).as('getFeatureFlags');
+      mockGetFeatureFlagClientstream().as('getClientStream');
+      mockGetLoadBalancer(loadbalancer).as('getLoadBalancer');
+      mockGetLoadBalancerConfigurations(loadbalancer.id, configurations).as(
+        'getConfigurations'
+      );
+
+      cy.visitWithLogin(`/loadbalancers/${loadbalancer.id}/configurations`);
+      cy.wait([
+        '@getFeatureFlags',
+        '@getClientStream',
+        '@getLoadBalancer',
+        '@getConfigurations',
+      ]);
+
+      const configuration = configurations[0];
+
+      const accordionHeader = cy.findByText(configuration.label);
+
+      // Click the accordion header to open the accordion
+      accordionHeader.click();
+
+      // Get the Configuration's entire accordion area
+      accordionHeader.closest('[data-qa-panel]').within(() => {
+        // Click the Delete button to open the delete dialog
+        ui.button.findByTitle('Delete').click();
+      });
+
+      mockDeleteLoadBalancerConfigurationError(
+        loadbalancer.id,
+        configuration.id,
+        'Control Plane Error'
+      ).as('deleteConfiguration');
+
+      ui.dialog
+        .findByTitle(`Delete Configuration ${configuration.label}?`)
+        .within(() => {
+          cy.findByText(
+            'Are you sure you want to delete this configuration?'
+          ).should('be.visible');
+
+          ui.button.findByTitle('Delete').click();
+
+          cy.wait(['@deleteConfiguration']);
+
+          cy.findByText('Control Plane Error').should('be.visible');
+        });
+    });
   });
 });
