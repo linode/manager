1a6d151748b70db720e1d0d0e7fa1c8b10020fb2
coliu-akamai <coliu@akamai.com>
2023-11-27 11:50:25 -0500

    address feedback + add labels to styled components

--- packages/manager/src/components/DismissibleBanner/DismissibleBanner.styles.ts
+++ packages/manager/src/components/DismissibleBanner/DismissibleBanner.styles.ts
@@ -2,25 +2,30 @@ import { styled } from '@mui/material/styles';
 
 import { Notice } from 'src/components/Notice/Notice';
 
-export const StyledNotice = styled(Notice)(({ theme }) => ({
-  '&&': {
-    p: {
-      lineHeight: '1.25rem',
-    },
-  },
-  alignItems: 'center',
-  background: theme.bg.bgPaper,
-  borderRadius: 1,
-  display: 'flex',
-  flexFlow: 'row nowrap',
-  justifyContent: 'space-between',
-  marginBottom: theme.spacing(),
-  padding: theme.spacing(2),
-}));
+import { StyledLinkButton } from '../Button/StyledLinkButton';
 
-export const StyledButton = styled('button')(({ theme }) => ({
-  ...theme.applyLinkStyles,
-  color: theme.textColors.tableStatic,
-  display: 'flex',
-  marginLeft: 20,
-}));
+export const StyledNotice = styled(Notice, { label: 'StyledNotice' })(
+  ({ theme }) => ({
+    '&&': {
+      p: {
+        lineHeight: '1.25rem',
+      },
+    },
+    alignItems: 'center',
+    background: theme.bg.bgPaper,
+    borderRadius: 1,
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(),
+    padding: theme.spacing(2),
+  })
+);
+
+export const StyledButton = styled(StyledLinkButton, { label: 'StyledButton' })(
+  ({ theme }) => ({
+    color: theme.textColors.tableStatic,
+    display: 'flex',
+    marginLeft: 20,
+  })
+);
