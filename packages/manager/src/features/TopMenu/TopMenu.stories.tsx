841d04a36174e9aff0285569104f4dffe3477ecf
Alban Bailly <abailly@akamai.com>
2023-11-30 16:23:48 -0500

    Tags + TopMenu

---
+++ packages/manager/src/features/TopMenu/TopMenu.stories.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { TopMenu } from './TopMenu';
+
+import type { TopMenuProps } from './TopMenu';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export const Default: StoryObj<TopMenuProps> = {
+  render: (args) => <TopMenu {...args} />,
+};
+
+const meta: Meta<TopMenuProps> = {
+  args: {
+    desktopMenuToggle: () => null,
+    isSideMenuOpen: false,
+    openSideMenu: () => null,
+    username: 'User 1',
+  },
+  component: TopMenu,
+  title: 'Features/Navigation/Top Menu',
+};
+export default meta;
