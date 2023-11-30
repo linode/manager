5a36fcd9c7c705d8a23575562afef318e967513e
Banks Nussman <banks@nussman.us>
2023-11-30 14:28:08 -0500

    clean up

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeActivity/LinodeActivity.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeActivity/LinodeActivity.tsx
@@ -1,9 +1,7 @@
-import { Params } from '@linode/api-v4';
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 
 import EventsLanding from 'src/features/Events/EventsLanding';
-import { getEventsForEntity } from 'src/utilities/getEventsForEntity';
 
 const LinodeActivity = () => {
   const { linodeId } = useParams<{ linodeId: string }>();
@@ -11,13 +9,9 @@ const LinodeActivity = () => {
 
   return (
     <EventsLanding
-      getEventsRequest={(params: Params = {}) =>
-        getEventsForEntity(params, 'linode', id)
-      }
       data-testid="linode-events-table"
       emptyMessage="No recent activity for this Linode."
       entityId={id}
-      errorMessage="There was an error retrieving activity for this Linode."
     />
   );
 };
