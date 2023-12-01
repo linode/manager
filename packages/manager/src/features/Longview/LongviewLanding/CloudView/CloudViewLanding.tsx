4fab23fe27407fc795d6ab908deea50eeeff323d
Vinay Rajesh <vrajesh@akamai.com>
2023-12-01 15:43:13 +0530

    DI-16203: Cors testing for dev api

--- packages/manager/src/features/Longview/LongviewLanding/CloudView/CloudViewLanding.tsx
+++ packages/manager/src/features/Longview/LongviewLanding/CloudView/CloudViewLanding.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { Paper } from "src/components/Paper";
 import CloudViewTabs from "./CloudViewTabs";
+import { useAPIRequest } from 'src/hooks/useAPIRequest';
+import { testCors } from "./CloudViewRequest";
 
 export const CloudViewLanding = (props: any) => {
+
+    const logAndReturnData = (response: any) => {
+        console.log("data", response.data, "error", error);
+        return response.data;
+    }
+
+    const { data, loading, error } = useAPIRequest(
+        () => testCors().
+        then((response) => logAndReturnData(response)),
+        [],
+        [props.start, props.end, props.linodeId]
+    );
     
     return (
         <>
