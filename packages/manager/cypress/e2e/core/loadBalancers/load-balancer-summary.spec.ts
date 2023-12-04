0aca116b4b3bf6a8689a6ce24d545cdc53b6cd7d
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-12-04 06:33:24 -0600

    Deletes Loadbalancer from Settings tab

--- packages/manager/cypress/e2e/core/loadBalancers/load-balancer-summary.spec.ts
+++ packages/manager/cypress/e2e/core/loadBalancers/load-balancer-summary.spec.ts
@@ -2,12 +2,19 @@
  * @file Integration tests for Akamai Global Load Balancer summary page.
  */
 
-import { loadbalancerFactory } from '@src/factories/aglb';
+import { loadbalancerFactory, configurationFactory } from '@src/factories/aglb';
 import {
   mockAppendFeatureFlags,
   mockGetFeatureFlagClientstream,
 } from 'support/intercepts/feature-flags';
-import { mockGetLoadBalancer } from 'support/intercepts/load-balancers';
+
+import {
+  mockGetLoadBalancer,
+  mockGetLoadBalancers,
+  mockDeleteLoadBalancer,
+} from 'support/intercepts/load-balancers';
+import { randomLabel } from 'support/util/random';
+import { ui } from 'support/ui';
 import { makeFeatureFlagData } from 'support/util/feature-flags';
 
 describe('Akamai Global Load Balancer details page', () => {
@@ -39,5 +46,96 @@ describe('Akamai Global Load Balancer details page', () => {
 
     cy.findByText(mockLoadBalancer.label).should('be.visible');
     cy.findByText(mockLoadBalancer.hostname).should('be.visible');
+  });
+});
+
+describe('Delete', () => {
+  /*
+   * Deleting a load balancer from the AGLB load balancer details page "Settings" tab (route: /loadbalancers/:id/settings)
+   * Confirms User is redirected to AGLB landing page upon deleting from Load Balancer details page "Settings" tab, and load balancer is not listed on the landing page.
+   */
+
+  // Test case for deleting a load balancer from the Settings tab.
+  it('Deletes a loadbalancer from Settings tab', () => {
+    const mockLoadBalancer = loadbalancerFactory.build();
+
+    mockAppendFeatureFlags({
+      aglb: makeFeatureFlagData(true),
+    }).as('getFeatureFlags');
+
+    mockGetFeatureFlagClientstream().as('getClientStream');
+    mockGetLoadBalancer(mockLoadBalancer).as('getLoadBalancer');
+
+    // Visit the specific load balancer's page with login.
+    cy.visitWithLogin(`/loadbalancers/${mockLoadBalancer.id}`);
+
+    // Wait for all the mock API calls to complete.
+    cy.wait(['@getFeatureFlags', '@getClientStream', '@getLoadBalancer']);
+
+    // Navigate to the 'Settings' tab.
+    cy.findByText('Settings').should('be.visible').click();
+
+    cy.findByText(mockLoadBalancer.label).should('be.visible');
+
+    cy.findByText('Delete Load Balancer').should('be.visible');
+
+    // Mock the API call for deleting the load balancer.
+    mockDeleteLoadBalancer(mockLoadBalancer.id).as('deleteLoadBalancer');
+
+    // Setup additional mock load balancer data.
+    const loadBalancerConfiguration = configurationFactory.build();
+    const loadbalancerMocks = [
+      loadbalancerFactory.build({
+        id: 1,
+        label: randomLabel(),
+        configurations: [
+          {
+            id: loadBalancerConfiguration.id,
+            label: loadBalancerConfiguration.label,
+          },
+        ],
+        regions: ['us-east'],
+      }),
+    ];
+    const loadbalancerMock = loadbalancerMocks[0];
+
+    mockGetLoadBalancers(loadbalancerMocks).as('getLoadBalancers');
+
+    ui.button.findByTitle('Delete').should('be.visible').click();
+
+    // Handle the delete confirmation dialog.
+    ui.dialog
+      .findByTitle(`Delete ${mockLoadBalancer.label}?`)
+      .should('be.visible')
+      .within(() => {
+        cy.findByTestId('textfield-input')
+          .should('be.visible')
+          .click()
+          .type(mockLoadBalancer.label);
+
+        ui.buttonGroup
+          .findButtonByTitle('Delete')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+    // Wait for the delete operation and subsequent data retrieval to complete.
+    cy.wait(['@deleteLoadBalancer', '@getLoadBalancers']);
+
+    // Confirm user is navigated to the load balancers landing page list.
+    cy.findByText(loadbalancerMock.label)
+      .should('be.visible')
+      .closest('tr')
+      .within(() => {
+        ui.actionMenu
+          .findByTitle(
+            `Action menu for Load Balancer ${loadbalancerMock.label}`
+          )
+          .should('be.visible');
+      });
+
+    // Verify that the deleted load balancer no longer exists in the list.
+    cy.findByText(mockLoadBalancer.label).should('not.exist');
   });
 });
