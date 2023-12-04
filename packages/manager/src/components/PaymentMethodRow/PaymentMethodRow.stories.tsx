fe876dd622af5c683f325293a5e380b9973cd63d
coliu-akamai <coliu@akamai.com>
2023-12-04 12:36:00 -0500

    starting story

--- packages/manager/src/components/PaymentMethodRow/PaymentMethodRow.stories.tsx
+++ packages/manager/src/components/PaymentMethodRow/PaymentMethodRow.stories.tsx
@@ -1,16 +1,34 @@
+import { CardType } from '@linode/api-v4';
 import React from 'react';
+
+import { paymentMethodFactory } from 'src/factories';
 
 import { PaymentMethodRow } from './PaymentMethodRow';
 
 import type { PaymentMethodRowProps } from './PaymentMethodRow';
 import type { Meta, StoryObj } from '@storybook/react';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const onDelete = () => {};
 
-const supportedCreditCards = [
+const supportedCreditCards: CardType[] = [
   'Visa',
   'MasterCard',
   'American Express',
   'Discover',
   'JCB',
-  'Other',
 ];
+
+const CreditCards = () =>
+  supportedCreditCards.map((creditCard) => (
+    <PaymentMethodRow
+      paymentMethod={paymentMethodFactory.build({
+        data: {
+          card_type: creditCard,
+        },
+        type: 'credit_card',
+      })}
+      key={creditCard}
+      onDelete={onDelete}
+    />
+  ));
