ba02eb59150a0afe5b7c0c42caa7028014def6cb
Banks Nussman <banks@nussman.us>
2023-12-05 14:08:36 -0500

    correct path

--- packages/manager/cypress/support/plugins/vite-preprocessor.ts
+++ packages/manager/cypress/support/plugins/vite-preprocessor.ts
@@ -1,14 +1,20 @@
 import vitePreprocessor from 'cypress-vite';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 import { CypressPlugin } from './plugin';
-import { fileURLToPath } from 'url';
 
 export const vitePreprocess: CypressPlugin = (on, _config): void => {
   on(
     'file:preprocessor',
     vitePreprocessor(
-      resolve(fileURLToPath(import.meta.url), '..', '..', 'vite.config.ts')
+      resolve(
+        fileURLToPath(import.meta.url),
+        '..',
+        '..',
+        '..',
+        'vite.config.ts'
+      )
     )
   );
 };
