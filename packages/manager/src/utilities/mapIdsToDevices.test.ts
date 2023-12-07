9d4d9066f5ead7c7ce261755d0ed9f05ea26d101
Tyler Jones <tjones@akamai.com>
2023-12-07 14:29:40 -0500

    changed improved mapIdsToDevices tests to cover nodebalancers

--- packages/manager/src/utilities/mapIdsToDevices.test.ts
+++ packages/manager/src/utilities/mapIdsToDevices.test.ts
@@ -1,24 +1,46 @@
+import { Linode } from '@linode/api-v4';
+import { NodeBalancer } from '@linode/api-v4';
+
 import { linodeFactory } from 'src/factories';
+import { nodeBalancerFactory } from 'src/factories';
 
 import { mapIdsToDevices } from './mapIdsToDevices';
 
 describe('mapIdsToDevices', () => {
   const linodes = linodeFactory.buildList(5);
-  it('works with a single id', () => {
-    expect(mapIdsToDevices(1, linodes)).toBe(linodes[1]);
+  const nodebalancers = nodeBalancerFactory.buildList(5);
+  it('works with a single Linode ID', () => {
+    expect(mapIdsToDevices<Linode>(1, linodes)).toBe(linodes[1]);
   });
-  it('works with a multiple ids', () => {
-    expect(mapIdsToDevices([0, 1, 2], linodes)).toEqual([
+  it('works with a single NodeBalancer ID', () => {
+    expect(mapIdsToDevices<NodeBalancer>(1, nodebalancers)).toBe(
+      nodebalancers[1]
+    );
+  });
+  it('works with a multiple Linode IDs', () => {
+    expect(mapIdsToDevices<Linode>([0, 1, 2], linodes)).toEqual([
       linodes[0],
       linodes[1],
       linodes[2],
     ]);
   });
-  it('omits missing ids', () => {
-    expect(mapIdsToDevices(99, linodes)).toBe(null);
-    expect(mapIdsToDevices([0, 99, 2], linodes)).toEqual([
+  it('works with a multiple NodeBalancer IDs', () => {
+    expect(mapIdsToDevices<NodeBalancer>([0, 1, 2], nodebalancers)).toEqual([
+      nodebalancers[0],
+      nodebalancers[1],
+      nodebalancers[2],
+    ]);
+  });
+  it('omits missing IDs', () => {
+    expect(mapIdsToDevices<Linode>(99, linodes)).toBe(null);
+    expect(mapIdsToDevices<NodeBalancer>(99, nodebalancers)).toBe(null);
+    expect(mapIdsToDevices<Linode>([0, 99, 2], linodes)).toEqual([
       linodes[0],
       linodes[2],
+    ]);
+    expect(mapIdsToDevices<NodeBalancer>([0, 99, 2], nodebalancers)).toEqual([
+      nodebalancers[0],
+      nodebalancers[2],
     ]);
   });
 });
