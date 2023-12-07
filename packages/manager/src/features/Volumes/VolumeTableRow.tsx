f9edbfe927962c6aa81bd57a1e2ba0fe2da77633
Banks Nussman <banks@nussman.us>
2023-12-07 16:41:42 -0500

    remove commit

--- packages/manager/src/features/Volumes/VolumeTableRow.tsx
+++ packages/manager/src/features/Volumes/VolumeTableRow.tsx
@@ -16,7 +16,6 @@ import { useRegionsQuery } from 'src/queries/regions';
 import { ActionHandlers, VolumesActionMenu } from './VolumesActionMenu';
 // import useEvents from 'src/hooks/useEvents';
 
-// TODO jss-to-tss-react codemod: usages of this hook outside of this file will not be converted.
 export const useStyles = makeStyles()({
   chipWrapper: {
     alignSelf: 'center',
