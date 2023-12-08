dd47074c8da9c3f1230e2ed6dc14569f3d919b70
Connie Liu <139280159+coliu-akamai@users.noreply.github.com>
2023-12-08 18:00:59 -0500

    refactor: [M3-7537] - PaginationControls Storybook v7 Story (#9959)
    * pagination story
    
    * update comments and tests
    
    * changeset
    
    * fix page arg not taking effect
    
    * Update packages/manager/src/components/PaginationControls/PaginationControls.stories.tsx
    
    Co-authored-by: Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
    
    * update import
    
    ---------
    
    Co-authored-by: Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>

---
+++ packages/manager/src/components/PaginationControls/PaginationControls.stories.tsx
@@ -0,0 +1,34 @@
+import { useArgs } from '@storybook/preview-api';
+import * as React from 'react';
+
+import { PaginationControls } from './PaginationControls';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+type Story = StoryObj<typeof PaginationControls>;
+
+export const PaginationControl: Story = {
+  args: {
+    count: 250,
+    page: 1,
+    pageSize: 25,
+  },
+  render: (args) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [, setArgs] = useArgs();
+
+    return (
+      <PaginationControls
+        {...args}
+        onClickHandler={(page) => setArgs({ page })}
+      />
+    );
+  },
+};
+
+const meta: Meta<typeof PaginationControls> = {
+  component: PaginationControls,
+  title: 'Components/Pagination Control',
+};
+
+export default meta;
