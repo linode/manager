533ddcf7a7c81a672f54c078804a843905fbceef
cpathipa <119517080+cpathipa@users.noreply.github.com>
2023-12-12 14:25:26 -0600

    Revert "upcoming: [M3-7423] - Show regions instead of clusterIds in Create Drawer"
    This reverts commit be7305bf3584947ba669996e696d51382f8167dc.

--- packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.tsx
@@ -1,6 +1,8 @@
+import { Region } from '@linode/api-v4/lib/regions';
 import * as React from 'react';
 
 import { RegionSelect } from 'src/components/RegionSelect/RegionSelect';
+import { useObjectStorageClusters } from 'src/queries/objectStorage';
 import { useRegionsQuery } from 'src/queries/regions';
 
 interface Props {
@@ -22,10 +24,23 @@ export const ClusterSelect: React.FC<Props> = (props) => {
     selectedCluster,
   } = props;
 
-  const { data: regions, error: regionsError } = useRegionsQuery();
+  const { data: clusters, error: clustersError } = useObjectStorageClusters();
+  const { data: regions } = useRegionsQuery();
+
+  const regionOptions = clusters?.reduce<Region[]>((acc, cluster) => {
+    const region = regions?.find((r) => r.id === cluster.region);
+    if (region) {
+      acc.push({ ...region, id: cluster.id });
+    }
+    return acc;
+  }, []);
 
   // Error could be: 1. General Clusters error, 2. Field error, 3. Nothing
-  const errorText = error || regionsError?.[0]?.reason;
+  const errorText = clustersError
+    ? 'Error loading regions'
+    : error
+    ? error
+    : undefined;
 
   return (
     <RegionSelect
@@ -38,7 +53,7 @@ export const ClusterSelect: React.FC<Props> = (props) => {
       label="Region"
       onBlur={onBlur}
       placeholder="Select a Region"
-      regions={regions ?? []}
+      regions={regionOptions ?? []}
       required={required}
       selectedId={selectedCluster}
     />
