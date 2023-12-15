e0de715c8673ac1abc84b4b437defc8e51168826
coliu-akamai <coliu@akamai.com>
2023-12-15 16:43:38 -0500

    add test

--- packages/manager/cypress/e2e/core/domains/domains-landing-page.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Domains landing page', () => {
-  /**
-   * - Confirms Domains landing page empty state is shown when no Domains are present:
-   * - Confirms that "Getting Started Guides" and "Video Playlist" are listed on landing page.
-   * - Confirms that clicking "Create Domain" navigates user to domain create page.
-   * - Confirms that clicking on "Import A Zone" opens "Import a Zone" drawer
-   */
-  it('shows the empty state when there are no domains', () => {});
-});
