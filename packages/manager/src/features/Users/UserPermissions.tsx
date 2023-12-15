9a7b210c3721b048610192ae3422256a9fa63388
Tyler Jones <tjones@akamai.com>
2023-12-15 12:09:22 -0500

    removed unrestricted render

--- packages/manager/src/features/Users/UserPermissions.tsx
+++ packages/manager/src/features/Users/UserPermissions.tsx
@@ -611,22 +611,6 @@ class UserPermissions extends React.Component<CombinedProps, State> {
     );
   };
 
-  renderUnrestricted = () => {
-    /* TODO: render all permissions disabled with this message above */
-    return (
-      <Paper
-        sx={(theme) => ({
-          marginTop: theme.spacing(2),
-          padding: theme.spacing(3),
-        })}
-      >
-        <Typography data-qa-unrestricted-msg>
-          This user has unrestricted access to the account.
-        </Typography>
-      </Paper>
-    );
-  };
-
   savePermsType = (type: string) => () => {
     this.setState({ errors: undefined });
     const { clearNewUser, username } = this.props;
