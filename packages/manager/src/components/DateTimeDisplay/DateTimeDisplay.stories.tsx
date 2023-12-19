4f9591feadb7de9955cb906029e7309fdadac308
Connie Liu <139280159+coliu-akamai@users.noreply.github.com>
2023-12-19 11:01:11 -0500

    refactor: [M3-7569] - Currency and DateTimeDisplay v7 Storybook migration (#10007)
    * storybook and additional tests
    
    * changeset

---
+++ packages/manager/src/components/DateTimeDisplay/DateTimeDisplay.stories.tsx
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon';
+import * as React from 'react';
+
+import { DateTimeDisplay } from './DateTimeDisplay';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+type Story = StoryObj<typeof DateTimeDisplay>;
+
+export const Default: Story = {
+  args: {
+    displayTime: true,
+    value: DateTime.now().minus({ day: 3 }).toISO(),
+  },
+  render: (args) => <DateTimeDisplay {...args} />,
+};
+
+const meta: Meta<typeof DateTimeDisplay> = {
+  argTypes: {
+    value: {
+      control: {
+        type: 'date',
+      },
+    },
+  },
+  component: DateTimeDisplay,
+  title: 'Components/Typography/Date Time Display',
+};
+
+export default meta;
