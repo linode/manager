6b3b99e15ca18528a3c421be69cffb4d7877dbac
Tyler Jones <tjones@akamai.com>
2023-12-28 12:49:31 -0500

    add unit tests but one is failing

---
+++ packages/manager/src/features/Account/CloseAccountSetting.test.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+import { accountFactory, profileFactory } from 'src/factories';
+import { grantsFactory } from 'src/factories/grants';
+import { rest, server } from 'src/mocks/testServer';
+import { renderWithTheme } from 'src/utilities/testHelpers';
+
+import CloseAccountSetting from './CloseAccountSetting';
+
+describe('Close Account Settings', () => {
+  it('should render subheading text', () => {
+    const { container } = renderWithTheme(<CloseAccountSetting />);
+    const subheading = container.querySelector(
+      '[data-qa-panel-subheading="true"]'
+    );
+    expect(subheading).toBeInTheDocument();
+    expect(subheading?.textContent).toBe('Close Account');
+  });
+
+  it('should render a Close Account Button', () => {
+    const { getByTestId } = renderWithTheme(<CloseAccountSetting />);
+    const button = getByTestId('close-account-button');
+    expect(button).toBeInTheDocument();
+    const span = button.querySelector('span');
+    expect(span).toHaveTextContent('Close Account');
+  });
+
+  it('should render a disabled Close Account button when there are child account', () => {
+    server.use(
+      rest.get('*/profile', (req, res, ctx) => {
+        return res(ctx.json(profileFactory.build({ restricted: true })));
+      }),
+      rest.get('*/profile/grants', (req, res, ctx) => {
+        return res(
+          ctx.json(
+            grantsFactory.build({ global: { child_account_access: true } })
+          )
+        );
+      }),
+      rest.get('*/account/child-accounts', (req, res, ctx) => {
+        return res(ctx.json(accountFactory.buildList(1)));
+      })
+    );
+
+    const { getByText } = renderWithTheme(<CloseAccountSetting />, {
+      flags: { parentChildAccountAccess: true },
+    });
+    const notice = getByText(
+      'Remove child accounts before closing the account.'
+    );
+    expect(notice).toBeInTheDocument();
+    // const button = getByTestId('close-account-button');
+    // expect(button).toBeDisabled();
+  });
+});
