e3e88aa5d4b3d1f94a6848ed784075facefe56d3
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-01-02 08:34:36 -0600

    Billing Summary sections

--- packages/manager/src/features/LoadBalancers/LoadBalancerCreate/LoadBalancerSummary/LoadBalancerSummary.tsx
+++ packages/manager/src/features/LoadBalancers/LoadBalancerCreate/LoadBalancerSummary/LoadBalancerSummary.tsx
@@ -2,12 +2,19 @@ import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Unstable_Grid2';
 import * as React from 'react';
 
+import { DisplayPrice } from 'src/components/DisplayPrice';
+import { Divider } from 'src/components/Divider';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import { LandingHeader } from 'src/components/LandingHeader';
+import { Link } from 'src/components/Link';
+import { Notice } from 'src/components/Notice/Notice';
+import { Typography } from 'src/components/Typography';
 
+import { EditLoadBalancerConfigurations } from './EditLoadBalancerConfigurations';
 import { EditLoadBalancerLabel } from './EditLoadBalancerLabel';
 import { EditLoadBalancerRegions } from './EditLoadBalancerRegions';
 import {
+  StyledButton,
   StyledMainGridItem,
   StyledSidebarGridItem,
 } from './LoadBalancerSummary.styles';
@@ -20,8 +27,12 @@ export const LoadBalancerSummary = () => {
         breadcrumbProps={{
           crumbOverrides: [
             {
-              label: 'Create',
+              label: 'Global Load Balancers',
               position: 1,
+            },
+            {
+              label: 'Create',
+              position: 2,
             },
           ],
           pathname: location.pathname,
@@ -33,10 +44,39 @@ export const LoadBalancerSummary = () => {
           <Stack spacing={3}>
             <EditLoadBalancerLabel />
             <EditLoadBalancerRegions />
+            <EditLoadBalancerConfigurations />
           </Stack>
         </StyledMainGridItem>
         <StyledSidebarGridItem lg={3} md={4} xs={12}>
-          <p>Right side content</p>
+          <Stack spacing={3}>
+            <Typography variant="h2">Billing Summary</Typography>
+            <Divider dark />
+            <Stack spacing={1}>
+              <Typography variant="body1">Regions (5)</Typography>
+              <div>
+                <DisplayPrice interval="mo" price={60} />
+              </div>
+            </Stack>
+
+            <Divider dark />
+            <div>
+              <DisplayPrice interval="hour" price={2} />
+            </div>
+            <Divider dark />
+            <Notice important variant="warning">
+              <Typography>
+                Upload and apply certificates after LB creation.
+                <Link to="#">Learn more</Link>.
+              </Typography>
+            </Notice>
+          </Stack>
+          <StyledButton
+            buttonType="primary"
+            disabled={false}
+            onClick={() => null}
+          >
+            Create Load Balancer
+          </StyledButton>
         </StyledSidebarGridItem>
       </Grid>
     </div>
