f60611829198e957d0285133324d3c7bf54d5d03
Alban Bailly <abailly@akamai.com>
2024-01-03 11:53:28 -0500

    cleanup

--- docs/plugins/sidebar.ts
+++ docs/plugins/sidebar.ts
@@ -16,14 +16,13 @@ function scanDirectory(directoryPath: string): MarkdownInfo[] {
     const filePath = path.join(directoryPath, file);
     const fileContent = fs.readFileSync(filePath, "utf-8");
 
-    // Extract title from the Markdown file (assuming titles are specified with '#')
     const titleMatch = fileContent.match(/^#\s+(.*)/m);
     const title = titleMatch ? titleMatch[1] : "Untitled";
 
     // Create object with "text" and "link" properties
     const markdownInfo: MarkdownInfo = {
       text: title,
-      link: `/development-guide/${file}`, // You can customize the link format as needed
+      link: `/development-guide/${file}`,
     };
 
     markdownInfoArray.push(markdownInfo);
