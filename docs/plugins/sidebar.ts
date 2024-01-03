e2bb29859c74a0d1223bb3aa65e652c541020719
Alban Bailly <abailly@akamai.com>
2024-01-03 11:10:40 -0500

    Initial commit - config

---
+++ docs/plugins/sidebar.ts
@@ -0,0 +1,40 @@
+import * as fs from "fs";
+import * as path from "path";
+
+interface MarkdownInfo {
+  text: string;
+  link: string;
+}
+
+function scanDirectory(directoryPath: string): MarkdownInfo[] {
+  const markdownFiles = fs
+    .readdirSync(directoryPath)
+    .filter((file) => file.endsWith(".md"));
+  const markdownInfoArray: MarkdownInfo[] = [];
+
+  markdownFiles.forEach((file) => {
+    const filePath = path.join(directoryPath, file);
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+
+    // Extract title from the Markdown file (assuming titles are specified with '#')
+    const titleMatch = fileContent.match(/^#\s+(.*)/m);
+    const title = titleMatch ? titleMatch[1] : "Untitled";
+
+    // Create object with "text" and "link" properties
+    const markdownInfo: MarkdownInfo = {
+      text: title,
+      link: `/development-guide/${file}`, // You can customize the link format as needed
+    };
+
+    markdownInfoArray.push(markdownInfo);
+  });
+
+  return markdownInfoArray;
+}
+
+// Example usage
+const directoryPath = "./docs/development-guide";
+
+export const guides = scanDirectory(directoryPath);
+
+console.log(guides);
