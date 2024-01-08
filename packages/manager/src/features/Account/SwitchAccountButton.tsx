0c983a81d08a9bfa302d54f65af68279e947c8af
mjac0bs <mjacobs@akamai.com>
2024-01-08 07:15:08 -0800

    Address feedback: use startIcon

--- packages/manager/src/features/Account/SwitchAccountButton.tsx
+++ packages/manager/src/features/Account/SwitchAccountButton.tsx
@@ -1,31 +1,12 @@
-import { styled } from '@mui/material/styles';
 import * as React from 'react';
 
 import SwapIcon from 'src/assets/icons/swapSmall.svg';
 import { Button, ButtonProps } from 'src/components/Button/Button';
 
 export const SwitchAccountButton = (props: ButtonProps) => {
-  const { ...rest } = props;
-
   return (
-    <StyledButton {...rest}>
-      <StyledSwapIcon />
+    <Button startIcon={<SwapIcon />} {...props}>
       Switch Account
-    </StyledButton>
+    </Button>
   );
 };
-
-const StyledButton = styled(Button)(({ theme }) => ({
-  '& path': {
-    fill: theme.textColors.linkActiveLight,
-  },
-  '&:hover, &:focus': {
-    '& path': {
-      fill: theme.name === 'dark' ? '#fff' : theme.textColors.linkActiveLight,
-    },
-  },
-}));
-
-const StyledSwapIcon = styled(SwapIcon)(({ theme }) => ({
-  marginRight: theme.spacing(1),
-}));
