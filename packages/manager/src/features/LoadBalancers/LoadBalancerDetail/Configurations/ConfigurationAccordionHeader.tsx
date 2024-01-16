894db74e577ccbc825467c1278debc37ff66ab71
Banks Nussman <banks@nussman.us>
2024-01-16 14:51:39 -0500

    Make Configurations health behavior consistent with others

--- packages/manager/src/features/LoadBalancers/LoadBalancerDetail/Configurations/ConfigurationAccordionHeader.tsx
+++ packages/manager/src/features/LoadBalancers/LoadBalancerDetail/Configurations/ConfigurationAccordionHeader.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 import { Stack } from 'src/components/Stack';
 import { Typography } from 'src/components/Typography';
-import { useLoadBalancerConfigurationsEndpointsHealth } from 'src/queries/aglb/configurations';
 import { pluralize } from 'src/utilities/pluralize';
 
-import { EndpointHealth } from '../EndpointHealth';
+import { ConfigurationEndpointHealth } from './ConfigurationEndpointHealth';
 
 import type { Configuration } from '@linode/api-v4';
 
@@ -16,9 +15,6 @@ interface Props {
 
 export const ConfigurationAccordionHeader = (props: Props) => {
   const { configuration, loadbalancerId } = props;
-  const { data } = useLoadBalancerConfigurationsEndpointsHealth(loadbalancerId);
-
-  const health = data?.configurations.find((c) => c.id === configuration.id);
 
   return (
     <Stack
@@ -37,18 +33,11 @@ export const ConfigurationAccordionHeader = (props: Props) => {
           {pluralize('Route', 'Routes', configuration.routes.length)}
         </Typography>
       </Stack>
-      <Stack direction="row" spacing={2}>
-        <Stack direction="row" spacing={1}>
-          {health && (
-            <>
-              <Typography>Endpoints:</Typography>
-              <EndpointHealth
-                down={health.total_endpoints - health.healthy_endpoints}
-                up={health.healthy_endpoints}
-              />
-            </>
-          )}
-        </Stack>
+      <Stack alignItems="center" direction="row" spacing={2}>
+        <ConfigurationEndpointHealth
+          configurationId={configuration.id}
+          loadBalancerId={loadbalancerId}
+        />
         <Typography>ID: {configuration.id}</Typography>
       </Stack>
     </Stack>
