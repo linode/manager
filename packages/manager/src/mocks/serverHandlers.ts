5f82d150909c62a8070d8356fdc9a19366ff5fbe
Jaalah Ramos <jaalah.ramos@gmail.com>
2024-01-19 12:32:21 -0500

    Undo changes

--- packages/manager/src/mocks/serverHandlers.ts
+++ packages/manager/src/mocks/serverHandlers.ts
@@ -1078,12 +1078,6 @@ export const handlers = [
       active_promotions: promoFactory.buildList(1),
       active_since: '2022-11-30',
       balance: 50,
-      capabilities: [
-        ...accountFactory.build().capabilities,
-        // 'Account Relationship: Child',
-        'Account Relationship: Parent',
-        // 'Account Relationship: Proxy',
-      ],
       company: 'Mock Company',
     });
     return res(ctx.json(account));
