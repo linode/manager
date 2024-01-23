39a9ef4a3d6f339c64033085763aaaffaf524138
mjac0bs <mjacobs@akamai.com>
2024-01-23 10:00:46 -0700

    Address feedback: Add error, loading states to Close Account button

--- packages/manager/src/features/Account/CloseAccountSetting.tsx
+++ packages/manager/src/features/Account/CloseAccountSetting.tsx
@@ -3,12 +3,14 @@ import * as React from 'react';
 
 import { Accordion } from 'src/components/Accordion';
 import { Button } from 'src/components/Button/Button';
+import { Notice } from 'src/components/Notice/Notice';
 import { useFlags } from 'src/hooks/useFlags';
 import { useChildAccounts } from 'src/queries/account';
 import { useProfile } from 'src/queries/profile';
 
 import CloseAccountDialog from './CloseAccountDialog';
 import {
+  CHILD_ACCOUNT_LOADING_ERROR,
   CHILD_PROXY_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT,
   PARENT_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT,
 } from './constants';
@@ -17,34 +19,43 @@ const CloseAccountSetting = () => {
   const [dialogOpen, setDialogOpen] = React.useState<boolean>(false);
 
   const { data: profile } = useProfile();
-  const { data: childAccounts } = useChildAccounts({});
+  const { data: childAccounts, isError, isLoading } = useChildAccounts({});
   const flags = useFlags();
 
+  const isParentAccount = profile?.user_type === 'parent';
   const hasChildAccounts = Boolean(
-    profile?.user_type === 'parent' &&
-      childAccounts &&
-      childAccounts?.data?.length > 0
+    isParentAccount && childAccounts && childAccounts?.data?.length > 0
   );
-  const isCloseAccountDisabled =
+  const isCloseAccountDisabled = Boolean(
     flags.parentChildAccountAccess &&
-    (hasChildAccounts ||
-      profile?.user_type === 'child' ||
-      profile?.user_type === 'proxy');
+      (hasChildAccounts ||
+        profile?.user_type === 'child' ||
+        profile?.user_type === 'proxy' ||
+        isError)
+  );
+  const closeAccountButtonTooltipText =
+    isCloseAccountDisabled && isParentAccount
+      ? PARENT_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT
+      : CHILD_PROXY_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT;
 
   return (
     <>
       <Accordion defaultExpanded={true} heading="Close Account">
         <Grid container direction="column">
           <Grid>
+            {isParentAccount && isError && (
+              <Notice text={CHILD_ACCOUNT_LOADING_ERROR} variant="error" />
+            )}
             <Button
               tooltipText={
-                hasChildAccounts
-                  ? PARENT_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT
-                  : CHILD_PROXY_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT
+                !isLoading && !isError
+                  ? closeAccountButtonTooltipText
+                  : undefined
               }
               buttonType="outlined"
               data-testid="close-account-button"
               disabled={isCloseAccountDisabled}
+              loading={isLoading}
               onClick={() => setDialogOpen(true)}
             >
               Close Account
--- packages/manager/src/features/Account/constants.ts
+++ packages/manager/src/features/Account/constants.ts
@@ -5,3 +5,6 @@ export const PARENT_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT =
   'Remove indirect customers before closing the account.';
 export const CHILD_PROXY_USER_CLOSE_ACCOUNT_TOOLTIP_TEXT =
   'Contact your business partner to close your account.';
+
+export const CHILD_ACCOUNT_LOADING_ERROR =
+  'There was an error loading indirect customer accounts.';
