b036c521fee40c6e6fb556d22ab86d2c20192bfa
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2024-01-23 12:23:50 -0500

    upcoming: [M3-7430] - Implement Account Switching Functionality (#10064)
    Co-authored-by: Jaalah Ramos <jaalah.ramos@gmail.com>
    Co-authored-by: Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>

---
+++ packages/manager/src/features/Account/utils.ts
@@ -0,0 +1,78 @@
+import { getStorage, setStorage } from 'src/utilities/storage';
+
+import type { Token } from '@linode/api-v4';
+
+// TODO: Parent/Child: FOR MSW ONLY, REMOVE WHEN API IS READY
+// ================================================================
+// const mockExpiredTime =
+//   'Mon Nov 20 2023 22:50:52 GMT-0800 (Pacific Standard Time)';
+// ================================================================
+
+/**
+ * Determine whether the tokens used for switchable accounts are still valid.
+ */
+export const isParentTokenValid = ({
+  isProxyUser,
+}: {
+  isProxyUser: boolean;
+}) => {
+  const now = new Date().toISOString();
+
+  // From a proxy user, check whether parent token is still valid before switching.
+  if (
+    isProxyUser &&
+    now >
+      new Date(getStorage('authentication/parent_token/expire')).toISOString()
+
+    // TODO: Parent/Child: FOR MSW ONLY, REMOVE WHEN API IS READY
+    // ================================================================
+    // new Date(mockExpiredTime).toISOString()
+    // ================================================================
+  ) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * Set token information in the local storage.
+ * This allows us to store a token for later use, such as switching between parent and proxy accounts.
+ */
+export const setTokenInLocalStorage = ({
+  prefix,
+  token = { expiry: '', scopes: '', token: '' },
+}: {
+  prefix: string;
+  token?: Pick<Token, 'expiry' | 'scopes' | 'token'>;
+}) => {
+  const { expiry, scopes, token: tokenValue } = token;
+
+  if (!tokenValue || !expiry) {
+    return;
+  }
+
+  setStorage(`${prefix}/token`, tokenValue);
+  setStorage(`${prefix}/expire`, expiry);
+  setStorage(`${prefix}/scopes`, scopes);
+};
+
+/**
+ * Set the active token in the local storage.
+ */
+export const updateCurrentTokenBasedOnUserType = ({
+  userType,
+}: {
+  userType: 'parent' | 'proxy';
+}) => {
+  const storageKeyPrefix = `authentication/${userType}_token`;
+
+  const userToken = getStorage(`${storageKeyPrefix}/token`);
+  const userScope = getStorage(`${storageKeyPrefix}/scopes`);
+  const userExpiry = getStorage(`${storageKeyPrefix}/expire`);
+
+  if (userToken) {
+    setStorage('authentication/token', userToken);
+    setStorage('authentication/scopes', userScope);
+    setStorage('authentication/expire', userExpiry);
+  }
+};
