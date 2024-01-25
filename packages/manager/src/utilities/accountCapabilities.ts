7e80308766bfbfa6fd413f788d9e71465fd59336
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2024-01-25 12:37:57 -0500

    Update packages/manager/src/utilities/accountCapabilities.ts
    Co-authored-by: Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>

--- packages/manager/src/utilities/accountCapabilities.ts
+++ packages/manager/src/utilities/accountCapabilities.ts
@@ -6,7 +6,7 @@ import type { AccountCapability } from '@linode/api-v4';
  * @returns true if the feature is returned from account.capabilities **or** if it is explicitly enabled
  * by a feature flag
  *
- * We use "or" insted of "and" here to allow us to enable features in "lower" environments
+ * We use "or" instead of "and" here to allow us to enable features in "lower" environments
  * without needing the customer capability.
  *
  * If you need to launch a production feature, but have it be gated,
