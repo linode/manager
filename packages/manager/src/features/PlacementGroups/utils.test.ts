75f0fa2576d6b7747cbc697695d83fd26287b8c0
Alban Bailly <abailly@akamai.com>
2024-01-29 09:32:15 -0500

    moar covering

--- packages/manager/src/features/PlacementGroups/utils.test.ts
+++ packages/manager/src/features/PlacementGroups/utils.test.ts
@@ -1,4 +1,4 @@
-import { getPlacementGroupLinodeCount } from './utils';
+import { affinityTypeOptions, getPlacementGroupLinodeCount } from './utils';
 
 describe('getPlacementGroupLinodeCount', () => {
   it('returns the length of the linode_ids array', () => {
@@ -7,5 +7,18 @@ describe('getPlacementGroupLinodeCount', () => {
         linode_ids: [1, 2, 3],
       } as any)
     ).toBe(3);
+  });
+});
+
+describe('affinityTypeOptions', () => {
+  it('returns an array of objects with label and value properties', () => {
+    expect(affinityTypeOptions).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          label: expect.any(String),
+          value: expect.any(String),
+        }),
+      ])
+    );
   });
 });
