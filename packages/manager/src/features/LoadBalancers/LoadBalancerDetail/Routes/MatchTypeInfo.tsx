ef4841f654735d0f7c0e16e47606eb4836bac751
Banks Nussman <banks@nussman.us>
2024-01-30 12:44:45 -0500

    add new match type

--- packages/manager/src/features/LoadBalancers/LoadBalancerDetail/Routes/MatchTypeInfo.tsx
+++ packages/manager/src/features/LoadBalancers/LoadBalancerDetail/Routes/MatchTypeInfo.tsx
@@ -15,8 +15,12 @@ export const MatchTypeInfo = () => {
       title: 'HTTP Method',
     },
     {
-      description: 'Match is on a network path.',
-      title: 'Path',
+      description: 'Match is on the prefix of a network path.',
+      title: 'Path Prefix',
+    },
+    {
+      description: '[need copy]',
+      title: 'Path Regex',
     },
     {
       description:
--- packages/manager/src/features/LoadBalancers/LoadBalancerDetail/Routes/constants.ts
+++ packages/manager/src/features/LoadBalancers/LoadBalancerDetail/Routes/constants.ts
@@ -23,11 +23,11 @@ export const ROUTE_COPY = {
         'For TCP load balancers, a rule consists of service targets and the percentage of incoming requests that should be directed to each target. Add as many service targets as required, but the percentages for all targets must total 100%.',
     },
     MatchValue: {
-      header: 'The format for http header is: X-name=value.',
-      host: '',
+      header: 'The format for http header is: X-name:value.',
       method: 'The request methods include: DELETE, GET, HEAD, POST, and PUT.',
       path_prefix:
         'The format of the path rule is: /pathname1/pathame2. The initial slash is required, but the trailing slash is not.',
+      path_regex: '[need copy]',
       query:
         'The format for query string is: ?name=value. The query string name must be preceded by a question mark (?).',
     },
