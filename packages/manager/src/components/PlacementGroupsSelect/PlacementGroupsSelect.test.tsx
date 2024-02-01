c41d9f4f5920c649e0aae5b31f4fa62776dc953e
ecarrill <ecarrill@akamai.com>
2024-02-01 10:39:45 -0800

    Update unit test

--- packages/manager/src/components/PlacementGroupsSelect/PlacementGroupsSelect.test.tsx
+++ packages/manager/src/components/PlacementGroupsSelect/PlacementGroupsSelect.test.tsx
@@ -7,15 +7,15 @@ import {
   PlacementGroupsSelectProps,
 } from './PlacementGroupsSelect';
 
-describe('PlacementGroupSelect', () => {
-  const props: PlacementGroupsSelectProps = {
-    errorText: '',
-    id: '',
-    label: '',
-    noOptionsMessage: '',
-    selectedRegionID: 'us-southeast',
-  };
+const props: PlacementGroupsSelectProps = {
+  errorText: '',
+  id: '',
+  label: 'Placement Groups in Atlanta, GA(us-southeast)',
+  noOptionsMessage: '',
+  selectedRegionID: 'us-southeast',
+};
 
+describe('PlacementGroupSelect', () => {
   it('should render a Select component', () => {
     const { getByTestId } = renderWithTheme(
       <PlacementGroupsSelect {...props} />
@@ -24,30 +24,7 @@ describe('PlacementGroupSelect', () => {
   });
 
   it('should render a Select component with the correct label', () => {
-    const { getByText } = renderWithTheme(
-      <PlacementGroupsSelect {...props} label="Placement Group" />
-    );
-    expect(getByText('Region')).toBeInTheDocument();
-  });
-
-  it('should render a Select component with the correct error text', () => {
-    const { getByText } = renderWithTheme(
-      <PlacementGroupsSelect
-        {...props}
-        errorText="There are no Placement Groups in this region."
-      />
-    );
-    expect(
-      getByText('There are no Placement Groups in this region.')
-    ).toBeInTheDocument();
-  });
-
-  it('should render a Select component with the Notice component', () => {
-    const { getByText } = renderWithTheme(
-      <PlacementGroupsSelect {...props} selectedRegionID={undefined} />
-    );
-    expect(
-      getByText('Select a region above to see available Placement Groups.')
-    ).toBeInTheDocument();
+    const { getByText } = renderWithTheme(<PlacementGroupsSelect {...props} />);
+    expect(getByText(/Placement Groups in /)).toBeInTheDocument();
   });
 });
