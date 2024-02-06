081a83990fa3600e737e1663f5a0185e40fde9c3
mjac0bs <mjacobs@akamai.com>
2024-02-06 08:38:19 -0700

    Fix typo in factory

--- packages/manager/src/factories/accountLogin.ts
+++ packages/manager/src/factories/accountLogin.ts
@@ -4,7 +4,7 @@ import * as Factory from 'factory.ts';
 export const accountLoginFactory = Factory.Sync.makeFactory<AccountLogin>({
   datetime: '2021-05-21T14:27:51',
   id: Factory.each((id) => id),
-  ip: '1.127.0.0.1',
+  ip: '127.0.0.1',
   restricted: false,
   status: 'successful',
   username: 'mock-user',
