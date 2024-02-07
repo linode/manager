35dccda8c3794053be62ce77a6cb291a186df9ba
Alban Bailly <abailly@akamai.com>
2024-02-07 17:16:47 -0500

    Improve add IP drawer

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/AddIPDrawer.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/AddIPDrawer.tsx
@@ -3,6 +3,7 @@ import { useTheme } from '@mui/material/styles';
 import * as React from 'react';
 
 import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
+import { Divider } from 'src/components/Divider';
 import { Drawer } from 'src/components/Drawer';
 import { Item } from 'src/components/EnhancedSelect/Select';
 import { FormControlLabel } from 'src/components/FormControlLabel';
@@ -10,6 +11,7 @@ import { Link } from 'src/components/Link';
 import { Notice } from 'src/components/Notice/Notice';
 import { Radio } from 'src/components/Radio/Radio';
 import { RadioGroup } from 'src/components/RadioGroup';
+import { Stack } from 'src/components/Stack';
 import { Tooltip } from 'src/components/Tooltip';
 import { Typography } from 'src/components/Typography';
 import {
@@ -163,7 +165,7 @@ export const AddIPDrawer = (props: Props) => {
 
   return (
     <Drawer onClose={onClose} open={open} title="Add an IP Address">
-      <React.Fragment>
+      <Stack spacing={2}>
         <Typography variant="h2">IPv4</Typography>
         {Boolean(ipv4Error) && (
           <Notice spacingTop={8} text={ipv4Error?.[0].reason} variant="error" />
@@ -204,7 +206,6 @@ export const AddIPDrawer = (props: Props) => {
                   label: 'Allocate',
                   loading: ipv4Loading,
                   onClick: handleAllocateIPv4,
-                  sx: { marginBottom: 8 },
                 }}
               />
             </div>
@@ -216,11 +217,11 @@ export const AddIPDrawer = (props: Props) => {
               label: 'Allocate',
               loading: ipv4Loading,
               onClick: handleAllocateIPv4,
-              sx: { marginBottom: 8 },
             }}
           />
         )}
-        <Typography sx={{ marginTop: theme.spacing(4) }} variant="h2">
+        <Divider sx={{ pt: 2 }} />
+        <Typography sx={{ pt: 2 }} variant="h2">
           IPv6
         </Typography>
         {Boolean(ipv6Error) && (
@@ -269,7 +270,7 @@ export const AddIPDrawer = (props: Props) => {
             sx: { marginBottom: 8 },
           }}
         />
-      </React.Fragment>
+      </Stack>
     </Drawer>
   );
 };
