8464b46cee049224674990981564b9bf190503d1
cliu-akamai <126020611+cliu-akamai@users.noreply.github.com>
2024-02-08 12:18:01 -0500

    test: [M3-7698] - Add test to check proxy user disabled username/email field (#10139)
    * M3-7689: Add test to check proxy user disabled username/email field
    
    * fix comments
    
    * fix comments
    
    * Added changeset: Add test to check proxy user disabled username/email field

--- packages/manager/cypress/e2e/core/account/change-username.spec.ts
+++ packages/manager/cypress/e2e/core/account/change-username.spec.ts
@@ -1,3 +1,11 @@
+import { Profile } from '@linode/api-v4';
+import { profileFactory } from '@src/factories';
+import {
+  mockAppendFeatureFlags,
+  mockGetFeatureFlagClientstream,
+} from 'support/intercepts/feature-flags';
+import { makeFeatureFlagData } from 'support/util/feature-flags';
+import { mockGetProfile } from 'support/intercepts/profile';
 import { getProfile } from 'support/api/account';
 import { interceptGetProfile } from 'support/intercepts/profile';
 import {
@@ -6,6 +14,50 @@ import {
 } from 'support/intercepts/account';
 import { ui } from 'support/ui';
 import { randomString } from 'support/util/random';
+
+const verifyUsernameAndEmail = (
+  mockRestrictedProxyProfile: Profile,
+  tooltip: string,
+  checkEmail: boolean
+) => {
+  // TODO: Parent/Child - M3-7559 clean up when feature is live in prod and feature flag is removed.
+  mockAppendFeatureFlags({
+    parentChildAccountAccess: makeFeatureFlagData(true),
+  }).as('getFeatureFlags');
+  mockGetFeatureFlagClientstream().as('getClientStream');
+
+  mockGetProfile(mockRestrictedProxyProfile);
+
+  // Navigate to User Profile page
+  cy.visitWithLogin('/profile/display');
+
+  // Confirm the username and email address fields are disabled, as well their respective save buttons
+  cy.get('[id="username"]').should('be.disabled');
+  ui.button
+    .findByTitle('Update Username')
+    .should('be.visible')
+    .should('be.disabled')
+    .trigger('mouseover');
+  // Click the button first, then confirm the tooltip is shown
+  ui.tooltip.findByText(tooltip).should('be.visible');
+
+  // Refresh the page
+  mockGetProfile(mockRestrictedProxyProfile);
+  cy.reload();
+
+  if (checkEmail) {
+    cy.get('[id="email"]').should('be.disabled');
+    ui.button
+      .findByTitle('Update Email')
+      .should('be.visible')
+      .should('be.disabled')
+      .trigger('mouseover');
+    // Click the button first, then confirm the tooltip is shown
+    ui.tooltip
+      .findByText('This account type cannot update this field.')
+      .should('be.visible');
+  }
+};
 
 describe('username', () => {
   /*
@@ -95,5 +147,46 @@ describe('username', () => {
 
       cy.findByText('Username updated successfully.').should('be.visible');
     });
+  });
+
+  it('disables username/email fields for restricted proxy user', () => {
+    const mockRestrictedProxyProfile = profileFactory.build({
+      username: 'restricted-proxy-user',
+      user_type: 'proxy',
+      restricted: true,
+    });
+
+    verifyUsernameAndEmail(
+      mockRestrictedProxyProfile,
+      'This account type cannot update this field.',
+      true
+    );
+  });
+
+  it('disables username/email fields for unrestricted proxy user', () => {
+    const mockUnrestrictedProxyProfile = profileFactory.build({
+      username: 'unrestricted-proxy-user',
+      user_type: 'proxy',
+    });
+
+    verifyUsernameAndEmail(
+      mockUnrestrictedProxyProfile,
+      'This account type cannot update this field.',
+      true
+    );
+  });
+
+  it('disables username/email fields for regular restricted user', () => {
+    const mockRegularRestrictedProfile = profileFactory.build({
+      username: 'regular-restricted-user',
+      user_type: null,
+      restricted: true,
+    });
+
+    verifyUsernameAndEmail(
+      mockRegularRestrictedProfile,
+      'Restricted users cannot update their username. Please contact an account administrator.',
+      false
+    );
   });
 });
