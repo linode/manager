342169c7d2035183b3fb08d8189eed3413e0edfc
ecarrill <ecarrill@akamai.com>
2024-02-08 10:22:08 -0800

    Add notice for non-compliant placement groups

--- packages/manager/src/features/PlacementGroups/PlacementGroupsDetail/PlacementGroupsSummary/PlacementGroupsSummary.tsx
+++ packages/manager/src/features/PlacementGroups/PlacementGroupsDetail/PlacementGroupsSummary/PlacementGroupsSummary.tsx
@@ -2,8 +2,10 @@ import { AFFINITY_TYPES } from '@linode/api-v4';
 import Grid from '@mui/material/Unstable_Grid2';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Box } from 'src/components/Box';
+import { Notice } from 'src/components/Notice/Notice';
 import { Paper } from 'src/components/Paper';
 import { TooltipIcon } from 'src/components/TooltipIcon';
 import { Typography } from 'src/components/Typography';
@@ -20,55 +22,68 @@ interface Props {
 export const PlacementGroupsSummary = (props: Props) => {
   const { placementGroup } = props;
   const { data: regions } = useRegionsQuery();
+
   const regionLabel =
     regions?.find((region) => region.id === placementGroup.region)?.label ??
     placementGroup.region;
 
+  const warningNoticeText = `Placement Group ${placementGroup.label} (${placementGroup.affinity_type}) is non-compliant.
+  We are working to resolve compliance issues so that you can continue assigning Linodes to this Placement Group.`;
+
   return (
-    <Paper>
-      <Typography
-        sx={(theme) => ({ marginBottom: theme.spacing(3) })}
-        variant="h3"
-      >
-        Placement Group Configuration
-      </Typography>
-
-      <Grid container spacing={1}>
-        <Grid md={8} sm={12}>
-          <Box display="flex">
-            <StyledLabel>Linodes</StyledLabel>
-            <Typography sx={{ mx: 8 }}>
-              {`${placementGroup.linode_ids.length} of ${placementGroup.capacity}`}
-              <TooltipIcon
-                sxTooltipIcon={{
-                  marginLeft: '10px',
-                  padding: 0,
-                }}
-                status="help"
-                text={tooltipText}
-                tooltipPosition="right"
-              />
-            </Typography>
-          </Box>
+    <>
+      {!placementGroup.compliant && (
+        <Notice spacingBottom={0} spacingTop={12} variant="warning">
+          {warningNoticeText}
+          <br />
+          {/* TODO VM_Placement: Get link location */}
+          <Link className="secondaryLink" to={'#'}>
+            Learn more.
+          </Link>
+        </Notice>
+      )}
+      <Paper>
+        <Typography
+          sx={(theme) => ({ marginBottom: theme.spacing(3) })}
+          variant="h3"
+        >
+          Placement Group Configuration
+        </Typography>
+        <Grid container spacing={1}>
+          <Grid md={8} sm={12}>
+            <Box display="flex">
+              <StyledLabel>Linodes</StyledLabel>
+              <Typography sx={{ mx: 8 }}>
+                {`${placementGroup.linode_ids.length} of ${placementGroup.capacity}`}
+                <TooltipIcon
+                  sxTooltipIcon={{
+                    marginLeft: '10px',
+                    padding: 0,
+                  }}
+                  status="help"
+                  text={tooltipText}
+                  tooltipPosition="right"
+                />
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid md={8} sm={12}>
+            <Box display="flex">
+              <StyledLabel>Affinity Type</StyledLabel>
+              <Typography sx={{ mx: 8 }}>
+                {AFFINITY_TYPES[placementGroup?.affinity_type]}
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid md={8} sm={12}>
+            <Box display="flex">
+              <StyledLabel>Region</StyledLabel>
+              <Typography sx={{ mx: 8 }}>{regionLabel}</Typography>
+            </Box>
+          </Grid>
         </Grid>
-
-        <Grid md={8} sm={12}>
-          <Box display="flex">
-            <StyledLabel>Affinity Type</StyledLabel>
-            <Typography sx={{ mx: 8 }}>
-              {AFFINITY_TYPES[placementGroup?.affinity_type]}
-            </Typography>
-          </Box>
-        </Grid>
-
-        <Grid md={8} sm={12}>
-          <Box display="flex">
-            <StyledLabel>Region</StyledLabel>
-            <Typography sx={{ mx: 8 }}>{regionLabel}</Typography>
-          </Box>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </>
   );
 };
 
