9e03e40d6ff72d9535d55d2fd98c2e400f7dbed7
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2024-02-20 11:59:31 -0700

    upcoming: [M3-7775] - Disable ability to edit or delete a proxy user via User Profile page (#10202)
    * Disable fields and buttons on proxy user's user profile page
    
    * Separate Display and User Profile test specs; add coverage
    
    * Finish test coverage
    
    * Clean up
    
    * Meant to push the changesets too
    
    * Remove proxy user language

--- packages/manager/cypress/e2e/core/account/display-settings.spec.ts
+++ packages/manager/cypress/e2e/core/account/display-settings.spec.ts
@@ -8,10 +8,7 @@ import { makeFeatureFlagData } from 'support/util/feature-flags';
 import { mockGetProfile } from 'support/intercepts/profile';
 import { getProfile } from 'support/api/account';
 import { interceptGetProfile } from 'support/intercepts/profile';
-import {
-  interceptGetUser,
-  mockUpdateUsername,
-} from 'support/intercepts/account';
+import { mockUpdateUsername } from 'support/intercepts/account';
 import { ui } from 'support/ui';
 import { randomString } from 'support/util/random';
 
@@ -59,54 +56,7 @@ const verifyUsernameAndEmail = (
   }
 };
 
-describe('username', () => {
-  /*
-   * - Validates username update flow via the user profile page using mocked data.
-   */
-  it('can change username via user profile page', () => {
-    const newUsername = randomString(12);
-
-    getProfile().then((profile) => {
-      const username = profile.body.username;
-
-      interceptGetUser(username).as('getUser');
-      mockUpdateUsername(username, newUsername).as('updateUsername');
-
-      cy.visitWithLogin(`account/users/${username}`);
-      cy.wait('@getUser');
-
-      cy.findByText('Username').should('be.visible');
-      cy.findByText('Email').should('be.visible');
-      cy.findByText('Delete User').should('be.visible');
-
-      cy.get('[id="username"]')
-        .should('be.visible')
-        .should('have.value', username)
-        .clear()
-        .type(newUsername);
-
-      cy.get('[data-qa-textfield-label="Username"]')
-        .parent()
-        .parent()
-        .parent()
-        .within(() => {
-          ui.button
-            .findByTitle('Save')
-            .should('be.visible')
-            .should('be.enabled')
-            .click();
-        });
-
-      cy.wait('@updateUsername');
-
-      // No confirmation gets shown on this page when changes are saved.
-      // Confirm that the text field has the correct value instead.
-      cy.get('[id="username"]')
-        .should('be.visible')
-        .should('have.value', newUsername);
-    });
-  });
-
+describe('Display Settings', () => {
   /*
    * - Validates username update flow via the profile display page using mocked data.
    */
