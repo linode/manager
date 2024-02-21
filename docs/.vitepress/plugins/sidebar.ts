8017ae6ab8c7e091785dc8f21cc2c31ba2a6f856
Banks Nussman <banks@nussman.us>
2024-02-21 16:43:48 -0500

    normalize file names  more

--- docs/.vitepress/plugins/sidebar.ts
+++ docs/.vitepress/plugins/sidebar.ts
@@ -1,22 +1,30 @@
 import { readdirSync } from "fs";
 import { join, resolve } from "path";
 
-type LinkItem = { text: string; link: string }
+type LinkItem = { text: string; link: string };
 
-type SidebarItem = {
-  text: string;
-  collapsed?: boolean;
-  items: SidebarItem[] | LinkItem[]
-} | LinkItem;
+type SidebarItem =
+  | {
+      text: string;
+      collapsed?: boolean;
+      items: SidebarItem[] | LinkItem[];
+    }
+  | LinkItem;
 
-const DOCS_PATH = resolve(__dirname + '/../../');
+const DOCS_PATH = resolve(__dirname + "/../../");
 
-const exclude = ["cache", "public", "PULL_REQUEST_TEMPLATE.md", ".vitepress", "index.md"];
+const exclude = [
+  "cache",
+  "public",
+  "PULL_REQUEST_TEMPLATE.md",
+  ".vitepress",
+  "index.md",
+];
 
 const replacements = [
-  ['-', ' '],
-  ['_', ' '],
-  ['.md', ''],
+  ["-", " "],
+  ["_", " "],
+  [".md", ""],
 ];
 
 function isPathIgnored(path: string) {
@@ -28,10 +36,21 @@ function isPathIgnored(path: string) {
   return false;
 }
 
+function capitalize(s: string) {
+  return (
+    s.substring(0, 1).toUpperCase() + s.substring(1, s.length).toLowerCase()
+  );
+}
+
 function formatSidebarItemText(fileName: string) {
+  // removes -, _, and .md from files names to generate the title
   for (const [from, to] of replacements) {
     fileName = fileName.replaceAll(from, to);
   }
+  // Removes any number prefix. This allows us to order things by putting numbers in file names.
+  fileName = fileName.replace(/^[0-9]*/, "");
+  // Capitalizes each word in the file name
+  fileName = fileName.split(" ").map(capitalize).join(" ");
   return fileName;
 }
 
@@ -48,9 +67,16 @@ export function generateSidebar(dir: string) {
     }
 
     if (file.isDirectory()) {
-      sidebar.push({ text: formatSidebarItemText(file.name), collapsed: false, items: generateSidebar(filepath) });
+      sidebar.push({
+        text: formatSidebarItemText(file.name),
+        collapsed: false,
+        items: generateSidebar(filepath),
+      });
     } else {
-      sidebar.push({ text: formatSidebarItemText(file.name), link: filepath.split(DOCS_PATH)[1] });
+      sidebar.push({
+        text: formatSidebarItemText(file.name),
+        link: filepath.split(DOCS_PATH)[1],
+      });
     }
   }
 
