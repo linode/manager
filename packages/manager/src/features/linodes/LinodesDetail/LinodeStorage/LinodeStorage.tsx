14d122d6ecdf6209d9b4983565105478b7a3d00d
AlanH60 <85328997+AlanH60@users.noreply.github.com>
2024-02-23 01:09:40 -0500

    Hide Disk in Storage Tab

--- packages/manager/src/features/linodes/LinodesDetail/LinodeStorage/LinodeStorage.tsx
+++ packages/manager/src/features/linodes/LinodesDetail/LinodeStorage/LinodeStorage.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import LinodeDisks from '../LinodeAdvanced/LinodeDisks';
+/* -- Clanode Change -- */
+// import LinodeDisks from '../LinodeAdvanced/LinodeDisks';
+/* -- Clanoe Change End -- */
 import LinodeVolumes from 'src/features/linodes/LinodesDetail/LinodeAdvanced/LinodeVolumes';
 
 export const LinodeStorage: React.FC = () => {
   return (
     <>
-      <LinodeDisks />
+      {/* -- Clanode Change -- */
+      /* <LinodeDisks /> */
+      /* -- Clanode Change End -- */}
       <LinodeVolumes />
     </>
   );
