fd841e99f322ede1e424ee3dd20a052e9f518c74
Alban Bailly <abailly@akamai.com>
2024-02-27 16:06:48 -0500

    Affinity Enforcement

--- packages/manager/src/features/PlacementGroups/PlacementGroupsCreateDrawer.tsx
+++ packages/manager/src/features/PlacementGroups/PlacementGroupsCreateDrawer.tsx
@@ -5,11 +5,17 @@ import * as React from 'react';
 
 import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
 import { Autocomplete } from 'src/components/Autocomplete/Autocomplete';
+import { Box } from 'src/components/Box';
 import { Drawer } from 'src/components/Drawer';
+import { FormControlLabel } from 'src/components/FormControlLabel';
+import { FormLabel } from 'src/components/FormLabel';
 import { Notice } from 'src/components/Notice/Notice';
+import { Radio } from 'src/components/Radio/Radio';
+import { RadioGroup } from 'src/components/RadioGroup';
 import { RegionSelect } from 'src/components/RegionSelect/RegionSelect';
 import { Stack } from 'src/components/Stack';
 import { TextField } from 'src/components/TextField';
+import { Typography } from 'src/components/Typography';
 import { useFormValidateOnChange } from 'src/hooks/useFormValidateOnChange';
 import { useCreatePlacementGroup } from 'src/queries/placementGroups';
 import { useRegionsQuery } from 'src/queries/regions';
@@ -151,6 +157,53 @@ export const PlacementGroupsCreateDrawer = (
             options={affinityTypeOptions}
             placeholder="Select an Affinity Type"
           />
+          <Box sx={{ pt: 2 }}>
+            <Notice
+              text="Once you create a placement group, you cannot change its Affinity Enforcement setting."
+              variant="warning"
+            />
+            <FormLabel htmlFor="affinity-enforcement-radio-group">
+              Affinity Enforcement
+            </FormLabel>
+            {errors.is_strict !== undefined && (
+              <Notice variant="error">
+                <Typography>{errors.is_strict}</Typography>
+              </Notice>
+            )}
+            <RadioGroup
+              onChange={(event) => {
+                handleChange(event);
+                setFieldValue('is_strict', event.target.value === 'true');
+              }}
+              id="affinity-enforcement-radio-group"
+              name="is_strict"
+              value={values.is_strict}
+            >
+              <FormControlLabel
+                label={
+                  <Typography>
+                    <strong>Strict.</strong> You cannot assign a Linode to your
+                    placement group if it will violate the policy of your
+                    selected Affinity Type (best practice).
+                  </Typography>
+                }
+                control={<Radio />}
+                value={true}
+              />
+              <FormControlLabel
+                label={
+                  <Typography>
+                    <strong>Flexible.</strong> Flexible. You can assign a Linode
+                    to your placement group, even if it violates the policy of
+                    your selected Affinity Type.
+                  </Typography>
+                }
+                control={<Radio />}
+                sx={{ mt: 2 }}
+                value={false}
+              />
+            </RadioGroup>
+          </Box>
           <ActionsPanel
             primaryButtonProps={{
               'data-testid': 'submit',
