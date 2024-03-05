2b06c8fa00cb3a2fcd84db2238a4509e24762264
Hana Xu <hxu@akamai.com>
2024-03-05 14:19:10 -0500

    update unit test

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeBackup/BackupsPlaceholder.test.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeBackup/BackupsPlaceholder.test.tsx
@@ -5,7 +5,7 @@ import { renderWithTheme } from 'src/utilities/testHelpers';
 import { BackupsPlaceholder } from './BackupsPlaceholder';
 
 describe('BackupsPlaceholder', () => {
-  it('should display a warning notice if linodeIsInEdgeRegion is true and disable the enable backups button', () => {
+  it('should disable the enable backups button if linodeIsInEdgeRegion is true', () => {
     const { getByTestId } = renderWithTheme(
       <BackupsPlaceholder
         disabled={false}
@@ -13,21 +13,19 @@ describe('BackupsPlaceholder', () => {
         linodeIsInEdgeRegion={true}
       />
     );
-    expect(getByTestId('notice-warning')).toBeInTheDocument();
     expect(getByTestId('placeholder-button')).toHaveAttribute(
       'aria-disabled',
       'true'
     );
   });
-  it('should not display a warning notice if linodeIsInEdgeRegion is false and the enable backups button should not be disabled', () => {
-    const { getByTestId, queryByTestId } = renderWithTheme(
+  it('should not disable the enable backups button if linodeIsInEdgeRegion is false', () => {
+    const { getByTestId } = renderWithTheme(
       <BackupsPlaceholder
         disabled={false}
         linodeId={1}
         linodeIsInEdgeRegion={false}
       />
     );
-    expect(queryByTestId('notice-warning')).not.toBeInTheDocument();
     expect(getByTestId('placeholder-button')).toHaveAttribute(
       'aria-disabled',
       'false'
