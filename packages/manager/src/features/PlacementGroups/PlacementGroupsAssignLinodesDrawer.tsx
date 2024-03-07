0500853f523dfabd76edd5520a5435f5bc80ef8c
Alban Bailly <abailly@akamai.com>
2024-03-07 10:38:57 -0500

    feedback

--- packages/manager/src/features/PlacementGroups/PlacementGroupsAssignLinodesDrawer.tsx
+++ packages/manager/src/features/PlacementGroups/PlacementGroupsAssignLinodesDrawer.tsx
@@ -1,5 +1,4 @@
 import { AFFINITY_TYPES } from '@linode/api-v4';
-import Grid from '@mui/material/Unstable_Grid2';
 import * as React from 'react';
 
 import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
@@ -130,62 +129,60 @@ export const PlacementGroupsAssignLinodesDrawer = (
 
   return (
     <Drawer onClose={handleDrawerClose} open={open} title={drawerTitle}>
-      <Grid>
-        {generalError ? <Notice text={generalError} variant="error" /> : null}
-        <Typography my={4}>
-          <strong>Affinity Enforcement: </strong>
-          {getAffinityEnforcement(selectedPlacementGroup.is_strict)}
-        </Typography>
-        <Divider sx={{ mb: 4 }} />
-        <form onSubmit={handleAssignLinode}>
-          <Stack spacing={1}>
-            {hasReachedCapacity && open && (
-              <Notice
-                text="This Placement Group has the maximum number of Linodes allowed"
-                variant="error"
-              />
-            )}
-            <Typography>
-              A Linode can only be assigned to a single Placement Group.
-            </Typography>
-
-            <Typography>
-              If you need to create a new Linode, go to{' '}
-              <Link to="/linodes/create">Create Linode</Link> and return to this
-              page to assign it to this Placement Group.
-            </Typography>
-            <Box sx={{ alignItems: 'flex-end', display: 'flex' }}>
-              <LinodeSelect
-                onSelectionChange={(value) => {
-                  setSelectedLinode(value);
-                }}
-                disabled={hasReachedCapacity}
-                label={linodeSelectLabel}
-                options={getLinodeSelectOptions()}
-                placeholder="Select Linode or type to search"
-                sx={{ flexGrow: 1 }}
-                value={selectedLinode?.id ?? null}
-              />
-              <TooltipIcon
-                placement="right"
-                status="help"
-                sxTooltipIcon={{ position: 'relative', top: 4 }}
-                text="Only displaying Linodes that aren’t assigned to a Placement Group"
-              />
-            </Box>
-
-            <ActionsPanel
-              primaryButtonProps={{
-                'data-testid': 'submit',
-                disabled: !selectedLinode || hasReachedCapacity,
-                label: 'Assign Linode',
-                type: 'submit',
-              }}
-              sx={{ pt: 2 }}
+      {generalError ? <Notice text={generalError} variant="error" /> : null}
+      <Typography my={4}>
+        <strong>Affinity Enforcement: </strong>
+        {getAffinityEnforcement(selectedPlacementGroup.is_strict)}
+      </Typography>
+      <Divider sx={{ mb: 4 }} />
+      <form onSubmit={handleAssignLinode}>
+        <Stack spacing={1}>
+          {hasReachedCapacity && open && (
+            <Notice
+              text="This Placement Group has the maximum number of Linodes allowed"
+              variant="error"
             />
-          </Stack>
-        </form>
-      </Grid>
+          )}
+          <Typography>
+            A Linode can only be assigned to a single Placement Group.
+          </Typography>
+
+          <Typography>
+            If you need to create a new Linode, go to{' '}
+            <Link to="/linodes/create">Create Linode</Link> and return to this
+            page to assign it to this Placement Group.
+          </Typography>
+          <Box sx={{ alignItems: 'flex-end', display: 'flex' }}>
+            <LinodeSelect
+              onSelectionChange={(value) => {
+                setSelectedLinode(value);
+              }}
+              disabled={hasReachedCapacity}
+              label={linodeSelectLabel}
+              options={getLinodeSelectOptions()}
+              placeholder="Select Linode or type to search"
+              sx={{ flexGrow: 1 }}
+              value={selectedLinode?.id ?? null}
+            />
+            <TooltipIcon
+              placement="right"
+              status="help"
+              sxTooltipIcon={{ position: 'relative', top: 4 }}
+              text="Only displaying Linodes that aren’t assigned to a Placement Group"
+            />
+          </Box>
+
+          <ActionsPanel
+            primaryButtonProps={{
+              'data-testid': 'submit',
+              disabled: !selectedLinode || hasReachedCapacity,
+              label: 'Assign Linode',
+              type: 'submit',
+            }}
+            sx={{ pt: 2 }}
+          />
+        </Stack>
+      </form>
     </Drawer>
   );
 };
