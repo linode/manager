bcdf914f45585cd6af5a72efb7da6e784a9c9dcf
Hana Xu <115299789+hana-linode@users.noreply.github.com>
2024-03-14 17:36:50 -0400

    feat: [M3-7829] - Linode Clone UI refinements (#10280)
    ## Description 📝
    Address refinements identified during the review process for Clone UI changes
    
    ## Changes  🔄
    - Don't need to make a request for every Linode in the Table view since we already have the data
      - No more jumpy loading spinners
    - Added "No results" state for mobile card view
    
    ## How to test 🧪
    ### Verification steps
    (How to verify changes)
    - Go to the Clone Linode page and observe that a network request is not made for each Linode in the table view
    - On mobile view, search for a Linode that does not exist. You should see "No results" text

--- packages/manager/src/features/Linodes/LinodesCreate/SelectLinodePanel/SelectLinodeCards.tsx
+++ packages/manager/src/features/Linodes/LinodesCreate/SelectLinodePanel/SelectLinodeCards.tsx
@@ -1,6 +1,8 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import React from 'react';
 
+import { Typography } from 'src/components/Typography';
+
 import { SelectLinodeCard } from './SelectLinodeCard';
 import { RenderLinodeProps } from './SelectLinodePanel';
 
@@ -11,16 +13,20 @@ export const SelectLinodeCards = ({
   selectedLinodeId,
 }: RenderLinodeProps) => (
   <Grid container spacing={2}>
-    {linodes.map((linode) => (
-      <SelectLinodeCard
-        handleSelection={() =>
-          handleSelection(linode.id, linode.type, linode.specs.disk)
-        }
-        disabled={disabled}
-        key={linode.id}
-        linode={linode}
-        selected={linode.id == selectedLinodeId}
-      />
-    ))}
+    {linodes.length > 0 ? (
+      linodes.map((linode) => (
+        <SelectLinodeCard
+          handleSelection={() =>
+            handleSelection(linode.id, linode.type, linode.specs.disk)
+          }
+          disabled={disabled}
+          key={linode.id}
+          linode={linode}
+          selected={linode.id == selectedLinodeId}
+        />
+      ))
+    ) : (
+      <Typography padding={1}>No results</Typography>
+    )}
   </Grid>
 );
