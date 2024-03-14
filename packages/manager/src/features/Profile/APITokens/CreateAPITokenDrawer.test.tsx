cfc12d4abe62a8a718acfa07704232a67e69df0c
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2024-03-14 14:19:48 -0400

    upcoming: [M3-7855] - Updates to PATs for Restricted Parent Users (#10269)
    Co-authored-by: Jaalah Ramos <jaalah.ramos@gmail.com>
    Co-authored-by: mjac0bs <mjacobs@akamai.com>

--- packages/manager/src/features/Profile/APITokens/CreateAPITokenDrawer.test.tsx
+++ packages/manager/src/features/Profile/APITokens/CreateAPITokenDrawer.test.tsx
@@ -3,14 +3,16 @@ import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
 import { appTokenFactory } from 'src/factories';
+import { grantsFactory } from 'src/factories/grants';
 import { profileFactory } from 'src/factories/profile';
 import { rest, server } from 'src/mocks/testServer';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
 import { CreateAPITokenDrawer } from './CreateAPITokenDrawer';
 
-// Mock the useProfile hooks to immediately return the expected data, circumventing the HTTP request and loading state.
+// Mock the useProfile and useGrants hooks to immediately return the expected data, circumventing the HTTP request and loading state.
 const queryMocks = vi.hoisted(() => ({
+  useGrants: vi.fn().mockReturnValue({}),
   useProfile: vi.fn().mockReturnValue({}),
 }));
 
@@ -19,6 +21,14 @@ vi.mock('src/queries/profile', async () => {
   return {
     ...actual,
     useProfile: queryMocks.useProfile,
+  };
+});
+
+vi.mock('src/queries/grants', async () => {
+  const actual = await vi.importActual<any>('src/queries/grants');
+  return {
+    ...actual,
+    useGrants: queryMocks.useGrants,
   };
 });
 
@@ -97,6 +107,24 @@ describe('Create API Token Drawer', () => {
     });
     const childScope = getByText('Child Account Access');
     expect(childScope).toBeInTheDocument();
+  });
+
+  it('Should not the Child Account Access scope for a restricted parent user account without the child_account_access grant', () => {
+    queryMocks.useProfile.mockReturnValue({
+      data: profileFactory.build({ user_type: 'parent' }),
+    });
+    queryMocks.useProfile.mockReturnValue({
+      data: grantsFactory.build({ global: { child_account_access: false } }),
+    });
+
+    const { queryByText } = renderWithTheme(
+      <CreateAPITokenDrawer {...props} />,
+      {
+        flags: { parentChildAccountAccess: true },
+      }
+    );
+    const childScope = queryByText('Child Account Access');
+    expect(childScope).not.toBeInTheDocument();
   });
 
   it('Should not show the Child Account Access scope for a non-parent user account with the parent/child feature flag on', () => {
