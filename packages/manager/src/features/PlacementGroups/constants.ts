483747fb645ea13e30d6e3bc54b329db4429c0c4
Alban Bailly <abailly@akamai.com>
2024-03-19 11:19:19 -0400

    feedback

--- packages/manager/src/features/PlacementGroups/constants.ts
+++ packages/manager/src/features/PlacementGroups/constants.ts
@@ -10,3 +10,8 @@ export const PLACEMENT_GROUP_LINODES_ERROR_MESSAGE =
   'There was an error loading Linodes for this Placement Group.';
 
 export const PLACEMENT_GROUP_TOOLTIP_TEXT = `The Affinity Type and Region determine the maximum number of VMs per group.`;
+
+export const PG_SELECT_TOOLTIP_COPY = `
+Add your virtual machine (VM) to a group to best meet your needs.
+You may want to group VMs closer together to help improve performance, or further apart to enable high-availability configurations.
+Learn more.`;
