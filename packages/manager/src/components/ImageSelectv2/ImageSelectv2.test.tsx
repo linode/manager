1bf9aa5a72658f1ce4146515c75520748f847bf9
Banks Nussman <banks@nussman.us>
2024-03-19 11:12:58 -0400

    add a unit test for `ImageSelectv2`

--- packages/manager/src/components/ImageSelectv2/ImageSelectv2.test.tsx
+++ packages/manager/src/components/ImageSelectv2/ImageSelectv2.test.tsx
@@ -1,5 +1,9 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
+import { imageFactory } from 'src/factories';
+import { makeResourcePage } from 'src/mocks/serverHandlers';
+import { HttpResponse, http, server } from 'src/mocks/testServer';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
 import { ImageSelectv2 } from './ImageSelectv2';
@@ -17,5 +21,71 @@ describe('ImageSelectv2', () => {
     );
 
     expect(getByPlaceholderText('Choose an image')).toBeVisible();
+  });
+
+  it('should render items returned by the API', async () => {
+    const images = imageFactory.buildList(5);
+
+    server.use(
+      http.get('*/v4/images', () => {
+        return HttpResponse.json(makeResourcePage(images));
+      })
+    );
+
+    const { getByPlaceholderText, getByText } = renderWithTheme(
+      <ImageSelectv2 value={null} />
+    );
+
+    await userEvent.click(getByPlaceholderText('Choose an image'));
+
+    for (const image of images) {
+      expect(getByText(image.label)).toBeVisible();
+    }
+  });
+
+  it('should call onChange when a value is selected', async () => {
+    const image = imageFactory.build();
+    const onChange = vi.fn();
+
+    server.use(
+      http.get('*/v4/images', () => {
+        return HttpResponse.json(makeResourcePage([image]));
+      })
+    );
+
+    const { getByPlaceholderText, getByText } = renderWithTheme(
+      <ImageSelectv2 onChange={onChange} value={null} />
+    );
+
+    await userEvent.click(getByPlaceholderText('Choose an image'));
+
+    const imageOption = getByText(image.label);
+
+    expect(imageOption).toBeVisible();
+
+    await userEvent.click(imageOption);
+
+    expect(onChange).toHaveBeenCalledWith(
+      expect.anything(),
+      image,
+      'selectOption',
+      expect.anything()
+    );
+  });
+
+  it('should correctly initialize with a default value', async () => {
+    const image = imageFactory.build();
+
+    server.use(
+      http.get('*/v4/images', () => {
+        return HttpResponse.json(makeResourcePage([image]));
+      })
+    );
+
+    const { findByDisplayValue } = renderWithTheme(
+      <ImageSelectv2 value={image.id} />
+    );
+
+    await findByDisplayValue(image.label);
   });
 });
