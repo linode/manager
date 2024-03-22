4d81095592acddf815261bb3f60d7d98ce367e9f
Banks Nussman <banks@nussman.us>
2024-03-22 15:41:01 -0400

    use constant for input size

--- packages/manager/src/features/Linodes/LinodeCreatev2/Access.tsx
+++ packages/manager/src/features/Linodes/LinodeCreatev2/Access.tsx
@@ -5,6 +5,7 @@ import UserSSHKeyPanel from 'src/components/AccessPanel/UserSSHKeyPanel';
 import { Divider } from 'src/components/Divider';
 import { Paper } from 'src/components/Paper';
 import { Skeleton } from 'src/components/Skeleton';
+import { inputMaxWidth } from 'src/foundations/themes/light';
 import { useRestrictedGlobalGrantCheck } from 'src/hooks/useRestrictedGlobalGrantCheck';
 
 import type { CreateLinodeRequest } from '@linode/api-v4';
@@ -23,7 +24,7 @@ export const Access = () => {
   return (
     <Paper>
       <React.Suspense
-        fallback={<Skeleton sx={{ height: '89px', maxWidth: '416px' }} />}
+        fallback={<Skeleton sx={{ height: '89px', maxWidth: inputMaxWidth }} />}
       >
         <Controller
           render={({ field, fieldState }) => (
