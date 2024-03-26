d6c9ee81e9cece22ac0913f3d10fbe2dc3aefc77
Banks Nussman <banks@nussman.us>
2024-03-26 17:31:52 -0400

    handle permissions in more places

--- packages/manager/src/features/Linodes/LinodeCreatev2/Tabs/Distributions.tsx
+++ packages/manager/src/features/Linodes/LinodeCreatev2/Tabs/Distributions.tsx
@@ -4,6 +4,7 @@ import { useController } from 'react-hook-form';
 import { ImageSelectv2 } from 'src/components/ImageSelectv2/ImageSelectv2';
 import { Paper } from 'src/components/Paper';
 import { Typography } from 'src/components/Typography';
+import { useRestrictedGlobalGrantCheck } from 'src/hooks/useRestrictedGlobalGrantCheck';
 
 import type { CreateLinodeRequest } from '@linode/api-v4';
 
@@ -12,10 +13,15 @@ export const Distributions = () => {
     name: 'image',
   });
 
+  const isCreateLinodeRestricted = useRestrictedGlobalGrantCheck({
+    globalGrantType: 'add_linodes',
+  });
+
   return (
     <Paper>
       <Typography variant="h2">Choose a Distribution</Typography>
       <ImageSelectv2
+        disabled={isCreateLinodeRestricted}
         errorText={fieldState.error?.message}
         onChange={(_, image) => field.onChange(image?.id ?? null)}
         value={field.value}
