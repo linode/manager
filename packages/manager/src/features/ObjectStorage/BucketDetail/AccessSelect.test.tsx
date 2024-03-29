607bba20d3a9cd6abf37b7dc62ecbc4978c31d69
Joe D'Amore <jdamore@linode.com>
2024-03-29 11:54:33 -0400

    Update tests for AccessSelect refactor

--- packages/manager/src/features/ObjectStorage/BucketDetail/AccessSelect.test.tsx
+++ packages/manager/src/features/ObjectStorage/BucketDetail/AccessSelect.test.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { act, fireEvent, screen, waitFor } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import * as React from 'react';
 
 import { renderWithTheme } from 'src/utilities/testHelpers';
@@ -11,39 +12,60 @@ const mockGetAccess = vi.fn();
 const mockUpdateAccess = vi.fn();
 
 const props: Props = {
-  getAccess: mockGetAccess.mockResolvedValue({
-    acl: 'public-read',
-    cors_enabled: true,
-  }),
+  getAccess: mockGetAccess.mockResolvedValue({ acl: 'private' }),
   name: 'my-object-name',
-  updateAccess: mockUpdateAccess,
+  updateAccess: mockUpdateAccess.mockResolvedValue({}),
   variant: 'object',
 };
 
 describe('AccessSelect', () => {
-  beforeEach(() => {
-    vi.resetAllMocks();
-    mockGetAccess.mockResolvedValue({ acl: 'public-read', cors_enabled: true });
-    mockUpdateAccess.mockResolvedValue({});
-  });
-
   it('shows the access', async () => {
     renderWithTheme(<AccessSelect {...props} />);
+    const aclSelect = screen.getByRole('combobox');
+
+    // Confirm that combobox input field value is 'Private'.
     await waitFor(() => {
-      expect(screen.getByRole('combobox')).toHaveValue('Public Read');
+      expect(aclSelect).toBeEnabled();
+      expect(aclSelect).toHaveValue('Private');
     });
+
+    // Confirm that 'Private' is selected upon opening the Autocomplete drop-down.
+    act(() => {
+      fireEvent.click(aclSelect);
+      fireEvent.change(aclSelect, { target: { value: 'P' } });
+    });
+
+    expect(screen.getByText('Private').closest('li')!).toHaveAttribute(
+      'aria-selected',
+      'true'
+    );
   });
 
   it('updates the access and submits the appropriate value', async () => {
     renderWithTheme(<AccessSelect {...props} />);
 
-    await waitFor(() => {
-      const aclSelect = screen.getByRole('combobox');
-      fireEvent.change(aclSelect, {
-        target: { value: 'private' },
-      });
+    const aclSelect = screen.getByRole('combobox');
+    const saveButton = screen.getByText('Save').closest('button')!;
 
-      expect(screen.getByRole('combobox')).toHaveValue('Public Read');
+    await waitFor(() => {
+      expect(aclSelect).toBeEnabled();
+      expect(aclSelect).toHaveValue('Private');
     });
+
+    act(() => {
+      fireEvent.click(aclSelect);
+      fireEvent.change(aclSelect, { target: { value: 'Authenticated Read' } });
+    });
+
+    expect(aclSelect).toHaveValue('Authenticated Read');
+    userEvent.click(screen.getByText('Authenticated Read'));
+
+    await waitFor(() => {
+      expect(screen.getByRole('combobox')).toHaveValue('Authenticated Read');
+      expect(saveButton).toBeEnabled();
+    });
+
+    fireEvent.click(saveButton);
+    expect(mockUpdateAccess).toHaveBeenCalledWith('authenticated-read', true);
   });
 });
