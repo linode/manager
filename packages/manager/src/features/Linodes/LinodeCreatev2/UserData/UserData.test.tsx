016866a2fef4f2ad1708d8b4d096c7051ab208f9
Banks Nussman <banks@nussman.us>
2024-03-29 11:29:47 -0400

    unit testing

--- packages/manager/src/features/Linodes/LinodeCreatev2/UserData/UserData.test.tsx
+++ packages/manager/src/features/Linodes/LinodeCreatev2/UserData/UserData.test.tsx
@@ -1,57 +1,63 @@
 import { fireEvent } from '@testing-library/react';
 import * as React from 'react';
 
+import { imageFactory, regionFactory } from 'src/factories';
+import { makeResourcePage } from 'src/mocks/serverHandlers';
+import { HttpResponse, http, server } from 'src/mocks/testServer';
 import { renderWithThemeAndHookFormContext } from 'src/utilities/testHelpers';
 
 import { UserData } from './UserData';
 
 describe('Linode Create v2 UserData', () => {
-  it('should display a warning message if the user data is not in an accepted format', () => {
-    const inputValue = '#test-string';
-    const { getByLabelText, getByText } = renderWithThemeAndHookFormContext({
+  it('should render if the selected image supports cloud-init and the region supports metadata', async () => {
+    const image = imageFactory.build({ capabilities: ['cloud-init'] });
+    const region = regionFactory.build({ capabilities: ['Metadata'] });
+
+    server.use(
+      http.get('*/v4/images/*', () => {
+        return HttpResponse.json(image);
+      }),
+      http.get('*/v4/regions', () => {
+        return HttpResponse.json(makeResourcePage([region]));
+      })
+    );
+
+    const { findByText } = renderWithThemeAndHookFormContext({
       component: <UserData />,
+      useFormOptions: { defaultValues: { image: image.id, region: region.id } },
     });
 
-    const input = getByLabelText('User Data');
+    const userDataHeading = await findByText('Add User Data');
+
+    expect(userDataHeading).toBeVisible();
+    expect(userDataHeading.tagName).toBe('H2');
+  });
+
+  it('should display a warning message if the user data is not in an accepted format', async () => {
+    const image = imageFactory.build({ capabilities: ['cloud-init'] });
+    const region = regionFactory.build({ capabilities: ['Metadata'] });
+
+    server.use(
+      http.get('*/v4/images/*', () => {
+        return HttpResponse.json(image);
+      }),
+      http.get('*/v4/regions', () => {
+        return HttpResponse.json(makeResourcePage([region]));
+      })
+    );
+
+    const inputValue = '#test-string';
+    const { findByLabelText, getByText } = renderWithThemeAndHookFormContext({
+      component: <UserData />,
+      useFormOptions: { defaultValues: { image: image.id, region: region.id } },
+    });
+
+    const input = await findByLabelText('User Data');
     fireEvent.change(input, { target: { value: inputValue } });
     fireEvent.blur(input); // triggers format check
 
     expect(
       getByText('The user data may be formatted incorrectly.')
     ).toBeInTheDocument();
-  });
-
-  it('should display a warning in the header for cloning', () => {
-    const { getByText } = renderWithThemeAndHookFormContext({
-      component: <UserData />,
-      options: {
-        MemoryRouter: {
-          initialEntries: [{ pathname: '/linodes/create?type=Clone+Linode' }],
-        },
-      },
-    });
-
-    expect(
-      getByText(
-        'Existing user data is not cloned. You may add new user data now.'
-      )
-    ).toBeVisible();
-  });
-
-  it('should display a warning in the header for creating from a Linode backup', () => {
-    const { getByText } = renderWithThemeAndHookFormContext({
-      component: <UserData />,
-      options: {
-        MemoryRouter: {
-          initialEntries: [{ pathname: '/linodes/create?type=Backups' }],
-        },
-      },
-    });
-
-    expect(
-      getByText(
-        'Existing user data is not accessible when creating a Linode from a backup. You may add new user data now.'
-      )
-    ).toBeVisible();
   });
 });
