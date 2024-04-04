36473ebfd42f05848ae66bb74658c629564a1ac0
Joe D'Amore <jdamore@linode.com>
2024-04-04 17:49:55 -0400

    Whoops, skipped the wrong test

--- packages/manager/cypress/e2e/core/parentChild/account-switching.spec.ts
+++ packages/manager/cypress/e2e/core/parentChild/account-switching.spec.ts
@@ -334,7 +334,7 @@ describe('Parent/Child account switching', () => {
      * - Confirms that Parent account information is displayed in user menu button after switch.
      * - Confirms that Cloud updates local storage auth values upon account switch.
      */
-    it('can switch from Proxy user back to Parent account user from Billing page', () => {
+    it.skip('can switch from Proxy user back to Parent account user from Billing page', () => {
       const mockParentToken = randomString(32);
       const mockParentExpiration = DateTime.now().plus({ minutes: 15 }).toISO();
 
@@ -419,7 +419,7 @@ describe('Parent/Child account switching', () => {
      * - Confirms that alternate Child account information is displayed in user menu button after switch.
      * - Confirms that Cloud updates local storage auth values upon account switch.
      */
-    it.skip('can switch to another Child account as a Proxy user', () => {
+    it('can switch to another Child account as a Proxy user', () => {
       const mockParentToken = randomString(32);
       const mockParentExpiration = DateTime.now().plus({ minutes: 15 }).toISO();
 
