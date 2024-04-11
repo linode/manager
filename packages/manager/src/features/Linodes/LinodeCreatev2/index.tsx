de158af0f8cb764747628332dd4d22b5f22f12f3
Banks Nussman <banks@nussman.us>
2024-04-11 00:37:43 -0400

    Revert "hook up validation packages for realtime validation"
    This reverts commit f56e4ad16d281c5b8ee1a20c9cd1f1ec2992f18b.

--- packages/manager/src/features/Linodes/LinodeCreatev2/index.tsx
+++ packages/manager/src/features/Linodes/LinodeCreatev2/index.tsx
@@ -1,5 +1,3 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import { CreateLinodeSchema } from '@linode/validation';
 import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
@@ -41,11 +39,7 @@ import type { CreateLinodeRequest } from '@linode/api-v4';
 import type { SubmitHandler } from 'react-hook-form';
 
 export const LinodeCreatev2 = () => {
-  const methods = useForm<CreateLinodeRequest>({
-    defaultValues,
-    resolver: yupResolver(CreateLinodeSchema),
-  });
-
+  const methods = useForm<CreateLinodeRequest>({ defaultValues });
   const history = useHistory();
 
   const { mutateAsync: createLinode } = useCreateLinodeMutation();
