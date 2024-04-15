25000789f537c35c9548c5b58b8f254c325e23d3
Hana Xu <hxu@akamai.com>
2024-04-15 12:47:11 -0400

    move to v2 function

--- packages/manager/src/utilities/accountCapabilities.ts
+++ packages/manager/src/utilities/accountCapabilities.ts
@@ -10,10 +10,33 @@ import type { AccountCapability } from '@linode/api-v4';
  * you would turn the flag *off* for that environment, but have the API return
  * the account capability.
  */
-export const isFeatureEnabled = (
+export const isFeatureEnabledV2 = (
   featureName: AccountCapability,
   isFeatureFlagEnabled: boolean,
   capabilities: AccountCapability[]
 ) => {
   return isFeatureFlagEnabled && capabilities.includes(featureName);
+};
+
+/**
+ * DEPRECATED USE isFeatureEnabledV2
+ *
+ * Determines if a feature should be enabled.
+ *
+ * @returns true if the feature is returned from account.capabilities **or** if it is explicitly enabled
+ * by a feature flag
+ *
+ * We use "or" instead of "and" here to allow us to enable features in "lower" environments
+ * without needing the customer capability.
+ *
+ * If you need to launch a production feature, but have it be gated,
+ * you would turn the flag *off* for that environment, but have the API return
+ * the account capability.
+ */
+export const isFeatureEnabled = (
+  featureName: AccountCapability,
+  isFeatureFlagEnabled: boolean,
+  capabilities: AccountCapability[]
+) => {
+  return isFeatureFlagEnabled || capabilities.includes(featureName);
 };
