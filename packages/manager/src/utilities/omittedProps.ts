c1e806b6a94c1981871117448d8abd0a972d9f9f
Alban Bailly <abailly@akamai.com>
2024-04-17 12:52:36 -0400

    Initial commit: omit & isNumber

--- packages/manager/src/utilities/omittedProps.ts
+++ packages/manager/src/utilities/omittedProps.ts
@@ -13,3 +13,25 @@
 export const omittedProps = <Props>(props: Array<keyof Props>) => (
   prop: keyof Props
 ): boolean => !props.includes(prop);
+
+/**
+ * Helper to filter out props we spread into a component.
+ * This helpers differs from `omittedProps` in that it omits the props
+ * from the object instead of returning a boolean.
+ * This util is a direct replacement for `omit` from lodash.
+ 
+ * @param props Array of props to filter out
+ * @param toRemove Array of props to remove
+ * @returns Object with props removed
+ */
+export function omitProps<Props extends {}, Keys extends keyof Props>(
+  props: Props,
+  toRemove: Keys[]
+): Omit<Props, Keys> {
+  return Object.keys(props).reduce((obj, key) => {
+    if (!toRemove.includes(key as any)) {
+      (obj as any)[key] = props[key as keyof Props];
+    }
+    return obj;
+  }, {} as Omit<Props, Keys>);
+}
