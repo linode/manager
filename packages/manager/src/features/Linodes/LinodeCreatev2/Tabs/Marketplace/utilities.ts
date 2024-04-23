c2ae14ba4f42d9c6728efa7a52ffdc12f9a234d7
Banks Nussman <banks@nussman.us>
2024-04-23 13:22:34 -0400

    initial marketplace UI

---
+++ packages/manager/src/features/Linodes/LinodeCreatev2/Tabs/Marketplace/utilities.ts
@@ -0,0 +1,16 @@
+const categories = [
+  'Control Panels',
+  'Databases',
+  'Development',
+  'Games',
+  'Media and Entertainment',
+  'Monitoring',
+  'Productivity',
+  'Security',
+  'Stacks',
+  'Website',
+];
+
+export const categoryOptions = categories.map((category) => ({
+  label: category,
+}));
