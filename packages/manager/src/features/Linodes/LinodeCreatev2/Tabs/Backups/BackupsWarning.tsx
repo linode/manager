7bab06778d8104fea790f3a05eef63e6b622a3a5
Banks Nussman <banks@nussman.us>
2024-04-26 10:38:51 -0400

    use an MUI `<List />`

--- packages/manager/src/features/Linodes/LinodeCreatev2/Tabs/Backups/BackupsWarning.tsx
+++ packages/manager/src/features/Linodes/LinodeCreatev2/Tabs/Backups/BackupsWarning.tsx
@@ -1,27 +1,22 @@
+import { ListItem } from '@mui/material';
 import React from 'react';
 
+import { List } from 'src/components/List';
 import { Notice } from 'src/components/Notice/Notice';
 
 export const BackupsWarning = () => {
   return (
     <Notice variant="warning">
-      <ul
-        style={{
-          lineHeight: 1.5,
-          marginBottom: 4,
-          marginLeft: -20,
-          marginTop: 4,
-        }}
-      >
-        <li>
+      <List sx={{ listStyleType: 'disc', pl: 2.5 }}>
+        <ListItem sx={{ display: 'list-item', pl: 1, py: '4px' }}>
           This newly created Linode will be created with the same password and
           SSH Keys (if any) as the original Linode.
-        </li>
-        <li>
+        </ListItem>
+        <ListItem sx={{ display: 'list-item', pl: 1, py: '4px' }}>
           This Linode will need to be manually booted after it finishes
           provisioning.
-        </li>
-      </ul>
+        </ListItem>
+      </List>
     </Notice>
   );
 };
