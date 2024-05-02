53c9753e4de958e51d345626c43d25e4a74e0c5d
jdamore-linode <97627410+jdamore-linode@users.noreply.github.com>
2024-05-02 09:03:14 -0400

    test: [M3-7781 & M3-7782] - Add Cypress integration tests for Placement Group deletion flows (#10425)
    * Add Placement Group deletion UI tests
    
    * Added changeset: Add Cypress tests for Placement Group deletion flows

--- packages/manager/cypress/e2e/core/placementGroups/placement-groups-landing-page.spec.ts
+++ packages/manager/cypress/e2e/core/placementGroups/placement-groups-landing-page.spec.ts
@@ -3,7 +3,7 @@ import {
   mockGetFeatureFlagClientstream,
 } from 'support/intercepts/feature-flags';
 import { makeFeatureFlagData } from 'support/util/feature-flags';
-import { mockGetPlacementGroups } from 'support/intercepts/vm-placement';
+import { mockGetPlacementGroups } from 'support/intercepts/placement-groups';
 import { ui } from 'support/ui';
 import { accountFactory } from 'src/factories';
 
