c2dd6eed09a6ae9123f67253299e153107ccb02f
Alban Bailly <130582365+abailly-akamai@users.noreply.github.com>
2024-05-03 19:02:09 -0400

    upcoming: [M3 -8049] - Last round of placement group copy updates (#10434)
    * Affinity type enforcement
    
    * other updates
    
    * Added changeset: Placement Groups final copy updates

--- packages/manager/src/features/PlacementGroups/PlacementGroupsCreateDrawer.tsx
+++ packages/manager/src/features/PlacementGroups/PlacementGroupsCreateDrawer.tsx
@@ -24,7 +24,7 @@ import { getFormikErrorsFromAPIErrors } from 'src/utilities/formikErrorUtils';
 import { scrollErrorIntoView } from 'src/utilities/scrollErrorIntoView';
 
 import { MAXIMUM_NUMBER_OF_PLACEMENT_GROUPS_IN_REGION } from './constants';
-import { PlacementGroupsAffinityEnforcementRadioGroup } from './PlacementGroupsAffinityEnforcementRadioGroup';
+import { PlacementGroupsAffinityTypeEnforcementRadioGroup } from './PlacementGroupsAffinityEnforcementRadioGroup';
 import { PlacementGroupsAffinityTypeSelect } from './PlacementGroupsAffinityTypeSelect';
 import {
   getMaxPGsPerCustomer,
@@ -212,7 +212,7 @@ export const PlacementGroupsCreateDrawer = (
               helperText={values.region && pgRegionLimitHelperText}
               regions={regions ?? []}
               selectedId={selectedRegionId ?? values.region}
-              tooltipText="Only regions supporting Placement Groups are listed."
+              tooltipText="Only Linode data center regions that support placement groups are listed."
             />
           )}
           <PlacementGroupsAffinityTypeSelect
@@ -222,7 +222,7 @@ export const PlacementGroupsCreateDrawer = (
             error={errors.affinity_type}
             setFieldValue={setFieldValue}
           />
-          <PlacementGroupsAffinityEnforcementRadioGroup
+          <PlacementGroupsAffinityTypeEnforcementRadioGroup
             disabledPlacementGroupCreateButton={
               disabledPlacementGroupCreateButton
             }
