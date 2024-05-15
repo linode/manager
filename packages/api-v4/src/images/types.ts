a60cf2ffbb9b76cef170a2702ea490dd69928209
Banks Nussman <banks@nussman.us>
2024-05-15 09:49:39 -0400

    improve api-v4 docs

--- packages/api-v4/src/images/types.ts
+++ packages/api-v4/src/images/types.ts
@@ -30,13 +30,31 @@ export interface UploadImageResponse {
 }
 
 export interface BaseImagePayload {
+  /**
+   * A short title of this Image.
+   *
+   * Defaults to the label of the Disk it is being created from if not provided.
+   */
   label?: string;
+  /**
+   * A detailed description of this Image.
+   */
   description?: string;
+  /**
+   * Whether this Image supports cloud-init.
+   * @default false
+   */
   cloud_init?: boolean;
+  /**
+   * An array of Tags applied to this Image. Tags are for organizational purposes only.
+   */
   tags?: string[];
 }
 
 export interface CreateImagePayload extends BaseImagePayload {
+  /**
+   * The ID of the Linode Disk that this Image will be created from.
+   */
   disk_id: number;
 }
 
