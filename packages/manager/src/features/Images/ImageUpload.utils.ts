f8d9f3a163dc8afdfccf55331ca986c7473f5509
Banks Nussman <banks@nussman.us>
2024-05-17 14:20:16 -0400

    add comments

--- packages/manager/src/features/Images/ImageUpload.utils.ts
+++ packages/manager/src/features/Images/ImageUpload.utils.ts
@@ -14,14 +14,27 @@ export const recordImageAnalytics = (
   sendImageUploadEvent(action, readableFileSize);
 };
 
+/**
+ * We extend the image upload payload to conatin the file
+ * so we can use react-hook-form to manage all of the form state.
+ */
 export interface ImageUploadFormData extends ImageUploadPayload {
   file: File;
 }
 
+/**
+ * We extend the image upload schema to conatin the file
+ * so we can use react-hook-form to validate all of the
+ * form state at once.
+ */
 export const ImageUploadSchema = uploadImageSchema.shape({
   file: mixed().required('Image is required.'),
 });
 
+/**
+ * We use navigation state to pre-fill the upload form
+ * when the user "retries" an upload.
+ */
 export interface ImageUploadNavigationState {
   imageDescription?: string;
   imageLabel?: string;
