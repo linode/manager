4ba42917b26743712c98211c349b21de7b3efc34
mjac0bs <mjacobs@akamai.com>
2024-05-16 23:55:23 -0700

    Update OveragePricing test coverage

--- packages/manager/src/features/ObjectStorage/BucketLanding/OveragePricing.test.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/OveragePricing.test.tsx
@@ -1,7 +1,10 @@
 import { fireEvent } from '@testing-library/react';
 import React from 'react';
 
-import { objectStorageTypeFactory } from 'src/factories';
+import {
+  objectStorageTypeFactory,
+  objectStorageTypeOverageFactory,
+} from 'src/factories';
 import { OBJ_STORAGE_PRICE } from 'src/utilities/pricing/constants';
 import { objectStoragePriceIncreaseMap } from 'src/utilities/pricing/dynamicPricing';
 import { renderWithTheme } from 'src/utilities/testHelpers';
@@ -12,15 +15,35 @@ import {
   OveragePricing,
 } from './OveragePricing';
 
-const mockObjectStorageTypes = objectStorageTypeFactory.build();
-const storageOveragePriceType = mockObjectStorageTypes[1];
+const mockObjectStorageTypes = [
+  objectStorageTypeFactory.build(),
+  objectStorageTypeOverageFactory.build(),
+];
+
+const queryMocks = vi.hoisted(() => ({
+  useObjectStorageTypesQuery: vi.fn().mockReturnValue({}),
+}));
+
+vi.mock('src/queries/objectStorage', async () => {
+  const actual = await vi.importActual('src/queries/objectStorage');
+  return {
+    ...actual,
+    useObjectStorageTypesQuery: queryMocks.useObjectStorageTypesQuery,
+  };
+});
 
 describe('OveragePricing', async () => {
-  it.skip('Renders base overage pricing for a region without price increases', () => {
+  beforeAll(() => {
+    queryMocks.useObjectStorageTypesQuery.mockReturnValue({
+      data: mockObjectStorageTypes,
+    });
+  });
+
+  it('Renders base overage pricing for a region without price increases', () => {
     const { getByText } = renderWithTheme(
       <OveragePricing regionId="us-east" />
     );
-    getByText(`$${storageOveragePriceType.price.hourly} per GB`, {
+    getByText(`$${mockObjectStorageTypes[1].price.hourly?.toFixed(3)} per GB`, {
       exact: false,
     });
     getByText(`$${OBJ_STORAGE_PRICE.transfer_overage} per GB`, {
@@ -28,14 +51,11 @@ describe('OveragePricing', async () => {
     });
   });
 
-  it.skip('Renders DC-specific overage pricing for a region with price increases', () => {
+  it('Renders DC-specific overage pricing for a region with price increases', () => {
     const { getByText } = renderWithTheme(<OveragePricing regionId="br-gru" />);
-    getByText(
-      `$${storageOveragePriceType?.region_prices?.['br-gru']?.hourly} per GB`,
-      {
-        exact: false,
-      }
-    );
+    getByText(`$${mockObjectStorageTypes[1].region_prices[1].hourly} per GB`, {
+      exact: false,
+    });
     getByText(
       `$${objectStoragePriceIncreaseMap['br-gru'].transfer_overage} per GB`,
       { exact: false }
@@ -66,5 +86,41 @@ describe('OveragePricing', async () => {
 
     expect(tooltip).toBeInTheDocument();
     expect(getByText(GLOBAL_TRANSFER_POOL_TOOLTIP_TEXT)).toBeVisible();
+  });
+
+  it('Renders a loading state while prices are loading', () => {
+    queryMocks.useObjectStorageTypesQuery.mockReturnValue({
+      isLoading: true,
+    });
+
+    const { getByRole } = renderWithTheme(
+      <OveragePricing regionId="us-east" />
+    );
+
+    expect(getByRole('progressbar')).toBeVisible();
+  });
+
+  it('Renders placeholder unknown pricing when there is an error', async () => {
+    queryMocks.useObjectStorageTypesQuery.mockReturnValue({
+      isError: true,
+    });
+
+    const { getAllByText } = renderWithTheme(
+      <OveragePricing regionId="us-east" />
+    );
+
+    expect(getAllByText('$--.-- per GB')).toHaveLength(1);
+  });
+
+  it('Renders placeholder unknown pricing when prices are undefined', async () => {
+    queryMocks.useObjectStorageTypesQuery.mockReturnValue({
+      data: undefined,
+    });
+
+    const { getAllByText } = renderWithTheme(
+      <OveragePricing regionId="us-east" />
+    );
+
+    expect(getAllByText('$--.-- per GB')).toHaveLength(1);
   });
 });
