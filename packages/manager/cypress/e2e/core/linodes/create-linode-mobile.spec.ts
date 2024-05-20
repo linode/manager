5d66f93dbb82b5af3da4d229fc0227517d0a1a23
Joe D'Amore <jdamore@linode.com>
2024-05-20 14:45:29 -0400

    Only test iPhone portrait viewport for mobile tests as experiment

--- packages/manager/cypress/e2e/core/linodes/create-linode-mobile.spec.ts
+++ packages/manager/cypress/e2e/core/linodes/create-linode-mobile.spec.ts
@@ -25,58 +25,53 @@ describe('Linode create mobile smoke', () => {
   });
 
   MOBILE_VIEWPORTS.forEach((viewport) => {
-    ['portrait', 'landscape'].forEach((orientation) => {
-      /*
-       * - Confirms Linode create flow can be completed on common mobile screen sizes
-       * - Creates a basic Nanode and confirms interactions succeed and outgoing request contains expected data.
-       */
-      it(`can create Linode (${viewport.label}, ${orientation})`, () => {
-        const mockLinodeRegion = chooseRegion();
-        const mockLinode = linodeFactory.build({
-          id: randomNumber(),
-          label: randomLabel(),
-          region: mockLinodeRegion.id,
-        });
-
-        mockCreateLinode(mockLinode).as('createLinode');
-
-        cy.viewport(
-          orientation === 'portrait' ? viewport.width : viewport.height,
-          orientation === 'portrait' ? viewport.height : viewport.width
-        );
-        cy.visitWithLogin('/linodes/create');
-
-        linodeCreatePage.selectImage('Debian 11');
-        linodeCreatePage.selectRegionById(mockLinodeRegion.id);
-        linodeCreatePage.selectPlanCard('Shared CPU', 'Nanode 1 GB');
-        linodeCreatePage.setLabel(mockLinode.label);
-        linodeCreatePage.setRootPassword(randomString(32));
-
-        cy.get('[data-qa-linode-create-summary]')
-          .scrollIntoView()
-          .within(() => {
-            cy.findByText('Nanode 1 GB').should('be.visible');
-            cy.findByText('Debian 11').should('be.visible');
-            cy.findByText(mockLinodeRegion.label).should('be.visible');
-          });
-
-        ui.button
-          .findByTitle('Create Linode')
-          .should('be.visible')
-          .should('be.enabled')
-          .click();
-
-        cy.wait('@createLinode').then((xhr) => {
-          const requestBody = xhr.request.body;
-
-          expect(requestBody['image']).to.equal('linode/debian11');
-          expect(requestBody['label']).to.equal(mockLinode.label);
-          expect(requestBody['region']).to.equal(mockLinodeRegion.id);
-          expect(requestBody['type']).to.equal('g6-nanode-1');
-        });
-
-        cy.url().should('endWith', `/linodes/${mockLinode.id}`);
+    /*
+     * - Confirms Linode create flow can be completed on common mobile screen sizes
+     * - Creates a basic Nanode and confirms interactions succeed and outgoing request contains expected data.
+     */
+    it(`can create Linode (${viewport.label})`, () => {
+      const mockLinodeRegion = chooseRegion();
+      const mockLinode = linodeFactory.build({
+        id: randomNumber(),
+        label: randomLabel(),
+        region: mockLinodeRegion.id,
       });
+
+      mockCreateLinode(mockLinode).as('createLinode');
+
+      cy.viewport(viewport.width, viewport.height);
+      cy.visitWithLogin('/linodes/create');
+
+      linodeCreatePage.selectImage('Debian 11');
+      linodeCreatePage.selectRegionById(mockLinodeRegion.id);
+      linodeCreatePage.selectPlanCard('Shared CPU', 'Nanode 1 GB');
+      linodeCreatePage.setLabel(mockLinode.label);
+      linodeCreatePage.setRootPassword(randomString(32));
+
+      cy.get('[data-qa-linode-create-summary]')
+        .scrollIntoView()
+        .within(() => {
+          cy.findByText('Nanode 1 GB').should('be.visible');
+          cy.findByText('Debian 11').should('be.visible');
+          cy.findByText(mockLinodeRegion.label).should('be.visible');
+        });
+
+      ui.button
+        .findByTitle('Create Linode')
+        .should('be.visible')
+        .should('be.enabled')
+        .click();
+
+      cy.wait('@createLinode').then((xhr) => {
+        const requestBody = xhr.request.body;
+
+        expect(requestBody['image']).to.equal('linode/debian11');
+        expect(requestBody['label']).to.equal(mockLinode.label);
+        expect(requestBody['region']).to.equal(mockLinodeRegion.id);
+        expect(requestBody['type']).to.equal('g6-nanode-1');
+      });
+
+      cy.url().should('endWith', `/linodes/${mockLinode.id}`);
     });
   });
 });
