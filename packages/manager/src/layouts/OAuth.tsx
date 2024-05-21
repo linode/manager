ee47eb2d4617b823c132926a1c374ccd9135275f
mkaminsk <mkaminsk@akamai.com>
2024-05-21 15:51:58 +0200

    feat: [LILO-418] - Modify Cloud Manager to use PKCE instead of implicit flow

--- packages/manager/src/layouts/OAuth.tsx
+++ packages/manager/src/layouts/OAuth.tsx
@@ -1,31 +1,47 @@
+import * as React from 'react';
 import { Component } from 'react';
-import { MapDispatchToProps, connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
+import { CircleProgress } from 'src/components/CircleProgress';
+import { CLIENT_ID, LOGIN_ROOT } from 'src/constants';
 import { handleStartSession } from 'src/store/authentication/authentication.actions';
+import { clearNonceAndCodeVerifierFromLocalStorage, clearTokenDataFromLocalStorage } from 'src/store/authentication/authentication.helpers';
 import { getQueryParamsFromQueryString } from 'src/utilities/queryParams';
-import { authentication } from 'src/utilities/storage';
+import {
+  authentication,
+  getEnvLocalStorageOverrides,
+} from 'src/utilities/storage';
 
-interface OAuthCallbackPageProps extends DispatchProps, RouteComponentProps {}
+export type CombinedProps = DispatchProps & RouteComponentProps;
 
-interface OAuthQueryParams {
-  access_token: string; // token for auth
-  expires_in: string; // amount of time (in seconds) the token has before expiry
-  return: string;
-  scope: string;
+type OAuthQueryParams = {
+  code: string;
+  returnTo: string;
   state: string; // nonce
-  token_type: string; // token prefix AKA "Bearer"
-}
+};
 
-export class OAuthCallbackPage extends Component<OAuthCallbackPageProps> {
+type DispatchProps = {
+  dispatchStartSession: (
+    token: string,
+    tokenType: string,
+    scopes: string,
+    expiry: string
+  ) => void;
+};
+
+type State = {
+  isLoading: boolean;
+};
+
+export class OAuthCallbackPage extends Component<CombinedProps, State> {
   checkNonce(nonce: string) {
-    const { history } = this.props;
     // nonce should be set and equal to ours otherwise retry auth
     const storedNonce = authentication.nonce.get();
+    authentication.nonce.set('');
     if (!(nonce && storedNonce === nonce)) {
-      authentication.nonce.set('');
-      history.push('/');
+      clearStorageAndRedirectToLogout();
     }
   }
 
@@ -39,111 +55,158 @@ export class OAuthCallbackPage extends Component<OAuthCallbackPageProps> {
      *
      */
 
-    const { history, location } = this.props;
+    const { location } = this.props;
 
     /**
      * If the hash doesn't contain a string after the #, there's no point continuing as we dont have
      * the query params we need.
      */
 
-    if (!location.hash || location.hash.length < 2) {
-      return history.push('/');
+    if (!location.search || location.search.length < 2) {
+      clearStorageAndRedirectToLogout();
     }
 
-    const hashParams = (getQueryParamsFromQueryString(
-      location.hash.substr(1)
-    ) as unknown) as OAuthQueryParams;
+    const { code, returnTo, state: nonce } = getQueryParamsFromQueryString(
+      location.search
+    ) as OAuthQueryParams;
 
-    const {
-      access_token: accessToken,
-      expires_in: expiresIn,
-      scope: scopes,
-      state: nonce,
-      token_type: tokenType,
-    } = hashParams;
-
-    /** If the access token wasn't returned, something is wrong and we should bail. */
-    if (!accessToken) {
-      return history.push('/');
+    if (!code || !returnTo || !nonce) {
+      clearStorageAndRedirectToLogout();
     }
 
-    /**
-     * Build the path we're going to redirect to after we're done (back to where the user was when they started authentication).
-     * This has to be handled specially; the hashParams object above already has a "return" property, but query parsers
-     * don't handle URLs as query params very well. Any query params in the returnTo URL will be parsed as if they were separate params.
-     */
+    this.exchangeAuthorizationCodeForToken(code, returnTo, nonce);
+  }
 
-    // Find the returnTo= param directly
-    const returnIdx = location.hash.indexOf('returnTo');
-    // If it exists, take everything after its index (plus 9 to remove the returnTo=)
-    const returnPath = returnIdx ? location.hash.substr(returnIdx + 9) : null;
-    // If this worked, we have a return URL. If not, default to the root path.
-    const returnTo = returnPath ?? '/';
+  createFormData(
+    clientID: string,
+    code: string,
+    nonce: string,
+    codeVerifier: string
+  ): FormData {
+    const formData = new FormData();
+    formData.append('grant_type', 'authorization_code');
+    formData.append('client_id', clientID);
+    formData.append('code', code);
+    formData.append('state', nonce);
+    formData.append('code_verifier', codeVerifier);
+    return formData;
+  }
 
-    /**
-     * We need to validate that the nonce returned (comes from the location.hash as the state param)
-     * matches the one we stored when authentication was started. This confirms the initiator
-     * and receiver are the same.
-     */
-    this.checkNonce(nonce);
+  async exchangeAuthorizationCodeForToken(
+    code: string,
+    returnTo: string,
+    nonce: string
+  ) {
+    try {
+      const expireDate = new Date();
+      const codeVerifier = authentication.codeVerifier.get();
 
-    /**
-     * We multiply the expiration time by 1000 ms because JavaSript returns time in ms, while
-     * the API returns the expiry time in seconds
-     */
-    const expireDate = new Date();
-    expireDate.setTime(expireDate.getTime() + +expiresIn * 1000);
+      if (codeVerifier) {
+        authentication.codeVerifier.set('');
 
-    /**
-     * We have all the information we need and can persist it to localStorage and Redux.
-     */
-    this.props.dispatchStartSession(
-      accessToken,
-      tokenType,
-      scopes,
-      expireDate.toString()
-    );
+        const localStorageOverrides = getEnvLocalStorageOverrides();
+        const loginURL = localStorageOverrides?.loginRoot ?? LOGIN_ROOT;
+        const clientID = localStorageOverrides?.clientID ?? CLIENT_ID;
 
-    /**
-     * All done, redirect this bad-boy to the returnTo URL we generated earlier.
-     */
-    history.push(returnTo);
+        /**
+         * We need to validate that the nonce returned (comes from the location.hash as the state param)
+         * matches the one we stored when authentication was started. This confirms the initiator
+         * and receiver are the same.
+         */
+
+        this.checkNonce(nonce);
+
+        const formData = this.createFormData(
+          `${clientID}`,
+          code,
+          nonce,
+          codeVerifier
+        );
+
+        this.setState({ isLoading: true });
+
+        const response = await fetch(`${loginURL}/oauth/token`, {
+          body: formData,
+          method: 'POST',
+        });
+
+        this.setState({ isLoading: false });
+
+        if (response.ok) {
+          const tokenParams = await response.json();
+
+          /**
+           * We multiply the expiration time by 1000 ms because JavaSript returns time in ms, while
+           * the API returns the expiry time in seconds
+           */
+
+          expireDate.setTime(
+            expireDate.getTime() + +tokenParams.expires_in * 1000
+          );
+
+          this.props.dispatchStartSession(
+            tokenParams.access_token,
+            tokenParams.token_type,
+            tokenParams.scopes,
+            expireDate.toString()
+          );
+
+          /**
+           * All done, redirect this bad-boy to the returnTo URL we generated earlier.
+           */
+          this.props.history.push(returnTo);
+        } else {
+          clearStorageAndRedirectToLogout();
+        }
+      } else {
+        clearStorageAndRedirectToLogout();
+      }
+    } catch (error) {
+      clearStorageAndRedirectToLogout();
+    }
   }
 
   render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return <CircleProgress />;
+    }
+
     return null;
   }
-}
 
-interface DispatchProps {
-  dispatchStartSession: (
-    token: string,
-    tokenType: string,
-    scopes: string,
-    expiry: string
-  ) => void;
-}
-
-const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (
-  dispatch
-) => {
-  return {
-    dispatchStartSession: (token, tokenType, scopes, expiry) =>
-      dispatch(
-        handleStartSession({
-          expires: expiry,
-          scopes,
-          token: `${tokenType.charAt(0).toUpperCase()}${tokenType.substr(
-            1
-          )} ${token}`,
-        })
-      ),
+  state: State = {
+    isLoading: false,
   };
+}
+
+const clearStorageAndRedirectToLogout = () => {
+  clearLocalStorage();
+  window.location.assign(`${LOGIN_ROOT}` + '/logout');
 };
+
+const clearLocalStorage = () => {
+  clearNonceAndCodeVerifierFromLocalStorage();
+  clearTokenDataFromLocalStorage();
+}
+
+const mapDispatchToProps = (dispatch: any): DispatchProps => ({
+  dispatchStartSession: (token, tokenType, scopes, expiry) =>
+    dispatch(
+      handleStartSession({
+        expires: expiry,
+        scopes,
+        token: `${tokenType.charAt(0).toUpperCase()}${tokenType.substr(
+          1
+        )} ${token}`,
+      })
+    ),
+});
 
 const connected = connect(undefined, mapDispatchToProps);
 
-export default compose<OAuthCallbackPageProps, {}>(
+export default compose<CombinedProps, {}>(
   connected,
   withRouter
 )(OAuthCallbackPage);
