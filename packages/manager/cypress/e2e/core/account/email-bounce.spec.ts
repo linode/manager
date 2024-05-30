d995b6554550a11810c583caf7cce9433f044142
Azure Shi <azsh@akamai.com>
2024-05-30 17:36:04 -0400

    Update tests

--- packages/manager/cypress/e2e/core/account/email-bounce.spec.ts
+++ packages/manager/cypress/e2e/core/account/email-bounce.spec.ts
@@ -10,7 +10,7 @@ import { ui } from 'support/ui';
 import { mockUpdateProfile } from 'support/intercepts/profile';
 import { randomString } from 'support/util/random';
 import { accountFactory } from 'src/factories/account';
-import { mockGetAccount } from 'support/intercepts/account';
+import { mockGetAccount, mockUpdateAccount } from 'support/intercepts/account';
 
 const notifications_billing_email_bounce: Notification[] = [
   notificationFactory.build({
@@ -132,19 +132,22 @@ describe('Email bounce banners', () => {
    */
   it('Billing email bounce is visible and can be confirmed by users', () => {
     const accountData = accountFactory.build();
-    const billingemail = accountData.email;
+    // mock the user's account data and confirm that it is displayed correctly upon page load
+    mockUpdateAccount(accountData).as('updateAccount');
+    // get the user's account data for Cloud to inject the email address into the notification
+    mockGetAccount(accountData).as('getAccount');
 
+    const billingemail = accountData.email;
     const BillingEmailBounceBanner = `An email to your account’s email address couldn’t be delivered. Is ${billingemail} the correct address?`;
 
     mockGetNotifications(notifications_billing_email_bounce).as(
       'mockNotifications'
     );
-    // mock the user's account data and confirm that it is displayed correctly upon page load
-    mockGetAccount(accountData).as('getAccount');
 
     cy.visitWithLogin('/account/billing');
-    cy.wait('@mockNotifications');
+    cy.wait(['@mockNotifications', '@getAccount']);
 
+    // check the billing email bounce banner and click the confirm button
     cy.contains(BillingEmailBounceBanner)
       .should('be.visible')
       .parent()
@@ -157,10 +160,14 @@ describe('Email bounce banners', () => {
           .click();
       });
 
-    cy.wait('@getAccount');
+    // check the toast notification
+    cy.wait('@updateAccount');
     ui.toast.assertMessage('Email confirmed');
 
+    // confirm the billing email bounce banner not exist after clicking the confirm button
     cy.contains(BillingEmailBounceBanner).should('not.exist');
+
+    // confirm the email address is visible in Billing Contact
     cy.findByText('Billing Contact')
       .should('be.visible')
       .parent()
@@ -177,16 +184,17 @@ describe('Email bounce banners', () => {
   // TODO unskip the test once https://jira.linode.com/browse/M3-8181 is fixed
   it.skip('Billing email bounce is visible and can be updated by users', () => {
     const accountData = accountFactory.build();
-    const billingemail = accountData.email;
+    // mock the user's account data and confirm that it is displayed correctly upon page load
+    mockUpdateAccount(accountData).as('updateAccount');
+    // get the user's account data for Cloud to inject the email address into the notification
+    mockGetAccount(accountData).as('getAccount');
 
+    const billingemail = accountData.email;
     const BillingEmailBounceBanner = `An email to your account’s email address couldn’t be delivered. Is ${billingemail} the correct address?`;
 
     mockGetNotifications(notifications_billing_email_bounce).as(
       'mockNotifications'
     );
-
-    // mock the user's account data and confirm that it is displayed correctly upon page load
-    mockGetAccount(accountData).as('getAccount');
 
     cy.visitWithLogin('/account/billing');
     cy.wait(['@mockNotifications', '@getAccount']);
