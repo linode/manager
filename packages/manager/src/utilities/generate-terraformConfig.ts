e79000cb119e5b7a3b9c51f21f9063338d784935
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-06-06 07:37:26 -0500

    Generate Terraform Config

---
+++ packages/manager/src/utilities/generate-terraformConfig.ts
@@ -0,0 +1,53 @@
+import { CreateLinodeRequest } from '@linode/api-v4/lib/linodes';
+
+import { escapeStringForCLI } from './escapeStringForCLI';
+
+/**
+ * Generates a Terraform config to setup a Linode instance.
+ * @param {Object} config - The configuration object for the Linode instance.
+ * @returns {string} - Bash commands to write a Terraform config.
+ */
+export function generateTerraformConfig(config: CreateLinodeRequest): string {
+  let terraformConfig = `resource "linode_instance" "web" {\n`;
+
+  if (config.label) {
+    terraformConfig += `  label = "${escapeStringForCLI(config.label)}"\n`;
+  }
+  if (config.image) {
+    terraformConfig += `  image = "${escapeStringForCLI(config.image)}"\n`;
+  }
+  if (config.region) {
+    terraformConfig += `  region = "${escapeStringForCLI(config.region)}"\n`;
+  }
+  if (config.type) {
+    terraformConfig += `  type = "${escapeStringForCLI(config.type)}"\n`;
+  }
+  if (config.authorized_keys && config.authorized_keys.length > 0) {
+    const authorizedKeysFormatted = config.authorized_keys
+      ?.map((key) => `"${escapeStringForCLI(key)}"`)
+      ?.join(', ');
+    terraformConfig += `  authorized_keys = [${authorizedKeysFormatted}]\n`;
+  }
+  if (config.root_pass) {
+    terraformConfig += `  root_pass = "${escapeStringForCLI(
+      config.root_pass
+    )}"\n`;
+  }
+  if (config.tags && config.tags.length > 0) {
+    const tagsFormatted = config.tags
+      ?.map((tag) => `"${escapeStringForCLI(tag)}"`)
+      ?.join(', ');
+    terraformConfig += `  tags = [${tagsFormatted}]\n`;
+  }
+  if (config.swap_size) {
+    terraformConfig += `  swap_size = ${config.swap_size}\n`;
+  }
+  if (config.hasOwnProperty('private_ip')) {
+    // Checks explicitly for property existence
+    terraformConfig += `  private_ip = ${config.private_ip}\n`;
+  }
+
+  terraformConfig += `}\n`;
+
+  return terraformConfig;
+}
