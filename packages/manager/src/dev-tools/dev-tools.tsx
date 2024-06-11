f84bb28015ae7fe80f2831c7a194a3dc0d110742
Joe D'Amore <jdamore@linode.com>
2024-06-11 11:24:51 -0400

    WIP dev tools restyle

--- packages/manager/src/dev-tools/dev-tools.tsx
+++ packages/manager/src/dev-tools/dev-tools.tsx
@@ -1,50 +1,82 @@
 import Handyman from '@mui/icons-material/Handyman';
-import Grid from '@mui/material/Unstable_Grid2';
+// import Grid from '@mui/material/Unstable_Grid2';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { ENABLE_DEV_TOOLS, isProductionBuild } from 'src/constants';
+// import { ENABLE_DEV_TOOLS, isProductionBuild } from 'src/constants';
 import { ApplicationStore } from 'src/store';
 
 import './dev-tools.css';
 import { EnvironmentToggleTool } from './EnvironmentToggleTool';
 import { FeatureFlagTool } from './FeatureFlagTool';
-import { MockDataTool } from './MockDataTool';
-import { Preferences } from './Preferences';
+// import { MockDataTool } from './MockDataTool';
+// import { Preferences } from './Preferences';
 import { isMSWEnabled } from './ServiceWorkerTool';
-import { ThemeSelector } from './ThemeSelector';
+// import { ThemeSelector } from './ThemeSelector';
 
 function install(store: ApplicationStore) {
   function DevTools() {
+    const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
     return (
-      <div className={isMSWEnabled ? 'mswEnabled' : ''} id="dev-tools">
-        <div>
-          <Handyman />
+      <div
+        className={`dev-tools ${isMSWEnabled && 'dev-tools--msw'} ${
+          isOpen && 'dev-tools--open'
+        }`}
+      >
+        <div className="dev-tools__toggle">
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <Handyman />
+          </button>
         </div>
-        <Grid className="tools" container spacing={2}>
-          <Grid sm={2} xs={4}>
-            <FeatureFlagTool />
-          </Grid>
-          {import.meta.env.DEV && (
-            <Grid md={3} sm={5} xs={4}>
+        <div className="dev-tools__body">
+          <div className="dev-tools__content">
+            <div className="dev-tools__status-bar">
               <EnvironmentToggleTool />
-            </Grid>
-          )}
-          {!isProductionBuild || ENABLE_DEV_TOOLS ? (
-            <Grid md={3} sm={5} xs={4}>
-              <MockDataTool />
-              <ThemeSelector />
-              <Preferences />
-            </Grid>
-          ) : null}
-        </Grid>
+            </div>
+            <div className="dev-tools__main">
+              <FeatureFlagTool />
+            </div>
+          </div>
+        </div>
       </div>
+      // <div className={isMSWEnabled ? 'mswEnabled' : ''} id="dev-tools">
+      //   <div>
+      //     <Handyman />
+      //   </div>
+      //   <Grid className="tools" container spacing={2}>
+      //     <Grid sm={2} xs={4}>
+      //       <FeatureFlagTool />
+      //     </Grid>
+      //     {import.meta.env.DEV && (
+      //       <Grid md={3} sm={5} xs={4}>
+      //         <EnvironmentToggleTool />
+      //       </Grid>
+      //     )}
+      //     {!isProductionBuild || ENABLE_DEV_TOOLS ? (
+      //       <Grid md={3} sm={5} xs={4}>
+      //         <MockDataTool />
+      //         <ThemeSelector />
+      //         <Preferences />
+      //       </Grid>
+      //     ) : null}
+      //   </Grid>
+      // </div>
     );
   }
 
-  const devToolsRoot = document.createElement('div');
-  document.body.appendChild(devToolsRoot);
+  const devToolsRoot = (() => {
+    const existingRoot = document.getElementById('dev-tools-root');
+    if (existingRoot) {
+      return existingRoot;
+    }
+    const newRoot = document.createElement('div');
+    newRoot.id = 'dev-tools-root';
+    document.body.appendChild(newRoot);
+    return newRoot;
+  })();
+
   const root = createRoot(devToolsRoot);
   root.render(
     <Provider store={store}>
