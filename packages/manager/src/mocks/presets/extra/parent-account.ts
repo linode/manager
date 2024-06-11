264cbc4697b49541cf1b475af2d7e7a17786e5d6
Joe D'Amore <jdamore@linode.com>
2024-06-11 11:23:13 -0400

    WIP

---
+++ packages/manager/src/mocks/presets/extra/parent-account.ts
@@ -0,0 +1,8 @@
+import { MockPreset } from 'src/mocks/mockPreset';
+
+export const parentAccountPreset: MockPreset = {
+  label: 'Parent Account',
+  id: 'parent-child-parent-account',
+  group: 'Account Management',
+  handlers: [],
+};
