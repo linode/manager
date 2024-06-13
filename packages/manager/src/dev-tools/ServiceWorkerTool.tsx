db87fc867dc8846928a269b82988f866896596f8
Joe D'Amore <jdamore@linode.com>
2024-06-13 13:01:14 -0400

    WIP Allow presets and content to be set and loaded via dev tool

--- packages/manager/src/dev-tools/ServiceWorkerTool.tsx
+++ packages/manager/src/dev-tools/ServiceWorkerTool.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { getMockPresetGroups } from 'src/mocks/mockPreset';
+import { getContextPopulatorGroups } from 'src/mocks/mockContext';
 import {
   baselineMockPresets,
   defaultBaselineMockPreset,
-  // extraMockPresets,
+  extraMockPresets,
 } from 'src/mocks/presets';
+
+import { allContextPopulators } from 'src/mocks/context/populators';
 
 const LOCAL_STORAGE_KEY = 'msw';
 const LOCAL_STORAGE_POPULATORS_KEY = 'msw-populators';
@@ -59,12 +62,18 @@ export const getMSWExtraPresets = (): string[] => {
   if (!encodedPresets) {
     return [];
   }
-  return encodedPresets.split(',');
+  const storedPresets = encodedPresets.split(',');
+
+  // Filter out any stored presets that no longer exist in the code base.
+  return storedPresets.filter((storedPreset) =>
+    extraMockPresets.find(
+      (extraMockPreset) => extraMockPreset.id === storedPreset
+    )
+  );
 };
 
 export const setMSWExtraPresets = (presets: string[]) => {
   localStorage.setItem(LOCAL_STORAGE_PRESET_EXTRAS_KEY, presets.join(','));
-  window.location.reload();
 };
 
 export const getMSWContextPopulators = (): string[] => {
@@ -72,12 +81,19 @@ export const getMSWContextPopulators = (): string[] => {
   if (!encodedPopulators) {
     return [];
   }
-  return encodedPopulators.split(',');
+  const storedPopulators = encodedPopulators.split(',');
+
+  // Filter out any stored populators that no longer exist in the code base.
+  return storedPopulators.filter((storedPopulator) =>
+    allContextPopulators.find(
+      (contextPopulator) => contextPopulator.id === storedPopulator
+    )
+  );
 };
 
 export const setMSWContextPopulators = (populators: string[]) => {
   localStorage.setItem(LOCAL_STORAGE_POPULATORS_KEY, populators.join(','));
-  window.location.reload();
+  //window.location.reload();
 };
 
 const renderBaselinePresetOptions = () =>
@@ -97,29 +113,88 @@ const renderBaselinePresetOptions = () =>
     );
   });
 
-// const renderBaselinePresetOptions = () => baselineMockPresets.map((mockPreset) => {
-//   return <option value={mockPreset.id}>{mockPreset.label}</option>;
-// });
+const renderContentPopulatorOptions = (
+  populators: string[],
+  onChange: (e: React.ChangeEvent, populatorId: string) => void
+) => {
+  return (
+    <ul>
+      {getContextPopulatorGroups(allContextPopulators).map((group) => (
+        <>
+          <li
+            key={group || 'Uncategorized'}
+            className="dev-tools__list-box__separator"
+          >
+            {group || 'Uncategorized'}
+          </li>
+          {allContextPopulators
+            .filter((contextPopulator) => contextPopulator.group === group)
+            .map((contextPopulator) => (
+              <li key={contextPopulator.id}>
+                <input
+                  type="checkbox"
+                  style={{ marginRight: 12 }}
+                  checked={populators.includes(contextPopulator.id)}
+                  onChange={(e) => onChange(e, contextPopulator.id)}
+                />
+                <span title={contextPopulator.desc || contextPopulator.label}>
+                  {contextPopulator.label}
+                </span>
+              </li>
+            ))}
+        </>
+      ))}
+    </ul>
+  );
+};
 
-// const renderExtraPresetOptions = () => {
-//   return (
-//     <div>
-//       <form>
-//         {extraMockPresets.map((extraMockPreset) => {
-//           return (
-//             <div key={extraMockPreset.id}>
-//               <input type="checkbox" />
-//               {extraMockPreset.label}
-//             </div>
-//           );
-//         })}
-//       </form>
-//     </div>
-//   );
-// };
+const renderExtraPresetOptions = (
+  handlers: string[],
+  onChange: (e: React.ChangeEvent, presetId: string) => void
+) => {
+  return (
+    <ul>
+      {getMockPresetGroups(extraMockPresets).map((group) => (
+        <>
+          <li
+            key={group || 'Uncategorized'}
+            className="dev-tools__list-box__separator"
+          >
+            {group || 'Uncategorized'}
+          </li>
+          {extraMockPresets
+            .filter((extraMockPreset) => extraMockPreset.group === group)
+            .map((extraMockPreset) => (
+              <li key={extraMockPreset.id}>
+                <input
+                  type="checkbox"
+                  style={{ marginRight: 12 }}
+                  checked={handlers.includes(extraMockPreset.id)}
+                  onChange={(e) => onChange(e, extraMockPreset.id)}
+                />
+                <span title={extraMockPreset.desc || extraMockPreset.label}>
+                  {extraMockPreset.label}
+                </span>
+              </li>
+            ))}
+        </>
+      ))}
+    </ul>
+  );
+};
 
 export const ServiceWorkerTool = () => {
+  const loadedPresets = getMSWExtraPresets();
+  const loadedPopulators = getMSWContextPopulators();
+
   const [MSWPreset, setMSWPreset] = React.useState<string | null>(null);
+  const [MSWHandlers, setMSWHandlers] = React.useState<string[]>(loadedPresets);
+  const [MSWPopulators, setMSWPopulators] = React.useState<string[]>(
+    loadedPopulators
+  );
+  const [hasUnsavedChanges, setHasUnsavedChanges] = React.useState<boolean>(
+    false
+  );
 
   React.useEffect(() => {
     if (!MSWPreset) {
@@ -130,6 +205,57 @@ export const ServiceWorkerTool = () => {
       saveMSWPreset(MSWPreset);
     }
   }, [MSWPreset]);
+
+  const handleChangePopulator = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    populatorId: string
+  ) => {
+    const willEnable = e.target.checked;
+    if (willEnable && !MSWPopulators.includes(populatorId)) {
+      setMSWPopulators([...MSWPopulators, populatorId]);
+      setHasUnsavedChanges(true);
+    } else if (!willEnable && MSWPopulators.includes(populatorId)) {
+      setMSWPopulators(
+        MSWPopulators.filter((populator) => {
+          return populator !== populatorId;
+        })
+      );
+      setHasUnsavedChanges(true);
+    }
+  };
+
+  const handleChangeHandler = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    handlerPresetId: string
+  ) => {
+    const willEnable = e.target.checked;
+    if (willEnable && !MSWHandlers.includes(handlerPresetId)) {
+      setMSWHandlers([...MSWHandlers, handlerPresetId]);
+      setHasUnsavedChanges(true);
+    } else if (!willEnable && MSWHandlers.includes(handlerPresetId)) {
+      setMSWHandlers(
+        MSWHandlers.filter((handler) => {
+          return handler !== handlerPresetId;
+        })
+      );
+      setHasUnsavedChanges(true);
+    }
+  };
+
+  const handleApplyChanges = () => {
+    setMSWExtraPresets(MSWHandlers);
+    setMSWContextPopulators(MSWPopulators);
+
+    // We only have to reload the window if MSW is already enabled. Otherwise,
+    // the changes will automatically be picked up next time MSW is enabled.
+    if (isMSWEnabled) {
+      window.location.reload();
+    }
+  };
+
+  // const saveContentAndHandlers = () => {
+
+  // };
 
   return (
     <div className="dev-tools__tool">
@@ -163,22 +289,35 @@ export const ServiceWorkerTool = () => {
           <div className="dev-tools__msw__column">
             <div className="dev-tools__msw__column__heading">Content</div>
             <div className="dev-tools__msw__column__body">
-              <div className="dev-tools__list-box"></div>
+              <div className="dev-tools__list-box">
+                {renderContentPopulatorOptions(
+                  MSWPopulators,
+                  handleChangePopulator
+                )}
+              </div>
             </div>
           </div>
           <div className="dev-tools__msw__column">
-            <div className="dev-tools__msw__column__heading">Handlers</div>
+            <div className="dev-tools__msw__column__heading">Presets</div>
             <div className="dev-tools__msw__column__body">
-              <div className="dev-tools__list-box"></div>
+              <div className="dev-tools__list-box">
+                {renderExtraPresetOptions(MSWHandlers, handleChangeHandler)}
+              </div>
             </div>
           </div>
         </div>
-        {/*{renderExtraPresetOptions()}*/}
       </div>
       <div className="dev-tools__tool__footer">
         <div className="dev-tools__button-list">
-          <button disabled>Discard Changes</button>
-          <button disabled>Apply</button>
+          <button disabled={hasUnsavedChanges ? false : true}>
+            Discard Changes
+          </button>
+          <button
+            disabled={hasUnsavedChanges ? false : true}
+            onClick={handleApplyChanges}
+          >
+            Apply
+          </button>
         </div>
       </div>
     </div>
