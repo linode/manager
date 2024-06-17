f47e91c9f45c7808887d61ae8e227ecf1eb107d6
mjac0bs <mjacobs@akamai.com>
2024-06-17 11:30:34 -0700

    Add SMTP integration test with a TODO

--- packages/manager/cypress/e2e/core/helpAndSupport/open-support-ticket.spec.ts
+++ packages/manager/cypress/e2e/core/helpAndSupport/open-support-ticket.spec.ts
@@ -1,11 +1,5 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable sonarjs/no-duplicate-string */
-import {
-  getClick,
-  containsClick,
-  getVisible,
-  containsVisible,
-} from 'support/helpers';
 import 'cypress-file-upload';
 import { interceptGetProfile } from 'support/intercepts/profile';
 import {
@@ -19,8 +13,9 @@ import {
   randomLabel,
   randomNumber,
   randomPhrase,
+  randomString,
 } from 'support/util/random';
-import { supportTicketFactory } from 'src/factories';
+import { accountFactory, supportTicketFactory } from 'src/factories';
 import {
   mockAttachSupportTicketFile,
   mockCreateSupportTicket,
@@ -28,9 +23,29 @@ import {
   mockGetSupportTickets,
   mockGetSupportTicketReplies,
 } from 'support/intercepts/support';
-import { SEVERITY_LABEL_MAP } from 'src/features/Support/SupportTickets/constants';
+import {
+  SEVERITY_LABEL_MAP,
+  SMTP_DIALOG_TITLE,
+  SMTP_HELPER_TEXT,
+} from 'src/features/Support/SupportTickets/constants';
+import { formatDescription } from 'src/features/Support/SupportTickets/ticketUtils';
+import { mockGetAccount } from 'support/intercepts/account';
+import {
+  EntityType,
+  TicketType,
+} from 'src/features/Support/SupportTickets/SupportTicketDialog';
+import { createTestLinode } from 'support/util/linodes';
+import { cleanUp } from 'support/util/cleanup';
+import { authenticate } from 'support/api/authentication';
+import { MAGIC_DATE_THAT_EMAIL_RESTRICTIONS_WERE_IMPLEMENTED } from 'src/constants';
 
 describe('help & support', () => {
+  after(() => {
+    cleanUp(['linodes']);
+  });
+
+  authenticate();
+
   /*
    * - Opens a Help & Support ticket using mock API data.
    * - Confirms that "Severity" field is not present when feature flag is disabled.
@@ -78,25 +93,27 @@ describe('help & support', () => {
       mockGetSupportTicketReplies(ticketId, []).as('getReplies');
       mockAttachSupportTicketFile(ticketId).as('attachmentPost');
 
-      containsClick('Open New Ticket');
+      cy.contains('Open New Ticket').click();
       cy.get('input[placeholder="Enter a title for your ticket."]')
         .click({ scrollBehavior: false })
         .type(ticketLabel);
       cy.findByLabelText('Severity').should('not.exist');
-      getClick('[data-qa-ticket-entity-type]');
-      containsVisible('General/Account/Billing');
-      getClick('[data-qa-ticket-description="true"]').type(ticketDescription);
+      cy.get('[data-qa-ticket-entity-type]').click();
+      cy.contains('General/Account/Billing').should('be.visible');
+      cy.get('[data-qa-ticket-description="true"]')
+        .click()
+        .type(ticketDescription);
       cy.get('[id="attach-file"]').attachFile(image);
-      getVisible('[value="test_screenshot.png"]');
-      getClick('[data-qa-submit="true"]');
+      cy.get('[value="test_screenshot.png"]').should('be.visible');
+      cy.get('[data-qa-submit="true"]').click();
 
       cy.wait('@createTicket').its('response.statusCode').should('eq', 200);
       cy.wait('@attachmentPost').its('response.statusCode').should('eq', 200);
       cy.wait('@getReplies').its('response.statusCode').should('eq', 200);
 
-      containsVisible(`#${ticketId}: ${ticketLabel}`);
-      containsVisible(ticketDescription);
-      containsVisible(image);
+      cy.contains(`#${ticketId}: ${ticketLabel}`).should('be.visible');
+      cy.contains(ticketDescription).should('be.visible');
+      cy.contains(image).should('be.visible');
     });
   });
 
@@ -187,5 +204,127 @@ describe('help & support', () => {
       .within(() => {
         cy.findByText(severityLabel).should('be.visible');
       });
+  });
+
+  /*
+   * - Opens a SMTP Restriction Removal ticket using mock API data.
+   * - Confirms that the SMTP-specific fields are displayed and handled correctly.
+   */
+  it.only('can create an SMTP support ticket', () => {
+    const mockAccount = accountFactory.build({
+      first_name: 'Jane',
+      last_name: 'Doe',
+      company: 'Acme Co.',
+      active_since: MAGIC_DATE_THAT_EMAIL_RESTRICTIONS_WERE_IMPLEMENTED,
+    });
+
+    const mockFormFields = {
+      useCase: randomString(),
+      publicInfo: randomString(),
+      description: '',
+      entityId: '',
+      entityInputValue: '',
+      entityType: 'general' as EntityType,
+      selectedSeverity: undefined,
+      summary: 'SMTP Restriction Removal on',
+      ticketType: 'smtp' as TicketType,
+      customerName: `${mockAccount.first_name} ${mockAccount.last_name}`,
+      companyName: mockAccount.company,
+      emailDomains: randomString(),
+    };
+
+    const mockSMTPTicket = supportTicketFactory.build({
+      summary: mockFormFields.summary,
+      id: randomNumber(),
+      description: formatDescription(mockFormFields, 'smtp'),
+      status: 'new',
+    });
+
+    mockGetAccount(mockAccount);
+    mockCreateSupportTicket(mockSMTPTicket).as('createTicket');
+    mockGetSupportTickets([]);
+    mockGetSupportTicket(mockSMTPTicket);
+    mockGetSupportTicketReplies(mockSMTPTicket.id, []);
+
+    cy.visitWithLogin('/support/tickets');
+
+    cy.defer(() => createTestLinode({ booted: true })).then((linode) => {
+      cy.visitWithLogin(`/linodes/${linode.id}`);
+      cy.findByText('open a support ticket').should('be.visible').click();
+
+      // Fill out ticket form.
+      ui.dialog
+        .findByTitle('Contact Support: SMTP Restriction Removal')
+        .should('be.visible')
+        .within(() => {
+          cy.findByText(SMTP_DIALOG_TITLE).should('be.visible');
+          cy.findByText(SMTP_HELPER_TEXT).should('be.visible');
+
+          // Confirm summary, customer name, and company name fields are pre-populated with user account data.
+          cy.findByLabelText('Title', { exact: false })
+            .should('be.visible')
+            .should('contain.value', mockFormFields.summary);
+
+          cy.findByLabelText('First and last name', { exact: false })
+            .should('be.visible')
+            .should('have.value', mockFormFields.customerName);
+
+          cy.findByLabelText('Business or company name', { exact: false })
+            .should('be.visible')
+            .should('have.value', mockFormFields.companyName);
+
+          ui.button
+            .findByTitle('Open Ticket')
+            .scrollIntoView()
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+
+          // Confirm validation errors display when trying to submit without required fields.
+          cy.findByText('Use case is required.');
+          cy.findByText('Email domains are required.');
+          cy.findByText('Links to public information are required.');
+
+          // Complete the rest of the form.
+          cy.get('[data-qa-ticket-use-case]')
+            .should('be.visible')
+            .click()
+            .type(mockFormFields.useCase);
+
+          cy.get('[data-qa-ticket-email-domains]')
+            .should('be.visible')
+            .click()
+            .type(mockFormFields.emailDomains);
+
+          cy.get('[data-qa-ticket-public-info]')
+            .should('be.visible')
+            .click()
+            .type(mockFormFields.publicInfo);
+
+          // Confirm there is no description field or file upload section.
+          cy.findByText('Description').should('not.exist');
+          cy.findByText('Attach a File').should('not.exist');
+
+          ui.button
+            .findByTitle('Open Ticket')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+        });
+
+      // Confirm that ticket create payload contains the expected data.
+      // TODO: fix this failure.
+      cy.wait('@createTicket').then((xhr) => {
+        expect(xhr.request.body?.summary).to.eq(mockSMTPTicket.summary);
+        expect(xhr.request.body?.description).to.eq(mockSMTPTicket.description);
+      });
+
+      // Confirm the new ticket is listed with the expected information upon redirecting to the details page.
+      cy.url().should('endWith', `support/tickets/${mockSMTPTicket.id}`);
+      cy.contains(`#${mockSMTPTicket}: ${mockSMTPTicket.summary}`).should(
+        'be.visible'
+      );
+      cy.contains(mockSMTPTicket.description).should('be.visible');
+    });
   });
 });
