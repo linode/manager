5ec87a1febcf5fbd51cee069c89b8b649657b564
jdamore-linode <97627410+jdamore-linode@users.noreply.github.com>
2024-06-18 18:13:12 -0400

    test: [M3-7330] - Attempt to fix hanging unit tests (#10591)
    * Use React Query infinity cache time for unit tests
    
    * Improve event test stability by mocking the current system time when comparing relative date strings
    
    * Apply `pool: forks` option to eliminate hanging
    
    * Added changeset: Fix hanging unit tests

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
         'src/**/*.utils.{js,jsx,ts,tsx}',
       ],
     },
+    pool: 'forks',
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/testSetup.ts',
