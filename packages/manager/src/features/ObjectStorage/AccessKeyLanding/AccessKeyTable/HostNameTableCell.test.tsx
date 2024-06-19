b274baf67e27d79fd4e764607ded7c5aa755ee8b
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-06-19 09:06:48 -0500

    unit test coverage for HostNameTableCell

---
+++ packages/manager/src/features/ObjectStorage/AccessKeyLanding/AccessKeyTable/HostNameTableCell.test.tsx
@@ -0,0 +1,115 @@
+import '@testing-library/jest-dom';
+import { waitFor } from '@testing-library/react';
+import React from 'react';
+
+import { regionFactory } from 'src/factories';
+import { makeResourcePage } from 'src/mocks/serverHandlers';
+import { HttpResponse, http, server } from 'src/mocks/testServer';
+import { renderWithThemeAndHookFormContext } from 'src/utilities/testHelpers';
+
+import { HostNameTableCell } from './HostNameTableCell';
+
+const storageKeyData = {
+  access_key: 'test_key',
+  bucket_access: null,
+  id: 12345,
+  label: 'this is regular key',
+  limited: false,
+  regions: [
+    {
+      id: 'us-east',
+      s3_endpoint: 'alpha.test.com',
+    },
+  ],
+  secret_key: '[test]',
+};
+
+describe('HostNameTableCell', () => {
+  it('should render  "None" when there are no regions', () => {
+    const { getByText } = renderWithThemeAndHookFormContext({
+      component: (
+        <HostNameTableCell
+          storageKeyData={{
+            access_key: 'test_key',
+            bucket_access: null,
+            id: 0,
+            label: 'test',
+            limited: false,
+            regions: [],
+            secret_key: '',
+          }}
+          setHostNames={vi.fn()}
+          setShowHostNamesDrawers={vi.fn()}
+        />
+      ),
+    });
+
+    expect(getByText('None')).toBeInTheDocument();
+  });
+
+  test('should render  "Regions/S3 Hostnames" cell when there are regions', async () => {
+    const region = regionFactory.build({
+      capabilities: ['Object Storage'],
+      id: 'us-east',
+      label: 'Newark, NJ',
+    });
+
+    server.use(
+      http.get('*/v4/regions', () => {
+        return HttpResponse.json(makeResourcePage([region]));
+      })
+    );
+    const { findByText } = renderWithThemeAndHookFormContext({
+      component: (
+        <HostNameTableCell
+          setHostNames={vi.fn()}
+          setShowHostNamesDrawers={vi.fn()}
+          storageKeyData={storageKeyData}
+        />
+      ),
+    });
+
+    const hostname = await findByText('Newark, NJ: alpha.test.com');
+
+    await waitFor(() => expect(hostname).toBeInTheDocument());
+  });
+  test('should render all  "Regions/S3 Hostnames" in the cell when there are multiple regions', async () => {
+    const region = regionFactory.build({
+      capabilities: ['Object Storage'],
+      id: 'us-east',
+      label: 'Newark, NJ',
+    });
+
+    server.use(
+      http.get('*/v4/regions', () => {
+        return HttpResponse.json(makeResourcePage([region]));
+      })
+    );
+    const { findByText } = renderWithThemeAndHookFormContext({
+      component: (
+        <HostNameTableCell
+          storageKeyData={{
+            ...storageKeyData,
+            regions: [
+              {
+                id: 'us-east',
+                s3_endpoint: 'alpha.test.com',
+              },
+              {
+                id: 'us-south',
+                s3_endpoint: 'alpha.test.com',
+              },
+            ],
+          }}
+          setHostNames={vi.fn()}
+          setShowHostNamesDrawers={vi.fn()}
+        />
+      ),
+    });
+    const hostname = await findByText('Newark, NJ: alpha.test.com');
+    const moreButton = await findByText(/and\s+1\s+more\.\.\./);
+    await waitFor(() => expect(hostname).toBeInTheDocument());
+
+    await expect(moreButton).toBeInTheDocument();
+  });
+});
