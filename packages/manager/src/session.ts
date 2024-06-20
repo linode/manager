801259a161b69b4fd5c9cb40f6761448d7c238aa
mkaminsk <mkaminsk@akamai.com>
2024-06-20 16:55:53 +0200

    feat: [LILO-418] - Modify Cloud Manager to use OAuth PKCE instead of implicit flow

--- packages/manager/src/session.ts
+++ packages/manager/src/session.ts
@@ -2,10 +2,21 @@ import Axios from 'axios';
 import { v4 } from 'uuid';
 
 import { APP_ROOT, CLIENT_ID, LOGIN_ROOT } from 'src/constants';
+import { generateCodeChallenge, generateCodeVerifier } from 'src/pkce';
+import { clearNonceAndCodeVerifierFromLocalStorage } from 'src/store/authentication/authentication.helpers';
 import {
   authentication,
   getEnvLocalStorageOverrides,
 } from 'src/utilities/storage';
+
+let codeVerifier: string = '';
+let codeChallenge: string = '';
+
+export async function generateCodeVerifierAndChallenge(): Promise<void> {
+  codeVerifier = await generateCodeVerifier();
+  codeChallenge = await generateCodeChallenge(codeVerifier);
+  authentication.codeVerifier.set(codeVerifier);
+}
 
 /**
  * Creates a URL with the supplied props as a stringified query. The shape of the query is required
@@ -18,9 +29,9 @@ import {
  */
 export const genOAuthEndpoint = (
   redirectUri: string,
-  scope = '*',
+  scope: string = '*',
   nonce: string
-) => {
+): string => {
   // If there are local storage overrides, use those. Otherwise use variables set in the ENV.
   const localStorageOverrides = getEnvLocalStorageOverrides();
   const clientID = localStorageOverrides?.clientID ?? CLIENT_ID;
@@ -32,8 +43,10 @@ export const genOAuthEndpoint = (
 
   const query = {
     client_id: clientID,
+    code_challenge: codeChallenge,
+    code_challenge_method: 'S256',
     redirect_uri: `${APP_ROOT}/oauth/callback?returnTo=${redirectUri}`,
-    response_type: 'token',
+    response_type: 'code',
     scope,
     state: nonce,
   };
@@ -51,7 +64,7 @@ export const genOAuthEndpoint = (
  * @param scope {string}
  * @returns {string} - OAuth authorization endpoint URL
  */
-export const prepareOAuthEndpoint = (redirectUri: string, scope = '*') => {
+export const prepareOAuthEndpoint = (redirectUri: string, scope: string = '*'): string => {
   const nonce = v4();
   authentication.nonce.set(nonce);
   return genOAuthEndpoint(redirectUri, scope, nonce);
@@ -65,10 +78,12 @@ export const prepareOAuthEndpoint = (redirectUri: string, scope = '*') => {
  * @param {string} queryString - any additional query you want to add
  * to the returnTo path
  */
-export const redirectToLogin = (
+export const redirectToLogin = async (
   returnToPath: string,
   queryString: string = ''
 ) => {
+  clearNonceAndCodeVerifierFromLocalStorage();
+  await generateCodeVerifierAndChallenge();
   const redirectUri = `${returnToPath}${queryString}`;
   window.location.assign(prepareOAuthEndpoint(redirectUri));
 };
