c1c957c201ba4e7dca62f884a3d511f9d52b81b1
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-06-20 15:56:55 -0500

    Update sort-by.ts

--- packages/manager/src/utilities/sort-by.ts
+++ packages/manager/src/utilities/sort-by.ts
@@ -45,3 +45,25 @@ export const sortByArrayLength = (a: any[], b: any[], order: SortOrder) => {
 
   return order === 'asc' ? result : -result;
 };
+
+export const sortByVersion = (a: string, b: string, order: SortOrder) => {
+  const aParts = a.split('.');
+  const bParts = b.split('.');
+
+  const result = (() => {
+    for (let i = 0; i < Math.max(aParts.length, bParts.length); i += 1) {
+      // If one version has a part and another doesn't (e.g. 3.1 vs 3.1.1),
+      // treat the missing part as 0.
+      const aNumber = Number(aParts[i]) || 0;
+      const bNumber = Number(bParts[i]) || 0;
+      const diff = aNumber - bNumber;
+
+      if (diff !== 0) {
+        return diff;
+      }
+    }
+    return 0;
+  })();
+
+  return order === 'asc' ? result : -result;
+};
