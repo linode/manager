6d18ade4a0a34348565926f1c784cc7f18e37759
Hussain Khalil <hkhalil@akamai.com>
2024-06-26 10:14:25 -0400

    Remove errant call to debug()

--- packages/manager/src/features/Images/ImagesCreate/CreateImageTab.test.tsx
+++ packages/manager/src/features/Images/ImagesCreate/CreateImageTab.test.tsx
@@ -1,7 +1,6 @@
 import { waitFor } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import React from 'react';
-import { debug } from 'vitest-preview';
 
 import {
   imageFactory,
@@ -87,8 +86,6 @@ describe('CreateImageTab', () => {
     const submitButton = getByText('Create Image').closest('button');
 
     await userEvent.click(submitButton!);
-
-    debug();
 
     expect(getByText('Disk is required.')).toBeVisible();
   });
