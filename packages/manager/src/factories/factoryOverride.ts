ad1a27720ade7cb6e197e9638b1ca1260e9ff411
Alban Bailly <abailly@akamai.com>
2024-06-26 17:58:00 -0400

    comment and cleanup

--- packages/manager/src/factories/factoryOverride.ts
+++ packages/manager/src/factories/factoryOverride.ts
@@ -2,8 +2,16 @@ import * as Factory from 'factory.ts';
 
 const originalEach = Factory.each;
 
+/**
+ * Override the each method of the factory.ts library to start the index from 1
+ * This prevents a a variety of issues with entity IDs being falsy.
+ *
+ * Monkey patching here in oder to avoid patching the library itself.
+ * While not ideal, it prevents us from having to maintain a fork of the library,
+ * or having to modify and remember to increment every `each` at the factory level.
+ */
 Object.defineProperty(Factory, 'each', {
-  value: (fn: any) => {
+  value: (fn: (index: number) => number | string) => {
     return originalEach((i) => {
       return fn(i + 1);
     });
