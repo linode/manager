6bcdfa825ee986b1704b6148e7001764f9f92329
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-06-27 10:31:25 -0500

    Add interfaces and additional fields to Ansible snippet

--- packages/manager/src/utilities/codesnippets/generate-ansibleConfig.ts
+++ packages/manager/src/utilities/codesnippets/generate-ansibleConfig.ts
@@ -1,4 +1,4 @@
-import { CreateLinodeRequest } from '@linode/api-v4/lib/linodes';
+import type { CreateLinodeRequest } from '@linode/api-v4/lib/linodes';
 
 /**
  * Escapes special characters in a string for use in YAML and shell commands.
@@ -38,6 +38,9 @@ export function generateAnsibleConfig(config: CreateLinodeRequest): string {
   if (config.stackscript_id) {
     configStr += `    stackscript_id: ${config.stackscript_id}\n`;
   }
+  if (config.hasOwnProperty('private_ip')) {
+    configStr += `    private_ip: ${config.private_ip}\n`;
+  }
   if (config.stackscript_data) {
     configStr += `    stackscript_data:\n`;
     Object.keys(config.stackscript_data).forEach((key) => {
@@ -46,10 +49,35 @@ export function generateAnsibleConfig(config: CreateLinodeRequest): string {
       )}"\n`;
     });
   }
-  if (config.tags) {
+  if (config.backups_enabled) {
+    configStr += `    backups_enabled: ${config.backups_enabled}\n`;
+  }
+  if (config.tags && config.tags.length > 0) {
     configStr += `    tags:\n      - "${config.tags
       .map((tag) => escapeYAMLString(tag))
       .join('"\n      - "')}"\n`;
+  }
+  if (config.interfaces && config.interfaces.length > 0) {
+    configStr += `    interfaces:\n`;
+    config.interfaces.forEach((iface) => {
+      configStr += `      - purpose: "${escapeYAMLString(iface.purpose)}"\n`;
+      if (iface.subnet_id) {
+        configStr += `        subnet_id: ${iface.subnet_id}\n`;
+      }
+      if (iface.ip_ranges) {
+        configStr += `        ip_ranges:\n          - ${iface.ip_ranges
+          .map((ip) => escapeYAMLString(ip))
+          .join('\n          - ')}\n`;
+      }
+      if (iface.label) {
+        configStr += `        label: "${escapeYAMLString(iface.label)}"\n`;
+      }
+      if (iface.ipam_address) {
+        configStr += `        ipam_address: "${escapeYAMLString(
+          iface.ipam_address
+        )}"\n`;
+      }
+    });
   }
 
   return configStr;
