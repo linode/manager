5a7f5c59d9d45bd41fe2589247c69de452091ab3
Alban Bailly <abailly@akamai.com>
2024-06-27 00:32:34 -0400

    exclude cypress for now

--- packages/manager/src/factories/factoryOverride.ts
+++ packages/manager/src/factories/factoryOverride.ts
@@ -2,6 +2,8 @@ import * as Factory from 'factory.ts';
 
 const originalEach = Factory.each;
 
+const isCypress = typeof window !== 'undefined' && (window as any).Cypress;
+
 /**
  * Override the each method of the factory.ts library to start the index from 1
  * This prevents a a variety of issues with entity IDs being falsy.
@@ -10,11 +12,13 @@ const originalEach = Factory.each;
  * While not ideal, it prevents us from having to maintain a fork of the library,
  * or having to modify and remember to increment every `each` at the factory level.
  */
-Object.defineProperty(Factory, 'each', {
-  value: (fn: (index: number) => number | string) => {
-    return originalEach((i) => {
-      return fn(i + 1);
-    });
-  },
-  writable: false,
-});
+if (!isCypress) {
+  Object.defineProperty(Factory, 'each', {
+    value: (fn: (index: number) => number | string) => {
+      return originalEach((i) => {
+        return fn(i + 1);
+      });
+    },
+    writable: false,
+  });
+}
