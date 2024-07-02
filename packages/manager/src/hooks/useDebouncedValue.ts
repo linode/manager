9219c720c54273d852b3cb616ff04bd59ef0cd42
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2024-07-02 13:25:08 -0400

    upcoming: [M3-8066] - Debounce the Linode Create v2 `VLANSelect` (#10628)
    * debounce input value so less requests are made
    
    * add unit test
    
    * changeset and improve test name
    
    * add a default debounce of `500` ms
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

---
+++ packages/manager/src/hooks/useDebouncedValue.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export const useDebouncedValue = <T>(value: T, delay: number = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
