6a85dcc5eb97990940770c26ba3963c7848983da
Cassie Liu <cliu@akamai.com>
2024-07-18 15:24:35 -0400

    M3-8136: Add Cypress integration test for closing support tickets

---
+++ packages/manager/cypress/support/constants/help-and-support.ts
@@ -0,0 +1,14 @@
+/**
+ * Close button text that is displayed in the detail of closable support ticket.
+ */
+export const closeButtonText = 'close this ticket';
+
+/**
+ * Closable message that is displayed in the detail of closable support ticket.
+ */
+export const closableMessage = 'If everything is resolved, you can';
+
+/**
+ * Dialog title that is in close ticket dialog.
+ */
+export const closeTicketDialogTitle = 'Confirm Ticket Close';
