77597b9ece52ce39e6d2538636f93ceec0e31840
Banks Nussman <banks@nussman.us>
2024-07-19 11:57:46 -0400

    update cypress test to account for optimization of fetching agreements only if EU region is selected

--- packages/manager/cypress/e2e/core/general/gdpr-agreement.spec.ts
+++ packages/manager/cypress/e2e/core/general/gdpr-agreement.spec.ts
@@ -49,12 +49,14 @@ describe('GDPR agreement', () => {
     }).as('getAgreements');
 
     cy.visitWithLogin('/linodes/create');
-    cy.wait(['@getAgreements', '@getRegions']);
+    cy.wait('@getRegions');
 
     // Paris should have the agreement
     ui.regionSelect.find().click();
     ui.regionSelect.findItemByRegionId('fr-par').click();
     cy.get('[data-testid="eu-agreement-checkbox"]').should('be.visible');
+
+    cy.wait('@getAgreements');
 
     // London should have the agreement
     ui.regionSelect.find().click();
@@ -75,12 +77,14 @@ describe('GDPR agreement', () => {
     }).as('getAgreements');
 
     cy.visitWithLogin('/linodes/create');
-    cy.wait(['@getAgreements', '@getRegions']);
+    cy.wait('@getRegions');
 
     // Paris should not have the agreement
     ui.regionSelect.find().click();
     ui.regionSelect.findItemByRegionId('fr-par').click();
     cy.get('[data-testid="eu-agreement-checkbox"]').should('not.exist');
+    
+    cy.wait('@getAgreements');
 
     // London should not have the agreement
     ui.regionSelect.find().click();
