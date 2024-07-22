8d553c123d69db5ce82bac315def6cf9cf09033e
Hana Xu <hxu@akamai.com>
2024-07-22 15:44:40 -0400

    update region label to country, city

--- packages/manager/src/features/ObjectStorage/AccessKeyLanding/BucketPermissionsTable.tsx
+++ packages/manager/src/features/ObjectStorage/AccessKeyLanding/BucketPermissionsTable.tsx
@@ -1,8 +1,8 @@
-import { AccessType, Scope } from '@linode/api-v4/lib/object-storage/types';
 import { update } from 'ramda';
 import * as React from 'react';
 
 import { Radio } from 'src/components/Radio/Radio';
+import { useIsGeckoEnabled } from 'src/components/RegionSelect/RegionSelect.utils';
 import { TableBody } from 'src/components/TableBody';
 import { TableCell } from 'src/components/TableCell';
 import { TableHead } from 'src/components/TableHead';
@@ -22,6 +22,10 @@ import {
 } from './AccessTable.styles';
 
 import type { MODE } from './types';
+import type {
+  AccessType,
+  Scope,
+} from '@linode/api-v4/lib/object-storage/types';
 
 export const getUpdatedScopes = (
   oldScopes: Scope[],
@@ -56,7 +60,10 @@ interface Props {
 export const BucketPermissionsTable = React.memo((props: Props) => {
   const { bucket_access, checked, mode, selectedRegions, updateScopes } = props;
 
-  const { data: regionsData } = useRegionsQuery();
+  const { isGeckoGAEnabled } = useIsGeckoEnabled();
+  const { data: regionsData } = useRegionsQuery({
+    transformRegionLabel: isGeckoGAEnabled,
+  });
   const regionsLookup = regionsData && getRegionsByRegionId(regionsData);
 
   if (!bucket_access || !regionsLookup) {
