4fd50c51b947274bd52f9521fc93d92003ac10b7
Joe D'Amore <jdamore@linode.com>
2024-07-25 11:58:05 -0400

    Temporarily trigger test failure again

--- packages/manager/cypress/e2e/core/objectStorage/object-storage.e2e.spec.ts
+++ packages/manager/cypress/e2e/core/objectStorage/object-storage.e2e.spec.ts
@@ -157,6 +157,7 @@ describe('object storage end-to-end tests', () => {
    */
   it('can create and delete object storage buckets', () => {
     cy.tag('purpose:syntheticTesting');
+    throw new Error('This failure was triggered intentionally.');
 
     const bucketLabel = randomLabel();
     const bucketRegion = 'Atlanta, GA';
