f351435ec450a51c3d8cf5b3e861dd4c8c0177b9
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-07-31 10:27:20 -0500

    Covert E2E test to cypress integration test

--- packages/manager/cypress/e2e/core/linodes/smoke-linode-view-code-snippet.spec.ts
+++ packages/manager/cypress/e2e/core/linodes/smoke-linode-view-code-snippet.spec.ts
@@ -11,11 +11,103 @@ import {
   mockAppendFeatureFlags,
   mockGetFeatureFlagClientstream,
 } from 'support/intercepts/feature-flags';
-import { interceptCreateLinode } from 'support/intercepts/linodes';
 import { makeFeatureFlagData } from 'support/util/feature-flags';
-import { interceptGetProfile } from 'support/intercepts/profile';
+
+import {
+  regionFactory,
+  imageFactory,
+  profileFactory,
+  userPreferencesFactory,
+  accountFactory,
+  firewallFactory,
+  accountUserFactory,
+  vpcFactory,
+  linodeTypeFactory,
+  regionAvailabilityFactory,
+} from 'src/factories';
+
+import { mockGetLinodeTypes } from 'support/intercepts/linodes';
+import { mockGetAllImages } from 'support/intercepts/images';
+import {
+  mockGetProfile,
+  mockGetUserPreferences,
+} from 'support/intercepts/profile';
+import { accountSettingsFactory } from '@src/factories/accountSettings';
+import {
+  mockGetAccount,
+  mockGetAccountSettings,
+  mockGetUsers,
+} from 'support/intercepts/account';
+import type { Region } from '@linode/api-v4';
+import {
+  mockGetRegions,
+  mockGetRegionAvailability,
+} from 'support/intercepts/regions';
+import { mockGetFirewalls } from 'support/intercepts/firewalls';
+import { mockGetVPCs } from 'support/intercepts/vpc';
 
 authenticate();
+
+const mockProfile = profileFactory.build({
+  username: 'mock-user',
+  restricted: false,
+});
+const mockAccount = accountFactory.build();
+const mockAccountSettings = accountSettingsFactory.build();
+const mockPreferencesListView = userPreferencesFactory.build();
+const mockRegions: Region[] = [
+  regionFactory.build({
+    capabilities: ['Linodes', 'Placement Group'],
+    id: 'us-east',
+    label: 'Newark, NJ',
+    country: 'us',
+  }),
+  regionFactory.build({
+    capabilities: ['Linodes'],
+    id: 'us-central',
+    label: 'Dallas, TX',
+    country: 'us',
+  }),
+];
+const mockImages = [imageFactory.build()];
+const mockFirewall = [firewallFactory.build()];
+const mockUsers = [accountUserFactory.build()];
+const mockVPCs = [vpcFactory.build()];
+const mockLinodeTypes = [
+  linodeTypeFactory.build({
+    id: 'nanode-1',
+    label: 'Nanode 1 GB',
+    class: 'nanode',
+  }),
+  linodeTypeFactory.build({
+    id: 'dedicated-1',
+    label: 'dedicated-1',
+    class: 'dedicated',
+  }),
+];
+const mockRegionAvailability = [
+  regionAvailabilityFactory.build({
+    plan: 'dedicated-3',
+    available: false,
+    region: 'us-east',
+  }),
+  regionAvailabilityFactory.build({
+    plan: 'dedicated-4',
+    available: false,
+    region: 'us-east',
+  }),
+  regionAvailabilityFactory.build({
+    plan: 'highmem-1',
+    available: false,
+    region: 'us-east',
+  }),
+  regionAvailabilityFactory.build({
+    plan: 'shared-3',
+    available: false,
+    region: 'us-east',
+  }),
+];
+
 describe('Create Linode', () => {
   /*
    * End-to-end tests for create Linode flow and validate code snippet modal.
@@ -34,15 +126,40 @@ describe('Create Linode', () => {
       const linodeLabel = randomLabel();
       const rootPass = randomString(32);
 
-      interceptGetProfile().as('getProfile');
+      mockGetProfile(mockProfile).as('getProfile');
+      mockGetAccount(mockAccount).as('getAccount');
+      mockGetAccountSettings(mockAccountSettings).as('getAccountSettings');
+      mockGetUserPreferences(mockPreferencesListView).as('getUserPreferences');
+      mockGetRegions(mockRegions).as('getRegions');
+      mockGetAllImages(mockImages).as('getImages');
+      mockGetFirewalls(mockFirewall).as('getFirewalls');
+      mockGetUsers(mockUsers).as('getUsers');
+      mockGetVPCs(mockVPCs).as('getVPCs');
+      mockGetLinodeTypes(mockLinodeTypes).as('getLinodeTypes');
+      mockGetRegionAvailability(mockRegions[0].id, mockRegionAvailability).as(
+        'getRegionAvailability'
+      );
 
-      interceptCreateLinode().as('createLinode');
       cy.visitWithLogin('/linodes/create');
+
+      cy.wait([
+        '@getProfile',
+        '@getAccount',
+        '@getAccountSettings',
+        '@getUserPreferences',
+        '@getRegions',
+        '@getImages',
+        '@getFirewalls',
+        '@getUsers',
+        '@getVPCs',
+        '@getLinodeTypes',
+      ]);
 
       // Set Linode label, distribution, plan type, password, etc.
       linodeCreatePage.setLabel(linodeLabel);
-      linodeCreatePage.selectImage('Debian 11');
+      linodeCreatePage.selectImage('image-1');
       linodeCreatePage.selectRegionById('us-east');
+      cy.wait(['@getRegionAvailability']);
       linodeCreatePage.selectPlan('Shared CPU', 'Nanode 1 GB');
       linodeCreatePage.setRootPassword(rootPass);
 
@@ -169,15 +286,40 @@ describe('Create Linode', () => {
       const linodeLabel = randomLabel();
       const rootPass = randomString(32);
 
-      interceptGetProfile().as('getProfile');
+      mockGetProfile(mockProfile).as('getProfile');
+      mockGetAccount(mockAccount).as('getAccount');
+      mockGetAccountSettings(mockAccountSettings).as('getAccountSettings');
+      mockGetUserPreferences(mockPreferencesListView).as('getUserPreferences');
+      mockGetRegions(mockRegions).as('getRegions');
+      mockGetAllImages(mockImages).as('getImages');
+      mockGetFirewalls(mockFirewall).as('getFirewalls');
+      mockGetUsers(mockUsers).as('getUsers');
+      mockGetVPCs(mockVPCs).as('getVPCs');
+      mockGetLinodeTypes(mockLinodeTypes).as('getLinodeTypes');
+      mockGetRegionAvailability(mockRegions[0].id, mockRegionAvailability).as(
+        'getRegionAvailability'
+      );
 
-      interceptCreateLinode().as('createLinode');
       cy.visitWithLogin('/linodes/create');
+
+      cy.wait([
+        '@getProfile',
+        '@getAccount',
+        '@getAccountSettings',
+        '@getUserPreferences',
+        '@getRegions',
+        '@getImages',
+        '@getFirewalls',
+        '@getUsers',
+        '@getVPCs',
+        '@getLinodeTypes',
+      ]);
 
       // Set Linode label, distribution, plan type, password, etc.
       linodeCreatePage.setLabel(linodeLabel);
-      linodeCreatePage.selectImage('Debian 11');
+      linodeCreatePage.selectImage('image-1');
       linodeCreatePage.selectRegionById('us-east');
+      cy.wait(['@getRegionAvailability']);
       linodeCreatePage.selectPlan('Shared CPU', 'Nanode 1 GB');
       linodeCreatePage.setRootPassword(rootPass);
 
