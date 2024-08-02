f9a165ae39529ecae1a9cfc504abe9a80535af77
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2024-08-02 11:58:59 -0400

    refactor: [M3-7438] - Query Key Factory for Object Storage (#10726)
    * attempt 1
    
    * more query key factory progress
    
    * use factory for objects in bucket
    
    * clean up and organize
    
    * fix unit test
    
    * Added changeset: Query Key Factory for Object Storage
    
    * fix more unit tests
    
    * readd optimization @hana-linode
    
    * feedback @jaalah-akamai
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

---
+++ packages/manager/src/queries/object-storage/queries.ts
@@ -0,0 +1,295 @@
+import {
+  createBucket,
+  deleteBucket,
+  deleteBucketWithRegion,
+  deleteSSLCert,
+  getObjectList,
+  getObjectStorageKeys,
+  getObjectURL,
+  getSSLCert,
+  uploadSSLCert,
+} from '@linode/api-v4';
+import { createQueryKeys } from '@lukemorales/query-key-factory';
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+
+import { OBJECT_STORAGE_DELIMITER as delimiter } from 'src/constants';
+import { useFlags } from 'src/hooks/useFlags';
+import { isFeatureEnabledV2 } from 'src/utilities/accountCapabilities';
+
+import { useAccount } from '../account/account';
+import { accountQueries } from '../account/queries';
+import { queryPresets } from '../base';
+import { useRegionsQuery } from '../regions/regions';
+import {
+  getAllBucketsFromClusters,
+  getAllBucketsFromRegions,
+  getAllObjectStorageClusters,
+  getAllObjectStorageTypes,
+} from './requests';
+import { prefixToQueryKey } from './utilities';
+
+import type { BucketsResponse } from './requests';
+import type {
+  APIError,
+  CreateObjectStorageBucketPayload,
+  CreateObjectStorageBucketSSLPayload,
+  CreateObjectStorageObjectURLPayload,
+  ObjectStorageBucket,
+  ObjectStorageBucketSSL,
+  ObjectStorageCluster,
+  ObjectStorageKey,
+  ObjectStorageObjectList,
+  ObjectStorageObjectURL,
+  Params,
+  PriceType,
+  ResourcePage,
+} from '@linode/api-v4';
+
+export const objectStorageQueries = createQueryKeys('object-storage', {
+  accessKeys: (params: Params) => ({
+    queryFn: () => getObjectStorageKeys(params),
+    queryKey: [params],
+  }),
+  bucket: (clusterOrRegion: string, bucketName: string) => ({
+    contextQueries: {
+      objects: {
+        // This is a placeholder queryFn and QueryKey. View the `useObjectBucketObjectsInfiniteQuery` implementation for details.
+        queryFn: null,
+        queryKey: null,
+      },
+      ssl: {
+        queryFn: () => getSSLCert(clusterOrRegion, bucketName),
+        queryKey: null,
+      },
+    },
+    queryKey: [clusterOrRegion, bucketName],
+  }),
+  buckets: {
+    queryFn: () => null, // This is a placeholder queryFn. Look at `useObjectStorageBuckets` for the actual logic.
+    queryKey: null,
+  },
+  clusters: {
+    queryFn: getAllObjectStorageClusters,
+    queryKey: null,
+  },
+  types: {
+    queryFn: getAllObjectStorageTypes,
+    queryKey: null,
+  },
+});
+
+export const useObjectStorageClusters = (enabled: boolean = true) =>
+  useQuery<ObjectStorageCluster[], APIError[]>({
+    ...objectStorageQueries.clusters,
+    ...queryPresets.oneTimeFetch,
+    enabled,
+  });
+
+export const useObjectStorageBuckets = (enabled = true) => {
+  const flags = useFlags();
+  const { data: account } = useAccount();
+
+  const isObjMultiClusterEnabled = isFeatureEnabledV2(
+    'Object Storage Access Key Regions',
+    Boolean(flags.objMultiCluster),
+    account?.capabilities ?? []
+  );
+
+  const { data: allRegions } = useRegionsQuery();
+  const { data: clusters } = useObjectStorageClusters(
+    enabled && !isObjMultiClusterEnabled
+  );
+
+  const regions = allRegions?.filter((r) =>
+    r.capabilities.includes('Object Storage')
+  );
+
+  return useQuery<BucketsResponse, APIError[]>({
+    enabled: isObjMultiClusterEnabled
+      ? regions !== undefined && enabled
+      : clusters !== undefined && enabled,
+    queryFn: isObjMultiClusterEnabled
+      ? () => getAllBucketsFromRegions(regions)
+      : () => getAllBucketsFromClusters(clusters),
+    queryKey: objectStorageQueries.buckets.queryKey,
+    retry: false,
+  });
+};
+
+export const useObjectStorageAccessKeys = (params: Params) =>
+  useQuery<ResourcePage<ObjectStorageKey>, APIError[]>({
+    ...objectStorageQueries.accessKeys(params),
+    keepPreviousData: true,
+  });
+
+export const useCreateBucketMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<
+    ObjectStorageBucket,
+    APIError[],
+    CreateObjectStorageBucketPayload
+  >({
+    mutationFn: createBucket,
+    onSuccess(bucket) {
+      // Invalidate account settings because object storage will become enabled
+      // if a user created their first bucket.
+      queryClient.invalidateQueries({
+        queryKey: accountQueries.settings.queryKey,
+      });
+
+      // Add the new bucket to the cache
+      queryClient.setQueryData<BucketsResponse>(
+        objectStorageQueries.buckets.queryKey,
+        (oldData) => ({
+          buckets: [...(oldData?.buckets ?? []), bucket],
+          errors: oldData?.errors ?? [],
+        })
+      );
+
+      // Invalidate buckets and cancel existing requests to GET buckets
+      // because a user might create a bucket bfore all buckets have been fetched.
+      queryClient.invalidateQueries(
+        {
+          queryKey: objectStorageQueries.buckets.queryKey,
+        },
+        {
+          cancelRefetch: true,
+        }
+      );
+    },
+  });
+};
+
+export const useDeleteBucketMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<{}, APIError[], { cluster: string; label: string }>({
+    mutationFn: deleteBucket,
+    onSuccess: (_, variables) => {
+      queryClient.setQueryData<BucketsResponse>(
+        objectStorageQueries.buckets.queryKey,
+        (oldData) => ({
+          buckets:
+            oldData?.buckets.filter(
+              (bucket) =>
+                !(
+                  bucket.cluster === variables.cluster &&
+                  bucket.label === variables.label
+                )
+            ) ?? [],
+          errors: oldData?.errors ?? [],
+        })
+      );
+    },
+  });
+};
+
+/*
+ @TODO OBJ Multicluster: useDeleteBucketWithRegionMutation is a temporary hook,
+ once feature is rolled out we replace it with existing useDeleteBucketMutation
+ by updating it with region instead of cluster.
+*/
+export const useDeleteBucketWithRegionMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<{}, APIError[], { label: string; region: string }>({
+    mutationFn: deleteBucketWithRegion,
+    onSuccess: (_, variables) => {
+      queryClient.setQueryData<BucketsResponse>(
+        objectStorageQueries.buckets.queryKey,
+        (oldData) => ({
+          buckets:
+            oldData?.buckets.filter(
+              (bucket: ObjectStorageBucket) =>
+                !(
+                  bucket.region === variables.region &&
+                  bucket.label === variables.label
+                )
+            ) ?? [],
+          errors: oldData?.errors ?? [],
+        })
+      );
+    },
+  });
+};
+
+export const useObjectBucketObjectsInfiniteQuery = (
+  clusterId: string,
+  bucket: string,
+  prefix: string
+) =>
+  useInfiniteQuery<ObjectStorageObjectList, APIError[]>({
+    getNextPageParam: (lastPage) => lastPage.next_marker,
+    queryFn: ({ pageParam }) =>
+      getObjectList({
+        bucket,
+        clusterId,
+        params: { delimiter, marker: pageParam, prefix },
+      }),
+    queryKey: [
+      ...objectStorageQueries.bucket(clusterId, bucket)._ctx.objects.queryKey,
+      ...prefixToQueryKey(prefix),
+    ],
+  });
+
+export const useCreateObjectUrlMutation = (
+  clusterId: string,
+  bucketName: string
+) =>
+  useMutation<
+    ObjectStorageObjectURL,
+    APIError[],
+    {
+      method: 'DELETE' | 'GET' | 'POST' | 'PUT';
+      name: string;
+      options?: CreateObjectStorageObjectURLPayload;
+    }
+  >({
+    mutationFn: ({ method, name, options }) =>
+      getObjectURL(clusterId, bucketName, name, method, options),
+  });
+
+export const useBucketSSLQuery = (cluster: string, bucket: string) =>
+  useQuery(objectStorageQueries.bucket(cluster, bucket)._ctx.ssl);
+
+export const useBucketSSLMutation = (cluster: string, bucket: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    ObjectStorageBucketSSL,
+    APIError[],
+    CreateObjectStorageBucketSSLPayload
+  >({
+    mutationFn: (data) => uploadSSLCert(cluster, bucket, data),
+    onSuccess(data) {
+      queryClient.setQueryData<ObjectStorageBucketSSL>(
+        objectStorageQueries.bucket(cluster, bucket)._ctx.ssl.queryKey,
+        data
+      );
+    },
+  });
+};
+
+export const useBucketSSLDeleteMutation = (cluster: string, bucket: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<{}, APIError[]>({
+    mutationFn: () => deleteSSLCert(cluster, bucket),
+    onSuccess() {
+      queryClient.setQueryData<ObjectStorageBucketSSL>(
+        objectStorageQueries.bucket(cluster, bucket)._ctx.ssl.queryKey,
+        { ssl: false }
+      );
+    },
+  });
+};
+
+export const useObjectStorageTypesQuery = (enabled = true) =>
+  useQuery<PriceType[], APIError[]>({
+    ...objectStorageQueries.types,
+    ...queryPresets.oneTimeFetch,
+    enabled,
+  });
