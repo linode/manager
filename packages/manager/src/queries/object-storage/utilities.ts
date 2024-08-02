d1c99d4240997bfb527887ebafdd2499a43c261a
Banks Nussman <banks@nussman.us>
2024-08-02 09:31:10 -0400

    readd optimization @hana-linode

--- packages/manager/src/queries/object-storage/utilities.ts
+++ packages/manager/src/queries/object-storage/utilities.ts
@@ -1,3 +1,10 @@
+import { getBucket } from '@linode/api-v4';
+
+import { objectStorageQueries } from './queries';
+
+import type { BucketsResponse } from './requests';
+import type { QueryClient } from '@tanstack/react-query';
+
 /**
  * Used to make a nice React Query queryKey by splitting the prefix
  * by the '/' character.
@@ -10,4 +17,50 @@
  */
 export const prefixToQueryKey = (prefix: string) => {
   return prefix.split('/', prefix.split('/').length - 1);
+};
+
+/**
+ * Fetches a single bucket and updates it in the cache.
+ *
+ * We have this function so we have an efficent way to update a single bucket
+ * as opposed to re-fetching all buckets.
+ */
+export const fetchBucketAndUpdateCache = async (
+  regionOrCluster: string,
+  bucketName: string,
+  queryClient: QueryClient
+) => {
+  const bucket = await getBucket(regionOrCluster, bucketName);
+
+  queryClient.setQueryData<BucketsResponse>(
+    objectStorageQueries.buckets.queryKey,
+    (previousData) => {
+      const indexOfBucket = previousData?.buckets?.findIndex(
+        (b) =>
+          (b.region === regionOrCluster || b.cluster === regionOrCluster) &&
+          b.label === bucketName
+      );
+
+      if (
+        indexOfBucket === undefined ||
+        indexOfBucket === -1 ||
+        previousData?.buckets === undefined
+      ) {
+        // If the bucket does not exist in the cache or the list of buckets has not been fecthed yet,
+        // don't update the cache.
+        return undefined;
+      }
+
+      const newBuckets = [...(previousData?.buckets ?? [])];
+
+      newBuckets[indexOfBucket] = bucket;
+
+      return {
+        buckets: newBuckets,
+        errors: previousData?.errors ?? [],
+      };
+    }
+  );
+
+  return bucket;
 };
