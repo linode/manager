904bbdecc9f5d1d277d9628672c3bb725175209d
Alban Bailly <abailly@akamai.com>
2024-08-08 00:49:35 +0900

    Small cleanup

--- packages/manager/src/utilities/rootManager.ts
+++ packages/manager/src/utilities/rootManager.ts
@@ -4,6 +4,11 @@ import type { Root } from 'react-dom/client';
 
 export const rootInstances = new Map<HTMLElement, Root>();
 
+/**
+ * This utility helps manage React roots efficiently,
+ * ensuring that only one root is created per container and allowing reuse of existing roots when possible.
+ * It's particularly useful in scenarios where you need to dynamically create and manage multiple React roots in an application. (In our case, the APP and DevTools)
+ */
 export function handleRoot(container: HTMLElement): Root {
   let root = rootInstances.get(container);
 
