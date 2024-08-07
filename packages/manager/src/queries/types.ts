92028a1ceb0d76d8e14faff220ff75e985e2af3f
Banks Nussman <banks@nussman.us>
2024-08-07 12:08:32 -0400

    initial refactor

--- packages/manager/src/queries/types.ts
+++ packages/manager/src/queries/types.ts
@@ -1,40 +1,19 @@
-import { LinodeType, getLinodeTypes, getType } from '@linode/api-v4';
-import { APIError } from '@linode/api-v4/lib/types';
-import {
-  QueryClient,
-  UseQueryOptions,
-  useQueries,
-  useQuery,
-  useQueryClient,
-} from '@tanstack/react-query';
-
-import { getAll } from 'src/utilities/getAll';
+import { useQueries, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { queryPresets } from './base';
+import { linodeQueries } from './linodes/linodes';
 
-const queryKey = 'types';
+import type { APIError, LinodeType } from '@linode/api-v4';
+import type { UseQueryOptions } from '@tanstack/react-query';
 
-const getAllTypes = () =>
-  getAll(getLinodeTypes)().then((results) => results.data);
-
-const getSingleType = async (type: string, queryClient: QueryClient) => {
-  const allTypesCache = queryClient.getQueryData<LinodeType[]>(
-    allTypesQueryKey
-  );
-  return (
-    allTypesCache?.find((cachedType) => cachedType.id === type) ?? getType(type)
-  );
-};
-
-const allTypesQueryKey = [queryKey, 'all'];
 export const useAllTypes = (enabled = true) => {
-  return useQuery<LinodeType[], APIError[]>(allTypesQueryKey, getAllTypes, {
+  return useQuery<LinodeType[], APIError[]>({
+    ...linodeQueries.types._ctx.all,
     enabled,
     ...queryPresets.oneTimeFetch,
   });
 };
 
-const specificTypesQueryKey = (type: string) => [queryKey, 'detail', type];
 /**
  * Some Linodes may have types that aren't returned by the /types and /types-legacy endpoints. This
  * hook may be useful in fetching these "shadow plans".
@@ -43,16 +22,36 @@ const specificTypesQueryKey = (type: string) => [queryKey, 'detail', type];
  */
 export const useSpecificTypes = (types: string[], enabled = true) => {
   const queryClient = useQueryClient();
+
   return useQueries({
     queries: types.map<UseQueryOptions<LinodeType, APIError[]>>((type) => ({
-      enabled: Boolean(type) && enabled,
-      queryFn: () => getSingleType(type, queryClient),
-      queryKey: specificTypesQueryKey(type),
+      enabled,
+      ...linodeQueries.types._ctx.type(type),
       ...queryPresets.oneTimeFetch,
+      initialData() {
+        const allTypesFromCache = queryClient.getQueryData<LinodeType[]>(
+          linodeQueries.types._ctx.all.queryKey
+        );
+
+        return allTypesFromCache?.find((t) => t.id === type);
+      },
     })),
   });
 };
 
 export const useTypeQuery = (type: string, enabled = true) => {
-  return useSpecificTypes([type], enabled)[0];
+  const queryClient = useQueryClient();
+
+  return useQuery<LinodeType, APIError[]>({
+    ...linodeQueries.types._ctx.type(type),
+    ...queryPresets.oneTimeFetch,
+    enabled,
+    initialData() {
+      const allTypesFromCache = queryClient.getQueryData<LinodeType[]>(
+        linodeQueries.types._ctx.all.queryKey
+      );
+
+      return allTypesFromCache?.find((t) => t.id === type);
+    },
+  });
 };
