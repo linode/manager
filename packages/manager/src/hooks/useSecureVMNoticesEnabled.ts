01ef5c4627e8c6b1fc08da06721321f04a49d7d0
Hussain Khalil <hkhalil@akamai.com>
2024-08-08 12:15:10 -0400

    Disable notices if feature flag turned off

--- packages/manager/src/hooks/useSecureVMNoticesEnabled.ts
+++ packages/manager/src/hooks/useSecureVMNoticesEnabled.ts
@@ -1,25 +1,33 @@
 import { usePreferences } from 'src/queries/profile/preferences';
 
+import { useFlags } from './useFlags';
 import { useIsAkamaiAccount } from './useIsAkamaiAccount';
 
+import type { FlagSet } from 'src/featureFlags';
 import type { ManagerPreferences } from 'src/types/ManagerPreferences';
 
 export const useSecureVMNoticesEnabled = () => {
   const { data: preferences } = usePreferences();
   const { isAkamaiAccount: isInternalAccount } = useIsAkamaiAccount();
+  const flags = useFlags();
 
   return {
     secureVMNoticesEnabled: getSecureVMNoticesEnabled(
       preferences,
-      isInternalAccount
+      isInternalAccount,
+      flags
     ),
   };
 };
 
 const getSecureVMNoticesEnabled = (
-  preferences?: ManagerPreferences,
-  isInternalAccount?: boolean
+  preferences: ManagerPreferences | undefined,
+  isInternalAccount: boolean | undefined,
+  flags: FlagSet
 ) => {
+  if (Object.keys(flags.secureVmCopy ?? {}).length === 0) {
+    return false;
+  }
   switch (preferences?.secure_vm_notices) {
     case 'always':
       return true;
