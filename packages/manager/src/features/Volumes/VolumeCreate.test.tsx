fd00798c67720e3c01fb6c44da9dc232cf78aaae
Dajahi Wiley <dwiley@linode.com>
2024-08-09 16:16:51 -0400

    Feedback: unit test adjustments

--- packages/manager/src/features/Volumes/VolumeCreate.test.tsx
+++ packages/manager/src/features/Volumes/VolumeCreate.test.tsx
@@ -1,51 +1,60 @@
 import * as React from 'react';
 
+import { accountFactory } from 'src/factories';
+import { HttpResponse, http, server } from 'src/mocks/testServer';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
 import { VolumeCreate } from './VolumeCreate';
 
-const blockStorageEncryptionEnabledMock = vi.hoisted(() => {
-  return {
-    useIsBlockStorageEncryptionFeatureEnabled: vi.fn(),
-  };
-});
+const accountEndpoint = '*/v4/account';
+const encryptVolumeSectionHeader = 'Encrypt Volume';
 
 describe('VolumeCreate', () => {
-  // @TODO BSE: Remove feature flagging/conditionality once BSE is fully rolled out
-  vi.mock('src/components/Encryption/utils.ts', async () => {
-    const actual = await vi.importActual<any>(
-      'src/components/Encryption/utils.ts'
-    );
-    return {
-      ...actual,
-      __esModule: true,
-      useIsBlockStorageEncryptionFeatureEnabled: blockStorageEncryptionEnabledMock.useIsBlockStorageEncryptionFeatureEnabled.mockImplementation(
-        () => {
-          return {
-            isBlockStorageEncryptionFeatureEnabled: false, // indicates the feature flag is off or account capability is absent
-          };
-        }
-      ),
-    };
-  });
+  /* @TODO BSE: Remove feature flagging/conditionality once BSE is fully rolled out */
 
-  it('should not have a "Volume Encryption" section visible if the feature flag is off and user does not have capability', () => {
-    const { queryByText } = renderWithTheme(<VolumeCreate />);
-
-    expect(queryByText('Encrypt Volume')).not.toBeInTheDocument();
-  });
-
-  it('should have a "Volume Encryption" section visible if feature flag is on and user has the capability', () => {
-    blockStorageEncryptionEnabledMock.useIsBlockStorageEncryptionFeatureEnabled.mockImplementationOnce(
-      () => {
-        return {
-          isBlockStorageEncryptionFeatureEnabled: true,
-        };
-      }
+  it('should not have a "Volume Encryption" section visible if the user has the account capability but the feature flag is off', () => {
+    server.use(
+      http.get(accountEndpoint, () => {
+        return HttpResponse.json(
+          accountFactory.build({ capabilities: ['Block Storage Encryption'] })
+        );
+      })
     );
 
-    const { queryByText } = renderWithTheme(<VolumeCreate />);
+    const { queryByText } = renderWithTheme(<VolumeCreate />, {
+      flags: { blockStorageEncryption: false },
+    });
 
-    expect(queryByText('Encrypt Volume')).toBeInTheDocument();
+    expect(queryByText(encryptVolumeSectionHeader)).not.toBeInTheDocument();
+  });
+
+  it('should not have a "Volume Encryption" section visible if the user does not have the account capability but the feature flag is on', () => {
+    server.use(
+      http.get(accountEndpoint, () => {
+        return HttpResponse.json(accountFactory.build({ capabilities: [] }));
+      })
+    );
+
+    const { queryByText } = renderWithTheme(<VolumeCreate />, {
+      flags: { blockStorageEncryption: true },
+    });
+
+    expect(queryByText(encryptVolumeSectionHeader)).not.toBeInTheDocument();
+  });
+
+  it('should have a "Volume Encryption" section visible if feature flag is on and user has the capability', async () => {
+    server.use(
+      http.get(accountEndpoint, () => {
+        return HttpResponse.json(
+          accountFactory.build({ capabilities: ['Block Storage Encryption'] })
+        );
+      })
+    );
+
+    const { findByText } = renderWithTheme(<VolumeCreate />, {
+      flags: { blockStorageEncryption: true },
+    });
+
+    await findByText(encryptVolumeSectionHeader);
   });
 });
