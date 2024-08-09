23eaf5916be03f7f7b906da2581dc44dde21adc9
Purvesh Makode <pmakode@akamai.com>
2024-08-09 15:00:39 +0530

    fix: [M3-8409] - Community Notifications in Event Messages v2 Refactor (#10742)
    * Fix v2 community like notfications in event messages
    
    * Add type to ACTION_WITHOUT_USERNAMES
    
    * Added changeset: Community Like notfications in event messages v2
    
    * Update changeset file
    
    * Small clean up...
    
    * Add unit test cases for getEventUsername util
    
    * Exclude usernames from community_mention and community_question_reply events
    
    * Update changeset file
    
    * Update community_question_reply event message

--- packages/manager/src/features/Events/Event.helpers.ts
+++ packages/manager/src/features/Events/Event.helpers.ts
@@ -9,7 +9,7 @@ export const maybeRemoveTrailingPeriod = (string: string) => {
   return string;
 };
 
-const ACTIONS_WITHOUT_USERNAMES = [
+export const ACTIONS_WITHOUT_USERNAMES: EventAction[] = [
   'entity_transfer_accept',
   'entity_transfer_accept_recipient',
   'entity_transfer_cancel',
@@ -18,6 +18,8 @@ const ACTIONS_WITHOUT_USERNAMES = [
   'entity_transfer_stale',
   'lassie_reboot',
   'community_like',
+  'community_mention',
+  'community_question_reply',
 ];
 
 export const formatEventWithUsername = (
