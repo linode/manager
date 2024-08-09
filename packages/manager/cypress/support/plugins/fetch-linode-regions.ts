2397b25738f15b899d135ec8c688d5a61073e443
Joe D'Amore <jdamore@linode.com>
2024-08-09 13:19:34 -0400

    Fix Cypress config issue related to import aliases

--- packages/manager/cypress/support/plugins/fetch-linode-regions.ts
+++ packages/manager/cypress/support/plugins/fetch-linode-regions.ts
@@ -1,9 +1,27 @@
 import { getRegions } from '@linode/api-v4';
-import { extendRegion } from 'support/util/regions';
 
 import type { CypressPlugin } from './plugin';
 import type { ResourcePage, Region } from '@linode/api-v4';
-import type { ExtendedRegion } from 'support/util/regions';
+
+// TODO Clean up.
+/**
+ * Returns Region label formatted for Cloud Manager UI.
+ *
+ * This is a re-implementation of a similar util in the Cloud Manager code base.
+ * Because it is needed by our Cypress config, which does not go through Vite, we
+ * have to re-implement this util rather than import it because Cloud Manager
+ * source code cannot be imported without Vite.
+ *
+ * @see {@link src/components/RegionSelect/RegionSelect.utils}
+ */
+export const getCloudManagerLabel = (region: Region) => {
+  const [city] = region.label.split(', ');
+  // Include state for the US
+  if (region.country === 'us') {
+    return `${region.country.toUpperCase()}, ${region.label}`;
+  }
+  return `${region.country.toUpperCase()}, ${city}`;
+};
 
 /**
  * Fetches Linode regions and stores data in Cypress `cloudManagerRegions` env.
@@ -13,9 +31,13 @@ import type { ExtendedRegion } from 'support/util/regions';
 export const fetchLinodeRegions: CypressPlugin = async (on, config) => {
   const regions: ResourcePage<Region> = await getRegions({ page_size: 500 });
 
-  const extendedRegions: ExtendedRegion[] = regions.data.map(
-    (apiRegion: Region): ExtendedRegion => extendRegion(apiRegion)
-  );
+  const extendedRegions = regions.data.map((apiRegion: Region) => {
+    return {
+      ...apiRegion,
+      label: getCloudManagerLabel(apiRegion),
+      apiLabel: apiRegion.label,
+    };
+  });
 
   return {
     ...config,
