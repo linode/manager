956b1f0a834ef1ca81bb0e7613136ddd2804ab8c
Joe D'Amore <jdamore@linode.com>
2024-08-12 15:04:48 -0400

    Clean up run splitting / test weight logic

--- packages/manager/cypress/support/plugins/split-run.ts
+++ packages/manager/cypress/support/plugins/split-run.ts
@@ -8,6 +8,112 @@ import { readFileSync } from 'fs';
 import { SpecWeight, SpecWeights, specWeightsSchema } from './generate-weights';
 import type { CypressPlugin } from './plugin';
 
+/**
+ * Describes weighted specs for a single test runner.
+ */
+interface WeightedRunnerSpecs {
+  /** Array of spec filepaths for runner. */
+  specs: string[];
+
+  /** Total test weight of specs in `specs`. */
+  weight: number;
+}
+
+/**
+ * Reads a test weights file at the given path and returns its data.
+ *
+ * Weights data is sorted from highest to lowest weight.
+ *
+ * @param weightsFilepath - Path to weights file.
+ *
+ * @throws If `weightsFilepath` does not exist or is not readable.
+ * @throws If weights data is invalid.
+ *
+ * @returns Spec weights data.
+ */
+const readTestWeightsFile = (weightsFilepath: string): SpecWeights => {
+  const weightsContents = readFileSync(resolve(weightsFilepath), 'utf-8');
+  const weightsData = JSON.parse(weightsContents) as SpecWeights;
+  specWeightsSchema.validateSync(weightsData);
+
+  // Sort spec weights from highest weight to lowest.
+  weightsData.weights.sort((a, b) => b.weight - a.weight);
+
+  return weightsData;
+};
+
+/**
+ * Returns an array of `SpecWeight` objects for each spec file with corresponding weight data.
+ *
+ * @param allSpecs - String of spec filepaths for this run.
+ * @param specWeights - Spec weights data.
+ *
+ * @returns Array of `SpecWeight` objects for each spec file that has weight data.
+ */
+const getWeightedSpecs = (
+  allSpecs: string[],
+  specWeights: SpecWeights
+): SpecWeight[] => {
+  return allSpecs
+    .map((specPath: string): SpecWeight | undefined => {
+      return specWeights.weights.find(
+        (specWeight) => specWeight.filepath === specPath
+      );
+    })
+    .filter((specWeight): specWeight is SpecWeight => !!specWeight);
+};
+
+/**
+ * Returns an array of spec filepaths for each spec file that does not have weight data.
+ *
+ * @param allSpecs - String of spec filepaths for this run.
+ * @param specWeights - Spec weights data.
+ *
+ * @returns Array of spec filepaths for each spec file that does not have corresponding weight data.
+ */
+const getUnweightedSpecs = (
+  allSpecs: string[],
+  specWeights: SpecWeights
+): string[] => {
+  return allSpecs.filter((specPath: string) => {
+    return !specWeights.weights.find(
+      (specWeight) => specWeight.filepath === specPath
+    );
+  });
+};
+
+/**
+ * Returns weighted specs for a single runner in a split run.
+ *
+ * @param runnerIndex - Index of the runner for which to retrieve specs.
+ * @param totalRunners - Total number of runners.
+ * @param weightedSpecs - Weighted spec data from which to retrieve specs.
+ *
+ * @returns Weighted specs for runner with index `runnerIndex`.
+ */
+const getWeightedRunnerSpecs = (
+  runnerIndex: number,
+  totalRunners: number,
+  weightedSpecs: SpecWeight[]
+): WeightedRunnerSpecs => {
+  const weightSimulationResults: {
+    specs: string[];
+    weight: number;
+  }[] = Array.from({ length: totalRunners }, () => ({
+    specs: [],
+    weight: 0,
+  }));
+
+  weightedSpecs.forEach((weightedSpec) => {
+    // Ensure lowest weighed runner is at index 0.
+    weightSimulationResults.sort((a, b) => a.weight - b.weight);
+    weightSimulationResults[0].specs.push(weightedSpec.filepath);
+    weightSimulationResults[0].weight += weightedSpec.weight;
+  });
+
+  return weightSimulationResults[runnerIndex - 1];
+};
+
 export const splitCypressRun: CypressPlugin = (_on, config) => {
   const {
     CY_TEST_SPLIT_RUN: splitRunEnabled,
@@ -41,51 +147,20 @@ export const splitCypressRun: CypressPlugin = (_on, config) => {
   // Override configuration spec pattern to reflect test subset for this runner...
   const specs = globSync(config.specPattern);
 
-  // Information about test weights to be included in the info table, if applicable.
-  let weightsInfo: Record<string, string | number> = {
-    'Test Weights': '(Unavailable)',
-  };
+  let totalWeight = 0;
+  let weightedSpecs: SpecWeight[] = [];
+  let unweightedSpecs: string[] = [...specs];
 
-  let weighedSpecs: SpecWeight[] = [];
-  let unweighedSpecs: string[] = [...specs];
-
-  // Read spec weights file, if it's available, and identify weighed and unweighed specs.
+  // If spec weights file path is specified, attempt to read its contents.
+  // If weights file does not exist, is inaccessible, or is malformed, weights
+  // data will be discarded and run splitting will fall back on round-robin
+  // distribution method.
   if (splitRunWeightsPath) {
     try {
-      const weightsContents = readFileSync(
-        resolve(splitRunWeightsPath),
-        'utf-8'
-      );
-      const weightsData = JSON.parse(weightsContents) as SpecWeights;
-      specWeightsSchema.validateSync(weightsData);
-      const specWeights = weightsData.weights.sort(
-        (a, b) => b.weight - a.weight
-      );
-
-      // Get an array of `SpecWeight`s for each actual spec that has a corresponding weight entry.
-      weighedSpecs = specs
-        .map((specPath: string): SpecWeight | undefined => {
-          return specWeights.find(
-            (specWeight) => specWeight.filepath === specPath
-          );
-        })
-        .filter((specWeight): specWeight is SpecWeight => !!specWeight);
-
-      // Get an array of relative paths to specs that do not have weight data available.
-      unweighedSpecs = specs.filter((specPath: string) => {
-        return !specWeights.find(
-          (specWeight) => specWeight.filepath === specPath
-        );
-      });
-
-      // Reassign the weights info object to later output to the console.
-      weightsInfo = {
-        'Test Weights': splitRunWeightsPath,
-        'Total Test Weight': weightsData.meta.totalWeight,
-        'Runner Test Weight': 0,
-        'Weighed Specs': weighedSpecs.length,
-        'Unweighed Specs': unweighedSpecs.length,
-      };
+      const specWeights = readTestWeightsFile(splitRunWeightsPath);
+      weightedSpecs = getWeightedSpecs(specs, specWeights);
+      unweightedSpecs = getUnweightedSpecs(specs, specWeights);
+      totalWeight = specWeights.meta.totalWeight;
     } catch (err) {
       // Swallow error here; it's OK if test weights file doesn't exist / can't be read.
       // Wrap messages in IIFEs to avoid issue where info messages get printed first.
@@ -106,35 +181,17 @@ export const splitCypressRun: CypressPlugin = (_on, config) => {
     }
   }
 
-  // Create an array containing objects to track specs and weight for each runner.
-  const weightSimulationResults: {
-    specs: string[];
-    weight: number;
-  }[] = Array.from({ length: totalRunners }, () => ({
-    specs: [],
-    weight: 0,
-  }));
+  // Distribute specs based on their weights and get an array of weighted specs
+  // for this runner.
+  const weightedSpecsForRunner = getWeightedRunnerSpecs(
+    runner,
+    totalRunners,
+    weightedSpecs
+  );
 
-  weighedSpecs.forEach((weighedSpec) => {
-    // Ensure lowest weighed runner is at index 0.
-    weightSimulationResults.sort((a, b) => a.weight - b.weight);
-    weightSimulationResults[0].specs.push(weighedSpec.filepath);
-    weightSimulationResults[0].weight += weighedSpec.weight;
-  });
-
-  // Calculate this runner's total test weight.
-  // Wrapped in an if to prevent this from appearing when weights are unavailable.
-  if (weightSimulationResults[splitRunRunnerIndex - 1]?.weight) {
-    const weight =
-      Math.round(
-        weightSimulationResults[splitRunRunnerIndex - 1].weight * 100
-      ) / 100;
-    weightsInfo['Runner Test Weight'] = `${weight}%`;
-  }
-
-  // Distribute remaining unweighed specs, if applicable.
+  // Distribute remaining unweighted specs round-robin style, if applicable.
   // Sort spec filenames as deterministically as we easily can.
-  unweighedSpecs.sort((a: string, b: string): number => {
+  unweightedSpecs.sort((a: string, b: string): number => {
     if (a.toLowerCase() < b.toLowerCase()) {
       return -1;
     } else if (a.toLowerCase() > b.toLowerCase()) {
@@ -144,21 +201,43 @@ export const splitCypressRun: CypressPlugin = (_on, config) => {
   });
 
   config.specPattern = [
-    // Only include the weighed specs at index N, where N is this runner's index - 1.
-    ...weightSimulationResults[splitRunRunnerIndex - 1].specs,
+    // Include weighted specs first.
+    ...weightedSpecsForRunner.specs,
 
-    // Only include every Nth spec, where N is the total number of runners.
-    ...unweighedSpecs.filter((_spec: string, index: number) => {
+    // If there are any unweighted specs remaining, only include every Nth
+    // spec, where N is the total number of runners.
+    ...unweightedSpecs.filter((_spec: string, index: number) => {
       return (index + runner - 1) % totalRunners === 0;
     }),
   ];
 
-  console.info('Cypress split running is enabled.');
-  console.table({
+  const splitRunInfo = {
     '# of Specs Total': specs.length,
     '# of Specs for This Run': config.specPattern.length,
     Runner: runner,
     'Total Runners': totalRunners,
+  };
+
+  const weightsInfo = (() => {
+    if (weightedSpecs.length < 1) {
+      return {
+        'Test Weights': 'Unavailable',
+      };
+    }
+    return {
+      'Test Weights': splitRunWeightsPath,
+      'Total Test Weight': `${Math.round(totalWeight * 100) / 100}%`,
+      'Runner Test Weight': `${
+        Math.round(weightedSpecsForRunner.weight * 100) / 100
+      }%`,
+      'Weighted Specs': weightedSpecs.length,
+      'Unweighted Specs': unweightedSpecs.length,
+    };
+  })();
+
+  console.info('Cypress split running is enabled.');
+  console.table({
+    ...splitRunInfo,
     ...weightsInfo,
   });
 
