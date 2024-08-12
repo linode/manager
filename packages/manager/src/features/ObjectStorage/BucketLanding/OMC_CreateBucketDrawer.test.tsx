c346ea263f37268d7e182cfea359f2403fd439de
Connie Liu <coliu@akamai.com>
2024-08-12 10:57:49 -0400

    starting unit tests for obj storage

---
+++ packages/manager/src/features/ObjectStorage/BucketLanding/OMC_CreateBucketDrawer.test.tsx
@@ -0,0 +1,91 @@
+import { fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as React from 'react';
+
+import { objectStorageEndpointsFactory } from 'src/factories';
+import { makeResourcePage } from 'src/mocks/serverHandlers';
+import { HttpResponse, http, server } from 'src/mocks/testServer';
+import {
+  mockMatchMedia,
+  renderWithThemeAndHookFormContext,
+} from 'src/utilities/testHelpers';
+
+import { OMC_CreateBucketDrawer } from './OMC_CreateBucketDrawer';
+
+beforeAll(() => mockMatchMedia());
+
+const props = {
+  isOpen: true,
+  onClose: vi.fn(),
+};
+
+describe('OMC_CreateBucketDrawer', () => {
+  it('should render the drawer', () => {
+    const {
+      getByTestId,
+      getByText,
+      queryByText,
+    } = renderWithThemeAndHookFormContext({
+      component: <OMC_CreateBucketDrawer {...props} />,
+      options: {
+        flags: {
+          objMultiCluster: true,
+          objectStorageGen2: { enabled: true },
+        },
+      },
+    });
+
+    const header = getByTestId('drawer-title');
+    expect(header).toBeVisible();
+    const label = getByText('Label');
+    expect(label).toBeVisible();
+    const region = getByText('Region');
+    expect(region).toBeVisible();
+    const cancelButton = getByText('Cancel');
+    expect(cancelButton).toBeVisible();
+    const createButton = getByTestId('create-bucket-button');
+    expect(createButton).toBeVisible();
+    const endpointType = queryByText('Object Storage Endpoint Type');
+    expect(endpointType).not.toBeInTheDocument();
+  });
+
+  it('should display the endpoint selector if endpoints exist', async () => {
+    server.use(
+      http.get('*/v4/object-storage/endpoints', () => {
+        return HttpResponse.json(
+          makeResourcePage([
+            objectStorageEndpointsFactory.build({
+              endpoint_type: 'E0',
+              region: 'us-sea',
+              s3_endpoint: null,
+            }),
+          ])
+        );
+      })
+    );
+
+    const { findByText } = renderWithThemeAndHookFormContext({
+      component: <OMC_CreateBucketDrawer {...props} />,
+      options: {
+        flags: {
+          objMultiCluster: true,
+          objectStorageGen2: { enabled: true },
+        },
+      },
+    });
+
+    const endpointType = await findByText('Object Storage Endpoint Type');
+    expect(endpointType).toBeVisible();
+  });
+
+  it('should close the drawer', () => {
+    const { getByText } = renderWithThemeAndHookFormContext({
+      component: <OMC_CreateBucketDrawer {...props} />,
+    });
+
+    const cancelButton = getByText('Cancel');
+    expect(cancelButton).toBeVisible();
+    fireEvent.click(cancelButton);
+    expect(props.onClose).toHaveBeenCalled();
+  });
+});
