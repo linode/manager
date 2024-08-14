0cdd3906621dba309f92433ffee368620ece89d5
pmakode-akamai <pmakode@akamai.com>
2024-08-14 21:39:20 +0530

    Add more stories for CheckoutBar

--- packages/manager/src/components/CheckoutBar/CheckoutBar.stories.tsx
+++ packages/manager/src/components/CheckoutBar/CheckoutBar.stories.tsx
@@ -4,19 +4,54 @@ import { CheckoutBar } from './CheckoutBar';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
+const meta: Meta<typeof CheckoutBar> = {
+  component: CheckoutBar,
+  title: 'Components/CheckoutBar',
+};
+
 type Story = StoryObj<typeof CheckoutBar>;
 
 export const Default: Story = {
   args: {
+    calculatedPrice: 30.0,
+    children: (
+      <div>
+        <i style={{ color: 'white' }}>Children items can go here!</i>
+      </div>
+    ),
     heading: 'Checkout',
     onDeploy: () => alert('Deploy clicked'),
+    submitText: 'Submit',
   },
   render: (args) => <CheckoutBar {...args} />,
 };
 
-const meta: Meta<typeof CheckoutBar> = {
-  component: CheckoutBar,
-  title: 'Components/CheckoutBar',
+export const WithAgreement: Story = {
+  args: {
+    ...Default.args,
+    agreement: (
+      <div>
+        <i style={{ color: 'white' }}>Agreement item can go here</i>
+      </div>
+    ),
+  },
+  render: (args) => <CheckoutBar {...args} />,
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+  render: (args) => <CheckoutBar {...args} />,
+};
+
+export const Loading: Story = {
+  args: {
+    ...Default.args,
+    isMakingRequest: true,
+  },
+  render: (args) => <CheckoutBar {...args} />,
 };
 
 export default meta;
