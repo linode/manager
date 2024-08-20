9402a55318be80e78d732c6a7ab0384ee1b0acd4
nikhagra <nagrawal@akamai.com>
2024-08-20 19:41:27 +0530

    upcoming: [DI-20284] - Added support for multiple services in the dashboards list

--- packages/manager/src/features/CloudPulse/shared/CloudPulseDashboardSelect.tsx
+++ packages/manager/src/features/CloudPulse/shared/CloudPulseDashboardSelect.tsx
@@ -4,12 +4,14 @@ import { Autocomplete } from 'src/components/Autocomplete/Autocomplete';
 import { Box } from 'src/components/Box';
 import { Typography } from 'src/components/Typography';
 import { useCloudPulseDashboardsQuery } from 'src/queries/cloudpulse/dashboards';
+import { useCloudPulseServiceTypes } from 'src/queries/cloudpulse/services';
 
 import { DASHBOARD_ID, REGION, RESOURCES } from '../Utils/constants';
 import {
   getUserPreferenceObject,
   updateGlobalFilterPreference,
 } from '../Utils/UserPreference';
+import { formattedServiceTypes, getAllDashboards } from '../Utils/utils';
 
 import type { Dashboard } from '@linode/api-v4';
 
@@ -23,35 +25,57 @@ export interface CloudPulseDashboardSelectProps {
 export const CloudPulseDashboardSelect = React.memo(
   (props: CloudPulseDashboardSelectProps) => {
     const {
-      data: dashboardsList,
-      error,
-      isLoading,
-    } = useCloudPulseDashboardsQuery(true); // Fetch the list of dashboards
+      data: serviceTypesList,
+      error: serviceTypesError,
+      isLoading: serviceTypesLoading,
+    } = useCloudPulseServiceTypes(true);
 
+    const serviceTypes: string[] = formattedServiceTypes(serviceTypesList);
+
+    const {
+      data: dashboardsList,
+      error: dashboardsError,
+      isLoading: dashboardsLoading,
+    } = getAllDashboards(
+      useCloudPulseDashboardsQuery(serviceTypes),
+      serviceTypes
+    );
     const [
       selectedDashboard,
       setSelectedDashboard,
     ] = React.useState<Dashboard>();
 
-    const errorText: string = error ? 'Error loading dashboards' : '';
+    const getErrorText = () => {
+      if (serviceTypesError) {
+        return 'Unable to load service types';
+      }
+
+      if (dashboardsError.length > 0) {
+        return `Unable to load ${dashboardsError.slice(0, -1)}`;
+      }
+
+      return '';
+    };
+
+    const errorText: string = getErrorText();
 
     const placeHolder = 'Select a Dashboard';
 
     // sorts dashboards by service type. Required due to unexpected autocomplete grouping behaviour
-    const getSortedDashboardsList = (options: Dashboard[]) => {
+    const getSortedDashboardsList = (options: Dashboard[]): Dashboard[] => {
       return options.sort(
         (a, b) => -b.service_type.localeCompare(a.service_type)
       );
     };
-
     // Once the data is loaded, set the state variable with value stored in preferences
     React.useEffect(() => {
-      if (dashboardsList) {
+      // only call this code when the component is rendered initially
+      if (dashboardsList.length > 0 && selectedDashboard === undefined) {
         const dashboardId = getUserPreferenceObject()?.dashboardId;
 
         if (dashboardId) {
-          const dashboard = dashboardsList.data.find(
-            (obj) => obj.id === dashboardId
+          const dashboard = dashboardsList.find(
+            (obj: Dashboard) => obj.id === dashboardId
           );
           setSelectedDashboard(dashboard);
           props.handleDashboardChange(dashboard, true);
@@ -61,7 +85,6 @@ export const CloudPulseDashboardSelect = React.memo(
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [dashboardsList]);
-
     return (
       <Autocomplete
         onChange={(_: any, dashboard: Dashboard) => {
@@ -96,8 +119,8 @@ export const CloudPulseDashboardSelect = React.memo(
         groupBy={(option: Dashboard) => option.service_type}
         isOptionEqualToValue={(option, value) => option.id === value.id}
         label="Select a Dashboard"
-        loading={isLoading}
-        options={getSortedDashboardsList(dashboardsList?.data ?? [])}
+        loading={dashboardsLoading || serviceTypesLoading}
+        options={getSortedDashboardsList(dashboardsList ?? [])}
         placeholder={placeHolder}
         value={selectedDashboard ?? null} // Undefined is not allowed for uncontrolled component
       />
--- packages/manager/src/mocks/serverHandlers.ts
+++ packages/manager/src/mocks/serverHandlers.ts
@@ -2267,7 +2267,14 @@ export const handlers = [
 
     return HttpResponse.json(response);
   }),
-  http.get('*/v4/monitor/services/linode/dashboards', () => {
+  http.get('*/v4/monitor/services', () => {
+    const response = {
+      data: [{ service_type: 'linode' }],
+    };
+
+    return HttpResponse.json(response);
+  }),
+  http.get('*/v4/monitor/services/:serviceType/dashboards', () => {
     const response = {
       data: [
         {
