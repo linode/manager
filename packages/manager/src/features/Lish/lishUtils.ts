8802f02cacc3163efd4a1cafff2ab2c1e32b2e54
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2024-08-20 13:18:47 -0400

    change: [M3-8456] - Update LISH experience based on feedback (#10789)
    * revert lish changes based on feedback
    
    * add chanegsets
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

--- packages/manager/src/features/Lish/lishUtils.ts
+++ packages/manager/src/features/Lish/lishUtils.ts
@@ -1,6 +1,7 @@
 export const lishLaunch = (linodeId: number) => {
   window.open(
     `${window.location.protocol}//${window.location.host}/linodes/${linodeId}/lish/weblish`,
-    `weblish_con_${linodeId}`
+    `weblish_con_${linodeId}`,
+    'width=1080,height=730,toolbar=0,resizable=1'
   );
 };
