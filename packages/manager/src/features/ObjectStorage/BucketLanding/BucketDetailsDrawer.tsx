a17d04b2602c2892871102a828714c6897739ffd
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2024-08-21 15:29:14 -0400

    Update packages/manager/src/features/ObjectStorage/BucketLanding/BucketDetailsDrawer.tsx
    Co-authored-by: Connie Liu <139280159+coliu-akamai@users.noreply.github.com>

--- packages/manager/src/features/ObjectStorage/BucketLanding/BucketDetailsDrawer.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/BucketDetailsDrawer.tsx
@@ -115,8 +115,9 @@ export const BucketDetailsDrawer = React.memo(
             <StyledCopyTooltip sx={{ marginLeft: 4 }} text={hostname} />
           </StyledLinkContainer>
         )}
-        {formattedCreated ||
-          (cluster && <Divider spacingBottom={16} spacingTop={16} />)}
+        {(formattedCreated || cluster) && (
+          <Divider spacingBottom={16} spacingTop={16} />
+        )}
         {typeof size === 'number' && (
           <Typography variant="subtitle2">
             {readableBytes(size).formatted}
