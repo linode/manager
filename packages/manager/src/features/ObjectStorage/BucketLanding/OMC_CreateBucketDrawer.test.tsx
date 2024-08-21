ef80abf4285396b36026522edd48446ee1449c9d
Connie Liu <coliu@akamai.com>
2024-08-21 09:31:08 -0400

    reset mocks before tests

--- packages/manager/src/features/ObjectStorage/BucketLanding/OMC_CreateBucketDrawer.test.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/OMC_CreateBucketDrawer.test.tsx
@@ -14,6 +14,10 @@ const props = {
 };
 
 describe('OMC_CreateBucketDrawer', () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
   it('should render the drawer', () => {
     const {
       getByTestId,
