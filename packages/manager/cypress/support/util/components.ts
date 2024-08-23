4195ff6e3d0637578bd1823069c52629512c980f
Joe D'Amore <jdamore@linode.com>
2024-08-23 18:02:14 -0400

    Clean up, docs

--- packages/manager/cypress/support/util/components.ts
+++ packages/manager/cypress/support/util/components.ts
@@ -25,54 +25,15 @@ const capitalize = (uncapitalizedString: string): string => {
 };
 
 /**
+ * Describes a Cypress command that can be used to mount a component.
  *
+ * @param jsx - React node to mount.
+ * @param flags - Optional feature flags to apply.
  */
 export type MountCommand = (
   jsx: React.ReactNode,
   flags?: any
 ) => Cypress.Chainable<MountReturn>;
-
-/**
- * Describes a group of tests for a component.
- *
- * Additionally parameterizes a test callback for the component against each
- * Cloud Manager theme.
- *
- * @param componentName - Name of component to test.
- * @param callback - Test context callback.
- *
- * @example
- * // Using `describeComponent`...
- * describeComponent('BetaChip', (theme) => {
- *   it('renders text', () => {
- *     cy.mountWithTheme(<BetaChip />, theme);
- *     // Assertions here...
- *   });
- * });
- * // Is equivalent to...
- * describe('BetaChip component (light theme)', () => {
- *   it('renders text', () => {
- *     cy.mountWithTheme(<BetaChip />, 'light');
- *     // Assertions here...
- *   });
- * });
- * describe('BetaChip component (dark theme)', () => {
- *   it('renders text', () => {
- *     cy.mountWithTheme(<BetaChip />, 'dark');
- *     // Assertions here...
- *   })
- * })
- */
-// export const describeComponent = (
-//   componentName: string,
-//   callback: (themeName: ThemeName) => void
-// ) => {
-//   componentThemes.forEach((themeName: ThemeName) => {
-//     describe(`${componentName} component (${themeName} theme)`, () => {
-//       callback(themeName);
-//     });
-//   });
-// };
 
 /**
  * Describes a group of tests for a component.
