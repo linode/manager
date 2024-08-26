301a075fb7760f506d5dd41a8b02891dce3561ef
Hussain Khalil <hkhalil@akamai.com>
2024-08-26 15:57:49 -0400

    Remove redundant `useEffect`

--- packages/manager/src/features/Linodes/LinodeCreatev2/FirewallAuthorization.tsx
+++ packages/manager/src/features/Linodes/LinodeCreatev2/FirewallAuthorization.tsx
@@ -1,33 +1,23 @@
 import { Typography } from '@mui/material';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 
 import { AkamaiBanner } from 'src/components/AkamaiBanner/AkamaiBanner';
 import { Checkbox } from 'src/components/Checkbox';
 import { FormControlLabel } from 'src/components/FormControlLabel';
 import { useFlags } from 'src/hooks/useFlags';
-import { isNotNullOrUndefined } from 'src/utilities/nullOrUndefined';
 
 import type { LinodeCreateFormValues } from './utilities';
 
 export const FirewallAuthorization = () => {
   const flags = useFlags();
-  const {
-    clearErrors,
-    control,
-    watch,
-  } = useFormContext<LinodeCreateFormValues>();
+  const { control, watch } = useFormContext<LinodeCreateFormValues>();
   const { field, fieldState } = useController({
     control,
     name: 'firewallOverride',
   });
 
   const watchFirewall = watch('firewall_id');
-  useEffect(() => {
-    if (isNotNullOrUndefined(watchFirewall)) {
-      clearErrors('firewallOverride');
-    }
-  }, [clearErrors, watchFirewall]);
 
   if (
     watchFirewall !== undefined ||
