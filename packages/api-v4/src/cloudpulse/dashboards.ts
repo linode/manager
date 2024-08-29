a7ce52f5ab76b6ecac38fbe61d5ea87906138dd4
nikhagra <nagrawal@akamai.com>
2024-08-29 19:40:42 +0530

    upcoming: [DI-20360] - remove sensitive information

--- packages/api-v4/src/cloudpulse/dashboards.ts
+++ packages/api-v4/src/cloudpulse/dashboards.ts
@@ -1,5 +1,5 @@
 import { ResourcePage } from 'src/types';
-import Request, { setHeaders, setMethod, setURL } from '../request';
+import Request, { setMethod, setURL } from '../request';
 import { Dashboard } from './types';
 import { API_ROOT } from 'src/constants';
 
@@ -17,8 +17,5 @@ export const getDashboards = (serviceType: string) =>
 export const getDashboardById = (dashboardId: number) =>
   Request<Dashboard>(
     setURL(`${API_ROOT}/monitor/dashboards/${encodeURIComponent(dashboardId)}`),
-    setMethod('GET'),
-    setHeaders({
-      Authorization: 'Bearer vagrant',
-    })
+    setMethod('GET')
   );
