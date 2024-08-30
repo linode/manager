18cd81fb9cdeef7c1b43d697e0eab15e4101744c
Banks Nussman <banks@nussman.us>
2024-08-30 10:28:25 -0400

    add some more unit testing

--- packages/manager/src/features/Users/UserProfile/UserDetailsPanel.test.tsx
+++ packages/manager/src/features/Users/UserProfile/UserDetailsPanel.test.tsx
@@ -6,12 +6,49 @@ import { renderWithTheme } from 'src/utilities/testHelpers';
 import { UserDetailsPanel } from './UserDetailsPanel';
 
 describe('UserDetailsPanel', () => {
-  it('shows username and email', async () => {
+  it("renders the user's username and email", async () => {
     const user = accountUserFactory.build();
 
-    const { findByText } = renderWithTheme(<UserDetailsPanel user={user} />);
+    const { getByText } = renderWithTheme(<UserDetailsPanel user={user} />);
 
-    await findByText(user.username);
-    await findByText(user.email);
+    expect(getByText('Username')).toBeVisible();
+    expect(getByText(user.username)).toBeVisible();
+
+    expect(getByText('Email')).toBeVisible();
+    expect(getByText(user.email)).toBeVisible();
+  });
+  it("renders 'limited' if the user is restricted", async () => {
+    const user = accountUserFactory.build({ restricted: true });
+
+    const { getByText } = renderWithTheme(<UserDetailsPanel user={user} />);
+
+    expect(getByText('Account Access')).toBeVisible();
+    expect(getByText('Limited')).toBeVisible();
+  });
+  it("renders 'full' if the user is unrestricted", async () => {
+    const user = accountUserFactory.build({ restricted: false });
+
+    const { getByText } = renderWithTheme(<UserDetailsPanel user={user} />);
+
+    expect(getByText('Account Access')).toBeVisible();
+    expect(getByText('Full')).toBeVisible();
+  });
+  it("renders the user's phone number", async () => {
+    const user = accountUserFactory.build({
+      verified_phone_number: '+17040000000',
+    });
+
+    const { getByText } = renderWithTheme(<UserDetailsPanel user={user} />);
+
+    expect(getByText('Verified Phone Number')).toBeVisible();
+    expect(getByText(user.verified_phone_number!)).toBeVisible();
+  });
+  it("renders the user's 2FA status", async () => {
+    const user = accountUserFactory.build({ tfa_enabled: true });
+
+    const { getByText } = renderWithTheme(<UserDetailsPanel user={user} />);
+
+    expect(getByText('2FA')).toBeVisible();
+    expect(getByText('Enabled')).toBeVisible();
   });
 });
