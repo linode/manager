e7d17b156adb2543c72b5da9d9abdd294c8fb729
jdamore-linode <97627410+jdamore-linode@users.noreply.github.com>
2024-09-03 14:35:54 -0400

    test: [M3-5858] - Cypress component tests (#10134)
    * Upgrade cypress-axe
    
    * Add initial component testing configuration and setup
    
    * Add a11y and component testing utils
    
    * Allow feature flags to be overridden via `cy.mountWithTheme`
    
    * Add simple BetaChip POC component tests
    
    * Add RegionSelect POC component test
    
    * Add "cy:component" command to root package.json

---
+++ packages/manager/cypress/component/poc/beta-chip.spec.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { BetaChip } from 'src/components/BetaChip/BetaChip';
+import { componentTests, visualTests } from 'support/util/components';
+import { checkComponentA11y } from 'support/util/accessibility';
+
+componentTests('BetaChip', () => {
+  visualTests((mount) => {
+    it('renders "BETA" text indicator with primary color', () => {
+      mount(<BetaChip color="primary" />);
+      cy.findByText('beta').should('be.visible');
+    });
+
+    it('renders "BETA" text indicator with default color', () => {
+      mount(<BetaChip color="default" />);
+      cy.findByText('beta').should('be.visible');
+    });
+
+    it('passes aXe check with primary color', () => {
+      mount(<BetaChip color="primary" />);
+      checkComponentA11y();
+    });
+
+    it('passes aXe check with default color', () => {
+      mount(<BetaChip color="default" />);
+      checkComponentA11y();
+    });
+  });
+});
