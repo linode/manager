fffe240a830e97106b802fc704b5beade26e4f06
pmakode-akamai <pmakode@akamai.com>
2024-09-05 12:44:45 +0530

    Add unit tests for SelectableTableRow component

---
+++ packages/manager/src/components/SelectableTableRow/SelectableTableRow.test.tsx
@@ -0,0 +1,80 @@
+import { fireEvent } from '@testing-library/react';
+import * as React from 'react';
+
+import { renderWithTheme } from 'src/utilities/testHelpers';
+
+import { TableCell } from '../TableCell';
+import { SelectableTableRow } from './SelectableTableRow';
+
+const cells = [
+  { id: 1, label: 'child-cell-1' },
+  { id: 2, label: 'child-cell-2' },
+  { id: 3, label: 'child-cell-3' },
+];
+
+const defaultArgs = {
+  children: cells.map((cell) => (
+    <TableCell key={cell.id}>{cell.label}</TableCell>
+  )),
+  handleToggleCheck: vi.fn(),
+  isChecked: false,
+};
+
+const ariaLabel = 'Select all entities on page';
+
+describe('SelectableTableRow', () => {
+  it('should render table row with checkbox and child content', () => {
+    const { getAllByText, getByRole, getByText } = renderWithTheme(
+      <SelectableTableRow {...defaultArgs} />
+    );
+
+    const checkbox = getByRole('checkbox', {
+      name: ariaLabel,
+    });
+
+    expect(checkbox).toBeInTheDocument();
+
+    cells.forEach((cell) => {
+      expect(getByText(cell.label)).toBeInTheDocument();
+    });
+
+    expect(getAllByText(/child-cell-/i)).toHaveLength(cells.length);
+  });
+
+  it('should call handleToggleCheck on click', () => {
+    const { getByRole } = renderWithTheme(
+      <SelectableTableRow {...defaultArgs} />
+    );
+
+    const checkbox = getByRole('checkbox', {
+      name: ariaLabel,
+    });
+
+    fireEvent.click(checkbox);
+    expect(defaultArgs.handleToggleCheck).toHaveBeenCalled();
+  });
+
+  it('should show checked checkbox correctly if checked', () => {
+    const { getByRole } = renderWithTheme(
+      <SelectableTableRow {...defaultArgs} isChecked={true} />
+    );
+
+    const checkbox = getByRole('checkbox', {
+      name: ariaLabel,
+    });
+
+    expect(checkbox).toBeChecked();
+  });
+
+  it('should show unchecked checkbox correctly if unchecked', () => {
+    const { getByRole } = renderWithTheme(
+      <SelectableTableRow {...defaultArgs} />
+    );
+
+    const checkbox = getByRole('checkbox', {
+      name: ariaLabel,
+    });
+
+    expect(checkbox).not.toBeChecked();
+  });
+});
