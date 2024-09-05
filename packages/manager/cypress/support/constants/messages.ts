38aa26844542547c92003bdfed21aa987948a05d
agorthi <agorthi@akamai.com>
2024-09-05 18:03:12 +0530

    upcoming:[DI-20585]- added missing message constants file

---
+++ packages/manager/cypress/support/constants/messages.ts
@@ -0,0 +1,6 @@
+export const expectedText = {
+  aclpMessage: 'Akamai Cloud Pulse',
+  noResult: 'No results',
+  nooption: 'You have no options to choose from',
+  selectDashboardMessage: 'Select Dashboard and filters to visualize metrics.',
+};
