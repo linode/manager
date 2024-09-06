702cab44f1c6b45a9966263f1b38f593dd26ae65
Alban Bailly <abailly@akamai.com>
2024-09-06 14:09:13 -0400

    improve useGravatar hook

--- packages/manager/src/hooks/useGravatar.ts
+++ packages/manager/src/hooks/useGravatar.ts
@@ -2,18 +2,38 @@ import { useEffect, useState } from 'react';
 
 import { checkForGravatar, getGravatarUrl } from 'src/utilities/gravatar';
 
-export function useGravatar(email: string | undefined) {
+// set a cache to prevent duplicate requests
+const gravatarCache: Record<string, boolean> = {};
+
+/**
+ * useGravatar
+ *
+ * @description
+ * This hook checks if a user has a Gravatar associated with their email.
+ * It uses a cache to prevent duplicate requests.
+ *
+ * @param email - The email address to check for a Gravatar.
+ * @returns - A boolean indicating whether the user has a Gravatar.
+ */
+export const useGravatar = (email: string | undefined) => {
   const [hasGravatar, setHasGravatar] = useState(false);
 
   useEffect(() => {
-    if (email) {
-      checkForGravatar(getGravatarUrl(email)).then((res) =>
-        setHasGravatar(res)
-      );
-    } else {
+    if (!email) {
       setHasGravatar(false);
+
+      return;
+    }
+
+    if (gravatarCache.hasOwnProperty(email)) {
+      setHasGravatar(gravatarCache[email]);
+    } else {
+      checkForGravatar(getGravatarUrl(email)).then((result) => {
+        gravatarCache[email] = result;
+        setHasGravatar(result);
+      });
     }
   }, [email]);
 
   return hasGravatar;
-}
+};
