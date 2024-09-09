22df56883b0d46a4dcac6557f1501ef47a345b77
Connie Liu <coliu@akamai.com>
2024-09-09 15:44:11 -0400

    update landing state test

--- packages/manager/src/features/NodeBalancers/NodeBalancersLanding/NodeBalancersLandingEmptyState.test.tsx
+++ packages/manager/src/features/NodeBalancers/NodeBalancersLanding/NodeBalancersLandingEmptyState.test.tsx
@@ -1,28 +1,21 @@
 import { waitFor } from '@testing-library/react';
 import * as React from 'react';
 
-import { grantsFactory, profileFactory } from 'src/factories';
-import { HttpResponse, http, server } from 'src/mocks/testServer';
+import { useRestrictedGlobalGrantCheck } from 'src/hooks/useRestrictedGlobalGrantCheck';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
 import { NodeBalancerLandingEmptyState } from './NodeBalancersLandingEmptyState';
 
-describe('NodeBalancersLandingEmptyState', () => {
-  // Note: An integration test confirming the helper text and enabled Create NodeBalancer button already exists, so we're just checking for a disabled create button here
+vi.mock('src/hooks/useRestrictedGlobalGrantCheck');
 
+describe('NodeBalancersLandingEmptyState', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  // Note: An integration test confirming the helper text and enabled Create NodeBalancer button already exists, so we're just checking for a disabled create button here
   it('disables the Create NodeBalancer button if user does not have permissions to create a NodeBalancer', async () => {
-    server.use(
-      http.get('*/v4/profile', () => {
-        const profile = profileFactory.build({ restricted: true });
-        return HttpResponse.json(profile);
-      }),
-      http.get('*/v4/profile/grants', () => {
-        const grants = grantsFactory.build({
-          global: { add_nodebalancers: false },
-        });
-        return HttpResponse.json(grants);
-      })
-    );
+    vi.mocked(useRestrictedGlobalGrantCheck).mockReturnValue(true);
 
     const { getByText } = renderWithTheme(<NodeBalancerLandingEmptyState />);
 
