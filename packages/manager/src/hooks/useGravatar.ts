6a3eca7291d6c6bca3542d0cda51f35db6ae87ba
mjac0bs <mjacobs@akamai.com>
2024-09-10 09:10:57 -0700

    Address feedback: use Map

--- packages/manager/src/hooks/useGravatar.ts
+++ packages/manager/src/hooks/useGravatar.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { checkForGravatar, getGravatarUrl } from 'src/utilities/gravatar';
 
 // set a cache to prevent duplicate requests
-const gravatarCache: Record<string, boolean> = {};
+const gravatarCache = new Map<string, boolean>();
 
 /**
  * useGravatar
@@ -15,24 +15,26 @@ const gravatarCache: Record<string, boolean> = {};
  * @param email - The email address to check for a Gravatar.
  * @returns - A boolean indicating whether the user has a Gravatar.
  */
+
 export const useGravatar = (email: string | undefined) => {
-  const [hasGravatar, setHasGravatar] = useState(false);
+  const [hasGravatar, setHasGravatar] = useState(
+    email && gravatarCache.has(email) ? gravatarCache.get(email) : false
+  );
   const [isLoadingGravatar, setIsLoadingGravatar] = useState<boolean>(true);
 
   useEffect(() => {
     if (!email) {
       setHasGravatar(false);
       setIsLoadingGravatar(false);
-
       return;
     }
 
-    if (gravatarCache.hasOwnProperty(email)) {
-      setHasGravatar(gravatarCache[email]);
+    if (gravatarCache.has(email)) {
+      setHasGravatar(gravatarCache.get(email));
       setIsLoadingGravatar(false);
     } else {
       checkForGravatar(getGravatarUrl(email)).then((result) => {
-        gravatarCache[email] = result;
+        gravatarCache.set(email, result);
         setHasGravatar(result);
         setIsLoadingGravatar(false);
       });
