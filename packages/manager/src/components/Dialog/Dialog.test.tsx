2be41d169efabd75d24a198a68d7f257463f76f0
pmakode-akamai <pmakode@akamai.com>
2024-09-11 18:42:51 +0530

    Add unit tests for dialog and deletiondialog components

---
+++ packages/manager/src/components/Dialog/Dialog.test.tsx
@@ -0,0 +1,72 @@
+import { fireEvent } from '@testing-library/react';
+import * as React from 'react';
+
+import { renderWithTheme } from 'src/utilities/testHelpers';
+
+import { Dialog } from './Dialog';
+
+import type { DialogProps } from './Dialog';
+
+describe('Dialog', () => {
+  const defaultArgs: DialogProps = {
+    onClose: vi.fn(),
+    open: false,
+    title: 'This is a Dialog',
+  };
+
+  it.each([
+    ['not render', false],
+    ['render', true],
+  ])('should %s a Dialog with title when open is %s', (_, isOpen) => {
+    const { queryByTestId, queryByText } = renderWithTheme(
+      <Dialog {...defaultArgs} open={isOpen} />
+    );
+
+    const title = queryByText('This is a Dialog');
+    const dialog = queryByTestId('drawer');
+
+    if (isOpen) {
+      expect(title).toBeInTheDocument();
+      expect(dialog).toBeInTheDocument();
+    } else {
+      expect(title).not.toBeInTheDocument();
+      expect(dialog).not.toBeInTheDocument();
+    }
+  });
+
+  it('should render a Dialog with childrens if provided', () => {
+    const { getByText } = renderWithTheme(
+      <Dialog {...defaultArgs} open={true}>
+        <p>Child items can go here!</p>
+      </Dialog>
+    );
+
+    expect(getByText('Child items can go here!')).toBeInTheDocument();
+  });
+
+  it('should call onClose when the Dialog close button is clicked', () => {
+    const { getByTestId } = renderWithTheme(
+      <Dialog {...defaultArgs} open={true} />
+    );
+
+    const closeButton = getByTestId('CloseIcon').closest('button');
+
+    if (closeButton) {
+      fireEvent.click(closeButton);
+    }
+
+    expect(defaultArgs.onClose).toHaveBeenCalled();
+  });
+
+  it('should render a Dialog with an error message if provided', () => {
+    const { getByText } = renderWithTheme(
+      <Dialog
+        {...defaultArgs}
+        error="Error that will be shown in the dialog."
+        open={true}
+      />
+    );
+
+    expect(getByText('Error that will be shown in the dialog.')).toBeVisible();
+  });
+});
