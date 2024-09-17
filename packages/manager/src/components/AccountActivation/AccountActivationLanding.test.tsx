2336bfc2b6b450474ec8f15ad921bd4980022141
Connie Liu <coliu@akamai.com>
2024-09-17 11:19:34 -0400

    add AccountActivationLanding tests + cleanup styling

---
+++ packages/manager/src/components/AccountActivation/AccountActivationLanding.test.tsx
@@ -0,0 +1,38 @@
+import { fireEvent } from '@testing-library/react';
+import * as React from 'react';
+
+import { renderWithTheme } from 'src/utilities/testHelpers';
+
+import AccountActivationLanding from './AccountActivationLanding';
+
+const openSupportTicket = 'Open a Support Ticket';
+
+describe('AccountActivationLanding', () => {
+  it('renders the AccountActivationLanding component', () => {
+    const { getByText, queryByText } = renderWithTheme(
+      <AccountActivationLanding />
+    );
+
+    expect(
+      getByText('Your account is currently being reviewed.')
+    ).toBeVisible();
+    expect(
+      getByText(
+        /Thanks for signing up! You’ll receive an email from us once our review is complete, so hang tight. If you have questions during this process/
+      )
+    ).toBeVisible();
+    expect(getByText(/please open a Support ticket/)).toBeVisible();
+    expect(queryByText(openSupportTicket)).not.toBeInTheDocument();
+  });
+
+  it('toggles open the Support Ticket dialog', () => {
+    const { getByText, queryByText } = renderWithTheme(
+      <AccountActivationLanding />
+    );
+
+    expect(queryByText(openSupportTicket)).not.toBeInTheDocument();
+    const supportButtonLink = getByText(/please open a Support ticket/);
+    fireEvent.click(supportButtonLink);
+    expect(getByText(openSupportTicket)).toBeVisible();
+  });
+});
