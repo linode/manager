17a524efa03ce47f396aa61557d0144f191a9c15
mjac0bs <mjacobs@akamai.com>
2024-09-18 14:05:35 -0700

    Update test coverage

--- packages/manager/src/features/Profile/DisplaySettings/AvatarColorPickerDialog.test.tsx
+++ packages/manager/src/features/Profile/DisplaySettings/AvatarColorPickerDialog.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
 import { renderWithTheme } from 'src/utilities/testHelpers';
@@ -14,25 +15,46 @@ const mockProps: AvatarColorPickerDialogProps = {
 
 describe('AvatarColorPicker', () => {
   it('should render a dialog with a title, color picker, and avatar components', () => {
-    const {
-      getByLabelText,
-      getByRole,
-      getByTestId,
-      getByTitle,
-    } = renderWithTheme(<AvatarColorPickerDialog {...mockProps} />);
+    const { getByLabelText, getByTestId, getByTitle } = renderWithTheme(
+      <AvatarColorPickerDialog {...mockProps} />
+    );
 
     expect(getByTitle('Change Avatar Color')).toBeVisible();
     expect(getByLabelText('Avatar color picker')).toBeVisible();
     expect(getByTestId('avatar')).toBeVisible();
-    expect(getByRole('button', { name: 'Save' })).toBeDisabled();
   });
 
-  it('calls onClose when Close button is clicked', async () => {
-    const { getByText } = renderWithTheme(
+  it('calls onClose when Close/X buttons are clicked', async () => {
+    const { getAllByRole } = renderWithTheme(
       <AvatarColorPickerDialog {...mockProps} />
     );
 
-    await fireEvent.click(getByText('Close'));
+    const closeButtons = getAllByRole('button', { name: 'Close' });
+    closeButtons.forEach(async (button) => {
+      await userEvent.click(button);
+      expect(mockProps.handleClose).toHaveBeenCalled();
+    });
+  });
+
+  it('confirms when Save button is enabled and the dialog closes when Save is clicked', async () => {
+    const { getByLabelText, getByRole } = renderWithTheme(
+      <AvatarColorPickerDialog {...mockProps} />
+    );
+
+    const saveButton = getByRole('button', { name: 'Save' });
+
+    expect(saveButton).toBeDisabled();
+
+    fireEvent.input(getByLabelText('Avatar color picker'), {
+      target: { value: '#333333' },
+    });
+
+    // Verify save button becomes enabled after changing the color
+    expect(saveButton).toBeEnabled();
+
+    await userEvent.click(saveButton);
+
+    // Verify dialog closes after saving
     expect(mockProps.handleClose).toHaveBeenCalled();
   });
 });
