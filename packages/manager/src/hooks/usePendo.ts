60ad3c6aef2b5407c59c757867b7bce6e34de045
mjac0bs <mjacobs@akamai.com>
2024-09-18 09:24:01 -0700

    Add sha256 hashing for ids

--- packages/manager/src/hooks/usePendo.ts
+++ packages/manager/src/hooks/usePendo.ts
@@ -1,15 +1,15 @@
+import { sha256 } from 'js-sha256';
 import React from 'react';
 
-// import { useAccount } from 'src/queries/account/account.js';
-// import { useProfile } from 'src/queries/profile/profile';
-
-import { PENDO_API_KEY } from 'src/constants';
+import { /* APP_ROOT,*/ PENDO_API_KEY } from 'src/constants';
+import { useAccount } from 'src/queries/account/account.js';
+import { useProfile } from 'src/queries/profile/profile';
 
 import { loadScript } from './useScript';
 
 declare global {
   interface Window {
-    pendo: any; //TODO: can we type this any better?
+    pendo: any;
   }
 }
 
@@ -17,9 +17,16 @@ declare global {
  * Initializes our Pendo analytics script on mount.
  */
 export const usePendo = () => {
-  // TODO: update metadata with these values
-  // const { data: profile } = useProfile();
-  // const { data: account } = useAccount();
+  const { data: profile } = useProfile();
+  const { data: account } = useAccount();
+
+  const visitorId = sha256(profile?.uid.toString() ?? '');
+  const accountId = sha256(account?.euuid ?? '');
+
+  // const STAGING_URL = '';
+  const PROD_URL = `https://cdn.pendo.io/agent/static/${PENDO_API_KEY}/pendo.js`;
+  // const PENDO_CDN_URL =
+  //   APP_ROOT !== 'https://cloud.linode.com' ? STAGING_URL : PROD_URL;
 
   React.useEffect(() => {
     // Adapted the Pendo install script:
@@ -51,12 +58,12 @@ export const usePendo = () => {
     }
 
     // Load Pendo script into the head HTML tag, then initialize Pendo with metadata
-    loadScript(`https://cdn.pendo.io/agent/static/${PENDO_API_KEY}/pendo.js`, {
+    loadScript(PROD_URL, {
       location: 'head',
     }).then(() => {
       window.pendo.initialize({
         account: {
-          id: `test-account-dev`, // Highly recommended, required if using Pendo Feedback
+          id: accountId, // Highly recommended, required if using Pendo Feedback
           // name:         // Optional
           // is_paying:    // Recommended if using Pendo Feedback
           // monthly_value:// Recommended if using Pendo Feedback
@@ -69,7 +76,7 @@ export const usePendo = () => {
         },
 
         visitor: {
-          id: `test-visitor-dev`, // Required if user is logged in
+          id: visitorId, // Required if user is logged in
           // email:        // Recommended if using Pendo Feedback, or NPS Email
           // full_name:    // Recommended if using Pendo Feedback
           // role:         // Optional
