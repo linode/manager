5365e6df362922018d6445119b6814922ed996af
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2024-09-19 17:31:08 -0400

    Update packages/manager/src/features/CloudPulse/shared/CloudPulseDashboardFilterBuilder.tsx
    Co-authored-by: Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>

--- packages/manager/src/features/CloudPulse/shared/CloudPulseDashboardFilterBuilder.tsx
+++ packages/manager/src/features/CloudPulse/shared/CloudPulseDashboardFilterBuilder.tsx
@@ -219,7 +219,7 @@ export const CloudPulseDashboardFilterBuilder = React.memo(
             p: 0,
           }}
           item
-          key={'toggleFilter'}
+          key="toggleFilter"
           xs={12}
         >
           <Button
