7ee3147126c813a8fb00f68742051ca4c41b626b
Connie Liu <coliu@akamai.com>
2024-09-24 16:29:51 -0400

    need to cleanup

--- packages/manager/cypress/e2e/core/objectStorageGen2/bucket-create-gen2.spec.ts
+++ packages/manager/cypress/e2e/core/objectStorageGen2/bucket-create-gen2.spec.ts
@@ -21,6 +21,38 @@ import type {
   ObjectStorageEndpointTypes,
 } from '@linode/api-v4';
 
+export const checkRateLimitsTable = (
+  endpointType: ObjectStorageEndpointTypes
+) => {
+  const expectedHeaders = ['Limits', 'GET', 'PUT', 'LIST', 'DELETE', 'OTHER'];
+  const expectedBasicValues = ['Basic', '2,000', '500', '100', '200', '400'];
+  const expectedHighValues =
+    endpointType === 'E3'
+      ? ['High', '20,000', '2,000', '400', '400', '1,000']
+      : ['High', '5,000', '1,000', '200', '200', '800'];
+
+  cy.get('[data-testid="bucket-rate-limit-table"]').within(() => {
+    expectedHeaders.forEach((header, index) => {
+      cy.get('th').eq(index).should('contain.text', header);
+    });
+
+    cy.contains('tr', 'Basic').within(() => {
+      expectedBasicValues.forEach((value, index) => {
+        cy.get('td').eq(index).should('contain.text', value);
+      });
+    });
+
+    cy.contains('tr', 'High').within(() => {
+      expectedHighValues.forEach((value, index) => {
+        cy.get('td').eq(index).should('contain.text', value);
+      });
+    });
+
+    // Check that Basic radio button is checked
+    cy.findByLabelText('Basic').should('be.checked');
+  });
+};
+
 describe('Object Storage Gen2 create bucket tests', () => {
   beforeEach(() => {
     mockAppendFeatureFlags({
@@ -71,36 +103,6 @@ describe('Object Storage Gen2 create bucket tests', () => {
       s3_endpoint: null,
     }),
   ];
-
-  const checkRateLimitsTable = (endpointType: ObjectStorageEndpointTypes) => {
-    const expectedHeaders = ['Limits', 'GET', 'PUT', 'LIST', 'DELETE', 'OTHER'];
-    const expectedBasicValues = ['Basic', '2,000', '500', '100', '200', '400'];
-    const expectedHighValues =
-      endpointType === 'E3'
-        ? ['High', '20,000', '2,000', '400', '400', '1,000']
-        : ['High', '5,000', '1,000', '200', '200', '800'];
-
-    cy.get('[data-testid="bucket-rate-limit-table"]').within(() => {
-      expectedHeaders.forEach((header, index) => {
-        cy.get('th').eq(index).should('contain.text', header);
-      });
-
-      cy.contains('tr', 'Basic').within(() => {
-        expectedBasicValues.forEach((value, index) => {
-          cy.get('td').eq(index).should('contain.text', value);
-        });
-      });
-
-      cy.contains('tr', 'High').within(() => {
-        expectedHighValues.forEach((value, index) => {
-          cy.get('td').eq(index).should('contain.text', value);
-        });
-      });
-
-      // Check that Basic radio button is checked
-      cy.findByLabelText('Basic').should('be.checked');
-    });
-  };
 
   /**
    * Confirms UI flow for creating a gen2 Object Storage bucket with endpoint E0
--- packages/manager/cypress/e2e/core/objectStorageGen2/bucket-details-gen2.spec.ts
+++ packages/manager/cypress/e2e/core/objectStorageGen2/bucket-details-gen2.spec.ts
@@ -12,6 +12,7 @@ import {
   regionFactory,
 } from 'src/factories';
 import { ACLType, ObjectStorageEndpointTypes } from '@linode/api-v4';
+import { checkRateLimitsTable } from './bucket-create-gen2.spec';
 
 describe('Object Storage Gen 2 bucket details tabs', () => {
   beforeEach(() => {
@@ -41,8 +42,8 @@ describe('Object Storage Gen 2 bucket details tabs', () => {
       cors_enabled: true,
       cors_xml: '',
     };
-  
-    const createMocksAndNavigateToPage = (
+
+    const createMocksAndNavigateToTab = (
       endpointType: ObjectStorageEndpointTypes
     ) => {
       const mockBucket = objectStorageBucketFactoryGen2.build({
@@ -54,7 +55,7 @@ describe('Object Storage Gen 2 bucket details tabs', () => {
         endpoint_type: endpointType,
         region: mockRegion.id,
       });
-  
+
       mockGetBucketAccess(label, cluster, mockAccess).as('getBucketAccess');
       mockGetBucketsForRegion(mockRegion.id, [mockBucket]).as(
         'getBucketsForRegion'
@@ -62,7 +63,7 @@ describe('Object Storage Gen 2 bucket details tabs', () => {
       mockGetObjectStorageEndpoints([mockEndpoint]).as(
         'getObjectStorageEndpoints'
       );
-  
+
       cy.visitWithLogin(`/object-storage/buckets/${cluster}/${label}/access`);
       cy.wait([
         '@getFeatureFlags',
@@ -72,7 +73,7 @@ describe('Object Storage Gen 2 bucket details tabs', () => {
         '@getBucketAccess',
       ]);
     };
-  
+
     const confirmCORSToggleAndSSLTabPresent = () => {
       cy.findByText('Bucket Access').should('be.visible');
       cy.findByLabelText('Access Control List (ACL)').should('be.visible');
@@ -82,12 +83,12 @@ describe('Object Storage Gen 2 bucket details tabs', () => {
         /Whether Cross-Origin Resource Sharing is enabled for all origins. For more fine-grained control of CORS, please use another/
       ).should('be.visible');
       cy.findByText(/S3-compatible tool/).should('be.visible');
-  
+
       // Confirm SSL/TLS tab is not hidden and is clickable
       cy.findByText('SSL/TLS').should('be.visible').click();
       cy.url().should('endWith', '/ssl');
     };
-  
+
     const confirmCORSNotPresent = () => {
       cy.findByText('Bucket Access').should('be.visible');
       cy.findByLabelText('Access Control List (ACL)').should('be.visible');
@@ -96,54 +97,54 @@ describe('Object Storage Gen 2 bucket details tabs', () => {
         /CORS \(Cross Origin Sharing\) is not available for endpoint types E2 and E3/
       ).should('be.visible');
     };
-  
+
     /**
      * - Confirms the CORS toggle still appears for buckets with endpoint type E0
      * - Confirms the SSL/TLS tab appears for buckets with endpoint type E0
      */
     it('does not hide the CORS toggle or SSL/TLS tab for buckets with an E0 endpoint', () => {
-      createMocksAndNavigateToPage('E0');
-  
+      createMocksAndNavigateToTab('E0');
+
       // confirm CORS toggle is visible and the SSL/TLS tab is present
       confirmCORSToggleAndSSLTabPresent();
     });
-  
+
     /**
      * - Confirms the CORS toggle still appears for buckets with endpoint type E1
      * - Confirms the SSL/TLS tab appears for buckets with endpoint type E1
      */
     it('does not hide the CORS toggle or SSL/TLS tab for buckets with an E1 endpoint', () => {
-      createMocksAndNavigateToPage('E1');
-  
+      createMocksAndNavigateToTab('E1');
+
       // confirm CORS toggle is visible and the SSL/TLS tab is present
       confirmCORSToggleAndSSLTabPresent();
     });
-  
+
     /**
      * - Confirms the CORS and display notice is hidden for buckets with endpoint type E2
      * - Confirms the SSL/TLS tab appears for buckets with endpoint type E2
      */
     it('hides the CORS toggle and displays a notice for buckets with an E2 endpoint', () => {
-      createMocksAndNavigateToPage('E2');
-  
+      createMocksAndNavigateToTab('E2');
+
       // confirms the CORS toggle is not visible
       confirmCORSNotPresent();
-  
+
       // Confirm SSL/TLS tab is not hidden and is clickable
       cy.findByText('SSL/TLS').should('be.visible').click();
       cy.url().should('endWith', '/ssl');
     });
-  
+
     /**
      * - Confirms the CORS and display notice is hidden for buckets with endpoint type E3
      * - Confirms the SSL/TLS tab is hidden for buckets with endpoint type E3
      */
     it('hides the CORS toggle, displays a notice, and disables the SSL/TLS tab for buckets with an E3 endpoint', () => {
-      createMocksAndNavigateToPage('E3');
-  
+      createMocksAndNavigateToTab('E3');
+
       // confirms the CORS toggle is not visible
       confirmCORSNotPresent();
-  
+
       // Confirm SSL/TLS tab is hidden
       cy.findByText('SSL/TLS').should('not.exist');
     });
@@ -160,27 +161,89 @@ describe('Object Storage Gen 2 bucket details tabs', () => {
 
     // /**
     //  * - Confirms absence of Properties tab for buckets with endpoint type E1
-    //  */    
+    //  */
     // it('does not show the properties tab for buckets with endpoint type E1', () => {
 
-    // });    
-    
+    // });
+
     /**
      * For buckets with endpoint type E2,
      * - Confirms Properties tab is present
      * - Confirms the Properties tab contains the bucket rate limit table
-     */    
+     */
     it('shows the properties tab and rate limit table for buckets with endpoint type E2', () => {
+      const mockBucket = objectStorageBucketFactoryGen2.build({
+        endpoint_type: 'E2',
+        region: mockRegion.id,
+      });
+      const { cluster, label } = mockBucket;
+      const mockEndpoint = objectStorageEndpointsFactory.build({
+        endpoint_type: 'E2',
+        region: mockRegion.id,
+      });
 
+      mockGetBucketsForRegion(mockRegion.id, [mockBucket]).as(
+        'getBucketsForRegion'
+      );
+      mockGetObjectStorageEndpoints([mockEndpoint]).as(
+        'getObjectStorageEndpoints'
+      );
+
+      cy.visitWithLogin(
+        `/object-storage/buckets/${cluster}/${label}/properties`
+      );
+      cy.wait([
+        '@getFeatureFlags',
+        '@getAccount',
+        '@getObjectStorageEndpoints',
+        '@getBucketsForRegion',
+      ]);
+
+      cy.findByText('Bucket Rate Limits');
+      cy.contains(
+        'Specifies the maximum Requests Per Second (RPS) for a bucket. To increase it to High, open a support ticket. Understand bucket rate limits.'
+      ).should('be.visible');
+      checkRateLimitsTable('E2');
     });
 
     /**
      * For buckets with endpoint type E3,
      * - Confirms Properties tab is present
      * - Confirms the Properties tab contains the bucket rate limit table
-     */    
+     */
     it('shows the properties tab and rate limit table for buckets with endpoint type E3', () => {
+      const mockBucket = objectStorageBucketFactoryGen2.build({
+        endpoint_type: 'E3',
+        region: mockRegion.id,
+      });
+      const { cluster, label } = mockBucket;
+      const mockEndpoint = objectStorageEndpointsFactory.build({
+        endpoint_type: 'E3',
+        region: mockRegion.id,
+      });
 
-    });   
+      mockGetBucketsForRegion(mockRegion.id, [mockBucket]).as(
+        'getBucketsForRegion'
+      );
+      mockGetObjectStorageEndpoints([mockEndpoint]).as(
+        'getObjectStorageEndpoints'
+      );
+
+      cy.visitWithLogin(
+        `/object-storage/buckets/${cluster}/${label}/properties`
+      );
+      cy.wait([
+        '@getFeatureFlags',
+        '@getAccount',
+        '@getObjectStorageEndpoints',
+        '@getBucketsForRegion',
+      ]);
+
+      cy.findByText('Bucket Rate Limits');
+      cy.contains(
+        'Specifies the maximum Requests Per Second (RPS) for a bucket. To increase it to High, open a support ticket. Understand bucket rate limits.'
+      ).should('be.visible');
+      checkRateLimitsTable('E3');
+    });
   });
 });
