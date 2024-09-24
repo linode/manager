cb3adedaabe8f35e8774b2cefa91f4c6c4ad3614
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-09-24 14:42:49 -0500

    fix: [M3-8578] - Fix console error on Volume Create - Linode Config selection (#10970)
    * unit test coverage for HostNameTableCell
    
    * Revert "unit test coverage for HostNameTableCell"
    
    This reverts commit b274baf67e27d79fd4e764607ded7c5aa755ee8b.
    
    * fix console error in Create Volume page
    
    * Added changeset: Fix console error on Volume Create - Linode Config selection

--- packages/manager/src/features/Volumes/VolumeDrawer/ConfigSelect.tsx
+++ packages/manager/src/features/Volumes/VolumeDrawer/ConfigSelect.tsx
@@ -36,12 +36,15 @@ export const ConfigSelect = React.memo((props: Props) => {
     return { label: config.label, value: config.id };
   });
 
-  if (configList?.length === 1) {
-    const newValue = configList[0].value;
-    if (value !== newValue) {
-      onChange(newValue);
+  // Use useEffect to handle the side effect
+  React.useEffect(() => {
+    if (configList?.length === 1) {
+      const newValue = configList[0].value;
+      if (value !== newValue) {
+        onChange(newValue);
+      }
     }
-  }
+  }, [configList, onChange, value]);
 
   return (
     <FormControl
