c6b18c3aa101a8b36ddc12830b92041ed3c46bed
Connie Liu <coliu@akamai.com>
2024-09-24 14:22:41 -0400

    remove duplication

--- packages/manager/cypress/e2e/core/objectStorageGen2/bucket-details-gen2.spec.ts
+++ packages/manager/cypress/e2e/core/objectStorageGen2/bucket-details-gen2.spec.ts
@@ -11,7 +11,7 @@ import {
   objectStorageEndpointsFactory,
   regionFactory,
 } from 'src/factories';
-import { ACLType } from '@linode/api-v4';
+import { ACLType, ObjectStorageEndpointTypes } from '@linode/api-v4';
 
 describe('Object Storage Gen 2 bucket details tab tests', () => {
   beforeEach(() => {
@@ -41,19 +41,16 @@ describe('Object Storage Gen 2 bucket details tab tests', () => {
     cors_xml: '',
   };
 
-  // TODO - figure out how to simulate it.each with mocha
-  /**
-   * - Confirms the CORS toggle still appears for buckets with endpoint type E0
-   * - Confirms the SSL/TLS tab appears for buckets with endpoint type E0
-   */
-  it('does not hide the CORS toggle or SSL/TLS tab for buckets with the E0 endpoint', () => {
+  const createMocksAndNavigateToPage = (
+    endpointType: ObjectStorageEndpointTypes
+  ) => {
     const mockBucket = objectStorageBucketFactoryGen2.build({
-      endpoint_type: 'E0',
+      endpoint_type: endpointType,
       region: mockRegion.id,
     });
     const { cluster, label } = mockBucket;
     const mockEndpoint = objectStorageEndpointsFactory.build({
-      endpoint_type: 'E0',
+      endpoint_type: endpointType,
       region: mockRegion.id,
     });
 
@@ -73,7 +70,9 @@ describe('Object Storage Gen 2 bucket details tab tests', () => {
       '@getBucketsForRegion',
       '@getBucketAccess',
     ]);
+  };
 
+  const confirmCORSToggleAndSSLTabPresent = () => {
     cy.findByText('Bucket Access').should('be.visible');
     cy.findByLabelText('Access Control List (ACL)').should('be.visible');
     // confirm CORS is visible
@@ -86,6 +85,26 @@ describe('Object Storage Gen 2 bucket details tab tests', () => {
     // Confirm SSL/TLS tab is not hidden and is clickable
     cy.findByText('SSL/TLS').should('be.visible').click();
     cy.url().should('endWith', '/ssl');
+  };
+
+  const confirmCORSNotPresent = () => {
+    cy.findByText('Bucket Access').should('be.visible');
+    cy.findByLabelText('Access Control List (ACL)').should('be.visible');
+    // confirm CORS is not visible
+    cy.findByText(
+      /CORS \(Cross Origin Sharing\) is not available for endpoint types E2 and E3/
+    ).should('be.visible');
+  };
+
+  /**
+   * - Confirms the CORS toggle still appears for buckets with endpoint type E0
+   * - Confirms the SSL/TLS tab appears for buckets with endpoint type E0
+   */
+  it('does not hide the CORS toggle or SSL/TLS tab for buckets with the E0 endpoint', () => {
+    createMocksAndNavigateToPage('E0');
+
+    // confirm CORS toggle is visible and the SSL/TLS tab is present
+    confirmCORSToggleAndSSLTabPresent();
   });
 
   /**
@@ -93,45 +112,10 @@ describe('Object Storage Gen 2 bucket details tab tests', () => {
    * - Confirms the SSL/TLS tab appears for buckets with endpoint type E1
    */
   it('does not hide the CORS toggle or SSL/TLS tab for buckets with the E1 endpoint', () => {
-    const mockBucket = objectStorageBucketFactoryGen2.build({
-      endpoint_type: 'E1',
-      region: mockRegion.id,
-    });
-    const { cluster, label } = mockBucket;
-    const mockEndpoint = objectStorageEndpointsFactory.build({
-      endpoint_type: 'E1',
-      region: mockRegion.id,
-    });
+    createMocksAndNavigateToPage('E1');
 
-    mockGetBucketAccess(label, cluster, mockAccess).as('getBucketAccess');
-    mockGetBucketsForRegion(mockRegion.id, [mockBucket]).as(
-      'getBucketsForRegion'
-    );
-    mockGetObjectStorageEndpoints([mockEndpoint]).as(
-      'getObjectStorageEndpoints'
-    );
-
-    cy.visitWithLogin(`/object-storage/buckets/${cluster}/${label}/access`);
-    cy.wait([
-      '@getFeatureFlags',
-      '@getAccount',
-      '@getObjectStorageEndpoints',
-      '@getBucketsForRegion',
-      '@getBucketAccess',
-    ]);
-
-    cy.findByText('Bucket Access').should('be.visible');
-    cy.findByLabelText('Access Control List (ACL)').should('be.visible');
-    // confirm CORS is visible
-    cy.findByText('CORS Enabled').should('be.visible');
-    cy.findByText(
-      /Whether Cross-Origin Resource Sharing is enabled for all origins. For more fine-grained control of CORS, please use another/
-    ).should('be.visible');
-    cy.findByText(/S3-compatible tool/).should('be.visible');
-
-    // Confirm SSL/TLS tab is not hidden and is clickable
-    cy.findByText('SSL/TLS').should('be.visible').click();
-    cy.url().should('endWith', '/ssl');
+    // confirm CORS toggle is visible and the SSL/TLS tab is present
+    confirmCORSToggleAndSSLTabPresent();
   });
 
   /**
@@ -139,39 +123,10 @@ describe('Object Storage Gen 2 bucket details tab tests', () => {
    * - Confirms the SSL/TLS tab is visible for buckets with endpoint type E2
    */
   it('hides the CORS and display notice and hides the SSL/TLS tab for the E2 endpoint', () => {
-    const mockBucket = objectStorageBucketFactoryGen2.build({
-      endpoint_type: 'E2',
-      region: mockRegion.id,
-    });
-    const { cluster, label } = mockBucket;
-    const mockEndpoint = objectStorageEndpointsFactory.build({
-      endpoint_type: 'E2',
-      region: mockRegion.id,
-    });
+    createMocksAndNavigateToPage('E2');
 
-    mockGetBucketAccess(label, cluster, mockAccess).as('getBucketAccess');
-    mockGetBucketsForRegion(mockRegion.id, [mockBucket]).as(
-      'getBucketsForRegion'
-    );
-    mockGetObjectStorageEndpoints([mockEndpoint]).as(
-      'getObjectStorageEndpoints'
-    );
-
-    cy.visitWithLogin(`/object-storage/buckets/${cluster}/${label}/access`);
-    cy.wait([
-      '@getFeatureFlags',
-      '@getAccount',
-      '@getObjectStorageEndpoints',
-      '@getBucketsForRegion',
-      '@getBucketAccess',
-    ]);
-
-    cy.findByText('Bucket Access').should('be.visible');
-    cy.findByLabelText('Access Control List (ACL)').should('be.visible');
-    // confirm CORS is not visible
-    cy.findByText(
-      /CORS \(Cross Origin Sharing\) is not available for endpoint types E2 and E3./
-    ).should('be.visible');
+    // confirms the CORS toggle is not visible
+    confirmCORSNotPresent();
 
     // Confirm SSL/TLS tab is not hidden and is clickable
     cy.findByText('SSL/TLS').should('be.visible').click();
@@ -183,39 +138,10 @@ describe('Object Storage Gen 2 bucket details tab tests', () => {
    * - Confirms the SSL/TLS tab is hidden for buckets with endpoint type E3
    */
   it('hides the CORS and display notice and disables the SSL/TLS tab for the E3 endpoint', () => {
-    const mockBucket = objectStorageBucketFactoryGen2.build({
-      endpoint_type: 'E3',
-      region: mockRegion.id,
-    });
-    const { cluster, label } = mockBucket;
-    const mockEndpoint = objectStorageEndpointsFactory.build({
-      endpoint_type: 'E3',
-      region: mockRegion.id,
-    });
+    createMocksAndNavigateToPage('E3');
 
-    mockGetBucketAccess(label, cluster, mockAccess).as('getBucketAccess');
-    mockGetBucketsForRegion(mockRegion.id, [mockBucket]).as(
-      'getBucketsForRegion'
-    );
-    mockGetObjectStorageEndpoints([mockEndpoint]).as(
-      'getObjectStorageEndpoints'
-    );
-
-    cy.visitWithLogin(`/object-storage/buckets/${cluster}/${label}/access`);
-    cy.wait([
-      '@getFeatureFlags',
-      '@getAccount',
-      '@getObjectStorageEndpoints',
-      '@getBucketsForRegion',
-      '@getBucketAccess',
-    ]);
-
-    cy.findByText('Bucket Access').should('be.visible');
-    cy.findByLabelText('Access Control List (ACL)').should('be.visible');
-    // confirm CORS is not visible
-    cy.findByText(
-      /CORS \(Cross Origin Sharing\) is not available for endpoint types E2 and E3/
-    ).should('be.visible');
+    // confirms the CORS toggle is not visible
+    confirmCORSNotPresent();
 
     // Confirm SSL/TLS tab is hidden
     cy.findByText('SSL/TLS').should('not.exist');
