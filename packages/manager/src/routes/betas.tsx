7846dc0a327aa3b748c495dbfc431d66e0c999cf
Alban Bailly <abailly@akamai.com>
2024-09-27 10:39:03 -0400

    betas

---
+++ packages/manager/src/routes/betas.tsx
@@ -0,0 +1,44 @@
+import { Outlet, createRoute } from '@tanstack/react-router';
+import React from 'react';
+
+import { ProductInformationBanner } from 'src/components/ProductInformationBanner/ProductInformationBanner';
+import { SuspenseLoader } from 'src/components/SuspenseLoader';
+
+import { rootRoute } from './root';
+import { strictLazyRouteComponent } from './utils';
+
+export const BetaRoutes = () => {
+  return (
+    <React.Suspense fallback={<SuspenseLoader />}>
+      <ProductInformationBanner bannerLocation="Volumes" />
+      <Outlet />
+    </React.Suspense>
+  );
+};
+
+const betaRoute = createRoute({
+  component: BetaRoutes,
+  getParentRoute: () => rootRoute,
+  path: 'betas',
+});
+
+const betaLandingRoute = createRoute({
+  component: strictLazyRouteComponent(
+    () => import('src/features/Betas/BetasLanding')
+  ),
+  getParentRoute: () => betaRoute,
+  path: '/',
+});
+
+const betaSignupRoute = createRoute({
+  component: strictLazyRouteComponent(
+    () => import('src/features/Betas/BetaSignup')
+  ),
+  getParentRoute: () => betaRoute,
+  path: 'signup',
+});
+
+export const betaRouteTree = betaRoute.addChildren([
+  betaLandingRoute,
+  betaSignupRoute,
+]);
