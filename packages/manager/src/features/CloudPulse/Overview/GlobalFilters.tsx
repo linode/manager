dbaa13cac7f86f5d123ee5a5f37cb74cb11bbd18
vmangalr <vmangalr@akamai.com>
2024-10-01 08:48:33 +0530

    upcoming: [DI-20800] - Global Filters

--- packages/manager/src/features/CloudPulse/Overview/GlobalFilters.tsx
+++ packages/manager/src/features/CloudPulse/Overview/GlobalFilters.tsx
@@ -115,6 +115,7 @@ export const GlobalFilters = React.memo((props: GlobalFilterProperties) => {
                 sx={{
                   marginBlockEnd: 'auto',
                 }}
+                data-testid="global-refresh"
                 disabled={!selectedDashboard}
                 onClick={handleGlobalRefresh}
                 size="small"
