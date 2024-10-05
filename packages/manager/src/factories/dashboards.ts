5e67e04f7e4c051c152daa28779b2708dc10a969
agorthi <agorthi@akamai.com>
2024-10-05 13:33:30 +0530

    upcoming:[DI-20585]- Added code review comments

--- packages/manager/src/factories/dashboards.ts
+++ packages/manager/src/factories/dashboards.ts
@@ -12,18 +12,6 @@ const color = ['blue', 'red', 'green', 'yellow'];
 const chart_type = ['area', 'area', 'area', 'line'];
 const scrape_interval = ['2m', '30s', '30s', '30s'];
 
-/**
- * Factory function to create instances of the `Dashboard` model with predefined properties and values.
- *
- * @param {string} dashboardLabel - The label to assign to the dashboard instance.
- * @param {string[]} widgetLabels - An array of labels for widgets to be included in the dashboard.
- * @param {string[]} metricsLabels - An array of labels for metrics associated with the widgets.
- * @param {string[]} y_labels - An array of Y-axis labels for the metrics.
- * @param {string} service - The type of service to be assigned to the dashboard.
- *
- * @returns {Factory<Dashboard>} A Factory instance for creating `Dashboard` objects with the specified properties.
- */
-
 export const dashboardFactory = Factory.Sync.makeFactory<Dashboard>({
   created: new Date().toISOString(),
   id: Factory.each((i) => i),
@@ -36,15 +24,7 @@ export const dashboardFactory = Factory.Sync.makeFactory<Dashboard>({
   updated: new Date().toISOString(),
   widgets: [],
 });
-/**
- * Factory function to create instances of the `Widgets` model with predefined properties and values.
- *
- * @param {string[]} widgetLabels - An array of labels for widgets to be created.
- * @param {string[]} metricsLabels - An array of metrics labels associated with each widget.
- * @param {string[]} y_labels - An array of Y-axis labels for the metrics.
- *
- * @returns {Factory<Widgets>} A Factory instance for creating `Widgets` objects with the specified properties.
- */
+
 export const widgetFactory = Factory.Sync.makeFactory<Widgets>({
   aggregate_function: 'avg',
   chart_type: Factory.each((i) => chart_type[i % chart_type.length]),
@@ -70,22 +50,13 @@ export const widgetFactory = Factory.Sync.makeFactory<Widgets>({
   unit: 'defaultUnit',
   y_label: Factory.each((i) => `y_label_${i}`),
 });
-/**
- * Factory function to create instances of the `AvailableMetrics` model with predefined properties and values.
- *
- * @param {string[]} widgetLabels - An array of labels for widgets that will be associated with the metrics.
- * @param {string[]} metricsLabels - An array of labels for metrics to be used in the `AvailableMetrics` instances.
- *
- * @returns {Factory<AvailableMetrics>} A Factory instance for creating `AvailableMetrics` objects with the specified properties.
- *
- */
 
 export const dashboardMetricFactory =
   Factory.Sync.makeFactory<AvailableMetrics>({
     available_aggregate_functions: ['min', 'max', 'avg', 'sum'],
     dimensions: [],
-    label: Factory.each((i) => `widget_label_${i}`), // This might be overridden
-    metric: Factory.each((i) => `widget_metric_${i}`), // This might be overridden
+    label: Factory.each((i) => `widget_label_${i}`),
+    metric: Factory.each((i) => `widget_metric_${i}`),
     metric_type: 'gauge',
     scrape_interval: Factory.each(
       (i) => scrape_interval[i % scrape_interval.length]
@@ -93,25 +64,23 @@ export const dashboardMetricFactory =
     unit: 'defaultUnit',
   });
 
-// Factory for CloudPulseMetricsResponseData
 export const cloudPulseMetricsResponseDataFactory =
   Factory.Sync.makeFactory<CloudPulseMetricsResponseData>({
     result: [
       {
         metric: {},
-        values: [], // Generate 10 data points with 1-minute interval
+        values: [],
       },
     ],
-    result_type: 'matrix', // Default result type
+    result_type: 'matrix',
   });
 
-// Factory for CloudPulseMetricsResponse
 export const cloudPulseMetricsResponseFactory =
   Factory.Sync.makeFactory<CloudPulseMetricsResponse>({
-    data: cloudPulseMetricsResponseDataFactory.build(), // Use the data factory here
+    data: cloudPulseMetricsResponseDataFactory.build(),
     isPartial: false,
     stats: {
-      series_fetched: 2, // Adjust based on the number of metrics
+      series_fetched: 2,
     },
     status: 'success',
   });
