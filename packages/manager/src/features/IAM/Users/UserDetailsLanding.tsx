f97f74a85cec8b4e03c6b5973f31a94c1aba27cc
Anastasiia Alekseenko <aaleksee@akamai.com>
2024-10-07 14:54:10 +0200

    feat: [UIE-8133] - add new permissions component

--- packages/manager/src/features/IAM/Users/UserDetailsLanding.tsx
+++ packages/manager/src/features/IAM/Users/UserDetailsLanding.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import {
+  matchPath,
   useHistory,
   useLocation,
   useParams,
-  matchPath,
 } from 'react-router-dom';
+
 import { LandingHeader } from 'src/components/LandingHeader';
 import { SafeTabPanel } from 'src/components/Tabs/SafeTabPanel';
 import { TabLinkList } from 'src/components/Tabs/TabLinkList';
 import { TabPanels } from 'src/components/Tabs/TabPanels';
 import { Tabs } from 'src/components/Tabs/Tabs';
+import { Permissions } from 'src/features/IAM/Shared/Permissions/Permissions';
+
 import { IAM_LABEL } from '../Shared/constants';
+
+import type { PermissionType } from '@linode/api-v4/lib/iam/types';
+
+// just for demonstaring the Permissions component.
+// it will be gone with the AssignedPermissions Component in the next PR
+const mockPermissionsLong: PermissionType[] = [
+  'create_nodebalancer',
+  'list_nodebalancers',
+  'view_nodebalancer',
+  'list_nodebalancer_firewalls',
+  'view_nodebalancer_statistics',
+  'list_nodebalancer_configs',
+  'view_nodebalancer_config',
+  'list_nodebalancer_config_nodes',
+  'view_nodebalancer_config_node',
+  'update_nodebalancer',
+  'add_nodebalancer_config',
+  'update_nodebalancer_config',
+  'rebuild_nodebalancer_config',
+  'add_nodebalancer_config_node',
+  'update_nodebalancer_config_node',
+  'delete_nodebalancer',
+  'delete_nodebalancer_config',
+  'delete_nodebalancer_config_node',
+];
 
 export const UserDetailsLanding = () => {
   const { username } = useParams<{ username: string }>();
@@ -37,11 +65,9 @@ export const UserDetailsLanding = () => {
   };
 
   const getDefaultTabIndex = () => {
-    const tabChoice = tabs.findIndex((tab) =>
+    return tabs.findIndex((tab) =>
       Boolean(matchPath(tab.routeName, { path: location.pathname }))
     );
-
-    return tabChoice;
   };
 
   let idx = 0;
@@ -72,6 +98,27 @@ export const UserDetailsLanding = () => {
           </SafeTabPanel>
           <SafeTabPanel index={++idx}>
             <p>UIE-8138 - User Roles - Assigned Roles Table</p>
+            <div
+              style={{
+                outline: '1px dashed',
+                paddingLeft: '42px',
+                paddingRight: '10px',
+                width: 390,
+              }}
+            >
+              <Permissions permissions={mockPermissionsLong} />
+            </div>
+
+            <div
+              style={{
+                outline: '1px dashed',
+                paddingLeft: '32px',
+                paddingRight: '10px',
+                width: 1190,
+              }}
+            >
+              <Permissions permissions={mockPermissionsLong} />
+            </div>
           </SafeTabPanel>
           <SafeTabPanel index={++idx}>
             <p>UIE-8139 - User Roles - Resources Table</p>
