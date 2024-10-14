6d618c646806198c9dd4434f1c13867a18bcc056
hasyed-akamai <hasyed@akamai.com>
2024-10-14 12:11:17 +0530

    feat: [M3-8703] - Disable Create VPC button with tooltip text on empty state Landing Page for restricted users (#11052)
    * feat: [M3-8703] - Disable Create VPC button with tooltip text on empty state Landing Page for restricted users
    
    * Added changeset: Disable Create VPC button with tooltip text on empty state Landing Page for restricted users ([#11052](https://github.com/linode/manager/pull/11052))
    
    * Added changeset: Disable Create VPC button with tooltip text on empty state Landing Page for restricted users
    
    * Changed changeset: Disable Create VPC button with tooltip text on empty state Landing Page for restricted users
    
    * refactor: [M3-8703] Making variables more descriptive

--- packages/manager/src/features/VPCs/VPCLanding/VPCEmptyState.tsx
+++ packages/manager/src/features/VPCs/VPCLanding/VPCEmptyState.tsx
@@ -4,6 +4,8 @@ import { useHistory } from 'react-router-dom';
 import VPC from 'src/assets/icons/entityIcons/vpc.svg';
 import { ResourcesSection } from 'src/components/EmptyLandingPageResources/ResourcesSection';
 import { gettingStartedGuides } from 'src/features/VPCs/VPCLanding/VPCLandingEmptyStateData';
+import { useRestrictedGlobalGrantCheck } from 'src/hooks/useRestrictedGlobalGrantCheck';
+import { getRestrictedResourceText } from 'src/features/Account/utils';
 import { sendEvent } from 'src/utilities/analytics/utils';
 
 import { headers, linkAnalyticsEvent } from './VPCEmptyStateData';
@@ -11,11 +13,16 @@ import { headers, linkAnalyticsEvent } from './VPCEmptyStateData';
 export const VPCEmptyState = () => {
   const { push } = useHistory();
 
+  const isVPCCreationRestricted = useRestrictedGlobalGrantCheck({
+    globalGrantType: 'add_vpcs',
+  });
+
   return (
     <ResourcesSection
       buttonProps={[
         {
           children: 'Create VPC',
+          disabled: isVPCCreationRestricted,
           onClick: () => {
             sendEvent({
               action: 'Click:button',
@@ -24,6 +31,11 @@ export const VPCEmptyState = () => {
             });
             push('/vpcs/create');
           },
+          tooltipText: getRestrictedResourceText({
+            action: 'create',
+            isSingular: false,
+            resourceType: 'VPCs',
+          }),
         },
       ]}
       gettingStartedGuidesData={gettingStartedGuides}
