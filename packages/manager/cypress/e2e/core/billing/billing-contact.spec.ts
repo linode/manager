887be4094c51b3e92cb4e231628b1fa2f6e8590f
Joe D'Amore <jdamore@linode.com>
2024-10-17 14:02:37 -0400

    Remove forced test failure

--- packages/manager/cypress/e2e/core/billing/billing-contact.spec.ts
+++ packages/manager/cypress/e2e/core/billing/billing-contact.spec.ts
@@ -73,7 +73,6 @@ describe('Billing Contact', () => {
     });
   });
   it('Edit Contact Info', () => {
-    throw new Error('Forcing this to fail temporarily sorry');
     // mock the user's account data and confirm that it is displayed correctly upon page load
     mockGetAccount(accountData).as('getAccount');
     cy.visitWithLogin('/account/billing');
