635227ea6894b8e8a068454683903c8a09dfcf36
Alban Bailly <abailly@akamai.com>
2024-10-23 17:12:14 -0400

    add missing keyboard sensor

--- packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallRuleTable.tsx
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallRuleTable.tsx
@@ -1,6 +1,7 @@
 import {
   DndContext,
   MouseSensor,
+  KeyboardSensor,
   PointerSensor,
   TouchSensor,
   closestCorners,
@@ -162,7 +163,8 @@ export const FirewallRuleTable = (props: FirewallRuleTableProps) => {
         distance: 4,
       },
     }),
-    useSensor(TouchSensor)
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor)
   );
 
   return (
