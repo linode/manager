72014716b004be98f21cc3118f91c46e65a34230
cpathipa <119517080+cpathipa@users.noreply.github.com>
2024-10-24 10:26:14 -0500

    Remove feature flag, tracking events used for A/B testing in API CLI Tools Modal

--- packages/manager/src/features/Linodes/LinodeCreate/ApiAwarenessModal/GoSDKResources.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/ApiAwarenessModal/GoSDKResources.tsx
@@ -1,11 +1,7 @@
-import { useLDClient } from 'launchdarkly-react-client-sdk';
 import React from 'react';
 
 import { ResourceLinks } from 'src/components/EmptyLandingPageResources/ResourcesLinks';
 import { Typography } from 'src/components/Typography';
-import { LD_DX_TOOLS_METRICS_KEYS } from 'src/constants';
-import { useFlags } from 'src/hooks/useFlags';
-import { useIsAkamaiAccount } from 'src/hooks/useIsAkamaiAccount';
 
 import type { ResourcesLinks } from 'src/components/EmptyLandingPageResources/ResourcesLinksTypes';
 
@@ -26,21 +22,6 @@ export const gettingStartedGuides: ResourcesLinks['links'] = [
 ];
 
 export const GoSDKResources = () => {
-  const ldClient = useLDClient();
-  const flags = useFlags();
-  const { isAkamaiAccount: isInternalAccount } = useIsAkamaiAccount();
-
-  const apicliButtonCopy = flags?.testdxtoolabexperiment;
-
-  const handleClick = () => {
-    if (!isInternalAccount) {
-      ldClient?.track(LD_DX_TOOLS_METRICS_KEYS.SDK_GO_RESOURCE_LINKS, {
-        variation: apicliButtonCopy,
-      });
-    }
-
-    ldClient?.flush();
-  };
   return (
     <>
       <Typography sx={(theme) => ({ mt: theme.spacing(2) })} variant="h3">
@@ -49,7 +30,6 @@ export const GoSDKResources = () => {
       <ResourceLinks
         linkAnalyticsEvent={linkAnalyticsEvent}
         links={gettingStartedGuides}
-        onClick={handleClick}
       />
     </>
   );
