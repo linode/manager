ec4b7803fdf0eef418ffba07037c8d38497d9ffe
Alban Bailly <abailly@akamai.com>
2024-10-31 15:49:44 -0400

    Added changeset: Migrate `/volumes` to Tanstack router

--- packages/manager/src/routes/volumes/index.ts
+++ packages/manager/src/routes/volumes/index.ts
@@ -43,7 +43,7 @@ export const buildVolumeXFilters = ({
   query,
 }: BuildVolumeXFiltersParams) =>
   buildXFilters({
-    additionalFilters: query
+    nonPaginationFilters: query
       ? {
           label: { '+contains': query },
         }
