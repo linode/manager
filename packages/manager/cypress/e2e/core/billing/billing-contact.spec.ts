c354b173a1bd54d908294b6762a68bd6b29e3a9f
Joe D'Amore <jdamore@akamai.com>
2024-10-31 15:25:01 -0400

    Undo forced test failure

--- packages/manager/cypress/e2e/core/billing/billing-contact.spec.ts
+++ packages/manager/cypress/e2e/core/billing/billing-contact.spec.ts
@@ -73,7 +73,6 @@ describe('Billing Contact', () => {
     });
   });
   it('Edit Contact Info', () => {
-    throw new Error('This is a test failure!');
     // mock the user's account data and confirm that it is displayed correctly upon page load
     mockGetAccount(accountData).as('getAccount');
     cy.visitWithLogin('/account/billing');
