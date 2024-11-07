7ffae3075ca0de5d923aec21efe7fdc788628009
hrao <hrao@akamai.com>
2024-11-07 12:14:30 +0530

    updated the import for omittedProps

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './BetaChip';
 export * from './Box';
 export * from './Chip';
+export * from './Divider';
 export * from './FormControl';
 export * from './FormHelperText';
 export * from './IconButton';
@@ -9,5 +10,4 @@ export * from './InputAdornment';
 export * from './InputLabel';
 export * from './Paper';
 export * from './Tooltip';
-export * from './Divider';
 export * from './VisibilityTooltip';
