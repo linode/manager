ea32af867c5a100ff969c1e092d10ad158fb3bc2
vmangalr <vmangalr@akamai.com>
2024-11-07 19:25:15 +0530

    upcoming: [DI-21811] - comment updates

--- packages/manager/src/features/CloudPulse/Utils/CloudPulseWidgetUtils.ts
+++ packages/manager/src/features/CloudPulse/Utils/CloudPulseWidgetUtils.ts
@@ -357,7 +357,7 @@ export const fillMissingTimeStampsAcrossDimensions = (...arraysToBeFilled: [numb
   // Step 1: Collect all unique keys from all arrays
   const allTimestamps = new Set<number>();
 
-  // Collect keys from each array
+  // Collect timeStamps from each array, array[0], contains the number timestamp
   arraysToBeFilled.forEach(array => {
     array.forEach(([timeStamp]) => allTimestamps.add(timeStamp));
   });
