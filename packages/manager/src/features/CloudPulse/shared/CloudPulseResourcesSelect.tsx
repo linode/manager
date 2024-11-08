9a7695357f5684751be257954de6d477e4a99db5
ankitaakamai <ankitaan@akamai.com>
2024-11-08 20:55:23 +0530

    upcoming: [DI-21814] - ACLP UI - DBaaS instances order by label (#11226)
    * upcoming: [DI-21814] - DBaaS instances order by label
    
    * upcoming: [DI-21814] - Added changeset
    
    * DI-21814: use map for better readability and optimisations
    
    ---------
    
    Co-authored-by: vmangalr <vmangalr@akamai.com>

--- packages/manager/src/features/CloudPulse/shared/CloudPulseResourcesSelect.tsx
+++ packages/manager/src/features/CloudPulse/shared/CloudPulseResourcesSelect.tsx
@@ -43,8 +43,9 @@ export const CloudPulseResourcesSelect = React.memo(
       xFilter,
     } = props;
 
-    const platformFilter =
-      resourceType === 'dbaas' ? { platform: 'rdbms-default' } : {};
+    const resourceFilterMap: Record<string, Filter> = {
+      dbaas: { '+order': 'asc', '+order_by': 'label', platform: 'rdbms-default' },
+    };
 
     const { data: resources, isLoading, isError } = useResourcesQuery(
       disabled !== undefined ? !disabled : Boolean(region && resourceType),
@@ -52,13 +53,13 @@ export const CloudPulseResourcesSelect = React.memo(
       {},
       xFilter
         ? {
-            ...platformFilter,
-            ...xFilter,
-          }
+          ...(resourceFilterMap[resourceType ?? ''] ?? {}),
+          ...xFilter, // the usual xFilters
+        }
         : {
-            ...platformFilter,
-            region,
-          }
+          ...(resourceFilterMap[resourceType ?? ''] ?? {}),
+          region,
+        }
     );
 
     const [selectedResources, setSelectedResources] = React.useState<
