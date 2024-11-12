f43cca65b5b59238a10a2e95f0399205be81dc87
pmakode-akamai <pmakode@akamai.com>
2024-11-12 20:48:40 +0530

    Add `vite-plugin-svgr` plugin to UI package

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, UserConfig } from 'vitest/config';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  plugins: [svgr({ exportAsDefault: true })] as UserConfig['plugins'],
   test: {
     environment: 'jsdom',
     setupFiles: './testSetup.ts',
