f46fa3f79b757b2544da2d4e0e48e9695500b575
pmakode-akamai <pmakode@akamai.com>
2024-11-18 14:11:53 +0530

    Fix conflicts and reorder imports

--- packages/manager/src/components/Tag/Tag.styles.ts
+++ packages/manager/src/components/Tag/Tag.styles.ts
@@ -1,4 +1,4 @@
-import { StyledLinkButton, omittedProps, Chip } from '@linode/ui';
+import { Chip, StyledLinkButton, omittedProps } from '@linode/ui';
 import { styled } from '@mui/material/styles';
 
 import type { TagProps } from './Tag';
