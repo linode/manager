cf84d85adb3e4f6b80e1ac33519b735c1737fbdb
hasyed-akamai <hasyed@akamai.com>
2024-11-26 10:40:58 +0530

    refactor: [M3-8639] - Move `Accordion` to `linode/ui` package (#11316)
    * refactor: [M3-8639] - Move `Accordion` to `linode/ui` package
    
    * Added changeset: Move `Accordion` from `manager` to `ui` package
    
    * Added changeset: Move `Accordion` from `manager` to `ui` package
    
    * Change import location for `TypographyProps`
    
    Co-authored-by: Harsh Shankar Rao <hrao@akamai.com>
    
    ---------
    
    Co-authored-by: Harsh Shankar Rao <hrao@akamai.com>

--- packages/manager/src/features/Account/EnableManaged.tsx
+++ packages/manager/src/features/Account/EnableManaged.tsx
@@ -1,10 +1,9 @@
 import { enableManaged } from '@linode/api-v4/lib/managed';
-import { Button, Typography } from '@linode/ui';
+import { Accordion, Button, Typography } from '@linode/ui';
 import Grid from '@mui/material/Unstable_Grid2';
 import { useQueryClient } from '@tanstack/react-query';
 import * as React from 'react';
 
-import { Accordion } from 'src/components/Accordion';
 import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
 import { ConfirmationDialog } from 'src/components/ConfirmationDialog/ConfirmationDialog';
 import { Link } from 'src/components/Link';
