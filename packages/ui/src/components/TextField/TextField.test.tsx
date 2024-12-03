b317a03ff8d0dd3c15e62aba8e969bb005606779
Alban Bailly <abailly@akamai.com>
2024-12-02 22:04:28 -0500

    cleanup

--- packages/ui/src/components/TextField/TextField.test.tsx
+++ packages/ui/src/components/TextField/TextField.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, getDefaultNormalizer } from '@testing-library/react';
 import * as React from 'react';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import { renderWithTheme } from '../../utilities/testHelpers';
 import { InputAdornment } from '../InputAdornment';
