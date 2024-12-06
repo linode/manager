4f52ff5a29455f39fb24ee8b5e06b0bda133ecb4
Alban Bailly <abailly@akamai.com>
2024-12-06 12:04:02 -0500

    save progress

--- packages/ui/src/components/Select/Select.stories.tsx
+++ packages/ui/src/components/Select/Select.stories.tsx
@@ -1,26 +1,116 @@
 import React from 'react';
 
+import { Box } from '../Box';
+import { Typography } from '../Typography';
 import { Select } from './Select';
 
+import type { SelectProps } from './Select';
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof Select> = {
+const meta: Meta<SelectProps> = {
   component: Select,
+  decorators: [(Story) => <Box sx={{ height: 300 }}>{Story()}</Box>],
   title: 'Components/Selects/Select',
 };
 
-type Story = StoryObj<typeof Select>;
+type Story = StoryObj<SelectProps>;
+
+const defaultArgs: SelectProps = {
+  label: 'A Select with a couple options',
+  options: [
+    { label: 'Option 1', value: 'option-1' },
+    { label: 'Option 2', value: 'option-2' },
+  ],
+  placeholder: 'Select an option',
+};
 
 export const Default: Story = {
+  args: defaultArgs,
+  render: (args) => <Select {...args} />,
+};
+
+export const Searchable: Story = {
   args: {
-    label: 'A Select with a couple options',
-    options: [
-      { label: 'Option 1', value: 'option-1' },
-      { label: 'Option 2', value: 'option-2' },
-    ],
-    placeholder: 'Select an option',
+    ...defaultArgs,
+    searchable: true,
   },
   render: (args) => <Select {...args} />,
+};
+
+export const Required: Story = {
+  args: {
+    ...defaultArgs,
+    required: true,
+  },
+  render: (args) => <Select {...args} />,
+};
+
+export const Clearable: Story = {
+  args: {
+    ...defaultArgs,
+    clearable: true,
+  },
+  render: (args) => <Select {...args} />,
+};
+
+export const Loading: Story = {
+  args: {
+    ...defaultArgs,
+    loading: true,
+  },
+  render: (args) => <Select {...args} />,
+};
+
+export const WithHelperText: Story = {
+  args: {
+    ...defaultArgs,
+    helperText: 'This is some helper text',
+  },
+  render: (args) => <Select {...args} />,
+};
+
+export const WithErrorText: Story = {
+  args: {
+    ...defaultArgs,
+    errorText: 'This is some error text',
+  },
+  render: (args) => <Select {...args} />,
+};
+
+export const Creatable: Story = {
+  args: {
+    ...defaultArgs,
+    creatable: true,
+  },
+  render: (args) => {
+    const Wrapper = () => {
+      const [value, setValue] = React.useState(args.value);
+
+      return (
+        <>
+          <Select
+            {...args}
+            textFieldProps={{
+              onChange: (e) =>
+                setValue({
+                  label: e.target.value,
+                  value: e.target.value.replace(' ', '-').toLowerCase(),
+                }),
+            }}
+            onChange={(_, newValue) => setValue(newValue)}
+            value={value}
+          />
+          <Box sx={{ mt: 2 }}>
+            <Typography>
+              <strong>Selected Value: </strong> {JSON.stringify(value)}
+            </Typography>
+          </Box>
+        </>
+      );
+    };
+
+    return <Wrapper />;
+  },
 };
 
 export default meta;
--- packages/ui/src/components/Select/Select.tsx
+++ packages/ui/src/components/Select/Select.tsx
@@ -3,50 +3,124 @@ import React from 'react';
 
 import { Autocomplete } from '../Autocomplete';
 import { Box } from '../Box';
+import { TextField } from '../TextField';
 
 import type { EnhancedAutocompleteProps } from '../Autocomplete';
+import type { BoxProps } from '../Box';
 
 type OptionType = { label: string; value: string };
 
-type SelectProps = Pick<
-  EnhancedAutocompleteProps<OptionType>,
-  | 'errorText'
-  | 'helperText'
-  | 'label'
-  | 'onChange'
-  | 'options'
-  | 'placeholder'
-  | 'textFieldProps'
-  | 'value'
->;
+export interface SelectProps
+  extends Pick<
+    EnhancedAutocompleteProps<OptionType>,
+    | 'errorText'
+    | 'helperText'
+    | 'loading'
+    | 'options'
+    | 'placeholder'
+    | 'textFieldProps'
+    | 'value'
+  > {
+  clearable?: boolean;
+  creatable?: boolean;
+  hideLabel?: boolean;
+  label: string;
+  onChange?: (event: React.SyntheticEvent, value: OptionType | null) => void;
+  required?: boolean;
+  searchable?: boolean;
+}
 
 /**
- * An abstracted Autocomplete component with some props overridden for convenience and consistency.
+ * An abstracted Autocomplete component with a limited set of props.
+ * This component is meant to be used when wanting:
+ * - A simple select without search ability
+ * - A create-able select
+ *
+ * For any other use-cases, use the Autocomplete component directly.
  */
 export const Select = (props: SelectProps) => {
-  const { textFieldProps, ...rest } = props;
+  const {
+    clearable = false,
+    creatable = false,
+    hideLabel = false,
+    label,
+    onChange,
+    searchable = false,
+    textFieldProps,
+    ...rest
+  } = props;
+
+  const handleChange = (
+    event: React.SyntheticEvent,
+    value: OptionType | null | string
+  ) => {
+    if (creatable && typeof value === 'string') {
+      onChange?.(event, {
+        label: value,
+        value: value.toLowerCase().replace(/\s+/g, '-'),
+      });
+    } else {
+      onChange?.(event, value as OptionType | null);
+    }
+  };
 
   return (
-    <SelectContainer>
+    <SelectContainer creatable={creatable}>
       <Autocomplete
         {...rest}
-        textFieldProps={{
-          ...textFieldProps,
-          inputProps: {
-            ...textFieldProps?.inputProps,
-            readOnly: true,
-          },
-        }}
+        renderInput={
+          creatable
+            ? (params) => (
+                <TextField
+                  {...params}
+                  {...textFieldProps}
+                  errorText={props.errorText}
+                  helperText={props.helperText}
+                  hideLabel={hideLabel}
+                  inputId={params.id}
+                  label={label}
+                  placeholder={props.placeholder}
+                  required={props.required}
+                />
+              )
+            : undefined
+        }
+        textFieldProps={
+          !creatable
+            ? {
+                ...textFieldProps,
+                InputProps: {
+                  ...textFieldProps?.InputProps,
+                  required: props.required,
+                },
+                hideLabel,
+                inputProps: {
+                  ...textFieldProps?.inputProps,
+                  readOnly: !searchable,
+                },
+              }
+            : undefined
+        }
+        disableClearable={!clearable}
+        freeSolo={creatable}
+        label={label}
+        onChange={handleChange}
       />
     </SelectContainer>
   );
 };
 
-const SelectContainer = styled(Box)(({}) => ({
+interface SelectContainerProps extends BoxProps {
+  creatable?: boolean;
+}
+
+const SelectContainer = styled(Box)<SelectContainerProps>(({ creatable }) => ({
   '& .MuiInputBase-root, & .MuiInputBase-input': {
-    '&::selection': {
-      backgroundColor: 'transparent',
-    },
+    ...(!creatable && {
+      '&::selection': {
+        backgroundColor: 'transparent !important',
+      },
+    }),
     cursor: 'pointer',
   },
 }));
