e92e6f4a0a57010d31f27f6a6102eeaf1883aa41
carrillo-erik <119514965+carrillo-erik@users.noreply.github.com>
2024-12-10 09:27:44 -0800

    fix: [M3-8538] - Incorrect Commands in Linode CLI Modal (#11303)
    * fix: [M3-8538] - Fix Linode clone CLI Modal
    
    * remove comments
    
    * Fix cli command
    
    * Fix typos and unit tests and add missing slash in api url
    
    * Add changeset

--- packages/manager/src/features/Linodes/LinodeCreate/ApiAwarenessModal/ApiAwarenessModal.test.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/ApiAwarenessModal/ApiAwarenessModal.test.tsx
@@ -2,10 +2,11 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-import { renderWithTheme } from 'src/utilities/testHelpers';
+import { renderWithThemeAndHookFormContext } from 'src/utilities/testHelpers';
 
 import { ApiAwarenessModal } from './ApiAwarenessModal';
 
+import type { LinodeCreateFormValues } from '../utilities';
 import type { ApiAwarenessModalProps } from './ApiAwarenessModal';
 
 const defaultProps: ApiAwarenessModalProps = {
@@ -19,19 +20,22 @@ const renderComponent = (overrideProps?: Partial<ApiAwarenessModalProps>) => {
     ...defaultProps,
     ...overrideProps,
   };
-  return renderWithTheme(<ApiAwarenessModal {...props} />);
+
+  return renderWithThemeAndHookFormContext<LinodeCreateFormValues>({
+    component: <ApiAwarenessModal {...props} />,
+  });
 };
 
 describe('ApiAwarenessModal', () => {
-  it('Should not render ApiAwarenessModal componet', () => {
+  it('Should not render ApiAwarenessModal component', () => {
     renderComponent();
     expect(screen.queryByText('Create Linode')).not.toBeInTheDocument();
   });
-  it('Should render ApiAwarenessModal componet when enabled', () => {
+  it('Should render ApiAwarenessModal component when enabled', () => {
     renderComponent({ isOpen: true });
     screen.getByText('Create Linode');
   });
-  it('Should invoke onClose handler upon cliking close button', async () => {
+  it('Should invoke onClose handler upon clicking close button', async () => {
     renderComponent({ isOpen: true });
     await userEvent.click(screen.getByTestId('close-button'));
     expect(defaultProps.onClose).toHaveBeenCalledTimes(1);
