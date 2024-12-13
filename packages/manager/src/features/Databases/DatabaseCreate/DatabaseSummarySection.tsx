7cac6560a3e3f294a174d3d0f73f694e21c9dc81
mpolotsk <mpolotsk@akamai.com>
2024-12-13 14:41:22 +0100

    fix: [UIE-8228] - Database Resize: review fix

--- packages/manager/src/features/Databases/DatabaseCreate/DatabaseSummarySection.tsx
+++ packages/manager/src/features/Databases/DatabaseCreate/DatabaseSummarySection.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { StyledPlanSummarySpan } from '../DatabaseDetail/DatabaseResize/DatabaseResize.style';
 import { useIsDatabasesEnabled } from '../utilities';
 import { StyledSpan } from './DatabaseCreate.style';
+import { getSuffix } from './utilities';
 
 import type {
   ClusterSize,
@@ -67,22 +68,14 @@ export const DatabaseSummarySection = (props: Props) => {
       )}
       <Typography component="span">
         {currentClusterSize} Node
-        {currentClusterSize > 1 ? 's - HA ' : ' '}
+        {getSuffix(isNewDatabase, currentClusterSize)}
       </Typography>
       {currentNodePrice}
     </Box>
   ) : (
     'Please specify your cluster configuration'
   );
-  const getSuffix = (numberOfNodes: number) => {
-    let suffix = '';
-    if (numberOfNodes > 1) {
-      suffix = isNewDatabase ? 's - HA ' : 's: ';
-    } else {
-      suffix = isNewDatabase ? ' ' : ': ';
-    }
-    return suffix;
-  };
+
   const resizeSummary = (
     <Box
       sx={(theme: Theme) => ({
@@ -98,16 +91,9 @@ export const DatabaseSummarySection = (props: Props) => {
               : resizeData.plan}
           </StyledPlanSummarySpan>{' '}
           {isNewDatabase && <StyledSpan>{resizeData.basePrice}</StyledSpan>}
-          <Typography
-            sx={
-              isNewDatabase
-                ? (theme) => ({ marginRight: theme.spacing(1) })
-                : null
-            }
-            component="span"
-          >
+          <Typography component="span">
             {resizeData.numberOfNodes} Node
-            {getSuffix(resizeData.numberOfNodes)}
+            {getSuffix(isNewDatabase, resizeData.numberOfNodes)}
           </Typography>
           {resizeData.price}
         </>
