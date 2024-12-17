c1241a2384860349c7ceb04e93c0a23394c11c99
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2024-12-17 22:54:39 +0530

    upcoming: [DI-22184] - Added Metric Criteria, Metric components (#11392)
    * upcoming: [DI-22184] - Added Metric, MetricCriteria components with Unit Tests and minor changes to align with changes in API spec
    
    * upcoming: [DI-22184] - Internal Review Changes: Assertion checks in Unit tests, added a type for the Form, renamed prop
    
    * upcoming: [DI-22184] - Removing few initial values used for internal testing
    
    * upcoming: [DI-22184] - Fixing previoius accidental commit
    
    * upcoming: [DI-22184] - Made the Clear icon a separate Component with Unit Test
    
    * upcoming: [DI-22184] - Added changesets
    
    * upcoming: [DI-22184] - Review changes: replaced watch with useWatch, removed non-null assertion and using default values while filtering form values, modified code to include just side effects in the useEffect in MetricCriteria component
    
    * upcoming: [DI-22184] - Removed braces where values where plain strings
    
    * upcoming: [DI-22184] - Renamed the variable for error and loading state for better clarity
    
    * upcoming: [DI-22184] - Display maxScrapeInterval to pass the typecheck-manager test

--- packages/manager/src/features/CloudPulse/Alerts/CreateAlert/utilities.ts
+++ packages/manager/src/features/CloudPulse/Alerts/CreateAlert/utilities.ts
@@ -15,18 +15,48 @@ export const filterFormValues = (
     'region',
     'engineType',
     'severity',
+    'rule_criteria',
   ]);
   // severity has a need for null in the form for edge-cases, so null-checking and returning it as an appropriate type
-  const severity = formValues.severity!;
+  const severity = formValues.severity ?? 1;
   const entityIds = formValues.entity_ids;
-  return { ...values, entity_ids: entityIds, severity };
+  const rules = formValues.rule_criteria.rules;
+  return {
+    ...values,
+    entity_ids: entityIds,
+    rule_criteria: { rules: filterMetricCriteriaFormValues(rules) },
+    severity,
+  };
 };
 
 export const filterMetricCriteriaFormValues = (
-  formValues: MetricCriteriaForm
+  formValues: MetricCriteriaForm[]
 ): MetricCriteria[] => {
-  const aggregationType = formValues.aggregation_type!;
-  const operator = formValues.operator!;
-  const values = omitProps(formValues, ['aggregation_type', 'operator']);
-  return [{ ...values, aggregation_type: aggregationType, operator }];
+  return formValues.map((rule) => {
+    const values = omitProps(rule, ['aggregation_type', 'operator', 'metric']);
+    return {
+      ...values,
+      aggregation_type: rule.aggregation_type ?? 'avg',
+      dimension_filters: rule.dimension_filters,
+      metric: rule.metric ?? '',
+      operator: rule.operator ?? 'eq',
+    };
+  });
+};
+
+export const convertToSeconds = (secondsList: string[]) => {
+  return secondsList.map((second) => {
+    const unit = second.slice(-1)[0];
+    const number = parseInt(second.slice(0, -1), 10);
+    switch (unit) {
+      case 's':
+        return number;
+      case 'm':
+        return number * 60;
+      case 'h':
+        return number * 3600;
+      default:
+        return number * 0;
+    }
+  });
 };
