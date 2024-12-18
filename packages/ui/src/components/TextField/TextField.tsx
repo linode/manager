2baabcb895b78788d9730d2135b2552b5a0e7613
Jaalah Ramos <jaalah.ramos@gmail.com>
2024-12-17 22:19:07 -0500

    Add ability to use sx prop for helper text

--- packages/ui/src/components/TextField/TextField.tsx
+++ packages/ui/src/components/TextField/TextField.tsx
@@ -13,6 +13,7 @@ import { TooltipIcon } from '../TooltipIcon';
 
 import type { BoxProps } from '../Box';
 import type { TooltipProps } from '../Tooltip';
+import type { SxProps } from '@mui/material/styles';
 import type { StandardTextFieldProps } from '@mui/material/TextField';
 
 interface BaseProps {
@@ -93,6 +94,10 @@ interface BaseProps {
    */
   required?: boolean;
   /**
+   * Custom styles for the helper text
+   */
+  sxHelperText?: SxProps;
+  /**
    * The leading and trailing spacing should be trimmed from the textfield on blur; intended to be used for username, emails, and SSH key input only
    */
   trimmed?: boolean;
@@ -153,6 +158,7 @@ export const TextField = (props: TextFieldProps) => {
     onChange,
     optional,
     required,
+    sxHelperText,
     tooltipClasses,
     tooltipOnMouseEnter,
     tooltipPosition,
@@ -313,6 +319,7 @@ export const TextField = (props: TextFieldProps) => {
       {helperText && helperTextPosition === 'top' && (
         <FormHelperText
           sx={{
+            ...sxHelperText,
             marginTop: 0,
           }}
           data-qa-textfield-helper-text
