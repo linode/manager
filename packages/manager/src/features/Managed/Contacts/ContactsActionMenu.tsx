26847311924672fa091273f3b16ab8c961497194
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2024-12-19 11:42:48 -0800

    fix: [M3-9021] - Various bugs in Managed tables (#11431)
    * Fix loading state on small screens
    
    * Fix column mismatches on smaller screens
    
    * Use action menu list on Contacts table with small screens
    
    * Remove odd left padding on Monitor table column
    
    * Fix Monitor table loading columns; clean up
    
    * More clean up
    
    * Fix SSH Access table loading state
    
    * Added changeset: Various bugs in Managed tables

--- packages/manager/src/features/Managed/Contacts/ContactsActionMenu.tsx
+++ packages/manager/src/features/Managed/Contacts/ContactsActionMenu.tsx
@@ -1,7 +1,11 @@
+import { useMediaQuery } from '@mui/material';
 import * as React from 'react';
 
-import { Action } from 'src/components/ActionMenu/ActionMenu';
+import { ActionMenu } from 'src/components/ActionMenu/ActionMenu';
 import { InlineMenuAction } from 'src/components/InlineMenuAction/InlineMenuAction';
+
+import type { Theme } from '@mui/material';
+import type { Action } from 'src/components/ActionMenu/ActionMenu';
 
 interface ContactsActionMenuProps {
   contactId: number;
@@ -11,7 +15,10 @@ interface ContactsActionMenuProps {
 }
 
 export const ContactsActionMenu = (props: ContactsActionMenuProps) => {
-  const { contactId, openDialog, openDrawer } = props;
+  const { contactId, contactName, openDialog, openDrawer } = props;
+  const matchesSmDown = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down('md')
+  );
 
   const actions: Action[] = [
     {
@@ -31,15 +38,22 @@ export const ContactsActionMenu = (props: ContactsActionMenuProps) => {
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      {actions.map((action) => {
-        return (
-          <InlineMenuAction
-            actionText={action.title}
-            key={action.title}
-            onClick={action.onClick}
-          />
-        );
-      })}
+      {matchesSmDown ? (
+        <ActionMenu
+          actionsList={actions}
+          ariaLabel={`Action menu for Managed Contacts for ${contactName}`}
+        />
+      ) : (
+        actions.map((action) => {
+          return (
+            <InlineMenuAction
+              actionText={action.title}
+              key={action.title}
+              onClick={action.onClick}
+            />
+          );
+        })
+      )}
     </>
   );
 };
