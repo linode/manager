147080696397a3a5a4823e64bbf0c1512c5f3cad
Banks Nussman <banks@nussman.us>
2024-12-24 11:23:38 -0500

    clean up profile settings

---
+++ packages/manager/src/features/Profile/Settings/MaskSensitiveData.tsx
@@ -0,0 +1,39 @@
+import { FormControlLabel, Paper, Toggle, Typography } from '@linode/ui';
+import React from 'react';
+
+import {
+  useMutatePreferences,
+  usePreferences,
+} from 'src/queries/profile/preferences';
+
+export const MaskSensitiveData = () => {
+  const { data: isSensitiveDataMasked } = usePreferences(
+    (preferences) => preferences?.maskSensitiveData
+  );
+
+  const { mutateAsync: updatePreferences } = useMutatePreferences();
+
+  return (
+    <Paper>
+      <Typography marginBottom={1} variant="h2">
+        Mask Sensitive Data
+      </Typography>
+      <Typography marginBottom={1} variant="body1">
+        Mask IP addresses and user contact information for data privacy.
+      </Typography>
+      <FormControlLabel
+        control={
+          <Toggle
+            onChange={(_, checked) =>
+              updatePreferences({ maskSensitiveData: checked })
+            }
+            checked={isSensitiveDataMasked}
+          />
+        }
+        label={`Sensitive data is ${
+          isSensitiveDataMasked ? 'masked' : 'visible'
+        }`}
+      />
+    </Paper>
+  );
+};
