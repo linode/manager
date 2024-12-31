f7139e97d459a9a3f6d3d8601f57416fc3ae28c9
vmangalr <vmangalr@akamai.com>
2024-12-31 18:59:08 +0530

    upcoming: [DI-22131] - Util function test optimisation

--- packages/manager/src/features/CloudPulse/Alerts/Utils/utils.test.ts
+++ packages/manager/src/features/CloudPulse/Alerts/Utils/utils.test.ts
@@ -4,16 +4,12 @@ import { getServiceTypeLabel } from './utils';
 
 it('test getServiceTypeLabel method', () => {
   const services = serviceTypesFactory.buildList(3);
-
-  expect(
-    getServiceTypeLabel(services[0].service_type, { data: services })
-  ).toBe(services[0].label);
-  expect(
-    getServiceTypeLabel(services[1].service_type, { data: services })
-  ).toBe(services[1].label);
-  expect(
-    getServiceTypeLabel(services[2].service_type, { data: services })
-  ).toBe(services[2].label);
+  services.forEach((service) => {
+    // validate for proper labels
+    expect(getServiceTypeLabel(service.service_type, { data: services })).toBe(
+      service.label
+    );
+  });
   expect(getServiceTypeLabel('test', { data: services })).toBe('test');
   expect(getServiceTypeLabel('', { data: services })).toBe('');
 });
