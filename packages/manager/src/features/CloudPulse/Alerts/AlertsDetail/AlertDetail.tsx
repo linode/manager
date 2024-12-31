862bf76258ad617650099f3cede2b4c4d5fab996
vmangalr <vmangalr@akamai.com>
2024-12-31 12:50:24 +0530

    upcoming: [DI-22131] - CSS and factory updates for alert detail overview

--- packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/AlertDetail.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/AlertDetail.tsx
@@ -92,7 +92,7 @@ export const AlertDetail = () => {
       <Box display="flex" flexDirection="column" gap={2}>
         <Box display="flex" flexDirection={{ md: 'row', xs: 'column' }} gap={2}>
           <Box
-            flexBasis="100%"
+            flexBasis="50%"
             maxHeight={theme.spacing(98.125)}
             sx={{ ...getAlertBoxStyles(theme), overflow: 'auto' }}
           >
