ed7357c007f4257db593352f3610431cea2c4105
hasyed-akamai <hasyed@akamai.com>
2025-01-08 00:03:30 +0530

    refactor: [M3-6910] - Replace instances in : Object Storage (#11456)
    * refactor: [M3-6910] - Replace instances in : Object Storage
    
    * Added changeset: Replace Select with Autocomplete component in Object Storage
    
    * Remove unnecessary `vi.mocks`

--- packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.test.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/ClusterSelect.test.tsx
@@ -4,8 +4,6 @@ import { renderWithTheme } from 'src/utilities/testHelpers';
 
 import ClusterSelect from './ClusterSelect';
 
-vi.mock('src/components/EnhancedSelect/Select');
-
 describe('ClusterSelect', () => {
   it('Renders a select with object storage clusters', () => {
     const { getByText } = renderWithTheme(
