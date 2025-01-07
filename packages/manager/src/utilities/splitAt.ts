e555d41cd26d8cf5c826c309fa421a2d714df352
pmakode-akamai <pmakode@akamai.com>
2025-01-07 19:24:16 +0530

    Fix comments

--- packages/manager/src/utilities/splitAt.ts
+++ packages/manager/src/utilities/splitAt.ts
@@ -1,27 +1,18 @@
+export function splitAt<T>(index: number, input: T[]): [T[], T[]];
+
+export function splitAt(index: number, input: string): [string, string];
+
 /**
- * Splits an array into two parts at the given index.
+ * Splits a given list or string at a given index.
  *
  * @param index - The index to split at.
- * @param input - The list (array) to split.
- * @returns An array containing two subarrays: the first part (0 to index), the second part (index to end).
+ * @param input - The list (array) or string to split.
+ * @returns An array containing two parts: the first part (0 to index), the second part (index to end).
  *
  * @example
  * splitAt(3, [1, 2, 3, 4, 5]); // [[1, 2, 3], [4, 5]]
- */
-export function splitAt<T>(index: number, input: T[]): [T[], T[]];
-
-/**
- * Splits a string into two parts at the given index.
- *
- * @param index - The index to split at.
- * @param input - The string to split.
- * @returns An array containing two strings: the first part (0 to index), the second part (index to end).
- *
- * @example
  * splitAt(3, "hello"); // ["hel", "lo"]
  */
-export function splitAt(index: number, input: string): [string, string];
-
 export function splitAt<T>(index: number, input: T[] | string) {
   return [input.slice(0, index), input.slice(index)];
 }
