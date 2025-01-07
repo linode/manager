6c2853f6d46874b7845682b7e84369fc9f53163d
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2025-01-07 12:45:36 -0800

    fix: [M3-9024] - Mask sensitive data in Managed and Longview (#11476)
    * Add maskable text to Managed Contacts
    
    * Update the docs link URL to a more helpful doc
    
    * Make component for reusable copy
    
    * Use component in Billing Contact Info and SSH Access Pub Key
    
    * Make MaskableText icon styling more flexible; add to Longview command
    
    * Add masking to Longview API Key
    
    * Clean up styles
    
    * Mask IP in Longview
    
    * Mask IP Address on SSH Access Managed tab
    
    * Fix margin for API Key
    
    * Fix spacing for Longview API key
    
    * Mask user in Managed and Longview
    
    * Added changeset: Visibility of sensitive data in Managed and Longview with Mask Sensitive Data setting enabled

--- packages/manager/src/features/Longview/LongviewDetail/DetailTabs/ActiveConnections/ConnectionRow.tsx
+++ packages/manager/src/features/Longview/LongviewDetail/DetailTabs/ActiveConnections/ConnectionRow.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 
+import { MaskableText } from 'src/components/MaskableText/MaskableText';
 import { TableCell } from 'src/components/TableCell';
 import { TableRow } from 'src/components/TableRow';
-import { LongviewPort } from 'src/features/Longview/request.types';
+
+import type { LongviewPort } from 'src/features/Longview/request.types';
 
 interface Props {
   connection: LongviewPort;
@@ -17,7 +19,7 @@ export const ConnectionRow = (props: Props) => {
         {connection.name}
       </TableCell>
       <TableCell data-qa-active-connection-user parentColumn="User">
-        {connection.user}
+        <MaskableText isToggleable text={connection.user} />
       </TableCell>
       <TableCell data-qa-active-connection-count parentColumn="Count">
         {connection.count}
