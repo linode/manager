511d7572f9920cc7645a4a1a08639ae149095341
agorthi <agorthi@akamai.com>
2025-01-08 10:34:53 +0530

    DI-22827:E2E Automation Cypress for Alerting:  Show Details Page code review comments

--- packages/manager/cypress/e2e/core/cloudpulse/alert-show-details.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/alert-show-details.spec.ts
@@ -5,7 +5,7 @@
 import { mockAppendFeatureFlags } from 'support/intercepts/feature-flags';
 import {
   accountFactory,
-   alertFactory,
+  alertFactory,
   databaseFactory,
   linodeFactory,
   regionFactory,
@@ -41,7 +41,7 @@ const mockLinodeAlertDetails = alertFactory.build({
   created_by: 'user1',
   service_type: 'linode',
   severity: 1,
-  status: 'enabled',
+  status: 'disabled',
   type: 'system',
   updated_by: 'user1',
   entity_ids: ['3'],
@@ -55,8 +55,8 @@ const mockDBaaSRegion = regionFactory.build({
 
 const mockLinodeRegion = regionFactory.build({
   capabilities: ['Linodes'],
-  id: 'us-ord',
-  label: 'Chicago, IL',
+  id: 'us-east',
+  label: 'US, Newark',
 });
 
 const databaseMock: Database = databaseFactory.build({
@@ -94,10 +94,13 @@ describe('Integration Tests for Dbaas Alert Show Detail Page', () => {
     mockAppendFeatureFlags(flags);
     mockGetAccount(mockAccount);
     mockGetRegions([mockDBaaSRegion, mockLinodeRegion]);
-    mockGetAllAlertDefinitions([mockDBaaSAlertDetails,mockLinodeAlertDetails])
-      .as('getAlertDefinitionsList');
-    mockGetDatabases([databaseMock, extendedDatabaseMock])
-    .as('getMockedDbaasDatabases');
+    mockGetAllAlertDefinitions([
+      mockDBaaSAlertDetails,
+      mockLinodeAlertDetails,
+    ]).as('getAlertDefinitionsList');
+    mockGetDatabases([databaseMock, extendedDatabaseMock]).as(
+      'getMockedDbaasDatabases'
+    );
     mockGetLinodes([mockLinode]).as('getLiodeDatabase');
     mockGetAlertDefinitions(
       mockDBaaSAlertDetails.service_type,
@@ -113,20 +116,18 @@ describe('Integration Tests for Dbaas Alert Show Detail Page', () => {
 
     cy.wait('@getAlertDefinitionsList');
 
-    cy.get(`[data-qa-alert-cell="${mockDBaaSAlertDetails.id}"]`)
-     .within(() => {
-    // Ensure the alert label is visible
-    cy.findByText(mockDBaaSAlertDetails.label)
-      .should('be.visible');
+    cy.get(`[data-qa-alert-cell="${mockDBaaSAlertDetails.id}"]`).within(() => {
+      // Ensure the alert label is visible
+      cy.findByText(mockDBaaSAlertDetails.label).should('be.visible');
 
-    // Action button within the alert cell should be visible and clickable
-    cy.get(`[data-qa-alert-action-cell="alert_${mockDBaaSAlertDetails.id}"]`)
-      .find('button') 
-      .should('be.visible')
-      .click();
-  });
+      // Action button within the alert cell should be visible and clickable
+      cy.get(`[data-qa-alert-action-cell="alert_${mockDBaaSAlertDetails.id}"]`)
+        .find('button')
+        .should('be.visible')
+        .click();
+    });
 
-      cy.get('[data-qa-action-menu-item="Show Details"]')
+    cy.get('[data-qa-action-menu-item="Show Details"]')
       .should('be.visible')
       .click();
 
@@ -134,6 +135,7 @@ describe('Integration Tests for Dbaas Alert Show Detail Page', () => {
   });
 
   it('should correctly display the details of the DBaaS alert in the alert details view', () => {
+    // Validating contents of Overview Section
     cy.get('[data-qa-section="Overview"]').within(() => {
       // Validate Name field
       cy.get('[data-qa-item="Name"]').within(() => {
@@ -149,58 +151,58 @@ describe('Integration Tests for Dbaas Alert Show Detail Page', () => {
       // Validate Status field
       cy.get('[data-qa-item="Status"]').within(() => {
         cy.findByText('Status:').should('be.visible');
-        cy.findByText( mockDBaaSAlertDetails.status.replace('enabled', 'Enabled') )
-        .should('be.visible');
+        cy.findByText(
+          mockDBaaSAlertDetails.status.replace('enabled', 'Enabled')
+        ).should('be.visible');
       });
 
       cy.get('[data-qa-item="Severity"]').within(() => {
         cy.findByText('Severity:').should('be.visible');
-        cy.findByText(String(mockDBaaSAlertDetails.severity).replace('1', 'Medium')
-         ).should('be.visible');
+        cy.findByText(
+          String(mockDBaaSAlertDetails.severity).replace('1', 'Medium')
+        ).should('be.visible');
       });
       // Validate Service field
       cy.get('[data-qa-item="Service"]').within(() => {
         cy.findByText('Service:').should('be.visible');
-        cy.findByText( mockDBaaSAlertDetails.service_type.replace('dbaas', 'Databases'))
-        .should('be.visible');
+        cy.findByText(
+          mockDBaaSAlertDetails.service_type.replace('dbaas', 'Databases')
+        ).should('be.visible');
       });
 
       // Validate Type field
       cy.get('[data-qa-item="Type"]').within(() => {
-        cy.findByText('Type:')
-         .should('be.visible');
+        cy.findByText('Type:').should('be.visible');
 
-        cy.findByText( mockDBaaSAlertDetails.type.replace('system', 'System'))
-        .should('be.visible');
+        cy.findByText(
+          mockDBaaSAlertDetails.type.replace('system', 'System')
+        ).should('be.visible');
       });
 
       // Validate Created By field
       cy.get('[data-qa-item="Created By"]').within(() => {
+        cy.findByText('Created By:').should('be.visible');
 
-        cy.findByText('Created By:')
-        .should('be.visible');
-
-        cy.findByText(mockDBaaSAlertDetails.created_by)
-        .should('be.visible');
+        cy.findByText(mockDBaaSAlertDetails.created_by).should('be.visible');
       });
 
       // Validate Last Modified field
       cy.get('[data-qa-item="Last Modified"]').within(() => {
-        cy.findByText('Last Modified:')
-           .should('be.visible');
-        cy.findByText(formatDate(mockDBaaSAlertDetails.updated, {format: 'MMM dd, yyyy, h:mm a',}))
+        cy.findByText('Last Modified:').should('be.visible');
+        cy.findByText( formatDate(mockDBaaSAlertDetails.updated, {format: 'MMM dd, yyyy, h:mm a', }))
         .should('be.visible');
       });
     });
 
+    // Validating contents of Criteria Section
     cy.get('[data-qa-section="Criteria"]').within(() => {
       mockDBaaSAlertDetails.rule_criteria.rules.forEach((rule, index) => {
         cy.get('[data-qa-item="Metric Threshold"]')
           .eq(index)
           .within(() => {
-            cy.get(`[data-qa-chip="${rule.aggregation_type.replace('avg', 'Average')}"]` )
+            cy.get( `[data-qa-chip="${rule.aggregation_type.replace('avg', 'Average')}"]`)
               .should('be.visible')
-              .should( 'have.text', rule.aggregation_type.replace('avg', 'Average'));
+              .should( 'have.text',rule.aggregation_type.replace('avg', 'Average'));
 
             cy.get(`[data-qa-chip="${rule.metric}"]`)
               .should('be.visible')
@@ -218,6 +220,8 @@ describe('Integration Tests for Dbaas Alert Show Detail Page', () => {
               .should('be.visible')
               .should('have.text', rule.unit);
           });
+
+        // Validating contents of Dimension Filter
         cy.get('[data-qa-item="Dimension Filter"]')
           .eq(index)
           .within(() => {
@@ -228,13 +232,13 @@ describe('Integration Tests for Dbaas Alert Show Detail Page', () => {
                 .each(($chip) => {
                   expect($chip).to.have.text(filter.label);
                 });
-               // Validate the filter operator
+              // Validate the filter operator
               cy.get(`[data-qa-chip="${filter.operator}"]`)
                 .should('be.visible')
                 .each(($chip) => {
                   expect($chip).to.have.text(filter.operator);
                 });
-                 // Validate the filter value
+              // Validate the filter value
               cy.get(`[data-qa-chip="${filter.value}"]`)
                 .should('be.visible')
                 .each(($chip) => {
@@ -243,77 +247,96 @@ describe('Integration Tests for Dbaas Alert Show Detail Page', () => {
             });
           });
       });
+
+      // Validating contents of Polling Interval
+      cy.get('[data-qa-item="Polling Interval"]')
+        .find('[data-qa-chip]')
+        .should('be.visible')
+        .should('have.text',String(mockDBaaSAlertDetails.trigger_conditions.polling_interval_seconds)
+        .replace('120', '2 minutes') );
+
+      // Validating contents of Evaluation Periods
+      cy.get('[data-qa-item="Evaluation Periods"]')
+        .find('[data-qa-chip]')
+        .should('be.visible')
+        .should( 'have.text',String(mockDBaaSAlertDetails.trigger_conditions.evaluation_period_seconds)
+            .replace('240', '4 minutes'));
+
+      cy.get(`[data-qa-chip="${mockDBaaSAlertDetails.trigger_conditions.criteria_condition
+        .replace('ALL','All' )}"]`)
+        .should('be.visible')
+        .should('have.text', mockDBaaSAlertDetails.trigger_conditions.criteria_condition.
+          replace('ALL','All' ));
+
+      cy.get( `[data-qa-chip="${String( mockDBaaSAlertDetails.trigger_conditions.evaluation_period_seconds)
+        .replace('240', '4 minutes')}"]`)
+        .should('be.visible')
+        .should('have.text',
+          String(mockDBaaSAlertDetails.trigger_conditions.evaluation_period_seconds)
+          .replace('240', '4 minutes'));
+
+      cy.get('[data-qa-item="criteria are met for"]').should('be.visible');
+
+      cy.get('[data-qa-item="consecutive occurrences"]').should('be.visible');
     });
-// Get the element with data-qa-item="Polling Interval", find the child chip element, and assert its visibility and text
-    cy.get('[data-qa-item="Polling Interval"]')
-      .find('[data-qa-chip]')
-      .should('be.visible')
-      .should( 'have.text', String(mockDBaaSAlertDetails.trigger_conditions.polling_interval_seconds)
-      .replace('120', '2 minutes') );
 
-   // Get the element with data-qa-item="Evaluation Periods", find the child chip element, and assert its visibility and text   
-    cy.get('[data-qa-item="Evaluation Periods"]')
-      .find('[data-qa-chip]')
-       .should('be.visible') 
-.       should('have.text', String(mockDBaaSAlertDetails.trigger_conditions.evaluation_period_seconds) 
-      .replace('240', '4 minutes')); 
+    cy.get('[data-qa-section="Resources"]').within(() => {
+      // Assert headers are visible and have the correct text
+      cy.get('[data-qa-header="resource"]')
+        .scrollIntoView()
+        .should('be.visible')
+        .should('have.text', 'Resource');
 
-
-    cy.get( `[data-qa-chip="${mockDBaaSAlertDetails.trigger_conditions.criteria_condition
-      .replace('ALL', 'All')}"]` )
-      .should('be.visible')
-      .should('have.text',mockDBaaSAlertDetails.trigger_conditions.criteria_condition
-        .replace( 'ALL', 'All' ) );
-
-    cy.get(`[data-qa-chip="${String(mockDBaaSAlertDetails.trigger_conditions.evaluation_period_seconds)
-      .replace('240', '4 minutes')}"]` )
-      .should('be.visible')
-      .should( 'have.text', String( mockDBaaSAlertDetails.trigger_conditions.evaluation_period_seconds)
-      .replace('240', '4 minutes'));
-
-    cy.get('[data-qa-item="criteria are met for"]').should('be.visible');
-
-    cy.get('[data-qa-item="consecutive occurrences"]').should('be.visible');
-
-   // Assert that the header with data-qa-header="resource" is visible and contains the correct text "Resource"
-     cy.get('[data-qa-header="resource"]')
-      .should('be.visible')
-      .should('have.text', 'Resource');
-
-   // Assert that the header with data-qa-header="region" is visible and contains the correct text "Region"
       cy.get('[data-qa-header="region"]')
-      .should('be.visible')
-      .should('have.text', 'Region');
+        .should('be.visible')
+        .should('have.text', 'Region');
 
-    // Assert the placeholder value of the first input field (Search for a Resource)
-    cy.findByPlaceholderText('Search for a Resource').should('be.visible');
+      // Assert the placeholder values for input fields
+      cy.findByPlaceholderText('Search for a Resource').should('be.visible');
+      cy.findByPlaceholderText('Select Regions').should('be.visible');
 
-    // Assert the placeholder value of the second input field (Select Regions)
-    cy.findByPlaceholderText('Select Regions').should('be.visible');
+      // Assert the number of rows in the table
+      cy.get('[data-qa-alert-row]').should('have.length', 2);
 
-    // Get all elements with the attribute 'data-qa-alert-row' and assert that the number of rows is 2
-       cy.get('[data-qa-alert-row]')
-      .should('have.length', 2);
+      // Assert specific table cell values for databaseMock
+      cy.get(`[data-qa-alert-cell="${databaseMock.id}_resource"]`)
+        .should('be.visible')
+        .should('have.text', databaseMock.label);
 
-// Assert that the cell for the resource corresponding to databaseMock is visible and contains the correct label text
-cy.get(`[data-qa-alert-cell="${databaseMock.id}_resource"]`)
-  .should('be.visible') 
-  .should('have.text', databaseMock.label);
+      cy.get(`[data-qa-alert-cell="${databaseMock.id}_region"]`)
+        .should('be.visible')
+        .should('have.text', mockDBaaSRegion.label);
 
-// Assert that the cell for the region corresponding to databaseMock is visible and contains the correct label text
-cy.get(`[data-qa-alert-cell="${databaseMock.id}_region"]`)
-  .should('be.visible') 
-  .should('have.text', mockDBaaSRegion.label);
+      // Assert specific table cell values for extendedDatabaseMock
+      cy.get(`[data-qa-alert-cell="${extendedDatabaseMock.id}_resource"]`)
+        .should('be.visible')
+        .should('have.text', extendedDatabaseMock.label);
 
-// Assert that the cell for the resource corresponding to extendedDatabaseMock is visible and contains the correct label text
-cy.get(`[data-qa-alert-cell="${extendedDatabaseMock.id}_resource"]`)
-  .should('be.visible') 
-  .should('have.text', extendedDatabaseMock.label); 
+      cy.get(`[data-qa-alert-cell="${extendedDatabaseMock.id}_region"]`)
+        .should('be.visible')
+        .should('have.text', mockDBaaSRegion.label);
 
-// Assert that the cell for the region corresponding to databaseMock is visible and contains the correct label text
-cy.get(`[data-qa-alert-cell="${databaseMock.id}_region"]`)
-  .should('be.visible')
-  .should('have.text', mockDBaaSRegion.label); 
+      //Click on the 'resource' column header to sort the resources in ascending order
+      cy.get('[data-qa-header="resource"]').should('be.visible').click();
 
+      // Get all rows with the attribute data-qa-alert-row
+      cy.get('[data-qa-alert-row]').then(($rows) => {
+        const alertRowIds = $rows
+          .map((index, row) => row.getAttribute('data-qa-alert-row'))
+          .get();
+        expect(alertRowIds).to.deep.equal(['2', '1']);
+      });
+
+      //Click on the 'resource' column header to sort the resources in descending order
+      cy.get('[data-qa-header="resource"]').should('be.visible').click();
+
+      // Get all rows with the attribute data-qa-alert-row
+      cy.get('[data-qa-alert-row]').then(($rows) => {
+        const alertRowIds = $rows
+          .map((index, row) => row.getAttribute('data-qa-alert-row'))
+          .get();
+        expect(alertRowIds).to.deep.equal(['1', '2']);
+      });
+    });
   });
-})
+});
