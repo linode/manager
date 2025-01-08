cb4526de12ecbd2787b92b8619edbbce3453a324
nikhagra-akamai <nagrawal@akamai.com>
2025-01-08 15:18:55 +0530

    upcoming: [DI-20931] - Added date range picker support in contextual view

--- packages/manager/src/features/CloudPulse/Utils/ReusableDashboardFilterUtils.ts
+++ packages/manager/src/features/CloudPulse/Utils/ReusableDashboardFilterUtils.ts
@@ -1,9 +1,10 @@
+import { defaultTimeDuration } from '../shared/CloudPulseDateTimeRangePicker';
 import { FILTER_CONFIG } from './FilterConfig';
 
 import type { DashboardProperties } from '../Dashboard/CloudPulseDashboard';
 import type { FilterValueType } from '../Dashboard/CloudPulseDashboardLanding';
 import type { CloudPulseMetricsAdditionalFilters } from '../Widget/CloudPulseWidget';
-import type { Dashboard, TimeDuration } from '@linode/api-v4';
+import type { Dashboard, TimeDurationDate } from '@linode/api-v4';
 
 /**
  * This interface is used to get method parameters for this utility
@@ -24,7 +25,7 @@ interface ReusableDashboardFilterUtilProps {
   /**
    * The selected time duration
    */
-  timeDuration?: TimeDuration;
+  timeDuration?: TimeDurationDate;
 }
 
 /**
@@ -42,7 +43,7 @@ export const getDashboardProperties = (
       resource,
     }),
     dashboardId: dashboardObj.id,
-    duration: timeDuration ?? { unit: 'min', value: 30 },
+    duration: timeDuration ?? defaultTimeDuration(),
     resources: [String(resource)],
     savePref: false,
   };
