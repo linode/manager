8f3c34b4c97920b98d6d50aebcdf26202361465e
hrao <hrao@akamai.com>
2025-01-13 16:52:49 +0530

    pathOr file change

--- packages/manager/src/utilities/pathOr.ts
+++ packages/manager/src/utilities/pathOr.ts
@@ -3,7 +3,7 @@
  * @param defaultValue {T} The value to return if the path is not found or the value is `undefined`
  * @param path {(string | number)[]} An array representing the path to the value in the object or array
  * @param object {Record<string, any> | undefined} The object or array to traverse
- * @returns {T} The value at the specified path, or the default value if the path is not found or is `undefined`
+ * @returns The value at the specified path, or the default value if the path is not found or is `undefined`
  */
 export const pathOr = <T, O>(
   defaultValue: T,
@@ -14,34 +14,3 @@ export const pathOr = <T, O>(
     (acc: any, key) => (acc && acc[key] !== undefined ? acc[key] : undefined),
     object
   ) ?? defaultValue;
-
-/**
- * Compares two values for equality, including deep equality for objects and arrays.
- * @param a {*} The first value to compare
- * @param b {*} The second value to compare
- * @returns {boolean} `true` if the values are equal (deeply for objects and arrays), otherwise `false`
- */
-export const equals = (a: any, b: any): boolean => {
-  if (a === b) {
-    return true;
-  }
-
-  // If both are objects/arrays, compare deeply
-  if (
-    typeof a === 'object' &&
-    typeof b === 'object' &&
-    a !== null &&
-    b !== null
-  ) {
-    const aKeys = Object.keys(a);
-    const bKeys = Object.keys(b);
-
-    if (aKeys.length !== bKeys.length) {
-      return false;
-    }
-
-    return aKeys.every((key) => equals(a[key], b[key])); // Recursively check each key-value pair
-  }
-
-  return false;
-};
