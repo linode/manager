7db48f06b60656535e577fdf3a23e291be0275e6
Jaalah Ramos <jaalah.ramos@gmail.com>
2025-01-14 09:38:44 -0500

    Update link

--- packages/manager/src/features/ObjectStorage/BucketLanding/BucketRateLimitTable.tsx
+++ packages/manager/src/features/ObjectStorage/BucketLanding/BucketRateLimitTable.tsx
@@ -83,7 +83,7 @@ export const BucketRateLimitTable = ({
           'This endpoint type supports up to 750 Requests Per Second (RPS). '
         )}
         Understand{' '}
-        <Link to="https://techdocs.akamai.com/cloud-computing/docs/object-storage#optimize-applications-to-avoid-rate-limiting">
+        <Link to="https://techdocs.akamai.com/cloud-computing/docs/object-storage-product-limits#optimize-to-avoid-rate-limiting">
           bucket rate limits
         </Link>
         .
