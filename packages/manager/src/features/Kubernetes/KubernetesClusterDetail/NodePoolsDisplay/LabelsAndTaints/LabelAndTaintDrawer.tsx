66693c29014d804a87038382c0433c1ac6a73605
mjac0bs <mjacobs@akamai.com>
2025-01-14 17:31:24 -0700

    Add ability to remove label from table

--- packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/LabelsAndTaints/LabelAndTaintDrawer.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/LabelsAndTaints/LabelAndTaintDrawer.tsx
@@ -1,6 +1,7 @@
 import { Button, Typography } from '@linode/ui';
 import * as React from 'react';
-// import { useState } from 'react';
+
+import { FormProvider, useForm } from 'react-hook-form';
 
 import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
 import { Drawer } from 'src/components/Drawer';
@@ -9,10 +10,9 @@ import { useSpecificTypes } from 'src/queries/types';
 import { extendType } from 'src/utilities/extendType';
 
 import { LabelTable } from './LabelTable';
-// import { MultipleLabelInput } from './MultipleLabelInput';
 import { TaintTable } from './TaintTable';
 
-import type { KubeNodePoolResponse } from '@linode/api-v4';
+import type { KubeNodePoolResponse, Label, Taint } from '@linode/api-v4';
 
 export interface Props {
   nodePool: KubeNodePoolResponse | undefined;
@@ -20,72 +20,98 @@ export interface Props {
   open: boolean;
 }
 
+interface LabelsAndTaintsFormFields {
+  labels: Label;
+  taints: Taint[];
+}
+
 export const LabelAndTaintDrawer = (props: Props) => {
   const { nodePool, onClose, open } = props;
 
-  // const [labels, setLabels] = useState<Label>({});
-
   const typesQuery = useSpecificTypes(nodePool?.type ? [nodePool.type] : []);
+
+  const { control, setValue, ...form } = useForm<LabelsAndTaintsFormFields>({
+    defaultValues: {
+      labels: {},
+      taints: [],
+    },
+  });
+
+  React.useEffect(() => {
+    if (!nodePool) {
+      return;
+    }
+    if (open) {
+      setValue('labels', nodePool?.labels);
+    }
+  }, [nodePool, open]);
+
+  const handleClose = () => {
+    onClose();
+    form.reset(); // TODO: remove if this isn't doing anything
+  };
 
   const planType = typesQuery[0]?.data
     ? extendType(typesQuery[0].data)
     : undefined;
 
-  // const handleAddLabelClick = () => {
-  //   setLabels((labels) => ({
-  //     ...labels,
-  //     ['']: '',
-  //   }));
-  // };
-
-  // const handleAddLabel = (newLabel: string) => {
-  //   setLabels((labels) => ({
-  //     ...labels,
-  //     newLabel,
-  //   }));
-  // };
-
   return (
     <Drawer
-      onClose={onClose}
+      onClose={handleClose}
       open={open}
       title={`Labels and Taints: ${planType?.formattedLabel ?? 'Unknown'} Plan`}
     >
-      <Typography
-        marginBottom={(theme) => theme.spacing(4)}
-        marginTop={(theme) => theme.spacing()}
-      >
-        Labels and Taints will be applied to Nodes in this Node Pool. They can
-        be further defined using the Kubernetes API, although edits with be
-        overwritten when Nodes or Pools are recycled.
-      </Typography>
-      <Typography variant="h3"> Labels </Typography>
-      <LabelTable labels={nodePool?.labels} />
-      {/* <MultipleLabelInput
-        labels={labels}
-        // onChange={handleAddLabel}
-        onClick={handleAddLabelClick}
-      /> */}
-      <Typography marginTop={(theme) => theme.spacing(2)} variant="h3">
-        Taints
-      </Typography>
-      <TaintTable taints={nodePool?.taints} />
-      <Button buttonType="secondary">Add Taint</Button>
+      <FormProvider control={control} setValue={setValue} {...form}>
+        <Typography
+          marginBottom={(theme) => theme.spacing(4)}
+          marginTop={(theme) => theme.spacing()}
+        >
+          Labels and Taints will be applied to Nodes in this Node Pool. They can
+          be further defined using the Kubernetes API, although edits with be
+          overwritten when Nodes or Pools are recycled.
+        </Typography>
 
-      <ActionsPanel
-        primaryButtonProps={{
-          'data-testid': 'submit',
-          // disabled: userCannotAddVPC,
-          label: 'Save Changes',
-          // loading: isLoadingCreateVPC,
-          // onClick: handleSubmit(onCreateVPC),
-        }}
-        secondaryButtonProps={{
-          'data-testid': 'cancel',
-          label: 'Cancel',
-          onClick: onClose,
-        }}
-      />
+        <Typography variant="h3"> Labels </Typography>
+        <LabelTable />
+        <Button
+          buttonType="secondary"
+          onClick={() => {
+            {
+              /* TODO */
+            }
+          }}
+        >
+          Add Label
+        </Button>
+
+        <Typography marginTop={(theme) => theme.spacing(2)} variant="h3">
+          Taints
+        </Typography>
+        <TaintTable taints={nodePool?.taints} />
+        <Button
+          buttonType="secondary"
+          onClick={() => {
+            {
+              /* TODO */
+            }
+          }}
+        >
+          Add Taint
+        </Button>
+
+        <ActionsPanel
+          primaryButtonProps={{
+            'data-testid': 'submit',
+            label: 'Save Changes',
+            // onClick: handleSubmit,
+          }}
+          secondaryButtonProps={{
+            'data-testid': 'cancel',
+            label: 'Cancel',
+            onClick: handleClose,
+          }}
+        />
+      </FormProvider>
     </Drawer>
   );
 };
--- packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/LabelsAndTaints/LabelTable.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/LabelsAndTaints/LabelTable.tsx
@@ -1,18 +1,28 @@
-import { TableBody, TableCell, TableHead, Typography } from '@mui/material';
+import { IconButton, Stack, Typography } from '@linode/ui';
+import Close from '@mui/icons-material/Close';
 import * as React from 'react';
+import { useFormContext } from 'react-hook-form';
 
+import { TableBody } from 'src/components/TableBody';
+import { TableCell } from 'src/components/TableCell';
+import { TableHead } from 'src/components/TableHead';
 import { TableRow } from 'src/components/TableRow/TableRow';
 
 import { StyledLabelTable } from './LabelTable.styles';
 
 import type { Label } from '@linode/api-v4';
 
-interface Props {
-  labels: Label | undefined;
-}
+export const LabelTable = () => {
+  const { setValue, watch } = useFormContext();
 
-export const LabelTable = (props: Props) => {
-  const { labels } = props;
+  const labels: Label = watch('labels');
+
+  const handleRemoveLabel = (labelKey: string) => {
+    const newLabels = Object.fromEntries(
+      Object.entries(labels).filter(([key]) => key !== labelKey)
+    );
+    setValue('labels', newLabels);
+  };
 
   return (
     <StyledLabelTable aria-label="List of Node Pool Labels">
@@ -23,13 +33,24 @@ export const LabelTable = (props: Props) => {
       </TableHead>
       <TableBody>
         {labels && Object.entries(labels).length > 0 ? (
-          Object.entries(labels).map(([key, label]) => {
+          Object.entries(labels).map(([key, value]) => {
             return (
               <TableRow key={`label-row-${key}`}>
                 <TableCell>
-                  <Typography>
-                    {key}: {label}
-                  </Typography>
+                  <Stack alignItems="center" direction="row">
+                    <Typography>
+                      {key}: {value}
+                    </Typography>
+                    <IconButton
+                      aria-label={`remove ${key}: ${value}`}
+                      disableRipple
+                      onClick={() => handleRemoveLabel(key)}
+                      size="medium"
+                      sx={{ marginLeft: 'auto' }}
+                    >
+                      <Close />
+                    </IconButton>
+                  </Stack>
                 </TableCell>
               </TableRow>
             );
