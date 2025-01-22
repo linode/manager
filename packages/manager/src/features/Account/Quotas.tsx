e660f344219505ec170cbd2e638340faacb7b859
Hussain Khalil <hkhalil@akamai.com>
2025-01-22 18:18:05 -0500

    Add DocumentTitleSegment

--- packages/manager/src/features/Account/Quotas.tsx
+++ packages/manager/src/features/Account/Quotas.tsx
@@ -2,32 +2,36 @@ import { Autocomplete, Divider, Paper, Stack, Typography } from '@linode/ui';
 import { styled } from '@mui/material';
 import * as React from 'react';
 
+import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import { Link } from 'src/components/Link';
 import { RegionSelect } from 'src/components/RegionSelect/RegionSelect';
 
 export const Quotas = () => {
   return (
-    <MainStack spacing={2}>
-      <Typography variant="h3">Quotas</Typography>
-      <Paper>
-        <Stack divider={<Divider spacingBottom={20} spacingTop={40} />}>
-          <Stack spacing={1}>
-            <Autocomplete label="Select a Service" options={[]} />
-            <RegionSelect
-              currentCapability={undefined}
-              regions={[]}
-              value={undefined}
-            />
-          </Stack>
-          <Stack>
-            <Stack direction="row-reverse">
-              <Link to="#">Learn More About Quotas</Link>
+    <>
+      <DocumentTitleSegment segment="Quotas" />
+      <MainStack spacing={2}>
+        <Typography variant="h3">Quotas</Typography>
+        <Paper>
+          <Stack divider={<Divider spacingBottom={20} spacingTop={40} />}>
+            <Stack spacing={1}>
+              <Autocomplete label="Select a Service" options={[]} />
+              <RegionSelect
+                currentCapability={undefined}
+                regions={[]}
+                value={undefined}
+              />
             </Stack>
-            <Typography variant="h3">Quotas</Typography>
+            <Stack>
+              <Stack direction="row-reverse">
+                <Link to="#">Learn More About Quotas</Link>
+              </Stack>
+              <Typography variant="h3">Quotas</Typography>
+            </Stack>
           </Stack>
-        </Stack>
-      </Paper>
-    </MainStack>
+        </Paper>
+      </MainStack>
+    </>
   );
 };
 
