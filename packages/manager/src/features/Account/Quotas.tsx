d9fad20aff024f11c72395a298bcadc3054475f4
Hussain Khalil <hkhalil@akamai.com>
2025-01-22 17:54:20 -0500

    Create feature-flagged placeholder Account Quotas tab

---
+++ packages/manager/src/features/Account/Quotas.tsx
@@ -0,0 +1,36 @@
+import { Autocomplete, Divider, Paper, Stack, Typography } from '@linode/ui';
+import { styled } from '@mui/material';
+import * as React from 'react';
+
+import { Link } from 'src/components/Link';
+import { RegionSelect } from 'src/components/RegionSelect/RegionSelect';
+
+export const Quotas = () => {
+  return (
+    <MainStack spacing={2}>
+      <Typography variant="h3">Quotas</Typography>
+      <Paper>
+        <Stack divider={<Divider spacingBottom={20} spacingTop={40} />}>
+          <Stack spacing={1}>
+            <Autocomplete label="Select a Service" options={[]} />
+            <RegionSelect
+              currentCapability={undefined}
+              regions={[]}
+              value={undefined}
+            />
+          </Stack>
+          <Stack>
+            <Stack direction="row-reverse">
+              <Link to="#">Learn More About Quotas</Link>
+            </Stack>
+            <Typography variant="h3">Quotas</Typography>
+          </Stack>
+        </Stack>
+      </Paper>
+    </MainStack>
+  );
+};
+
+const MainStack = styled(Stack, { label: 'MainStack' })(({ theme }) => ({
+  marginTop: theme.spacing(2),
+}));
