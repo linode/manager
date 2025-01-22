df7a04aadf9041a3ef72ecd46fa822f4e5882a5a
Banks Nussman <banks@nussman.us>
2025-01-22 14:04:32 -0500

    enforce that there is atleast one image in create schema

--- packages/validation/src/stackscripts.schema.ts
+++ packages/validation/src/stackscripts.schema.ts
@@ -6,7 +6,9 @@ export const stackScriptSchema = object({
     .required('Label is required.')
     .min(3, 'Label must be between 3 and 128 characters.')
     .max(128, 'Label must be between 3 and 128 characters.'),
-  images: array(string().required()).required('An image is required.'),
+  images: array(string().required())
+    .min(1, 'An image is required.')
+    .required('An image is required.'),
   description: string(),
   is_public: boolean(),
   rev_note: string(),
