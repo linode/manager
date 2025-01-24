d5cccdeff3f8e42ec5151ed188acc8a20f9e8b68
venkatmano-akamai <chk-Venkatesh@outlook.com>
2025-01-24 19:27:49 +0530

    upcoming: [DI-23081] - Add filtering, pagination and sorting for resources section in CloudPulse alerts show details page (#11541)
    * upcoming: [DI-22132] - Initial changes for adding resources section
    
    * upcoming: [DI-22838] - Added unit tests
    
    * upcoming: [DI-22838] - Error message corrections
    
    * upcoming: [DI-22838] - Add a skeletal table
    
    * upcoming: [DI-22838] - Update comments
    
    * upcoming: [DI-22838] - Add UT for utils
    
    * upcoming: [DI-22838] - Sorting, filtering and pagination support
    
    * upcoming: [DI-22838] - Add UT for utils functions
    
    * upcoming: [DI-22838] - Lint formatting fixes
    
    * upcoming: [DI-22838] - Added changeset
    
    * upcoming: [DI-22838] - UT fixes
    
    * upcoming: [DI-23081] - Code review and changes
    
    * upcoming: [DI-23081] - Code refactoring and UT updates
    
    * upcoming: [DI-23081] - Code refactoring and UT updates
    
    * upcoming: [DI-23081] - Code refactoring
    
    * upcoming: [DI-23081] - Code refactoring
    
    * upcoming: [DI-23081] - Added comments
    
    * upcoming: [DI-23081] - Code optimisations
    
    * upcoming: [DI-23081] - Code optimisations
    
    * upcoming: [DI-23081] - keys update
    
    * upcoming: [DI-23081] -Scroll fix
    
    * upcoming: [DI-23081] -Scroll fix
    
    * upcoming: [DI-23081] - Remove unused variable
    
    * upcoming: [DI-23081] - Label validations and code updates
    
    * upcoming: [DI-23081] - Reusable function
    
    * upcoming: [DI-23081] - Code comments
    
    * upcoming: [DI-23081] - Test error fixes
    
    * upcoming: [DI-23081] - Mock for scroll
    
    * upcoming: [DI-23081] - Code refactoring
    
    * upcoming: [DI-23081] - Code refactoring
    
    * upcoming: [DI-23081] - Code refactoring
    
    ---------
    
    Co-authored-by: vmangalr <vmangalr@akamai.com>

--- packages/manager/src/features/CloudPulse/Alerts/Utils/AlertResourceUtils.ts
+++ packages/manager/src/features/CloudPulse/Alerts/Utils/AlertResourceUtils.ts
@@ -1,4 +1,5 @@
 import type { CloudPulseResources } from '../../shared/CloudPulseResourcesSelect';
+import type { AlertInstance } from '../AlertsResources/DisplayAlertResources';
 import type { Region } from '@linode/api-v4';
 
 interface FilterResourceProps {
@@ -7,6 +8,10 @@ interface FilterResourceProps {
    */
   data?: CloudPulseResources[];
   /**
+   * The selected regions on which the data needs to be filtered and it is in format US, Newark, NJ (us-east)
+   */
+  filteredRegions?: string[];
+  /**
    * The map that holds the id of the region to Region object, helps in building the alert resources
    */
   regionsIdToRegionMap: Map<string, Region>;
@@ -14,6 +19,11 @@ interface FilterResourceProps {
    * The resources associated with the alerts
    */
   resourceIds: string[];
+
+  /**
+   * The search text with which the resources needed to be filtered
+   */
+  searchText?: string;
 }
 
 /**
@@ -52,4 +62,63 @@ export const getRegionOptions = (
     }
   });
   return Array.from(uniqueRegions);
+};
+
+/**
+ * @param filterProps Props required to filter the resources on the table
+ * @returns Filtered instances to be displayed on the table
+ */
+export const getFilteredResources = (
+  filterProps: FilterResourceProps
+): AlertInstance[] => {
+  const {
+    data,
+    filteredRegions,
+    regionsIdToRegionMap,
+    resourceIds,
+    searchText,
+  } = filterProps;
+  if (!data || resourceIds.length === 0) {
+    return [];
+  }
+  return data // here we always use the base data from API for filtering as source of truth
+    .filter(({ id }) => resourceIds.includes(String(id)))
+    .map((resource) => {
+      const regionObj = resource.region
+        ? regionsIdToRegionMap.get(resource.region)
+        : undefined;
+      return {
+        ...resource,
+        region: resource.region // here replace region id, formatted to Chicago, US(us-west) compatible to display in table
+          ? regionObj
+            ? `${regionObj.label} (${regionObj.id})`
+            : resource.region
+          : '',
+      };
+    })
+    .filter(({ label, region }) => {
+      const matchesSearchText =
+        !searchText ||
+        region.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) ||
+        label.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()); // check with search text
+
+      const matchesFilteredRegions =
+        !filteredRegions?.length ||
+        (region.length && filteredRegions.includes(region)); // check with filtered region
+
+      return matchesSearchText && matchesFilteredRegions; // match the search text and match the region selected
+    });
+};
+
+/**
+ * This methods scrolls to the given HTML Element
+ * @param scrollToElement The HTML Element to which we need to scroll
+ */
+export const scrollToElement = (scrollToElement: HTMLDivElement | null) => {
+  if (scrollToElement) {
+    window.scrollTo({
+      behavior: 'smooth',
+      top: scrollToElement.getBoundingClientRect().top + window.scrollY - 40,
+    });
+  }
 };
