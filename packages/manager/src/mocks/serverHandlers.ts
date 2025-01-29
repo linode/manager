7b7ae3dd9ac4af0a60b5409a7a70533fc6e365ff
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2025-01-29 15:16:43 +0530

    upcoming: [DI-23103] - added AddChannelListing and RenderChannelDetails component to Create-alert-form (#11547)
    * upcoming: [DI-23103] - added AddChannelListing and RenderChannelDetails components with unit tests along with relevant query requests, minor changes to include the added components
    
    * upcoming: [DI-23103] - Added changeset
    
    * upcoming: [DI-23103] - Review changes
    
    * upcoming: [DI-23103] - Review changes: Added form context to AddChannelListing and localized AddNotificationChannel and the state management to the AddChannelListing component
    
    * upcoming: [DI-23103] - Review comments
    
    * upcoming: [DI-23103] - Memoized the NotificationChannelCard component
    
    * upcoming: [DI-23103] - Renamed the NotificationChannels fetching query for consistency

--- packages/manager/src/mocks/serverHandlers.ts
+++ packages/manager/src/mocks/serverHandlers.ts
@@ -72,6 +72,7 @@ import {
   nodeBalancerFactory,
   nodeBalancerTypeFactory,
   nodePoolFactory,
+  notificationChannelFactory,
   notificationFactory,
   objectStorageBucketFactoryGen2,
   objectStorageClusterFactory,
@@ -2478,6 +2479,11 @@ export const handlers = [
       return HttpResponse.json({}, { status: 404 });
     }
   ),
+  http.get('*/monitor/alert-channels', () => {
+    return HttpResponse.json(
+      makeResourcePage(notificationChannelFactory.buildList(3))
+    );
+  }),
   http.get('*/monitor/services', () => {
     const response: ServiceTypesList = {
       data: [
