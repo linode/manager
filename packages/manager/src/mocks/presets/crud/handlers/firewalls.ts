078ec2d8e4157b5ef4d608cffe836cbd4d8e9e7b
Connie Liu <coliu@akamai.com>
2025-01-29 15:41:06 -0500

    begin adding mock crud operations

--- packages/manager/src/mocks/presets/crud/handlers/firewalls.ts
+++ packages/manager/src/mocks/presets/crud/handlers/firewalls.ts
@@ -1,6 +1,15 @@
+import { DateTime } from 'luxon';
 import { http } from 'msw';
 
-import { makePaginatedResponse } from 'src/mocks/utilities/response';
+import { firewallFactory } from 'src/factories';
+import { queueEvents } from 'src/mocks/utilities/events';
+import {
+  makeNotFoundResponse,
+  makePaginatedResponse,
+  makeResponse,
+} from 'src/mocks/utilities/response';
+
+import { mswDB } from '../../../indexedDB';
 
 import type { Firewall } from '@linode/api-v4';
 import type { StrictResponse } from 'msw';
@@ -10,7 +19,6 @@ import type {
   APIPaginatedResponse,
 } from 'src/mocks/utilities/response';
 
-// TODO add CRUD handlers
 export const getFirewalls = (mockState: MockState) => [
   http.get(
     '*/v4beta/networking/firewalls',
@@ -23,6 +31,136 @@ export const getFirewalls = (mockState: MockState) => [
       });
     }
   ),
+
+  http.get(
+    '*/v4beta/networking/firewalls/:id',
+    async ({
+      params,
+    }): Promise<StrictResponse<APIErrorResponse | Firewall>> => {
+      const id = Number(params.id);
+      const firewall = await mswDB.get('firewalls', id);
+
+      if (!firewall) {
+        return makeNotFoundResponse();
+      }
+
+      return makeResponse(firewall);
+    }
+  ),
 ];
 
-// TODO add create, update, delete handlers
+export const createFirewall = (mockState: MockState) => [
+  http.post(
+    '*/v4beta/networking/firewalls',
+    async ({
+      request,
+    }): Promise<StrictResponse<APIErrorResponse | Firewall>> => {
+      const payload = await request.clone().json();
+
+      const firewall = firewallFactory.build({
+        ...payload,
+        created: DateTime.now().toISO(),
+        updated: DateTime.now().toISO(),
+      });
+
+      await mswDB.add('firewalls', firewall, mockState);
+
+      queueEvents({
+        event: {
+          action: 'firewall_create',
+          entity: {
+            id: firewall.id,
+            label: firewall.label,
+            type: 'firewall',
+            url: `/v4beta/networking/firewalls/${firewall.id}`,
+          },
+        },
+        mockState,
+        sequence: [{ status: 'notification' }],
+      });
+
+      return makeResponse(firewall);
+    }
+  ),
+];
+
+export const updateFirewall = (mockState: MockState) => [
+  http.put(
+    '*/v4beta/networking/firewalls/:id',
+    async ({
+      params,
+      request,
+    }): Promise<StrictResponse<APIErrorResponse | Firewall>> => {
+      const id = Number(params.id);
+      const firewall = await mswDB.get('firewalls', id);
+
+      if (!firewall) {
+        return makeNotFoundResponse();
+      }
+
+      const payload = {
+        ...(await request.clone().json()),
+        updated: DateTime.now().toISO(),
+      };
+      const updatedDomain = { ...firewall, ...payload };
+
+      await mswDB.update('firewalls', id, updatedDomain, mockState);
+
+      queueEvents({
+        event: {
+          action: 'firewall_update',
+          entity: {
+            id: firewall.id,
+            label: firewall.label,
+            type: 'firewall',
+            url: `/v4beta/networking/firewalls/${firewall.id}`,
+          },
+        },
+        mockState,
+        sequence: [{ status: 'notification' }],
+      });
+
+      return makeResponse(updatedDomain);
+    }
+  ),
+];
+
+export const deleteFirewall = (mockState: MockState) => [
+  http.delete(
+    '*/v4beta/networking/firewalls/:id',
+    async ({ params }): Promise<StrictResponse<{} | APIErrorResponse>> => {
+      const id = Number(params.id);
+      const firewall = await mswDB.get('firewalls', id);
+
+      if (!firewall) {
+        return makeNotFoundResponse();
+      }
+
+      await mswDB.delete('firewalls', id, mockState);
+
+      queueEvents({
+        event: {
+          action: 'firewall_delete',
+          entity: {
+            id: firewall.id,
+            label: firewall.label,
+            type: 'firewall',
+            url: `/v4beta/networking/firewalls/${firewall.id}`,
+          },
+        },
+        mockState,
+        sequence: [{ status: 'notification' }],
+      });
+
+      return makeResponse({});
+    }
+  ),
+];
+
+export const getFirewallSettings = (mockState: MockState) => [];
+
+export const updateFirewallSettings = (mockState: MockState) => [];
+
+export const getFirewallDevices = (mockState: MockState) => [];
+
+export const createFirewallDevices = (mockState: MockState) => [];
