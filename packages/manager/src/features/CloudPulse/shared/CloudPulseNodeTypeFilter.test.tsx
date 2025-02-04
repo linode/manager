00560474796b08879d8287be5c0bb9fea57f1b88
ankitaakamai <ankitaan@akamai.com>
2025-02-04 12:52:04 +0530

    upcoming: [DI-21796] - Type fix

--- packages/manager/src/features/CloudPulse/shared/CloudPulseNodeTypeFilter.test.tsx
+++ packages/manager/src/features/CloudPulse/shared/CloudPulseNodeTypeFilter.test.tsx
@@ -1,4 +1,4 @@
-import { screen, within } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
