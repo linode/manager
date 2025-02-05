5dbfee0a1a92293a46811459c2aaf84cb712c907
pmakode-akamai <pmakode@akamai.com>
2025-02-05 22:21:44 +0530

    More tests and refactoring...

--- packages/manager/src/features/Billing/PdfGenerator/PdfGenerator.test.ts
+++ packages/manager/src/features/Billing/PdfGenerator/PdfGenerator.test.ts
@@ -14,13 +14,11 @@ import { formatDate } from 'src/utilities/formatDate';
 import { getShouldUseAkamaiBilling } from '../billingUtils';
 import { printInvoice } from './PdfGenerator';
 
+import type { Account } from '@linode/api-v4/lib/account/types';
 import type { FlagSet } from 'src/featureFlags';
 
-server.use(
-  http.get('/src/features/Billing/PdfGenerator/akamai-logo.png', () => {
-    return HttpResponse.json({});
-  })
-);
+type Taxes = FlagSet['taxBanner'] | FlagSet['taxes'];
+type AccountType = 'CA' | 'International (AE)' | 'International (IN)' | 'US';
 
 const getExpectedRemitAddressText = (
   country: string,
@@ -72,105 +70,175 @@ const extractPdfText = (pdfDataBuffer: Buffer): Promise<string> => {
 
 describe('PdfGenerator', () => {
   describe('printInvoice', () => {
-    it('generates a valid PDF', async () => {
-      const account = accountFactory.build({
-        city: 'Mumbai',
-        country: 'IN',
-        state: 'MH',
-        zip: '400001',
-      });
-      const invoice = invoiceFactory.build({ label: 'invoice-test-1' });
-      const items = invoiceItemFactory.buildList(5, {
-        region: 'id-cgk',
-      });
-      const regions = regionFactory.buildList(1, {
-        id: 'id-cgk',
-      });
-      const taxes: FlagSet['taxBanner'] | FlagSet['taxes'] = {
-        country_tax: {
-          tax_id: '9922CHE29001OSR',
-          tax_name: 'India GST',
-        },
-        provincial_tax_ids: {},
-      };
-      // const flags = {
-      //   /* mock flags data */
-      // };
-      const timezone = 'UTC';
-
-      const isAkamaiBilling = getShouldUseAkamaiBilling(invoice.date);
-      const isInternational = !['CA', 'US'].includes(account.country);
-
-      const date = formatDate(invoice.date, {
-        displayTime: true,
-        timezone,
-      });
-
-      // Call the printInvoice function
-      const pdfResult = await printInvoice({
-        account,
-        invoice,
-        items,
-        regions,
-        taxes,
-        timezone,
-      });
-
-      // Expect the PDF generation to be successful
-      expect(pdfResult.status).toEqual('success');
-
-      // Load the generated PDF content
-      const pdfDataBuffer = fs.readFileSync(`invoice-${date}.pdf`);
-      const pdfText = await extractPdfText(pdfDataBuffer);
-
-      // Check the content of the PDF
-      expect(pdfText).toContain('Page 1 of 1');
-      expect(pdfText).toContain(`Invoice Date: ${invoice.date}`);
-      expect(pdfText).toContain('Invoice To:');
-      expect(pdfText).toContain(
-        `Remit to: ${getExpectedRemitAddressText(
-          account.country,
-          isAkamaiBilling,
-          isInternational
-        )}`
-      );
-      expect(pdfText).toContain('Tax ID(s):');
-      expect(pdfText).toContain(`Invoice: #${invoice.id}`);
-
-      // Verify table header and rows
-      const colHeaders = [
-        'Description',
-        'From',
-        'To',
-        'Quantity',
-        'Unit Price',
-        'Amount',
-        'Tax',
-        'Total',
-      ];
-      colHeaders.forEach((header) => {
-        expect(pdfText).toContain(header);
-      });
-      items.forEach((row) => {
-        expect(pdfText).toContain(row.amount);
-        expect(pdfText).toContain(row.label.replace(' -', ''));
-        expect(pdfText).toContain(row.quantity);
-        expect(pdfText).toContain(row.unit_price);
-        expect(pdfText).toContain(row.tax);
-        expect(pdfText).toContain(row.total);
-      });
-
-      expect(pdfText).toContain(`Subtotal (USD) $${invoice.subtotal}`);
-      expect(pdfText).toContain(`Tax Subtotal (USD) $${invoice.tax}`);
-      expect(pdfText).toContain(`Total (USD) $${invoice.total}`);
-
-      // Verify footer text
-      expect(pdfText).toContain(
-        'This invoice may include Linode Compute Instances that have been powered off as the data is maintained and resources are still reserved. If you no longer need powered-down Linodes, you can remove the service (https://techdocs.akamai.com/cloud-computing/docs/stop-further-billing) from your account.'
-      );
-
-      // Cleanup: Delete the generated PDF file after testing
-      fs.unlinkSync(`invoice-${date}.pdf`);
+    const invoice = invoiceFactory.build({ label: 'invoice-test-1' });
+    const items = invoiceItemFactory.buildList(5, {
+      region: 'id-cgk',
     });
+    const regions = regionFactory.buildList(1, {
+      id: 'id-cgk',
+    });
+    const isAkamaiBilling = getShouldUseAkamaiBilling(invoice.date);
+    const timezone = 'UTC';
+    const date = formatDate(invoice.date, {
+      displayTime: true,
+      timezone,
+    });
+
+    const accountInternational1 = accountFactory.build({
+      city: 'Mumbai',
+      country: 'IN',
+      state: 'MH',
+      zip: '400001',
+    });
+    const accountInternational2 = accountFactory.build({
+      country: 'AE',
+    });
+    const accountUS = accountFactory.build({ country: 'US' });
+    const accountCA = accountFactory.build({
+      city: 'Calgary',
+      country: 'CA',
+      state: 'Alberta',
+      zip: 'T2A',
+    });
+
+    const taxesInternational1: Taxes = {
+      country_tax: {
+        tax_id: '9922CHE29001OSR',
+        tax_name: 'India GST',
+      },
+      provincial_tax_ids: {},
+    };
+    const taxesInternational2: Taxes = {
+      country_tax: {
+        tax_id: '104038424800003',
+        tax_name: 'United Arab Emirates',
+      },
+      provincial_tax_ids: {},
+    };
+    const taxesUS: Taxes = {
+      country_tax: {
+        tax_id: '04-3432319',
+        tax_name: 'United States EIN',
+      },
+      provincial_tax_ids: {},
+    };
+    const taxesCA: Taxes = {
+      country_tax: {
+        tax_id: '871275582RT0001',
+        tax_name: 'Canadian GST',
+      },
+      provincial_tax_ids: {
+        BC: {
+          tax_id: '1471-1731',
+          tax_name: 'British Columbia PST',
+        },
+        MB: {
+          tax_id: '141763-3',
+          tax_name: 'Manitoba RST',
+        },
+        QC: {
+          tax_id: '1229976512TQ0001',
+          tax_name: 'Quebec QST',
+        },
+        SK: {
+          tax_id: '7648249',
+          tax_name: 'Saskatchewan PST',
+        },
+      },
+    };
+
+    const tests: [AccountType, Account, Taxes][] = [
+      ['International (IN)', accountInternational1, taxesInternational1],
+      ['International (AE)', accountInternational2, taxesInternational2],
+      ['US', accountUS, taxesUS],
+      ['CA', accountCA, taxesCA],
+    ];
+
+    it.each(tests)(
+      'generates a valid PDF for %s accounts',
+      async (_, account, taxes) => {
+        server.use(
+          http.get('/src/features/Billing/PdfGenerator/akamai-logo.png', () => {
+            return HttpResponse.json({});
+          })
+        );
+
+        const isInternational = !['CA', 'US'].includes(account.country);
+
+        // Call the printInvoice function
+        const pdfResult = await printInvoice({
+          account,
+          invoice,
+          items,
+          regions,
+          taxes,
+          timezone,
+        });
+
+        // Expect the PDF generation to be successful
+        expect(pdfResult.status).toEqual('success');
+
+        // Load the generated PDF content
+        const pdfDataBuffer = fs.readFileSync(`invoice-${date}.pdf`);
+        const pdfText = await extractPdfText(pdfDataBuffer);
+
+        // Check the content of the PDF
+        expect(pdfText).toContain('Page 1 of 1');
+        expect(pdfText).toContain(`Invoice Date: ${invoice.date}`);
+        expect(pdfText).toContain('Invoice To:');
+        expect(pdfText).toContain(
+          `Remit to: ${getExpectedRemitAddressText(
+            account.country,
+            isAkamaiBilling,
+            isInternational
+          )}`
+        );
+        expect(pdfText).toContain('Tax ID(s):');
+
+        if (account.country === 'AE') {
+          expect(pdfText).toContain(`Tax Invoice: #${invoice.id}`);
+        } else {
+          expect(pdfText).toContain(`Invoice: #${invoice.id}`);
+        }
+
+        // Verify table header
+        const colHeaders = [
+          'Description',
+          'From',
+          'To',
+          'Quantity',
+          'Unit Price',
+          'Amount',
+          'Tax',
+          'Total',
+        ];
+        colHeaders.forEach((header) => {
+          expect(pdfText).toContain(header);
+        });
+
+        // Verify table rows
+        items.forEach((row) => {
+          expect(pdfText).toContain(row.amount);
+          expect(pdfText).toContain(row.label.replace(' -', ''));
+          expect(pdfText).toContain(row.quantity);
+          expect(pdfText).toContain(row.unit_price);
+          expect(pdfText).toContain(row.tax);
+          expect(pdfText).toContain(row.total);
+        });
+
+        // Verify amount section
+        expect(pdfText).toContain(`Subtotal (USD) $${invoice.subtotal}`);
+        expect(pdfText).toContain(`Tax Subtotal (USD) $${invoice.tax}`);
+        expect(pdfText).toContain(`Total (USD) $${invoice.total}`);
+
+        // Verify footer text
+        expect(pdfText).toContain(
+          'This invoice may include Linode Compute Instances that have been powered off as the data is maintained and resources are still reserved. If you no longer need powered-down Linodes, you can remove the service (https://techdocs.akamai.com/cloud-computing/docs/stop-further-billing) from your account.'
+        );
+
+        // Cleanup: Delete the generated PDF file after testing
+        fs.unlinkSync(`invoice-${date}.pdf`);
+      }
+    );
   });
 });
