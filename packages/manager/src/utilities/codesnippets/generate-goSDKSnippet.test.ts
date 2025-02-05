508323221e588f5e4e0dbbf65feeb9aba1b8dae4
Banks Nussman <banks@nussman.us>
2025-02-04 19:19:59 -0500

    fix tests now that we remove random new lines

--- packages/manager/src/utilities/codesnippets/generate-goSDKSnippet.test.ts
+++ packages/manager/src/utilities/codesnippets/generate-goSDKSnippet.test.ts
@@ -9,7 +9,7 @@ describe('generateGoLinodeSnippet', () => {
       type: 'g6-nanode-1',
     };
 
-    const expectedOutput = `linodeClient.CreateInstance(\n    context.Background(),\n    linodego.InstanceCreateOptions{\n        Type: "g6-nanode-1",\n        Region: "us-central",\n        Label: "TestInstance",\n        RootPass: "securePassword123",\n    },\n)\n`;
+    const expectedOutput = `linodeClient.CreateInstance(\n    context.Background(),\n    linodego.InstanceCreateOptions{\n        Type: "g6-nanode-1",\n        Region: "us-central",\n        Label: "TestInstance",\n        RootPass: "securePassword123",\n    },\n)`;
 
     expect(generateGoLinodeSnippet(config)).toEqual(expectedOutput);
   });
@@ -22,7 +22,7 @@ describe('generateGoLinodeSnippet', () => {
       type: 'g6-"nanode-1',
     };
 
-    const expectedOutput = `linodeClient.CreateInstance(\n    context.Background(),\n    linodego.InstanceCreateOptions{\n        Type: "g6-\\"nanode-1",\n        Region: "us-\\"central",\n        Label: "Test\\"Instance",\n        RootPass: "secure\\"Password123",\n    },\n)\n`;
+    const expectedOutput = `linodeClient.CreateInstance(\n    context.Background(),\n    linodego.InstanceCreateOptions{\n        Type: "g6-\\"nanode-1",\n        Region: "us-\\"central",\n        Label: "Test\\"Instance",\n        RootPass: "secure\\"Password123",\n    },\n)`;
 
     expect(generateGoLinodeSnippet(config)).toEqual(expectedOutput);
   });
@@ -46,7 +46,7 @@ describe('generateGoLinodeSnippet', () => {
       type: 'g6-standard-1',
     };
 
-    const expectedOutput = `linodeClient.CreateInstance(\n    context.Background(),\n    linodego.InstanceCreateOptions{\n        Type: "g6-standard-1",\n        Region: "us-east",\n        Label: "TestInstance",\n        Image: "linode/ubuntu20.04",\n        RootPass: "examplePassword",\n        AuthorizedKeys: []string{"ssh AAA..."},\n        AuthorizedUsers: []string{"user1"},\n        SwapSize: 512,\n        BackupsEnabled: true,\n        PrivateIP: true,\n        Tags: []string{"tag1", "tag2"},\n        StackScriptID: 67890,\n        BackupID: 23456,\n        FirewallID: 12345,\n        Metadata: &linodego.InstanceMetadataOptions{\n            UserData: "echo Hello World",\n        },\n    },\n)\n`;
+    const expectedOutput = `linodeClient.CreateInstance(\n    context.Background(),\n    linodego.InstanceCreateOptions{\n        Type: "g6-standard-1",\n        Region: "us-east",\n        Label: "TestInstance",\n        Image: "linode/ubuntu20.04",\n        RootPass: "examplePassword",\n        AuthorizedKeys: []string{"ssh AAA..."},\n        AuthorizedUsers: []string{"user1"},\n        SwapSize: 512,\n        BackupsEnabled: true,\n        PrivateIP: true,\n        Tags: []string{"tag1", "tag2"},\n        StackScriptID: 67890,\n        BackupID: 23456,\n        FirewallID: 12345,\n        Metadata: &linodego.InstanceMetadataOptions{\n            UserData: "echo Hello World",\n        },\n    },\n)`;
 
     expect(generateGoLinodeSnippet(config)).toEqual(expectedOutput);
   });
