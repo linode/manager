a346294c48cae6cdc8f3846ca3ca807497ced9ca
vmangalr <vmangalr@akamai.com>
2025-02-10 18:25:06 +0530

    upcoming: [DI-23226] - UT updates

--- packages/manager/src/features/CloudPulse/Alerts/AlertsResources/AlertsResourcesFilterRenderer.test.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsResources/AlertsResourcesFilterRenderer.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
+import { getAlertResourceFilterProps } from '../Utils/AlertResourceUtils';
 import { AlertResourcesFilterRenderer } from './AlertsResourcesFilterRenderer';
 import { serviceToFiltersMap } from './constants';
 
@@ -9,11 +10,20 @@ describe('AlertsResourcesFilterRenderer', () => {
   const filters = serviceToFiltersMap['dbaas']; // get filters for dbaas service type
   it('renders the correct filter components based on properties passed', () => {
     const handleFilterChangeMock = vi.fn();
+    const engineProps = getAlertResourceFilterProps({
+      filterKey: 'engineType',
+      handleFilterChange: handleFilterChangeMock,
+      handleFilteredRegionsChange: handleFilterChangeMock,
+      regionOptions: [],
+    });
+    const enginePropKeys = Object.keys(engineProps);
+    expect(enginePropKeys.includes('handleFilterChange')).toBeTruthy();
+    expect(enginePropKeys.includes('handleSelectionChange')).toBeFalsy();
     const { getByPlaceholderText } = renderWithTheme(
       // check for database engine filter
       <AlertResourcesFilterRenderer
         component={filters[0].component}
-        componentProps={{ handleFilterChange: handleFilterChangeMock }}
+        componentProps={engineProps}
       />
     );
 
@@ -21,14 +31,21 @@ describe('AlertsResourcesFilterRenderer', () => {
       getByPlaceholderText('Select a Database Engine')
     ).toBeInTheDocument();
 
+    const regionProps = getAlertResourceFilterProps({
+      filterKey: 'region',
+      handleFilterChange: handleFilterChangeMock,
+      handleFilteredRegionsChange: handleFilterChangeMock,
+      regionOptions: [],
+    });
+    const regionPropKeys = Object.keys(regionProps);
+    expect(regionPropKeys.includes('handleFilterChange')).toBeFalsy();
+    expect(regionPropKeys.includes('handleSelectionChange')).toBeTruthy();
+
     // check for region filter
     renderWithTheme(
       <AlertResourcesFilterRenderer
-        componentProps={{
-          handleSelectionChange: handleFilterChangeMock,
-          regionOptions: [],
-        }}
         component={filters[1].component}
+        componentProps={regionProps}
       />
     );
 
