fcadf4ad9f1b937bf66d4228a72831d7a6f2819d
vmangalr <vmangalr@akamai.com>
2025-02-12 12:45:01 +0530

    DI-23436: notice panel button corrections

--- packages/manager/src/features/CloudPulse/Alerts/AlertsResources/AlertsResourcesNotice.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsResources/AlertsResourcesNotice.tsx
@@ -1,6 +1,8 @@
-import { Notice, StyledLinkButton, Typography } from '@linode/ui';
+import { Notice, Typography } from '@linode/ui';
 import { styled } from '@mui/material/styles';
 import React from 'react';
+
+import { LinkButton } from 'src/components/LinkButton';
 
 import type { SelectUnselectAll } from './AlertsResources';
 
@@ -38,22 +40,31 @@ export const AlertsResourcesNotice = React.memo(
           {selectedResources} of {totalResources} resources are selected.
         </Typography>
         {isSelectAll && (
-          <StyledLinkButton
+          <LinkButton
+            onClick={() => {
+              event?.stopPropagation();
+
+              handleSelectionChange('Select All');
+            }}
             aria-label="Select All Resources"
             data-testid="select_all_notice"
-            onClick={() => handleSelectionChange('Select All')}
+            style={{ display: 'inline-block' }}
           >
             Select All
-          </StyledLinkButton>
+          </LinkButton>
         )}
         {!isSelectAll && (
-          <StyledLinkButton
+          <LinkButton
+            onClick={() => {
+              event?.stopPropagation();
+              handleSelectionChange('Unselect All');
+            }}
             aria-label="Unselect All Resources"
             data-testid="unselect_all_notice"
-            onClick={() => handleSelectionChange('Unselect All')}
+            style={{ display: 'inline-block' }}
           >
             Unselect All
-          </StyledLinkButton>
+          </LinkButton>
         )}
       </StyledNotice>
     );
