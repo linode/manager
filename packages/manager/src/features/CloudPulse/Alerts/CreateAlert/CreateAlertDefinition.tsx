74784cb923b56264eb3bd00d9d1d099ce395b152
hasyed-akamai <hasyed@akamai.com>
2025-02-13 22:48:30 +0530

    fix: [M3-9281] - Ensure document titles for Cloud Pulse pages have appropriate keywords

--- packages/manager/src/features/CloudPulse/Alerts/CreateAlert/CreateAlertDefinition.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/CreateAlert/CreateAlertDefinition.tsx
@@ -7,6 +7,7 @@ import { useHistory } from 'react-router-dom';
 
 import { ActionsPanel } from 'src/components/ActionsPanel/ActionsPanel';
 import { Breadcrumb } from 'src/components/Breadcrumb/Breadcrumb';
+import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import { useCreateAlertDefinition } from 'src/queries/cloudpulse/alerts';
 
 import { MetricCriteriaField } from './Criteria/MetricCriteria';
@@ -111,81 +112,86 @@ export const CreateAlertDefinition = () => {
   });
 
   return (
-    <Paper sx={{ paddingLeft: 1, paddingRight: 1, paddingTop: 2 }}>
-      <Breadcrumb crumbOverrides={overrides} pathname="/Definitions/Create" />
-      <FormProvider {...formMethods}>
-        <form onSubmit={onSubmit}>
-          <Typography marginTop={2} variant="h2">
-            1. General Information
-          </Typography>
-          <Controller
-            render={({ field, fieldState }) => (
-              <TextField
-                data-testid="alert-name"
-                errorText={fieldState.error?.message}
-                label="Name"
-                name="label"
-                onBlur={field.onBlur}
-                onChange={(e) => field.onChange(e.target.value)}
-                placeholder="Enter Name"
-                value={field.value ?? ''}
-              />
+    <React.Fragment>
+      <DocumentTitleSegment segment="Create an Alert" />
+      <Paper sx={{ paddingLeft: 1, paddingRight: 1, paddingTop: 2 }}>
+        <Breadcrumb crumbOverrides={overrides} pathname="/Definitions/Create" />
+        <FormProvider {...formMethods}>
+          <form onSubmit={onSubmit}>
+            <Typography marginTop={2} variant="h2">
+              1. General Information
+            </Typography>
+            <Controller
+              render={({ field, fieldState }) => (
+                <TextField
+                  data-testid="alert-name"
+                  errorText={fieldState.error?.message}
+                  label="Name"
+                  name="label"
+                  onBlur={field.onBlur}
+                  onChange={(e) => field.onChange(e.target.value)}
+                  placeholder="Enter Name"
+                  value={field.value ?? ''}
+                />
+              )}
+              control={control}
+              name="label"
+            />
+            <Controller
+              render={({ field, fieldState }) => (
+                <TextField
+                  errorText={fieldState.error?.message}
+                  label="Description"
+                  name="description"
+                  onBlur={field.onBlur}
+                  onChange={(e) => field.onChange(e.target.value)}
+                  optional
+                  placeholder="Enter Description"
+                  value={field.value ?? ''}
+                />
+              )}
+              control={control}
+              name="description"
+            />
+            <CloudPulseServiceSelect name="serviceType" />
+            {serviceTypeWatcher === 'dbaas' && (
+              <EngineOption name="engineType" />
             )}
-            control={control}
-            name="label"
-          />
-          <Controller
-            render={({ field, fieldState }) => (
-              <TextField
-                errorText={fieldState.error?.message}
-                label="Description"
-                name="description"
-                onBlur={field.onBlur}
-                onChange={(e) => field.onChange(e.target.value)}
-                optional
-                placeholder="Enter Description"
-                value={field.value ?? ''}
-              />
-            )}
-            control={control}
-            name="description"
-          />
-          <CloudPulseServiceSelect name="serviceType" />
-          {serviceTypeWatcher === 'dbaas' && <EngineOption name="engineType" />}
-          <CloudPulseRegionSelect name="region" />
-          <CloudPulseMultiResourceSelect
-            engine={useWatch({ control, name: 'engineType' })}
-            name="entity_ids"
-            region={useWatch({ control, name: 'region' })}
-            serviceType={serviceTypeWatcher}
-          />
-          <CloudPulseAlertSeveritySelect name="severity" />
-          <MetricCriteriaField
-            setMaxInterval={(interval: number) =>
-              setMaxScrapeInterval(interval)
-            }
-            name="rule_criteria.rules"
-            serviceType={serviceTypeWatcher!}
-          />
-          <TriggerConditions
-            maxScrapingInterval={maxScrapeInterval}
-            name="trigger_conditions"
-          />
-          <AddChannelListing name="channel_ids" />
-          <ActionsPanel
-            primaryButtonProps={{
-              label: 'Submit',
-              loading: formState.isSubmitting,
-              type: 'submit',
-            }}
-            secondaryButtonProps={{
-              label: 'Cancel',
-              onClick: alertCreateExit,
-            }}
-            sx={{ display: 'flex', justifyContent: 'flex-end' }}
-          />
-        </form>
-      </FormProvider>
-    </Paper>
+            <CloudPulseRegionSelect name="region" />
+            <CloudPulseMultiResourceSelect
+              engine={useWatch({ control, name: 'engineType' })}
+              name="entity_ids"
+              region={useWatch({ control, name: 'region' })}
+              serviceType={serviceTypeWatcher}
+            />
+            <CloudPulseAlertSeveritySelect name="severity" />
+            <MetricCriteriaField
+              setMaxInterval={(interval: number) =>
+                setMaxScrapeInterval(interval)
+              }
+              name="rule_criteria.rules"
+              serviceType={serviceTypeWatcher!}
+            />
+            <TriggerConditions
+              maxScrapingInterval={maxScrapeInterval}
+              name="trigger_conditions"
+            />
+            <AddChannelListing name="channel_ids" />
+            <ActionsPanel
+              primaryButtonProps={{
+                label: 'Submit',
+                loading: formState.isSubmitting,
+                type: 'submit',
+              }}
+              secondaryButtonProps={{
+                label: 'Cancel',
+                onClick: alertCreateExit,
+              }}
+              sx={{ display: 'flex', justifyContent: 'flex-end' }}
+            />
+          </form>
+        </FormProvider>
+      </Paper>
+    </React.Fragment>
   );
 };
