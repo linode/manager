133d7aa6cd0aa81f684b68f88c479793764dc321
Banks Nussman <banks@nussman.us>
2025-02-18 22:18:54 -0500

    fix bucket ssl

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummaryClusterConfiguration.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseSummary/DatabaseSummaryClusterConfiguration.tsx
@@ -68,120 +68,130 @@ export const DatabaseSummaryClusterConfiguration = (props: Props) => {
   const STORAGE_COPY =
     'The total disk size is smaller than the selected plan capacity due to overhead from the OS.';
 
-  return (<>
-    <Typography className={classes.header} variant="h3">
-      Cluster Configuration
-    </Typography>
-    <StyledGridContainer container md={11} spacing={0}>
-      <Grid
-        size={{
-          lg: 1,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>Status</StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={2} md={4} xs={8}>
-        <DatabaseStatusDisplay database={database} events={events} />
-      </StyledValueGrid>
-      <Grid
-        size={{
-          lg: 0.9,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>Plan</StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={2.2} md={4} xs={8}>
-        {formatStorageUnits(type.label)}
-      </StyledValueGrid>
-      <Grid
-        size={{
-          lg: 1,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>Nodes</StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={1.7} md={4} xs={8}>
-        {configuration}
-      </StyledValueGrid>
-      <Grid
-        size={{
-          lg: 1.7,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>CPUs</StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={1.5} md={4} xs={8}>
-        {type.vcpus}
-      </StyledValueGrid>
-      <Grid
-        size={{
-          lg: 1,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>Engine</StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={2} md={4} xs={8}>
-        <DatabaseEngineVersion
-          databaseEngine={database.engine}
-          databaseID={database.id}
-          databasePendingUpdates={database.updates.pending}
-          databasePlatform={database.platform}
-          databaseVersion={database.version}
-        />
-      </StyledValueGrid>
-      <Grid
-        size={{
-          lg: 0.9,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>Region</StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={2.2} md={4} xs={8}>
-        {region?.label ?? database.region}
-      </StyledValueGrid>
-      <Grid
-        size={{
-          lg: 1,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>RAM</StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={1.7} md={4} xs={8}>
-        {type.memory / 1024} GB
-      </StyledValueGrid>
-      <Grid
-        size={{
-          lg: 1.7,
-          md: 2,
-          xs: 4
-        }}>
-        <StyledLabelTypography>
-          {database.total_disk_size_gb ? 'Total Disk Size' : 'Storage'}
-        </StyledLabelTypography>
-      </Grid>
-      <StyledValueGrid lg={1.5} md={4} xs={8}>
-        {database.total_disk_size_gb ? (
-          <>
-            {database.total_disk_size_gb} GB
-            <TooltipIcon
-              status="help"
-              sxTooltipIcon={sxTooltipIcon}
-              text={STORAGE_COPY}
-            />
-          </>
-        ) : (
-          convertMegabytesTo(type.disk, true)
-        )}
-      </StyledValueGrid>
-    </StyledGridContainer>
-  </>);
+  return (
+    <>
+      <Typography className={classes.header} variant="h3">
+        Cluster Configuration
+      </Typography>
+      <StyledGridContainer container spacing={0} sx={{ md: 11 }}>
+        <Grid
+          size={{
+            lg: 1,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>Status</StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid lg={2} md={4} xs={8}>
+          <DatabaseStatusDisplay database={database} events={events} />
+        </StyledValueGrid>
+        <Grid
+          size={{
+            lg: 0.9,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>Plan</StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid size={{ lg: 2.2, md: 4, xs: 8 }}>
+          {formatStorageUnits(type.label)}
+        </StyledValueGrid>
+        <Grid
+          size={{
+            lg: 1,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>Nodes</StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid size={{ lg: 1.7, md: 4, xs: 8 }}>
+          {configuration}
+        </StyledValueGrid>
+        <Grid
+          size={{
+            lg: 1.7,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>CPUs</StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid size={{ lg: 1.5, md: 4, xs: 8 }}>
+          {type.vcpus}
+        </StyledValueGrid>
+        <Grid
+          size={{
+            lg: 1,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>Engine</StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid sx={{ lg: 2, md: 4, xs: 8 }}>
+          <DatabaseEngineVersion
+            databaseEngine={database.engine}
+            databaseID={database.id}
+            databasePendingUpdates={database.updates.pending}
+            databasePlatform={database.platform}
+            databaseVersion={database.version}
+          />
+        </StyledValueGrid>
+        <Grid
+          size={{
+            lg: 0.9,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>Region</StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid size={{ lg: 2.2, md: 4, xs: 8 }}>
+          {region?.label ?? database.region}
+        </StyledValueGrid>
+        <Grid
+          size={{
+            lg: 1,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>RAM</StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid size={{ lg: 1.7, md: 4, xs: 8 }}>
+          {type.memory / 1024} GB
+        </StyledValueGrid>
+        <Grid
+          size={{
+            lg: 1.7,
+            md: 2,
+            xs: 4,
+          }}
+        >
+          <StyledLabelTypography>
+            {database.total_disk_size_gb ? 'Total Disk Size' : 'Storage'}
+          </StyledLabelTypography>
+        </Grid>
+        <StyledValueGrid size={{ lg: 1.5, md: 4, xs: 8 }}>
+          {database.total_disk_size_gb ? (
+            <>
+              {database.total_disk_size_gb} GB
+              <TooltipIcon
+                status="help"
+                sxTooltipIcon={sxTooltipIcon}
+                text={STORAGE_COPY}
+              />
+            </>
+          ) : (
+            convertMegabytesTo(type.disk, true)
+          )}
+        </StyledValueGrid>s
+      </StyledGridContainer>
+    </>
+  );
 };
 
 export default DatabaseSummaryClusterConfiguration;
