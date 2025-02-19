807300e76eabfc188d7330bdfdcffc96db87c5f0
mjac0bs <mjacobs@akamai.com>
2025-02-18 19:57:23 -0800

    Update autoscaler copy now rather than in drawer refactor

--- packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AutoscalePoolDialog.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AutoscalePoolDialog.tsx
@@ -162,12 +162,14 @@ export const AutoscalePoolDialog = (props: Props) => {
         </Notice>
       ) : null}
       <Typography>
-        Set minimum and maximum node pool constraints for LKE to resize your
-        cluster automatically based on resource demand and overall usage.
-        Maximum limit is 100 nodes.{' '}
-        <Link to="https://techdocs.akamai.com/cloud-computing/docs/manage-nodes-and-node-pools">
-          Learn more.
+        Enable the built-in autoscaler (and define the minimum and maximum node
+        constraints) to automatically add and remove nodes based on resource
+        demand and usage.
+        <br />
+        <Link to="https://techdocs.akamai.com/cloud-computing/docs/manage-nodes-and-node-pools#autoscale-automatically-resize-node-pools">
+          Learn more
         </Link>
+        .
       </Typography>
       <form onSubmit={handleSubmit}>
         <FormControlLabel
