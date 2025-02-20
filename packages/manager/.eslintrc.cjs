4241754c1d79c225ed52ed25d167ca9396a0bc0d
dmcintyr-akamai <dmcintyr@akamai.com>
2025-02-20 11:33:02 -0500

    M3-8070 another directory linted

--- packages/manager/.eslintrc.cjs
+++ packages/manager/.eslintrc.cjs
@@ -22,7 +22,6 @@ module.exports = {
     'build',
     'storybook-static',
     '.storybook',
-    'e2e/core/general',
     'e2e/core/helpAndSupport',
     'e2e/core/images',
     'e2e/core/kubernetes',
