cf9f20c98b2e66ef54b46964f39d1b49eb785077
Connie Liu <139280159+coliu-akamai@users.noreply.github.com>
2025-02-20 09:33:25 -0500

    refactor: [M3-9340] - Refactor CreateFirewallDrawer to use react-hook-form (#11677)
    * a lot of tweaks to that schema......
    
    * convert createFirewallDrawer to react-hook-form
    
    * error update
    
    * changesets
    
    * address feedback, investigate more
    
    * address feedback @abailly-akamai @dwiley-akamai

--- packages/manager/src/features/Firewalls/FirewallLanding/constants.ts
+++ packages/manager/src/features/Firewalls/FirewallLanding/constants.ts
@@ -1,2 +1,11 @@
 export const LINODE_CREATE_FLOW_TEXT = 'Additional Linodes';
 export const NODEBALANCER_CREATE_FLOW_TEXT = 'Additional NodeBalancers';
+
+export const FIREWALL_LABEL_TEXT = 'Assign services to the Firewall';
+export const FIREWALL_HELPER_TEXT =
+  'Assign one or more services to this firewall. You can add services later if you want to customize your rules first.';
+
+export const READ_ONLY_DEVICES_HIDDEN_MESSAGE =
+  'Only services you have permission to modify are shown.';
+export const NODEBALANCER_HELPER_TEXT =
+  "Only the firewall's inbound rules apply to NodeBalancers.";
