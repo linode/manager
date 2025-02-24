df6c54a0acbc6e6a709b524d0828a175cf53a0aa
pmakode-akamai <pmakode@akamai.com>
2025-02-24 20:55:56 +0530

    Avoid importing css in `accessibilty.ts`

--- packages/utilities/src/helpers/accessibility.ts
+++ packages/utilities/src/helpers/accessibility.ts
@@ -1,5 +1,3 @@
-import './sr-only.css';
-
 /**
  * Function to send aria-live messages
  * For instance, when page is loading
@@ -9,7 +7,21 @@ export const srSpeak = (text: string, priority: 'assertive' | 'polite') => {
   const id = 'speak-' + Math.random().toString(36).substr(2, 9);
   el.setAttribute('id', id);
   el.setAttribute('aria-live', priority || 'polite');
-  el.classList.add('sr-only');
+
+  const srOnlyStyles = {
+    borderWidth: '0',
+    clip: 'rect(0, 0, 0, 0)',
+    height: '1px',
+    margin: '-1px',
+    overflow: 'hidden',
+    padding: '0',
+    position: 'absolute',
+    whiteSpace: 'nowrap',
+    width: '1px',
+  };
+
+  Object.assign(el.style, srOnlyStyles);
+
   document.body.appendChild(el);
   const elementById: HTMLElement | null = document.getElementById(id);
 
