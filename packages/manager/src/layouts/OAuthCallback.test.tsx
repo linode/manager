cbda2502bffae2dcb05524799ae28bab8548aea1
Hussain Khalil <122488130+hkhalil-akamai@users.noreply.github.com>
2025-02-25 11:51:22 -0500

    refactor: [M3-8711] - Read auth token from local storage (#11650)
    * PROGRESS...
    
    * Fix unit tests
    
    * Refactor OAuth callback into functional component
    
    * Refactor session and OAuthCallback
    
    * Re-introduce redirect from useInitialRequests
    
    * Move util
    
    * Fix bug in revoke token util
    
    * Fix failing e2e test
    
    * Add dependency array to useEffect @abailly-akamai
    
    * Fix Logout unit test (hopefully)
    
    * Fix failing unit test
    
    * More unit test fixes
    
    * loggedInAsCustomer -> isLoggedInAsCustomer

--- packages/manager/src/layouts/OAuthCallback.test.tsx
+++ packages/manager/src/layouts/OAuthCallback.test.tsx
@@ -1,16 +1,38 @@
+import { waitFor } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { isEmpty } from 'ramda';
 import * as React from 'react';
 import { act } from 'react-dom/test-utils';
 
 import { LOGIN_ROOT } from 'src/constants';
-import { OAuthCallbackPage } from 'src/layouts/OAuth';
+import { getAuthToken } from 'src/utilities/authentication';
 import { getQueryParamsFromQueryString } from 'src/utilities/queryParams';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
-import type { OAuthQueryParams } from './OAuth';
+import { OAuthCallback } from './OAuthCallback';
+
+import type { OAuthQueryParams } from './OAuthCallback';
 import type { MemoryHistory } from 'history';
-import type { CombinedProps } from 'src/layouts/OAuth';
+
+const mockHistory = {
+  push: vi.fn(),
+  replace: vi.fn(),
+};
+
+const mockLocation = {
+  search:
+    '?returnTo=%2F&state=9f16ac6c-5518-4b96-b4a6-26a16f85b127&code=bf952e05db75a45a51f5',
+};
+
+// Mock router
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useHistory: vi.fn(() => mockHistory),
+    useLocation: vi.fn(() => mockLocation),
+  };
+});
 
 describe('layouts/OAuth', () => {
   describe('parseQueryParams', () => {
@@ -18,40 +40,6 @@ describe('layouts/OAuth', () => {
     const CODE_VERIFIER_KEY = 'authentication/code-verifier';
     const history: MemoryHistory = createMemoryHistory();
     history.push = vi.fn();
-
-    const location = {
-      hash: '',
-      pathname: '/oauth/callback',
-      search:
-        '?returnTo=%2F&state=9f16ac6c-5518-4b96-b4a6-26a16f85b127&code=bf952e05db75a45a51f5',
-      state: {},
-    };
-
-    const match = {
-      isExact: false,
-      params: {},
-      path: '',
-      url: '',
-    };
-
-    const mockProps: CombinedProps = {
-      dispatchStartSession: vi.fn(),
-      history: {
-        ...history,
-        location: {
-          ...location,
-          search:
-            '?code=test-code&returnTo=/&state=9f16ac6c-5518-4b96-b4a6-26a16f85b127',
-        },
-        push: vi.fn(),
-      },
-      location: {
-        ...location,
-        search:
-          '?code=test-code&returnTo=/&state=9f16ac6c-5518-4b96-b4a6-26a16f85b127',
-      },
-      match,
-    };
 
     const localStorageMock = (() => {
       let store: { [key: string]: string } = {};
@@ -121,11 +109,15 @@ describe('layouts/OAuth', () => {
         ok: false,
       });
 
-      await act(async () => {
-        renderWithTheme(<OAuthCallbackPage {...mockProps} />);
-      });
+      renderWithTheme(<OAuthCallback />);
 
-      expect(mockProps.dispatchStartSession).not.toHaveBeenCalled();
+      await waitFor(() =>
+        expect(getAuthToken()).toEqual({
+          expiration: '',
+          scopes: '',
+          token: '',
+        })
+      );
       expect(window.location.assign).toHaveBeenCalledWith(
         `${LOGIN_ROOT}` + '/logout'
       );
@@ -145,10 +137,10 @@ describe('layouts/OAuth', () => {
       });
 
       await act(async () => {
-        renderWithTheme(<OAuthCallbackPage {...mockProps} />);
+        renderWithTheme(<OAuthCallback />);
       });
 
-      expect(mockProps.dispatchStartSession).not.toHaveBeenCalled();
+      expect(getAuthToken()).toEqual({ expiration: '', scopes: '', token: '' });
       expect(window.location.assign).toHaveBeenCalledWith(
         `${LOGIN_ROOT}` + '/logout'
       );
@@ -168,10 +160,10 @@ describe('layouts/OAuth', () => {
       });
 
       await act(async () => {
-        renderWithTheme(<OAuthCallbackPage {...mockProps} />);
+        renderWithTheme(<OAuthCallback />);
       });
 
-      expect(mockProps.dispatchStartSession).not.toHaveBeenCalled();
+      expect(getAuthToken()).toEqual({ expiration: '', scopes: '', token: '' });
       expect(window.location.assign).toHaveBeenCalledWith(
         `${LOGIN_ROOT}` + '/logout'
       );
@@ -179,10 +171,10 @@ describe('layouts/OAuth', () => {
 
     it('Should redirect to logout path when no code verifier in local storage', async () => {
       await act(async () => {
-        renderWithTheme(<OAuthCallbackPage {...mockProps} />);
+        renderWithTheme(<OAuthCallback />);
       });
 
-      expect(mockProps.dispatchStartSession).not.toHaveBeenCalled();
+      expect(getAuthToken()).toEqual({ expiration: '', scopes: '', token: '' });
       expect(window.location.assign).toHaveBeenCalledWith(
         `${LOGIN_ROOT}` + '/logout'
       );
@@ -211,7 +203,7 @@ describe('layouts/OAuth', () => {
       });
 
       await act(async () => {
-        renderWithTheme(<OAuthCallbackPage {...mockProps} />);
+        renderWithTheme(<OAuthCallback />);
       });
 
       expect(global.fetch).toHaveBeenCalledWith(
@@ -222,27 +214,27 @@ describe('layouts/OAuth', () => {
         })
       );
 
-      expect(mockProps.dispatchStartSession).toHaveBeenCalledWith(
-        '198864fedc821dbb5941cd5b8c273b4e25309a08d31c77cbf65a38372fdfe5b5',
-        'bearer',
-        '*',
-        expect.any(String)
-      );
-      expect(mockProps.history.push).toHaveBeenCalledWith('/');
+      expect(getAuthToken()).toEqual({
+        expiration: expect.any(String),
+        scopes: '*',
+        token:
+          'Bearer 198864fedc821dbb5941cd5b8c273b4e25309a08d31c77cbf65a38372fdfe5b5',
+      });
+      expect(mockHistory.push).toHaveBeenCalledWith('/');
     });
 
     it('Should redirect to login when no code parameter in URL', async () => {
-      mockProps.location.search =
+      mockLocation.search =
         '?returnTo=%2F&state=9f16ac6c-5518-4b96-b4a6-26a16f85b127&code1=bf952e05db75a45a51f5';
       await act(async () => {
-        renderWithTheme(<OAuthCallbackPage {...mockProps} />);
+        renderWithTheme(<OAuthCallback />);
       });
 
-      expect(mockProps.dispatchStartSession).not.toHaveBeenCalled();
+      expect(getAuthToken()).toEqual({ expiration: '', scopes: '', token: '' });
       expect(window.location.assign).toHaveBeenCalledWith(
         `${LOGIN_ROOT}` + '/logout'
       );
-      mockProps.location.search =
+      mockLocation.search =
         '?returnTo=%2F&state=9f16ac6c-5518-4b96-b4a6-26a16f85b127&code=bf952e05db75a45a51f5';
     });
   });
