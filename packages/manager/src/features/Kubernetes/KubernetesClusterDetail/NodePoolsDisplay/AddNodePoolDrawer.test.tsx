fa553020339350dd66b4e85c11f5888c34b092fa
mjac0bs <mjacobs@akamai.com>
2025-02-25 10:37:47 -0800

    Add test coverage for Add Node Pool table

--- packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AddNodePoolDrawer.test.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AddNodePoolDrawer.test.tsx
@@ -22,4 +22,18 @@ describe('AddNodePoolDrawer', () => {
 
     await findByText('Dedicated CPU');
   });
+
+  it('should display the GPU tab for standard clusters', async () => {
+    const { findByText } = renderWithTheme(<AddNodePoolDrawer {...props} />);
+
+    expect(await findByText('GPU')).toBeInTheDocument();
+  });
+
+  it('should not display the GPU tab for enterprise clusters', async () => {
+    const { queryByText } = renderWithTheme(
+      <AddNodePoolDrawer {...props} clusterTier="enterprise" />
+    );
+
+    await expect(queryByText('GPU')).toBeNull();
+  });
 });
