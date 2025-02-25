a98372dd047b762fc40c8c6bd2ef220921db06d9
pmakode-akamai <pmakode@akamai.com>
2025-02-25 18:33:35 +0530

    Move `getDisplayName & `isNumber` utilities

--- packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AddNodePoolDrawer.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AddNodePoolDrawer.tsx
@@ -1,4 +1,5 @@
 import { Box, Notice, Typography } from '@linode/ui';
+import { isNumber } from '@linode/utilities';
 import * as React from 'react';
 import { makeStyles } from 'tss-react/mui';
 
@@ -9,7 +10,6 @@ import { useCreateNodePoolMutation } from 'src/queries/kubernetes';
 import { useAllTypes } from 'src/queries/types';
 import { extendType } from 'src/utilities/extendType';
 import { filterCurrentTypes } from 'src/utilities/filterCurrentLinodeTypes';
-import { isNumber } from 'src/utilities/isNumber';
 import { plansNoticesUtils } from 'src/utilities/planNotices';
 import { pluralize } from 'src/utilities/pluralize';
 import { PRICES_RELOAD_ERROR_NOTICE_TEXT } from 'src/utilities/pricing/constants';
