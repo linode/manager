726dde5dcbec5b218d107bdee293702b59992d40
mjac0bs <mjacobs@akamai.com>
2025-02-25 10:29:20 -0800

    Update test coverage

--- packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AddNodePoolDrawer.test.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesClusterDetail/NodePoolsDisplay/AddNodePoolDrawer.test.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
-import { AddNodePoolDrawer, Props } from './AddNodePoolDrawer';
+import { AddNodePoolDrawer } from './AddNodePoolDrawer';
+
+import type { Props } from './AddNodePoolDrawer';
 
 const props: Props = {
   clusterId: 0,
   clusterLabel: 'test',
   clusterRegionId: 'us-east',
+  clusterTier: 'standard',
   onClose: vi.fn(),
   open: true,
   regionsData: [],
