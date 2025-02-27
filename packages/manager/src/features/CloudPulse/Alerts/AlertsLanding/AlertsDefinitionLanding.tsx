b8541b8bce4b7977f5f70bd48b9cfbb9179ed15f
ankitaakamai <ankitaan@akamai.com>
2025-02-27 11:05:39 +0530

    upcoming: [DI-22920] - PR comments

--- packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/AlertsDefinitionLanding.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/AlertsDefinitionLanding.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import { AlertDetail } from '../AlertsDetail/AlertDetail';
 import { AlertListing } from '../AlertsListing/AlertListing';
@@ -7,23 +7,18 @@ import { CreateAlertDefinition } from '../CreateAlert/CreateAlertDefinition';
 import { EditAlertLanding } from '../EditAlert/EditAlertLanding';
 
 export const AlertDefinitionLanding = () => {
+  const { url } = useRouteMatch();
+
   return (
     <Switch>
-      <Route component={AlertListing} exact path="/alerts/definitions" />
-      <Route exact path="/alerts/definitions/detail/:serviceType/:alertId">
+      <Route component={AlertListing} exact path={`${url}`} />
+      <Route exact path={`${url}/detail/:serviceType/:alertId`}>
         <AlertDetail />
       </Route>
-      <Route
-        component={CreateAlertDefinition}
-        path="/alerts/definitions/create"
-      />
-      <Route exact path="/alerts/definitions/edit/:serviceType/:alertId">
+      <Route component={CreateAlertDefinition} path={`${url}/create`} />
+      <Route exact path={`${url}/edit/:serviceType/:alertId`}>
         <EditAlertLanding />
       </Route>
-      <Route
-        component={CreateAlertDefinition}
-        path="/alerts/definitions/create"
-      />
     </Switch>
   );
 };
