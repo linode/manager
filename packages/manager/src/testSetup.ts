3ba98e38069092852e0a8bd89b5652ff3196b51b
Banks Nussman <banks@nussman.us>
2025-02-27 11:28:37 -0500

    fix things

--- packages/manager/src/testSetup.ts
+++ packages/manager/src/testSetup.ts
@@ -1,7 +1,5 @@
 import * as matchers from '@testing-library/jest-dom/matchers';
-
 import { cleanup } from '@testing-library/react';
-
 import { expect } from 'vitest';
 
 import { server } from './mocks/testServer';
