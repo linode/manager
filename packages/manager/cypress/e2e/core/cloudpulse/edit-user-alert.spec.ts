6245cc7019f69f94a39788e3746fdd46aac90c8f
agorthi <agorthi@akamai.com>
2025-02-27 08:35:51 +0530

    Moving tooltip messages to the constants file

--- packages/manager/cypress/e2e/core/cloudpulse/edit-user-alert.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/edit-user-alert.spec.ts
@@ -5,6 +5,12 @@
  * It verifies that alert details are correctly displayed, interactive, and editable.
  */
 
+import {
+  EVALUATION_PERIOD_DESCRIPTION,
+  METRIC_DESCRIPTION_DATA_FIELD,
+  POLLING_INTERVAL_DESCRIPTION,
+  SEVERITY_LEVEL_DESCRIPTION,
+} from 'support/constants/cloudpulse';
 import { widgetDetails } from 'support/constants/widgets';
 import { mockGetAccount } from 'support/intercepts/account';
 import {
@@ -113,15 +119,6 @@ const notificationChannels = notificationChannelFactory.build({
   label: 'Channel-1',
   type: 'custom',
 });
-
-const METRIC_DESCRIPTION_DATA_FIELD =
-  'Represents the metric you want to receive alerts for. Choose the one that helps you evaluate performance of your service in the most efficient way. For multiple metrics we use the AND method by default.';
-const SEVERITY_LEVEL_DESCRIPTION =
-  'Define a severity level associated with the alert to help you prioritize and manage alerts in the Recent activity tab.';
-const EVALUATION_PERIOD_DESCRIPTION =
-  'Defines the timeframe for collecting data in polling intervals to understand the service performance. Choose the data lookback period where the thresholds are applied to gather the information impactful for your business.';
-const POLLING_INTERVAL_DESCRIPTION =
-  'Choose how often you intend to evaluate the alert condition.';
 
 describe('Integration Tests for Edit Alert', () => {
   /*
