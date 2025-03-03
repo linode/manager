c172e5492159fb6d319d775b66b97c9a03b7f423
hasyed-akamai <hasyed@akamai.com>
2025-03-03 12:04:45 +0530

    fix: [M3-9270] - Confusing wording on DBaaS suspend dialog

--- packages/manager/src/features/Databases/DatabaseDetail/DatabaseSettings/DatabaseSettingsSuspendClusterDialog.tsx
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseSettings/DatabaseSettingsSuspendClusterDialog.tsx
@@ -57,9 +57,7 @@ export const DatabaseSettingsSuspendClusterDialog = (
     setHasConfirmed(false);
   };
 
-  const suspendClusterCopy = `A suspended cluster stops working immediately and you won't be billed for it.
-    You can resume the clusters work within 180 days from its suspension.
-    After that time, the cluster will be deleted permanently.`;
+  const suspendClusterCopy = `A suspended cluster stops immediately and you won't be billed for it. You can resume the cluster within 180 days from its suspension. After that time, the cluster will be deleted permanently.`;
 
   const actions = (
     <ActionsPanel
