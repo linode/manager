ccdb13a94c77063e3140c2beed174f6e84794f87
hasyed-akamai <hasyed@akamai.com>
2025-03-04 13:16:00 +0530

    fix: [M3-9243] - Added show detail dailog for selected Stackscript (#11765)
    * fix: [M3-9243] - Added show detail dailog for selected Stackscript
    
    * Added changeset: Show details button for selected Stackscript
    
    * Type conversion

--- packages/manager/src/features/Linodes/LinodeCreate/Tabs/StackScripts/StackScriptSelectionRow.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/Tabs/StackScripts/StackScriptSelectionRow.tsx
@@ -66,6 +66,7 @@ export const StackScriptSelectionRow = (props: Props) => {
           justifyContent: 'flex-end',
           minWidth: 120,
           paddingRight: 0,
+          height: 44,
         }}
         actionCell
       >
