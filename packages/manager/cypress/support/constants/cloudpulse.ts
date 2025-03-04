b9efe9f39054fc491d822d76539c11921dc14edd
agorthi-akamai <agorthi@akamai.com>
2025-03-04 08:23:33 +0530

    test: [DI-23514] - Add Automation for edit functionality of user defined alert. (#11719)
    * test[DI-23514]:Add Automation for Edit functionality of User defined alert
    
    * test[DI-23514]:Add Automation for Edit functionality of User defined alert
    
    * fixing issue of the specs
    
    * Moving tooltip messages to the constants file
    
    * fixing timerange-verification spec issue
    
    * fixing timerange-verification spec issue
    
    * fixing timerange-verification spec issue
    
    * Rename the variable isStart instead of isCurrent.

--- packages/manager/cypress/support/constants/cloudpulse.ts
+++ packages/manager/cypress/support/constants/cloudpulse.ts
@@ -7,3 +7,30 @@ export const cloudPulseServiceMap: Record<string, string> = {
   dbaas: 'Databases',
   linode: 'Linode',
 };
+/**
+ * Descriptions used in the Create/Edit Alert form to guide users
+ * in configuring alert conditions effectively.
+ */
+export const METRIC_DESCRIPTION_DATA_FIELD =
+  'Represents the metric you want to receive alerts for. Choose the one that helps you evaluate performance of your service in the most efficient way. For multiple metrics we use the AND method by default.';
+
+/**
+ * Defines a severity level associated with the alert
+ * to help prioritize and manage alerts in the Recent Activity tab.
+ */
+export const SEVERITY_LEVEL_DESCRIPTION =
+  'Define a severity level associated with the alert to help you prioritize and manage alerts in the Recent activity tab.';
+
+/**
+ * Defines the timeframe for collecting data in polling intervals
+ * to understand service performance.
+ * Determines the data lookback period where thresholds are applied.
+ */
+export const EVALUATION_PERIOD_DESCRIPTION =
+  'Defines the timeframe for collecting data in polling intervals to understand the service performance. Choose the data lookback period where the thresholds are applied to gather the information impactful for your business.';
+
+/**
+ * Specifies how often the alert condition should be evaluated.
+ */
+export const POLLING_INTERVAL_DESCRIPTION =
+  'Choose how often you intend to evaluate the alert condition.';
