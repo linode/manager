e7bd3ac68032d6a475e2a4c44783b8c3730a6202
pmakode-akamai <pmakode@akamai.com>
2025-03-06 22:42:50 +0530

    Revert LKE-E RegionSelect tooltiptext

--- packages/manager/cypress/e2e/core/kubernetes/lke-create.spec.ts
+++ packages/manager/cypress/e2e/core/kubernetes/lke-create.spec.ts
@@ -1194,7 +1194,9 @@ describe('LKE Cluster Creation with LKE-E', () => {
 
       // Confirm that there is a tooltip explanation for the region dropdown options
       ui.tooltip
-        .findByText('Only regions that support placement groups are listed.')
+        .findByText(
+          'Only regions that support LKE Enterprise clusters are listed.'
+        )
         .should('be.visible');
 
       // Selects an enterprise version
