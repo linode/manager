1e52a36d767b6fd4e8edfc998ab6680293b51976
Connie Liu <139280159+coliu-akamai@users.noreply.github.com>
2025-03-07 10:29:59 -0500

    upcoming: [M3-9482] Enable Firewall query on LinodeEntityDetail only if Linode uses legacy interfaces and fix typo in Firewall Template feature (#11796)
    * enable firewall query only if linode uses legacy configs
    
    * update condition to enable query
    
    * Added changeset: Disable query to get Linode's firewalls for Linodes using new interfaces in LinodeEntityDetail
    
    * fix typo in copy for firewall template
    
    * changeset for typo

--- packages/manager/src/queries/linodes/firewalls.ts
+++ packages/manager/src/queries/linodes/firewalls.ts
@@ -4,7 +4,11 @@ import { linodeQueries } from './linodes';
 
 import type { APIError, Firewall, ResourcePage } from '@linode/api-v4';
 
-export const useLinodeFirewallsQuery = (linodeId: number) =>
-  useQuery<ResourcePage<Firewall>, APIError[]>(
-    linodeQueries.linode(linodeId)._ctx.firewalls
-  );
+export const useLinodeFirewallsQuery = (
+  linodeId: number,
+  enabled: boolean = true
+) =>
+  useQuery<ResourcePage<Firewall>, APIError[]>({
+    ...linodeQueries.linode(linodeId)._ctx.firewalls,
+    enabled,
+  });
