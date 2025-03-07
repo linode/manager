9b088ee84453b2c71c0cfca41e99141053fff9f8
ankitaakamai <ankitaan@akamai.com>
2025-03-07 16:05:25 +0530

    upcoming: [DI-22920] - Add megamenu changes

--- packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/AlertsLanding.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/AlertsLanding.tsx
@@ -1,4 +1,5 @@
 import { Box, Paper } from '@linode/ui';
+import { createLazyRoute } from '@tanstack/react-router';
 import * as React from 'react';
 import {
   Redirect,
@@ -9,14 +10,20 @@ import {
   useRouteMatch,
 } from 'react-router-dom';
 
-import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import { LandingHeader } from 'src/components/LandingHeader';
+import { SuspenseLoader } from 'src/components/SuspenseLoader';
 import { TabLinkList } from 'src/components/Tabs/TabLinkList';
 import { Tabs } from 'src/components/Tabs/Tabs';
 import { useFlags } from 'src/hooks/useFlags';
 
 import { AlertDefinitionLanding } from './AlertsDefinitionLanding';
 
-import type { EnabledAlertTab } from '../../CloudPulseTabs';
+import type { Tab } from 'src/components/Tabs/TabLinkList';
+
+export type EnabledAlertTab = {
+  isEnabled: boolean;
+  tab: Tab;
+};
 
 export const AlertsLanding = React.memo(() => {
   const flags = useFlags();
@@ -52,14 +59,19 @@ export const AlertsLanding = React.memo(() => {
   };
 
   return (
-    <React.Fragment>
-      <DocumentTitleSegment segment="Alerts" />
-      <Paper sx={{ padding: 2 }}>
-        <Tabs
-          index={activeTabIndex}
-          onChange={handleChange}
-          sx={{ width: '100%' }}
-        >
+    <React.Suspense fallback={<SuspenseLoader />}>
+      <LandingHeader
+        breadcrumbProps={{ pathname: '/alerts' }}
+        docsLabel="Docs"
+        docsLink="https://techdocs.akamai.com/cloud-computing/docs/akamai-cloud-pulse"
+      />
+      <Tabs
+        index={activeTabIndex}
+        onChange={handleChange}
+        sx={{ width: '100%' }}
+      >
+        <TabLinkList tabs={accessibleTabs} />
+        <Paper sx={{ padding: 2 }}>
           <Box
             sx={{
               aligneItems: 'center',
@@ -68,18 +80,20 @@ export const AlertsLanding = React.memo(() => {
               p: 1,
               width: '100%',
             }}
-          >
-            <TabLinkList tabs={accessibleTabs} />
-          </Box>
+          />
           <Switch>
             <Route
               component={AlertDefinitionLanding}
-              path={'/monitor/alerts/definitions'}
+              path={'/alerts/definitions'}
             />
-            <Redirect from="*" to="/monitor/alerts/definitions" />
+            <Redirect from="*" to="/alerts/definitions" />
           </Switch>
-        </Tabs>
-      </Paper>
-    </React.Fragment>
+        </Paper>
+      </Tabs>
+    </React.Suspense>
   );
+});
+
+export const cloudPulseAlertsLandingLazyRoute = createLazyRoute('/alerts')({
+  component: AlertsLanding,
 });
