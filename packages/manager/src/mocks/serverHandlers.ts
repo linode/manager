675c52a10f721f6cfe4eafab86eb2d9bc16efffa
ankitaakamai <ankitaan@akamai.com>
2025-03-12 11:40:53 +0530

    upcoming: [DI-23784] - Enahnce mocks

--- packages/manager/src/mocks/serverHandlers.ts
+++ packages/manager/src/mocks/serverHandlers.ts
@@ -2523,6 +2523,13 @@ export const handlers = [
         type: 'user',
         updated_by: 'user1',
       }),
+      ...alertFactory.buildList(2, {
+        created_by: 'user1',
+        service_type: 'linode',
+        status: 'failed',
+        type: 'user',
+        updated_by: 'user1',
+      }),
     ];
     return HttpResponse.json(makeResourcePage(alerts));
   }),
