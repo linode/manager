d4f83240d716b91698dfdb583f817610050a1a3a
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2025-03-19 10:12:21 -0700

    upcoming: [M3-9517] - Update LKE-E flows to account for LDE being disabled at LA launch (#11880)
    * Update copy in node pool panel for LKE-E
    
    * Always remove the tooltip for LKE-E
    
    * Added changeset: Update LKE-E flows to account for LDE status at LA launch

--- packages/manager/src/features/Kubernetes/ClusterList/constants.ts
+++ packages/manager/src/features/Kubernetes/ClusterList/constants.ts
@@ -1,5 +1,11 @@
 export const ADD_NODE_POOLS_DESCRIPTION =
   'Add groups of Linodes to your cluster. You can have a maximum of 100 Linodes per node pool and a maximum of 250 Linodes per cluster.';
 
+export const ADD_NODE_POOLS_ENTERPRISE_DESCRIPTION =
+  'Add groups of Linodes to your cluster. You can have a maximum of 100 Linodes per node pool.';
+
 export const ADD_NODE_POOLS_ENCRYPTION_DESCRIPTION =
   'Node Pool data is encrypted at rest.';
+
+export const ADD_NODE_POOLS_NO_ENCRYPTION_DESCRIPTION =
+  'Node Pool data is not encrypted at rest for LKE Enterprise clusters.';
