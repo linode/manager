3d1afc8475c0c4c8a92ce7a3d5ce786e0e485a34
Bill Coloe <bcoloe@akamai.com>
2025-03-19 11:49:29 -0500

    Use existing PreContainerDiv style

--- packages/manager/src/components/Breadcrumb/Breadcrumb.styles.tsx
+++ packages/manager/src/components/Breadcrumb/Breadcrumb.styles.tsx
@@ -1,13 +1,11 @@
 import { styled } from '@mui/material';
 
-export const StyledPreContainerDiv = styled('div', { label: 'StyledDiv' })(
-  () => ({
-    alignItems: 'center',
-    display: 'flex',
-    flexWrap: 'wrap',
-    minHeight: 48,
-  })
-);
+export const StyledPreContainerDiv = styled('div', { label: 'StyledDiv' })({
+  alignItems: 'center',
+  display: 'flex',
+  flexWrap: 'wrap',
+  minHeight: 48,
+});
 
 export const StyledRootDiv = styled('div', { label: 'StyledRootDiv' })(
   ({ theme }) => ({
