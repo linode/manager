ca63e9426a12e05fc453fc3bb6a0e28121a88223
nikhagra-akamai <nagrawal@akamai.com>
2025-03-19 22:32:53 +0530

    change: [DI-24111] - Removed styled components

--- packages/manager/src/features/CloudPulse/Widget/components/Zoomer.tsx
+++ packages/manager/src/features/CloudPulse/Widget/components/Zoomer.tsx
@@ -1,12 +1,10 @@
-import { IconButton, styled } from '@mui/material';
+import { IconButton } from '@mui/material';
 import * as React from 'react';
 
 import ZoomInMap from 'src/assets/icons/zoomin.svg';
 import ZoomOutMap from 'src/assets/icons/zoomout.svg';
 
 import { CloudPulseTooltip } from '../../shared/CloudPulseTooltip';
-
-import type { Theme } from '@mui/material';
 
 export interface ZoomIconProperties {
   className?: string;
@@ -15,62 +13,39 @@ export interface ZoomIconProperties {
 }
 
 export const ZoomIcon = React.memo((props: ZoomIconProperties) => {
-  const handleClick = (needZoomIn: boolean) => {
-    props.handleZoomToggle(needZoomIn);
-  };
+  const { handleZoomToggle } = props;
 
-  const ToggleZoomer = () => {
-    if (props.zoomIn) {
-      return (
-        <CloudPulseTooltip placement="bottom-end" title="Minimize">
-          <IconButton
-            sx={{
-              fontSize: 'x-large',
-              padding: 0,
-            }}
-            aria-label="Zoom Out"
-            data-testid="zoom-out"
-            onClick={() => handleClick(false)}
-          >
-            <StyledZoomOut />
-          </IconButton>
-        </CloudPulseTooltip>
-      );
-    }
-
+  if (props.zoomIn) {
     return (
-      <CloudPulseTooltip placement="bottom-end" title="Maximize">
+      <CloudPulseTooltip placement="bottom-end" title="Minimize">
         <IconButton
           sx={{
-            fontSize: 'x-large',
             padding: 0,
           }}
-          aria-label="Zoom In"
-          data-testid="zoom-in"
-          onClick={() => handleClick(true)}
+          aria-label="Zoom Out"
+          color="inherit"
+          data-testid="zoom-out"
+          onClick={() => handleZoomToggle(false)}
         >
-          <StyledZoomIn />
+          <ZoomOutMap />
         </IconButton>
       </CloudPulseTooltip>
     );
-  };
+  }
 
-  return <ToggleZoomer />;
-});
-
-const StyledZoomIn = styled(ZoomInMap, { label: 'StyledZoomIn' })(({ theme }) =>
-  getHoverStyles(theme, true)
-);
-const StyledZoomOut = styled(ZoomOutMap, {
-  label: 'StyledZoomOut',
-})(({ theme }) => getHoverStyles(theme, false));
-
-const getHoverStyles = (theme: Theme, isZoomIn: boolean) => ({
-  '&:hover': {
-    '& path': {
-      fill: theme.color.blue,
-      stroke: isZoomIn ? theme.color.blue : 'none',
-    },
-    cursor: 'pointer',
-  },
+  return (
+    <CloudPulseTooltip placement="bottom-end" title="Maximize">
+      <IconButton
+        sx={{
+          padding: 0,
+        }}
+        aria-label="Zoom In"
+        color="inherit"
+        data-testid="zoom-in"
+        onClick={() => handleZoomToggle(true)}
+      >
+        <ZoomInMap />
+      </IconButton>
+    </CloudPulseTooltip>
+  );
 });
