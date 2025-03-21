35d32618d382ba8fa7334d5ac3defd0ac01aeffa
ecarrill <ecarrill@akamai.com>
2025-03-21 12:14:17 -0700

    PR feedback and merge conflicts

--- packages/manager/src/factories/tags.ts
+++ packages/manager/src/factories/tags.ts
@@ -1,6 +1,6 @@
 import { Factory } from '@linode/utilities';
 
-import type { Tag } from '@linode/api-v4/lib/tags/types';
+import type { Tag } from '@linode/api-v4';
 
 export const tagFactory = Factory.Sync.makeFactory<Tag>({
   label: Factory.each((id) => `tag-${id + 1}`),
