87652bd7d597365a9d9da3e93106825cbdcfe3a8
Hussain Khalil <hkhalil@akamai.com>
2025-03-24 17:50:39 -0400

    Fix unit test

--- packages/shared/vitest.config.ts
+++ packages/shared/vitest.config.ts
@@ -1,6 +1,9 @@
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [svgr({ exportAsDefault: true })],
+
   test: {
     environment: 'jsdom',
     setupFiles: './testSetup.ts',
