678da14ca6ce5c7e756f110c7f24a5ce6260e32b
carrillo-erik <119514965+carrillo-erik@users.noreply.github.com>
2025-03-31 10:02:23 -0700

    refactor: [M3-9390] - Move Tags queries (#11897)
    * refactor: [M3-9390] - Move Tags queries
    
    * Add changesets
    
    * PR feedback and merge conflicts

--- packages/manager/src/factories/tags.ts
+++ packages/manager/src/factories/tags.ts
@@ -1,5 +1,6 @@
-import { Tag } from '@linode/api-v4/lib/tags/types';
 import { Factory } from '@linode/utilities';
+
+import type { Tag } from '@linode/api-v4';
 
 export const tagFactory = Factory.Sync.makeFactory<Tag>({
   label: Factory.each((id) => `tag-${id + 1}`),
--- packages/queries/src/index.ts
+++ packages/queries/src/index.ts
@@ -8,6 +8,7 @@ export * from './nodebalancers';
 export * from './placementGroups';
 export * from './profile';
 export * from './regions';
+export * from './tags';
 export * from './support';
 export * from './vlans';
 export * from './vpcs';
