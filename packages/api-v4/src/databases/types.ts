cc5e777c8fc3b11f32b7ac94bf38736b19bb13f2
mpolotsk-akamai <157619599+mpolotsk-akamai@users.noreply.github.com>
2025-04-01 17:12:24 +0200

    upcoming: [UIE-8516] - DBaaS: Advanced Configuration (Part 2) - add/remove configs (#11885)
    * upcoming: [UIE-8516] - DBaaS: Advanced Config - add/remove new configs, validation
    
    * upcoming: [UIE-8516] - DBaaS: Advanced Config - update styles, add unit tests, timezone validation
    
    * upcoming: [UIE-8516] - DBaaS: Advanced Config - close drawer fix
    
    * fix: [UIE-8516] - DBaaS: Advanced Config - style fix after recent updates
    
    * Added changeset: Remove `engine_config` from the DatabaseEngineConfig type
    
    * Added changeset: DBaaS Advanced Configurations: set up Autocomplete to display categorized options, add/remove configs, and implement a dynamic validation schema for all field types
    
    * fix: [UIE-8516] - DBaaS: Advanced Config - refactoring, validation fix, unit test
    
    * rebase with the latest changes
    
    * update background using correct tokens
    
    ---------
    
    Co-authored-by: cpathipa <119517080+cpathipa@users.noreply.github.com>

--- packages/api-v4/src/databases/types.ts
+++ packages/api-v4/src/databases/types.ts
@@ -58,7 +58,7 @@ export interface ConfigurationItem {
   maxLength?: number; // max length for the text input
   minLength?: number; // min length for the text input
   pattern?: string;
-  type?: string | number | boolean | [string, null] | string[];
+  type?: string | [string, null] | string[];
   enum?: string[];
   restart_cluster?: boolean;
 }
@@ -66,12 +66,10 @@ export interface ConfigurationItem {
 export type ConfigValue = number | string | boolean;
 
 export type ConfigCategoryValues = Record<string, ConfigValue>;
-export interface DatabaseEngineConfig {
-  engine_config: Record<
-    string,
-    Record<string, ConfigurationItem> | ConfigurationItem
-  >;
-}
+export type DatabaseEngineConfig = Record<
+  string,
+  Record<string, ConfigurationItem> | ConfigurationItem
+>;
 export interface DatabaseInstanceAdvancedConfig {
   [category: string]: ConfigCategoryValues | ConfigValue;
 }
