669fff4e74497fccdcbe1c624c62afe3f5870b0f
hasyed-akamai <hasyed@akamai.com>
2025-04-01 11:46:50 +0530

    cleanup

--- packages/utilities/src/hooks/useFormattedDate.test.ts
+++ packages/utilities/src/hooks/useFormattedDate.test.ts
@@ -1,41 +1,13 @@
 import { renderHook } from '@testing-library/react';
 import { useFormattedDate } from './useFormattedDate';
 import { DateTime } from 'luxon';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-// Solution - 01
 describe('useFormattedDate', () => {
   it('returns the correctly formatted date', () => {
     const { result } = renderHook(() => useFormattedDate());
 
     const expectedDate = DateTime.local().toFormat('yyyy-MM-dd');
     expect(result.current).toBe(expectedDate);
-  });
-});
-
-// Solution - 02
-describe('useFormattedDate', () => {
-  it('returns the correctly formatted date', () => {
-    const { result } = renderHook(() => useFormattedDate());
-
-    const expectedDate = new Date().toISOString().split('T')[0];
-    expect(result.current).toBe(expectedDate);
-  });
-});
-
-// Solution - 03
-vi.mock('luxon', () => ({
-  DateTime: {
-    local: vi.fn().mockReturnValue({
-      toFormat: vi.fn().mockReturnValue('2025-03-27'),
-    }),
-  },
-}));
-
-describe('useFormattedDate', () => {
-  it('returns the correctly formatted date', () => {
-    const { result } = renderHook(() => useFormattedDate());
-
-    expect(result.current).toBe('2025-03-27');
   });
 });
