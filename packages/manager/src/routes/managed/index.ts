1fc5398af2055e18c0ad05588ca474509b77a373
Alban Bailly <abailly@akamai.com>
2025-04-03 15:47:16 -0400

    Initial commit: landing routing

--- packages/manager/src/routes/managed/index.ts
+++ packages/manager/src/routes/managed/index.ts
@@ -13,54 +13,42 @@ const managedIndexRoute = createRoute({
   getParentRoute: () => managedRoute,
   path: '/',
 }).lazy(() =>
-  import('src/features/Managed/ManagedLanding').then(
-    (m) => m.managedLandingLazyRoute
-  )
+  import('./managedLazyRoutes').then((m) => m.managedLandingLazyRoute)
 );
 
 const managedSummaryRoute = createRoute({
   getParentRoute: () => managedRoute,
   path: 'summary',
 }).lazy(() =>
-  import('src/features/Managed/ManagedLanding').then(
-    (m) => m.managedLandingLazyRoute
-  )
+  import('./managedLazyRoutes').then((m) => m.managedLandingLazyRoute)
 );
 
 const managedMonitorsRoute = createRoute({
   getParentRoute: () => managedRoute,
   path: 'monitors',
 }).lazy(() =>
-  import('src/features/Managed/ManagedLanding').then(
-    (m) => m.managedLandingLazyRoute
-  )
+  import('./managedLazyRoutes').then((m) => m.managedLandingLazyRoute)
 );
 
 const managedSSHAccessRoute = createRoute({
   getParentRoute: () => managedRoute,
   path: 'ssh-access',
 }).lazy(() =>
-  import('src/features/Managed/ManagedLanding').then(
-    (m) => m.managedLandingLazyRoute
-  )
+  import('./managedLazyRoutes').then((m) => m.managedLandingLazyRoute)
 );
 
 const managedCredentialsRoute = createRoute({
   getParentRoute: () => managedRoute,
   path: 'credentials',
 }).lazy(() =>
-  import('src/features/Managed/ManagedLanding').then(
-    (m) => m.managedLandingLazyRoute
-  )
+  import('./managedLazyRoutes').then((m) => m.managedLandingLazyRoute)
 );
 
 const managedContactsRoute = createRoute({
   getParentRoute: () => managedRoute,
   path: 'contacts',
 }).lazy(() =>
-  import('src/features/Managed/ManagedLanding').then(
-    (m) => m.managedLandingLazyRoute
-  )
+  import('./managedLazyRoutes').then((m) => m.managedLandingLazyRoute)
 );
 
 export const managedRouteTree = managedRoute.addChildren([
