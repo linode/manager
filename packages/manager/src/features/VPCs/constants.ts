0c22eb0997e6d7f3dba1a3b7066e59f71b1b464e
Connie Liu <coliu@akamai.com>
2025-04-09 15:42:44 -0400

    small copy update

--- packages/manager/src/features/VPCs/constants.ts
+++ packages/manager/src/features/VPCs/constants.ts
@@ -15,7 +15,7 @@ export const REGION_CAVEAT_HELPER_TEXT =
   'A Linode may be assigned only to a VPC in the same region.';
 
 export const ASSIGN_LINODES_DRAWER_REBOOT_MESSAGE =
-  'Assigning a Linode to a subnet requires you to reboot the Linode to update its configuration.';
+  'Assigning a Linode using Configuration Profile Interfaces to a subnet requires you to reboot the Linode to update its configuration.';
 
 export const REGIONAL_LINODE_MESSAGE = `Select the Linodes you would like to assign to this subnet. Only Linodes in this VPC's region are displayed.`;
 
@@ -23,9 +23,9 @@ export const MULTIPLE_CONFIGURATIONS_MESSAGE =
   'This Linode has multiple configurations. Select which configuration you would like added to the subnet.';
 
 export const REBOOT_LINODE_WARNING_VPCDETAILS =
-  'Assigned or unassigned Linodes will not take affect until the Linodes are rebooted.';
+  'Assigned or unassigned Linodes using Configuration Profile Interfaces will not take affect until the Linodes are rebooted.';
 
-export const SUBNET_UNASSIGN_LINODES_WARNING = `Unassigning Linodes from a subnet requires you to reboot the Linodes to update its configuration.`;
+export const SUBNET_UNASSIGN_LINODES_WARNING = `Unassigning Linodes using Configuration Profile Interfaces from a subnet requires you to reboot the Linodes to update its configuration.`;
 
 export const VPC_AUTO_ASSIGN_IPV4_TOOLTIP =
   'Automatically assign an IPv4 address as the private IP address for this Linode in the VPC.';
