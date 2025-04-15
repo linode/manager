6d14c7e5982eda99cc250118eb81ef56b1ce6893
Banks Nussman <banks@nussman.us>
2025-04-15 17:20:06 -0400

    update error handling

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/LinodeInterfaces/EditInterfaceDrawer/VPCInterface/VPCIPv4Address.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/LinodeInterfaces/EditInterfaceDrawer/VPCInterface/VPCIPv4Address.tsx
@@ -1,6 +1,14 @@
-import { Checkbox, FormControlLabel, Stack, TextField } from '@linode/ui';
+import {
+  Checkbox,
+  FormControlLabel,
+  Notice,
+  Stack,
+  TextField,
+} from '@linode/ui';
 import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
+
+import { VPCPublicIPLabel } from 'src/features/VPCs/components/VPCPublicIPLabel';
 
 import type {
   LinodeInterface,
@@ -19,39 +27,59 @@ export const VPCIPv4Address = (props: Props) => {
     formState: { errors },
   } = useFormContext<ModifyLinodeInterfacePayload>();
 
+  const error = errors.vpc?.ipv4?.addresses?.[index]?.message;
+
   return (
-    <Controller
-      control={control}
-      name={`vpc.ipv4.addresses.${index}.address`}
-      render={({ field, fieldState }) => (
-        <Stack>
-          <FormControlLabel
-            checked={field.value === 'auto'}
-            control={<Checkbox />}
-            label="Auto-assign a VPC IPv4 Address"
-            onChange={(e, checked) =>
-              field.onChange(
-                checked
-                  ? 'auto'
-                  : (linodeInterface.vpc?.ipv4.addresses[index].address ?? '')
-              )
-            }
-          />
-          {field.value !== 'auto' && (
-            <TextField
-              errorText={
-                fieldState.error?.message ??
-                errors.vpc?.ipv4?.addresses?.[index]?.message
-              }
-              label="VPC IPv4 Address" // @todo update this when we support many VPC IPv4s
-              onChange={field.onChange}
-              // eslint-disable-next-line sonarjs/no-hardcoded-ip
-              placeholder="10.0.0.5"
-              value={field.value}
+    <Stack spacing={1}>
+      {error && <Notice text={error} variant="error" />}
+      <Stack spacing={1.5}>
+        <Controller
+          control={control}
+          name={`vpc.ipv4.addresses.${index}.address`}
+          render={({ field, fieldState }) => (
+            <Stack rowGap={1}>
+              <FormControlLabel
+                checked={field.value === 'auto'}
+                control={<Checkbox />}
+                label="Auto-assign a VPC IPv4 Address"
+                sx={{ pl: 0.3 }}
+                onChange={(e, checked) =>
+                  field.onChange(
+                    checked
+                      ? 'auto'
+                      : (linodeInterface.vpc?.ipv4.addresses[index].address ??
+                          '')
+                  )
+                }
+              />
+              {field.value !== 'auto' && (
+                <TextField
+                  errorText={fieldState.error?.message}
+                  label="VPC IPv4 Address"
+                  noMarginTop
+                  onChange={field.onChange}
+                  // eslint-disable-next-line sonarjs/no-hardcoded-ip
+                  placeholder="10.0.0.5"
+                  value={field.value}
+                />
+              )}
+            </Stack>
+          )}
+        />
+        <Controller
+          control={control}
+          name={`vpc.ipv4.addresses.${index}.nat_1_1_address`}
+          render={({ field }) => (
+            <FormControlLabel
+              checked={Boolean(field.value)}
+              control={<Checkbox />}
+              label={<VPCPublicIPLabel />}
+              onChange={(e, checked) => field.onChange(checked ? 'auto' : null)}
+              sx={{ ml: '-8px !important' }}
             />
           )}
-        </Stack>
-      )}
-    />
+        />
+      </Stack>
+    </Stack>
   );
 };
