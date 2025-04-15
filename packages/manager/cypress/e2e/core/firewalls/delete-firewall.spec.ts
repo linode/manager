28b62d50da448d02ca336447e91091d338353ea2
Joe D'Amore <jdamore@akamai.com>
2025-04-15 17:42:19 -0400

    Add Firewall landing page tests for Linode Interfaces

--- packages/manager/cypress/e2e/core/firewalls/delete-firewall.spec.ts
+++ packages/manager/cypress/e2e/core/firewalls/delete-firewall.spec.ts
@@ -1,20 +1,30 @@
 import { createFirewall } from '@linode/api-v4';
+import { DEFAULT_FIREWALL_TOOLTIP_TEXT } from 'src/features/Firewalls/FirewallLanding/constants';
+
 import { authenticate } from 'support/api/authentication';
 import { ui } from 'support/ui';
-import { cleanUp } from 'support/util/cleanup';
 import { randomLabel } from 'support/util/random';
 
 import { firewallFactory } from 'src/factories/firewalls';
+
+import { accountFactory } from 'src/factories';
+import { mockGetAccount } from 'support/intercepts/account';
+import { mockAppendFeatureFlags } from 'support/intercepts/feature-flags';
+import {
+  mockGetFirewalls,
+  mockGetFirewallSettings,
+} from 'support/intercepts/firewalls';
 
 import type { Firewall } from '@linode/api-v4';
 
 authenticate();
 // Firewall GET API request performance issues need to be addressed in order to unskip this test
 // See M3-9619
-describe.skip('delete firewall', () => {
-  before(() => {
-    cleanUp('firewalls');
-  });
+describe('delete firewall', () => {
+  // TODO Restore clean-up when `deletes a firewall` test is unskipped.
+  // before(() => {
+  //   cleanUp('firewalls');
+  // });
   beforeEach(() => {
     cy.tag('method:e2e');
   });
@@ -25,7 +35,7 @@ describe.skip('delete firewall', () => {
    * - Confirms that firewall is still in landing page list after canceled operation.
    * - Confirms that firewall is removed from landing page list after confirmed operation.
    */
-  it('deletes a firewall', () => {
+  it.skip('deletes a firewall', () => {
     const firewallRequest = firewallFactory.build({
       label: randomLabel(),
     });
@@ -81,5 +91,121 @@ describe.skip('delete firewall', () => {
         cy.findByText(firewall.label).should('not.exist');
       }
     );
+  });
+
+  /*
+   * - Confirms that Firewalls that are designated as default cannot be deleted or disabled.
+   * - Confirms that Firewalls that are not designated as default can be deleted and disabled.
+   */
+  it('cannot delete default Firewalls', () => {
+    const mockFirewallDefaultConfig = firewallFactory.build();
+    const mockFirewallDefaultVpc = firewallFactory.build();
+    const mockFirewallDefaultPublic = firewallFactory.build();
+    const mockFirewallDefaultNodeBalancer = firewallFactory.build();
+    const mockFirewallNotDefault = firewallFactory.build();
+
+    const mockFirewallDefaults = [
+      mockFirewallDefaultConfig,
+      mockFirewallDefaultVpc,
+      mockFirewallDefaultPublic,
+      mockFirewallDefaultNodeBalancer,
+    ];
+    const mockFirewalls = [...mockFirewallDefaults, mockFirewallNotDefault];
+
+    // TODO M3-9775 - Delete feature flag mocks once `linodeInterfaces` flag is deleted.
+    mockAppendFeatureFlags({
+      linodeInterfaces: {
+        enabled: true,
+      },
+    });
+
+    // TODO Delete account capability mock once all accounts have Linode interfaces capability.
+    mockGetAccount(
+      accountFactory.build({
+        capabilities: ['Cloud Firewall', 'Linode Interfaces', 'Linodes'],
+      })
+    );
+
+    mockGetFirewalls(mockFirewalls);
+    mockGetFirewallSettings({
+      default_firewall_ids: {
+        linode: mockFirewallDefaultConfig.id,
+        nodebalancer: mockFirewallDefaultNodeBalancer.id,
+        vpc_interface: mockFirewallDefaultVpc.id,
+        public_interface: mockFirewallDefaultPublic.id,
+      },
+    });
+
+    cy.visitWithLogin('/firewalls');
+
+    // Confirm that each Firewall that is designated as a default is listed
+    // as expected and that they cannot be disabled or deleted.
+    mockFirewallDefaults.forEach((mockFirewall) => {
+      cy.findByText(mockFirewall.label)
+        .should('be.visible')
+        .closest('tr')
+        .within(() => {
+          cy.findByText('DEFAULT').should('be.visible');
+          ui.button
+            .findByTitle('Disable')
+            .should('be.visible')
+            .should('be.disabled')
+            .focus();
+
+          ui.tooltip
+            .findByText(DEFAULT_FIREWALL_TOOLTIP_TEXT)
+            .should('be.visible');
+
+          ui.button
+            .findByTitle('Delete')
+            .should('be.visible')
+            .should('be.disabled')
+            .focus();
+
+          ui.tooltip
+            .findByText(DEFAULT_FIREWALL_TOOLTIP_TEXT)
+            .should('be.visible');
+
+          // Dismiss the tooltip by focusing on another element.
+          cy.findByText(mockFirewall.label).focus();
+        });
+    });
+
+    // Confirm that Firewalls that are not designated as default can be disabled
+    // and deleted as expected.
+    cy.findByText(mockFirewallNotDefault.label)
+      .should('be.visible')
+      .closest('tr')
+      .within(() => {
+        cy.findByText('DEFAULT').should('not.exist');
+
+        ui.button
+          .findByTitle('Disable')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+    ui.dialog
+      .findByTitle(`Disable Firewall ${mockFirewallNotDefault.label}?`)
+      .should('be.visible')
+      .within(() => {
+        ui.button.findByTitle('Cancel').should('be.visible').click();
+      });
+
+    cy.findByText(mockFirewallNotDefault.label)
+      .should('be.visible')
+      .closest('tr')
+      .within(() => {
+        ui.button
+          .findByTitle('Delete')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+    ui.dialog
+      .findByTitle(`Delete Firewall ${mockFirewallNotDefault.label}?`)
+      .should('be.visible');
   });
 });
