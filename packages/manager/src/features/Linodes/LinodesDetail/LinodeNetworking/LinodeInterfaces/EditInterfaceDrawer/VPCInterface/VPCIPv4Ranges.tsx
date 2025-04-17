d637fc4aaaa6b784c3cf1c9eae9aa6ec3e9c6d13
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2025-04-17 15:41:46 -0400

    upcoming: [M3-9650] - Edit VPC Interface Drawer UI for Linode Interfaces (#12039)
    * initial UI for editing a VPC interface
    
    * update error handling
    
    * better error handling and handle some edge cases
    
    * fix spelling
    
    * Added changeset: Edit VPC Interface Drawer UI for Linode Interfaces
    
    * feedback @coliu-akamai
    
    * don't allow VLAN interfaces to be edited
    
    * surface a `vpc.ipv4.addresses` field error @mjac0bs
    
    * fix  typo
    
    * fix placement of the `.`
    
    * Added changeset: `CloseButton` svg enforcing a hardcoded color
    
    * fix `CloseButton` enforcing a specific color
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

---
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/LinodeInterfaces/EditInterfaceDrawer/VPCInterface/VPCIPv4Ranges.tsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  CloseIcon,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from '@linode/ui';
+import React from 'react';
+import { Controller, useFieldArray, useFormContext } from 'react-hook-form';
+
+import { LinkButton } from 'src/components/LinkButton';
+import { VPCRangesDescription } from 'src/features/VPCs/components/VPCRangesDescription';
+
+import type { ModifyLinodeInterfacePayload } from '@linode/api-v4';
+
+export const VPCIPv4Ranges = () => {
+  const { control } = useFormContext<ModifyLinodeInterfacePayload>();
+  const { fields, remove, append } = useFieldArray({
+    control,
+    name: 'vpc.ipv4.ranges',
+  });
+
+  return (
+    <Stack spacing={1}>
+      <Typography variant="h3">IPv4 Ranges</Typography>
+      <VPCRangesDescription />
+      {fields.map((field, index) => (
+        <Stack
+          alignItems="flex-start"
+          direction="row"
+          key={field.id}
+          spacing={1}
+        >
+          <Controller
+            control={control}
+            name={`vpc.ipv4.ranges.${index}.range`}
+            render={({ field, fieldState }) => (
+              <TextField
+                containerProps={{ flexGrow: 1 }}
+                errorText={fieldState.error?.message}
+                hideLabel
+                label={`VPC IPv4 Range ${index}`}
+                onChange={field.onChange}
+                value={field.value}
+              />
+            )}
+          />
+          <IconButton
+            onClick={() => remove(index)}
+            sx={{ p: 1 }}
+            title="Remove"
+          >
+            <CloseIcon />
+          </IconButton>
+        </Stack>
+      ))}
+      <Box>
+        <LinkButton onClick={() => append({ range: '' })}>
+          Add IPv4 Range
+        </LinkButton>
+      </Box>
+    </Stack>
+  );
+};
