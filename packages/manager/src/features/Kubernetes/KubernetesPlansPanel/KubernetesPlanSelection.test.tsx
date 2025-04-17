7c57fe69850891f4994a318cb8f767857982c23a
mjac0bs <mjacobs@akamai.com>
2025-04-17 10:05:41 -0700

    Fix typecheck

--- packages/manager/src/features/Kubernetes/KubernetesPlansPanel/KubernetesPlanSelection.test.tsx
+++ packages/manager/src/features/Kubernetes/KubernetesPlansPanel/KubernetesPlanSelection.test.tsx
@@ -1,6 +1,5 @@
 import { breakpoints } from '@linode/ui';
 import { fireEvent, render, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
 import { extendedTypeFactory } from 'src/factories/types';
