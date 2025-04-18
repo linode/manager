614be9d6433e81824e9225a55b6f8b9b3d6093ff
Bill Coloe <bcoloe@akamai.com>
2025-04-18 14:21:21 -0500

    Set relative positioning on toast parent for smoother animation

--- packages/manager/src/components/Snackbar/Snackbar.tsx
+++ packages/manager/src/components/Snackbar/Snackbar.tsx
@@ -27,10 +27,11 @@ const StyledMaterialDesignContent = styled(MaterialDesignContent)(
   ({ theme }: { theme: Theme }) => ({
     '#notistack-snackbar': {
       alignItems: 'flex-start',
+      position: 'relative',
     },
     '#notistack-snackbar > svg': {
       position: 'absolute',
-      left: '14px',
+      left: '-45px',
     },
     '&.notistack-MuiContent': {
       color: theme.notificationToast.default.color,
