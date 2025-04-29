28745e16843834e09582ba6a631f809cd8fe651c
Bill Coloe <bcoloe@akamai.com>
2025-04-29 17:13:27 -0500

    Add more sections

--- packages/manager/src/features/NewDetailTemplate/NewDetailTemplate.tsx
+++ packages/manager/src/features/NewDetailTemplate/NewDetailTemplate.tsx
@@ -3,7 +3,6 @@ import Grid from '@mui/material/Grid2';
 import { Typography, Box } from '@linode/ui';
 import { TagCell } from 'src/components/TagCell/TagCell';
 
-// SectionHeader styled component
 interface SectionHeaderProps {
   children: React.ReactNode;
 }
@@ -150,79 +149,80 @@ export const NewDetailTemplate = () => {
           </Grid>
         </Grid>
 
-        {/* SECTION 7/8/9: 66%/17%/17% width, 3 columns */}
+        {/* SECTION 7/8/9: 66%/33% width - 3 columns */}
         <Grid container sx={{ mb: 4 }}>
           <Grid size={8} sx={{ pr: 2 }}>
             <SectionHeader>66%/33% width - 3 columns</SectionHeader>
-            <LabelValue label="Label 7.1" value="Value 7.1" />
-            <LabelValue label="Label 7.2" value="Value 7.2" />
-            <LabelValue label="Label 7.3" value="Value 7.3" />
+
+            <Grid container>
+              <Grid size={6}>
+                <LabelValue label="Label 7.1" value="Value 7.1" />
+                <LabelValue label="Label 7.2" value="Value 7.2" />
+                <LabelValue label="Label 7.3" value="Value 7.3" />
+              </Grid>
+
+              <Grid size={6}>
+                <LabelValue label="Label 8.1" value="Value 8.1" />
+                <LabelValue label="Label 8.2" value="Value 8.2" />
+                <LabelValue label="Label 8.3" value="Value 8.3" />
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid size={2} sx={{ pr: 2 }}>
-            <SectionHeader>SECTION NAME</SectionHeader>
-            <LabelValue label="Label 8.1" value="Value 8.1" />
-            <LabelValue label="Label 8.2" value="Value 8.2" />
-            <LabelValue label="Label 8.3" value="Value 8.3" />
-          </Grid>
-          <Grid size={2}>
-            <SectionHeader>SECTION NAME</SectionHeader>
+
+          <Grid size={4}>
+            <SectionHeader>66%/33% width - 3 columns</SectionHeader>
             <LabelValue label="Label 9.1" value="Value 9.1" />
             <LabelValue label="Label 9.2" value="Value 9.2" />
             <LabelValue label="Label 9.3" value="Value 9.3" />
           </Grid>
         </Grid>
 
-        {/* SECTION 10/11/12: 3 columns, equal width */}
+        {/* SECTION 10/11/12: 33%/66% width - 3 columns */}
         <Grid container sx={{ mb: 4 }}>
           <Grid size={4} sx={{ pr: 2 }}>
-            <SectionHeader>33%/33%/33% width - 3 columns</SectionHeader>
+            <SectionHeader>33%/66% width - 3 columns</SectionHeader>
             <LabelValue label="Label 10.1" value="Value 10.1" />
             <LabelValue label="Label 10.2" value="Value 10.2" />
             <LabelValue label="Label 10.3" value="Value 10.3" />
           </Grid>
-          <Grid size={4} sx={{ pr: 2 }}>
-            <SectionHeader>SECTION NAME</SectionHeader>
-            <LabelValue label="Label 11.1" value="Value 11.1" />
-            <LabelValue label="Label 11.2" value="Value 11.2" />
-            <LabelValue label="Label 11.3" value="Value 11.3" />
-          </Grid>
-          <Grid size={4}>
-            <SectionHeader>SECTION NAME</SectionHeader>
-            <LabelValue label="Label 12.1" value="Value 12.1" />
-            <LabelValue label="Label 12.2" value="Value 12.2" />
-            <LabelValue label="Label 12.3" value="Value 12.3" />
+
+          <Grid size={8}>
+            <SectionHeader>33%/66% width - 3 columns</SectionHeader>
+            <Grid container>
+              <Grid size={6}>
+                <LabelValue label="Label 11.1" value="Value 11.1" />
+                <LabelValue label="Label 11.2" value="Value 11.2" />
+                <LabelValue label="Label 11.3" value="Value 11.3" />
+              </Grid>
+
+              <Grid size={6}>
+                <LabelValue label="Label 12.1" value="Value 12.1" />
+                <LabelValue label="Label 12.2" value="Value 12.2" />
+                <LabelValue label="Label 12.3" value="Value 12.3" />
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
 
-        {/* SECTION 13/14/15/16: 3 columns with stacked sections in the middle column */}
+        {/* SECTION 13/14/15/16: 33%/33%/33% width - 3 columns */}
         <Grid container sx={{ mb: 4 }}>
           <Grid size={4} sx={{ pr: 2 }}>
-            <SectionHeader>
-              33%/33%/33% width - 3 columns + several sections in 2nd column
-            </SectionHeader>
+            <SectionHeader>SECTION NAME</SectionHeader>
             <LabelValue label="Label 13.1" value="Value 13.1" />
             <LabelValue label="Label 13.2" value="Value 13.2" />
             <LabelValue label="Label 13.3" value="Value 13.3" />
           </Grid>
           <Grid size={4} sx={{ pr: 2 }}>
-            <Box sx={{ mb: 4 }}>
-              <SectionHeader>SECTION NAME</SectionHeader>
-              <LabelValue label="Label 14.1" value="Value 14.1" />
-              <LabelValue label="Label 14.2" value="Value 14.2" />
-              <LabelValue label="Label 14.3" value="Value 14.3" />
-            </Box>
-            <Box>
-              <SectionHeader>SECTION NAME</SectionHeader>
-              <LabelValue label="Label 15.1" value="Value 15.1" />
-              <LabelValue label="Label 15.2" value="Value 15.2" />
-              <LabelValue label="Label 15.3" value="Value 15.3" />
-            </Box>
+            <SectionHeader>SECTION NAME</SectionHeader>
+            <LabelValue label="Label 14.1" value="Value 14.1" />
+            <LabelValue label="Label 14.2" value="Value 14.2" />
+            <LabelValue label="Label 14.3" value="Value 14.3" />
           </Grid>
           <Grid size={4}>
             <SectionHeader>SECTION NAME</SectionHeader>
-            <LabelValue label="Label 16.1" value="Value 16.1" />
-            <LabelValue label="Label 16.2" value="Value 16.2" />
-            <LabelValue label="Label 16.3" value="Value 16.3" />
+            <LabelValue label="Label 15.1" value="Value 15.1" />
+            <LabelValue label="Label 15.2" value="Value 15.2" />
+            <LabelValue label="Label 15.3" value="Value 15.3" />
           </Grid>
         </Grid>
 
