0229d8173d3717bf1c19c9c8f3cad8399d93c1a8
venkatmano-akamai <vmangalr@akamai.com>
2025-04-30 09:05:01 +0530

    Upcoming: [DI-24747] - Pass filters from widgets using dashboards API call (#12079)
    * DI-24747 - Initial changes for passing dimension filters
    
    * DI-24747 - Changeset
    
    * DI-2474y - fix types
    
    * DI-24748 - fix types
    
    * DI-24747 - Fix cypress
    
    * DI-24747 - Fix eslint warning
    
    * DI-24747 - Fix types
    
    * DI-24747 - Fix changeset
    
    * DI-24747 - Fix cypress
    
    * DI-24747 - PR comments
    
    * DI-24747 - Not pass empty filter array

--- packages/manager/src/factories/dashboards.ts
+++ packages/manager/src/factories/dashboards.ts
@@ -4,6 +4,7 @@ import type {
   CloudPulseMetricsResponse,
   CloudPulseMetricsResponseData,
   Dashboard,
+  Filters,
   MetricDefinition,
   Widgets,
 } from '@linode/api-v4';
@@ -26,12 +27,18 @@ export const dashboardFactory = Factory.Sync.makeFactory<Dashboard>({
   widgets: [],
 });
 
+export const dimensionFilterFactory = Factory.Sync.makeFactory<Filters>({
+  dimension_label: Factory.each((i) => `dimension_${i}`),
+  operator: 'startswith',
+  value: Factory.each((i) => `value_${i}`),
+});
+
 export const widgetFactory = Factory.Sync.makeFactory<Widgets>({
   aggregate_function: 'avg',
   chart_type: Factory.each((i) => chart_type[i % chart_type.length]),
   color: Factory.each((i) => color[i % color.length]),
   entity_ids: Factory.each((i) => [`resource-${i}`]),
-  filters: [],
+  filters: dimensionFilterFactory.buildList(3),
   group_by: Factory.each((i) => [`group_by_${i}`]),
   label: Factory.each((i) => `widget_label_${i}`),
   metric: Factory.each((i) => `widget_metric_${i}`),
