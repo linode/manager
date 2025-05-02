00d9982c640e1c67b0335f47882bc8496a8e51fa
Connie Liu <coliu@akamai.com>
2025-05-02 10:41:44 -0400

    vpc invalidations so far

--- packages/manager/src/hooks/useInterfaceAndFirewallDataForLinode.ts
+++ packages/manager/src/hooks/useInterfaceAndFirewallDataForLinode.ts
@@ -18,13 +18,17 @@ export const useInterfaceAndFirewallDataForLinode = (inputs: {
   linodeId: number;
 }) => {
   const { configId, interfaceId, isLinodeInterface, linodeId } = inputs;
+  console.log('explain', configId, interfaceId, isLinodeInterface, linodeId);
 
   // query to fetch firewalls if this Linode uses config profile interfaces
   const {
     data: attachedFirewallsConfig,
     error: firewallsErrorConfig,
     isLoading: firewallsLoadingConfig,
-  } = useLinodeFirewallsQuery(linodeId, !isLinodeInterface);
+  } = useLinodeFirewallsQuery(
+    linodeId,
+    Boolean(configId && linodeId && !isLinodeInterface)
+  );
 
   // query to fetch firewalls for a Linode Interface (firewalls are attached at the interface level)
   const {
@@ -34,8 +38,9 @@ export const useInterfaceAndFirewallDataForLinode = (inputs: {
   } = useLinodeInterfaceFirewallsQuery(
     linodeId,
     interfaceId,
-    isLinodeInterface
+    Boolean(isLinodeInterface && interfaceId && !configId)
   );
+  console.log(firewallsErrorConfig, firewallsErrorLinodeInterface);
 
   // Depending on which query was fired to fetch firewalls, return appropriate data
   const attachedFirewalls =
--- packages/queries/src/linodes/interfaces.ts
+++ packages/queries/src/linodes/interfaces.ts
@@ -9,6 +9,7 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { firewallQueries } from '../firewalls';
 import { networkingQueries } from '../networking';
+import { vpcQueries } from '../vpcs';
 import { linodeQueries } from './linodes';
 
 import type {
@@ -249,20 +250,27 @@ export const useUpgradeToLinodeInterfacesMutation = (linodeId: number) => {
           queryClient.invalidateQueries({
             queryKey: linodeQueries.linode(linodeId)._ctx.configs.queryKey,
           });
-          queryClient.invalidateQueries({
-            queryKey: linodeQueries.linode(linodeId)._ctx.firewalls.queryKey,
-          });
 
           // Simlar to deleting the interface - because we don't easily know the interface's Firewall here,
           // we'll just invalidate all firewall queries.
-          // If this ever needs to be optimized, we can fetch the interface's firewalls before deletion,
-          // and do a more granular invalidation knowing the firewall ID.
           queryClient.invalidateQueries({
             queryKey: firewallQueries.firewall._def,
           });
           queryClient.invalidateQueries({
             queryKey: firewallQueries.firewalls.queryKey,
           });
+
+          for (const iface of upgradeData.interfaces) {
+            if (iface.vpc) {
+              queryClient.invalidateQueries({
+                queryKey: vpcQueries.vpc(iface.vpc.vpc_id).queryKey,
+              });
+              queryClient.invalidateQueries({
+                queryKey: vpcQueries.vpc(iface.vpc.vpc_id)._ctx.subnets
+                  .queryKey,
+              });
+            }
+          }
         }
       },
     },
