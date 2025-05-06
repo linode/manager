dea9b33c1998dcc11bac70146e88c1c15c99757b
Banks Nussman <banks@nussman.us>
2025-05-05 20:52:32 -0400

    support Linode Interfaces in Linode Create sumary and add unit testing

--- packages/manager/src/features/Linodes/LinodeCreate/Summary/Summary.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/Summary/Summary.tsx
@@ -8,18 +8,20 @@ import { useFormContext, useWatch } from 'react-hook-form';
 
 import { useImageQuery } from 'src/queries/images';
 import { useTypeQuery } from 'src/queries/types';
+import { useIsLinodeInterfacesEnabled } from 'src/utilities/linodes';
 import { getMonthlyBackupsPrice } from 'src/utilities/pricing/backups';
 import { renderMonthlyPriceToCorrectDecimalPlace } from 'src/utilities/pricing/dynamicPricing';
 
 import { getLinodePrice } from './utilities';
 
-import type { CreateLinodeRequest } from '@linode/api-v4';
+import type { LinodeCreateFormValues } from '../utilities';
 
 export const Summary = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const { isLinodeInterfacesEnabled } = useIsLinodeInterfacesEnabled();
 
-  const { control } = useFormContext<CreateLinodeRequest>();
+  const { control } = useFormContext<LinodeCreateFormValues>();
 
   const [
     label,
@@ -34,6 +36,7 @@ export const Summary = () => {
     vpcId,
     diskEncryption,
     clusterSize,
+    linodeInterfaces,
   ] = useWatch({
     control,
     name: [
@@ -49,6 +52,7 @@ export const Summary = () => {
       'interfaces.0.vpc_id',
       'disk_encryption',
       'stackscript_data.cluster_size',
+      'linodeInterfaces',
     ],
   });
 
@@ -63,6 +67,18 @@ export const Summary = () => {
   );
 
   const price = getLinodePrice({ clusterSize, regionId, type });
+
+  const hasVPC = isLinodeInterfacesEnabled
+    ? linodeInterfaces?.some((i) => i.vpc?.subnet_id)
+    : vpcId;
+
+  const hasVLAN = isLinodeInterfacesEnabled
+    ? linodeInterfaces?.some((i) => i.vlan?.vlan_label)
+    : vlanLabel;
+
+  const hasFirewall = isLinodeInterfacesEnabled
+    ? linodeInterfaces.some((i) => i.firewall_id)
+    : firewallId;
 
   const summaryItems = [
     {
@@ -95,7 +111,7 @@ export const Summary = () => {
       item: {
         title: 'VLAN Attached',
       },
-      show: Boolean(vlanLabel),
+      show: hasVLAN,
     },
     {
       item: {
@@ -113,13 +129,13 @@ export const Summary = () => {
       item: {
         title: 'VPC Assigned',
       },
-      show: Boolean(vpcId),
+      show: hasVPC,
     },
     {
       item: {
         title: 'Firewall Assigned',
       },
-      show: Boolean(firewallId),
+      show: hasFirewall,
     },
     {
       item: {
