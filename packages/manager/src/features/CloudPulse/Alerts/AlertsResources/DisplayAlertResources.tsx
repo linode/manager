00c2fe3ad69821fbc1678f3123bc3f8ed449bb01
nikhagra-akamai <nagrawal@akamai.com>
2025-05-07 18:13:19 +0530

    upcoming: [DI-24900] - Updated import

--- packages/manager/src/features/CloudPulse/Alerts/AlertsResources/DisplayAlertResources.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsResources/DisplayAlertResources.tsx
@@ -12,8 +12,8 @@ import { TableRow } from 'src/components/TableRow';
 import { TableRowError } from 'src/components/TableRowError/TableRowError';
 import { TableSortCell } from 'src/components/TableSortCell';
 
-import { isAllPageSelected, isSomeSelected } from '../Utils/AlertResourceUtils';
 import { AlertMaxSelectionText } from '../Utils/AlertMaxSelectionText';
+import { isAllPageSelected, isSomeSelected } from '../Utils/AlertResourceUtils';
 import { serviceTypeBasedColumns } from './constants';
 
 import type { AlertServiceType } from '@linode/api-v4';
