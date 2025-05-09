ee0e0a99020e6b6439afad0bd68a261b0b504f6e
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2025-05-09 14:30:50 -0400

    fix: [M3-9917] - Internal Akamai Employee Firewall Policy banner logic (#12166)
    * save progress
    
    * clean up logic
    
    * unit test
    
    * add comment
    
    * Added changeset: Issue preventing Internal Akamai Employees from creating Linodes using VLAN interfaces
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

--- packages/manager/src/features/Linodes/LinodeCreate/FirewallAuthorization.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/FirewallAuthorization.tsx
@@ -5,7 +5,10 @@ import { useController, useFormContext, useWatch } from 'react-hook-form';
 import { AkamaiBanner } from 'src/components/AkamaiBanner/AkamaiBanner';
 import { useFlags } from 'src/hooks/useFlags';
 
-import type { LinodeCreateFormValues } from './utilities';
+import {
+  getDoesEmployeeNeedToAssignFirewall,
+  type LinodeCreateFormValues,
+} from './utilities';
 
 export const FirewallAuthorization = () => {
   const flags = useFlags();
@@ -17,34 +20,18 @@ export const FirewallAuthorization = () => {
     name: 'firewallOverride',
   });
 
-  const [
-    legacyFirewallId,
-    firstLinodeInterfaceFirewallId,
-    firstLinodeInterfaceType,
-    interfaceGeneration,
-  ] = useWatch({
-    name: [
-      'firewall_id',
-      'linodeInterfaces.0.firewall_id',
-      'linodeInterfaces.0.purpose',
-      'interface_generation',
-    ],
+  const [legacyFirewallId, linodeInterfaces, interfaceGeneration] = useWatch({
     control,
+    name: ['firewall_id', 'linodeInterfaces', 'interface_generation'],
   });
 
-  // Special case ❗️
-  // VLAN interfaces do not support Firewalls, so we hide this notice
-  // if that's what the user selects.
-  if (firstLinodeInterfaceType === 'vlan' && interfaceGeneration === 'linode') {
-    return null;
-  }
+  const userNeedsToAssignFirewall = getDoesEmployeeNeedToAssignFirewall(
+    legacyFirewallId,
+    linodeInterfaces,
+    interfaceGeneration
+  );
 
-  const firewallId =
-    interfaceGeneration === 'linode'
-      ? firstLinodeInterfaceFirewallId
-      : legacyFirewallId;
-
-  if (firewallId || !(fieldState.isDirty || fieldState.error)) {
+  if (!userNeedsToAssignFirewall || !(fieldState.isDirty || fieldState.error)) {
     return null;
   }
 
