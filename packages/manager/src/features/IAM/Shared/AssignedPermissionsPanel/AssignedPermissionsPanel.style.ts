51c7bf3000dc929ad36133b3d51ad7cf900e580e
mpolotsk <mpolotsk@akamai.com>
2025-05-09 16:17:30 +0200

    fix: [UIE-8679, UIE-8532] - Assigned Permissions Panel, Entities Table: Styling Issues

---
+++ packages/manager/src/features/IAM/Shared/AssignedPermissionsPanel/AssignedPermissionsPanel.style.ts
@@ -0,0 +1,27 @@
+import { Box, Paper, styled, Typography } from '@linode/ui';
+
+export const StylesPaper = styled(Paper)(({ theme }) => ({
+  backgroundColor:
+    theme.name === 'light'
+      ? theme.tokens.color.Neutrals[5]
+      : theme.tokens.color.Neutrals[100],
+  marginTop: theme.tokens.spacing.S8,
+  padding: `${theme.tokens.spacing.S12} ${theme.tokens.spacing.S8}`,
+}));
+
+export const StyledTitle = styled(Typography)(({ theme }) => ({
+  font: theme.tokens.alias.Typography.Label.Bold.S,
+}));
+
+export const StyledDescription = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.tokens.spacing.S12,
+  marginTop: theme.tokens.spacing.S6,
+  overflowWrap: 'anywhere',
+  wordBreak: 'normal',
+}));
+
+export const StyledEntityBox = styled(Box)<{
+  hideDetails: boolean | undefined;
+}>(({ theme, hideDetails }) => ({
+  marginTop: !hideDetails ? theme.tokens.spacing.S12 : undefined,
+}));
