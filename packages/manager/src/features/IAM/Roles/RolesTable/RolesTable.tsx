8cc73332ad142c1bdf4fade28e8ea756f45dc0ba
rodonnel-akamai <rodonnel@akamai.com>
2025-05-14 14:02:07 -0400

    feat: [UIE-8734] - IAM RBAC - Assign Selected Roles drawer (#12182)
    * feat: [UIE-8734] - IAM RBAC - Assign Selected Roles drawer
    
    * updated small change
    
    * Addressed Chandras feedback
    
    * Update packages/manager/src/features/IAM/Roles/RolesTable/AssignSelectedRolesDrawer.tsx
    
    Co-authored-by: Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
    
    * fixes from PR comments
    
    ---------
    
    Co-authored-by: Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>

--- packages/manager/src/features/IAM/Roles/RolesTable/RolesTable.tsx
+++ packages/manager/src/features/IAM/Roles/RolesTable/RolesTable.tsx
@@ -16,6 +16,7 @@ import React, { useState } from 'react';
 
 import { DebouncedSearchTextField } from 'src/components/DebouncedSearchTextField';
 import { Link } from 'src/components/Link';
+import { AssignSelectedRolesDrawer } from 'src/features/IAM/Roles/RolesTable/AssignSelectedRolesDrawer';
 import { RolesTableActionMenu } from 'src/features/IAM/Roles/RolesTable/RolesTableActionMenu';
 import { RolesTableExpandedRow } from 'src/features/IAM/Roles/RolesTable/RolesTableExpandedRow';
 import {
@@ -55,6 +56,7 @@ export const RolesTable = ({ roles }: Props) => {
   >(undefined);
 
   const [selectedRows, setSelectedRows] = useState<RoleView[]>([]);
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
   const areAllSelected = React.useMemo(() => {
     return (
@@ -106,148 +108,164 @@ export const RolesTable = ({ roles }: Props) => {
     setRows(filteredRows);
   };
 
+  const assignRoleRow = (row: RoleView) => {
+    setSelectedRows([row]);
+    handleAssignSelectedRoles();
+  };
+
   const handleAssignSelectedRoles = () => {
     // Logic to assign selected roles
-    // console.log('Assigning roles:', selectedRows);
+    setIsDrawerOpen(true);
   };
 
   return (
-    <Paper sx={(theme) => ({ marginTop: theme.tokens.spacing.S16 })}>
-      <Grid
-        container
-        direction="row"
-        spacing={2}
-        sx={{ justifyContent: 'space-between' }}
-      >
+    <>
+      <Paper sx={(theme) => ({ marginTop: theme.tokens.spacing.S16 })}>
         <Grid
           container
           direction="row"
-          sx={{
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-          }}
+          spacing={2}
+          sx={{ justifyContent: 'space-between' }}
         >
-          <DebouncedSearchTextField
-            clearable
-            containerProps={{
-              sx: {
-                width: { md: '416px', xs: '100%' },
-                height: 34,
-              },
+          <Grid
+            container
+            direction="row"
+            sx={{
+              alignItems: 'center',
+              justifyContent: 'flex-start',
             }}
-            debounceTime={250}
-            hideLabel
-            label="Search"
-            onSearch={handleTextFilter}
-            placeholder="Search"
-            value={filterString}
-          />
-          <Select
-            hideLabel
-            label="Select type"
-            onChange={handleChangeEntityTypeFilter}
-            options={filterableOptions}
-            placeholder="All Roles"
-            sx={{ minWidth: 250 }}
-            value={filterableEntityType}
-          />
-        </Grid>
-        <Button
-          buttonType="primary"
-          disabled={selectedRows.length === 0}
-          onClick={() => handleAssignSelectedRoles()}
-          tooltipText={
-            selectedRows.length === 0
-              ? 'You must select some roles to assign them.'
-              : undefined
-          }
-        >
-          Assign Selected Roles
-        </Button>
-      </Grid>
-
-      <Table data-testid="roles-table">
-        <TableHead>
-          <TableRow
-            headerborder
-            select={(event) => handleSelect(event, 'all')}
-            selectable
-            selected={areAllSelected}
           >
-            <TableHeaderCell
-              sort={(event) => handleSort(event, 'name')}
-              sortable
-              sorted={sort?.column === 'name' ? sort.order : undefined}
-              style={{ minWidth: '26%' }}
+            <DebouncedSearchTextField
+              clearable
+              containerProps={{
+                sx: {
+                  width: { md: '416px', xs: '100%' },
+                  height: 34,
+                },
+              }}
+              debounceTime={250}
+              hideLabel
+              label="Search"
+              onSearch={handleTextFilter}
+              placeholder="Search"
+              value={filterString}
+            />
+            <Select
+              hideLabel
+              label="Select type"
+              onChange={handleChangeEntityTypeFilter}
+              options={filterableOptions}
+              placeholder="All Roles"
+              sx={{ minWidth: 250 }}
+              value={filterableEntityType}
+            />
+          </Grid>
+          <Button
+            buttonType="primary"
+            disabled={selectedRows.length === 0}
+            onClick={() => handleAssignSelectedRoles()}
+            tooltipText={
+              selectedRows.length === 0
+                ? 'You must select some roles to assign them.'
+                : undefined
+            }
+          >
+            Assign Selected Roles
+          </Button>
+        </Grid>
+
+        <Table data-testid="roles-table">
+          <TableHead>
+            <TableRow
+              headerborder
+              select={(event) => handleSelect(event, 'all')}
+              selectable
+              selected={areAllSelected}
             >
-              Role
-            </TableHeaderCell>
-            <TableHeaderCell
-              sort={(event) => handleSort(event, 'access')}
-              sortable
-              sorted={sort?.column === 'access' ? sort.order : undefined}
-              style={{ minWidth: '14%' }}
-            >
-              Role Type
-            </TableHeaderCell>
-            <TableHeaderCell
-              sort={(event) => handleSort(event, 'description')}
-              sortable
-              sorted={sort?.column === 'description' ? sort.order : undefined}
-              style={{ minWidth: '38%' }}
-            >
-              Description
-            </TableHeaderCell>
-            <TableHeaderCell style={{ minWidth: '10%' }} />
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {!rows?.length ? (
-            <TableRow>
-              <TableCell>No items to display.</TableCell>
-            </TableRow>
-          ) : (
-            rows.map((roleRow) => (
-              <TableRow
-                expandable
-                hoverable
-                key={roleRow.name}
-                rowborder
-                select={(event) => handleSelect(event, roleRow)}
-                selectable
-                selected={selectedRows.includes(roleRow)}
+              <TableHeaderCell
+                sort={(event) => handleSort(event, 'name')}
+                sortable
+                sorted={sort?.column === 'name' ? sort.order : undefined}
+                style={{ minWidth: '26%' }}
               >
-                <TableCell style={{ minWidth: '26%' }}>
-                  {roleRow.name}
-                </TableCell>
-                <TableCell style={{ minWidth: '14%' }}>
-                  {capitalizeAllWords(roleRow.access, '_')}
-                </TableCell>
-                <TableCell style={{ minWidth: '38%' }}>
-                  {roleRow.permissions.length ? (
-                    roleRow.description
-                  ) : (
-                    // TODO: update the link for the description when it's ready - UIE-8534
-                    <Typography>
-                      {getFacadeRoleDescription(roleRow)}{' '}
-                      <Link to="#">Learn more.</Link>
-                    </Typography>
-                  )}
-                </TableCell>
-                <TableCell style={{ minWidth: '10%' }}>
-                  <RolesTableActionMenu />
-                </TableCell>
-                <TableRowExpanded
-                  slot="expanded"
-                  style={{ marginBottom: 12, padding: 0, width: '100%' }}
-                >
-                  <RolesTableExpandedRow permissions={roleRow.permissions} />
-                </TableRowExpanded>
+                Role
+              </TableHeaderCell>
+              <TableHeaderCell
+                sort={(event) => handleSort(event, 'access')}
+                sortable
+                sorted={sort?.column === 'access' ? sort.order : undefined}
+                style={{ minWidth: '14%' }}
+              >
+                Role Type
+              </TableHeaderCell>
+              <TableHeaderCell
+                sort={(event) => handleSort(event, 'description')}
+                sortable
+                sorted={sort?.column === 'description' ? sort.order : undefined}
+                style={{ minWidth: '38%' }}
+              >
+                Description
+              </TableHeaderCell>
+              <TableHeaderCell style={{ minWidth: '10%' }} />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {!rows?.length ? (
+              <TableRow>
+                <TableCell>No items to display.</TableCell>
               </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
-    </Paper>
+            ) : (
+              rows.map((roleRow) => (
+                <TableRow
+                  expandable
+                  hoverable
+                  key={roleRow.name}
+                  rowborder
+                  select={(event) => handleSelect(event, roleRow)}
+                  selectable
+                  selected={selectedRows.includes(roleRow)}
+                >
+                  <TableCell style={{ minWidth: '26%' }}>
+                    {roleRow.name}
+                  </TableCell>
+                  <TableCell style={{ minWidth: '14%' }}>
+                    {capitalizeAllWords(roleRow.access, '_')}
+                  </TableCell>
+                  <TableCell style={{ minWidth: '38%' }}>
+                    {roleRow.permissions.length ? (
+                      roleRow.description
+                    ) : (
+                      // TODO: update the link for the description when it's ready - UIE-8534
+                      <Typography>
+                        {getFacadeRoleDescription(roleRow)}{' '}
+                        <Link to="#">Learn more.</Link>
+                      </Typography>
+                    )}
+                  </TableCell>
+                  <TableCell style={{ minWidth: '10%' }}>
+                    <RolesTableActionMenu
+                      onClick={() => {
+                        assignRoleRow(roleRow);
+                      }}
+                    />
+                  </TableCell>
+                  <TableRowExpanded
+                    slot="expanded"
+                    style={{ marginBottom: 12, padding: 0, width: '100%' }}
+                  >
+                    <RolesTableExpandedRow permissions={roleRow.permissions} />
+                  </TableRowExpanded>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </Paper>
+      <AssignSelectedRolesDrawer
+        onClose={() => setIsDrawerOpen(false)}
+        open={isDrawerOpen}
+        selectedRoles={selectedRows}
+      />
+    </>
   );
 };
