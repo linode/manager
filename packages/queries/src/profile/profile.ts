881ef426bf59551d19beaeaccf353aa4faf9360a
Banks Nussman <banks@nussman.us>
2025-05-15 11:55:07 -0400

    Revert "add safety to grants fetch to fix e2e"
    This reverts commit 5b6a5379a83dd606576114744861734b964d3447.

--- packages/queries/src/profile/profile.ts
+++ packages/queries/src/profile/profile.ts
@@ -50,22 +50,7 @@ export const profileQueries = createQueryKeys('profile', {
     queryKey: [params, filter],
   }),
   grants: {
-    queryFn: async () => {
-      const grants = await listGrants();
-      // `listGrants` claims to return `Grants`, but it will return an empty string
-      // if an unrestricted makes the request.
-      if (typeof grants === 'string') {
-        // If we get a string, I'm just thowing an APIError[] to prevent our React Query cache
-        // from containing the wrong data, which could cause the app to crash.
-        return Promise.reject([
-          {
-            reason:
-              'Got an empty grants response. The user is likely unrestricted.',
-          },
-        ]);
-      }
-      return grants;
-    },
+    queryFn: listGrants,
     queryKey: null,
   },
   personalAccessTokens: (params: Params = {}, filter: Filter = {}) => ({
