b62d750232e5a4cffb081bc888d168e5e6e0b922
dmcintyr-akamai <dmcintyr@akamai.com>
2025-05-20 10:01:44 -0400

    quota testing w/ feature flag enabled/disabled

---
+++ packages/manager/cypress/e2e/core/cloudpulse/quotas.spec.ts
@@ -0,0 +1,84 @@
+import { mockAppendFeatureFlags } from 'support/intercepts/feature-flags';
+import { ui } from 'support/ui';
+
+describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
+  beforeEach(() => {
+    mockAppendFeatureFlags({
+      limitsEvolution: {
+        enabled: true,
+      },
+    }).as('getFeatureFlags');
+  });
+  it('can navigate directly to Quotas page', () => {
+    cy.visitWithLogin('/account/quotas');
+    cy.wait('@getFeatureFlags');
+    cy.url().should('endWith', '/quotas');
+    cy.findByText('Object Storage').should('be.visible');
+  });
+
+  it('can navigate to the Quotas page via the User Menu', () => {
+    cy.visitWithLogin('/');
+    cy.wait('@getFeatureFlags');
+    cy.get('[data-testid="nav-group-profile"]').should('be.visible').click();
+    ui.userMenu.find().within(() => {
+      cy.get('[data-testid="menu-item-Quotas"]').should('be.visible').click();
+      cy.url().should('endWith', '/quotas');
+    });
+  });
+
+  it('Quotas tab is visible from all other tabs in Account tablist', () => {
+    cy.visitWithLogin('/account/billing');
+    cy.wait('@getFeatureFlags');
+    ui.tabList.find().within(() => {
+      cy.get('a').each(($link) => {
+        cy.wrap($link).click();
+        cy.get('[data-testid="Quotas"]').should('be.visible');
+      });
+    });
+    cy.get('[data-testid="Quotas"]').should('be.visible').click();
+    cy.url().should('endWith', '/quotas');
+  });
+});
+
+describe('Quotas inaccessible when limitsEvolution feature flag disabled', () => {
+  beforeEach(() => {
+    mockAppendFeatureFlags({
+      limitsEvolution: {
+        enabled: false,
+      },
+    }).as('getFeatureFlags');
+  });
+  it('Quotas page is inaccessible', () => {
+    cy.visitWithLogin('/account/quotas');
+    cy.wait('@getFeatureFlags');
+    cy.url().should('endWith', '/billing');
+  });
+
+  it('cannot navigate to the Quotas tab via the Users & Grants link in the User Menu', () => {
+    cy.visitWithLogin('/');
+    cy.wait('@getFeatureFlags');
+    cy.get('[data-testid="nav-group-profile"]').should('be.visible').click();
+    ui.userMenu.find().within(() => {
+      cy.get('[data-testid="menu-item-Quotas"]').should('not.exist');
+      cy.get('[data-testid="menu-item-Users & Grants"]')
+        .should('be.visible')
+        .click();
+    });
+    cy.url().should('endWith', '/users');
+    cy.get('[data-testid="Quotas"]').should('not.exist');
+  });
+
+  it('cannot navigate to the Quotas tab via the Billing link in the User Menu', () => {
+    cy.visitWithLogin('/');
+    cy.wait('@getFeatureFlags');
+    cy.get('[data-testid="nav-group-profile"]').should('be.visible').click();
+    ui.userMenu.find().within(() => {
+      cy.get('[data-testid="menu-item-Quotas"]').should('not.exist');
+      cy.get('[data-testid="menu-item-Billing & Contact Information"]')
+        .should('be.visible')
+        .click();
+    });
+    cy.url().should('endWith', '/billing');
+    cy.get('[data-testid="Quotas"]').should('not.exist');
+  });
+});
