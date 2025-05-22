6c98f21d0a7a2ef54b5d532ed6373a8ec5bcab60
mjac0bs <mjacobs@akamai.com>
2025-05-22 08:48:49 -0700

    Remove check for certain script count

--- packages/manager/src/hooks/useAdobeAnalytics.ts
+++ packages/manager/src/hooks/useAdobeAnalytics.ts
@@ -2,7 +2,7 @@ import { loadScript } from '@linode/utilities'; // `loadScript` from `useScript`
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { ADOBE_ANALYTICS_URL, NUM_ADOBE_SCRIPTS } from 'src/constants';
+import { ADOBE_ANALYTICS_URL } from 'src/constants';
 import { reportException } from 'src/exceptionReporting';
 
 /**
@@ -16,15 +16,8 @@ export const useAdobeAnalytics = () => {
     if (ADOBE_ANALYTICS_URL) {
       loadScript(ADOBE_ANALYTICS_URL, { location: 'head' })
         .then((data) => {
-          const adobeScriptTags = document.querySelectorAll(
-            'script[src^="https://assets.adobedtm.com/"]'
-          );
-          // Log an error; if the promise resolved, the _satellite object and 3 Adobe scripts should be present in the DOM.
-          if (
-            data.status !== 'ready' ||
-            !window._satellite ||
-            adobeScriptTags.length !== NUM_ADOBE_SCRIPTS
-          ) {
+          // Log an error; if the promise resolved and the _satellite object should be present in the DOM.
+          if (data.status !== 'ready' || !window._satellite) {
             reportException(
               'Adobe Analytics error: Not all Adobe Launch scripts and extensions were loaded correctly; analytics cannot be sent.'
             );
