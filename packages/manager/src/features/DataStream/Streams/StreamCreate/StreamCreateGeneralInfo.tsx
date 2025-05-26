2df219702ab3338511e9ebea415c9cfd04c59f26
mduda-akamai <mduda@akamai.com>
2025-05-26 07:55:31 +0200

    [DPS-33112] Add Streams empty state and Create Stream views (#12235)

---
+++ packages/manager/src/features/DataStream/Streams/StreamCreate/StreamCreateGeneralInfo.tsx
@@ -0,0 +1,10 @@
+import { Paper, Typography } from '@linode/ui';
+import React from 'react';
+
+export const StreamCreateGeneralInfo = () => {
+  return (
+    <Paper>
+      <Typography variant="h2">General Information</Typography>
+    </Paper>
+  );
+};
