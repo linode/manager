0d4509cd61e6b1bfa388e63fe42a564a69c11043
dmcintyr-akamai <dmcintyr@akamai.com>
2025-05-28 13:47:37 -0400

    initial commit

--- packages/manager/cypress/e2e/core/account/quotas.spec.ts
+++ packages/manager/cypress/e2e/core/account/quotas.spec.ts
@@ -1,16 +1,22 @@
 import { regionFactory } from '@linode/utilities';
+import { profileFactory } from '@linode/utilities';
 import { mockAppendFeatureFlags } from 'support/intercepts/feature-flags';
+import { mockApiInternalUser } from 'support/intercepts/general';
 import {
   mockGetObjectStorageEndpoints,
   mockGetObjectStorageQuotaError,
   mockGetObjectStorageQuotas,
   mockGetObjectStorageQuotaUsages,
 } from 'support/intercepts/object-storage';
+import { mockGetProfile } from 'support/intercepts/profile';
+import { mockCreateSupportTicket } from 'support/intercepts/support';
 import { ui } from 'support/ui';
 import { randomDomainName, randomLabel } from 'support/util/random';
 
+import { supportTicketFactory } from 'src/factories';
 import { objectStorageEndpointsFactory } from 'src/factories';
 import { quotaFactory, quotaUsageFactory } from 'src/factories/quotas';
+import { getQuotaIncreaseMessage } from 'src/features/Account/Quotas/utils';
 
 const mockRegions = regionFactory.buildList(4, {
   capabilities: ['Object Storage'],
@@ -46,10 +52,12 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
     mockAppendFeatureFlags({
       limitsEvolution: {
         enabled: true,
+        requestForIncreaseDisabledForInternalAccountsOnly: false,
+        // requestForIncreaseDisabledForAll: false
       },
     }).as('getFeatureFlags');
   });
-  it('can navigate directly to Quotas page', () => {
+  xit('can navigate directly to Quotas page', () => {
     cy.visitWithLogin('/account/quotas');
     cy.wait('@getFeatureFlags');
     cy.url().should('endWith', '/quotas');
@@ -58,7 +66,7 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
     ).should('be.visible');
   });
 
-  it('can navigate to the Quotas page via the User Menu', () => {
+  xit('can navigate to the Quotas page via the User Menu', () => {
     cy.visitWithLogin('/');
     cy.wait('@getFeatureFlags');
     // Open user menu
@@ -69,7 +77,7 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
     });
   });
 
-  it('Quotas tab is visible from all other tabs in Account tablist', () => {
+  xit('Quotas tab is visible from all other tabs in Account tablist', () => {
     cy.visitWithLogin('/account/billing');
     cy.wait('@getFeatureFlags');
     ui.tabList.find().within(() => {
@@ -83,6 +91,11 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
   });
 
   it('Endpoint workflow follows proper sequence', () => {
+    const mockProfile = profileFactory.build({
+      email: 'mock-user@linode.com',
+      restricted: false,
+    });
+    mockGetProfile(mockProfile).as('getProfile');
     const mockSelectedEndpoint = mockEndpoints[1];
     const selectedDomain = mockSelectedEndpoint.s3_endpoint || '';
     const mockQuotas = [
@@ -117,21 +130,15 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
     const mockQuotaUsages = [
       quotaUsageFactory.build({
         quota_limit: mockQuotas[0].quota_limit,
-        usage:
-          mockQuotas[0].quota_limit -
-          Math.round(mockQuotas[0].quota_limit * 0.1),
+        usage: Math.round(mockQuotas[0].quota_limit * 0.1),
       }),
       quotaUsageFactory.build({
         quota_limit: mockQuotas[1].quota_limit,
-        usage:
-          mockQuotas[1].quota_limit -
-          Math.round(mockQuotas[0].quota_limit * 0.1),
+        usage: Math.round(mockQuotas[1].quota_limit * 0.1),
       }),
       quotaUsageFactory.build({
         quota_limit: mockQuotas[2].quota_limit,
-        usage:
-          mockQuotas[2].quota_limit -
-          Math.round(mockQuotas[0].quota_limit * 0.1),
+        usage: Math.round(mockQuotas[2].quota_limit * 0.1),
       }),
     ];
     mockGetObjectStorageEndpoints(mockEndpoints).as(
@@ -153,8 +160,9 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
       'objects',
       mockQuotaUsages[2]
     ).as('getQuotaUsages');
+    mockApiInternalUser();
     cy.visitWithLogin('/account/quotas');
-    cy.wait('@getFeatureFlags');
+    cy.wait(['@getFeatureFlags', '@getProfile']);
     // Quotas table placeholder text is shown
     cy.get('[data-testid="table-row-empty"]').should('be.visible');
 
@@ -215,9 +223,109 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
           });
         });
       });
+    const ticketSummary = 'Increase Object Storage Quota';
+    const expectedResults = [
+      {
+        newQuotaLimit: mockQuotas[0].quota_limit * 2,
+        description: randomLabel(),
+        metric: 'Bytes',
+      },
+      {
+        newQuotaLimit: mockQuotas[1].quota_limit * 2,
+        description: randomLabel(),
+        metric: 'Buckets',
+      },
+      {
+        newQuotaLimit: mockQuotas[1].quota_limit * 2,
+        description: randomLabel(),
+        metric: 'Objects',
+      },
+    ];
+    mockQuotas.forEach((mockQuota, index) => {
+      // Quotas increase request workflow
+      ui.actionMenu
+        .findByTitle(`Action menu for quota ${mockQuota.quota_name}`)
+        .should('be.visible')
+        .should('be.enabled')
+        .click();
+      ui.actionMenuItem
+        .findByTitle('Request Increase')
+        .should('be.visible')
+        .click();
+      ui.dialog
+        .findByTitle(`Contact Support: ${ticketSummary}`)
+        .should('be.visible')
+        .within(() => {
+          cy.get('input[name="quantity"]').should(
+            'have.value',
+            mockQuota.quota_limit
+          );
+          cy.findByLabelText('New Quota (required)')
+            .should('be.visible')
+            .should('be.enabled')
+            .clear();
+          cy.focused().type(expectedResults[index].newQuotaLimit.toString());
+          cy.findByLabelText('Description (required)')
+            .should('be.visible')
+            .should('be.enabled')
+            .type(expectedResults[index].description);
+          const mockTicket = supportTicketFactory.build({
+            summary: ticketSummary,
+            description: getQuotaIncreaseMessage({
+              convertedMetrics: {
+                limit: mockQuota.quota_limit,
+                metric: expectedResults[index].metric,
+              },
+              neededIn: 'Fewer than 7 days',
+              profile: mockProfile,
+              quantity: expectedResults[index].newQuotaLimit,
+              quota: mockQuota,
+              selectedService: {
+                label: 'Object Storage',
+                value: 'object-storage',
+              },
+            }).description,
+          });
+          mockCreateSupportTicket(mockTicket).as('createTicket');
+
+          // TODO: preview
+          ui.accordionHeading
+            .findByTitle('Ticket Preview')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+          ui.accordion.findByTitle('Ticket Preview').scrollIntoView();
+          ui.accordion.findByTitle('Ticket Preview').within(() => {
+            cy.get(
+              '[data-testid="quota-increase-form-preview-content"]'
+            ).within(() => {
+              cy.contains(`Quota Name: ${mockQuota.quota_name}`);
+              cy.contains(
+                `Current Quota: ${mockQuota.quota_limit} ${expectedResults[index].metric}`
+              );
+              cy.contains(
+                `New Quota Requested: ${expectedResults[index].newQuotaLimit} ${expectedResults[index].metric}`
+              );
+            });
+          });
+
+          ui.button.findByTitle('Submit').should('be.visible').scrollIntoView();
+          ui.button.findByTitle('Submit').should('be.enabled').click();
+
+          // TODO: cancel
+          // ui.button.findByTitle('Cancel').scrollIntoView().should('be.visible').click();
+          cy.wait('@createTicket').then((xhr) => {
+            expect(xhr.request.body?.summary).to.eq(mockTicket.summary);
+            expect(xhr.request.body?.description).startWith(
+              mockTicket.description
+            );
+          });
+          cy.url().should('endWith', `support/tickets/${mockTicket.id}`);
+        });
+    });
   });
 
-  it('API error results in error message being displayed', () => {
+  xit('API error results in error message being displayed', () => {
     const mockSelectedEndpoint = mockEndpoints[1];
     const selectedDomain = mockSelectedEndpoint.s3_endpoint || '';
     const errorMsg = 'Request failed.';
@@ -242,7 +350,7 @@ describe('Quotas accessible when limitsEvolution feature flag enabled', () => {
   });
 });
 
-describe('Quotas inaccessible when limitsEvolution feature flag disabled', () => {
+xdescribe('Quotas inaccessible when limitsEvolution feature flag disabled', () => {
   beforeEach(() => {
     mockAppendFeatureFlags({
       limitsEvolution: {
