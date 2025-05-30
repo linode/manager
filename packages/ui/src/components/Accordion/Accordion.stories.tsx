6d3deb343b03cc997a62ccfed40e3a79dc98389d
hasyed-akamai <hasyed@akamai.com>
2025-05-30 10:57:44 +0530

    change: [M3-10049] - Add Subheading support to Accordion Component (#12286)
    * change: [M3-10049] - Add Subheading support to Accordion Component
    
    * added styling to headingNumberCount and changed logic
    
    * Added changeset: Subheading Support to MUI Accordion Component
    
    * Update subheading text and some fixes...
    
    ---------
    
    Co-authored-by: pmakode-akamai <pmakode@akamai.com>

--- packages/ui/src/components/Accordion/Accordion.stories.tsx
+++ packages/ui/src/components/Accordion/Accordion.stories.tsx
@@ -4,6 +4,28 @@ import { Accordion } from './Accordion';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
+/**
+ * Pretend this is `react-router-dom`'s Link component.
+ * This is just an example to show usage with `Accordion`
+ */
+const Link = (props: React.PropsWithChildren<{ to?: string }>) => {
+  return (
+    <a {...props} href={props.to} rel="noreferrer" target="_blank">
+      {props.children}
+    </a>
+  );
+};
+
+const subHeadingString =
+  'This is a subheading. It provides context and an explanation of what this section is about.';
+
+const subHeadingNode = (
+  <>
+    {subHeadingString}{' '}
+    <Link to="https://techdocs.akamai.com/home">Learn more</Link>.
+  </>
+);
+
 const meta: Meta<typeof Accordion> = {
   component: Accordion,
   title: 'Foundations/Accordion',
@@ -16,7 +38,6 @@ export const Default: Story = {
     children: <p>Any children can go here!</p>,
     heading: 'This is an Accordion',
   },
-  render: (args) => <Accordion {...args} />,
 };
 
 export const WithHeadingNumberCount: Story = {
@@ -25,7 +46,22 @@ export const WithHeadingNumberCount: Story = {
     heading: 'This is an Accordion',
     headingNumberCount: 1,
   },
-  render: (args) => <Accordion {...args} />,
+};
+
+export const WithSubheadingString: Story = {
+  args: {
+    children: <p>Any children can go here!</p>,
+    heading: 'This is an Accordion',
+    subHeading: subHeadingString,
+  },
+};
+
+export const WithSubheadingNode: Story = {
+  args: {
+    children: <p>Any children can go here!</p>,
+    heading: 'This is an Accordion',
+    subHeading: subHeadingNode,
+  },
 };
 
 export default meta;
