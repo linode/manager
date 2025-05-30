08fcd6d1e34d570e2e8c97af94d8ec0ed16235d3
mjac0bs <mjacobs@akamai.com>
2025-05-30 09:26:01 -0700

    Remove unneeded word

--- packages/manager/src/features/Kubernetes/CreateCluster/PremiumCPUPlanNotice.tsx
+++ packages/manager/src/features/Kubernetes/CreateCluster/PremiumCPUPlanNotice.tsx
@@ -6,9 +6,9 @@ import type { NoticeProps } from '@linode/ui';
 export const PremiumCPUPlanNotice = (props: NoticeProps) => {
   return (
     <Notice variant="info" {...props}>
-      To accommodate enterprise workloads, especially where potential resource
-      contention can impact your workloads, using Premium instances is highly
-      recommended and required to achieve peak performance.
+      To accommodate enterprise workloads, especially where resource contention
+      can impact your workloads, using Premium instances is highly recommended
+      and required to achieve peak performance.
     </Notice>
   );
 };
