4d74b3a115a2ecce443724335b961cf162c090bc
Hussain Khalil <122488130+hkhalil-akamai@users.noreply.github.com>
2025-05-31 20:05:32 -0400

    upcoming: [M3-9501] - Host & VM Maintenance Table Updates (#12277)
    * Update table columns
    
    * Rough idea for status in MaintenanceTableRow
    
    * Fix styling for MaintenanceTableRow
    
    * Flag-ify table changes
    
    * Added changeset: Update Account Maintenance table
    
    * Remove extraneous spaces
    
    * Fix unit tests
    
    * Fix e2e tests
    
    * Feedback @coliu-akamai

--- packages/manager/src/features/Account/Maintenance/utilities.ts
+++ packages/manager/src/features/Account/Maintenance/utilities.ts
@@ -1,6 +1,30 @@
+import type { MaintenanceTableType } from './MaintenanceTable';
+
+export const COMPLETED_MAINTENANCE_FILTER = Object.freeze({
+  status: { '+or': ['completed', 'canceled'] },
+});
+
+export const IN_PROGRESS_MAINTENANCE_FILTER = Object.freeze({
+  status: { '+or': ['in-progress', 'started'] },
+});
+
+export const SCHEDULED_MAINTENANCE_FILTER = Object.freeze({
+  status: { '+or': ['pending', 'scheduled'] },
+});
+
 export const PENDING_MAINTENANCE_FILTER = Object.freeze({
   status: { '+or': ['pending', 'started', 'scheduled'] },
 });
+
+export const maintenanceDateColumnMap: Record<
+  MaintenanceTableType,
+  ['complete_time' | 'start_time' | 'when', string]
+> = {
+  completed: ['complete_time', 'End Date'],
+  'in progress': ['start_time', 'Start Date'],
+  scheduled: ['start_time', 'Start Date'],
+  pending: ['when', 'Date'],
+};
 
 export const PLATFORM_MAINTENANCE_TYPE =
   'security_reboot_maintenance_scheduled';
