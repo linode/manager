a91a06822107d0815837edcb1d14549e50ee925d
Banks Nussman <115251059+bnussman-akamai@users.noreply.github.com>
2025-06-02 12:07:40 -0400

    refactor: [M3-9994] - Remove `recompose` from NodeBalancers (#12288)
    * initial
    
    * remove recompose and promiseloader
    
    * Added changeset: Remove `recompose` from NodeBalancers
    
    ---------
    
    Co-authored-by: Banks Nussman <banks@nussman.us>

---
+++ packages/manager/src/features/NodeBalancers/NodeBalancerDetail/requests.ts
@@ -0,0 +1,30 @@
+import {
+  getNodeBalancerConfigNodes,
+  getNodeBalancerConfigs,
+} from '@linode/api-v4';
+
+import { parseAddresses } from '../utils';
+
+/**
+ * A function to fetch data for the NodeBalancerConfigurations page.
+ * When we refactor NodeBalancerConfigurations to not be total chaos,
+ * this may change.
+ */
+export const getConfigsWithNodes = (nodeBalancerId: number) => {
+  return getNodeBalancerConfigs(nodeBalancerId).then((configs) => {
+    return Promise.all(
+      configs.data.map((config) => {
+        return getNodeBalancerConfigNodes(nodeBalancerId, config.id).then(
+          ({ data: nodes }) => {
+            return {
+              ...config,
+              nodes: parseAddresses(nodes),
+            };
+          }
+        );
+      })
+    );
+  });
+};
+
+export type ConfigsWithNodes = Awaited<ReturnType<typeof getConfigsWithNodes>>;
