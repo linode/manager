08d0868f2360e097f9c24ffb60d9a3d11776a758
Hana Xu <hxu@akamai.com>
2025-06-02 13:32:37 -0400

    add VPC CIDR section to VPC Create page

--- packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.tsx
+++ packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.tsx
@@ -1,13 +1,17 @@
 import { useIsGeckoEnabled } from '@linode/shared';
-import { TextField } from '@linode/ui';
+import { Box, FormControlLabel, TextField } from '@linode/ui';
+import { Radio, RadioGroup, Typography } from '@linode/ui';
 import { getQueryParamsFromQueryString } from '@linode/utilities';
+import Grid from '@mui/material/Grid';
 import * as React from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import { Controller, useFieldArray, useFormContext } from 'react-hook-form';
 // eslint-disable-next-line no-restricted-imports
 import { useLocation } from 'react-router-dom';
 
+import { FormLabel } from 'src/components/FormLabel';
 import { Link } from 'src/components/Link';
 import { RegionSelect } from 'src/components/RegionSelect/RegionSelect';
+import { SelectionCard } from 'src/components/SelectionCard/SelectionCard';
 import { useFlags } from 'src/hooks/useFlags';
 import { sendLinodeCreateFormInputEvent } from 'src/utilities/analytics/formEventAnalytics';
 
@@ -38,7 +42,13 @@ export const VPCTopSectionContent = (props: Props) => {
     location.search
   );
 
+  const [cidrOption, setCIDROption] = React.useState('ipv4');
+
   const { control } = useFormContext<CreateVPCPayload>();
+  const { append, fields, remove } = useFieldArray({
+    control,
+    name: 'ipv6',
+  });
 
   return (
     <>
@@ -109,6 +119,90 @@ export const VPCTopSectionContent = (props: Props) => {
           />
         )}
       />
+      {flags.vpcIpv6 && (
+        <Box marginTop={2}>
+          <FormLabel>VPC CIDR</FormLabel>
+          <Typography>
+            Select the IP Addresses type. We will auto-allocate an IPv6 address
+            for for you.
+          </Typography>
+          <RadioGroup sx={{ display: 'block' }}>
+            <Grid container spacing={2}>
+              <SelectionCard
+                checked={cidrOption === 'ipv4'}
+                gridSize={{
+                  md: 3,
+                  sm: 12,
+                  xs: 12,
+                }}
+                heading="IPv4"
+                onClick={() => {
+                  setCIDROption('ipv4');
+                  remove(0);
+                }}
+                renderIcon={() => <Radio checked={cidrOption === 'ipv4'} />}
+                subheadings={[]}
+                sxCardBaseIcon={{ svg: { fontSize: '20px' } }}
+              />
+              <SelectionCard
+                checked={cidrOption === 'dualstack'}
+                gridSize={{
+                  md: 3,
+                  sm: 12,
+                  xs: 12,
+                }}
+                heading="IPv4 + IPv6 (Dual Stack)"
+                onClick={() => {
+                  setCIDROption('dualstack');
+                  if (fields.length === 0) {
+                    append({
+                      range: '/52',
+                    });
+                  }
+                }}
+                renderIcon={() => (
+                  <Radio checked={cidrOption === 'dualstack'} />
+                )}
+                subheadings={[]}
+                sxCardBaseIcon={{ svg: { fontSize: '20px' } }}
+              />
+            </Grid>
+          </RadioGroup>
+        </Box>
+      )}
+      {flags.vpcIpv6 && (
+        <>
+          <FormLabel>IPv4 CIDR</FormLabel>
+          <Typography>
+            Lorum you will have full access to the full RFC 1918 range.
+          </Typography>
+        </>
+      )}
+      {flags.vpcIpv6 && cidrOption === 'dualstack' && (
+        <Controller
+          control={control}
+          name="ipv6"
+          render={() => (
+            <RadioGroup
+              onChange={(_, value) => {
+                remove(0);
+                append({
+                  range: value,
+                });
+              }}
+              value={fields[0].range}
+            >
+              <FormLabel sx={{ marginTop: 1, marginBottom: 0 }}>
+                IPv6 CIDR
+              </FormLabel>
+              <FormControlLabel control={<Radio />} label="/52" value="/52" />
+              {/* @TODO VPCIPv6: Revisit /48 and /44 options once we have something in place to determine high reputation/specific customers
+              <FormControlLabel control={<Radio />} label="/48" value="/48" />
+              <FormControlLabel control={<Radio />} label="/44" value="/44" /> */}
+            </RadioGroup>
+          )}
+        />
+      )}
     </>
   );
 };
