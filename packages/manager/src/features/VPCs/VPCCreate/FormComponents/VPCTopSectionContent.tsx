427d9ef5c83bc126c63d3f3ee8f6b8086cb9c2c9
Hana Xu <hxu@akamai.com>
2025-06-02 15:14:54 -0400

    add info tooltip to IP CIDR labels

--- packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.tsx
+++ packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.tsx
@@ -1,5 +1,11 @@
 import { useIsGeckoEnabled } from '@linode/shared';
-import { Box, FormControlLabel, TextField } from '@linode/ui';
+import {
+  Box,
+  FormControlLabel,
+  styled,
+  TextField,
+  TooltipIcon,
+} from '@linode/ui';
 import { Radio, RadioGroup, Typography } from '@linode/ui';
 import { getQueryParamsFromQueryString } from '@linode/utilities';
 import Grid from '@mui/material/Grid';
@@ -121,11 +127,7 @@ export const VPCTopSectionContent = (props: Props) => {
       />
       {flags.vpcIpv6 && (
         <Box marginTop={2}>
-          <FormLabel>VPC CIDR</FormLabel>
-          <Typography>
-            Select the IP Addresses type. We will auto-allocate an IPv6 address
-            for for you.
-          </Typography>
+          <FormLabel>VPC Stack Type </FormLabel>
           <RadioGroup sx={{ display: 'block' }}>
             <Grid container spacing={2}>
               <SelectionCard
@@ -172,7 +174,35 @@ export const VPCTopSectionContent = (props: Props) => {
       )}
       {flags.vpcIpv6 && (
         <>
-          <FormLabel>IPv4 CIDR</FormLabel>
+          <StyledFormLabel>
+            IPv4 CIDR
+            <TooltipIcon
+              labelTooltipIconSize="small"
+              status="info"
+              sxTooltipIcon={{
+                padding: '8px',
+              }}
+              text={
+                <>
+                  <Typography>
+                    RFC1918 defines the IP address ranges that are reserved for
+                    private networks—these IPs are not routable on the public
+                    internet and are commonly used in internal networking (like
+                    VPCs).
+                  </Typography>
+                  <Typography mt={1}>
+                    Examples:
+                    <StyledList>
+                      <li>10.0.0.0/8</li>
+                      <li>172.16.0.0/12</li>
+                      <li>192.168.0.0/16</li>
+                    </StyledList>
+                  </Typography>
+                </>
+              }
+              width={250}
+            />
+          </StyledFormLabel>
           <Typography>
             Lorum you will have full access to the full RFC 1918 range.
           </Typography>
@@ -192,9 +222,22 @@ export const VPCTopSectionContent = (props: Props) => {
               }}
               value={fields[0].range}
             >
-              <FormLabel sx={{ marginTop: 1, marginBottom: 0 }}>
+              <StyledFormLabel sx={{ marginTop: 1, marginBottom: 0 }}>
                 IPv6 CIDR
-              </FormLabel>
+                <TooltipIcon
+                  labelTooltipIconSize="small"
+                  status="info"
+                  sxTooltipIcon={{
+                    padding: '8px',
+                  }}
+                  text={
+                    <Typography>
+                      /52 is a default that impacts subnetting.
+                    </Typography>
+                  }
+                  width={250}
+                />
+              </StyledFormLabel>
               <FormControlLabel control={<Radio />} label="/52" value="/52" />
               {/* @TODO VPCIPv6: Revisit /48 and /44 options once we have something in place to determine high reputation/specific customers
               <FormControlLabel control={<Radio />} label="/48" value="/48" />
@@ -206,3 +249,13 @@ export const VPCTopSectionContent = (props: Props) => {
     </>
   );
 };
+
+const StyledFormLabel = styled(FormLabel)(() => ({
+  alignItems: 'center',
+  display: 'flex',
+}));
+
+const StyledList = styled('ul')(({ theme }) => ({
+  marginTop: 0,
+  paddingLeft: theme.spacingFunction(24),
+}));
