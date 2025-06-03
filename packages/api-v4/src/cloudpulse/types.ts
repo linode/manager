e0111709ff8bb699e1b3c40e7e4f917e77dd5c62
ankitaakamai <ankitaan@akamai.com>
2025-06-03 12:33:15 +0530

    upcoming:[DI-25165] - Optimize state updates

--- packages/api-v4/src/cloudpulse/types.ts
+++ packages/api-v4/src/cloudpulse/types.ts
@@ -346,7 +346,7 @@ export interface DeleteAlertPayload {
 
 export type AlertScopes = 'account' | 'entity' | 'region';
 
-export interface ServiceAlertsUpdatePayload {
-  system: Alert['id'][];
-  user: Alert['id'][];
+export interface CloudPulseAlertsPayload {
+  system: number[];
+  user: number[];
 }
