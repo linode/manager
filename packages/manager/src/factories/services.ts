1a1af7659ffbd37c029048484ced441bda1428a3
nikhagra-akamai <nagrawal@akamai.com>
2025-06-04 16:10:23 +0530

    upcoming: [DI-25349] - Removed redundant services factory

--- packages/manager/src/factories/services.ts
+++
@@ -1,8 +0,0 @@
-import { Factory } from '@linode/utilities';
-
-import type { ServiceTypes } from '@linode/api-v4';
-
-export const serviceTypesFactory = Factory.Sync.makeFactory<ServiceTypes>({
-  label: Factory.each((i) => `Factory ServiceType-${i}`),
-  service_type: Factory.each((i) => `Factory ServiceType-${i}`),
-});
