ddcc2fd030b25f18f0fa00b3fc6acd617224514d
Bill Coloe <bcoloe@akamai.com>
2025-06-04 12:23:56 -0500

    Add new page

--- packages/manager/src/MainContent.tsx
+++ packages/manager/src/MainContent.tsx
@@ -111,6 +111,13 @@ const Account = React.lazy(() =>
     default: module.Account,
   }))
 );
+const NewDetailTemplateA = React.lazy(() =>
+  import('src/features/NewDetailTemplate/NewDetailTemplateA').then(
+    (module) => ({
+      default: module.NewDetailTemplateA,
+    })
+  )
+);
 const LinodesRoutes = React.lazy(() =>
   import('src/features/Linodes').then((module) => ({
     default: module.LinodesRoutes,
@@ -306,6 +313,10 @@ export const MainContent = () => {
                                 )}
                                 <Route component={Account} path="/account" />
                                 <Route component={Profile} path="/profile" />
+                                <Route
+                                  component={NewDetailTemplateA}
+                                  path="/new-detail-template-a"
+                                />
                                 <Route
                                   component={EventsLanding}
                                   path="/events"
