ba62012f32f2b7fa8d082e31256a837cf379b38d
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2025-06-05 15:56:29 +0530

    upcoming: [DI-25348] - Support for Port dimension filter

--- packages/manager/src/features/CloudPulse/Alerts/CreateAlert/schemas.ts
+++ packages/manager/src/features/CloudPulse/Alerts/CreateAlert/schemas.ts
@@ -9,6 +9,82 @@ import { array, mixed, number, object, string } from 'yup';
 import type { AlertDefinitionGroup, AlertDefinitionType } from '@linode/api-v4';
 
 const fieldErrorMessage = 'This field is required.';
+const PORTS_ERROR_MESSAGE = 'Input must be an integer.';
+const MULTIPLE_PORTS_ERROR_MESSAGE =
+  'Input must be an integer or comma-separated list of integers.';
+const PORTS_RANGE_ERROR_MESSAGE = 'Must be 1-65535';
+const PORTS_LEADING_ZEROES_ERROR_MESSAGE = 'Port must not have leading zeroes';
+const PORTS_CONSECUTIVE_COMMAS_ERROR_MESSAGE =
+  'Consecutive commas are not allowed.';
+const PORTS_LEADING_COMMA_ERROR_MESSAGE = 'Ports must not have leading commas.';
+
+// Validation schema for a single input port
+const singlePortSchema = string().test(
+  'validate-single-port',
+  PORTS_ERROR_MESSAGE,
+  function (value) {
+    if (!value || typeof value !== 'string') return false;
+
+    if (value.includes(',') || value.includes('.') || isNaN(Number(value))) {
+      return this.createError({ message: PORTS_ERROR_MESSAGE });
+    }
+
+    const num = Number(value);
+    if (!Number.isInteger(num) || num < 1 || num > 65535) {
+      return this.createError({ message: PORTS_RANGE_ERROR_MESSAGE });
+    }
+
+    if (/^0\d+/.test(value)) {
+      return this.createError({ message: PORTS_LEADING_ZEROES_ERROR_MESSAGE });
+    }
+
+    return true;
+  }
+);
+
+// Validation schema for a multiple comma-separated ports
+const commaSeparatedPortListSchema = string().test(
+  'validate-port-list',
+  MULTIPLE_PORTS_ERROR_MESSAGE,
+  function (value) {
+    if (!value || typeof value !== 'string') return false;
+
+    if (value.trim().startsWith(',')) {
+      return this.createError({ message: PORTS_LEADING_COMMA_ERROR_MESSAGE });
+    }
+
+    if (value.includes('.')) {
+      return this.createError({ message: MULTIPLE_PORTS_ERROR_MESSAGE });
+    }
+
+    if (value.includes(',,')) {
+      return this.createError({
+        message: PORTS_CONSECUTIVE_COMMAS_ERROR_MESSAGE,
+      });
+    }
+
+    const ports = value.split(',').map((p) => p.trim());
+
+    for (const port of ports) {
+      const num = Number(port);
+      if (isNaN(num)) {
+        return this.createError({ message: MULTIPLE_PORTS_ERROR_MESSAGE });
+      }
+
+      if (!Number.isInteger(num) || num < 1 || num > 65535) {
+        return this.createError({ message: PORTS_RANGE_ERROR_MESSAGE });
+      }
+
+      if (/^0\d+/.test(port)) {
+        return this.createError({
+          message: PORTS_LEADING_ZEROES_ERROR_MESSAGE,
+        });
+      }
+    }
+
+    return true;
+  }
+);
 
 export const dimensionFiltersSchema = object({
   dimension_label: string()
@@ -23,7 +99,21 @@ export const dimensionFiltersSchema = object({
   value: string()
     .required(fieldErrorMessage)
     .nullable()
-    .test('nonNull', fieldErrorMessage, (value) => value !== null),
+    .test('nonNull', fieldErrorMessage, (value) => value !== null)
+    .when(
+      ['dimension_label', 'operator'],
+      ([dimensionLabel, operator], schema) => {
+        if (dimensionLabel === 'port' && operator === 'in') {
+          return commaSeparatedPortListSchema;
+        }
+
+        if (dimensionLabel === 'port' && operator !== 'in') {
+          return singlePortSchema;
+        }
+
+        return schema;
+      }
+    ),
 });
 
 export const metricCriteriaSchema = metricCriteria.concat(
