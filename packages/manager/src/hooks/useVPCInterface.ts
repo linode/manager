e8fadd38fed8bc7e712c5c666e4f8bf8beb8e445
Connie Liu <139280159+coliu-akamai@users.noreply.github.com>
2025-06-06 09:30:00 -0400

    upcoming: [M3-9882, M3-9879] - Update Upgrade Interfaces dialog height and Public IP addresses copy (#12332)
    * fix dialog height
    
    * vpc only linode copy update
    
    * changesets + update name
    
    * update copy
    
    * add check
    
    * constants file

--- packages/manager/src/hooks/useVPCInterface.ts
+++ packages/manager/src/hooks/useVPCInterface.ts
@@ -14,6 +14,7 @@ export const useVPCInterface = (inputs: {
   const { isLinodeInterface, linodeId } = inputs;
 
   const {
+    hasPublicLinodeInterface,
     isVPCOnlyLinodeInterface,
     linodeInterfaceWithVPC,
     vpcLinodeIsAssignedTo: vpcLinodeIsAssignedToInterface,
@@ -31,6 +32,7 @@ export const useVPCInterface = (inputs: {
 
   return {
     configs, // undefined if this Linode is using Linode Interfaces
+    hasPublicLinodeInterface, // undefined if this Linode is using config interfaces. Is only used when the Linode is known to be using Linode Interfaces
     interfaceWithVPC: linodeInterfaceWithVPC ?? configInterfaceWithVPC,
     isVPCOnlyLinode,
     vpcLinodeIsAssignedTo,
@@ -49,6 +51,12 @@ export const useVPCLinodeInterface = (
   const { data: interfaces } = useLinodeInterfacesQuery(linodeId, enabled);
 
   const vpcInterfaces = interfaces?.interfaces.filter((iface) => iface.vpc);
+
+  // if a Linode is a VPCOnlyLinode but has a public interface, its public IPv4 address will be associated with
+  // this public interface, but just won't be the default route
+  const hasPublicLinodeInterface = interfaces?.interfaces.some(
+    (iface) => iface.public
+  );
 
   // Some Linodes may have multiple VPC Linode interfaces. If so, we want the interface that
   // is a default route (otherwise just get the first one)
@@ -71,6 +79,7 @@ export const useVPCLinodeInterface = (
   );
 
   return {
+    hasPublicLinodeInterface,
     isVPCOnlyLinodeInterface,
     linodeInterfaceWithVPC,
     vpcLinodeIsAssignedTo,
