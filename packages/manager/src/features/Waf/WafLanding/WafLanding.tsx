65fc9ca35e5cf702de1d1968d3c5f61f4860907f
siddharth <ssiddhar@akamai.com>
2025-06-16 16:21:34 +0530

    LIWAF-30-remove test code

--- packages/manager/src/features/Waf/WafLanding/WafLanding.tsx
+++ packages/manager/src/features/Waf/WafLanding/WafLanding.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
 
-import { Link } from 'src/components/Link';
-
 export const WafLanding = () => {
-  return (
-    <div>
-      WafLanding works!
-      {/*TODO - below links are added for testing routes - remove when implementing actual landing page and components*/}
-      <Link to={`/waf/123`}>Waf Details</Link>
-      <Link to={`/waf/create`}>Waf Create</Link>
-    </div>
-  );
+  return <div>WafLanding works!</div>;
 };
