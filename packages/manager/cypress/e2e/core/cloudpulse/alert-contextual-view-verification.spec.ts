af72afa7b7a55ac6c0de7bf1f28a7a3fe42bebc3
agorthi <agorthi@akamai.com>
2025-06-16 16:52:13 +0530

    Contextual_view changes

--- packages/manager/cypress/e2e/core/cloudpulse/alert-contextual-view-verification.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/alert-contextual-view-verification.spec.ts
@@ -1,46 +1,43 @@
+import { linodeFactory, regionFactory } from '@linode/utilities';
 /**
  * @file Integration Tests for contextual view of Entity Listing.
  */
-import { mockDatabaseNodeTypes } from 'support/constants/databases';
-import { widgetDetails } from 'support/constants/widgets';
 import { mockGetAccount } from 'support/intercepts/account';
 import {
   mockAddEntityToAlert,
   mockDeleteEntityFromAlert,
   mockGetAlertDefinition,
 } from 'support/intercepts/cloudpulse';
-import {
-  mockGetDatabase,
-  mockGetDatabaseTypes,
-} from 'support/intercepts/databases';
 import { mockAppendFeatureFlags } from 'support/intercepts/feature-flags';
+import { mockGetLinodes } from 'support/intercepts/linodes';
+import { mockGetRegions } from 'support/intercepts/regions';
 import { ui } from 'support/ui';
 
-import { accountFactory, alertFactory, databaseFactory } from 'src/factories';
+import { accountFactory, alertFactory } from 'src/factories';
 
 import type {
   AlertDefinitionType,
-  Database,
+  AlertStatusType,
   MetricAggregationType,
 } from '@linode/api-v4';
 import type { RecPartial } from 'factory.ts';
+import type { Flags } from 'src/featureFlags';
 
 const DBaaS = 'dbaas';
 const ALERT_TYPE = 'alert-definition-id';
 
-// Mock data
-const { clusterName, engine, region } = widgetDetails.dbaas;
 const mockAccount = accountFactory.build();
-const databaseMock: Database = databaseFactory.build({
-  cluster_size: 3,
-  engine: 'mysql',
-  id: 100,
-  label: clusterName,
-  region,
-  status: 'active',
-  type: engine,
+
+const mockRegion = regionFactory.build({
+  capabilities: ['Linodes'],
+  id: 'us-ord',
+  label: 'Chicago, IL',
 });
 
+const mockLinode = linodeFactory.build({
+  id: 1,
+  region: 'us-ord',
+});
 const alertConfigs = [
   {
     aggregate_function: 'avg' as RecPartial<MetricAggregationType>,
@@ -48,6 +45,8 @@ const alertConfigs = [
     metric: 'CPU Usage',
     threshold: 55,
     type: 'system',
+    status: 'enabled',
+    scope: 'region',
   },
   {
     aggregate_function: 'min' as RecPartial<MetricAggregationType>,
@@ -55,13 +54,17 @@ const alertConfigs = [
     metric: 'Memory Usage',
     threshold: 100,
     type: 'user',
+    status: 'in progress',
+    scope: 'account',
   },
   {
     aggregate_function: 'sum' as RecPartial<MetricAggregationType>,
     created_by: 'user3',
     metric: 'Disk Usage',
     threshold: 75,
-    type: 'system',
+    type: 'user',
+    status: 'in progress',
+    scope: 'entity',
   },
   {
     aggregate_function: 'max' as RecPartial<MetricAggregationType>,
@@ -69,13 +72,32 @@ const alertConfigs = [
     metric: 'Network Usage',
     threshold: 90,
     type: 'user',
+    status: 'enabled',
+    scope: 'entity',
   },
 ];
+const flags: Partial<Flags> = {
+  aclp: { beta: true, enabled: true },
+  aclpResourceTypeMap: [
+    {
+      dimensionKey: 'LINODE_ID',
+      maxResourceSelections: 10,
+      serviceType: 'linode',
+      supportedRegionIds: 'us-ord',
+    },
+    {
+      dimensionKey: 'cluster_id',
+      maxResourceSelections: 10,
+      serviceType: 'dbaas',
+      supportedRegionIds: 'us-ord',
+    },
+  ],
+};
 const alerts = alertConfigs.flatMap((config) => {
   // Create the alert
   const alert = alertFactory.build({
     created_by: config.created_by,
-    entity_ids: ['1'],  // Default to ['1']
+    entity_ids: ['1'], // Default to ['1']
     rule_criteria: {
       rules: [
         {
@@ -88,10 +110,12 @@ const alerts = alertConfigs.flatMap((config) => {
         },
       ],
     },
+    label: `Alert-${config.created_by.split('user')[1]}`,
     service_type: DBaaS,
     severity: 1,
-    status: 'enabled',
+    status: config.status as AlertStatusType,
     type: config.type as AlertDefinitionType,
+    scope: config.scope as 'account' | 'entity' | 'region',
   });
 
   // Update the entity_ids if the label is 'alert-4'
@@ -102,7 +126,6 @@ const alerts = alertConfigs.flatMap((config) => {
 
   return alert;
 });
-
 
 // Verify Sorting Function
 const verifyTableSorting = (
@@ -130,7 +153,7 @@ const verifyTableSorting = (
 const sortCases = [
   { ascending: [1, 2, 3, 4], column: 'label', descending: [4, 3, 2, 1] },
   { ascending: [1, 2, 3, 4], column: 'id', descending: [4, 3, 2, 1] },
-  { ascending: [1, 3, 4, 2], column: 'type', descending: [2, 1, 3, 4] },
+  { ascending: [1, 4, 2, 3], column: 'type', descending: [2, 3, 1, 4] },
 ];
 
 /*
@@ -149,22 +172,20 @@ const sortCases = [
  * - Verifies that sorting by different columns (ID, label, type) works as expected with ascending and descending orders.
  */
 // Reason: Checking by anantha
-it.skip('should verify sorting, alert management, and search functionality for contextual view of entity listing.', () => {
-  mockAppendFeatureFlags({ aclp: { beta: true, enabled: true } });
+it('should verify sorting, alert management, and search functionality for contextual view of entity listing.', () => {
+  mockAppendFeatureFlags(flags);
   mockGetAccount(mockAccount);
-  mockGetDatabase(databaseMock).as('getDatabase');
-  mockGetDatabaseTypes(mockDatabaseNodeTypes).as('getDatabaseTypes');
+  mockGetLinodes([mockLinode]);
   mockGetAlertDefinition(DBaaS, alerts).as('getDBaaSAlertDefinitions');
   mockAddEntityToAlert(DBaaS, '100', { [ALERT_TYPE]: 100 }).as(
     'addEntityToAlert'
   );
+  mockGetRegions([mockRegion]);
+
   mockDeleteEntityFromAlert(DBaaS, '100', 4).as('deleteEntityToAlert');
 
   // Visit the database alerts page
-  cy.visitWithLogin(
-    `/databases/${databaseMock.engine}/${databaseMock.id}/alerts`
-  );
-  cy.wait('@getDBaaSAlertDefinitions');
+  cy.visitWithLogin('linodes/1/alerts');
 
   // Test sorting
   sortCases.forEach(({ ascending, column, descending }) => {
@@ -172,12 +193,6 @@ it.skip('should verify sorting, alert management, and search functionality for c
     verifyTableSorting(column, 'ascending', ascending);
   });
 
-  // Tooltip and UI Element Verifications
-  ui.tooltip
-    .findByText(
-      'The list contains only the alerts enabled in the Monitor centralized view.'
-    )
-    .should('be.visible');
   ui.buttonGroup.findButtonByTitle('Manage Alerts').should('be.visible');
 
   // Alert Links Verification
@@ -216,31 +231,19 @@ it.skip('should verify sorting, alert management, and search functionality for c
 
   // Enable Alert
   cy.get('[data-qa-alert-cell="1"]')
-    .find('[data-qa-toggle="false"]')
+    .find('[data-qa-toggle="true"]')
     .find('[type="checkbox"]')
-    .check();
-  cy.get('[data-testid="button"]')
-    .should('be.enabled')
-    .contains('Enable')
-    .click();
+    .should('be.disabled');
 
-  // Assert successful API call for disabling the alert
-  cy.wait('@addEntityToAlert').then(({ response }) => {
-    expect(response).to.have.property('statusCode', 200);
-    ui.toast.assertMessage(
-      'The alert settings for mysql-cluster saved successfully.'
-    );
-  });
+  cy.findByPlaceholderText('Search for Alerts')
+    .should('be.visible')
+    .type('Alert-4');
+  // Untoggle the switch
 
-  cy.findByPlaceholderText('Search for Alerts').should('be.visible').type('Alert-4');
-  // Disable Alert
   cy.get('[data-qa-alert-cell="4"]')
     .find('[data-qa-toggle="true"]')
     .find('[type="checkbox"]')
-    .uncheck();
-  cy.get('[data-testid="button"]')
     .should('be.enabled')
-    .contains('Disable')
     .click();
 
   // Assert successful API call for disabling the alert
