754c37d483ca1e9e5a30b3fce6a706cd950aa71c
nikhagra-akamai <nagrawal@akamai.com>
2025-06-16 21:58:33 +0530

    upcoming: [DI-25385] - Updated failing typechecks

--- packages/manager/src/factories/cloudpulse/services.ts
+++ packages/manager/src/factories/cloudpulse/services.ts
@@ -5,7 +5,6 @@ import type { ServiceTypes } from '@linode/api-v4';
 export const serviceTypesFactory = Factory.Sync.makeFactory<ServiceTypes>({
   label: Factory.each((i) => `Factory ServiceType-${i}`),
   service_type: Factory.each((i) => `Factory ServiceType-${i}`),
-  is_beta: false,
   regions: '*',
   alert: {
     polling_interval_seconds: [1, 5, 10, 15],
