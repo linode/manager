33d39ec647c26138926b707d986567507dbfa2df
ankitaakamai <ankitaan@akamai.com>
2025-06-24 17:37:19 +0530

    [DI-25347] - Update query

--- packages/manager/src/queries/cloudpulse/queries.ts
+++ packages/manager/src/queries/cloudpulse/queries.ts
@@ -6,12 +6,9 @@ import {
   getDashboards,
   getJWEToken,
   getMetricDefinitionsByServiceType,
+  getNodeBalancers,
 } from '@linode/api-v4';
-import {
-  getAllLinodesRequest,
-  nodebalancerQueries,
-  volumeQueries,
-} from '@linode/queries';
+import { getAllLinodesRequest, volumeQueries } from '@linode/queries';
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 
 import { databaseQueries } from '../databases/databases';
@@ -120,7 +117,13 @@ export const queryFactory = createQueryKeys(key, {
         };
 
       case 'nodebalancer':
-        return nodebalancerQueries.nodebalancers._ctx.all;
+        return {
+          queryFn: async () => {
+            const response = await getNodeBalancers(params, filters);
+            return response.data;
+          },
+          queryKey: ['nodebalancers', params, filters],
+        };
 
       case 'volumes':
         return volumeQueries.lists._ctx.all(params, filters); // in this we don't need to define our own query factory, we will reuse existing implementation in volumes.ts
