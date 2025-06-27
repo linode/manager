7b7f6b959fa90a0798d1abbcafb7e1454f3ed98c
nikhagra-akamai <nagrawal@akamai.com>
2025-06-27 18:14:24 +0530

    updated schema

--- packages/validation/src/cloudpulse.schema.ts
+++ packages/validation/src/cloudpulse.schema.ts
@@ -78,7 +78,7 @@ export const createAlertDefinitionSchema = object({
   tags: array().of(string().defined()).optional(),
   entity_ids: array().of(string().defined()).optional(),
   regions: array().of(string().defined()).optional(),
-  scope: string().oneOf(['entity', 'region', 'account']).optional(),
+  scope: string().oneOf(['entity', 'region', 'account']).nullable().optional(),
 });
 
 export const editAlertDefinitionSchema = object({
@@ -122,6 +122,6 @@ export const editAlertDefinitionSchema = object({
   status: string()
     .oneOf(['enabled', 'disabled', 'in progress', 'failed'])
     .optional(),
-  scope: string().oneOf(['entity', 'region', 'account']).optional(),
+  scope: string().oneOf(['entity', 'region', 'account']).nullable().optional(),
   regions: array().of(string().defined()).optional(),
 });
