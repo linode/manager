12855f4071545e047e7ffee13c61bb03ea3ca4ef
nikhagra-akamai <nagrawal@akamai.com>
2025-06-27 18:25:26 +0530

    bug: [DI-25992] - Updated CreateAlertDefinition form interface

--- packages/manager/src/features/CloudPulse/Alerts/CreateAlert/types.ts
+++ packages/manager/src/features/CloudPulse/Alerts/CreateAlert/types.ts
@@ -21,7 +21,7 @@ export interface CreateAlertDefinitionForm
   rule_criteria: {
     rules: MetricCriteriaForm[];
   };
-  scope: AlertDefinitionScope | null;
+  scope?: AlertDefinitionScope | null;
   serviceType: AlertServiceType | null;
   severity: AlertSeverityType | null;
   trigger_conditions: TriggerConditionForm;
