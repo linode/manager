6ce5063aaee8ddf715746df5081863874e4aefaa
hasyed-akamai <hasyed@akamai.com>
2025-07-03 11:38:42 +0530

    upcoming: [M3-10247] - Update Alerts Accordion Subheading in Create Linode Flow to Match Latest UX Mocks

--- packages/manager/src/features/Linodes/LinodeCreate/AdditionalOptions/Alerts/Alerts.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/AdditionalOptions/Alerts/Alerts.tsx
@@ -3,6 +3,7 @@ import { Accordion, BetaChip } from '@linode/ui';
 import * as React from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 
+import { Link } from 'src/components/Link';
 import { AlertReusableComponent } from 'src/features/CloudPulse/Alerts/ContextualView/AlertReusableComponent';
 import { AclpPreferenceToggle } from 'src/features/Linodes/AclpPreferenceToggle';
 import { AlertsPanel } from 'src/features/Linodes/LinodesDetail/LinodeAlerts/AlertsPanel';
@@ -28,6 +29,19 @@ export const Alerts = () => {
     field.onChange(updatedAlerts);
   };
 
+  const subHeading = isAclpAlertsPreferenceBeta ? (
+    <>
+      Receive notifications through System Alerts when metric thresholds are
+      exceeded. After you've created your Linode, you can create and manage
+      associated alerts on the <strong>centralized Alerts</strong> page.{' '}
+      <Link to="https://techdocs.akamai.com/cloud-computing/docs/configure-email-alerts-for-resource-usage-on-compute-instances">
+        Learn more.
+      </Link>
+    </>
+  ) : (
+    'Configure the alert notifications to be sent when metric thresholds are exceeded.'
+  );
+
   return (
     <Accordion
       detailProps={{ sx: { p: 0 } }}
@@ -37,7 +51,7 @@ export const Alerts = () => {
           <BetaChip />
         ) : null
       }
-      subHeading="Receive notifications through system alerts when metric thresholds are exceeded."
+      subHeading={subHeading}
       summaryProps={{ sx: { p: 0 } }}
     >
       {aclpBetaServices?.linode?.alerts && (
