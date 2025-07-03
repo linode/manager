cec70bead207da9b438eb8af8300985f2b33098d
cpathipa <119517080+cpathipa@users.noreply.github.com>
2025-07-03 13:12:54 -0500

    Update constants.ts

--- packages/utilities/src/constants.ts
+++ packages/utilities/src/constants.ts
@@ -1,7 +1,3 @@
 // Maximum page size allowed by the API. Used in the `getAll()` helper function
 // to request as many items at once as possible.
 export const API_MAX_PAGE_SIZE = 500;
-
-export const LINODE_STATUS_PAGE_URL =
-  import.meta.env.REACT_APP_STATUS_PAGE_URL ||
-  'https://status.linode.com/api/v2';
