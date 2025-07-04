66fbc5ea41e78febdc863d738e86920cfa297efc
nikhagra-akamai <nagrawal@akamai.com>
2025-07-04 16:09:15 +0530

    upcoming: [DI-25634] - Added linode_id to label translation in cloudpulse graph

--- packages/manager/src/mocks/serverHandlers.ts
+++ packages/manager/src/mocks/serverHandlers.ts
@@ -3157,6 +3157,7 @@ export const handlers = [
             metric: {
               entity_id: '123',
               metric_name: 'average_cpu_usage',
+              linode_id: '1',
               node_id: 'primary-1',
             },
             values: [
