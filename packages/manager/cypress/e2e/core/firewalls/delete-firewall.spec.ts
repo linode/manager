c98fc161a1f09b1988d21854ab63c3e05859f579
Joe D'Amore <jdamore@akamai.com>
2025-07-07 14:00:20 -0400

    Update Firewall delete and update tests to address remaining UI failures

--- packages/manager/cypress/e2e/core/firewalls/delete-firewall.spec.ts
+++ packages/manager/cypress/e2e/core/firewalls/delete-firewall.spec.ts
@@ -155,36 +155,33 @@ describe('delete firewall', () => {
           cy.findByText('DEFAULT').should('be.visible');
 
           ui.actionMenu
-            .findByTitle(
-              `Action menu for Firewall ${mockFirewallNotDefault.label}`
-            )
+            .findByTitle(`Action menu for Firewall ${mockFirewall.label}`)
             .should('be.visible')
             .should('be.enabled')
             .click();
-
-          ui.actionMenuItem
-            .findByTitle('Disable')
-            .should('be.visible')
-            .should('be.disabled')
-            .focus();
-
-          ui.tooltip
-            .findByText(DEFAULT_FIREWALL_TOOLTIP_TEXT)
-            .should('be.visible');
-
-          ui.actionMenuItem
-            .findByTitle('Delete')
-            .should('be.visible')
-            .should('be.disabled')
-            .focus();
-
-          ui.tooltip
-            .findByText(DEFAULT_FIREWALL_TOOLTIP_TEXT)
-            .should('be.visible');
-
-          // Dismiss the tooltip by focusing on another element.
-          cy.findByText(mockFirewall.label).focus();
         });
+
+      ui.actionMenuItem
+        .findByTitle('Disable')
+        .should('be.visible')
+        .should('be.disabled')
+        .focus();
+
+      ui.tooltip.findByText(DEFAULT_FIREWALL_TOOLTIP_TEXT).should('be.visible');
+
+      ui.actionMenuItem
+        .findByTitle('Delete')
+        .should('be.visible')
+        .should('be.disabled')
+        .focus();
+
+      ui.tooltip.findByText(DEFAULT_FIREWALL_TOOLTIP_TEXT).should('be.visible');
+
+      // Dismiss the tooltip by focusing on another element.
+      cy.findByText(mockFirewall.label).focus();
+
+      // Dismiss the action menu by typing the `escape` key.
+      cy.get('body').type('{esc}');
     });
 
     // Confirm that Firewalls that are not designated as default can be disabled
