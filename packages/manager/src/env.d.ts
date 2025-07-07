f7b097436a940a6191f50cec908c2957b14d5912
Banks Nussman <banks@nussman.us>
2025-07-07 16:42:51 -0400

    fix up vite types

--- packages/manager/src/env.d.ts
+++ packages/manager/src/env.d.ts
@@ -1,11 +1,10 @@
+/// <reference types="./vite.d.ts" />
+/// <reference types="vite/client" />
+
 // This file is where we override Vite types for env typesafety
 // https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
 
 interface ImportMetaEnv {
-  BASE_URL: string;
-  DEV: boolean;
-  MODE: string;
-  PROD: boolean;
   REACT_APP_ACCESS_TOKEN?: string;
   REACT_APP_ADOBE_ANALYTICS_URL?: string;
   REACT_APP_ALGOLIA_APPLICATION_ID?: string;
@@ -24,25 +23,13 @@ interface ImportMetaEnv {
   REACT_APP_PAYPAL_CLIENT_ID?: string;
   REACT_APP_PAYPAL_ENV?: string;
   REACT_APP_PENDO_API_KEY?: string;
-  // TODO: Parent/Child - Remove once we're off mocks.
-  REACT_APP_PROXY_PAT?: string;
+  REACT_APP_PROXY_PAT?: string; // @TODO: Parent/Child - Remove once we're off mocks.
   REACT_APP_SENTRY_URL?: string;
   REACT_APP_STATUS_PAGE_URL?: string;
-  SSR: boolean;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
-}
-
-declare module '*.svg' {
-  const src: ComponentClass<any, any>;
-  export default src;
-}
-
-declare module '*?raw' {
-  const src: string;
-  export default src;
 }
 
 declare module 'logic-query-parser';
