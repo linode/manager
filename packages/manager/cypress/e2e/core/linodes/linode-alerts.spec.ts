79cf7d70417cbe57574bb7b64ce8d3bc379bb112
dmcintyr-akamai <dmcintyr@akamai.com>
2025-07-10 14:17:14 -0400

    fix tests after breaking changes

--- packages/manager/cypress/e2e/core/linodes/linode-alerts.spec.ts
+++ packages/manager/cypress/e2e/core/linodes/linode-alerts.spec.ts
@@ -1,20 +1,56 @@
 import { linodeFactory, regionFactory } from '@linode/utilities';
+import { mockGetAlertDefinition } from 'support/intercepts/cloudpulse';
 import { mockAppendFeatureFlags } from 'support/intercepts/feature-flags';
-import { mockGetLinodeDetails } from 'support/intercepts/linodes';
-import { mockGetUserPreferences } from 'support/intercepts/profile';
+import {
+  mockGetLinodeDetails,
+  mockUpdateLinode,
+} from 'support/intercepts/linodes';
 import { mockGetRegions } from 'support/intercepts/regions';
 import { ui } from 'support/ui';
+import { assertLinodeAlertsEnabled } from 'support/util/linodes';
 import { randomLabel, randomNumber } from 'support/util/random';
 
+import { alertFactory } from 'src/factories';
 import {
   ALERTS_BETA_MODE_BANNER_TEXT,
   ALERTS_BETA_MODE_BUTTON_TEXT,
   ALERTS_LEGACY_MODE_BANNER_TEXT,
   ALERTS_LEGACY_MODE_BUTTON_TEXT,
 } from 'src/features/Linodes/constants';
+
+const mockDisabledLegacyAlerts = {
+  cpu: 0,
+  io: 0,
+  network_in: 0,
+  network_out: 0,
+  transfer_quota: 0,
+};
+const mockEnabledLegacyAlerts = {
+  cpu: 180,
+  io: 10000,
+  network_in: 10,
+  network_out: 10,
+  transfer_quota: 80,
+};
+
+const mockDisabledBetaAlerts = {
+  system: [],
+  user: [],
+};
+
+const mockEnabledBetaAlerts = {
+  system: [1, 2],
+  user: [3],
+};
+
 /*
- * UI of Linode alerts tab based on region enablement of alerts and isAclpAlertsBeta user preference
+ * UI of Linode alerts tab based on beta and legacy alert values in linode.alerts. Dependent on region support for alerts
+ * Legacy alerts = 0, Beta alerts = [],  => legacy disabled
+ * Legacy alerts > 0, Beta alerts = [], => legacy enabled
+ * Legacy alerts = 0, Beta alerts > 0,  => beta enabled
+ * Legacy alerts > 0, Beta alerts > 0,  => beta enabled
  */
+
 describe('region enables alerts', function () {
   beforeEach(() => {
     mockAppendFeatureFlags({
@@ -31,26 +67,61 @@ describe('region enables alerts', function () {
         alerts: ['Linodes'],
       },
     });
+    cy.wrap(mockEnabledRegion).as('mockEnabledRegion');
     mockGetRegions([mockEnabledRegion]).as('getRegions');
+
+    const alertDefinitions = [
+      alertFactory.build({
+        id: 1,
+        description: randomLabel(),
+        // entity_ids: ['1', '2', '3'],
+        label: randomLabel(),
+        service_type: 'linode',
+        severity: 1,
+        status: 'enabled',
+        type: 'system',
+      }),
+      alertFactory.build({
+        id: 2,
+
+        description: randomLabel(),
+        // entity_ids: ['1', '2', '3'],
+        label: randomLabel(),
+        service_type: 'linode',
+        severity: 1,
+        status: 'enabled',
+        type: 'system',
+      }),
+      alertFactory.build({
+        id: 3,
+        description: randomLabel(),
+        // entity_ids: ['1', '2', '3'],
+        label: randomLabel(),
+        service_type: 'linode',
+        severity: 1,
+        status: 'enabled',
+        type: 'user',
+      }),
+    ];
+    cy.wrap(alertDefinitions).as('alertDefinitions');
+    mockGetAlertDefinition('linode', alertDefinitions).as(
+      'getAlertDefinitions'
+    );
+  });
+
+  it('Legacy alerts = 0, Beta alerts = [] => legacy disabled', function () {
     const mockLinode = linodeFactory.build({
       id: randomNumber(),
       label: randomLabel(),
-      region: mockEnabledRegion.id,
+      region: this.mockEnabledRegion.id,
+      alerts: {
+        ...mockDisabledLegacyAlerts,
+        ...mockDisabledBetaAlerts,
+      },
     });
     mockGetLinodeDetails(mockLinode.id, mockLinode).as('getLinode');
     cy.visitWithLogin(`/linodes/${mockLinode.id}/alerts`);
-  });
-
-  it('isAclpAlertsBeta disabled', function () {
-    mockGetUserPreferences({ isAclpAlertsBeta: false }).as(
-      'getUserPreferences'
-    );
-    cy.wait([
-      '@getFeatureFlags',
-      '@getRegions',
-      '@getLinode',
-      '@getUserPreferences',
-    ]);
+    cy.wait(['@getFeatureFlags', '@getRegions', '@getLinode']);
     ui.tabList.findTabByTitle('Alerts').within(() => {
       cy.get('[data-testid="betaChip"]').should('not.exist');
     });
@@ -63,23 +134,19 @@ describe('region enables alerts', function () {
           .within(() => {
             cy.contains(ALERTS_LEGACY_MODE_BANNER_TEXT);
           });
+        // alerts are disabled so all toggles are off
+        ui.toggle.find().each(($toggle) => {
+          cy.wrap($toggle).should('have.attr', 'data-qa-toggle', 'false');
+        });
       });
 
-    // possible to upgrade from legacy alerts to ACLP alerts
+    // upgrade from legacy alerts to ACLP alerts
     ui.button
       .findByTitle(ALERTS_LEGACY_MODE_BUTTON_TEXT)
       .should('be.visible')
-      .should('be.enabled');
-  });
+      .should('be.enabled')
+      .click();
 
-  it('isAclpAlertsBeta enabled', () => {
-    mockGetUserPreferences({ isAclpAlertsBeta: true }).as('getUserPreferences');
-    cy.wait([
-      '@getFeatureFlags',
-      '@getRegions',
-      '@getLinode',
-      '@getUserPreferences',
-    ]);
     ui.tabList.findTabByTitle('Alerts').within(() => {
       cy.get('[data-testid="betaChip"]').should('be.visible');
     });
@@ -97,11 +164,237 @@ describe('region enables alerts', function () {
           .findByTitle(ALERTS_BETA_MODE_BUTTON_TEXT)
           .should('be.visible')
           .should('be.enabled');
+
+        cy.wait(['@getAlertDefinitions']);
+        assertLinodeAlertsEnabled(this.alertDefinitions);
       });
+  });
+
+  it('Legacy alerts > 0, Beta alerts = [] => legacy enabled. can upgrade to beta disabled', function () {
+    const mockLinode = linodeFactory.build({
+      id: randomNumber(),
+      label: randomLabel(),
+      region: this.mockEnabledRegion.id,
+      alerts: {
+        // legacy alerts are enabled if value > 0
+        ...mockEnabledLegacyAlerts,
+      },
+    });
+    mockGetLinodeDetails(mockLinode.id, mockLinode).as('getLinode');
+    cy.visitWithLogin(`/linodes/${mockLinode.id}/alerts`);
+    cy.wait(['@getFeatureFlags', '@getRegions', '@getLinode']);
+    ui.tabList.findTabByTitle('Alerts').within(() => {
+      cy.get('[data-testid="betaChip"]').should('not.exist');
+    });
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]')
+          .should('be.visible')
+          .within(() => {
+            cy.contains(ALERTS_LEGACY_MODE_BANNER_TEXT);
+          });
+
+        // alerts are enabled so all toggles are on if val > 0
+        ui.toggle.find().each(($toggle) => {
+          cy.wrap($toggle).should('have.attr', 'data-qa-toggle', 'true');
+        });
+      });
+
+    // upgrade from legacy alerts to ACLP alerts
+    ui.button
+      .findByTitle(ALERTS_LEGACY_MODE_BUTTON_TEXT)
+      .should('be.visible')
+      .should('be.enabled')
+      .click();
+    ui.tabList.findTabByTitle('Alerts').within(() => {
+      cy.get('[data-testid="betaChip"]').should('be.visible');
+    });
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]')
+          .should('be.visible')
+          .within(() => {
+            cy.contains(ALERTS_BETA_MODE_BANNER_TEXT);
+          });
+        cy.wait(['@getAlertDefinitions']);
+        // toggles in table are off but can be turned on
+        assertLinodeAlertsEnabled(this.alertDefinitions);
+
+        mockUpdateLinode(mockLinode.id).as('updateLinode');
+        ui.button
+          .findByTitle('Save')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+    // TODO: modal maybe shouldnt appear in all scenarios
+    // TODO: content of request.body not match prod, 'alerts' attribute missing here
+    ui.dialog
+      .findByTitle('Save Alerts?')
+      .should('be.visible')
+      .within(() => {
+        ui.button.findByTitle('Save').should('be.visible').click();
+      });
+    cy.wait('@updateLinode').then((xhr) => {
+      // can save changes. new beta alerts added in assertLinodeAlertsEnabled tests
+      const edits = xhr.request.body;
+      expect(JSON.stringify(edits.system)).to.equal(
+        JSON.stringify(mockEnabledBetaAlerts.system)
+      );
+      expect(JSON.stringify(edits.user)).to.equal(
+        JSON.stringify(mockEnabledBetaAlerts.user)
+      );
+    });
+  });
+
+  it('Legacy alerts = 0, Beta alerts > 0, => beta disabled', function () {
+    const mockLinode = linodeFactory.build({
+      id: randomNumber(),
+      label: randomLabel(),
+      region: this.mockEnabledRegion.id,
+      alerts: {
+        ...mockDisabledLegacyAlerts,
+        ...mockEnabledBetaAlerts,
+      },
+    });
+    mockGetLinodeDetails(mockLinode.id, mockLinode).as('getLinode');
+    cy.visitWithLogin(`/linodes/${mockLinode.id}/alerts`);
+    cy.wait(['@getFeatureFlags', '@getRegions', '@getLinode']);
+    ui.tabList.findTabByTitle('Alerts').within(() => {
+      cy.get('[data-testid="betaChip"]').should('be.visible');
+    });
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]')
+          .should('be.visible')
+          .within(() => {
+            cy.contains(ALERTS_BETA_MODE_BANNER_TEXT);
+          });
+        cy.wait(['@getAlertDefinitions']);
+        assertLinodeAlertsEnabled(this.alertDefinitions);
+      });
+
+    // downgrade from ACLP alerts to legacy alerts
+    ui.button
+      .findByTitle(ALERTS_BETA_MODE_BUTTON_TEXT)
+      .should('be.visible')
+      .should('be.enabled')
+      .click();
+
+    ui.tabList.findTabByTitle('Alerts').within(() => {
+      cy.get('[data-testid="betaChip"]').should('not.exist');
+    });
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]')
+          .should('be.visible')
+          .within(() => {
+            cy.contains(ALERTS_LEGACY_MODE_BANNER_TEXT);
+          });
+        // alerts are disabled so all toggles are off
+        ui.toggle.find().each(($toggle) => {
+          cy.wrap($toggle)
+            .should('have.attr', 'data-qa-toggle', 'false')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+        });
+        // possible to upgrade to beta
+        ui.button
+          .findByTitle(ALERTS_LEGACY_MODE_BUTTON_TEXT)
+          .should('be.visible')
+          .should('be.enabled');
+
+        // can save changes
+        ui.button.findByTitle('Save').should('be.visible').should('be.enabled');
+      });
+  });
+
+  it('Legacy alerts > 0, Beta alerts > 0, => beta disabled. can downgrade to legacy enabled', function () {
+    const mockLinode = linodeFactory.build({
+      id: randomNumber(),
+      label: randomLabel(),
+      region: this.mockEnabledRegion.id,
+      alerts: {
+        ...mockEnabledLegacyAlerts,
+        ...mockEnabledBetaAlerts,
+      },
+    });
+    mockGetLinodeDetails(mockLinode.id, mockLinode).as('getLinode');
+    cy.visitWithLogin(`/linodes/${mockLinode.id}/alerts`);
+    cy.wait(['@getFeatureFlags', '@getRegions', '@getLinode']);
+    ui.tabList.findTabByTitle('Alerts').within(() => {
+      cy.get('[data-testid="betaChip"]').should('be.visible');
+    });
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]')
+          .should('be.visible')
+          .within(() => {
+            cy.contains(ALERTS_BETA_MODE_BANNER_TEXT);
+          });
+        cy.wait(['@getAlertDefinitions']);
+        assertLinodeAlertsEnabled(this.alertDefinitions);
+        // downgrade from ACLP alerts to legacy alerts
+        ui.button
+          .findByTitle(ALERTS_BETA_MODE_BUTTON_TEXT)
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+
+    // beta alerts UI loaded
+    ui.tabList.findTabByTitle('Alerts').within(() => {
+      cy.get('[data-testid="betaChip"]').should('not.exist');
+    });
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]')
+          .should('be.visible')
+          .within(() => {
+            cy.contains(ALERTS_LEGACY_MODE_BANNER_TEXT);
+          });
+        // turn the toggles off
+        ui.toggle
+          .find()
+          .should('have.attr', 'data-qa-toggle', 'true')
+          .should('be.visible')
+          .click({ multiple: true });
+        ui.toggle.find().should('have.attr', 'data-qa-toggle', 'false');
+
+        mockUpdateLinode(mockLinode.id).as('updateLinode');
+        cy.scrollTo('bottom');
+        // save changes
+        ui.button
+          .findByTitle('Save')
+          .should('be.visible')
+          .should('be.enabled')
+          .click();
+      });
+    // TODO: not prompted by Save Alerts? dialog
+    cy.wait('@updateLinode').then((xhr) => {
+      // can save changes. new beta alerts added in assertLinodeAlertsEnabled tests
+      const actual = xhr.request.body;
+      const expected = { alerts: mockDisabledLegacyAlerts };
+      expect(JSON.stringify(actual)).to.equal(JSON.stringify(expected));
+    });
   });
 });
 
-describe('region disables alerts. beta alerts not available regardless of user preference', function () {
+describe('region disables alerts. beta alerts not available regardless of linode settings', function () {
   beforeEach(() => {
     mockAppendFeatureFlags({
       aclpBetaServices: {
@@ -118,44 +411,76 @@ describe('region disables alerts. beta alerts not available regardless of user p
         alerts: [],
       },
     });
+    cy.wrap(mockDisabledRegion).as('mockDisabledRegion');
     mockGetRegions([mockDisabledRegion]).as('getRegions');
+  });
+
+  it('Legacy alerts = 0, Beta alerts > 0,  => legacy disabled', function () {
     const mockLinode = linodeFactory.build({
       id: randomNumber(),
       label: randomLabel(),
-      region: mockDisabledRegion.id,
+      region: this.mockDisabledRegion.id,
+      alerts: {
+        ...mockDisabledLegacyAlerts,
+        ...mockEnabledBetaAlerts,
+      },
     });
     mockGetLinodeDetails(mockLinode.id, mockLinode).as('getLinode');
     cy.visitWithLogin(`/linodes/${mockLinode.id}/alerts`);
-  });
-
-  it('isAclpAlertsBeta disabled', () => {
-    mockGetUserPreferences({ isAclpAlertsBeta: false }).as(
-      'getUserPreferences'
-    );
-    cy.wait([
-      '@getFeatureFlags',
-      '@getRegions',
-      '@getLinode',
-      '@getUserPreferences',
-    ]);
+    cy.wait(['@getFeatureFlags', '@getRegions', '@getLinode']);
     ui.tabList.findTabByTitle('Alerts').within(() => {
       cy.get('[data-testid="betaChip"]').should('not.exist');
     });
-    cy.get('[data-testid="notice-info"]').should('not.exist');
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]').should('not.exist');
+        // not possible to upgrade or downgrade
+        cy.findByText(ALERTS_LEGACY_MODE_BUTTON_TEXT).should('not.exist');
+        cy.findByText(ALERTS_BETA_MODE_BUTTON_TEXT).should('not.exist');
+        // alerts are disabled so all toggles are off but are not readonly
+        ui.toggle.find().each(($toggle) => {
+          cy.wrap($toggle)
+            .should('have.attr', 'data-qa-toggle', 'false')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+          cy.wrap($toggle).should('have.attr', 'data-qa-toggle', 'true');
+        });
+      });
   });
 
-  it('isAclpAlertsBeta enabled', () => {
-    mockGetUserPreferences({ isAclpAlertsBeta: true }).as('getUserPreferences');
-    cy.wait([
-      '@getFeatureFlags',
-      '@getRegions',
-      '@getLinode',
-      '@getUserPreferences',
-    ]);
-
+  it('Legacy alerts > 0, Beta alerts = 0,  => legacy disabled', function () {
+    const mockLinode = linodeFactory.build({
+      id: randomNumber(),
+      label: randomLabel(),
+      region: this.mockDisabledRegion.id,
+      alerts: { ...mockEnabledLegacyAlerts },
+    });
+    mockGetLinodeDetails(mockLinode.id, mockLinode).as('getLinode');
+    cy.visitWithLogin(`/linodes/${mockLinode.id}/alerts`);
+    cy.wait(['@getFeatureFlags', '@getRegions', '@getLinode']);
     ui.tabList.findTabByTitle('Alerts').within(() => {
       cy.get('[data-testid="betaChip"]').should('not.exist');
     });
-    cy.get('[data-testid="notice-info"]').should('not.exist');
+    cy.get('[data-reach-tab-panels]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Alerts').should('be.visible');
+        cy.get('[data-testid="notice-info"]').should('not.exist');
+        // not possible to upgrade or downgrade
+        cy.findByText(ALERTS_LEGACY_MODE_BUTTON_TEXT).should('not.exist');
+        cy.findByText(ALERTS_BETA_MODE_BUTTON_TEXT).should('not.exist');
+        // alerts are disabled so all toggles are off but are not readonly
+        ui.toggle.find().each(($toggle) => {
+          cy.wrap($toggle)
+            .should('have.attr', 'data-qa-toggle', 'true')
+            .should('be.visible')
+            .should('be.enabled')
+            .click();
+          cy.wrap($toggle).should('have.attr', 'data-qa-toggle', 'false');
+        });
+      });
   });
 });
