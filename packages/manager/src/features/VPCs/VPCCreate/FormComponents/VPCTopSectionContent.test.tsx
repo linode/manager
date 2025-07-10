cf5647ad2f4b7c07d67593d95715bc9dd0b6fc68
Hana Xu <115299789+hana-akamai@users.noreply.github.com>
2025-07-10 16:00:29 -0400

    upcoming: [M3-9862] - Add Networking IP Stack section to VPC Create page (#12309)
    ## Description 📝
    Add feature flagged support for creating Dual Stack VPCs (IPv4 + IPv6) in the VPC Create page
    
    ## Changes  🔄
    - Add `Networking IP Stack` section with `IPv4` and `IPv4 + IPv6 (Dual Stack)` options
      - A `/52` prefix length is automatically assigned for normal customers
    - Add support for trusted customers (enterprise)
      - Trusted customers have a choice between `/52` and `/48` prefix lengths
    
    ## How to test 🧪
    
    ### Prerequisites
    
    (How to setup test environment)
    
    - You will need to test using devenv (reach out for more info)
    
    ### Verification steps
    
    (How to verify changes)
    
    - [ ] Ensure the `VPC IPv6` feature flag is enabled and devenv account has the `VPC Dual Stack` account capability
    - [ ] Go to the VPC Create page while pointing to devenv locally
    - [ ] You should see a `Networking IP Stack` section with `IPv4` selected by default
    - [ ] Select `IPv6` and click `Create VPC`
    - [ ] You should see `ipv6: [{range: "/52"}]` in the Network Payload and the auto-allocated IPv6 address with the prefix length from the API
    - [ ] There should be no regressions with VPC IPv4

--- packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.test.tsx
+++ packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.test.tsx
@@ -1,5 +1,10 @@
+import { waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
+import { accountFactory } from 'src/factories';
+import { http, HttpResponse, server } from 'src/mocks/testServer';
 import { renderWithThemeAndHookFormContext } from 'src/utilities/testHelpers';
 
 import { VPCTopSectionContent } from './VPCTopSectionContent';
@@ -10,8 +15,14 @@ const props = {
 
 describe('VPC Top Section form content', () => {
   it('renders the vpc top section form content correctly', () => {
-    const { getByText } = renderWithThemeAndHookFormContext({
+    renderWithThemeAndHookFormContext({
       component: <VPCTopSectionContent {...props} />,
+      // @TODO VPC IPv6: Remove this flag check once VPC IPv6 is in GA
+      options: {
+        flags: {
+          vpcIpv6: false,
+        },
+      },
       useFormOptions: {
         defaultValues: {
           description: '',
@@ -22,8 +33,123 @@ describe('VPC Top Section form content', () => {
       },
     });
 
-    expect(getByText('Region')).toBeVisible();
-    expect(getByText('VPC Label')).toBeVisible();
-    expect(getByText('Description')).toBeVisible();
+    expect(screen.getByText('Region')).toBeVisible();
+    expect(screen.getByText('VPC Label')).toBeVisible();
+    expect(screen.getByText('Description')).toBeVisible();
+    // @TODO VPC IPv6: Remove this check once VPC IPv6 is in GA
+    expect(screen.queryByText('Networking IP Stack')).not.toBeInTheDocument();
+  });
+
+  it('renders a Networking IP Stack section with IPv4 pre-checked if the vpcIpv6 feature flag is enabled', async () => {
+    const account = accountFactory.build({
+      capabilities: ['VPC Dual Stack'],
+    });
+
+    server.use(http.get('*/v4/account', () => HttpResponse.json(account)));
+
+    renderWithThemeAndHookFormContext({
+      component: <VPCTopSectionContent {...props} />,
+      // @TODO VPC IPv6: Remove this flag check once VPC IPv6 is in GA
+      options: {
+        flags: {
+          vpcIpv6: true,
+        },
+      },
+      useFormOptions: {
+        defaultValues: {
+          description: '',
+          label: '',
+          region: '',
+          subnets: [],
+        },
+      },
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('Networking IP Stack')).toBeVisible();
+    });
+
+    const NetworkingIPStackRadios = screen.getAllByRole('radio');
+    expect(NetworkingIPStackRadios[0]).toBeChecked(); // IPv4
+    expect(NetworkingIPStackRadios[1]).not.toBeChecked(); // Dual Stack
+  });
+
+  it('renders VPC IPv6 Prefix Length options with /52 selected if Dual Stack is checked and the customer is enterprise', async () => {
+    const account = accountFactory.build({
+      capabilities: ['VPC Dual Stack', 'VPC IPv6 Large Prefixes'],
+    });
+
+    server.use(http.get('*/v4/account', () => HttpResponse.json(account)));
+
+    renderWithThemeAndHookFormContext({
+      component: <VPCTopSectionContent {...props} />,
+      // @TODO VPC IPv6: Remove this flag check once VPC IPv6 is in GA
+      options: {
+        flags: {
+          vpcIpv6: true,
+        },
+      },
+      useFormOptions: {
+        defaultValues: {
+          description: '',
+          label: '',
+          region: '',
+          subnets: [],
+        },
+      },
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('Networking IP Stack')).toBeVisible();
+    });
+
+    const NetworkingIPStackRadios = screen.getAllByRole('radio');
+    await userEvent.click(NetworkingIPStackRadios[1]);
+    expect(NetworkingIPStackRadios[0]).not.toBeChecked(); // IPv4
+    expect(NetworkingIPStackRadios[1]).toBeChecked(); // Dual Stack
+
+    expect(screen.getByText('VPC IPv6 Prefix Length')).toBeVisible();
+    const IPv6CIDRRadios = screen.getAllByRole('radio');
+    expect(IPv6CIDRRadios[2]).toBeChecked(); // /52
+    expect(IPv6CIDRRadios[3]).not.toBeChecked(); // /48
+  });
+
+  it('does not render VPC IPv6 Prefix Length options if the customer is not enterprise', async () => {
+    const account = accountFactory.build({
+      capabilities: ['VPC Dual Stack'],
+    });
+
+    server.use(http.get('*/v4/account', () => HttpResponse.json(account)));
+
+    renderWithThemeAndHookFormContext({
+      component: <VPCTopSectionContent {...props} />,
+      // @TODO VPC IPv6: Remove this flag check once VPC IPv6 is in GA
+      options: {
+        flags: {
+          vpcIpv6: true,
+        },
+      },
+      useFormOptions: {
+        defaultValues: {
+          description: '',
+          label: '',
+          region: '',
+          subnets: [],
+        },
+      },
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('Networking IP Stack')).toBeVisible();
+    });
+
+    const NetworkingIPStackRadios = screen.getAllByRole('radio');
+    await userEvent.click(NetworkingIPStackRadios[1]);
+    expect(NetworkingIPStackRadios[0]).not.toBeChecked(); // IPv4
+    expect(NetworkingIPStackRadios[1]).toBeChecked(); // Dual Stack
+
+    expect(
+      screen.queryByText('VPC IPv6 Prefix Length')
+    ).not.toBeInTheDocument();
   });
 });
