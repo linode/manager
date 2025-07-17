abc3a15a3d156f4f46dc998d5a2f5a731fa80b1e
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2025-07-17 17:55:22 +0530

    upcoming: [DI-26201] - Address review comments

--- packages/manager/src/features/CloudPulse/Alerts/AlertsResources/constants.ts
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsResources/constants.ts
@@ -1,9 +1,13 @@
+import React from 'react';
+
 import { engineTypeMap } from '../constants';
 import { AlertsEngineTypeFilter } from './AlertsEngineTypeFilter';
 import { AlertsRegionFilter } from './AlertsRegionFilter';
 import { AlertsTagFilter } from './AlertsTagsFilter';
+import { TextWithExtraInfo } from './TextWithExtraInfo';
 
 import type { AlertInstance } from './DisplayAlertResources';
+import type { TextWithInfoProp } from './TextWithExtraInfo';
 import type {
   AlertAdditionalFilterKey,
   ServiceColumns,
@@ -67,7 +71,10 @@ export const serviceTypeBasedColumns: ServiceColumns<AlertInstance> = {
       sortingKey: 'region',
     },
     {
-      accessor: ({ tags }) => tags?.join(',') ?? '',
+      accessor: ({ tags }) =>
+        React.createElement<Required<TextWithInfoProp>>(TextWithExtraInfo, {
+          values: tags ?? [],
+        }),
       label: 'Tags',
       sortingKey: 'tags',
     },
