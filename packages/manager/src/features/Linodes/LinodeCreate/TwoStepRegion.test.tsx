1da64df910daaf53915ffa07c95d65f396b3e331
Hana Xu <hxu@akamai.com>
2025-07-25 14:26:10 -0400

    clean up unit test file

--- packages/manager/src/features/Linodes/LinodeCreate/TwoStepRegion.test.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/TwoStepRegion.test.tsx
@@ -31,12 +31,12 @@ describe('TwoStepRegion', () => {
   });
 
   it('should render a heading and docs link', () => {
-    const { getAllByText, getByText } = renderWithThemeAndHookFormContext({
+    renderWithThemeAndHookFormContext({
       component: <TwoStepRegion onChange={vi.fn()} />,
     });
 
-    const heading = getAllByText('Region')[0];
-    const link = getByText(DOCS_LINK_LABEL_DC_PRICING);
+    const heading = screen.getAllByText('Region')[0];
+    const link = screen.getByText(DOCS_LINK_LABEL_DC_PRICING);
 
     expect(heading).toBeVisible();
     expect(heading.tagName).toBe('H2');
@@ -47,36 +47,36 @@ describe('TwoStepRegion', () => {
   });
 
   it('should render two tabs, Core and Distributed', () => {
-    const { getAllByRole } = renderWithThemeAndHookFormContext({
+    renderWithThemeAndHookFormContext({
       component: <TwoStepRegion onChange={vi.fn()} />,
     });
 
-    const tabs = getAllByRole('tab');
-    expect(tabs[0]).toHaveTextContent('Core');
-    expect(tabs[1]).toHaveTextContent('Distributed');
+    const [coreTab, distributedTab] = screen.getAllByRole('tab');
+
+    expect(coreTab).toHaveTextContent('Core');
+    expect(distributedTab).toHaveTextContent('Distributed');
   });
 
   it('should render a Region Select for the Core tab', () => {
-    const { getByPlaceholderText } = renderWithThemeAndHookFormContext({
+    renderWithThemeAndHookFormContext({
       component: <TwoStepRegion onChange={vi.fn()} />,
     });
 
-    const select = getByPlaceholderText('Select a Region');
+    const regionSelect = screen.getByPlaceholderText('Select a Region');
 
-    expect(select).toBeVisible();
-    expect(select).toBeEnabled();
+    expect(regionSelect).toBeVisible();
+    expect(regionSelect).toBeEnabled();
   });
 
   it('should only display core regions in the Core tab region select', async () => {
-    const { getByPlaceholderText, getByRole } =
-      renderWithThemeAndHookFormContext({
-        component: <TwoStepRegion onChange={vi.fn()} />,
-      });
+    renderWithThemeAndHookFormContext({
+      component: <TwoStepRegion onChange={vi.fn()} />,
+    });
 
-    const select = getByPlaceholderText('Select a Region');
-    await userEvent.click(select);
+    const regionSelect = screen.getByPlaceholderText('Select a Region');
+    await userEvent.click(regionSelect);
 
-    const dropdown = getByRole('listbox');
+    const dropdown = screen.getByRole('listbox');
     expect(dropdown.innerHTML).toContain('US, Newark');
     expect(dropdown.innerHTML).not.toContain(
       'US, Gecko Distributed Region Test'
@@ -84,33 +84,31 @@ describe('TwoStepRegion', () => {
   });
 
   it('should only display distributed regions in the Distributed tab region select', async () => {
-    const { getAllByRole, getByPlaceholderText, getByRole } =
-      renderWithThemeAndHookFormContext({
-        component: <TwoStepRegion onChange={vi.fn()} />,
-      });
+    renderWithThemeAndHookFormContext({
+      component: <TwoStepRegion onChange={vi.fn()} />,
+    });
 
-    const tabs = getAllByRole('tab');
-    await userEvent.click(tabs[1]);
+    const [, distributedTab] = screen.getAllByRole('tab');
+    await userEvent.click(distributedTab);
 
-    const select = getByPlaceholderText('Select a Region');
-    await userEvent.click(select);
+    const regionSelect = screen.getByPlaceholderText('Select a Region');
+    await userEvent.click(regionSelect);
 
-    const dropdown = getByRole('listbox');
+    const dropdown = screen.getByRole('listbox');
     expect(dropdown.innerHTML).toContain('US, Gecko Distributed Region Test');
     expect(dropdown.innerHTML).not.toContain('US, Newark');
   });
 
   it('should render a Geographical Area select with All pre-selected and a Region Select for the Distributed tab', async () => {
-    const { getAllByRole } = renderWithThemeAndHookFormContext({
+    renderWithThemeAndHookFormContext({
       component: <TwoStepRegion onChange={vi.fn()} />,
     });
 
-    const tabs = getAllByRole('tab');
-    await userEvent.click(tabs[1]);
+    const [, distributedTab] = screen.getAllByRole('tab');
+    await userEvent.click(distributedTab);
 
-    const inputs = getAllByRole('combobox');
-    const geographicalAreaSelect = inputs[0];
-    const regionSelect = inputs[1];
+    const [geographicalAreaSelect, regionSelect] =
+      screen.getAllByRole('combobox');
 
     expect(geographicalAreaSelect).toHaveAttribute('value', 'All');
     expect(regionSelect).toHaveAttribute('placeholder', 'Select a Region');
