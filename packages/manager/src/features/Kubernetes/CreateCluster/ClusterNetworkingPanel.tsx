6bcf323aba897e5c53deb5327351068f4ed05dae
mjac0bs <mjacobs@akamai.com>
2025-07-28 15:25:27 -0700

    Add stack type radio button selection to networking panel

--- packages/manager/src/features/Kubernetes/CreateCluster/ClusterNetworkingPanel.tsx
+++ packages/manager/src/features/Kubernetes/CreateCluster/ClusterNetworkingPanel.tsx
@@ -1,8 +1,73 @@
-import { Stack, Typography } from '@linode/ui';
+import {
+  Divider,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+  Typography,
+} from '@linode/ui';
 import React from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { FormLabel } from 'src/components/FormLabel';
+
+import { useIsLkeEnterpriseEnabled } from '../kubeUtils';
+
+import type { KubernetesStackType } from '@linode/api-v4';
 
 export const ClusterNetworkingPanel = () => {
-  return (
+  const { isLkeEnterprisePhase2FeatureEnabled } = useIsLkeEnterpriseEnabled();
+
+  const { control } = useFormContext();
+
+  const [selectedStackType, setSelectedStackType] =
+    React.useState<KubernetesStackType>();
+
+  return isLkeEnterprisePhase2FeatureEnabled ? (
+    <>
+      <Stack divider={<Divider />} spacing={4}>
+        <Controller
+          control={control}
+          name="stack_type"
+          render={() => (
+            <RadioGroup
+              onChange={(e, value: KubernetesStackType) => {
+                setSelectedStackType(value);
+              }}
+              value={selectedStackType}
+            >
+              <FormLabel>IP Version</FormLabel>
+              <>
+                <FormControlLabel
+                  control={<Radio />}
+                  label="IPv4"
+                  value="ipv4"
+                />
+                <FormControlLabel
+                  control={<Radio />}
+                  label="IPv4 + IPv6"
+                  value="ipv4-ipv6"
+                />
+              </>
+            </RadioGroup>
+          )}
+        />
+      </Stack>
+      <Stack marginTop={3}>
+        <Typography
+          sx={(theme) => ({
+            font: theme.tokens.alias.Typography.Label.Bold.S,
+          })}
+        >
+          VPC
+        </Typography>
+        <Typography marginTop={1}>
+          Allow for private communications within and across clusters in the
+          same data center.
+        </Typography>
+      </Stack>
+    </>
+  ) : (
     <Stack>
       <Typography variant="h3">VPC & Firewall</Typography>
       <Typography marginTop={1}>
