f8763243cf2e462181a4d6e20aea98c17ee071a3
Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
2025-07-29 14:25:27 -0700

    upcoming: [M3-10203] - Add IPv6 (dual stack) support to LKE-E create flow (#12594)
    * Add stack_type to api-v4 package
    
    * Add stack type radio button selection to networking panel
    
    * Add stack_type to parent RHF
    
    * Mock stack_type in POST, fix payload
    
    * Add changesets

--- packages/manager/src/features/Kubernetes/CreateCluster/ClusterNetworkingPanel.tsx
+++ packages/manager/src/features/Kubernetes/CreateCluster/ClusterNetworkingPanel.tsx
@@ -1,8 +1,61 @@
-import { Stack, Typography } from '@linode/ui';
+import {
+  Divider,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+  Typography,
+} from '@linode/ui';
 import React from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { FormLabel } from 'src/components/FormLabel';
+
+import { useIsLkeEnterpriseEnabled } from '../kubeUtils';
 
 export const ClusterNetworkingPanel = () => {
-  return (
+  const { isLkeEnterprisePhase2FeatureEnabled } = useIsLkeEnterpriseEnabled();
+
+  const { control } = useFormContext();
+
+  return isLkeEnterprisePhase2FeatureEnabled ? (
+    <>
+      <Stack divider={<Divider />} spacing={4}>
+        <Controller
+          control={control}
+          name="stack_type"
+          render={({ field }) => (
+            <RadioGroup
+              {...field}
+              onChange={(e) => field.onChange(e.target.value)}
+              value={field.value ?? null}
+            >
+              <FormLabel>IP Version</FormLabel>
+              <FormControlLabel control={<Radio />} label="IPv4" value="ipv4" />
+              <FormControlLabel
+                control={<Radio />}
+                label="IPv4 + IPv6"
+                value="ipv4-ipv6"
+              />
+            </RadioGroup>
+          )}
+        />
+      </Stack>
+      <Stack marginTop={3}>
+        <Typography
+          sx={(theme) => ({
+            font: theme.tokens.alias.Typography.Label.Bold.S,
+          })}
+        >
+          VPC
+        </Typography>
+        <Typography marginTop={1}>
+          Allow for private communications within and across clusters in the
+          same data center.
+        </Typography>
+      </Stack>
+    </>
+  ) : (
     <Stack>
       <Typography variant="h3">VPC & Firewall</Typography>
       <Typography marginTop={1}>
