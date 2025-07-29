2c76d35287bf28195b622fed6d7966dd26734aae
Dmytro Chyrva <dchyrva@akamai.com>
2025-07-29 12:47:12 +0200

    ref: [STORIF-62] - Quotas informational banner updated.

--- packages/manager/src/features/ObjectStorage/QuotasInfoNotice.tsx
+++ packages/manager/src/features/ObjectStorage/QuotasInfoNotice.tsx
@@ -1,12 +1,20 @@
-import { Notice } from '@linode/ui';
-import { Link } from '@tanstack/react-router';
+import { Typography } from '@linode/ui';
 import React from 'react';
+
+import { DismissibleBanner } from 'src/components/DismissibleBanner/DismissibleBanner';
+import { Link } from 'src/components/Link';
 
 export const QuotasInfoNotice = ({ action }: { action: string }) => {
   return (
-    <Notice variant="info">
-      Did you know you can check your usage and quotas before {action}?
-      <Link to="/account/quotas"> View Quotas.</Link>
-    </Notice>
+    <DismissibleBanner
+      preferenceKey="quotas-info-banner"
+      spacingBottom={8}
+      variant="info"
+    >
+      <Typography fontSize="inherit">
+        Did you know you can check your usage and quotas before {action}?{' '}
+        <Link to="/account/quotas">View Quotas</Link>.
+      </Typography>
+    </DismissibleBanner>
   );
 };
