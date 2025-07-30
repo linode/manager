53f7e10f677f2e894b1e12fa437eed5dfc4527a8
Conal Ryan <136115382+corya-akamai@users.noreply.github.com>
2025-07-30 13:59:43 -0400

    feat: [UIE-8999] - IAM RBAC Adjust the parameters to useQueryWithPermissions and return errors (#12560)
    * feat: [UIE-8999] - IAM RBAC add parameters to the userQueryWithPermission hook, return errors and loading indicators
    
    * Changeset
    
    * Better API
    
    * Fix test
    
    ---------
    
    Co-authored-by: Conal Ryan <corya@akamai.coms>

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeBackup/LinodeBackups.test.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeBackup/LinodeBackups.test.tsx
@@ -19,6 +19,11 @@ const queryMocks = vi.hoisted(() => ({
       restore_linode_backup: true,
     },
   })),
+  useQueryWithPermissions: vi.fn().mockReturnValue({
+    data: [],
+    isLoading: false,
+    isError: false,
+  }),
 }));
 
 vi.mock('@tanstack/react-router', async () => {
@@ -31,6 +36,7 @@ vi.mock('@tanstack/react-router', async () => {
 
 vi.mock('src/features/IAM/hooks/usePermissions', () => ({
   usePermissions: queryMocks.userPermissions,
+  useQueryWithPermissions: queryMocks.useQueryWithPermissions,
 }));
 
 describe('LinodeBackups', () => {
