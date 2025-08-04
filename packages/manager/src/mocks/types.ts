1e37879497b1ab2474758d725425aa7758ed8e61
mjac0bs <mjacobs@akamai.com>
2025-08-04 00:16:49 -0700

    Update LinodeIPsResponse type for IPv6 VPCs and factories

--- packages/manager/src/mocks/types.ts
+++ packages/manager/src/mocks/types.ts
@@ -12,6 +12,7 @@ import type {
   KubernetesCluster,
   Linode,
   LinodeInterface,
+  LinodeIPsResponse,
   NodeBalancer,
   NodeBalancerConfig,
   NodeBalancerConfigNode,
@@ -170,6 +171,7 @@ export interface MockState {
   kubernetesNodePools: KubeNodePoolResponse[];
   linodeConfigs: [number, Config][]; // number is Linode ID
   linodeInterfaces: [number, LinodeInterface][]; // number is Linode ID
+  linodeIps: [number, LinodeIPsResponse][]; // number is Linode ID
   linodes: Linode[];
   nodeBalancerConfigNodes: NodeBalancerConfigNode[];
   nodeBalancerConfigs: NodeBalancerConfig[];
