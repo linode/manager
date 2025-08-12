f52d8acdea42cd4ee54e95ec3967c65c1ac73ad1
Alban Bailly <130582365+abailly-akamai@users.noreply.github.com>
2025-08-12 09:33:30 +0200

    fix: [M3-10456] - Routing refactor follow-up bugs (#12666)
    * Lish
    
    * no more dashboard link
    
    * feedback @bnussman-akamai

--- packages/manager/src/routes/root.ts
+++ packages/manager/src/routes/root.ts
@@ -3,15 +3,11 @@ import {
   ErrorComponent,
 } from '@tanstack/react-router';
 
-import { Root } from '../Root';
+import { RootSwitch } from './../RootSwitch';
 
 import type { RouterContext } from './types';
 
-/**
- * The root route should render the <Root /> component. However, because we are progressively migrating to TanStack Router,
- * we are using the Outlet component since the Provider is still sitting in <MainContent />.
- */
 export const rootRoute = createRootRouteWithContext<RouterContext>()({
-  component: Root,
+  component: RootSwitch,
   errorComponent: ErrorComponent,
 });
