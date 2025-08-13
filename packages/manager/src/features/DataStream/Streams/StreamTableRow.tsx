a8668eb8c894721aed956454d5b3a9fcc028119a
mduda <mduda@akamai.com>
2025-08-13 12:48:27 +0200

    upcoming: [DPS-34039] CR changes 1

--- packages/manager/src/features/DataStream/Streams/StreamTableRow.tsx
+++ packages/manager/src/features/DataStream/Streams/StreamTableRow.tsx
@@ -1,14 +1,6 @@
-import { streamStatus } from '@linode/api-v4';
-import {
-  useDeleteStreamMutation,
-  useUpdateStreamMutation,
-} from '@linode/queries';
 import { Hidden } from '@linode/ui';
-import { useNavigate } from '@tanstack/react-router';
-import { enqueueSnackbar } from 'notistack';
 import * as React from 'react';
 
-import { ActionMenu } from 'src/components/ActionMenu/ActionMenu';
 import { DateTimeDisplay } from 'src/components/DateTimeDisplay';
 import { StatusIcon } from 'src/components/StatusIcon/StatusIcon';
 import { TableCell } from 'src/components/TableCell';
@@ -17,21 +9,17 @@ import {
   getDestinationTypeOption,
   getStreamTypeOption,
 } from 'src/features/DataStream/dataStreamUtils';
-import { getAPIErrorOrDefault } from 'src/utilities/errorUtils';
+import { StreamActionMenu } from 'src/features/DataStream/Streams/StreamActionMenu';
 
+import type { Handlers as StreamHandlers } from './StreamActionMenu';
 import type { Stream, StreamStatus } from '@linode/api-v4';
 
-interface StreamTableRowProps {
+interface StreamTableRowProps extends StreamHandlers {
   stream: Stream;
 }
 
 export const StreamTableRow = React.memo((props: StreamTableRowProps) => {
-  const { stream } = props;
-
-  const navigate = useNavigate();
-
-  const { mutateAsync: updateStream } = useUpdateStreamMutation();
-  const { mutateAsync: deleteStream } = useDeleteStreamMutation();
+  const { stream, onDelete, onDisableOrEnable, onEdit } = props;
 
   return (
     <TableRow key={stream.id}>
@@ -53,79 +41,11 @@ export const StreamTableRow = React.memo((props: StreamTableRowProps) => {
         </TableCell>
       </Hidden>
       <TableCell actionCell>
-        <ActionMenu
-          actionsList={[
-            {
-              onClick: () => {
-                navigate({ to: `/datastream/streams/${stream.id}/edit` });
-              },
-              title: 'Edit',
-            },
-            {
-              onClick: () => {
-                updateStream({
-                  id: stream.id,
-                  destinations: stream.destinations.map(({ id }) => id),
-                  details: stream.details,
-                  label: stream.label,
-                  type: stream.type,
-                  status:
-                    stream.status === streamStatus.Active
-                      ? streamStatus.Inactive
-                      : streamStatus.Active,
-                })
-                  .then(() => {
-                    return enqueueSnackbar(
-                      `Stream  ${stream.label} ${stream.status === streamStatus.Active ? 'disabled' : 'enabled'}`,
-                      {
-                        variant: 'success',
-                      }
-                    );
-                  })
-                  .catch((error) => {
-                    return enqueueSnackbar(
-                      getAPIErrorOrDefault(
-                        error,
-                        `There was an issue ${stream.status === streamStatus.Active ? 'disabling' : 'enabling'} your stream`
-                      )[0].reason,
-                      {
-                        variant: 'error',
-                      }
-                    );
-                  });
-              },
-              title:
-                stream.status === streamStatus.Active ? 'Disable' : 'Enable',
-            },
-            {
-              onClick: () => {
-                deleteStream({
-                  id: stream.id,
-                })
-                  .then(() => {
-                    return enqueueSnackbar(
-                      `Stream  ${stream.label} deleted successfully`,
-                      {
-                        variant: 'success',
-                      }
-                    );
-                  })
-                  .catch((error) => {
-                    return enqueueSnackbar(
-                      getAPIErrorOrDefault(
-                        error,
-                        `There was an issue deleting your stream`
-                      )[0].reason,
-                      {
-                        variant: 'error',
-                      }
-                    );
-                  });
-              },
-              title: 'Delete',
-            },
-          ]}
-          ariaLabel={`Action menu for ${stream.label} stream`}
+        <StreamActionMenu
+          onDelete={onDelete}
+          onDisableOrEnable={onDisableOrEnable}
+          onEdit={onEdit}
+          stream={stream}
         />
       </TableCell>
     </TableRow>
