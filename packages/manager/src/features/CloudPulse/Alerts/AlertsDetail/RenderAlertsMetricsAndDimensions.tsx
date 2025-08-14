4ecbc37c4e459953c8bede38d905959155b32cf8
Ankita <ankitaan@akamai.com>
2025-08-14 11:16:24 +0530

    [DI-26291] - Introduce dimension value label transformation config (#12676)
    * [DI-26291] - Introduce dimension value transformation config
    
    * [DI-26291] - Update util tc
    
    * [DI-26291] - Update util tc
    
    * [DI-26291] - Create new types
    
    * [DI-26291] - Update comment
    
    * [DI-26291] - Update alert tcs
    
    * [DI-26291] - Update cypress
    
    * [DI-26291] - Update transformation key
    
    * [DI-26291] - Update props
    
    * [DI-26291] - Update props
    
    * [DI-26291] - Add desc
    
    * [DI-26291] - Add test
    
    * [DI-26291] - Move types, remove types
    
    * [DI-26291] - Remove type for apiv4 types
    
    * [DI-26291] - update func
    
    * test[DI-26291]: Fix unit test
    
    * [DI-26291] - fix linting issue
    
    * [DI-26291] - Add changeset

--- packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/RenderAlertsMetricsAndDimensions.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/RenderAlertsMetricsAndDimensions.tsx
@@ -1,9 +1,9 @@
 import { Divider } from '@linode/ui';
-import { capitalize } from '@linode/utilities';
 import { GridLegacy } from '@mui/material';
 import React from 'react';
 
 import NullComponent from 'src/components/NullComponent';
+import { transformDimensionValue } from 'src/features/CloudPulse/Alerts/Utils/utils';
 
 import {
   aggregationTypeMap,
@@ -12,7 +12,10 @@ import {
 } from '../constants';
 import { DisplayAlertDetailChips } from './DisplayAlertDetailChips';
 
-import type { AlertDefinitionMetricCriteria } from '@linode/api-v4';
+import type {
+  AlertDefinitionMetricCriteria,
+  CloudPulseServiceType,
+} from '@linode/api-v4';
 
 interface AlertMetricAndDimensionsProp {
   /*
@@ -21,11 +24,15 @@ interface AlertMetricAndDimensionsProp {
   ruleCriteria: {
     rules: AlertDefinitionMetricCriteria[];
   };
+  /**
+   * The service type of the alert for which the criteria needs to be displayed
+   */
+  serviceType: CloudPulseServiceType;
 }
 
 export const RenderAlertMetricsAndDimensions = React.memo(
   (props: AlertMetricAndDimensionsProp) => {
-    const { ruleCriteria } = props;
+    const { ruleCriteria, serviceType } = props;
 
     if (!ruleCriteria.rules?.length) {
       return <NullComponent />;
@@ -66,12 +73,17 @@ export const RenderAlertMetricsAndDimensions = React.memo(
                 values={dimensionFilters.map(
                   ({
                     label: dimensionLabel,
+                    dimension_label: dimensionFilterKey,
                     operator: dimensionOperator,
                     value,
                   }) => [
                     dimensionLabel,
                     dimensionOperatorTypeMap[dimensionOperator],
-                    capitalize(value),
+                    transformDimensionValue(
+                      serviceType,
+                      dimensionFilterKey,
+                      value
+                    ),
                   ]
                 )}
               />
