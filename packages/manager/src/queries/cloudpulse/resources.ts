b7cf3dc267c05d8f2bfb246021ab2c3dd9e0f98d
nikhagra-akamai <nagrawal@akamai.com>
2025-08-18 12:46:56 +0530

    upcoming: [DI-26793] - Updated logic for linode id to label mappings

--- packages/manager/src/queries/cloudpulse/resources.ts
+++ packages/manager/src/queries/cloudpulse/resources.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import { queryFactory } from './queries';
 
-import type { Filter, Params } from '@linode/api-v4';
+import type { Filter, FirewallDeviceEntity, Params } from '@linode/api-v4';
 import type { CloudPulseResources } from 'src/features/CloudPulse/shared/CloudPulseResourcesSelect';
 
 export const useResourcesQuery = (
@@ -20,19 +20,19 @@ export const useResourcesQuery = (
 
         // handle separately for firewall resource type
         if (resourceType === 'firewall') {
-          resource.entities?.forEach(
-            (entity: { id: number; label: string; type: string }) => {
-              if (entity.type === 'linode') {
-                entities[String(entity.id)] = entity.label;
-              } else if (entity.type === 'linode_interface') {
-                resource.entities?.parent_entity?.forEach(
-                  (parent: { id: number; label: string }) => {
-                    entities[String(parent.id)] = parent.label;
-                  }
-                );
+          resource.entities?.forEach((entity: FirewallDeviceEntity) => {
+            if (entity.type === 'linode' && entity.label) {
+              entities[String(entity.id)] = entity.label;
+            } else if (
+              entity.type === 'linode_interface' &&
+              entity.parent_entity
+            ) {
+              const { id, label } = entity.parent_entity;
+              if (label) {
+                entities[String(id)] = label;
               }
             }
-          );
+          });
         }
         return {
           engineType: resource.engine,
