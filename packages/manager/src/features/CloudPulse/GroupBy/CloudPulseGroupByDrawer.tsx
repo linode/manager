cc05634974af438374f72d0f334c51ccc751549a
nikhagra-akamai <nagrawal@akamai.com>
2025-08-20 14:17:07 +0530

    upcoming: [DI-22916] - Added memo to group by drawer component

--- packages/manager/src/features/CloudPulse/GroupBy/CloudPulseGroupByDrawer.tsx
+++ packages/manager/src/features/CloudPulse/GroupBy/CloudPulseGroupByDrawer.tsx
@@ -19,6 +19,7 @@ export interface GroupByDrawerProps {
   onCancel: () => void;
   open?: boolean;
   options: GroupByOption[];
+  serviceType: string;
   subtitle?: string;
   title: string;
 }
@@ -29,107 +30,122 @@ export interface GroupByOption {
 }
 
 const groupBySelectionLimit = 3;
+export const CloudPulseGroupByDrawer = React.memo(
+  (props: GroupByDrawerProps) => {
+    const {
+      open,
+      title,
+      subtitle,
+      message,
+      options,
+      defaultValue = [],
+      onApply,
+      onCancel,
+      serviceType,
+    } = props;
 
-export const CloudPulseGroupByDrawer = (props: GroupByDrawerProps) => {
-  const {
-    open,
-    title,
-    subtitle,
-    message,
-    options,
-    defaultValue = [],
-    onApply,
-    onCancel,
-  } = props;
+    const [selectedValue, setSelectedValue] = React.useState<GroupByOption[]>(
+      defaultValue?.slice(
+        0,
+        Math.min(defaultValue.length ?? 0, groupBySelectionLimit)
+      )
+    );
+    const previousValueRef = React.useRef<GroupByOption[]>(defaultValue);
 
-  const [selectedValue, setSelectedValue] = React.useState<GroupByOption[]>(
-    defaultValue?.slice(
-      0,
-      Math.min(defaultValue.length ?? 0, groupBySelectionLimit)
-    )
-  );
-  const previousValueRef = React.useRef<GroupByOption[]>(defaultValue);
+    const handleApply = () => {
+      previousValueRef.current = selectedValue;
+      onApply(selectedValue);
+    };
 
-  const handleApply = () => {
-    previousValueRef.current = selectedValue;
-    onApply(selectedValue);
-  };
+    React.useEffect(() => {
+      const value = defaultValue?.slice(
+        0,
+        Math.min(defaultValue.length ?? 0, groupBySelectionLimit)
+      );
+      onApply(value);
+      setSelectedValue(value);
+    }, [serviceType]);
+    const handleClose = () => {
+      setSelectedValue(previousValueRef.current);
+      onCancel();
+    };
 
-  const handleClose = () => {
-    setSelectedValue(previousValueRef.current);
-    onCancel();
-  };
-
-  return (
-    <Drawer onClose={(_) => handleClose()} open={open} title={title}>
-      <Stack gap={4}>
-        <Typography
-          component="p"
-          sx={(theme) => ({ marginTop: -1, font: theme.font.normal })}
-          variant="h2"
-        >
-          {subtitle}
-        </Typography>
-        <Typography sx={(theme) => ({ font: theme.font.normal })} variant="h2">
-          {message}
-        </Typography>
-        <Autocomplete
-          data-testid="dimension-select"
-          helperText={`You can select upto ${groupBySelectionLimit} dimensions.`}
-          isOptionEqualToValue={(option, value) => option.value === value.value}
-          label="Select Dimensions"
-          multiple
-          onChange={(_event, value) => {
-            setSelectedValue(value);
-          }}
-          options={options}
-          placeholder="Select Dimnensions"
-          renderOption={(props, option) => {
-            const { key, ...rest } = props;
-            const isSelectAllORDeslectAllOption =
-              option.label === 'Select All ' ||
-              option.label === 'Deselect All ';
-            const isSelected = props['aria-selected'] === true;
-            const ListItem = isSelectAllORDeslectAllOption
-              ? StyledListItem
-              : 'li';
-            const isDisabled =
-              selectedValue.length >= groupBySelectionLimit && !isSelected;
-            const isHidden =
-              isSelectAllORDeslectAllOption &&
-              options.length > groupBySelectionLimit;
-
-            if (isHidden) {
-              return <NullComponent />;
+    return (
+      <Drawer onClose={(_) => handleClose()} open={open} title={title}>
+        <Stack gap={4}>
+          <Typography
+            component="p"
+            sx={(theme) => ({ marginTop: -1, font: theme.font.normal })}
+            variant="h2"
+          >
+            {subtitle}
+          </Typography>
+          <Typography
+            sx={(theme) => ({ font: theme.font.normal })}
+            variant="h2"
+          >
+            {message}
+          </Typography>
+          <Autocomplete
+            data-testid="dimension-select"
+            helperText={`You can select upto ${groupBySelectionLimit} dimensions.`}
+            isOptionEqualToValue={(option, value) =>
+              option.value === value.value
             }
-            return (
-              <ListItem key={key} {...rest} aria-disabled={isDisabled}>
-                <Box sx={{ flexGrow: 1 }}>{option.label}</Box>
-                <SelectedIcon visible={isSelected} />
-              </ListItem>
-            );
-          }}
-          value={selectedValue}
-        />
+            label="Select Dimensions"
+            multiple
+            onChange={(_event, value) => {
+              setSelectedValue(value);
+            }}
+            options={options}
+            placeholder="Select Dimnensions"
+            renderOption={(props, option) => {
+              const { key, ...rest } = props;
+              const isSelectAllORDeslectAllOption =
+                option.label === 'Select All ' ||
+                option.label === 'Deselect All ';
+              const isSelected = props['aria-selected'] === true;
+              const ListItem = isSelectAllORDeslectAllOption
+                ? StyledListItem
+                : 'li';
+              const isDisabled =
+                selectedValue.length >= groupBySelectionLimit && !isSelected;
+              const isHidden =
+                isSelectAllORDeslectAllOption &&
+                options.length > groupBySelectionLimit;
 
-        <Stack direction="row" gap={2} justifyContent="flex-end">
-          <Button
-            buttonType="outlined"
-            data-testid="cancel"
-            onClick={handleClose}
-          >
-            Cancel
-          </Button>
-          <Button
-            buttonType="primary"
-            color="primary"
-            data-testid="apply"
-            onClick={handleApply}
-          >
-            Apply
-          </Button>
+              if (isHidden) {
+                return <NullComponent />;
+              }
+              return (
+                <ListItem key={key} {...rest} aria-disabled={isDisabled}>
+                  <Box sx={{ flexGrow: 1 }}>{option.label}</Box>
+                  <SelectedIcon visible={isSelected} />
+                </ListItem>
+              );
+            }}
+            value={selectedValue}
+          />
+
+          <Stack direction="row" gap={2} justifyContent="flex-end">
+            <Button
+              buttonType="outlined"
+              data-testid="cancel"
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              buttonType="primary"
+              color="primary"
+              data-testid="apply"
+              onClick={handleApply}
+            >
+              Apply
+            </Button>
+          </Stack>
         </Stack>
-      </Stack>
-    </Drawer>
-  );
-};
+      </Drawer>
+    );
+  }
+);
