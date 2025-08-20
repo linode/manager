6ac41607c964b9bdac77fcac48309f39031aad2a
Banks Nussman <banks@nussman.us>
2025-08-20 18:24:14 -0400

    support type to confirm on Images

---
+++ packages/manager/src/features/Images/ImagesLanding/DeleteImageDialog.tsx
@@ -0,0 +1,53 @@
+import { useDeleteImageMutation, useImageQuery } from '@linode/queries';
+import { useSnackbar } from 'notistack';
+import React from 'react';
+
+import { TypeToConfirmDialog } from 'src/components/TypeToConfirmDialog/TypeToConfirmDialog';
+
+interface Props {
+  imageId: string;
+  onClose: () => void;
+  open: boolean;
+}
+
+export const DeleteImageDialog = (props: Props) => {
+  const { imageId, open, onClose } = props;
+  const { data: image, isLoading, error } = useImageQuery(imageId);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const { mutate } = useDeleteImageMutation({
+    onSuccess() {
+      enqueueSnackbar('Image has been scheduled for deletion.', {
+        variant: 'info',
+      });
+      onClose();
+    },
+  });
+
+  const isPendingUpload = image?.status === 'pending_upload';
+
+  return (
+    <TypeToConfirmDialog
+      entity={{
+        type: 'Image',
+        primaryBtnText: isPendingUpload ? 'Cancel Upload' : 'Delete',
+        action: isPendingUpload ? 'cancellation' : 'deletion',
+        name: image?.label,
+      }}
+      errors={error}
+      label="Image Label"
+      loading={isLoading}
+      onClick={() => mutate({ imageId })}
+      onClose={onClose}
+      open={open}
+      secondaryButtonProps={{
+        label: isPendingUpload ? 'Keep Image' : 'Cancel',
+      }}
+      title={
+        isPendingUpload
+          ? 'Cancel Upload?'
+          : `Delete Image ${image?.label ?? imageId}?`
+      }
+    />
+  );
+};
