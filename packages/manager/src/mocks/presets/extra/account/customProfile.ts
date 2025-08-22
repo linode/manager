f7e16f39898700f802421536f1affc3a682f8ebf
Connie Liu <coliu@akamai.com>
2025-08-22 12:19:38 -0400

    where are we going with this

--- packages/manager/src/mocks/presets/extra/account/customProfile.ts
+++ packages/manager/src/mocks/presets/extra/account/customProfile.ts
@@ -1,15 +1,20 @@
-import { profileFactory } from '@linode/utilities';
+import { grantsFactory, profileFactory } from '@linode/utilities';
 import { http } from 'msw';
 
 import { makeResponse } from 'src/mocks/utilities/response';
 
-import type { Profile } from '@linode/api-v4';
+import type { Grants, Profile } from '@linode/api-v4';
 import type { MockPresetExtra } from 'src/mocks/types';
 
 let customProfileData: null | Profile = null;
+let customGrantsData: Grants | null = null;
 
 export const setCustomProfileData = (data: null | Profile) => {
   customProfileData = data;
+};
+
+export const setCustomGrantData = (data: Grants | null) => {
+  customGrantsData = data;
 };
 
 const mockCustomProfile = () => {
@@ -24,10 +29,22 @@ const mockCustomProfile = () => {
   ];
 };
 
+const mockCustomGrants = () => {
+  return [
+    http.get('*/v4*/grants', async () => {
+      return makeResponse(
+        customGrantsData
+          ? { ...grantsFactory.build(), ...customGrantsData }
+          : grantsFactory.build()
+      );
+    }),
+  ];
+};
+
 export const customProfilePreset: MockPresetExtra = {
   desc: 'Custom Profile',
   group: { id: 'Profile', type: 'profile' },
-  handlers: [mockCustomProfile],
+  handlers: [mockCustomProfile, mockCustomGrants],
   id: 'profile:custom',
   label: 'Custom Profile',
 };
