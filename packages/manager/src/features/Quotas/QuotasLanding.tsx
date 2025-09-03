6c5e62dcb1a6c90c1607fe2210deca68881d2fdf
cpathipa <119517080+cpathipa@users.noreply.github.com>
2025-09-03 11:31:50 -0500

    upcoming: [M3-10529] -  UX feedback: Change /settings to /account-settings and  profile/settings to profile/preferences (#12785)
    * Change settings route to account-settings
    
    * Change rounte 'profile/settings' to 'profile/preferences'
    
    * Update PrimaryNav.test.tsx
    
    * Update Cypress tests to account for account settings route change
    
    * Update packages/manager/src/features/TopMenu/UserMenu/UserMenuPopover.tsx
    
    Co-authored-by: Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>
    
    * Remove redundant document titles
    
    * PR feedback - @coliu-akamai
    
    * Added changeset: UX feedback: Change /settings to /account-settings and profile/settings to profile/preferences
    
    ---------
    
    Co-authored-by: Joe D'Amore <jdamore@akamai.com>
    Co-authored-by: Mariah Jacobs <114685994+mjac0bs@users.noreply.github.com>

--- packages/manager/src/features/Quotas/QuotasLanding.tsx
+++ packages/manager/src/features/Quotas/QuotasLanding.tsx
@@ -1,7 +1,6 @@
 import { Navigate, useLocation } from '@tanstack/react-router';
 import * as React from 'react';
 
-import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import { LandingHeader } from 'src/components/LandingHeader';
 import { MaintenanceBannerV2 } from 'src/components/MaintenanceBanner/MaintenanceBannerV2';
 import { PlatformMaintenanceBanner } from 'src/components/PlatformMaintenanceBanner/PlatformMaintenanceBanner';
@@ -36,7 +35,6 @@ export const QuotasLanding = () => {
     <>
       <PlatformMaintenanceBanner pathname={location.pathname} />
       <MaintenanceBannerV2 pathname={location.pathname} />
-      <DocumentTitleSegment segment="Quotas" />
       <LandingHeader {...landingHeaderProps} spacingBottom={4} />
       <Quotas />
     </>
