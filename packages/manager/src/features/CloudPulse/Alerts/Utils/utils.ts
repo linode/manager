5752b1d39b8f5a50e76fe9e789549ff82e1d85ed
ankitaakamai <ankitaan@akamai.com>
2025-09-03 12:31:40 +0530

    [DI-27060] - Remove service level invalidation from aclp invalidations

--- packages/manager/src/features/CloudPulse/Alerts/Utils/utils.ts
+++ packages/manager/src/features/CloudPulse/Alerts/Utils/utils.ts
@@ -672,10 +672,6 @@ export const invalidateAlerts = (
   );
 
   qc.invalidateQueries({
-    queryKey: queryFactory.resources(serviceType).queryKey,
-  });
-
-  qc.invalidateQueries({
     queryKey: queryFactory.alerts._ctx.all().queryKey,
   });
 
