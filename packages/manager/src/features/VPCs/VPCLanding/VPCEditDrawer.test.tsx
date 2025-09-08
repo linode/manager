fe793ca32a364d402c41165b89aa0cafcd984c70
mpolotsk-akamai <157619599+mpolotsk-akamai@users.noreply.github.com>
2025-09-08 12:14:32 +0200

    feat: [UIE-9125] - IAM RBAC: VPC Landing page permissions check  (#12758)
    * IAM RBAC: VPC permission check for Landing Page
    
    * IAM RBAC: VPC permission check, unit tests
    
    * review fix
    
    * add permission check to Edit and Delete drawers
    
    * Added changeset: IAM RBAC: VPC Landing Page permissions
    
    * add permission check VPC Empty State
    
    * review fix

--- packages/manager/src/features/VPCs/VPCLanding/VPCEditDrawer.test.tsx
+++ packages/manager/src/features/VPCs/VPCLanding/VPCEditDrawer.test.tsx
@@ -5,6 +5,17 @@ import { renderWithTheme } from 'src/utilities/testHelpers';
 
 import { VPCEditDrawer } from './VPCEditDrawer';
 
+const queryMocks = vi.hoisted(() => ({
+  useVPCsQuery: vi.fn().mockReturnValue({}),
+  userPermissions: vi.fn(() => ({
+    data: {
+      update_vpc: true,
+    },
+  })),
+}));
+vi.mock('src/features/IAM/hooks/usePermissions', () => ({
+  usePermissions: queryMocks.userPermissions,
+}));
 describe('Edit VPC Drawer', () => {
   const props = {
     isFetching: false,
@@ -37,5 +48,35 @@ describe('Edit VPC Drawer', () => {
     const cancelBtn = getByText(/Cancel/);
     expect(cancelBtn).not.toHaveAttribute('aria-disabled', 'true');
     expect(cancelBtn).toBeVisible();
+  });
+
+  it('Should disable the Label and Description inputs when user does not have "update_vpc" permission', async () => {
+    queryMocks.userPermissions.mockReturnValue({
+      data: {
+        update_vpc: false,
+      },
+    });
+    const { getByLabelText } = renderWithTheme(<VPCEditDrawer {...props} />);
+
+    const labelInput = getByLabelText('Label');
+    expect(labelInput).toHaveAttribute('disabled');
+
+    const descriptionInput = getByLabelText('Description');
+    expect(descriptionInput).toHaveAttribute('disabled');
+  });
+
+  it('Should enable the Label and Description inputs when user has "update_vpc" permission', async () => {
+    queryMocks.userPermissions.mockReturnValue({
+      data: {
+        update_vpc: true,
+      },
+    });
+    const { getByLabelText } = renderWithTheme(<VPCEditDrawer {...props} />);
+
+    const labelInput = getByLabelText('Label');
+    expect(labelInput).not.toHaveAttribute('disabled');
+
+    const descriptionInput = getByLabelText('Description');
+    expect(descriptionInput).not.toHaveAttribute('disabled');
   });
 });
