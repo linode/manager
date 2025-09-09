0758bd367ac7d6dc6fdb8c0bcc554687c385ec60
Dajahi Wiley <114682940+dwiley-akamai@users.noreply.github.com>
2025-09-09 17:07:58 -0400

    upcoming: [M3-9981] – Add support for VPC IPv6 in the Assign/Unassign Linodes drawers (#12778)

--- packages/manager/src/features/VPCs/components/VPCRangesDescription.tsx
+++ packages/manager/src/features/VPCs/components/VPCRangesDescription.tsx
@@ -19,3 +19,16 @@ export const VPCRangesDescription = (props: TypographyProps) => {
     </Typography>
   );
 };
+
+/**
+ * A shared component that contains a brief description of VPC IPv4 and IPv6 ranges
+ */
+export const DualStackVPCRangesDescription = (props: TypographyProps) => {
+  return (
+    <Typography {...props}>
+      If you need more IPs, you can add IPv4 and IPv6 address ranges to let your
+      VPC connect to services running on this Linode.{' '}
+      <Link to={ASSIGN_COMPUTE_INSTANCE_TO_VPC_LINK}>Learn more</Link>.
+    </Typography>
+  );
+};
