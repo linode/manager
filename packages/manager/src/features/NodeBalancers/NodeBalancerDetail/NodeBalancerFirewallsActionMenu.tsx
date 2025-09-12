902a4206c308a38603efcb1f60b03bb8ad48882c
Anastasiia Alekseenko <aaleksee@akamai.com>
2025-09-12 12:24:15 +0200

    feat: [UIE-9144] - IAM RBAC: perm check nodebalancer settings tab

--- packages/manager/src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerFirewallsActionMenu.tsx
+++ packages/manager/src/features/NodeBalancers/NodeBalancerDetail/NodeBalancerFirewallsActionMenu.tsx
@@ -1,35 +1,40 @@
-import { useGrants, useProfile } from '@linode/queries';
 import * as React from 'react';
 
 import { InlineMenuAction } from 'src/components/InlineMenuAction/InlineMenuAction';
 import { NO_PERMISSIONS_TOOLTIP_TEXT } from 'src/features/Firewalls/FirewallLanding/constants';
-import { checkIfUserCanModifyFirewall } from 'src/features/Firewalls/shared';
+import { usePermissions } from 'src/features/IAM/hooks/usePermissions';
 
 import type { Action } from 'src/components/ActionMenu/ActionMenu';
 
 interface Props {
   firewallID: number;
+  nodeBalancerId: number;
   onUnassign: () => void;
 }
 
 export const NodeBalancerFirewallsActionMenu = (props: Props) => {
-  const { firewallID, onUnassign } = props;
+  const { firewallID, onUnassign, nodeBalancerId } = props;
 
-  const { data: profile } = useProfile();
-  const { data: grants } = useGrants();
-
-  const userCanModifyFirewall = checkIfUserCanModifyFirewall(
-    firewallID,
-    profile,
-    grants
+  const { data: firewallPermissions } = usePermissions(
+    'firewall',
+    ['delete_firewall_device'],
+    firewallID
   );
 
-  const disabledProps = !userCanModifyFirewall
-    ? {
-        disabled: true,
-        tooltip: NO_PERMISSIONS_TOOLTIP_TEXT,
-      }
-    : {};
+  const { data: nodeBalancerPermissions } = usePermissions(
+    'nodebalancer',
+    ['update_nodebalancer'],
+    nodeBalancerId
+  );
+
+  const disabledProps =
+    !firewallPermissions.delete_firewall_device &&
+    !nodeBalancerPermissions.update_nodebalancer
+      ? {
+          disabled: true,
+          tooltip: NO_PERMISSIONS_TOOLTIP_TEXT,
+        }
+      : {};
 
   const action: Action = {
     onClick: () => {
