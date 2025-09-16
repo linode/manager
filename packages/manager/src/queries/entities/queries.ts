851ed4e09b86e1e3f7d0adcd4e3d545f2cea0eb5
mpolotsk <mpolotsk@akamai.com>
2025-09-16 10:41:33 +0200

    fetch all entities

--- packages/manager/src/queries/entities/queries.ts
+++ packages/manager/src/queries/entities/queries.ts
@@ -1,10 +1,24 @@
 import { getAccountEntities } from '@linode/api-v4';
+import { getAll } from '@linode/utilities';
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 
+import type { AccountEntity, Filter, Params } from '@linode/api-v4';
+
+const getAllAccountEntitiesRequest = (
+  _params: Params = {},
+  _filter: Filter = {}
+) =>
+  getAll<AccountEntity>((params) =>
+    getAccountEntities({ ...params, ..._params })
+  )().then((data) => data.data);
+
 export const entitiesQueries = createQueryKeys('entities', {
-  entities: {
-    queryFn: ({ pageParam }) =>
-      getAccountEntities({ page: pageParam as number, page_size: 500 }),
-    queryKey: null,
-  },
+  all: (params: Params = {}, filter: Filter = {}) => ({
+    queryFn: () => getAllAccountEntitiesRequest(params, filter),
+    queryKey: [params, filter],
+  }),
+  paginated: (params: Params, filter: Filter) => ({
+    queryFn: () => getAccountEntities(params),
+    queryKey: [params, filter],
+  }),
 });
