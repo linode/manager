44bfec18f75996db2ff34c153efb262ee69a1024
mduda-akamai <mduda@akamai.com>
2025-09-16 20:49:18 +0200

    upcoming: [DPS-33210] Handle creation and edit for stream and destination (#12823)
    * upcoming: [DPS-33210] Handle creation and edit for stream and destination
    
    * upcoming: [DPS-33210] Add changesets
    
    * upcoming: [DPS-33210] CR changes 1
    
    * upcoming: [DPS-33120] CR changes 2
    
    * upcoming: [DPS-33120] Add field info to error snackbars
    
    * CR changes 3
    
    ---------
    
    Co-authored-by: kagora <kagora@akamai.com>

--- packages/manager/src/features/DataStream/Destinations/DestinationForm/DestinationEdit.test.tsx
+++ packages/manager/src/features/DataStream/Destinations/DestinationForm/DestinationEdit.test.tsx
@@ -3,6 +3,7 @@ import {
   waitFor,
   waitForElementToBeRemoved,
 } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { describe, expect } from 'vitest';
 
@@ -58,5 +59,89 @@ describe('DestinationEdit', () => {
     assertInputHasValue('Access Key ID', 'Access Id');
     assertInputHasValue('Secret Access Key', 'Access Secret');
     assertInputHasValue('Log Path Prefix', 'file');
+  });
+
+  describe('given Test Connection and Edit Destination buttons', () => {
+    const testConnectionButtonText = 'Test Connection';
+    const editDestinationButtonText = 'Edit Destination';
+    const editDestinationSpy = vi.fn();
+    const verifyDestinationSpy = vi.fn();
+
+    describe('when Test Connection button clicked and connection verified positively', () => {
+      it("should enable Edit Destination button and perform proper call when it's clicked", async () => {
+        server.use(
+          http.get(`*/monitor/streams/destinations/${destinationId}`, () => {
+            return HttpResponse.json(mockDestination);
+          }),
+          http.post('*/monitor/streams/destinations/verify', () => {
+            verifyDestinationSpy();
+            return HttpResponse.json({});
+          }),
+          http.put(`*/monitor/streams/destinations/${destinationId}`, () => {
+            editDestinationSpy();
+            return HttpResponse.json({});
+          })
+        );
+
+        renderWithThemeAndHookFormContext({
+          component: <DestinationEdit />,
+        });
+        const loadingElement = screen.queryByTestId(loadingTestId);
+        await waitForElementToBeRemoved(loadingElement);
+
+        const testConnectionButton = screen.getByRole('button', {
+          name: testConnectionButtonText,
+        });
+        const editDestinationButton = screen.getByRole('button', {
+          name: editDestinationButtonText,
+        });
+
+        expect(editDestinationButton).toBeDisabled();
+        await userEvent.click(testConnectionButton);
+        expect(verifyDestinationSpy).toHaveBeenCalled();
+
+        await waitFor(() => {
+          expect(editDestinationButton).toBeEnabled();
+        });
+
+        await userEvent.click(editDestinationButton);
+        expect(editDestinationSpy).toHaveBeenCalled();
+      });
+    });
+
+    describe('when Test Connection button clicked and connection verified negatively', () => {
+      it('should not enable Edit Destination button', async () => {
+        server.use(
+          http.get(`*/monitor/streams/destinations/${destinationId}`, () => {
+            return HttpResponse.json(mockDestination);
+          }),
+          http.post('*/monitor/streams/destinations/verify', () => {
+            verifyDestinationSpy();
+            return HttpResponse.error();
+          })
+        );
+
+        renderWithThemeAndHookFormContext({
+          component: <DestinationEdit />,
+        });
+
+        const loadingElement = screen.queryByTestId(loadingTestId);
+        await waitForElementToBeRemoved(loadingElement);
+        const testConnectionButton = screen.getByRole('button', {
+          name: testConnectionButtonText,
+        });
+        const editDestinationButton = screen.getByRole('button', {
+          name: editDestinationButtonText,
+        });
+
+        expect(editDestinationButton).toBeDisabled();
+        await userEvent.click(testConnectionButton);
+        expect(verifyDestinationSpy).toHaveBeenCalled();
+
+        await waitFor(() => {
+          expect(editDestinationButton).toBeDisabled();
+        });
+      });
+    });
   });
 });
