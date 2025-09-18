4131cf258ad5ba85c1a97785f2da4997b0e8456a
vmangalr <vmangalr@akamai.com>
2025-09-18 09:30:51 +0530

    DI-27257: Fix for bugs, disable metric and dimension filter button if service type is not selected and disable useResources query in alerts section if no supported regions

--- packages/manager/src/queries/cloudpulse/resources.ts
+++ packages/manager/src/queries/cloudpulse/resources.ts
@@ -15,6 +15,9 @@ export const useResourcesQuery = (
     ...queryFactory.resources(resourceType, params, filters),
     enabled,
     select: (resources) => {
+      if (!enabled) {
+        return []; // Return empty array if the query is not enabled
+      }
       return resources.map((resource) => {
         const entities: Record<string, string> = {};
 
