417a6572bde17fcb6424ec530b839f825665123b
Banks Nussman <bnussman@akamai.com>
2025-09-23 10:32:00 -0400

    add error state

--- packages/manager/src/routes/betas/index.tsx
+++ packages/manager/src/routes/betas/index.tsx
@@ -1,11 +1,16 @@
+import { ErrorState } from '@linode/ui';
 import { createRoute, lazyRouteComponent } from '@tanstack/react-router';
+import React from 'react';
 
 import { rootRoute } from '../root';
 
 export const betaRouteTree = createRoute({
   getParentRoute: () => rootRoute,
-  preload: false,
+  wrapInSuspense: true,
   path: 'betas/*',
+  errorComponent: () => (
+    <ErrorState errorText="Betas app is currently offline." />
+  ),
   // @ts-expect-error No types for MF modules currently
   component: lazyRouteComponent(() => import('betas/app')),
 });
