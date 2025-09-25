0dd53bada22a271bec863899cbde840bd414b6ff
Banks Nussman <bnussman@akamai.com>
2025-09-25 12:02:36 -0400

    make standalone app kind of work alone

--- apps/betas/src/App.tsx
+++ apps/betas/src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
 import { QueryClient, QueryClientProvider } from '@linode/queries'
 import React from 'react';
-import { baseRequest } from '@linode/api-v4';
+import { APIError, baseRequest } from '@linode/api-v4';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +14,13 @@ baseRequest.interceptors.request.use(async (config) => {
   }
 
   return config;
+});
+
+baseRequest.interceptors.response.use(undefined, (error) => {
+  const errors: APIError[] = error.response?.data?.errors ?? [
+    { reason: "Unexpected Error" },
+  ];
+  return Promise.reject(errors);
 });
 
 export default function App() {
--- apps/betas/src/main.tsx
+++ apps/betas/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import React from 'react';
+import { light, ThemeProvider } from '@linode/ui';
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <ThemeProvider theme={light}>
+    <App />
+  </ThemeProvider>
+)
