d42c66a657a790d8497f3f416736f27d33dda93b
Hana Xu <hxu@akamai.com>
2025-09-26 12:28:28 -0400

    fix unit tests

--- packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.test.tsx
+++ packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.test.tsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
 import { accountFactory } from 'src/factories';
+import { makeResourcePage } from 'src/mocks/serverHandlers';
 import { http, HttpResponse, server } from 'src/mocks/testServer';
 import { renderWithThemeAndHookFormContext } from 'src/utilities/testHelpers';
 
@@ -75,19 +76,21 @@ describe('VPC Top Section form content', () => {
     expect(NetworkingIPStackRadios[1]).not.toBeChecked(); // Dual Stack
   });
 
-  it.only('renders VPC IPv6 Prefix Length options with /52 selected if the selected region has multiple prefix lengths available', async () => {
+  it('renders VPC IPv6 Prefix Length options with /52 selected if the selected region has multiple prefix lengths available', async () => {
     const account = accountFactory.build({
       capabilities: ['VPC Dual Stack', 'VPC IPv6 Large Prefixes'],
     });
 
     server.use(http.get('*/account', () => HttpResponse.json(account)));
     server.use(
-      http.get('*/regions/vpc-availability', () =>
+      http.get('*/regions/vpc-availability*', () =>
         HttpResponse.json(
-          regionVPCAvailabilityFactory.build({
-            region: 'us-east',
-            available_ipv6_prefix_lengths: [48, 52],
-          })
+          makeResourcePage([
+            regionVPCAvailabilityFactory.build({
+              region: 'us-east',
+              available_ipv6_prefix_lengths: [48, 52],
+            }),
+          ])
         )
       )
     );
@@ -145,49 +148,54 @@ describe('VPC Top Section form content', () => {
     expect(IPv6CIDRRadios[3]).toBeChecked(); // /52
   });
 
-  // it('does not render VPC IPv6 Prefix Length options if there are none available', async () => {
-  //   const account = accountFactory.build({
-  //     capabilities: ['VPC Dual Stack'],
-  //   });
+  it('does not render VPC IPv6 Prefix Length options if there are none available or only /52 available', async () => {
+    const account = accountFactory.build({
+      capabilities: ['VPC Dual Stack'],
+    });
 
-  //   server.use(http.get('*/account', () => HttpResponse.json(account)));
-  //   server.use(
-  //     http.get('*/regions/vpc-availability', () =>
-  //       HttpResponse.json(
-  //         regionVPCAvailabilityFactory.build({ region: 'us-east' })
-  //       )
-  //     )
-  //   );
+    server.use(http.get('*/account', () => HttpResponse.json(account)));
+    server.use(
+      http.get('*/regions/vpc-availability*', () =>
+        HttpResponse.json(
+          makeResourcePage([
+            regionVPCAvailabilityFactory.build({
+              region: 'us-east',
+              available_ipv6_prefix_lengths: [52],
+            }),
+          ])
+        )
+      )
+    );
 
-  //   renderWithThemeAndHookFormContext({
-  //     component: <VPCTopSectionContent {...props} />,
-  //     // @TODO VPC IPv6: Remove this flag check once VPC IPv6 is in GA
-  //     options: {
-  //       flags: {
-  //         vpcIpv6: true,
-  //       },
-  //     },
-  //     useFormOptions: {
-  //       defaultValues: {
-  //         description: '',
-  //         label: '',
-  //         region: '',
-  //         subnets: [],
-  //       },
-  //     },
-  //   });
+    renderWithThemeAndHookFormContext({
+      component: <VPCTopSectionContent {...props} />,
+      // @TODO VPC IPv6: Remove this flag check once VPC IPv6 is in GA
+      options: {
+        flags: {
+          vpcIpv6: true,
+        },
+      },
+      useFormOptions: {
+        defaultValues: {
+          description: '',
+          label: '',
+          region: '',
+          subnets: [],
+        },
+      },
+    });
 
-  //   await waitFor(() => {
-  //     expect(screen.getByText('IP Stack')).toBeVisible();
-  //   });
+    await waitFor(() => {
+      expect(screen.getByText('IP Stack')).toBeVisible();
+    });
 
-  //   const NetworkingIPStackRadios = screen.getAllByRole('radio');
-  //   await userEvent.click(NetworkingIPStackRadios[1]);
-  //   expect(NetworkingIPStackRadios[0]).not.toBeChecked(); // IPv4
-  //   expect(NetworkingIPStackRadios[1]).toBeChecked(); // Dual Stack
+    const NetworkingIPStackRadios = screen.getAllByRole('radio');
+    await userEvent.click(NetworkingIPStackRadios[1]);
+    expect(NetworkingIPStackRadios[0]).not.toBeChecked(); // IPv4
+    expect(NetworkingIPStackRadios[1]).toBeChecked(); // Dual Stack
 
-  //   expect(
-  //     screen.queryByText('VPC IPv6 Prefix Length')
-  //   ).not.toBeInTheDocument();
-  // });
+    expect(
+      screen.queryByText('VPC IPv6 Prefix Length')
+    ).not.toBeInTheDocument();
+  });
 });
