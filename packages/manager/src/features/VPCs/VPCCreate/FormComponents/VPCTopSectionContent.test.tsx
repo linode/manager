730c2f042fa434dc2a254d44c4c25ffa39fde812
Hana Xu <115299789+hana-akamai@users.noreply.github.com>
2025-09-30 09:44:40 -0400

    upcoming: [M3-10614] - Check Region VPC capability for VPC Create IPv6 prefix lengths (#12919)
    ## Description 📝
    Display IPv6 prefix lengths in the VPC Create flow by checking Region VPC capability instead of hardcoded prefix lengths
    
    ## How to test 🧪
    
    ### Prerequisites
    
    (How to setup test environment)
    
    - Ensure the VPC IPv6 feature flag is on and your account has VPC IPv6 customer tags.
    
    ### Verification steps
    
    (How to verify changes)
    
    - [ ] Checkout this PR and point to devcloud locally
    - [ ] Have the network tab open and go to the VPC Create page
    - [ ] You should see a request to the `vpc-availability` endpoint
    - [ ] Select a region and then select dual-stack as the IP Stack
    - [ ] You should see a `VPC IPv6 Prefix Length` section with radio options for each IPv6 prefix length available
    - [ ] The section won't show if there are no available prefixes or there is only one prefix (which we can assume to be /52). You can test this by pointing to prod locally and selecting the NO, Oslo region.
      - Note: Since all regions should be supporting dual-stack VPC by end of October and current customers have probably been communicated which regions to use, we will not be disabling the selection card if there are no prefixes available (requests will just fail and an error is displayed)
    
    ```
    pnpm test VPCTopSectionContent
    ```

--- packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.test.tsx
+++ packages/manager/src/features/VPCs/VPCCreate/FormComponents/VPCTopSectionContent.test.tsx
@@ -1,9 +1,11 @@
+import { regionFactory, regionVPCAvailabilityFactory } from '@linode/utilities';
 import { waitFor } from '@testing-library/react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
 import { accountFactory } from 'src/factories';
+import { makeResourcePage } from 'src/mocks/serverHandlers';
 import { http, HttpResponse, server } from 'src/mocks/testServer';
 import { renderWithThemeAndHookFormContext } from 'src/utilities/testHelpers';
 
@@ -45,7 +47,7 @@ describe('VPC Top Section form content', () => {
       capabilities: ['VPC Dual Stack'],
     });
 
-    server.use(http.get('*/v4*/account', () => HttpResponse.json(account)));
+    server.use(http.get('*/account', () => HttpResponse.json(account)));
 
     renderWithThemeAndHookFormContext({
       component: <VPCTopSectionContent {...props} />,
@@ -74,15 +76,38 @@ describe('VPC Top Section form content', () => {
     expect(NetworkingIPStackRadios[1]).not.toBeChecked(); // Dual Stack
   });
 
-  it('renders VPC IPv6 Prefix Length options with /52 selected if Dual Stack is checked and the customer is enterprise', async () => {
+  it('renders VPC IPv6 Prefix Length options with /52 selected if the selected region has multiple prefix lengths available', async () => {
     const account = accountFactory.build({
       capabilities: ['VPC Dual Stack', 'VPC IPv6 Large Prefixes'],
     });
 
-    server.use(http.get('*/v4*/account', () => HttpResponse.json(account)));
+    server.use(http.get('*/account', () => HttpResponse.json(account)));
+    server.use(
+      http.get('*/regions/vpc-availability*', () =>
+        HttpResponse.json(
+          makeResourcePage([
+            regionVPCAvailabilityFactory.build({
+              region: 'us-east',
+              available_ipv6_prefix_lengths: [48, 52],
+            }),
+          ])
+        )
+      )
+    );
 
     renderWithThemeAndHookFormContext({
-      component: <VPCTopSectionContent {...props} />,
+      component: (
+        <VPCTopSectionContent
+          {...props}
+          regions={[
+            regionFactory.build({
+              id: 'us-east',
+              capabilities: ['VPCs', 'VPC Dual Stack'],
+              label: 'US, Newark, NJ',
+            }),
+          ]}
+        />
+      ),
       // @TODO VPC IPv6: Remove this flag check once VPC IPv6 is in GA
       options: {
         flags: {
@@ -99,6 +124,15 @@ describe('VPC Top Section form content', () => {
       },
     });
 
+    const regionSelect = screen.getByPlaceholderText('Select a Region');
+
+    await userEvent.click(regionSelect);
+    await userEvent.type(regionSelect, 'US, Newark, NJ (us-east)');
+    await waitFor(async () => {
+      const selectedRegionOption = screen.getByText('US, Newark, NJ (us-east)');
+      await userEvent.click(selectedRegionOption);
+    });
+
     await waitFor(() => {
       expect(screen.getByText('IP Stack')).toBeVisible();
     });
@@ -110,16 +144,28 @@ describe('VPC Top Section form content', () => {
 
     expect(screen.getByText('VPC IPv6 Prefix Length')).toBeVisible();
     const IPv6CIDRRadios = screen.getAllByRole('radio');
-    expect(IPv6CIDRRadios[2]).toBeChecked(); // /52
-    expect(IPv6CIDRRadios[3]).not.toBeChecked(); // /48
+    expect(IPv6CIDRRadios[2]).not.toBeChecked(); // /48
+    expect(IPv6CIDRRadios[3]).toBeChecked(); // /52
   });
 
-  it('does not render VPC IPv6 Prefix Length options if the customer is not enterprise', async () => {
+  it('does not render VPC IPv6 Prefix Length options if there are none available or only /52 available', async () => {
     const account = accountFactory.build({
       capabilities: ['VPC Dual Stack'],
     });
 
-    server.use(http.get('*/v4*/account', () => HttpResponse.json(account)));
+    server.use(http.get('*/account', () => HttpResponse.json(account)));
+    server.use(
+      http.get('*/regions/vpc-availability*', () =>
+        HttpResponse.json(
+          makeResourcePage([
+            regionVPCAvailabilityFactory.build({
+              region: 'us-east',
+              available_ipv6_prefix_lengths: [52],
+            }),
+          ])
+        )
+      )
+    );
 
     renderWithThemeAndHookFormContext({
       component: <VPCTopSectionContent {...props} />,
