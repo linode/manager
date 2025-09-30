b72715443ea72c3cfc7c3f988d8047121110c1f4
Alban Bailly <abailly@akamai.com>
2025-09-30 11:34:42 +0200

    search behavior

--- packages/manager/src/features/IAM/Users/UserDelegations/UserDelegations.tsx
+++ packages/manager/src/features/IAM/Users/UserDelegations/UserDelegations.tsx
@@ -1,4 +1,7 @@
-import { useGetDelegatedChildAccountsForUserQuery } from '@linode/queries';
+import {
+  useAllGetDelegatedChildAccountsForUserQuery,
+  useGetDelegatedChildAccountsForUserQuery,
+} from '@linode/queries';
 import {
   CircleProgress,
   ErrorState,
@@ -20,11 +23,14 @@ import { TableRowEmpty } from 'src/components/TableRowEmpty/TableRowEmpty';
 import { useFlags } from 'src/hooks/useFlags';
 import { usePaginationV2 } from 'src/hooks/usePaginationV2';
 
+import type { ChildAccount } from '@linode/api-v4';
+
 export const UserDelegations = () => {
   const { username } = useParams({ from: '/iam/users/$username' });
   const flags = useFlags();
   const isIAMDelegationEnabled = flags?.iamDelegation?.enabled;
   const [search, setSearch] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState<ChildAccount[]>([]);
 
   const pagination = usePaginationV2({
     currentRoute: '/iam/users/$username/delegations',
@@ -37,8 +43,37 @@ export const UserDelegations = () => {
     isLoading,
     error,
   } = useGetDelegatedChildAccountsForUserQuery({
+    params: {
+      page: pagination.page,
+      page_size: pagination.pageSize,
+    },
     username,
   });
+  const {
+    data: allDelegatedChildAccounts,
+    isLoading: allDelegatedChildAccountsLoading,
+  } = useAllGetDelegatedChildAccountsForUserQuery({
+    username,
+    enabled: search.length > 0,
+  });
+
+  const searchIsActive = (value: string) =>
+    value.length > 0 &&
+    allDelegatedChildAccounts &&
+    !allDelegatedChildAccountsLoading;
+
+  const handleSearch = (value: string) => {
+    setSearch(value);
+    if (searchIsActive(value)) {
+      setSearchResults(
+        allDelegatedChildAccounts?.filter((childAccount) =>
+          childAccount.company.toLowerCase().includes(value.toLowerCase())
+        ) || []
+      );
+    } else {
+      setSearchResults([]);
+    }
+  };
 
   if (!isIAMDelegationEnabled) {
     return null;
@@ -57,12 +92,11 @@ export const UserDelegations = () => {
       <Stack>
         <Typography variant="h2">Account Delegations</Typography>
         <DebouncedSearchTextField
-          debounceTime={250}
+          debounceTime={0}
           hideLabel
           label="Search"
-          onSearch={(e) => {
-            setSearch(e);
-          }}
+          loading={allDelegatedChildAccountsLoading}
+          onSearch={handleSearch}
           placeholder="Search"
           sx={{ mt: 3 }}
           value={search}
@@ -74,24 +108,35 @@ export const UserDelegations = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {delegatedChildAccounts?.results === 0 && (
+            {(delegatedChildAccounts?.results === 0 ||
+              (search.length > 0 && searchResults.length === 0)) && (
               <TableRowEmpty colSpan={1} message="No accounts found" />
             )}
-            {delegatedChildAccounts?.data.map((childAccount) => (
-              <TableRow key={childAccount.euuid}>
-                <TableCell>{childAccount.company}</TableCell>
-              </TableRow>
-            ))}
+            {searchIsActive(search) &&
+              searchResults.length > 0 &&
+              searchResults.map((childAccount) => (
+                <TableRow key={childAccount.euuid}>
+                  <TableCell>{childAccount.company}</TableCell>
+                </TableRow>
+              ))}
+            {!searchIsActive(search) &&
+              delegatedChildAccounts?.data.map((childAccount) => (
+                <TableRow key={childAccount.euuid}>
+                  <TableCell>{childAccount.company}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
-        <PaginationFooter
-          count={delegatedChildAccounts?.results ?? 0}
-          eventCategory="delegated child accounts"
-          handlePageChange={pagination.handlePageChange}
-          handleSizeChange={pagination.handlePageSizeChange}
-          page={pagination.page}
-          pageSize={pagination.pageSize}
-        />
+        {!searchIsActive && (
+          <PaginationFooter
+            count={delegatedChildAccounts?.results ?? 0}
+            eventCategory="Delegated Child Accounts"
+            handlePageChange={pagination.handlePageChange}
+            handleSizeChange={pagination.handlePageSizeChange}
+            page={pagination.page}
+            pageSize={pagination.pageSize}
+          />
+        )}
       </Stack>
     </Paper>
   );
