236970a8d5d4893af1feab3978f708c5d18b8a3e
Alban Bailly <abailly@akamai.com>
2025-10-13 09:34:06 +0200

    feedback @aaleksee-akamai

--- packages/manager/src/features/IAM/hooks/useDelegationRole.ts
+++ packages/manager/src/features/IAM/hooks/useDelegationRole.ts
@@ -4,10 +4,10 @@ import type { UserType } from '@linode/api-v4';
 
 type DelegationRole = {
   isChildAccount: boolean;
-  isDefaultUser: boolean;
-  isDelegateUser: boolean;
+  isDefaultAccount: boolean;
+  isDelegateAccount: boolean;
   isParentAccount: boolean;
-  isProxyUser: boolean;
+  isProxyAccount: boolean;
   profileUserName: string | undefined;
   profileUserType: undefined | UserType;
 };
@@ -16,11 +16,11 @@ export const useDelegationRole = (): DelegationRole => {
   const { data: profile } = useProfile();
 
   return {
-    isProxyUser: profile?.user_type === 'proxy',
-    isDefaultUser: profile?.user_type === 'default',
+    isProxyAccount: profile?.user_type === 'proxy',
+    isDefaultAccount: profile?.user_type === 'default',
     isParentAccount: profile?.user_type === 'parent',
     isChildAccount: profile?.user_type === 'child',
-    isDelegateUser: profile?.user_type === 'delegate',
+    isDelegateAccount: profile?.user_type === 'delegate',
     profileUserType: profile?.user_type,
     profileUserName: profile?.username,
   };
