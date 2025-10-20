04d46cb7afe792d62bd2d44587d44af3f3276ab6
Banks Nussman <bnussman@akamai.com>
2025-10-20 16:55:36 -0400

    fix url

--- apps/betas/src/oauth.ts
+++ apps/betas/src/oauth.ts
@@ -2,5 +2,5 @@ import { OAuthClient } from "@linode/oauth";
 
 export const oauthClient = new OAuthClient({
   clientId: import.meta.env.REACT_APP_CLIENT_ID ?? '99f2a0aca6bf702572c7',
-  callbackUrl: import.meta.env.REACT_APP_APP_ROOT ?? "http://localhost:4000/oauth/callback",
+  callbackUrl: import.meta.env.REACT_APP_APP_ROOT ? `${import.meta.env.REACT_APP_APP_ROOT}/oauth/callback` : "http://localhost:4000/oauth/callback",
 });
