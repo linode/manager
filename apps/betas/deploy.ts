44b5155bc46f7386978407f90cfd019560540491
Banks Nussman <bnussman@akamai.com>
2025-10-20 16:45:33 -0400

    betas independent auth in prod

--- apps/betas/deploy.ts
+++ apps/betas/deploy.ts
@@ -1,5 +1,9 @@
 import { $ } from 'bun';
 
-await $`pnpm build`;
+await $`pnpm build`.env({
+  ...process.env,
+  REACT_APP_CLIENT_ID: '88a1c71c5558497f80d8',
+  REACT_APP_APP_ROOT: 'https://betas.nussman.us',
+});
 
 await $`scp -r ./dist/* root@192.168.0.114:/usr/share/caddy/betas`;
