e9be9cfff07f84232859978b7016963c8ee9935f
vmangalr <vmangalr@akamai.com>
2025-10-24 14:28:08 +0530

    DI-27358: add interface props

--- packages/manager/src/features/CloudPulse/Widget/components/DimensionFilters/types.ts
+++ packages/manager/src/features/CloudPulse/Widget/components/DimensionFilters/types.ts
@@ -1,10 +1,24 @@
 export interface MetricsDimensionFilterForm {
+  /**
+   * A list of filters applied on different metric dimensions.
+   */
   dimension_filters: MetricsDimensionFilter[];
 }
 
 export interface MetricsDimensionFilter {
+  /**
+   * The label or name of the metric dimension to filter on.
+   */
   dimension_label: null | string;
+
+  /**
+   * The comparison operator used for filtering.
+   */
   operator: MetricsDimensionFilterOperatorType | null;
+
+  /**
+   * The value to compare against.
+   */
   value: null | string;
 }
 
