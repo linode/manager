c4a4e53e1f44bfa3a77741a281e04f1f3086d20d
kagora-akamai <kagora@akamai.com>
2025-10-27 16:48:33 +0100

    upcoming: [DPS-35222] - Logs destination/stream delete modal error state does not reset (#12996)
    * upcoming: [DPS-35222] - Logs destination/stream delete modal error state does not reset
    
    * Added changeset: Logs Delivery Destinations/Stream Delete confirmation modal error state reset fix

--- packages/manager/src/features/Delivery/Destinations/DestinationsLanding.test.tsx
+++ packages/manager/src/features/Delivery/Destinations/DestinationsLanding.test.tsx
@@ -204,6 +204,56 @@ describe('Destinations Landing Table', () => {
 
         await checkClosedModal(deleteDestinationModal);
       });
+
+      it('should show error when cannot delete destination', async () => {
+        const mockDeleteDestinationMutation = vi.fn().mockRejectedValue([
+          {
+            reason:
+              'Destination with id 1 is attached to a stream and cannot be deleted',
+          },
+        ]);
+        queryMocks.useDeleteDestinationMutation.mockReturnValue({
+          mutateAsync: mockDeleteDestinationMutation,
+        });
+
+        renderComponent();
+        await clickOnActionMenu();
+        await clickOnActionMenuItem('Delete');
+
+        const deleteDestinationModal = screen.getByText('Delete Destination');
+        expect(deleteDestinationModal).toBeInTheDocument();
+
+        let errorIcon = screen.queryByTestId('ErrorOutlineIcon');
+        expect(errorIcon).not.toBeInTheDocument();
+
+        // get delete Destination button
+        const deleteDestinationButton = screen.getByRole('button', {
+          name: 'Delete',
+        });
+        await userEvent.click(deleteDestinationButton);
+
+        expect(mockDeleteDestinationMutation).toHaveBeenCalledWith({
+          id: 1,
+        });
+
+        // check for error state in modal
+        screen.getByTestId('ErrorOutlineIcon');
+
+        // close modal with Cancel button
+        const cancelModalDialogButton = screen.getByRole('button', {
+          name: 'Cancel',
+        });
+        await userEvent.click(cancelModalDialogButton);
+        await checkClosedModal(deleteDestinationModal);
+
+        // open delete confirmation modal again
+        await clickOnActionMenu();
+        await clickOnActionMenuItem('Delete');
+
+        // check for error state to be reset
+        errorIcon = screen.queryByTestId('ErrorOutlineIcon');
+        expect(errorIcon).not.toBeInTheDocument();
+      });
     });
   });
 });
