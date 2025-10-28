c1cff6d80804365e13268b9a70ed9f3c32a82f86
Ankita <ankitaan@akamai.com>
2025-10-28 21:10:39 +0530

    upcoming: [DI-27803] - Firewall-Nodebalancer dashboard enhancements - filtering of firewalls & single select firewall filter (#13014)
    * add filtering for firewalls
    
    * update comments
    
    * upcoming: [DI-27803] - Update util
    
    * upcoming: [DI-27803] - Update comments
    
    * upcoming: [DI-27803] - Update util and comments
    
    * upcoming: [DI-27803] - Update custom-select, config, utils
    
    * upcoming: [DI-27803] - Remove type error
    
    * upcoming: [DI-27803] - Update comments
    
    * upcoming: [DI-27803] - Improve consistency
    
    * [DI-27803] - fix linode interface filtering bug
    
    * upcoming: [DI-27803] - Add changeset
    
    * upcoming: [DI-27803] - Avoid type casting, convert to string array
    
    ---------
    
    Co-authored-by: Nikhil Agrawal <165884194+nikhagra-akamai@users.noreply.github.com>

--- packages/manager/src/features/CloudPulse/shared/CloudPulseResourcesSelect.tsx
+++ packages/manager/src/features/CloudPulse/shared/CloudPulseResourcesSelect.tsx
@@ -9,6 +9,8 @@ import { RESOURCE_FILTER_MAP } from '../Utils/constants';
 import { deepEqual, filterUsingDependentFilters } from '../Utils/FilterBuilder';
 
 import type { CloudPulseMetricsFilter } from '../Dashboard/CloudPulseDashboardLanding';
+import type { QueryFunctionType } from '../Utils/models';
+import type { AssociatedEntityType } from './types';
 import type { CloudPulseServiceType, FilterValue } from '@linode/api-v4';
 
 export interface CloudPulseResources {
@@ -23,8 +25,16 @@ export interface CloudPulseResources {
 }
 
 export interface CloudPulseResourcesSelectProps {
+  /**
+   * The associated entity type for the dashboard
+   */
+  associatedEntityType?: AssociatedEntityType;
   defaultValue?: Partial<FilterValue>;
   disabled?: boolean;
+  /**
+   * The filter function to apply to the resources
+   */
+  filterFn?: (resources: QueryFunctionType) => QueryFunctionType;
   handleResourcesSelection: (
     resources: CloudPulseResources[],
     savePref?: boolean
@@ -50,6 +60,8 @@ export const CloudPulseResourcesSelect = React.memo(
       resourceType,
       savePreferences,
       xFilter,
+      associatedEntityType,
+      filterFn,
     } = props;
 
     const flags = useFlags();
@@ -63,7 +75,9 @@ export const CloudPulseResourcesSelect = React.memo(
       resourceType,
       {},
 
-      RESOURCE_FILTER_MAP[resourceType ?? ''] ?? {}
+      RESOURCE_FILTER_MAP[resourceType ?? ''] ?? {},
+      associatedEntityType, // This is based on the filter configuration, used to keep associated entity id to label mapping for the supported entity type
+      filterFn
     );
 
     const [selectedResources, setSelectedResources] =
