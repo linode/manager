e4373e07d5a0b049ef1b9540381ebe721fc972a4
ankitaakamai <ankitaan@akamai.com>
2025-11-05 11:54:05 +0530

    upcoming: [DI-28000] - Remove temp override

--- packages/manager/src/features/CloudPulse/shared/CloudPulseDashboardSelect.tsx
+++ packages/manager/src/features/CloudPulse/shared/CloudPulseDashboardSelect.tsx
@@ -52,15 +52,7 @@ export const CloudPulseDashboardSelect = React.memo(
       isLoading: serviceTypesLoading,
     } = useCloudPulseServiceTypes(true);
 
-    // const { aclpServices } = useFlags();
-    const aclpServices = {
-      lke: {
-        metrics: {
-          enabled: true,
-          beta: true,
-        },
-      },
-    }
+    const { aclpServices } = useFlags();
 
     // Get formatted enabled service types based on the LD flag
     const serviceTypes: CloudPulseServiceType[] = getEnabledServiceTypes(
