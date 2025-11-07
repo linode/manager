71060bee263bedf7a39e29cdcf2cc92e640bfbdc
ankitaakamai <ankitaan@akamai.com>
2025-11-07 14:00:22 +0530

    upcoming: [DI-28175] - Add linode_id to label conversion for blockstorage

--- packages/manager/src/features/CloudPulse/Utils/CloudPulseWidgetUtils.ts
+++ packages/manager/src/features/CloudPulse/Utils/CloudPulseWidgetUtils.ts
@@ -456,9 +456,17 @@ export const getDimensionName = (props: DimensionNameProperties): string => {
     }
 
     if (key === 'linode_id') {
-      const linodeLabel =
-        resources.find((resource) => resource.entities?.[value] !== undefined)
-          ?.entities?.[value] ?? value;
+      let linodeLabel = value;
+      if (serviceType === 'firewall') {
+        linodeLabel =
+          resources.find((resource) => resource.entities?.[value] !== undefined)
+            ?.entities?.[value] ?? value;
+      }
+      if (serviceType === 'blockstorage') {
+        linodeLabel =
+          resources.find((resource) => resource.volumeLinodeId === value)
+            ?.volumeLinodeLabel ?? value;
+      }
       const index = groupBy.indexOf('linode_id');
       if (index !== -1) {
         labels[index] = linodeLabel;
