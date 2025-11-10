05611be541d2ae21c6547c3e97416fda8c272df1
vmangalr <vmangalr@akamai.com>
2025-11-10 15:16:05 +0530

    DI-27358: Code review, aclp part2

---
+++ packages/manager/src/features/CloudPulse/Widget/components/DimensionFilters/CloudPulseDimensionFilterRenderer.tsx
@@ -0,0 +1,186 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { ActionsPanel, Box, Button } from '@linode/ui';
+import React from 'react';
+import {
+  FormProvider,
+  useFieldArray,
+  useForm,
+  useWatch,
+} from 'react-hook-form';
+
+import { metricDimensionFiltersSchema } from './schema';
+
+import type {
+  MetricsDimensionFilter,
+  MetricsDimensionFilterForm,
+} from './types';
+import type { CloudPulseServiceType, Dimension } from '@linode/api-v4';
+
+interface CloudPulseDimensionFilterRendererProps {
+  /**
+   * The clear all trigger to reset the form
+   */
+  clearAllTrigger: number;
+
+  /**
+   * The list of dimensions associated with the selected metric
+   */
+  dimensionOptions: Dimension[];
+  /**
+   * Callback triggered to close the drawer
+   */
+  onClose: () => void;
+
+  /**
+   * Callback to publish any change in form
+   * @param isDirty indicated the changes
+   */
+  onDimensionChange: (isDirty: boolean) => void;
+  /**
+   * Callback triggered on form submission
+   * @param data The form data on submission
+   */
+  onSubmit: (data: MetricsDimensionFilterForm) => void;
+
+  /**
+   * The selected dimension filters for the metric
+   */
+  selectedDimensions?: MetricsDimensionFilter[];
+  /**
+   * The selected entities for the dimension filter
+   */
+  selectedEntities?: string[];
+
+  /**
+   * The selected regions of the associated entities
+   */
+  selectedRegions?: string[];
+
+  /**
+   * The service type of the associated metric
+   */
+  serviceType: CloudPulseServiceType;
+}
+export const CloudPulseDimensionFilterRenderer = React.memo(
+  (props: CloudPulseDimensionFilterRendererProps) => {
+    const {
+      selectedDimensions,
+      onSubmit,
+      clearAllTrigger,
+      onClose,
+      onDimensionChange,
+    } = props;
+
+    const formMethods = useForm<MetricsDimensionFilterForm>({
+      defaultValues: {
+        dimension_filters:
+          selectedDimensions && selectedDimensions.length > 0
+            ? selectedDimensions
+            : [],
+      },
+      mode: 'onBlur',
+      resolver: yupResolver(metricDimensionFiltersSchema),
+    });
+    const { control, handleSubmit, formState, setValue, clearErrors } =
+      formMethods;
+
+    const { isDirty } = formState;
+
+    const formRef = React.useRef<HTMLFormElement>(null);
+    const handleFormSubmit = handleSubmit(async (values) => {
+      onSubmit({
+        dimension_filters: values.dimension_filters,
+      });
+    });
+
+    const { append, fields } = useFieldArray({
+      control,
+      name: 'dimension_filters',
+    });
+
+    const dimensionFilterWatcher = useWatch({
+      control,
+      name: 'dimension_filters',
+    });
+
+    React.useEffect(() => {
+      // set a single empty filter
+      if (clearAllTrigger > 0) {
+        setValue('dimension_filters', [], {
+          shouldDirty: true,
+          shouldValidate: false,
+        });
+        clearErrors('dimension_filters');
+      }
+    }, [clearAllTrigger, clearErrors, setValue]);
+
+    React.useEffect(() => {
+      if (fields.length) {
+        onDimensionChange(true);
+      } else {
+        onDimensionChange(false);
+      }
+    }, [fields, onDimensionChange]);
+
+    return (
+      <FormProvider {...formMethods}>
+        <form onSubmit={handleFormSubmit} ref={formRef}>
+          <Box display="flex" flexDirection="column" gap={1}>
+            {/* TODO: Integrate with dimension filter row component */}
+            <Button
+              compactX
+              data-qa-buttons="true"
+              data-testid="Add Filter"
+              disabled={
+                dimensionFilterWatcher && dimensionFilterWatcher.length === 5
+              }
+              onClick={() =>
+                append({
+                  dimension_label: null,
+                  operator: null,
+                  value: null,
+                })
+              }
+              size="small"
+              sx={(theme) => ({
+                justifyContent: 'start',
+                width: '160px',
+                height: '20px',
+                fontSize: '16px',
+                marginTop: theme.spacingFunction(12),
+              })}
+            >
+              Add Filter
+            </Button>
+          </Box>
+          <ActionsPanel
+            primaryButtonProps={{
+              label: 'Apply',
+              type: 'submit',
+              disabled:
+                selectedDimensions && selectedDimensions.length > 0 && !isDirty,
+              sx: {
+                width: '65px',
+              },
+            }}
+            secondaryButtonProps={{
+              label: 'Cancel',
+              onClick: () => {
+                onClose();
+              },
+              buttonType: 'outlined',
+              sx: {
+                width: '70px',
+              },
+            }}
+            sx={(theme) => ({
+              display: 'flex',
+              justifyContent: 'flex-end',
+              gap: theme.spacingFunction(12),
+            })}
+          />
+        </form>
+      </FormProvider>
+    );
+  }
+);
