0a13398f79d291acc2bc1cd232d8adafdcdf174a
agorthi-akamai <agorthi@akamai.com>
2025-11-17 11:58:26 +0530

    fixing failed test

--- packages/manager/cypress/e2e/core/cloudpulse/contextual-dbaas-widget-verification.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/contextual-dbaas-widget-verification.spec.ts
@@ -175,7 +175,7 @@ describe('Integration Tests for DBaaS Dashboard ', () => {
     // Select a time duration
     cy.get('[aria-labelledby="start-date"]').parent().as('startDateInput');
     cy.get('@startDateInput').click();
-    cy.get(`[data-qa-preset="Last day"]`).click();
+    cy.get('[data-qa-preset="Last day"]').click();
     cy.get('[data-qa-buttons="apply"]')
       .should('be.visible')
       .should('be.enabled')
