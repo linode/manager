eb9ed17e31d210ca076c551797331e8ab0a8c73c
hrao <hrao@akamai.com>
2025-11-17 10:22:34 +0530

    removed nlb routes that are not yet implemented

--- packages/manager/src/routes/networkLoadBalancer/index.ts
+++ packages/manager/src/routes/networkLoadBalancer/index.ts
@@ -1,4 +1,4 @@
-import { createRoute, redirect } from '@tanstack/react-router';
+import { createRoute } from '@tanstack/react-router';
 
 import { rootRoute } from '../root';
 import { NetworkLoadBalancersRoute } from './networkLoadBalancersRoute';
@@ -18,50 +18,5 @@ const networkLoadBalancersIndexRoute = createRoute({
   ).then((m) => m.networkLoadBalancersLazyRoute)
 );
 
-const networkLoadBalancerDetailRoute = createRoute({
-  beforeLoad: async ({ params }) => {
-    throw redirect({
-      params: {
-        nlbId: params.nlbId,
-      },
-      to: '/netloadbalancers/$nlbId/listeners',
-    });
-  },
-  getParentRoute: () => networkLoadBalancersRoute,
-  path: '$nlbId',
-});
-
-const networkLoadBalancerListenersRoute = createRoute({
-  getParentRoute: () => networkLoadBalancersRoute,
-  path: '$nlbId/listeners',
-});
-
-const networkLoadBalancerListenerDetailRoute = createRoute({
-  beforeLoad: async ({ params }) => {
-    throw redirect({
-      params: {
-        nlbId: params.nlbId,
-        listenerId: params.listenerId,
-      },
-      to: '/netloadbalancers/$nlbId/listeners/$listenerId/nodes',
-    });
-  },
-  getParentRoute: () => networkLoadBalancerListenersRoute,
-  path: '$listenerId',
-});
-
-const networkLoadBalancerNodesRoute = createRoute({
-  getParentRoute: () => networkLoadBalancerListenersRoute,
-  path: '$listenerId/nodes',
-});
-
 export const networkLoadBalancersRouteTree =
-  networkLoadBalancersRoute.addChildren([
-    networkLoadBalancersIndexRoute,
-    networkLoadBalancerDetailRoute.addChildren([
-      networkLoadBalancerListenersRoute.addChildren([
-        networkLoadBalancerListenerDetailRoute,
-        networkLoadBalancerNodesRoute,
-      ]),
-    ]),
-  ]);
+  networkLoadBalancersRoute.addChildren([networkLoadBalancersIndexRoute]);
