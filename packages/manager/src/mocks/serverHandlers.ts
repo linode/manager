9eccb2ff2d5c52988d18e4c287184f2a827fc0d9
nikhagra-akamai <nagrawal@akamai.com>
2025-11-19 15:26:03 +0530

    upcoming: [DI-28395] - Updated server handlers

--- packages/manager/src/mocks/serverHandlers.ts
+++ packages/manager/src/mocks/serverHandlers.ts
@@ -3113,9 +3113,9 @@ export const handlers = [
           rules: [firewallMetricRulesFactory.build()],
         },
       }),
-      ...alertFactory.buildList(3, { status: 'enabling' }),
-      ...alertFactory.buildList(3, { status: 'disabling' }),
-      ...alertFactory.buildList(3, { status: 'provisioning' }),
+      ...alertFactory.buildList(3, { status: 'enabling', type: 'user' }),
+      ...alertFactory.buildList(3, { status: 'disabling', type: 'user' }),
+      ...alertFactory.buildList(3, { status: 'provisioning', type: 'user' }),
     ];
     return HttpResponse.json(makeResourcePage(alerts));
   }),
