a0e78d30dbd46f3a245b382d1bc091eb3f82dffb
pmakode-akamai <pmakode@akamai.com>
2025-11-24 13:09:45 +0530

    Some clean up

--- packages/manager/src/features/Firewalls/FirewallDetail/Rules/shared.styles.ts
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/shared.styles.ts
@@ -1,6 +1,14 @@
-import { Box, omittedProps, styled, Typography, WarningIcon } from '@linode/ui';
+import {
+  Box,
+  Chip,
+  omittedProps,
+  styled,
+  Typography,
+  WarningIcon,
+} from '@linode/ui';
 import { makeStyles } from 'tss-react/mui';
 
+import type { FirewallPolicyType } from '@linode/api-v4';
 import type { Theme } from '@linode/ui';
 
 interface StyledListItemProps {
@@ -35,6 +43,25 @@ export const StyledWarningIcon = styled(WarningIcon, {
   marginRight: theme.spacingFunction(4),
   width: '16px',
   height: '16px',
+}));
+
+export const StyledChip = styled(Chip, {
+  shouldForwardProp: omittedProps(['action']),
+})<{ action?: FirewallPolicyType | null }>(({ theme, action }) => ({
+  background:
+    action === 'ACCEPT'
+      ? theme.tokens.component.Badge.Positive.Subtle.Background
+      : theme.tokens.component.Badge.Negative.Subtle.Background,
+  color:
+    action === 'ACCEPT'
+      ? theme.tokens.component.Badge.Positive.Subtle.Text
+      : theme.tokens.component.Badge.Negative.Subtle.Text,
+  font: theme.font.bold,
+  width: '51px',
+  fontSize: theme.tokens.font.FontSize.Xxxs,
+  marginRight: theme.spacingFunction(6),
+  flexShrink: 0,
+  alignSelf: 'flex-start',
 }));
 
 export const useStyles = makeStyles()((theme: Theme) => ({
