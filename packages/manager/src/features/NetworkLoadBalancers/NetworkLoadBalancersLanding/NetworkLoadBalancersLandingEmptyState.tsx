86c191fcc792b864888fbff8a0a2860c08dbfaa2
hrao <hrao@akamai.com>
2025-11-25 12:57:04 +0530

    upcoming: [UIE-9565] - Implement Empty Landing State for Network Load Balancers

--- packages/manager/src/features/NetworkLoadBalancers/NetworkLoadBalancersLanding/NetworkLoadBalancersLandingEmptyState.tsx
+++ packages/manager/src/features/NetworkLoadBalancers/NetworkLoadBalancersLanding/NetworkLoadBalancersLandingEmptyState.tsx
@@ -1,14 +1,44 @@
+import { Typography } from '@linode/ui';
 import * as React from 'react';
 
+import DocsIcon from 'src/assets/icons/docs.svg';
+import NetworkIcon from 'src/assets/icons/entityIcons/networking.svg';
 import { DocumentTitleSegment } from 'src/components/DocumentTitle';
 import { Placeholder } from 'src/components/Placeholder/Placeholder';
 
-// This will be implemented as part of a different story.
+import { NLB_API_DOCS_LINK } from '../constants';
+
 export const NetworkLoadBalancersLandingEmptyState = () => {
   return (
     <React.Fragment>
       <DocumentTitleSegment segment="Network Load Balancer" />
-      <Placeholder isEntity title="Network Load Balancer"></Placeholder>
+      <Placeholder
+        buttonProps={[
+          {
+            buttonType: 'outlined',
+            startIcon: <DocsIcon />,
+            children: 'Learn More',
+            href: NLB_API_DOCS_LINK,
+            target: '_blank',
+            sx: {
+              textDecoration: 'none',
+              '&:hover, &:focus, &:focus-visible': {
+                textDecoration: 'none',
+              },
+            },
+            rel: 'noopener noreferrer',
+          },
+        ]}
+        icon={NetworkIcon}
+        isEntity
+        subtitle="High Capacity load balancing service"
+        title="Network Load Balancer"
+      >
+        <Typography variant="h3">
+          Deliver real-time, high-volume traffic management for your most
+          demanding workloads.
+        </Typography>
+      </Placeholder>
     </React.Fragment>
   );
 };
