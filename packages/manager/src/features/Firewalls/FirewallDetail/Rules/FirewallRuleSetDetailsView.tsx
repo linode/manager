effc35d005f7f8f0f8d7e60b095768242c4e2a9d
Purvesh Makode <pmakode@akamai.com>
2025-11-25 14:27:32 +0530

    upcoming: [UIE-9512, UIE-9530] - New Rule Set Details Drawer with Marked for Deletion status (#13108)
    * Save progress
    
    * Update tests
    
    * Few more changes
    
    * Add more changes
    
    * Clean up tests
    
    * Few changes
    
    * Layout updates
    
    * Update tests
    
    * Add ruleset loading state
    
    * Clean up mocks
    
    * Fix mocks
    
    * Add comments to the type
    
    * Added changeset: Update FirewallRuleType to support ruleset
    
    * Added changeset: Update FirewallRuleTypeSchema to support ruleset
    
    * Added changeset: Add new Firewall RuleSet row layout
    
    * Update ruleset action text - Delete to Remove
    
    * Save progress...
    
    * Update comment
    
    * Exclude 'addresses' from rulesets reference payloads
    
    * Some fixes
    
    * Add more details to the drawer for rulset
    
    * More changes...
    
    * Move Action column and improve table responsiveness for long labels
    
    * Update Cypress component test
    
    * Add more changes to the drawer for rulesets
    
    * Update gap & fontsize of firwall add rules selection card
    
    * Fix Chip shrink issue
    
    * Revert Action column movement since its not yet confirmed
    
    * More Refactoring + better formstate typesaftey
    
    * Add renderOptions for Add ruleset Autocomplete
    
    * Fix typos
    
    * Few updates
    
    * Few fixes
    
    * Update cypress component tests
    
    * More changes
    
    * Update Add rulesets button copy
    
    * More Updates
    
    * Feature flag create entity selection for ruleset
    
    * More refactoring - separating form states
    
    * Add ruleset details drawer
    
    * Some clean up...
    
    * Save progress
    
    * Add more changes
    
    * Show only rulsets in dropdown applicable to the given catergory
    
    * Update Date format and some minor changes
    
    * Update mark for deletion date format
    
    * Update date format
    
    * Update badge color tokens
    
    * Capitalize action label in chip
    
    * Update Chip width
    
    * Added changeset: Update Firewall Rule Drawer to support referencing Rule Set
    
    * Use right color tokens for badge
    
    * Update placeholder for Select Rule Set
    
    * Some clean up
    
    * Few updates and clean up
    
    * Make cy test work
    
    * Clean up: remove duplicate validation
    
    * Add cancel btn for rules form + some design tokens for dropdown options
    
    * Add cancel btn and some styling fixes
    
    * Add mocks for Marked for deletion status
    
    * Add unit tests for Add Rule Set Drawer
    
    * Update test title
    
    * Mock useIsFirewallRulesetsPrefixlistsEnabled instead of feature flag
    
    * Fix styling and a bit of clean up
    
    * Clean up and refactor
    
    * Minor styling fixes
    
    * Add unit tests
    
    * Add omitted props for StyledListItem
    
    * Added changeset: New Rule Set Details drawer with Marked for Deletion status
    
    * Some clean up
    
    * Update unit tests
    
    * Fix typo
    
    * Make Rule Drawer accessible via routes
    
    * Improve tests
    
    * Add error state
    
    * Mock getUserTimezone
    
    * Some Clean up and allow route-based access only for view and create modes
    
    * Few changes

---
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallRuleSetDetailsView.tsx
@@ -0,0 +1,196 @@
+import { useFirewallRuleSetQuery } from '@linode/queries';
+import {
+  ActionsPanel,
+  Box,
+  CircleProgress,
+  ErrorState,
+  NotFound,
+  Paper,
+  TooltipIcon,
+} from '@linode/ui';
+import { capitalize } from '@linode/utilities';
+import * as React from 'react';
+
+import { CopyTooltip } from 'src/components/CopyTooltip/CopyTooltip';
+import { DateTimeDisplay } from 'src/components/DateTimeDisplay';
+
+import {
+  generateAddressesLabel,
+  useIsFirewallRulesetsPrefixlistsEnabled,
+} from '../../shared';
+import { RULESET_MARKED_FOR_DELETION_TEXT } from './shared';
+import {
+  StyledChip,
+  StyledLabel,
+  StyledListItem,
+  StyledWarningIcon,
+  useStyles,
+} from './shared.styles';
+
+import type { Category } from './shared';
+import type { FirewallRuleType } from '@linode/api-v4';
+
+interface FirewallRuleSetDetailsViewProps {
+  category: Category;
+  closeDrawer: () => void;
+  ruleset: FirewallRuleType['ruleset'];
+}
+
+export const FirewallRuleSetDetailsView = (
+  props: FirewallRuleSetDetailsViewProps
+) => {
+  const { category, closeDrawer, ruleset } = props;
+
+  const { isFirewallRulesetsPrefixlistsEnabled } =
+    useIsFirewallRulesetsPrefixlistsEnabled();
+  const { classes } = useStyles();
+
+  const isValidRuleSetId = ruleset !== undefined && ruleset !== null;
+
+  const {
+    data: ruleSetDetails,
+    isFetching,
+    isError,
+    error,
+  } = useFirewallRuleSetQuery(
+    ruleset ?? -1,
+    isValidRuleSetId && isFirewallRulesetsPrefixlistsEnabled
+  );
+
+  if (!isValidRuleSetId) {
+    return <NotFound alignTop />;
+  }
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" mt={12}>
+        <CircleProgress size="md" />
+      </Box>
+    );
+  }
+
+  if (isError) {
+    return <ErrorState errorText={error[0].reason} />;
+  }
+
+  return (
+    <Box mt={2}>
+      {[
+        { label: 'Label', value: ruleSetDetails?.label },
+        { label: 'ID', value: ruleSetDetails?.id, copy: true },
+        {
+          label: 'Description',
+          value: ruleSetDetails?.description,
+          column: true,
+        },
+        {
+          label: 'Service Defined',
+          value: ruleSetDetails?.is_service_defined ? 'Yes' : 'No',
+        },
+        { label: 'Version', value: ruleSetDetails?.version },
+        {
+          label: 'Created',
+          value: ruleSetDetails?.created && (
+            <DateTimeDisplay value={ruleSetDetails.created} />
+          ),
+        },
+        {
+          label: 'Updated',
+          value: ruleSetDetails?.updated && (
+            <DateTimeDisplay value={ruleSetDetails.updated} />
+          ),
+        },
+      ].map((item, idx) => (
+        <StyledListItem
+          key={`item-${idx}`}
+          paddingMultiplier={2}
+          sx={
+            item.column
+              ? { flexDirection: 'column', alignItems: 'flex-start' }
+              : {}
+          }
+        >
+          {item.label && (
+            <StyledLabel component="span">{item.label}:</StyledLabel>
+          )}
+          {item.value}
+          {item.copy && (
+            <CopyTooltip
+              className={classes.copyIcon}
+              text={String(ruleSetDetails?.id)}
+            />
+          )}
+        </StyledListItem>
+      ))}
+
+      {ruleSetDetails?.deleted && (
+        <StyledListItem paddingMultiplier={2}>
+          <StyledWarningIcon />
+          <StyledLabel
+            component="span"
+            sx={(theme) => ({
+              color: theme.tokens.alias.Content.Text.Negative,
+            })}
+          >
+            Marked for deletion:
+          </StyledLabel>
+          <DateTimeDisplay
+            sx={(theme) => ({
+              color: theme.tokens.alias.Content.Text.Negative,
+              marginRight: theme.spacingFunction(4),
+            })}
+            value={ruleSetDetails.deleted}
+          />
+          <TooltipIcon
+            status="info"
+            sxTooltipIcon={{
+              '& svg': { width: '16px', height: '16px' },
+              padding: 0,
+              mb: 0.1,
+            }}
+            text={RULESET_MARKED_FOR_DELETION_TEXT}
+          />
+        </StyledListItem>
+      )}
+
+      <Paper
+        sx={(theme) => ({
+          backgroundColor: theme.tokens.alias.Background.Neutral,
+          padding: theme.spacingFunction(12),
+          marginTop: theme.spacingFunction(8),
+        })}
+      >
+        <StyledLabel
+          sx={(theme) => ({ marginBottom: theme.spacingFunction(4) })}
+        >
+          {capitalize(category)} Rules
+        </StyledLabel>
+        {ruleSetDetails?.rules.map((rule, idx) => (
+          <StyledListItem
+            component="span"
+            key={`firewall-ruleset-rule-${idx}`}
+            sx={(theme) => ({
+              padding: `${theme.spacingFunction(4)} 0`,
+            })}
+          >
+            <StyledChip
+              action={rule.action}
+              label={capitalize(rule.action?.toLowerCase() ?? '')}
+            />
+            <Box>
+              {rule.protocol};&nbsp;{rule.ports};&nbsp;
+              {generateAddressesLabel(rule.addresses)}
+            </Box>
+          </StyledListItem>
+        ))}
+      </Paper>
+
+      <ActionsPanel
+        primaryButtonProps={{
+          label: 'Cancel',
+          onClick: closeDrawer,
+        }}
+      />
+    </Box>
+  );
+};
