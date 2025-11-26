5aee884498d0656c16d3d8e7cbfb468cd7026a24
ankitaakamai <ankitaan@akamai.com>
2025-11-26 14:41:25 +0530

    upcoming: [DI-28504] - Remove typo

--- packages/manager/src/features/CloudPulse/shared/CloudPulseFirewallNodebalancersSelect.tsx
+++ packages/manager/src/features/CloudPulse/shared/CloudPulseFirewallNodebalancersSelect.tsx
@@ -168,7 +168,7 @@ export const CloudPulseFirewallNodebalancersSelect = React.memo(
         autoHighlight
         clearOnBlur
         data-testid="nodebalancer-select"
-        disabled={disabled || isLoading}
+        disabled={disabled}
         errorText={
           isError ? `Failed to fetch ${label || 'NodeBalancers'}.` : ''
         }
