02e82e6b2a98fd2411e7a2279c35f14bf584f8ef
Purvesh Makode <pmakode@akamai.com>
2025-12-02 18:30:02 +0530

    upcoming: [UIE-9511, UIE-9513, UIE-9632] - Add and Integrate Prefix List Details Drawer (#13146)
    * Add and integrate PL details drawer
    
    * Update comment
    
    * Update some old references
    
    * Added changeset: Add and Integrate Prefix List Details Drawer
    
    * Added changeset: Add `deleted` property to `FirewallPrefixList` type after API update
    
    * Some refactor and clean up
    
    * Some refactor and update button text from cancel to close
    
    * Rename the 'reference' to 'context' prop for clarity and update all related instances

--- packages/manager/src/features/Firewalls/FirewallDetail/Rules/shared.ts
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/shared.ts
@@ -35,6 +35,9 @@ export const PORT_PRESETS_ITEMS = sortBy(
 export const RULESET_MARKED_FOR_DELETION_TEXT =
   'This rule set will be automatically deleted when it’s no longer referenced by other firewalls.';
 
+export const PREFIXLIST_MARKED_FOR_DELETION_TEXT =
+  'This Prefix List will be automatically deleted when it’s no longer referenced by other firewalls.';
+
 /**
  * The API returns very good Firewall error messages that look like this:
  *
@@ -129,3 +132,15 @@ export const firewallRuleCreateOptions = [
     value: 'ruleset',
   },
 ] as const;
+
+type PrefixListGroup = 'Account' | 'Other' | 'System';
+
+export const getPrefixListType = (name: string): PrefixListGroup => {
+  if (name.startsWith('pl::')) {
+    return 'Account';
+  }
+  if (name.startsWith('pl:system:')) {
+    return 'System';
+  }
+  return 'Other'; // Safe fallback
+};
