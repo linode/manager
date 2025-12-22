6c6047034848e0c3e992d6683eaf200792e4f5f9
vmangalr <vmangalr@akamai.com>
2025-12-22 14:41:04 +0530

    Added: [DI-28776] - Add humanization support for selected units in CloudPulse metrics yAxis graph, legend and tooltip

--- packages/manager/src/featureFlags.ts
+++ packages/manager/src/featureFlags.ts
@@ -100,6 +100,11 @@ interface AclpFlag {
   enabled: boolean;
 
   /**
+   * This property indicates for which unit, we need to humanize the values e.g., count, iops etc.,
+   */
+  humanizableUnits?: string[];
+
+  /**
    * This property indicates whether to show widget dimension filters or not
    */
   showWidgetDimensionFilters?: boolean;
