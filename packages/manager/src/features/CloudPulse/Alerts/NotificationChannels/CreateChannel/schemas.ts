0f2134592522694495fe98118fcfff8805165c72
Ankita <ankitaan@akamai.com>
2025-12-30 12:29:42 +0530

    upcoming: [DI-28503] - Add create notification channel page (#13225)
    * upcoming: [DI-28503] - Add initial changes for create notification channel page
    
    * upcoming: [DI-28503] - Update type of name field, update schema and fix linting issue
    
    * upcoming: [DI-28503] - Fix linting issue
    
    * upcoming: [DI-28503] - Make notification type select a reusable component, move controller to parent
    
    * upcoming: [DI-28503] - Use recipients filter, handle submit
    
    * upcoming: [DI-28503] - Add UT
    
    * upcoming: [DI-28503] - Use reusable component, remove redundancy
    
    * upcoming: [DI-28503] - Update type
    
    * upcoming: [DI-28503] - Add mocks
    
    * upcoming: [DI-28503] - Use reusable component, remove redundancy
    
    * upcoming: [DI-28503] - Handle errors gracefully for non-arrays
    
    * Revert "upcoming: [DI-28503] - Handle errors gracefully for non-arrays"
    
    This reverts commit fb0907e9351122e9179e89542fcab67146142b59.
    
    * upcoming: [DI-28503] - linting fix
    
    * upcoming: [DI-28503] - Add mocks
    
    * upcoming: [DI-28503] - Add changesets
    
    * upcoming: [DI-28503] - Remove unnecessery fallback
    
    ---------
    
    Co-authored-by: venkatmano-akamai <vmangalr@akamai.com>

---
+++ packages/manager/src/features/CloudPulse/Alerts/NotificationChannels/CreateChannel/schemas.ts
@@ -0,0 +1,17 @@
+import { array, mixed, object, string } from 'yup';
+
+import type { ChannelType } from '@linode/api-v4';
+
+const fieldErrorMessage = 'This field is required.';
+
+export const createNotificationChannelSchema = object({
+  name: string().required(fieldErrorMessage),
+  type: mixed<ChannelType>()
+    .required(fieldErrorMessage)
+    .nullable()
+    .test('nonNull', fieldErrorMessage, (value) => value !== null),
+  recipients: array()
+    .of(string().defined())
+    .required(fieldErrorMessage)
+    .min(1, fieldErrorMessage),
+});
