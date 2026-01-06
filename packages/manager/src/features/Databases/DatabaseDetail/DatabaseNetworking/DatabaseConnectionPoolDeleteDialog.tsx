1bec5a3a7822716e4d4513f793bce308ed6e2c68
Hana Xu <115299789+hana-akamai@users.noreply.github.com>
2026-01-06 13:29:33 -0500

    upcoming: [UIE-9430] - Delete Database Connection Pool dialog (#13236)
    ## Description 📝
    Add ability to delete a Connection Pool in the Database details -> Networking tab -> Manage PgBouncer Connection Pools section
    
    ## How to test 🧪
    
    ### Prerequisites
    
    (How to setup test environment)
    
    - Ensure the `Database PgBouncer` feature flag is on
    - Turn on the legacy MSW
    
    ### Verification steps
    
    (How to verify changes)
    
    - [ ] Go to the networking tab of a postgresql Database Cluster (`http://localhost:3000/databases/postgresql/1/networking`)
    - [ ] Try deleting a mocked connection pool via action menu

---
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseNetworking/DatabaseConnectionPoolDeleteDialog.tsx
@@ -0,0 +1,65 @@
+import { useDeleteDatabaseConnectionPoolMutation } from '@linode/queries';
+import { ActionsPanel, Notice } from '@linode/ui';
+import { useSnackbar } from 'notistack';
+import * as React from 'react';
+
+import { ConfirmationDialog } from 'src/components/ConfirmationDialog/ConfirmationDialog';
+
+interface Props {
+  databaseId: number;
+  onClose: () => void;
+  open: boolean;
+  poolLabel: string;
+}
+
+export const DatabaseConnectionPoolDeleteDialog = (props: Props) => {
+  const { onClose, open, databaseId, poolLabel } = props;
+  const { enqueueSnackbar } = useSnackbar();
+  const {
+    error,
+    isPending,
+    reset,
+    mutateAsync: deleteConnectionPool,
+  } = useDeleteDatabaseConnectionPoolMutation(databaseId, poolLabel);
+
+  const onDelete = () => {
+    deleteConnectionPool().then(() => {
+      enqueueSnackbar(`Connection Pool ${poolLabel} deleted successfully.`, {
+        variant: 'success',
+      });
+      onClose();
+    });
+  };
+
+  const clearErrorAndClose = () => {
+    reset();
+    onClose();
+  };
+
+  const actions = (
+    <ActionsPanel
+      primaryButtonProps={{
+        label: 'Delete Connection Pool',
+        loading: isPending,
+        onClick: onDelete,
+      }}
+      secondaryButtonProps={{ label: 'Cancel', onClick: clearErrorAndClose }}
+      style={{ padding: 0 }}
+    />
+  );
+
+  return (
+    <ConfirmationDialog
+      actions={actions}
+      error={error}
+      onClose={() => clearErrorAndClose()}
+      open={open}
+      title={`Delete Connection Pool ${poolLabel}?`}
+    >
+      <Notice variant="warning">
+        <strong>Warning:</strong> Deletion will break the service URI for any
+        clients using this pool.
+      </Notice>
+    </ConfirmationDialog>
+  );
+};
