94c5c04d4f0429774ecfad6bcda5839cb08055b4
Hana Xu <115299789+hana-akamai@users.noreply.github.com>
2026-01-06 13:29:33 -0500

    upcoming: [UIE-9430] - Delete Database Connection Pool dialog (#13236)
    ## Description 📝
    Add ability to delete a Connection Pool in the Database details -> Networking tab -> Manage PgBouncer Connection Pools section
    
    ## How to test 🧪
    
    ### Prerequisites
    
    (How to setup test environment)
    
    - Ensure the `Database PgBouncer` feature flag is on
    - Turn on the legacy MSW
    
    ### Verification steps
    
    (How to verify changes)
    
    - [ ] Go to the networking tab of a postgresql Database Cluster (`http://localhost:3000/databases/postgresql/1/networking`)
    - [ ] Try deleting a mocked connection pool via action menu

---
+++ packages/manager/src/features/Databases/DatabaseDetail/DatabaseNetworking/DatabaseConnectionPoolRow.tsx
@@ -0,0 +1,63 @@
+import { Hidden } from '@linode/ui';
+import { TableCell, TableRow } from 'akamai-cds-react-components/Table';
+import * as React from 'react';
+
+import { ActionMenu } from 'src/components/ActionMenu/ActionMenu';
+import { CONNECTION_POOL_LABEL_CELL_STYLES } from 'src/features/Databases/constants';
+import { StyledActionMenuWrapper } from 'src/features/Databases/shared.styles';
+
+import type { ConnectionPool } from '@linode/api-v4';
+import type { Action } from 'src/components/ActionMenu/ActionMenu';
+
+interface Props {
+  /**
+   * Function called when the delete button in the Action Menu is pressed.
+   */
+  onDelete: (pool: ConnectionPool) => void;
+  /**
+   * Payment method type and data.
+   */
+  pool: ConnectionPool;
+}
+
+export const DatabaseConnectionPoolRow = (props: Props) => {
+  const { pool, onDelete } = props;
+
+  const connectionPoolActions: Action[] = [
+    {
+      onClick: () => null,
+      title: 'Edit', // TODO: UIE-9395 Implement edit functionality
+    },
+    {
+      onClick: () => onDelete(pool),
+      title: 'Delete',
+    },
+  ];
+
+  return (
+    <TableRow hoverable zebra>
+      <TableCell style={CONNECTION_POOL_LABEL_CELL_STYLES}>
+        {pool.label}
+      </TableCell>
+      <Hidden smDown>
+        <TableCell>
+          {`${pool.mode.charAt(0).toUpperCase()}${pool.mode.slice(1)}`}
+        </TableCell>
+      </Hidden>
+      <Hidden smDown>
+        <TableCell>{pool.size}</TableCell>
+      </Hidden>
+      <Hidden smDown>
+        <TableCell>
+          {pool.username === null ? 'Reuse inbound user' : pool.username}
+        </TableCell>
+      </Hidden>
+      <StyledActionMenuWrapper>
+        <ActionMenu
+          actionsList={connectionPoolActions}
+          ariaLabel={`Action menu for connection pool ${pool.label}`}
+        />
+      </StyledActionMenuWrapper>
+    </TableRow>
+  );
+};
--- packages/manager/src/features/Databases/constants.ts
+++ packages/manager/src/features/Databases/constants.ts
@@ -71,3 +71,7 @@ export const ADVANCED_CONFIG_LEARN_MORE_LINK =
   'https://techdocs.akamai.com/cloud-computing/docs/advanced-configuration-parameters';
 export const MANAGE_NETWORKING_LEARN_MORE_LINK =
   'https://techdocs.akamai.com/cloud-computing/docs/aiven-manage-database#manage-networking';
+
+export const CONNECTION_POOL_LABEL_CELL_STYLES = {
+  flex: '.5 1 20.5%',
+};
