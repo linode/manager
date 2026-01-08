045cfa76cec4f8c3fffe4c5a79a4ed11284f1895
Ganesh Revanakar <grevanak@akamai.com>
2026-01-08 17:35:48 +0530

    Fix e2e test failures

--- packages/manager/cypress/e2e/core/linodes/create-linode-with-vpc.spec.ts
+++ packages/manager/cypress/e2e/core/linodes/create-linode-with-vpc.spec.ts
@@ -476,6 +476,9 @@ describe('Create Linode with VPCs (Linode Interfaces)', () => {
     // Confirm the Linode Interfaces section is shown.
     assertNewLinodeInterfacesIsAvailable();
 
+    // Switch to legacy Config Interfaces
+    linodeCreatePage.selectLegacyConfigInterfacesType();
+
     // Select VPC
     linodeCreatePage.selectInterface('vpc');
 
@@ -612,9 +615,6 @@ describe('Create Linode with VPCs (Linode Interfaces)', () => {
     // Confirm the Linode Interfaces section is shown.
     assertNewLinodeInterfacesIsAvailable();
 
-    // Switch to Linode Interfaces
-    linodeCreatePage.selectLinodeInterfacesType();
-
     // Select VPC option
     linodeCreatePage.selectInterface('vpc');
 
@@ -749,6 +749,9 @@ describe('Create Linode with VPCs (Linode Interfaces)', () => {
 
     // Confirm the Linode Interfaces section is shown.
     assertNewLinodeInterfacesIsAvailable();
+
+    // Switch to legacy Config Interfaces
+    linodeCreatePage.selectLegacyConfigInterfacesType();
 
     // Select VPC card
     linodeCreatePage.selectInterface('vpc');
@@ -932,9 +935,6 @@ describe('Create Linode with VPCs (Linode Interfaces)', () => {
 
     // Confirm the Linode Interfaces section is shown.
     assertNewLinodeInterfacesIsAvailable();
-
-    // Switch to Linode Interfaces
-    linodeCreatePage.selectLinodeInterfacesType();
 
     // Select VPC card
     linodeCreatePage.selectInterface('vpc');
