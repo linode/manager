34dc05a0af9cab9e1948dd1238290ba57894a55a
Tanushree Bhattacharji <tbhattac@akamai.com>
2026-01-21 18:01:22 +0530

    Refactored the types to re-use Entity and entityFactory.

--- packages/manager/src/factories/locks.ts
+++ packages/manager/src/factories/locks.ts
@@ -1,16 +1,13 @@
 import { Factory } from '@linode/utilities';
 
-import type { LockEntity, ResourceLock } from '@linode/api-v4';
+import { entityFactory } from './events';
 
-export const lockEntityFactory = Factory.Sync.makeFactory<LockEntity>({
-  id: Factory.each((i) => i + 10),
-  type: 'linode',
-  label: Factory.each((i) => `test-linode-${i}`),
-  url: Factory.each((i) => `/v4/linode/instances/${i + 10}`),
-});
+import type { ResourceLock } from '@linode/api-v4';
 
 export const lockFactory = Factory.Sync.makeFactory<ResourceLock>({
   id: Factory.each((i) => i),
   lock_type: 'cannot_delete',
-  entity: lockEntityFactory.build(),
+  entity: entityFactory.build({
+    type: 'linode',
+  }),
 });
