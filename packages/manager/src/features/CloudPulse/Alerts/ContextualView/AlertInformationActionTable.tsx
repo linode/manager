52478b7c6a745361af7d9c3a20308406514d002d
Ankita <ankitaan@akamai.com>
2026-01-21 18:31:00 +0530

    upcoming: [DI-29177] - Exclude account/region alerts in api payload while updating alerts for a linode and fix state reset issue on save (#13301)
    * upcoming: [DI-29177] - Remove account/region alerts from update linode payload
    
    * upcoming: [DI-29177] - Add tc
    
    * upcoming: [DI-29177] - Update tc
    
    * upcoming: [DI-29177] - Update payload for create flow
    
    * Revert "upcoming: [DI-29177] - Update payload for create flow"
    
    This reverts commit 122d252b8fb7b76b10be52067c89c3d38cef6824.
    
    * upcoming: [DI-29177] - Initial payload for account and region alerts isnt required
    
    * upcoming: [DI-29177] - Remove immediate state
    
    * upcoming: [DI-29177] - Add changeset

--- packages/manager/src/features/CloudPulse/Alerts/ContextualView/AlertInformationActionTable.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/ContextualView/AlertInformationActionTable.tsx
@@ -149,23 +149,16 @@ export const AlertInformationActionTable = (
   const payloadAlertType = (alert: Alert) =>
     alert.type === 'system' ? 'system_alerts' : 'user_alerts';
 
-  const {
-    enabledAlerts,
-    setEnabledAlerts,
-    hasUnsavedChanges,
-    initialState,
-    resetToInitialState,
-  } = useContextualAlertsState(alerts, entityId);
+  const { enabledAlerts, setEnabledAlerts, hasUnsavedChanges, initialState } =
+    useContextualAlertsState(alerts, entityId);
+
+  const isAccountOrRegionAlert = (alert: Alert) =>
+    alert.scope === 'region' || alert.scope === 'account';
 
   // Mutation to update alerts as per service type
   const updateAlerts = useAlertsMutation(serviceType, entityId ?? '');
 
   React.useEffect(() => {
-    // To send initial state of alerts through toggle handler function (For Create Flow)
-    if (!isEditMode && onToggleAlert) {
-      onToggleAlert(enabledAlerts);
-    }
-
     return () => {
       // Cleanup on unmount (For Edit flow)
       if (isEditMode && onToggleAlert) {
@@ -195,8 +188,7 @@ export const AlertInformationActionTable = (
           enqueueSnackbar('Your settings for alerts have been saved.', {
             variant: 'success',
           });
-          // Reset the state to sync with the updated alerts from API
-          resetToInitialState();
+          onToggleAlert?.({}, false);
           invalidateAclpAlerts(queryClient, serviceType, entityId, payload);
         })
         .catch(() => {
@@ -209,7 +201,7 @@ export const AlertInformationActionTable = (
           setIsDialogOpen(false);
         });
     },
-    [updateAlerts, enqueueSnackbar, resetToInitialState]
+    [updateAlerts, enqueueSnackbar, onToggleAlert]
   );
 
   const handleToggleAlert = React.useCallback(
@@ -325,9 +317,12 @@ export const AlertInformationActionTable = (
                           if (!(isEditMode || isCreateMode)) {
                             return null;
                           }
-                          const status = enabledAlerts[
-                            payloadAlertType(alert)
-                          ]?.includes(alert.id);
+                          // If alert is account or region level, enable it by default and if it is entity type alert, check if it is enabled for that entity
+                          const status =
+                            isAccountOrRegionAlert(alert) ||
+                            enabledAlerts[payloadAlertType(alert)]?.includes(
+                              alert.id
+                            );
 
                           return (
                             <AlertInformationActionRow
