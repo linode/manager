f65c894cf37c902c7f1f09125cc86469d6474c3d
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2026-01-21 17:17:35 +0530

    fix: [DI-29215] - End character validation for specific characters (#13297)
    * fix: [DI-29215] - End character validation for specific characters
    
    * add changesets

--- packages/manager/src/features/CloudPulse/Alerts/NotificationChannels/CreateChannel/schemas.ts
+++ packages/manager/src/features/CloudPulse/Alerts/NotificationChannels/CreateChannel/schemas.ts
@@ -4,7 +4,8 @@ import type { ChannelType } from '@linode/api-v4';
 
 const fieldErrorMessage = 'This field is required.';
 
-const specialStartEndRegex = /^[^a-zA-Z0-9]/;
+const specialStartRegex = /^[^a-zA-Z0-9]/;
+const specialEndRegex = /[^a-zA-Z0-9]$/;
 
 export const createNotificationChannelSchema = object({
   name: string()
@@ -18,7 +19,10 @@ export const createNotificationChannelSchema = object({
       'no-special-start-end',
       'Name cannot start or end with a special character.',
       (value) => {
-        return !specialStartEndRegex.test(value ?? '');
+        return !(
+          specialStartRegex.test(value ?? '') ||
+          specialEndRegex.test(value ?? '')
+        );
       }
     ),
   type: mixed<ChannelType>()
