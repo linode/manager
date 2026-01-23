4d4b6ea0887875d48299bcf4b1c74fbcf4ac226a
Dmytro Chyrva <dchyrva@akamai.com>
2026-01-23 10:56:16 +0100

    new: STORIF-231 - ObjectStorageContextualMetrics feature flag added.

--- packages/manager/src/dev-tools/FeatureFlagTool.tsx
+++ packages/manager/src/dev-tools/FeatureFlagTool.tsx
@@ -77,6 +77,10 @@ const options: { flag: keyof Flags; label: string }[] = [
     label: 'VM Host Maintenance Policy',
   },
   { flag: 'volumeSummaryPage', label: 'Volume Summary Page' },
+  {
+    flag: 'objectStorageContextualMetrics',
+    label: 'Object Storage Contextual Metrics',
+  },
   { flag: 'objSummaryPage', label: 'OBJ Summary Page' },
   { flag: 'vpcIpv6', label: 'VPC IPv6' },
 ];
