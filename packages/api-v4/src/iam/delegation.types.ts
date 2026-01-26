a39186a2f5144bc00631546306cec9f33c8060ef
Alban Bailly <130582365+abailly-akamai@users.noreply.github.com>
2026-01-26 09:21:45 +0100

    upcoming: [UIE-9298] IAM Parent/Child - Enable server side filters on User Delegations (#13298)
    * save progress
    
    * query invalidation
    
    * test
    
    * Added changeset: IAM Parent/Child - Enable server side filters on User Delegations

--- packages/api-v4/src/iam/delegation.types.ts
+++ packages/api-v4/src/iam/delegation.types.ts
@@ -1,4 +1,4 @@
-import type { Params } from 'src/types';
+import type { Filter, Params } from 'src/types';
 
 export interface ChildAccount {
   company: string;
@@ -20,6 +20,7 @@ export interface GetMyDelegatedChildAccountsParams {
 
 export interface GetDelegatedChildAccountsForUserParams {
   enabled?: boolean;
+  filter?: Filter;
   params?: Params;
   username: string;
 }
