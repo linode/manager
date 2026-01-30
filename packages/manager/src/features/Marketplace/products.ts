3d60b01fb9fa8a50eff907b8156f99271e7dde77
Purvesh Makode <pmakode@akamai.com>
2026-01-30 18:33:37 +0530

    upcoming: [UIE-9816] - Add Marketplace filters to the Products landing page (#13292)
    * Add filters & waypoint lazyloading
    
    * Added changeset: Add Marketplace filters to the Products landing page
    
    * Minor clean up
    
    * searchDerivedTypeIds only populated if no type is selected
    
    * Update margin bottom
    
    * Minor change
    
    * Use URL search params for marketplace filters
    
    * Center empty state vertically
    
    * Auto-fetch next batch if current batch has no results when filters are applied
    
    * Add pendo ids
    
    * Ensure onLoaded once per filter change
    
    * Revamp - save progress
    
    * Update tests
    
    * Add few changes
    
    * Doc props
    
    * Update changeset
    
    * Input slot props fix
    
    * Order category sections by product count
    
    * Reorganizing utils, types and add test cases
    
    * Keep logos for both theme modes under same dir
    
    * Minor fix
    
    * Separate type imports

---
+++ packages/manager/src/features/Marketplace/products.ts
@@ -0,0 +1,139 @@
+import type { Product } from './shared';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 100001,
+    name: 'Akamai Cloud Computing',
+    shortDescription:
+      'Akamai provides cloud computing, security, and content delivery services...',
+    tileTag: 'Partner Spotlight',
+    productTags: ['Cloud Infrastructure', 'CDN', 'Security'],
+    infoBanner: 'Special Offer: Get started with $100 credit',
+    partner: {
+      name: 'Akamai',
+      logoLightMode: 'akamai-logo-color.svg',
+      logoDarkMode: 'akamai-logo.svg',
+      url: 'https://www.akamai.com',
+      email: '',
+    },
+    type: {
+      name: 'SaaS & APIs',
+    },
+    categories: ['CDN Affiliated', 'Networking'],
+  },
+  {
+    id: 100002,
+    name: 'Product 2',
+    shortDescription:
+      'Akamai provides cloud computing, security, and content delivery services...',
+    tileTag: 'Partner Spotlight',
+    productTags: ['Cloud Infrastructure', 'CDN', 'Security'],
+    infoBanner: 'Special Offer: Get started with $100 credit',
+    partner: {
+      name: 'Oracle',
+      logoLightMode: 'akamai-logo-color.svg',
+      logoDarkMode: 'akamai-logo.svg',
+      url: 'https://www.akamai.com',
+      email: '',
+    },
+    type: {
+      name: 'Agentic Systems',
+    },
+    categories: ['CDN Affiliated'],
+  },
+  {
+    id: 100003,
+    name: 'Product 3',
+    shortDescription:
+      'Akamai provides cloud computing, security, and content delivery services...',
+    productTags: ['Cloud Infrastructure', 'CDN', 'Security'],
+    infoBanner: 'Special Offer: Get started with $100 credit',
+    partner: {
+      name: 'Akamai',
+      logoLightMode: 'akamai-logo-color.svg',
+      logoDarkMode: 'akamai-logo.svg',
+      url: 'https://www.akamai.com',
+      email: '',
+    },
+    type: {
+      name: 'Data Service',
+    },
+    categories: ['CDN Affiliated', 'Networking'],
+  },
+  {
+    id: 100004,
+    name: 'Product 4',
+    shortDescription:
+      'Akamai provides cloud computing, security, and content delivery services...',
+    productTags: ['Cloud Infrastructure', 'CDN', 'Security'],
+    infoBanner: 'Special Offer: Get started with $100 credit',
+    partner: {
+      name: 'Akamai',
+      logoLightMode: 'akamai-logo-color.svg',
+      logoDarkMode: 'akamai-logo.svg',
+      url: 'https://www.akamai.com',
+      email: '',
+    },
+    type: {
+      name: 'Data Service',
+    },
+    categories: ['CDN Affiliated'],
+  },
+  {
+    id: 100005,
+    name: 'Product 5',
+    shortDescription:
+      'Akamai provides cloud computing, security, and content delivery services...',
+    productTags: ['Cloud Infrastructure', 'CDN', 'Security'],
+    infoBanner: 'Special Offer: Get started with $100 credit',
+    partner: {
+      name: 'Akamai',
+      logoLightMode: 'akamai-logo-color.svg',
+      logoDarkMode: 'akamai-logo.svg',
+      url: 'https://www.akamai.com',
+      email: '',
+    },
+    type: {
+      name: 'Data Service',
+    },
+    categories: ['CDN Affiliated'],
+  },
+  {
+    id: 100006,
+    name: 'Product 6',
+    shortDescription:
+      'Akamai provides cloud computing, security, and content delivery services...',
+    productTags: ['Cloud Infrastructure', 'CDN', 'Security'],
+    infoBanner: 'Special Offer: Get started with $100 credit',
+    partner: {
+      name: 'Akamai',
+      logoLightMode: 'akamai-logo-color.svg',
+      logoDarkMode: 'akamai-logo.svg',
+      url: 'https://www.akamai.com',
+      email: '',
+    },
+    type: {
+      name: 'Data Service',
+    },
+    categories: ['CDN Affiliated'],
+  },
+  {
+    id: 100007,
+    name: 'Product 7',
+    shortDescription:
+      'Akamai provides cloud computing, security, and content delivery services...',
+    productTags: ['Cloud Infrastructure', 'CDN', 'Security'],
+    infoBanner: 'Special Offer: Get started with $100 credit',
+    partner: {
+      name: 'Akamai',
+      logoLightMode: 'akamai-logo-color.svg',
+      logoDarkMode: 'akamai-logo.svg',
+      url: 'https://www.akamai.com',
+      email: '',
+    },
+    type: {
+      name: 'Data Service',
+    },
+    categories: ['CDN Affiliated'],
+  },
+];
