de2de2032faa16e679c55057430b55e51223a953
aaleksee-akamai <aaleksee@akamai.com>
2026-02-04 09:53:38 +0100

    feat: [UIE-10145, UIE-10146] - IAM Delegation: fixes for delegate users (#13357)
    * feat: [UIE-10145] - IAM Delegation: update table for delegate users
    
    * feat: [UIE-10146] - IAM Delegation: hide tab for delegate users
    
    * Added changeset: IAM Delegation: update users table and hide a tab for delegate profile

--- packages/manager/src/features/IAM/Users/UsersTable/Users.tsx
+++ packages/manager/src/features/IAM/Users/UsersTable/Users.tsx
@@ -1,4 +1,4 @@
-import { useAccountUsers, useProfile } from '@linode/queries';
+import { useAccountUsers } from '@linode/queries';
 import { getAPIFilterFromQuery } from '@linode/search';
 import { Button, Paper, Select } from '@linode/ui';
 import { Grid, useMediaQuery } from '@mui/material';
@@ -13,6 +13,7 @@ import { TableBody } from 'src/components/TableBody';
 import { useOrderV2 } from 'src/hooks/useOrderV2';
 import { usePaginationV2 } from 'src/hooks/usePaginationV2';
 
+import { useDelegationRole } from '../../hooks/useDelegationRole';
 import { useIsIAMDelegationEnabled } from '../../hooks/useIsIAMEnabled';
 import { usePermissions } from '../../hooks/usePermissions';
 import { UserDeleteConfirmation } from '../../Shared/UserDeleteConfirmation';
@@ -31,7 +32,8 @@ const ALL_USERS_OPTION: SelectOption = {
 export const UsersLanding = () => {
   const navigate = useNavigate();
   const { isIAMDelegationEnabled } = useIsIAMDelegationEnabled();
-  const { data: profile } = useProfile();
+
+  const { isChildAccount, isDelegateAccount } = useDelegationRole();
 
   const { query, users: usersParam } = useSearch({
     from: '/iam',
@@ -67,10 +69,10 @@ export const UsersLanding = () => {
     searchableFieldsWithoutOperator: ['username', 'email'],
   });
 
-  // Determine if the current user is a child account with isIAMDelegationEnabled enabled
+  // Determine if the current user is a child or delegate profile with isIAMDelegationEnabled enabled
   // If so, we need to show both 'child' and 'delegate_user' users in the table
-  const isChildWithDelegationEnabled =
-    isIAMDelegationEnabled && Boolean(profile?.user_type === 'child');
+  const isChildOrDelegateWithDelegationEnabled =
+    isIAMDelegationEnabled && (isChildAccount || isDelegateAccount);
 
   const filterableOptions = React.useMemo(
     () => [
@@ -106,7 +108,9 @@ export const UsersLanding = () => {
     ['+order']: order.order,
     ['+order_by']: order.orderBy,
     ...filter,
-    ...(isChildWithDelegationEnabled && userType && userType.value !== 'all'
+    ...(isChildOrDelegateWithDelegationEnabled &&
+    userType &&
+    userType.value !== 'all'
       ? {
           user_type: userType.value === 'users' ? 'child' : 'delegate',
         }
@@ -189,7 +193,7 @@ export const UsersLanding = () => {
               placeholder="Filter"
               value={query ?? ''}
             />
-            {isChildWithDelegationEnabled && (
+            {isChildOrDelegateWithDelegationEnabled && (
               <Select
                 hideLabel
                 label="Select user type"
