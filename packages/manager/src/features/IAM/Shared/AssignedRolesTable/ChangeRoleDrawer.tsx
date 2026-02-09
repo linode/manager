dd4f2b672501ad77a2059da12b982ba9e8cc6b41
aaleksee-akamai <aaleksee@akamai.com>
2026-02-09 09:26:29 +0100

    feat: [UIE-10182, UIE-10168] - IAM Delegations: notifications and error state (#13374)
    * feat: [UIE-10182] - IAM: add notifications for drawers
    
    * feat: [UIE-10168] - IAM: error handling
    
    * Added changeset: IAM Delegations: notifications and error state for tables

--- packages/manager/src/features/IAM/Shared/AssignedRolesTable/ChangeRoleDrawer.tsx
+++ packages/manager/src/features/IAM/Shared/AssignedRolesTable/ChangeRoleDrawer.tsx
@@ -14,6 +14,7 @@ import {
 } from '@linode/ui';
 import { useTheme } from '@mui/material/styles';
 import { useParams } from '@tanstack/react-router';
+import { enqueueSnackbar } from 'notistack';
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
 
@@ -146,6 +147,8 @@ export const ChangeRoleDrawer = ({ mode, onClose, open, role }: Props) => {
       });
 
       await mutationFn(updatedUserRoles);
+
+      enqueueSnackbar(`Role changed.`, { variant: 'success' });
 
       handleClose();
     } catch (errors) {
