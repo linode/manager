30229454dc4314ba60a40dad72580945b25ecc07
Alban Bailly <abailly@akamai.com>
2026-02-25 09:26:50 +0100

    Pull in #13427

--- packages/manager/src/features/Account/Quotas/utils.ts
+++ packages/manager/src/features/Account/Quotas/utils.ts
@@ -272,6 +272,10 @@ export const convertResourceMetric = ({
  * Note: the value should be the raw values in bytes, not an existing conversion
  */
 export const pluralizeMetric = (value: number, unit: string) => {
+  if (unit === 'byte_per_second') {
+    return unit;
+  }
+
   if (unit !== 'byte') {
     return value > 1 ? `${unit}s` : unit;
   }
