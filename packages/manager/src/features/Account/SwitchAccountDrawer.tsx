5f09760a1dd9fcbe9ee8e0242145972c37905678
Joe D'Amore <jdamore@akamai.com>
2026-03-05 15:20:11 -0500

    Fix bug preventing child accounts from being fetched when signed in as a proxy user

--- packages/manager/src/features/Account/SwitchAccountDrawer.tsx
+++ packages/manager/src/features/Account/SwitchAccountDrawer.tsx
@@ -103,7 +103,7 @@ export const SwitchAccountDrawer = (props: Props) => {
           }
         : undefined,
     },
-    isIAMDelegationEnabled === false && isParentUserType
+    isIAMDelegationEnabled === false && (isParentUserType || isProxyUserType)
   );
 
   const {
