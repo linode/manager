bbd69956572a9689200d1885f3e758b9a04d9f60
tvijay-akamai <51293194+tvijay-akamai@users.noreply.github.com>
2026-03-17 17:16:06 +0530

    fix: [UIE-10338] marketplace fixes (#13498)
    * fix: [UIE-10338] marketplace fixes
    
    * fix: [UIE-10338] added changeset
    
    * fix: [UIE-10338] fixed unit tests

--- packages/manager/src/features/Marketplace/ProductDetails/ContactSalesDrawer.tsx
+++ packages/manager/src/features/Marketplace/ProductDetails/ContactSalesDrawer.tsx
@@ -11,6 +11,7 @@ import {
   InputAdornment,
   LinkButton,
   Notice,
+  PlusSignIcon,
   Stack,
   TextField,
   Typography,
@@ -124,6 +125,17 @@ export const ContactSalesDrawer = (props: ContactSalesDrawerProps) => {
   });
 
   const tcConsent = watch('tc_consent_given');
+  const countryCode = watch('country_code');
+  const phone = watch('phone');
+
+  const isSubmitDisabled =
+    isSubmitting ||
+    !tcConsent ||
+    !countryCode ||
+    !phone ||
+    !!errors.country_code ||
+    !!errors.phone ||
+    !!errors.phone_country_code;
 
   const dialingCodeFilterOptions = createFilterOptions({
     ignoreCase: true,
@@ -138,6 +150,7 @@ export const ContactSalesDrawer = (props: ContactSalesDrawerProps) => {
   const handleFormReset = () => {
     reset();
     setSelectedCountry(null);
+    setSelectedPhoneCountry(defaultCountry);
   };
 
   const onSubmit = handleSubmit(async (values) => {
@@ -212,7 +225,16 @@ export const ContactSalesDrawer = (props: ContactSalesDrawerProps) => {
               return (
                 // Using MultipleIPInput component for additional emails since it allows for easy addition and removal of multiple entries, and it can display individual error messages for each email address.
                 <MultipleIPInput
-                  buttonText="Add a second, additional email address"
+                  buttonText={
+                    <>
+                      <PlusSignIcon
+                        height={12}
+                        style={{ marginRight: 4 }}
+                        width={12}
+                      />
+                      Click to add a second, additional email address
+                    </>
+                  }
                   className={
                     field.value?.length === MAX_ADDITIONAL_EMAILS
                       ? classes.hideAddEmailButton
@@ -265,6 +287,7 @@ export const ContactSalesDrawer = (props: ContactSalesDrawerProps) => {
                   }
                   keepSearchEnabledOnMobile
                   label="Region"
+                  noOptionsText="No regions match your search"
                   onBlur={field.onBlur}
                   onChange={(_event, country) => {
                     setSelectedCountry(country);
@@ -337,6 +360,7 @@ export const ContactSalesDrawer = (props: ContactSalesDrawerProps) => {
                       option.label === value.label
                     }
                     label="Phone Number"
+                    noOptionsText="No country codes match your search"
                     onBlur={field.onBlur}
                     onChange={(_, country) => {
                       setSelectedPhoneCountry(country);
@@ -625,11 +649,11 @@ export const ContactSalesDrawer = (props: ContactSalesDrawerProps) => {
           primaryButtonProps={{
             'data-pendo-id': 'Cloud Marketplace Contact Sales-Submit',
             label: 'Submit',
-            disabled: isSubmitting || !tcConsent,
+            disabled: isSubmitDisabled,
             type: 'submit',
             tooltipText:
-              'Please agree to share your information with the partner to proceed.',
-            alwaysShowTooltip: !tcConsent,
+              'Please complete all required fields and agree to share your information with the partner to proceed',
+            alwaysShowTooltip: isSubmitDisabled,
           }}
           secondaryButtonProps={{
             'data-pendo-id': 'Cloud Marketplace Contact Sales-Cancel',
