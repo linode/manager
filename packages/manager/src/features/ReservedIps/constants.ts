7c604e217c737a343a0063af36419e55344e3ffd
Ganesh Revanakar <grevanak@akamai.com>
2026-04-02 14:55:57 +0530

    Addressed review comments

--- packages/manager/src/features/ReservedIps/constants.ts
+++ packages/manager/src/features/ReservedIps/constants.ts
@@ -2,4 +2,4 @@ export const RESERVE_IP_DESCRIPTION =
   'Reserve a public IPv4 address from the selected region. It will be added to your reserved IPs and ready to assign to resources, such as Linodes or NodeBalancers. Reserved IPs and resources must be in the same region.';
 
 export const RESERVED_IPS_DOCS_LINK =
-  'https://techdocs.akamai.com/cloud-computing/update/docs/reserved-ips';
+  'https://techdocs.akamai.com/cloud-computing/docs/reserved-ips';
