b023c4d539d4b94b3f3fef7d5bfdf44dabd47a22
jdamore-linode <97627410+jdamore-linode@users.noreply.github.com>
2026-04-03 10:36:31 -0400

    change: [UIE-10701] - Upgrade Cypress to v15.13.0, replace deprecated `cy.env` calls (#13555)
    * Upgrade Cypress and related dependencies; replace calls to deprecated `cy.env` with `Cypress.expose`
    
    * Upgrade Cypress to 15.13.0 in Dockerfile
    
    * Added changeset: Upgrade Cypress to v15.13.0

--- packages/manager/cypress/support/constants/api.ts
+++ packages/manager/cypress/support/constants/api.ts
@@ -1,7 +1,7 @@
 /**
  * API OAuth token for authenticating API requests and Cloud Manager interactions.
  */
-export const oauthToken = Cypress.env('MANAGER_OAUTH');
+export const oauthToken = Cypress.expose('MANAGER_OAUTH');
 
 /**
  * API request pagination page size.
