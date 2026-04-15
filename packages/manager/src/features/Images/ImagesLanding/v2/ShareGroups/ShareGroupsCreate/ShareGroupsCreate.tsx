39931e6f798fd9f75f2b36d0a2571217f4b07604
fabrice-akamai <fmikobi@akamai.com>
2026-04-15 14:49:42 -0400

    fix bugs from merging develop

--- packages/manager/src/features/Images/ImagesLanding/v2/ShareGroups/ShareGroupsCreate/ShareGroupsCreate.tsx
+++ packages/manager/src/features/Images/ImagesLanding/v2/ShareGroups/ShareGroupsCreate/ShareGroupsCreate.tsx
@@ -17,12 +17,6 @@ import { IMAGE_SELECT_TABLE_SHARE_GROUP_CREATE_PENDO_IDS } from 'src/components/
 import { ImageSelectTable } from 'src/components/ImageSelect/ImageSelectTable';
 
 import { CREATE_SHARE_GROUP_PENDO_IDS } from '../../constants';
-import { Controller, useController, useForm } from 'react-hook-form';
-
-import { IMAGE_SELECT_TABLE_SHARE_GROUP_CREATE_PENDO_IDS } from 'src/components/ImageSelect/constants';
-import { ImageSelectTable } from 'src/components/ImageSelect/ImageSelectTable';
-
-import { CREATE_SHARE_GROUP_PENDO_IDS } from '../../constants';
 
 import type {
   CreateSharegroupPayload,
@@ -159,7 +153,6 @@ export const ShareGroupsCreate = () => {
                 required
                 {...field}
                 data-pendo-id={CREATE_SHARE_GROUP_PENDO_IDS.label}
-                data-pendo-id={CREATE_SHARE_GROUP_PENDO_IDS.label}
                 errorText={fieldState.error?.message}
                 onChange={(e) =>
                   field.onChange(
@@ -276,12 +269,6 @@ export const ShareGroupsCreate = () => {
         </Stack>
       </Paper>
       <Box display="flex" flexWrap="wrap" justifyContent="flex-end" mt={2}>
-        <Button
-          buttonType="primary"
-          data-pendo-id={CREATE_SHARE_GROUP_PENDO_IDS.createButton}
-          loading={isSubmitting}
-          type="submit"
-        >
         <Button
           buttonType="primary"
           data-pendo-id={CREATE_SHARE_GROUP_PENDO_IDS.createButton}
