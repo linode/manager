912a71925552da7948c4b817e3fba9c6d43e9569
Harsh Rao <hrao@akamai.com>
2026-04-21 15:22:21 +0000

    Pull request #72: upcoming: [UIE-10690, UIE-10691, UIE-11126] – Create new Summary section in the NodeBalancer Details tab
    Merge in FEE/cloud-manager from feature/UIE-10691-update-summary-section-in-nb-landing-page to develop
    
    Squashed commit of the following:
    
    commit f304c62d45e004470c4e787538be4dc077c105a2
    Author: hrao <hrao@akamai.com>
    Date:   Tue Apr 21 18:07:46 2026 +0530
    
        changeset file name edit
    
    commit 52edc4a5835907aa169f537a8d64a64ff9692560
    Merge: 8ee6ebdfa4 28e0fc1392
    Author: hrao <hrao@akamai.com>
    Date:   Tue Apr 21 16:20:30 2026 +0530
    
        Merge branch 'develop' into feature/UIE-10691-update-summary-section-in-nb-landing-page
    
    commit 8ee6ebdfa4ae375f4c514d4affaa896ac015cc87
    Author: hrao <hrao@akamai.com>
    Date:   Tue Apr 21 14:06:57 2026 +0530
    
        update failing test
    
    commit af1e7c9d68f106b5d2d8b277aebe765d0098408c
    Author: hrao <hrao@akamai.com>
    Date:   Tue Apr 21 12:48:57 2026 +0530
    
        update changeset
    
    commit 219dc1117c096a4cce3ac475f0e7132bc320dfca
    Author: hrao <hrao@akamai.com>
    Date:   Tue Apr 21 12:44:22 2026 +0530
    
        test fix
    
    commit 537b2e9657088f86a675a8fa229108c2c88101a8
    Author: hrao <hrao@akamai.com>
    Date:   Tue Apr 21 12:05:06 2026 +0530
    
        Added changeset: Create new Summary Section in the  Nodebalancer Details Summary tab
    
    commit 1bbba1ca56628ce4a0e26456f52476d8cf301530
    Author: hrao <hrao@akamai.com>
    Date:   Tue Apr 21 10:58:51 2026 +0530
    
        PR feedback
    
    commit 0bf8fcb1249f6ab3f6179e90616a2dddcae5cf87
    Author: hrao <hrao@akamai.com>
    Date:   Fri Apr 17 12:31:16 2026 +0530
    
        upcoming: [UIE-10690, UIE-10691] – Create new Summary section in the NodeBalancer Details tab

--- packages/manager/src/features/NodeBalancers/utils.test.ts
+++ packages/manager/src/features/NodeBalancers/utils.test.ts
@@ -3,6 +3,7 @@ import { renderHook, waitFor } from '@testing-library/react';
 import { wrapWithTheme } from 'src/utilities/testHelpers';
 
 import {
+  getBackendStatusIndicator,
   useIsNodebalancerIpv6Enabled,
   useIsNodebalancerVPCEnabled,
 } from './utils';
@@ -56,5 +57,32 @@ describe('useIsNodebalancerIpv6Enabled', () => {
     await waitFor(() => {
       expect(result.current.isNodebalancerIpv6Enabled).toBe(false);
     });
+  });
+});
+
+describe('getBackendStatusIndicator', () => {
+  it.each([
+    [
+      'returns inactive when both values are undefined',
+      undefined,
+      undefined,
+      'inactive',
+    ],
+    ['returns inactive when both values are zero', 0, 0, 'inactive'],
+    [
+      'returns active when there are no down backends and some up backends',
+      3,
+      0,
+      'active',
+    ],
+    [
+      'returns error when there are no up backends and some down backends',
+      0,
+      2,
+      'error',
+    ],
+    ['returns other when both up and down backends are present', 2, 1, 'other'],
+  ])('%s', (_name, up, down, expected) => {
+    expect(getBackendStatusIndicator(up, down)).toBe(expected);
   });
 });
